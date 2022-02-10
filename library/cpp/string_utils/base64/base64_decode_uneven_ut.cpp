b9b5c8262e0319799862d51f98a92b291494f5f1
popov-ng <popov-ng@yandex-team.ru>
2022-02-10 16:49:57 +0300

    Restoring authorship annotation for <popov-ng@yandex-team.ru>. Commit 1 of 2.

--- library/cpp/string_utils/base64/base64_decode_uneven_ut.cpp
+++ library/cpp/string_utils/base64/base64_decode_uneven_ut.cpp
@@ -42,5 +42,5 @@ Y_UNIT_TEST_SUITE(TBase64DecodeUneven) {
         const TString dp = "ADP GmbH\nAnalyse Design & Programmierung\nGesellschaft mit beschränkter Haftung";
         UNIT_ASSERT_VALUES_EQUAL(dp, Base64DecodeUneven(Base64Encode(dp)));
         UNIT_ASSERT_VALUES_EQUAL(dp, Base64DecodeUneven(Base64EncodeUrl(dp)));
-    }
+    } 
 }

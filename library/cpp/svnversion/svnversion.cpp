5183541d46a8a69d1c5cb30f16332b274981add4
maximm <maximm@yandex-team.ru>
2022-02-10 16:49:58 +0300

    Restoring authorship annotation for <maximm@yandex-team.ru>. Commit 1 of 2.

--- library/cpp/svnversion/svnversion.cpp
+++ library/cpp/svnversion/svnversion.cpp
@@ -5,8 +5,8 @@
 #define FROM_IMPL
 #include "svnversion.h"
 
-#include <util/generic/strbuf.h>
-
+#include <util/generic/strbuf.h> 
+ 
 extern "C" void PrintProgramSvnVersion() {
     puts(GetProgramSvnVersion());
 }
@@ -17,17 +17,17 @@ extern "C" void PrintSvnVersionAndExit0() {
 }
 
 extern "C" void PrintSvnVersionAndExitEx(int argc, char* argv[], const char* opts) {
-    if (2 == argc) {
+    if (2 == argc) { 
         for (TStringBuf all = opts, versionOpt; all.NextTok(';', versionOpt);) {
-            if (versionOpt == argv[1]) {
-                PrintSvnVersionAndExit0();
-            }
-        }
+            if (versionOpt == argv[1]) { 
+                PrintSvnVersionAndExit0(); 
+            } 
+        } 
     }
 }
 
 extern "C" void PrintSvnVersionAndExit(int argc, char* argv[]) {
-    PrintSvnVersionAndExitEx(argc, argv, "--version");
-}
-
+    PrintSvnVersionAndExitEx(argc, argv, "--version"); 
+} 
+ 
 #undef FROM_IMPL

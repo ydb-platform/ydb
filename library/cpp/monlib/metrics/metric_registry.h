4ea9ba77900523cdb8765a260b28d0a7ef906806
arcadia-devtools <arcadia-devtools@yandex-team.ru>
2022-02-10 11:11:27 +0300

    intermediate changes ref:2c3e46cc24129798623a4e38cf6e79cae4c95dac

--- library/cpp/monlib/metrics/metric_registry.h
+++ library/cpp/monlib/metrics/metric_registry.h
@@ -79,7 +79,14 @@ namespace NMonitoring {
                 TLabels labels,
                 IHistogramCollectorPtr collector);
 
+        /**
+         * Set all registered metrics to zero
+         */
         void Reset();
+        /**
+         * Remove all registered metrics from registry
+         */
+        void Clear();
 
         void Accept(TInstant time, IMetricConsumer* consumer) const override;
         void Append(TInstant time, IMetricConsumer* consumer) const override;

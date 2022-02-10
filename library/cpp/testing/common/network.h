4db1a5fe7b31f98546be9a912f4710bfb05d7a5f
staroverovad <staroverovad@yandex-team.ru>
2022-02-10 16:50:04 +0300

    Restoring authorship annotation for <staroverovad@yandex-team.ru>. Commit 1 of 2.

--- library/cpp/testing/common/network.h
+++ library/cpp/testing/common/network.h
@@ -27,8 +27,8 @@ namespace NTesting {
         operator ui16() const&& = delete;
     };
 
-    IOutputStream& operator<<(IOutputStream& out, const TPortHolder& port);
-
+    IOutputStream& operator<<(IOutputStream& out, const TPortHolder& port); 
+ 
     //@brief Get first free port.
     [[nodiscard]] TPortHolder GetFreePort();
 

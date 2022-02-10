d1534e9f0d4be18cd2fff6367630bfd4c167168d
yuramalinov <yuramalinov@yandex-team.ru>
2022-02-10 16:51:40 +0300

    Restoring authorship annotation for <yuramalinov@yandex-team.ru>. Commit 2 of 2.

--- build/plugins/java.py
+++ build/plugins/java.py
@@ -143,8 +143,8 @@ def onjava_module(unit, *args):
         data['WITH_KOTLIN'] = extract_macro_calls(unit, 'WITH_KOTLIN_VALUE', args_delim)
         if unit.get('KOTLIN_JVM_TARGET'):
             data['KOTLIN_JVM_TARGET'] = extract_macro_calls(unit, 'KOTLIN_JVM_TARGET', args_delim)
-        if unit.get('KOTLINC_FLAGS_VALUE'): 
-            data['KOTLINC_FLAGS'] = extract_macro_calls(unit, 'KOTLINC_FLAGS_VALUE', args_delim) 
+        if unit.get('KOTLINC_FLAGS_VALUE'):
+            data['KOTLINC_FLAGS'] = extract_macro_calls(unit, 'KOTLINC_FLAGS_VALUE', args_delim)
         if unit.get('KOTLINC_OPTS_VALUE'):
             data['KOTLINC_OPTS'] = extract_macro_calls(unit, 'KOTLINC_OPTS_VALUE', args_delim)
 

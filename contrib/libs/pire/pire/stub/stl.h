9ef1bf9e69d369800773d810755391694086d4a4
ulgen4a <ulgen4a@yandex-team.ru>
2022-02-10 16:48:08 +0300

    Restoring authorship annotation for <ulgen4a@yandex-team.ru>. Commit 1 of 2.

--- contrib/libs/pire/pire/stub/stl.h
+++ contrib/libs/pire/pire/stub/stl.h
@@ -25,41 +25,41 @@
 #include <util/string/reverse.h>
 #include <util/string/vector.h>
 
-namespace Pire {
+namespace Pire { 
     using ystring = TString;
     template<size_t N> using ybitset = std::bitset<N>;
     template<typename T1, typename T2> using ypair = std::pair<T1, T2>;
     template<typename T> using yauto_ptr = std::auto_ptr<T>;
     template<typename Arg1, typename Arg2, typename Result> using ybinary_function = std::binary_function<Arg1, Arg2, Result>;
 
-    template<typename T1, typename T2>
-    inline ypair<T1, T2> ymake_pair(T1 v1, T2 v2) {
+    template<typename T1, typename T2> 
+    inline ypair<T1, T2> ymake_pair(T1 v1, T2 v2) { 
         return  std::make_pair(v1, v2);
-    }
+    } 
 
-    template<typename T>
-    inline T ymax(T v1, T v2) {
+    template<typename T> 
+    inline T ymax(T v1, T v2) { 
         return std::max(v1, v2);
-    }
+    } 
 
-    template<typename T>
-    inline T ymin(T v1, T v2) {
+    template<typename T> 
+    inline T ymin(T v1, T v2) { 
         return std::min(v1, v2);
-    }
+    } 
 
-    template<class Iter, class T>
+    template<class Iter, class T> 
     void Fill(Iter begin, Iter end, T t) { std::fill(begin, end, t); }
 
-    class Error: public yexception {
-    public:
-        Error(const char* msg)    { *this << msg; }
-        Error(const ystring& msg) { *this << msg; }
-    };
+    class Error: public yexception { 
+    public: 
+        Error(const char* msg)    { *this << msg; } 
+        Error(const ystring& msg) { *this << msg; } 
+    }; 
 
     typedef IOutputStream yostream;
     typedef IInputStream yistream;
-
-    template<class Iter>
+ 
+    template<class Iter> 
     ystring Join(Iter begin, Iter end, const ystring& separator) { return JoinStrings(begin, end, separator); }
 }
 

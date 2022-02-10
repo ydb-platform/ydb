e9e4986eda97ffe44139887e03f9ae77852ff8d9
r-vetrov <r-vetrov@yandex-team.ru>
2022-02-10 16:48:37 +0300

    Restoring authorship annotation for <r-vetrov@yandex-team.ru>. Commit 1 of 2.

--- build/scripts/gen_tasklet_reg.py
+++ build/scripts/gen_tasklet_reg.py
@@ -1,33 +1,33 @@
 import argparse
 
-TEMPLATE = '''\
-{includes}\
-#include <tasklet/runtime/lib/{language}_wrapper.h>
+TEMPLATE = '''\ 
+{includes}\ 
+#include <tasklet/runtime/lib/{language}_wrapper.h> 
 #include <tasklet/runtime/lib/registry.h>
 
 static const NTasklet::TRegHelper REG(
     "{name}",
-    new NTasklet::{wrapper}
+    new NTasklet::{wrapper} 
 );
 '''
 
-WRAPPER = {
-    'cpp': 'TCppWrapper<{impl}>()',
+WRAPPER = { 
+    'cpp': 'TCppWrapper<{impl}>()', 
     'js': 'TJsWrapper("{impl}")',
-    'go': 'TGoWrapper("{impl}")',
-    'py': 'TPythonWrapper("{impl}")',
+    'go': 'TGoWrapper("{impl}")', 
+    'py': 'TPythonWrapper("{impl}")', 
     'java': 'TJavaWrapper("{impl}", "{py_wrapper}")',
-}
+} 
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('name')
-    parser.add_argument('output')
-    parser.add_argument('-l', '--lang', choices=WRAPPER, required=True)
-    parser.add_argument('-i', '--impl', required=True)
+    parser.add_argument('name') 
+    parser.add_argument('output') 
+    parser.add_argument('-l', '--lang', choices=WRAPPER, required=True) 
+    parser.add_argument('-i', '--impl', required=True) 
     parser.add_argument('-w', '--wrapper', required=False)
-    parser.add_argument('includes', nargs='*')
+    parser.add_argument('includes', nargs='*') 
 
     return parser.parse_args()
 
@@ -35,17 +35,17 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
 
-    includes = ''.join(
-        '#include <{}>\n'.format(include)
+    includes = ''.join( 
+        '#include <{}>\n'.format(include) 
         for include in args.includes
     )
 
     code = TEMPLATE.format(
         includes=includes,
-        language=args.lang,
+        language=args.lang, 
         name=args.name,
         wrapper=WRAPPER[args.lang].format(impl=args.impl, py_wrapper=args.wrapper),
     )
 
-    with open(args.output, 'w') as f:
+    with open(args.output, 'w') as f: 
         f.write(code)

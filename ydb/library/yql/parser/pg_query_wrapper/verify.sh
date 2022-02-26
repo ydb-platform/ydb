ed2cd4470267ae1bbe21e78e8319a02376e609ad
aneporada <aneporada@yandex-team.ru>
2022-02-26 21:31:23 +0300

    [yql] Simplify exit code logic
    Note: mandatory check (NEED_CHECK) was skipped
    ref:bdc7f61a30c88983bddd0396b5246d04088ca7bf

--- ydb/library/yql/parser/pg_query_wrapper/verify.sh
+++ ydb/library/yql/parser/pg_query_wrapper/verify.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -eu
 ya make
-err=0
-objdump libyql-parser-pg_query_wrapper.a -t | grep -E "\.data\.|\.bss\." | \
+cnt=$(objdump libyql-parser-pg_query_wrapper.a -t | grep -E "\.data\.|\.bss\." | \
 grep -v -E "progname|pg_popcount32|pg_popcount64|pg_comp_crc32c|_ZN4NYqlL10GlobalInitE|BlockSig|StartupBlockSig|UnBlockSig" | \
-grep -v -E "on_proc_exit_index|on_shmem_exit_index|before_shmem_exit_index" || err=$?
-if [ $err -eq 1 ]; then
+grep -v -E "on_proc_exit_index|on_shmem_exit_index|before_shmem_exit_index" | wc -l)
+if [ $cnt -eq 0 ]; then
     echo "***PASSED***"
-fi
-if [ $err -ne 1 ]; then
-    echo "***UNEXPECTED SYMBOLS***"
+else
+    echo "***GOT $cnt UNEXPECTED SYMBOLS***"
+    exit 1
 fi

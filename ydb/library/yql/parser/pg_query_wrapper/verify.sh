2e1f9a0db7d1506ab7ec87aeb835d57ed1d9f8fe
aneporada <aneporada@yandex-team.ru>
2022-03-01 00:11:22 +0300

    [yql] Improve error reporting
    Note: mandatory check (NEED_CHECK) was skipped
    ref:0fb21134dd1a194e185b2efded9cb1ed8e3f01ef

--- ydb/library/yql/parser/pg_query_wrapper/verify.sh
+++ ydb/library/yql/parser/pg_query_wrapper/verify.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 set -eu
-ya make
-cnt=$(objdump libyql-parser-pg_query_wrapper.a -t | grep -E "\.data\.|\.bss\." | \
+
+echo "Building library"
+ya make || exit $?
+
+echo "Checking static variables"
+
+data=$(objdump libyql-parser-pg_query_wrapper.a -t | grep -E "\.data\.|\.bss\." | \
 grep -v -E "progname|pg_popcount32|pg_popcount64|pg_comp_crc32c|TMkqlPgAdapter|_ZN4NYqlL10GlobalInitE|BlockSig|StartupBlockSig|UnBlockSig" | \
-grep -v -E "on_proc_exit_index|on_shmem_exit_index|before_shmem_exit_index" | wc -l)
-if [ $cnt -eq 0 ]; then
-    echo "***PASSED***"
-else
-    echo "***GOT $cnt UNEXPECTED SYMBOLS***"
-    exit 1
+grep -v -E "on_proc_exit_index|on_shmem_exit_index|before_shmem_exit_index")
+
+if [ ${#data} -eq 0 ]; then
+    echo "OK";
+    exit 0;
 fi
+
+cnt=$(echo "$data" | wc -l)
+
+echo "***GOT $cnt UNEXPECTED SYMBOLS***"
+echo "$data"
+exit 1;

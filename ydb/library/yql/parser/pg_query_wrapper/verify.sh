e94eb2a87b175451a364d14a4624a2524f3c37cb
vvvv <vvvv@yandex-team.ru>
2022-02-28 15:39:42 +0300

    YQL-13710 verify.sh: added singleton for memory context
    ref:a8eb7f0330a1a0d23e1f79a9dea4554342b49889

--- ydb/library/yql/parser/pg_query_wrapper/verify.sh
+++ ydb/library/yql/parser/pg_query_wrapper/verify.sh
@@ -2,7 +2,7 @@
 set -eu
 ya make
 cnt=$(objdump libyql-parser-pg_query_wrapper.a -t | grep -E "\.data\.|\.bss\." | \
-grep -v -E "progname|pg_popcount32|pg_popcount64|pg_comp_crc32c|_ZN4NYqlL10GlobalInitE|BlockSig|StartupBlockSig|UnBlockSig" | \
+grep -v -E "progname|pg_popcount32|pg_popcount64|pg_comp_crc32c|TMkqlPgAdapter|_ZN4NYqlL10GlobalInitE|BlockSig|StartupBlockSig|UnBlockSig" | \
 grep -v -E "on_proc_exit_index|on_shmem_exit_index|before_shmem_exit_index" | wc -l)
 if [ $cnt -eq 0 ]; then
     echo "***PASSED***"

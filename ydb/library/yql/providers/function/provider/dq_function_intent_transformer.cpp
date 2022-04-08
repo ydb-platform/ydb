b86aa3727057d9997ca28ef128e7550b98745688
hrustyashko <hrustyashko@yandex-team.ru>
2022-04-08 16:12:21 +0300

    KIKIMR-14667 OSS сборка ydb тянет cloud зависимость
    move cloud_function's specific provider to /yql/providers
    
    rename providers/external_function to providers/funtion
    
    ref:9e1f75312ba88c116e4d007207d2cec6f62d532a

---
+++ ydb/library/yql/providers/function/provider/dq_function_intent_transformer.cpp
@@ -0,0 +1,44 @@
+#include "dq_function_provider.h"
+
+#include <ydb/library/yql/providers/common/transform/yql_visit.h>
+#include <ydb/library/yql/providers/function/expr_nodes/dq_function_expr_nodes.h>
+
+namespace NYql::NDqFunction {
+namespace {
+
+using namespace NNodes;
+
+class TDqFunctionIntentTransformer : public TVisitorTransformerBase {
+public:
+    TDqFunctionIntentTransformer(TDqFunctionState::TPtr state)
+        : TVisitorTransformerBase(false)
+        , State(state)
+    {
+        AddHandler({TFunctionDataSource::CallableName()}, Hndl(&TDqFunctionIntentTransformer::HandleDataSource));
+    }
+
+    TStatus HandleDataSource(TExprBase input, TExprContext& ctx) {
+        Y_UNUSED(ctx);
+
+        if (!TFunctionDataSource::Match(input.Raw()))
+            return TStatus::Ok;
+
+        auto source = input.Cast<TFunctionDataSource>();
+        TDqFunctionType functionType{source.Type().Value()};
+        TString functionName{source.FunctionName().Value()};
+        TString connection{source.Connection().Value()};
+        State->FunctionsResolver->AddFunction(functionType, functionName, connection);
+
+        return TStatus::Ok;
+    }
+
+private:
+    TDqFunctionState::TPtr State;
+};
+}
+
+THolder<TVisitorTransformerBase> CreateDqFunctionIntentTransformer(TDqFunctionState::TPtr state) {
+    return THolder(new TDqFunctionIntentTransformer(state));
+}
+
+}

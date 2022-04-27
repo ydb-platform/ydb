9be24968e281eca692df77ac5a6a7e5377e91f5e
aneporada <aneporada@yandex-team.ru>
2022-04-27 15:54:27 +0300

    [yql] Apply patches in copy_src.sh
    Note: mandatory check (NEED_CHECK) was skipped
    ref:78c8118fa7d3f8e49d639b44d7e9fb7936206223

--- ydb/library/yql/parser/pg_wrapper/copy_src.sh
+++ ydb/library/yql/parser/pg_wrapper/copy_src.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -eux
 rm -f ../../../../../contrib/libs/postgresql/libpostgres.a
-ya make ../../../../../contrib/libs/postgresql
+ya make ../../../../../contrib/libs/postgresql --checkout
 python3 copy_src.py
+for i in $(ls patches/); do (cd postgresql && patch -p1 < ../patches/$i); done

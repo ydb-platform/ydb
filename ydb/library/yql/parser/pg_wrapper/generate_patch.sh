d24943f728c369b6a1feb806c103b25cb5f945bd
aneporada <aneporada@yandex-team.ru>
2022-04-27 20:01:48 +0300

    [yql] Remove ya svn from script
    Note: mandatory check (NEED_CHECK) was skipped
    ref:d123647a7bf4519b7ade9a66007d53836dfa6e77

--- ydb/library/yql/parser/pg_wrapper/generate_patch.sh
+++ ydb/library/yql/parser/pg_wrapper/generate_patch.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -u
 rm -rf postgresql.patched
-ya svn revert -R postgresql
 cp -a postgresql postgresql.patched
 ./copy_src.sh > /dev/null 2>&1
 diff -ruN postgresql postgresql.patched
 rm -rf postgresql.patched
-ya svn revert -R postgresql

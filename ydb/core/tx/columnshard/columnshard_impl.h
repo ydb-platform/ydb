0fc17c063fb04779ceefcfc272368703a0b632e3
Artem Zuikov <chertus@gmail.com>
2022-05-20 13:24:16 +0300

    KIKIMR-14904: fix counter ms -> us
    ref:666e957b27577e1c22cccb31fdb3529557ed43d6

--- ydb/core/tx/columnshard/columnshard_impl.h
+++ ydb/core/tx/columnshard/columnshard_impl.h
@@ -158,7 +158,7 @@ class TColumnShard
     }
 
     void IncCounter(NColumnShard::EPercentileCounters counter, const TDuration& latency) const {
-        TabletCounters->Percentile()[counter].IncrementFor(latency.MilliSeconds());
+        TabletCounters->Percentile()[counter].IncrementFor(latency.MicroSeconds());
     }
 
 protected:

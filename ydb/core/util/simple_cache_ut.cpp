3d80db4ae60a700529a4e45d782862bb7d58e004
Alexey Efimov <xeno@prnwatch.com>
2022-06-01 20:41:24 +0300

    add not so simple cache with lifetime checking KIKIMR-15015
    ref:607a2c25f091e77893b82640122a16daa6ae3b1a

--- ydb/core/util/simple_cache_ut.cpp
+++ ydb/core/util/simple_cache_ut.cpp
@@ -22,6 +22,37 @@ Y_UNIT_TEST_SUITE(TSimpleCacheTest) {
         ptr = cache.FindPtr("2");
         UNIT_ASSERT(ptr == nullptr);
     }
+
+    struct TValue {
+        TString Str;
+        bool SafeToRelease = true;
+
+        bool IsSafeToRelease() {
+            return SafeToRelease;
+        }
+    };
+
+    Y_UNIT_TEST(TestNotSoSimpleCache) {
+        TNotSoSimpleCache<TString, TValue> cache;
+
+        cache.MaxSize = 3;
+        TValue* ptr = cache.FindPtr("1");
+        UNIT_ASSERT(ptr == nullptr);
+        cache.Update("1", {"one"});
+        ptr = cache.FindPtr("1");
+        UNIT_ASSERT(ptr != nullptr);
+        UNIT_ASSERT(ptr->Str == "one");
+        cache.Update("2", {"two", false});
+        cache.Update("3", {"three"});
+        ptr = cache.FindPtr("1");
+        UNIT_ASSERT(ptr != nullptr);
+        UNIT_ASSERT(ptr->Str == "one");
+        cache.Update("4", {"four"}); // we evicting oldest one - "three" (but not "two" - it's not safe to release)
+        ptr = cache.FindPtr("2");
+        UNIT_ASSERT(ptr != nullptr);
+        ptr = cache.FindPtr("3");
+        UNIT_ASSERT(ptr == nullptr);
+    }
 }
 
 } // NKikimr

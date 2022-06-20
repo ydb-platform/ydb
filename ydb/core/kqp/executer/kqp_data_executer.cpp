cfe2e50abfa852c2d69942c4d255a20147e46343
Sergei Puchin <s.puchin@gmail.com>
2022-06-20 20:44:33 +0300

    Add TEvUnlink for followers pipe cache. (KIKIMR-15152)
    ref:6f663bf2d2a5d180473cad71dd7e42a24b43b10a

--- ydb/core/kqp/executer/kqp_data_executer.cpp
+++ ydb/core/kqp/executer/kqp_data_executer.cpp
@@ -1752,6 +1752,11 @@ private:
         Counters->TxProxyMon->TxExecuteTimeHgram->Collect(totalTime.MilliSeconds());
 
         Send(MakePipePeNodeCacheID(false), new TEvPipeCache::TEvUnlink(0));
+
+        if (UseFollowers) {
+            Send(MakePipePeNodeCacheID(true), new TEvPipeCache::TEvUnlink(0));
+        }
+
         TBase::PassAway();
     }
 

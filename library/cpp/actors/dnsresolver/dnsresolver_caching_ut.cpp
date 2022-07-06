8b2fafa9888f6a85ad33912eccfb691f037fb285
Daniil Cherednik <dan.cherednik@gmail.com>
2022-07-06 23:04:30 +0300

    Missed changes for 33e0b0f01d29fe0a000c45feea0d5c855e0e78ae
    x-ydb-stable-ref: b5a1b56ccae72d0629280e8a1247f041f9c72ce7

--- library/cpp/actors/dnsresolver/dnsresolver_caching_ut.cpp
+++ library/cpp/actors/dnsresolver/dnsresolver_caching_ut.cpp
@@ -88,6 +88,18 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
         static TMockReply SingleV4(const TString& addr, TDuration delay = DefaultDelay) {
             return ManyV4({ addr }, delay);
         }
+
+        friend TMockReply operator+(const TMockReply& a, const TMockReply& b) {
+            Y_VERIFY(a.Status == b.Status);
+            TMockReply result;
+            result.Status = a.Status;
+            result.Delay = Max(a.Delay, b.Delay);
+            result.AddrsV6.insert(result.AddrsV6.end(), a.AddrsV6.begin(), a.AddrsV6.end());
+            result.AddrsV6.insert(result.AddrsV6.end(), b.AddrsV6.begin(), b.AddrsV6.end());
+            result.AddrsV4.insert(result.AddrsV4.end(), a.AddrsV4.begin(), a.AddrsV4.end());
+            result.AddrsV4.insert(result.AddrsV4.end(), b.AddrsV4.begin(), b.AddrsV4.end());
+            return result;
+        }
     };
 
     using TMockDnsCallback = std::function<TMockReply (const TString&, int)>;
@@ -166,8 +178,10 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
         TActorId Sleeper;
         TString Section_;
 
+        NMonitoring::TDynamicCounters::TCounterPtr InFlightUnspec;
         NMonitoring::TDynamicCounters::TCounterPtr InFlight6;
         NMonitoring::TDynamicCounters::TCounterPtr InFlight4;
+        NMonitoring::TDynamicCounters::TCounterPtr TotalUnspec;
         NMonitoring::TDynamicCounters::TCounterPtr Total6;
         NMonitoring::TDynamicCounters::TCounterPtr Total4;
         NMonitoring::TDynamicCounters::TCounterPtr Misses;
@@ -175,6 +189,7 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
 
         THashMap<TString, TMockReply> ReplyV6;
         THashMap<TString, TMockReply> ReplyV4;
+        THashMap<TString, TMockReply> ReplyUnspec;
 
         TCachingDnsRuntime() {
             SetScheduledEventFilter([](auto&&, auto&&, auto&&, auto&&) { return false; });
@@ -186,6 +201,9 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
             ReplyV4["yandex.ru"] = TMockReply::SingleV4("77.88.55.77", TDuration::MilliSeconds(250));
             ReplyV6["router.asus.com"] = TMockReply::Error(ARES_ENODATA);
             ReplyV4["router.asus.com"] = TMockReply::SingleV4("192.168.0.1");
+            ReplyUnspec["localhost"] = ReplyV6.at("localhost") + ReplyV4.at("localhost");
+            ReplyUnspec["yandex.ru"] = ReplyV6.at("yandex.ru") + ReplyV4.at("yandex.ru");
+            ReplyUnspec["router.asus.com"] = ReplyV4.at("router.asus.com");
         }
 
         void Start(TMockDnsCallback callback) {
@@ -194,8 +212,10 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
             Resolver = Register(CreateCachingDnsResolver(MockResolver, ResolverOptions));
             Sleeper = AllocateEdgeActor();
 
+            InFlightUnspec = ResolverOptions.MonCounters->GetCounter("DnsResolver/Outgoing/InFlight/Unspec", false);
             InFlight6 = ResolverOptions.MonCounters->GetCounter("DnsResolver/Outgoing/InFlight/V6", false);
             InFlight4 = ResolverOptions.MonCounters->GetCounter("DnsResolver/Outgoing/InFlight/V4", false);
+            TotalUnspec = ResolverOptions.MonCounters->GetCounter("DnsResolver/Outgoing/Total/Unspec", true);
             Total6 = ResolverOptions.MonCounters->GetCounter("DnsResolver/Outgoing/Total/V6", true);
             Total4 = ResolverOptions.MonCounters->GetCounter("DnsResolver/Outgoing/Total/V4", true);
             Misses = ResolverOptions.MonCounters->GetCounter("DnsResolver/Cache/Misses", true);
@@ -205,6 +225,13 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
         void Start() {
             Start([this](const TString& name, int family) {
                 switch (family) {
+                    case AF_UNSPEC: {
+                        auto it = ReplyUnspec.find(name);
+                        if (it != ReplyUnspec.end()) {
+                            return it->second;
+                        }
+                        break;
+                    }
                     case AF_INET6: {
                         auto it = ReplyV6.find(name);
                         if (it != ReplyV6.end()) {
@@ -234,10 +261,10 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
         }
 
         void WaitNoInFlight() {
-            if (*InFlight6 || *InFlight4) {
+            if (*InFlightUnspec || *InFlight6 || *InFlight4) {
                 TDispatchOptions options;
                 options.CustomFinalCondition = [&]() {
-                    return !*InFlight6 && !*InFlight4;
+                    return !*InFlightUnspec && !*InFlight6 && !*InFlight4;
                 };
                 DispatchEvents(options);
                 UNIT_ASSERT_C(!*InFlight6 && !*InFlight4, "Failed to wait for no inflight in " << Section_);
@@ -260,6 +287,10 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
             return GrabEdgeEventRethrow<TEvDns::TEvGetAddrResult>(sender);
         }
 
+        void ExpectInFlightUnspec(i64 count) {
+            UNIT_ASSERT_VALUES_EQUAL_C(InFlightUnspec->Val(), count, Section_);
+        }
+
         void ExpectInFlight6(i64 count) {
             UNIT_ASSERT_VALUES_EQUAL_C(InFlight6->Val(), count, Section_);
         }
@@ -268,12 +299,23 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
             UNIT_ASSERT_VALUES_EQUAL_C(InFlight4->Val(), count, Section_);
         }
 
+        void ExpectTotalUnspec(i64 count) {
+            UNIT_ASSERT_VALUES_EQUAL_C(TotalUnspec->Val(), count, Section_);
+        }
+
         void ExpectTotal6(i64 count) {
             UNIT_ASSERT_VALUES_EQUAL_C(Total6->Val(), count, Section_);
         }
 
         void ExpectTotal4(i64 count) {
             UNIT_ASSERT_VALUES_EQUAL_C(Total4->Val(), count, Section_);
+        }
+
+        void ExpectUnspec(i64 total, i64 inflight) {
+            UNIT_ASSERT_C(
+                TotalUnspec->Val() == total && InFlightUnspec->Val() == inflight,
+                Section_ << ": Expect6(" << total << ", " << inflight << ") "
+                << " but got (" << TotalUnspec->Val() << ", " << InFlightUnspec->Val() << ")");
         }
 
         void Expect6(i64 total, i64 inflight) {
@@ -357,61 +399,41 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
 
         auto sender = runtime.AllocateEdgeActor();
 
-        // First time resolve, ipv4 and ipv6 sent in parallel, we wait for ipv6 result
+        // First time resolve, we expect AF_UNSPEC result to be cached
         runtime.Section("First time resolve");
         runtime.SendGetAddr(sender, "yandex.ru", AF_UNSPEC);
         runtime.ExpectGetAddrSuccess(sender, "2a02:6b8:a::a");
-        runtime.Expect6(1, 0);
-        runtime.Expect4(1, 0);
+        runtime.ExpectUnspec(1, 0);
         runtime.ExpectMisses(1);
         runtime.ExpectHits(0);
 
-        // Second resolve, ipv6 and ipv4 queries result in a cache hit
-        runtime.Section("Second resolve, ipv6");
-        runtime.SendGetAddr(sender, "yandex.ru", AF_INET6);
+        // Second resolve, unspec is a cache hit, ipv6 and ipv4 result in cache misses
+        runtime.Section("Second resolve");
+        runtime.SendGetAddr(sender, "yandex.ru", AF_UNSPEC);
         runtime.ExpectGetAddrSuccess(sender, "2a02:6b8:a::a");
-        runtime.Expect6(1, 0);
+        runtime.ExpectUnspec(1, 0);
         runtime.ExpectHits(1);
-        runtime.Section("Second resolve, ipv4");
-        runtime.SendGetAddr(sender, "yandex.ru", AF_INET);
-        runtime.ExpectGetAddrSuccess(sender, "77.88.55.77");
-        runtime.Expect4(1, 0);
-        runtime.ExpectHits(2);
 
-        // Wait until soft expiration and try ipv4 again
-        // Will cause a cache hit, but will start a new ipv4 request in background
-        runtime.Section("Retry ipv4 after soft expiration");
-        runtime.Sleep(TDuration::Seconds(15));
-        runtime.SendGetAddr(sender, "yandex.ru", AF_INET);
-        runtime.ExpectGetAddrSuccess(sender, "77.88.55.77");
-        runtime.Expect6(1, 0);
-        runtime.Expect4(2, 1);
-        runtime.ExpectMisses(1);
-        runtime.ExpectHits(3);
-        runtime.WaitNoInFlight();
-
-        // Wait until soft expiration and try both again
-        // Will cause a cache hit, but will start a new ipv6 request in background
+        // Wait until soft expiration and try unspec again
+        // Will cause a cache hit, but will start a new request in background
         runtime.Section("Retry both after soft expiration");
         runtime.Sleep(TDuration::Seconds(15));
         runtime.SendGetAddr(sender, "yandex.ru", AF_UNSPEC);
         runtime.ExpectGetAddrSuccess(sender, "2a02:6b8:a::a");
-        runtime.Expect6(2, 1);
-        runtime.Expect4(2, 0);
+        runtime.ExpectUnspec(2, 1);
         runtime.ExpectMisses(1);
-        runtime.ExpectHits(4);
+        runtime.ExpectHits(2);
         runtime.WaitNoInFlight();
 
-        // Wait until hard expiration and try both again
+        // Wait until hard expiration and try unspec again
         // Will cause a cache miss and new resolve requests
         runtime.Section("Retry both after hard expiration");
         runtime.Sleep(TDuration::Hours(2));
         runtime.SendGetAddr(sender, "yandex.ru", AF_UNSPEC);
         runtime.ExpectGetAddrSuccess(sender, "2a02:6b8:a::a");
-        runtime.Expect6(3, 0);
-        runtime.Expect4(3, 0);
+        runtime.ExpectUnspec(3, 0);
         runtime.ExpectMisses(2);
-        runtime.ExpectHits(4);
+        runtime.ExpectHits(2);
 
         // Wait half the hard expiration time, must always result in a cache hit
         runtime.Section("Retry both after half hard expiration");
@@ -419,39 +441,38 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
             runtime.Sleep(TDuration::Hours(1));
             runtime.SendGetAddr(sender, "yandex.ru", AF_UNSPEC);
             runtime.ExpectGetAddrSuccess(sender, "2a02:6b8:a::a");
-            runtime.Expect6(3 + i, 1);
-            runtime.ExpectHits(4 + i);
+            runtime.ExpectUnspec(3 + i, 1);
+            runtime.ExpectHits(2 + i);
             runtime.WaitNoInFlight();
         }
 
-        // Change v6 result to a timeout, must keep using cached result until hard expiration
+        // Change unspec result to a timeout, must keep using cached result until hard expiration
         runtime.Section("Dns keeps timing out");
-        runtime.ReplyV6["yandex.ru"] = TMockReply::Error(ARES_ETIMEOUT);
+        runtime.ReplyUnspec["yandex.ru"] = TMockReply::Error(ARES_ETIMEOUT);
         for (ui64 i = 1; i <= 4; ++i) {
             runtime.Sleep(TDuration::Seconds(15));
             runtime.SendGetAddr(sender, "yandex.ru", AF_UNSPEC);
             runtime.ExpectGetAddrSuccess(sender, "2a02:6b8:a::a");
-            runtime.Expect6(7 + i, 1);
-            runtime.ExpectHits(8 + i);
+            runtime.ExpectUnspec(7 + i, 1);
+            runtime.ExpectHits(6 + i);
             runtime.WaitNoInFlight();
         }
 
-        // Change v6 result to nodata, must switch to a v4 result eventually
+        // Change unspec result to v4, must switch to a v4 result eventually
         runtime.Section("Host changes to being ipv4 only");
-        runtime.ReplyV6["yandex.ru"] = TMockReply::Error(ARES_ENODATA);
+        runtime.ReplyUnspec["yandex.ru"] = runtime.ReplyV4.at("yandex.ru");
         runtime.Sleep(TDuration::Seconds(2));
         runtime.SendGetAddr(sender, "yandex.ru", AF_UNSPEC);
         runtime.ExpectGetAddrSuccess(sender, "2a02:6b8:a::a");
         runtime.WaitNoInFlight();
         runtime.SendGetAddr(sender, "yandex.ru", AF_UNSPEC);
         runtime.ExpectGetAddrSuccess(sender, "77.88.55.77");
-        runtime.Expect6(12, 0);
-        runtime.Expect4(4, 0);
-        runtime.ExpectMisses(3);
+        runtime.ExpectUnspec(12, 0);
+        runtime.ExpectMisses(2);
 
-        // Change v6 result to nxdomain, must not fall back to a v4 result
+        // Change unspec result to nxdomain, must start returning it
         runtime.Section("Host is removed from dns");
-        runtime.ReplyV6["yandex.ru"] = TMockReply::Error(ARES_ENOTFOUND);
+        runtime.ReplyUnspec["yandex.ru"] = TMockReply::Error(ARES_ENOTFOUND);
         runtime.Sleep(TDuration::Seconds(15));
         runtime.SendGetAddr(sender, "yandex.ru", AF_UNSPEC);
         runtime.ExpectGetAddrSuccess(sender, "77.88.55.77");
@@ -478,20 +499,18 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
         runtime.ExpectHits(1);
 
         runtime.Section("Dns keeps timing out");
-        runtime.ReplyV6["router.asus.com"] = TMockReply::Error(ARES_ETIMEOUT);
-        runtime.ReplyV4["router.asus.com"] = TMockReply::Error(ARES_ETIMEOUT);
+        runtime.ReplyUnspec["router.asus.com"] = TMockReply::Error(ARES_ETIMEOUT);
         for (ui64 i = 1; i <= 4; ++i) {
             runtime.Sleep(TDuration::Seconds(15));
             runtime.SendGetAddr(sender, "router.asus.com", AF_UNSPEC);
             runtime.ExpectGetAddrSuccess(sender, "192.168.0.1");
-            runtime.Expect6(1 + i, 1);
-            runtime.Expect4(1 + i, 1);
+            runtime.ExpectUnspec(1 + i, 1);
             runtime.ExpectHits(1 + i);
             runtime.WaitNoInFlight();
         }
 
-        runtime.Section("Host is removed from ipv4 dns");
-        runtime.ReplyV4["router.asus.com"] = TMockReply::Error(ARES_ENOTFOUND);
+        runtime.Section("Host is removed from dns");
+        runtime.ReplyUnspec["router.asus.com"] = TMockReply::Error(ARES_ENOTFOUND);
         runtime.Sleep(TDuration::Seconds(15));
         runtime.SendGetAddr(sender, "router.asus.com", AF_UNSPEC);
         runtime.ExpectGetAddrSuccess(sender, "192.168.0.1");
@@ -507,12 +526,11 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
 
         auto sender = runtime.AllocateEdgeActor();
 
-        runtime.ReplyV6["notfound.ru"] = TMockReply::Error(ARES_ENODATA);
-        runtime.ReplyV4["notfound.ru"] = TMockReply::Error(ARES_ENOTFOUND);
+        runtime.ReplyUnspec["notfound.ru"] = TMockReply::Error(ARES_ENOTFOUND);
         runtime.SendGetAddr(sender, "notfound.ru", AF_UNSPEC);
         runtime.ExpectGetAddrError(sender, ARES_ENOTFOUND);
 
-        runtime.ReplyV4["notfound.ru"] = TMockReply::Error(ARES_ETIMEOUT);
+        runtime.ReplyUnspec["notfound.ru"] = TMockReply::Error(ARES_ETIMEOUT);
         runtime.SendGetAddr(sender, "notfound.ru", AF_UNSPEC);
         runtime.ExpectGetAddrError(sender, ARES_ENOTFOUND);
         runtime.WaitNoInFlight();
@@ -546,7 +564,7 @@ Y_UNIT_TEST_SUITE(CachingDnsResolver) {
         runtime.SendGetAddr(sender, "yandex.ru", AF_UNSPEC);
         runtime.ExpectGetHostByNameSuccess(sender, "192.168.0.1");
         runtime.ExpectGetAddrSuccess(sender, "192.168.0.1");
-        runtime.ExpectGetHostByNameSuccess(sender, "2a02:6b8:a::a");
+        runtime.ExpectGetHostByNameSuccess(sender, "2a02:6b8:a::a,77.88.55.77");
         runtime.ExpectGetAddrSuccess(sender, "2a02:6b8:a::a");
 
         runtime.SendGetHostByName(sender, "notfound.ru", AF_UNSPEC);

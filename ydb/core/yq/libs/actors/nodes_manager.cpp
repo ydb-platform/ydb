e2a6f2c26d958d2e174489cff6e6d19e9814aa4c
hor911 <hor911@ydb.tech>
2022-07-12 13:45:17 +0300

    Suppress DC locality

--- ydb/core/yq/libs/actors/nodes_manager.cpp
+++ ydb/core/yq/libs/actors/nodes_manager.cpp
@@ -42,7 +42,7 @@ public:
         const ::NYql::NCommon::TServiceCounters& serviceCounters,
         const NConfig::TPrivateApiConfig& privateApiConfig,
         const ui32& icPort,
-        const TString& dataCenter,
+        const TString&,
         const TString& tenant,
         ui64 mkqlInitialMemoryLimit)
         : WorkerManagerCounters(workerManagerCounters)
@@ -54,7 +54,7 @@ public:
         , MkqlInitialMemoryLimit(mkqlInitialMemoryLimit)
         , YqSharedResources(yqSharedResources)
         , IcPort(icPort)
-        , DataCenter(dataCenter)
+        , DataCenter("")
         , InternalServiceId(MakeInternalServiceActorId())
 
     {

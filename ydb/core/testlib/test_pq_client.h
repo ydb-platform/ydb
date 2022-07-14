f9a769cdfc6999a2fe5d0da2e20c40af02bbb5f4
alexbogo <alexbogo@ydb.tech>
2022-07-14 16:08:39 +0300

    [test_pq_client] fix consumers codecs init in tests
    fix

--- ydb/core/testlib/test_pq_client.h
+++ ydb/core/testlib/test_pq_client.h
@@ -158,7 +158,7 @@ struct TRequestCreatePQ {
             config->AddReadFromTimestampsMs(0);
             config->AddConsumerFormatVersions(0);
             config->AddReadRuleVersions(0);
-            config->AddConsumerCodecs()->AddIds(0);
+            config->AddConsumerCodecs();
         }
 //        if (!ReadRules.empty()) {
 //            config->SetRequireAuthRead(true);

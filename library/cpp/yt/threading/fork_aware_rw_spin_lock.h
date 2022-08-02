d34d0090f4ff0c5c7661d485ddd5d28a61ef3841
vvvv <vvvv@ydb.tech>
2022-08-02 22:47:21 +0300

    coalesce

---
+++ library/cpp/yt/threading/fork_aware_rw_spin_lock.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "rw_spin_lock.h"
+
+namespace NYT::NThreading {
+
+////////////////////////////////////////////////////////////////////////////////
+
+//! Wraps TReaderWriterSpinLock and additionally acquires a global fork lock (in read mode)
+//! preventing concurrent forks from happening.
+class TForkAwareReaderWriterSpinLock
+{
+public:
+    TForkAwareReaderWriterSpinLock() = default;
+    TForkAwareReaderWriterSpinLock(const TForkAwareReaderWriterSpinLock&) = delete;
+    TForkAwareReaderWriterSpinLock& operator =(const TForkAwareReaderWriterSpinLock&) = delete;
+
+    void AcquireReader() noexcept;
+    void ReleaseReader() noexcept;
+
+    void AcquireWriter() noexcept;
+    void ReleaseWriter() noexcept;
+
+    bool IsLocked() const noexcept;
+    bool IsLockedByReader() const noexcept;
+    bool IsLockedByWriter() const noexcept;
+
+private:
+    TReaderWriterSpinLock SpinLock_;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NThreading

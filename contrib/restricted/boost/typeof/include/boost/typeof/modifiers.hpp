d930aa2ae8568d58b39909b29be61130cedfad15
robot-contrib <robot-contrib@yandex-team.com>
2022-08-08 21:17:19 +0300

    Update contrib/restricted/boost/typeof to 1.79.0

--- contrib/restricted/boost/typeof/include/boost/typeof/modifiers.hpp
+++ contrib/restricted/boost/typeof/include/boost/typeof/modifiers.hpp
@@ -19,11 +19,11 @@
             typename boost::type_of::encode_type<\
             typename boost::type_of::push_back<\
             V\
-            , boost::mpl::size_t<ID> >::type\
+            , boost::type_of::constant<std::size_t,ID> >::type\
             , T>::type\
             type;\
     };\
-    template<class Iter> struct decode_type_impl<boost::mpl::size_t<ID>, Iter>\
+    template<class Iter> struct decode_type_impl<boost::type_of::constant<std::size_t,ID>, Iter>\
     {\
         typedef boost::type_of::decode_type<Iter> d1;\
         typedef Fun(typename d1::type) type;\
@@ -37,7 +37,7 @@
 #define BOOST_TYPEOF_pointer_fun(T) T*
 #define BOOST_TYPEOF_reference_fun(T) T&
 
-#if defined(__BORLANDC__) && (__BORLANDC__ < 0x600)
+#if defined(__BORLANDC__) && !defined(__clang__) && (__BORLANDC__ < 0x600)
 //Borland incorrectly handles T const, T const volatile and T volatile.
 //It drops the decoration no matter what, so we need to try to handle T* const etc. without loosing the top modifier.
 #define BOOST_TYPEOF_const_pointer_fun(T) T const *
@@ -56,7 +56,7 @@ BOOST_TYPEOF_modifier_support(BOOST_TYPEOF_UNIQUE_ID(), BOOST_TYPEOF_volatile_co
 BOOST_TYPEOF_modifier_support(BOOST_TYPEOF_UNIQUE_ID(), BOOST_TYPEOF_pointer_fun);
 BOOST_TYPEOF_modifier_support(BOOST_TYPEOF_UNIQUE_ID(), BOOST_TYPEOF_reference_fun);
 
-#if defined(__BORLANDC__) && (__BORLANDC__ < 0x600)
+#if defined(__BORLANDC__) && !defined(__clang__) && (__BORLANDC__ < 0x600)
 BOOST_TYPEOF_modifier_support(BOOST_TYPEOF_UNIQUE_ID(), BOOST_TYPEOF_const_pointer_fun);
 BOOST_TYPEOF_modifier_support(BOOST_TYPEOF_UNIQUE_ID(), BOOST_TYPEOF_const_reference_fun);
 BOOST_TYPEOF_modifier_support(BOOST_TYPEOF_UNIQUE_ID(), BOOST_TYPEOF_volatile_pointer_fun);
@@ -74,7 +74,7 @@ BOOST_TYPEOF_END_ENCODE_NS
 #undef BOOST_TYPEOF_pointer_fun
 #undef BOOST_TYPEOF_reference_fun
 
-#if defined(__BORLANDC__) && (__BORLANDC__ < 0x600)
+#if defined(__BORLANDC__) && !defined(__clang__) && (__BORLANDC__ < 0x600)
 #undef BOOST_TYPEOF_const_pointer_fun
 #undef BOOST_TYPEOF_const_reference_fun
 #undef BOOST_TYPEOF_volatile_pointer_fun
@@ -94,13 +94,13 @@ BOOST_TYPEOF_END_ENCODE_NS
             typename boost::type_of::push_back<\
             typename boost::type_of::push_back<\
             V\
-            , boost::mpl::size_t<ID> >::type\
-            , boost::mpl::size_t<N> >::type\
+            , boost::type_of::constant<std::size_t,ID> >::type\
+            , boost::type_of::constant<std::size_t,N> >::type\
             , T>::type\
         type;\
     };\
     template<class Iter>\
-    struct decode_type_impl<boost::mpl::size_t<ID>, Iter>\
+    struct decode_type_impl<boost::type_of::constant<std::size_t,ID>, Iter>\
     {\
         enum{n = Iter::type::value};\
         typedef boost::type_of::decode_type<typename Iter::next> d;\
--- contrib/restricted/boost/typeof/include/boost/typeof/template_template_param.hpp
+++ contrib/restricted/boost/typeof/include/boost/typeof/template_template_param.hpp
@@ -81,10 +81,10 @@ namespace boost { namespace type_of {
         BOOST_PP_ENUM_PARAMS(\
         BOOST_PP_SEQ_SIZE(Params),\
         P)> >\
-        : boost::type_of::push_back<V, boost::mpl::size_t<ID> >\
+        : boost::type_of::push_back<V, boost::type_of::constant<std::size_t,ID> >\
     {\
     };\
-    template<class Iter> struct decode_template_impl<boost::mpl::size_t<ID>, Iter>\
+    template<class Iter> struct decode_template_impl<boost::type_of::constant<std::size_t,ID>, Iter>\
     {\
         BOOST_PP_REPEAT(BOOST_PP_SEQ_SIZE(Params),BOOST_TYPEOF_TYPEDEF_INT_PN,_)\
         typedef Name<BOOST_TYPEOF_SEQ_ENUM(Params,BOOST_TYPEOF_PLACEHOLDER) > type;\
@@ -93,7 +93,7 @@ namespace boost { namespace type_of {
 
 #define BOOST_TYPEOF_TYPEDEF_INT_PN(z,n,Params) typedef int BOOST_PP_CAT(P,n);
 
-#ifdef __BORLANDC__
+#if defined(__BORLANDC__) && !defined(__clang__)
 #define BOOST_TYPEOF_DECODE_NESTED_TEMPLATE_HELPER_NAME BOOST_PP_CAT(\
         BOOST_PP_CAT(\
             BOOST_PP_CAT(\

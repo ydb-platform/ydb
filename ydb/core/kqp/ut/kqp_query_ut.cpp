be2bb7b8834b39ffe7fdba646f380c229f273ced
ulya-sidorina <yulia@ydb.tech>
2022-08-19 00:06:42 +0300

    deduplicate internal bindings
    fix(peephole): deduplicate internal bindings

--- ydb/core/kqp/ut/kqp_query_ut.cpp
+++ ydb/core/kqp/ut/kqp_query_ut.cpp
@@ -1200,6 +1200,21 @@ Y_UNIT_TEST_SUITE(KqpQuery) {
         }
     }
 
+    Y_UNIT_TEST_NEW_ENGINE(MultipleCurrentUtcTimestamp) {
+        TKikimrRunner kikimr;
+        auto db = kikimr.GetTableClient();
+        auto session = db.CreateSession().GetValueSync().GetSession();
+
+        auto query = Q1_(R"(
+            SELECT * FROM `/Root/Logs` WHERE Ts > Cast(CurrentUtcTimestamp() as Int64)
+            UNION ALL
+            SELECT * FROM `/Root/Logs` WHERE Ts < Cast(CurrentUtcTimestamp() as Int64);
+        )");
+
+        auto result = session.ExecuteDataQuery(query, TTxControl::BeginTx().CommitTx()).ExtractValueSync();
+        UNIT_ASSERT_VALUES_EQUAL_C(result.GetStatus(), EStatus::SUCCESS, result.GetIssues().ToString());
+    }
+
     Y_UNIT_TEST_NEW_ENGINE(SelectWhereInSubquery) {
         TKikimrRunner kikimr;
         auto db = kikimr.GetTableClient();

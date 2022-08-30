8469cf6fb80772e2aa59c9208ae0e25db4b9b681
mokhotskii <mokhotskii@ydb.tech>
2022-08-30 11:24:30 +0300

    Move implementation of test-related functions to avoid double linkage
    Move implementation of test-related functions to cpp

--- ydb/library/persqueue/tests/counters.h
+++ ydb/library/persqueue/tests/counters.h
@@ -1,126 +1,24 @@
 #pragma once
 
-#include <library/cpp/testing/unittest/registar.h>
-#include <library/cpp/http/io/stream.h>
-#include <library/cpp/json/json_reader.h>
 #include <library/cpp/json/json_value.h>
-
-#include <util/string/builder.h>
-#include <util/string/vector.h>
-#include <util/string/join.h>
-#include <util/network/socket.h>
 
 
 namespace NKikimr::NPersQueueTests {
 
-namespace {
-    NJson::TJsonValue SendQuery(ui16 port, const TString& query, bool mayFail = false) {
-        Cerr << "===Request counters with query: " << query << Endl;
-        TNetworkAddress addr("localhost", port);
-        TSocket s(addr);
-        SendMinimalHttpRequest(s, "localhost", query);
-        TSocketInput si(s);
-        THttpInput input(&si);
-        TString firstLine = input.FirstLine();
-
-        const auto httpCode = ParseHttpRetCode(firstLine);
-        if (mayFail && httpCode != 200u) {
-            return {};
-        } else {
-            UNIT_ASSERT_VALUES_EQUAL(httpCode, 200u);
-        }
-        NJson::TJsonValue value;
-        UNIT_ASSERT(NJson::ReadJsonTree(&input, &value));
-
-        Cerr << "counters: " << value.GetStringRobust() << "\n";
-        return value;
-    }
-}
+NJson::TJsonValue SendQuery(ui16 port, const TString& query, bool mayFail = false);
 
 NJson::TJsonValue GetCountersLegacy(ui16 port, const TString& counters, const TString& subsystem,
                                     const TString& topicPath, const TString& clientDc = "",
                                     const TString& originalDc = "", const TString& client = "",
-                                    const TString& consumerPath = "") {
-    TString dcFilter = "";
-    {
-        if (originalDc) {
-            dcFilter = TStringBuilder() << "/OriginDC=" << originalDc;
-        } else if (clientDc) {
-            dcFilter = TStringBuilder() << "/ClientDC=" << clientDc;
-        }
-    }
-    TVector<TString> pathItems = SplitString(topicPath, "/");
-    UNIT_ASSERT(pathItems.size() >= 2);
-    TString account = pathItems.front();
-    TString producer = JoinRange("@", pathItems.begin(), pathItems.end() - 1);
-    TStringBuilder queryBuilder = TStringBuilder() <<
-        "/counters/counters=" << counters <<
-        "/subsystem=" << subsystem <<
-        "/Account=" << account <<
-        "/Producer=" << producer <<
-        "/Topic=" << Join("--", producer, pathItems.back()) <<
-        "/TopicPath=" << JoinRange("%2F", pathItems.begin(), pathItems.end()) <<
-        dcFilter;
-
-    if (consumerPath) {
-        auto consumerPathItems = SplitString(consumerPath, "/");
-        queryBuilder <<
-            "/Client=" << client <<
-            "/ConsumerPath=" << JoinRange("%2F", consumerPathItems.begin(), consumerPathItems.end());
-    }
-    queryBuilder << "/json";
-
-    return SendQuery(port, queryBuilder);
-}
+                                    const TString& consumerPath = "");
 
 NJson::TJsonValue GetClientCountersLegacy(ui16 port, const TString& counters, const TString& subsystem,
-                                          const TString& client, const TString& consumerPath) {
-    TVector<TString> consumerPathItems = SplitString(consumerPath, "/");
-    UNIT_ASSERT(consumerPathItems.size() >= 2);
-    TStringBuilder queryBuilder = TStringBuilder() <<
-        "/counters/counters=" << counters <<
-        "/subsystem=" << subsystem <<
-        "/Client=" << client <<
-        "/ConsumerPath=" << JoinRange("%2F", consumerPathItems.begin(), consumerPathItems.end()) <<
-        "/json";
-
-    return SendQuery(port, queryBuilder);
-}
+                                          const TString& client, const TString& consumerPath);
 
 NJson::TJsonValue GetCounters1stClass(ui16 port, const TString& counters,
                                       const TString& cloudId, const TString& databaseId,
                                       const TString& folderId, const TString& streamName,
                                       const TString& consumer, const TString& host,
-                                      const TString& shard) {
-    bool mayFail = false;
-    TVector<TString> pathItems = SplitString(streamName, "/");
-    TStringBuilder queryBuilder;
-    queryBuilder <<
-        "/counters/counters=" << counters <<
-        "/cloud=" << cloudId <<
-        "/folder=" << folderId <<
-        "/database=" << databaseId <<
-        "/stream=" << JoinRange("%2F", pathItems.begin(), pathItems.end());
-
-    if (consumer) {
-        queryBuilder <<
-            "/consumer=" << consumer;
-    }
-
-    if (host) {
-        queryBuilder <<
-            "/host=" << host;
-    }
-
-    if (shard) {
-        queryBuilder <<
-            "/shard=" << shard;
-        mayFail = true;
-    }
-
-    queryBuilder << "/json";
-
-    return SendQuery(port, queryBuilder, mayFail);
-}
+                                      const TString& shard);
 
 } // NKikimr::NPersQueueTests

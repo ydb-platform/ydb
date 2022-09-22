e42584062fd350346c55dc5811b2cd5fe4aa3f85
alexnick <alexnick@ydb.tech>
2022-09-22 10:39:59 +0300

    fix for ChooseProxy

--- ydb/core/client/server/grpc_proxy_status.cpp
+++ ydb/core/client/server/grpc_proxy_status.cpp
@@ -100,7 +100,7 @@ public:
         Y_VERIFY(!nodesInfo->Nodes.empty());
         Nodes.reserve(nodesInfo->Nodes.size());
         for (const auto& ni : nodesInfo->Nodes) {
-            if (ni.Port != AppData(ctx)->PQConfig.GetLegacyApiPort()) continue;
+            if (ni.Port > AppData(ctx)->PQConfig.GetMaxStorageNodePort()) continue;
             NodeNames[ni.NodeId] = ni.Host;
             NodeDataCenter[ni.NodeId] = ni.Location.GetDataCenterId();
             SendRequest(ni.NodeId, ctx);

443eba7b70f9c860e2555791429053ec87e0a94c
molotkov-and <molotkov-and@yandex-team.com>
2022-09-29 11:23:38 +0300

    Extract common code of ticket parser to base class
    Refactoring of ticket parser's base class
    
    Small refactoring to move common code to base class
    
    Extract Bootstrap function to base class
    
    Extract HttpInfo handle function to base class
    
    Extract RefreshHandle function to base class
    
    Extract SetToken and SetError functions to base class

--- ydb/core/security/ticket_parser.cpp
+++ ydb/core/security/ticket_parser.cpp
@@ -1,8 +1,5 @@
 #include <library/cpp/actors/core/log.h>
 #include <library/cpp/actors/core/hfunc.h>
-#include <library/cpp/digest/md5/md5.h>
-#include <ydb/core/base/counters.h>
-#include <ydb/core/mon/mon.h>
 #include <ydb/library/security/util.h>
 #include <util/string/vector.h>
 #include "ticket_parser_impl.h"
@@ -14,6 +11,7 @@ class TTicketParser : public TTicketParserImpl<TTicketParser> {
     using TThis = TTicketParser;
     using TBase = TTicketParserImpl<TTicketParser>;
     using TBase::TBase;
+    using TTokenRecord = TBase::TTokenRecordBase;
 
     friend TBase;
 
@@ -22,38 +20,6 @@ class TTicketParser : public TTicketParserImpl<TTicketParser> {
         Unsupported,
         Builtin,
         Login,
-    };
-
-    ETokenType ParseTokenType(const TStringBuf tokenType) const {
-        if (tokenType == "Login") {
-            if (UseLoginProvider) {
-                return ETokenType::Login;
-            } else {
-                return ETokenType::Unsupported;
-            }
-        }
-        return ETokenType::Unknown;
-    }
-
-    struct TTokenRecord : TBase::TTokenRecordBase {
-        using TBase::TTokenRecordBase::TTokenRecordBase;
-
-        TString GetSubject() const {
-            return Subject;
-        }
-
-        TString GetAuthType() const {
-            switch (TokenType) {
-                case ETokenType::Unknown:
-                    return "Unknown";
-                case ETokenType::Unsupported:
-                    return "Unsupported";
-                case ETokenType::Builtin:
-                    return "Builtin";
-                case ETokenType::Login:
-                    return "Login";
-            }
-        }
     };
 
     THashMap<TString, TTokenRecord> UserTokens;
@@ -66,41 +32,8 @@ class TTicketParser : public TTicketParserImpl<TTicketParser> {
         return request->Ticket;
     }
 
-    void SetToken(const TString& key, TTokenRecord& record, TIntrusivePtr<NACLib::TUserToken> token, const TActorContext& ctx) {
-        TInstant now = ctx.Now();
-        record.Error.clear();
-        record.Token = token;
-        EnrichUserTokenWithBuiltins(record);
-        if (!token->GetUserSID().empty()) {
-            record.Subject = token->GetUserSID();
-        }
-        record.SerializedToken = token->SerializeAsString();
-        if (!record.ExpireTime) {
-            record.ExpireTime = GetExpireTime(now);
-        }
-        CounterTicketsSuccess->Inc();
-        CounterTicketsBuildTime->Collect((now - record.InitTime).MilliSeconds());
-        LOG_DEBUG_S(ctx, NKikimrServices::TICKET_PARSER, "Ticket " << MaskTicket(record.Ticket) << " ("
-                    << record.PeerName << ") has now valid token of " << record.Subject);
-        RefreshQueue.push({key, record.ExpireTime});
-    }
-
-    void SetError(const TString& key, TTokenRecord& record, const TEvTicketParser::TError& error, const TActorContext& ctx) {
-        record.Error = error;
-        if (record.Error.Retryable) {
-            record.ExpireTime = GetExpireTime(ctx.Now());
-            CounterTicketsErrorsRetryable->Inc();
-            LOG_DEBUG_S(ctx, NKikimrServices::TICKET_PARSER, "Ticket " << MaskTicket(record.Ticket) << " ("
-                        << record.PeerName << ") has now retryable error message '" << error.Message << "'");
-        } else {
-            record.Token = nullptr;
-            record.SerializedToken.clear();
-            CounterTicketsErrorsPermanent->Inc();
-            LOG_DEBUG_S(ctx, NKikimrServices::TICKET_PARSER, "Ticket " << MaskTicket(record.Ticket) << " ("
-                        << record.PeerName << ") has now permanent error message '" << error.Message << "'");
-        }
-        CounterTicketsErrors->Inc();
-        RefreshQueue.push({key, record.ExpireTime});
+    TTokenRefreshRecord MakeTokenRefreshRecord(const TString& key, const TTokenRecord& record) const {
+        return {key, record.ExpireTime};
     }
 
     bool IsTicketEmpty(const TStringBuf ticket, TEvTicketParser::TEvAuthorizeTicket::TPtr&) {
@@ -111,162 +44,20 @@ class TTicketParser : public TTicketParserImpl<TTicketParser> {
         UserTokens.erase(ev->Get()->Ticket);
     }
 
-    void Handle(TEvTicketParser::TEvDiscardTicket::TPtr& ev, const TActorContext&) {
-        UserTokens.erase(ev->Get()->Ticket);
+    void Refresh(const TString&, const TTokenRecord&, const TActorContext&) {
+        // we don't need to refresh anything
     }
 
-    void HandleRefresh(const NActors::TActorContext& ctx) {
-        while (!RefreshQueue.empty() && RefreshQueue.top().RefreshTime <= ctx.Now()) {
-            TString key = RefreshQueue.top().Key;
-            RefreshQueue.pop();
-            auto itToken = UserTokens.find(key);
-            if (itToken == UserTokens.end()) {
-                continue;
-            }
-            auto& record(itToken->second);
-            if ((record.ExpireTime > ctx.Now()) && (record.AccessTime + GetLifeTime() > ctx.Now())) {
-                // we don't need to refresh anything
-            } else {
-                LOG_DEBUG_S(ctx, NKikimrServices::TICKET_PARSER, "Expired ticket " << MaskTicket(record.Ticket));
-                if (!record.AuthorizeRequests.empty()) {
-                    record.Error = {"Timed out", true};
-                    Respond(record, ctx);
-                }
-                UserTokens.erase(itToken);
-            }
-        }
-        ctx.Schedule(RefreshPeriod, new NActors::TEvents::TEvWakeup());
-    }
-
-    void Handle(NMon::TEvHttpInfo::TPtr& ev, const TActorContext& ctx) {
-        const auto& params = ev->Get()->Request.GetParams();
-        TStringBuilder html;
-        if (params.Has("token")) {
-            TString token = params.Get("token");
-            html << "<head>";
-            html << "<style>";
-            html << "table.ticket-parser-proplist > tbody > tr > td { padding: 1px 3px; } ";
-            html << "table.ticket-parser-proplist > tbody > tr > td:first-child { font-weight: bold; text-align: right; } ";
-            html << "</style>";
-            for (const std::pair<const TString, TTokenRecord>& pr : UserTokens) {
-                if (MD5::Calc(pr.first) == token) {
-                    const TTokenRecord& record = pr.second;
-                    html << "<div>";
-                    html << "<table class='ticket-parser-proplist'>";
-                    html << "<tr><td>Ticket</td><td>" << MaskTicket(record.Ticket) << "</td></tr>";
-                    if (record.TokenType == ETokenType::Login) {
-                        TVector<TString> tokenData;
-                        Split(record.Ticket, ".", tokenData);
-                        if (tokenData.size() > 1) {
-                            TString header;
-                            TString payload;
-                            try {
-                                header = Base64DecodeUneven(tokenData[0]);
-                            }
-                            catch (const std::exception&) {
-                                header = tokenData[0];
-                            }
-                            try {
-                                payload = Base64DecodeUneven(tokenData[1]);
-                            }
-                            catch (const std::exception&) {
-                                payload = tokenData[1];
-                            }
-                            html << "<tr><td>Header</td><td>" << header << "</td></tr>";
-                            html << "<tr><td>Payload</td><td>" << payload << "</td></tr>";
-                        }
-                    }
-                    html << "<tr><td>Subject</td><td>" << record.Subject << "</td></tr>";
-                    html << "<tr><td>Additional SIDs</td><td>" << JoinStrings(record.AdditionalSIDs.begin(), record.AdditionalSIDs.end(), ", ") << "</td></tr>";
-                    html << "<tr><td>Error</td><td>" << record.Error << "</td></tr>";
-                    html << "<tr><td>Requests Infly</td><td>" << record.AuthorizeRequests.size() << "</td></tr>";
-                    html << "<tr><td>Responses Left</td><td>" << record.ResponsesLeft << "</td></tr>";
-                    html << "<tr><td>Expire Time</td><td>" << record.ExpireTime << "</td></tr>";
-                    html << "<tr><td>Access Time</td><td>" << record.AccessTime << "</td></tr>";
-                    html << "<tr><td>Peer Name</td><td>" << record.PeerName << "</td></tr>";
-                    if (record.Token != nullptr) {
-                        html << "<tr><td>User SID</td><td>" << record.Token->GetUserSID() << "</td></tr>";
-                        for (const TString& group : record.Token->GetGroupSIDs()) {
-                            html << "<tr><td>Group SID</td><td>" << group << "</td></tr>";
-                        }
-                    }
-                    html << "</table>";
-                    html << "</div>";
-                    break;
-                }
-            }
-            html << "</head>";
-        } else {
-            html << "<head>";
-            html << "<script>$('.container').css('width', 'auto');</script>";
-            html << "<style>";
-            html << "table.ticket-parser-proplist > tbody > tr > td { padding: 1px 3px; } ";
-            html << "table.ticket-parser-proplist > tbody > tr > td:first-child { font-weight: bold; text-align: right; } ";
-            html << "table.simple-table1 th { margin: 0px 3px; text-align: center; } ";
-            html << "table.simple-table1 > tbody > tr > td:nth-child(6) { text-align: right; }";
-            html << "table.simple-table1 > tbody > tr > td:nth-child(7) { text-align: right; }";
-            html << "table.simple-table1 > tbody > tr > td:nth-child(8) { white-space: nowrap; }";
-            html << "table.simple-table1 > tbody > tr > td:nth-child(9) { white-space: nowrap; }";
-            html << "table.simple-table1 > tbody > tr > td:nth-child(10) { white-space: nowrap; }";
-            html << "table.table-hover tbody tr:hover > td { background-color: #9dddf2; }";
-            html << "</style>";
-            html << "</head>";
-            html << "<div style='margin-bottom: 10px; margin-left: 100px'>";
-            html << "<table class='ticket-parser-proplist'>";
-            html << "<tr><td>User Tokens</td><td>" << UserTokens.size() << "</td></tr>";
-            html << "<tr><td>Refresh Queue</td><td>" << RefreshQueue.size() << "</td></tr>";
-            if (!RefreshQueue.empty()) {
-                html << "<tr><td>Refresh Queue Time</td><td>" << RefreshQueue.top().RefreshTime << "</td></tr>";
-            }
-            html << "<tr><td>Refresh Period</td><td>" << RefreshPeriod << "</td></tr>";
-            html << "<tr><td>Life Time</td><td>" << LifeTime << "</td></tr>";
-            html << "<tr><td>Expire Time</td><td>" << ExpireTime << "</td></tr>";
-            if (UseLoginProvider) {
-                for (const auto& [databaseName, loginProvider] : LoginProviders) {
-                    html << "<tr><td>LoginProvider Database</td><td>" << databaseName << " (" << loginProvider.Audience << ")</td></tr>";
-                    html << "<tr><td>LoginProvider Keys</td><td>" << GetLoginProviderKeys(loginProvider) << "</td></tr>";
-                    html << "<tr><td>LoginProvider Sids</td><td>" << loginProvider.Sids.size() << "</td></tr>";
-                }
-            }
-            html << "<tr><td>Login</td><td>" << HtmlBool(UseLoginProvider) << "</td></tr>";
-            html << "</table>";
-            html << "</div>";
-
-            html << "<div>";
-            html << "<table class='table simple-table1 table-hover table-condensed'>";
-            html << "<thead><tr>";
-            html << "<th>Ticket</th>";
-            html << "<th>UID</th>";
-            html << "<th>Database</th>";
-            html << "<th>Subject</th>";
-            html << "<th>Error</th>";
-            html << "<th>Token</th>";
-            html << "<th>Requests</th>";
-            html << "<th>Responses Left</th>";
-            html << "<th>Refresh</th>";
-            html << "<th>Expire</th>";
-            html << "<th>Access</th>";
-            html << "<th>Peer</th>";
-            html << "</tr></thead><tbody>";
-            for (const std::pair<const TString, TTokenRecord>& pr : UserTokens) {
-                const TTokenRecord& record = pr.second;
-                html << "<tr>";
-                html << "<td>" << MaskTicket(record.Ticket) << "</td>";
-                html << "<td>" << record.Database << "</td>";
-                html << "<td>" << record.Subject << "</td>";
-                html << "<td>" << record.Error << "</td>";
-                html << "<td>" << "<a href='ticket_parser?token=" << MD5::Calc(pr.first) << "'>" << HtmlBool(record.Token != nullptr) << "</a>" << "</td>";
-                html << "<td>" << record.AuthorizeRequests.size() << "</td>";
-                html << "<td>" << record.ResponsesLeft << "</td>";
-                html << "<td>" << record.ExpireTime << "</td>";
-                html << "<td>" << record.AccessTime << "</td>";
-                html << "<td>" << record.PeerName << "</td>";
-                html << "</tr>";
-            }
-            html << "</tbody></table>";
-            html << "</div>";
-        }
-        ctx.Send(ev->Sender, new NMon::TEvHttpInfoRes(html));
+    static void MakeHtmlTable(TStringBuilder& html) {
+        html << "table.ticket-parser-proplist > tbody > tr > td { padding: 1px 3px; } ";
+        html << "table.ticket-parser-proplist > tbody > tr > td:first-child { font-weight: bold; text-align: right; } ";
+        html << "table.simple-table1 th { margin: 0px 3px; text-align: center; } ";
+        html << "table.simple-table1 > tbody > tr > td:nth-child(6) { text-align: right; }";
+        html << "table.simple-table1 > tbody > tr > td:nth-child(7) { text-align: right; }";
+        html << "table.simple-table1 > tbody > tr > td:nth-child(8) { white-space: nowrap; }";
+        html << "table.simple-table1 > tbody > tr > td:nth-child(9) { white-space: nowrap; }";
+        html << "table.simple-table1 > tbody > tr > td:nth-child(10) { white-space: nowrap; }";
+        html << "table.table-hover tbody tr:hover > td { background-color: #9dddf2; }";
     }
 
 public:
@@ -274,49 +65,12 @@ public:
         switch (ev->GetTypeRewrite()) {
             HFunc(TEvTicketParser::TEvAuthorizeTicket, TBase::Handle);
             HFunc(TEvTicketParser::TEvRefreshTicket, Handle);
-            HFunc(TEvTicketParser::TEvDiscardTicket, Handle);
+            HFunc(TEvTicketParser::TEvDiscardTicket, TBase::Handle);
             HFunc(TEvTicketParser::TEvUpdateLoginSecurityState, TBase::Handle);
-            HFunc(NMon::TEvHttpInfo, Handle);
+            HFunc(NMon::TEvHttpInfo, TBase::Handle);
             CFunc(TEvents::TSystem::Wakeup, HandleRefresh);
             CFunc(TEvents::TSystem::PoisonPill, Die);
         }
-    }
-
-    void Bootstrap(const TActorContext& ctx) {
-        TIntrusivePtr<::NMonitoring::TDynamicCounters> rootCounters = AppData(ctx)->Counters;
-        TIntrusivePtr<::NMonitoring::TDynamicCounters> authCounters = GetServiceCounters(rootCounters, "auth");
-        ::NMonitoring::TDynamicCounterPtr counters = authCounters->GetSubgroup("subsystem", "TicketParser");
-        CounterTicketsReceived = counters->GetCounter("TicketsReceived", true);
-        CounterTicketsSuccess = counters->GetCounter("TicketsSuccess", true);
-        CounterTicketsErrors = counters->GetCounter("TicketsErrors", true);
-        CounterTicketsErrorsRetryable = counters->GetCounter("TicketsErrorsRetryable", true);
-        CounterTicketsErrorsPermanent = counters->GetCounter("TicketsErrorsPermanent", true);
-        CounterTicketsBuiltin = counters->GetCounter("TicketsBuiltin", true);
-        CounterTicketsLogin = counters->GetCounter("TicketsLogin", true);
-        CounterTicketsCacheHit = counters->GetCounter("TicketsCacheHit", true);
-        CounterTicketsCacheMiss = counters->GetCounter("TicketsCacheMiss", true);
-        CounterTicketsBuildTime = counters->GetHistogram("TicketsBuildTimeMs",
-                                                         NMonitoring::ExplicitHistogram({0, 1, 5, 10, 50, 100, 500, 1000, 2000, 5000, 10000, 30000, 60000}));
-
-        if (Config.GetUseLoginProvider()) {
-            UseLoginProvider = true;
-        }
-        if (AppData() && AppData()->DomainsInfo && !AppData()->DomainsInfo->Domains.empty()) {
-            DomainName = "/" + AppData()->DomainsInfo->Domains.begin()->second->Name;
-        }
-
-        RefreshPeriod = TDuration::Parse(Config.GetRefreshPeriod());
-        LifeTime = TDuration::Parse(Config.GetLifeTime());
-        ExpireTime = TDuration::Parse(Config.GetExpireTime());
-
-        NActors::TMon* mon = AppData(ctx)->Mon;
-        if (mon) {
-            NMonitoring::TIndexMonPage* actorsMonPage = mon->RegisterIndexPage("actors", "Actors");
-            mon->RegisterActorPage(actorsMonPage, "ticket_parser", "Ticket Parser", false, ctx.ExecutorThread.ActorSystem, ctx.SelfID);
-        }
-
-        ctx.Schedule(RefreshPeriod, new NActors::TEvents::TEvWakeup());
-        TBase::Become(&TThis::StateWork);
     }
 };
 

c5533b2142bf51a05a75c9b22b0e899b93520829
alexv-smirnov <alex@ydb.tech>
2022-10-19 20:15:00 +0300

    PR from branch users/alexv-smirnov/fix/-move-public-yc-protos-2
    1. Протоспеки внешнего публичного API YC размещены в ydb/public/api/client/yc_public.
    2. Альтернативный внутренний вариант IAM public (kikimr/public/sdk/cpp/client/iam) удален, зависимости перенаправлены на ydb/public/sdk/cpp/client/iam
    3. kikimr/public/sdk/cpp/client/iam_private разделена на две части:
       a) iam_private теперь содержит только зависимость на yc private api, в имя методов добавлено Private. Те зависимые приложения, где использование private api задавалось константой true, рефакторены, и теперь у них нет в зависимостях yc public iam proto
       b) iam_both содержит зависимости как на private, так и на public yc api, и содержит методы с флагом usePrivateApi для выбора какое вызывать (как было раньше в iam_private)
    4. Из kikimr/persqueue/sdk/deprecated/cpp/v2 убрана наружу зависимость на yc iam, явно включена в те проекты где он используется

--- ydb/public/sdk/cpp/client/iam/iam.cpp
+++ ydb/public/sdk/cpp/client/iam/iam.cpp
@@ -1,7 +1,7 @@
-#include "impl/iam_impl.h"
+#include <ydb/public/sdk/cpp/client/iam/common/iam.h>
 
-#include <ydb/public/sdk/cpp/client/iam/proto/v1/iam_token_service.pb.h>
-#include <ydb/public/sdk/cpp/client/iam/proto/v1/iam_token_service.grpc.pb.h>
+#include <ydb/public/api/client/yc_public/iam/iam_token_service.pb.h>
+#include <ydb/public/api/client/yc_public/iam/iam_token_service.grpc.pb.h>
 
 using namespace yandex::cloud::iam::v1;
 
@@ -19,10 +19,6 @@ TCredentialsProviderFactoryPtr CreateIamJwtParamsCredentialsProviderFactory(cons
     return std::make_shared<TIamJwtCredentialsProviderFactory<CreateIamTokenRequest,
                                                               CreateIamTokenResponse,
                                                               IamTokenService>>(std::move(jwtParams));
-}
-
-TCredentialsProviderFactoryPtr CreateIamCredentialsProviderFactory(const TIamHost& params) {
-    return std::make_shared<TIamCredentialsProviderFactory>(params);
 }
 
 TCredentialsProviderFactoryPtr CreateIamOAuthCredentialsProviderFactory(const TIamOAuth& params) {

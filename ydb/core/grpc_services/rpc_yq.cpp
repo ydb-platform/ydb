b4da588c0be28d60d4e20245dd7488054268000d
hcpp <hcpp@ydb.tech>
2022-10-28 16:53:03 +0300

    cloud id for audit logs has been improved

--- ydb/core/grpc_services/rpc_yq.cpp
+++ ydb/core/grpc_services/rpc_yq.cpp
@@ -151,6 +151,7 @@ protected:
 
         NYq::TEvAuditService::TExtraInfo extraInfo{
             .Token = Token,
+            .CloudId = response.AuditDetails.CloudId,
             .FolderId = FolderId,
             .User = User,
             .PeerName = PeerName,

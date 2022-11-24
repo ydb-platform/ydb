5fbd67db573577fafc46098602461037b0828c71
dcherednik <dcherednik@ydb.tech>
2022-11-24 15:29:40 +0300

    add missed arch-arm.h file for pg_wrapper.

--- ydb/library/yql/parser/pg_wrapper/postgresql/src/include/port/atomics.h
+++ ydb/library/yql/parser/pg_wrapper/postgresql/src/include/port/atomics.h
@@ -65,7 +65,7 @@
  */
 #if defined(__arm__) || defined(__arm) || \
 	defined(__aarch64__) || defined(__aarch64)
-#error #include "port/atomics/arch-arm.h"
+#include "port/atomics/arch-arm.h"
 #elif defined(__i386__) || defined(__i386) || defined(__x86_64__)
 #include "port/atomics/arch-x86.h"
 #elif defined(__ia64__) || defined(__ia64)

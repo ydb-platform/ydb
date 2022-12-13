3e76f9f3fefc86eb2f0151ce8ad18d0816ce3522
va-kuznecov <va-kuznecov@ydb.tech>
2022-12-13 14:06:03 +0300

    Rename cli command, now it is 'load-test'

--- ydb/core/driver_lib/run/driver.h
+++ ydb/core/driver_lib/run/driver.h
@@ -35,7 +35,7 @@ namespace NKikimr {
     XX(EDM_PERSQUEUE_REQUEST, "persqueue-request", "send protobuf request to a persqueue tablet") \
     XX(EDM_PERSQUEUE_STRESS, "persqueue-stress", "stress read or write to a persqueue tablet") \
     XX(EDM_PERSQUEUE_DISCOVER_CLUSTERS, "persqueue-discover-clusters", "persqueue session clusters discovery") \
-    XX(EDM_LOAD_REQUEST, "bs-load-test", "send protobuf request to blobstorage test load actor (https://wiki.yandex-team.ru/kikimr/developers/BSLoadTest/)") \
+    XX(EDM_LOAD_REQUEST, "load-test", "send protobuf request to test load actor") \
     XX(EDM_DS_LOAD_REQUEST, "ds-load-test", "send protobuf request to datashard test load actor (https://wiki.yandex-team.ru/kikimr/developers/DSLoadTest/)") \
     XX(EDM_ACTORSYS_PERFTEST, "actorsys-perf-test", "make actorsystem performance test") \
 

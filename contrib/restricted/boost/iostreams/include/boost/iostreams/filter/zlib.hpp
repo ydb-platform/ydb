9a65d94b5624becec67095637fad81e93fdaa35f
robot-contrib <robot-contrib@yandex-team.com>
2022-12-22 18:40:56 +0300

    Update contrib/restricted/boost/iostreams to 1.81.0

--- contrib/restricted/boost/iostreams/include/boost/iostreams/filter/zlib.hpp
+++ contrib/restricted/boost/iostreams/include/boost/iostreams/filter/zlib.hpp
@@ -389,7 +389,8 @@ bool zlib_decompressor_impl<Alloc>::filter
     int result = xinflate(zlib::sync_flush);
     after(src_begin, dest_begin, false);
     zlib_error::check BOOST_PREVENT_MACRO_SUBSTITUTION(result);
-    return !(eof_ = result == zlib::stream_end);
+    eof_ = result == zlib::stream_end;
+    return !eof_;
 }
 
 template<typename Alloc>

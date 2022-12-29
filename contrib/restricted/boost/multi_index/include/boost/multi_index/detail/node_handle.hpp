088ae1ec99d2a8a45ad40fdf42f595d87c57c37a
robot-contrib <robot-contrib@yandex-team.com>
2022-12-29 10:01:51 +0300

    Update contrib/restricted/boost/multi_index to 1.81.0

--- contrib/restricted/boost/multi_index/include/boost/multi_index/detail/node_handle.hpp
+++ contrib/restricted/boost/multi_index/include/boost/multi_index/detail/node_handle.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2003-2021 Joaquin M Lopez Munoz.
+/* Copyright 2003-2022 Joaquin M Lopez Munoz.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
@@ -26,9 +26,9 @@
 #include <new>
 
 #if !defined(BOOST_NO_SFINAE)
+#include <boost/core/enable_if.hpp>
 #include <boost/type_traits/is_const.hpp>
 #include <boost/type_traits/is_same.hpp>
-#include <boost/utility/enable_if.hpp>
 #endif
 
 namespace boost{
--- contrib/restricted/boost/multi_index/include/boost/multi_index/member.hpp
+++ contrib/restricted/boost/multi_index/include/boost/multi_index/member.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2003-2015 Joaquin M Lopez Munoz.
+/* Copyright 2003-2022 Joaquin M Lopez Munoz.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
@@ -14,9 +14,9 @@
 #endif
 
 #include <boost/config.hpp> /* keep it first to prevent nasty warns in MSVC */
+#include <boost/core/enable_if.hpp>
 #include <boost/mpl/if.hpp>
 #include <boost/type_traits/is_const.hpp>
-#include <boost/utility/enable_if.hpp>
 #include <cstddef>
 
 #if !defined(BOOST_NO_SFINAE)

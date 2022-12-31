0ec27b614f46afcfa62cd3dc7e86bdd44b60f096
thegeorg <thegeorg@yandex-team.com>
2022-12-31 10:34:59 +0300

    Remove patch from boost/config

--- contrib/restricted/boost/config/include/boost/config/detail/select_compiler_config.hpp
+++ contrib/restricted/boost/config/include/boost/config/detail/select_compiler_config.hpp
@@ -39,7 +39,7 @@
 //  Intel
 #   define BOOST_COMPILER_CONFIG "boost/config/compiler/intel.hpp"
 
-#elif defined __clang__ && !defined(__ibmxl__) && !defined(__CODEGEARC__) && !defined(_MSC_VER)
+#elif defined __clang__ && !defined(__ibmxl__) && !defined(__CODEGEARC__)
 //  Clang C++ emulates GCC, so it has to appear early.
 #   define BOOST_COMPILER_CONFIG "boost/config/compiler/clang.hpp"
 

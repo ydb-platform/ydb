aac4f8462008395659ca657daf715ba68b41cedf
ivanmorozov <ivanmorozov@yandex-team.com>
2023-01-11 18:26:21 +0300

    parse objects synchronization with calc graph (parse immediately before using is useful for memory consumption and not affect performance)

--- ydb/library/yql/dq/runtime/dq_input_channel.cpp
+++ ydb/library/yql/dq/runtime/dq_input_channel.cpp
@@ -6,27 +6,10 @@ namespace NYql::NDq {
 class TDqInputChannel : public TDqInputImpl<TDqInputChannel, IDqInputChannel> {
     using TBaseImpl = TDqInputImpl<TDqInputChannel, IDqInputChannel>;
     friend TBaseImpl;
-public:
-    TDqInputChannel(ui64 channelId, NKikimr::NMiniKQL::TType* inputType, ui64 maxBufferBytes, bool collectProfileStats,
-        const NKikimr::NMiniKQL::TTypeEnvironment& typeEnv, const NKikimr::NMiniKQL::THolderFactory& holderFactory,
-        NDqProto::EDataTransportVersion transportVersion)
-        : TBaseImpl(inputType, maxBufferBytes)
-        , ChannelId(channelId)
-        , BasicStats(ChannelId)
-        , ProfileStats(collectProfileStats ? &BasicStats : nullptr)
-        , DataSerializer(typeEnv, holderFactory, transportVersion) {}
+private:
+    std::deque<NDqProto::TData> DataForDeserialize;
 
-    ui64 GetChannelId() const override {
-        return ChannelId;
-    }
-
-    void Push(NDqProto::TData&& data) override {
-        YQL_ENSURE(!Finished, "input channel " << ChannelId << " already finished");
-
-        if (Y_UNLIKELY(data.GetRows() == 0)) {
-            return;
-        }
-
+    void PushImpl(NDqProto::TData&& data) {
         const i64 space = data.GetRaw().size();
 
         NKikimr::NMiniKQL::TUnboxedValueVector buffer;
@@ -41,6 +24,44 @@ public:
         }
 
         AddBatch(std::move(buffer), space);
+    }
+
+public:
+    TDqInputChannel(ui64 channelId, NKikimr::NMiniKQL::TType* inputType, ui64 maxBufferBytes, bool collectProfileStats,
+        const NKikimr::NMiniKQL::TTypeEnvironment& typeEnv, const NKikimr::NMiniKQL::THolderFactory& holderFactory,
+        NDqProto::EDataTransportVersion transportVersion)
+        : TBaseImpl(inputType, maxBufferBytes)
+        , ChannelId(channelId)
+        , BasicStats(ChannelId)
+        , ProfileStats(collectProfileStats ? &BasicStats : nullptr)
+        , DataSerializer(typeEnv, holderFactory, transportVersion)
+    {}
+
+    ui64 GetChannelId() const override {
+        return ChannelId;
+    }
+
+    [[nodiscard]]
+    bool Pop(NKikimr::NMiniKQL::TUnboxedValueVector& batch) override {
+        if (Batches.empty()) {
+            if (DataForDeserialize.size()) {
+                PushImpl(std::move(DataForDeserialize.front()));
+                DataForDeserialize.pop_front();
+                return TBaseImpl::Pop(batch);
+            } else {
+                return false;
+            }
+        } else {
+            return TBaseImpl::Pop(batch);
+        }
+    }
+
+    void Push(NDqProto::TData&& data) override {
+        YQL_ENSURE(!Finished, "input channel " << ChannelId << " already finished");
+        if (Y_UNLIKELY(data.GetRows() == 0)) {
+            return;
+        }
+        DataForDeserialize.emplace_back(std::move(data));
     }
 
     const TDqInputChannelStats* GetStats() const override {

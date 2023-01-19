899159c396b4ab1dbc5a20f53a110fe8de786c28
ilnaz <ilnaz@ydb.tech>
2023-01-19 22:54:49 +0300

    (refactoring) Introduce TChangeRecordBodySerializer

--- ydb/core/tx/datashard/change_collector_base.h
+++ ydb/core/tx/datashard/change_collector_base.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "change_record.h"
+#include "change_record_body_serializer.h"
 
 #include <ydb/core/engine/minikql/change_collector_iface.h>
 #include <ydb/core/protos/change_exchange.pb.h>
@@ -19,21 +20,13 @@ public:
     virtual void SetGroup(ui64 group) = 0;
 };
 
-class TBaseChangeCollector: public IBaseChangeCollector {
+class TBaseChangeCollector
+    : public IBaseChangeCollector
+    , protected TChangeRecordBodySerializer
+{
     using TDataChange = NKikimrChangeExchange::TChangeRecord::TDataChange;
-    using TSerializedCells = TDataChange::TSerializedCells;
-
-    static void SerializeCells(TSerializedCells& out, TArrayRef<const TRawTypeValue> in, TArrayRef<const NTable::TTag> tags);
-    static void SerializeCells(TSerializedCells& out, TArrayRef<const NTable::TUpdateOp> in);
-    static void SerializeCells(TSerializedCells& out, const NTable::TRowState& state, TArrayRef<const NTable::TTag> tags);
 
 protected:
-    static void Serialize(TDataChange& out, NTable::ERowOp rop,
-        TArrayRef<const TRawTypeValue> key, TArrayRef<const NTable::TTag> keyTags, TArrayRef<const NTable::TUpdateOp> updates);
-    static void Serialize(TDataChange& out, NTable::ERowOp rop,
-        TArrayRef<const TRawTypeValue> key, TArrayRef<const NTable::TTag> keyTags,
-        const NTable::TRowState* oldState, const NTable::TRowState* newState, TArrayRef<const NTable::TTag> valueTags);
-
     void Persist(const TTableId& tableId, const TPathId& pathId, TChangeRecord::EKind kind, const TDataChange& body);
 
 public:

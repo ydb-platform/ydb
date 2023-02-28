38ec3bde944ec1d54c28a316a1b188106145fd27
valerybitsoev <valerybitsoev@yandex-team.com>
2023-02-28 12:17:32 +0300

    separate counters for each file

--- library/cpp/unified_agent_client/dynamic_counters_wrapper.h
+++ library/cpp/unified_agent_client/dynamic_counters_wrapper.h
@@ -10,6 +10,30 @@ namespace NUnifiedAgent {
         {
         }
 
+        NMonitoring::TDynamicCounters::TCounterPtr MakeCounterWithLabels(
+                const std::vector<std::pair<TString, TString>>& labels,
+                const char* name,
+                bool derivative
+                ) {
+            auto ptr = Unwrap();
+            for (const auto& p : labels) {
+                ptr = ptr->GetSubgroup(p.first, p.second);
+            }
+            return ptr->GetCounter(name, derivative);
+        }
+
+        NMonitoring::THistogramPtr MakeHistogramWithLabels(
+                const std::vector<std::pair<TString, TString>>& labels,
+                const char* name,
+                NMonitoring::IHistogramCollectorPtr hist
+                ) {
+            auto ptr = Unwrap();
+            for (const auto& p : labels) {
+                ptr = ptr->GetSubgroup(p.first, p.second);
+            }
+            return ptr->GetHistogram(name, std::move(hist));
+        }
+
         virtual ~TDynamicCountersWrapper() = default;
 
         const TIntrusivePtr<NMonitoring::TDynamicCounters>& Unwrap() const {

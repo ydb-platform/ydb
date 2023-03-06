ef1844705fb7c97205e18f8d635990225bb901a7
kovalad <kovalad@ydb.tech>
2023-03-06 12:16:42 +0300

    refresh ydb ui from github
    refresh ydb in arcadia

--- ydb/core/viewer/monitoring/static/js/81.6eded120.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpydb-embedded-ui"]=this["webpackJsonpydb-embedded-ui"]||[]).push([[81],{1165:function(e,d,s){"use strict";s.r(d),d.default=s.p+"static/media/403.6367e52f.svg"}}]);
-//# sourceMappingURL=81.6eded120.chunk.js.map

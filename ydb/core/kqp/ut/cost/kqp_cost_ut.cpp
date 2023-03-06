55060209757c994259968a68420de3126687fa38
dcherednik <dcherednik@ydb.tech>
2023-03-06 15:07:49 +0300

    Tests for read cost calculation.

---
+++ ydb/core/kqp/ut/cost/kqp_cost_ut.cpp
@@ -0,0 +1,144 @@
+#include <ydb/core/kqp/ut/common/kqp_ut_common.h>
+
+#include <ydb/core/kqp/counters/kqp_counters.h>
+#include <ydb/public/sdk/cpp/client/ydb_proto/accessor.h>
+
+#include <library/cpp/json/json_reader.h>
+
+namespace NKikimr {
+namespace NKqp {
+
+using namespace NYdb;
+using namespace NYdb::NTable;
+
+static NKikimrConfig::TAppConfig GetAppConfig(bool sourceRead) {
+    auto app = NKikimrConfig::TAppConfig();
+    app.MutableTableServiceConfig()->SetEnableKqpDataQuerySourceRead(sourceRead);
+    app.MutableTableServiceConfig()->SetEnableKqpScanQuerySourceRead(sourceRead);
+    return app;
+}
+
+static NYdb::NTable::TExecDataQuerySettings GetDataQuerySettings() {
+    NYdb::NTable::TExecDataQuerySettings execSettings;
+    execSettings.CollectQueryStats(ECollectQueryStatsMode::Basic);
+    return execSettings;
+}
+
+Y_UNIT_TEST_SUITE(KqpCost) {
+    Y_UNIT_TEST_TWIN(PointLookup, SourceRead) {
+        TKikimrRunner kikimr(GetAppConfig(SourceRead));
+        auto db = kikimr.GetTableClient();
+        auto session = db.CreateSession().GetValueSync().GetSession();
+
+        auto query = Q_(R"(
+            SELECT * FROM `/Root/Test` WHERE Group = 1u AND Name = "Anna";
+        )");
+
+        auto txControl = TTxControl::BeginTx().CommitTx();
+
+        auto result = session.ExecuteDataQuery(query, txControl, GetDataQuerySettings()).ExtractValueSync();
+        UNIT_ASSERT_VALUES_EQUAL(result.GetStatus(), EStatus::SUCCESS);
+
+        CompareYson(R"(
+            [
+                [[3500u];["None"];
+                [1u];["Anna"]]
+            ]
+        )", NYdb::FormatResultSetYson(result.GetResultSet(0)));
+
+        auto stats = NYdb::TProtoAccessor::GetProto(*result.GetStats());
+
+        UNIT_ASSERT_VALUES_EQUAL(stats.query_phases(0).table_access(0).reads().rows(), 1);
+        UNIT_ASSERT_VALUES_EQUAL(stats.query_phases(0).table_access(0).reads().bytes(), 20);
+    }
+
+    Y_UNIT_TEST_TWIN(Range, SourceRead) {
+        TKikimrRunner kikimr(GetAppConfig(SourceRead));
+        auto db = kikimr.GetTableClient();
+        auto session = db.CreateSession().GetValueSync().GetSession();
+
+        auto query = Q_(R"(
+            SELECT * FROM `/Root/Test` WHERE Group < 2u ORDER BY Group;
+        )");
+
+        auto txControl = TTxControl::BeginTx().CommitTx();
+
+        auto result = session.ExecuteDataQuery(query, txControl, GetDataQuerySettings()).ExtractValueSync();
+        UNIT_ASSERT_VALUES_EQUAL(result.GetStatus(), EStatus::SUCCESS);
+
+        CompareYson(R"(
+            [
+                [[3500u];["None"];[1u];["Anna"]];
+                [[300u];["None"];[1u];["Paul"]]
+            ]
+        )", NYdb::FormatResultSetYson(result.GetResultSet(0)));
+
+        auto stats = NYdb::TProtoAccessor::GetProto(*result.GetStats());
+
+        UNIT_ASSERT_VALUES_EQUAL(stats.query_phases(0).table_access(0).reads().rows(), 2);
+        UNIT_ASSERT_VALUES_EQUAL(stats.query_phases(0).table_access(0).reads().bytes(), 40);
+    }
+
+    Y_UNIT_TEST_TWIN(RangeFullScan, SourceRead) {
+        TKikimrRunner kikimr(GetAppConfig(SourceRead));
+
+        auto db = kikimr.GetTableClient();
+        auto session = db.CreateSession().GetValueSync().GetSession();
+
+        auto query = Q_(R"(
+            SELECT * FROM `/Root/Test` WHERE Amount < 5000ul ORDER BY Group LIMIT 1;
+        )");
+
+        auto txControl = TTxControl::BeginTx().CommitTx();
+
+        auto result = session.ExecuteDataQuery(query, txControl, GetDataQuerySettings()).ExtractValueSync();
+        UNIT_ASSERT_VALUES_EQUAL(result.GetStatus(), EStatus::SUCCESS);
+
+        CompareYson(R"(
+            [
+                [[3500u];["None"];[1u];["Anna"]]
+            ]
+        )", NYdb::FormatResultSetYson(result.GetResultSet(0)));
+
+        auto stats = NYdb::TProtoAccessor::GetProto(*result.GetStats());
+
+        Cerr << stats.DebugString() << Endl;
+        UNIT_ASSERT_VALUES_EQUAL(stats.query_phases(0).table_access(0).reads().rows(), 1);
+        UNIT_ASSERT_VALUES_EQUAL(stats.query_phases(0).table_access(0).reads().bytes(), 20);
+    }
+
+    Y_UNIT_TEST_TWIN(ScanQueryRangeFullScan, SourceRead) {
+        TKikimrRunner kikimr(GetAppConfig(SourceRead));
+
+        auto db = kikimr.GetTableClient();
+
+        auto query = Q_(R"(
+            SELECT * FROM `/Root/Test` WHERE Amount < 5000ul ORDER BY Group LIMIT 1;
+        )");
+
+        NYdb::NTable::TStreamExecScanQuerySettings execSettings;
+        execSettings.CollectQueryStats(ECollectQueryStatsMode::Basic);
+
+        auto it = db.StreamExecuteScanQuery(query, execSettings).ExtractValueSync();
+        UNIT_ASSERT_VALUES_EQUAL(it.GetStatus(), EStatus::SUCCESS);
+        auto res = CollectStreamResult(it);
+
+        CompareYson(R"(
+            [
+                [[3500u];["None"];[1u];["Anna"]]
+            ]
+        )", res.ResultSetYson);
+
+/*        const auto& stats = *res.QueryStats;
+
+        Cerr << stats.DebugString() << Endl;
+        UNIT_ASSERT_VALUES_EQUAL(stats.query_phases(0).table_access(0).reads().rows(), 1);
+        UNIT_ASSERT_VALUES_EQUAL(stats.query_phases(0).table_access(0).reads().bytes(), 20);
+*/
+    }
+
+
+}
+
+}
+}

5e0b4427e78b2c36babe490cd2b6dce5f1cc9b99
chertus <azuikov@ydb.tech>
2023-03-22 18:15:03 +0300

    hotfix

--- ydb/core/formats/arrow_helpers.cpp
+++ ydb/core/formats/arrow_helpers.cpp
@@ -492,7 +492,10 @@ std::vector<std::shared_ptr<arrow::RecordBatch>> MergeSortedBatches(const std::v
 
     auto mergeStream = std::make_shared<NArrow::TMergingSortedInputStream>(streams, description, maxBatchRows);
     while (std::shared_ptr<arrow::RecordBatch> batch = mergeStream->Read()) {
-        out.push_back(batch);
+        Y_VERIFY(batch);
+        if (batch->num_rows()) {
+            out.push_back(batch);
+        }
     }
     return out;
 }

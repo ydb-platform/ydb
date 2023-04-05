91b2e25f78a8c61578837d8cfa75acd1b427c4e8
stanly <stanly@yandex-team.com>
2023-04-05 16:33:13 +0300

    Improve TPredicate
    * move implementation details into cpp
    * reduce sizeof(TPredicate) from 32 bytes to 24 by ordering the fields

---
+++ ydb/core/tx/columnshard/engines/predicate.cpp
@@ -0,0 +1,68 @@
+#include "predicate.h"
+
+#include <ydb/core/formats/arrow_helpers.h>
+#include <ydb/core/formats/switch_type.h>
+
+namespace NKikimr::NOlap {
+
+TPredicate::TPredicate(EOperation op, std::shared_ptr<arrow::RecordBatch> batch, bool inclusive) noexcept
+    : Batch(std::move(batch))
+    , Operation(op)
+    , Inclusive(inclusive)
+{}
+
+TPredicate::TPredicate(EOperation op, const TString& serializedBatch, const std::shared_ptr<arrow::Schema>& schema, bool inclusive)
+    : Operation(op)
+    , Inclusive(inclusive)
+{
+    if (!serializedBatch.empty()) {
+        Batch = NArrow::DeserializeBatch(serializedBatch, schema);
+        Y_VERIFY(Batch);
+    }
+}
+
+TVector<TString> TPredicate::ColumnNames() const {
+    TVector<TString> out;
+    out.reserve(Batch->num_columns());
+    for (const auto& field : Batch->schema()->fields()) {
+        out.emplace_back(field->name());
+    }
+    return out;
+}
+
+IOutputStream& operator << (IOutputStream& out, const TPredicate& pred) {
+    out << NSsa::GetFunctionName(pred.Operation);
+    if (pred.Inclusive) {
+        out << "(incl) ";
+    } else {
+        out << "(excl) ";
+    }
+
+    for (i32 i = 0; i < pred.Batch->num_columns(); ++i) {
+        auto array = pred.Batch->column(i);
+        out << pred.Batch->schema()->field(i)->name() << ": ";
+        NArrow::SwitchType(array->type_id(), [&](const auto& type) {
+            using TWrap = std::decay_t<decltype(type)>;
+            using TArray = typename arrow::TypeTraits<typename TWrap::T>::ArrayType;
+
+            auto& typedArray = static_cast<const TArray&>(*array);
+            if (typedArray.IsNull(0)) {
+                out << "NULL";
+            } else {
+                auto value = typedArray.GetView(0);
+                using T = std::decay_t<decltype(value)>;
+                if constexpr (std::is_same_v<T, arrow::util::string_view>) {
+                    out << "'" << std::string_view(value.data(), value.size()) << "'";
+                } else {
+                    out << "'" << value << "'";
+                }
+            }
+            return true;
+        });
+        out << " ";
+    }
+
+    return out;
+}
+
+} // namespace NKikimr::NOlap

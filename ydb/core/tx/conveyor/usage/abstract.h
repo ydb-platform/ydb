0fbef0f9c08b91f3238020e5d13c6ad2c232e814
ivanmorozov <ivanmorozov@yandex-team.com>
2023-04-25 09:57:15 +0300

    error message into conveyor task

--- ydb/core/tx/conveyor/usage/abstract.h
+++ ydb/core/tx/conveyor/usage/abstract.h
@@ -1,17 +1,32 @@
 #pragma once
 #include <memory>
+#include <ydb/library/accessor/accessor.h>
+#include <util/generic/string.h>
 
 namespace NKikimr::NConveyor {
 
 class ITask {
+private:
+    TString ErrorMessage;
 protected:
+    ITask& SetErrorMessage(const TString& message) {
+        ErrorMessage = message;
+        return *this;
+    }
     virtual bool DoExecute() = 0;
 public:
     using TPtr = std::shared_ptr<ITask>;
     virtual ~ITask() = default;
-    bool Execute() {
-        return DoExecute();
+
+    bool HasError() const {
+        return !!ErrorMessage;
     }
+
+    const TString& GetErrorMessage() const {
+        return ErrorMessage;
+    }
+
+    bool Execute();
 };
 
 }

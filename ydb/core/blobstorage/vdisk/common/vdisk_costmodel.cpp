055a72ff7416f1104f1c2130c38e61397627c2be
Michael Stetsyuk <stetsyuk@yandex-team.ru>
2023-05-03 23:16:35 +0100

    more logging in ReadCostBySize

--- ydb/core/blobstorage/vdisk/common/vdisk_costmodel.cpp
+++ ydb/core/blobstorage/vdisk/common/vdisk_costmodel.cpp
@@ -85,6 +85,9 @@ namespace NKikimr {
 
     ui64 TCostModel::ReadCostBySize(ui64 size) const {
         Y_VERIFY(ReadBlockSize != 0);
+        std::cout << "[ReadCostBySize] " << "size = " << size << ", "
+                                         << "ReadBlockSize = " << ReadBlockSize << ", "
+                                         << "SeekTimeUs = " << SeekTimeUs << "\n";
         ui64 seekCost = (size / ReadBlockSize + 1) * SeekTimeUs;
         ui64 readCost = size * ui64(1000000000) / ReadSpeedBps;
         return seekCost + readCost;

1ae414d53159cbb50c7bc23d2a6f873a9dae99fa
dcherednik <dcherednik@ydb.tech>
2023-05-17 20:50:29 +0300

    Temporarily disable channel pool for asan sdk build to workaround leak at grpc client stop.

--- ydb/public/sdk/cpp/client/impl/ydb_internal/internal_header.h
+++ ydb/public/sdk/cpp/client/impl/ydb_internal/internal_header.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <util/system/compiler.h>
+
 #if !defined(INCLUDE_YDB_INTERNAL_H)
 #error "you are trying to use internal ydb header"
 #endif // INCLUDE_YDB_INTERNAL_H
@@ -9,3 +11,10 @@
 
 // This macro is used for grpc debug purpose only
 //#define YDB_GRPC_BYPASS_CHANNEL_POOL
+
+// gRpc issue temporal workaround.
+// In case of early TryCancel call and sharing shannel interfaces
+// grpc doesn`t free allocated memory at the client shutdown
+#if defined(_asan_enabled_)
+#define YDB_GRPC_BYPASS_CHANNEL_POOL
+#endif

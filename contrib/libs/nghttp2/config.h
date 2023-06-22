1b1d6eb18019d479b08ffa90e633d8f96440f43b
robot-contrib <robot-contrib@yandex-team.com>
2023-06-22 08:34:35 +0300

    Update contrib/libs/nghttp2 to 1.54.0

--- contrib/libs/nghttp2/config.h
+++ contrib/libs/nghttp2/config.h
@@ -244,7 +244,7 @@
 #define PACKAGE_NAME "nghttp2"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "nghttp2 1.53.0"
+#define PACKAGE_STRING "nghttp2 1.54.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "nghttp2"
@@ -253,7 +253,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.53.0"
+#define PACKAGE_VERSION "1.54.0"
 
 /* The size of `int *', as computed by sizeof. */
 #define SIZEOF_INT_P 8
@@ -358,7 +358,7 @@
 
 
 /* Version number of package */
-#define VERSION "1.53.0"
+#define VERSION "1.54.0"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */

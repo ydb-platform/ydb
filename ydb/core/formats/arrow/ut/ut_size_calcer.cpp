48ac40272844f8e875067d7e785b879304bdb9ce
ivanmorozov <ivanmorozov@yandex-team.com>
2023-06-29 19:55:01 +0300

    filler for timestamp and tests

---
+++ ydb/core/formats/arrow/ut/ut_size_calcer.cpp
@@ -0,0 +1,44 @@
+#include <library/cpp/testing/unittest/registar.h>
+#include <ydb/core/formats/arrow/arrow_helpers.h>
+#include <ydb/core/formats/arrow/serializer/batch_only.h>
+#include <ydb/core/formats/arrow/serializer/full.h>
+#include <ydb/core/formats/arrow/simple_builder/array.h>
+#include <ydb/core/formats/arrow/simple_builder/batch.h>
+#include <ydb/core/formats/arrow/simple_builder/filler.h>
+#include <ydb/core/formats/arrow/dictionary/conversion.h>
+
+Y_UNIT_TEST_SUITE(SizeCalcer) {
+
+    using namespace NKikimr::NArrow;
+
+    Y_UNIT_TEST(SimpleStrings) {
+        NConstruction::IArrayBuilder::TPtr column = std::make_shared<NConstruction::TSimpleArrayConstructor<NConstruction::TStringPoolFiller>>(
+            "field", NConstruction::TStringPoolFiller(1024, 512));
+        std::shared_ptr<arrow::RecordBatch> batch = NConstruction::TRecordBatchConstructor({ column }).BuildBatch(2048);
+        Cerr << GetBatchDataSize(batch) << Endl;
+        UNIT_ASSERT(GetBatchDataSize(batch) == 2048 * 512);
+    }
+
+    Y_UNIT_TEST(DictionaryStrings) {
+        NConstruction::IArrayBuilder::TPtr column = std::make_shared<NConstruction::TDictionaryArrayConstructor<NConstruction::TStringPoolFiller>>(
+            "field", NConstruction::TStringPoolFiller(1024, 512));
+        std::shared_ptr<arrow::RecordBatch> batch = NConstruction::TRecordBatchConstructor({ column }).BuildBatch(2048);
+        Cerr << GetBatchDataSize(batch) << Endl;
+        UNIT_ASSERT(GetBatchDataSize(batch) == 2048 * 512);
+    }
+
+    Y_UNIT_TEST(SimpleInt64) {
+        NConstruction::IArrayBuilder::TPtr column = std::make_shared<NConstruction::TSimpleArrayConstructor<NConstruction::TIntSeqFiller<arrow::Int64Type>>>("field");
+        std::shared_ptr<arrow::RecordBatch> batch = NConstruction::TRecordBatchConstructor({ column }).BuildBatch(2048);
+        Cerr << GetBatchDataSize(batch) << Endl;
+        UNIT_ASSERT(GetBatchDataSize(batch) == 2048 * 8);
+    }
+
+    Y_UNIT_TEST(SimpleTimestamp) {
+        NConstruction::IArrayBuilder::TPtr column = std::make_shared<NConstruction::TSimpleArrayConstructor<NConstruction::TIntSeqFiller<arrow::TimestampType>>>("field");
+        std::shared_ptr<arrow::RecordBatch> batch = NConstruction::TRecordBatchConstructor({ column }).BuildBatch(2048);
+        Cerr << GetBatchDataSize(batch) << Endl;
+        UNIT_ASSERT(GetBatchDataSize(batch) == 2048 * 8);
+    }
+
+};

0a78431e3d51505091495c6a27559b1120f79b98
qrort <qrort@yandex-team.com>
2023-07-04 13:58:22 +0300

    EnableNotNullDataColumns = true

--- ydb/services/ydb/ydb_bulk_upsert_ut.cpp
+++ ydb/services/ydb/ydb_bulk_upsert_ut.cpp
@@ -230,17 +230,14 @@ Y_UNIT_TEST_SUITE(YdbTableBulkUpsert) {
 
         TString tableName = "/Root/TestNotNullColumns";
 
-        {  /* create table with disabled not null data column */
+        {  /* create table with not null data column */
             auto tableBuilder = client.GetTableBuilder();
             tableBuilder
                     .AddNonNullableColumn("Key", EPrimitiveType::Uint64)
                     .AddNonNullableColumn("Value", EPrimitiveType::Uint64)
                 .SetPrimaryKeyColumns({"Key"});
             auto result = session.CreateTable(tableName, tableBuilder.Build()).ExtractValueSync();
-
-            Cerr << result.GetIssues().ToString() << Endl;
-            UNIT_ASSERT_EQUAL(result.IsTransportError(), false);
-            UNIT_ASSERT_EQUAL(result.GetStatus(), EStatus::PRECONDITION_FAILED);
+            UNIT_ASSERT_C(result.IsSuccess(), result.GetIssues().ToString());
         }
 
         {  /* create table with not null primary key column */

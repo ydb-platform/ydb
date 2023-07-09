9c5e432f6a6547d1ce4f317e3bd69134c6cfc221
Nikita Kozlovskiy <nikitka@gmail.com>
2023-07-09 18:42:34 +0200

    ci: prewarm: fix C_FLAGS

--- .github/prewarm/build.sh
+++ .github/prewarm/build.sh
@@ -13,8 +13,8 @@ echo "::group::cmake"
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_C_COMPILER_LAUNCHER=/usr/local/bin/ccache -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/local/bin/ccache \
 -DCMAKE_TOOLCHAIN_FILE=../ydb/clang.toolchain \
--DCMAKE_C_FLAGS_RELWITHDEBINFO="-O2 -UNDEBUG" \
--DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 -UNDEBUG" \
+-DCMAKE_C_FLAGS_RELEASE="-O2 -UNDEBUG" \
+-DCMAKE_CXX_FLAGS_RELEASE="-O2 -UNDEBUG" \
 ../ydb
 echo "::endgroup::"
 

a66bf06ecf75e6e40f231df118da29ca31f85612
hcpp <hcpp@ydb.tech>
2023-07-20 20:39:51 +0300

    syntax has been fixed

--- ydb/core/viewer/json_query.h
+++ ydb/core/viewer/json_query.h
@@ -148,7 +148,7 @@ public:
         }
         if (Syntax == "yql_v1") {
             request.SetSyntax(Ydb::Query::Syntax::SYNTAX_YQL_V1);
-        } else if (Syntax = "pg") {
+        } else if (Syntax == "pg") {
             request.SetSyntax(Ydb::Query::Syntax::SYNTAX_PG);
         }
         ActorIdToProto(SelfId(), event->Record.MutableRequestActorId());

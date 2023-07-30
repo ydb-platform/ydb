6a255711240ea5207dbf4c50863ab29980490b7d
azevaykin <azevaykin@yandex-team.com>
2023-07-30 09:17:50 +0300

    Reduce logs

--- ydb/public/sdk/cpp/client/ydb_persqueue_core/ut/ut_utils/test_server.cpp
+++ ydb/public/sdk/cpp/client/ydb_persqueue_core/ut/ut_utils/test_server.cpp
@@ -5,10 +5,8 @@ namespace NPersQueue {
 const TVector<NKikimrServices::EServiceKikimr> TTestServer::LOGGED_SERVICES = {
     NKikimrServices::PQ_READ_PROXY,
     NKikimrServices::PQ_WRITE_PROXY,
-    NKikimrServices::PQ_MIRRORER,
-    NKikimrServices::PQ_METACACHE,
     NKikimrServices::PERSQUEUE,
-    NKikimrServices::PERSQUEUE_CLUSTER_TRACKER
+    NKikimrServices::PERSQUEUE_READ_BALANCER
 };
 
 } // namespace NPersQueue

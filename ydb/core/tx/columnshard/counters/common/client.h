fd7c47251710d4b4ada857f26ac35460e65c3422
ivanmorozov <ivanmorozov@yandex-team.com>
2023-08-11 10:33:36 +0300

    fix thread sanitizer panic

--- ydb/core/tx/columnshard/counters/common/client.h
+++ ydb/core/tx/columnshard/counters/common/client.h
@@ -4,14 +4,27 @@
 #include <util/generic/noncopyable.h>
 #include <list>
 #include <memory>
+#include <atomic>
 
 namespace NKikimr::NColumnShard {
 class TValueAggregationAgent;
 
 class TValueAggregationClient: TNonCopyable {
 private:
-    YDB_ACCESSOR(i64, Value, 0);
+    std::atomic<i64> Value;
 public:
+    TValueAggregationClient() {
+        Value = 0;
+    }
+
+    i64 GetValue() const {
+        return Value;
+    }
+
+    void SetValue(const i64 value) {
+        Value = value;
+    }
+
     void Add(const i64 v) {
         Value += v;
     }

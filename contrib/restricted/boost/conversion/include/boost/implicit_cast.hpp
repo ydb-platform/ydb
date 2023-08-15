f364ccb4b0a2bff6936436eb8a9e3b1e8cd4d4dd
robot-contrib <robot-contrib@yandex-team.com>
2023-08-15 13:46:54 +0300

    Update contrib/restricted/boost/conversion to 1.83.0

--- contrib/restricted/boost/conversion/include/boost/implicit_cast.hpp
+++ contrib/restricted/boost/conversion/include/boost/implicit_cast.hpp
@@ -11,6 +11,24 @@
 # pragma once
 #endif
 
+#include <boost/config/pragma_message.hpp>
+#if defined(BOOST_NO_CXX11_RVALUE_REFERENCES) || \
+    defined(BOOST_NO_CXX11_AUTO_DECLARATIONS) || \
+    defined(BOOST_NO_CXX11_CONSTEXPR) || \
+    defined(BOOST_NO_CXX11_NULLPTR) || \
+    defined(BOOST_NO_CXX11_NOEXCEPT) || \
+    defined(BOOST_NO_CXX11_DEFAULTED_FUNCTIONS) || \
+    defined(BOOST_NO_CXX11_FINAL) || \
+    defined(BOOST_NO_CXX11_ALIGNOF) || \
+    defined(BOOST_NO_CXX11_STATIC_ASSERT) || \
+    defined(BOOST_NO_CXX11_SMART_PTR) || \
+    defined(BOOST_NO_CXX11_HDR_INITIALIZER_LIST) || \
+    defined(BOOST_NO_CXX11_HDR_TYPE_TRAITS)
+
+BOOST_PRAGMA_MESSAGE("C++03 support is deprecated in Boost.Conversion 1.82 and will be removed in Boost.Conversion 1.84.")
+
+#endif
+
 namespace boost {
 
 namespace detail {

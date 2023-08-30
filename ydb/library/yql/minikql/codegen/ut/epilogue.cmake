cb3da9494c53283f0230ad37e4e8d0ea61b7d8fc
svidyuk <svidyuk@yandex-team.com>
2023-08-30 12:12:06 +0300

    All .ll files support in LLVM_BC

--- ydb/library/yql/minikql/codegen/ut/epilogue.cmake
+++
@@ -1,7 +0,0 @@
-llvm_compile_cxx(ydb-library-yql-minikql-codegen-ut
-  ${CMAKE_SOURCE_DIR}/ydb/library/yql/minikql/codegen/ut/128_bit.ll
-  ${CMAKE_BINARY_DIR}/ydb/library/yql/minikql/codegen/ut/128_bit.ll.bc
-  ${CLANGPLUSPLUS}
-  -Wno-unknown-warning-option
-  -emit-llvm
-)

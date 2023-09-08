49707605aa95a86c0e0fde0cb2166241afb0324a
snermolaev <snermolaev@yandex-team.com>
2023-09-08 13:30:05 +0300

    fix use of --cxx-compiler flag after rXXXXXX

--- build/ymake_conf.py
+++ build/ymake_conf.py
@@ -1199,7 +1199,8 @@ class GnuToolchain(Toolchain):
             ])
 
         if self.tc.is_clang:
-            self.c_flags_platform.append('-isystem{}/share/include'.format(self.tc.name_marker))
+            if not self.tc.is_system_cxx:
+                self.c_flags_platform.append('-isystem{}/share/include'.format(self.tc.name_marker))
             target_triple = self.tc.triplet_opt.get(target.arch, None)
             if not target_triple:
                 target_triple = select(default=None, selectors=[

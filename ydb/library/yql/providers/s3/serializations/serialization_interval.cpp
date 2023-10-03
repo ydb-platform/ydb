a14e6df4ad207375e3b08b57c0355f94bca2532f
uzhas <uzhas@ydb.tech>
2023-10-03 19:08:33 +0300

    fix compilation for win

--- ydb/library/yql/providers/s3/serializations/serialization_interval.cpp
+++ ydb/library/yql/providers/s3/serializations/serialization_interval.cpp
@@ -1,5 +1,9 @@
 #include "serialization_interval.h"
 
+#include <ydb/library/yql/udfs/common/clickhouse/client/src/DataTypes/DataTypeFactory.h>
+#include <ydb/library/yql/udfs/common/clickhouse/client/src/DataTypes/DataTypeInterval.h>
+#include <ydb/library/yql/udfs/common/clickhouse/client/src/DataTypes/Serializations/SerializationNumber.h>
+
 #include <ydb/library/yql/udfs/common/clickhouse/client/src/IO/ReadHelpers.h>
 #include <ydb/library/yql/udfs/common/clickhouse/client/src/IO/WriteHelpers.h>
 
@@ -36,87 +40,88 @@ TSerializationInterval::EUnit TSerializationInterval::ToUnit(const TString& unit
     return EUnit::MICROSECONDS;
 }
 
-TSerializationInterval::TSerializationInterval(EUnit unit)
-    : Multiplier(ToAvgMicroSeconds(unit))
-{}
+class TSerializationIntervalImpl : public SerializationNumber<Int64> {
+public:
+    using Base = NDB::SerializationNumber<Int64>;
 
-void TSerializationInterval::serializeText(const IColumn & column, size_t rowNum, WriteBuffer & ostr, const FormatSettings &) const
-{
-    writeText(assert_cast<const ColumnVector<Int64> &>(column).getData()[rowNum] / Multiplier , ostr);
-}
-
-void TSerializationInterval::deserializeText(IColumn & column, ReadBuffer & istr, const FormatSettings & formatSettings) const
-{
-    Base::deserializeText(column, istr, formatSettings);
-    assert_cast<ColumnVector<Int64> &>(column).getData().back() *= Multiplier;
-}
-
-void TSerializationInterval::serializeTextJSON(const IColumn & column, size_t rowNum, WriteBuffer & ostr, const FormatSettings & settings) const
-{
-    auto x = assert_cast<const ColumnVector<Int64> &>(column).getData()[rowNum] / Multiplier;
-    writeJSONNumber(x, ostr, settings);
-}
-
-void TSerializationInterval::deserializeTextJSON(IColumn & column, ReadBuffer & istr, const FormatSettings & formatSettings) const
-{
-    Base::deserializeTextJSON(column, istr, formatSettings);
-    assert_cast<ColumnVector<Int64> &>(column).getData().back() *= Multiplier;
-}
-
-void TSerializationInterval::deserializeTextCSV(IColumn & column, ReadBuffer & istr, const FormatSettings & formatSettings) const
-{
-    Base::deserializeTextCSV(column, istr, formatSettings);
-    assert_cast<ColumnVector<Int64> &>(column).getData().back() *= Multiplier;
-}
-
-void TSerializationInterval::serializeBinary(const Field & field, WriteBuffer & ostr) const
-{
-    /// ColumnVector<T>::ValueType is a narrower type. For example, UInt8, when the Field type is UInt64
-    typename ColumnVector<Int64>::ValueType x = get<FieldType>(field) / Multiplier;
-    writeBinary(x, ostr);
-}
-
-void TSerializationInterval::deserializeBinary(Field & field, ReadBuffer & istr) const
-{
-    Base::deserializeBinary(field, istr);
-    field.get<Int64>() *= Multiplier;
-}
-
-void TSerializationInterval::serializeBinary(const IColumn & column, size_t rowNum, WriteBuffer & ostr) const
-{
-    writeBinary(assert_cast<const ColumnVector<Int64> &>(column).getData()[rowNum] / Multiplier, ostr);
-}
-
-void TSerializationInterval::deserializeBinary(IColumn & column, ReadBuffer & istr) const
-{
-    Base::deserializeBinary(column, istr);
-    assert_cast<ColumnVector<Int64> &>(column).getData().back() *= Multiplier;
-}
-
-void TSerializationInterval::serializeBinaryBulk(const IColumn & column, WriteBuffer & ostr, size_t offset, size_t limit) const
-{
-    const typename ColumnVector<NDB::Int64>::Container & x = typeid_cast<const ColumnVector<NDB::Int64> &>(column).getData();
-    size_t size = x.size();
-
-    if (limit == 0 || offset + limit > size)
-        limit = size - offset;
-
-    for (size_t i = offset; i < limit; i++) {
-        serializeBinary(column, i, ostr);
+public:
+    TSerializationIntervalImpl(TSerializationInterval::EUnit unit)
+      : Multiplier(ToAvgMicroSeconds(unit))
+    {
     }
-}
-
-void TSerializationInterval::deserializeBinaryBulk(IColumn & column, ReadBuffer & istr, size_t limit, double avgValueSizeHint) const
-{
-    Base::deserializeBinaryBulk(column, istr, limit, avgValueSizeHint);
-    ColumnVector<Int64>::Container & x = typeid_cast<ColumnVector<Int64> &>(column).getData();
-    for (size_t i = x.size() - limit; i < x.size(); i++) {
-        assert_cast<ColumnVector<Int64> &>(column).getData()[i] *= Multiplier;
+    
+    void serializeText(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings &) const override {
+        writeText(assert_cast<const ColumnVector<Int64> &>(column).getData()[row_num] / Multiplier , ostr);
     }
-}
+
+    void deserializeText(IColumn & column, ReadBuffer & istr, const FormatSettings & settings) const override {
+        Base::deserializeText(column, istr, settings);
+        assert_cast<ColumnVector<Int64> &>(column).getData().back() *= Multiplier;
+    }
+
+    void serializeTextJSON(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & settings) const override {
+        auto x = assert_cast<const ColumnVector<Int64> &>(column).getData()[row_num] / Multiplier;
+        writeJSONNumber(x, ostr, settings);
+    }
+
+    void deserializeTextJSON(IColumn & column, ReadBuffer & istr, const FormatSettings & settings) const override {
+        Base::deserializeTextJSON(column, istr, settings);
+        assert_cast<ColumnVector<Int64> &>(column).getData().back() *= Multiplier;
+    }
+
+    void deserializeTextCSV(IColumn & column, ReadBuffer & istr, const FormatSettings & settings) const override {
+        Base::deserializeTextCSV(column, istr, settings);
+        assert_cast<ColumnVector<Int64> &>(column).getData().back() *= Multiplier;
+    }
+
+    /** Format is platform-dependent. */
+    void serializeBinary(const Field & field, WriteBuffer & ostr) const override {
+        /// ColumnVector<T>::ValueType is a narrower type. For example, UInt8, when the Field type is UInt64
+        typename ColumnVector<Int64>::ValueType x = get<FieldType>(field) / Multiplier;
+        writeBinary(x, ostr);
+    }
+
+    void deserializeBinary(Field & field, ReadBuffer & istr) const override {
+        Base::deserializeBinary(field, istr);
+        field.get<Int64>() *= Multiplier;
+    }
+
+    void serializeBinary(const IColumn & column, size_t row_num, WriteBuffer & ostr) const override {
+        writeBinary(assert_cast<const ColumnVector<Int64> &>(column).getData()[row_num] / Multiplier, ostr);
+    }
+
+    void deserializeBinary(IColumn & column, ReadBuffer & istr) const override {
+        Base::deserializeBinary(column, istr);
+        assert_cast<ColumnVector<Int64> &>(column).getData().back() *= Multiplier;
+    }
+
+    void serializeBinaryBulk(const IColumn & column, WriteBuffer & ostr, size_t offset, size_t limit) const override {
+        const typename ColumnVector<NDB::Int64>::Container & x = typeid_cast<const ColumnVector<NDB::Int64> &>(column).getData();
+        size_t size = x.size();
+
+        if (limit == 0 || offset + limit > size) {
+            limit = size - offset;
+        }
+
+        for (size_t i = offset; i < limit; i++) {
+            serializeBinary(column, i, ostr);
+        }
+    }
+
+    void deserializeBinaryBulk(IColumn & column, ReadBuffer & istr, size_t limit, double avg_value_size_hint) const override {
+        Base::deserializeBinaryBulk(column, istr, limit, avg_value_size_hint);
+        ColumnVector<Int64>::Container & x = typeid_cast<ColumnVector<Int64> &>(column).getData();
+        for (size_t i = x.size() - limit; i < x.size(); i++) {
+            assert_cast<ColumnVector<Int64> &>(column).getData()[i] *= Multiplier;
+        }
+    }
+
+private:
+    const Int64 Multiplier;
+};
 
 NDB::DataTypePtr GetInterval(TSerializationInterval::EUnit unit) {
-    auto customDescr = std::make_unique<NDB::DataTypeCustomDesc>(std::make_unique<NDB::DataTypeCustomFixedName>("IntervalSecond"), std::make_shared<TSerializationInterval>(unit));
+    auto customDescr = std::make_unique<NDB::DataTypeCustomDesc>(std::make_unique<NDB::DataTypeCustomFixedName>("IntervalSecond"), std::make_shared<TSerializationIntervalImpl>(unit));
     return NDB::DataTypeFactory::instance().getCustom(std::move(customDescr));
 }
 
--- ydb/library/yql/providers/s3/serializations/ya.make
+++ ydb/library/yql/providers/s3/serializations/ya.make
@@ -1,10 +1,14 @@
 LIBRARY()
 
+IF (CLANG AND NOT WITH_VALGRIND)
+
 SRCS(
     serialization_interval.cpp
 )
 
 PEERDIR(
+    contrib/libs/fmt
+    contrib/libs/poco/Util
     ydb/library/yql/udfs/common/clickhouse/client
 )
 
@@ -14,6 +18,12 @@ ADDINCL(
     ydb/library/yql/udfs/common/clickhouse/client/src
 )
 
+CFLAGS (
+    -DARCADIA_BUILD -DUSE_PARQUET
+)
+
 GENERATE_ENUM_SERIALIZATION(serialization_interval.h)
+
+ENDIF()
 
 END()

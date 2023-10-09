e89dbe374dee3933e0bc172b8cf61d878a9e1ea9
ivanmorozov <ivanmorozov@yandex-team.com>
2023-10-09 13:06:51 +0300

    fix build

--- ydb/core/blobstorage/vdisk/skeleton/blobstorage_monactors.h
+++ ydb/core/blobstorage/vdisk/skeleton/blobstorage_monactors.h
@@ -4,6 +4,7 @@
 #include <ydb/core/blobstorage/vdisk/common/vdisk_events.h>
 #include <ydb/core/blobstorage/groupinfo/blobstorage_groupinfo.h>
 #include <ydb/core/protos/node_whiteboard.pb.h>
+#include <ydb/core/blobstorage/vdisk/common/vdisk_mongroups.h>
 
 namespace NKikimr {
 

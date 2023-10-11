1d645bbaca687855582cc1808b4f4508dfc6ba3f
robot-contrib <robot-contrib@yandex-team.com>
2023-10-11 13:22:30 +0300

    Update contrib/python/s3transfer/py3 to 0.7.0

--- contrib/python/s3transfer/py3/s3transfer/__init__.py
+++ contrib/python/s3transfer/py3/s3transfer/__init__.py
@@ -144,7 +144,7 @@ import s3transfer.compat
 from s3transfer.exceptions import RetriesExceededError, S3UploadFailedError
 
 __author__ = 'Amazon Web Services'
-__version__ = '0.6.2'
+__version__ = '0.7.0'
 
 
 class NullHandler(logging.Handler):

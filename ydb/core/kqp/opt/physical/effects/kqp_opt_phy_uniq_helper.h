2d0cc96c4598cc167b92b483da910a511a2c13c0
Daniil Cherednik <dcherednik@ydb.tech>
2023-10-20 13:20:56 +0000

    Fix update in case of uniq constraint. We must allow to update value on the previous one.

--- ydb/core/kqp/opt/physical/effects/kqp_opt_phy_uniq_helper.h
+++ ydb/core/kqp/opt/physical/effects/kqp_opt_phy_uniq_helper.h
@@ -11,6 +11,8 @@ namespace NKikimr::NKqp::NOpt {
 
 class TUniqBuildHelper {
 public:
+    // table - metadata of table
+    // skipPkCheck - false for insert mode, generate check on PK to issue an arror on PK conflict
     TUniqBuildHelper(const NYql::TKikimrTableDescription& table, NYql::TPositionHandle pos,
         NYql::TExprContext& ctx, bool skipPkCheck);
     size_t GetChecksNum() const;
@@ -34,14 +36,39 @@ private:
         TIndexId IndexId = NOT_INDEX_ID;
     };
 
+    class TChecks {
+        public:
+            TChecks(std::pair<TVector<TUniqCheckNodes>, NYql::TExprNode::TPtr>&& pair)
+                : Checks(std::move(pair.first))
+                , PkDict(pair.second)
+            {}
+
+            size_t Size() const {
+                return Checks.size();
+            }
+
+            const TUniqCheckNodes& operator [](size_t i) const {
+                return Checks[i];
+            }
+
+            const NYql::TExprNode::TPtr GetPkDict() const {
+                return PkDict;
+            }
+        private:
+            const TVector<TUniqCheckNodes> Checks;
+            const NYql::TExprNode::TPtr PkDict;
+    };
+
+    size_t CalcComputeKeysStageOutputNum() const;
+
     static TUniqCheckNodes MakeUniqCheckNodes(const NYql::NNodes::TCoLambda& selector,
         const NYql::NNodes::TExprBase& rowsListArg, NYql::TPositionHandle pos, NYql::TExprContext& ctx);
-    static TVector<TUniqCheckNodes> Prepare(const NYql::NNodes::TCoArgument& rowsListArg,
+    static std::pair<TVector<TUniqCheckNodes>, NYql::TExprNode::TPtr> Prepare(const NYql::NNodes::TCoArgument& rowsListArg,
         const NYql::TKikimrTableDescription& table, NYql::TPositionHandle pos, NYql::TExprContext& ctx,
         bool skipPkCheck);
 
     NYql::NNodes::TCoArgument RowsListArg;
-    TVector<TUniqCheckNodes> Checks;
+    const TChecks Checks;
 };
 
 }

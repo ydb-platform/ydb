0d3b0812c3aeca9e23e18b28b5586863e82eb1ae
Kirill Grachev <kirill.grachoff@gmail.com>
2023-10-26 18:09:48 +0000

    Implement correct benchmark

--- ydb/core/blobstorage/vdisk/common/benchmark/proto/main.cpp
+++ ydb/core/blobstorage/vdisk/common/benchmark/proto/main.cpp
@@ -1,5 +1,9 @@
+#include "library/cpp/actors/core/event_pb.h"
+#include "library/cpp/testing/unittest/registar.h"
+#include "util/system/compiler.h"
 #include <cstring>
 
+#include <library/cpp/actors/core/event.h>
 #include <library/cpp/testing/benchmark/bench.h>
 
 #include <util/generic/yexception.h>
@@ -7,6 +11,7 @@
 #include <util/stream/output.h>
 #include <util/system/yassert.h>
 
+#include <ydb/core/base/blobstorage.h>
 #include <ydb/core/blobstorage/vdisk/common/vdisk_events.h>
 #include <ydb/core/protos/blobstorage.pb.h>
 
@@ -58,28 +63,101 @@ void SerDeLoop(Event& ev, Buffer<bytes>& buffer, size_t iterations, size_t read_
     }
 }
 
+const char* storeString32 =
+"................................"
+;
+
+const char* storeString64 =
+"................................"
+"................................"
+;
+
+const char* storeString128 =
+"................................"
+"................................"
+"................................"
+"................................"
+;
+
+const char* storeString256 =
+"................................"
+"................................"
+"................................"
+"................................"
+"................................"
+"................................"
+"................................"
+"................................"
+;
+
+template <std::derived_from<NActors::IEventBase> Event>
+void SerDeLoopIteration(const char* payload, char* buffer, size_t buffer_len, size_t iterations) {
+    Event ev;
+    ev.StorePayload(TRope(payload));
+    NActors::TCoroutineChunkSerializer serializer;
+    for (size_t i = 0; i < iterations; ++i) {
+        serializer.SetSerializingEvent(&ev);
+        while (!serializer.IsComplete()) {
+            auto chunk = serializer.FeedBuf(buffer, buffer_len);
+            for (auto v : chunk) {
+                Y_UNUSED(v);
+            }
+        }
+    }
+}
+
 } // namespace
 
-Y_CPU_BENCHMARK(Put_1_1, info) {
-    NKikimrBlobStorage::TEvVPut evPut;
-    Buffer<2048> buf;
-    SerDeLoop(evPut, buf, info.Iterations(), 1);
+Y_CPU_BENCHMARK(Put_32, info) {
+    char buffer[512];
+    SerDeLoopIteration<NKikimr::TEvBlobStorage::TEvVPut>(storeString32, buffer, sizeof(buffer), info.Iterations());
 }
 
-Y_CPU_BENCHMARK(Get_1_1, info) {
-    NKikimrBlobStorage::TEvVGet evGet;
-    Buffer<2048> buf;
-    SerDeLoop(evGet, buf, info.Iterations(), 1);
+Y_CPU_BENCHMARK(Put_64, info) {
+    char buffer[512];
+    SerDeLoopIteration<NKikimr::TEvBlobStorage::TEvVPut>(storeString64, buffer, sizeof(buffer), info.Iterations());
 }
 
-Y_CPU_BENCHMARK(Put_1_3, info) {
-    NKikimrBlobStorage::TEvVPut evPut;
-    Buffer<2048> buf;
-    SerDeLoop(evPut, buf, info.Iterations(), 3);
+Y_CPU_BENCHMARK(Put_128, info) {
+    char buffer[512];
+    SerDeLoopIteration<NKikimr::TEvBlobStorage::TEvVPut>(storeString128, buffer, sizeof(buffer), info.Iterations());
 }
 
-Y_CPU_BENCHMARK(Get_1_3, info) {
-    NKikimrBlobStorage::TEvVGet evGet;
-    Buffer<2048> buf;
-    SerDeLoop(evGet, buf, info.Iterations(), 3);
+Y_CPU_BENCHMARK(Put_256, info) {
+    char buffer[512];
+    SerDeLoopIteration<NKikimr::TEvBlobStorage::TEvVPut>(storeString256, buffer, sizeof(buffer), info.Iterations());
 }
+
+// Y_CPU_BENCHMARK(Get_32, info) {
+//     SerDeLoopIteration<NKikimr::TEvBlobStorage::TEvVGet>(storeString32, info.Iterations());
+// }
+
+// Y_CPU_BENCHMARK(Get_64, info) {
+//     SerDeLoopIteration<NKikimr::TEvBlobStorage::TEvVGet>(storeString64, info.Iterations());
+// }
+
+// Y_CPU_BENCHMARK(Get_128, info) {
+//     SerDeLoopIteration<NKikimr::TEvBlobStorage::TEvVGet>(storeString128, info.Iterations());
+// }
+
+// Y_CPU_BENCHMARK(Get_256, info) {
+//     SerDeLoopIteration<NKikimr::TEvBlobStorage::TEvVGet>(storeString256, info.Iterations());
+// }
+
+// Y_CPU_BENCHMARK(Get_1_1, info) {
+//     NKikimrBlobStorage::TEvVGet evGet;
+//     Buffer<2048> buf;
+//     SerDeLoop(evGet, buf, info.Iterations(), 1);
+// }
+
+// Y_CPU_BENCHMARK(Put_1_3, info) {
+//     NKikimrBlobStorage::TEvVPut evPut;
+//     Buffer<2048> buf;
+//     SerDeLoop(evPut, buf, info.Iterations(), 3);
+// }
+
+// Y_CPU_BENCHMARK(Get_1_3, info) {
+//     NKikimrBlobStorage::TEvVGet evGet;
+//     Buffer<2048> buf;
+//     SerDeLoop(evGet, buf, info.Iterations(), 3);
+// }

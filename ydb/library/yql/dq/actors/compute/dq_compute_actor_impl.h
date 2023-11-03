1178192f5cd2d8de16c71acdea284cb42327165a
qrort <qrort@yandex-team.com>
2023-11-03 18:04:59 +0300

    KIKIMR-19392: acquire pgcontext in most recent alloc, disable mem check in ComputeActor after InternalError

--- ydb/library/yql/dq/actors/compute/dq_compute_actor_impl.h
+++ ydb/library/yql/dq/actors/compute/dq_compute_actor_impl.h
@@ -625,7 +625,8 @@ protected:
 
     void InternalError(NYql::NDqProto::StatusIds::StatusCode statusCode, TIssues issues) {
         CA_LOG_E(InternalErrorLogString(statusCode, issues));
-
+        TaskRunner->GetAllocatorPtr()->InvalidateMemInfo();
+        TaskRunner->GetAllocatorPtr()->DisableStrictAllocationCheck();
         std::optional<TGuard<NKikimr::NMiniKQL::TScopedAlloc>> guard = MaybeBindAllocator();
         State = NDqProto::COMPUTE_STATE_FAILURE;
         ReportStateAndMaybeDie(statusCode, issues);

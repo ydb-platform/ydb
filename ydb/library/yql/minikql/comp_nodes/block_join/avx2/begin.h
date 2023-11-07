111e8ac04cf0d6593952ecb3cd74bc73cf4d19f8
alexv-smirnov <alex@ydb.tech>
2023-11-07 20:55:15 +0300

    Change "config.yaml"

---
+++ ydb/library/yql/minikql/comp_nodes/block_join/avx2/begin.h
@@ -0,0 +1,3 @@
+#pragma clang attribute push(__attribute__((target("avx2"))), apply_to=function)
+
+#include "simd.h"
---
+++ ydb/library/yql/minikql/comp_nodes/block_join/sse42/begin.h
@@ -0,0 +1,3 @@
+#pragma clang attribute push(__attribute__((target("sse4.2"))), apply_to=function)
+
+#include "simd.h"

2acc7a8b4af543c57c503ac39a15e44e5134305f
robot-contrib <robot-contrib@yandex-team.com>
2023-11-21 10:06:43 +0300

    Update contrib/python/botocore/py3 to 1.31.79

--- contrib/python/botocore/py3/botocore/__init__.py
+++ contrib/python/botocore/py3/botocore/__init__.py
@@ -16,7 +16,7 @@ import logging
 import os
 import re
 
-__version__ = '1.31.78'
+__version__ = '1.31.79'
 
 
 class NullHandler(logging.Handler):
--- contrib/python/botocore/py3/ya.make
+++ contrib/python/botocore/py3/ya.make
@@ -2,7 +2,7 @@
 
 PY3_LIBRARY()
 
-VERSION(1.31.78)
+VERSION(1.31.79)
 
 LICENSE(Apache-2.0)
 

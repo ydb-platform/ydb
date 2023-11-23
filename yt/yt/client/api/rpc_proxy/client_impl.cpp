39b5747f8c9c99ab475c64922e33edd81450cf06
kvk1920 <kvk1920@yandex-team.com>
2023-11-23 18:28:58 +0300

    Sorted map in RemoveMinatnenace response

--- yt/yt/client/api/rpc_proxy/client_impl.cpp
+++ yt/yt/client/api/rpc_proxy/client_impl.cpp
@@ -1862,9 +1862,8 @@ TFuture<TMaintenanceCounts> TClient::RemoveMaintenance(
             counts[EMaintenanceType::DisableTabletCells] = rspValue->disable_tablet_cells();
             counts[EMaintenanceType::PendingRestart] = rspValue->pending_restart();
         } else {
-            for (auto type : TEnumTraits<EMaintenanceType>::GetDomainValues()) {
-                auto it = protoCounts.find(ConvertMaintenanceTypeToProto(type));
-                counts[type] = it == protoCounts.end() ? 0 : it->second;
+            for (auto [type, count] : protoCounts) {
+                counts[CheckedEnumCast<EMaintenanceType>(type)] = count;
             }
         }
 

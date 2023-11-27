2d217ef4d8a69dfb8bdf34986b724c585e8655db
andrew-rykov <arykov@ydb.tech>
2023-11-27 10:19:12 +0300

    KIKIMR-20261 wrong Content-Length for caching monaco-editor resources
    Content-Length fix
    
    monaco-editor-one-more-time

--- ydb/core/cms/http.cpp
+++ ydb/core/cms/http.cpp
@@ -112,7 +112,7 @@ private:
         }
     }
 
-    void ReplyWithFile(NMon::TEvHttpInfo::TPtr &ev, const TActorContext &ctx, const TString& name, const bool cached = false) {
+    void ReplyWithFile(NMon::TEvHttpInfo::TPtr &ev, const TActorContext &ctx, const TString& name) {
         TString filename = TString("cms/ui") + name;
         if (filename.EndsWith('/'))
             filename += "index.html";
@@ -134,12 +134,22 @@ private:
             type = "text/html";
         }
 
+        bool cached = false;
+        if (filename.StartsWith("cms/ui/ext/monaco-editor/")) {
+            cached = true;
+            if (filename.EndsWith(".css")) {
+                type = "text/css; charset=utf-8";
+            } else if (filename.EndsWith(".js")) {
+                type = "application/javascript; charset=utf-8";
+            }
+        }
+
         TStringStream response;
         response << "HTTP/1.1 200 Ok\r\n";
         response << "Content-Type: " << type << "\r\n";
         response << "Content-Length: " << blob.size() << "\r\n";
         if (cached) {
-            response << "Cache-Control: public, max-age=31536000\r\n" << "\r\n";
+            response << "Cache-Control: public, max-age=31536000, immutable\r\n";
         }
         response << "\r\n";
         response.Write(blob.data(), blob.size());
@@ -208,10 +218,6 @@ private:
                                                           + R"({"enabled":)" + (AppData()->YamlConfigEnabled ? "true" : "false") + "}",
                                                           0,
                                                           NMon::IEvHttpInfoRes::EContentType::Custom));
-            return;
-        }
-        if (msg->Request.GetPathInfo().StartsWith("/ext/monaco-editor/")) {
-            ReplyWithFile(ev, ctx, TString{msg->Request.GetPathInfo()}, true);
             return;
         }
         ReplyWithFile(ev, ctx, TString{msg->Request.GetPathInfo()});

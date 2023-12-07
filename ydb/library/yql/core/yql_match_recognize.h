47a71a513c3a47a60340f08f67e310902e5d9b2f
Evgeny Zverev <zverevgeny@yandex-team.ru>
2023-12-07 18:30:40 +0300

    YQ-2651 use ChildPtr instead of ChildRef

--- ydb/library/yql/core/yql_match_recognize.h
+++ ydb/library/yql/core/yql_match_recognize.h
@@ -12,14 +12,14 @@ inline TRowPattern ConvertPattern(const TExprNode::TPtr& pattern, TExprContext &
         for (const auto& factor: term->Children()) {
             YQL_ENSURE(factor->ChildrenSize() == 6, "Expect 6 args");
             result.back().push_back(TRowPatternFactor{
-                factor->ChildRef(0)->IsAtom() ?
-                    TRowPatternPrimary(TString(factor->ChildRef(0)->Content())) :
-                    ConvertPattern(factor->ChildRef(0), ctx, nestingLevel + 1),
-                FromString<ui64>(factor->ChildRef(1)->Content()),
-                FromString<ui64>(factor->ChildRef(2)->Content()),
-                FromString<bool>(factor->ChildRef(3)->Content()),
-                FromString<bool>(factor->ChildRef(4)->Content()),
-                FromString<bool>(factor->ChildRef(5)->Content())
+                factor->ChildPtr(0)->IsAtom() ?
+                    TRowPatternPrimary(TString(factor->ChildPtr(0)->Content())) :
+                    ConvertPattern(factor->ChildPtr(0), ctx, nestingLevel + 1),
+                FromString<ui64>(factor->ChildPtr(1)->Content()),
+                FromString<ui64>(factor->ChildPtr(2)->Content()),
+                FromString<bool>(factor->ChildPtr(3)->Content()),
+                FromString<bool>(factor->ChildPtr(4)->Content()),
+                FromString<bool>(factor->ChildPtr(5)->Content())
             });
         }
     }

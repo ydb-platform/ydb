5d2d67895cf665987aeea7b8c8a59eb4c2c26efe
Oleg Doronin <hcpp@ydb.tech>
2023-12-12 21:27:27 +0300

    unknown data source has been fixed (#505)
    * move http api tests to ydb
    
    * unknown data source has been fixed
    
    ---------
    
    Co-authored-by: uzhas <uzhas@ydb.tech>

---
+++ ydb/tests/fq/common/test_unknown_data_source.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+import ydb.public.api.protos.draft.fq_pb2 as fq
+from ydb.tests.tools.fq_runner.kikimr_utils import yq_all
+
+
+class TestUnknownDataSource:
+    @yq_all
+    @pytest.mark.parametrize("client", [{"folder_id": "my_folder"}], indirect=True)
+    def test_should_fail_unknown_data_source(self, kikimr, client, yq_version):
+        kikimr.control_plane.wait_bootstrap(1)
+        sql = R'''
+            $h = "hahn";
+
+            SELECT
+                *
+            FROM kikimr:$h.`home/yql/tutorial/users`
+            LIMIT 100;
+            '''
+
+        query_id = client.create_query("simple", sql, type=fq.QueryContent.QueryType.ANALYTICS).result.query_id
+        client.wait_query_status(query_id, fq.QueryMeta.FAILED)
+        issues = str(client.describe_query(query_id).result.query.issue)
+
+        if yq_version == 'v1':
+            assert "Unknown DataSource: kikimr" in issues, "Incorrect Issues: " + issues
+        else:
+            assert "ATOM evaluation is not supported in YDB queries." in issues, "Incorrect Issues: " + issues
---
+++ ydb/tests/fq/common/ya.make
@@ -0,0 +1,28 @@
+PY3TEST()
+
+INCLUDE(${ARCADIA_ROOT}/ydb/tests/tools/fq_runner/ydb_runner_with_datastreams.inc)
+
+PEERDIR(
+    library/python/testing/yatest_common
+)
+
+TEST_SRCS(
+    test_unknown_data_source.py
+)
+
+PY_SRCS(
+    conftest.py
+)
+
+IF (SANITIZER_TYPE == "thread")
+    TIMEOUT(2400)
+    SIZE(LARGE)
+    TAG(ya:fat)
+ELSE()
+    TIMEOUT(600)
+    SIZE(MEDIUM)
+ENDIF()
+
+REQUIREMENTS(ram:16)
+
+END()

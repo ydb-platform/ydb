0e3397eaf38c1a11d2411c75ef09da9c7413fa9c
udovichenko-r <rvu@ydb.tech>
2023-12-13 17:29:31 +0300

    [yql] test auto partition

--- ydb/library/yql/tests/sql/yt_native_file/part0/test.py
+++ ydb/library/yql/tests/sql/yt_native_file/part0/test.py
@@ -1,11 +1,14 @@
 import pytest
+import yatest
 
 from yt_file import run_test
 from utils import pytest_generate_tests_for_part
+from yql_utils import pytest_get_current_part
 
 
 def pytest_generate_tests(metafunc):
-    return pytest_generate_tests_for_part(metafunc, 0, 10)
+    current_part, part_count = pytest_get_current_part(yatest.common.source_path(__file__))
+    return pytest_generate_tests_for_part(metafunc, current_part, part_count)
 
 
 @pytest.mark.parametrize('what', ['Results', 'Plan', 'Debug', 'RunOnOpt', 'Peephole', 'Lineage', 'ForceBlocks'])

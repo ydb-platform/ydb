eacd45a9e4ad83e8f1309cf95f5c7b5c819b6698
ijon <ijon@ydb.tech>
2023-12-13 21:26:24 +0300

    fix stream-write refresh-token rights
    RefreshToken issued by StreamTopicWrite was picking up basic permission set,
    while it should pick up extended set, the same as StreamTopicWrite itself
    (with additional ydb.streams.write permission).
    
    KIKIMR-20251

--- ydb/core/grpc_services/grpc_request_proxy.cpp
+++ ydb/core/grpc_services/grpc_request_proxy.cpp
@@ -71,7 +71,6 @@ public:
     }
 
 private:
-    void HandleRefreshToken(TRefreshTokenImpl::TPtr& ev, const TActorContext& ctx);
     void HandleConfig(NConsole::TEvConfigsDispatcher::TEvSetConfigSubscriptionResponse::TPtr& ev);
     void HandleConfig(NConsole::TEvConsole::TEvConfigNotificationRequest::TPtr& ev);
     void HandleProxyService(TEvTxUserProxy::TEvGetProxyServicesResponse::TPtr& ev);
@@ -104,7 +103,8 @@ private:
         }
     }
 
-    void Handle(TRefreshTokenImpl::TPtr& event, const TActorContext& ctx) {
+    template <ui32 TRpcId>
+    void Handle(TAutoPtr<TEventHandle<TRefreshTokenImpl<TRpcId>>>& event, const TActorContext& ctx) {
         const auto record = event->Get();
         ctx.Send(record->GetFromId(), new TGRpcRequestProxy::TEvRefreshTokenResponse {
             record->GetAuthState().State == NYdbGrpc::TAuthState::EAuthState::AS_OK,
@@ -356,15 +356,6 @@ void TGRpcRequestProxyImpl::ReplayEvents(const TString& databaseName, const TAct
     }
 }
 
-void TGRpcRequestProxyImpl::HandleRefreshToken(TRefreshTokenImpl::TPtr& ev, const TActorContext& ctx) {
-    const auto record = ev->Get();
-    ctx.Send(record->GetFromId(), new TGRpcRequestProxy::TEvRefreshTokenResponse {
-        record->GetAuthState().State == NYdbGrpc::TAuthState::EAuthState::AS_OK,
-        record->GetInternalToken(),
-        record->GetAuthState().State == NYdbGrpc::TAuthState::EAuthState::AS_UNAVAILABLE,
-        NYql::TIssues()});
-}
-
 void TGRpcRequestProxyImpl::HandleConfig(NConsole::TEvConfigsDispatcher::TEvSetConfigSubscriptionResponse::TPtr&) {
     LOG_INFO(*TlsActivationContext, NKikimrServices::GRPC_SERVER, "Subscribed for config changes");
 }
@@ -540,7 +531,8 @@ void TGRpcRequestProxyImpl::StateFunc(TAutoPtr<IEventHandle>& ev) {
 
     // handle external events
     switch (ev->GetTypeRewrite()) {
-        HFunc(TRefreshTokenImpl, PreHandle);
+        HFunc(TRefreshTokenGenericRequest, PreHandle);
+        HFunc(TRefreshTokenStreamWriteSpecificRequest, PreHandle);
         HFunc(TEvLoginRequest, PreHandle);
         HFunc(TEvListEndpointsRequest, PreHandle);
         HFunc(TEvBiStreamPingRequest, PreHandle);

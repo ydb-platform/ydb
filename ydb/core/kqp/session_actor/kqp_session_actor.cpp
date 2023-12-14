b7de9dee347a8aed89ce89b63bfe36f4e0d985a1
Nikita Vasilev <ns-vasilev@ydb.tech>
2023-12-14 16:20:49 +0300

    tnx-cache

--- ydb/core/kqp/session_actor/kqp_session_actor.cpp
+++ ydb/core/kqp/session_actor/kqp_session_actor.cpp
@@ -632,7 +632,7 @@ public:
         }
 
         Counters->ReportTxCreated(Settings.DbCounters);
-        Counters->ReportBeginTransaction(Settings.DbCounters, Transactions.EvictedTx, Transactions.Size(), Transactions.ToBeAbortedSize());
+        Counters->ReportBeginTransaction(Settings.DbCounters, Transactions.EvictedTx, Transactions.Size(), Transactions.ToBeAbortedSize()); // TODO: check 
     }
 
     bool PrepareQueryTransaction() {
@@ -680,7 +680,7 @@ public:
 
         const NKqpProto::TKqpPhyQuery& phyQuery = QueryState->PreparedQuery->GetPhysicalQuery();
         QueryState->TxCtx->SetTempTables(QueryState->TempTablesState);
-        auto [success, issues] = QueryState->TxCtx->ApplyTableOperations(phyQuery.GetTableOps(), phyQuery.GetTableInfos(),
+        auto [success, issues] = QueryState->TxCtx->ApplyTableOperations(phyQuery.GetTableOps(), phyQuery.GetTableInfos(), //TODO:
             EKikimrQueryType::Dml);
         if (!success) {
             YQL_ENSURE(!issues.Empty());

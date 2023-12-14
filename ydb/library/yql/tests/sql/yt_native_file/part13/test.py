e938f3027e06f8f4f3f2f0d79d0262d91caac192
udovichenko-r <rvu@ydb.tech>
2023-12-14 07:47:21 +0300

    [yql] extend yt_native_file test parts

---
+++ ydb/library/yql/tests/sql/yt_native_file/part13/test.py
@@ -0,0 +1,16 @@
+import pytest
+import yatest
+
+from yt_file import run_test
+from utils import pytest_generate_tests_for_part
+from yql_utils import pytest_get_current_part
+
+
+def pytest_generate_tests(metafunc):
+    current_part, part_count = pytest_get_current_part(yatest.common.source_path(__file__))
+    return pytest_generate_tests_for_part(metafunc, current_part, part_count)
+
+
+@pytest.mark.parametrize('what', ['Results', 'Plan', 'Debug', 'RunOnOpt', 'Peephole', 'Lineage', 'ForceBlocks'])
+def test(suite, case, cfg, tmpdir, what, yql_http_file_server):
+    return run_test(suite, case, cfg, tmpdir, what, yql_http_file_server)

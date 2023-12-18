a6124d1f345091dc41b01ab97d7c39609a1ea93e
Innokentii Mokin <innokentii@ydb.tech>
2023-12-18 12:17:11 +0000

    add comments

--- ydb/core/change_mirroring/reader.h
+++ ydb/core/change_mirroring/reader.h
@@ -10,11 +10,27 @@ class IReader {
 public:
     virtual ~IReader() {};
 
+    /* Returns whether there are
+     * any polled but unprocessed messages
+     */
     virtual bool HasNext() const = 0;
+    /* Returns amount of polled messages
+     * remaining to process
+     */
     virtual ui64 Remaining() const = 0;
+    /* Returns next unprocessed message
+     * Previous one can be cleaned up
+     */
     virtual TRcBuf ReadNext() = 0;
 
+    /* Return false if reading stream is closed
+     * or all data read (e.g. from S3 storage)
+     */
     virtual bool NeedPoll() const = 0;
+    /* Polls new messages
+     * it's up to specific implementation to distinguish
+     * how to notify the user about polling result
+     */
     virtual void Poll() = 0;
 };
 

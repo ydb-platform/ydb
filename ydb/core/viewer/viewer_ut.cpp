f647c0a16d794378996bcab622e4607c83ac60d7
Andrei Rykov <arykov@ydb.tech>
2023-12-19 23:32:11 +0000

    revert method invoking SetNodeCount

--- ydb/core/viewer/viewer_ut.cpp
+++ ydb/core/viewer/viewer_ut.cpp
@@ -372,8 +372,8 @@ Y_UNIT_TEST_SUITE(Viewer) {
         TPortManager tp;
         ui16 port = tp.GetPort(2134);
         ui16 grpcPort = tp.GetPort(2135);
-        auto settings = TServerSettings(port);
-        settings.SetNodeCount(nodesTotal)
+        auto settings = TServerSettings(port)
+                .SetNodeCount(nodesTotal)
                 .SetUseRealThreads(false)
                 .SetDomainName("Root")
                 .InitKikimrRunConfig();

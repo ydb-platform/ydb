30a12a41f605bb95bceca0dbaa5b5b3a3f42acff
spreis <spreis@yandex-team.com>
2023-12-19 08:26:25 +0300

    Add WITH_CONTEXT parameter to COPY and support multiple OUTPUT_INCLUDES

--- build/plugins/cp.py
+++ build/plugins/cp.py
@@ -4,16 +4,25 @@ from _common import sort_by_keywords
 
 
 def oncopy(unit, *args):
-    keywords = {'RESULT': 1, 'KEEP_DIR_STRUCT': 0, 'DESTINATION': 1, 'FROM': 1, 'OUTPUT_INCLUDES': 1, 'AUTO': 0}
+    keywords = {
+        'RESULT': 1,
+        'KEEP_DIR_STRUCT': 0,
+        'DESTINATION': 1,
+        'FROM': 1,
+        'OUTPUT_INCLUDES': -1,
+        'AUTO': 0,
+        'WITH_CONTEXT': 0,
+    }
 
     flat_args, spec_args = sort_by_keywords(keywords, args)
 
     dest_dir = spec_args['DESTINATION'][0] if 'DESTINATION' in spec_args else ''
     from_dir = spec_args['FROM'][0] if 'FROM' in spec_args else ''
-    output_include = spec_args['OUTPUT_INCLUDES'][0] if 'OUTPUT_INCLUDES' in spec_args else ''
+    output_includes = spec_args['OUTPUT_INCLUDES'] if 'OUTPUT_INCLUDES' in spec_args else None
     keep_struct = 'KEEP_DIR_STRUCT' in spec_args
     save_in_var = 'RESULT' in spec_args
     auto = 'AUTO' in spec_args
+    with_context = 'WITH_CONTEXT' in spec_args
     targets = []
 
     for source in flat_args:
@@ -29,7 +38,8 @@ def oncopy(unit, *args):
             targets.append(target_path)
         unit.oncopy_file(
             [source_path, target_path]
-            + (['OUTPUT_INCLUDES', output_include] if output_include else [])
+            + (['OUTPUT_INCLUDES'] + output_includes if output_includes else [])
+            + (['OUTPUT_INCLUDES', source_path] if with_context else [])
             + (['AUTO'] if auto else [])
         )
 

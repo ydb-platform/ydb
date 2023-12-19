848f1a33070c5f6e6d25b47d0f57067916e41947
Alexey Ozeritskiy <aozeritsky@ydb.tech>
2023-12-19 15:19:55 +0100

    Polish

--- ydb/library/yql/providers/dq/task_runner_actor/task_runner_actor.cpp
+++ ydb/library/yql/providers/dq/task_runner_actor/task_runner_actor.cpp
@@ -149,7 +149,7 @@ public:
                 auto message = TStringBuilder() << "Unexpected event: " << ev->GetTypeRewrite() << " (" << ev->GetTypeName() << ")" << " stageId: " << StageId;
                 auto issue = TIssue(message).SetCode(TIssuesIds::DQ_GATEWAY_NEED_FALLBACK_ERROR, TSeverityIds::S_ERROR);
                 auto reply = MakeHolder<NDq::TEvDq::TEvAbortExecution>(NYql::NDqProto::StatusIds::INTERNAL_ERROR, TVector<TIssue>{issue});
-                Send(ev->Sender, reply.Release());
+                Send(ParentId, reply.Release());
             }
         }
     }

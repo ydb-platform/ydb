51b4a35f84ae1ccccaebf8fc3e4c0d90bdbf3238
Oleg Shatov <olegsh@ydb.tech>
2023-12-20 16:13:22 +0000

    KIKIMR-20533 Implemented conversion from OTEL header to traceId

--- ydb/library/actors/wilson/wilson_trace.h
+++ ydb/library/actors/wilson/wilson_trace.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "util/generic/algorithm.h"
 #include <ydb/library/actors/core/monotonic.h>
 #include <ydb/library/actors/protos/actors.pb.h>
 
@@ -186,6 +187,69 @@ namespace NWilson {
             return TTraceId();
         }
 
+        const void *GetTraceIdPtr() const { return TraceId.data(); }
+        static constexpr size_t GetTraceIdSize() { return sizeof(TTrace); }
+        const void *GetSpanIdPtr() const { return &SpanId; }
+        static constexpr size_t GetSpanIdSize() { return sizeof(ui64); }
+
+        static TTraceId FromTraceparentHeader(const TStringBuf header) {
+            constexpr size_t versionChars = 2; // Only version 0 is supported
+            constexpr size_t versionStart = 0;
+
+            constexpr size_t traceIdChars = 32;
+            constexpr size_t traceIdStart = versionStart + versionChars + 1;
+            static_assert(traceIdChars == TTraceId::GetTraceIdSize() * 2);
+
+            constexpr size_t parentSpanIdChars = 16;
+            constexpr size_t parentSpanIdStart = traceIdStart + traceIdChars + 1;
+            static_assert(parentSpanIdChars == TTraceId::GetSpanIdSize() * 2);
+
+            constexpr size_t traceFlagsChars = 2;
+            constexpr size_t traceFlagsStart = parentSpanIdStart + parentSpanIdChars + 1;
+
+            constexpr size_t expectedHeaderSize =
+                versionChars + traceIdChars + parentSpanIdChars + traceFlagsChars + 3;
+
+            if (header.Size() != expectedHeaderSize) {
+                return {};
+            }
+
+            auto isHex = [](char c) {
+                return (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
+            };
+
+            if (!AllOf(header.substr(versionStart, versionChars), isHex) ||
+                    !AllOf(header.substr(traceIdStart, traceIdChars), isHex) ||
+                    !AllOf(header.substr(parentSpanIdStart, parentSpanIdChars), isHex) ||
+                    !AllOf(header.substr(traceFlagsStart, traceFlagsChars), isHex)) {
+                return {};
+            }
+
+            if (header[traceIdStart - 1] != '-' || header[parentSpanIdStart - 1] != '-' || header[traceIdChars - 1] != '-') {
+                return {};
+            }
+
+            ui8 version;
+            HexDecode(header.Data(), versionChars, &version);
+            if (version != 0) {
+                return {};
+            }
+
+
+            TTrace traceId;
+            ui64 spanId;
+            static_assert(traceIdChars == 2 * sizeof(traceId));
+            static_assert(parentSpanIdChars == 2 * sizeof(spanId));
+            HexDecode(header.Data() + traceIdStart, traceIdChars, &traceId);
+            HexDecode(header.Data() + parentSpanIdStart, parentSpanIdChars, &spanId);
+
+            if ((traceId[0] == 0 && traceId[1] == 0) || spanId == 0) {
+                return {};
+            }
+
+            return TTraceId(traceId, spanId, 15, Max<ui32>());
+        }
+
         TTraceId Span(ui8 verbosity) const {
             Validate();
             if (!*this || !TimeToLive) {
@@ -217,11 +281,6 @@ namespace NWilson {
         ui8 GetVerbosity() const {
             return Verbosity;
         }
-
-        const void *GetTraceIdPtr() const { return TraceId.data(); }
-        static constexpr size_t GetTraceIdSize() { return sizeof(TTrace); }
-        const void *GetSpanIdPtr() const { return &SpanId; }
-        static constexpr size_t GetSpanIdSize() { return sizeof(ui64); }
 
         TString GetHexTraceId() const {
             return HexEncode(GetTraceIdPtr(), GetTraceIdSize());

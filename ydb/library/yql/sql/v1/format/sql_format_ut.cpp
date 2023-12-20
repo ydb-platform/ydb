502c02b17836e3c898fd6c0c4547f6d2e299da60
Daniil Demin <deminds@ydb.tech>
2023-12-20 14:31:18 +0000

    CREATE / DROP VIEW queries support
    SELECTing from VIEWs is not supported yet!
    
    - Make changes to the YQL grammar to add CREATE / DROP VIEW statements
    - Save the query text specified in the CREATE VIEW statement in the VIEW object
    - Implement VIEW as a wrapped generic Object: CREATE / DROP VIEW
    commands call Create/DropObject commands with appropriate settings
    - Query text is stored in the object's features

--- ydb/library/yql/sql/v1/format/sql_format_ut.cpp
+++ ydb/library/yql/sql/v1/format/sql_format_ut.cpp
@@ -1500,4 +1500,24 @@ FROM Input MATCH_RECOGNIZE (PATTERN (A) DEFINE A AS A);
         TSetup setup;
         setup.Run(cases, NSQLFormat::EFormatMode::Obfuscate);
     }    
+
+    Y_UNIT_TEST(CreateView) {
+        TCases cases = {
+            {"creAte vIEw TheView wiTh (security_invoker = trUE) As SELect 1",
+             "CREATE VIEW TheView WITH (security_invoker = TRUE) AS\nSELECT\n\t1;\n"},
+        };
+
+        TSetup setup;
+        setup.Run(cases);
+    }
+
+    Y_UNIT_TEST(DropView) {
+        TCases cases = {
+            {"dRop viEW theVIEW",
+             "DROP VIEW theVIEW;\n"},
+        };
+
+        TSetup setup;
+        setup.Run(cases);
+    }
 }

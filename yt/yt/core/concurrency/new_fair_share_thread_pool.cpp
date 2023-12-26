0809f9919907e3d098d0170dac3d2011be160578
robot-piglet <robot-piglet@yandex-team.com>
2023-12-26 07:46:15 +0300

    Intermediate changes

--- yt/yt/core/concurrency/new_fair_share_thread_pool.cpp
+++ yt/yt/core/concurrency/new_fair_share_thread_pool.cpp
@@ -391,7 +391,9 @@ public:
         , ThreadNamePrefix_(threadNamePrefix)
         , Profiler_(TProfiler{"/fair_share_queue"}
             .WithHot())
-        , CumulativeSchedulingTimeCounter_(Profiler_.TimeCounter("/time/scheduling_cumulative"))
+        , CumulativeSchedulingTimeCounter_(Profiler_
+            .WithTags(GetThreadTags(ThreadNamePrefix_))
+            .TimeCounter("/time/scheduling_cumulative"))
         , PoolWeightProvider_(options.PoolWeightProvider)
         , PoolRetentionTime_(options.PoolRetentionTime)
         , VerboseLogging_(options.VerboseLogging)

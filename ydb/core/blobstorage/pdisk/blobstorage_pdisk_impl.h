58934ccb3604efa2c2ee8d3b4fd61666332bd401
Oleg Shatov <35371391+domwst@users.noreply.github.com>
2023-12-26 15:07:15 +0700

    KIKIMR-20042 Partially implemented PDisk tracing (#574)
    * KIKIMR-20042 Partially implemented PDisk tracing
    
    * Grooming
    
    * Another grooming

--- ydb/core/blobstorage/pdisk/blobstorage_pdisk_impl.h
+++ ydb/core/blobstorage/pdisk/blobstorage_pdisk_impl.h
@@ -352,7 +352,7 @@ public:
     void ProcessChunkTrimQueue();
     void ClearQuarantineChunks();
     // Should be called to initiate TRIM (on chunk delete or prev trim done)
-    void TryTrimChunk(bool prevDone, ui64 trimmedSize);
+    void TryTrimChunk(bool prevDone, ui64 trimmedSize, const NWilson::TSpan& parentSpan);
     void ProcessFastOperationsQueue();
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Drive info and write cache

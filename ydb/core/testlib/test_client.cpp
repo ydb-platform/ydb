f2aac2fc1ac06ce681cbf1173cd46f8f20db43a2
Aleksandr Dmitriev <108884553+alexd65536@users.noreply.github.com>
2023-12-27 12:21:33 +0300

    use statistics aggregator for basic statistics KIKIMR-18323 (#623)

--- ydb/core/testlib/test_client.cpp
+++ ydb/core/testlib/test_client.cpp
@@ -728,7 +728,7 @@ namespace Tests {
                 TMailboxType::Revolving, appData.SystemPoolId));
         localConfig.TabletClassInfo[TTabletTypes::StatisticsAggregator] =
             TLocalConfig::TTabletClassInfo(new TTabletSetupInfo(
-                &NStat::CreateStatisticsAggregator, TMailboxType::Revolving, appData.UserPoolId,
+                &NStat::CreateStatisticsAggregatorForTests, TMailboxType::Revolving, appData.UserPoolId,
                 TMailboxType::Revolving, appData.SystemPoolId));
     }
 

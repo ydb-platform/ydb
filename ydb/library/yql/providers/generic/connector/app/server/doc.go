60568b34b69ce1bfd79d4fca36b42808d1261172
Vitaly Isaev <vitalyisaev@ydb.tech>
2023-12-28 15:46:38 +0300

    YQ Connector: YQ-2323: remove Golang implementaion from YDB (#772)
    * YQ Connector: drop Golang implementation from YDB repo

--- ydb/library/yql/providers/generic/connector/app/server/doc.go
+++
@@ -1,4 +0,0 @@
-// Package server containes the code base of Connector GRPC service -
-// the component of YDB's Federated Query system providing the unified interface
-// to the external data sources.
-package server
--- ydb/library/yql/providers/generic/connector/app/server/paging/ut/ya.make
+++
@@ -1,5 +0,0 @@
-GO_TEST_FOR(ydb/library/yql/providers/generic/connector/app/server/paging)
-
-SIZE(SMALL)
-
-END()

a97be623a6d208a3b121dbb5ed6ace7a3ef158cb
ivanmorozov333 <ivanmorozov@hotmail.com>
2023-12-28 12:27:59 +0300

    fix

--- ydb/core/tx/columnshard/columnshard__stats_scan.cpp
+++ ydb/core/tx/columnshard/columnshard__stats_scan.cpp
@@ -82,7 +82,7 @@ void TStatsIterator::AppendStats(const std::vector<std::unique_ptr<arrow::ArrayB
         NArrow::Append<arrow::UInt64Type>(*builders[10], r->BlobRange.Offset);
         NArrow::Append<arrow::UInt64Type>(*builders[11], r->BlobRange.Size);
         NArrow::Append<arrow::UInt64Type>(*builders[12], portion.HasRemoveSnapshot());
-        NArrow::Append<arrow::UInt64Type>(*builders[13], portion.GetTierName());
+        NArrow::Append<arrow::UInt64Type>(*builders[13], portion.GetMeta().GetTierName());
     }
 }
 

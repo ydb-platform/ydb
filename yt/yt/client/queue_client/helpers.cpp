bb70c7bcc5f1b756817763ddf18fc7a140d139e3
robot-piglet <robot-piglet@yandex-team.com>
2023-12-30 01:23:51 +0300

    Intermediate changes

---
+++ yt/yt/client/queue_client/helpers.cpp
@@ -0,0 +1,95 @@
+#include "helpers.h"
+
+#include <yt/yt/ytlib/api/native/client.h>
+
+#include <yt/yt/client/table_client/helpers.h>
+
+namespace NYT::NQueueClient {
+
+using namespace NApi;
+using namespace NObjectClient;
+using namespace NConcurrency;
+using namespace NYPath;
+using namespace NLogging;
+using namespace NTableClient;
+using namespace NTabletClient;
+using namespace NQueueClient;
+
+////////////////////////////////////////////////////////////////////////////////
+
+THashMap<int, THashMap<i64, TPartitionRowInfo>> CollectPartitionRowInfos(
+    const TYPath& path,
+    const IClientPtr& client,
+    const std::vector<std::pair<int, i64>>& tabletAndRowIndices,
+    const TCollectPartitionRowInfoParams& params,
+    const TLogger& logger)
+{
+    const auto& Logger = logger;
+
+    if (tabletAndRowIndices.empty()) {
+        return {};
+    }
+
+    TStringBuilder queryBuilder;
+    queryBuilder.AppendString("[$tablet_index], [$row_index]");
+
+    std::optional<size_t> cumulativeDataWeightColumnId;
+    std::optional<size_t> timestampColumnId;
+
+    ui32 expectedRowSize = 2;
+    if (params.HasCumulativeDataWeightColumn) {
+        queryBuilder.AppendFormat(", [%v]", CumulativeDataWeightColumnName);
+        cumulativeDataWeightColumnId = expectedRowSize;
+        ++expectedRowSize;
+    }
+    if (params.HasTimestampColumn) {
+        queryBuilder.AppendFormat(", [%v]", TimestampColumnName);
+        timestampColumnId = expectedRowSize;
+        ++expectedRowSize;
+    }
+
+    queryBuilder.AppendFormat("from [%v] where ([$tablet_index], [$row_index]) in (",
+        path);
+
+    bool isFirstTuple = true;
+    for (const auto& [partitionIndex, rowIndex] : tabletAndRowIndices) {
+        if (!isFirstTuple) {
+            queryBuilder.AppendString(", ");
+        }
+        queryBuilder.AppendFormat("(%vu, %vu)", partitionIndex, rowIndex);
+        isFirstTuple = false;
+    }
+
+    queryBuilder.AppendString(")");
+
+    YT_VERIFY(!isFirstTuple);
+
+    auto query = queryBuilder.Flush();
+    TSelectRowsOptions options;
+    options.ReplicaConsistency = EReplicaConsistency::Sync;
+    YT_LOG_TRACE("Executing query for partition row infos (Query: %v)", query);
+    auto selectResult = WaitFor(client->SelectRows(query, options))
+        .ValueOrThrow();
+
+    THashMap<int, THashMap<i64, TPartitionRowInfo>> result;
+
+    for (const auto& row : selectResult.Rowset->GetRows()) {
+        YT_VERIFY(row.GetCount() == expectedRowSize);
+
+        auto tabletIndex = FromUnversionedValue<int>(row[0]);
+        auto rowIndex = FromUnversionedValue<i64>(row[1]);
+
+        result[tabletIndex].emplace(rowIndex, TPartitionRowInfo{
+            .CumulativeDataWeight = cumulativeDataWeightColumnId
+                ? FromUnversionedValue<std::optional<i64>>(row[*cumulativeDataWeightColumnId])
+                : std::nullopt,
+            .Timestamp = timestampColumnId
+                ? FromUnversionedValue<std::optional<ui64>>(row[*timestampColumnId])
+                : std::nullopt,
+        });
+    }
+
+    return result;
+}
+
+} // namespace NYT::NQueueClient

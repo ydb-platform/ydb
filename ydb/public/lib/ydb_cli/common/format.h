48744491975601666f75bcab5aec0df3d3852fc0
Pavel Velikhov <pavelvelikhov@ydb.tech>
2024-01-02 15:43:31 +0300

    Added stage and local inputs for JSON plans (#699)
    * Squashed all work into a single commit
    
    * Cannonized most test except postgres
    
    * Cannonized CLI tests

--- ydb/public/lib/ydb_cli/common/format.h
+++ ydb/public/lib/ydb_cli/common/format.h
@@ -105,13 +105,16 @@ private:
     void PrintPrettyTable(const NJson::TJsonValue& plan);
     void PrintPrettyTableImpl(const NJson::TJsonValue& plan, TString& offset, TPrettyTable& table);
     void PrintJson(const TString& plan);
+    void PrintSimplifyJson(const NJson::TJsonValue& plan);
     TString JsonToString(const NJson::TJsonValue& jsonValue);
 
-    void SplitJoinsPlan(NJson::TJsonValue& plan);
+    void SplitPlanInTree(NJson::TJsonValue& plan);
     void SimplifyQueryPlan(NJson::TJsonValue& plan);
     TVector<NJson::TJsonValue> RemoveRedundantNodes(NJson::TJsonValue& plan, const THashSet<TString>& redundantNodes);
     THashMap<TString, NJson::TJsonValue> ExtractPrecomputes(NJson::TJsonValue& planJson);
     void ResolvePrecomputeLinks(NJson::TJsonValue& planJson, const THashMap<TString, NJson::TJsonValue>& precomputes);
+    void DeleteSplitNodes(NJson::TJsonValue& planJson);
+
 
 private:
     EOutputFormat Format;

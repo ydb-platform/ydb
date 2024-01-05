bd601c1ae519969c95f5f0540af8e5c53fcc6e84
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2024-01-05 15:30:59 +0000

    Get rid of lambdas in logging macros

--- ydb/core/tx/ctor_logger.h
+++ ydb/core/tx/ctor_logger.h
@@ -3,31 +3,52 @@
 #include <ydb/library/actors/core/log.h>
 #include <ydb/library/services/services.pb.h>
 
+#include <optional>
+
 // You have to define TLogThis in local namespace to use these macros
-#define LOG_S_EMERG(stream) { TLogThis(*TlsActivationContext, NActors::NLog::PRI_EMERG, [&](TStringBuilder& ss){ ss << stream; }); }
-#define LOG_S_ALERT(stream) { TLogThis(*TlsActivationContext, NActors::NLog::PRI_ALERT, [&](TStringBuilder& ss){ ss << stream; }); }
-#define LOG_S_CRIT(stream) { TLogThis(*TlsActivationContext, NActors::NLog::PRI_CRIT, [&](TStringBuilder& ss){ ss << stream; }); }
-#define LOG_S_ERROR(stream) { TLogThis(*TlsActivationContext, NActors::NLog::PRI_ERROR, [&](TStringBuilder& ss){ ss << stream; }); }
-#define LOG_S_WARN(stream) { TLogThis(*TlsActivationContext, NActors::NLog::PRI_WARN, [&](TStringBuilder& ss){ ss << stream; }); }
-#define LOG_S_NOTICE(stream) { TLogThis(*TlsActivationContext, NActors::NLog::PRI_NOTICE, [&](TStringBuilder& ss){ ss << stream; }); }
-#define LOG_S_INFO(stream) { TLogThis(*TlsActivationContext, NActors::NLog::PRI_INFO, [&](TStringBuilder& ss){ ss << stream; }); }
-#define LOG_S_DEBUG(stream) { TLogThis(*TlsActivationContext, NActors::NLog::PRI_DEBUG, [&](TStringBuilder& ss){ ss << stream; }); }
-#define LOG_S_TRACE(stream) { TLogThis(*TlsActivationContext, NActors::NLog::PRI_TRACE, [&](TStringBuilder& ss){ ss << stream; }); }
+#define __LOG_S_PRIORITY_IMPL__(stream, priority)                   \
+    if (auto logThis = TLogThis(*TlsActivationContext, priority)) { \
+        logThis.Str() << stream;                                    \
+        logThis.WriteLog();                                         \
+    }
+
+#define LOG_S_EMERG(stream) __LOG_S_PRIORITY_IMPL__(stream, NActors::NLog::PRI_EMERG)
+#define LOG_S_ALERT(stream) __LOG_S_PRIORITY_IMPL__(stream, NActors::NLog::PRI_ALERT)
+#define LOG_S_CRIT(stream) __LOG_S_PRIORITY_IMPL__(stream, NActors::NLog::PRI_CRIT)
+#define LOG_S_ERROR(stream) __LOG_S_PRIORITY_IMPL__(stream, NActors::NLog::PRI_ERROR)
+#define LOG_S_WARN(stream) __LOG_S_PRIORITY_IMPL__(stream, NActors::NLog::PRI_WARN)
+#define LOG_S_NOTICE(stream) __LOG_S_PRIORITY_IMPL__(stream, NActors::NLog::PRI_NOTICE)
+#define LOG_S_INFO(stream) __LOG_S_PRIORITY_IMPL__(stream, NActors::NLog::PRI_INFO)
+#define LOG_S_DEBUG(stream) __LOG_S_PRIORITY_IMPL__(stream, NActors::NLog::PRI_DEBUG)
+#define LOG_S_TRACE(stream) __LOG_S_PRIORITY_IMPL__(stream, NActors::NLog::PRI_TRACE)
 
 namespace NKikimr {
 
 template <NKikimrServices::EServiceKikimr Component>
 class TCtorLogger
 {
+    const NActors::TActivationContext& Ctx;
+    NActors::NLog::EPriority Priority;
+    std::optional<TStringBuilder> StrStream;
+
 public:
-    template <typename TFunc>
-    TCtorLogger(const NActors::TActivationContext& ctx, NActors::NLog::EPriority priority, TFunc logFunc)
+    TCtorLogger(const NActors::TActivationContext& ctx, NActors::NLog::EPriority priority)
+        : Ctx(ctx)
+        , Priority(priority)
     {
-        if (IS_LOG_PRIORITY_ENABLED(priority, Component)) {
-            TStringBuilder strStream;
-            logFunc(strStream);
-            ::NActors::MemLogAdapter(ctx, priority, Component, "%s", strStream.data());
-        }
+    }
+
+    operator bool() const {
+        return IS_LOG_PRIORITY_ENABLED(Priority, Component);
+    }
+
+    TStringBuilder& Str() {
+        StrStream.emplace();
+        return *StrStream;
+    }
+
+    void WriteLog() {
+        ::NActors::MemLogAdapter(Ctx, Priority, Component, std::move(*StrStream));
     }
 };
 

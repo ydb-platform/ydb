9a53b1b93a5fa7ad353f451c68f63d48519a9382
AlexSm <alex@ydb.tech>
2024-01-08 19:00:23 +0100

    Move epilogue.cmake after sqlparser (#869)
    * Move epilogue.cmake after sqlparser
    
    * Fix path

--- ydb/library/yql/parser/proto_ast/gen/v1_proto_split/epilogue.cmake
+++ ydb/library/yql/parser/proto_ast/gen/v1_proto_split/epilogue.cmake
@@ -4,6 +4,6 @@ set(GRAMMAR_MULTILINE_COMMENT_CORE       ".")
 
 configure_file(
   ${CMAKE_SOURCE_DIR}/ydb/library/yql/sql/v1/SQLv1.g.in
-  ${CMAKE_BINARY_DIR}/ydb/library/yql/parser/proto_ast/gen/v1_proto/SQLv1.g
+  ${CMAKE_BINARY_DIR}/ydb/library/yql/parser/proto_ast/gen/v1_proto_split/SQLv1.g
 )
 

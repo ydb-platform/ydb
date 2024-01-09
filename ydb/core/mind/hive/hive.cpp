8c1f7ef7a4091b7bb300ee3030ec339b6cf77a6e
vporyadke <zalyalov@ydb.tech>
2024-01-09 18:25:16 +0300

    Storage Balancer KIKIMR-20636 (#770)

--- ydb/core/mind/hive/hive.cpp
+++ ydb/core/mind/hive/hive.cpp
@@ -41,6 +41,7 @@ TString EBalancerTypeName(EBalancerType value) {
         case EBalancerType::Emergency: return "Emergency";
         case EBalancerType::SpreadNeighbours: return "Spread";
         case EBalancerType::Manual: return "Manual";
+        case EBalancerType::Storage: return "Storage";
     }
 }
 

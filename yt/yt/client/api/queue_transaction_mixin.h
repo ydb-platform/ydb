212b35cc4f8411e3abcf47b64ac51403ad221f93
robot-piglet <robot-piglet@yandex-team.com>
2024-01-09 03:16:35 +0300

    Intermediate changes

---
+++ yt/yt/client/api/queue_transaction_mixin.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "transaction.h"
+
+#include <yt/yt/core/ypath/public.h>
+
+namespace NYT::NApi {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TQueueTransactionMixin
+    : public virtual ITransaction
+{
+public:
+    // TODO(nadya73): Remove it: YT-20712
+    void AdvanceConsumer(
+        const NYPath::TYPath& path,
+        int partitionIndex,
+        std::optional<i64> oldOffset,
+        i64 newOffset) override;
+
+    // TODO(nadya73): Remove it: YT-20712
+    void AdvanceConsumer(
+        const NYPath::TRichYPath& consumerPath,
+        const NYPath::TRichYPath& queuePath,
+        int partitionIndex,
+        std::optional<i64> oldOffset,
+        i64 newOffset) override;;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NApi
+

81bc06a50d9307a9a9a41bfc53a073c825021ca1
Aleksandr Mikishev <amikish@ydb.tech>
2024-01-11 19:14:35 +0300

    Find ya tool both in Arcadia and github

--- ydb/library/yql/tools/yqlrun/ui.sh
+++ ydb/library/yql/tools/yqlrun/ui.sh
@@ -8,15 +8,24 @@
 #
 
 SCRIPT_DIR="$(dirname $(readlink -f "$0"))"
-UDFS_DIR="${SCRIPT_DIR}/../../udfs;${SCRIPT_DIR}/../../../../../../yql/udfs"
-ASSETS_DIR=${SCRIPT_DIR}/http/www
-MOUNTS_CFG=${SCRIPT_DIR}/mounts.txt
-GATEWAYS_CFG=${SCRIPT_DIR}/../../cfg/tests/gateways.conf
+cd $SCRIPT_DIR/../../../../..
+ROOT_DIR=$PWD
+cd -
+
+ASSETS_DIR="${SCRIPT_DIR}/http/www"
+MOUNTS_CFG="${SCRIPT_DIR}/mounts.txt"
+UDFS_DIR="${ROOT_DIR}/ydb/library/yql/udfs"
+GATEWAYS_CFG="${ROOT_DIR}/ydb/library/yql/cfg/tests/gateways.conf"
 
 PORT=${1:-3000}
 
+YA="$ROOT_DIR/ya"
+if [ ! -x "$YA" ];  then
+    YA=ya
+fi
+
 if [ "$2" = "--gdb" ]; then
-    GDB="ya tool gdb --args"
+    GDB="$YA tool gdb --args"
 fi
 
 ${GDB} ${SCRIPT_DIR}/yqlrun ui \

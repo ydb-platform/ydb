7bea4d463ef62b901baec3724a780af918cb40e4
ivanmorozov333 <111685085+ivanmorozov333@users.noreply.github.com>
2024-01-18 15:10:45 +0300

    KIKIMR-20009: fix race condition on blobs removing (#1118)

--- ydb/core/tx/columnshard/blobs_action/bs/remove.h
+++ ydb/core/tx/columnshard/blobs_action/bs/remove.h
@@ -18,12 +18,16 @@ protected:
     virtual void DoOnExecuteTxAfterRemoving(NColumnShard::TColumnShard& /*self*/, NColumnShard::TBlobManagerDb& dbBlobs, const bool success) {
         if (success) {
             for (auto&& i : GetDeclaredBlobs()) {
-                Manager->DeleteBlob(i, dbBlobs);
+                Manager->DeleteBlobOnExecute(i, dbBlobs);
             }
         }
     }
-    virtual void DoOnCompleteTxAfterRemoving(NColumnShard::TColumnShard& /*self*/) {
-
+    virtual void DoOnCompleteTxAfterRemoving(NColumnShard::TColumnShard& /*self*/, const bool success) {
+        if (success) {
+            for (auto&& i : GetDeclaredBlobs()) {
+                Manager->DeleteBlobOnComplete(i);
+            }
+        }
     }
 public:
     TDeclareRemovingAction(const TString& storageId, NColumnShard::TBlobManager& manager)

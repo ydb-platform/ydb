662899ef8cb76701905d795a5c80b11b37b8e4a7
Aleksandr Mikishev <amikish@ydb.tech>
2024-01-21 17:08:30 +0300

    Add canonical tests

---
+++ ydb/library/yql/tests/sql/suites/bigdate/output.sql
@@ -0,0 +1,10 @@
+select cast(cast(-53375810 as date32) as string)
+, cast(cast(-53375809 as date32) as string)
+, cast(cast(-1 as date32) as string)
+, cast(cast(0 as date32) as string)
+, cast(cast(1 as date32) as string)
+, cast(cast(18000u as date32) as string)
+, cast(cast(49673u - 1u as date32) as string)
+, cast(cast(49673u as date32) as string)
+, cast(cast(53375807ul as date32) as string)
+, cast(cast(53375808ul as date32) as string);

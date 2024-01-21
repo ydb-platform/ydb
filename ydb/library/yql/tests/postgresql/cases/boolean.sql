833183155d363e78910d0ee7ae9515136a0a4f3e
Mars Agliullin <marsaly@ydb.tech>
2024-01-22 01:41:37 +0300

    Added patches to workaround unsupported INSERT without column list & updated passing regression tests

--- ydb/library/yql/tests/postgresql/cases/boolean.sql
+++ ydb/library/yql/tests/postgresql/cases/boolean.sql
@@ -90,18 +90,24 @@ INSERT INTO BOOLTBL3 (d, b, o) VALUES ('null', null, 3);
 -- correct. Use a table as source to prevent constant simplification
 -- to interfer.
 CREATE TABLE booltbl4(isfalse bool, istrue bool, isnul bool);
-INSERT INTO booltbl4 VALUES (false, true, null);
+INSERT INTO booltbl4 (isfalse, istrue, isnul) VALUES (false, true, null);
 \pset null '(null)'
 -- AND expression need to return null if there's any nulls and not all
 -- of the value are true
 SELECT istrue AND isnul AND istrue FROM booltbl4;
 SELECT istrue AND istrue AND isnul FROM booltbl4;
 SELECT isnul AND istrue AND istrue FROM booltbl4;
+SELECT isfalse AND isnul AND istrue FROM booltbl4;
+SELECT istrue AND isfalse AND isnul FROM booltbl4;
+SELECT isnul AND istrue AND isfalse FROM booltbl4;
 -- OR expression need to return null if there's any nulls and none
 -- of the value is true
 SELECT isfalse OR isnul OR isfalse FROM booltbl4;
 SELECT isfalse OR isfalse OR isnul FROM booltbl4;
 SELECT isnul OR isfalse OR isfalse FROM booltbl4;
+SELECT isfalse OR isnul OR istrue FROM booltbl4;
+SELECT istrue OR isfalse OR isnul FROM booltbl4;
+SELECT isnul OR istrue OR isfalse FROM booltbl4;
 --
 -- Clean up
 -- Many tables are retained by the regression test, but these do not seem

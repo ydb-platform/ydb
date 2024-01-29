d0648a18e88f140d909c3047133e1f02ac3d240e
Alexey Ozeritskiy <aozeritsky@ydb.tech>
2024-01-29 21:39:12 +0100

    Use the same constants in both tpc-ds version (yql and pg) (#1399)

--- ydb/library/benchmarks/queries/tpcds/pg/q48.sql
+++ ydb/library/benchmarks/queries/tpcds/pg/q48.sql
@@ -4,36 +4,36 @@ select sum (ss_quantity)
  from {{store_sales}}, {{store}}, {{customer_demographics}}, {{customer_address}}, {{date_dim}}
  where s_store_sk = ss_store_sk
  and  ss_sold_date_sk = d_date_sk and d_year = 1998
- and  
+ and
  (
   (
    cd_demo_sk = ss_cdemo_sk
-   and 
+   and
    cd_marital_status = 'M'
-   and 
-   cd_education_status = '4 yr Degree'
-   and 
+   and
+   cd_education_status = 'Unknown'
+   and
    ss_sales_price between 100.00::numeric and 150.00::numeric
    )
  or
   (
   cd_demo_sk = ss_cdemo_sk
-   and 
-   cd_marital_status = 'D'
-   and 
-   cd_education_status = 'Primary'
-   and 
-   ss_sales_price between 50.00::numeric and 100.00::numeric   
+   and
+   cd_marital_status = 'W'
+   and
+   cd_education_status = 'College'
+   and
+   ss_sales_price between 50.00::numeric and 100.00::numeric
   )
- or 
+ or
  (
   cd_demo_sk = ss_cdemo_sk
-  and 
-   cd_marital_status = 'U'
-   and 
-   cd_education_status = 'Advanced Degree'
-   and 
-   ss_sales_price between 150.00::numeric and 200.00::numeric  
+  and
+   cd_marital_status = 'D'
+   and
+   cd_education_status = 'Primary'
+   and
+   ss_sales_price between 150.00::numeric and 200.00::numeric
  )
  )
  and
@@ -43,24 +43,24 @@ select sum (ss_quantity)
   and
   ca_country = 'United States'
   and
-  ca_state in ('KY', 'GA', 'NM')
-  and ss_net_profit between 0::numeric and 2000::numeric  
+  ca_state in ('MI', 'GA', 'NH')
+  and ss_net_profit between 0::numeric and 2000::numeric
   )
  or
   (ss_addr_sk = ca_address_sk
   and
   ca_country = 'United States'
   and
-  ca_state in ('MT', 'OR', 'IN')
-  and ss_net_profit between 150::numeric and 3000::numeric 
+  ca_state in ('TX', 'KY', 'SD')
+  and ss_net_profit between 150::numeric and 3000::numeric
   )
  or
   (ss_addr_sk = ca_address_sk
   and
   ca_country = 'United States'
   and
-  ca_state in ('WI', 'MO', 'WV')
-  and ss_net_profit between 50::numeric and 25000::numeric 
+  ca_state in ('NY', 'OH', 'FL')
+  and ss_net_profit between 50::numeric and 25000::numeric
   )
  )
 ;

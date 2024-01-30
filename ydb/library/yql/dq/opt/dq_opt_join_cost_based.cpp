7b37345d5394994f2d508e45d47f103430e9106e
Dark-Avery <ingv0rr@ydb.tech>
2024-01-30 14:12:51 +0000

    Change max size of DP table

--- ydb/library/yql/dq/opt/dq_opt_join_cost_based.cpp
+++ ydb/library/yql/dq/opt/dq_opt_join_cost_based.cpp
@@ -718,7 +718,7 @@ template <int N> std::bitset<N> TDPccpSolver<N>::NextBitset(const std::bitset<N>
     return res;
 
     // TODO: We can optimize this with a few long integer operations,
-    // but it will only work for 64 bit bitsets
+    // but it will only work for 254 bit bitsets
     // return std::bitset<N>((prev | ~final).to_ulong() + 1) & final;
 }
 
@@ -1014,7 +1014,7 @@ std::shared_ptr<TJoinOptimizerNode> OptimizeSubtree(const std::shared_ptr<TJoinO
         return PickBestNonReorderabeJoin(joinTree->LeftArg, joinTree->RightArg, joinTree->JoinConditions, joinTree->JoinType, ctx);
     }
 
-    TGraph<64> joinGraph;
+    TGraph<254> joinGraph;
     TVector<std::shared_ptr<IBaseOptimizerNode>> rels;
     std::set<std::pair<TJoinColumn, TJoinColumn>> joinConditions;
 
@@ -1024,10 +1024,10 @@ std::shared_ptr<TJoinOptimizerNode> OptimizeSubtree(const std::shared_ptr<TJoinO
         joinGraph.AddNode(i, rels[i]->Labels());
     }
 
-    // Check if we have more rels than DPccp can handle (64)
+    // Check if we have more rels than DPccp can handle (254)
     // If that's the case - don't optimize the plan and just return it with
     // computed statistics
-    if (rels.size() >= 64) {
+    if (rels.size() >= 254) {
         ComputeStatistics(joinTree, ctx);
         return joinTree;
     }
@@ -1055,7 +1055,7 @@ std::shared_ptr<TJoinOptimizerNode> OptimizeSubtree(const std::shared_ptr<TJoinO
         YQL_CLOG(TRACE, CoreDq) << str.str();
     }
 
-    TDPccpSolver<64> solver(joinGraph, rels, ctx);
+    TDPccpSolver<254> solver(joinGraph, rels, ctx);
 
     // Check that the dynamic table of DPccp is not too big
     // If it is, just compute the statistics for the join tree and return it
@@ -1172,7 +1172,7 @@ public:
 
     TOutput JoinSearch() override {
         auto dummyProviderCtx = TDummyProviderContext();
-        TDPccpSolver<64> solver(JoinGraph, Rels, dummyProviderCtx);
+        TDPccpSolver<254> solver(JoinGraph, Rels, dummyProviderCtx);
         std::shared_ptr<TJoinOptimizerNode> result = solver.Solve();
         if (Log) {
             std::stringstream str;
@@ -1282,7 +1282,7 @@ private:
     const std::function<void(const TString&)> Log;
 
     TVector<std::shared_ptr<IBaseOptimizerNode>> Rels;
-    TGraph<64> JoinGraph;
+    TGraph<254> JoinGraph;
 };
 
 IOptimizer* MakeNativeOptimizer(const IOptimizer::TInput& input, const std::function<void(const TString&)>& log) {

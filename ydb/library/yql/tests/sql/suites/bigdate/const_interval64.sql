ba3b1d5ba90f06ea79f08417e7bf60fa575c130d
Aleksandr Mikishev <amikish@ydb.tech>
2024-01-31 11:15:29 +0300

    Check max days to prevent ui64 overflow

--- ydb/library/yql/tests/sql/suites/bigdate/const_interval64.sql
+++ ydb/library/yql/tests/sql/suites/bigdate/const_interval64.sql
@@ -1,10 +1,35 @@
-select date32("-144169-01-01"), cast(date32("-144169-01-01") as string);
-select date32("-1-1-1"), cast(date32("-1-1-1") as string);
-select date32("1-1-1"), cast(date32("1-1-1") as string);
-select date32("1969-12-31"), cast(date32("1969-12-31") as string);
-select date32("1970-01-01"), cast(date32("1970-01-01") as string);
-select date32("2000-04-05"), cast(date32("2000-04-05") as string);
-select date32("2100-03-01"), cast(date32("2100-03-01") as string);
-select date32("2105-12-31"), cast(date32("2105-12-31") as string);
-select date32("2106-01-01"), cast(date32("2106-01-01") as string);
-select date32("148107-12-31"), cast(date32("148107-12-31") as string);
+select interval64("-P106751616DT23H59M59.999999S"), cast(interval64("-P106751616DT23H59M59.999999S") as string);
+select interval64("P106751616DT23H59M59.999999S"), cast(interval64("P106751616DT23H59M59.999999S") as string);
+select interval64("P106709244DT999999H999999M999999.999999S"), cast(interval64("P106709244DT999999H999999M999999.999999S") as string);
+select interval64("P000000000DT00H00M00.000000S"), cast(interval64("P000000000DT00H00M00.000000S") as string);
+
+select interval64("PT0S"), cast(interval64("PT0S") as string);
+select interval64("-PT0S"), cast(interval64("-PT0S") as string);
+select interval64("PT0.000001S"), cast(interval64("PT0.000001S") as string);
+select interval64("-PT0.000001S"), cast(interval64("-PT0.000001S") as string);
+
+select interval64("PT0S"), cast(interval64("PT0S") as string);
+select interval64("PT0M"), cast(interval64("PT0M") as string);
+select interval64("PT0H"), cast(interval64("PT0H") as string);
+select interval64("P0D"), cast(interval64("P0D") as string);
+select interval64("P0W"), cast(interval64("P0W") as string);
+
+select interval64("PT999999S"), cast(interval64("PT999999S") as string);
+select interval64("PT999999M"), cast(interval64("PT999999M") as string);
+select interval64("PT999999H"), cast(interval64("PT999999H") as string);
+
+select interval64("P106751616D"), cast(interval64("P106751616D") as string);
+select interval64("P15250230W"), cast(interval64("P15250230W") as string);
+
+select interval64("PT1S"), cast(interval64("PT1S") as string);
+select interval64("PT1M"), cast(interval64("PT1M") as string);
+select interval64("PT1H"), cast(interval64("PT1H") as string);
+select interval64("P1D"), cast(interval64("P1D") as string);
+select interval64("P1W"), cast(interval64("P1W") as string);
+
+select interval64("-PT1S"), cast(interval64("-PT1S") as string);
+select interval64("-PT1M"), cast(interval64("-PT1M") as string);
+select interval64("-PT1H"), cast(interval64("-PT1H") as string);
+select interval64("-P1D"), cast(interval64("-P1D") as string);
+select interval64("-P1W"), cast(interval64("-P1W") as string);
+

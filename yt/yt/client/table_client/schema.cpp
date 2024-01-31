24224c8cb74d4d8a6cc0fe7ecb4e78e315e0d945
robot-piglet <robot-piglet@yandex-team.com>
2024-01-31 11:50:40 +0300

    Intermediate changes

--- yt/yt/client/table_client/schema.cpp
+++ yt/yt/client/table_client/schema.cpp
@@ -1514,6 +1514,7 @@ void ValidateColumnSchema(
         "first",
         "xdelta",
         "_yt_stored_replica_set",
+        "_yt_last_seen_replica_set",
     };
 
     const auto& stableName = columnSchema.StableName();

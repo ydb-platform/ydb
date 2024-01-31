c34b9305be00237b62b070ba301c426a73489ef8
Aleksandr Mikishev <amikish@ydb.tech>
2024-01-31 11:24:05 +0300

    Revert ui.sh

--- ydb/library/yql/tools/yqlrun/ui.sh
+++ ydb/library/yql/tools/yqlrun/ui.sh
@@ -8,24 +8,15 @@
 #
 
 SCRIPT_DIR="$(dirname $(readlink -f "$0"))"
-cd $SCRIPT_DIR/../../../../..
-ROOT_DIR=$PWD
-cd -
-
-ASSETS_DIR="${SCRIPT_DIR}/http/www"
-MOUNTS_CFG="${SCRIPT_DIR}/mounts.txt"
-UDFS_DIR="${ROOT_DIR}/ydb/library/yql/udfs"
-GATEWAYS_CFG="${ROOT_DIR}/ydb/library/yql/cfg/tests/gateways.conf"
+UDFS_DIR="${SCRIPT_DIR}/../../udfs;${SCRIPT_DIR}/../../../../../../yql/udfs"
+ASSETS_DIR=${SCRIPT_DIR}/http/www
+MOUNTS_CFG=${SCRIPT_DIR}/mounts.txt
+GATEWAYS_CFG=${SCRIPT_DIR}/../../cfg/tests/gateways.conf
 
 PORT=${1:-3000}
 
-YA="$ROOT_DIR/ya"
-if [ ! -x "$YA" ];  then
-    YA=ya
-fi
-
 if [ "$2" = "--gdb" ]; then
-    GDB="$YA tool gdb --args"
+    GDB="ya tool gdb --args"
 fi
 
 ${GDB} ${SCRIPT_DIR}/yqlrun ui \

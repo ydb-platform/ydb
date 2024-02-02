7975803ac2bf9fa5ee89888743a50cd3b382d7c7
robot-piglet <robot-piglet@yandex-team.com>
2024-02-02 21:28:02 +0300

    Intermediate changes

--- yt/yt/client/bundle_controller_client/public.h
+++ yt/yt/client/bundle_controller_client/public.h
@@ -13,15 +13,21 @@ namespace NYT::NBundleControllerClient {
 DECLARE_REFCOUNTED_STRUCT(TCpuLimits)
 DECLARE_REFCOUNTED_STRUCT(TMemoryLimits)
 DECLARE_REFCOUNTED_STRUCT(TInstanceResources)
+DECLARE_REFCOUNTED_STRUCT(TDefaultInstanceConfig)
+DECLARE_REFCOUNTED_STRUCT(TInstanceSize)
 DECLARE_REFCOUNTED_STRUCT(TBundleTargetConfig)
 DECLARE_REFCOUNTED_STRUCT(TBundleConfigDescriptor)
+DECLARE_REFCOUNTED_STRUCT(TBundleConfigConstraints)
 
 struct TBundleConfigDescriptor;
 
 struct TCpuLimits;
 struct TMemoryLimits;
 struct TInstanceResources;
+struct TDefaultInstanceConfig;
+struct TInstanceSize;
 struct TBundleTargetConfig;
+struct TBundleConfigConstraints;
 
 ////////////////////////////////////////////////////////////////////////////////
 

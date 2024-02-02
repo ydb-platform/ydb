2a9a789ffdaab5f8d1425c573ba3b8ac592cdb5c
Sergey Uzhakov <uzhas@ydb.tech>
2024-02-02 14:58:43 +0000

    fix build

--- ydb/library/yql/core/common_opt/yql_co_simple1.cpp
+++ ydb/library/yql/core/common_opt/yql_co_simple1.cpp
@@ -3087,7 +3087,7 @@ std::unordered_set<ui32> GetUselessSortedJoinInputs(const TCoEquiJoin& equiJoin)
         if (!joinTree->Head().IsAtom("Cross")) {
             std::unordered_map<std::string_view, TPartOfConstraintBase::TSetType> tableJoinKeys;
             for (const auto keys : {joinTree->Child(3), joinTree->Child(4)}) {
-                for (ui32 i = 0U; i < keys->ChildrenSize(); ++i)
+                for (ui32 i = 0U; i < keys->ChildrenSize(); ++i) {
                     tableJoinKeys[keys->Child(i)->Content()].insert_unique(TPartOfConstraintBase::TPathType(1U, keys->Child(i + 1)->Content()));
                     ++i;
                 }

f2d03cc90e79311d66c8378125b338c5f400b7cb
Vitaly Stoyan <vvvv@ydb.tech>
2024-02-05 14:21:14 +0000

    init

---
+++ ydb/library/yql/parser/pg_catalog/dump_system_columns.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eux
+docker run --network=host --detach --name=postgres -e POSTGRES_PASSWORD=1234 -e POSTGRES_USER=root -e POSTGRES_DB=local --rm postgres:14
+sleep 5
+docker exec -i postgres psql postgres://root:1234@localhost:5432/local < columns.sql > columns.txt
+docker exec -i postgres psql postgres://root:1234@localhost:5432/local < pg_class.sql > pg_class.txt
+docker rm -f postgres
+

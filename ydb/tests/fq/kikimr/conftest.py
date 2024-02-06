568911e117b5a4b0b3ef1c8ac96df7c38a923036
Dmitry Kardymon <kardymon-d@ydb.tech>
2024-02-06 10:51:22 +0000

    remove ExtensionPoint

--- ydb/tests/fq/kikimr/conftest.py
+++ ydb/tests/fq/kikimr/conftest.py
@@ -5,7 +5,6 @@ import pytest
 
 from ydb.tests.tools.fq_runner.fq_client import FederatedQueryClient
 from ydb.tests.tools.fq_runner.custom_hooks import *  # noqa: F401,F403 Adding custom hooks for YQv2 support
-from ydb.tests.tools.fq_runner.kikimr_utils import ExtensionPoint
 from ydb.tests.tools.fq_runner.kikimr_utils import YQv2Extension
 from ydb.tests.tools.fq_runner.kikimr_utils import ComputeExtension
 from ydb.tests.tools.fq_runner.kikimr_utils import DefaultConfigExtension

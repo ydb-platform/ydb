ef8b57d3b1c648fcd9e9f667692e906e40f89211
psydmm <psydmm@yandex-team.com>
2024-02-09 11:14:01 +0300

    baseline per project для дефолтного ktlint
    add baseline per project for ktline

--- build/plugins/ytest.py
+++ build/plugins/ytest.py
@@ -655,7 +655,12 @@ def onadd_check(unit, *args):
             extra_test_dart_data['KTLINT_BINARY'] = '$(KTLINT_OLD)/run.bat'
             extra_test_dart_data['USE_KTLINT_OLD'] = 'yes'
         else:
-            extra_test_data = serialize_list([KTLINT_CURRENT_EDITOR_CONFIG])
+            data_list = [KTLINT_CURRENT_EDITOR_CONFIG]
+            baseline_path = unit.get('_KTLINT_BASELINE_FILE')
+            if baseline_path:
+                data_list += [baseline_path]
+                extra_test_dart_data['KTLINT_BASELINE_FILE'] = baseline_path
+            extra_test_data = serialize_list(data_list)
             extra_test_dart_data['KTLINT_BINARY'] = '$(KTLINT)/run.bat'
     elif check_type == "JAVA_STYLE":
         if ymake_java_test and not unit.get('ALL_SRCDIRS'):

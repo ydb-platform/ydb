24479f49110ef282ddb4c8c9075b37d2d540894e
Pisarenko Grigoriy <79596613+GrigoriyPA@users.noreply.github.com>
2024-02-09 14:26:13 +0300

    Moved optimizers from providers/dq to dq (#1758)

--- ydb/library/yql/providers/clickhouse/provider/yql_clickhouse_logical_opt.cpp
+++ ydb/library/yql/providers/clickhouse/provider/yql_clickhouse_logical_opt.cpp
@@ -1,5 +1,6 @@
 #include "yql_clickhouse_provider_impl.h"
 
+#include <ydb/library/yql/dq/expr_nodes/dq_expr_nodes.h>
 #include <ydb/library/yql/providers/clickhouse/expr_nodes/yql_clickhouse_expr_nodes.h>
 #include <ydb/library/yql/providers/dq/expr_nodes/dqs_expr_nodes.h>
 #include <ydb/library/yql/providers/common/provider/yql_provider.h>

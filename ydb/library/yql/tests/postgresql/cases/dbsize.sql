d65c56cfef5d097145e3ac6c98e3eae461882eb6
Vitaly Stoyan <vvvv@ydb.tech>
2024-02-13 12:11:27 +0300

    quick fix for column name of select version() (#1801)

--- ydb/library/yql/tests/postgresql/cases/dbsize.sql
+++ ydb/library/yql/tests/postgresql/cases/dbsize.sql
@@ -1,27 +1,3 @@
-SELECT size, pg_size_pretty(size), pg_size_pretty(-1 * size) FROM
-    (VALUES (10::bigint), (1000::bigint), (1000000::bigint),
-            (1000000000::bigint), (1000000000000::bigint),
-            (1000000000000000::bigint)) x(size);
-SELECT size, pg_size_pretty(size), pg_size_pretty(-1 * size) FROM
-    (VALUES (10::numeric), (1000::numeric), (1000000::numeric),
-            (1000000000::numeric), (1000000000000::numeric),
-            (1000000000000000::numeric),
-            (10.5::numeric), (1000.5::numeric), (1000000.5::numeric),
-            (1000000000.5::numeric), (1000000000000.5::numeric),
-            (1000000000000000.5::numeric)) x(size);
--- test where units change up
-SELECT size, pg_size_pretty(size), pg_size_pretty(-1 * size) FROM
-    (VALUES (10239::bigint), (10240::bigint),
-            (10485247::bigint), (10485248::bigint),
-            (10736893951::bigint), (10736893952::bigint),
-            (10994579406847::bigint), (10994579406848::bigint),
-            (11258449312612351::bigint), (11258449312612352::bigint)) x(size);
-SELECT size, pg_size_pretty(size), pg_size_pretty(-1 * size) FROM
-    (VALUES (10239::numeric), (10240::numeric),
-            (10485247::numeric), (10485248::numeric),
-            (10736893951::numeric), (10736893952::numeric),
-            (10994579406847::numeric), (10994579406848::numeric),
-            (11258449312612351::numeric), (11258449312612352::numeric)) x(size);
 -- pg_size_bytes() tests
 SELECT size, pg_size_bytes(size) FROM
     (VALUES ('1'), ('123bytes'), ('1kB'), ('1MB'), (' 1 GB'), ('1.5 GB '),

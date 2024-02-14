36b8a7a8a031e80d45240cb21af38c78501b303b
Andrey Neporada <aneporada@ydb.tech>
2024-02-14 15:04:08 +0300

    [YQL-17789] Do node type / column order checks for already annotated new nodes

--- ydb/library/yql/core/yql_expr_type_annotation.cpp
+++ ydb/library/yql/core/yql_expr_type_annotation.cpp
@@ -6677,4 +6677,31 @@ TStringBuf NormalizeCallableName(TStringBuf name) {
     return name;
 }
 
+void CheckExpectedTypeAndColumnOrder(const TExprNode& node, TExprContext& ctx, TTypeAnnotationContext& typesCtx) {
+    auto it = typesCtx.ExpectedTypes.find(node.UniqueId());
+    if (it != typesCtx.ExpectedTypes.end()) {
+        YQL_ENSURE(IsSameAnnotation(*node.GetTypeAnn(), *it->second),
+            "Rewrite error, type should be : " <<
+            *it->second << ", but it is: " << *node.GetTypeAnn() << " for node " << node.Content());
+    }
+
+    auto coIt = typesCtx.ExpectedColumnOrders.find(node.UniqueId());
+    if (coIt != typesCtx.ExpectedColumnOrders.end()) {
+        TColumnOrder oldColumnOrder = coIt->second;
+        TMaybe<TColumnOrder> newColumnOrder = typesCtx.LookupColumnOrder(node);
+        if (!newColumnOrder) {
+            // keep column order after rewrite
+            // TODO: check if needed
+            auto status = typesCtx.SetColumnOrder(node, oldColumnOrder, ctx);
+            YQL_ENSURE(status == IGraphTransformer::TStatus::Ok);
+        } else {
+            YQL_ENSURE(newColumnOrder == oldColumnOrder,
+                "Rewrite error, column order should be: "
+                << FormatColumnOrder(oldColumnOrder) << ", but it is: "
+                << FormatColumnOrder(newColumnOrder) << " for node "
+                << node.Content());
+        }
+    }
+}
+
 } // NYql

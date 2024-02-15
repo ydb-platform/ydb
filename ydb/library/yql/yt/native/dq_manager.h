c79a50623450477e6f22124699835707e3400cfa
Marina Pereskokova <35163152+Krisha11@users.noreply.github.com>
2024-02-15 13:59:10 +0300

    Add dq initialization in yt yql plugin (#1615)

---
+++ ydb/library/yql/yt/native/dq_manager.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <yt/yt/core/ytree/yson_struct.h>
+
+#include <ydb/library/actors/core/actorsystem.h>
+#include <ydb/library/yql/core/file_storage/file_storage.h>
+#include <ydb/library/yql/providers/dq/global_worker_manager/coordination_helper.h>
+#include <ydb/library/yql/providers/dq/service/service_node.h>
+#include <ydb/library/yql/providers/common/metrics/metrics_registry.h>
+
+#include <library/cpp/yt/yson_string/string.h>
+#include <library/cpp/yt/memory/ref_counted.h>
+
+namespace NYT::NYqlPlugin {
+
+using namespace NYson;
+using namespace NYql;
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct TDqManagerConfig
+    : public NYTree::TYsonStruct
+{
+    ui16 InterconnectPort;
+    ui16 GrpcPort;
+    ui32 ActorThreads;
+    bool UseIPv4;
+
+    std::vector<NYTree::INodePtr> YtBackends;
+
+    NYTree::INodePtr YtCoordinator;
+    NYTree::INodePtr Scheduler;
+    NYTree::INodePtr ICSettings;
+
+    TMap<TString, TString> UdfsWithMd5; // autofilled by yql_plugin
+    NYql::TFileStoragePtr FileStorage; // autofilled by yql_plugin
+
+    REGISTER_YSON_STRUCT(TDqManagerConfig);
+
+    static void Register(TRegistrar registrar);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+DECLARE_REFCOUNTED_STRUCT(TDqManagerConfig)
+DEFINE_REFCOUNTED_TYPE(TDqManagerConfig)
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TDqManager
+    : public TRefCounted
+{
+public:
+    explicit TDqManager(const TDqManagerConfigPtr& config);
+    void Start();
+
+private:
+    const TDqManagerConfigPtr& Config_;
+
+    NActors::TActorSystem* ActorSystem_;
+    ICoordinationHelper::TPtr Coordinator_;
+    THolder<TServiceNode> ServiceNode_;
+    IMetricsRegistryPtr MetricsRegistry_;
+    NActors::IActor* StatsCollector_;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+DECLARE_REFCOUNTED_CLASS(TDqManager)
+DEFINE_REFCOUNTED_TYPE(TDqManager)
+
+///////////////////////////////////////////
+
+} // namespace NYT::NYqlPlugin
+

9b0c77002c17234c278368adcf7dc25b0c6a082f
Vadim Averin <avevad@ydb.tech>
2024-02-20 16:32:26 +0000

    Separate simple wide node implementation from general headers

--- ydb/library/yql/minikql/comp_nodes/mkql_skip.cpp
+++ ydb/library/yql/minikql/comp_nodes/mkql_skip.cpp
@@ -1,6 +1,7 @@
 #include "mkql_skip.h"
 #include <ydb/library/yql/minikql/computation/mkql_computation_node_holders.h>
 #include <ydb/library/yql/minikql/computation/mkql_computation_node_codegen.h>  // Y_IGNORE
+#include <ydb/library/yql/minikql/computation/mkql_computation_node_codegen_impl.h>
 #include <ydb/library/yql/minikql/mkql_node_cast.h>
 
 namespace NKikimr {

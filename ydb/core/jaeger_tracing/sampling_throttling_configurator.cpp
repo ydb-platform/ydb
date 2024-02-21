3f32ce5f9b944475ea91b999969a4acb8eb1cd02
Oleg Shatov <olegsh@ydb.tech>
2024-02-21 09:48:12 +0100

    Rewritten sampling and throttling controls to share limits and implement scoping (#1902)

--- ydb/core/jaeger_tracing/sampling_throttling_configurator.cpp
+++ ydb/core/jaeger_tracing/sampling_throttling_configurator.cpp
@@ -1,7 +1,7 @@
-#include "sampler.h"
 #include "sampling_throttling_configurator.h"
+
 #include "sampling_throttling_control.h"
-#include "throttler.h"
+#include "sampling_throttling_control_internals.h"
 
 #include <library/cpp/random_provider/random_provider.h>
 #include <library/cpp/time_provider/time_provider.h>
@@ -9,50 +9,40 @@
 
 namespace NKikimr::NJaegerTracing {
 
-TIntrusivePtr<TSamplingThrottlingControl> TSamplingThrottlingConfigurator::GetControl() {
-    TSampler sampler(SamplingPPM, RandomProvider->GenRand64());
-    TThrottler sampledThrottler(MaxSampledPerMinute, MaxSampledBurst, TimeProvider);
-    TThrottler externalThrottler(MaxExternalPerMinute, MaxExternalBurst, TimeProvider);
+TSamplingThrottlingConfigurator::TSamplingThrottlingConfigurator(TIntrusivePtr<ITimeProvider> timeProvider,
+                                                                 TIntrusivePtr<IRandomProvider>& randomProvider)
+    : TimeProvider(std::move(timeProvider))
+    , Rng(randomProvider->GenRand64())
+    , CurrentSettings(GenerateThrottlers({}))
+{}
 
-    return TIntrusivePtr(new TSamplingThrottlingControl(std::move(sampler), SamplingLevel, std::move(sampledThrottler), std::move(externalThrottler)));
+TIntrusivePtr<TSamplingThrottlingControl> TSamplingThrottlingConfigurator::GetControl() {
+    auto control = TIntrusivePtr(new TSamplingThrottlingControl(GenerateSetup()));
+    IssuedControls.push_back(control);
+    return control;
 }
 
-TMaybe<TString> TSamplingThrottlingConfigurator::HandleConfigs(const NKikimrConfig::TTracingConfig& config) {
-    if (config.SamplingSize() > 1) {
-        return "Only global scope is supported, thus no more than one sampling scope is allowed";
+void TSamplingThrottlingConfigurator::UpdateSettings(TSettings<double, TThrottlingSettings> settings) {
+    CurrentSettings = GenerateThrottlers(std::move(settings));
+
+    for (auto& control : IssuedControls) {
+        control->UpdateImpl(GenerateSetup());
     }
-    if (config.ExternalThrottlingSize() > 1) {
-        return "Only global scope is supported, thus no more than one throttling scope is allowed";
-    }
+}
 
-    if (config.SamplingSize() == 1) {
-        const auto& samplingConfig = config.GetSampling(0);
-        if (!(samplingConfig.HasFraction() && samplingConfig.HasLevel() && samplingConfig.HasMaxRatePerMinute())) {
-            return "At least one required field (fraction, level, max_rate_per_minute) is missing in scope " + samplingConfig.ShortDebugString();
-        }
-        const auto samplingFraction = samplingConfig.GetFraction();
+TSettings<double, TIntrusivePtr<TThrottler>> TSamplingThrottlingConfigurator::GenerateThrottlers(
+    TSettings<double, TThrottlingSettings> settings) {
+    return settings.MapThrottler([this](const TThrottlingSettings& settings) {
+        return MakeIntrusive<TThrottler>(settings.MaxRatePerMinute, settings.MaxBurst, TimeProvider);
+    });
+}
 
-        if (samplingFraction < 0 || samplingFraction > 1) {
-            return "Sampling fraction violates range [0, 1] " + ToString(samplingFraction);
-        }
+std::unique_ptr<TSamplingThrottlingControl::TSamplingThrottlingImpl> TSamplingThrottlingConfigurator::GenerateSetup() {
+    auto setup = CurrentSettings.MapSampler([this](double fraction) {
+        return TSampler(fraction, Rng());
+    });
 
-        SamplingPPM.Set(samplingFraction * 1000000);
-        SamplingLevel.Set(samplingConfig.GetLevel());
-        MaxSampledPerMinute.Set(samplingConfig.GetMaxRatePerMinute());
-        MaxSampledBurst.Set(samplingConfig.GetMaxBurst());
-    }
-
-    if (config.ExternalThrottlingSize()) {
-        const auto& throttlingConfig = config.externalthrottling(0);
-        if (!throttlingConfig.HasMaxRatePerMinute()) {
-            return "max_rate_per_minute is missing in this scope: " + throttlingConfig.ShortDebugString();
-        }
-
-        MaxExternalPerMinute.Set(throttlingConfig.GetMaxRatePerMinute());
-        MaxExternalBurst.Set(throttlingConfig.GetMaxBurst());
-    }
-
-    return {};
+    return std::make_unique<TSamplingThrottlingControl::TSamplingThrottlingImpl>(std::move(setup));
 }
 
 } // namespace NKikimr::NJaegerTracing
--- ydb/tools/cfg/validation.py
+++ ydb/tools/cfg/validation.py
@@ -128,7 +128,9 @@ SYS_SCHEMA = {
 
 SELECTORS_CONFIGS = dict(
     type="object",
-    properties={},
+    properties=dict(
+        request_type=dict(type="string"),
+    ),
     required=[],
     additionalProperties=False,
 )

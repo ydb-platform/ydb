333feede63ea492968b7dc2fb822aae172567a99
Mars Agliullin <marsaly@ydb.tech>
2024-02-23 18:01:39 +0300

    YQL-17315: PG test table deps, part 2 (#2209)

--- ydb/library/yql/tests/postgresql/cases/expressions.sql
+++ ydb/library/yql/tests/postgresql/cases/expressions.sql
@@ -13,6 +13,14 @@ SELECT now()::timetz(4)::text = current_time(4)::text;
 SELECT current_timestamp = NOW();
 -- precision
 SELECT length(current_timestamp::text) >= length(current_timestamp(0)::text);
+select count(*) from date_tbl
+  where f1 between '1997-01-01' and '1998-01-01';
+select count(*) from date_tbl
+  where f1 not between '1997-01-01' and '1998-01-01';
+select count(*) from date_tbl
+  where f1 between symmetric '1997-01-01' and '1998-01-01';
+select count(*) from date_tbl
+  where f1 not between symmetric '1997-01-01' and '1998-01-01';
 --
 -- Test parsing of a no-op cast to a type with unspecified typmod
 --

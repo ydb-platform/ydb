b4e9dfe985839c0020627622e058da945d832850
nsofya <nsofya@ydb.tech>
2024-02-23 15:07:14 +0300

    Fix memory usage for heavy normalizers (#2204)
    Co-authored-by: nsofya <nsofya@yandex.ru>

--- ydb/core/tx/columnshard/normalizer/abstract/abstract.h
+++ ydb/core/tx/columnshard/normalizer/abstract/abstract.h
@@ -49,6 +49,11 @@ namespace NKikimr::NOlap {
     class TNormalizationContext {
         YDB_ACCESSOR_DEF(TActorId, ResourceSubscribeActor);
         YDB_ACCESSOR_DEF(TActorId, ColumnshardActor);
+        std::shared_ptr<NOlap::NResourceBroker::NSubscribe::TResourcesGuard> ResourcesGuard;
+    public:
+        void SetResourcesGuard(std::shared_ptr<NOlap::NResourceBroker::NSubscribe::TResourcesGuard> rg) {
+            ResourcesGuard = rg;
+        }
     };
 
     class TNormalizationController;
@@ -117,7 +122,7 @@ namespace NKikimr::NOlap {
         TString DebugString() const {
             return TStringBuilder() << "normalizers_count=" << Normalizers.size()
                                     << ";current_normalizer_idx=" << CurrentNormalizerIndex
-                                    << ";current_normalizer=" << (CurrentNormalizerIndex < Normalizers.size()) ? Normalizers[CurrentNormalizerIndex]->GetName() : "";
+                                    << ";current_normalizer=" << (CurrentNormalizerIndex < Normalizers.size() ? Normalizers[CurrentNormalizerIndex]->GetName() : "");
         }
 
         const INormalizerComponent::TPtr& GetNormalizer() const;

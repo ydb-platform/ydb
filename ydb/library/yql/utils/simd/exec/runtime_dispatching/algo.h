a3d91d9895b4e469b6640a046fcc947e533a00be
san-kir-k <dfomka@list.ru>
2024-02-24 20:05:31 +0300

    Implementation of columns merge algo
    Intermediate implementation of the columns merge algorith using simd
    library.
    
    Currently algorithm doesn't work for columns of arbitary length.
    
    Also some code improvement in commit.

--- ydb/library/yql/utils/simd/exec/runtime_dispatching/algo.h
+++ ydb/library/yql/utils/simd/exec/runtime_dispatching/algo.h
@@ -21,22 +21,15 @@ struct Perfomancer {
 
         virtual ~Interface() = default;
 
-        inline virtual void Add(vvl& columns, vl& result) {
-
-            // to avoid clang(-Wunused-parameter)
-            columns[0];
-            result[0];
-        }
-
+        virtual void Add(vvl& /* columns */, vl& /* result */) = 0;
+        virtual void MergeColumns(ui8* /* result */, const ui8* /* data */ [4], size_t /* sizes */ [4]) = 0;
     };
 
 
     template <typename Trait>
-    struct Algo : Interface {
-
-        Algo() {}
-
-        inline void Add(vvl& columns, vl& result) override {
+    class Algo : public Interface {
+    public:
+        void Add(vvl& columns, vl& result) override {
             std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
             std::vector<Trait> Registers(columns.size());
@@ -57,11 +50,164 @@ struct Perfomancer {
             Cerr << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - begin).count() << "ms\n";
         }
 
-        ~Algo() = default;
+        void MergeColumns(ui8* result, const ui8* data[4], size_t sizes[4]) override {
+            std::vector<Trait> regs(16);
+
+            Trait mask1  = CreateShuffleToBlendMask(sizes[0], sizes[1], false);
+            Trait mask2  = CreateShuffleToBlendMask(sizes[1], sizes[0], true);
+            Trait mask3  = CreateShuffleToBlendMask(sizes[2], sizes[3], false);
+            Trait mask4  = CreateShuffleToBlendMask(sizes[3], sizes[2], true);
+            Trait mask5  = CreateBlendMask(sizes[0] + sizes[1], sizes[2] + sizes[3], false);
+            Trait mask6  = CreatePureShuffleMask(sizes[0], sizes[1]);
+            Trait mask7  = CreatePureShuffleMask(sizes[1], sizes[0]);
+            Trait mask8  = CreatePureShuffleMask(sizes[2], sizes[3]);
+            Trait mask9  = CreatePureShuffleMask(sizes[3], sizes[2]);
+            Trait mask10 = CreatePureShuffleMask(sizes[0] + sizes[1], sizes[2] + sizes[3]);
+            Trait mask11 = CreatePureShuffleMask(sizes[2] + sizes[3], sizes[0] + sizes[1]);
+            Trait mask12 = CreateBlendMask(sizes[0], sizes[1], false);
+            Trait mask13 = CreateBlendMask(sizes[2], sizes[3], false);
+            Trait mask14 = CreateShuffleToBlendMask(sizes[0] + sizes[1], sizes[2] + sizes[3], false);
+            Trait mask15 = CreateShuffleToBlendMask(sizes[2] + sizes[3], sizes[0] + sizes[1], true);
+
+            regs[0].Load(data[0]);
+            regs[1].Load(data[1]);
+            regs[2].Load(data[2]);
+            regs[3].Load(data[3]);
+
+            regs[4] = regs[0].Shuffle<false>(mask1);
+            regs[5] = regs[1].Shuffle<false>(mask2);
+            regs[6] = regs[2].Shuffle<false>(mask3);
+            regs[7] = regs[3].Shuffle<false>(mask4);
+
+            regs[0] = regs[0].Shuffle<false>(mask6);
+            regs[0] = regs[0].Shuffle<false>(mask1);
+
+            regs[1] = regs[1].Shuffle<false>(mask7);
+            regs[1] = regs[1].Shuffle<false>(mask2);
+
+            regs[2] = regs[2].Shuffle<false>(mask8);
+            regs[2] = regs[2].Shuffle<false>(mask3);
+
+            regs[3] = regs[3].Shuffle<false>(mask9);
+            regs[3] = regs[3].Shuffle<false>(mask4);
+
+            regs[8] = regs[4].BlendVar(regs[5], mask12);
+            regs[9] = regs[0].BlendVar(regs[1], mask12);
+
+            regs[10] = regs[6].BlendVar(regs[7], mask13);
+            regs[11] = regs[2].BlendVar(regs[3], mask13);
+
+            regs[0] = regs[8].Shuffle<false>(mask14);
+            regs[2] = regs[10].Shuffle<false>(mask15);
+
+            regs[8]  = regs[10].Shuffle<false>(mask10);
+            regs[10] = regs[10].Shuffle<false>(mask11);
+
+            regs[8]  = regs[8].Shuffle<false>(mask14);
+            regs[10] = regs[10].Shuffle<false>(mask15);
+
+            regs[12] = regs[0].BlendVar(regs[2], mask5);
+            regs[13] = regs[8].BlendVar(regs[10], mask5);
+
+            regs[1] = regs[9].Shuffle<false>(mask14);
+            regs[3] = regs[11].Shuffle<false>(mask15);
+
+            regs[9]  = regs[9].Shuffle<false>(mask10);
+            regs[11] = regs[11].Shuffle<false>(mask11);
+
+            regs[9]  = regs[9].Shuffle<false>(mask14);
+            regs[11] = regs[11].Shuffle<false>(mask15);
+
+            regs[14] = regs[1].BlendVar(regs[3], mask5);
+            regs[15] = regs[9].BlendVar(regs[11], mask5);
+
+            regs[12].Store(result);
+        }
+
+    private:
+        Trait CreateBlendMask(size_t size1, size_t size2, bool shift) {
+            i8 result[Trait::SIZE];
+            size_t cnt = 0;
+
+            if (shift) {
+                for (size_t i = 0; i < size2; ++i) {
+                    result[cnt++] = 0;
+                }
+            }
+
+            while (cnt + size1 + size2 <= Trait::SIZE) {
+                for (size_t i = 0; i < size1; ++i) {
+                    result[cnt++] = 0x80;
+                }
+
+                if (cnt + size2 > Trait::SIZE) {
+                    break;
+                }
+
+                for (size_t i = 0; i < size2; ++i) {
+                    result[cnt++] = 0;
+                }
+            }
+
+            Trait reg{result};
+            return reg;
+        }
+
+        Trait CreateShuffleToBlendMask(size_t size1, size_t size2, bool shift) {
+            i8 result[Trait::SIZE]{0};
+            size_t packs = Trait::SIZE / (size1 + size2);
+            size_t cnt = 0;
+            size_t order = 0;
+
+            if (shift) {
+                while (cnt < size2) {
+                    result[cnt++] = 0;
+                }
+            }
+
+            while (cnt < packs * (size1 + size2)) {
+                if (shift) {
+                    if (cnt % (size1 + size2) < size2) {
+                        result[cnt++] = 0;
+                    } else {
+                        result[cnt++] = order++;
+                    }
+                } else {
+                    if (cnt % (size1 + size2) < size1) {
+                        result[cnt++] = order++;
+                    } else {
+                        result[cnt++] = 0;
+                    }
+                }
+            }
+
+            Trait reg{result};
+            return reg;
+        }
+
+        Trait CreatePureShuffleMask(size_t size1, size_t size2) {
+            i8 result[Trait::SIZE];
+            size_t packs = Trait::SIZE / (size1 + size2);
+            size_t cnt = 0;
+            size_t start = packs * size1;
+
+            //start with pack * size1 
+            while (cnt < packs * size1) {
+                result[cnt++] = start++;
+            }
+
+            start = 0;
+            while (cnt < Trait::SIZE) {
+                result[cnt++] = start++;
+            }
+
+            Trait reg{result};
+            return reg;
+        } 
     };
 
     template <typename Trait>
-    inline THolder<Interface> Create() {
+    THolder<Interface> Create() {
         return MakeHolder<Interface>();
     }
 

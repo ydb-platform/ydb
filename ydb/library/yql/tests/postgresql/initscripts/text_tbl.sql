132c28161b6ecbaeecf19ab793f6c8b91e6ff2c4
Mars Agliullin <marsaly@ydb.tech>
2024-03-04 20:47:55 +0300

    Added lowercase version of text_tbl table

--- ydb/library/yql/tests/postgresql/initscripts/text_tbl.sql
+++ ydb/library/yql/tests/postgresql/initscripts/text_tbl.sql
@@ -3,3 +3,7 @@ CREATE TABLE TEXT_TBL (f1 text);
 INSERT INTO TEXT_TBL VALUES ('doh!');
 INSERT INTO TEXT_TBL VALUES ('hi de ho neighbor');
 
+CREATE TABLE text_tbl (f1 text);
+INSERT INTO text_tbl
+SELECT * from TEXT_TBL;
+

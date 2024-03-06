b636bcc179dc80813983a82f581e8f9d3ce2ee9c
azevaykin <azevaykin@ydb.tech>
2024-03-06 13:03:05 +0000

    InnerProductDistance

--- ydb/library/yql/udfs/common/knn/knn.cpp
+++ ydb/library/yql/udfs/common/knn/knn.cpp
@@ -60,7 +60,7 @@ SIMPLE_STRICT_UDF(TToBinaryString, char*(TListType<double>)) {
 SIMPLE_STRICT_UDF(TFromBinaryString, TOptional<TListType<double>>(const char*)) {
     TStringRef str = args[0].AsStringRef();
     if (str.Size() == 0)
-        return TUnboxedValue(TUnboxedValuePod());
+        return TUnboxedValue();
 
     const EFormat format = (EFormat)str.Data()[0];
     str = TStringRef{str.Data() + 1, str.Size() -1};
@@ -70,13 +70,60 @@ SIMPLE_STRICT_UDF(TFromBinaryString, TOptional<TListType<double>>(const char*)) 
             break;
         }
         default:
-            return TUnboxedValue(TUnboxedValuePod());
+            return TUnboxedValue();
     }
+}
+
+
+std::optional<double> InnerProductDistance(const TUnboxedValuePod vector1, const TUnboxedValuePod vector2) {
+    double ret = 0;
+
+    if (vector1.HasFastListLength() && vector2.HasFastListLength()) {
+        const auto elements1 = vector1.GetElements();
+        const auto elements2 = vector2.GetElements();
+        const auto size1 = vector1.GetListLength();
+        const auto size2 = vector2.GetListLength();
+        
+        if (size1 != size2)
+            return {};
+
+        for (ui32 i = 0; i < size1; ++i) {
+            double element1 = elements1[i].Get<double>();
+            double element2 = elements2[i].Get<double>();
+            ret += element1 * element2;
+        }
+    } else {
+        TUnboxedValue value1, value2;
+        const auto it1 = vector1.GetListIterator();
+        const auto it2 = vector2.GetListIterator();    
+        for (; it1.Next(value1) && it2.Next(value2);) {
+            ret += value1.Get<double>() * value2.Get<double>();
+        }
+
+        // Lenght mismatch
+        if (it1.Next(value1) || it2.Next(value2))
+            return {};
+    }
+
+    return ret;
+}
+
+SIMPLE_STRICT_UDF(TInnerProductDistance, TOptional<double>(TOptional<TListType<double>>, TOptional<TListType<double>>)) {
+    Y_UNUSED(valueBuilder);
+
+    if (!args[0].HasValue() || !args[1].HasValue())
+        return TUnboxedValue(TUnboxedValuePod());
+
+    if (auto distance = InnerProductDistance(args[0], args[1]))
+        return TUnboxedValuePod{distance.value()};
+    else 
+        return TUnboxedValuePod();
 }
 
 SIMPLE_MODULE(TKnnModule,
     TFromBinaryString, 
-    TToBinaryString
+    TToBinaryString,
+    TInnerProductDistance
     )
 
 REGISTER_MODULES(TKnnModule)
---
+++ ydb/library/yql/udfs/common/knn/test/cases/InnerProductDistance.sql
@@ -0,0 +1,26 @@
+--fixed size vector
+$vector1 = [1.0, 2.0, 3.0];
+$vector2 = [4.0, 5.0, 6.0];
+$extected_distance = 32;
+$distance = Knn::InnerProductDistance($vector1, $vector2);
+select $distance = $extected_distance;
+
+--lazy vector
+$lazy_vector1 = ListFromRange(1.0, 4.0);
+$lazy_vector2 = ListFromRange(4.0, 7.0);
+$distance = Knn::InnerProductDistance($lazy_vector1, $lazy_vector2);
+select $distance = $extected_distance;
+
+--good deserialized vector
+$deserialized_vector2 = Knn::FromBinaryString(Knn::ToBinaryString($vector2));
+$distance =  Knn::InnerProductDistance($vector1, $deserialized_vector2);
+select $distance = $extected_distance;
+
+--size mismatch
+$bad_vector1 = [1.0];
+$bad_vector2 = [4.0, 5.0, 6.0];
+select Knn::InnerProductDistance($bad_vector1, $bad_vector2);
+
+--bad deserialized vector
+$bad_deserialized_vector2 = Knn::FromBinaryString("WrongString");
+select Knn::InnerProductDistance($vector1, $bad_deserialized_vector2);

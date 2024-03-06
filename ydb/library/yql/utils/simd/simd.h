6f31bd8b9500a93a56884d2899d8cf153e6f6826
starlinskiy <starlinskii@ydb.tech>
2024-03-06 14:11:03 +0300

    speed x2

--- ydb/library/yql/utils/simd/simd.h
+++ ydb/library/yql/utils/simd/simd.h
@@ -316,7 +316,7 @@ struct Perfomancer {
 
             size_t i = 0;
 
-            for (; i * sizes[0] + Trait::SIZE < length * sizes[0]; i += Trait::SIZE / pack) {
+            for (; i * sizes[0] + Trait::SIZE < length * sizes[0]; i += Trait::SIZE / pack * 2) {
                 Iteration(sizes, data, result, i, block * i, block, reg, mask);
             }
             MergeEnds(result, data, sizes, length, i, pack * i);

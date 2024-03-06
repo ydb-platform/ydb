ydb-platform/ydb

commit d872d6d07925a43f816723d3831acd01f2b36538

Disable partial Sorted constraints (#2194)

ydb/library/yql/core/ut/yql_expr_constraint_ut.cpp
@@ -290,7 +290,7 @@ Y_UNIT_TEST_SUITE(TYqlExprConstraints) {
 
         TExprContext exprCtx;
         const auto exprRoot = ParseAndAnnotate(s, exprCtx);
-        CheckConstraint<TSortedConstraintNode>(exprRoot, "Sort", "Sorted(key[asc])");
+        CheckConstraint<TSortedConstraintNode>(exprRoot, "Sort", "");
     }
 
     Y_UNIT_TEST(SortDesc) {

ydb/library/yql/core/yql_expr_constraint.cpp
@@ -787,8 +787,7 @@ private:
                         else if (columns.cend() != it)
                             it = columns.erase(it);
                     } else {
-                        columns.resize(i);
-                        break;
+                        return {};
                     }
                 }
             } else

ydb/library/yql/tests/sql/suites/optimizers/keep_sort_by_calc_field.cfg
@@ -1,3 +1,0 @@
-in Input input3.txt
-out Output output.txt
-res result.txt

ydb/library/yql/tests/sql/suites/optimizers/keep_sort_by_calc_field.sql
@@ -1,14 +1,0 @@
-/* postgres can not */
-/* multirun can not */
-/* kikimr can not - table truncate */
-USE plato;
-
-INSERT INTO Output WITH TRUNCATE
-SELECT *
-FROM Input
-ORDER BY key, length(value);
-
-COMMIT;
-
-SELECT * FROM Output
-WHERE key != "";

ydb/library/yql/tests/sql/suites/order_by/order_by_expr_over_sorted_table.cfg
@@ -1,0 +1,2 @@
+in Input sorted_table1.txt
+udf string_udf

ydb/library/yql/tests/sql/suites/order_by/order_by_expr_over_sorted_table.sql
@@ -1,0 +1,2 @@
+select key, value from plato.Input
+order by key, String::SplitToList(value, "$", 2 as Limit)[0]

ydb/library/yql/tests/sql/suites/order_by/sorted_table1.txt.attr
@@ -1,0 +1,12 @@
+{"_yql_row_spec"={
+	"Type"=["StructType";[
+		["key";["DataType";"String"]];
+		["subkey";["DataType";"String"]];
+		["value";["DataType";"String"]]
+	]];
+	"SortDirections"=[1;1;];
+	"SortedBy"=["key";"subkey";];
+	"SortedByTypes"=[["DataType";"String";];["DataType";"String";];];
+	"SortMembers"=["key";"subkey";];
+}}
+

commit 849e3c114d51bcd7996197472f7489606f77ee6a

Return back wrong behavior introduced in https://github.com/ydb-platform/ydb/pull/2194

ydb/library/yql/core/ut/yql_expr_constraint_ut.cpp
@@ -111,7 +111,7 @@ Y_UNIT_TEST_SUITE(TYqlExprConstraints) {
 
         TExprContext exprCtx;
         const auto exprRoot = ParseAndAnnotate(s, exprCtx);
-        CheckConstraint<TSortedConstraintNode>(exprRoot, "Sort", "Sorted(key[desc])");
+        CheckConstraint<TSortedConstraintNode>(exprRoot, "Sort", "");
     }
 
     Y_UNIT_TEST(SortByTranspentIfPresent) {
@@ -309,7 +309,7 @@ Y_UNIT_TEST_SUITE(TYqlExprConstraints) {
 
         TExprContext exprCtx;
         const auto exprRoot = ParseAndAnnotate(s, exprCtx);
-        CheckConstraint<TSortedConstraintNode>(exprRoot, "Sort", "Sorted(key[asc])");
+        CheckConstraint<TSortedConstraintNode>(exprRoot, "Sort", "");
     }
 
     Y_UNIT_TEST(SortDesc) {

ydb/library/yql/core/yql_expr_constraint.cpp
@@ -787,8 +787,7 @@ private:
                         else if (columns.cend() != it)
                             it = columns.erase(it);
                     } else {
-                        columns.resize(i);
-                        break;
+                        return {};
                     }
                 }
             } else
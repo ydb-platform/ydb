40afce6e596d9166b8512d1d2d9f3822ab7185fd
azevaykin <azevaykin@ydb.tech>
2024-03-07 09:04:34 +0000

    Trivial PR fixes

--- ydb/library/yql/udfs/common/knn/knn.cpp
+++ ydb/library/yql/udfs/common/knn/knn.cpp
@@ -37,7 +37,7 @@ TString SerializeDoubleVector(const TUnboxedValuePod x) {
 
 NYql::NUdf::TUnboxedValue DeserializeDoubleVector(const IValueBuilder *valueBuilder, TStringRef str) {
     if (str.Size() % sizeof(double) != 0)    
-        return TUnboxedValue(TUnboxedValuePod());
+        return {};
     
     const ui32 count = str.Size() / sizeof(double);
 
@@ -60,17 +60,15 @@ SIMPLE_STRICT_UDF(TToBinaryString, char*(TListType<double>)) {
 SIMPLE_STRICT_UDF(TFromBinaryString, TOptional<TListType<double>>(const char*)) {
     TStringRef str = args[0].AsStringRef();
     if (str.Size() == 0)
-        return TUnboxedValue();
+        return {};
 
-    const EFormat format = (EFormat)str.Data()[0];
+    const EFormat format = static_cast<EFormat>(str.Data()[0]);
     str = TStringRef{str.Data() + 1, str.Size() -1};
     switch (format) {
-        case EFormat::DoubleVector: {
+        case EFormat::DoubleVector: 
             return DeserializeDoubleVector(valueBuilder, str);
-            break;
-        }
         default:
-            return TUnboxedValue();
+            return {};
     }
 }
 
@@ -78,9 +76,9 @@ SIMPLE_STRICT_UDF(TFromBinaryString, TOptional<TListType<double>>(const char*)) 
 std::optional<double> InnerProductDistance(const TUnboxedValuePod vector1, const TUnboxedValuePod vector2) {
     double ret = 0;
 
-    if (vector1.HasFastListLength() && vector2.HasFastListLength()) {
-        const auto elements1 = vector1.GetElements();
-        const auto elements2 = vector2.GetElements();
+    const auto elements1 = vector1.GetElements();
+    const auto elements2 = vector2.GetElements();
+    if (elements1 && elements2) {
         const auto size1 = vector1.GetListLength();
         const auto size2 = vector2.GetListLength();
         
@@ -97,7 +95,9 @@ std::optional<double> InnerProductDistance(const TUnboxedValuePod vector1, const
         const auto it1 = vector1.GetListIterator();
         const auto it2 = vector2.GetListIterator();    
         for (; it1.Next(value1) && it2.Next(value2);) {
-            ret += value1.Get<double>() * value2.Get<double>();
+            double element1 = value1.Get<double>();
+            double element2 = value2.Get<double>();
+            ret += element1 * element2;
         }
 
         // Lenght mismatch
@@ -112,12 +112,13 @@ SIMPLE_STRICT_UDF(TInnerProductDistance, TOptional<double>(TOptional<TListType<d
     Y_UNUSED(valueBuilder);
 
     if (!args[0].HasValue() || !args[1].HasValue())
-        return TUnboxedValue(TUnboxedValuePod());
+        return {};
 
-    if (auto distance = InnerProductDistance(args[0], args[1]))
-        return TUnboxedValuePod{distance.value()};
-    else 
-        return TUnboxedValuePod();
+    auto distance = InnerProductDistance(args[0], args[1]);
+    if (!distance)
+        return {};
+
+    return TUnboxedValuePod{distance.value()};
 }
 
 SIMPLE_MODULE(TKnnModule,

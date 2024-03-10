fe08f0ff2f7daa6207524d3f577da495543525d1
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-03-10 20:56:05 +0300

    fix build

--- ydb/core/tx/columnshard/columnshard_impl.cpp
+++ ydb/core/tx/columnshard/columnshard_impl.cpp
@@ -735,6 +735,9 @@ bool TColumnShard::SetupTtl(const THashMap<ui64, NOlap::TTiering>& pathTtls) {
         ACFL_DEBUG("background", "ttl")("skip_reason", "in_progress");
         return false;
     }
+    if (force) {
+        TablesManager.MutablePrimaryIndex().OnTieringModified(Tiers, TablesManager.GetTtl(), {});
+    }
     THashMap<ui64, NOlap::TTiering> eviction = pathTtls;
     for (auto&& i : eviction) {
         ACFL_DEBUG("background", "ttl")("path", i.first)("info", i.second.GetDebugString());

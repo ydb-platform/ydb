320c113da1b09c697370a91a849205e237d8de4a
Oleg Shatov <olegsh@ydb.tech>
2024-03-12 11:07:15 +0000

    Removed tracing level Full

--- ydb/core/tx/datashard/datashard_pipeline.cpp
+++ ydb/core/tx/datashard/datashard_pipeline.cpp
@@ -1783,7 +1783,7 @@ EExecutionStatus TPipeline::RunExecutionPlan(TOperation::TPtr op,
             return EExecutionStatus::Reschedule;
         }
 
-        NWilson::TSpan unitSpan(TWilsonTablet::TabletFull, txc.TransactionExecutionSpan.GetTraceId(), "Datashard.Unit");
+        NWilson::TSpan unitSpan(TWilsonTablet::TabletDetailed, txc.TransactionExecutionSpan.GetTraceId(), "Datashard.Unit");
         
         NCpuTime::TCpuTimer timer;
         auto status = unit.Execute(op, txc, ctx);

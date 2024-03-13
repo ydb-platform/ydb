a8967ea9a553c8df35a6d4dd8e9ca505d6439c26
Evgeny Zverev <zverevgeny@ydb.tech>
2024-03-13 18:58:43 +0300

    fix typo

--- ydb/library/yql/providers/generic/actors/yql_generic_read_actor.cpp
+++ ydb/library/yql/providers/generic/actors/yql_generic_read_actor.cpp
@@ -98,7 +98,7 @@ namespace NYql::NDq {
         template<typename T>
         T ExtractFromConstFuture(const NThreading::TFuture<T>& f) {
             //We want to avoid making a copy of data stored in a future.
-            //But there is no direct way to extract data from a const future5
+            //But there is no direct way to extract data from a const future
             //So, we make a copy of the future, that is cheap. Then, extract the value from this copy.
             //It destructs the value in the original future, but this trick is legal and documented here:
             //https://docs.yandex-team.ru/arcadia-cpp/cookbook/concurrency

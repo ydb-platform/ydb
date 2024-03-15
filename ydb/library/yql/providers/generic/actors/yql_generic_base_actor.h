28aabd988a033c901e58cd292447089ba709d916
Evgeny Zverev <zverevgeny@ydb.tech>
2024-03-15 14:52:30 +0000

    fix style

--- ydb/library/yql/providers/generic/actors/yql_generic_base_actor.h
+++ ydb/library/yql/providers/generic/actors/yql_generic_base_actor.h
@@ -8,90 +8,88 @@
 
 namespace NYql::NDq {
 
-template<typename TDerived>
-class TGenericBaseActor: public NActors::TActorBootstrapped<TDerived> {
+    template <typename TDerived>
+    class TGenericBaseActor: public NActors::TActorBootstrapped<TDerived> {
+    protected: //Events
+        // Event ids
+        enum EEventIds: ui32 {
+            EvBegin = EventSpaceBegin(NActors::TEvents::ES_PRIVATE),
+            EvListSplitsIterator = EvBegin,
+            EvListSplitsPart,
+            EvListSplitsFinished,
+            EvReadSplitsIterator,
+            EvReadSplitsPart,
+            EvReadSplitsFinished,
+            EvError,
+            EvEnd
+        };
 
-protected: //Events
-    // Event ids
-    enum EEventIds: ui32 {
-        EvBegin = EventSpaceBegin(NActors::TEvents::ES_PRIVATE),
-        EvListSplitsIterator = EvBegin,
-        EvListSplitsPart,
-        EvListSplitsFinished,
-        EvReadSplitsIterator,
-        EvReadSplitsPart,
-        EvReadSplitsFinished,
-        EvError,
-        EvEnd
+        static_assert(EEventIds::EvEnd < EventSpaceEnd(NActors::TEvents::ES_PRIVATE), "expect EvEnd < EventSpaceEnd(TEvents::ES_PRIVATE)");
+
+        struct TEvListSplitsIterator: NActors::TEventLocal<TEvListSplitsIterator, EvListSplitsIterator> {
+            explicit TEvListSplitsIterator(NConnector::IListSplitsStreamIterator::TPtr&& iterator)
+                : Iterator(std::move(iterator))
+            {
+            }
+
+            NConnector::IListSplitsStreamIterator::TPtr Iterator;
+        };
+
+        struct TEvListSplitsPart: NActors::TEventLocal<TEvListSplitsPart, EvListSplitsPart> {
+            explicit TEvListSplitsPart(NConnector::NApi::TListSplitsResponse&& response)
+                : Response(std::move(response))
+            {
+            }
+
+            NConnector::NApi::TListSplitsResponse Response;
+        };
+
+        struct TEvListSplitsFinished: NActors::TEventLocal<TEvListSplitsFinished, EvListSplitsFinished> {
+            explicit TEvListSplitsFinished(NYdbGrpc::TGrpcStatus&& status)
+                : Status(std::move(status))
+            {
+            }
+
+            NYdbGrpc::TGrpcStatus Status;
+        };
+
+        struct TEvReadSplitsIterator: NActors::TEventLocal<TEvReadSplitsIterator, EvReadSplitsIterator> {
+            explicit TEvReadSplitsIterator(NConnector::IReadSplitsStreamIterator::TPtr&& iterator)
+                : Iterator(std::move(iterator))
+            {
+            }
+
+            NConnector::IReadSplitsStreamIterator::TPtr Iterator;
+        };
+
+        struct TEvReadSplitsPart: NActors::TEventLocal<TEvReadSplitsPart, EvReadSplitsPart> {
+            explicit TEvReadSplitsPart(NConnector::NApi::TReadSplitsResponse&& response)
+                : Response(std::move(response))
+            {
+            }
+
+            NConnector::NApi::TReadSplitsResponse Response;
+        };
+
+        struct TEvReadSplitsFinished: NActors::TEventLocal<TEvReadSplitsFinished, EvReadSplitsFinished> {
+            explicit TEvReadSplitsFinished(NYdbGrpc::TGrpcStatus&& status)
+                : Status(std::move(status))
+            {
+            }
+
+            NYdbGrpc::TGrpcStatus Status;
+        };
+
+        struct TEvError: NActors::TEventLocal<TEvError, EvError> {
+            explicit TEvError(const NConnector::NApi::TError& error)
+                : Error(error)
+            {
+            }
+
+            NConnector::NApi::TError Error;
+        };
+
+    protected: //TODO move common logic here
     };
-
-    static_assert(EEventIds::EvEnd < EventSpaceEnd(NActors::TEvents::ES_PRIVATE), "expect EvEnd < EventSpaceEnd(TEvents::ES_PRIVATE)");
-
-    struct TEvListSplitsIterator: NActors::TEventLocal<TEvListSplitsIterator, EvListSplitsIterator> {
-        explicit TEvListSplitsIterator(NConnector::IListSplitsStreamIterator::TPtr&& iterator)
-            : Iterator(std::move(iterator))
-        {
-        }
-
-        NConnector::IListSplitsStreamIterator::TPtr Iterator;
-    };
-
-    struct TEvListSplitsPart: NActors::TEventLocal<TEvListSplitsPart, EvListSplitsPart> {
-        explicit TEvListSplitsPart(NConnector::NApi::TListSplitsResponse&& response)
-            : Response(std::move(response))
-        {
-        }
-
-        NConnector::NApi::TListSplitsResponse Response;
-    };
-
-    struct TEvListSplitsFinished: NActors::TEventLocal<TEvListSplitsFinished, EvListSplitsFinished> {
-        explicit TEvListSplitsFinished(NYdbGrpc::TGrpcStatus&& status)
-            : Status(std::move(status))
-        {
-        }
-
-        NYdbGrpc::TGrpcStatus Status;
-    };
-
-    struct TEvReadSplitsIterator: NActors::TEventLocal<TEvReadSplitsIterator, EvReadSplitsIterator> {
-        explicit TEvReadSplitsIterator(NConnector::IReadSplitsStreamIterator::TPtr&& iterator)
-            : Iterator(std::move(iterator))
-        {
-        }
-
-        NConnector::IReadSplitsStreamIterator::TPtr Iterator;
-    };
-
-    struct TEvReadSplitsPart: NActors::TEventLocal<TEvReadSplitsPart, EvReadSplitsPart> {
-        explicit TEvReadSplitsPart(NConnector::NApi::TReadSplitsResponse&& response)
-            : Response(std::move(response))
-        {
-        }
-
-        NConnector::NApi::TReadSplitsResponse Response;
-    };
-
-    struct TEvReadSplitsFinished: NActors::TEventLocal<TEvReadSplitsFinished, EvReadSplitsFinished> {
-        explicit TEvReadSplitsFinished(NYdbGrpc::TGrpcStatus&& status)
-            : Status(std::move(status))
-        {
-        }
-
-        NYdbGrpc::TGrpcStatus Status;
-    };
-
-    struct TEvError: NActors::TEventLocal<TEvError, EvError> {
-        explicit TEvError(const NConnector::NApi::TError& error)
-            : Error(error)
-        {
-        }
-
-        NConnector::NApi::TError Error;
-    };
-
-protected: //TODO move common logic here
-
-};
 
 } // namespace NYql::NDq
--- ydb/library/yql/providers/generic/actors/yql_generic_lookup_actor.h
+++ ydb/library/yql/providers/generic/actors/yql_generic_lookup_actor.h
@@ -11,62 +11,60 @@
 
 namespace NYql::NDq {
 
+    namespace NGenericProviderLookupActorEvents { //TODO move to some common header to define interface for LookupSource in any provider
+        enum EEventIds: ui32 {
+            EvBegin = EventSpaceBegin(NActors::TEvents::ES_USERSPACE),
+            EvLookupRequest = EvBegin,
+            EvLookupResult,
+            EvEnd
+        };
+        static_assert(EvEnd < EventSpaceEnd(NActors::TEvents::ES_USERSPACE), "expect EvEnd < EventSpaceEnd(TEvents::ES_PRIVATE)");
 
-namespace NGenericProviderLookupActorEvents { //TODO move to some common header to define interface for LookupSource in any provider
-    enum EEventIds: ui32 {
-        EvBegin = EventSpaceBegin(NActors::TEvents::ES_USERSPACE),
-        EvLookupRequest = EvBegin,
-        EvLookupResult,
-        EvEnd
-    };
-    static_assert(EvEnd < EventSpaceEnd(NActors::TEvents::ES_USERSPACE), "expect EvEnd < EventSpaceEnd(TEvents::ES_PRIVATE)");
+        struct TEvLookupRequest: NActors::TEventLocal<TEvLookupRequest, EvLookupRequest> {
+            TEvLookupRequest(std::shared_ptr<NKikimr::NMiniKQL::TScopedAlloc> alloc, NKikimr::NMiniKQL::TUnboxedValueVector&& keys)
+                : Alloc(alloc)
+                , Keys(std::move(keys))
+            {
+            }
+            ~TEvLookupRequest() {
+                auto guard = Guard(*Alloc.get());
+                Keys = NKikimr::NMiniKQL::TUnboxedValueVector{};
+            }
 
-    struct TEvLookupRequest: NActors::TEventLocal<TEvLookupRequest, EvLookupRequest> {
-        TEvLookupRequest(std::shared_ptr<NKikimr::NMiniKQL::TScopedAlloc> alloc, NKikimr::NMiniKQL::TUnboxedValueVector&& keys)
-            : Alloc(alloc)
-            , Keys(std::move(keys))
-        {
-        }
-        ~TEvLookupRequest() {
-            auto guard = Guard(*Alloc.get());
-            Keys = NKikimr::NMiniKQL::TUnboxedValueVector{};
-        }
+            std::shared_ptr<NKikimr::NMiniKQL::TScopedAlloc> Alloc;
+            NKikimr::NMiniKQL::TUnboxedValueVector Keys;
+        };
 
-        std::shared_ptr<NKikimr::NMiniKQL::TScopedAlloc> Alloc;
-        NKikimr::NMiniKQL::TUnboxedValueVector Keys;
-    };
+        struct TEvLookupResult: NActors::TEventLocal<TEvLookupResult, EvLookupResult> {
+            TEvLookupResult(std::shared_ptr<NKikimr::NMiniKQL::TScopedAlloc> alloc, NKikimr::NMiniKQL::TKeyPayloadPairVector&& data)
+                : Alloc(alloc)
+                , Data(std::move(data))
+            {
+            }
+            ~TEvLookupResult() {
+                auto guard = Guard(*Alloc.get());
+                Data = NKikimr::NMiniKQL::TKeyPayloadPairVector{};
+            }
 
-    struct TEvLookupResult: NActors::TEventLocal<TEvLookupResult, EvLookupResult> {
-        TEvLookupResult(std::shared_ptr<NKikimr::NMiniKQL::TScopedAlloc> alloc, NKikimr::NMiniKQL::TKeyPayloadPairVector&& data)
-            : Alloc(alloc)
-            , Data(std::move(data))
-        {
-        }
-        ~TEvLookupResult() {
-            auto guard = Guard(*Alloc.get());
-            Data = NKikimr::NMiniKQL::TKeyPayloadPairVector{};
-        }
+            std::shared_ptr<NKikimr::NMiniKQL::TScopedAlloc> Alloc;
+            NKikimr::NMiniKQL::TKeyPayloadPairVector Data;
+        };
+    } // namespace NGenericProviderLookupActorEvents
 
-        std::shared_ptr<NKikimr::NMiniKQL::TScopedAlloc> Alloc;
-        NKikimr::NMiniKQL::TKeyPayloadPairVector Data;
-    };
-} // namespace NGenericProviderLookupActorEvents
-
-NActors::IActor*
-CreateGenericLookupActor(
-    NConnector::IClient::TPtr connectorClient,
-    const TString& serviceAccountId,
-    const TString& serviceAccountSignature,
-    ISecuredServiceAccountCredentialsFactory::TPtr credentialsFactory,
-    NActors::TActorId&& parentId,
-    std::shared_ptr<NKikimr::NMiniKQL::TScopedAlloc> alloc,
-    NYql::NConnector::NApi::TDataSourceInstance&& dataSource,
-    TString&& table,
-    const NKikimr::NMiniKQL::TStructType* lookupKeyType,
-    const NKikimr::NMiniKQL::TStructType* resultType,
-    const NKikimr::NMiniKQL::TTypeEnvironment& typeEnv,
-    const NKikimr::NMiniKQL::THolderFactory& holderFactory,
-    const size_t maxKeysInRequest
-);
+    NActors::IActor*
+    CreateGenericLookupActor(
+        NConnector::IClient::TPtr connectorClient,
+        const TString& serviceAccountId,
+        const TString& serviceAccountSignature,
+        ISecuredServiceAccountCredentialsFactory::TPtr credentialsFactory,
+        NActors::TActorId&& parentId,
+        std::shared_ptr<NKikimr::NMiniKQL::TScopedAlloc> alloc,
+        NYql::NConnector::NApi::TDataSourceInstance&& dataSource,
+        TString&& table,
+        const NKikimr::NMiniKQL::TStructType* lookupKeyType,
+        const NKikimr::NMiniKQL::TStructType* resultType,
+        const NKikimr::NMiniKQL::TTypeEnvironment& typeEnv,
+        const NKikimr::NMiniKQL::THolderFactory& holderFactory,
+        const size_t maxKeysInRequest);
 
 } // namespace NYql::NDq

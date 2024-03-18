b44b4c1bb5312abd2f838140ba5b5a463ed0fd08
kungasc <kungasc@ydb.tech>
2024-03-19 01:12:03 +0300

    return defaults

--- ydb/core/tablet_flat/flat_page_conf.h
+++ ydb/core/tablet_flat/flat_page_conf.h
@@ -83,8 +83,8 @@ namespace NPage {
 
         bool Final = true;
         bool CutIndexKeys = true;
-        bool WriteBTreeIndex = true;
-        bool WriteFlatIndex = false;
+        bool WriteBTreeIndex = false;
+        bool WriteFlatIndex = true;
         ui32 MaxLargeBlob = 8 * 1024 * 1024 - 8; /* Maximum large blob size */
         ui32 LargeEdge = Max<ui32>();   /* External blob edge size      */
         ui32 SmallEdge = Max<ui32>();   /* Outer blobs edge bytes limit */

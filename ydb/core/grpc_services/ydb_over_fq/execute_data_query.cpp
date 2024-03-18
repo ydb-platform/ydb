ba17ee0664907e3d14acb591ebe0e5337e464e11
Egor Zudin <e-zudin@ydb.tech>
2024-03-18 09:45:58 +0000

    YQ-2863: add ExplainDataQuery, DescribeTable and KeepAlive handles

--- ydb/core/grpc_services/ydb_over_fq/execute_data_query.cpp
+++ ydb/core/grpc_services/ydb_over_fq/execute_data_query.cpp
@@ -22,17 +22,12 @@ public:
     using TBase::TBase;
 
     void Bootstrap(const TActorContext& ctx) {
-        Become(&ExecuteDataQueryRPC::CreateQueryState);
         CreateQuery(ctx);
     }
 
     // overall algorithm: CreateQuery -> WaitForExecution -> GatherResultSetSizes -> GatherResultSets -> reply
 
     // CreateQueryImpl
-
-    STRICT_STFUNC(CreateQueryState,
-        HFunc(TEvFqCreateQueryResponse, TBase::HandleResponse<FederatedQuery::CreateQueryRequest>);
-    )
 
     void CreateQuery(const TActorContext& ctx) {
         if (!GetProtoRequest()->query().has_yql_text()) {
@@ -47,108 +42,36 @@ public:
         }
         const auto& text = GetProtoRequest()->query().yql_text();
 
-        FederatedQuery::CreateQueryRequest req;
-        req.set_execute_mode(FederatedQuery::ExecuteMode::RUN);
-        auto& queryContent = *req.mutable_content();
-        queryContent.set_type(FederatedQuery::QueryContent_QueryType_ANALYTICS);
-        queryContent.set_name("Generated query from within");
-        queryContent.mutable_text()->assign(text);
-        queryContent.set_automatic(true);
-        auto& acl = *queryContent.mutable_acl();
-        acl.set_visibility(FederatedQuery::Acl_Visibility::Acl_Visibility_SCOPE);
-
-        LOG_TRACE_S(ctx, NKikimrServices::FQ_INTERNAL_SERVICE,
-                        "pseudo ExecuteDataQuery actorId: " << SelfId().ToString() << ", creating query");
-
-        MakeLocalCall(std::move(req), ctx);
+        TBase::CreateQuery(text, FederatedQuery::ExecuteMode::RUN, ctx);
     }
 
     void Handle(const FederatedQuery::CreateQueryResult& result, const TActorContext& ctx) {
         LOG_TRACE_S(ctx, NKikimrServices::FQ_INTERNAL_SERVICE,
             "pseudo ExecuteDataQuery actorId: " << SelfId().ToString() << ", created query: " << result.query_id());
 
-        WaitForExecution(result.query_id(), ctx);
+        WaitForTermination(result.query_id(), ctx);
     }
 
     // WaitForExecutionImpl
 
-    STRICT_STFUNC(WaitForExecutionState,
-        HFunc(TEvFqGetQueryStatusRequest, HandleStatusRequest);
-        HFunc(TEvFqGetQueryStatusResponse, TBase::HandleResponse<FederatedQuery::GetQueryStatusRequest>);
-    )
-
-    void WaitForExecution(const TString& queryId, const TActorContext& ctx) {
-        QueryId_ = queryId;
-        auto reqEv = CreateStatusRequest();
-
-        Become(&ExecuteDataQueryRPC::WaitForExecutionState);
-        ctx.Send(SelfId(), reqEv);
-    }
-
-    using WaitRetryPolicy = IRetryPolicy<FederatedQuery::QueryMeta::ComputeStatus>;
-
-    static WaitRetryPolicy::IRetryState::TPtr CreateRetryState() {
-        return WaitRetryPolicy::GetExponentialBackoffPolicy([](FederatedQuery::QueryMeta::ComputeStatus status) {
-            return NFq::IsTerminalStatus(status) ? ERetryErrorClass::NoRetry : ERetryErrorClass::ShortRetry;
-        }, TDuration::MilliSeconds(10), TDuration::Seconds(1), TDuration::Seconds(5))->CreateRetryState();
-    }
-
-    TEvFqGetQueryStatusRequest* CreateStatusRequest() {
-        FederatedQuery::GetQueryStatusRequest req;
-        req.set_query_id(QueryId_);
-        return new TEvFqGetQueryStatusRequest(std::move(req));
-    }
-
-    void HandleStatusRequest(typename TEvFqGetQueryStatusRequest::TPtr& ev, const TActorContext& ctx) {
-        MakeLocalCall(std::move(ev->Get()->Message), ctx);
-    }
-
-    void Handle(const FederatedQuery::GetQueryStatusResult& result, const TActorContext& ctx) {
-        if (!NFq::IsTerminalStatus(result.status())) {
-            LOG_TRACE_S(ctx, NKikimrServices::FQ_INTERNAL_SERVICE,
-                "pseudo ExecuteDataQuery actorId: " << SelfId().ToString() << ", still waiting for query: " << QueryId_ <<
-                ", current status: " << FederatedQuery::QueryMeta::ComputeStatus_Name(result.status()));
-            auto delay = WaitRetryState_->GetNextRetryDelay(result.status());
-            if (!delay) [[unlikely]] {
-                Reply(Ydb::StatusIds_StatusCode_INTERNAL_ERROR,
-                    TStringBuilder{} << "Created query " << QueryId_ << ", couldn't wait for finish, final status: " <<
-                    FederatedQuery::QueryMeta::ComputeStatus_Name(result.status()), NKikimrIssues::TIssuesIds::DEFAULT_ERROR, ctx);
-                return;
-            }
-            ctx.Schedule(*delay, CreateStatusRequest());
-            return;
-        }
-
-        if (result.status() != FederatedQuery::QueryMeta_ComputeStatus_COMPLETED) {
+    void OnQueryTermination(TStringBuf queryId, FederatedQuery::QueryMeta_ComputeStatus status, const TActorContext& ctx) {
+        if (status != FederatedQuery::QueryMeta_ComputeStatus_COMPLETED) {
+            TString errorMsg = TStringBuilder{} << "created query " << queryId << " finished with non-success status: " <<
+                    FederatedQuery::QueryMeta::ComputeStatus_Name(status);
             LOG_INFO_S(ctx, NKikimrServices::FQ_INTERNAL_SERVICE,
-                "pseudo ExecuteDataQuery actorId: " << SelfId().ToString() << ", queryId: " << QueryId_ <<
-                ", finished with bad status: " << FederatedQuery::QueryMeta::ComputeStatus_Name(result.status()));
-            Reply(Ydb::StatusIds_StatusCode_INTERNAL_ERROR,
-                TStringBuilder{} << "Created query " << QueryId_ << " finished with non-success status: " <<
-                    FederatedQuery::QueryMeta::ComputeStatus_Name(result.status()), NKikimrIssues::TIssuesIds::DEFAULT_ERROR, ctx);
+                "pseudo ExecuteDataQuery actorId: " << SelfId().ToString() << ", error: " << errorMsg);
+            Reply(Ydb::StatusIds_StatusCode_INTERNAL_ERROR, errorMsg, NKikimrIssues::TIssuesIds::DEFAULT_ERROR, ctx);
             return;
         }
 
         LOG_INFO_S(ctx, NKikimrServices::FQ_INTERNAL_SERVICE,
-            "pseudo ExecuteDataQuery actorId: " << SelfId().ToString() << ", queryId: " << QueryId_ <<
+            "pseudo ExecuteDataQuery actorId: " << SelfId().ToString() << ", queryId: " << queryId <<
             ", finished query execution");
 
-        GatherResultSetSizes(ctx);
+        TBase::DescribeQuery(queryId, ctx);
     }
 
     // GatherResultSetSizesImpl
-
-    STRICT_STFUNC(GatherResultSetSizesState,
-        HFunc(TEvFqDescribeQueryResponse, TBase::HandleResponse<FederatedQuery::DescribeQueryRequest>);
-    )
-
-    void GatherResultSetSizes(const TActorContext& ctx) {
-        FederatedQuery::DescribeQueryRequest req;
-        req.set_query_id(QueryId_);
-
-        Become(&ExecuteDataQueryRPC::GatherResultSetSizesState);
-        MakeLocalCall(std::move(req), ctx);
-    }
 
     void Handle(const FederatedQuery::DescribeQueryResult& result, const TActorContext& ctx) {
         ResultSetSizes_.reserve(result.query().result_set_meta_size());
@@ -157,7 +80,7 @@ public:
             ResultSetSizes_.push_back(meta.rows_count());
         }
 
-        GatherResultSets(ctx);
+        GatherResultSets(result.query().meta().common().id(), ctx);
     }
 
     // GatherResultSetsImpl
@@ -167,17 +90,18 @@ public:
         HFunc(TEvFqGetResultDataResponse, TBase::HandleResponse<FederatedQuery::GetResultDataRequest>);
     )
 
-    void GatherResultSets(const TActorContext& ctx) {
+    void GatherResultSets(TStringBuf queryId, const TActorContext& ctx) {
         Become(&ExecuteDataQueryRPC::GatherResultSetsState);
-        MakeLocalCall(CreateResultSetRequest(0, 0), ctx);
+        QueryId_ = queryId;
+        MakeLocalCall(CreateResultSetRequest(queryId, 0, 0), ctx);
     }
 
-    FederatedQuery::GetResultDataRequest CreateResultSetRequest(i32 index, i64 offset) {
+    FederatedQuery::GetResultDataRequest CreateResultSetRequest(TStringBuf queryId, i32 index, i64 offset) {
         FederatedQuery::GetResultDataRequest msg;
 
         constexpr i64 RowsLimit = 1000;
 
-        msg.set_query_id(QueryId_);
+        msg.mutable_query_id()->assign(queryId);
         msg.set_result_set_index(index);
         msg.set_offset(offset);
         msg.set_limit(RowsLimit);
@@ -203,11 +127,11 @@ public:
         }
 
         if (resultSet->rows_size() < ResultSetSizes_[CurrentResultSet_]) {
-            MakeLocalCall(CreateResultSetRequest(CurrentResultSet_, resultSet->rows_size()), ctx);
+            MakeLocalCall(CreateResultSetRequest(QueryId_, CurrentResultSet_, resultSet->rows_size()), ctx);
         } else {
             Y_ABORT_UNLESS(resultSet->rows_size() == ResultSetSizes_[CurrentResultSet_]);
             if (++CurrentResultSet_ < static_cast<i64>(ResultSetSizes_.size())) {
-                MakeLocalCall(CreateResultSetRequest(CurrentResultSet_, 0), ctx);
+                MakeLocalCall(CreateResultSetRequest(QueryId_, CurrentResultSet_, 0), ctx);
             } else {
                 SendReply(ctx);
             }
@@ -230,7 +154,6 @@ private:
     std::vector<i64> ResultSetSizes_;
     i32 CurrentResultSet_ = 0;
     std::vector<Ydb::ResultSet> ResultSets_;
-    WaitRetryPolicy::IRetryState::TPtr WaitRetryState_ = CreateRetryState();
 };
 
 std::function<void(std::unique_ptr<IRequestOpCtx>, const IFacilityProvider&)> GetExecuteDataQueryExecutor(NActors::TActorId grpcProxyId) {
---
+++ ydb/core/grpc_services/ydb_over_fq/explain_data_query.cpp
@@ -0,0 +1,69 @@
+#include "fq_local_grpc_events.h"
+#include "rpc_base.h"
+
+namespace NKikimr::NGRpcService::NYdbOverFq {
+
+class ExplainDataQueryRPC
+    : public TRpcBase<ExplainDataQueryRPC, Ydb::Table::ExplainDataQueryRequest, Ydb::Table::ExplainDataQueryResponse> {
+public:
+    using TBase = TRpcBase<ExplainDataQueryRPC, Ydb::Table::ExplainDataQueryRequest, Ydb::Table::ExplainDataQueryResponse>;
+    static constexpr std::string_view RpcName = "ExplainDataQuery";
+
+    using TBase::TBase;
+
+    void Bootstrap(const TActorContext& ctx) {
+        // plan: CreateQuery -> WaitForTermination -> DescribeQuery -> Reply
+        CreateQuery(ctx);
+    }
+
+    void CreateQuery(const TActorContext& ctx) {
+        TBase::CreateQuery(GetProtoRequest()->yql_text(), FederatedQuery::ExecuteMode::EXPLAIN, ctx);
+    }
+
+    void Handle(const FederatedQuery::CreateQueryResult& result, const TActorContext& ctx) {
+        LOG_TRACE_S(ctx, NKikimrServices::FQ_INTERNAL_SERVICE,
+            "pseudo ExplainDataQuery actorId: " << SelfId().ToString() << ", created query: " << result.query_id());
+
+        WaitForTermination(result.query_id(), ctx);
+    }
+
+    // WaitForTermination
+    void OnQueryTermination(TStringBuf queryId, FederatedQuery::QueryMeta_ComputeStatus status, const TActorContext& ctx) {
+        if (status != FederatedQuery::QueryMeta_ComputeStatus_COMPLETED) {
+            TString errorMsg = TStringBuilder{} << "created query " << queryId << " finished with non-success status: " <<
+                    FederatedQuery::QueryMeta::ComputeStatus_Name(status);
+            LOG_INFO_S(ctx, NKikimrServices::FQ_INTERNAL_SERVICE,
+                "pseudo ExplainDataQuery actorId: " << SelfId().ToString() << ", error: " << errorMsg);
+            Reply(Ydb::StatusIds_StatusCode_INTERNAL_ERROR, errorMsg, NKikimrIssues::TIssuesIds::DEFAULT_ERROR, ctx);
+            return;
+        }
+
+        LOG_INFO_S(ctx, NKikimrServices::FQ_INTERNAL_SERVICE,
+            "pseudo ExplainDataQuery actorId: " << SelfId().ToString() << ", queryId: " << queryId <<
+            ", finished query execution");
+
+        TBase::DescribeQuery(queryId, ctx);
+    }
+
+    // DescribeQuery
+    void Handle(const FederatedQuery::DescribeQueryResult& result, const TActorContext& ctx) {
+        Ydb::Table::ExplainQueryResult response;
+        if (result.query().ast().data()) {
+            response.set_query_ast(result.query().ast().data());
+        }
+        if (result.query().plan().json()) {
+            response.set_query_plan(result.query().plan().json());
+        }
+        if (result.query().statistics().json()) {
+            response.set_query_full_diagnostics(result.query().statistics().json());
+        }
+        ReplyWithResult(Ydb::StatusIds_StatusCode_SUCCESS, response, ctx);
+    }
+};
+
+std::function<void(std::unique_ptr<IRequestOpCtx>, const IFacilityProvider&)> GetExplainDataQueryExecutor(NActors::TActorId grpcProxyId) {
+    return [grpcProxyId = std::move(grpcProxyId)](std::unique_ptr<IRequestOpCtx> p, const IFacilityProvider& f) {
+        f.RegisterActor(new ExplainDataQueryRPC(p.release(), grpcProxyId));
+    };
+}
+}

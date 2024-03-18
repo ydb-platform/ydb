1f1722ab2b48a6ae6786a8748af2fba7a6ceeb21
Vadim Averin <avevad@ydb.tech>
2024-03-18 13:39:59 +0000

    Eliminate some code bloat

---
+++ ydb/library/yql/minikql/comp_nodes/mkql_simple_codegen.h
@@ -0,0 +1,116 @@
+#pragma once
+
+namespace NKikimr {
+namespace NMiniKQL {
+
+class TMaybeFetchResult final {
+    ui64 Raw;
+
+    explicit TMaybeFetchResult(ui64 raw) : Raw(raw) {}
+
+public:
+    /* implicit */ TMaybeFetchResult(EFetchResult res) : TMaybeFetchResult(static_cast<ui64>(static_cast<ui32>(res))) {}
+
+
+    [[nodiscard]] bool Empty() const {
+        return Raw >> ui64(32);
+    }
+
+    [[nodiscard]] EFetchResult Get() const {
+        Y_ABORT_IF(Empty());
+        return static_cast<EFetchResult>(static_cast<ui32>(Raw));
+    }
+
+    [[nodiscard]] ui64 RawU64() const {
+        return Raw;
+    }
+
+    static TMaybeFetchResult None() {
+        return TMaybeFetchResult(ui64(1) << ui64(32));
+    }
+
+#ifndef MKQL_DISABLE_CODEGEN
+    static Type* LLVMType(LLVMContext& context) {
+        return Type::getInt64Ty(context);
+    }
+
+    static Value* LLVMFromFetchResult(Value *fetchRes, const Twine& name, BasicBlock* block) {
+        return new ZExtInst(fetchRes, LLVMType(fetchRes->getContext()), name, block);
+    }
+
+    Value* LLVMConst(LLVMContext& context) const {
+        return ConstantInt::get(LLVMType(context), RawU64());
+    }
+#endif
+};
+
+#ifndef MKQL_DISABLE_CODEGEN
+using TResultCodegenerator = std::function<ICodegeneratorInlineWideNode::TGenerateResult(const TCodegenContext&, BasicBlock*&)>;
+#endif
+
+class TSimpleStatefulWideFlowCodegeneratorNodeLLVMBase {
+public:
+    struct TMethPtrTable {
+        uintptr_t InitStateMethPtr;
+        uintptr_t PrepareInputMethPtr;
+        uintptr_t DoProcessMethPtr;
+    };
+
+    TSimpleStatefulWideFlowCodegeneratorNodeLLVMBase(IComputationWideFlowNode* source, ui32 inWidth, ui32 outWidth, TMethPtrTable ptrTable)
+        : SourceFlow(source), InWidth(inWidth), OutWidth(outWidth)
+        , PtrTable(ptrTable) {}
+
+#ifndef MKQL_DISABLE_CODEGEN
+    virtual ICodegeneratorInlineWideNode::TGenerateResult GenFetchProcess(Value*, const TCodegenContext&, const TResultCodegenerator&, BasicBlock*&) const {
+        return {nullptr, {}};
+    }
+
+    ICodegeneratorInlineWideNode::TGenerateResult DoGenGetValues(const TCodegenContext& ctx, Value* statePtrVal, BasicBlock*& genToBlock) const;
+#endif
+
+protected:
+    IComputationWideFlowNode* const SourceFlow;
+    const ui32 InWidth, OutWidth;
+
+private:
+    const TMethPtrTable PtrTable;
+};
+
+template<typename TDerived, typename TState, EValueRepresentation StateKind = EValueRepresentation::Embedded>
+class TSimpleStatefulWideFlowCodegeneratorNode
+        : public TStatefulWideFlowCodegeneratorNode<TSimpleStatefulWideFlowCodegeneratorNode<TDerived, TState, StateKind>>
+        , public TSimpleStatefulWideFlowCodegeneratorNodeLLVMBase {
+    using TBase = TStatefulWideFlowCodegeneratorNode<TSimpleStatefulWideFlowCodegeneratorNode>;
+    using TLLVMBase = TSimpleStatefulWideFlowCodegeneratorNodeLLVMBase;
+
+protected:
+    TSimpleStatefulWideFlowCodegeneratorNode(TComputationMutables& mutables, IComputationWideFlowNode* source, ui32 inWidth, ui32 outWidth)
+            : TBase(mutables, source, StateKind)
+            , TLLVMBase(source, inWidth, outWidth, {
+                .InitStateMethPtr = GetMethodPtr(&TDerived::InitState),
+                .PrepareInputMethPtr = GetMethodPtr(&TDerived::PrepareInput),
+                .DoProcessMethPtr = GetMethodPtr(&TDerived::DoProcess)
+            }) {}
+
+public:
+    EFetchResult DoCalculate(NUdf::TUnboxedValue& state, TComputationContext& ctx, NUdf::TUnboxedValue*const* output) const {
+        if (state.IsInvalid()) {
+            state = NUdf::TUnboxedValuePod();
+            static_cast<const TDerived*>(this)->InitState(state, ctx);
+        }
+        NUdf::TUnboxedValue *const stub = nullptr;
+        if (!output && !OutWidth) {
+            output = &stub;
+        }
+        auto result = TMaybeFetchResult::None();
+        while (result.Empty()) {
+            NUdf::TUnboxedValue*const* input = static_cast<const TDerived*>(this)->PrepareInput(state, ctx, output);
+            TMaybeFetchResult fetchResult = input ? SourceFlow->FetchValues(ctx, input) : TMaybeFetchResult::None();
+            result = static_cast<const TDerived*>(this)->DoProcess(state, ctx, fetchResult, output);
+        }
+        return result.Get();
+    }
+};
+
+}
+}

bc769ccf3b82305e740a2b047c2dc24866845490
Egor Zudin <e-zudin@ydb.tech>
2024-03-19 08:21:25 +0000

    Fix include

--- ydb/core/fq/libs/control_plane_storage/internal/task_ping.cpp
+++ ydb/core/fq/libs/control_plane_storage/internal/task_ping.cpp
@@ -2,6 +2,7 @@
 
 #include <util/datetime/base.h>
 
+#include <ydb/core/fq/libs/compute/common/utils.h>
 #include <ydb/core/fq/libs/control_plane_storage/util.h>
 #include <ydb/core/fq/libs/db_schema/db_schema.h>
 #include <ydb/core/metering/metering.h>

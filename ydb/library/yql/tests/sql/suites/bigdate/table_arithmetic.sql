f6dfa5c9751e77bb46f1889b1e7d0188eea73e8c
Aleksandr Mikishev <mikishev.aleksandr@yandex.ru>
2024-03-27 11:57:44 +0300

    Divide arithmetic sql file

--- ydb/library/yql/tests/sql/suites/bigdate/table_arithmetic.sql
+++ ydb/library/yql/tests/sql/suites/bigdate/table_arithmetic.sql
@@ -1,13 +1,6 @@
 /* postgres can not */
 use plato;
 
-select l.row, r.row
-, 1, l.d32 - r.d32, l.d32 - r.dt64, l.d32 - r.ts64
-, 2, l.dt64 - r.d32, l.dt64 - r.dt64, l.dt64 - r.ts64
-, 3, l.ts64 - r.d32, l.ts64 - r.dt64, l.ts64 - r.ts64
-from BigDates as l cross join BigDates as r
-order by l.row, r.row;
-
 select row, -i64, +i64, abs(i64)
 from BigDates
 order by row;
@@ -20,36 +13,19 @@ select l.row, r.row
 , 1, l.d32 - r.i64, l.dt64 - r.i64, l.ts64 - r.i64, l.i64 - r.i64
 , 2, l.d32 + r.i64, l.dt64 + r.i64, l.ts64 + r.i64, l.i64 + r.i64
 from BigDates as l cross join BigDates as r
+where abs(l.row) <= 7 and abs(r.row) <= 7
 order by l.row, r.row;
 
 select l.row, r.row
-, 1, l.d32 - r.d, l.d32 - r.dt, l.d32 - r.ts
-, 2, l.dt64 - r.d, l.dt64 - r.dt, l.dt64 - r.ts
-, 3, l.ts64 - r.d, l.ts64 - r.dt, l.ts64 - r.ts
-from BigDates as l cross join NarrowDates as r
+, 1, l.i64*i8, l.i64*i16, l.i64*i32, l.i64*r.i64
+, 2, l.i64/i8, l.i64/i16, l.i64/i32, l.i64/r.i64
+from BigDates as l cross join Signed as r
+where abs(l.row) <= 7 and abs(r.row) <= 7
 order by l.row, r.row;
 
 select l.row, r.row
-, 1, l.d - r.d32, l.d - r.dt64, l.d - r.ts64, l.d - r.i64, l.d + r.i64
-, 2, l.dt - r.d32, l.dt - r.dt64, l.dt - r.ts64, l.dt - r.i64, l.dt + r.i64
-, 3, l.ts - r.d32, l.ts - r.dt64, l.ts - r.ts64, l.ts - r.i64, l.ts + r.i64
-from NarrowDates as l cross join BigDates as r
-order by l.row, r.row;
-
-select l.row, r.row
-, 1, l.d32 - r.i, l.dt64 - r.i, l.ts64 - r.i, l.i64 - r.i
-, 2, l.d32 + r.i, l.dt64 + r.i, l.ts64 + r.i, l.i64 + r.i
-from BigDates as l cross join NarrowInterval as r
-order by l.row, r.row;
-
-select i.row, s.row
-, 1, i.i64*i8, i.i64*i16, i.i64*i32, i.i64*s.i64
-, 2, i.i64/i8, i.i64/i16, i.i64/i32, i.i64/s.i64
-from BigDates as i cross join Signed as s
-order by i.row, s.row;
-
-select i.row, u.row
 , 1, i64*ui8, i64*ui16, i64*ui32, i64*ui64
 , 2, i64/ui8, i64/ui16, i64/ui32, i64/ui64
-from BigDates as i cross join Unsigned as u
-order by i.row, u.row;
+from BigDates as l cross join Unsigned as r
+where abs(l.row) <= 7 and abs(r.row) <= 7
+order by l.row, r.row;

3ec5ff4e96e079ea9619a13b9213719976f1fd49
Alexander Kotov <akotov@ydb.tech>
2024-03-27 13:38:59 +0300

    [*] безымянное пространство в начало файла

--- ydb/core/persqueue/partition.cpp
+++ ydb/core/persqueue/partition.cpp
@@ -1574,18 +1574,6 @@ void TPartition::ProcessTxsAndUserActs(const TActorContext& ctx)
     ContinueProcessTxsAndUserActs(ctx);
 }
 
-namespace {
-
-template <class T>
-struct TIsSimpleSharedPtr : std::false_type {
-};
-
-template <class U>
-struct TIsSimpleSharedPtr<TSimpleSharedPtr<U>> : std::true_type {
-};
-
-}
-
 void TPartition::ContinueProcessTxsAndUserActs(const TActorContext& ctx)
 {
     Y_ABORT_UNLESS(!UsersInfoWriteInProgress);

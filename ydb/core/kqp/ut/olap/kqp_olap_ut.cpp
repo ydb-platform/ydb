9e6c7227d98db34cb3f64876d597da88e1164aaa
Mikhail Surin <surinmike@gmail.com>
2024-03-29 08:19:15 -0300

    fix WithParameters

--- ydb/core/kqp/ut/olap/kqp_olap_ut.cpp
+++ ydb/core/kqp/ut/olap/kqp_olap_ut.cpp
@@ -5181,12 +5181,11 @@ Y_UNIT_TEST_SUITE(KqpOlap) {
             }
 
             builder << R"(
-                DECLARE $in_timestamp AS Timestamp;
                 DECLARE $in_uid AS Utf8;
                 DECLARE $in_level AS Int32;
 
                 SELECT `timestamp` FROM `/Root/olapStore/olapTable` WHERE
-                    `timestamp` > $in_timestamp AND uid > $in_uid AND level > $in_level
+                    uid > $in_uid AND level > $in_level
                 ORDER BY `timestamp`;
             )" << Endl;
 
@@ -5197,9 +5196,6 @@ Y_UNIT_TEST_SUITE(KqpOlap) {
         auto pushQuery = buildQuery(true);
 
         auto params = tableClient.GetParamsBuilder()
-            .AddParam("$in_timestamp")
-                .Timestamp(TInstant::MicroSeconds(3000990))
-                .Build()
             .AddParam("$in_uid")
                 .Utf8("uid_3000980")
                 .Build()

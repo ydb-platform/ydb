a9e556b1c9d6b35dcaa88935de2544c94d42ab74
ivanmorozov333 <111685085+ivanmorozov333@users.noreply.github.com>
2024-03-29 12:12:51 +0300

    tx volume limit usage on indexation (#3279)

--- ydb/core/tx/columnshard/engines/insert_table/data.h
+++ ydb/core/tx/columnshard/engines/insert_table/data.h
@@ -24,6 +24,7 @@ private:
     };
 
     std::shared_ptr<TBlobStorageGuard> BlobDataGuard;
+
 public:
     ui64 PlanStep = 0;
     ui64 WriteTxId = 0;
@@ -39,6 +40,10 @@ public:
         } else {
             return {};
         }
+    }
+
+    ui64 GetTxVolume() const {
+        return Meta.GetTxVolume() + sizeof(TBlobRange);
     }
 
     const TInsertedDataMeta& GetMeta() const {

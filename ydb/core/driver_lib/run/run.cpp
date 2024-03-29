3818df92f5041b38fbb1ddf69618d4f83e378024
Vitalii Gridnev <gridnevvvit@gmail.com>
2024-03-29 16:07:27 +0300

    fix query service enable order

--- ydb/core/driver_lib/run/run.cpp
+++ ydb/core/driver_lib/run/run.cpp
@@ -584,10 +584,6 @@ void TKikimrRunner::InitializeGRpc(const TKikimrRunConfig& runConfig) {
         TServiceCfg hasKeyValue = services.empty();
         names["keyvalue"] = &hasKeyValue;
 
-        if (hasTableService || hasYql) {
-            hasQueryService = true;
-        }
-
         std::unordered_set<TString> enabled;
         for (const auto& name : services) {
             enabled.insert(name);
@@ -639,6 +635,10 @@ void TKikimrRunner::InitializeGRpc(const TKikimrRunConfig& runConfig) {
         if (hasExport || hasImport) {
             hasExport = true;
             hasImport = true;
+        }
+
+        if (hasTableService || hasYql) {
+            hasQueryService = true;
         }
 
         // Enable RL for all services if enabled list is empty

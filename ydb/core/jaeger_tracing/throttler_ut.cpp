b765db382c7bf88806290cdf1f9eda1cff5691d8
Oleg Shatov <olegsh@ydb.tech>
2024-04-01 15:38:53 +0300

    Added tests for jaeger tracing configurator (#3309)

--- ydb/core/jaeger_tracing/throttler_ut.cpp
+++ ydb/core/jaeger_tracing/throttler_ut.cpp
@@ -1,4 +1,6 @@
 #include "throttler.h"
+#include "util/generic/scope.h"
+#include "util/system/thread.h"
 
 #include <library/cpp/testing/unittest/registar.h>
 
@@ -6,18 +8,18 @@ namespace NKikimr::NJaegerTracing {
 
 class TTimeProviderMock : public ITimeProvider {
 public:
-    TTimeProviderMock(TInstant now) : CurrentTime(now) {}
+    TTimeProviderMock(TInstant now) : CurrentTimeUS(now.GetValue()) {}
 
     void Advance(TDuration delta) {
-        CurrentTime += delta;
+        CurrentTimeUS.fetch_add(delta.GetValue());
     }
 
     TInstant Now() final {
-        return CurrentTime;
+        return TInstant::FromValue(CurrentTimeUS.load());
     }
 
 private:
-    TInstant CurrentTime;
+    std::atomic<ui64> CurrentTimeUS;
 };
 
 Y_UNIT_TEST_SUITE(ThrottlerControlTests) {
@@ -83,6 +85,70 @@ Y_UNIT_TEST_SUITE(ThrottlerControlTests) {
         timeProvider->Advance(TDuration::Days(365 * 10));
 
         CheckAtLeast(throttler, 1);
+    }
+
+    Y_UNIT_TEST(MultiThreaded) {
+        auto check = []<size_t Threads, ui64 Ticks, ui64 Init, ui64 Step>() {
+            constexpr TDuration delay = TDuration::MilliSeconds(1);
+            auto timeProvider = MakeIntrusive<TTimeProviderMock>(TInstant::Now());
+
+            TThrottler throttler(60, Init - 1, timeProvider);
+
+            auto shouldStop = std::make_shared<std::atomic<bool>>(false);
+            TVector<THolder<TThread>> workers;
+            Y_SCOPE_EXIT(shouldStop, &workers) {
+                shouldStop->store(true);
+
+                try {
+                    for (auto& worker : workers) {
+                        worker->Join();
+                    }
+                } catch (yexception& e) {
+                    Cerr << "Failed to join worker:" << Endl;
+                    Cerr << e.what() << Endl;
+                }
+            };
+
+            std::atomic<ui64> totalConsumed{0};
+            workers.reserve(Threads);
+            for (size_t i = 0; i < Threads; ++i) {
+                workers.push_back(MakeHolder<TThread>([&, shouldStop]() {
+                    while (!shouldStop->load(std::memory_order_relaxed)) {
+                        if (!throttler.Throttle()) {
+                            totalConsumed.fetch_add(1);
+                        }
+                    }
+                }));
+            }
+            for (auto& worker : workers) {
+                worker->Start();
+            }
+
+            Sleep(delay);
+            ui64 expected = Init;
+            UNIT_ASSERT_EQUAL(totalConsumed.load(), expected);
+
+            auto advance = [&, delay](ui64 seconds, ui64 expectedIncreace) {
+                timeProvider->Advance(TDuration::Seconds(seconds));
+                expected += expectedIncreace;
+                Sleep(delay);
+                UNIT_ASSERT_EQUAL(totalConsumed.load(), expected);
+            };
+
+            advance(1, 1);
+
+            for (size_t i = 0; i < Ticks; ++i) {
+                advance(Step, Step);
+                Sleep(delay);
+                UNIT_ASSERT_EQUAL(totalConsumed.load(), expected);
+            }
+
+            advance(Init + 1000, Init);
+        };
+
+        check.operator()<2, 200, 30, 7>();
+        check.operator()<5, 150, 500, 15>();
+        check.operator()<10, 100, 1000, 22>();
     }
 }
 

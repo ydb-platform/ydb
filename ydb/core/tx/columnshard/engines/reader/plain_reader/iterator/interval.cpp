b7c657240f21de18ed4f0cb829cfbae940366d4e
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-04-02 19:42:21 +0300

    fix build

--- ydb/core/tx/columnshard/engines/reader/plain_reader/iterator/interval.cpp
+++ ydb/core/tx/columnshard/engines/reader/plain_reader/iterator/interval.cpp
@@ -9,6 +9,7 @@ class TMergeTask: public IDataTasksProcessor::ITask {
 private:
     using TBase = IDataTasksProcessor::ITask;
     std::shared_ptr<arrow::RecordBatch> ResultBatch;
+    std::shared_ptr<arrow::Table> ResultBatch;
     std::shared_ptr<arrow::RecordBatch> LastPK;
     const NColumnShard::TCounterGuard Guard;
     std::shared_ptr<TSpecialReadContext> Context;

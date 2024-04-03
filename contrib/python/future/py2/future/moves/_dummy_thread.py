bd6fa12b5d3bc14249eb09846c0c4e65fc12368c
robot-piglet <robot-piglet@yandex-team.com>
2024-04-03 16:40:15 +0300

    Intermediate changes

--- contrib/python/future/py2/future/moves/_dummy_thread.py
+++ contrib/python/future/py2/future/moves/_dummy_thread.py
@@ -2,10 +2,7 @@ from __future__ import absolute_import
 from future.utils import PY3
 
 if PY3:
-    try:
-        from _dummy_thread import *
-    except ImportError:
-        from _thread import *
+    from _dummy_thread import *
 else:
     __future_module__ = True
     from dummy_thread import *

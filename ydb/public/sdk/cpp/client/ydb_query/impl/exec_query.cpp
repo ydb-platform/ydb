d85fe32424a94e17a539bf2714f6d319814a3f6f
Daniil Cherednik <dcherednik@ydb.tech>
2024-04-05 20:00:42 +0200

    Option to limit batch size (bytes) for query service (#3471)

--- ydb/public/sdk/cpp/client/ydb_query/impl/exec_query.cpp
+++ ydb/public/sdk/cpp/client/ydb_query/impl/exec_query.cpp
@@ -219,6 +219,10 @@ TFuture<std::pair<TPlainStatus, TExecuteQueryProcessorPtr>> StreamExecuteQueryIm
         request.set_concurrent_result_sets(*settings.ConcurrentResultSets_);
     }
 
+    if (settings.OutputChunkMaxSize_) {
+        request.set_response_part_limit_bytes(*settings.OutputChunkMaxSize_);
+    }
+
     if (txControl.HasTx()) {
         auto requestTxControl = request.mutable_tx_control();
         requestTxControl->set_commit_tx(txControl.CommitTx_);

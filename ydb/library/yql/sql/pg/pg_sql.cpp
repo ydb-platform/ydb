20ee051fca60c937a9c7a44f9ddd63f4db32e314
Timofey Koolin <rekby@users.noreply.github.com>
2024-04-08 13:16:35 +0300

    patches for zabbix and wiki (#2912)

--- ydb/library/yql/sql/pg/pg_sql.cpp
+++ ydb/library/yql/sql/pg/pg_sql.cpp
@@ -475,6 +475,7 @@ public:
                     "escape_string_warning",               // zabbix
                     "bytea_output",                        // zabbix
                     "datestyle",                           // pgadmin 4
+                    "timezone",                            // mediawiki
                     NULL,
                 };
 
@@ -2486,6 +2487,13 @@ public:
         if (varName == "standard_conforming_strings"){
             return "on";
         }
+        if (varName == "search_path"){
+            auto searchPath = Settings.GUCSettings->Get("search_path");
+            return searchPath ? *searchPath : "public";
+        }
+        if (varName == "default_transaction_read_only"){
+            return "off"; // mediawiki
+        }
         if (varName == "transaction_isolation"){
             return "serializable";
         }
@@ -3721,6 +3729,17 @@ public:
         auto name = names.back();
         if (name == "shobj_description" || name == "obj_description") {
             AddWarning(TIssuesIds::PG_COMPAT, name + " function forced to NULL");
+            return L(A("Null"));
+        }
+
+        // for zabbix https://github.com/ydb-platform/ydb/issues/2904
+        if (name == "pg_try_advisory_lock" || name == "pg_try_advisory_lock_shared" || name == "pg_advisory_unlock" || name == "pg_try_advisory_xact_lock" || name == "pg_try_advisory_xact_lock_shared"){
+            AddWarning(TIssuesIds::PG_COMPAT, name + " function forced to return OK without waiting and without really lock/unlock");
+                return L(A("PgConst"), QA("true"), L(A("PgType"), QA("bool")));
+        }
+
+        if (name == "pg_advisory_lock" || name == "pg_advisory_lock_shared" || name == "pg_advisory_unlock_all" || name == "pg_advisory_xact_lock" || name == "pg_advisory_xact_lock_shared"){
+            AddWarning(TIssuesIds::PG_COMPAT, name + " function forced to return OK without waiting and without really lock/unlock");
             return L(A("Null"));
         }
 

1d7add528cf01b6268db280c1d39f10705bd6446
Filitov Mikhail <filitovme@gmail.com>
2024-04-09 16:07:41 +0300

    added tests for single vector case: int + char

--- ydb/library/yql/minikql/computation/mkql_vector_spiller_adapter_ut.cpp
+++ ydb/library/yql/minikql/computation/mkql_vector_spiller_adapter_ut.cpp
@@ -5,8 +5,11 @@
 
 namespace NKikimr::NMiniKQL {
 
-    std::vector<int> CreateSimpleVectorOfSize(size_t size) {
-        std::vector<int> v;
+namespace {
+
+    template <typename T>
+    std::vector<T> CreateSimpleVectorOfSize(size_t size) {
+        std::vector<T> v;
         v.reserve(size);
 
         for (size_t i = 0; i < size; ++i) {
@@ -16,35 +19,77 @@ namespace NKikimr::NMiniKQL {
         return v;
     }
 
-Y_UNIT_TEST_SUITE(TVectorSpillerAdapterTest) {
+    template <typename T>
+    void SaveRestoreAndCompareVectors(const std::vector<std::vector<T>>& vectors, size_t spillerChunkSizeInBytes) {
+        auto spiller = TVectorSpillerAdapter<T>(CreateMockSpiller(), spillerChunkSizeInBytes);
 
-    Y_UNIT_TEST(Simple) {
+        for (const auto& vec : vectors) {
+            std::vector<T> copiedVector = vec;
+            spiller.AddData(std::move(copiedVector));
 
-        std::vector<int> initialVector = CreateSimpleVectorOfSize(20);
-
-        auto spiller = TVectorSpillerAdapter<int>(CreateMockSpiller(), 20_B);
-
-        std::vector<int> vectorToSpill(initialVector);
-        spiller.AddData(std::move(vectorToSpill));
-
-        while (!spiller.IsAcceptingData()) {
-            spiller.Update();
+            while (!spiller.IsAcceptingData()) {
+                spiller.Update();
+            }
         }
+
         spiller.Finalize();
 
         while (!spiller.IsAcceptingDataRequests()) {
             spiller.Update();
         }
 
-        spiller.RequestNextVector();
-        
-        while (!spiller.IsDataReady()) {
-            spiller.Update();
-        }
+        for (const auto& vec : vectors) {
+            spiller.RequestNextVector();
 
-        auto vectorFromSpilling = spiller.ExtractVector();
+            while (!spiller.IsDataReady()) {
+                spiller.Update();
+            }
 
-        UNIT_ASSERT_VALUES_EQUAL(vectorFromSpilling, initialVector);
+            auto extractedVector = spiller.ExtractVector();
+
+            UNIT_ASSERT_VALUES_EQUAL(vec, extractedVector);
+        }   
+    }
+
+    template <typename T>
+    void RunTestForSingleVector(size_t vectorSize, size_t chunkSize, bool sizeInBytes) {
+        std::vector v = CreateSimpleVectorOfSize<T>(vectorSize);
+        size_t chunkSizeInBytes = sizeInBytes ? chunkSize : chunkSize * sizeof(T);
+        SaveRestoreAndCompareVectors<T>({v}, chunkSizeInBytes);
+    }
+}
+
+
+Y_UNIT_TEST_SUITE(TVectorSpillerAdapterTest_SingleVector) {
+    Y_UNIT_TEST(VectorOfExactChunkSize) {
+        size_t vectorSize = 5;
+
+        RunTestForSingleVector<int>(vectorSize, vectorSize, false);
+        RunTestForSingleVector<char>(vectorSize, vectorSize, false);
+    }
+
+    Y_UNIT_TEST(VectorLargerThanChunkSize) {
+        size_t vectorSize = 10;
+        size_t chunkSize = 3;
+
+        RunTestForSingleVector<int>(vectorSize, chunkSize, false);
+        RunTestForSingleVector<char>(vectorSize, chunkSize, false);
+    }
+
+    Y_UNIT_TEST(VectorLargerThanChunkSizePrime) {
+        size_t vectorSize = 10;
+        size_t chunkSizeBytes = 7;
+
+        RunTestForSingleVector<int>(vectorSize, chunkSizeBytes, true);
+        RunTestForSingleVector<char>(vectorSize, chunkSizeBytes, true);
+    }
+
+    Y_UNIT_TEST(VectorLessThanChunkSize) {
+        size_t vectorSize = 5;
+        size_t chunkSize = 10;
+
+        RunTestForSingleVector<int>(vectorSize, chunkSize, false);
+        RunTestForSingleVector<char>(vectorSize, chunkSize, false);
     }
 
 }

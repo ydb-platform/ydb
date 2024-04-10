08dfbffaf557fa5094baa7032d01f54e9c1e687d
Filitov Mikhail <filitovme@gmail.com>
2024-04-10 14:47:31 +0300

    added comments

--- ydb/library/yql/minikql/computation/mkql_vector_spiller_adapter.h
+++ ydb/library/yql/minikql/computation/mkql_vector_spiller_adapter.h
@@ -27,23 +27,29 @@ public:
     {
     }
 
+    ///Returns current stete of the adapter
     EState GetState() const {
         return State;
     }
 
+    ///Is adapter ready to spill next vector via AddData method.
+    ///Returns false in case when there are async operations in progress.
     bool IsAcceptingData() {
         return State == EState::AcceptingData;
     }
 
+    ///When data is ready ExtractVector() is expected to be called.
     bool IsDataReady() {
         return State == EState::DataReady;
     }
 
+    ///Adapter is ready to accept requests for vectors.
     bool IsAcceptingDataRequests() {
         return State == EState::AcceptingDataRequests;
     }
 
-    
+    ///Adds new vector to storage. Will not launch real disk operation if case of small vectors
+    ///(if inner buffer is not full).
     void AddData(std::vector<T>&& vec) {
         MKQL_ENSURE(CurrentVector.empty(), "Internal logic error");
         MKQL_ENSURE(State == EState::AcceptingData, "Internal logic error");
@@ -55,6 +61,9 @@ public:
         SaveNextPartOfVector();
     }
 
+    ///Should be used to update async operatrions statuses. 
+    ///For SpillingData state it will try to spill more content of inner buffer.
+    ///ForRestoringData state it will try to load more content of requested vector.
     void Update() {
         switch (State) {
             case EState::SpillingData:
@@ -86,12 +95,16 @@ public:
 
     }
 
+    ///Get requested vector.
     std::vector<T> ExtractVector() {
         StoredChunksElementsCount.pop();
         State = EState::AcceptingDataRequests;
         return CurrentVector;
     }
 
+    ///Start restoring next vector. If th eentire contents of the vector are in memory
+    ///State will be changed to DataREady without any async read operation. ExtractVector is expected
+    ///to be called immediately.
     void RequestNextVector() {
         MKQL_ENSURE(State == EState::AcceptingDataRequests, "Internal logic error");
         MKQL_ENSURE(!StoredChunksElementsCount.empty(), "Internal logic error");
@@ -103,6 +116,8 @@ public:
         LoadNextVector();
     }
 
+    ///Finalize will spill all the contents of inner buffer if any.
+    ///Is case if buffer is not ready async write operation will be started.
     void Finalize() {
         MKQL_ENSURE(CurrentVector.empty(), "Internal logic error");
         if (CurrentChunk.empty()) {

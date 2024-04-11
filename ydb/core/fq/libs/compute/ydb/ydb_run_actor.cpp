70ccac851e8170e999af39bae707c9ce55cc27a9
Grigoriy Pisarenko <grigoriypisar@ydb.tech>
2024-04-11 07:32:34 +0000

    Fixed error in ydb run actor

--- ydb/core/fq/libs/compute/ydb/ydb_run_actor.cpp
+++ ydb/core/fq/libs/compute/ydb/ydb_run_actor.cpp
@@ -114,10 +114,17 @@ public:
             ResignAndPassAway(response.Issues);
             return;
         }
+
+        if (OperationIsFailing()) {
+            LOG_I("StatusTrackerResponse (operation is failing). Status: " << response.Status << " Issues: " << response.Issues.ToOneLineString());
+            CreateResourcesCleaner();
+            return;
+        }
+
         ExecStatus = response.ExecStatus;
         Params.Status = response.ComputeStatus;
         LOG_I("StatusTrackerResponse (success) " << response.Status << " ExecStatus: " << static_cast<int>(response.ExecStatus) << " Issues: " << response.Issues.ToOneLineString());
-        if (response.ExecStatus == NYdb::NQuery::EExecStatus::Completed && !OperationIsFailing()) {
+        if (response.ExecStatus == NYdb::NQuery::EExecStatus::Completed) {
             Register(ActorFactory->CreateResultWriter(SelfId(), Connector, Pinger, Params.OperationId, true).release());
         } else {
             CreateResourcesCleaner();

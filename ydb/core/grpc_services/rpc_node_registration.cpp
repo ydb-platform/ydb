29583979be93a541267a5b5438f2c243d32d2ee0
Ilia Shakhov <pixcc@ydb.tech>
2024-04-11 14:56:32 +0300

    Fix for empty name in rpc (#3670)

--- ydb/core/grpc_services/rpc_node_registration.cpp
+++ ydb/core/grpc_services/rpc_node_registration.cpp
@@ -106,7 +106,9 @@ public:
 
         auto request = TEvNodeRegistrationRequest::GetProtoRequest(Request);
         Result.set_node_id(rec.GetNode().GetNodeId());
-        Result.set_node_name(rec.GetNode().GetName());
+        if (rec.GetNode().HasName()) {
+             Result.set_node_name(rec.GetNode().GetName());
+        }
         Result.set_expire(rec.GetNode().GetExpire());
         Result.set_domain_path(request->domain_path());
         CopyNodeInfo(Result.add_nodes(), rec.GetNode());

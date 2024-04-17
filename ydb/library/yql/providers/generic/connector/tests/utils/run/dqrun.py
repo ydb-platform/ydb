780b0eb1c8ef34af3a4307fa7699a494c005adac
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-04-17 22:01:53 +0000

    Add timeouts for calling subprocess kqprun/dqrun

--- ydb/library/yql/providers/generic/connector/tests/utils/run/dqrun.py
+++ ydb/library/yql/providers/generic/connector/tests/utils/run/dqrun.py
@@ -228,13 +228,25 @@ class DqRunner(Runner):
 
         # For debug add option --trace-opt to args
         cmd = f'{self.dqrun_path} -s -p {script_path} --fs-cfg={fs_conf_path} --gateways-cfg={gateways_conf_path} --result-file={result_path} --format="binary" -v 7'
-        out = subprocess.run(cmd, shell=True, capture_output=True)
 
+        output = None
         data_out = None
         data_out_with_types = None
         schema = None
+        returncode = 0
 
-        if out.returncode == 0:
+        try:
+            output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT, timeout=60)
+        except subprocess.CalledProcessError as e:
+            LOGGER.error(
+                'Execution failed:\n\nSTDOUT: %s\n\nSTDERR: %s\n\n',
+                e.stdout.decode('utf-8') if e.stdout else None,
+                e.stderr.decode('utf-8') if e.stderr else None,
+            )
+
+            output = e.stdout
+            returncode = e.returncode
+        else:
             LOGGER.info('Execution succeeded: ')
             # Parse output
             with open(result_path, 'r') as f:
@@ -250,24 +262,15 @@ class DqRunner(Runner):
 
             artifacts.dump_yson(data_out, test_name, "data_out.yson")
             artifacts.dump_str(data_out_with_types, test_name, "data_out_with_types.yson")
-        else:
-            LOGGER.error(
-                'Execution failed:\n\nSTDOUT: %s\n\nSTDERR: %s\n\n',
-                out.stdout.decode('utf-8'),
-                out.stderr.decode('utf-8'),
-            )
 
-        with open(artifacts.make_path(test_name, "dqrun.err"), "w") as f:
-            f.write(out.stderr.decode('utf-8'))
-
-        with open(artifacts.make_path(test_name, "dqrun.out"), "w") as f:
-            f.write(out.stdout.decode('utf-8'))
+        finally:
+            with open(artifacts.make_path(test_name, "kqprun.out"), "w") as f:
+                f.write(output.decode('utf-8'))
 
         return Result(
             data_out=data_out,
             data_out_with_types=data_out_with_types,
             schema=schema,
-            stdout=out.stdout.decode('utf-8'),
-            stderr=out.stderr.decode('utf-8'),
-            returncode=out.returncode,
+            output=output.decode('utf-8') if output else None,
+            returncode=returncode,
         )

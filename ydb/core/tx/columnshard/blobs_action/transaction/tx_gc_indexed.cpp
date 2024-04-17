1cb8956483cfa04dc01abc06e4c71df46505c13a
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-04-17 15:16:15 +0300

    profile memory in transactions

--- ydb/core/tx/columnshard/blobs_action/transaction/tx_gc_indexed.cpp
+++ ydb/core/tx/columnshard/blobs_action/transaction/tx_gc_indexed.cpp
@@ -2,12 +2,14 @@
 
 namespace NKikimr::NColumnShard {
 bool TTxGarbageCollectionFinished::Execute(TTransactionContext& txc, const TActorContext& /*ctx*/) {
+    TMemoryProfileGuard mpg("TTxGarbageCollectionFinished::Execute");
     AFL_DEBUG(NKikimrServices::TX_COLUMNSHARD)("tx", "TxGarbageCollectionFinished")("event", "execute");
     NOlap::TBlobManagerDb blobManagerDb(txc.DB);
     Action->OnExecuteTxAfterCleaning(*Self, blobManagerDb);
     return true;
 }
 void TTxGarbageCollectionFinished::Complete(const TActorContext& /*ctx*/) {
+    TMemoryProfileGuard mpg("TTxGarbageCollectionFinished::Complete");
     AFL_DEBUG(NKikimrServices::TX_COLUMNSHARD)("tx", "TxGarbageCollectionFinished")("event", "complete");
     Action->OnCompleteTxAfterCleaning(*Self, Action);
 }

4fb3bd2aa5fa0266b229d111eb7a66d1e78eb6c1
Nikolay Perfilov <pnv1@yandex-team.ru>
2024-04-18 15:56:56 +0300

    Add Explain-analyze mode

--- ydb/public/lib/ydb_cli/commands/ydb_sql.cpp
+++ ydb/public/lib/ydb_cli/commands/ydb_sql.cpp
@@ -1,32 +1,41 @@
 #include "ydb_sql.h"
 
+#include <library/cpp/json/json_reader.h>
 #include <ydb/public/lib/json_value/ydb_json_value.h>
 #include <ydb/public/lib/ydb_cli/common/pretty_table.h>
 #include <ydb/public/lib/ydb_cli/common/print_operation.h>
 #include <ydb/public/lib/ydb_cli/common/query_stats.h>
 #include <ydb/public/lib/ydb_cli/common/interactive.h>
+#include <ydb/public/sdk/cpp/client/ydb_proto/accessor.h>
 #include <util/generic/queue.h>
+#include <google/protobuf/text_format.h>
 
 namespace NYdb {
 namespace NConsoleClient {
 
 TCommandSql::TCommandSql()
-    : TYdbOperationCommand("sql", {}, "Execute SQL query")
+    : TYdbCommand("sql", {}, "Execute SQL query")
 {}
 
 TCommandSql::TCommandSql(TString query, TString collectStatsMode)
-    : TYdbOperationCommand("sql", {}, "Execute SQL query")
+    : TYdbCommand("sql", {}, "Execute SQL query")
 {
     Query = std::move(query);
     CollectStatsMode = std::move(collectStatsMode);
 }
 
 void TCommandSql::Config(TConfig& config) {
-    TYdbOperationCommand::Config(config);
+    TYdbCommand::Config(config);
     AddExamplesOption(config);
-    config.Opts->AddLongOption("explain", "Execute query in explain mode. Shows query plan")
+    config.Opts->AddLongOption("explain", "Execute explain request for the query. Shows query plan. "
+            "The query is not actually executed, thus does not affect the database.")
         .StoreTrue(&ExplainMode);
-    config.Opts->AddLongOption("stats", "Collect statistics mode [none, basic, full, profile]")
+    config.Opts->AddLongOption("explain-analyze", "Execute query in explain-analyze mode. Shows query plan only. "
+            "Query results are ignored.\n"
+            "Important! The query is actually executed, so any changes will be applied in the database.")
+        .StoreTrue(&ExplainAnalyzeMode);
+    config.Opts->AddLongOption("stats", "Statistics collection mode [none, basic, full, profile]\n"
+            "Has no effect with analyze mode.")
         .RequiredArgument("[String]").StoreResult(&CollectStatsMode);
     config.Opts->AddLongOption('q', "query", "Text of query to execute").RequiredArgument("[String]")
         .StoreResult(&Query);
@@ -79,6 +88,28 @@ void TCommandSql::Parse(TConfig& config) {
         throw TMisuseException() << "Both mutually exclusive options \"Text of query\" (\"--query\", \"-q\") "
             << "and \"Path to file with query text\" (\"--file\", \"-f\") were provided.";
     }
+    if (ExplainMode && ExplainAnalyzeMode) {
+        throw TMisuseException() << "Both mutually exclusive options \"Explain mode\" (\"--explain\") "
+            << "and \"Explain-analyze mode\" (\"--explain-analyze\") were provided.";
+    }
+    if (ExplainAnalyzeMode && !CollectStatsMode.Empty()) {
+        auto optStatsMode = NQuery::ParseStatsMode(CollectStatsMode);
+        if (optStatsMode.has_value()) {
+            auto statsMode = optStatsMode.value();
+            switch (statsMode) {
+                case NQuery::EStatsMode::None:
+                case NQuery::EStatsMode::Basic:
+                throw TMisuseException() << "Statistics collection mode \"" << CollectStatsMode
+                    << "\" is too low for explain-analyze mode to show any statistics. Use at least \"full\" mode.";
+                default:
+                break;
+            }
+        }
+        if (ExplainAnalyzeMode && (CollectStatsMode == "")) {
+            throw TMisuseException() << "Both mutually exclusive options \"Explain mode\" (\"--explain\") "
+                << "and \"Explain-analyze mode\" (\"--explain-analyze\") were provided.";
+        }
+    }
     if (QueryFile) {
         Query = ReadFromFile(QueryFile, "query");
     }
@@ -94,20 +125,41 @@ int TCommandSql::RunCommand(TConfig& config) {
     NQuery::TQueryClient client(driver);
 
     NQuery::TExecuteQuerySettings settings;
-    settings.StatsMode(ParseQueryStatsModeOrThrow(CollectStatsMode, NQuery::EStatsMode::None));
-    settings.ExecMode(ExplainMode ? NQuery::EExecMode::Explain : NQuery::EExecMode::Execute);
 
     SetInterruptHandlers();
 
-    if (!Parameters.empty() || !IsStdinInteractive()) {
-        THolder<TParamsBuilder> paramBuilder;
-        while (!IsInterrupted() && GetNextParams(paramBuilder)) {
+    if (ExplainMode) {
+        // Execute explain request for the query
+        settings.ExecMode(NQuery::EExecMode::Explain);
+    } else {
+        // Execute query
+        settings.ExecMode(NQuery::EExecMode::Execute);
+        auto defaultStatsMode = ExplainAnalyzeMode ? NQuery::EStatsMode::Full : NQuery::EStatsMode::None;
+        settings.StatsMode(ParseQueryStatsModeOrThrow(CollectStatsMode, defaultStatsMode));
+        if (!Parameters.empty() || !IsStdinInteractive()) {
+            // Execute query with parameters
+            THolder<TParamsBuilder> paramBuilder;
+            while (!IsInterrupted() && GetNextParams(paramBuilder)) {
+                auto asyncResult = client.StreamExecuteQuery(
+                        Query,
+                        NQuery::TTxControl::BeginTx().CommitTx(),
+                        paramBuilder->Build(),
+                        settings
+                    );
+
+                auto result = asyncResult.GetValueSync();
+                ThrowOnError(result);
+                if (!PrintResponse(result)) {
+                    return EXIT_FAILURE;
+                }
+            }
+        } else {
+            // Execute query without parameters
             auto asyncResult = client.StreamExecuteQuery(
-                    Query,
-                    NQuery::TTxControl::BeginTx().CommitTx(),
-                    paramBuilder->Build(),
-                    settings
-                );
+                Query,
+                NQuery::TTxControl::BeginTx().CommitTx(),
+                settings
+            );
 
             auto result = asyncResult.GetValueSync();
             ThrowOnError(result);
@@ -115,20 +167,28 @@ int TCommandSql::RunCommand(TConfig& config) {
                 return EXIT_FAILURE;
             }
         }
-    } else {
-        auto asyncResult = client.StreamExecuteQuery(
-            Query,
-            NQuery::TTxControl::BeginTx().CommitTx(),
-            settings
-        );
-
-        auto result = asyncResult.GetValueSync();
-        ThrowOnError(result);
-        if (!PrintResponse(result)) {
-            return EXIT_FAILURE;
-        }
     }
     return EXIT_SUCCESS;
+}
+
+using namespace NJson;
+
+namespace {
+    void PrintJson(const TString &jsonString) {
+        auto stringBuf = TStringBuf(jsonString);
+
+        TJsonValue jsonValue;
+        try {
+            ReadJsonTree(stringBuf, &jsonValue, true/*throw on error*/);
+        }
+        catch (const TJsonException &ex) {
+            Cerr << "Can't parse json: " << ex.what();
+        }
+        NJsonWriter::TBuf w;
+        w.SetIndentSpaces(2);
+        w.WriteJsonValue(&jsonValue);
+        Cout << w.Str() << Endl;
+    }
 }
 
 bool TCommandSql::PrintResponse(NQuery::TExecuteQueryIterator& result) {
@@ -146,16 +206,29 @@ bool TCommandSql::PrintResponse(NQuery::TExecuteQueryIterator& result) {
                 ThrowOnError(streamPart);
             }
 
-            if (streamPart.HasResultSet()) {
+            if (streamPart.HasResultSet() && !ExplainAnalyzeMode) {
                 printer.Print(streamPart.GetResultSet());
             }
 
             if (!streamPart.GetStats().Empty()) {
-                const auto& queryStats = *streamPart.GetStats();
-                stats = queryStats.ToString();
+                if (OutputFormat == EOutputFormat::JsonUnicode) {
+                    if (streamPart.GetStats()->GetPlan().Defined()) {
+                        Cout << "Plan:" << Endl;
+                        PrintJson(streamPart.GetStats()->GetPlan().GetRef());
+                    }
+                    if (streamPart.GetStats()->GetAst().Defined()) {
+                        Cout << "Ast:" << Endl << streamPart.GetStats()->GetAst().GetRef() << Endl;
+                    }
+                } else if (OutputFormat == EOutputFormat::JsonBase64) {
+                    auto& proto = NYdb::TProtoAccessor::GetProto(*streamPart.GetStats());
+                    Cout << "Stats proto:" << Endl << proto.DebugString() << Endl;
+                } else {
+                    const auto& queryStats = *streamPart.GetStats();
+                    stats = queryStats.ToString();
 
-                if (queryStats.GetPlan()) {
-                    plan = queryStats.GetPlan();
+                    if (queryStats.GetPlan()) {
+                        plan = queryStats.GetPlan();
+                    }
                 }
             }
         }

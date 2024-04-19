a68faed0a7b525a750d5f566e5c3fc60424cc91e
Egor Zudin <e-zudin@ydb.tech>
2024-04-19 13:16:14 +0300

    YQ-2863: add StreamExecuteScanQuery and DeleteSession for ydb_over_fq (#3787)

--- ydb/core/grpc_services/ydb_over_fq/create_session.cpp
+++ ydb/core/grpc_services/ydb_over_fq/create_session.cpp
@@ -33,9 +33,45 @@ private:
     }
 };
 
+class TDeleteSessionRPC : public TRpcOperationRequestActor<
+    TCreateSessionRPC, TGrpcRequestOperationCall<Ydb::Table::DeleteSessionRequest, Ydb::Table::DeleteSessionResponse>> {
+public:
+    using TBase = TRpcOperationRequestActor<
+        TCreateSessionRPC,
+        TGrpcRequestOperationCall<Ydb::Table::DeleteSessionRequest, Ydb::Table::DeleteSessionResponse>>;
+    using TBase::Become;
+    using TBase::Send;
+    using TBase::PassAway;
+    using TBase::Request_;
+    using TBase::GetProtoRequest;
+
+    using TBase::TBase;
+
+    void Bootstrap(const TActorContext& ctx) {
+        DeleteSessionImpl(ctx);
+    }
+
+private:
+    void DeleteSessionImpl(const TActorContext& ctx) {
+        const TString FakeSessionId = "fake-session-id";
+
+        if (GetProtoRequest()->session_id() == FakeSessionId) {
+            Reply(Ydb::StatusIds::SUCCESS, ctx);
+        } else {
+            Reply(Ydb::StatusIds::BAD_REQUEST, ctx);
+        }
+    }
+};
+
 std::function<void(std::unique_ptr<IRequestOpCtx>, const IFacilityProvider&)> GetCreateSessionExecutor(NActors::TActorId) {
     return [](std::unique_ptr<IRequestOpCtx> p, const IFacilityProvider& f) {
         f.RegisterActor(new TCreateSessionRPC(p.release()));
+    };
+}
+
+std::function<void(std::unique_ptr<IRequestOpCtx>, const IFacilityProvider&)> GetDeleteSessionExecutor(NActors::TActorId) {
+    return [](std::unique_ptr<IRequestOpCtx> p, const IFacilityProvider& f) {
+        f.RegisterActor(new TDeleteSessionRPC(p.release()));
     };
 }
 

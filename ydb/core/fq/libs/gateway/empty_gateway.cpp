394a83e1e5a898b09ae858acb478ea1c9998feaf
Grigoriy Pisarenko <grigoriypisar@ydb.tech>
2024-04-19 07:45:10 +0000

    Fixed error in empty gateway

--- ydb/core/fq/libs/gateway/empty_gateway.cpp
+++ ydb/core/fq/libs/gateway/empty_gateway.cpp
@@ -63,28 +63,30 @@ public:
         }
         params.SetSession(sessionId);
 
+        auto result = NThreading::NewPromise<NYql::IDqGateway::TResult>();
+        auto event = MakeHolder<TEvents::TEvGraphParams>(params);
+        event->IsEvaluation = FromString<bool>(queryParams.Value("Evaluation", "false")) || FromString<bool>(queryParams.Value("Precompute", "false"));
+
         // result promise should be resolved here
         // in the same thread which execute yql facade functions
         // to avoid data races for TExprNode
-        auto result = NThreading::NewPromise<NYql::IDqGateway::TResult>();
-
-        NYql::IDqGateway::TResult gatewayResult;
-        // fake it till you make it
-        // generate dummy result for YQL facade now, remove this gateway completely
-        // when top-level YQL facade call like Preprocess() is implemented
-        if (params.GetResultType()) {
-            // for resultable graphs return dummy "select 1" result (it is not used and is required to satisfy YQL facade only)
-            gatewayResult.SetSuccess();
-            gatewayResult.Data = "[[\001\0021]]";
-            gatewayResult.Truncated = true;
-            gatewayResult.RowsCount = 0;
-        } else {
-            // for resultless results expect infinite INSERT FROM SELECT and just return "nothing"
+        if (!event->IsEvaluation) {
+            NYql::IDqGateway::TResult gatewayResult;
+            // fake it till you make it
+            // generate dummy result for YQL facade now, remove this gateway completely
+            // when top-level YQL facade call like Preprocess() is implemented
+            if (params.GetResultType()) {
+                // for resultable graphs return dummy "select 1" result (it is not used and is required to satisfy YQL facade only)
+                gatewayResult.SetSuccess();
+                gatewayResult.Data = "[[\001\0021]]";
+                gatewayResult.Truncated = true;
+                gatewayResult.RowsCount = 0;
+            } else {
+                // for resultless results expect infinite INSERT FROM SELECT and just return "nothing"
+            }
+            result.SetValue(gatewayResult);
         }
-        result.SetValue(gatewayResult);
 
-        auto event = MakeHolder<TEvents::TEvGraphParams>(params);
-        event->IsEvaluation = FromString<bool>(queryParams.Value("Evaluation", "false")) || FromString<bool>(queryParams.Value("Precompute", "false"));
         event->Result = result;
         NActors::TActivationContext::Send(new NActors::IEventHandle(RunActorId, {}, event.Release()));
 

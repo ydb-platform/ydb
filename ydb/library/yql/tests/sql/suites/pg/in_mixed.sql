48a73d439eadadd987fb1972b4b9907e4d713de5
Mars Agliullin <marsaly@ydb.tech>
2024-04-24 00:29:54 +0300

    Canonization

--- ydb/library/yql/tests/sql/suites/pg/in_mixed.sql
+++ ydb/library/yql/tests/sql/suites/pg/in_mixed.sql
@@ -4,7 +4,7 @@
 select 1 in ('1', '2');
 
 -- splits into IN over numerics and IN over text
-select '1' in (0, 0.0, 1, '1'::text);
+select '1' in (0, 0.0, 1, '1'::text, '3'::char(3));
 
 -- mixture of types in rhs, expression in lhs
 select (c::int + 1) in (1, 2.9, '4') from (values ('0'), ('1')) as t(c);
@@ -14,9 +14,8 @@ select array[1, 2] in (array[2, 4], array[1, 2]);
 
 -- NULL in rhs
 select 1 in (0, NULL);
-select 1 in (1, NULL);
+select 1 in ('1', NULL);
 
 -- non-PG types handling
-select index in ('2', 4, '8'::char(3)) from plato."Input";
-select '2' in (NULL, index, index::text) from plato."Input";
+select index in ('2', 4) from plato."Input";
 

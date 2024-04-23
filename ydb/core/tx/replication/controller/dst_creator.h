e65cd650dafc1b8d686023105bf782cb07791ec8
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2024-04-23 10:59:51 +0300

    Allow to specify directories in CREATE ASYNC REPLICATION (#4011)

--- ydb/core/tx/replication/controller/dst_creator.h
+++ ydb/core/tx/replication/controller/dst_creator.h
@@ -5,7 +5,7 @@
 namespace NKikimr::NReplication::NController {
 
 IActor* CreateDstCreator(TReplication::TPtr replication, ui64 targetId, const TActorContext& ctx);
-IActor* CreateDstCreator(const TActorId& parent, ui64 schemeShardId, const TActorId& proxy,
+IActor* CreateDstCreator(const TActorId& parent, ui64 schemeShardId, const TActorId& proxy, const TPathId& pathId,
     ui64 rid, ui64 tid, TReplication::ETargetKind kind, const TString& srcPath, const TString& dstPath);
 
 }

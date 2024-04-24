db0caed1d029610d813de7341cd39a85fbaf8802
Tony-Romanov <150126326+Tony-Romanov@users.noreply.github.com>
2024-04-24 17:58:55 +0200

    Simplify TPredicateNode structure. (#4070)

--- ydb/core/kqp/opt/physical/predicate_collector.h
+++ ydb/core/kqp/opt/physical/predicate_collector.h
@@ -5,38 +5,16 @@ namespace NKikimr::NKqp::NOpt {
 
 using namespace NYql;
 
-struct TPredicateNode {
-    enum class EBoolOp {
-        Undefined = 0,
-        And,
-        Or,
-        Xor,
-        Not
-    };
-
-    TPredicateNode() = default;
-
-    TPredicateNode(TExprNode::TPtr nodePtr)
-        : ExprNode(std::move(nodePtr))
-    {}
-
-    TPredicateNode(const NNodes::TExprBase& node)
-        : ExprNode(node)
-    {}
+struct TOLAPPredicateNode {
+    TExprNode::TPtr ExprNode;
+    std::vector<TOLAPPredicateNode> Children;
+    bool CanBePushed = false;
 
     bool IsValid() const {
-        return ExprNode.IsValid() &&
-            (Op == EBoolOp::Undefined || std::all_of(Children.cbegin(), Children.cend(), std::bind(&TPredicateNode::IsValid, std::placeholders::_1)));
+        return ExprNode && std::all_of(Children.cbegin(), Children.cend(), std::bind(&TOLAPPredicateNode::IsValid, std::placeholders::_1));
     }
-
-    void SetPredicates(const std::vector<TPredicateNode>& predicates, TExprContext& ctx, TPositionHandle pos);
-
-    NNodes::TMaybeNode<NNodes::TExprBase> ExprNode;
-    std::vector<TPredicateNode> Children;
-    EBoolOp Op = EBoolOp::Undefined;
-    bool CanBePushed = false;
 };
 
-void CollectPredicates(const NNodes::TExprBase& predicate, TPredicateNode& predicateTree, const TExprNode* lambdaArg, const NNodes::TExprBase& lambdaBody);
+void CollectPredicates(const NNodes::TExprBase& predicate, TOLAPPredicateNode& predicateTree, const TExprNode* lambdaArg, const NNodes::TExprBase& lambdaBody);
 
 }

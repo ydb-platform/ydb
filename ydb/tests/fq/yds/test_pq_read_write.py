7e84e51fb19f51c79c663eaa47e3831339487856
Dmitry Kardymon <kardymon-d@ydb.tech>
2024-04-24 14:42:39 +0000

    use actor count to wait starting

--- ydb/tests/fq/yds/test_pq_read_write.py
+++ ydb/tests/fq/yds/test_pq_read_write.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time
 import pytest
 from ydb.tests.tools.fq_runner.kikimr_utils import yq_v1
 from ydb.tests.tools.datastreams_helpers.test_yds_base import TestYdsBase
@@ -19,7 +18,7 @@ def start_yds_query(kikimr, client, sql, with_checkpoints) -> str:
     if with_checkpoints:
         kikimr.compute_plane.wait_zero_checkpoint(query_id)
     else:
-        time.sleep(5)
+        kikimr.control_plane.wait_worker_count(1, "DQ_PQ_READ_ACTOR", 1)
     return query_id
 
 

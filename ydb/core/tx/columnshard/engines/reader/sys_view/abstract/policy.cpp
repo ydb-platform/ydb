c5590ab728cbf07cf85ce72532ca195cf346ebb3
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-05-04 12:54:47 +0300

    fix

--- ydb/core/tx/columnshard/engines/reader/sys_view/abstract/policy.cpp
+++ ydb/core/tx/columnshard/engines/reader/sys_view/abstract/policy.cpp
@@ -6,7 +6,7 @@ namespace NKikimr::NOlap::NReader::NSysView::NAbstract {
 
 THolder<ISysViewPolicy> ISysViewPolicy::BuildByPath(const TString& tablePath) {
     auto fixed = TFsPath(tablePath).Fix();
-    if (fixed.Parent().GetName() != NSysView::SysPathName) {
+    if (fixed.Parent().GetName() != ::NKikimr::NSysView::SysPathName) {
         return nullptr;
     } else {
         return TFactory::MakeHolder(fixed.GetName());

849d83d472db6b7f6512beef8c66b1853904a5da
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-05-04 12:41:04 +0300

    fix

--- ydb/core/tx/columnshard/engines/reader/sys_view/abstract/policy.cpp
+++ ydb/core/tx/columnshard/engines/reader/sys_view/abstract/policy.cpp
@@ -1,11 +1,12 @@
 #include "policy.h"
 #include <util/folder/path.h>
+#include <ydb/core/sys_view/common/path.h>
 
 namespace NKikimr::NOlap::NReader::NSysView::NAbstract {
 
 THolder<ISysViewPolicy> ISysViewPolicy::BuildByPath(const TString& tablePath) {
     auto fixed = TFsPath(tablePath).Fix();
-    if (fixed.Parent().GetName() != ".sys") {
+    if (fixed.Parent().GetName() != NSysView::SysPathName) {
         return nullptr;
     } else {
         return TFactory::MakeHolder(fixed.GetName());

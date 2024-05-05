9f47f08ced61b17fdfa4118ac183d25558b71ae5
azevaykin <azevaykin@ydb.tech>
2024-05-05 07:13:54 +0000

    Remove bit indexes

--- ydb/library/yql/udfs/common/knn/knn.cpp
+++ ydb/library/yql/udfs/common/knn/knn.cpp
@@ -83,53 +83,13 @@ SIMPLE_STRICT_UDF(TManhattanDistance, TOptional<ui16>(TAutoMap<const char*>, TAu
     return TUnboxedValuePod{ret.value()};
 }
 
-SIMPLE_STRICT_UDF(TBitIndexes, TOptional<TListType<ui64>>(TAutoMap<const char*>, TAutoMap<const char*>, ui16, ui16, ui64)) {
-    Y_UNUSED(valueBuilder);
-
-    const TArrayRef<const ui64> targetVector = TKnnBitVectorSerializer::GetArray64(args[0].AsStringRef()); 
-    const TArrayRef<const ui64> storedVector = TKnnBitVectorSerializer::GetArray64(args[1].AsStringRef()); 
-    const ui16 topK = args[2].Get<ui16>();
-    const ui16 distanceThreshold = args[3].Get<ui16>();
-    const ui64 seed = args[4].Get<ui64>();
-
-    if (Y_UNLIKELY(targetVector.empty() || storedVector.empty() || storedVector.size() % targetVector.size() != 0))
-        return {};    
-
-    constexpr ui16 MAX_INDEXES = 128;
-    typedef std::pair<ui16, ui16> TSimilarityAndIndex;
-    TPriorityQueue<TSimilarityAndIndex, TStackVec<TSimilarityAndIndex, MAX_INDEXES + 1>> heap;
-
-    // Add vector distances to priority queue
-    const ui32 totalVectors = storedVector.size() / targetVector.size();
-    for (ui32 index = 0; index < totalVectors; ++index) {
-        const TArrayRef<const ui64> nextVector = {storedVector.data() + index * targetVector.size(), targetVector.size()};
-        ui32 distance = KnnManhattanDistance(targetVector, nextVector);
-        if (distance > distanceThreshold)
-            continue;
-        heap.push({distance, index});
-        if (heap.size() == topK + 1)        
-            heap.pop();
-    }
-
-    // Return min elemens from priority queue
-    TUnboxedValue* items = nullptr;
-    ui64 heapSize = heap.size();
-    auto res = valueBuilder->NewArray(heapSize, items);
-    for (ui64 i = 0; i < heapSize; ++i) {
-        items[heapSize-i-1] = TUnboxedValuePod{heap.top().second + seed};
-        heap.pop();
-    }
-    return res.Release();
-}
-
 SIMPLE_MODULE(TKnnModule,
     TFromBinaryString, 
     TToBinaryString,
     TInnerProductSimilarity,
     TCosineSimilarity,
     TCosineDistance,
-    TManhattanDistance,
-    TBitIndexes
+    TManhattanDistance
     )
 
 REGISTER_MODULES(TKnnModule)

6792f9c0980f977a76c50c4c5b693fae9ad96912
Hor911 <hor911@ydb.tech>
2024-05-09 02:56:12 +0300

    Fix uninitialized mem access in msan UT (#4413)

--- ydb/core/kqp/host/kqp_host.cpp
+++ ydb/core/kqp/host/kqp_host.cpp
@@ -1011,7 +1011,9 @@ public:
         SessionCtx->SetCluster(cluster);
         SessionCtx->SetTempTables(std::move(tempTablesState));
 
-        ExternalSourceFactory = NExternalSource::CreateExternalSourceFactory({}, FederatedQuerySetup->S3GatewayConfig.GetGeneratorPathsLimit());
+        if (FederatedQuerySetup) {
+            ExternalSourceFactory = NExternalSource::CreateExternalSourceFactory({}, FederatedQuerySetup->S3GatewayConfig.GetGeneratorPathsLimit());
+        }
     }
 
     IAsyncQueryResultPtr ExecuteSchemeQuery(const TKqpQueryRef& query, bool isSql, const TExecSettings& settings) override {

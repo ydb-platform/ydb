840f2c1173142f95264150a69f24e600bdb2dc13
Nikita Saveliev <savnik@ydb.tech>
2024-05-13 06:10:23 +0000

    Fix

--- ydb/core/persqueue/fetch_request_actor.cpp
+++ ydb/core/persqueue/fetch_request_actor.cpp
@@ -481,6 +481,7 @@ public:
 
         if (IsQuotaRequired()) {
             PendingQuotaAmount = CalcRuConsumption(GetPayloadSize(record)) + Settings.ChargeExtraRU ? 1 : 0;
+            Settings.ChargeExtraRU = 0;
             RequestDataQuota(PendingQuotaAmount, ctx);
         } else {
             ProceedFetchRequest(ctx);

7522bb040ca10e7dd53e237a65e0c18f25638587
Filitov Mikhail <filitovme@gmail.com>
2024-05-14 14:17:41 +0200

    YQL-17175 added maximum memory limit reached flag (#4480)

--- ydb/library/yql/minikql/aligned_page_pool.cpp
+++ ydb/library/yql/minikql/aligned_page_pool.cpp
@@ -503,8 +503,8 @@ void TAlignedPagePoolImpl<T>::Free(void* ptr, size_t size) noexcept {
 
 template<typename T>
 void TAlignedPagePoolImpl<T>::UpdateMemoryYellowZone() {
-    if (IncreaseMemoryLimitCallback) return;
     if (Limit == 0) return;
+    if (IncreaseMemoryLimitCallback && !IsMaximumLimitValueReached) return;
 
     ui8 usedMemoryPercent = 100 * GetUsed() / Limit;
     if (usedMemoryPercent >= EnableMemoryYellowZoneThreshold) {

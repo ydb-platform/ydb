0c1b3e36cf57af64cb0bd3eafda5e70417862af5
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2024-05-14 13:20:16 +0000

    fix

--- .github/scripts/send_build_stats.py
+++ .github/scripts/send_build_stats.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import json
+import datetime
 import os
-import ydb 
+import ydb
 import uuid
 import subprocess
 
@@ -37,60 +37,67 @@ from_env_columns = [
     "github_head_ref",
     "github_workflow",
     "github_workflow_ref",
-
     "github_sha",
     "github_repository",
-
     "github_event_name",
-
     "github_ref_type",
     "github_ref_name",
     "github_ref",
 ]
 
-string_types = from_env_columns + [
+string_columns = from_env_columns + [
     "id",
-
-
+    "git_commit_message",
     "binary_path",
     "build_type",
 ]
 
-uint64_types = [
+datetime_colums = [
+    "git_commit_time",
+]
+
+uint64_columns = [
     "size_bytes",
     "size_stripped_bytes",
 ]
 
+all_columns = string_columns + datetime_colums + uint64_columns
+
+
+def sanitize_str(s):
+    # YDB SDK expects bytes for 'String' columns
+    if s is None:
+        s = "N\A"
+    return s.encode("utf-8")
+
 
 def main():
-    # token = open("/home/maxim-yurchuk/.yc_token").read().strip()
-    # os.environ["IAM_TOKEN"] = "/home/maxim-yurchuk/.iam"
-    
-    # os.environ["YDB_ACCESS_TOKEN_CREDENTIALS"] = "YDB_ACCESS_TOKEN_CREDENTIALS"
-    
-    # token = open("/home/maxim-yurchuk/.iam").read().strip()
-    # os.environ["YDB_ACCESS_TOKEN_CREDENTIALS"] = token
+    """
+    os.environ[
+        "YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS"
+    ] = "/tmp/ydb_service_account.json"
+    """
 
-
-    os.environ["YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS"] = "/tmp/ydb_service_account.json"
-    
     with ydb.Driver(
         endpoint="grpcs://ydb.serverless.yandexcloud.net:2135",
         database="/ru-central1/b1ggceeul2pkher8vhb6/etn6d1qbals0c29ho4lf",
-        # credentials=ydb.credentials.AccessTokenCredentials(token),
-        credentials=ydb.credentials_from_env_variables()
+        credentials=ydb.credentials_from_env_variables(),
     ) as driver:
         driver.wait(timeout=10, fail_fast=True)
-        session = ydb.retry_operation_sync(lambda: driver.table_client.session().create())
-        session = driver.table_client.session().create()  # TODO REMOVE
+        session = ydb.retry_operation_sync(
+            lambda: driver.table_client.session().create()
+        )
         with session.transaction() as tx:
             text_query_builder = []
-            for type_ in string_types:
+            for type_ in string_columns:
                 text_query_builder.append("DECLARE ${} as String;".format(type_))
-            for type_ in uint64_types:
+            for type_ in uint64_columns:
                 text_query_builder.append("DECLARE ${} as Uint64;".format(type_))
-            
-            text_query_builder.append("""INSERT INTO binary_size
+            for type_ in datetime_colums:
+                text_query_builder.append("DECLARE ${} as Datetime;".format(type_))
+
+            text_query_builder.append(
+                """INSERT INTO binary_size
 (
     {}
 )
@@ -98,60 +105,64 @@ VALUES
 (
     {}                   
 );                         
-""".format(", \n    ".join(string_types + uint64_types), ", \n    ".join(["$" + column for column in string_types + uint64_types])))
+""".format(
+                    ", \n    ".join(all_columns),
+                    ", \n    ".join(["$" + column for column in all_columns]),
+                )
+            )
 
             text_query = "\n".join(text_query_builder)
 
             prepared_query = session.prepare(text_query)
-            
-            binary_size_bytes = subprocess.check_output(["bash", "-c", "cat {} | wc -c".format(YDBD_PATH)])
-            binary_size_stripped_bytes = subprocess.check_output(["bash", "-c", "./ya tool strip {} -o - | wc -c".format(YDBD_PATH)])
 
-            build_type = os.environ.get("build_type", "N/A").encode("utf-8")
+            binary_size_bytes = subprocess.check_output(
+                ["bash", "-c", "cat {} | wc -c".format(YDBD_PATH)]
+            )
+            binary_size_stripped_bytes = subprocess.check_output(
+                ["bash", "-c", "./ya tool strip {} -o - | wc -c".format(YDBD_PATH)]
+            )
+
+            build_type = os.environ.get("build_type", None)
+            github_sha = os.environ.get("GITHUB_SHA", None)
+
+            if github_sha is not None:
+                git_commit_time_bytes = subprocess.check_output(
+                    ["git", "show", "--no-patch", "--format=%cI", github_sha]
+                )
+                git_commit_message_bytes = subprocess.check_output(
+                    ["git", "log", "--format=%B", "-n", "1", github_sha]
+                )
+                git_commit_time = datetime.datetime.fromisoformat(
+                    git_commit_time_bytes.decode("utf-8").strip()
+                )
+                git_commit_message = git_commit_message_bytes.decode("utf-8")
+                git_commit_time_unix = int(git_commit_time.timestamp())
+            else:
+                git_commit_time = None
+                git_commit_message = None
+                git_commit_time_unix = 0
+
             parameters = {
-                "$id": str(uuid.uuid4()).encode("utf-8"),
-                "$build_type": build_type,
-                "$binary_path": YDBD_PATH.encode("utf-8"),
+                "$id": sanitize_str(str(uuid.uuid4())),
+                "$build_type": sanitize_str(build_type),
+                "$binary_path": sanitize_str(YDBD_PATH),
                 "$size_stripped_bytes": int(binary_size_bytes.decode("utf-8")),
                 "$size_bytes": int(binary_size_stripped_bytes.decode("utf-8")),
+                "$git_commit_time": git_commit_time_unix,
+                "$git_commit_message": sanitize_str(git_commit_message),
             }
 
             for column in from_env_columns:
-                value = os.environ.get(column.upper(), "N/A")
-                value_bytes = value.encode("utf-8")
-                parameters["$" + column] = value_bytes
-            
+                value = os.environ.get(column.upper(), None)
+                parameters["$" + column] = sanitize_str(value)
+
             print("Executing query:\n{}".format(text_query))
             print("With parameters:")
             for k, v in parameters.items():
                 print("{}: {}".format(k, v))
 
             result_sets = tx.execute(prepared_query, parameters, commit_tx=True)
-            
 
-            for result_set in result_sets:
-                print(result_set.rows)
-                # assert len(result_set.rows) == 1
-                # assert result_set.rows[0]["column0"] == result
-            
-            
-        """
-        with ydb.SessionPool(driver) as pool:
-            def callee(session):
-                res = session.transaction().execute(
-                    "SELECT $seasonsData",
-                    {
-                        "$seriesData": 1,
-                        "$seasonsData": "2",
-                        "$episodesData": 3,
-                    },
-                    commit_tx=True,
-                )
-                print(res)
 
-            return pool.retry_operation_sync(callee)
-            """
-        print("Hi")
-        
 if __name__ == "__main__":
     main()

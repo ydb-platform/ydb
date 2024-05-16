1e43b9b06e1fdaf15830b6dc1917c148fcd246e1
ivanmorozov333 <ivanmorozov@ydb.tech>
2024-05-16 12:28:32 +0300

    async propose phase on CS (config stage on SS) (#4560)

--- ydb/core/tx/columnshard/transactions/operators/schema.h
+++ ydb/core/tx/columnshard/transactions/operators/schema.h
@@ -1,13 +1,32 @@
 #pragma once
 
+#include "ss_operation.h"
 #include <ydb/core/tx/columnshard/columnshard_impl.h>
 
 namespace NKikimr::NColumnShard {
 
-    class TSchemaTransactionOperator : public TTxController::ITransactionOperator {
-        using TBase = TTxController::ITransactionOperator;
+    class TSchemaTransactionOperator : public ISSTransactionOperator {
+    private:
+        using TBase = ISSTransactionOperator;
+
         using TProposeResult = TTxController::TProposeResult;
         static inline auto Registrator = TFactory::TRegistrator<TSchemaTransactionOperator>(NKikimrTxColumnShard::TX_KIND_SCHEMA);
+
+        virtual TTxController::TProposeResult DoStartProposeOnExecute(TColumnShard& owner, NTabletFlatExecutor::TTransactionContext& txc) override;
+        virtual void DoStartProposeOnComplete(TColumnShard& /*owner*/, const TActorContext& /*ctx*/) override {
+            AFL_VERIFY(false)("error", "method not implemented for non-async operator by default");
+        }
+        virtual void DoFinishProposeOnExecute(TColumnShard& /*owner*/, NTabletFlatExecutor::TTransactionContext& /*txc*/) override {
+            AFL_VERIFY(false)("error", "method not implemented for non-async operator by default");
+        }
+        virtual void DoFinishProposeOnComplete(TColumnShard& /*owner*/, const TActorContext& /*ctx*/) override {
+        }
+        virtual bool DoIsAsync() const override {
+            return false;
+        }
+        virtual bool DoParse(TColumnShard& /*owner*/, const TString& data) override {
+            return SchemaTxBody.ParseFromString(data);
+        }
     public:
         using TBase::TBase;
 
@@ -15,63 +34,8 @@ namespace NKikimr::NColumnShard {
             return true;
         }
 
-        virtual bool Parse(TColumnShard& /*owner*/, const TString& data) override {
-            if (!SchemaTxBody.ParseFromString(data)) {
-                return false;
-            }
-            return true;
-        }
-
         bool TxWithDeadline() const override {
             return false;
-        }
-
-        TProposeResult ExecuteOnPropose(TColumnShard& owner, NTabletFlatExecutor::TTransactionContext& txc) const override {
-            switch (SchemaTxBody.TxBody_case()) {
-                case NKikimrTxColumnShard::TSchemaTxBody::kInitShard:
-                    {
-                        auto validationStatus = ValidateTables(SchemaTxBody.GetInitShard().GetTables());
-                        if (validationStatus.IsFail()) {
-                            return  TProposeResult(NKikimrTxColumnShard::EResultStatus::SCHEMA_ERROR, "Invalid schema: " + validationStatus.GetErrorMessage());
-                        }
-                    }
-                    break;
-                case NKikimrTxColumnShard::TSchemaTxBody::kEnsureTables:
-                    {
-                        auto validationStatus = ValidateTables(SchemaTxBody.GetEnsureTables().GetTables());
-                        if (validationStatus.IsFail()) {
-                            return  TProposeResult(NKikimrTxColumnShard::EResultStatus::SCHEMA_ERROR, "Invalid schema: " + validationStatus.GetErrorMessage());
-                        }
-                    }
-                    break;
-                case NKikimrTxColumnShard::TSchemaTxBody::kAlterTable:
-                case NKikimrTxColumnShard::TSchemaTxBody::kAlterStore:
-                case NKikimrTxColumnShard::TSchemaTxBody::kDropTable:
-                case NKikimrTxColumnShard::TSchemaTxBody::TXBODY_NOT_SET:
-                    break;
-            }
-
-            auto seqNo = SeqNoFromProto(SchemaTxBody.GetSeqNo());
-            auto lastSeqNo = owner.LastSchemaSeqNo;
-
-            // Check if proposal is outdated
-            if (seqNo < lastSeqNo) {
-                auto errorMessage = TStringBuilder()
-                    << "Ignoring outdated schema tx proposal at tablet "
-                    << owner.TabletID()
-                    << " txId " << GetTxId()
-                    << " ssId " << owner.CurrentSchemeShardId
-                    << " seqNo " << seqNo
-                    << " lastSeqNo " << lastSeqNo;
-                return TProposeResult(NKikimrTxColumnShard::EResultStatus::SCHEMA_CHANGED, errorMessage);
-            }
-
-            owner.UpdateSchemaSeqNo(seqNo, txc);
-            return TProposeResult();
-        }
-
-        virtual bool CompleteOnPropose(TColumnShard& /*owner*/, const TActorContext& /*ctx*/) const override {
-            return true;
         }
 
         virtual bool ExecuteOnProgress(TColumnShard& owner, const NOlap::TSnapshot& version, NTabletFlatExecutor::TTransactionContext& txc) override {
@@ -105,84 +69,9 @@ namespace NKikimr::NColumnShard {
         }
 
     private:
-        TConclusionStatus ValidateTables(::google::protobuf::RepeatedPtrField<::NKikimrTxColumnShard::TCreateTable> tables) const {
-            for (auto& table : tables) {
-                if (table.HasSchemaPreset()) {
-                    const auto validationStatus = ValidateTablePreset(table.GetSchemaPreset());
-                    if (validationStatus.IsFail()) {
-                        return validationStatus;
-                    }
-                }
-                if (table.HasSchema()) {
-                    const auto validationStatus = ValidateTableSchema(table.GetSchema());
-                    if (validationStatus.IsFail()) {
-                        return validationStatus;
-                    }
-                }
-            } return TConclusionStatus::Success();
-        }
+        TConclusionStatus ValidateTables(::google::protobuf::RepeatedPtrField<::NKikimrTxColumnShard::TCreateTable> tables) const;
 
-        TConclusionStatus ValidateTableSchema(const NKikimrSchemeOp::TColumnTableSchema& schema) const {
-            namespace NTypeIds = NScheme::NTypeIds;
-            static const THashSet<NScheme::TTypeId> pkSupportedTypes = {
-                NTypeIds::Timestamp,
-                NTypeIds::Date32,
-                NTypeIds::Datetime64,
-                NTypeIds::Timestamp64,
-                NTypeIds::Interval64,
-                NTypeIds::Int8,
-                NTypeIds::Int16,
-                NTypeIds::Int32,
-                NTypeIds::Int64,
-                NTypeIds::Uint8,
-                NTypeIds::Uint16,
-                NTypeIds::Uint32,
-                NTypeIds::Uint64,
-                NTypeIds::Date,
-                NTypeIds::Datetime,
-                //NTypeIds::Interval,
-                //NTypeIds::Float,
-                //NTypeIds::Double,
-                NTypeIds::String,
-                NTypeIds::Utf8
-            };
-            if (!schema.HasEngine() ||
-                schema.GetEngine() != NKikimrSchemeOp::EColumnTableEngine::COLUMN_ENGINE_REPLACING_TIMESERIES) {
-                return TConclusionStatus::Fail("Invalid scheme engine: " + (schema.HasEngine() ? NKikimrSchemeOp::EColumnTableEngine_Name(schema.GetEngine()) : TString("No")));
-            }
-
-            if (!schema.KeyColumnNamesSize()) {
-                return TConclusionStatus::Fail("There is no key columns");
-            }
-
-            THashSet<TString> keyColumns(schema.GetKeyColumnNames().begin(), schema.GetKeyColumnNames().end());
-            TVector<TString> columnErrors;
-            for (const NKikimrSchemeOp::TOlapColumnDescription& column : schema.GetColumns()) {
-                TString name = column.GetName();
-                void* typeDescr = nullptr;
-                if (column.GetTypeId() == NTypeIds::Pg && column.HasTypeInfo()) {
-                    typeDescr = NPg::TypeDescFromPgTypeId(column.GetTypeInfo().GetPgTypeId());
-                }
-
-                NScheme::TTypeInfo schemeType(column.GetTypeId(), typeDescr);
-                if (keyColumns.contains(name) && !pkSupportedTypes.contains(column.GetTypeId())) {
-                    columnErrors.emplace_back("key column " + name + " has unsupported type "  + column.GetTypeName());
-                }
-                auto arrowType = NArrow::GetArrowType(schemeType);
-                if (!arrowType.ok()) {
-                    columnErrors.emplace_back("column " + name + ": " + arrowType.status().ToString());
-                }
-                keyColumns.erase(name);
-            }
-            if (!columnErrors.empty()) {
-                return TConclusionStatus::Fail("Column errors: " + JoinSeq("; ", columnErrors));
-            }
-
-            if (!keyColumns.empty()) {
-                return TConclusionStatus::Fail("Key columns not in scheme: " + JoinSeq(", ", keyColumns));
-            }
-            return TConclusionStatus::Success();
-        }
+        TConclusionStatus ValidateTableSchema(const NKikimrSchemeOp::TColumnTableSchema& schema) const;
 
         TConclusionStatus ValidateTablePreset(const NKikimrSchemeOp::TColumnTableSchemaPreset& preset) const {
             if (preset.HasName() && preset.GetName() != "default") {
--- ydb/core/tx/columnshard/transactions/operators/sharing.h
+++ ydb/core/tx/columnshard/transactions/operators/sharing.h
@@ -6,17 +6,18 @@
 
 namespace NKikimr::NColumnShard {
 
-class TBackupTransactionOperator: public ISSTransactionOperator {
+class TSharingTransactionOperator: public ISSTransactionOperator {
 private:
     using TBase = ISSTransactionOperator;
 
-    std::shared_ptr<NOlap::NExport::TExportTask> ExportTask;
-    std::unique_ptr<NTabletFlatExecutor::ITransaction> TxAddTask;
-    std::unique_ptr<NTabletFlatExecutor::ITransaction> TxConfirm;
-    std::unique_ptr<NTabletFlatExecutor::ITransaction> TxAbort;
+    std::shared_ptr<NOlap::NDataSharing::TSessionsManager> SharingSessionsManager;
+    std::shared_ptr<NOlap::NDataSharing::TDestinationSession> SharingTask;
     using TProposeResult = TTxController::TProposeResult;
-    static inline auto Registrator = TFactory::TRegistrator<TBackupTransactionOperator>(NKikimrTxColumnShard::TX_KIND_BACKUP);
-
+    mutable std::unique_ptr<NTabletFlatExecutor::ITransaction> TxPropose;
+    mutable std::unique_ptr<NTabletFlatExecutor::ITransaction> TxConfirm;
+    mutable std::unique_ptr<NTabletFlatExecutor::ITransaction> TxAbort;
+    static inline auto Registrator = TFactory::TRegistrator<TSharingTransactionOperator>(NKikimrTxColumnShard::TX_KIND_SHARING);
+    THashSet<TActorId> NotifySubscribers;
     virtual TTxController::TProposeResult DoStartProposeOnExecute(TColumnShard& owner, NTabletFlatExecutor::TTransactionContext& txc) override;
     virtual void DoStartProposeOnComplete(TColumnShard& /*owner*/, const TActorContext& /*ctx*/) override;
     virtual void DoFinishProposeOnExecute(TColumnShard& /*owner*/, NTabletFlatExecutor::TTransactionContext& /*txc*/) override {
@@ -29,19 +30,21 @@ private:
     virtual bool DoParse(TColumnShard& owner, const TString& data) override;
 public:
     using TBase::TBase;
+    virtual void RegisterSubscriber(const TActorId& actorId) override {
+        NotifySubscribers.insert(actorId);
+    }
 
     virtual bool AllowTxDups() const override {
         return true;
     }
+
 
     virtual bool ExecuteOnProgress(TColumnShard& owner, const NOlap::TSnapshot& version, NTabletFlatExecutor::TTransactionContext& txc) override;
 
     virtual bool CompleteOnProgress(TColumnShard& owner, const TActorContext& ctx) override;
 
     virtual bool ExecuteOnAbort(TColumnShard& owner, NTabletFlatExecutor::TTransactionContext& txc) override;
-    virtual bool CompleteOnAbort(TColumnShard& /*owner*/, const TActorContext& /*ctx*/) override {
-        return true;
-    }
+    virtual bool CompleteOnAbort(TColumnShard& owner, const TActorContext& ctx) override;
 };
-}
 
+}

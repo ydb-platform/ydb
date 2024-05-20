2d474006f7f8668f3b9889dc64c5b9a1ea8da2a3
Filitov Mikhail <filitovme@gmail.com>
2024-05-20 19:52:12 +0300

    YQL-18391 added spilling for grace join

--- ydb/library/yql/minikql/comp_nodes/mkql_grace_join_imp.h
+++ ydb/library/yql/minikql/comp_nodes/mkql_grace_join_imp.h
@@ -9,6 +9,8 @@
 namespace NKikimr {
 namespace NMiniKQL {
 namespace GraceJoin {
+
+class TTableBucketSpiller;
         
 const ui64 BitsForNumberOfBuckets = 5; // 2^5 = 32
 const ui64 BucketsMask = (0x00000001 << BitsForNumberOfBuckets)  - 1;
@@ -93,126 +95,6 @@ struct TColTypeInterface {
     const THolderFactory& HolderFactory; // To use during unpacking 
 };
 
-
-// Class which represents single table data stored in buckets
-class TTable {
-    ui64 NumberOfKeyIntColumns = 0; // Key int columns always first and padded to sizeof(ui64).
-    ui64 NumberOfKeyStringColumns = 0; // String key columns go after key int columns
-    ui64 NumberOfKeyIColumns = 0; // Number of interface - provided key columns
-
-
-    ui64 NumberOfDataIntColumns = 0; //Number of integer data columns in the Table
-    ui64 NumberOfDataStringColumns = 0; // Number of strings data columns in the Table
-    ui64 NumberOfDataIColumns = 0; //  Number of interface - provided data columns
-    
-    TColTypeInterface * ColInterfaces = nullptr; // Array of interfaces to work with corresponding columns data
-
-
-    ui64 NumberOfColumns = 0; // Number of columns in the Table
-    ui64 NumberOfKeyColumns = 0; // Number of key columns in the Table
-    ui64 NumberOfDataColumns = 0; // Number of data columns in the Table
-    ui64 NumberOfStringColumns = 0; // Total number of String Columns
-    ui64 NumberOfIColumns = 0; // Total number of interface-based columns
-    ui64 NullsBitmapSize_ = 1; // Default size of ui64 values used for null columns bitmap.
-                                // Every bit set means null value. Order of columns is equal to order in AddTuple call.
-                                // First key int column is  bit 1 in bit mask, second - bit 2, etc.  Bit 0 is least significant in bitmask and tells if key columns contain nulls.
-    ui64 TotalStringsSize = 0; // Bytes in tuple header reserved to store total strings size key tuple columns
-    ui64 HeaderSize = HashSize + NullsBitmapSize_ + NumberOfKeyIntColumns + NumberOfKeyIColumns + TotalStringsSize; // Header of all tuples size
-
-    ui64 BytesInKeyIntColumns = sizeof(ui64) * NumberOfKeyIntColumns;
-    
-    // Table data is partitioned in buckets based on key value
-    std::vector<TTableBucket> TableBuckets;
-    // Statistics for buckets. Total number of tuples inside a single bucket and offsets.
-    std::vector<TTableBucketStats> TableBucketsStats;
-
-    // Temporary vector for tuples manipulation;
-    std::vector<ui64> TempTuple;
-
-    // Hashes for interface - based columns values
-    std::vector<ui64> IColumnsHashes;
-
-    // Serialized values for interface-based columns
-    std::vector<std::vector<char>> IColumnsVals;
-
-    // Current iterator index for NextTuple iterator
-    ui64 CurrIterIndex = 0;
-
-    // Index for NextJoinedData iterator
-    ui64 CurrJoinIdsIterIndex = 0;
-
-    // Current bucket for iterators
-    ui64 CurrIterBucket = 0;
-
-    // True if table joined from two other tables
-    bool IsTableJoined = false;
-
-    // Type of the join
-    EJoinKind JoinKind = EJoinKind::Inner;
-
-    // Pointers to the joined tables. Lifetime of source tables to join should be greater than joined table
-    TTable * JoinTable1 = nullptr;
-    TTable * JoinTable2 = nullptr;
-
-    // Returns tuple data in td from bucket with id bucketNum.  Tuple id inside bucket is tupleId.
-    inline void GetTupleData(ui32 bucketNum, ui32 tupleId, TupleData& td);
-
-    // True if current iterator of tuple in joinedTable has corresponding joined tuple in second table. Id of joined tuple in second table returns in tupleId2.
-    inline bool HasJoinedTupleId(TTable* joinedTable, ui32& tupleId2);
-
-    // Adds keys to KeysHashTable, return true if added, false if equal key already added
-    inline bool AddKeysToHashTable(KeysHashTable& t, ui64* keys);
-
-    ui64 TotalPacked = 0; // Total number of packed tuples
-    ui64 TotalUnpacked = 0; // Total number of unpacked tuples
-
-    bool LeftTableBatch_ = false; // True if left table is processed in batch mode
-    bool RightTableBatch_ = false; // True if right table is procesed in batch mode
-
-    bool HasMoreLeftTuples_  = false; // True if join is not completed, rows from left table are coming
-    bool HasMoreRightTuples_ = false; // True if join is not completed, rows from right table are coming
-
-    bool IsAny_ = false;  // True if key duplicates need to be removed from table (any join)
-
-    bool Table2Initialized_ = false;    // True when iterator counters for second table already initialized
-
-    ui64 TuplesFound_ = 0; // Total number of matching keys found during join
-
-public:
-
-    // Adds new tuple to the table.  intColumns, stringColumns - data of columns, 
-    // stringsSizes - sizes of strings columns.  Indexes of null-value columns
-    // in the form of bit array should be first values of intColumns.
-    void AddTuple(ui64* intColumns, char** stringColumns, ui32* stringsSizes, NYql::NUdf::TUnboxedValue * iColumns = nullptr);
-
-    // Resets iterators. In case of join results table it also resets iterators for joined tables
-    void ResetIterator();
-
-    // Returns value of next tuple. Returs true if there are more tuples
-    bool NextTuple(TupleData& td);
-
-    // Joins two tables and stores join result in table data. Tuples of joined table could be received by
-    // joined table iterator.  Life time of t1, t2 should be greater than lifetime of joined table
-    // hasMoreLeftTuples, hasMoreRightTuples is true if join is partial and more rows are coming.  For final batch hasMoreLeftTuples = false, hasMoreRightTuples = false
-    void Join(TTable& t1, TTable& t2, EJoinKind joinKind = EJoinKind::Inner, bool hasMoreLeftTuples = false, bool hasMoreRightTuples = false );
-
-
-    // Returns next jointed tuple data. Returs true if there are more tuples
-    bool NextJoinedData(TupleData& td1, TupleData& td2);
-
-    // Clears table content
-    void Clear();
-
-    // Creates new table with key columns and data columns
-    TTable(ui64 numberOfKeyIntColumns = 0, ui64 numberOfKeyStringColumns = 0,
-            ui64 numberOfDataIntColumns = 0, ui64 numberOfDataStringColumns = 0,
-            ui64 numberOfKeyIColumns = 0, ui64 numberOfDataIColumns = 0, 
-            ui64 nullsBitmapSize = 1, TColTypeInterface * colInterfaces = nullptr, bool isAny = false);
-    
-    ~TTable();
-
-};
-
 // Class that spills bucket data.
 // If, after saving, data has accumulated in the bucket again, you can spill it again.
 // After restoring the entire bucket, it will contain all the data saved over different iterations.
@@ -271,6 +153,172 @@ private:
 
     TTableBucket CurrentBucket;
 };
+
+
+// Class which represents single table data stored in buckets
+class TTable {
+    ui64 NumberOfKeyIntColumns = 0; // Key int columns always first and padded to sizeof(ui64).
+    ui64 NumberOfKeyStringColumns = 0; // String key columns go after key int columns
+    ui64 NumberOfKeyIColumns = 0; // Number of interface - provided key columns
+
+
+    ui64 NumberOfDataIntColumns = 0; //Number of integer data columns in the Table
+    ui64 NumberOfDataStringColumns = 0; // Number of strings data columns in the Table
+    ui64 NumberOfDataIColumns = 0; //  Number of interface - provided data columns
+    
+    TColTypeInterface * ColInterfaces = nullptr; // Array of interfaces to work with corresponding columns data
+
+
+    ui64 NumberOfColumns = 0; // Number of columns in the Table
+    ui64 NumberOfKeyColumns = 0; // Number of key columns in the Table
+    ui64 NumberOfDataColumns = 0; // Number of data columns in the Table
+    ui64 NumberOfStringColumns = 0; // Total number of String Columns
+    ui64 NumberOfIColumns = 0; // Total number of interface-based columns
+    ui64 NullsBitmapSize_ = 1; // Default size of ui64 values used for null columns bitmap.
+                                // Every bit set means null value. Order of columns is equal to order in AddTuple call.
+                                // First key int column is  bit 1 in bit mask, second - bit 2, etc.  Bit 0 is least significant in bitmask and tells if key columns contain nulls.
+    ui64 TotalStringsSize = 0; // Bytes in tuple header reserved to store total strings size key tuple columns
+    ui64 HeaderSize = HashSize + NullsBitmapSize_ + NumberOfKeyIntColumns + NumberOfKeyIColumns + TotalStringsSize; // Header of all tuples size
+
+    ui64 BytesInKeyIntColumns = sizeof(ui64) * NumberOfKeyIntColumns;
+    
+    // Table data is partitioned in buckets based on key value
+    std::vector<TTableBucket> TableBuckets;
+    // Statistics for buckets. Total number of tuples inside a single bucket and offsets.
+    std::vector<TTableBucketStats> TableBucketsStats;
+
+    std::vector<TTableBucketSpiller> TableBucketsSpillers;
+
+    // Temporary vector for tuples manipulation;
+    std::vector<ui64> TempTuple;
+
+    // Hashes for interface - based columns values
+    std::vector<ui64> IColumnsHashes;
+
+    // Serialized values for interface-based columns
+    std::vector<std::vector<char>> IColumnsVals;
+
+    // Current iterator index for NextTuple iterator
+    ui64 CurrIterIndex = 0;
+
+    // Index for NextJoinedData iterator
+    ui64 CurrJoinIdsIterIndex = 0;
+
+    // Current bucket for iterators
+    ui64 CurrIterBucket = 0;
+
+    // True if table joined from two other tables
+    bool IsTableJoined = false;
+
+    // Type of the join
+    EJoinKind JoinKind = EJoinKind::Inner;
+
+    // Pointers to the joined tables. Lifetime of source tables to join should be greater than joined table
+    TTable * JoinTable1 = nullptr;
+    TTable * JoinTable2 = nullptr;
+
+    // Returns tuple data in td from bucket with id bucketNum.  Tuple id inside bucket is tupleId.
+    inline void GetTupleData(ui32 bucketNum, ui32 tupleId, TupleData& td);
+
+    // True if current iterator of tuple in joinedTable has corresponding joined tuple in second table. Id of joined tuple in second table returns in tupleId2.
+    inline bool HasJoinedTupleId(TTable* joinedTable, ui32& tupleId2);
+
+    // Adds keys to KeysHashTable, return true if added, false if equal key already added
+    inline bool AddKeysToHashTable(KeysHashTable& t, ui64* keys);
+
+    ui64 TotalPacked = 0; // Total number of packed tuples
+    ui64 TotalUnpacked = 0; // Total number of unpacked tuples
+
+    bool LeftTableBatch_ = false; // True if left table is processed in batch mode
+    bool RightTableBatch_ = false; // True if right table is procesed in batch mode
+
+    bool HasMoreLeftTuples_  = false; // True if join is not completed, rows from left table are coming
+    bool HasMoreRightTuples_ = false; // True if join is not completed, rows from right table are coming
+
+    bool IsAny_ = false;  // True if key duplicates need to be removed from table (any join)
+
+    bool Table2Initialized_ = false;    // True when iterator counters for second table already initialized
+
+    ui64 TuplesFound_ = 0; // Total number of matching keys found during join
+
+    ui32 NextBucketToSpill = NumberOfBuckets - 1;
+
+public:
+
+    // Adds new tuple to the table.  intColumns, stringColumns - data of columns, 
+    // stringsSizes - sizes of strings columns.  Indexes of null-value columns
+    // in the form of bit array should be first values of intColumns.
+    void AddTuple(ui64* intColumns, char** stringColumns, ui32* stringsSizes, NYql::NUdf::TUnboxedValue * iColumns = nullptr);
+
+    // Resets iterators. In case of join results table it also resets iterators for joined tables
+    void ResetIterator();
+
+    // Returns value of next tuple. Returs true if there are more tuples
+    bool NextTuple(TupleData& td);
+
+    // Joins two tables and stores join result in table data. Tuples of joined table could be received by
+    // joined table iterator.  Life time of t1, t2 should be greater than lifetime of joined table
+    // hasMoreLeftTuples, hasMoreRightTuples is true if join is partial and more rows are coming.  For final batch hasMoreLeftTuples = false, hasMoreRightTuples = false
+    void Join(TTable& t1, TTable& t2, EJoinKind joinKind = EJoinKind::Inner, bool hasMoreLeftTuples = false, bool hasMoreRightTuples = false );
+
+
+    // Returns next jointed tuple data. Returs true if there are more tuples
+    bool NextJoinedData(TupleData& td1, TupleData& td2);
+
+    void InitializeBucketSpillers(ISpiller::TPtr spiller) {
+        for (size_t i = 0; i < NumberOfBuckets; ++i) {
+            TableBucketsSpillers.emplace_back(spiller, 1_MB);
+        }
+    }
+
+    ui64 GetSizeOfBucket(ui64 bucket) const {
+        return TableBuckets[bucket].KeyIntVals.size() * sizeof(ui64)
+        + TableBuckets[bucket].DataIntVals.size() * sizeof(ui64)
+        + TableBuckets[bucket].StringsValues.size()
+        + TableBuckets[bucket].StringsOffsets.size() * sizeof(ui32)
+        + TableBuckets[bucket].InterfaceValues.size()
+        + TableBuckets[bucket].InterfaceOffsets.size() * sizeof(ui32);
+    }
+
+    bool TryToReduceMemory() {
+        i32 largestBucketIndex = 0;
+        ui64 largestBucketSize = 0;
+        for (ui32 bucket = 0; bucket < NumberOfBuckets; ++bucket) {
+            if (TableBucketsSpillers[bucket].HasRunningAsyncIoOperation()) return true;
+
+            ui64 bucketSize = GetSizeOfBucket(bucket);
+            if (bucketSize > largestBucketSize) {
+                largestBucketSize = bucketSize;
+                largestBucketIndex = bucket;
+            }
+        }
+
+        TableBucketsSpillers[largestBucketIndex].SpillBucket(std::move(TableBuckets[largestBucketIndex]));
+        TableBuckets[largestBucketIndex] = TTableBucket{};
+
+        return TableBucketsSpillers[largestBucketIndex].HasRunningAsyncIoOperation();
+    }
+
+    void UpdateSpilling() {
+        for (ui64 i = 0; i < NumberOfBuckets; ++i) {
+            TableBucketsSpillers[i].Update();
+        }
+    }
+
+    // Clears table content
+    void Clear();
+
+    // Creates new table with key columns and data columns
+    TTable(ui64 numberOfKeyIntColumns = 0, ui64 numberOfKeyStringColumns = 0,
+            ui64 numberOfDataIntColumns = 0, ui64 numberOfDataStringColumns = 0,
+            ui64 numberOfKeyIColumns = 0, ui64 numberOfDataIColumns = 0, 
+            ui64 nullsBitmapSize = 1, TColTypeInterface * colInterfaces = nullptr, bool isAny = false);
+    
+    ~TTable();
+
+};
+
+
 
 
 

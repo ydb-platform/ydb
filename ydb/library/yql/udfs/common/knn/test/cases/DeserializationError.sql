6f9c3abca2f248b5f081e9d42ca5cfa0eff1c866
Valerii Mironov <mbkkt@ydb.tech>
2024-05-20 15:37:53 +0000

    Format

--- ydb/library/yql/udfs/common/knn/test/cases/DeserializationError.sql
+++ ydb/library/yql/udfs/common/knn/test/cases/DeserializationError.sql
@@ -5,4 +5,3 @@ select Knn::FromBinaryString("WrongString");
 $vector_wrong_format = Knn::ToBinaryString([1.0f, 2.0f, 3.0f], "wrong format");
 select $vector_wrong_format;
 select Knn::CosineDistance($vector_wrong_format, $vector_wrong_format);
-

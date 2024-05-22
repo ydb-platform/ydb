69e0cb4ee9bf95ebb4bf82c59bb56de210fc3738
Maxim Yurchuk <maxim.yurchuk@gmail.com>
2024-05-22 21:42:39 +0300

    Update main.cpp

--- ydb/apps/ydb/main.cpp
+++ ydb/apps/ydb/main.cpp
@@ -1,6 +1,8 @@
 #include <ydb/apps/ydb/commands/ydb_cloud_root.h>
 #include <ydb/public/lib/ydb_cli/commands/ydb_service_topic.h>
 
+// !
+
 TVector<NYdb::NTopic::ECodec> NYdb::NConsoleClient::InitAllowedCodecs() {
     return TVector<NYdb::NTopic::ECodec>{
             NYdb::NTopic::ECodec::RAW,

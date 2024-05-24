bff1cd94b2e47556c498f5fd4b24d93ff8664a94
azevaykin <azevaykin@ydb.tech>
2024-05-24 15:29:53 +0000

    DbgPrintValue for Pg types

--- ydb/core/scheme/scheme_tablecell.cpp
+++ ydb/core/scheme/scheme_tablecell.cpp
@@ -487,9 +487,14 @@ void DbgPrintValue(TString &res, const TCell &r, NScheme::TTypeInfo typeInfo) {
         case NScheme::NTypeIds::ActorId:
             res += ToString(r.AsValue<NActors::TActorId>());
             break;
-        case NScheme::NTypeIds::Pg:
-            // TODO: support pg types
+        case NScheme::NTypeIds::Pg: {
+            auto convert = NPg::PgNativeTextFromNativeBinary(r.AsBuf(), typeInfo.GetTypeDesc());
+            if (!convert.Error)
+                res += convert.Str;
+            else
+                res += *convert.Error;
             break;
+            }
         default:
             res += EscapeC(r.Data(), r.Size());
         }

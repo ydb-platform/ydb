7a6a0462ffb312a6c4a8c7655c8c83f4256e928c
Valerii Mironov <mbkkt@ydb.tech>
2024-05-24 17:58:52 +0000

    WIP

--- ydb/library/yql/udfs/common/knn/test/cases/Uint8Serialization.sql
+++ ydb/library/yql/udfs/common/knn/test/cases/Uint8Serialization.sql
@@ -11,4 +11,8 @@ select $deserialized_vector;
 --fixed size vector
 $vector1 = Knn::ToBinaryStringByte([1.0f, 2.0f, 3.0f]);
 $vector2 = Knn::ToBinaryStringByte([4.0f, 5.0f, 6.0f]);
+
 select Knn::CosineDistance($vector1, $vector2);
+select Knn::CosineDistance($vector1, "\u0004\u0005\u0006\u0002");
+select Knn::CosineDistance("\u0001\u0002\u0003\u0002", $vector2);
+select Knn::CosineDistance("\u0001\u0002\u0003\u0002", "\u0004\u0005\u0006\u0002");

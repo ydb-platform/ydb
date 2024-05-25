ca2da90fe9a03f21cc7d2e3a15f10c3d51a8d831
TjlikesHowK <82500619+TjlikesHowK@users.noreply.github.com>
2024-05-25 23:58:35 +0300

    Update events.h

--- tools/actor_model/events.h
+++ tools/actor_model/events.h
@@ -2,5 +2,22 @@
 #include <library/cpp/actors/core/events.h>
 
 struct TEvents {
-    // Вам нужно самостоятельно сюда добавить все необходимые events в NActors::TEvents::ES_PRIVATE
+    enum EEvents {
+        EvWriteValueRequest = EventSpaceBegin(NActors::TEvents::ES_PRIVATE),
+        EvDone,
+        EvMaximumPrimeDevisor,
+        EvEnd
+    };
+
+    struct TEvWriteValueRequest : NActors::TEventLocal<TEvWriteValueRequest, EvWriteValueRequest> {
+        int64_t Value;
+        TEvWriteValueRequest(int64_t value) : Value(value) {}
+    };
+
+    struct TEvDone : NActors::TEventLocal<TEvDone, EvDone> {};
+
+    struct TEvMaximumPrimeDevisor : NActors::TEventLocal<TEvMaximumPrimeDevisor, EvMaximumPrimeDevisor> {
+        int64_t Value;
+        TEvMaximumPrimeDevisor(int64_t value) : Value(value) {}
+    };
 };

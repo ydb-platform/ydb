fd31b4c59ea4b502c8f8be129d9d1ddec127fc1a
Gleb Solomennikov <solomennikov.glebbs@gmail.com>
2024-05-25 21:46:14 +0000

    fix

--- ydb/core/external_sources/external_source_factory.cpp
+++ ydb/core/external_sources/external_source_factory.cpp
@@ -56,10 +56,6 @@ IExternalSourceFactory::TPtr CreateExternalSourceFactory(const std::vector<TStri
             CreateExternalDataSource(TString{NYql::YtProviderName}, {"NONE", "TOKEN"}, {}, hostnamePatternsRegEx)
         },
         {
-            ToString(NYql::EDatabaseType::YT),
-            CreateExternalDataSource(TString{NYql::GenericProviderName}, {"MDB_BASIC", "BASIC"}, {"database_name", "protocol", "mdb_cluster_id", "use_tls", "schema"}, hostnamePatternsRegEx)
-        },
-        {
             ToString(NYql::EDatabaseType::MicrosoftSqlServer),
             CreateExternalDataSource(TString{NYql::GenericProviderName}, {"MDB_BASIC", "BASIC"}, {"database_name", "protocol", "mdb_cluster_id", "use_tls", "schema"}, hostnamePatternsRegEx)
         },

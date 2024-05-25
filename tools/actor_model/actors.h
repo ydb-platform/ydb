f584a8aca46bfdcca205f7c264055f6b47de119a
TjlikesHowK <82500619+TjlikesHowK@users.noreply.github.com>
2024-05-25 23:58:15 +0300

    Update actors.h

--- tools/actor_model/actors.h
+++ tools/actor_model/actors.h
@@ -1,8 +1,13 @@
-#include <iostream>
+#pragma once
+
 #include <library/cpp/actors/core/actor.h>
+#include <library/cpp/actors/core/actor_bootstrapped.h>
 #include <util/generic/ptr.h>
 #include <library/cpp/actors/util/should_continue.h>
+#include <memory>
 
 THolder<NActors::IActor> CreateSelfPingActor(const TDuration& latency);
-
+THolder<NActors::IActor> CreateReadActor(std::shared_ptr<TProgramShouldContinue> shouldContinue, const NActors::TActorId& writeActor);
+THolder<NActors::IActor> CreateMaximumPrimeDevisorActor(int64_t value, const NActors::TActorId& readActor, const NActors::TActorId& writeActor);
+THolder<NActors::IActor> CreateWriteActor(std::shared_ptr<TProgramShouldContinue> shouldContinue);
 std::shared_ptr<TProgramShouldContinue> GetProgramShouldContinue();

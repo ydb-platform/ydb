8f7cf93c82e0bccc98cff58c01dd41d5b577acff
ivanmorozov333 <ivanmorozov@ydb.tech>
2024-05-27 14:00:46 +0300

    fraction cpu usage in conveyors (#4870)

--- ydb/core/tx/conveyor/service/service.cpp
+++ ydb/core/tx/conveyor/service/service.cpp
@@ -16,7 +16,12 @@ void TDistributor::Bootstrap() {
     const ui32 workersCount = Config.GetWorkersCountForConveyor(NKqp::TStagePredictor::GetUsableThreads());
     AFL_NOTICE(NKikimrServices::TX_CONVEYOR)("action", "conveyor_registered")("actor_id", SelfId())("workers_count", workersCount)("config", Config.DebugString());
     for (ui32 i = 0; i < workersCount; ++i) {
-        Workers.emplace_back(Register(new TWorker(ConveyorName)));
+        const double usage = Config.GetWorkerCPUUsage(i);
+        Workers.emplace_back(Register(new TWorker(ConveyorName, usage, SelfId())));
+        if (usage < 1) {
+            AFL_VERIFY(!SlowWorkerId);
+            SlowWorkerId = Workers.back();
+        }
     }
     Counters.AvailableWorkersCount->Set(Workers.size());
     Counters.WorkersCountLimit->Set(Workers.size());
@@ -62,8 +67,13 @@ void TDistributor::HandleMain(TEvExecution::TEvNewTask::TPtr& ev) {
         Counters.WaitingHistogram->Collect(0);
 
         wTask.OnBeforeStart();
-        Send(Workers.back(), new TEvInternal::TEvNewTask(wTask));
-        Workers.pop_back();
+        if (Workers.size() == 1 || !SlowWorkerId || Workers.back() != *SlowWorkerId) {
+            Send(Workers.back(), new TEvInternal::TEvNewTask(wTask));
+            Workers.pop_back();
+        } else {
+            Send(Workers.front(), new TEvInternal::TEvNewTask(wTask));
+            Workers.pop_front();
+        }
         Counters.UseWorkerRate->Inc();
     } else if (Waiting.size() < Config.GetQueueSizeLimit()) {
         Waiting.push(wTask);

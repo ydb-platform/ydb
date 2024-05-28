62f32deafcfae312db43efb1d81dd084068f146a
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2024-05-28 16:09:14 +0000

    fix

--- ydb/ci/build_bloat/ydb_upload.py
+++ ydb/ci/build_bloat/ydb_upload.py
@@ -11,41 +11,39 @@ import subprocess
 DATABASE_PATH = "/ru-central1/b1ggceeul2pkher8vhb6/etn6d1qbals0c29ho4lf"
 
 FROM_ENV_COLUMNS = [
-    "github_head_ref",
-    "github_workflow",
-    "github_workflow_ref",
-    "github_sha",
-    "github_repository",
-    "github_event_name",
-    "github_ref_type",
-    "github_ref_name",
-    "github_ref",
+    "GITHUB_HEAD_REF",
+    "GITHUB_WORKFLOW",
+    "GITHUB_WORKFLOW_REF",
+    "GITHUB_SHA",
+    "GITHUB_REPOSITORY",
+    "GITHUB_EVENT_NAME",
+    "GITHUB_REF_TYPE",
+    "GITHUB_REF_NAME",
+    "GITHUB_REF",
+    "build_preset",
+    "build_target",
 ]
 
-UTF8_COLUMNS = FROM_ENV_COLUMNS + [
+UTF8_COLUMNS = [val.lower() for val in FROM_ENV_COLUMNS] + [
     "id",
     "git_commit_message",
     "path",
-    "build_preset",
 ]
 
 DATETIME_COLUMNS = [
     "git_commit_time",
 ]
 
-UINT64_COLUMNS = [
-]
+UINT64_COLUMNS = []
 
-DOUBLE_COLUMNS = [
-    "total_compilation_time_s",
-    "compilation_time_s"
-]
+DOUBLE_COLUMNS = ["total_compilation_time_s", "compilation_time_s"]
 
 ALL_COLUMNS = UTF8_COLUMNS + DATETIME_COLUMNS + UINT64_COLUMNS
 
 
 def sanitize_str(s):
     return s or "N\\A"
+
 
 def generate_column_types(row):
     column_types = ydb.BulkUpsertColumns()
@@ -67,15 +65,15 @@ def main():
     if "CI_YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS" not in os.environ:
         print("Env variable CI_YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS is missing, skipping")
         return 1
-    
-    # Do not set up 'real' variable from gh workflows because it interfere with ydb tests 
+
+    # Do not set up 'real' variable from gh workflows because it interfere with ydb tests
     # So, set up it locally
     os.environ["YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS"] = os.environ["CI_YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS"]
 
     with ydb.Driver(
         endpoint="grpcs://ydb.serverless.yandexcloud.net:2135",
         database=DATABASE_PATH,
-        credentials=ydb.credentials_from_env_variables()
+        credentials=ydb.credentials_from_env_variables(),
     ) as driver:
         driver.wait(timeout=10, fail_fast=True)
 
@@ -93,22 +91,16 @@ def main():
         github_sha = os.environ.get("GITHUB_SHA", None)
 
         if github_sha is not None:
-            git_commit_time_bytes = subprocess.check_output(
-                ["git", "show", "--no-patch", "--format=%cI", github_sha]
-            )
-            git_commit_message_bytes = subprocess.check_output(
-                ["git", "log", "--format=%s", "-n", "1", github_sha]
-            )
-            git_commit_time = datetime.datetime.fromisoformat(
-                git_commit_time_bytes.decode("utf-8").strip()
-            )
+            git_commit_time_bytes = subprocess.check_output(["git", "show", "--no-patch", "--format=%cI", github_sha])
+            git_commit_message_bytes = subprocess.check_output(["git", "log", "--format=%s", "-n", "1", github_sha])
+            git_commit_time = datetime.datetime.fromisoformat(git_commit_time_bytes.decode("utf-8").strip())
             git_commit_message = git_commit_message_bytes.decode("utf-8").strip()
             git_commit_time_unix = int(git_commit_time.timestamp())
         else:
             git_commit_time = None
             git_commit_message = None
             git_commit_time_unix = 0
-    
+
         common_parameters = {
             "build_preset": sanitize_str(build_preset),
             "git_commit_time": git_commit_time_unix,
@@ -116,8 +108,8 @@ def main():
         }
 
         for column in FROM_ENV_COLUMNS:
-            value = os.environ.get(column.upper(), None)
-            common_parameters[column] = sanitize_str(value)
+            value = os.environ.get(column, None)
+            common_parameters[column.lower()] = sanitize_str(value)
 
         with open("html_cpp_impact/output.json") as f:
             cpp_stats = json.load(f)
@@ -132,16 +124,17 @@ def main():
             row["compilation_time_s"] = time_s
             row["id"] = str(uuid.uuid4())
             rows.append(copy.copy(row))
-        
+
         if rows:
             row = rows[0]
-            driver.table_client.bulk_upsert(DATABASE_PATH + "/cpp_compile_time", rows, generate_column_types(row))
-        
+            driver.table_client.bulk_upsert(DATABASE_PATH + "/code-agility/cpp_compile_time", rows, generate_column_types(row))
+
         row = copy.copy(common_parameters)
         row["id"] = str(uuid.uuid4())
         row["total_compilation_time_s"] = cpp_stats["total_compilation_time"]
 
-        driver.table_client.bulk_upsert(DATABASE_PATH + "/total_compile_time", [row], generate_column_types(row))
+        driver.table_client.bulk_upsert(DATABASE_PATH + "/code-agility/total_compile_time", [row], generate_column_types(row))
+
 
 if __name__ == "__main__":
     exit(main())

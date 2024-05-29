53f75b6324c6fd06d25c69c693fdd693af42afab
kruall <kruall@ydb.tech>
2024-05-29 08:07:07 +0300

    Merge 24-1: New counter for activations (CurrentActivationTimeUsByActivity) (#4938) (#4949)

--- ydb/library/actors/core/worker_context.h
+++ ydb/library/actors/core/worker_context.h
@@ -50,6 +50,11 @@ namespace NActors {
             statsCopy.Aggregate(*Stats);
         }
 
+        void SetCurrentActivationTime(ui32 activityType, i64 elapsed) {
+            RelaxedStore(&Stats->CurrentActivationTime.LastActivity, activityType);
+            RelaxedStore(&Stats->CurrentActivationTime.TimeUs, (elapsed > 0 ? elapsed : 0));
+        }
+
         void AddElapsedCycles(ui32 activityType, i64 elapsed) {
             if (Y_LIKELY(elapsed > 0)) {
                 Y_DEBUG_ABORT_UNLESS(activityType < Stats->MaxActivityType());
@@ -153,6 +158,7 @@ namespace NActors {
         }
 #else
         void GetCurrentStats(TExecutorThreadStats&) const {}
+        void SetCurrentActivationTime(ui32, i64) {}
         inline void AddElapsedCycles(ui32, i64) {}
         inline void AddParkedCycles(i64) {}
         inline void AddBlockedCycles(i64) {}

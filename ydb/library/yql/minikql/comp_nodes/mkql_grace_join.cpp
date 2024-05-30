60e1b890270b012f4708c64f41ae8eaf2dfb19cc
Filitov Mikhail <filitovme@gmail.com>
2024-05-30 15:43:24 +0200

    YQL-18490 remove HasRunningAsyncOperation from mkql_grace_join.cpp (#4952)

--- ydb/library/yql/minikql/comp_nodes/mkql_grace_join.cpp
+++ ydb/library/yql/minikql/comp_nodes/mkql_grace_join.cpp
@@ -636,7 +636,6 @@ private:
         // return !HasMemoryForProcessing();
     }
 
-
     void SwitchMode(EOperatingMode mode, TComputationContext& ctx) {
         switch(mode) {
             case EOperatingMode::InMemory: {
@@ -816,18 +815,23 @@ private:
         RightPacker->TablePtr->UpdateSpilling();
     }
 
-    bool HasRunningAsyncOperation() const {
-        return LeftPacker->TablePtr->HasRunningAsyncIoOperation() || RightPacker->TablePtr->HasRunningAsyncIoOperation();
+
+    bool IsSpillingFinished() const {
+        return LeftPacker->TablePtr->IsSpillingFinished() && RightPacker->TablePtr->IsSpillingFinished();
     }
 
-    bool IsProcessingFinished() {
-        return LeftPacker->TablePtr->IsProcessingFinished() || RightPacker->TablePtr->IsProcessingFinished();
+    bool IsReadyForSpilledDataProcessing() const {
+        return LeftPacker->TablePtr->IsSpillingAcceptingDataRequests() && RightPacker->TablePtr->IsSpillingAcceptingDataRequests();
+    }
+
+    bool IsRestoringSpilledBuckets() const {
+        return LeftPacker->TablePtr->IsRestoringSpilledBuckets() || RightPacker->TablePtr->IsRestoringSpilledBuckets();
     }
 
 void DoCalculateWithSpilling(TComputationContext& ctx) {
     UpdateSpilling();
 
-    if (!HasMemoryForProcessing()) {
+    if (!HasMemoryForProcessing() && !IsSpillingFinalized) {
         bool isWaitingForReduce = TryToReduceMemoryAndWait();
         if (isWaitingForReduce) return;
     }
@@ -838,15 +842,16 @@ void DoCalculateWithSpilling(TComputationContext& ctx) {
     }
 
     if (!*HaveMoreLeftRows && !*HaveMoreRightRows) {
-        UpdateSpilling();
-        if (HasRunningAsyncOperation() || !IsProcessingFinished()) return;
+        if (!IsSpillingFinished()) return;
         if (!IsSpillingFinalized) {
             LeftPacker->TablePtr->FinalizeSpilling();
             RightPacker->TablePtr->FinalizeSpilling();
             IsSpillingFinalized = true;
 
-            if (HasRunningAsyncOperation()) return;
+            UpdateSpilling();
         }
+        if (!IsReadyForSpilledDataProcessing()) return;
+
         SwitchMode(EOperatingMode::ProcessSpilled, ctx);
         return;
     }
@@ -855,8 +860,15 @@ void DoCalculateWithSpilling(TComputationContext& ctx) {
 EFetchResult ProcessSpilledData(TComputationContext&, NUdf::TUnboxedValue*const* output) {
     while (NextBucketToJoin != GraceJoin::NumberOfBuckets) {
         UpdateSpilling();
+        if (IsRestoringSpilledBuckets()) return EFetchResult::Yield;
 
-        if (HasRunningAsyncOperation()) return EFetchResult::Yield;
+        if (LeftPacker->TablePtr->IsSpilledBucketWaitingForExtraction(NextBucketToJoin)) {
+            LeftPacker->TablePtr->PrepareBucket(NextBucketToJoin);
+        }
+
+        if (RightPacker->TablePtr->IsSpilledBucketWaitingForExtraction(NextBucketToJoin)) {
+            RightPacker->TablePtr->PrepareBucket(NextBucketToJoin);
+        } 
 
         if (!LeftPacker->TablePtr->IsBucketInMemory(NextBucketToJoin)) {
             LeftPacker->TablePtr->StartLoadingBucket(NextBucketToJoin);
@@ -886,8 +898,6 @@ EFetchResult ProcessSpilledData(TComputationContext&, NUdf::TUnboxedValue*const*
 
                 NextBucketToJoin++;
             } else {
-                LeftPacker->TablePtr->PrepareBucket(NextBucketToJoin);
-                RightPacker->TablePtr->PrepareBucket(NextBucketToJoin);
                 *PartialJoinCompleted = true;
                 LeftPacker->StartTime = std::chrono::system_clock::now();
                 RightPacker->StartTime = std::chrono::system_clock::now();

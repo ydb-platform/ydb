72236efc2bbaa99378c1a73120b6380a3c5defd7
kungasc <kungasc@ydb.tech>
2024-05-30 13:14:51 +0000

    wip nodes

--- ydb/core/tablet_flat/flat_stat_table_btree_index_histogram.h
+++ ydb/core/tablet_flat/flat_stat_table_btree_index_histogram.h
@@ -27,6 +27,7 @@ public:
         TCellsIterable EndKey;
         ui64 BeginDataSize;
         ui64 EndDataSize;
+        std::optional<TBtreeIndexNode> Node;
 
         TNodeState(TPageId pageId, TRowId beginRowId, TRowId endRowId, TCellsIterable beginKey, TCellsIterable endKey, ui64 beginDataSize, ui64 endDataSize)
             : PageId(pageId)
@@ -52,6 +53,13 @@ public:
         }
     };
 
+private:
+    struct TPartNodes {
+        const TPart* Part;
+        TVector<TNodeState> Nodes;
+    };
+
+public:
     TTableHistogramBuilderBtreeIndex(const TSubset& subset, IPages* env)
         : Subset(subset)
         , Env(env)
@@ -62,12 +70,13 @@ public:
     bool Build(THistogram& histogram, ui64 resolution, ui64 totalSize) {
         Resolution = resolution;
 
-        TVector<TVector<TNodeState>> parts;
+        TVector<TPartNodes> parts;
 
         for (const auto& part : Subset.Flatten) {
             auto& meta = part->IndexPages.GetBTree({});
-            parts.emplace_back();
-            parts.back().emplace_back(meta.PageId, 0, meta.RowCount, EmptyKey, EmptyKey, 0, meta.GetTotalDataSize());
+            parts.emplace_back(part.Part.Get(), TVector<TNodeState>{
+                {meta.PageId, 0, meta.RowCount, EmptyKey, EmptyKey, 0, meta.GetTotalDataSize()}
+            });
         }
 
         auto ready = BuildHistogramRecursive<TGetSize>(histogram, parts, 0, totalSize, 0);
@@ -82,7 +91,7 @@ public:
 
 private:
     template <typename TGetSize>
-    bool BuildHistogramRecursive(THistogram& histogram, TVector<TVector<TNodeState>>& parts, ui64 beginSize, ui64 endSize, ui32 depth) {
+    bool BuildHistogramRecursive(THistogram& histogram, TVector<TPartNodes>& parts, ui64 beginSize, ui64 endSize, ui32 depth) {
         const static ui32 MaxDepth = 100;
         bool ready = true;
 
@@ -97,8 +106,17 @@ private:
             Y_DEBUG_ABORT("Invalid part states");
             return true;
         }
+        auto& biggestPart = parts[biggestPartSize];
 
-        TCellsIterable splitKey = FindMiddlePartKey<TGetSize>(parts[biggestPartIndex], biggestPartSize);
+        // FIXME: load the biggest node if its size more than half
+        if (biggestPart.Nodes.size() == 1) {
+            auto node = biggestPart.Nodes.front();
+            biggestPart.Nodes.clear();
+            if (!TryLoadNode(biggestPart.Part, node, biggestPart.Nodes)) {
+                return false;
+            }
+        }
+        TCellsIterable splitKey = FindMiddlePartKey<TGetSize>(biggestPart, biggestPartSize);
 
         if (Y_UNLIKELY(!splitKey)) {
             // Note: an extremely rare scenario when we can't split biggest SST
@@ -112,14 +130,16 @@ private:
         ui64 splitSize = 0; // TODO: beginSize?
         ui64 leftSize = 0, rightSize = 0;
 
-        TVector<TVector<TNodeState>> leftParts, rightParts;
+        // TODO: don't copy nodes?
+
+        TVector<TPartNodes> leftParts, rightParts;
         // for (const auto& part : parts) {
 
         // }
 
         ready &= BuildHistogramRecursive<TGetSize>(histogram, leftParts, beginSize, beginSize + leftSize, depth + 1);
         
-        
+        AddBucket(histogram, splitKey, splitSize);
 
         ready &= BuildHistogramRecursive<TGetSize>(histogram, rightParts, SafeDiff(endSize, rightSize), endSize, depth + 1);
 
@@ -127,10 +147,10 @@ private:
     }
 
     template <typename TGetSize>
-    void FindBiggestPart(const TVector<TVector<TNodeState>>& parts, size_t& biggestPartIndex, ui64& biggestPartSize) {
+    void FindBiggestPart(const TVector<TPartNodes>& parts, size_t& biggestPartIndex, ui64& biggestPartSize) {
         for (auto index : xrange(parts.size())) {
             ui64 size = 0;
-            for (const auto& node : parts[index]) {
+            for (const auto& node : parts[index].Nodes) {
                 size += TGetSize::Get(node);
             }
             if (size > biggestPartSize) {
@@ -141,14 +161,14 @@ private:
     }
 
     template <typename TGetSize>
-    TCellsIterable FindMiddlePartKey(TVector<TNodeState>& part, ui64& partSize) {
-        Y_ABORT_UNLESS(part);
+    TCellsIterable FindMiddlePartKey(const TPartNodes& part, ui64& partSize) {
+        Y_ABORT_UNLESS(part.Nodes);
         
         TCellsIterable splitKey = EmptyKey;
         ui64 splitSize = 0, currentSize = 0;
         const ui64 middleSize = partSize / 2;
         
-        for (const auto& node : part) {
+        for (const auto& node : part.Nodes) {
             if (!splitKey || AbsDifference(currentSize, middleSize) < AbsDifference(splitSize, middleSize)) {
                 splitKey = node.BeginKey;
                 splitSize = currentSize;
@@ -156,9 +176,6 @@ private:
 
             currentSize += TGetSize::Get(node);
         }
-
-        // TODO: load node
-        Y_UNUSED(Env);
 
         return splitKey;
     }
@@ -183,6 +200,32 @@ private:
         histogram.push_back({serializedSplitKey, value});
     }
 
+    bool TryLoadNode(const TPart* part, TNodeState& node, TVector<TNodeState>& list) const noexcept {
+        auto page = Env->TryGetPage(part, node.PageId, {});
+        if (!page) {
+            return false;
+        }
+
+        node.Node.emplace(*page);
+        auto& groupInfo = part->Scheme->GetLayout({});
+
+        TRowId currentBeginRowId = node.BeginRowId;
+        const TCellsIterable& currentBeginKey = node.BeginKey;
+        ui64 currentBeginDataSize = node.BeginDataSize;
+        for (auto pos : xrange(node.Node->GetChildrenCount())) {
+            auto& child = node.Node->GetChild(pos);
+
+            TCellsIterable endKey = pos < node.Node->GetKeysCount() ? node.Node->GetKeyCellsIterable(pos, groupInfo.ColsKeyIdx) : node.EndKey;
+
+            list.emplace_back(child.PageId, 
+                currentBeginRowId, child.RowCount, 
+                currentBeginKey, endKey, 
+                currentBeginDataSize, child.GetTotalDataSize());
+        }
+
+        return true;
+    }
+
 private:
     ui64 AbsDifference(ui64 a, ui64 b) {
         return static_cast<ui64>(std::abs(static_cast<i64>(a) - static_cast<i64>(b)));
@@ -200,7 +243,7 @@ private:
 
 }
 
-bool BuildStatsHistogramsBTreeIndex(const TSubset& subset, TStats& stats, ui64 rowCountResolution, ui64 dataSizeResolution, IPages* env) {
+inline bool BuildStatsHistogramsBTreeIndex(const TSubset& subset, TStats& stats, ui64 rowCountResolution, ui64 dataSizeResolution, IPages* env) {
     bool ready = true;
     
     TTableHistogramBuilderBtreeIndex builder(subset, env);

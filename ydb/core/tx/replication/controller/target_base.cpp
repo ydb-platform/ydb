2ab60a4de13588d955339dc7d12872d1379083f6
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2024-05-30 16:31:22 +0300

    Switch replication to 'Done' state

--- ydb/core/tx/replication/controller/target_base.cpp
+++ ydb/core/tx/replication/controller/target_base.cpp
@@ -12,8 +12,10 @@ using ETargetKind = TReplication::ETargetKind;
 using EDstState = TReplication::EDstState;
 using EStreamState = TReplication::EStreamState;
 
-TTargetBase::TTargetBase(ETargetKind kind, ui64 id, const TString& srcPath, const TString& dstPath)
-    : Id(id)
+TTargetBase::TTargetBase(TReplication::TPtr replication, ETargetKind kind,
+        ui64 id, const TString& srcPath, const TString& dstPath)
+    : Replication(replication)
+    , Id(id)
     , Kind(kind)
     , SrcPath(srcPath)
     , DstPath(dstPath)
@@ -42,6 +44,14 @@ EDstState TTargetBase::GetDstState() const {
 
 void TTargetBase::SetDstState(const EDstState value) {
     DstState = value;
+    switch (DstState) {
+    case EDstState::Alter:
+        return Replication->AddPendingAlterTarget(Id);
+    case EDstState::Done:
+        return Replication->RemovePendingAlterTarget(Id);
+    default:
+        break;
+    }
 }
 
 const TPathId& TTargetBase::GetDstPathId() const {
@@ -77,30 +87,30 @@ void TTargetBase::SetIssue(const TString& value) {
     TruncatedIssue(Issue);
 }
 
-void TTargetBase::Progress(TReplication::TPtr replication, const TActorContext& ctx) {
+void TTargetBase::Progress(const TActorContext& ctx) {
     switch (DstState) {
     case EDstState::Creating:
         if (!DstCreator) {
-            DstCreator = ctx.Register(CreateDstCreator(replication, Id, ctx));
+            DstCreator = ctx.Register(CreateDstCreator(Replication, Id, ctx));
         }
         break;
     case EDstState::Syncing:
         break; // TODO
     case EDstState::Ready:
         if (!WorkerRegistar) {
-            WorkerRegistar = ctx.Register(CreateWorkerRegistar(replication, ctx));
+            WorkerRegistar = ctx.Register(CreateWorkerRegistar(ctx));
         }
         break;
     case EDstState::Alter:
         if (!DstAlterer) {
-            DstAlterer = ctx.Register(CreateDstAlterer(replication, Id, ctx));
+            DstAlterer = ctx.Register(CreateDstAlterer(Replication, Id, ctx));
         }
         break;
     case EDstState::Done:
         break;
     case EDstState::Removing:
         if (!DstRemover) {
-            DstRemover = ctx.Register(CreateDstRemover(replication, Id, ctx));
+            DstRemover = ctx.Register(CreateDstRemover(Replication, Id, ctx));
         }
         break;
     case EDstState::Error:

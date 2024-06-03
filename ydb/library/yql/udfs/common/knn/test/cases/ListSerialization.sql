03ecb11124d535d93ebcd268c8d1f0256b292376
azevaykin <145343289+azevaykin@users.noreply.github.com>
2024-06-03 19:14:56 +0300

    New serialization formats in Knn UDF (#4445)
    Co-authored-by: Valerii Mironov <mbkkt@ydb.tech>

--- ydb/library/yql/udfs/common/knn/test/cases/ListSerialization.sql
+++ ydb/library/yql/udfs/common/knn/test/cases/ListSerialization.sql
@@ -1,9 +1,18 @@
 --list serialization
 $vector = [1.2f, 2.3f, 3.4f, 4.5f, 5.6f];
-$vector_binary_str = Knn::ToBinaryString($vector);
+$vector_binary_str = Knn::ToBinaryStringFloat($vector);
 select $vector_binary_str;
 
 --deserialization
-$deserialized_vector = Knn::FromBinaryString($vector_binary_str);
+$deserialized_vector = Knn::FloatFromBinaryString($vector_binary_str);
 select $deserialized_vector;
 select $deserialized_vector = $vector;
+
+$vector_d  = Cast([0, 1, 0, 0, 0, 0, 0, 0] AS List<Double>);
+$vector_f  = Cast([0, 1, 0, 0, 0, 0, 0, 0] AS List<Float>);
+$vector_u8 = Cast([0, 1, 0, 0, 0, 0, 0, 0] AS List<Uint8>);
+$vector_i8 = Cast([0, 1, 0, 0, 0, 0, 0, 0] AS List<Int8>);
+select Knn::ToBinaryStringBit($vector_d);
+select Knn::ToBinaryStringBit($vector_f);
+select Knn::ToBinaryStringBit($vector_u8);
+select Knn::ToBinaryStringBit($vector_i8);

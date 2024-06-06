bb1c94d73def0a520bef03d48e25cf9be3308e22
Alexey Ozeritskiy <aozeritsky@ydb.tech>
2024-06-06 16:14:44 +0100

    Revert "GraceJoin: consume all inputs data (fix hang)"
    This reverts commit 9bbdb12273eb26b34aed68d0eaccc5ef9c2350ff.

--- ydb/library/yql/minikql/comp_nodes/mkql_grace_join.cpp
+++ ydb/library/yql/minikql/comp_nodes/mkql_grace_join.cpp
@@ -1256,6 +1256,10 @@ EFetchResult TGraceJoinState::FetchValues(TComputationContext& ctx, NUdf::TUnbox
                     }
                 }
 
+                if (resultLeft == EFetchResult::Yield || resultRight == EFetchResult::Yield) {
+                    return EFetchResult::Yield;
+                }
+
                 if (resultLeft == EFetchResult::Finish ) {
                     *HaveMoreLeftRows = false;
                 }
@@ -1263,12 +1267,6 @@ EFetchResult TGraceJoinState::FetchValues(TComputationContext& ctx, NUdf::TUnbox
 
                 if (resultRight == EFetchResult::Finish ) {
                     *HaveMoreRightRows = false;
-                }
-
-                if ((resultLeft == EFetchResult::Yield && (!*HaveMoreRightRows || resultRight == EFetchResult::Yield)) ||
-                    (resultRight == EFetchResult::Yield && !*HaveMoreLeftRows))
-                {
-                    return EFetchResult::Yield;
                 }
 
                 if (!*HaveMoreRightRows && !*PartialJoinCompleted && LeftPacker->TuplesBatchPacked >= LeftPacker->BatchSize ) {

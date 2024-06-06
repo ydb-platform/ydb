a32a61f7b7a35c1188f079e59d286c6c10773e7b
Grigorii Papashvili <papashviliga@ydb.tech>
2024-06-06 09:41:03 +0000

    Resolve merge conflict

--- ydb/library/yql/providers/generic/connector/tests/datasource/postgresql/select_positive.py
+++ ydb/library/yql/providers/generic/connector/tests/datasource/postgresql/select_positive.py
@@ -235,7 +235,7 @@ class Factory:
                     'buki',
                     'buki ',
                     'buki ',
-                    'buki',
+                    'buki', 
                     datetime.date(1988, 11, 20),
                     datetime.datetime(1988, 11, 20, 12, 00, 00),
 <<<<<<< HEAD
@@ -493,8 +493,11 @@ class Factory:
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> Connector: PostgreSQL: Json tests
+=======
+>>>>>>> Resolve merge conflict
     def _json(self) -> TestCase:
         schema = Schema(
             columns=ColumnList(
@@ -603,7 +606,52 @@ class Factory:
                 schema=schema,
             ),
         ]
+<<<<<<< HEAD
 >>>>>>> Connector: PostgreSQL: Json tests
+=======
+=======
+    # def _json(self) -> TestCase:
+    #     schema = Schema(
+    #         columns=ColumnList(
+    #             Column(
+    #                 name='col_json',
+    #                 ydb_type=Type.JSON,
+    #                 data_source_type=DataSourceType(pg=postgresql.Json()),
+    #             ),
+    #         ),
+    #     )
+
+    #     data_in = [
+    #         ['{ "friends": [{"name": "James Holden","age": 35},{"name": "Naomi Nagata","age": 30}]}'],
+    #         ['{ "TODO" : "unicode" }'],
+    #         [None],
+    #     ]
+
+    #     data_out_1 = [
+    #         ['{"name" : "James Holden", "age" : 35}'],
+    #         [None],
+    #         [None],
+    #     ]
+
+    #     data_source_kind = EDataSourceKind.POSTGRESQL
+
+    #     test_case_name = 'json'
+
+    #     return [
+    #         TestCase(
+    #             name_=test_case_name,
+    #             data_in=data_in,
+    #             data_out_=data_out_1,
+    #             protocol=EProtocol.NATIVE,
+    #             select_what=SelectWhat(SelectWhat.Item(name='JSON_QUERY(col_json, "$.friends[0]")', kind='expr')),
+    #             select_where=None,
+    #             data_source_kind=data_source_kind,
+    #             pragmas=dict(),
+    #             schema=schema,
+    #         ),
+    #     ]
+>>>>>>> 71cae95586... postgresql connector json tests
+>>>>>>> Resolve merge conflict
 
     def make_test_cases(self) -> Sequence[TestCase]:
         return list(
@@ -613,6 +661,6 @@ class Factory:
                 self._constant(),
                 self._count(),
                 self._pushdown(),
-                self._json(),
+                # self._json(), TODO need json2 udf module in kqprun
             )
         )

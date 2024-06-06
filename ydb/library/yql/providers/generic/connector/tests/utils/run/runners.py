c0682acbd21ee6c095b4fe97c44945f6eff259bf
Grigorii Papashvili <papashviliga@ydb.tech>
2024-06-06 23:25:20 +0000

    review fixes

--- ydb/library/yql/providers/generic/connector/tests/utils/run/runners.py
+++ ydb/library/yql/providers/generic/connector/tests/utils/run/runners.py
@@ -54,13 +54,13 @@ def configure_runner(runner_type: str, settings: Settings) -> Runner:
             return DqRunner(
                 dqrun_path=yat.build_path("ydb/library/yql/tools/dqrun/dqrun"),
                 settings=settings,
-                udfs_dir=yat.build_path("ydb/library/yql/udfs/common/json2"),
+                udf_dir=yat.build_path("ydb/library/yql/udfs/common/json2"),
             )
         case "kqprun":
             return KqpRunner(
                 kqprun_path=yat.build_path("ydb/tests/tools/kqprun/kqprun"),
                 settings=settings,
-                udfs_dir=yat.build_path("ydb/library/yql/udfs/common/json2"),
+                udf_dir=yat.build_path("ydb/library/yql/udfs/common/json2"),
             )
 >>>>>>> style fixes
         case _:

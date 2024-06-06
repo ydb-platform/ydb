0672091702d1e7b881fe2c78d5256e13dc32e40e
Oleg Doronin <dorooleg@yandex.ru>
2024-06-06 10:12:09 +0000

    cleanup

--- ydb/library/yql/providers/common/http_gateway/yql_aws_signature_ut.cpp
+++ ydb/library/yql/providers/common/http_gateway/yql_aws_signature_ut.cpp
@@ -6,19 +6,8 @@
 namespace NYql {
 
 Y_UNIT_TEST_SUITE(TAwsSignature) {
-    Y_UNIT_TEST(Sign1) {
+    Y_UNIT_TEST(Sign) {
         NYql::TAwsSignature signature("GET", "http://os.com/my-bucket/year=2024/day=03/", "application/json", {}, "key", "pwd");
-        UNIT_ASSERT_VALUES_EQUAL(signature.GetContentType(), "application/json");
-        UNIT_ASSERT_VALUES_EQUAL(signature.GetXAmzContentSha256(), "12ae32cb1ec02d01eda3581b127c1fee3b0dc53572ed6baf239721a03d82e126");
-        UNIT_ASSERT_STRING_CONTAINS(signature.GetAuthorization(), "SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date, Signature=");
-        UNIT_ASSERT_STRING_CONTAINS(signature.GetAuthorization(), "AWS4-HMAC-SHA256 Credential=/");
-        UNIT_ASSERT_STRING_CONTAINS(signature.GetAuthorization(), "///aws4_request");
-        UNIT_ASSERT_STRING_CONTAINS(signature.GetAuthorization(), "Signature=");
-        UNIT_ASSERT_VALUES_UNEQUAL(signature.GetAmzDate(), "");
-    }
-
-    Y_UNIT_TEST(Sign2) {
-        NYql::TAwsSignature signature("GET", "http://os.com?a=3&b=7", "application/json", {}, "key", "pwd");
         UNIT_ASSERT_VALUES_EQUAL(signature.GetContentType(), "application/json");
         UNIT_ASSERT_VALUES_EQUAL(signature.GetXAmzContentSha256(), "12ae32cb1ec02d01eda3581b127c1fee3b0dc53572ed6baf239721a03d82e126");
         UNIT_ASSERT_STRING_CONTAINS(signature.GetAuthorization(), "SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date, Signature=");

bea192aa7a7be05317fd85ac29e0b24534176890
Yuriy Kaminskiy <yumkam7@ydb.tech>
2024-06-07 13:52:07 +0300

    runner/run (internal) make out and err optional

--- ydb/library/benchmarks/runner/runner/runner.py
+++ ydb/library/benchmarks/runner/runner/runner.py
@@ -27,8 +27,8 @@ def run(argv, out, err, timeout=30*60, hard_timeout=5):
     try:
         start_time = time_ns()
         pid = os.posix_spawn(argv[0], argv, {}, setsigmask=oldmask, file_actions=(
-            (os.POSIX_SPAWN_OPEN, 1, out, os.O_WRONLY | os.O_CREAT, 0o666),
-            (os.POSIX_SPAWN_OPEN, 2, err, os.O_WRONLY | os.O_CREAT, 0o666),
+            ([(os.POSIX_SPAWN_OPEN, 1, out, os.O_WRONLY | os.O_CREAT, 0o666)] if out else []) +
+            ([(os.POSIX_SPAWN_OPEN, 2, err, os.O_WRONLY | os.O_CREAT, 0o666)] if err else [])
             ))
         assert pid > 0
         if timeout is not None:

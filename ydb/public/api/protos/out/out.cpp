13767953469da99adaf1be8204539f23bac6f1ad
Ilia Shakhov <pixcc@ydb.tech>
2024-06-10 10:55:16 +0000

    Add import start and end times

--- ydb/public/api/protos/out/out.cpp
+++ ydb/public/api/protos/out/out.cpp
@@ -2,6 +2,7 @@
 #include <ydb/public/api/protos/ydb_monitoring.pb.h>
 #include <ydb/public/api/protos/ydb_status_codes.pb.h>
 #include <ydb/public/api/protos/ydb_export.pb.h>
+#include <ydb/public/api/protos/ydb_import.pb.h>
 
 #include <util/stream/output.h>
 
@@ -23,4 +24,8 @@ Y_DECLARE_OUT_SPEC(, Ydb::Monitoring::StatusFlag::Status, stream, value) {
 
 Y_DECLARE_OUT_SPEC(, Ydb::Export::ExportProgress::Progress, stream, value) {
     stream << Ydb::Export::ExportProgress_Progress_Name(value);
+}
+
+Y_DECLARE_OUT_SPEC(, Ydb::Import::ImportProgress::Progress, stream, value) {
+    stream << Ydb::Import::ImportProgress_Progress_Name(value);
 }

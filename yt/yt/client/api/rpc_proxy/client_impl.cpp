9994490ac611e1e098a265c37f833f6ff7974d68
Marina Preskokova <mpereskokova@ytsaurus.tech>
2024-06-11 19:37:24 +0300

    QT; Multiple ACOs
    No description
    
    ---
    0309ad6d3e31b25f24aa81607fa7aac6ed9f0d9d
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/635
    
    Co-authored-by: mpereskokova <mpereskokova@yandex-team.com>

--- yt/yt/client/api/rpc_proxy/client_impl.cpp
+++ yt/yt/client/api/rpc_proxy/client_impl.cpp
@@ -2136,6 +2136,12 @@ TFuture<NQueryTrackerClient::TQueryId> TClient::StartQuery(
     if (options.AccessControlObject) {
         req->set_access_control_object(*options.AccessControlObject);
     }
+    if (options.AccessControlObjects) {
+        auto* protoAccessControlObjects = req->mutable_access_control_objects();
+        for (const auto& aco : *options.AccessControlObjects) {
+            protoAccessControlObjects->add_items(aco);
+        }
+    }
 
     for (const auto& file : options.Files) {
         auto* protoFile = req->add_files();
@@ -2318,6 +2324,12 @@ TFuture<void> TClient::AlterQuery(
     }
     if (options.AccessControlObject) {
         req->set_access_control_object(*options.AccessControlObject);
+    }
+    if (options.AccessControlObjects) {
+        auto* protoAccessControlObjects = req->mutable_access_control_objects();
+        for (const auto& aco : *options.AccessControlObjects) {
+            protoAccessControlObjects->add_items(aco);
+        }
     }
 
     return req->Invoke().AsVoid();

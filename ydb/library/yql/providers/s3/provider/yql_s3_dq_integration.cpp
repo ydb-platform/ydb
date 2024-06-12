863ad2c2e4d6c03ab74df7f8b2aa1ba1943007c4
Oleg Doronin <dorooleg@yandex.ru>
2024-06-12 16:49:19 +0200

    path and compression have been added for s3 plan (#5474)

--- ydb/library/yql/providers/s3/provider/yql_s3_dq_integration.cpp
+++ ydb/library/yql/providers/s3/provider/yql_s3_dq_integration.cpp
@@ -266,6 +266,7 @@ public:
                             .Name().Build(token)
                         .Build()
                         .RowsLimitHint(ctx.NewAtom(read->Pos(), ""))
+                        .Path(s3ReadObject.Path())
                         .Format(s3ReadObject.Object().Format())
                         .RowType(ExpandType(s3ReadObject.Pos(), *rowType, ctx))
                         .Settings(s3ReadObject.Object().Settings())
@@ -309,6 +310,7 @@ public:
                             .Name().Build(token)
                             .Build()
                         .RowsLimitHint(ctx.NewAtom(read->Pos(), ""))
+                        .Path(s3ReadObject.Path())
                         .SizeLimit(
                             sizeLimitIndex != -1 ? readSettings->Child(sizeLimitIndex)->TailPtr()
                                                  : emptyNode)
@@ -587,6 +589,9 @@ public:
             } else {
                 properties["Name"] = "Raw read from external data source";
             }
+            if (TString path = settings.Path().StringValue()) {
+                properties["Path"] = path;
+            }
             properties["Format"] = "raw";
             if (TString limit = settings.RowsLimitHint().StringValue()) {
                 properties["RowsLimitHint"] = limit;
@@ -599,7 +604,13 @@ public:
             } else {
                 properties["Name"] = "Parse from external data source";
             }
+            if (TString path = settings.Path().StringValue()) {
+                properties["Path"] = path;
+            }
             properties["Format"] = settings.Format().StringValue();
+            if (const auto& compression = GetCompression(settings.Settings().Ref()); !compression.empty()) {
+                properties["Compression"] = TString{compression};
+            }
             if (TString limit = settings.RowsLimitHint().StringValue()) {
                 properties["RowsLimitHint"] = limit;
             }

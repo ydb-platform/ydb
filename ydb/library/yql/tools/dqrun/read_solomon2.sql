8e0afc8b6d1268f2816ea5f1ec20811b5eef646c
Sergey Uzhakov <uzhas@ydb.tech>
2024-06-12 11:16:55 +0000

    tune sql

--- ydb/library/yql/tools/dqrun/read_solomon2.sql
+++ ydb/library/yql/tools/dqrun/read_solomon2.sql
@@ -1,11 +1,11 @@
 select * from solomon_prod.yq
 with (
-  selector = @@{execpool=User,activity=YQ_STORAGE_PROXY,sensor=ActorsAliveByActivity}@@,
+  program = @@{execpool=User,activity=YQ_STORAGE_PROXY,sensor=ActorsAliveByActivity}@@,
   labels = "label1, label2, label3",
   from = "2023-12-08T14:40:39Z",
   to = "2023-12-08T14:45:39Z",
-  `downsampling.disabled` = "true",
-  `downsampling.grid_interval` = "15000",
+  `downsampling.disabled` = "false",
+  `downsampling.grid_interval` = "15",
   `downsampling.grid_aggregation` = "AVG",
   `downsampling.gap_filling` = "PREVIOUS"
 ) as s

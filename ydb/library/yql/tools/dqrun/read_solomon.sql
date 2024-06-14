3c5cbdab7e4522df8e89cfbe80518e55813f0835
Sergey Uzhakov <uzhas@ydb.tech>
2024-06-14 14:40:33 +0000

    remove scripts

--- ydb/library/yql/tools/dqrun/read_solomon.sql
+++
@@ -1,8 +0,0 @@
-select * from solomon_prod.yq
-with (
-  program = @@{execpool=User,activity=YQ_STORAGE_PROXY,sensor=ActorsAliveByActivity}@@,
-  labels = "label1, label2, label3",
-  from = "2023-12-08T14:40:39Z",
-  to = "2023-12-08T14:45:39Z",
-) as s
-limit 10;

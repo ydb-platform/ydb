8e3ec1cce00fef685ca5e6339930ec9a4883806f
Sergey Uzhakov <uzhas@ydb.tech>
2024-06-15 19:30:00 +0300

    improve cfg validation

--- ydb/library/yql/tests/sql/suites/solomon/Downsampling.sql
+++ ydb/library/yql/tests/sql/suites/solomon/Downsampling.sql
@@ -3,7 +3,7 @@ SELECT * FROM local_solomon.my_project WITH (
   from = "2023-12-08T14:40:39Z",
   to = "2023-12-08T14:45:39Z",
   `downsampling.disabled` = "true",
-  `downsampling.grid_aggregation` = "SUM",
+  `downsampling.aggregation` = "SUM",
   `downsampling.grid_interval` = "25",
   `downsampling.fill` = "PREVIOUS"
 );

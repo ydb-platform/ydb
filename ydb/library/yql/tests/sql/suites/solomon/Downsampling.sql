b367dfab7fc455a57e37b78f7752cc8cb140d775
Sergey Uzhakov <uzhas@ydb.tech>
2024-06-15 16:51:31 +0300

    switch disabled

--- ydb/library/yql/tests/sql/suites/solomon/Downsampling.sql
+++ ydb/library/yql/tests/sql/suites/solomon/Downsampling.sql
@@ -2,7 +2,7 @@ SELECT * FROM local_solomon.my_project WITH (
   program = @@{}@@,
   from = "2023-12-08T14:40:39Z",
   to = "2023-12-08T14:45:39Z",
-  `downsampling.disabled` = "false",
+  `downsampling.disabled` = "true",
   `downsampling.grid_aggregation` = "SUM",
   `downsampling.grid_interval` = "25",
   `downsampling.fill` = "PREVIOUS"

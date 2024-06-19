721d18af80ed965b6ec976738f86582683055f21
Kirill Rysin <naspirato@gmail.com>
2024-06-19 10:44:54 +0000

    fix formating

--- .github/scripts/get_build_diff.py
+++ .github/scripts/get_build_diff.py
@@ -42,7 +42,7 @@ def main():
             Decimal(".001"), rounding=ROUND_HALF_UP
         )
         stripped_diff_perc = Decimal(
-            stripped_bytes_diff * 100 / main_size_bytes
+            stripped_bytes_diff * 100 / main_size_stripped_bytes
         ).quantize(Decimal(".001"), rounding=ROUND_HALF_UP)
 
         human_readable_size = bytes_to_human_iec(current_size_bytes)
@@ -71,8 +71,8 @@ def main():
             "<details><summary>Build size details</summary><p>\n\n"
             f"{branch}: {main_github_sha} |merge: {current_pr_commit_sha} |diff | diff %%|\n"
             f"| ---: | ---: | ---: | ---: |\n"
-            f"|**{format_number(main_size_bytes)}** Bytes |**{format_number(current_size_bytes)}** Bytes|**{sign}{human_readable_size_diff}|{sign}{diff_perc}%%|\n"
-            f"|**{format_number(main_size_stripped_bytes)}** Bytes|**{format_number(current_size_stripped_bytes)}|**{stripped_sign}{human_readable_stripped_size_diff}|{stripped_sign}{stripped_diff_perc}%%|\n\n"
+            f"|**{format_number(main_size_bytes)}** Bytes |**{format_number(current_size_bytes)}** Bytes|**{sign}{human_readable_size_diff}**|{sign}{diff_perc}%%**|\n"
+            f"|**{format_number(main_size_stripped_bytes)}** Bytes|**{format_number(current_size_stripped_bytes)}** Bytes|**{stripped_sign}{human_readable_stripped_size_diff}**|**{stripped_sign}{stripped_diff_perc}%%**|\n\n"
             "[ydbd size dashboard](https://datalens.yandex/cu6hzmpaki700)\n\n"
             "</p></details>"
         )

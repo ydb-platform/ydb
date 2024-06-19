d237693e761fe92f15e0f2dc4d56b8549310d2a1
Ilia Shakhov <pixcc@ydb.tech>
2024-06-19 15:59:47 +0000

    Add allow unknown fields

--- ydb/core/cms/cms_tx_load_state.cpp
+++ ydb/core/cms/cms_tx_load_state.cpp
@@ -91,7 +91,10 @@ public:
             request.Owner = owner;
             request.Order = order;
             request.Priority = priority;
-            google::protobuf::TextFormat::ParseFromString(requestStr, &request.Request);
+
+            google::protobuf::TextFormat::Parser parser;
+            parser.AllowUnknownField(true);
+            parser.ParseFromString(requestStr, &request.Request);
 
             LOG_DEBUG(ctx, NKikimrServices::CMS, "Loaded request %s owned by %s: %s",
                       id.data(), owner.data(), requestStr.data());
@@ -149,8 +152,11 @@ public:
             permission.PermissionId = id;
             permission.RequestId = requestId;
             permission.Owner = owner;
-            google::protobuf::TextFormat::ParseFromString(actionStr, &permission.Action);
             permission.Deadline = TInstant::MicroSeconds(deadline);
+
+            google::protobuf::TextFormat::Parser parser;
+            parser.AllowUnknownField(true);
+            parser.ParseFromString(actionStr, &permission.Action);
 
             LOG_DEBUG(ctx, NKikimrServices::CMS, "Loaded permission %s owned by %s valid until %s: %s",
                       id.data(), owner.data(), TInstant::MicroSeconds(deadline).ToStringLocalUpToSeconds().data(), actionStr.data());
@@ -185,7 +191,10 @@ public:
             TNotificationInfo notification;
             notification.NotificationId = id;
             notification.Owner = owner;
-            google::protobuf::TextFormat::ParseFromString(notificationStr, &notification.Notification);
+            
+            google::protobuf::TextFormat::Parser parser;
+            parser.AllowUnknownField(true);
+            parser.ParseFromString(notificationStr, &notification.Notification);
 
             LOG_DEBUG(ctx, NKikimrServices::CMS, "Loaded notification %s owned by %s: %s",
                       id.data(), owner.data(), notificationStr.data());

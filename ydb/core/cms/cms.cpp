9bc529fadfbceb97cc2757f6a278397469b687a4
Ilia Shakhov <pixcc@ydb.tech>
2024-06-19 17:13:18 +0000

    Dont store zero priority in proto

--- ydb/core/cms/cms.cpp
+++ ydb/core/cms/cms.cpp
@@ -303,7 +303,9 @@ bool TCms::CheckPermissionRequest(const TPermissionRequest &request,
             scheduled.SetDuration(request.GetDuration());
         scheduled.SetTenantPolicy(request.GetTenantPolicy());
         scheduled.SetAvailabilityMode(request.GetAvailabilityMode());
-        scheduled.SetPriority(request.GetPriority());
+        if (request.HasPriority()) {
+            scheduled.SetPriority(request.GetPriority());
+        }
     }
 
     LOG_INFO_S(ctx, NKikimrServices::CMS,
@@ -1467,7 +1469,7 @@ void TCms::CheckAndEnqueueRequest(TEvCms::TEvPermissionRequest::TPtr &ev, const 
         }
     }
 
-    if (rec.GetPriority() && !EnableCMSRequestPriorities) {
+    if (rec.HasPriority() && !EnableCMSRequestPriorities) {
         if (rec.GetUser() == WALLE_CMS_USER) {
             rec.ClearPriority();
         } else {

b2f26bffcfa633191846132c953bba745592b436
Kirill Rysin <naspirato@gmail.com>
2024-06-19 10:25:01 +0000

    view in table

--- .github/scripts/get_build_diff.py
+++ .github/scripts/get_build_diff.py
@@ -36,12 +36,18 @@ def main():
         current_size_stripped_bytes = int(current_sizes_result["size_stripped_bytes"])
 
         bytes_diff = current_size_bytes - main_size_bytes
+        stripped_bytes_diff = current_size_stripped_bytes - main_size_stripped_bytes
+
         diff_perc = Decimal(bytes_diff * 100 / main_size_bytes).quantize(
             Decimal(".001"), rounding=ROUND_HALF_UP
         )
+        stripped_diff_perc = Decimal(
+            stripped_bytes_diff * 100 / main_size_bytes
+        ).quantize(Decimal(".001"), rounding=ROUND_HALF_UP)
 
         human_readable_size = bytes_to_human_iec(current_size_bytes)
         human_readable_size_diff = bytes_to_human_iec(bytes_diff)
+        human_readable_stripped_size_diff = bytes_to_human_iec(stripped_diff_perc)
 
         if bytes_diff >= 0:
             sign = "+"
@@ -55,15 +61,18 @@ def main():
             sign = ""
             color = "green"
 
+        if stripped_diff_perc > 0:
+            stripped_sign = "+"
+        else:
+            stripped_sign = ""
+
         comment = (
             f"merge: {current_pr_commit_sha} ydbd size {human_readable_size} **{sign}{human_readable_size_diff} {sign}{diff_perc}%%** vs build {branch}: {main_github_sha}\n\n"
             "<details><summary>Build size details</summary><p>\n\n"
-            f"{branch}: {main_github_sha} ydbd build size:\n"
-            f"  - binary size **{format_number(main_size_bytes)}** Bytes \n"
-            f"  - stripped binary size **{format_number(main_size_stripped_bytes)}** Bytes\n\n"
-            f"merge: {current_pr_commit_sha} ydbd build size:\n"
-            f"  - binary size **{format_number(current_size_bytes)}** Bytes \n"
-            f"  - stripped binary size **{format_number(current_size_stripped_bytes)}** Bytes\n\n"
+            f"{branch}: {main_github_sha} |merge: {current_pr_commit_sha} |diff | diff %%|\n"
+            f"| ---: | ---: | ---: | ---: |\n"
+            f"|**{format_number(main_size_bytes)}** Bytes |**{format_number(current_size_bytes)}** Bytes|**{sign}{human_readable_size_diff}|{sign}{diff_perc}%%|\n"
+            f"|**{format_number(main_size_stripped_bytes)}** Bytes|**{format_number(current_size_stripped_bytes)}|**{stripped_sign}{human_readable_stripped_size_diff}|{stripped_sign}{stripped_diff_perc}%%|\n\n"
             "[ydbd size dashboard](https://datalens.yandex/cu6hzmpaki700)\n\n"
             "</p></details>"
         )

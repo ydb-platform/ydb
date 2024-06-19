4ccb78569fa336d5a57cb2d8f880013cfc2ba6fa
robot-piglet <robot-piglet@yandex-team.com>
2024-06-20 00:01:05 +0300

    Intermediate changes

--- yt/yt/core/misc/unittests/consistent_hashing_ut.cpp
+++ yt/yt/core/misc/unittests/consistent_hashing_ut.cpp
@@ -25,9 +25,10 @@ struct TStringComparer
 
 struct TStringHasher
 {
-    ui64 operator()(const TString& node, int index) const
+    ui64 operator()(const TString& node, ui64 index) const
     {
-        return ::THash<TStringBuf>()(node + std::to_string(index));
+        auto hashNode = ::THash<TStringBuf>()(node);
+        return (hashNode ^ (index << 3)) + hashNode << (index & 7);
     }
 };
 
@@ -563,7 +564,7 @@ TEST(TConsistentHashingRing, ManyNodesSimultaneouslyStress)
 
 TEST(TConsistentHashingRing, SmallTokenCount)
 {
-    const size_t testCases = 10;
+    const size_t testCases = 4;
 
     auto singleReplicaLargeResult = 0.0;
     auto manyReplicasLargeResult = 0.0;
@@ -612,8 +613,8 @@ TEST(TConsistentHashingRing, SmallTokenCount)
             /*batchSize*/ 200) / testCases;
     }
 
-    EXPECT_LE(std::fabs(singleReplicaLargeResult - singleReplicaSmallResult), 0.1);
-    EXPECT_LE(std::fabs(manyReplicasLargeResult - manyReplicasSmallResult), 0.1);
+    EXPECT_LE(std::fabs(singleReplicaLargeResult - singleReplicaSmallResult), 0.12);
+    EXPECT_LE(std::fabs(manyReplicasLargeResult - manyReplicasSmallResult), 0.12);
 }
 
 ////////////////////////////////////////////////////////////////////////////////

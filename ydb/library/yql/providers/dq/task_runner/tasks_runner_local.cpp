57a2050e6c22517725d9436377c0c8e285b3f3fc
Filitov Mikhail <filitovme@gmail.com>
2024-06-21 00:10:37 +0300

    use existing functions

--- ydb/library/yql/providers/dq/task_runner/tasks_runner_local.cpp
+++ ydb/library/yql/providers/dq/task_runner/tasks_runner_local.cpp
@@ -263,8 +263,8 @@ public:
         auto ctx = ExecutionContext;
         ctx.FuncProvider = TaskTransformFactory(settings.TaskParams, ctx.FuncRegistry);
         return MakeDqTaskRunner(alloc, ctx, settings, [taskId = task.GetId(), traceId = traceId](const TString& message) {
-            YQL_LOG(DEBUG) << "traceId: " << traceId
-                << ", task: " << taskId << ": " << message;
+            YQL_LOG_CTX_ROOT_SESSION_SCOPE(traceId, ToString(taskId));
+            YQL_CLOG(DEBUG, ProviderDq) << message;
         });
     }
 

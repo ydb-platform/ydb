6eb5ba37c3b1a3fa35cd1f149335b016d9393623
Sergey Belyakov <serg-belyakov@ydb.tech>
2024-06-21 16:38:59 +0000

    NBS version

--- ydb/apps/version/version_definition.cpp
+++ ydb/apps/version/version_definition.cpp
@@ -3,13 +3,20 @@
 NKikimrConfig::TCurrentCompatibilityInfo NKikimr::TCompatibilityInfo::MakeCurrent() {
     using TCurrentConstructor = NKikimr::TCompatibilityInfo::TProtoConstructor::TCurrentCompatibilityInfo;
     using TVersionConstructor = NKikimr::TCompatibilityInfo::TProtoConstructor::TVersion;
-    // using TCompatibilityRuleConstructor = NKikimr::TCompatibilityInfo::TProtoConstructor::TCompatibilityRule;
+    using TCompatibilityRuleConstructor = NKikimr::TCompatibilityInfo::TProtoConstructor::TCompatibilityRule;
 
     return TCurrentConstructor{
         .Application = "ydb",
         .Version = TVersionConstructor{
             .Year = 24,
             .Major = 2,
+        },
+        .CanConnectTo = {
+            TCompatibilityRuleConstructor{
+                .Application = "nbs",
+                .LowerLimit = TVersionConstructor{ .Year = 23, .Major = 4 },
+                .UpperLimit = TVersionConstructor{ .Year = 24, .Major = 2 },
+            },
         },
     }.ToPB();
 }

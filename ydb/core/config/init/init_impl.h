b4619675d26aed2542b01f846e19340cf6d68e4d
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-06-21 14:02:53 +0000

    Debug in progress

--- ydb/core/config/init/init_impl.h
+++ ydb/core/config/init/init_impl.h
@@ -492,23 +492,25 @@ struct TCommonAppOptions {
          // YQ-3253: derive Connector endpoint from YDB's Interconnect Port
             std::cout << "HERE 1 " << appConfig.HasQueryServiceConfig() << " " << appConfig.GetQueryServiceConfig().GetGeneric().HasConnector() << " " << InterconnectPort << std::endl;
         if (appConfig.GetQueryServiceConfig().GetGeneric().HasConnector() && InterconnectPort) {
-            std::cout << "HERE 2" << std::endl;
-            auto& connectorConfig = *appConfig.MutableQueryServiceConfig()->MutableConnector();
-
-            auto offset = connectorConfig.GetOffsetFromICPort();
+            std::cout << "HERE 2 " << appConfig.GetQueryServiceConfig().DebugString() << " " << InterconnectPort << std::endl;
+            auto& connectorConfig = *appConfig.MutableQueryServiceConfig()->MutableGeneric()->MutableConnector();
+            std::cout << "HERE 3 " << connectorConfig.DebugString() << std::endl;
+            auto offset = connectorConfig.GetOffsetFromIcPort();
+            std::cout << "HERE 4 " << offset << std::endl;
             if (offset) {
-            std::cout << "HERE 3" << std::endl;
+                std::cout << "HERE 5" << std::endl;
                 connectorConfig.MutableEndpoint()->Setport(InterconnectPort + offset) ;
 
                 // Assign default hostname 'localhost', because 
                 // connector is usually deployed to the same host as the dynamic node.
                 if (connectorConfig.GetEndpoint().host().Empty()) {
-            std::cout << "HERE 4" << std::endl;
+                    std::cout << "HERE 6" << std::endl;
                     connectorConfig.MutableEndpoint()->Sethost("localhost");
                 }
             }
         }
-            std::cout << "HERE 5" << appConfig.GetQueryServiceConfig().GetGeneric().GetConnector().DebugString() << std::endl;
+
+        std::cout << "HERE 7 " << appConfig.GetQueryServiceConfig().GetGeneric().GetConnector().GetEndpoint().DebugString() << std::endl;
 
         if (SuppressVersionCheck) {
             if (appConfig.HasNameserviceConfig()) {

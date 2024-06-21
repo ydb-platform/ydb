f9d1df644ea347af33483319a27d1447db3796ee
kungasc <kungasc@ydb.tech>
2024-06-21 14:15:34 +0000

    clean up

--- ydb/core/tx/datashard/datashard__init.cpp
+++ ydb/core/tx/datashard/datashard__init.cpp
@@ -707,8 +707,6 @@ bool TDataShard::SyncSchemeOnFollower(TTransactionContext &txc, const TActorCont
     bool precharged = true;
     bool updated = false;
     if (FollowerState.LastSysUpdate < lastSysUpdate) {
-        // txc.
-        // txc.DB.GetScheme().GetTableInfo(Schema::Sys::TableId)->PreloadData = true;
         if (!db.Table<Schema::Sys>().Precharge()) {
             precharged = false;
         }

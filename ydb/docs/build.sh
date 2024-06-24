6651f76cb6fd7f689c9d189e63f713fdad4df38d
Ivan Blinkov <ivan@ydb.tech>
2024-06-24 09:17:07 +0700

    Revert changes to build.sh from the last commit

--- ydb/docs/build.sh
+++ ydb/docs/build.sh
@@ -1,46 +1,46 @@
-#!/bin/bash
+# Use this script to build YDB docs with Open Source tools and start HTTP server
+# You may specify output directory as a parameter. If omitted, docs will be generated to a TEMP subdirectory
 
-# Use this script to build YDB docs with open-source tools and start an HTTP server
-# You may specify the output directory as a parameter. If omitted, the docs will be generated to a TEMP subdirectory
+echo Checking YFM installed...
+yfm --version
+if [[ $? -ge 1 ]]; then
+  echo
+  echo "You need to have YFM builder (https://diplodoc.com/docs/en/tools/docs/) installed to run this script, exiting"
+  exit
+fi
 
-set -e
+echo Checking Python3 installed...
+python3 --version
 
-check_dependency() {
-  if ! command -v $1 &> /dev/null; then
-    echo
-    echo "You need to have $2 installed to run this script, exiting"
-    echo "Installation instructions: $3"
-    exit 1
-  fi
-}
-
-start_server() {
+if [[ $? -ge 1 ]]; then
   echo
-  echo "Starting HTTP server, open the links in your browser:"
-  echo
-  echo "- http://localhost:8888/en (English)"
-  echo "- http://localhost:8888/ru (Russian)"
-  echo
-  echo "Press Ctrl+C in this window to stop the HTTP server."
-  echo
-  python3 -m http.server 8888 -d $1
-}
+  echo "You need to have Python3 (https://www.python.org/) installed to run this script, exiting"
+  exit
+fi
 
 DIR=${1:-"$TMPDIR"docs}
 
-check_dependency "yfm" "YFM builder" "https://diplodoc.com/docs/en/tools/docs/"
-check_dependency "python3" "Python3" "https://www.python.org/downloads/"
+echo Starting YFM builder
+echo Output directory: $DIR
 
-echo "Starting YFM builder"
-echo "Output directory: $DIR"
+yfm -i . -o $DIR --allowHTML
 
-if ! yfm -i . -o $DIR --allowHTML; then
+if [[ $? -ge 1 ]]; then
   echo
-  echo "================================"
-  echo "YFM build completed with ERRORS!"
-  echo "================================"
-  exit 1
+  echo ================================
+  echo YFM build completed with ERRORS!
+  echo ================================
 fi
 
-start_server $DIR
+echo
+echo Starting HTTP server, open the links in your browser:
+echo
+echo "- http://localhost:8888/en (English)"
+echo "- http://localhost:8888/ru (Russian)"
+echo
+echo Press Ctrl+C in this window to stop the HTTP server.
+echo
+
+python3 -m http.server 8888 -d $DIR
+
 

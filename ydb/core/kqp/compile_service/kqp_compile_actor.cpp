857be3730f4eedfc9caf361c87cd51531b920589
Gigoriy Pisarenko <grigoriypisar@ydb.tech>
2024-06-24 06:53:54 +0000

    Removed MetadataProviderConfig from compile actor

--- ydb/core/kqp/compile_service/kqp_compile_actor.cpp
+++ ydb/core/kqp/compile_service/kqp_compile_actor.cpp
@@ -174,7 +174,7 @@ private:
         counters->TxProxyMon = new NTxProxy::TTxProxyMon(AppData(ctx)->Counters);
         std::shared_ptr<NYql::IKikimrGateway::IKqpTableMetadataLoader> loader =
             std::make_shared<TKqpTableMetadataLoader>(
-                QueryId.Cluster, TlsActivationContext->ActorSystem(), Config, true, TempTablesState, 2 * TDuration::Seconds(MetadataProviderConfig.GetRefreshPeriodSeconds()));
+                QueryId.Cluster, TlsActivationContext->ActorSystem(), Config, true, TempTablesState);
         Gateway = CreateKikimrIcGateway(QueryId.Cluster, QueryId.Settings.QueryType, QueryId.Database, std::move(loader),
             ctx.ExecutorThread.ActorSystem, ctx.SelfID.NodeId(), counters, QueryServiceConfig);
         Gateway->SetToken(QueryId.Cluster, UserToken);

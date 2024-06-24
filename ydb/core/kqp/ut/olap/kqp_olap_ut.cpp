1e590caf2efbebd7ac0a8b893322a4f930dd0915
Nikita Vasilev <ns-vasilev@ydb.tech>
2024-06-24 16:18:01 +0300

    fix

--- ydb/core/kqp/ut/olap/kqp_olap_ut.cpp
+++ ydb/core/kqp/ut/olap/kqp_olap_ut.cpp
@@ -2662,64 +2662,6 @@ Y_UNIT_TEST_SUITE(KqpOlap) {
 
     }
 
-    Y_UNIT_TEST(TEST_BLOCKS) {
-        NKikimrConfig::TAppConfig appConfig;
-        auto settings = TKikimrSettings()
-            .SetAppConfig(appConfig)
-            .SetWithSampleTables(false);
-        TKikimrRunner kikimr(settings);
-        Tests::NCommon::TLoggerInit(kikimr).Initialize();
-
-        auto session = kikimr.GetTableClient().CreateSession().GetValueSync().GetSession();
-
-        const TString query = R"(
-            CREATE TABLE `/Root/DataShard` (
-                Col1 Uint32 NOT NULL,
-                Col2 String,
-                Col3 Int32 NOT NULL,
-                PRIMARY KEY (Col1)
-            )
-            WITH (
-                AUTO_PARTITIONING_BY_SIZE = DISABLED,
-                AUTO_PARTITIONING_MIN_PARTITIONS_COUNT = 16,
-                AUTO_PARTITIONING_MAX_PARTITIONS_COUNT = 16,
-                UNIFORM_PARTITIONS = 16);
-        )";
-
-        auto result = session.ExecuteSchemeQuery(query).GetValueSync();
-        UNIT_ASSERT_C(result.GetStatus() == NYdb::EStatus::SUCCESS, result.GetIssues().ToString());
-
-        auto client = kikimr.GetQueryClient();
-        {
-            auto prepareResult = client.ExecuteQuery(R"(
-                REPLACE INTO `/Root/DataShard` (Col1, Col2, Col3) VALUES
-                    (10u, "test1", 10), (20u, "test2", 11), (2147483647u, "test3", 12), (2147483640u, NULL, 13);
-            )", NYdb::NQuery::TTxControl::BeginTx().CommitTx()).ExtractValueSync();
-            UNIT_ASSERT_C(prepareResult.IsSuccess(), prepareResult.GetIssues().ToString());
-        }
-
-        {
-            auto it = client.StreamExecuteQuery(R"(
-                SELECT COUNT(*) FROM `/Root/DataShard`;
-            )", NYdb::NQuery::TTxControl::BeginTx().CommitTx()).ExtractValueSync();
-            UNIT_ASSERT_VALUES_EQUAL_C(it.GetStatus(), EStatus::SUCCESS, it.GetIssues().ToString());
-            TString output = StreamResultToYson(it);
-            CompareYson(
-                output,
-                R"([[4u]])");
-        }
-
-        {
-            auto it = client.StreamExecuteQuery(R"(
-                SELECT Col1 FROM `/Root/DataShard` WHERE Col3 = 12;
-            )", NYdb::NQuery::TTxControl::BeginTx().CommitTx()).ExtractValueSync();
-            UNIT_ASSERT_VALUES_EQUAL_C(it.GetStatus(), EStatus::SUCCESS, it.GetIssues().ToString());
-            TString output = StreamResultToYson(it);
-            CompareYson(
-                output,
-                R"([[2147483647u]])");
-        }
-    }
 }
 
 }

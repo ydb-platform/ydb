10d8655dd385fe03395d60abfbb5903fcc87b2a4
alevitskii <alevitskii@yandex-team.com>
2024-06-26 22:29:09 +0300

    Prepare python linting tools for new macroses
    Prepare python linting tools for new macroses
    bcc9d05e166bd25c4e6322bfdba226476ee7c896

--- build/plugins/lib/tests/utils/__init__.py
+++ build/plugins/lib/tests/utils/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 
-def get_ruff_config(path, config_paths, arc_root):
+def select_project_config(path, config_paths, arc_root):
     # type(str, dict, str) -> Optional[str]
     relative_path = os.path.relpath(path, arc_root)
 
@@ -10,8 +10,6 @@ def get_ruff_config(path, config_paths, arc_root):
     for p in config_paths.keys():
         if relative_path.startswith(p) and len(p) > len(deepest_path):
             deepest_path = p
-    if deepest_path:
-        config = config_paths[deepest_path]
-        full_config_path = os.path.join(arc_root, config)
-        return full_config_path
-    return None
+    config = config_paths[deepest_path]
+    full_config_path = os.path.join(arc_root, config)
+    return full_config_path

91ffcb85dbc7f22b8c2f8f030f737e41510d9557
Timur Sufiyanov <fa-luke16@mail.ru>
2024-06-26 11:24:14 +0300

    FQ Proxy: support Greenplum (#5932)
    Co-authored-by: Vitaly Isaev <vitalyisaev@ydb.tech>

--- ydb/core/fq/libs/control_plane_proxy/utils/utils.h
+++ ydb/core/fq/libs/control_plane_proxy/utils/utils.h
@@ -31,6 +31,9 @@ TString ExtractServiceAccountIdWithConnection(const T& setting) {
     case FederatedQuery::ConnectionSetting::kPostgresqlCluster: {
         return GetServiceAccountId(setting.postgresql_cluster().auth());
     }
+    case FederatedQuery::ConnectionSetting::kGreenplumCluster: {
+        return GetServiceAccountId(setting.greenplum_cluster().auth());
+    }
     // Do not replace with default. Adding a new connection should cause a compilation error
     case FederatedQuery::ConnectionSetting::CONNECTION_NOT_SET:
     break;

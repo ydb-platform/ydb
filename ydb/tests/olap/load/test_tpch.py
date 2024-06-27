001802f4727513e7c43bbab9d84f03d5efbd11fa
Oleg Geller <iddqd@ydb.tech>
2024-06-27 16:02:30 +0300

    fix style

--- ydb/tests/olap/load/test_tpch.py
+++ ydb/tests/olap/load/test_tpch.py
@@ -13,7 +13,7 @@ class TpchSuiteBase(LoadSuiteBase):
     @pytest.mark.parametrize('query_num', [i for i in range(1, 23)])
     def test_tpch(self, query_num: int):
         root_path = YdbCluster.tables_path
-        tpch_path = get_external_param(f'table-path-tpch', f'{root_path}/tpch')
+        tpch_path = get_external_param('table-path-tpch', f'{root_path}/tpch')
         path = get_external_param(f'table-path-{self.suite}', f'{tpch_path}/s{self.size}')
         self.run_workload_test(path, query_num)
 

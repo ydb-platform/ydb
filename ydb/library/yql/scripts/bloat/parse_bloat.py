fc8a67fc9649cbcd688da6c74bd1a335b0187e06
Vitaly Stoyan <vvvv@ydb.tech>
2024-07-01 12:50:43 +0000

    with files

--- ydb/library/yql/scripts/bloat/parse_bloat.py
+++ ydb/library/yql/scripts/bloat/parse_bloat.py
@@ -8,6 +8,8 @@ class Walker:
         self.stats = {}
 
     def process(self, node):
+        if node["type"] == "file":
+            self.current_file = node["name"]
         if "children" in node:
             for child in node["children"]:
                 self.process(child)
@@ -31,9 +33,15 @@ class Walker:
                     continue
                 final_name += c
         if final_name not in self.stats:
-            self.stats[final_name] = [0,0]
+            self.stats[final_name] = [0,0,set()]
         self.stats[final_name][0] += node["size"]
         self.stats[final_name][1] += 1
+        self.stats[final_name][2].add(self.current_file)
+
+def print_stat(f, p):
+    print("{} size={} count={}".format(p[0],p[1][0],p[1][1]), file=f)
+    for s in sorted(p[1][2]):
+        print("    " + s, file=f)
 
 def main():
     pgm = sys.argv[1]
@@ -43,10 +51,10 @@ def main():
     walker.process(data["tree"])
     with open(pgm + ".by_size.txt","w") as f:
         for p in sorted(walker.stats.items(), key=lambda p: p[1][0], reverse=True):
-            print(p, file=f)
+            print_stat(f, p)
     with open(pgm + ".by_count.txt","w") as f:
-        for p in sorted(walker.stats.items(), key=lambda p: p[1][1], reverse=True):
-            print(p, file=f)
+        for p in sorted(walker.stats.items(), key=lambda p: p[1][0], reverse=True):
+            print_stat(f, p)
     return 0
 
 if __name__ == "__main__":

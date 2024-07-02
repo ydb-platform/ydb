ydb-platform/ydb

commit 2788bd76137fc2b3951ecf21dbcc06b8f70f5193

Add consts to generator (#5933)

ydb/library/benchmarks/gen_queries/ya.make
@@ -18,6 +18,8 @@ RESOURCE(
     bindings_tpcds_pg.json bindings_ds_pg.json
     bindings_tpch.json bindings_h_yql.json
     bindings_tpch_pg.json bindings_h_pg.json
+    consts.yql consts.yql
+    consts_decimal.yql consts_decimal.yql
 )
 
 RESOURCE(

ydb/library/benchmarks/gen_queries/consts.yql
@@ -1,0 +1,17 @@
+pragma warning("disable", "4527");
+
+$z0 = 0;
+$z1_2 = 1.2;
+$z1_3 = 1.3;
+$z0_9 = 0.9;
+$z0_99 = 0.99;
+$z1_49 = 1.49;
+
+$z0_35 = 0;
+$z0_1_35 = 0.1;
+$z1_2_35 = 1.2;
+$z0_05_35 = 0.05;
+$z0_9_35 = 0.9;
+$z1_1_35 = 1.1;
+
+$round = ($x, $y) -> { return Math::Round($x, $y); };

ydb/library/benchmarks/gen_queries/consts_decimal.yql
@@ -1,0 +1,16 @@
+pragma warning("disable", "4527");
+
+$z0 = cast(0 as decimal(7,2));
+$z1_2 = cast("1.2" as decimal(7,2));
+$z1_3 = cast("1.3" as decimal(7,2));
+$z0_9 = cast("0.9" as decimal(7,2));
+$z0_99 = cast("0.99" as decimal(7,2));
+$z1_49 = cast("1.49" as decimal(7,2));
+
+$z0_35 = cast(0 as decimal(35,2));
+$z0_1_35 = cast("0.1" as decimal(35,2));
+$z1_2_35 = cast("1.2" as decimal(35,2));
+$z0_05_35 = cast("0.05" as decimal(35,2));
+$z0_9_35 = cast("0.9" as decimal(35,2));
+$z1_1_35 = cast("1.1" as decimal(35,2));
+$round = ($x,$y) -> {return $x;};

ydb/library/benchmarks/gen_queries/gen_queries.py
@@ -33,6 +33,7 @@ def main():
     parser.add_argument('--pragma', default=[], action='append', help='custom pragmas')
     parser.add_argument('--table-path-prefix', default=None, help='table path prefix')
     parser.add_argument('--cluster-name', default='hahn', help='YtSaurus cluster name')
+    parser.add_argument('--decimal', default=False)
     args = parser.parse_args()
     profile = None
     for p in profiles:
@@ -72,6 +73,11 @@ def main():
         b.add_link("pragmas.sql", p.pragmas)
     else:
         b.add("pragmas.sql", "")
+    if args.syntax == "yql":
+        if args.decimal:
+            b.add_link("consts.jinja", "consts_decimal.yql")
+        else:
+            b.add_link("consts.jinja", "consts.yql")
     b.add_link("tables.jinja", p.tables)
     queries = None
     if args.variant == "h":

ydb/library/benchmarks/gen_queries/header.sql.jinja
@@ -1,3 +1,4 @@
 {% include 'pragmas.sql' %}
 {% include 'tables.jinja' %}
+{% include 'consts.jinja' %}
 {% include 'custom_pragmas' %}

ydb/library/benchmarks/queries/tpcds/yql/q1.sql
@@ -15,7 +15,7 @@ group by a.sr_customer_sk
 ,a.sr_store_sk);
 
 $avg_total_returns = (
-    select ctr_store_sk, avg(ctr_total_return)*1.2 as ctr_avg
+    select ctr_store_sk, avg(ctr_total_return)*$z1_2_35 as ctr_avg
     from $customer_total_return
     group by ctr_store_sk
 );

ydb/library/benchmarks/queries/tpcds/yql/q11.sql
@@ -75,8 +75,8 @@ select
          and t_w_secyear.dyear = 1999+1
          and t_s_firstyear.year_total > 0
          and t_w_firstyear.year_total > 0
-         and case when t_w_firstyear.year_total > 0 then t_w_secyear.year_total / t_w_firstyear.year_total else 0.0 end
-             > case when t_s_firstyear.year_total > 0 then t_s_secyear.year_total / t_s_firstyear.year_total else 0.0 end
+         and case when t_w_firstyear.year_total > 0 then t_w_secyear.year_total / t_w_firstyear.year_total else $z0_35 end
+             > case when t_s_firstyear.year_total > 0 then t_s_secyear.year_total / t_s_firstyear.year_total else $z0_35 end
  order by t_s_secyear.customer_id
          ,t_s_secyear.customer_first_name
          ,t_s_secyear.customer_last_name

ydb/library/benchmarks/queries/tpcds/yql/q2.sql
@@ -33,13 +33,13 @@ $wswscs =
 select d_week_seq1
 -- NB: Use Math::Round
 -- NB: Negate second argument!!!
-       ,Math::Round(sun_sales1/sun_sales2,-2)
-       ,Math::Round(mon_sales1/mon_sales2,-2)
-       ,Math::Round(tue_sales1/tue_sales2,-2)
-       ,Math::Round(wed_sales1/wed_sales2,-2)
-       ,Math::Round(thu_sales1/thu_sales2,-2)
-       ,Math::Round(fri_sales1/fri_sales2,-2)
-       ,Math::Round(sat_sales1/sat_sales2,-2)
+       ,$round(sun_sales1/sun_sales2,-2)
+       ,$round(mon_sales1/mon_sales2,-2)
+       ,$round(tue_sales1/tue_sales2,-2)
+       ,$round(wed_sales1/wed_sales2,-2)
+       ,$round(thu_sales1/thu_sales2,-2)
+       ,$round(fri_sales1/fri_sales2,-2)
+       ,$round(sat_sales1/sat_sales2,-2)
  from
  (select wswscs.d_week_seq d_week_seq1
         ,sun_sales sun_sales1

commit ea2111962ec0b40f7737f4b83492fba0d25194e5

fix tpc queries generator for syntax pg
Broken by #5933

ydb/library/benchmarks/gen_queries/ya.make
@@ -20,6 +20,7 @@ RESOURCE(
     bindings_tpch_pg.json bindings_h_pg.json
     consts.yql consts.yql
     consts_decimal.yql consts_decimal.yql
+    consts.jinja consts.jinja
 )
 
 RESOURCE(
2847c6764c37d6d29eb9bc99194a1ec43d5ab620
Dmitry Kardymon <kardymon-d@ydb.tech>
2024-07-02 06:07:32 +0000

    Use topic api

--- ydb/tests/fq/pq_async_io/ut_helpers.cpp
+++ ydb/tests/fq/pq_async_io/ut_helpers.cpp
@@ -137,12 +137,12 @@ void PQWrite(
     cfg.SetDatabase(GetDefaultPqDatabase());
     cfg.SetLog(CreateLogBackend("cerr"));
     NYdb::TDriver driver(cfg);
-    NYdb::NPersQueue::TPersQueueClient client(driver);
-    NYdb::NPersQueue::TWriteSessionSettings sessionSettings;
+    NYdb::NTopic::TTopicClient client(driver);
+    NYdb::NTopic::TWriteSessionSettings sessionSettings;
     sessionSettings
         .Path(topic)
         .MessageGroupId("src_id")
-        .Codec(NYdb::NPersQueue::ECodec::RAW);
+        .Codec(NYdb::NTopic::ECodec::RAW);
     auto session = client.CreateSimpleBlockingWriteSession(sessionSettings);
     for (const TString& data : sequence) {
         UNIT_ASSERT_C(session->Write(data), "Failed to write message with body \"" << data << "\" to topic " << topic);
@@ -164,17 +164,16 @@ std::vector<TString> PQReadUntil(
     cfg.SetDatabase(GetDefaultPqDatabase());
     cfg.SetLog(CreateLogBackend("cerr"));
     NYdb::TDriver driver(cfg);
-    NYdb::NPersQueue::TPersQueueClient client(driver);
-    NYdb::NPersQueue::TReadSessionSettings sessionSettings;
+    NYdb::NTopic::TTopicClient client(driver);
+    NYdb::NTopic::TReadSessionSettings sessionSettings;
     sessionSettings
         .AppendTopics(topic)
-        .ConsumerName(DefaultPqConsumer)
-        .DisableClusterDiscovery(true);
+        .ConsumerName(DefaultPqConsumer);
 
     auto promise = NThreading::NewPromise();
     std::vector<TString> result;
 
-    sessionSettings.EventHandlers_.SimpleDataHandlers([&](NYdb::NPersQueue::TReadSessionEvent::TDataReceivedEvent& ev) {
+    sessionSettings.EventHandlers_.SimpleDataHandlers([&](NYdb::NTopic::TReadSessionEvent::TDataReceivedEvent& ev) {
         for (const auto& message : ev.GetMessages()) {
             result.emplace_back(message.GetData());
         }
@@ -183,7 +182,7 @@ std::vector<TString> PQReadUntil(
         }
     }, false, false);
 
-    std::shared_ptr<NYdb::NPersQueue::IReadSession> session = client.CreateReadSession(sessionSettings);
+    std::shared_ptr<NYdb::NTopic::IReadSession> session = client.CreateReadSession(sessionSettings);
     UNIT_ASSERT(promise.GetFuture().Wait(timeout));
     session->Close(TDuration::Zero());
     session = nullptr;
@@ -213,20 +212,18 @@ void PQCreateStream(const TString& streamName)
 }
 
 void AddReadRule(NYdb::TDriver& driver, const TString& streamName) {
-    NYdb::NPersQueue::TPersQueueClient client(driver);
+    NYdb::NTopic::TTopicClient client(driver);
 
-    auto result = client.AddReadRule(
-            streamName,
-            NYdb::NPersQueue::TAddReadRuleSettings()
-                .ReadRule(
-                    NYdb::NPersQueue::TReadRuleSettings()
-                        .ConsumerName(DefaultPqConsumer)
-                        .ServiceType("yandex-query")
-                        .SupportedCodecs({
-                            NYdb::NPersQueue::ECodec::RAW
-                        })
-                )
-        ).ExtractValueSync();
+   auto alterTopicSettings =
+        NYdb::NTopic::TAlterTopicSettings()
+            .BeginAddConsumer(DefaultPqConsumer)
+            .SetSupportedCodecs(
+                {
+                    NYdb::NTopic::ECodec::RAW
+                })
+            .EndAddConsumer();
+    auto result = client.AlterTopic(streamName, alterTopicSettings).ExtractValueSync();
+
     UNIT_ASSERT_VALUES_EQUAL_C(result.GetStatus(), NYdb::EStatus::SUCCESS, result.GetIssues().ToString());
     UNIT_ASSERT_VALUES_EQUAL(result.IsTransportError(), false);
 }

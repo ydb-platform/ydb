bc07740ae6d8d18b372108cd2f5851164f035bfe
azevaykin <145343289+azevaykin@users.noreply.github.com>
2024-07-02 13:45:58 +0300

    Big datetime in backup/restore CLI  (#6095)

--- ydb/library/backup/backup.cpp
+++ ydb/library/backup/backup.cpp
@@ -134,6 +134,10 @@ void PrintPrimitive(IOutputStream& out, const TValueParser& parser) {
         CASE_PRINT_PRIMITIVE_TYPE(out, Datetime);
         CASE_PRINT_PRIMITIVE_TYPE(out, Timestamp);
         CASE_PRINT_PRIMITIVE_TYPE(out, Interval);
+        CASE_PRINT_PRIMITIVE_TYPE(out, Date32);
+        CASE_PRINT_PRIMITIVE_TYPE(out, Datetime64);
+        CASE_PRINT_PRIMITIVE_TYPE(out, Timestamp64);
+        CASE_PRINT_PRIMITIVE_TYPE(out, Interval64);
         CASE_PRINT_PRIMITIVE_STRING_TYPE(out, TzDate);
         CASE_PRINT_PRIMITIVE_STRING_TYPE(out, TzDatetime);
         CASE_PRINT_PRIMITIVE_STRING_TYPE(out, TzTimestamp);

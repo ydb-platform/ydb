2f37457f15c0fe977b9996a1e9546c15778dae42
Kirill Rysin <naspirato@gmail.com>
2024-07-07 22:51:57 +0000

    new columns

--- .github/scripts/upload_tests_results.py
+++ .github/scripts/upload_tests_results.py
@@ -58,7 +58,7 @@ def parse_test_name(name):
     return rawname, filename, testname, fixture
 
 
-def parse_junit_xml(xml_file, build_type, job_name, commit, pull, run_time):
+def parse_junit_xml(xml_file, build_type, job_name, job_id, commit, branch, pull, run_time):
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
@@ -85,14 +85,16 @@ def parse_junit_xml(xml_file, build_type, job_name, commit, pull, run_time):
             elif testcase.find("skipped") is not None:
                 status = "skipped"
                 status_description = testcase.find("skipped").text
-
+                
             results.append(
                 {
                     "build_type": build_type, 
                     "commit": commit,
+                    "branch": branch,
                     "pull": pull,
                     "run_time": run_time,
                     "job_name": job_name,
+                    "job_id": job_id,
                     "suite_name": suite_name,
                     "raw_test_name": rawname,
                     "file": filename,
@@ -100,7 +102,11 @@ def parse_junit_xml(xml_file, build_type, job_name, commit, pull, run_time):
                     "fixture": fixture,
                     "time": float(time),
                     "status": status,
-                    "status_description": status_description.replace("\r\n", ";;").replace("\n", ";;").replace("\"", "'")
+                    "status_description": status_description.replace("\r\n", ";;").replace("\n", ";;").replace("\"", "'"),
+                    "log": "" if testcase.find("properties/property/[@name='url:log']") is None else testcase.find("properties/property/[@name='url:log']").get('value'),
+                    "logsdir": "" if testcase.find("properties/property/[@name='url:logsdir']") is None else testcase.find("properties/property/[@name='url:logsdir']").get('value'),
+                    "stderr": "" if testcase.find("properties/property/[@name='url:stderr']") is None else testcase.find("properties/property/[@name='url:stderr']").get('value'),
+                    "stdout": "" if testcase.find("properties/property/[@name='url:stdout']") is None else testcase.find("properties/property/[@name='url:stdout']").get('value'),
 
                 }
             )
@@ -166,7 +172,9 @@ def create_tests_table(session, table_path):
     CREATE TABLE IF NOT EXISTS `{table_path}` (
         build_type Utf8,
         job_name Utf8,
+        job_id Int32,
         commit Utf8,
+        branch Utf8,
         pull Utf8,
         run_time Timestamp,
         test_id Utf8,
@@ -179,6 +187,10 @@ def create_tests_table(session, table_path):
         status Utf8,
         status_description Utf8,
         owners Utf8,
+        log Utf8,
+        logsdir Utf8,
+        stderr Utf8,
+        stdout Utf8,
         PRIMARY KEY(test_id)
     );
     """
@@ -206,7 +218,28 @@ def prepare_and_upload_tests(pool, path, results, batch_size):
             batch = results[start:end]
 
             sql = f"""--!syntax_v1
-             UPSERT INTO `{path}` (build_type, job_name, commit, pull, run_time, test_id, suite_name, raw_test_name, file, test_name, fixture, time, status, status_description, owners) VALUES"""
+            UPSERT INTO `{path}` 
+            (build_type,
+            job_name,
+            job_id,
+            commit,
+            branch,
+            pull, 
+            run_time, 
+            test_id, 
+            suite_name, 
+            raw_test_name, 
+            file, 
+            test_name, 
+            fixture, 
+            time, 
+            status,
+            status_description,
+            log,
+            logsdir,
+            stderr,
+            stdout,
+            owners) VALUES"""
             values = []
             for index, result in enumerate(batch):
 
@@ -214,7 +247,9 @@ def prepare_and_upload_tests(pool, path, results, batch_size):
                     f"""
                 ("{result['build_type']}", 
                 "{result['job_name']}", 
+                {result['job_id']}, 
                 "{result['commit']}",
+                "{result['branch']}",
                 "{result['pull']}", 
                 DateTime::FromSeconds({result['run_time']}),
                 "{result['pull']}_{result['run_time']}_{start+index}",
@@ -226,6 +261,10 @@ def prepare_and_upload_tests(pool, path, results, batch_size):
                 {result['time']},
                 "{result['status']}",
                 "{result['status_description']}",
+                "{result['log']}",
+                "{result['logsdir']}",
+                "{result['stderr']}",
+                "{result['stdout']}",
                 "{result['owners']}")
                 """
                 )
@@ -256,21 +295,25 @@ def main():
     parser.add_argument('--result-file', action='store', dest="result_file", required=True, help='XML with results of tests')
     parser.add_argument('--build-type', action='store', dest="build_type", required=True, help='build type')
     parser.add_argument('--commit', default='store', dest="commit", required=True, help='commit sha')
+    parser.add_argument('--branch', default='store', dest="branch", required=True, help='branch name ')
     parser.add_argument('--pull', action='store', dest="pull",required=True, help='pull number')
     parser.add_argument('--run-time', action='store', dest="run_time",required=True, help='time of test run start')
     parser.add_argument('--job-name', action='store', dest="job_name",required=True, help='job name where launched')
+    parser.add_argument('--job-id', action='store', dest="job_id",required=True, help='job id of workflow')
 
     args = parser.parse_args()
 
     result_file=args.result_file
     build_type=args.build_type
     commit=args.commit
+    branch=args.branch
     pull=args.pull
     run_time=args.run_time
     job_name=args.job_name
+    job_id=args.job_id
 
     path_in_database = "test_results"
-    batch_size_default=50
+    batch_size_default=30
     dir = os.path.dirname(__file__)
     git_root = f"{dir}/../.."
     codeowners = f"{git_root}/.github/CODEOWNERS"
@@ -285,8 +328,7 @@ def main():
         print(
             "Error: Env variable CI_YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS is missing, skipping"
         )
-        #return 0
-        os.environ["YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS"]="/home/kirrysin/fork/ydb/.github/scripts/my-robot-key.json"
+        return 0
     else:
         # Do not set up 'real' variable from gh workflows because it interfere with ydb tests
         # So, set up it locally
@@ -294,15 +336,16 @@ def main():
             "CI_YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS"
         ]
   
+    test_table_name= f"{path_in_database}/{job_name.lower().replace('-','_')}"
     #Prepare connection and table
     pool = create_pool(DATABASE_ENDPOINT, DATABASE_PATH)#, token)
     with pool.checkout() as session:
-        create_tests_table(session, f"{path_in_database}/tests")
+        create_tests_table(session, test_table_name)
 
     # Parse and upload
-    results = parse_junit_xml(result_file, build_type, job_name, commit, pull, run_time)
+    results = parse_junit_xml(result_file, build_type, job_name, job_id, commit, branch, pull, run_time)
     result_with_owners = get_codeowners_for_tests(parse_codeowners(codeowners), results)
-    prepare_and_upload_tests(pool, f"{path_in_database}/tests", results, batch_size=batch_size_default)
+    prepare_and_upload_tests(pool, test_table_name, results, batch_size=batch_size_default)
 
 
 

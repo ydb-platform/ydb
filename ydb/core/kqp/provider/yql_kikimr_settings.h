0c72d379633727d08eb971664b87f91d72ada941
Pavel Velikhov <pavelvelikhov@ydb.tech>
2024-07-10 14:41:54 +0300

    Added table service flag for CBO (#6493)

--- ydb/core/kqp/provider/yql_kikimr_settings.h
+++ ydb/core/kqp/provider/yql_kikimr_settings.h
@@ -168,6 +168,7 @@ struct TKikimrConfiguration : public TKikimrSettings, public NCommon::TSettingDi
     bool EnableOltpSink = false;
     NKikimrConfig::TTableServiceConfig_EBlockChannelsMode BlockChannelsMode;
     bool EnableSpillingGenericQuery = false;
+    ui32 DefaultCostBasedOptimizationLevel = 3;
 };
 
 }

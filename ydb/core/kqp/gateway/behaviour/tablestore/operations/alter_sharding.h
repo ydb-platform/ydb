ab6c34f4eea3769c342c28edda367b04b6a9eb15
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-07-12 17:13:20 +0300

    fix build

--- ydb/core/kqp/gateway/behaviour/tablestore/operations/alter_sharding.h
+++ ydb/core/kqp/gateway/behaviour/tablestore/operations/alter_sharding.h
@@ -11,9 +11,7 @@ private:
     static inline const auto Registrator = TFactory::TRegistrator<TAlterShardingOperation>(GetTypeName());
 private:
     std::optional<bool> Increase;
-    virtual void DoSerializeScheme(NKikimrSchemeOp::TAlterColumnTableSchema& /*scheme*/) const override {
-        AFL_VERIFY(false);
-    }
+    virtual void DoSerializeScheme(NKikimrSchemeOp::TAlterColumnTableSchema& /*scheme*/) const override;
     virtual void DoSerializeScheme(NKikimrSchemeOp::TModifyScheme& scheme, const bool isStandalone) const override;
 
 public:

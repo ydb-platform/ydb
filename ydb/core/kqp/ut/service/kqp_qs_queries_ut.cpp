2a9697a8e4f1d9d5a46380a637d16408b829726b
Nikita Vasilev <ns-vasilev@ydb.tech>
2024-07-15 19:22:25 +0300

    fix

--- ydb/core/kqp/ut/service/kqp_qs_queries_ut.cpp
+++ ydb/core/kqp/ut/service/kqp_qs_queries_ut.cpp
@@ -2598,7 +2598,7 @@ Y_UNIT_TEST_SUITE(KqpQueryService) {
                 SELECT * FROM Table1 ORDER BY Key;
                 SELECT * FROM Table2 ORDER BY Key;
                 SELECT * FROM Table3 ORDER BY Key;
-            )", TTxControl::NoTx(), TExecuteQuerySettings().ClientTimeout(TDuration::MilliSeconds(500))).ExtractValueSync();
+            )", TTxControl::NoTx(), TExecuteQuerySettings()).ExtractValueSync();
             UNIT_ASSERT_VALUES_EQUAL_C(result.GetStatus(), EStatus::SUCCESS, result.GetIssues().ToString());
             UNIT_ASSERT_VALUES_EQUAL(result.GetResultSets().size(), 3);
             // Results are empty. Snapshot was taken before tables were created, so we don't see changes after snapshot.

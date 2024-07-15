4c011052c68ee0eaa4ffe499dfac94a32654f2c8
Yuriy Kaminskiy <yumkam7@ydb.tech>
2024-07-15 15:32:13 +0300

    grace_join_ut: add test simulating batch mode

--- ydb/library/yql/minikql/comp_nodes/ut/mkql_grace_join_ut.cpp
+++ ydb/library/yql/minikql/comp_nodes/ut/mkql_grace_join_ut.cpp
@@ -189,6 +189,10 @@ Y_UNIT_TEST_SUITE(TMiniKQLGraceJoinMemTest) {
 
 
 Y_UNIT_TEST_SUITE(TMiniKQLGraceJoinImpTest) {
+    constexpr ui64 BigTableTuples = 600000;
+    constexpr ui64 SmallTableTuples = 150000;
+    constexpr ui64 BigTupleSize = 40;
+
     Y_UNIT_TEST_LLVM(TestImp1) {
             TSetup<LLVM> setup;
             ui64 tuple[11] = {0,1,2,3,4,5,6,7,8,9,10};
@@ -224,10 +228,6 @@ Y_UNIT_TEST_SUITE(TMiniKQLGraceJoinImpTest) {
             ui64 milliseconds = 0;
 
 
-
-            const ui64 BigTableTuples = 600000;
-            const ui64 SmallTableTuples = 150000;
-            const ui64 BigTupleSize = 40;
 
             std::uniform_int_distribution<ui64> smallDist(0, SmallTableTuples - 1);
 
@@ -360,6 +360,189 @@ Y_UNIT_TEST_SUITE(TMiniKQLGraceJoinImpTest) {
 
 
             std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            CTEST << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << Endl;
+            CTEST << Endl;
+
+
+    }
+
+    Y_UNIT_TEST_LLVM(TestImp1Batch) {
+            TSetup<LLVM> setup;
+            ui64 tuple[11] = {0,1,2,3,4,5,6,7,8,9,10};
+            ui32 strSizes[2] = {4, 4};
+            char * strVals[] = {(char *)"aaaaa", (char *)"bbbb"};
+
+            char * bigStrVal[] = {(char *)"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                                 (char *)"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"};
+            ui32 bigStrSize[2] = {151, 151};
+
+
+            NMemInfo::TMemInfo mi = NMemInfo::GetMemInfo();
+            CTEST << "Mem usage before tables tuples added (MB): " << mi.RSS / (1024 * 1024) << Endl;
+
+            GraceJoin::TTable bigTable(1,1,1,1);
+            GraceJoin::TTable smallTable(1,1,1,1);
+            GraceJoin::TTable joinTable(1,1,1,1);
+
+            std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+            const ui64 TupleSize = 1024;
+
+            ui64 bigTuple[TupleSize];
+
+            std::mt19937_64 rng; // deterministic PRNG
+
+            std::uniform_int_distribution<ui64> dist(0, 10000 - 1);
+
+            for (ui64 i = 0; i < TupleSize; i++) {
+                bigTuple[i] = dist(rng);
+            }
+
+            ui64 millisecondsAdd = 0;
+            ui64 millisecondsJoin = 0;
+            ui64 millisecondsNextJoinTuple = 0;
+            ui64 millisecondsNextTuple = 0;
+
+
+
+            const ui64 BatchTuples = 100000;
+
+            std::uniform_int_distribution<ui64> smallDist(0, SmallTableTuples - 1);
+
+            {
+                std::chrono::steady_clock::time_point begin03 = std::chrono::steady_clock::now();
+
+                smallTable.AddTuple(tuple, bigStrVal, bigStrSize);
+
+                for ( ui64 i = 0; i < SmallTableTuples + 1; i++) {
+                    tuple[1] = smallDist(rng);
+                    tuple[2] = tuple[1];
+                    smallTable.AddTuple(tuple, strVals, strSizes);
+                }
+
+                std::chrono::steady_clock::time_point end03 = std::chrono::steady_clock::now();
+                millisecondsAdd += std::chrono::duration_cast<std::chrono::milliseconds>(end03 - begin03).count();
+            }
+
+
+            for ( ui64 pos = 0; pos < BigTableTuples; ) {
+                std::chrono::steady_clock::time_point begin03 = std::chrono::steady_clock::now();
+                ui64 limit = std::min(pos + BatchTuples, BigTableTuples);
+                for (; pos < limit; ++pos) {
+                    tuple[1] = smallDist(rng);
+                    tuple[2] = tuple[1];
+                    bigTable.AddTuple(tuple, strVals, strSizes);
+                }
+                bigTable.Clear();
+                std::chrono::steady_clock::time_point end03 = std::chrono::steady_clock::now();
+                millisecondsAdd += std::chrono::duration_cast<std::chrono::milliseconds>(end03 - begin03).count();
+            }
+
+            CTEST << "Time for hash = " << millisecondsAdd << "[ms]" << Endl;
+            CTEST << "Adding tuples speed: " << (BigTupleSize * (BigTableTuples + SmallTableTuples) * 1000) / ( millisecondsAdd * 1024 * 1024) << "MB/sec" << Endl;
+            CTEST << Endl;
+
+            mi = NMemInfo::GetMemInfo();
+            CTEST << "Mem usage after tables tuples added (MB): " << mi.RSS / (1024 * 1024) << Endl;
+            millisecondsAdd = 0;
+
+            smallTable.Clear();
+
+            {
+                auto begin03 = std::chrono::steady_clock::now();
+
+                smallTable.AddTuple(tuple, bigStrVal, bigStrSize);
+
+                for ( ui64 i = 0; i < SmallTableTuples + 1; i++) {
+                    tuple[1] = smallDist(rng);
+                    tuple[2] = tuple[1];
+                    smallTable.AddTuple(tuple, strVals, strSizes);
+                }
+
+                auto end03 = std::chrono::steady_clock::now();
+                millisecondsAdd += std::chrono::duration_cast<std::chrono::milliseconds>(end03 - begin03).count();
+
+            }
+            std::vector<ui64> vals1, vals2;
+            std::vector<char *> strVals1, strVals2;
+            std::vector<ui32> strSizes1, strSizes2;
+            GraceJoin::TupleData td1, td2;
+            vals1.resize(100);
+            vals2.resize(100);
+            strVals1.resize(100);
+            strVals2.resize(100);
+            strSizes1.resize(100);
+            strSizes2.resize(100);
+            td1.IntColumns = vals1.data();
+            td1.StrColumns = strVals1.data();
+            td1.StrSizes = strSizes1.data();
+            td2.IntColumns = vals2.data();
+            td2.StrColumns = strVals2.data();
+            td2.StrSizes = strSizes2.data();
+
+            ui64 numJoinedTuples = 0;
+            ui64 numBigTuples = 0;
+
+            for ( ui64 pos = 0; pos < BigTableTuples; ) {
+                std::chrono::steady_clock::time_point begin03 = std::chrono::steady_clock::now();
+                bigTable.Clear();
+                ui64 limit = std::min(pos + BatchTuples, BigTableTuples);
+                for (; pos < limit; ++pos) {
+                    tuple[1] = smallDist(rng);
+                    tuple[2] = tuple[1];
+                    bigTable.AddTuple(tuple, strVals, strSizes);
+                }
+                auto end03 = std::chrono::steady_clock::now();
+                millisecondsAdd += std::chrono::duration_cast<std::chrono::milliseconds>(end03 - begin03).count();
+
+                bigTable.ResetIterator();
+
+                std::chrono::steady_clock::time_point begin04 = std::chrono::steady_clock::now();
+
+                while(bigTable.NextTuple(td1)) { numBigTuples++; }
+                std::chrono::steady_clock::time_point end04 = std::chrono::steady_clock::now();
+
+                millisecondsNextTuple += std::chrono::duration_cast<std::chrono::milliseconds>(end04 - begin04).count();
+
+
+                std::chrono::steady_clock::time_point begin05 = std::chrono::steady_clock::now();
+
+                joinTable.Join(smallTable, bigTable, EJoinKind::Inner, false, pos < BigTableTuples);
+
+                std::chrono::steady_clock::time_point end05 = std::chrono::steady_clock::now();
+                millisecondsJoin += std::chrono::duration_cast<std::chrono::milliseconds>(end05 - begin05).count();
+
+
+                joinTable.ResetIterator();
+
+
+                std::chrono::steady_clock::time_point begin042 = std::chrono::steady_clock::now();
+                while(joinTable.NextJoinedData(td1, td2)) { numJoinedTuples++; }
+
+                std::chrono::steady_clock::time_point end042 = std::chrono::steady_clock::now();
+                millisecondsNextJoinTuple += std::chrono::duration_cast<std::chrono::milliseconds>(end042 - begin042).count();
+            }
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+            CTEST << "Num of big tuples 1: " << numBigTuples << Endl;
+
+            CTEST << "Time for get 1 = " << millisecondsNextTuple << "[ms]" << Endl;
+            CTEST << Endl;
+            CTEST << "Time for join = " << millisecondsJoin << "[ms]" << Endl;
+            CTEST << Endl;
+            CTEST << "Time for get joined tuples: = " << millisecondsNextJoinTuple << "[ms]" << Endl;
+            CTEST << Endl;
+
+            mi = NMemInfo::GetMemInfo();
+            CTEST << "Mem usage after tables add and join (MB): " << mi.RSS / (1024 * 1024) << Endl;
+            CTEST << "Time for hash = " << millisecondsAdd << "[ms]" << Endl;
+            CTEST << "Adding tuples speed: " << (BigTupleSize * (BigTableTuples + SmallTableTuples) * 1000) / ( millisecondsAdd * 1024 * 1024) << "MB/sec" << Endl;
+            CTEST << Endl;
+
+            CTEST << "Num of joined tuples : " << numJoinedTuples << Endl;
+
+
+
             CTEST << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << Endl;
             CTEST << Endl;
 

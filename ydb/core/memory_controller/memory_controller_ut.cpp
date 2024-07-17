8a6cef1039d4bb6ffd3a96a9ddee84cb1b1bc680
kungasc <kungasc@ydb.tech>
2024-07-17 11:28:18 +0000

    recurse for

--- ydb/core/memory_controller/memory_controller_ut.cpp
+++ ydb/core/memory_controller/memory_controller_ut.cpp
@@ -1,11 +1,53 @@
 #include <library/cpp/testing/unittest/registar.h>
+#include <library/cpp/testing/unittest/tests_data.h>
+#include <ydb/core/tablet_flat/shared_sausagecache.h>
+#include <ydb/core/tx/datashard/ut_common/datashard_ut_common.h>
+#include <ydb/library/actors/testlib/test_runtime.h>
 
 namespace NKikimr::NMemory {
 
+using namespace Tests;
+
+namespace {
+    void UpsertRows(TServer::TPtr server, TActorId sender, ui32 keyFrom = 0, ui32 keyTo = 2000) {
+        TString query = "UPSERT INTO `/Root/table-1` (key, value) VALUES ";
+        for (auto key : xrange(keyFrom, keyTo)) {
+            if (key != keyFrom)
+                query += ", ";
+            query += "(" + ToString(key) + ", " + ToString(key) + ") ";
+        }
+        ExecSQL(server, sender, query);
+    }
+}
+
 Y_UNIT_TEST_SUITE(TMemoryController) {
 
-Y_UNIT_TEST(Basics) {
-    Cerr << "hello" << Endl;
+Y_UNIT_TEST(SharedCache) {
+    TPortManager pm;
+    TServerSettings serverSettings(pm.GetPort(2134));
+    serverSettings.SetDomainName("Root")
+        .SetUseRealThreads(false);
+
+    TIntrusivePtr<TServer> server = new TServer(serverSettings);
+    auto& runtime = *server->GetRuntime();
+    auto sender = runtime.AllocateEdgeActor();
+
+    auto sharedCacheCounters = MakeIntrusive<TSharedPageCacheCounters>(runtime.GetDynamicCounters());
+
+    runtime.SetLogPriority(NKikimrServices::MEMORY_CONTROLLER, NLog::PRI_TRACE);
+    runtime.SetLogPriority(NKikimrServices::TABLET_SAUSAGECACHE, NLog::PRI_TRACE);
+
+    InitRoot(server, sender);
+
+    auto [shards, tableId1] = CreateShardedTable(server, sender, "/Root", "table-1", 1);
+    ui64 shard1 = shards.at(0);
+
+    UpsertRows(server, sender);
+
+    CompactTable(runtime, shard1, tableId1, false);
+
+    Cerr << "ActiveBytes = " << sharedCacheCounters->ActiveBytes->Val() << " PassiveBytes = " << sharedCacheCounters->PassiveBytes->Val() << Endl;
+    Cerr << "ConfigLimitBytes = " << sharedCacheCounters->ConfigLimitBytes->Val() << " MemLimitBytes = " << sharedCacheCounters->MemLimitBytes->Val() << Endl;
 }
 
 }
--- ydb/core/memory_controller/ut/ya.make
+++ ydb/core/memory_controller/ut/ya.make
@@ -8,6 +8,7 @@ PEERDIR(
     ydb/library/yql/sql/pg_dummy
     ydb/core/testlib
     ydb/core/tx/datashard/ut_common
+    ydb/core/tablet_flat
     library/cpp/testing/unittest
 )
 

cb3a122c6bea9fe8df25d17cdeeee92245173ad9
Semyon Yentsov <yentsovsemyon@ydb.tech>
2024-07-19 10:36:19 +0000

    add WaitTabletDeletionInHive()

--- ydb/core/kqp/ut/scheme/kqp_scheme_ut.cpp
+++ ydb/core/kqp/ut/scheme/kqp_scheme_ut.cpp
@@ -6425,10 +6425,7 @@ Y_UNIT_TEST_SUITE(KqpOlapScheme) {
         }
         testHelper.DropTable("/Root/ColumnTableTest");
         for (auto tablet: tabletIds) {
-            auto deadline = TInstant::Now() + TDuration::Seconds(1);
-            while (testHelper.GetKikimr().GetTestClient().TabletExistsInHive(&testHelper.GetRuntime(), tablet) && deadline >= TInstant::Now()) {
-                // do nothing
-            }
+            testHelper.WaitTabletDeletionInHive(tablet, TDuration::Seconds(5));
             UNIT_ASSERT_C(!testHelper.GetKikimr().GetTestClient().TabletExistsInHive(&testHelper.GetRuntime(), tablet), ToString(tablet) + " is alive");
         }
     }

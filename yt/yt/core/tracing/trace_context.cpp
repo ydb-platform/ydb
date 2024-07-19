cc6fbb0ddda7fdd63033840536eea785946b40d9
nadya02 <nadya02@yandex-team.com>
2024-07-19 09:53:06 +0300

    Set Allocation tags in TTraceContext::CreateChild method
    fix
    ce0b0af208c8f662a664cb99f5b3fead5245323a

--- yt/yt/core/tracing/trace_context.cpp
+++ yt/yt/core/tracing/trace_context.cpp
@@ -377,6 +377,7 @@ TTraceContextPtr TTraceContext::CreateChild(
     auto guard = Guard(Lock_);
     child->ProfilingTags_ = ProfilingTags_;
     child->TargetEndpoint_ = TargetEndpoint_;
+    child->AllocationTags_ = AllocationTags_;
     return child;
 }
 

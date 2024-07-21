affb5be3e3528088223b96ba2cf88edb9f82e1bd
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2024-07-21 11:16:27 +0000

    build fail

--- ydb/apps/pgwire/main.cpp
+++ ydb/apps/pgwire/main.cpp
@@ -1,6 +1,9 @@
 #include "pgwire.h"
 
+void unknown_foo();
+
 int main(int argc, char **argv) {
+    unknown_foo();
     try {
         return NPGW::TPgWire(argc, argv).Run();
     } catch (const yexception& e) {

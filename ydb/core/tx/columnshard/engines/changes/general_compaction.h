831f41c901b891fa15174730688bf1bdd47fbc99
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-07-21 17:33:41 +0300

    unify indexation (for committed data) and compaction

--- ydb/core/tx/columnshard/engines/changes/general_compaction.h
+++ ydb/core/tx/columnshard/engines/changes/general_compaction.h
@@ -10,7 +10,6 @@ private:
     using TBase = TCompactColumnEngineChanges;
     virtual void DoWriteIndexOnComplete(NColumnShard::TColumnShard* self, TWriteIndexCompleteContext& context) override;
     std::map<NArrow::NMerger::TSortableBatchPosition, bool> CheckPoints;
-    void BuildAppendedPortionsByFullBatches(TConstructionContext& context, std::vector<TReadPortionInfoWithBlobs>&& portions) noexcept;
     void BuildAppendedPortionsByChunks(TConstructionContext& context, std::vector<TReadPortionInfoWithBlobs>&& portions) noexcept;
 
     std::shared_ptr<NArrow::TColumnFilter> BuildPortionFilter(const std::optional<NKikimr::NOlap::TGranuleShardingInfo>& shardingActual,

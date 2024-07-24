0999099e6a6cf1ce7fe8aefa42256ff5fc2af1e2
Andrei Molotkov <molotkov-and@ydb.tech>
2024-07-24 08:22:41 +0000

    Add tests to check disabled builtin login

--- ydb/core/tx/schemeshard/ut_login/ut_login.cpp
+++ ydb/core/tx/schemeshard/ut_login/ut_login.cpp
@@ -11,7 +11,6 @@ Y_UNIT_TEST_SUITE(TSchemeShardLoginTest) {
         // AppData()->AuthConfig.SetUseInternalLoginMethod(true);
         TTestBasicRuntime runtime;
         TTestEnv env(runtime);
-        runtime.GetAppData().AuthConfig.SetUseInternalLoginMethod(false);
         ui64 txId = 100;
         TActorId sender = runtime.AllocateEdgeActor();
         std::unique_ptr<TEvSchemeShard::TEvModifySchemeTransaction> transaction(CreateAlterLoginCreateUser(++txId, "user1", "password1"));
@@ -30,5 +29,24 @@ Y_UNIT_TEST_SUITE(TSchemeShardLoginTest) {
         login.UpdateSecurityState(describe.GetPathDescription().GetDomainDescription().GetSecurityState());
         auto resultValidate = login.ValidateToken({.Token = resultLogin.token()});
         UNIT_ASSERT_VALUES_EQUAL(resultValidate.User, "user1");
+    }
+
+    Y_UNIT_TEST(UnavailableLoginOverBuiltinMechanism) {
+        TTestBasicRuntime runtime;
+        TTestEnv env(runtime);
+        runtime.GetAppData().AuthConfig.SetUseInternalLoginMethod(false);
+        ui64 txId = 100;
+        TActorId sender = runtime.AllocateEdgeActor();
+        std::unique_ptr<TEvSchemeShard::TEvModifySchemeTransaction> transaction(CreateAlterLoginCreateUser(++txId, "user1", "password1"));
+        transaction->Record.MutableTransaction(0)->SetWorkingDir("/MyRoot");
+        ForwardToTablet(runtime, TTestTxConfig::SchemeShard, sender, transaction.release());
+        auto resultLogin = Login(runtime, "user1", "password1");
+        UNIT_ASSERT_VALUES_EQUAL(resultLogin.error(), "Builtin user registry has been disabled in the cluster settings");
+        UNIT_ASSERT_VALUES_EQUAL(resultLogin.token(), "");
+        auto describe = DescribePath(runtime, TTestTxConfig::SchemeShard, "/MyRoot");
+        UNIT_ASSERT(describe.HasPathDescription());
+        UNIT_ASSERT(describe.GetPathDescription().HasDomainDescription());
+        UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().HasSecurityState());
+        UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().GetSecurityState().PublicKeysSize() > 0);
     }
 }

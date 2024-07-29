157f7d8388d66f46dadedb6d4c4730c3c65e9b1b
Kirill Rysin <naspirato@gmail.com>
2024-07-29 16:05:06 +0000

    for roll back

--- ydb/apps/ydb/main.cpp
+++ ydb/apps/ydb/main.cpp
@@ -20,6 +20,7 @@ int main(int argc, char **argv) {
     }
     catch (const NYdb::NConsoleClient::TYdbErrorException& e) {
         Cerr << e;
+        
         return EXIT_FAILURE;
     }
     catch (const yexception& e) {

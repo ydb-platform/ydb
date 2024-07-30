5ead379fac334f64891a48dbb82be3f64a8b02ca
azevaykin <145343289+azevaykin@users.noreply.github.com>
2024-07-30 15:44:31 +0300

    Refactoring statistics: scan -> traversal (#7210)

---
+++ ydb/core/statistics/aggregator/tx_analyze_table.cpp
@@ -0,0 +1,68 @@
+#include "aggregator_impl.h"
+
+#include <ydb/core/tx/datashard/datashard.h>
+
+namespace NKikimr::NStat {
+
+struct TStatisticsAggregator::TTxAnalyzeTable : public TTxBase {
+    TPathId PathId;
+    TActorId ReplyToActorId;
+    ui64 OperationId = 0;
+
+    TTxAnalyzeTable(TSelf* self, const TPathId& pathId, TActorId replyToActorId)
+        : TTxBase(self)
+        , PathId(pathId)
+        , ReplyToActorId(replyToActorId)
+    {}
+
+    TTxType GetTxType() const override { return TXTYPE_ANALYZE_TABLE; }
+
+    bool Execute(TTransactionContext& txc, const TActorContext&) override {
+        SA_LOG_D("[" << Self->TabletID() << "] TTxAnalyzeTable::Execute");
+
+        if (!Self->EnableColumnStatistics) {
+            return true;
+        }
+
+        auto itOp = Self->ForceTraversalsByPathId.find(PathId);
+        if (itOp != Self->ForceTraversalsByPathId.end()) {
+            itOp->second.ReplyToActorIds.insert(ReplyToActorId);
+            OperationId = itOp->second.OperationId;
+            return true;
+        }
+
+        NIceDb::TNiceDb db(txc.DB);
+
+        TForceTraversal& operation = Self->ForceTraversalsByPathId[PathId];
+        operation.PathId = PathId;
+        operation.OperationId = ++Self->LastForceTraversalOperationId;
+        operation.ReplyToActorIds.insert(ReplyToActorId);
+        Self->ForceTraversals.PushBack(&operation);
+
+        Self->PersistLastForceTraversalOperationId(db);
+
+        db.Table<Schema::ForceTraversals>().Key(operation.OperationId).Update(
+            NIceDb::TUpdate<Schema::ForceTraversals::OwnerId>(PathId.OwnerId),
+            NIceDb::TUpdate<Schema::ForceTraversals::LocalPathId>(PathId.LocalPathId));
+
+        OperationId = operation.OperationId;
+
+        return true;
+    }
+
+    void Complete(const TActorContext& ctx) override {
+        SA_LOG_D("[" << Self->TabletID() << "] TTxAnalyzeTable::Complete");
+    }
+};
+
+void TStatisticsAggregator::Handle(TEvStatistics::TEvAnalyze::TPtr& ev) {
+    const auto& record = ev->Get()->Record;
+
+    // TODO: replace by queue
+    for (const auto& table : record.GetTables()) {
+        Execute(new TTxAnalyzeTable(this, PathIdFromPathId(table.GetPathId()), ev->Sender), TActivationContext::AsActorContext());
+    }
+
+}
+
+} // NKikimr::NStat
--- ydb/core/statistics/aggregator/ya.make
+++ ydb/core/statistics/aggregator/ya.make
@@ -9,7 +9,9 @@ SRCS(
     schema.cpp
     tx_ack_timeout.cpp
     tx_aggr_stat_response.cpp
+    tx_analyze_table.cpp
     tx_configure.cpp
+    tx_datashard_scan_response.cpp
     tx_delete_query_response.cpp
     tx_init.cpp
     tx_init_schema.cpp
@@ -17,10 +19,8 @@ SRCS(
     tx_resolve.cpp
     tx_response_tablet_distribution.cpp
     tx_save_query_response.cpp
-    tx_scan_table.cpp
-    tx_schedule_scan.cpp
+    tx_schedule_traversal.cpp
     tx_schemeshard_stats.cpp
-    tx_statistics_scan_response.cpp
 )
 
 PEERDIR(

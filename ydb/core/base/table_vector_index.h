a750997f05608d0d2e0edbf825ca15424c986382
Valery Mironov <valera.mironow@gmail.com>
2024-07-30 17:26:11 +0300

    Vector Index should use for posting impl table reserved name for additional column

--- ydb/core/base/table_vector_index.h
+++ ydb/core/base/table_vector_index.h
@@ -6,16 +6,15 @@ namespace NKikimr::NTableIndex::NTableVectorKmeansTreeIndex {
 
 // Levels table
 inline constexpr const char* LevelTable = "indexImplLevelTable";
-inline constexpr const char* LevelTable_ParentIdColumn = "-parent";
+inline constexpr const char* LevelTable_ParentIdColumn = "__ydb_parent";
 inline constexpr const char* LevelTable_IdColumn = "-id";
 inline constexpr const char* LevelTable_EmbeddingColumn = "-embedding";
 
 // Posting table
 inline constexpr const char* PostingTable = "indexImplPostingTable";
-inline constexpr const char* PostingTable_ParentIdColumn = "-parent";
+inline constexpr const char* PostingTable_ParentIdColumn = LevelTable_ParentIdColumn;
 
 inline constexpr const char* TmpPostingTableSuffix0 = "0tmp";
 inline constexpr const char* TmpPostingTableSuffix1 = "1tmp";
 
 }
-

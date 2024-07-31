5872c8e0780708f1138757609dbf2b70abe28742
don-dron <don-dron@yandex-team.com>
2024-07-31 12:32:54 +0300

    Revert "YT-21931: Track memory for ahead blocks in write sessions"
    This reverts commit f34c547ac0041f6745654e8da71c886bc2ae5ebc, reversing
    changes made to 8c124ed0587173703d24131bf1565c0e85973b2a.
    890663587fbe5faa158f70ac86dbd66fe028ab9e

--- yt/yt/core/misc/memory_usage_tracker.cpp
+++ yt/yt/core/misc/memory_usage_tracker.cpp
@@ -233,14 +233,9 @@ TError TMemoryUsageTrackerGuard::SetSizeImpl(i64 size, auto acquirer)
     return {};
 }
 
-void TMemoryUsageTrackerGuard::IncreaseSize(i64 sizeDelta)
+void TMemoryUsageTrackerGuard::IncrementSize(i64 sizeDelta)
 {
     SetSize(Size_ + sizeDelta);
-}
-
-void TMemoryUsageTrackerGuard::DecreaseSize(i64 sizeDelta)
-{
-    SetSize(Size_ - sizeDelta);
 }
 
 TMemoryUsageTrackerGuard TMemoryUsageTrackerGuard::TransferMemory(i64 size)

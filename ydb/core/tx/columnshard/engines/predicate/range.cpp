c66255fa313593ae24e5c6f795f84bed8992fbc4
ivanmorozov333 <ivanmorozov@ydb.tech>
2024-07-31 16:40:34 +0300

    fix pk checker (#7266)

--- ydb/core/tx/columnshard/engines/predicate/range.cpp
+++ ydb/core/tx/columnshard/engines/predicate/range.cpp
@@ -39,8 +39,8 @@ NKikimr::NArrow::TColumnFilter TPKRangeFilter::BuildFilter(const arrow::Datum& d
     return result.And(PredicateFrom.BuildFilter(data));
 }
 
-bool TPKRangeFilter::IsPortionInUsage(const TPortionInfo& info, const TIndexInfo& indexInfo) const {
-    if (auto from = PredicateFrom.ExtractKey(indexInfo.GetPrimaryKey())) {
+bool TPKRangeFilter::IsPortionInUsage(const TPortionInfo& info) const {
+    if (const auto& from = PredicateFrom.GetReplaceKey()) {
         const auto& portionEnd = info.IndexKeyEnd();
         const int commonSize = std::min(from->Size(), portionEnd.Size());
         if (std::is_gt(from->ComparePartNotNull(portionEnd, commonSize))) {
@@ -48,7 +48,7 @@ bool TPKRangeFilter::IsPortionInUsage(const TPortionInfo& info, const TIndexInfo
         }
     }
 
-    if (auto to = PredicateTo.ExtractKey(indexInfo.GetPrimaryKey())) {
+    if (const auto& to = PredicateTo.GetReplaceKey()) {
         const auto& portionStart = info.IndexKeyStart();
         const int commonSize = std::min(to->Size(), portionStart.Size());
         if (std::is_lt(to->ComparePartNotNull(portionStart, commonSize))) {
@@ -56,38 +56,62 @@ bool TPKRangeFilter::IsPortionInUsage(const TPortionInfo& info, const TIndexInfo
         }
     }
 
+//    AFL_ERROR(NKikimrServices::TX_COLUMNSHARD)("start", info.IndexKeyStart().DebugString())("end", info.IndexKeyEnd().DebugString())(
+//        "from", PredicateFrom.DebugString())("to", PredicateTo.DebugString());
+
     return true;
 }
 
-bool TPKRangeFilter::IsPortionInPartialUsage(const NArrow::TReplaceKey& start, const NArrow::TReplaceKey& end, const TIndexInfo& indexInfo) const {
-    bool startUsage = false;
-    bool endUsage = false;
-    if (auto from = PredicateFrom.ExtractKey(indexInfo.GetPrimaryKey())) {
-        AFL_VERIFY(from->Size() <= start.Size());
-        if (PredicateFrom.IsInclude()) {
-            startUsage = std::is_lt(start.ComparePartNotNull(*from, from->Size()));
-        } else {
-            startUsage = std::is_lteq(start.ComparePartNotNull(*from, from->Size()));
+TPKRangeFilter::EUsageClass TPKRangeFilter::IsPortionInPartialUsage(const NArrow::TReplaceKey& start, const NArrow::TReplaceKey& end) const {
+    {
+        std::partial_ordering equalityStartWithFrom = std::partial_ordering::greater;
+        if (const auto& from = PredicateFrom.GetReplaceKey()) {
+            equalityStartWithFrom = start.ComparePartNotNull(*from, from->Size());
         }
-    } else {
-        startUsage = true;
+        std::partial_ordering equalityEndWithTo = std::partial_ordering::less;
+        if (const auto& to = PredicateTo.GetReplaceKey()) {
+            equalityEndWithTo = end.ComparePartNotNull(*to, to->Size());
+        }
+        const bool startInternal = (equalityStartWithFrom == std::partial_ordering::equivalent && PredicateFrom.IsInclude()) ||
+                                   (equalityStartWithFrom == std::partial_ordering::greater);
+        const bool endInternal = (equalityEndWithTo == std::partial_ordering::equivalent && PredicateTo.IsInclude()) ||
+                                 (equalityEndWithTo == std::partial_ordering::less);
+        if (startInternal && endInternal) {
+            return EUsageClass::FullUsage;
+        }
+    }
+    
+
+    if (const auto& from = PredicateFrom.GetReplaceKey()) {
+        const std::partial_ordering equalityEndWithFrom = end.ComparePartNotNull(*from, from->Size());
+        if (equalityEndWithFrom == std::partial_ordering::less) {
+            return EUsageClass::DontUsage;
+        } else if (equalityEndWithFrom == std::partial_ordering::equivalent) {
+            if (PredicateFrom.IsInclude()) {
+                return EUsageClass::PartialUsage;
+            } else {
+                return EUsageClass::DontUsage;
+            }
+        }
     }
 
-    if (auto to = PredicateTo.ExtractKey(indexInfo.GetPrimaryKey())) {
-        AFL_VERIFY(to->Size() <= end.Size());
-        if (PredicateTo.IsInclude()) {
-            endUsage = std::is_gt(end.ComparePartNotNull(*to, to->Size()));
-        } else {
-            endUsage = std::is_gteq(end.ComparePartNotNull(*to, to->Size()));
+    if (const auto& to = PredicateTo.GetReplaceKey()) {
+        const std::partial_ordering equalityStartWithTo = start.ComparePartNotNull(*to, to->Size());
+        if (equalityStartWithTo == std::partial_ordering::greater) {
+            return EUsageClass::DontUsage;
+        } else if (equalityStartWithTo == std::partial_ordering::equivalent) {
+            if (PredicateTo.IsInclude()) {
+                return EUsageClass::PartialUsage;
+            } else {
+                return EUsageClass::DontUsage;
+            }
         }
-    } else {
-        endUsage = true;
     }
 
-//    AFL_ERROR(NKikimrServices::TX_COLUMNSHARD)("start", start.DebugString())("end", end.DebugString())("from", PredicateFrom.DebugString())("to", PredicateTo.DebugString())
-//        ("start_usage", startUsage)("end_usage", endUsage);
+//    AFL_ERROR(NKikimrServices::TX_COLUMNSHARD)("start", start.DebugString())("end", end.DebugString())("from", PredicateFrom.DebugString())(
+//        "to", PredicateTo.DebugString());
 
-    return endUsage || startUsage;
+    return EUsageClass::PartialUsage;
 }
 
 std::optional<NKikimr::NOlap::TPKRangeFilter> TPKRangeFilter::Build(TPredicateContainer&& from, TPredicateContainer&& to) {

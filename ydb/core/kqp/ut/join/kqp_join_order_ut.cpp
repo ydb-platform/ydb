6b5d9b416f87d721d3afbd6df41c0e8e67dd059f
Pavel Velikhov <pavelvelikhov@ydb.tech>
2024-07-31 13:16:34 +0000

    Switched off GraceSelfJoinCore

--- ydb/core/kqp/ut/join/kqp_join_order_ut.cpp
+++ ydb/core/kqp/ut/join/kqp_join_order_ut.cpp
@@ -335,17 +335,17 @@ void ExecuteJoinOrderTestDataQueryWithStats(const TString& queryPath, const TStr
         );
     }
 
-/*
+
     Y_UNIT_TEST_TWIN(OverrideStatsTPCDS64, StreamLookupJoin) {
         JoinOrderTestWithOverridenStats(
             "queries/tpcds64.sql", "stats/tpcds1000s.json", "join_order/tpcds64_1000s.json", StreamLookupJoin
         );
     }
-*/
+
 
     Y_UNIT_TEST_TWIN(OverrideStatsTPCDS64_small, StreamLookupJoin) {
         JoinOrderTestWithOverridenStats(
-            "queries/tpcds64_small.sql", "stats/tpcds1000s.json", "join_order/tpcds64_1000s.json", StreamLookupJoin
+            "queries/tpcds64_small.sql", "stats/tpcds1000s.json", "join_order/tpcds64_small_1000s.json", StreamLookupJoin
         );
     }
    

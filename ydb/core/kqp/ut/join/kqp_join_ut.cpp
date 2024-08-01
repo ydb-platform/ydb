688281d4cdc42e057c6b72f968fb576db96f235f
Nikolay Shumkov <shumkovnd@ydb.tech>
2024-08-01 19:51:09 +0300

    Fixes

--- ydb/core/kqp/ut/join/kqp_join_ut.cpp
+++ ydb/core/kqp/ut/join/kqp_join_ut.cpp
@@ -877,7 +877,7 @@ Y_UNIT_TEST_SUITE(KqpJoin) {
                 SELECT ta.a, tb.bval, tc.cval FROM ta INNER JOIN tb ON ta.b = tb.b LEFT JOIN tc ON ta.c = tc.cval;
             )", NYdb::NQuery::TTxControl::BeginTx().CommitTx(), settings).ExtractValueSync();
             UNIT_ASSERT_VALUES_EQUAL_C(result.GetStatus(), EStatus::SUCCESS, result.GetIssues().ToString());
-            CompareYson(R"([[[1];[1001];[2001]];[[3];[1003];[2003]];[[2];[1002];[2002]]])", FormatResultSetYson(result.GetResultSet(0)));
+            CompareYson(R"([[[3];[1003];[2003]];[[2];[1002];[2002]];[[1];[1001];[2001]]])", FormatResultSetYson(result.GetResultSet(0)));
         }
     }
 

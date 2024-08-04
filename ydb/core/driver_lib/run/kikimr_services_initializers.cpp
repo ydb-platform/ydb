0789608d77b89024baccb90a1fe91f8cdefe2153
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2024-08-04 17:00:17 +0000

    Fix configuration of ticket parser. Pass server certificates file path

--- ydb/core/driver_lib/run/kikimr_services_initializers.cpp
+++ ydb/core/driver_lib/run/kikimr_services_initializers.cpp
@@ -1650,7 +1650,7 @@ void TSecurityServicesInitializer::InitializeServices(NActors::TActorSystemSetup
             .AuthConfig = Config.GetAuthConfig(),
             .CertificateAuthValues = {
                 .ClientCertificateAuthorization = Config.GetClientCertificateAuthorization(),
-                .ServerCertificateFilePath = grpcConfig.GetCert(),
+                .ServerCertificateFilePath = grpcConfig.GetPathToCertificateFile(),
                 .Domain = Config.GetAuthConfig().GetCertificateAuthenticationDomain()
             }
         };

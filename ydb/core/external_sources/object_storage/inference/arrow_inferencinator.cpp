1d7404c3ba420781562b34b3a11fca296a887859
Ivan Sukhov <evanevannnn@ydb.tech>
2024-08-05 10:10:30 +0000

    futher fixing inferring optional types

--- ydb/core/external_sources/object_storage/inference/arrow_inferencinator.cpp
+++ ydb/core/external_sources/object_storage/inference/arrow_inferencinator.cpp
@@ -14,64 +14,84 @@ namespace NKikimr::NExternalSource::NObjectStorage::NInference {
 
 namespace {
 
-bool ArrowToYdbType(Ydb::Type& optionalType, const arrow::DataType& type) {
-    auto& resType = *optionalType.mutable_optional_type()->mutable_item();
+bool ShouldBeOptional(const arrow::DataType& type) {
     switch (type.id()) {
     case arrow::Type::NA:
-        resType.set_type_id(Ydb::Type::UTF8);
+    case arrow::Type::STRING:
+    case arrow::Type::BINARY:
+    case arrow::Type::LARGE_BINARY:
+    case arrow::Type::FIXED_SIZE_BINARY:
+        return false;
+    default:
+        return true;
+    }
+}
+
+bool ArrowToYdbType(Ydb::Type& resType, const arrow::DataType& type) {
+    Ydb::Type* maybeOptionalType;
+    if (ShouldBeOptional(type)) {
+        maybeOptionalType = resType.mutable_optional_type()->mutable_item();
+    }
+    else {
+        maybeOptionalType = &resType;
+    }
+
+    switch (type.id()) {
+    case arrow::Type::NA:
+        maybeOptionalType->set_type_id(Ydb::Type::UTF8);
         return true;
     case arrow::Type::BOOL:
-        resType.set_type_id(Ydb::Type::BOOL);
+        maybeOptionalType->set_type_id(Ydb::Type::BOOL);
         return true;
     case arrow::Type::UINT8:
-        resType.set_type_id(Ydb::Type::UINT8);
+        maybeOptionalType->set_type_id(Ydb::Type::UINT8);
         return true;
     case arrow::Type::INT8:
-        resType.set_type_id(Ydb::Type::INT8);
+        maybeOptionalType->set_type_id(Ydb::Type::INT8);
         return true;
     case arrow::Type::UINT16:
-        resType.set_type_id(Ydb::Type::UINT16);
+        maybeOptionalType->set_type_id(Ydb::Type::UINT16);
         return true;
     case arrow::Type::INT16:
-        resType.set_type_id(Ydb::Type::INT16);
+        maybeOptionalType->set_type_id(Ydb::Type::INT16);
         return true;
     case arrow::Type::UINT32:
-        resType.set_type_id(Ydb::Type::UINT32);
+        maybeOptionalType->set_type_id(Ydb::Type::UINT32);
         return true;
     case arrow::Type::INT32:
-        resType.set_type_id(Ydb::Type::INT32);
+        maybeOptionalType->set_type_id(Ydb::Type::INT32);
         return true;
     case arrow::Type::UINT64:
-        resType.set_type_id(Ydb::Type::UINT64);
+        maybeOptionalType->set_type_id(Ydb::Type::UINT64);
         return true;
     case arrow::Type::INT64:
-        resType.set_type_id(Ydb::Type::INT64);
+        maybeOptionalType->set_type_id(Ydb::Type::INT64);
         return true;
     case arrow::Type::HALF_FLOAT: // TODO: is there anything?
         return false;
     case arrow::Type::FLOAT:
-        resType.set_type_id(Ydb::Type::FLOAT);
+        maybeOptionalType->set_type_id(Ydb::Type::FLOAT);
         return true;
     case arrow::Type::DOUBLE:
-        resType.set_type_id(Ydb::Type::DOUBLE);
+        maybeOptionalType->set_type_id(Ydb::Type::DOUBLE);
         return true;
     case arrow::Type::STRING: // TODO: is it true?
-        resType.set_type_id(Ydb::Type::UTF8);
+        maybeOptionalType->set_type_id(Ydb::Type::UTF8);
         return true;
     case arrow::Type::BINARY: // TODO: is it true?
-        resType.set_type_id(Ydb::Type::STRING);
+        maybeOptionalType->set_type_id(Ydb::Type::STRING);
         return true;
     case arrow::Type::FIXED_SIZE_BINARY: // TODO: is it true?
-        resType.set_type_id(Ydb::Type::STRING);
+        maybeOptionalType->set_type_id(Ydb::Type::STRING);
         return true;
     case arrow::Type::DATE32:
-        resType.set_type_id(Ydb::Type::DATE);
+        maybeOptionalType->set_type_id(Ydb::Type::DATE);
         return true;
     case arrow::Type::DATE64: // TODO: is it true?
-        resType.set_type_id(Ydb::Type::DATETIME64);
+        maybeOptionalType->set_type_id(Ydb::Type::DATETIME64);
         return true;
     case arrow::Type::TIMESTAMP:
-        resType.set_type_id(Ydb::Type::TIMESTAMP);
+        maybeOptionalType->set_type_id(Ydb::Type::TIMESTAMP);
         return true;
     case arrow::Type::TIME32: // TODO: is there anything?
         return false;
@@ -80,10 +100,10 @@ bool ArrowToYdbType(Ydb::Type& optionalType, const arrow::DataType& type) {
     case arrow::Type::INTERVAL_MONTHS: // TODO: is it true?
         return false;
     case arrow::Type::INTERVAL_DAY_TIME: // TODO: is it true?
-        resType.set_type_id(Ydb::Type::INTERVAL64);
+        maybeOptionalType->set_type_id(Ydb::Type::INTERVAL64);
         return true;
     case arrow::Type::DECIMAL128: // TODO: is it true?
-        resType.set_type_id(Ydb::Type::DOUBLE);
+        maybeOptionalType->set_type_id(Ydb::Type::DOUBLE);
         return true;
     case arrow::Type::DECIMAL256: // TODO: is there anything?
         return false;
@@ -93,7 +113,7 @@ bool ArrowToYdbType(Ydb::Type& optionalType, const arrow::DataType& type) {
         return false;
     }
     case arrow::Type::STRUCT: { // TODO: is ok?
-        auto& structType = *resType.mutable_struct_type();
+        auto& structType = *maybeOptionalType->mutable_struct_type();
         for (const auto& field : type.fields()) {
             auto& member = *structType.add_members();
             auto& memberType = *member.mutable_type();
@@ -106,7 +126,7 @@ bool ArrowToYdbType(Ydb::Type& optionalType, const arrow::DataType& type) {
     }
     case arrow::Type::SPARSE_UNION:
     case arrow::Type::DENSE_UNION: { // TODO: is ok?
-        auto& variant = *resType.mutable_variant_type()->mutable_struct_items();
+        auto& variant = *maybeOptionalType->mutable_variant_type()->mutable_struct_items();
         for (const auto& field : type.fields()) {
             auto& member = *variant.add_members();
             if (!ArrowToYdbType(*member.mutable_type(), *field->type())) {
@@ -127,13 +147,13 @@ bool ArrowToYdbType(Ydb::Type& optionalType, const arrow::DataType& type) {
     case arrow::Type::EXTENSION: // TODO: is representable?
         return false;
     case arrow::Type::DURATION: // TODO: is it true?
-        resType.set_type_id(Ydb::Type::INTERVAL64);
+        maybeOptionalType->set_type_id(Ydb::Type::INTERVAL64);
         return true;
     case arrow::Type::LARGE_STRING: // TODO: is it true?
-        resType.set_type_id(Ydb::Type::UTF8);
+        maybeOptionalType->set_type_id(Ydb::Type::UTF8);
         return true;
     case arrow::Type::LARGE_BINARY: // TODO: is it true?
-        resType.set_type_id(Ydb::Type::STRING);
+        maybeOptionalType->set_type_id(Ydb::Type::STRING);
         return true;
     case arrow::Type::MAX_ID:
         return false;

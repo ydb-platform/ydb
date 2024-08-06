9524a370d0e3582c88b05382e520de90eb9a98b9
Alexey Efimov <xeno@ydb.tech>
2024-08-06 16:29:24 +0700

    Storage groups group by (#7480)

--- ydb/core/viewer/json_handlers_storage.cpp
+++ ydb/core/viewer/json_handlers_storage.cpp
@@ -4,7 +4,7 @@
 namespace NKikimr::NViewer {
 
 void InitStorageGroupsJsonHandler(TJsonHandlers& jsonHandlers) {
-    jsonHandlers.AddHandler("/storage/groups", new TJsonHandler<TStorageGroups>(TStorageGroups::GetSwagger()));
+    jsonHandlers.AddHandler("/storage/groups", new TJsonHandler<TStorageGroups>(TStorageGroups::GetSwagger()), 2);
 }
 
 void InitStorageJsonHandlers(TJsonHandlers& jsonHandlers) {

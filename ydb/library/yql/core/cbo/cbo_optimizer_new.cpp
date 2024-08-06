ydb-platform/ydb

commit 002f1f3cfc532290d60b1c2717dd358d9d0933ab

Optimized DPccp (#1563)
* Optimized DPccp

* Cleaned up new optimizer interface

* Simplified the optimization a lot

* Added some comments

ydb/library/yql/core/cbo/cbo_optimizer_new.cpp
@@ -67,10 +67,14 @@ TJoinOptimizerNode::TJoinOptimizerNode(const std::shared_ptr<IBaseOptimizerNode>
     IBaseOptimizerNode(JoinNodeType), 
     LeftArg(left), 
     RightArg(right), 
-    JoinConditions(joinConditions), 
+    JoinConditions(joinConditions),
     JoinType(joinType),
     JoinAlgo(joinAlgo) {
         IsReorderable = (JoinType==EJoinKind::InnerJoin) && (nonReorderable==false);
+        for (auto [l,r] : joinConditions ) {
+            LeftJoinKeys.push_back(l.AttributeName);
+            RightJoinKeys.push_back(r.AttributeName);
+        }
     }
 
 TVector<TString> TJoinOptimizerNode::Labels() {
@@ -97,7 +101,9 @@ void TJoinOptimizerNode::Print(std::stringstream& stream, int ntabs) {
         stream << "\t";
     }
 
-    stream << *Stats << "\n";
+    if (Stats) {
+        stream << *Stats << "\n";
+    }
 
     LeftArg->Print(stream, ntabs+1);
     RightArg->Print(stream, ntabs+1);

ydb/core/kqp/opt/logical/kqp_opt_cbo.cpp
@@ -90,9 +90,12 @@ bool IsLookupJoinApplicableDetailed(const std::shared_ptr<NYql::TRelOptimizerNod
 bool IsLookupJoinApplicable(std::shared_ptr<IBaseOptimizerNode> left, 
     std::shared_ptr<IBaseOptimizerNode> right, 
     const std::set<std::pair<TJoinColumn, TJoinColumn>>& joinConditions,
+    const TVector<TString>& leftJoinKeys,
+    const TVector<TString>& rightJoinKeys,
     TKqpProviderContext& ctx) {
 
     Y_UNUSED(left);
+    Y_UNUSED(leftJoinKeys);
 
     auto rightStats = right->Stats;
 
@@ -114,12 +117,7 @@ bool IsLookupJoinApplicable(std::shared_ptr<IBaseOptimizerNode> left,
         }
     }
 
-    TVector<TString> joinKeys;
-    for( auto [leftJc, rightJc] : joinConditions ) {
-        joinKeys.emplace_back( rightJc.AttributeName);
-    }
-
-    return IsLookupJoinApplicableDetailed(std::static_pointer_cast<TRelOptimizerNode>(right), joinKeys, ctx);
+    return IsLookupJoinApplicableDetailed(std::static_pointer_cast<TRelOptimizerNode>(right), rightJoinKeys, ctx);
 }
 
 }
@@ -127,6 +125,8 @@ bool IsLookupJoinApplicable(std::shared_ptr<IBaseOptimizerNode> left,
 bool TKqpProviderContext::IsJoinApplicable(const std::shared_ptr<IBaseOptimizerNode>& left, 
     const std::shared_ptr<IBaseOptimizerNode>& right, 
     const std::set<std::pair<NDq::TJoinColumn, NDq::TJoinColumn>>& joinConditions,
+    const TVector<TString>& leftJoinKeys,
+    const TVector<TString>& rightJoinKeys,
     EJoinAlgoType joinAlgo)  {
 
     switch( joinAlgo ) {
@@ -134,7 +134,7 @@ bool TKqpProviderContext::IsJoinApplicable(const std::shared_ptr<IBaseOptimizerN
             if (OptLevel==2 && left->Stats->Nrows > 10e3) {
                 return false;
             }
-            return IsLookupJoinApplicable(left, right, joinConditions, *this);
+            return IsLookupJoinApplicable(left, right, joinConditions, leftJoinKeys, rightJoinKeys, *this);
 
         case EJoinAlgoType::DictJoin:
             return right->Stats->Nrows < 10e5;

ydb/core/kqp/opt/logical/kqp_opt_cbo.h
@@ -26,6 +26,7 @@ struct TKqpProviderContext : public NYql::IProviderContext {
     virtual bool IsJoinApplicable(const std::shared_ptr<NYql::IBaseOptimizerNode>& left, 
         const std::shared_ptr<NYql::IBaseOptimizerNode>& right, 
         const std::set<std::pair<NYql::NDq::TJoinColumn, NYql::NDq::TJoinColumn>>& joinConditions,
+        const TVector<TString>& leftJoinKeys, const TVector<TString>& rightJoinKeys,
         NYql::EJoinAlgoType joinAlgo) override;
 
     virtual double ComputeJoinCost(const NYql::TOptimizerStatistics& leftStats, const NYql::TOptimizerStatistics& rightStats, NYql::EJoinAlgoType joinAlgo) const override;

ydb/library/yql/core/cbo/cbo_optimizer_new.h
@@ -92,6 +92,8 @@ struct IProviderContext {
     virtual bool IsJoinApplicable(const std::shared_ptr<IBaseOptimizerNode>& left, 
         const std::shared_ptr<IBaseOptimizerNode>& right, 
         const std::set<std::pair<NDq::TJoinColumn, NDq::TJoinColumn>>& joinConditions,
+        const TVector<TString>& leftJoinKeys,
+        const TVector<TString>& rightJoinKeys,
         EJoinAlgoType joinAlgo) = 0;
 
 };
@@ -111,11 +113,15 @@ struct TDummyProviderContext : public IProviderContext {
     bool IsJoinApplicable(const std::shared_ptr<IBaseOptimizerNode>& left, 
         const std::shared_ptr<IBaseOptimizerNode>& right, 
         const std::set<std::pair<NDq::TJoinColumn, NDq::TJoinColumn>>& joinConditions,
+        const TVector<TString>& leftJoinKeys,
+        const TVector<TString>& rightJoinKeys,
         EJoinAlgoType joinAlgo) override {
 
         Y_UNUSED(left);
         Y_UNUSED(right);
         Y_UNUSED(joinConditions);
+        Y_UNUSED(leftJoinKeys);
+        Y_UNUSED(rightJoinKeys);
         Y_UNUSED(joinAlgo);
 
         return true;
@@ -137,13 +143,19 @@ struct TDummyProviderContext : public IProviderContext {
 struct TJoinOptimizerNode : public IBaseOptimizerNode {
     std::shared_ptr<IBaseOptimizerNode> LeftArg;
     std::shared_ptr<IBaseOptimizerNode> RightArg;
-    std::set<std::pair<NDq::TJoinColumn, NDq::TJoinColumn>> JoinConditions;
+    const std::set<std::pair<NDq::TJoinColumn, NDq::TJoinColumn>> JoinConditions;
+    TVector<TString> LeftJoinKeys;
+    TVector<TString> RightJoinKeys;
     EJoinKind JoinType;
     EJoinAlgoType JoinAlgo;
     bool IsReorderable;
 
-    TJoinOptimizerNode(const std::shared_ptr<IBaseOptimizerNode>& left, const std::shared_ptr<IBaseOptimizerNode>& right, 
-        const std::set<std::pair<NDq::TJoinColumn, NDq::TJoinColumn>>& joinConditions, const EJoinKind joinType, const EJoinAlgoType joinAlgo, bool nonReorderable=false);
+    TJoinOptimizerNode(const std::shared_ptr<IBaseOptimizerNode>& left, 
+        const std::shared_ptr<IBaseOptimizerNode>& right, 
+        const std::set<std::pair<NDq::TJoinColumn, NDq::TJoinColumn>>& joinConditions,
+        const EJoinKind joinType, 
+        const EJoinAlgoType joinAlgo, 
+        bool nonReorderable=false);
     virtual ~TJoinOptimizerNode() {}
     virtual TVector<TString> Labels();
     virtual void Print(std::stringstream& stream, int ntabs=0);

ydb/library/yql/core/yql_cost_function.cpp
@@ -43,11 +43,13 @@ TOptimizerStatistics NYql::ComputeJoinStats(const TOptimizerStatistics& leftStat
 
     double newCard;
     EStatisticsType outputType;
-    TVector<TString> joinedTableKeys;
+    bool leftKeyColumns = false;
+    bool rightKeyColumns = false;
+
 
     if (IsPKJoin(rightStats,rightJoinKeys)) {
         newCard = leftStats.Nrows;
-        joinedTableKeys = leftStats.KeyColumns;
+        leftKeyColumns = true;
         if (leftStats.Type == EStatisticsType::BaseTable){
             outputType = EStatisticsType::FilteredFactTable;
         } else {
@@ -56,7 +58,7 @@ TOptimizerStatistics NYql::ComputeJoinStats(const TOptimizerStatistics& leftStat
     }
     else if (IsPKJoin(leftStats,leftJoinKeys)) {
         newCard = rightStats.Nrows;
-        joinedTableKeys = rightStats.KeyColumns;
+        rightKeyColumns = true;
         if (rightStats.Type == EStatisticsType::BaseTable){
             outputType = EStatisticsType::FilteredFactTable;
         } else {
@@ -74,8 +76,10 @@ TOptimizerStatistics NYql::ComputeJoinStats(const TOptimizerStatistics& leftStat
         + newCard 
         + leftStats.Cost + rightStats.Cost;
 
-    return TOptimizerStatistics(outputType, newCard, newNCols, cost, joinedTableKeys);
+    return TOptimizerStatistics(outputType, newCard, newNCols, cost, 
+        leftKeyColumns ? leftStats.KeyColumns : ( rightKeyColumns ? rightStats.KeyColumns : TOptimizerStatistics::EmptyColumns));
 }
+
 
 TOptimizerStatistics NYql::ComputeJoinStats(const TOptimizerStatistics& leftStats, const TOptimizerStatistics& rightStats, 
     const std::set<std::pair<NDq::TJoinColumn, NDq::TJoinColumn>>& joinConditions, EJoinAlgoType joinAlgo, const IProviderContext& ctx) {
@@ -90,3 +94,4 @@ TOptimizerStatistics NYql::ComputeJoinStats(const TOptimizerStatistics& leftStat
 
     return ComputeJoinStats(leftStats, rightStats, leftJoinKeys, rightJoinKeys, joinAlgo, ctx);
 }
+

ydb/library/yql/core/yql_statistics.cpp
@@ -17,3 +17,5 @@ TOptimizerStatistics& TOptimizerStatistics::operator+=(const TOptimizerStatistic
     Cost += other.Cost;
     return *this;
 }
+
+const TVector<TString>& TOptimizerStatistics::EmptyColumns = TVector<TString>();

ydb/library/yql/core/yql_statistics.h
@@ -25,21 +25,19 @@ struct TOptimizerStatistics {
     double Nrows = 0;
     int Ncols = 0;
     double Cost;
-    TVector<TString> KeyColumns;
+    const TVector<TString>& KeyColumns;
 
-    TString Descr;
-
-    TOptimizerStatistics() {}
-    TOptimizerStatistics(double nrows, int ncols): Nrows(nrows), Ncols(ncols) {}
-    TOptimizerStatistics(double nrows, int ncols, double cost): Nrows(nrows), Ncols(ncols), Cost(cost) {}
-    TOptimizerStatistics(EStatisticsType type, double nrows, int ncols, double cost): Type(type), Nrows(nrows), Ncols(ncols), Cost(cost) {}
-    TOptimizerStatistics(EStatisticsType type, double nrows, int ncols, double cost, TVector<TString> keyColumns): Type(type), Nrows(nrows), Ncols(ncols), Cost(cost), KeyColumns(keyColumns) {}
-    TOptimizerStatistics(double nrows,int ncols, double cost, TString descr): Nrows(nrows), Ncols(ncols), Cost(cost), Descr(descr) {}
-
+    TOptimizerStatistics() : KeyColumns(EmptyColumns) {}
+    TOptimizerStatistics(double nrows, int ncols): Nrows(nrows), Ncols(ncols), KeyColumns(EmptyColumns) {}
+    TOptimizerStatistics(double nrows, int ncols, double cost): Nrows(nrows), Ncols(ncols), Cost(cost), KeyColumns(EmptyColumns) {}
+    TOptimizerStatistics(EStatisticsType type, double nrows, int ncols, double cost): Type(type), Nrows(nrows), Ncols(ncols), Cost(cost), KeyColumns(EmptyColumns) {}
+    TOptimizerStatistics(EStatisticsType type, double nrows, int ncols, double cost, const TVector<TString>& keyColumns): Type(type), Nrows(nrows), Ncols(ncols), Cost(cost), KeyColumns(keyColumns) {}
 
     TOptimizerStatistics& operator+=(const TOptimizerStatistics& other);
     bool Empty() const;
 
     friend std::ostream& operator<<(std::ostream& os, const TOptimizerStatistics& s);
+
+    static const TVector<TString>& EmptyColumns;
 };
 }

commit 2f3ec0ee563d4c2f232d446f2e41a19a0aa24493

cbo/debug: fix Print with missing Stats
regression by 002f1f3cfc532290d60b1c2717dd358d9d0933ab

ydb/library/yql/core/cbo/cbo_optimizer_new.cpp
@@ -101,11 +101,10 @@ void TJoinOptimizerNode::Print(std::stringstream& stream, int ntabs) {
     }
     stream << "\n";
 
-    for (int i = 0; i < ntabs; i++){
-        stream << "    ";
-    }
-
     if (Stats) {
+        for (int i = 0; i < ntabs; i++){
+            stream << "    ";
+        }
         stream << *Stats << "\n";
     }
 
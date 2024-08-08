205672943239ae5a6de712220f133191adc0ab15
Alexey Ozeritskiy <aozeritsky@ydb.tech>
2024-08-08 14:02:40 +0200

    Fix cond

--- ydb/library/yql/public/decimal/yql_decimal.h
+++ ydb/library/yql/public/decimal/yql_decimal.h
@@ -173,7 +173,7 @@ struct TDecimal {
         const auto a = l + r;
         if (IsNormal(l) && IsNormal(r) && IsNormal(a)) {
             Value = a;
-        } else if (IsNan(l) || IsNan(r)) {
+        } else if (IsNan(l) || IsNan(r) || !a /* inf - inf*/) {
             Value = Nan();
         } else {
             Value = a > 0

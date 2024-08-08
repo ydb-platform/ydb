4f4353513c87989cb47ef1869af61b1e81095885
Yuriy Kaminskiy <yumkam7@ydb.tech>
2024-08-08 15:07:00 +0300

    tests/fq: add streamlookup

--- ydb/tests/fq/generic/test_streaming_join.py
+++ ydb/tests/fq/generic/test_streaming_join.py
@@ -1,5 +1,6 @@
 import pytest
 import os
+import json
 
 import ydb.public.api.protos.draft.fq_pb2 as fq
 from ydb.tests.tools.fq_runner.kikimr_utils import yq_v1
@@ -51,6 +52,80 @@ class TestStreamingJoin(TestYdsBase):
 
         read_data = self.read_stream(len(messages))
         assert read_data == messages
+
+        fq_client.abort_query(query_id)
+        fq_client.wait_query(query_id)
+
+        describe_response = fq_client.describe_query(query_id)
+        status = describe_response.result.query.meta.status
+        assert not describe_response.issues, str(describe_response.issues)
+        assert status == fq.QueryMeta.ABORTED_BY_USER, fq.QueryMeta.ComputeStatus.Name(status)
+
+    @yq_v1
+    @pytest.mark.parametrize("mvp_external_ydb_endpoint", [{"endpoint": "tests-fq-generic-ydb:2136"}], indirect=True)
+    @pytest.mark.parametrize("fq_client", [{"folder_id": "my_folder_slj"}], indirect=True)
+    def test_streamlookup(self, kikimr, fq_client: FederatedQueryClient, settings: Settings, yq_version):
+        self.init_topics(f"pq_yq_streaming_test_streamlookup{yq_version}")
+        fq_client.create_yds_connection("myyds", os.getenv("YDB_DATABASE"), os.getenv("YDB_ENDPOINT"))
+
+        table_name = 'join_table'
+        ydb_conn_name = f'ydb_conn_{table_name}'
+
+        fq_client.create_ydb_connection(
+            name=ydb_conn_name,
+            database_id=settings.ydb.dbname,
+        )
+
+        sql = R'''
+            $input = SELECT * FROM myyds.`{input_topic}`
+                    WITH (
+                        FORMAT=json_each_row,
+                        SCHEMA (
+                            id Uint64,
+                            ts String,
+                            ev_type String,
+                            user Uint64,
+                        )
+                    )            ;
+
+            $formatTime = DateTime::Format("%H:%M:%S");
+
+            $enriched = select e.id as id, 
+                            $formatTime(DateTime::ParseIso8601(e.ts)) as ts, 
+                            e.user as user_id, u.data as lookup
+                from
+                    $input as e
+                left join /*+ streamlookup() */ ydb_conn_{table_name}.{table_name} as u
+                on(e.user = u.id)
+            ;
+
+            insert into myyds.`{output_topic}`
+            select Unwrap(Yson::SerializeJson(Yson::From(TableRow()))) from $enriched;
+            '''.format(
+            input_topic=self.input_topic, output_topic=self.output_topic, table_name=table_name
+        )
+
+        query_id = fq_client.create_query("streamlookup", sql, type=fq.QueryContent.QueryType.STREAMING).result.query_id
+        fq_client.wait_query_status(query_id, fq.QueryMeta.RUNNING)
+        kikimr.compute_plane.wait_zero_checkpoint(query_id)
+
+        messages = [
+            '{"id":1,"ts":"20240701T112233","ev_type":"foo","user":1}',
+            '{"id":2,"ts":"20240701T113344","ev_type":"foo","user":2}',
+            '{"id":3,"ts":"20240701T113355","ev_type":"foo","user":5}',
+            ]
+        self.write_stream(messages)
+
+        read_data = self.read_stream(len(messages))
+        print(read_data)
+        for r, exp in zip(read_data, [
+            '{"id":1,"ts":"11:22:33","user_id":1,"lookup":"ydb10"}',
+            '{"id":2,"ts":"11:33:44","user_id":2,"lookup":"ydb20"}',
+            '{"id":3,"ts":"11:33:55","user_id":3,"lookup":null}',
+            ]):
+            r = json.loads(r)
+            exp = json.loads(exp)
+            assert r == exp
 
         fq_client.abort_query(query_id)
         fq_client.wait_query(query_id)

99be3e831a678cc441045dd63a76807d2a6c20c5
Kirill Rysin <naspirato@gmail.com>
2024-08-08 15:45:36 +0000

    file name changed

--- .github/scripts/upload_tests_results.py
+++ .github/scripts/upload_tests_results.py
@@ -236,7 +236,7 @@ def main():
     batch_size_default = 30
     dir = os.path.dirname(__file__)
     git_root = f"{dir}/../.."
-    codeowners = f"{git_root}/.github/TESTOWNERS.ini"
+    codeowners = f"{git_root}/.github/TESTOWNERS"
     config = configparser.ConfigParser()
     config_file_path = f"{git_root}/.github/config/ydb_qa_db.ini"
     config.read(config_file_path)

dae932b4c2a85dc8ded718ca941789d0fae70d5c
Daniil Cherednik <dcherednik@ydb.tech>
2024-08-08 19:14:13 +0200

    Fix json float/double print format (#7572)
    SELECT CAST("311111111113.222222223" as Double) should not lost precision

--- ydb/core/viewer/json_query.h
+++ ydb/core/viewer/json_query.h
@@ -418,10 +418,18 @@ private:
             }
 
             TStringStream stream;
-            NJson::TJsonWriterConfig config;
-            config.ValidateUtf8 = false;
-            config.WriteNanAsString = true;
-            NJson::WriteJson(&stream, &jsonResponse, config);
+
+            constexpr ui32 doubleNDigits = std::numeric_limits<double>::max_digits10;
+            constexpr ui32 floatNDigits = std::numeric_limits<float>::max_digits10;
+            constexpr EFloatToStringMode floatMode = EFloatToStringMode::PREC_NDIGITS;
+            NJson::WriteJson(&stream, &jsonResponse, {
+                .DoubleNDigits = doubleNDigits,
+                .FloatNDigits = floatNDigits,
+                .FloatToStringMode = floatMode,
+                .ValidateUtf8 = false,
+                .WriteNanAsString = true,
+            });
+
             out << stream.Str();
 
             ReplyAndPassAway(out);

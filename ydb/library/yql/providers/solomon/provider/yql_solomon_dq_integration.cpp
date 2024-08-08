02b4c2d1f86070c6bade2c5507925d661eba979c
Nikita Vasilev <ns-vasilev@ydb.tech>
2024-08-08 13:53:02 +0300

    24-3:  Sinks improvements (#6856)
    Co-authored-by: uzhastik <uzhas@ydb.tech>

--- ydb/library/yql/providers/solomon/provider/yql_solomon_dq_integration.cpp
+++ ydb/library/yql/providers/solomon/provider/yql_solomon_dq_integration.cpp
@@ -44,19 +44,6 @@ NSo::NProto::ESolomonClusterType MapClusterType(TSolomonClusterConfig::ESolomonC
     }
 }
 
-const TTypeAnnotationNode* GetItemType(const TExprNode& node) {
-    const TTypeAnnotationNode* typeAnn = node.GetTypeAnn();
-    switch (typeAnn->GetKind()) {
-        case ETypeAnnotationKind::Flow:
-            return typeAnn->Cast<TFlowExprType>()->GetItemType();
-        case ETypeAnnotationKind::Stream:
-            return typeAnn->Cast<TStreamExprType>()->GetItemType();
-        default: break;
-    }
-    YQL_ENSURE(false, "Invalid solomon sink type " << typeAnn->GetKind());
-    return nullptr;
-}
-
 void FillScheme(const TTypeAnnotationNode& itemType, NSo::NProto::TDqSolomonShardScheme& scheme) {
     int index = 0;
     for (const TItemExprType* structItem : itemType.Cast<TStructExprType>()->GetItems()) {
@@ -316,7 +303,7 @@ public:
         shardDesc.SetClusterType(MapClusterType(clusterDesc->GetClusterType()));
         shardDesc.SetUseSsl(clusterDesc->GetUseSsl());
 
-        const TTypeAnnotationNode* itemType = GetItemType(node);
+        const TTypeAnnotationNode* itemType = shard.RowType().Ref().GetTypeAnn()->Cast<TTypeExprType>()->GetType();
         FillScheme(*itemType, *shardDesc.MutableScheme());
 
         if (auto maybeToken = shard.Token()) {

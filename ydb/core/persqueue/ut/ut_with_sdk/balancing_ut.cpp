27d60f1191ec10aa8bb63bbd9b73b71566570c2a
Nikolay Shestakov <tesseract@ydb.tech>
2024-08-12 13:40:27 +0000

    compiled

--- ydb/core/persqueue/ut/ut_with_sdk/balancing_ut.cpp
+++ ydb/core/persqueue/ut/ut_with_sdk/balancing_ut.cpp
@@ -9,12 +9,6 @@
 #include <util/datetime/base.h>
 #include <util/stream/output.h>
 
-
-static inline IOutputStream& operator<<(IOutputStream& o, std::set<size_t> t) {
-    o << "[" << JoinRange(", ", t.begin(), t.end()) << "]";
-    return o;
-}
-
 namespace NKikimr {
 
 using namespace NYdb::NTopic;
@@ -23,26 +17,24 @@ using namespace NSchemeShardUT_Private;
 
 Y_UNIT_TEST_SUITE(Balancing) {
 
-    Y_UNIT_TEST(Simple) {
+    void Simple(SdkVersion sdk) {
         TTopicSdkTestSetup setup = CreateSetup();
         setup.CreateTopic(TEST_TOPIC, TEST_CONSUMER, 10);
 
-        TTopicClient client = setup.MakeClient();
-
-        TTestReadSession readSession0("Session-0", client);
+        auto readSession0 = CreateTestReadSession({ .Name="Session-0", .Setup=setup, .Sdk = sdk });
         {
-            readSession0.WaitAndAssertPartitions({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, "Single reading session must read all partitions");
-            readSession0.Run();
+            readSession0->WaitAndAssertPartitions({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, "Single reading session must read all partitions");
+            readSession0->Run();
         }
 
-        TTestReadSession readSession1("Session-1", client);
+        auto readSession1 = CreateTestReadSession({ .Name="Session-1", .Setup=setup, .Sdk = sdk });
         {
-            readSession1.Run();
+            readSession1->Run();
 
             Sleep(TDuration::Seconds(1));
 
-            auto p0 = readSession0.GetPartitions();
-            auto p1 = readSession1.GetPartitions();
+            auto p0 = readSession0->GetPartitions();
+            auto p1 = readSession1->GetPartitions();
 
             UNIT_ASSERT_VALUES_EQUAL_C(5, p0.size(), "After the appearance of the second reading session, the partitions should be distributed evenly among them (p0, " << p0 << ")");
             UNIT_ASSERT_VALUES_EQUAL_C(5, p1.size(), "After the appearance of the second reading session, the partitions should be distributed evenly among them (p1, " << p1 << ")");
@@ -50,15 +42,15 @@ Y_UNIT_TEST_SUITE(Balancing) {
             UNIT_ASSERT_VALUES_EQUAL_C(10, p0.size(), "Must read all partitions but " << p0);
         }
 
-        TTestReadSession readSession2("Session-2", client, Max<size_t>(), true, {0, 1});
+        auto readSession2 = CreateTestReadSession({ .Name="Session-2", .Setup=setup, .Sdk = sdk, .Partitions = {0, 1} });
         {
-            readSession2.WaitAndAssertPartitions({0, 1}, "The reading session should read partitions 0 and 1 because it clearly required them to be read.");
-            readSession2.Run();
+            readSession2->WaitAndAssertPartitions({0, 1}, "The reading session should read partitions 0 and 1 because it clearly required them to be read.");
+            readSession2->Run();
 
             Sleep(TDuration::Seconds(1));
 
-            auto p0 = readSession0.GetPartitions();
-            auto p1 = readSession1.GetPartitions();
+            auto p0 = readSession0->GetPartitions();
+            auto p1 = readSession1->GetPartitions();
             auto pa = p0;
             pa.insert(p1.begin(), p1.end());
             UNIT_ASSERT_VALUES_EQUAL_C(4, p0.size(), "There should be an even distribution of partitions " << p0);
@@ -66,32 +58,33 @@ Y_UNIT_TEST_SUITE(Balancing) {
             UNIT_ASSERT_VALUES_EQUAL_C(8, pa.size(), "Must read all partitions but " << pa);
         }
 
-        TTestReadSession readSession3("Session-3", client, Max<size_t>(), true, {0});
+        auto readSession3 = CreateTestReadSession({ .Name="Session-3", .Setup=setup, .Sdk = sdk, .Partitions = {0} });
         {
-            readSession3.WaitAndAssertPartitions({0}, "The reading session should read partitions 0 and 1 because it clearly required them to be read.");
-            readSession2.WaitAndAssertPartitions({1}, "The reading session should read partitions 0 and 1 because it clearly required them to be read.");
+            readSession3->WaitAndAssertPartitions({0}, "The reading session should read partitions 0 and 1 because it clearly required them to be read.");
+            readSession2->WaitAndAssertPartitions({1}, "The reading session should read partitions 0 and 1 because it clearly required them to be read.");
 
-            auto p0 = readSession0.Impl->Partitions;
-            p0.insert(readSession1.Impl->Partitions.begin(), readSession1.Impl->Partitions.end());
+            auto p0 = readSession0->GetPartitions();
+            auto p1 = readSession1->GetPartitions();
+            p0.insert(p1.begin(), p1.end());
             UNIT_ASSERT_VALUES_EQUAL_C(8, p0.size(), "Must read all partitions but " << p0);
         }
 
         {
-            readSession3.Run();
-            readSession3.Close();
+            readSession3->Run();
+            readSession3->Close();
 
-            readSession2.WaitAndAssertPartitions({0, 1}, "The reading session should read partitions 0 and 1 because it clearly required them to be read. (after release Session-3)");
-            readSession2.Run();
+            readSession2->WaitAndAssertPartitions({0, 1}, "The reading session should read partitions 0 and 1 because it clearly required them to be read. (after release Session-3)");
+            readSession2->Run();
         }
 
         {
-            readSession2.Run();
-            readSession2.Close();
+            readSession2->Run();
+            readSession2->Close();
 
             Sleep(TDuration::Seconds(1));
 
-            auto p0 = readSession0.GetPartitions();
-            auto p1 = readSession1.GetPartitions();
+            auto p0 = readSession0->GetPartitions();
+            auto p1 = readSession1->GetPartitions();
 
             UNIT_ASSERT_VALUES_EQUAL_C(5, p0.size(), "After the appearance of the second reading session, the partitions should be distributed evenly among them (p0, " << p0 << ")");
             UNIT_ASSERT_VALUES_EQUAL_C(5, p1.size(), "After the appearance of the second reading session, the partitions should be distributed evenly among them (p1, " << p1 << ")");
@@ -100,56 +93,136 @@ Y_UNIT_TEST_SUITE(Balancing) {
         }
 
         {
-            readSession1.Run();
-            readSession1.Close();
+            readSession1->Run();
+            readSession1->Close();
 
-            readSession0.WaitAndAssertPartitions({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, "Single reading session must read all partitions");
-            readSession0.Run();
+            readSession0->WaitAndAssertPartitions({0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, "Single reading session must read all partitions");
+            readSession0->Run();
         }
 
 
-        readSession0.Close();
+        readSession0->Close();
     }
 
-/*
-    Y_UNIT_TEST(BalanceManySession) {
+    Y_UNIT_TEST(Balancing_OneTopic_TopicApi) {
+        Simple(SdkVersion::Topic);
+    }
+
+    Y_UNIT_TEST(Balancing_OneTopic_PQv1) {
+        Simple(SdkVersion::PQv1);
+    }
+
+
+    void ManyTopics(SdkVersion sdk) {
         TTopicSdkTestSetup setup = CreateSetup();
-        setup.CreateTopic(TEST_TOPIC, TEST_CONSUMER, 1000);
+        setup.CreateTopic(TEST_TOPIC, TEST_CONSUMER, 10);
+        setup.CreateTopic("other-test-topic", TEST_CONSUMER, 10);
 
         TTopicClient client = setup.MakeClient();
 
-        auto CreateClient = [&](size_t i) {
-            auto readSettings = TReadSessionSettings()
-                .ConsumerName(TEST_CONSUMER)
-                .AppendTopics(TEST_TOPIC);
-            readSettings.Topics_[0].AppendPartitionIds(i % 1000);
+        auto readSession0 = CreateTestReadSession({ .Name="Session-0", .Setup=setup, .Sdk = sdk, .Topics = {TEST_TOPIC, "other-test-topic"} });
+        Sleep(TDuration::Seconds(1));
 
-            return client.CreateReadSession(readSettings);
+        {
+            auto p = readSession0->GetPartitionsA();
+            UNIT_ASSERT_VALUES_EQUAL(10, p[TEST_TOPIC].size());
+            UNIT_ASSERT_VALUES_EQUAL(10, p["other-test-topic"].size());
+        }
+
+        auto readSession1 = CreateTestReadSession({ .Name="Session-1", .Setup=setup, .Sdk = sdk, .Topics = {TEST_TOPIC, "other-test-topic"} });
+        Sleep(TDuration::Seconds(1));
+
+        {
+            auto p = readSession0->GetPartitionsA();
+            UNIT_ASSERT_VALUES_EQUAL(5, p[TEST_TOPIC].size());
+            UNIT_ASSERT_VALUES_EQUAL(5, p["other-test-topic"].size());
+        }
+        {
+            auto p = readSession1->GetPartitionsA();
+            UNIT_ASSERT_VALUES_EQUAL(5, p[TEST_TOPIC].size());
+            UNIT_ASSERT_VALUES_EQUAL(5, p["other-test-topic"].size());
+        }
+    }
+
+    Y_UNIT_TEST(Balancing_ManyTopics_TopicApi) {
+        ManyTopics(SdkVersion::Topic);
+    }
+
+    Y_UNIT_TEST(Balancing_ManyTopics_PQv1) {
+        ManyTopics(SdkVersion::PQv1);
+    }
+
+    //
+    // PQv1
+    //
+    Y_UNIT_TEST(PQv1_Simple) {
+        TTopicSdkTestSetup setup = CreateSetup();
+        setup.CreateTopic(TEST_TOPIC, TEST_CONSUMER, 10);
+
+        auto client = NYdb::NPersQueue::TPersQueueClient(*(setup.GetServer().AnnoyingClient->GetDriver()));
+
+        class TestReader {
+        public:
+            TestReader(const TString& name, NYdb::NPersQueue::TPersQueueClient& client) {
+                Partitions = std::make_shared<std::unordered_set<ui32>>();
+                Lock = std::make_shared<TMutex>();
+
+                NYdb::NPersQueue::TReadSessionSettings settings;
+                settings
+                    .ConsumerName(TEST_CONSUMER)
+                    .AppendTopics(TEST_TOPIC);
+
+                settings.EventHandlers_.CreatePartitionStreamHandler([lock=Lock, partitions=Partitions, name=name](NYdb::NPersQueue::TReadSessionEvent::TCreatePartitionStreamEvent& e) {
+                    Cerr << ">>>>> " << name << " Received TCreatePartitionStreamEvent " << e.DebugString() << Endl<< Flush;
+                    e.Confirm();
+                    with_lock (*lock) {
+                        partitions->insert(e.GetPartitionStream()->GetPartitionId());
+                    }
+
+                   // UNIT_ASSERT_C(!Partitions.insert(e.GetPartitionStream()->GetPartitionId()).second, "Partition already is readed");
+                });
+                settings.EventHandlers_.DestroyPartitionStreamHandler([lock=Lock, partitions=Partitions, name=name](NYdb::NPersQueue::TReadSessionEvent::TDestroyPartitionStreamEvent& e) {
+                    Cerr << ">>>>> " << name << " Received TDestroyPartitionStreamEvent " << e.DebugString() << Endl<< Flush;
+                    e.Confirm();
+                    with_lock (*lock) {
+                        partitions->erase(e.GetPartitionStream()->GetPartitionId());
+                    }
+
+                    //UNIT_ASSERT_C(!Partitions.erase(e.GetPartitionStream()->GetPartitionId()), "Partition is not readed");
+                });
+
+                ReadSession = client.CreateReadSession(settings);
+            }
+
+
+
+            std::shared_ptr<TMutex> Lock;
+            std::shared_ptr<std::unordered_set<ui32>> Partitions;
+            std::shared_ptr<NYdb::NPersQueue::IReadSession> ReadSession;
         };
 
-        Cerr << ">>>>> " << TInstant::Now() << " Begin create sessions" << Endl << Flush;
-
-        std::deque<std::shared_ptr<IReadSession>> sessions;
-        for (int i = 0; i < 2000; ++i) {
-            sessions.push_back(CreateClient(i));
+        TestReader session1("Session-1", client);
+        {
+            Sleep(TDuration::Seconds(1));
+            with_lock (*session1.Lock) {
+                UNIT_ASSERT_VALUES_EQUAL(10, session1.Partitions->size());
+            }
         }
 
-        for (int i = 0 ; i < 1000 ; ++i) {
-            Cerr << ">>>>> " << TInstant::Now() << " Close session " << i << Endl << Flush;
-
-            auto s = sessions.front();
-            s->Close();
-            sessions.pop_front();
-
-            Sleep(TDuration::MilliSeconds(50));
-
-            sessions.push_back(CreateClient(i * 7));
+        TestReader session2("Session-2", client);
+        {
+            Sleep(TDuration::Seconds(1));
+            with_lock (*session1.Lock) {
+                UNIT_ASSERT_VALUES_EQUAL(5, session1.Partitions->size());
+            }
+            with_lock (*session2.Lock) {
+                UNIT_ASSERT_VALUES_EQUAL(5, session2.Partitions->size());
+            }
         }
 
-        Cerr << ">>>>> " << TInstant::Now() << " Finished" << Endl << Flush;
-        Sleep(TDuration::Seconds(10));
+        session1.ReadSession->Close();
+        session2.ReadSession->Close();
     }
-*/
  }
 
 } // namespace NKikimr

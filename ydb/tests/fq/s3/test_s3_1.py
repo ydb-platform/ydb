b331d70714da0e9873acd68ba2dbab8feab3eca5
Oleg Doronin <dorooleg@yandex.ru>
2024-08-12 05:38:43 +0000

    test has been added

--- ydb/tests/fq/s3/test_s3_1.py
+++ ydb/tests/fq/s3/test_s3_1.py
@@ -481,3 +481,67 @@ Pear,15,33'''
         assert result_set.rows[0].items[0].uint64_value == 1024 * 10
         # 1024 x 1024 x 10 = 10 MB of raw data + little overhead for header, eols etc
         assert sum(kikimr.control_plane.get_metering(1)) == 21
+
+    @yq_all
+    @pytest.mark.parametrize("client", [{"folder_id": "my_folder"}], indirect=True)
+    @pytest.mark.parametrize("runtime_listing", ["false", "true"])
+    def test_top_level_listing(self, kikimr, s3, client, runtime_listing, unique_prefix):
+        resource = boto3.resource(
+            "s3", endpoint_url=s3.s3_url, aws_access_key_id="key", aws_secret_access_key="secret_key"
+        )
+
+        bucket = resource.Bucket("fbucket")
+        bucket.create(ACL='public-read')
+        bucket.objects.all().delete()
+
+        s3_client = boto3.client(
+            "s3", endpoint_url=s3.s3_url, aws_access_key_id="key", aws_secret_access_key="secret_key"
+        )
+
+        fruits = '''Fruit,Price,Weight
+Banana,3,100
+Apple,2,22
+Pear,15,33'''
+        s3_client.put_object(Body=fruits, Bucket='fbucket', Key='/2024-08-09.csv', ContentType='text/plain')
+        s3_client.put_object(Body=fruits, Bucket='fbucket', Key='/2024-08-08.csv', ContentType='text/plain')
+
+        kikimr.control_plane.wait_bootstrap(1)
+        storage_connection_name = unique_prefix + "fruitbucket"
+        client.create_storage_connection(storage_connection_name, "fbucket")
+
+        sql = f'''
+            pragma s3.UseRuntimeListing="{runtime_listing}";
+
+            SELECT *
+            FROM `{storage_connection_name}`.`/2024-08-*`
+            WITH (format=csv_with_names, SCHEMA (
+                Fruit String NOT NULL,
+                Price Int NOT NULL,
+                Weight Int NOT NULL
+            ));
+            '''
+
+        query_id = client.create_query("simple", sql, type=fq.QueryContent.QueryType.ANALYTICS).result.query_id
+        client.wait_query_status(query_id, fq.QueryMeta.COMPLETED)
+
+        data = client.get_result_data(query_id)
+        result_set = data.result.result_set
+        logging.debug(str(result_set))
+        assert len(result_set.columns) == 3
+        assert result_set.columns[0].name == "Fruit"
+        assert result_set.columns[0].type.type_id == ydb.Type.STRING
+        assert result_set.columns[1].name == "Price"
+        assert result_set.columns[1].type.type_id == ydb.Type.INT32
+        assert result_set.columns[2].name == "Weight"
+        assert result_set.columns[2].type.type_id == ydb.Type.INT32
+        assert len(result_set.rows) == 3
+        assert result_set.rows[0].items[0].bytes_value == b"Banana"
+        assert result_set.rows[0].items[1].int32_value == 3
+        assert result_set.rows[0].items[2].int32_value == 100
+        assert result_set.rows[1].items[0].bytes_value == b"Apple"
+        assert result_set.rows[1].items[1].int32_value == 2
+        assert result_set.rows[1].items[2].int32_value == 22
+        assert result_set.rows[2].items[0].bytes_value == b"Pear"
+        assert result_set.rows[2].items[1].int32_value == 15
+        assert result_set.rows[2].items[2].int32_value == 33
+        assert sum(kikimr.control_plane.get_metering(1)) == 10

eda17f9a86239068acb53452e01b8514536c1f0a
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2024-08-12 11:21:12 +0300

    [Http] Reply with structured issues when client accepts json data (#7576)

--- ydb/core/viewer/viewer_query.h
+++ ydb/core/viewer/viewer_query.h
@@ -512,7 +512,7 @@ private:
     void MakeErrorReply(NJson::TJsonValue& jsonResponse, const NYdb::TStatus& status) {
         TString message;
 
-        NViewer::MakeErrorReply(jsonResponse, message, status);
+        MakeJsonErrorReply(jsonResponse, message, status);
 
         if (Span) {
             Span.EndError(message);
@@ -774,4 +774,3 @@ public:
 };
 
 }
-

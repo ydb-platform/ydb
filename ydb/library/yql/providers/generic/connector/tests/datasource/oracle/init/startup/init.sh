49bcf70b1b5b00769eb04315affe3eb7e39b1c0a
Grigorii Papashvili <papashviliga@ydb.tech>
2024-08-12 14:33:14 +0000

    some little fixes

--- ydb/library/yql/providers/generic/connector/tests/datasource/oracle/init/startup/init.sh
+++ ydb/library/yql/providers/generic/connector/tests/datasource/oracle/init/startup/init.sh
@@ -22,7 +22,7 @@ echo creating table constant
 whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE IF NOT EXISTS "C##ADMIN".constant (
-    id INTEGER NOT NULL PRIMARY KEY
+	id INTEGER NOT NULL PRIMARY KEY
  );
 
 INSERT INTO "C##ADMIN".constant
@@ -38,7 +38,7 @@ echo creating table count_rows
 whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE IF NOT EXISTS "C##ADMIN".count_rows (
-    id INTEGER NOT NULL PRIMARY KEY
+	id INTEGER NOT NULL PRIMARY KEY
  );
 
 INSERT INTO "C##ADMIN".count_rows
@@ -54,8 +54,8 @@ echo creating table json
 whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE IF NOT EXISTS "C##ADMIN".json (
-    id INTEGER NOT NULL PRIMARY KEY,
-    col_01_json JSON
+	id INTEGER NOT NULL PRIMARY KEY,
+	col_01_json JSON
  );
 
 INSERT INTO "C##ADMIN".json
@@ -72,12 +72,12 @@ whenever sqlerror exit sql.sqlcode;
 
 
 CREATE TABLE IF NOT EXISTS "C##ADMIN".primitives (
-    col_00_id INTEGER NOT NULL PRIMARY KEY,
-    col_01_int INTEGER,
-    col_02_float FLOAT,
-    col_03_int_number NUMBER(3,0),
-    col_05_binary_float BINARY_FLOAT,
-    col_06_binary_double BINARY_DOUBLE,
+	col_00_id INTEGER NOT NULL PRIMARY KEY,
+	col_01_int INTEGER,
+	col_02_float FLOAT,
+	col_03_int_number NUMBER(3,0),
+	col_05_binary_float BINARY_FLOAT,
+	col_06_binary_double BINARY_DOUBLE,
 	col_07_varchar2 VARCHAR2(7),
 	col_08_nvarchar2 NVARCHAR2(12),
 	col_09_char_one CHAR,
@@ -92,7 +92,7 @@ CREATE TABLE IF NOT EXISTS "C##ADMIN".primitives (
 	col_18_timestamp TIMESTAMP,
 	col_19_timestamp_w_timezone TIMESTAMP WITH TIME ZONE,
 	col_20_timestamp_w_local_timezone TIMESTAMP WITH LOCAL TIME ZONE,
-    col_21_json JSON
+	col_21_json JSON
  );
 
 INSERT INTO "C##ADMIN".primitives 
@@ -104,20 +104,20 @@ col_17_date,
 col_18_timestamp, col_19_timestamp_w_timezone, col_20_timestamp_w_local_timezone, col_21_json) 
 VALUES 
 (1, 1, 1.1, 123, 1.1, 1.1, 'varchar', N'варчар', 'c', 'cha', N'ч', N'чар', 'clob', N'клоб', utl_raw.cast_to_raw('ABCD'), utl_raw.cast_to_raw('EF'), 
-    TO_DATE('01 01, 1970, 00:00:00', 'mm dd, YYYY, HH24:MI:SS'), 
-    TO_TIMESTAMP('1970-01-01 01:01:01.111111', 'YYYY-mm-dd HH24:MI:SS.FF'), 
-    TO_TIMESTAMP_TZ('1970-01-01 01:01:01.111111 -1:00', 'YYYY-mm-dd HH24:MI:SS.FF TZH:TZM'), 
-    TO_TIMESTAMP_TZ('1970-01-01 01:01:01.111111 -1:11', 'YYYY-mm-dd HH24:MI:SS.FF TZH:TZM'),
-    '{ "friends": [{"name": "James Holden","age": 35},{"name": "Naomi Nagata","age": 30}]}'
-    ),
-(2, -1, -1.1, -123, -1.1, -1.1, 'varchar', N'варчар', 'c', 'cha', N'ч', N'чар', 'clob', N'клоб', utl_raw.cast_to_raw('1234'), utl_raw.cast_to_raw('5678'),
-            TO_DATE('01 01, 1970, 00:00:00', 'mm dd, YYYY, HH24:MI:SS'),
-            TO_TIMESTAMP('1970-01-01 01:01:01.111111', 'YYYY-mm-dd HH24:MI:SS.FF'),
-            TO_TIMESTAMP_TZ('1970-01-01 01:01:01.111111 -1:00', 'YYYY-mm-dd HH24:MI:SS.FF TZH:TZM'),
-            TO_TIMESTAMP_TZ('1970-01-01 01:01:01.111111 -1:11', 'YYYY-mm-dd HH24:MI:SS.FF TZH:TZM'),
-            '{ "TODO" : "unicode" }'
-            ),
-(3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+	TO_DATE('01 01, 1970, 00:00:00', 'mm dd, YYYY, HH24:MI:SS'), 
+	TO_TIMESTAMP('1970-01-01 01:01:01.111111', 'YYYY-mm-dd HH24:MI:SS.FF'), 
+	TO_TIMESTAMP_TZ('1970-01-01 01:01:01.111111 -1:00', 'YYYY-mm-dd HH24:MI:SS.FF TZH:TZM'), 
+	TO_TIMESTAMP_TZ('1970-01-01 01:01:01.111111 -1:11', 'YYYY-mm-dd HH24:MI:SS.FF TZH:TZM'),
+	'{ "friends": [{"name": "James Holden","age": 35},{"name": "Naomi Nagata","age": 30}]}'
+	),
+(2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, -1, -1.1, -123, -1.1, -1.1, 'varchar', N'варчар', 'c', 'cha', N'ч', N'чар', 'clob', N'клоб', utl_raw.cast_to_raw('1234'), utl_raw.cast_to_raw('5678'),
+	TO_DATE('01 01, 1970, 00:00:00', 'mm dd, YYYY, HH24:MI:SS'),
+	TO_TIMESTAMP('1970-01-01 01:01:01.111111', 'YYYY-mm-dd HH24:MI:SS.FF'),
+	TO_TIMESTAMP_TZ('1970-01-01 01:01:01.111111 -1:00', 'YYYY-mm-dd HH24:MI:SS.FF TZH:TZM'),
+	TO_TIMESTAMP_TZ('1970-01-01 01:01:01.111111 -1:11', 'YYYY-mm-dd HH24:MI:SS.FF TZH:TZM'),
+	'{ "TODO" : "unicode" }'
+	);
 exit;
 EOF
 
@@ -127,7 +127,7 @@ echo creating table long_table
 whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE IF NOT EXISTS "C##ADMIN".long_table (
-    col_00_id INTEGER NOT NULL PRIMARY KEY,
+	col_00_id INTEGER NOT NULL PRIMARY KEY,
 	col_01_long LONG
  );
 
@@ -144,16 +144,16 @@ echo creating table longraw
 whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE IF NOT EXISTS "C##ADMIN".longraw (
-    col_00_id INTEGER NOT NULL PRIMARY KEY,
+	col_00_id INTEGER NOT NULL PRIMARY KEY,
 	col_01_long_raw LONG RAW
  );
 
 INSERT INTO "C##ADMIN".longraw
-    VALUES (1, utl_raw.cast_to_raw('12'));
+	VALUES (1, utl_raw.cast_to_raw('12'));
 INSERT INTO "C##ADMIN".longraw
-    VALUES	(2, utl_raw.cast_to_raw(''));
+	VALUES	(2, utl_raw.cast_to_raw(''));
 INSERT INTO "C##ADMIN".longraw
-    VALUES	(3, NULL);
+	VALUES	(3, NULL);
 
 exit;
 EOF
@@ -163,9 +163,9 @@ echo creating table datetimes
 whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE "C##ADMIN".datetimes (
-    col_00_id INTEGER,
-    col_01_date DATE,
-    col_02_timestamp TIMESTAMP(6)
+	col_00_id INTEGER,
+	col_01_date DATE,
+	col_02_timestamp TIMESTAMP(6)
 );
 
 INSERT INTO "C##ADMIN".datetimes 
@@ -196,16 +196,16 @@ echo creating table pushdown
 whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE "C##ADMIN".pushdown (
-    col_00_id INTEGER NOT NULL,
-    col_01_integer INTEGER,
-    col_02_text VARCHAR(255)
+	col_00_id INTEGER NOT NULL,
+	col_01_integer INTEGER,
+	col_02_text VARCHAR(255)
 );
 
 INSERT INTO "C##ADMIN".pushdown VALUES
-                     (1, 10, 'a'),
-                     (2, 2, 'b'),
-                     (3, 30, 'c'),
-                     (4, NULL, NULL);
+	(1, 10, 'a'),
+	(2, 2, 'b'),
+	(3, 30, 'c'),
+	(4, NULL, NULL);
 
 exit;
 EOF
@@ -218,8 +218,8 @@ whenever sqlerror exit sql.sqlcode;
 CREATE TABLE "C##ADMIN"."column_selection_A_b_C_d_E" ("COL1" INTEGER, "col2" INTEGER);
 
 INSERT INTO "C##ADMIN"."column_selection_A_b_C_d_E" ("COL1", "col2") VALUES
-    (1, 2),
-    (10, 20);
+	(1, 2),
+	(10, 20);
 
 exit;
 EOF
@@ -230,9 +230,9 @@ whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE "C##ADMIN"."column_selection_COL1" ("COL1" INTEGER, "col2" INTEGER);
 INSERT INTO "C##ADMIN"."column_selection_COL1" ("COL1", "col2") VALUES
-    (1, 2),
-    (10, 20);
-    exit;
+	(1, 2),
+	(10, 20);
+	exit;
 EOF
 
 echo creating table column_selection_col1
@@ -241,9 +241,9 @@ whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE "C##ADMIN"."column_selection_col1" ("COL1" INTEGER, "col2" INTEGER);
 INSERT INTO "C##ADMIN"."column_selection_col1" ("COL1", "col2") VALUES
-    (1, 2),
-    (10, 20);
-    exit;
+	(1, 2),
+	(10, 20);
+	exit;
 EOF
 
 echo creating table column_selection_asterisk
@@ -252,9 +252,9 @@ whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE "C##ADMIN"."column_selection_asterisk" ("COL1" INTEGER, "col2" INTEGER);
 INSERT INTO "C##ADMIN"."column_selection_asterisk" ("COL1", "col2") VALUES
-    (1, 2),
-    (10, 20);
-    exit;
+	(1, 2),
+	(10, 20);
+	exit;
 EOF
 
 echo creating table column_selection_col2_COL1
@@ -263,9 +263,9 @@ whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE "C##ADMIN"."column_selection_col2_COL1" ("COL1" INTEGER, "col2" INTEGER);
 INSERT INTO "C##ADMIN"."column_selection_col2_COL1" ("COL1", "col2") VALUES
-    (1, 2),
-    (10, 20);
-    exit;
+	(1, 2),
+	(10, 20);
+	exit;
 EOF
 
 echo creating table column_selection_col2_col1
@@ -274,9 +274,9 @@ whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE "C##ADMIN"."column_selection_col2_col1" ("COL1" INTEGER, "col2" INTEGER);
 INSERT INTO "C##ADMIN"."column_selection_col2_col1" ("COL1", "col2") VALUES
-    (1, 2),
-    (10, 20);
-    exit;
+	(1, 2),
+	(10, 20);
+	exit;
 EOF
 
 echo creating table column_selection_col2
@@ -285,9 +285,9 @@ whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE "C##ADMIN"."column_selection_col2" ("COL1" INTEGER, "col2" INTEGER);
 INSERT INTO "C##ADMIN"."column_selection_col2" ("COL1", "col2") VALUES
-    (1, 2),
-    (10, 20);
-    exit;
+	(1, 2),
+	(10, 20);
+	exit;
 EOF
 
 echo creating table column_selection_col3
@@ -296,7 +296,7 @@ whenever sqlerror exit sql.sqlcode;
 
 CREATE TABLE "C##ADMIN"."column_selection_col3" ("COL1" INTEGER, "col2" INTEGER);
 INSERT INTO "C##ADMIN"."column_selection_col3" ("COL1", "col2") VALUES
-    (1, 2),
-    (10, 20);
-    exit;
+	(1, 2),
+	(10, 20);
+	exit;
 EOF

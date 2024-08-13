57b6afefc49400c256f7a10d200347f877fee102
Pavel Velikhov <pavelvelikhov@ydb.tech>
2024-08-13 09:16:03 +0000

    Cannonized dq_file tests

--- ydb/library/yql/tests/sql/suites/stream_lookup_join/lookup_join_narrow.sql
+++ ydb/library/yql/tests/sql/suites/stream_lookup_join/lookup_join_narrow.sql
@@ -3,7 +3,8 @@ use plato;
 
 select e.id as id, e.ts as ts, e.host as host, h.fqdn as fqdn, h.ip4 as ip4, h.ip6 as ip6
     from Event as e 
-    left join /*+ streamlookup() */ Host as h
+    --left join /*+ streamlookup() */ Host as h
+    left join Host as h
     on (e.host == h.hostname)
 ;
 

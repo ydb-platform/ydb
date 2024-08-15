a3a6e7e9c722fa071d6df6db4440c9ce5186650a
Ildar Khisambeev <ikhis@ydb.tech>
2024-08-15 09:57:44 +0000

    fix build failures

--- ydb/core/tx/columnshard/engines/storage/indexes/count_min_sketch/checker.cpp
+++ ydb/core/tx/columnshard/engines/storage/indexes/count_min_sketch/checker.cpp
@@ -7,15 +7,18 @@
 namespace NKikimr::NOlap::NIndexes::NCountMinSketch {
 
 void TCountMinSketchChecker::DoSerializeToProtoImpl(NKikimrSSA::TProgram::TOlapIndexChecker& proto) const {
+    Y_UNUSED(proto);
     Y_ABORT("Unimplemented");  // unimplemented, should not be used
 }
 
 bool TCountMinSketchChecker::DoCheckImpl(const std::vector<TString>& blobs) const {
+    Y_UNUSED(blobs);
     Y_ABORT("Unimplemented");  // unimplemented, should not be used
     return false;
 }
 
 bool TCountMinSketchChecker::DoDeserializeFromProtoImpl(const NKikimrSSA::TProgram::TOlapIndexChecker& proto) {
+    Y_UNUSED(proto);
     Y_ABORT("Unimplemented");  // unimplemented, should not be used
     return false;
 }

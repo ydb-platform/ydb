57e1328b3ef6a5beb919f396eb13e5565d5b2a73
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-08-15 19:25:54 +0300

    fix build

--- ydb/core/tx/columnshard/engines/changes/general_compaction.cpp
+++ ydb/core/tx/columnshard/engines/changes/general_compaction.cpp
@@ -142,7 +142,7 @@ void TGeneralCompactColumnEngineChanges::BuildAppendedPortionsByChunks(
     }
     AppendedPortions = merger.Execute(stats, CheckPoints, resultFiltered, GranuleMeta->GetPathId(), shardingActualVersion);
     for (auto&& p : AppendedPortions) {
-        i.GetPortionConstructor().GetMeta().UpdateRecordsMeta(NPortion::EProduced::SPLIT_COMPACTED);
+        p.GetPortionConstructor().GetMeta().UpdateRecordsMeta(NPortion::EProduced::SPLIT_COMPACTED);
     }
 }
 

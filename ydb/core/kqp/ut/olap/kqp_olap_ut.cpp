cd7249241a21aaa7e914fc5f5d6fbefcd2f1175d
Pavel Velikhov <pavelvelikhov@ydb.tech>
2024-08-16 19:08:41 +0000

    Applied Tony's PR  #7771

--- ydb/core/kqp/ut/olap/kqp_olap_ut.cpp
+++ ydb/core/kqp/ut/olap/kqp_olap_ut.cpp
@@ -1028,6 +1028,11 @@ Y_UNIT_TEST_SUITE(KqpOlap) {
             R"(ChooseMembers(TableRow(), ['level', 'uid', 'resource_id']) != <|level:1, uid:"uid_3000001", resource_id:"10001"|>)",
             R"(`uid` LIKE "_id%000_")",
             R"(`uid` ILIKE "UID%002")",
+            R"(IF(`level` > 0, 4 / `level`) < `level`)",
+            R"(IF(`level` == 0, NULL, 4 / `level`) < `level`)",
+            R"(`level` > IF(`level` > 0, 4 / `level`))",
+            R"(`level` > IF(`level` == 0, NULL, 4 / `level`))",
+#endif
 #endif
         };
 

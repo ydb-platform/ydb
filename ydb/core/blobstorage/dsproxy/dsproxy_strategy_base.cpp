534d8abc3d54d74d9749346b017896f0c8d76277
Sergey Belyakov <serg-belyakov@ydb.tech>
2024-08-16 17:35:59 +0000

    Fix TStrategyBase::MakeSlowSubgroupDiskMask

--- ydb/core/blobstorage/dsproxy/dsproxy_strategy_base.cpp
+++ ydb/core/blobstorage/dsproxy/dsproxy_strategy_base.cpp
@@ -412,7 +412,7 @@ ui32 TStrategyBase::MakeSlowSubgroupDiskMask(TBlobState &state, const TBlobStora
                 return 0;
             }
 
-            for (ui32 idx = 0; idx < 1; ++idx) {
+            for (ui32 idx = 0; idx < 2; ++idx) {
                 if (worstDisks[idx].PredictedNs > slowThreshold) {
                     slowDiskSubgroupMask |= 1 << worstDisks[idx].DiskIdx;
                 }
@@ -420,7 +420,12 @@ ui32 TStrategyBase::MakeSlowSubgroupDiskMask(TBlobState &state, const TBlobStora
 
             // Mark slow disks
             for (size_t idx = 0; idx < state.Disks.size(); ++idx) {
-                state.Disks[idx].IsSlow = slowDiskSubgroupMask & (1 << idx);
+                state.Disks[idx].IsSlow = false;
+            }
+
+            for (size_t idx = 0; idx < 2; ++idx) {
+                bool isSlow = slowDiskSubgroupMask & (1 << idx);
+                state.Disks[worstDisks[idx].DiskIdx].IsSlow = isSlow;
             }
             return slowDiskSubgroupMask;
         }

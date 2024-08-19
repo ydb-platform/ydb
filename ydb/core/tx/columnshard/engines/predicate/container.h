b41b81bfa1f0f7d636553c706a90cded3ef1fe0c
ivanmorozov333 <ivanmorozov@ydb.tech>
2024-08-19 13:05:19 +0300

    refactoring for ranges control (#7758)

--- ydb/core/tx/columnshard/engines/predicate/container.h
+++ ydb/core/tx/columnshard/engines/predicate/container.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "predicate.h"
+
 #include <ydb/core/formats/arrow/arrow_filter.h>
 #include <ydb/core/formats/arrow/replace_key.h>
+
 #include <ydb/library/accessor/accessor.h>
+
 #include <contrib/libs/apache/arrow/cpp/src/arrow/record_batch.h>
+
 #include <optional>
 
 namespace NKikimr::NOlap {
@@ -45,6 +49,9 @@ private:
     }
 
 public:
+    NArrow::ECompareType GetCompareType() const {
+        return CompareType;
+    }
 
     const std::shared_ptr<NArrow::TReplaceKey>& GetReplaceKey() const {
         return ReplaceKey;
@@ -55,8 +62,8 @@ public:
     }
 
     template <class TArrayColumn>
-    std::optional<typename TArrayColumn::value_type> Get(const ui32 colIndex, const ui32 rowIndex,
-        const std::optional<typename TArrayColumn::value_type> defaultValue = {}) const {
+    std::optional<typename TArrayColumn::value_type> Get(
+        const ui32 colIndex, const ui32 rowIndex, const std::optional<typename TArrayColumn::value_type> defaultValue = {}) const {
         if (!Object) {
             return defaultValue;
         } else {
@@ -80,13 +87,15 @@ public:
         return TPredicateContainer(NArrow::ECompareType::GREATER_OR_EQUAL);
     }
 
-    static std::optional<TPredicateContainer> BuildPredicateFrom(std::shared_ptr<NOlap::TPredicate> object, const TIndexInfo* indexInfo);
+    static TConclusion<TPredicateContainer> BuildPredicateFrom(
+        std::shared_ptr<NOlap::TPredicate> object, const std::shared_ptr<arrow::Schema>& pkSchema);
 
     static TPredicateContainer BuildNullPredicateTo() {
         return TPredicateContainer(NArrow::ECompareType::LESS_OR_EQUAL);
     }
 
-    static std::optional<TPredicateContainer> BuildPredicateTo(std::shared_ptr<NOlap::TPredicate> object, const TIndexInfo* indexInfo);
+    static TConclusion<TPredicateContainer> BuildPredicateTo(
+        std::shared_ptr<NOlap::TPredicate> object, const std::shared_ptr<arrow::Schema>& pkSchema);
 
     NKikimr::NArrow::TColumnFilter BuildFilter(const arrow::Datum& data) const {
         if (!Object) {
@@ -96,4 +105,4 @@ public:
     }
 };
 
-}
+}   // namespace NKikimr::NOlap

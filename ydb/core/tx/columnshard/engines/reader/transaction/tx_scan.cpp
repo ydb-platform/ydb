4226868442127bba8bbbf2bc15f4a67b6e3fb1fb
ivanmorozov333 <ivanmorozov@ydb.tech>
2024-08-19 13:05:19 +0300

    refactoring for ranges control (#7758)

--- ydb/core/tx/columnshard/engines/reader/transaction/tx_scan.cpp
+++ ydb/core/tx/columnshard/engines/reader/transaction/tx_scan.cpp
@@ -8,112 +8,6 @@
 
 namespace NKikimr::NOlap::NReader {
 
-std::vector<NScheme::TTypeInfo> ExtractTypes(const std::vector<std::pair<TString, NScheme::TTypeInfo>>& columns) {
-    std::vector<NScheme::TTypeInfo> types;
-    types.reserve(columns.size());
-    for (auto& [name, type] : columns) {
-        types.push_back(type);
-    }
-    return types;
-}
-
-TString FromCells(const TConstArrayRef<TCell>& cells, const std::vector<std::pair<TString, NScheme::TTypeInfo>>& columns) {
-    Y_ABORT_UNLESS(cells.size() == columns.size());
-    if (cells.empty()) {
-        return {};
-    }
-
-    std::vector<NScheme::TTypeInfo> types = ExtractTypes(columns);
-
-    NArrow::TArrowBatchBuilder batchBuilder;
-    batchBuilder.Reserve(1);
-    auto startStatus = batchBuilder.Start(columns);
-    Y_ABORT_UNLESS(startStatus.ok(), "%s", startStatus.ToString().c_str());
-
-    batchBuilder.AddRow(NKikimr::TDbTupleRef(), NKikimr::TDbTupleRef(types.data(), cells.data(), cells.size()));
-
-    auto batch = batchBuilder.FlushBatch(false);
-    Y_ABORT_UNLESS(batch);
-    Y_ABORT_UNLESS(batch->num_columns() == (int)cells.size());
-    Y_ABORT_UNLESS(batch->num_rows() == 1);
-    return NArrow::SerializeBatchNoCompression(batch);
-}
-
-std::pair<TPredicate, TPredicate> RangePredicates(const TSerializedTableRange& range, const std::vector<std::pair<TString, NScheme::TTypeInfo>>& columns) {
-    std::vector<TCell> leftCells;
-    std::vector<std::pair<TString, NScheme::TTypeInfo>> leftColumns;
-    bool leftTrailingNull = false;
-    {
-        TConstArrayRef<TCell> cells = range.From.GetCells();
-        const size_t size = cells.size();
-        Y_ASSERT(size <= columns.size());
-        leftCells.reserve(size);
-        leftColumns.reserve(size);
-        for (size_t i = 0; i < size; ++i) {
-            if (!cells[i].IsNull()) {
-                leftCells.push_back(cells[i]);
-                leftColumns.push_back(columns[i]);
-                leftTrailingNull = false;
-            } else {
-                leftTrailingNull = true;
-            }
-        }
-    }
-
-    std::vector<TCell> rightCells;
-    std::vector<std::pair<TString, NScheme::TTypeInfo>> rightColumns;
-    bool rightTrailingNull = false;
-    {
-        TConstArrayRef<TCell> cells = range.To.GetCells();
-        const size_t size = cells.size();
-        Y_ASSERT(size <= columns.size());
-        rightCells.reserve(size);
-        rightColumns.reserve(size);
-        for (size_t i = 0; i < size; ++i) {
-            if (!cells[i].IsNull()) {
-                rightCells.push_back(cells[i]);
-                rightColumns.push_back(columns[i]);
-                rightTrailingNull = false;
-            } else {
-                rightTrailingNull = true;
-            }
-        }
-    }
-
-    const bool fromInclusive = range.FromInclusive || leftTrailingNull;
-    const bool toInclusive = range.ToInclusive && !rightTrailingNull;
-
-    TString leftBorder = FromCells(leftCells, leftColumns);
-    TString rightBorder = FromCells(rightCells, rightColumns);
-    auto leftSchema = NArrow::MakeArrowSchema(leftColumns);
-    Y_ASSERT(leftSchema.ok());
-    auto rightSchema = NArrow::MakeArrowSchema(rightColumns);
-    Y_ASSERT(rightSchema.ok());
-    return std::make_pair(
-        TPredicate(fromInclusive ? NKernels::EOperation::GreaterEqual : NKernels::EOperation::Greater, leftBorder, leftSchema.ValueUnsafe()),
-        TPredicate(toInclusive ? NKernels::EOperation::LessEqual : NKernels::EOperation::Less, rightBorder, rightSchema.ValueUnsafe()));
-}
-
-static bool FillPredicatesFromRange(TReadDescription& read, const ::NKikimrTx::TKeyRange& keyRange,
-    const std::vector<std::pair<TString, NScheme::TTypeInfo>>& ydbPk, ui64 tabletId, const TIndexInfo* indexInfo, TString& error) {
-    TSerializedTableRange range(keyRange);
-    auto fromPredicate = std::make_shared<TPredicate>();
-    auto toPredicate = std::make_shared<TPredicate>();
-    std::tie(*fromPredicate, *toPredicate) = RangePredicates(range, ydbPk);
-
-    LOG_S_DEBUG("TTxScan range predicate. From key size: " << range.From.GetCells().size()
-        << " To key size: " << range.To.GetCells().size()
-        << " greater predicate over columns: " << fromPredicate->ToString()
-        << " less predicate over columns: " << toPredicate->ToString()
-        << " at tablet " << tabletId);
-
-    if (!read.PKRangesFilter.Add(fromPredicate, toPredicate, indexInfo)) {
-        error = "Error building filter";
-        return false;
-    }
-    return true;
-}
-
 void TTxScan::SendError(const TString& problem, const TString& details, const TActorContext& ctx) const {
     AFL_WARN(NKikimrServices::TX_COLUMNSHARD_SCAN)("event", "TTxScan failed")("problem", problem)("details", details);
     const auto& request = Ev->Get()->Record;
@@ -190,12 +84,12 @@ void TTxScan::Complete(const TActorContext& ctx) {
             }
         } else {
             auto ydbKey = scannerConstructor->GetPrimaryKeyScheme(Self);
-            auto* indexInfo = (vIndex && isIndex) ? &vIndex->GetSchema(snapshot)->GetIndexInfo() : nullptr;
-            for (auto& range : request.GetRanges()) {
-                TString errorDescription;
-                if (!FillPredicatesFromRange(read, range, ydbKey, Self->TabletID(), indexInfo, errorDescription)) {
-                    return SendError("cannot fill predicates", errorDescription, ctx);
+            {
+                auto filterConclusion = NOlap::TPKRangesFilter::BuildFromProto(request, request.GetReverse(), ydbKey);
+                if (filterConclusion.IsFail()) {
+                    return SendError("cannot build ranges filter", filterConclusion.GetErrorMessage(), ctx);
                 }
+                read.PKRangesFilter = filterConclusion.DetachResult();
             }
             auto newRange = scannerConstructor->BuildReadMetadata(Self, read);
             if (!newRange) {

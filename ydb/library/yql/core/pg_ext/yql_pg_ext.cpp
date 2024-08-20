9bdf96f51dde97e6d4414eeb590ae3fcc8616e43
Vitaly Stoyan <vvvv@ydb.tech>
2024-08-20 16:59:02 +0000

    init

--- ydb/library/yql/core/pg_ext/yql_pg_ext.cpp
+++ ydb/library/yql/core/pg_ext/yql_pg_ext.cpp
@@ -16,6 +16,7 @@ void PgExtensionsFromProto(const NYql::NProto::TPgExtensions& proto,
         desc.LibraryPath = e.GetLibraryPath();
         desc.TypesOnly = e.GetTypesOnly();
         desc.LibraryMD5 = e.GetLibraryMD5();
+        desc.Version = e.GetVersion();
         extensions.emplace_back(desc);
     }
 }

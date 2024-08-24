bddf866e4676939687f5ce55fe3282dcb32ecc71
ivanmorozov333 <ivanmorozov@ydb.tech>
2024-08-24 08:15:09 +0300

    dont create temporary sparsed array on merging (#8174)
    Conflicts:
    	ydb/core/tx/columnshard/engines/changes/compaction/abstract/merger.cpp

--- ydb/core/tx/columnshard/engines/changes/compaction/abstract/merger.cpp
+++ ydb/core/tx/columnshard/engines/changes/compaction/abstract/merger.cpp
@@ -5,10 +5,13 @@ namespace NKikimr::NOlap::NCompaction {
 void IColumnMerger::Start(const std::vector<std::shared_ptr<NArrow::NAccessor::IChunkedArray>>& input) {
     AFL_VERIFY(!Started);
     Started = true;
-    // for (auto&& i : input) {
-    //     AFL_VERIFY(i->GetDataType()->id() == Context.GetResultField()->type()->id())("input", i->GetDataType()->ToString())(
-    //                                              "result", Context.GetResultField()->ToString());
-    // }
+    for (auto&& i : input) {
+        if (!i) {
+            continue;
+        }
+        AFL_VERIFY(i->GetDataType()->id() == Context.GetResultField()->type()->id())("input", i->GetDataType()->ToString())(
+                                                 "result", Context.GetResultField()->ToString());
+    }
     return DoStart(input);
 }
 

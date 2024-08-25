583dbadf66144ddcd8312b1b1807b9a83697d3fc
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-08-25 21:04:12 +0300

    fix

--- ydb/core/tx/columnshard/columnshard_impl.h
+++ ydb/core/tx/columnshard/columnshard_impl.h
@@ -468,7 +468,7 @@ private:
     NOlap::NResourceBroker::NSubscribe::TTaskContext CompactTaskSubscription;
     NOlap::NResourceBroker::NSubscribe::TTaskContext TTLTaskSubscription;
 
-    std::optional<ui64> ProgressTxInFlight;
+    bool ProgressTxInFlight = false;
     THashMap<ui64, TInstant> ScanTxInFlight;
     THashMap<TWriteId, TLongTxWriteInfo> LongTxWrites;
     using TPartsForLTXShard = THashMap<ui32, TLongTxWriteInfo*>;

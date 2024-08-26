b030ba695194e10f2f50d2033f4983150f7ce7bf
Alexey Ozeritskiy <aozeritsky@ydb.tech>
2024-08-26 16:59:18 +0300

    Fix

--- ydb/library/yql/tests/sql/suites/blocks/decimal_comparison.sql
+++ ydb/library/yql/tests/sql/suites/blocks/decimal_comparison.sql
@@ -2,18 +2,21 @@ USE plato;
 
 SELECT
     cs_ext_list_price == cs_ext_tax,
+    cs_ext_list_price != cs_ext_tax,
     cs_ext_list_price < cs_ext_tax,
     cs_ext_list_price <= cs_ext_tax,
     cs_ext_list_price > cs_ext_tax,
     cs_ext_list_price >= cs_ext_tax,
 
     cs_ext_tax == decimal("26.91", 7, 2),
+    cs_ext_tax != decimal("26.91", 7, 2),
     cs_ext_tax < decimal("26.91", 7, 2),
     cs_ext_tax <= decimal("26.91", 7, 2),
     cs_ext_tax > decimal("26.91", 7, 2),
     cs_ext_tax >= decimal("26.91", 7, 2),
 
     decimal("26.91", 7, 2) == cs_ext_tax,
+    decimal("26.91", 7, 2) != cs_ext_tax,
     decimal("26.91", 7, 2) < cs_ext_tax,
     decimal("26.91", 7, 2) <= cs_ext_tax,
     decimal("26.91", 7, 2) > cs_ext_tax,

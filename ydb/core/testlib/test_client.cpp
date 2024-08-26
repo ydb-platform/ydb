9f0ca3445ddb156714b9e0256a04a34e14fc2614
Alexey Efimov <xeno@ydb.tech>
2024-08-26 13:41:54 +0200

    optimize nodes handler (#8111)

--- ydb/core/testlib/test_client.cpp
+++ ydb/core/testlib/test_client.cpp
@@ -606,7 +606,12 @@ namespace Tests {
 
         NKikimrBlobStorage::TDefineHostConfig hostConfig;
         hostConfig.SetHostConfigId(nodeId);
-        TString path = TStringBuilder() << Runtime->GetTempDir() << "pdisk_1.dat";
+        TString path;
+        if (Settings->UseSectorMap) {
+            path ="SectorMap:test-client[:2000]";
+        } else {
+            path = TStringBuilder() << Runtime->GetTempDir() << "pdisk_1.dat";
+        }
         hostConfig.AddDrive()->SetPath(path);
         if (Settings->Verbose) {
             Cerr << "test_client.cpp: SetPath # " << path << Endl;

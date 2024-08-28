e1a983355ebb6696b2c807f6f837b4051616fef2
Alexey Efimov <xeno@ydb.tech>
2024-08-28 06:37:25 +0200

    add storage/groups latencies (#8320)

--- ydb/core/viewer/json_handlers_storage.cpp
+++ ydb/core/viewer/json_handlers_storage.cpp
@@ -4,7 +4,7 @@
 namespace NKikimr::NViewer {
 
 void InitStorageGroupsJsonHandler(TJsonHandlers& jsonHandlers) {
-    jsonHandlers.AddHandler("/storage/groups", new TJsonHandler<TStorageGroups>(TStorageGroups::GetSwagger()), 2);
+    jsonHandlers.AddHandler("/storage/groups", new TJsonHandler<TStorageGroups>(TStorageGroups::GetSwagger()), 3);
 }
 
 void InitStorageJsonHandlers(TJsonHandlers& jsonHandlers) {

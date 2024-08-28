fd59a004670cb79bf0a236c6aead069cc23227bc
Vladislav Lukachik <vladluk@ydb.tech>
2024-08-28 13:00:18 +0000

    make flake happy

--- ydb/library/benchmarks/runner/run_tests/run_tests.py
+++ ydb/library/benchmarks/runner/run_tests/run_tests.py
@@ -10,17 +10,19 @@ def variant(string):
         raise ValueError("variant must be h or ds")
     return string
 
+
 def paths(string):
     return list(map(pathlib.Path, string.split(";")))
 
+
 def parse_args(passed=None):
-    
+
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument('--datasize', type=int, default=1)
     parser.add_argument('--variant', type=variant, default='h')
     parser.add_argument('--tasks', type=int, default=1)
-    
+
     parser.add_argument('--dqrun', type=pathlib.Path)
     parser.add_argument('--gen-queries', type=pathlib.Path)
     parser.add_argument('--downloaders-dir', type=pathlib.Path)
@@ -30,12 +32,13 @@ def parse_args(passed=None):
     parser.add_argument('--result-compare', type=pathlib.Path)
     parser.add_argument('--gateways-cfg', type=pathlib.Path)
     parser.add_argument('--runner-path', type=pathlib.Path)
-    
+
     parser.add_argument('-o', '--output', default="./results")
     parser.add_argument('--clean-old', action="store_true", default=False)
     parser.add_argument('--query-filter', action="append", default=[])
-    
+
     return parser.parse_args(passed)
+
 
 class Runner:
     def prepare_queries_dir(self, custom_pragmas):
@@ -49,16 +52,16 @@ class Runner:
         for it in custom_pragmas:
             cmd += ["--pragma", it]
         subprocess.run(cmd)
-    
+
     def prepare_tpc_dir(self):
         print("Preparing tpc...", file=stderr)
         cmd = [f"{self.args.downloaders_dir}/download_files_{self.args.variant}_{self.args.datasize}.sh"]
         subprocess.run(cmd)
-    
+
     def __init__(self, args, enable_spilling):
         self.args = args
         self.enable_spilling = enable_spilling
-        
+
         self.queries_dir = pathlib.Path(f"queries{"+" if self.enable_spilling else "-"}spilling-{args.datasize}-{args.tasks}").resolve()
         if self.args.clean_old or not self.queries_dir.exists():
             self.prepare_queries_dir([
@@ -68,13 +71,13 @@ class Runner:
                 "dq.UseFinalizeByKey=true",
                 "dq.EnableSpillingNodes=All",
             ] if self.enable_spilling else [])
-        
+
         self.tpc_dir = pathlib.Path(f"{self.args.downloaders_dir}/tpc/{self.args.variant}/{self.args.datasize}").resolve()
         if self.args.clean_old or not self.tpc_dir.exists():
             self.prepare_tpc_dir()
         if not pathlib.Path("./tpc").exists():
             os.symlink(f"{self.args.downloaders_dir}/tpc", f"{pathlib.Path("./tpc")}", target_is_directory=True)
-        
+
         self.result_dir = pathlib.Path(f"{self.args.output}/{"with" if self.enable_spilling else "no"}-spilling/{args.variant}-{args.datasize}-{args.tasks}").resolve()
         self.result_dir.mkdir(parents=True, exist_ok=True)
 
@@ -94,8 +97,9 @@ class Runner:
         cmd += ["--gateways-cfg", f"{str(self.args.gateways_cfg)}"]
         print("Running runner...", file=stderr)
         subprocess.run(cmd)
-        
+
         return self.result_dir
+
 
 def result_compare(args, to_compare):
     cmd = [f"{args.result_compare}"]
@@ -104,19 +108,22 @@ def result_compare(args, to_compare):
     with open(f"{args.output}/result-{args.variant}-{args.datasize}-{args.tasks}.htm", "w") as result_table:
         subprocess.run(cmd, stdout=result_table)
 
+
 def run(passed=None):
     args = parse_args(passed)
-    
+
     results = []
     print("With spilling...", file=stderr)
     results.append(Runner(args, True).run())
     print("No spilling...", file=stderr)
     results.append(Runner(args, False).run())
-    
+
     result_compare(args, results)
+
 
 def main():
     run()
+
 
 if __name__ == "__main__":
     main()

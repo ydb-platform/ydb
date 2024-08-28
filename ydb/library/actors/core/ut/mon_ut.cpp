d8d2a114727bb90e2e1e6b2cb660d5f7fccc3e38
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2024-08-28 10:11:40 +0000

    Check clearer

--- ydb/library/actors/core/ut/mon_ut.cpp
+++ ydb/library/actors/core/ut/mon_ut.cpp
@@ -22,6 +22,8 @@ Y_UNIT_TEST_SUITE(ActorSystemMon) {
         std::unique_ptr<TEvRemoteHttpInfo> restored(dynamic_cast<TEvRemoteHttpInfo*>(TEvRemoteHttpInfo::Load(buffer.Get())));
         UNIT_ASSERT(restored->Query == ev->Query);
         UNIT_ASSERT(restored->Query.size());
+        UNIT_ASSERT(1);
+        UNIT_ASSERT(0);
         UNIT_ASSERT(restored->Query[0] == '\0');
         UNIT_ASSERT(restored->ExtendedQuery);
         UNIT_ASSERT_VALUES_EQUAL(restored->ExtendedQuery->GetPath(), ev->ExtendedQuery->GetPath());

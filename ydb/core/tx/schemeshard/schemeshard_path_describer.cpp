9e7109e5de9edf19ffd5029110309542f3590474
Semyon <yentsovsemyon@ydb.tech>
2024-08-29 17:21:13 +0300

    Fix table stats aggregation for OLAP tables (#8417)

--- ydb/core/tx/schemeshard/schemeshard_path_describer.cpp
+++ ydb/core/tx/schemeshard/schemeshard_path_describer.cpp
@@ -479,7 +479,7 @@ void TPathDescriber::DescribeColumnTable(TPathId pathId, TPathElement::TPtr path
             description->MutableSchema()->SetVersion(description->GetSchema().GetVersion() + description->GetSchemaPresetVersionAdj());
         }
         if (tableInfo->GetStats().TableStats.contains(pathId)) {
-            FillTableStats(*pathDescription, tableInfo->GetStats().TableStats.at(pathId));
+            FillTableStats(*pathDescription, tableInfo->GetStats().TableStats.at(pathId).Aggregated);
         } else {
             FillTableStats(*pathDescription, TPartitionStats());
         }

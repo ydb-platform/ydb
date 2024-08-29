03de164552711e2af1ca90599cee43e7f2237df4
Nikita-Levuskin <81382697+Nikita-Levuskin@users.noreply.github.com>
2024-08-29 19:24:25 +0300

    Update __init__.py

--- ydb/public/tools/lib/cmds/__init__.py
+++ ydb/public/tools/lib/cmds/__init__.py
@@ -352,7 +352,6 @@ def deploy(arguments):
             'mon_port': node.mon_port,
             'command': node.command,
             'cwd': node.cwd,
-            'stdin_file': node.stdin_file_name,
             'stderr_file': node.stderr_file_name,
             'stdout_file': node.stdout_file_name,
             'pdisks': [
@@ -435,7 +434,6 @@ def start(arguments):
         files = {}
         if node_meta['stderr_file'] is not None and os.path.exists(node_meta['stderr_file']):
             files = {
-                'stdin_file': node_meta['stdin_file'],
                 'stderr_file': node_meta['stderr_file'],
                 'stdout_file': node_meta['stdout_file'],
             }

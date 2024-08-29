3985116d42f5629a430f1b05a5de3186e77e37b6
yumkam <yumkam7@ydb.tech>
2024-08-29 10:12:45 +0300

    tests/fq: add streamlookup (#7568)

--- ydb/tests/fq/generic/ydb/01_basic.sh
+++ ydb/tests/fq/generic/ydb/01_basic.sh
@@ -18,6 +18,23 @@ set -ex
       (2, "ydb20"),
       (3, "ydb30");
     COMMIT;
+    CREATE TABLE users (age Int32, id Int32, ip STRING, name STRING, region Int32, PRIMARY KEY(id));
+    COMMIT;
+    INSERT INTO users (age, id, ip, name, region) VALUES
+      (15, 1, "95.106.17.32", "Anya", 213),
+      (25, 2, "88.78.248.151", "Petr", 225),
+      (17, 3, "93.94.183.63", "Masha", 1),
+      (5, 4, "::ffff:193.34.173.188", "Alena", 225),
+      (15, 5, "93.170.111.29", "Irina", 2),
+      (13, 6, "93.170.111.28", "Inna", 21),
+      (33, 7, "::ffff:193.34.173.173", "Ivan", 125),
+      (45, 8, "::ffff:133.34.173.188", "Asya", 225),
+      (27, 9, "::ffff:133.34.172.188", "German", 125),
+      (41, 10, "::ffff:133.34.173.185", "Olya", 225),
+      (35, 11, "::ffff:193.34.163.188", "Slava", 2),
+      (56, 12, "2a02:1812:1713:4f00:517e:1d79:c88b:704", "Elena", 2),
+      (18, 17, "ivalid ip", "newUser", 12);
+    COMMIT;
   '
 
 retVal=$?

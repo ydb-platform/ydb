85e6d2e544b40a8f84904db938ea8aab90ed4d70
Vitaly Isaev <vitalyisaev@ydb.tech>
2024-08-29 22:38:08 +0000

    stabilize tests

--- ydb/tests/fq/generic/greenplum/init_db.sh
+++ ydb/tests/fq/generic/greenplum/init_db.sh
@@ -3,7 +3,7 @@ set -e
 
 psql -p 6432 -v ON_ERROR_STOP=1 --username gpadmin --dbname template1 <<-EOSQL
     CREATE TABLE simple_table (number INT);
-    INSERT INTO simple_table VALUES ((1)), ((2)), ((3));
+    INSERT INTO simple_table VALUES ((3)), ((14)), ((15));
 
     CREATE TABLE join_table (id INT, data bytea);
     INSERT INTO join_table VALUES (1, 'gp10'), (2, 'gp20'), (3, 'gp30');

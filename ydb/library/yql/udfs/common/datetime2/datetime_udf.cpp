ydb-platform/ydb

commit 8064a1b3f6cae4913bccd5366cb897d8196180ef

Make error for impossible TzDate casts more friendly (#5805)

ydb/library/yql/udfs/common/datetime2/datetime_udf.cpp
@@ -694,9 +694,16 @@ NUdf::TUnboxedValuePod DoAddYears(const NUdf::TUnboxedValuePod& date, i64 years,
     SIMPLE_STRICT_UDF(TMakeTzDate, TTzDate(TAutoMap<TResource<TMResourceName>>)) {
         auto& builder = valueBuilder->GetDateBuilder();
         auto& storage = Reference(args[0]);
-        TUnboxedValuePod result(storage.ToDate(builder, true));
-        result.SetTimezoneId(storage.TimezoneId);
-        return result;
+        try {
+            TUnboxedValuePod result(storage.ToDate(builder, true));
+            result.SetTimezoneId(storage.TimezoneId);
+            return result;
+        } catch (const std::exception& e) {
+            UdfTerminate((TStringBuilder() << Pos_ << "Timestamp "
+                                           << storage.ToString()
+                                           << " cannot be casted to TzDate"
+            ).data());
+        }
     }
 
     SIMPLE_STRICT_UDF(TMakeTzDatetime, TTzDatetime(TAutoMap<TResource<TMResourceName>>)) {

ydb/library/yql/tests/sql/yt_native_file/part18/canondata/test.test_datetime-date_tz_impossible_cast--Results_/extracted
@@ -1,0 +1,8 @@
+<tmp_path>/program.sql:<main>: Fatal: Execution
+
+    <tmp_path>/program.sql:<main>:1:1: Fatal: Execution of node: Result
+    	SELECT
+	^
+        <tmp_path>/program.sql:<main>:2:5: Fatal: Timestamp 1970-01-01T23:59:59.000000,Europe/Moscow cannot be casted to TzDate
+        	    CAST(AddTimezone(
+	    ^

ydb/library/yql/minikql/datetime/datetime.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <ydb/library/yql/public/udf/udf_value_builder.h>
+#include <ydb/library/yql/public/udf/tz/udf_tz.h>
 
 #include <util/datetime/base.h>
+#include <util/string/printf.h>
 
 namespace NYql::DateTime {
 
@@ -145,6 +147,13 @@ struct TTMStorage {
 
     inline ui64 ToTimeOfDay() const {
         return ((Hour * 60ull + Minute) * 60ull + Second) * 1000000ull + Microsecond;
+    }
+
+    const TString ToString() const {
+        const auto& tzName = NUdf::GetTimezones()[TimezoneId];
+        return Sprintf("%4d-%02d-%02dT%02d:%02d:%02d.%06d,%.*s",
+                       Year, Month, Day, Hour, Minute, Second, Microsecond,
+                       static_cast<int>(tzName.size()), tzName.data());
     }
 };
 

ydb/library/yql/tests/sql/suites/datetime/date_tz_impossible_cast.cfg
@@ -1,0 +1,1 @@
+xfail

ydb/library/yql/tests/sql/suites/datetime/date_tz_impossible_cast.sql
@@ -1,0 +1,6 @@
+SELECT
+    CAST(AddTimezone(
+        /* "1970-01-01T20:59:59Z" */
+        CAST(75599 AS DateTime),
+        "Europe/Moscow"
+    ) AS TzDate)

commit c6da778b9089eb5c3d8ea851e1a6b5bf2d19eec6

Make error for impossible TzDate casts more friendly (again)
The patch introduced in #5805 was reverted by mistake in #6472. This
patch returns the user friendly error with diagnostics back again.

Follows up #6472
Relates to #5805

ydb/library/yql/udfs/common/datetime2/datetime_udf.cpp
@@ -848,9 +848,16 @@ TValue DoAddYears(const TValue& date, i64 years, const NUdf::IDateBuilder& build
     BEGIN_SIMPLE_STRICT_ARROW_UDF(TMakeTzDate, TTzDate(TAutoMap<TResource<TMResourceName>>)) {
         auto& builder = valueBuilder->GetDateBuilder();
         auto& storage = Reference(args[0]);
-        TUnboxedValuePod result(storage.ToDate(builder, true));
-        result.SetTimezoneId(storage.TimezoneId);
-        return result;
+        try {
+            TUnboxedValuePod result(storage.ToDate(builder, true));
+            result.SetTimezoneId(storage.TimezoneId);
+            return result;
+        } catch (const std::exception& e) {
+            UdfTerminate((TStringBuilder() << Pos_ << "Timestamp "
+                                           << storage.ToString()
+                                           << " cannot be casted to TzDate"
+            ).data());
+        }
     }
     END_SIMPLE_ARROW_UDF(TMakeTzDate, TMakeDateKernelExec<TTzDate>::Do);
 

ydb/library/yql/tests/sql/yt_native_file/part18/canondata/test.test_datetime-date_tz_impossible_cast--Results_/extracted
@@ -3,7 +3,6 @@
     <tmp_path>/program.sql:<main>:1:1: Fatal: Execution of node: Result
     	SELECT
 	^
-        <tmp_path>/program.sql:<main>:2:5: Fatal: (yexception) ydb/library/yql/minikql/datetime/datetime.h:xxx: Error in MakeDatetime
-[MakeTzDate]
+        <tmp_path>/program.sql:<main>:2:5: Fatal: Timestamp 1970-01-01T23:59:59.000000,Europe/Moscow cannot be casted to TzDate
         	    CAST(AddTimezone(
 	    ^
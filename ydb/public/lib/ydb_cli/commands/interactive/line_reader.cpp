59c4040662775738c8caaec23c621eb3f9cd74f9
Nikolay Perfilov <pnv1@yandex-team.ru>
2024-09-03 11:59:10 +0300

    Always disconnect on Ctrl+D

--- ydb/public/lib/ydb_cli/commands/interactive/line_reader.cpp
+++ ydb/public/lib/ydb_cli/commands/interactive/line_reader.cpp
@@ -100,6 +100,8 @@ TLineReader::TLineReader(std::string prompt, std::string historyFilePath, Sugges
     Rx.bind_key(replxx::Replxx::KEY::control('N'), [&](char32_t code) { return Rx.invoke(replxx::Replxx::ACTION::HISTORY_NEXT, code); });
     Rx.bind_key(replxx::Replxx::KEY::control('P'), [&](char32_t code) { return Rx.invoke(replxx::Replxx::ACTION::HISTORY_PREVIOUS, code); });
     Rx.bind_key(replxx::Replxx::KEY::control('C'), [](char32_t) { return replxx::Replxx::ACTION_RESULT::BAIL; });
+    // Always disconnect on Ctrl+D (built-in implementation doesn't work if there is already any input text)
+    Rx.bind_key(replxx::Replxx::KEY::control('D'), [](char32_t) { return replxx::Replxx::ACTION_RESULT::BAIL; });
     auto commit_action = [&](char32_t code) {
         return Rx.invoke(replxx::Replxx::ACTION::COMMIT_LINE, code);
     };

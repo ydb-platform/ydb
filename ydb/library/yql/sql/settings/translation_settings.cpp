33b9933362dc13c796b6e1fb21ac25f33448b459
Nikolay Shumkov <shumkovnd@ydb.tech>
2024-09-06 00:32:55 +0300

    Add EnablePgSyntax (#8796)

--- ydb/library/yql/sql/settings/translation_settings.cpp
+++ ydb/library/yql/sql/settings/translation_settings.cpp
@@ -57,6 +57,7 @@ namespace NSQLTranslation {
         , InferSyntaxVersion(false)
         , V0Behavior(EV0Behavior::Disable)
         , V0ForceDisable(InTestEnvironment())
+        , PGDisable(false)
         , WarnOnV0(true)
         , V0WarnAsError(ISqlFeaturePolicy::MakeAlwaysDisallow())
         , DqDefaultAuto(ISqlFeaturePolicy::MakeAlwaysDisallow())

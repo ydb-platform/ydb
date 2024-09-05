20daceabc853095fc97b1f4b2f1ec9464b4d1046
Vlad Kuznecov <va-kuznecov@nebius.com>
2024-09-05 17:37:11 +0000

    Fix PDisk's ColorBorderOccupancy calculation

--- ydb/core/blobstorage/pdisk/blobstorage_pdisk_config.h
+++ ydb/core/blobstorage/pdisk/blobstorage_pdisk_config.h
@@ -304,6 +304,8 @@ struct TPDiskConfig : public TThrRefBase {
         str << " OrangeLogChunksMultiplier# " << OrangeLogChunksMultiplier << x;
         str << " WarningLogChunksMultiplier# " << WarningLogChunksMultiplier << x;
         str << " YellowLogChunksMultiplier# " << YellowLogChunksMultiplier << x;
+        str << " MaxMetadataMegabytes# " << MaxMetadataMegabytes << x;
+        str << " SpaceColorBorder# " << SpaceColorBorder << x;
         str << "}";
         return str.Str();
     }

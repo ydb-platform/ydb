cb6a33d90405395afaebf6de999efeb187f8f8e6
apollo1321 <apollo1321@yandex-team.com>
2024-09-06 11:24:31 +0300

    YT-21709: Empty template for shuffle service
    YT-21709: Empty template for shuffle service
    6dde8f6e6f5eeb4fc7ad91fe623e980439565545

--- yt/yt/client/api/rpc_proxy/client_impl.cpp
+++ yt/yt/client/api/rpc_proxy/client_impl.cpp
@@ -2628,6 +2628,37 @@ TFuture<TGetFlowViewResult> TClient::GetFlowView(
     }));
 }
 
+TFuture<TShuffleHandlePtr> TClient::StartShuffle(
+    const TString& /*account*/,
+    int /*partitionCount*/,
+    const TStartShuffleOptions& /*options*/)
+{
+    YT_UNIMPLEMENTED();
+}
+
+TFuture<void> TClient::FinishShuffle(
+    const TShuffleHandlePtr& /*shuffleHandle*/,
+    const TFinishShuffleOptions& /*options*/)
+{
+    YT_UNIMPLEMENTED();
+}
+
+TFuture<IRowBatchReaderPtr> TClient::CreateShuffleReader(
+    const TShuffleHandlePtr& /*shuffleHandle*/,
+    int /*partitionIndex*/,
+    const TTableReaderConfigPtr& /*config*/)
+{
+    YT_UNIMPLEMENTED();
+}
+
+TFuture<IRowBatchWriterPtr> TClient::CreateShuffleWriter(
+    const TShuffleHandlePtr& /*shuffleHandle*/,
+    const TString& /*partitionColumn*/,
+    const TTableWriterConfigPtr& /*config*/)
+{
+    YT_UNIMPLEMENTED();
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 
-} // namespace NYT::NRpcProxy
+} // namespace NYT::NApi::NRpcProxy

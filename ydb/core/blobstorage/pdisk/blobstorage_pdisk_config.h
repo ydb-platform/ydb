5e26354ada1897c80b497595aaa8ab498628c51d
Vlad Kuznecov <va-kuznecov@nebius.com>
2024-09-07 18:39:39 +0000

    Fix stream-nb-2 build, remove printing of unexisting field

--- ydb/core/blobstorage/pdisk/blobstorage_pdisk_config.h
+++ ydb/core/blobstorage/pdisk/blobstorage_pdisk_config.h
@@ -296,7 +296,6 @@ struct TPDiskConfig : public TThrRefBase {
         str << " OrangeLogChunksMultiplier# " << OrangeLogChunksMultiplier << x;
         str << " WarningLogChunksMultiplier# " << WarningLogChunksMultiplier << x;
         str << " YellowLogChunksMultiplier# " << YellowLogChunksMultiplier << x;
-        str << " MaxMetadataMegabytes# " << MaxMetadataMegabytes << x;
         str << " SpaceColorBorder# " << SpaceColorBorder << x;
         str << "}";
         return str.Str();

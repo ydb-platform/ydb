dacc580340308a730bf758df3f442490df1377a7
Alexey Ozeritskiy <aozeritsky@ydb.tech>
2024-09-07 13:33:54 +0200

    Add kernels

--- ydb/library/yql/minikql/comp_nodes/mkql_block_decimal_mul.cpp
+++ ydb/library/yql/minikql/comp_nodes/mkql_block_decimal_mul.cpp
@@ -38,6 +38,52 @@ struct TDecimalMulBlockExec {
         return NYql::NDecimal::IsNan(mul) ? NYql::NDecimal::Nan() : (mul > 0 ? +NYql::NDecimal::Inf() : -NYql::NDecimal::Inf());
     }
 
+    void ArrayScalarCore(
+        const NYql::NDecimal::TInt128* val1Ptr,
+        const ui8* valid1,
+        const NYql::NDecimal::TInt128* val2Ptr,
+        const ui8* valid2,
+        NYql::NDecimal::TInt128* resPtr,
+        ui8* resValid,
+        int64_t length,
+        int64_t offset1,
+        int64_t offset2) const {
+        val1Ptr += offset1;
+        Y_UNUSED(valid2);
+        Y_UNUSED(offset2);
+        for (int64_t i = 0; i < length; ++i, ++val1Ptr, ++resPtr) {
+            if (!valid1 || arrow::BitUtil::GetBit(valid1, i + offset1)) {
+                *resPtr = Do(*val1Ptr, *val2Ptr);
+                arrow::BitUtil::SetBit(resValid, i);
+            }
+
+            arrow::BitUtil::ClearBit(resValid, i);
+        }
+    }
+
+    void ScalarArrayCore(
+        const NYql::NDecimal::TInt128* val1Ptr,
+        const ui8* valid1,
+        const NYql::NDecimal::TInt128* val2Ptr,
+        const ui8* valid2,
+        NYql::NDecimal::TInt128* resPtr,
+        ui8* resValid,
+        int64_t length,
+        int64_t offset1,
+        int64_t offset2) const {
+        val2Ptr += offset2;
+        Y_UNUSED(valid1);
+        Y_UNUSED(offset1);
+        for (int64_t i = 0; i < length; ++i, ++val2Ptr, ++resPtr) {
+            if (!valid2 || arrow::BitUtil::GetBit(valid2, i + offset2)) {
+                *resPtr = Do(*val1Ptr, *val2Ptr);
+                arrow::BitUtil::SetBit(resValid, i);
+            }
+
+            arrow::BitUtil::ClearBit(resValid, i);
+        }
+    }
+
     void ArrayArrayCore(
         const NYql::NDecimal::TInt128* val1Ptr,
         const ui8* valid1,
@@ -58,6 +104,52 @@ struct TDecimalMulBlockExec {
                 arrow::BitUtil::SetBit(resValid, i);
             }
         }
+    }
+
+    arrow::Status ExecScalarArray(const arrow::compute::ExecBatch& batch, arrow::Datum* res) const
+    {
+        MKQL_ENSURE(batch.values.size() == 2, "Expected 2 args");
+        const auto& arg1 = batch.values[0];
+        const auto& arg2 = batch.values[1];
+        auto& resArr = *res->array();
+        if (arg1.scalar()->is_valid) {
+            const auto val1Ptr = reinterpret_cast<const NYql::NDecimal::TInt128*>(GetStringScalarValue(*arg1.scalar()).data());
+            const auto& arr2 = *arg2.array();
+            auto length = arr2.length;
+            const auto val2Ptr = reinterpret_cast<const NYql::NDecimal::TInt128*>(arr2.buffers[1]->data());
+            const auto nullCount2 = arr2.GetNullCount();
+            const auto valid2 = (nullCount2 == 0) ? nullptr : arr2.GetValues<uint8_t>(0);
+            auto resPtr = reinterpret_cast<NYql::NDecimal::TInt128*>(resArr.buffers[1]->mutable_data());
+            auto resValid = res->array()->GetMutableValues<uint8_t>(0);
+            ScalarArrayCore(val1Ptr, nullptr, val2Ptr, valid2, resPtr, resValid, length, 0, arr2.offset);
+        } else {
+            GetBitmap(resArr, 0).SetBitsTo(false);
+        }
+    
+        return arrow::Status::OK();
+    }
+
+    arrow::Status ExecArrayScalar(const arrow::compute::ExecBatch& batch, arrow::Datum* res) const
+    {
+        MKQL_ENSURE(batch.values.size() == 2, "Expected 2 args");
+        const auto& arg1 = batch.values[0];
+        const auto& arg2 = batch.values[1];
+        auto& resArr = *res->array();
+        if (arg2.scalar()->is_valid) {
+            const auto& arr1 = *arg1.array();
+            const auto val1Ptr = reinterpret_cast<const NYql::NDecimal::TInt128*>(arr1.buffers[1]->data());
+            auto length = arr1.length;
+            const auto nullCount1 = arr1.GetNullCount();
+            const auto valid1 = (nullCount1 == 0) ? nullptr : arr1.GetValues<uint8_t>(0);
+            const auto val2Ptr = reinterpret_cast<const NYql::NDecimal::TInt128*>(GetStringScalarValue(*arg2.scalar()).data());
+            auto resPtr = reinterpret_cast<NYql::NDecimal::TInt128*>(resArr.buffers[1]->mutable_data());
+            auto resValid = res->array()->GetMutableValues<uint8_t>(0);
+            ArrayScalarCore(val1Ptr, valid1, val2Ptr, nullptr, resPtr, resValid, length, arr1.offset, 0);
+        } else {
+            GetBitmap(resArr, 0).SetBitsTo(false);
+        }
+
+        return arrow::Status::OK();
     }
 
     arrow::Status ExecArrayArray(const arrow::compute::ExecBatch& batch, arrow::Datum* res) const {
@@ -92,13 +184,12 @@ struct TDecimalMulBlockExec {
             if (arg2.is_scalar()) {
                 // scalar-scalar
             } else {
-                // scalar-vector
+                return ExecScalarArray(batch, res);
             }
         } else {
             if (arg2.is_scalar()) {
-                // vector-scalar
+                return ExecArrayScalar(batch, res);
             } else {
-                // vector-vector
                 return ExecArrayArray(batch, res);
             }
         }

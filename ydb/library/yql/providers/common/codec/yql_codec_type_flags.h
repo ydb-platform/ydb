f11c0b20957bbb676ef18ca71b014c4d102de83d
Vadim Averin <avevad@ydb.tech>
2024-09-10 13:40:26 +0300

    Support type_v3 UUIDs (#8179)

--- ydb/library/yql/providers/common/codec/yql_codec_type_flags.h
+++ ydb/library/yql/providers/common/codec/yql_codec_type_flags.h
@@ -13,6 +13,7 @@ enum ENativeTypeCompatFlags: ui64 {
     NTCF_JSON       = 1ull << 5 /* "json" */,
     NTCF_DECIMAL    = 1ull << 6 /* "decimal" */,
     NTCF_BIGDATE    = 1ull << 7 /* "bigdate" */,
+    NTCF_UUID       = 1ull << 8 /* "uuid" */,
 
     NTCF_NO_YT_SUPPORT  = 1ull << 40 /* "_no_yt_support" */,
 
@@ -20,7 +21,7 @@ enum ENativeTypeCompatFlags: ui64 {
     NTCF_LEGACY = NTCF_COMPLEX | NTCF_DATE | NTCF_NULL | NTCF_VOID /* "legacy" */,
 
     // All supported types by all YT production clusters
-    NTCF_PRODUCTION = NTCF_COMPLEX | NTCF_DATE | NTCF_NULL | NTCF_VOID | NTCF_FLOAT | NTCF_JSON | NTCF_DECIMAL /* "production" */,
+    NTCF_PRODUCTION = NTCF_COMPLEX | NTCF_DATE | NTCF_NULL | NTCF_VOID | NTCF_FLOAT | NTCF_JSON | NTCF_DECIMAL | NTCF_UUID /* "production" */,
 
     // add all new types here, supported by YT
     NTCF_ALL = NTCF_PRODUCTION | NTCF_BIGDATE /* "all" */,

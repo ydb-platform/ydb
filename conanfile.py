3f40c8b97a112b89ca7f38b47ec04a6631486c6a
Dmitrii Dmitriev <dimdim11@yandex-team.ru>
2024-09-11 00:00:35 +0300

    Regenerate by new cmakegenerator

--- conanfile.py
+++ conanfile.py
@@ -5,16 +5,16 @@ class App(ConanFile):
 
     settings = "os", "compiler", "build_type", "arch"
 
-    options = {"libiconv:shared": "True"}
-
-    tool_requires = "bison/3.8.2", "m4/1.4.19", "ragel/6.10", "yasm/1.3.0"
+    default_options = {"libiconv:shared": "True"}
 
     def requirements(self):
-        requires = [ "libiconv/1.15" ]
-        for require in requires:
-            if ("linux-headers" in require) and (self.settings.os != "Linux"):
-                continue
-            self.requires(require)
+        self.requires("libiconv/1.15")
+
+    def build_requirements(self):
+        self.tool_requires("bison/3.8.2")
+        self.tool_requires("m4/1.4.19")
+        self.tool_requires("ragel/6.10")
+        self.tool_requires("yasm/1.3.0")
 
     generators = "cmake_find_package", "cmake_paths"
 

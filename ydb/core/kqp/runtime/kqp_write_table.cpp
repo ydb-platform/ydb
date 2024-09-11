cd7f618308a282c376cc71285881b83eda970e67
azevaykin <azevaykin@ydb.tech>
2024-09-11 17:27:36 +0000

    kqp_write_table fix

--- ydb/core/kqp/runtime/kqp_write_table.cpp
+++ ydb/core/kqp/runtime/kqp_write_table.cpp
@@ -61,15 +61,13 @@ TVector<TSysTables::TTableColumnInfo> BuildColumns(const TConstArrayRef<NKikimrK
     result.reserve(inputColumns.size());
     i32 number = 0;
     for (const auto& column : inputColumns) {
+        NScheme::TTypeInfo typeInfo = (column.GetTypeId() == NScheme::NTypeIds::Pg) ?
+            NScheme::TTypeInfo(column.GetTypeId(), NPg::TypeDescFromPgTypeId(column.GetTypeInfo().GetPgTypeId())) :
+            NScheme::TTypeInfo(column.GetTypeId());
         result.emplace_back(
             column.GetName(),
             column.GetId(),
-            NScheme::TTypeInfo {
-                static_cast<NScheme::TTypeId>(column.GetTypeId()),
-                column.GetTypeId() == NScheme::NTypeIds::Pg
-                    ? NPg::TypeDescFromPgTypeId(column.GetTypeInfo().GetPgTypeId())
-                    : nullptr
-            },
+            std::move(typeInfo),
             column.GetTypeInfo().GetPgTypeMod(),
             number++
         );

62a96b2938ccb2ab8f6f3e11ea26956a0f90f511
Igor Munkin <imunkin@ydb.tech>
2024-09-11 18:42:40 +0000

    Add helpers to build list from vectors

--- ydb/library/yql/minikql/comp_nodes/ut/mkql_block_map_join_ut.cpp
+++ ydb/library/yql/minikql/comp_nodes/ut/mkql_block_map_join_ut.cpp
@@ -308,6 +308,70 @@ void RunTestBlockJoinOnUint64(const TVector<TGotTupleType>& expected,
 }
 
 //
+// Auxiliary routines to build list nodes from the given vectors.
+//
+
+template<typename Type>
+const TVector<const TRuntimeNode> BuildListNodes(TProgramBuilder& pb,
+    const TVector<Type>& vector
+) {
+    TType* itemType;
+    if constexpr (std::is_same_v<Type, std::optional<TString>>) {
+        itemType = pb.NewOptionalType(pb.NewDataType(NUdf::EDataSlot::String));
+    } else if constexpr (std::is_same_v<Type, TString>) {
+        itemType = pb.NewDataType(NUdf::EDataSlot::String);
+    } else {
+        itemType =  pb.NewDataType(NUdf::TDataType<Type>::Id);
+    }
+
+    TRuntimeNode::TList listItems;
+    std::transform(vector.cbegin(), vector.cend(), std::back_inserter(listItems),
+        [&](const auto value) {
+            TRuntimeNode item;
+            if constexpr (std::is_same_v<Type, std::optional<TString>>) {
+                if (value == std::nullopt) {
+                    return pb.NewEmptyOptional(itemType);
+                } else {
+                    return pb.NewOptional(pb.NewDataLiteral<NUdf::EDataSlot::String>(*value));
+                }
+            } else if constexpr (std::is_same_v<Type, TString>) {
+                return pb.NewDataLiteral<NUdf::EDataSlot::String>(value);
+            } else {
+                return pb.NewDataLiteral<Type>(value);
+            }
+        });
+
+    return {pb.NewList(itemType, listItems)};
+}
+
+template<typename Type, typename... Tail>
+const TVector<const TRuntimeNode> BuildListNodes(TProgramBuilder& pb,
+    const TVector<Type>& vector, Tail... vectors
+) {
+    const auto frontList = BuildListNodes(pb, vector);
+    const auto tailLists = BuildListNodes(pb, std::forward<Tail>(vectors)...);
+    TVector<const TRuntimeNode> lists;
+    lists.reserve(tailLists.size() + 1);
+    lists.push_back(frontList.front());;
+    for (const auto& list : tailLists) {
+        lists.push_back(list);
+    }
+    return lists;
+}
+
+template<typename... TVectors>
+const std::pair<TType*, NUdf::TUnboxedValue> ConvertVectorsToTuples(
+    TSetup<false>& setup, TVectors... vectors
+) {
+    TProgramBuilder& pb = *setup.PgmBuilder;
+    const auto lists = BuildListNodes(pb, std::forward<TVectors>(vectors)...);
+    const auto tuplesNode = pb.Zip(lists);
+    const auto tuplesNodeType = tuplesNode.GetStaticType();
+    const auto tuples = setup.BuildGraph(tuplesNode)->GetValue();
+    return std::make_pair(tuplesNodeType, tuples);
+}
+
+//
 // Join type specific test wrappers.
 //
 

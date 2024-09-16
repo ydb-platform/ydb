c5a467c3cacd9a796e90a315e20da1e57bc8b268
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2024-09-16 17:06:09 +0300

    (refactoring) Get rid of template local table writer (#9270)

--- ydb/core/tx/replication/service/json_change_record.h
+++ ydb/core/tx/replication/service/json_change_record.h
@@ -2,19 +2,13 @@
 
 #include "lightweight_schema.h"
 
-#include <ydb/core/change_exchange/change_exchange.h>
 #include <ydb/core/change_exchange/change_record.h>
 #include <ydb/core/scheme/scheme_tablecell.h>
-#include <ydb/core/scheme_types/scheme_type_info.h>
-#include <ydb/core/tablet_flat/flat_row_eggs.h>
 
 #include <library/cpp/json/json_reader.h>
 
 #include <util/generic/maybe.h>
-#include <util/generic/ptr.h>
-#include <util/generic/vector.h>
 #include <util/memory/pool.h>
-#include <util/string/join.h>
 
 namespace NKikimrTxDataShard {
     class TEvApplyReplicationChanges_TChange;
@@ -28,26 +22,12 @@ class TChangeRecord: public NChangeExchange::TChangeRecordBase {
     friend class TChangeRecordBuilder;
 
 public:
-    using TBuilder = TChangeRecordBuilder;
-
-    struct TSerializationContext {
-        TMemoryPool MemoryPool;
-
-        TSerializationContext()
-            : MemoryPool(256)
-        {}
-
-        TSerializationContext(const TSerializationContext&)
-            : MemoryPool(256) // do not preserve any state between writers, just construct new one.
-        {}
-    };
-
     ui64 GetGroup() const override;
     ui64 GetStep() const override;
     ui64 GetTxId() const override;
     EKind GetKind() const override;
 
-    void Serialize(NKikimrTxDataShard::TEvApplyReplicationChanges_TChange& record, TSerializationContext& ctx) const;
+    void Serialize(NKikimrTxDataShard::TEvApplyReplicationChanges_TChange& record, TMemoryPool& pool) const;
 
     TConstArrayRef<TCell> GetKey(TMemoryPool& pool) const;
     TConstArrayRef<TCell> GetKey() const;

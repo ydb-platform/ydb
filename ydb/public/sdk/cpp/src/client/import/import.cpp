4d3c9f61e560189f9c8cb5163d0f862ebbc0ddc3
Bulat Gayazov <bylatgr@gmail.com>
2024-09-16 17:59:08 +0000

    Added no acl to import

--- ydb/public/sdk/cpp/src/client/import/import.cpp
+++ ydb/public/sdk/cpp/src/client/import/import.cpp
@@ -154,6 +154,10 @@ TFuture<TImportFromS3Response> TImportClient::ImportFromS3(const TImportFromS3Se
         request.mutable_settings()->set_number_of_retries(settings.NumberOfRetries_.value());
     }
 
+    if (settings.NoACL_) {
+        request.mutable_settings()->set_no_acl(settings.NoACL_.value());
+    }
+
     request.mutable_settings()->set_disable_virtual_addressing(!settings.UseVirtualAddressing_);
 
     return Impl_->ImportFromS3(std::move(request), settings);

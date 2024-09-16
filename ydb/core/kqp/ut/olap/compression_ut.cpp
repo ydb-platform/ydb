b7ae6b199a53df7e4a9a3ad0a6988539b292868d
vlad-gogov <vlad-gogov@ydb.tech>
2024-09-16 14:26:38 +0300

    Added additional functional in TTestHelper and refactoring test KqpOlapCompression::DisabledAlterCompression

--- ydb/core/kqp/ut/olap/compression_ut.cpp
+++ ydb/core/kqp/ut/olap/compression_ut.cpp
@@ -1,5 +1,3 @@
-#include "helpers/typed_local.h"
-
 #include <ydb/core/kqp/ut/common/columnshard.h>
 
 namespace NKikimr::NKqp {
@@ -7,36 +5,26 @@ namespace NKikimr::NKqp {
 Y_UNIT_TEST_SUITE(KqpOlapCompression) {
     Y_UNIT_TEST(DisabledAlterCompression) {
         TKikimrSettings settings = TKikimrSettings().SetWithSampleTables(false).SetEnableOlapCompression(false);
-        {
-            TKikimrRunner kikimr(settings);
-            TTypedLocalHelper helper("", kikimr, "olapTable", "olapStore");
-            helper.CreateTestOlapTable();
-            helper.FillPKOnly(0, 1);
-            helper.ExecuteSchemeQuery(
-                "ALTER OBJECT `/Root/olapStore/olapTable` (TYPE TABLE) SET (ACTION=ALTER_COLUMN, NAME=pk_int, "
-                "`SERIALIZER.CLASS_NAME`=`ARROW_SERIALIZER`, `COMPRESSION.TYPE`=`zstd`);", NYdb::EStatus::SCHEME_ERROR);
-            helper.ExecuteSchemeQuery(
-                "ALTER OBJECT `/Root/olapStore` (TYPE TABLESTORE) SET (ACTION=ALTER_COLUMN, NAME=pk_int, "
-                "`SERIALIZER.CLASS_NAME`=`ARROW_SERIALIZER`, `COMPRESSION.TYPE`=`zstd`);", NYdb::EStatus::PRECONDITION_FAILED);
-        }
+        TTestHelper testHelper(settings);
+        TVector<TTestHelper::TColumnSchema> schema = {
+            TTestHelper::TColumnSchema().SetName("pk_int").SetType(NScheme::NTypeIds::Uint64).SetNullable(false)
+        };
+        TTestHelper::TCompression compression = TTestHelper::TCompression().SetType(arrow::Compression::type::ZSTD);
 
-        {
-            TTestHelper helper(settings);
-            TVector<TTestHelper::TColumnSchema> schema = {
-                TTestHelper::TColumnSchema().SetName("pk_int").SetType(NScheme::NTypeIds::Uint64).SetNullable(false)
-            };
+        TTestHelper::TColumnTable standaloneTable;
+        standaloneTable.SetName("/Root/StandaloneTable").SetPrimaryKey({ "pk_int" }).SetSharding({ "pk_int" }).SetSchema(schema);
+        testHelper.CreateTable(standaloneTable);
+        testHelper.SetCompression(standaloneTable, "pk_int", compression, NYdb::EStatus::SCHEME_ERROR);
 
-            TTestHelper::TColumnTable table;
-            table.SetName("/Root/olapTable").SetPrimaryKey({ "pk_int" }).SetSharding({ "pk_int" }).SetSchema(schema);
-            helper.CreateTable(table);
+        TTestHelper::TColumnTableStore testTableStore;
+        testTableStore.SetName("/Root/TableStoreTest").SetPrimaryKey({ "pk_int" }).SetSchema(schema);
+        testHelper.CreateTable(testTableStore);
+        testHelper.SetCompression(testTableStore, "pk_int", compression, NYdb::EStatus::PRECONDITION_FAILED);
 
-            auto alterResult = helper.GetSession()
-                                   .ExecuteSchemeQuery(
-                                       "ALTER OBJECT `/Root/olapTable` (TYPE TABLE) SET (ACTION=ALTER_COLUMN, "
-                                       "NAME=pk_int,`SERIALIZER.CLASS_NAME`=`ARROW_SERIALIZER`, `COMPRESSION.TYPE`=`zstd`);")
-                                   .GetValueSync();
-            UNIT_ASSERT_VALUES_EQUAL_C(alterResult.GetStatus(), NYdb::EStatus::SCHEME_ERROR, alterResult.GetIssues().ToString());
-        }
+        TTestHelper::TColumnTable testTable;
+        testTable.SetName("/Root/TableStoreTest/ColumnTableTest").SetPrimaryKey({ "pk_int" }).SetSharding({ "pk_int" }).SetSchema(schema);
+        testHelper.CreateTable(testTable);
+        testHelper.SetCompression(testTable, "pk_int", compression, NYdb::EStatus::SCHEME_ERROR);
     }
 }
 }

376c44913c93a1f225b59fa07dacaf0c4f1a5bf0
Nikita-Levuskin <81382697+Nikita-Levuskin@users.noreply.github.com>
2024-09-16 20:39:12 +0300

    delete YDB_TABLE_ENABLE_PREPARED_DDL

--- ydb/tests/library/harness/kikimr_config.py
+++ ydb/tests/library/harness/kikimr_config.py
@@ -250,9 +250,6 @@ class KikimrConfigGenerator(object):
         if os.getenv('YDB_KQP_ENABLE_IMMEDIATE_EFFECTS', 'false').lower() == 'true':
             self.yaml_config["table_service_config"]["enable_kqp_immediate_effects"] = True
 
-        if os.getenv('YDB_TABLE_ENABLE_PREPARED_DDL', 'false').lower() == 'true':
-            self.yaml_config["table_service_config"]["enable_prepared_ddl"] = True
-
         if os.getenv('PGWIRE_LISTENING_PORT', ''):
             self.yaml_config["local_pg_wire_config"] = {}
             self.yaml_config["local_pg_wire_config"]["listening_port"] = os.getenv('PGWIRE_LISTENING_PORT')

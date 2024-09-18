73feebeb8c2f6650b61c3feb11f177f62d630b63
Alek5andr-Kotov <akotov@ydb.tech>
2024-09-18 09:39:56 +0300

    The `DescripeTopic` call with the `IncludeStats` flag freezes (#9392)

--- ydb/public/sdk/cpp/client/ydb_topic/ut/topic_to_table_ut.cpp
+++ ydb/public/sdk/cpp/client/ydb_topic/ut/topic_to_table_ut.cpp
@@ -60,6 +60,7 @@ protected:
                      const TString& consumer = TEST_CONSUMER,
                      size_t partitionCount = 1,
                      std::optional<size_t> maxPartitionCount = std::nullopt);
+    void DescribeTopic(const TString& path);
 
     void WriteToTopicWithInvalidTxId(bool invalidTxId);
 
@@ -320,6 +321,11 @@ void TFixture::CreateTopic(const TString& path,
 
 {
     Setup->CreateTopic(path, consumer, partitionCount, maxPartitionCount);
+}
+
+void TFixture::DescribeTopic(const TString& path)
+{
+    Setup->DescribeTopic(path);
 }
 
 const TDriver& TFixture::GetDriver() const
@@ -1106,6 +1112,8 @@ Y_UNIT_TEST_F(WriteToTopic_Demo_6, TFixture)
         UNIT_ASSERT_VALUES_EQUAL(messages[0], "message #1");
         UNIT_ASSERT_VALUES_EQUAL(messages[1], "message #2");
     }
+
+    DescribeTopic("topic_A");
 }
 
 Y_UNIT_TEST_F(WriteToTopic_Demo_7, TFixture)

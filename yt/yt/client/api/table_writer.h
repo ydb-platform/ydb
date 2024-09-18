1ff5503fb75269187625fb885f3b7961259c5b05
apollo1321 <apollo1321@yandex-team.com>
2024-09-18 21:18:13 +0300

    YT-21709: Introduce TRowBatchWriter for shuffle service commit_hash:3f47f41fc3e5b49173d99678eed1ff4c51f2b749

--- yt/yt/client/api/table_writer.h
+++ yt/yt/client/api/table_writer.h
@@ -8,7 +8,7 @@ namespace NYT::NApi {
 ////////////////////////////////////////////////////////////////////////////////
 
 struct ITableWriter
-    : public IRowBatchWriter
+    : public virtual IRowBatchWriter
 {
     //! Returns the schema to be used for constructing rows.
     virtual const NTableClient::TTableSchemaPtr& GetSchema() const = 0;

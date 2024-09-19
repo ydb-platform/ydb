21321c41285370f10239e11376fba02eafe5a82d
Innokentii Mokin <innokentii@ydb.tech>
2024-09-19 23:58:15 +0000

    WIP

--- ydb/core/tx/datashard/change_sender_incr_backup.cpp
+++ ydb/core/tx/datashard/change_sender_incr_backup.cpp
@@ -1,1 +1,240 @@
+#include "change_exchange.h"
+#include "change_exchange_impl.h"
+#include "change_record.h"
+#include "change_sender_base.h"
+#include "datashard_impl.h"
 
+#include <ydb/core/tablet_flat/tablet_flat_executor.h>
+
+#include <ydb/library/actors/core/actor_bootstrapped.h>
+#include <ydb/library/actors/core/hfunc.h>
+#include <ydb/library/actors/core/log.h>
+
+#include <util/generic/maybe.h>
+
+namespace NKikimr::NDataShard {
+
+class TIncrRestoreChangeSenderMain
+    : public TActorBootstrapped<TIncrRestoreChangeSenderMain>
+    , public NChangeExchange::TChangeSender
+    , public NChangeExchange::IChangeSenderIdentity
+    , public NChangeExchange::IChangeSenderPathResolver
+    , public NChangeExchange::IChangeSenderFactory
+    , private TSchemeChecksMixin<TIncrRestoreChangeSenderMain>
+    , private TResolveUserTableState<TIncrRestoreChangeSenderMain>
+    , private TResolveTargetTableState<TIncrRestoreChangeSenderMain>
+    , private TResolveKeysState<TIncrRestoreChangeSenderMain>
+{
+    friend struct TSchemeChecksMixin;
+
+    USE_STATE(ResolveUserTable);
+    USE_STATE(ResolveTargetTable);
+    USE_STATE(ResolveKeys);
+
+    TStringBuf GetLogPrefix() const {
+        if (!LogPrefix) {
+            LogPrefix = TStringBuilder()
+                << "[IncrRestoreChangeSenderMain]"
+                << "[" << DataShard.TabletId << ":" << DataShard.Generation << "]"
+                << SelfId() /* contains brackets */ << " ";
+        }
+
+        return LogPrefix.GetRef();
+    }
+
+    static TSerializedTableRange GetFullRange(ui32 keyColumnsCount) {
+        TVector<TCell> fromValues(keyColumnsCount);
+        TVector<TCell> toValues;
+        return TSerializedTableRange(fromValues, true, toValues, false);
+    }
+
+    bool IsResolving() const override {
+        return IsResolveUserTableState()
+            || IsResolveTargetTableState()
+            || IsResolveKeysState();
+    }
+
+    TStringBuf CurrentStateName() const {
+        if (IsResolveUserTableState()) {
+            return "ResolveUserTable";
+        } else if (IsResolveTargetTableState()) {
+            return "ResolveTargetTable";
+        } else if (IsResolveKeysState()) {
+            return "ResolveKeys";
+        } else {
+            return "";
+        }
+    }
+
+    void OnRetry(TResolveTargetTableState::TStateTag) {
+        ResolveTargetTable();
+    }
+
+    void OnRetry(TResolveKeysState::TStateTag) {
+        ResolveTargetTable();
+    }
+
+    void OnIndexUnderRemove() {
+        RemoveRecords();
+        KillSenders();
+
+        Become(&TThis::StatePendingRemove);
+    }
+
+    void NextState(TResolveUserTableState::TStateTag) {
+        ResolveTargetTable();
+    }
+
+    void NextState(TResolveTargetTableState::TStateTag) {
+        ResolveKeys();
+    }
+
+    void NextState(TResolveKeysState::TStateTag) {
+        Serve();
+    }
+
+    void Retry() {
+        Schedule(TDuration::Seconds(1), new TEvents::TEvWakeup());
+    }
+
+    void Serve() {
+        Become(&TThis::StateMain);
+    }
+
+    void LogCritAndRetry(const TString& error) {
+        LOG_C(error);
+        Retry();
+    }
+
+    void LogWarnAndRetry(const TString& error) {
+        LOG_W(error);
+        Retry();
+    }
+
+    /// Main
+
+    STATEFN(StateMain) {
+        return StateBase(ev);
+    }
+
+    void Resolve() override {
+        ResolveTargetTable();
+    }
+
+    bool IsResolved() const override {
+        return KeyDesc && KeyDesc->GetPartitions();
+    }
+
+    IActor* CreateSender(ui64 partitionId) const override {
+        Y_UNUSED(partitionId);
+        return nullptr;
+        // return new TBaseChangeSenderShard(SelfId(), DataShard, partitionId, TargetTablePathId, TagMap);
+    }
+
+    void Handle(NChangeExchange::TEvChangeExchange::TEvEnqueueRecords::TPtr& ev) {
+        LOG_D("Handle " << ev->Get()->ToString());
+        EnqueueRecords(std::move(ev->Get()->Records));
+    }
+
+    void Handle(NChangeExchange::TEvChangeExchange::TEvRecords::TPtr& ev) {
+        LOG_D("Handle " << ev->Get()->ToString());
+        ProcessRecords(std::move(ev->Get()->Records));
+    }
+
+    void Handle(NChangeExchange::TEvChangeExchange::TEvForgetRecords::TPtr& ev) {
+        LOG_D("Handle " << ev->Get()->ToString());
+        ForgetRecords(std::move(ev->Get()->Records));
+    }
+
+    void Handle(NChangeExchange::TEvChangeExchangePrivate::TEvReady::TPtr& ev) {
+        LOG_D("Handle " << ev->Get()->ToString());
+        OnReady(ev->Get()->PartitionId);
+    }
+
+    void Handle(NChangeExchange::TEvChangeExchangePrivate::TEvGone::TPtr& ev) {
+        LOG_D("Handle " << ev->Get()->ToString());
+        OnGone(ev->Get()->PartitionId);
+    }
+
+    void Handle(TEvChangeExchange::TEvRemoveSender::TPtr& ev) {
+        LOG_D("Handle " << ev->Get()->ToString());
+        Y_ABORT_UNLESS(ev->Get()->PathId == GetChangeSenderIdentity());
+
+        RemoveRecords();
+        PassAway();
+    }
+
+    void AutoRemove(NChangeExchange::TEvChangeExchange::TEvEnqueueRecords::TPtr& ev) {
+        LOG_D("Handle " << ev->Get()->ToString());
+        RemoveRecords(std::move(ev->Get()->Records));
+    }
+
+    void Handle(NMon::TEvRemoteHttpInfo::TPtr& ev, const TActorContext& ctx) {
+        RenderHtmlPage(DataShard.TabletId, ev, ctx);
+    }
+
+    void PassAway() override {
+        KillSenders();
+        TActorBootstrapped::PassAway();
+    }
+
+public:
+    static constexpr NKikimrServices::TActivity::EType ActorActivityType() {
+        return NKikimrServices::TActivity::CHANGE_SENDER_ASYNC_INDEX_ACTOR_MAIN;
+    }
+
+    explicit TIncrRestoreChangeSenderMain(const TDataShardId& dataShard, const TTableId& userTableId, const TPathId& indexPathId)
+        : TActorBootstrapped()
+        , TChangeSender(this, this, this, this, dataShard.ActorId)
+        , IndexPathId(indexPathId)
+        , DataShard(dataShard)
+        , UserTableId(userTableId)
+        , TargetTableVersion(0)
+    {
+    }
+
+    void Bootstrap() {
+        ResolveUserTable();
+    }
+
+    STFUNC(StateBase) {
+        switch (ev->GetTypeRewrite()) {
+            hFunc(NChangeExchange::TEvChangeExchange::TEvEnqueueRecords, Handle);
+            hFunc(NChangeExchange::TEvChangeExchange::TEvRecords, Handle);
+            hFunc(NChangeExchange::TEvChangeExchange::TEvForgetRecords, Handle);
+            hFunc(TEvChangeExchange::TEvRemoveSender, Handle);
+            hFunc(NChangeExchange::TEvChangeExchangePrivate::TEvReady, Handle);
+            hFunc(NChangeExchange::TEvChangeExchangePrivate::TEvGone, Handle);
+            HFunc(NMon::TEvRemoteHttpInfo, Handle);
+            sFunc(TEvents::TEvPoison, PassAway);
+        }
+    }
+
+    STFUNC(StatePendingRemove) {
+        switch (ev->GetTypeRewrite()) {
+            hFunc(NChangeExchange::TEvChangeExchange::TEvEnqueueRecords, AutoRemove);
+            hFunc(TEvChangeExchange::TEvRemoveSender, Handle);
+            HFunc(NMon::TEvRemoteHttpInfo, Handle);
+            sFunc(TEvents::TEvPoison, PassAway);
+        }
+    }
+
+    TPathId GetChangeSenderIdentity() const override final {
+        return IndexPathId;
+    }
+
+private:
+    const TPathId IndexPathId;
+    const TDataShardId DataShard;
+    const TTableId UserTableId;
+    mutable TMaybe<TString> LogPrefix;
+
+    THashMap<TString, TTag> MainColumnToTag;
+    TMap<TTag, TTag> TagMap; // from main to index
+
+    TPathId TargetTablePathId;
+    ui64 TargetTableVersion;
+    THolder<TKeyDesc> KeyDesc;
+}; // TIncrRestoreChangeSenderMain
+
+} // namespace NKikimr::NDataShard

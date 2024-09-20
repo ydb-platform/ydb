77a5f3a1d9bc1fd271af68a0efbccc2ad78b6ab4
Robert Drynkin <robdrynkin@ydb.tech>
2024-09-20 11:49:49 +0200

    Fix start balancing actor after timeout + fix release replication token (#9389)

--- ydb/core/blobstorage/vdisk/balance/balancing_actor.cpp
+++ ydb/core/blobstorage/vdisk/balance/balancing_actor.cpp
@@ -89,14 +89,13 @@ namespace NBalancing {
         void ContinueBalancing() {
             if (SendOnMainParts.Empty() && TryDeleteParts.Empty()) {
                 // no more parts to send or delete
-                STLOG(PRI_DEBUG, BS_VDISK_BALANCING, BSVB03, VDISKP(Ctx->VCtx, "Balancing completed"));
-                Send(Ctx->SkeletonId, new TEvStartBalancing());
+                STLOG(PRI_INFO, BS_VDISK_BALANCING, BSVB03, VDISKP(Ctx->VCtx, "Balancing completed"));
                 PassAway();
                 return;
             }
 
             // ask for repl token to continue balancing
-            STLOG(PRI_DEBUG, BS_VDISK_BALANCING, BSVB01, VDISKP(Ctx->VCtx, "Ask repl token to continue balancing"));
+            STLOG(PRI_INFO, BS_VDISK_BALANCING, BSVB01, VDISKP(Ctx->VCtx, "Ask repl token to continue balancing"), (SelfId, SelfId()), (PDiskId, Ctx->VDiskCfg->BaseInfo.PDiskId));
             Send(MakeBlobStorageReplBrokerID(), new TEvQueryReplToken(Ctx->VDiskCfg->BaseInfo.PDiskId), NActors::IEventHandle::FlagTrackDelivery);
         }
 
@@ -122,7 +121,6 @@ namespace NBalancing {
                 // not all vdisks are connected
                 STLOG(PRI_INFO, BS_VDISK_BALANCING, BSVB11, VDISKP(Ctx->VCtx, "Not all vdisks are connected, balancing should work only for full groups"),
                     (ConnectedVDisks, ConnectedVDisks.size()), (TotalVDisksInGroup, GInfo->GetTotalVDisksNum()));
-                Send(Ctx->SkeletonId, new TEvStartBalancing());
                 PassAway();
                 return;
             }
@@ -191,11 +189,13 @@ namespace NBalancing {
         }
 
         void Handle(NActors::TEvents::TEvCompleted::TPtr ev) {
+            STLOG(PRI_INFO, BS_VDISK_BALANCING, BSVB04, VDISKP(Ctx->VCtx, "TEvCompleted"), (Type, ev->Type));
             BatchManager.Handle(ev);
 
             if (StartTime + EPOCH_TIMEOUT < TlsActivationContext->Now()) {
                 Ctx->MonGroup.EpochTimeouts()++;
-                STLOG(PRI_DEBUG, BS_VDISK_BALANCING, BSVB04, VDISKP(Ctx->VCtx, "Epoch timeout"));
+                Send(MakeBlobStorageReplBrokerID(), new TEvReleaseReplToken);
+                STLOG(PRI_INFO, BS_VDISK_BALANCING, BSVB04, VDISKP(Ctx->VCtx, "Epoch timeout"));
                 PassAway();
             }
 
@@ -242,7 +242,7 @@ namespace NBalancing {
 
         void Handle(NActors::TEvents::TEvUndelivered::TPtr ev) {
             if (ev.Get()->Type == TEvReplToken::EventType) {
-                STLOG(PRI_WARN, BS_VDISK_BALANCING, BSVB06, VDISKP(Ctx->VCtx, "Ask repl token msg not delivered"));
+                STLOG(PRI_WARN, BS_VDISK_BALANCING, BSVB06, VDISKP(Ctx->VCtx, "Ask repl token msg not delivered"), (SelfId, SelfId()), (PDiskId, Ctx->VDiskCfg->BaseInfo.PDiskId));
                 ScheduleJobQuant();
             }
         }
@@ -276,6 +276,7 @@ namespace NBalancing {
             for (const auto& kv : *QueueActorMapPtr) {
                 Send(kv.second, new TEvents::TEvPoison);
             }
+            Send(Ctx->SkeletonId, new TEvStartBalancing());
             TActorBootstrapped::PassAway();
         }
 

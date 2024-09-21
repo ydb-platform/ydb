53e493a7d1de1dfb75532404d44f73b63fd79f79
azevaykin <azevaykin@ydb.tech>
2024-09-21 05:58:03 +0000

    repeated index fix

--- ydb/core/kqp/compute_actor/kqp_scan_fetcher_actor.cpp
+++ ydb/core/kqp/compute_actor/kqp_scan_fetcher_actor.cpp
@@ -46,8 +46,9 @@ TKqpScanFetcherActor::TKqpScanFetcherActor(const NKikimrKqp::TKqpSnapshot& snaps
     KeyColumnTypes.reserve(Meta.GetKeyColumnTypes().size());
     for (size_t i = 0; i < Meta.KeyColumnTypesSize(); i++) {
         NScheme::TTypeId typeId = Meta.GetKeyColumnTypes().at(i);
-        const auto& typeInfoProto = Meta.GetKeyColumnTypeInfos().at(i);
-        NScheme::TTypeInfo typeInfo = NScheme::TypeInfoFromProto(typeId, typeInfoProto);
+        NScheme::TTypeInfo typeInfo = NScheme::NTypeIds::IsParametrizedType(typeId) ?
+            NScheme::TypeInfoFromProto(typeId,Meta.GetKeyColumnTypeInfos().at(i)) :
+            NScheme::TTypeInfo(typeId);  
         KeyColumnTypes.push_back(typeInfo);
     }
 }

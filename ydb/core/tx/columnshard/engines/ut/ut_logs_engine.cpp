6862d3c0e8769366a7221ccabe621a18ff0549bf
ivanmorozov333 <ivanmorozov@ydb.tech>
2024-09-23 12:09:29 +0300

    fix mvcc tests. use write id as row feature for conflicts resolving (#9598)

--- ydb/core/tx/columnshard/engines/ut/ut_logs_engine.cpp
+++ ydb/core/tx/columnshard/engines/ut/ut_logs_engine.cpp
@@ -497,7 +497,7 @@ Y_UNIT_TEST_SUITE(TColumnEngineTestLogs) {
             ui64 txId = 1;
             auto selectInfo = engine.Select(paths[0], TSnapshot(planStep, txId), NOlap::TPKRangesFilter(false));
             UNIT_ASSERT_VALUES_EQUAL(selectInfo->PortionsOrderedPK.size(), 1);
-            UNIT_ASSERT_VALUES_EQUAL(selectInfo->PortionsOrderedPK[0]->NumChunks(), columnIds.size() + TIndexInfo::GetSnapshotColumnIdsSet().size());
+            UNIT_ASSERT_VALUES_EQUAL(selectInfo->PortionsOrderedPK[0]->NumChunks(), columnIds.size() + TIndexInfo::GetSnapshotColumnIdsSet().size() - 1);
         }
 
         { // select another pathId

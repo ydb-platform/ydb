c306ff3b4ddb2d4c5a452d51f10cd5aadf3f999a
Pavel Ivanov <pudge1000-7@ydb.tech>
2024-09-23 16:12:08 +0000

    [CLI] Cursed bug fix

--- ydb/public/lib/ydb_cli/common/format.cpp
+++ ydb/public/lib/ydb_cli/common/format.cpp
@@ -489,11 +489,11 @@ void TQueryPlanPrinter::PrintPrettyTableImpl(const NJson::TJsonValue& plan, TStr
                         // skip this attribute
                     }
                     else if (key == "Predicate" || key == "Condition" || key == "SortBy") {
-                        info.emplace_back(TStringBuilder() << ReplaceAll(ReplaceAll(JsonToString(value), " item.", ""), " state.", ""));
+                        info.emplace_back(TStringBuilder() << ReplaceAll(ReplaceAll(JsonToString(value), " item.", " "), " state.", " "));
                     } else if (key == "Table") {
-                        info.insert(info.begin(), TStringBuilder() << colors.LightYellow() << key << colors.Default() << ":" << colors.LightGreen() << " " << ReplaceAll(ReplaceAll(JsonToString(value), " item.", ""), " state.", "") << colors.Default());
+                        info.insert(info.begin(), TStringBuilder() << colors.LightYellow() << key << colors.Default() << ":" << colors.LightGreen() << " " << ReplaceAll(ReplaceAll(JsonToString(value), " item.", " "), " state.", " ") << colors.Default());
                     } else {
-                        info.emplace_back(TStringBuilder() << colors.LightYellow() << key << colors.Default() << ": " << ReplaceAll(ReplaceAll(JsonToString(value), " item.", ""), " state.", ""));
+                        info.emplace_back(TStringBuilder() << colors.LightYellow() << key << colors.Default() << ": " << ReplaceAll(ReplaceAll(JsonToString(value), " item.", " "), " state.", " "));
                     }
                 }
             }

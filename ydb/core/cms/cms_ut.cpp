8b6f2edef40bd57ca0935ec07c7384206597688a
Ilia Shakhov <pixcc@ydb.tech>
2024-09-25 16:30:37 +0300

    Add evict vdisks for a rack (#9740)

--- ydb/core/cms/cms_ut.cpp
+++ ydb/core/cms/cms_ut.cpp
@@ -1808,6 +1808,51 @@ Y_UNIT_TEST_SUITE(TCmsTest) {
         env.CheckRejectRequest("user", request3.GetRequestId());
     }
 
+    Y_UNIT_TEST(AllVDisksEvictionInRack)
+    {
+        auto opts = TTestEnvOpts(8)
+            .WithSentinel()
+            .WithNodeLocationCallback([](ui32 nodeId) {
+                NActorsInterconnect::TNodeLocation location;
+                location.SetRack(ToString(nodeId / 2 + 1));
+                return TNodeLocation(location); // Node = [0, 1, 2, 3, 4, 5, 6, 7]
+                                                // Rack = [1, 1, 2, 2, 3, 3, 4, 4]
+            });
+        TCmsTestEnv env(opts);
+        env.SetLogPriority(NKikimrServices::CMS, NLog::PRI_DEBUG);
+
+        // Evict all VDisks from rack 1
+        auto request1 = env.CheckPermissionRequest(
+            MakePermissionRequest(TRequestOptions("user").WithEvictVDisks(),
+                MakeAction(TAction::RESTART_SERVICES, env.GetNodeId(0), 600000000, "storage")
+            ),
+            TStatus::DISALLOW_TEMP // ok, waiting for move VDisks
+        );
+        auto request2 = env.CheckPermissionRequest(
+            MakePermissionRequest(TRequestOptions("user").WithEvictVDisks(),
+                MakeAction(TAction::RESTART_SERVICES, env.GetNodeId(1), 600000000, "storage")
+            ),
+            TStatus::DISALLOW_TEMP // ok, waiting for move VDisks
+        );
+
+        // Check that FAULTY BSC requests are sent
+        env.CheckBSCUpdateRequests({ env.GetNodeId(0), env.GetNodeId(1) }, NKikimrBlobStorage::FAULTY);
+
+        // "Move" VDisks from rack 1
+        auto& node1 = TFakeNodeWhiteboardService::Info[env.GetNodeId(0)];
+        node1.VDisksMoved = true;
+        node1.VDiskStateInfo.clear();
+        auto& node2 = TFakeNodeWhiteboardService::Info[env.GetNodeId(1)];
+        node2.VDisksMoved = true;
+        node2.VDiskStateInfo.clear();
+        env.RegenerateBSConfig(TFakeNodeWhiteboardService::Config.MutableResponse()->MutableStatus(0)->MutableBaseConfig(), opts);
+
+        auto permission1 = env.CheckRequest("user", request1.GetRequestId(), false, TStatus::ALLOW, 1);
+        auto permission2 = env.CheckRequest("user", request2.GetRequestId(), false, TStatus::ALLOW, 1);
+        env.CheckDonePermission("user", permission1.GetPermissions(0).GetId());
+        env.CheckDonePermission("user", permission2.GetPermissions(0).GetId());
+    }
+
     Y_UNIT_TEST(EmergencyDuringRollingRestart)
     {
         TCmsTestEnv env(TTestEnvOpts(8).WithEnableCMSRequestPriorities());

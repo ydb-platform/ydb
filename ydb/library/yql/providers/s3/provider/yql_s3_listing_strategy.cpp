d9ca689811eb99739ed4f7e8d2f6efe03a2e2d1d
Oleg Doronin <dorooleg@yandex.ru>
2024-09-25 17:19:50 +0200

    s3 listing strategy has been fixed (#9499)

--- ydb/library/yql/providers/s3/provider/yql_s3_listing_strategy.cpp
+++ ydb/library/yql/providers/s3/provider/yql_s3_listing_strategy.cpp
@@ -27,6 +27,11 @@ IOutputStream& operator<<(IOutputStream& stream, const TS3ListingOptions& option
 
 namespace {
 
+TString ParseBasePath(const TString& path) {
+    TString basePath = TString{TStringBuf{path}.RBefore('/')};
+    return basePath == path && !basePath.EndsWith('/') ? TString{} : basePath;
+}
+
 using namespace NThreading;
 using namespace NS3Lister;
 
@@ -493,15 +498,10 @@ public:
         return NextDirectoryListeningChunk;
     }
 
-    static TString ParseBasePath(const TString& path) {
-        TString basePath = TString{TStringBuf{path}.RBefore('/')};
-        return basePath == path && !basePath.EndsWith('/') ? TString{} : basePath;
-    }
-
     void PerformEarlyStop(TListEntries& result, const TString& sourcePrefix) {
         result.Directories.push_back({.Path = ParseBasePath(sourcePrefix)});
         for (auto& directoryPrefix : DirectoryPrefixQueue) {
-            result.Directories.push_back({.Path = directoryPrefix});
+            result.Directories.push_back({.Path = ParseBasePath(directoryPrefix)});
         }
         DirectoryPrefixQueue.clear();
     }
@@ -519,10 +519,10 @@ public:
             }
         } else {
             for (auto& directoryPrefix : listingResult.Directories) {
-                result.Directories.push_back({.Path = directoryPrefix.Path});
+                result.Directories.push_back({.Path = ParseBasePath(directoryPrefix.Path)});
             }
             for (auto& directoryPrefix : DirectoryPrefixQueue) {
-                result.Directories.push_back({.Path = directoryPrefix});
+                result.Directories.push_back({.Path = ParseBasePath(directoryPrefix)});
             }
             DirectoryPrefixQueue.clear();
         }
@@ -767,10 +767,10 @@ public:
                 // TODO: add verification
                 auto result = TListEntries{.Objects = Objects};
                 for (auto& directoryPrefix : DirectoryPrefixQueue) {
-                    result.Directories.push_back({.Path = directoryPrefix});
+                    result.Directories.push_back({.Path = ParseBasePath(directoryPrefix)});
                 }
                 for (auto& directoryPrefix: InProgressPaths) {
-                    result.Directories.push_back({.Path = directoryPrefix});
+                    result.Directories.push_back({.Path = ParseBasePath(directoryPrefix)});
                 }
                 for (auto& directoryEntry : Directories) {
                     result.Directories.push_back(directoryEntry);

5b1b4c354e57f520fe24327b6cc6ff379d073ce0
Aleksandr Khoroshilov <hor911@ydb.tech>
2024-09-26 19:06:47 +0000

    Fix bad merge

--- ydb/core/kqp/provider/yql_kikimr_settings.cpp
+++ ydb/core/kqp/provider/yql_kikimr_settings.cpp
@@ -88,7 +88,6 @@ TKikimrConfiguration::TKikimrConfiguration() {
     REGISTER_SETTING(*this, OptJoinOrderHints);
     REGISTER_SETTING(*this, OverridePlanner);
     REGISTER_SETTING(*this, UseGraceJoinCoreForMap);
-    REGISTER_SETTING(*this, OverridePlanner);
 
 
     REGISTER_SETTING(*this, OptUseFinalizeByKey);

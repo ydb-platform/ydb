c9843510b39357d5510030691beecfab56e3bd17
v-korovin <v-korovin@yandex-team.com>
2024-09-26 10:35:27 +0300

    futurize build/scripts commit_hash:41fb885eb1e03094e65521671349e66f4225321b

--- build/scripts/fetch_from_mds.py
+++ build/scripts/fetch_from_mds.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+from __future__ import print_function
+
 import os
 import sys
 import logging
@@ -43,7 +46,7 @@ if __name__ == '__main__':
         main(args)
     except Exception as e:
         logging.exception(e)
-        print >> sys.stderr, open(args.abs_log_path).read()
+        print(open(args.abs_log_path).read(), file=sys.stderr)
         sys.stderr.flush()
 
         import error

895704d62beddd440df8bee00b7d47e70a3aa140
Bulat Gayazov <bylatgr@gmail.com>
2024-09-27 11:44:05 +0000

    Fixed after rebase

--- ydb/public/lib/ydb_cli/common/parameters.cpp
+++ ydb/public/lib/ydb_cli/common/parameters.cpp
@@ -187,7 +187,7 @@ void TCommandWithParameters::AddParams(TParamsBuilder& paramBuilder) {
             }
         }
         const TType& type = (*paramIt).second;
-        paramBuilder.AddParam(name, JsonToYdbValue(value, type, InputBinaryStringEncoding));
+        paramBuilder.AddParam(name, JsonToYdbValue(std::string{value}, type, InputBinaryStringEncoding));
     }
 }
 
@@ -429,7 +429,7 @@ bool TCommandWithParameters::GetNextParams(const TDriver& driver, const TString&
                         case EDataFormat::Json:
                         case EDataFormat::JsonUnicode:
                         case EDataFormat::JsonBase64: {
-                            paramBuilder->AddParam(fullname, JsonToYdbValue(*data, type, InputBinaryStringEncoding));
+                            paramBuilder->AddParam(fullname, JsonToYdbValue(std::string{*data}, type, InputBinaryStringEncoding));
                             break;
                         }
                         case EDataFormat::Raw: {
@@ -513,7 +513,7 @@ bool TCommandWithParameters::GetNextParams(const TDriver& driver, const TString&
                     case EDataFormat::Json:
                     case EDataFormat::JsonUnicode:
                     case EDataFormat::JsonBase64: {
-                        valueBuilder.AddListItem(JsonToYdbValue(*data, type.GetProto().list_type().item(),
+                        valueBuilder.AddListItem(JsonToYdbValue(std::string{*data}, type.GetProto().list_type().item(),
                             InputBinaryStringEncoding));
                         break;
                     }

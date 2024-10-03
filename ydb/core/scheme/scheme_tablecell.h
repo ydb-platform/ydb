6e988c444a4f717bc05283ca342f7f0988b93d43
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2024-10-03 19:59:33 +0300

    24-3: parse topic's partitioning once and more efficiently, better handling of connection loss (#10028)

--- ydb/core/scheme/scheme_tablecell.h
+++ ydb/core/scheme/scheme_tablecell.h
@@ -541,6 +541,10 @@ public:
         return Cells;
     }
 
+    explicit operator bool() const {
+        return !Cells.empty();
+    }
+
     static void Serialize(TString& res, TConstArrayRef<TCell> cells);
 
     static TString Serialize(TConstArrayRef<TCell> cells);

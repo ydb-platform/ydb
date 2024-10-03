34a44c49cd44e97409c4244e9fc417a168b0c5ec
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-10-03 09:36:28 +0300

    fix build

--- ydb/core/tx/limiter/grouped_memory/service/process.h
+++ ydb/core/tx/limiter/grouped_memory/service/process.h
@@ -127,7 +127,7 @@ public:
     }
 
     void UnregisterGroup(const bool isPriorityProcess, const ui64 externalGroupId) {
-        if (auto internalGroupId = GroupIds.GetInternalIdOptional()) {
+        if (auto internalGroupId = GroupIds.GetInternalIdOptional(externalGroupId)) {
             AFL_INFO(NKikimrServices::GROUPED_MEMORY_LIMITER)("event", "remove_group")("external_group_id", externalGroupId)(
                 "internal_group_id", internalGroupId);
             UnregisterGroupImpl(*internalGroupId);

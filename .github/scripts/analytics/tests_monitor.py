0f53e5216ef79efe6ff0a22c59838d2a0c1350a5
Kirill Rysin <naspirato@gmail.com>
2024-10-04 14:21:33 +0000

    wip

--- .github/scripts/analytics/tests_monitor.py
+++ .github/scripts/analytics/tests_monitor.py
@@ -17,17 +17,16 @@ dir = os.path.dirname(__file__)
 config = configparser.ConfigParser()
 config_file_path = f"{dir}/../../config/ydb_qa_db.ini"
 config.read(config_file_path)
-
-
 DATABASE_ENDPOINT = config["QA_DB"]["DATABASE_ENDPOINT"]
 DATABASE_PATH = config["QA_DB"]["DATABASE_PATH"]
 
 
-def create_tables(pool,  table_path):
+def create_tables(pool, table_path):
     print(f"> create table if not exists:'{table_path}'")
 
     def callee(session):
-        session.execute_scheme(f"""
+        session.execute_scheme(
+            f"""
             CREATE table IF NOT EXISTS `{table_path}` (
                 `test_name` Utf8 NOT NULL,
                 `suite_folder` Utf8 NOT NULL,
@@ -55,7 +54,8 @@ def create_tables(pool,  table_path):
             )
                 PARTITION BY HASH(build_type,branch)
                 WITH (STORE = COLUMN)
-            """)
+            """
+        )
 
     return pool.retry_operation_sync(callee)
 
@@ -78,39 +78,40 @@ def bulk_upsert(table_client, table_path, rows):
         .add_column("fail_count", ydb.OptionalType(ydb.PrimitiveType.Uint64))
         .add_column("skip_count", ydb.OptionalType(ydb.PrimitiveType.Uint64))
         .add_column("success_rate", ydb.OptionalType(ydb.PrimitiveType.Uint64))
-        .add_column("summary", ydb.OptionalType(ydb.PrimitiveType.Utf8))  
-        .add_column("owner", ydb.OptionalType(ydb.PrimitiveType.Utf8)) 
+        .add_column("summary", ydb.OptionalType(ydb.PrimitiveType.Utf8))
+        .add_column("owner", ydb.OptionalType(ydb.PrimitiveType.Utf8))
         .add_column("is_muted", ydb.OptionalType(ydb.PrimitiveType.Uint32))
         .add_column("is_test_chunk", ydb.OptionalType(ydb.PrimitiveType.Uint32))
-        .add_column("state", ydb.OptionalType(ydb.PrimitiveType.Utf8))        
-        .add_column("previous_state", ydb.OptionalType(ydb.PrimitiveType.Utf8))   
-        .add_column("state_change_date", ydb.OptionalType(ydb.PrimitiveType.Date))     
+        .add_column("state", ydb.OptionalType(ydb.PrimitiveType.Utf8))
+        .add_column("previous_state", ydb.OptionalType(ydb.PrimitiveType.Utf8))
+        .add_column("state_change_date", ydb.OptionalType(ydb.PrimitiveType.Date))
         .add_column("days_in_state", ydb.OptionalType(ydb.PrimitiveType.Uint64))
-        
-        
     )
     table_client.bulk_upsert(table_path, rows, column_types)
 
-         
+
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--days-window', default=1, type=int, help='how many days back we collecting history')
-    parser.add_argument('--build_type',choices=['relwithdebinfo', 'release-asan'], default='relwithdebinfo', type=str, help='build : relwithdebinfo or release-asan')
-    parser.add_argument('--branch', default='main',choices=['main'], type=str, help='branch')
+    parser.add_argument(
+        '--build_type',
+        choices=['relwithdebinfo', 'release-asan'],
+        default='relwithdebinfo',
+        type=str,
+        help='build : relwithdebinfo or release-asan',
+    )
+    parser.add_argument('--branch', default='main', choices=['main'], type=str, help='branch')
 
     args, unknown = parser.parse_known_args()
     history_for_n_day = args.days_window
     build_type = args.build_type
     branch = args.branch
-    
-    print(f'Getting history in window {history_for_n_day} days')
-    
+
+    print(f'Getting aggregated history in window {history_for_n_day} days')
 
     if "CI_YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS" not in os.environ:
-        print(
-            "Error: Env variable CI_YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS is missing, skipping"
-        )
+        print("Error: Env variable CI_YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS is missing, skipping")
         return 1
     else:
         # Do not set up 'real' variable from gh workflows because it interfere with ydb tests
@@ -124,11 +125,11 @@ def main():
         credentials=ydb.credentials_from_env_variables(),
     ) as driver:
         driver.wait(timeout=10, fail_fast=True)
-        
+
         # settings, paths, consts
         tc_settings = ydb.TableClientSettings().with_native_date_in_result_sets(enabled=False)
         table_client = ydb.TableClient(driver, tc_settings)
-        
+
         table_path = f'test_results/analytics/tests_monitor'
 
         data = {
@@ -146,13 +147,13 @@ def main():
             'mute_count': [],
             'fail_count': [],
             'skip_count': [],
-            'is_muted': []
+            'is_muted': [],
         }
         base_date = datetime.datetime(1970, 1, 1)
 
         today = datetime.date.today()
-        default_start_date = datetime.date(2024, 9, 28)
-        date_list = [today - datetime.timedelta(days=x) for x in range((today - default_start_date).days+1)]
+        default_start_date = datetime.date(2024, 8, 1)
+        date_list = [today - datetime.timedelta(days=x) for x in range((today - default_start_date).days + 1)]
         for date in sorted(date_list):
             query_get_history = f"""
                 SELECT
@@ -182,6 +183,7 @@ def main():
                     AND branch = '{branch}'
                     and full_name in (
                     'ydb/tests/functional/compatibility/test_compatibility.py.TestCompatibility.test_simple')
+                    
                     
                 ) AS hist 
                 LEFT JOIN (
@@ -236,36 +238,43 @@ def main():
             print(f'transaction for {date} duration: {end_time - start_time}')
             start_time = time.time()
             for row in results:
-                data['test_name'].append(row['test_name'].decode('utf-8') if isinstance(row['test_name'], bytes) else row['test_name'])
-                data['suite_folder'].append(row['suite_folder'].decode('utf-8') if isinstance(row['suite_folder'], bytes) else row['suite_folder'])
-                data['full_name'].append(row['full_name'].decode('utf-8') if isinstance(row['full_name'], bytes) else row['full_name'])
+                data['test_name'].append(row['test_name'])
+                data['suite_folder'].append(row['suite_folder'])
+                data['full_name'].append(row['full_name'])
                 data['date_window'].append(base_date + datetime.timedelta(days=row['date_window']))
-                data['build_type'].append(row['build_type'].decode('utf-8') if isinstance(row['build_type'], bytes) else row['build_type'])
-                data['branch'].append(row['branch'].decode('utf-8') if isinstance(row['branch'], bytes) else row['branch'])
-                data['owners'].append(row['owners'].decode('utf-8') if isinstance(row['owners'], bytes) else row['owners'])
+                data['build_type'].append(row['build_type'])
+                data['branch'].append(row['branch'])
+                data['owners'].append(row['owners'])
                 data['days_ago_window'].append(row['days_ago_window'])
-                data['history'].append(row['history'].decode('utf-8') if isinstance(row['history'], bytes) else row['history'])
-                data['history_class'].append(row['history_class'].decode('utf-8') if isinstance(row['history_class'], bytes) else row['history_class'])
+                data['history'].append(
+                    row['history'].decode('utf-8') if isinstance(row['history'], bytes) else row['history']
+                )
+                data['history_class'].append(
+                    row['history_class'].decode('utf-8')
+                    if isinstance(row['history_class'], bytes)
+                    else row['history_class']
+                )
                 data['pass_count'].append(row['pass_count'])
                 data['mute_count'].append(row['mute_count'])
                 data['fail_count'].append(row['fail_count'])
                 data['skip_count'].append(row['skip_count'])
                 data['is_muted'].append(row['is_muted'])
-        
 
-        
         end_time = time.time()
-        print(f'df prepared: {end_time - start_time}')
+        print(f'Dataframe inited: {end_time - start_time}')
         start_time = time.time()
         df = pd.DataFrame(data)
 
-        # Сортируем по full_name и date_window для корректного определения предыдущих статусов и дат
         df = df.sort_values(by=['full_name', 'date_window'])
+        
+        end_time = time.time()
+        print(f'Dataframe sorting: {end_time - start_time}')
+        start_time = time.time()
 
         def determine_state(row):
             history_class = row['history_class']
             is_muted = row['is_muted']
-            
+
             if is_muted == 1:
                 if 'mute' in history_class:
                     return 'Muted Flaky'
@@ -287,16 +296,8 @@ def main():
                 else:
                     return history_class
 
-        
-        # Определяем флаг для стрика
         def calculate_streak_flag(row):
-            if row['state'] == row['previous_state'] :
-                return 0
-            else:
-                return 1     
-        # Определяем флаг для стрика
-        def calculate_filled_streak_flag(row):
-            if row['state_filled'] == row['previous_state'] :
+            if row['state'] == row['previous_state']:
                 return 0
             else:
                 return 1
@@ -307,14 +308,23 @@ def main():
                 return 0.0
             else:
                 return (row['pass_count'] / total_count) * 100
-            
+
         def calculate_summary(row):
-            return 'Pass:'+ str(row['pass_count']) + ' Fail:'+ str(row['fail_count']) + ' Mute:'+ str(row['mute_count'])  + ' Skip:'+ str(row['skip_count']) 
-        
+            return (
+                'Pass:'
+                + str(row['pass_count'])
+                + ' Fail:'
+                + str(row['fail_count'])
+                + ' Mute:'
+                + str(row['mute_count'])
+                + ' Skip:'
+                + str(row['skip_count'])
+            )
+
         def compute_owner(owner):
-            if not owner or owner == '':  
+            if not owner or owner == '':
                 return 'Unknown'
-            elif ';;' in owner:  
+            elif ';;' in owner:
                 parts = owner.split(';;', 1)
                 if 'TEAM' in parts[0]:
                     return parts[0]
@@ -323,11 +333,6 @@ def main():
             else:
                 return owner
 
-
-        end_time = time.time()
-        print(f'df iteration: {end_time - start_time}')
-        start_time = time.time()
-        
         df['success_rate'] = df.apply(calculate_success_rate, axis=1).astype(int)
         df['summary'] = df.apply(calculate_summary, axis=1)
         df['owner'] = df['owners'].apply(compute_owner)
@@ -335,23 +340,22 @@ def main():
         df['is_muted'] = df['is_muted'].fillna(0).astype(int)
         df['success_rate'].astype(int)
         df['state'] = df.apply(determine_state, axis=1)
-        
-        # Определяем предыдущий статус и предыдущую дату
+
+        # Determ state for prev date and saving change state date
         previous_state_list = []
         state_change_date_list = []
-      
+
         for name, group in df.groupby('full_name'):
             prev_status = 'no_runs'
             prev_date = datetime.datetime.strptime('01/08/2024', "%d/%m/%Y")
             for index, row in group.iterrows():
-                    
                 previous_state_list.append(prev_status)
-                state_change_date_list.append(prev_date)
-                
                 if row['state'] != prev_status:
 
                     prev_status = row['state']
                     prev_date = row['date_window']
+
+                state_change_date_list.append(prev_date)
 
         df['previous_state'] = previous_state_list
         df['state_change_date'] = state_change_date_list
@@ -361,38 +365,56 @@ def main():
         df['streak_id'] = df.groupby('full_name')['streak_flag'].cumsum()
         df['days_in_state'] = df.groupby(['full_name', 'streak_id']).cumcount() + 1
 
-        result = df[[
-            'full_name','date_window',
-            'suite_folder', 'test_name', 'days_ago_window', 'build_type', 'branch',
-            'history', 'history_class', 'pass_count', 'mute_count', 'fail_count' , 'skip_count',
-            'summary', 'owner','is_test_chunk', 'is_muted',
-            'state', 'previous_state', 'state_change_date', 'days_in_state', 'success_rate'
-        ]]
+        result = df[
+            [
+                'full_name',
+                'date_window',
+                'suite_folder',
+                'test_name',
+                'days_ago_window',
+                'build_type',
+                'branch',
+                'history',
+                'history_class',
+                'pass_count',
+                'mute_count',
+                'fail_count',
+                'skip_count',
+                'summary',
+                'owner',
+                'is_test_chunk',
+                'is_muted',
+                'state',
+                'previous_state',
+                'state_change_date',
+                'days_in_state',
+                'success_rate',
+            ]
+        ]
 
         end_time = time.time()
-        print(f'df data processed {end_time - start_time}')
+        print(f'Dataframe data processing {end_time - start_time}')
+        print(f'Data collected, {len(result)} rows')
         start_time = time.time()
-        print(f'monitor data collected, {len(result)} rows')
         prepared_for_update_rows = result.to_dict('records')
-       
         end_time = time.time()
-        print(f'preparing for upsert data: {end_time - start_time}')
+        print(f'Data converting to dict for upsert: {end_time - start_time}')
+
         start_upsert_time = time.time()
-     
+
         with ydb.SessionPool(driver) as pool:
 
             create_tables(pool, table_path)
-            
             full_path = posixpath.join(DATABASE_PATH, table_path)
+
             def chunk_data(data, chunk_size):
-                """Генератор для разбиения данных на чанки."""
                 for i in range(0, len(data), chunk_size):
-                    yield data[i:i + chunk_size]
+                    yield data[i : i + chunk_size]
 
-            chunk_size = 40000  # примерный размер чанка, не превышающий лимит
-
-            # Подсчет общего количества чанков
-            total_chunks = len(prepared_for_update_rows) // chunk_size + (1 if len(prepared_for_update_rows) % chunk_size != 0 else 0)
+            chunk_size = 40000
+            total_chunks = len(prepared_for_update_rows) // chunk_size + (
+                1 if len(prepared_for_update_rows) % chunk_size != 0 else 0
+            )
 
             for i, chunk in enumerate(chunk_data(prepared_for_update_rows, chunk_size), start=1):
                 start_time = time.time()
@@ -401,7 +423,6 @@ def main():
                 end_time = time.time()
                 print(f'upsert for: {end_time - start_time} ')
 
-        
         end_time = time.time()
         print(f'monitor data upserted: {end_time - start_upsert_time}')
 

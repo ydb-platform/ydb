f686bd4e452e313b70f60589895d06b83e67500b
nkltszrv <97711375+nkltszrv@users.noreply.github.com>
2024-10-05 10:03:42 +0300

    Update actors.h

--- tools/actor_model/actors.h
+++ tools/actor_model/actors.h
@@ -4,9 +4,21 @@
 #include <library/cpp/actors/util/should_continue.h>
 
 THolder<NActors::IActor> CreateSelfPingActor(const TDuration& latency);
+THolder<NActors::IActor> CreateSelfTReadActor(NActors::TActorId writeActor);
+THolder<NActors::IActor> CreateSelfTMaximumPrimeDevisorActor(int value, NActors::TActorIdentity readActor, NActors::TActorId writeActor);
+THolder<NActors::IActor> CreateSelfTWriteActor();
 
 std::shared_ptr<TProgramShouldContinue> GetProgramShouldContinue();
 
-THolder<NActors::IActor> CreateReadActor(std::istream& strm, NActors::TActorId recipient);
-THolder<NActors::IActor> CreateMaximumPrimeDevisorActor(int64_t value, NActors::TActorId readActorId, NActors::TActorId writeActorId);
-THolder<NActors::IActor> CreateWriteActor();
+class checkIsPrimeNumberReturns {
+public:
+    bool status;
+    bool result;
+    int received;
+
+    checkIsPrimeNumberReturns(bool status, bool result, int receivedValue) : status(status), result(result),
+                                                                             received(receivedValue){};
+};
+
+checkIsPrimeNumberReturns* checkIsPrimeNumber(int n, auto startTime, int received);
+

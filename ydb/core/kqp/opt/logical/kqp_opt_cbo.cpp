bf47906a918d8993bee2a0bfe56d04e2504a8130
pilik <pudge1000-7@ydb.tech>
2024-10-07 22:30:27 +0300

    [KQP] turn off CBO IsLookupJoinApplicable for not row column. (#10141)
    Co-authored-by: Pavel Ivanov <pudge1000-7@qavm-9f0570a4.qemu>

--- ydb/core/kqp/opt/logical/kqp_opt_cbo.cpp
+++ ydb/core/kqp/opt/logical/kqp_opt_cbo.cpp
@@ -111,11 +111,13 @@ bool IsLookupJoinApplicable(std::shared_ptr<IBaseOptimizerNode> left,
     const std::set<std::pair<TJoinColumn, TJoinColumn>>& joinConditions,
     const TVector<TString>& leftJoinKeys,
     const TVector<TString>& rightJoinKeys,
-    TKqpProviderContext& ctx) {
+    TKqpProviderContext& ctx
+) {
+    Y_UNUSED(left, joinConditions, leftJoinKeys);
 
-    Y_UNUSED(joinConditions);
-    Y_UNUSED(left);
-    Y_UNUSED(leftJoinKeys);
+    if (!(right->Stats->StorageType == EStorageType::RowStorage)) {
+        return false;
+    }
 
     auto rightStats = right->Stats;
 

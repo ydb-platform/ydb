6798d3a98d9e173ac86cd97669864bd03fe1fa9d
Innokentii Mokin <innokentii@ydb.tech>
2024-10-08 15:29:05 +0000

    fix

--- ydb/core/util/source_location.cpp
+++ ydb/core/util/source_location.cpp
@@ -6,7 +6,7 @@ constexpr inline auto ThisFileLocation = NCompat::TSourceLocation::current();
 
 TString TrimSourceFileName(const char* fileName) {
     if constexpr (NCompat::HasSourceLocation) {
-        constexpr static ui64 thisFileSuffix = 33;// length of "ydb/core/util/source_location.cpp"
+        constexpr static ui64 thisFileSuffix = 33; // length of "ydb/core/util/source_location.cpp"
         const static ui64 prefixLen = std::strlen(ThisFileLocation.file_name()) - thisFileSuffix;
 
         return TString(fileName + prefixLen);

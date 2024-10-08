4e62af94e308c337266703dce5028ab7e5e9baea
Innokentii Mokin <innokentii@ydb.tech>
2024-10-08 18:41:34 +0000

    fix

--- ydb/core/util/source_location.cpp
+++ ydb/core/util/source_location.cpp
@@ -5,15 +5,15 @@
 namespace NKikimr::NUtil {
 
 constexpr inline auto ThisFileLocation = NCompat::TSourceLocation::current();
-constexpr inline ui64 ThisFileSuffix = 33; // length of "ydb/core/util/source_location.cpp"
+constexpr inline ui64 ThisFileSuffixLength = 33; // length of "ydb/core/util/source_location.cpp"
 constexpr inline ui64 ThisFileLength = std::string_view(ThisFileLocation.file_name()).length();
-constexpr inline ui64 PrefixLength = ThisFileLength - ThisFileSuffix;
+constexpr inline ui64 PrefixLength = ThisFileLength - ThisFileSuffixLength;
 
 TString TrimSourceFileName(const char* fileName) {
     if constexpr (NCompat::HasSourceLocation) {
         static_assert(std::string_view(ThisFileLocation.file_name()).substr(PrefixLength) == std::string_view("ydb/core/util/source_location.cpp"));
 
-        if (std::strlen(fileName) < PrefixLength) {
+        if (!std::string_view(fileName).starts_with(std::string_view(ThisFileLocation.file_name()).substr(0, PrefixLength))) {
             return fileName;
         }
 

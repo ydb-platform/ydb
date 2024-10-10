4a4664958d08a6812618a341c44d4a7c5612cda7
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-10-10 12:11:26 +0300

    correction

--- ydb/core/tx/priorities/usage/abstract.cpp
+++ ydb/core/tx/priorities/usage/abstract.cpp
@@ -1,5 +1,20 @@
 #include "abstract.h"
+#include "events.h"
+
+#include <ydb/library/actors/core/actor.h>
+#include <ydb/library/actors/core/log.h>
 
 namespace NKikimr::NPrioritiesQueue {
+
+TAllocationGuard::~TAllocationGuard() {
+    AFL_VERIFY(Released);
+}
+
+void TAllocationGuard::Release(const NActors::TActorId& serviceActorId) {
+    AFL_VERIFY(!Released);
+    auto& context = NActors::TActorContext::AsActorContext();
+    context.Send(serviceActorId, new TEvExecution::TEvFree(ClientId, Count));
+    Released = true;
+}
 
 }   // namespace NKikimr::NPrioritiesQueue

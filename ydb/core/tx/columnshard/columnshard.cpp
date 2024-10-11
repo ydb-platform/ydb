21d39610fbc9a7efa78cbf5daf41e36685573efd
Semyon Yentsov <yentsovsemyon@ydb.tech>
2024-10-11 07:59:32 +0000

    fixxxxxxxx

--- ydb/core/tx/columnshard/columnshard.cpp
+++ ydb/core/tx/columnshard/columnshard.cpp
@@ -103,7 +103,7 @@ void TColumnShard::OnActivateExecutor(const TActorContext& ctx) {
 
 void TColumnShard::Handle(TEvPrivate::TEvTieringModified::TPtr& /*ev*/, const TActorContext& /*ctx*/) {
     if (NYDBTest::TControllers::GetColumnShardController()->OverrideTieringSnapshot(Tiers->MutableTiers(), Tiers->MutableTieringRules())) {
-        Tiers->OnConfigsUpdated();
+        Tiers->OnConfigsUpdated(false);
     }
 
     OnTieringModified();

7903be8daa37a82d0a7b7a233616df2056197588
Grigoriy Pisarenko <grigoriypisar@ydb.tech>
2024-10-14 09:03:54 +0000

    Added message accumulating

--- ydb/core/fq/libs/row_dispatcher/json_filter.cpp
+++ ydb/core/fq/libs/row_dispatcher/json_filter.cpp
@@ -54,7 +54,7 @@ private:
     TVector<NYT::TNode> Schemas;
 };
 
-class TFilterInputConsumer : public NYql::NPureCalc::IConsumer<std::pair<ui64, const TVector<TVector<std::string_view>>&>> {
+class TFilterInputConsumer : public NYql::NPureCalc::IConsumer<std::pair<const TVector<ui64>&, const TVector<TVector<std::string_view>>&>> {
 public:
     TFilterInputConsumer(
         const TFilterInputSpec& spec,
@@ -92,7 +92,7 @@ public:
         }
     }
 
-    void OnObject(std::pair<ui64, const TVector<TVector<std::string_view>>&> values) override {
+    void OnObject(std::pair<const TVector<ui64>&, const TVector<TVector<std::string_view>>&> values) override {
         Y_ENSURE(FieldsPositions.size() == values.second.size());
 
         NKikimr::NMiniKQL::TThrowingBindTerminator bind;
@@ -108,7 +108,7 @@ public:
                     static_cast<ui32>(values.second.size() + 1),
                     items);
 
-                items[OffsetPosition] = NYql::NUdf::TUnboxedValuePod(values.first++);
+                items[OffsetPosition] = NYql::NUdf::TUnboxedValuePod(values.first[rowId]);
 
                 size_t fieldId = 0;
                 for (const auto& column : values.second) {
@@ -200,7 +200,7 @@ struct NYql::NPureCalc::TInputSpecTraits<TFilterInputSpec> {
     static constexpr bool IsPartial = false;
     static constexpr bool SupportPushStreamMode = true;
 
-    using TConsumerType = THolder<NYql::NPureCalc::IConsumer<std::pair<ui64, const TVector<TVector<std::string_view>>&>>>;
+    using TConsumerType = THolder<NYql::NPureCalc::IConsumer<std::pair<const TVector<ui64>&, const TVector<TVector<std::string_view>>&>>>;
 
     static TConsumerType MakeConsumer(
         const TFilterInputSpec& spec,
@@ -242,9 +242,9 @@ public:
         LOG_ROW_DISPATCHER_DEBUG("Program created");
     }
 
-    void Push(ui64 offset, const TVector<TVector<std::string_view>>& values) {
+    void Push(const TVector<ui64>& offsets, const TVector<TVector<std::string_view>>& values) {
         Y_ENSURE(values, "Expected non empty schema");
-        InputConsumer->OnObject(std::make_pair(offset, values));
+        InputConsumer->OnObject(std::make_pair(offsets, values));
     }
 
     TString GetSql() const {
@@ -277,7 +277,7 @@ private:
 
 private:
     THolder<NYql::NPureCalc::TPushStreamProgram<TFilterInputSpec, TFilterOutputSpec>> Program;
-    THolder<NYql::NPureCalc::IConsumer<std::pair<ui64, const TVector<TVector<std::string_view>>&>>> InputConsumer;
+    THolder<NYql::NPureCalc::IConsumer<std::pair<const TVector<ui64>&, const TVector<TVector<std::string_view>>&>>> InputConsumer;
     const TString Sql;
 };
 
@@ -292,8 +292,8 @@ TJsonFilter::TJsonFilter(
 TJsonFilter::~TJsonFilter() {
 }
 
-void TJsonFilter::Push(ui64 offset, const TVector<TVector<std::string_view>>& values) {
-    Impl->Push(offset, values);
+void TJsonFilter::Push(const TVector<ui64>& offsets, const TVector<TVector<std::string_view>>& values) {
+    Impl->Push(offsets, values);
 }
 
 TString TJsonFilter::GetSql() {

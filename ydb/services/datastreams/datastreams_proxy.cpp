02cefd2c0d6b329ea8e78eeab62a49cfda8b0fd1
Nikita Saveliev <savnik@ydb.tech>
2024-10-14 09:51:42 +0000

    fix

--- ydb/services/datastreams/datastreams_proxy.cpp
+++ ydb/services/datastreams/datastreams_proxy.cpp
@@ -1648,12 +1648,9 @@ namespace NKikimr::NDataStreams::V1 {
                 record->set_approximate_arrival_timestamp(r.GetCreateTimestampMS());
                 record->set_partition_key(r.GetPartitionKey());
                 record->set_sequence_number(std::to_string(r.GetOffset()).c_str());
-
                 if (proto.GetCodec() > 0) {
                     record->set_codec(proto.GetCodec() + 1);
                 }
-
-                record->set_codec(proto.GetCodec() + 1);
             }
             if (!results.empty()) {
                 auto last = results.rbegin();

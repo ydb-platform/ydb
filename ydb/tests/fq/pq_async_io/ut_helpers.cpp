be518499426532d04cab8e561dc1e6c315c0190e
Dmitry Kardymon <kardymon-d@ydb.tech>
2024-10-16 20:15:16 +0000

    try to fix build

--- ydb/tests/fq/pq_async_io/ut_helpers.cpp
+++ ydb/tests/fq/pq_async_io/ut_helpers.cpp
@@ -106,6 +106,14 @@ void TPqIoTestFixture::InitAsyncOutput(
 {
     const THashMap<TString, TString> secureParams;
 
+    TPqGatewayServices pqServices(
+            Driver,
+            nullptr,
+            nullptr,
+            std::make_shared<TPqGatewayConfig>(),
+            nullptr
+        );
+
     CaSetup->Execute([&](TFakeActor& actor) {
         auto [dqAsyncOutput, dqAsyncOutputAsActor] = CreateDqPqWriteActor(
             std::move(settings),
@@ -118,6 +126,7 @@ void TPqIoTestFixture::InitAsyncOutput(
             nullptr,
             &actor.GetAsyncOutputCallbacks(),
             MakeIntrusive<NMonitoring::TDynamicCounters>(),
+            CreatePqNativeGateway(std::move(pqServices)),
             freeSpace);
 
         actor.InitAsyncOutput(dqAsyncOutput, dqAsyncOutputAsActor);

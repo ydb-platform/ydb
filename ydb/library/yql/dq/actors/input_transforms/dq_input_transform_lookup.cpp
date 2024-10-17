aadf67f0b404ed91cf4f0f4e4c858928fd3bab31
yumkam <yumkam7@ydb.tech>
2024-10-17 18:43:09 +0300

    Streamlookup special case nulls in keys (#10508)

--- ydb/library/yql/dq/actors/input_transforms/dq_input_transform_lookup.cpp
+++ ydb/library/yql/dq/actors/input_transforms/dq_input_transform_lookup.cpp
@@ -220,13 +220,19 @@ private: //IDqComputeActorAsyncInput
                 NUdf::TUnboxedValue key = HolderFactory.CreateDirectArrayHolder(LookupInputIndexes.size(), keyItems);
                 NUdf::TUnboxedValue* otherItems;
                 NUdf::TUnboxedValue other = HolderFactory.CreateDirectArrayHolder(OtherInputIndexes.size(), otherItems);
+                bool nullsInKey = false;
                 for (size_t i = 0; i != LookupInputIndexes.size(); ++i) {
                     keyItems[i] = inputRowItems[LookupInputIndexes[i]];
+                    if (!keyItems[i]) {
+                        nullsInKey = true;
+                    }
                 }
                 for (size_t i = 0; i != OtherInputIndexes.size(); ++i) {
                     otherItems[i] = inputRowItems[OtherInputIndexes[i]];
                 }
-                if (auto lookupPayload = LruCache->Get(key, now)) {
+                if (nullsInKey) {
+                    AddReadyQueue(key, other, nullptr);
+                } else if (auto lookupPayload = LruCache->Get(key, now)) {
                     AddReadyQueue(key, other, &*lookupPayload);
                 } else {
                     if (AwaitingQueue.empty()) {

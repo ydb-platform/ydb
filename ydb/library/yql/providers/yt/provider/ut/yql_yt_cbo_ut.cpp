d2ecb46aa1e3433632574e0559f8b58666b0a41c
Pavel Ivanov <pudge1000-7@ydb.tech>
2024-10-18 16:38:42 +0000

    fix test

--- ydb/library/yql/providers/yt/provider/ut/yql_yt_cbo_ut.cpp
+++ ydb/library/yql/providers/yt/provider/ut/yql_yt_cbo_ut.cpp
@@ -74,9 +74,8 @@ Y_UNIT_TEST(OrderJoinsDoesNothingWhenCBODisabled) {
 }
 
 Y_UNIT_TEST(NonReordable) {
-    auto stat = std::make_shared<TOptimizerStatistics>();
-    auto left = std::make_shared<TRelOptimizerNode>("a", stat);
-    auto right = std::make_shared<TRelOptimizerNode>("a", stat);
+    auto left = std::make_shared<TRelOptimizerNode>("a", TOptimizerStatistics());
+    auto right = std::make_shared<TRelOptimizerNode>("a", TOptimizerStatistics());
 
     TVector<NDq::TJoinColumn> leftKeys = {NDq::TJoinColumn{"a", "b"}};
     TVector<NDq::TJoinColumn> rightKeys = {NDq::TJoinColumn{"a","c"}};
@@ -90,12 +89,12 @@ Y_UNIT_TEST(NonReordable) {
     // Join tree is built from scratch with DPhyp, check the structure by comapring with Stats 
     UNIT_ASSERT(root->LeftArg->Kind == RelNodeType);
     UNIT_ASSERT(
-        std::static_pointer_cast<TRelOptimizerNode>(root->LeftArg)->Stats == left->Stats
+        &std::static_pointer_cast<TRelOptimizerNode>(root->LeftArg)->Stats == &left->Stats
     );
 
     UNIT_ASSERT(root->RightArg->Kind == RelNodeType);
     UNIT_ASSERT(
-        std::static_pointer_cast<TRelOptimizerNode>(root->RightArg)->Stats == right->Stats
+        &std::static_pointer_cast<TRelOptimizerNode>(root->RightArg)->Stats == &right->Stats
     );
 }
 

8957e61721eb08bbc27e3494a4547027fd4c3f63
Semyon Yentsov <yentsovsemyon@ydb.tech>
2024-10-21 11:23:51 +0000

    add TieringRuleInfo base class

---
+++ ydb/core/tx/tiering/rule/info.h
@@ -0,0 +1,76 @@
+#include <ydb/core/protos/flat_scheme_op.pb.h>
+
+#include <ydb/library/accessor/accessor.h>
+
+#include <util/datetime/base.h>
+#include <util/generic/fwd.h>
+
+namespace NKikimr::NColumnShard::NTiers {
+
+class TTieringInterval {
+private:
+    YDB_ACCESSOR_DEF(TString, TierName);
+    YDB_ACCESSOR_DEF(TDuration, DurationForEvict);
+
+public:
+    using TProto = NKikimrSchemeOp::TTieringIntervals::TTieringInterval;
+
+    TTieringInterval() = default;
+    TTieringInterval(const TString& name, const TDuration d)
+        : TierName(name)
+        , DurationForEvict(d) {
+    }
+
+    bool operator<(const TTieringInterval& item) const {
+        return DurationForEvict < item.DurationForEvict;
+    }
+
+    bool DeserializeFromProto(const NKikimrSchemeOp::TTieringIntervals::TTieringInterval& proto) {
+        TierName = proto.GetTierName();
+        DurationForEvict = TDuration::MilliSeconds(proto.GetEvictionDelayMs());
+        return true;
+    }
+
+    TProto SerializeToProto() const {
+        TProto serialized;
+        serialized.SetTierName(TierName);
+        serialized.SetEvictionDelayMs(DurationForEvict.MilliSeconds());
+        return serialized;
+    }
+};
+
+class TTieringRuleInfo {
+private:
+    YDB_ACCESSOR_DEF(TString, DefaultColumn);
+    YDB_ACCESSOR_DEF(std::vector<TTieringInterval>, Intervals);
+
+public:
+    using TProto = NKikimrSchemeOp::TTieringRuleProperties;
+
+    bool DeserializeFromProto(const TProto& proto) {
+        DefaultColumn = proto.GetDefaultColumn();
+
+        Intervals.clear();
+        for (const auto& interval : proto.GetTiers().GetIntervals()) {
+            Intervals.emplace_back();
+            if (!Intervals.back().DeserializeFromProto(interval)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    TProto SerializeToProto() const {
+        TProto serialized;
+
+        serialized.SetDefaultColumn(DefaultColumn);
+        auto* intervalsProto = serialized.MutableTiers()->MutableIntervals();
+        for (const auto& interval : Intervals) {
+            *intervalsProto->Add() = interval.SerializeToProto();
+        }
+
+        return serialized;
+    }
+};
+
+}   // namespace NKikimr::NColumnShard::NTiers

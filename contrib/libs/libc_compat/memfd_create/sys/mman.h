dfad77972624024ed1d37041483a8edd82b666d4
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2024-10-22 13:35:30 +0000

    Sample changes for ubuntu 22 + makefiles

--- contrib/libs/libc_compat/memfd_create/sys/mman.h
+++ contrib/libs/libc_compat/memfd_create/sys/mman.h
@@ -6,8 +6,13 @@
 extern "C" {
 #endif
 
+#ifndef MFD_CLOEXEC
 #define MFD_CLOEXEC 		0x0001U
+#endif
+
+#ifndef MADV_WIPEONFORK
 #define MADV_WIPEONFORK  	18
+#endif
 
 int memfd_create(const char *name, unsigned flags);
 

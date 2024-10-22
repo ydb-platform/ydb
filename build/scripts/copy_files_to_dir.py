54a66b6ba4d7d5b11ad688e8072df4c2f107b3c9
robot-brewer <robot-brewer@yandex-team.com>
2024-10-22 08:39:26 +0300

    Release python3-frozen #8
    https://github.com/yandex/toolchain-registry/releases/tag/python3-frozen-v8
    commit_hash:db14a768da738da2ea13bd7cb7af738d83793500

--- build/scripts/copy_files_to_dir.py
+++ build/scripts/copy_files_to_dir.py
@@ -1,9 +1,13 @@
 import argparse
 import errno
 import os
+import sys
+
+# Explicitly enable local imports
+# Don't forget to add imported scripts to inputs of the calling command!
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import process_command_files as pcf
 import shutil
-import sys
 
 
 def parse_args():

34bc19cd92a88697448d2ba23cd9c7f13d2b6490
Alexey Pozdniakov <pas.9250306384@gmail.com>
2024-10-23 13:06:40 +0300

    [YQ-3621] support AFTER MATCH SKIP PAST LAST ROW (#10597) (#10743)

--- ydb/library/yql/minikql/mkql_program_builder.h
+++ ydb/library/yql/minikql/mkql_program_builder.h
@@ -700,7 +700,8 @@ public:
         const TArrayRef<std::pair<TStringBuf, TBinaryLambda>>& getMeasures,
         const NYql::NMatchRecognize::TRowPattern& pattern,
         const TArrayRef<std::pair<TStringBuf, TTernaryLambda>>& getDefines,
-        bool streamingMode
+        bool streamingMode,
+        const NYql::NMatchRecognize::TAfterMatchSkipTo& skipTo
     );
 
     TRuntimeNode TimeOrderRecover(

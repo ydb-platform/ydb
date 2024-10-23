62d391233799b9236bb0a4d633cbe94bb28f053d
Gigoriy Pisarenko <grigoriypisar@ydb.tech>
2024-10-23 15:36:18 +0000

    Simplified include

--- ydb/library/yql/public/purecalc/ut/test_push_stream.cpp
+++ ydb/library/yql/public/purecalc/ut/test_push_stream.cpp
@@ -1,6 +1,6 @@
 #include <library/cpp/testing/unittest/registar.h>
 
-#include <ydb/library/yql/minikql/computation/mkql_computation_node_holders.h>
+#include <ydb/library/yql/minikql/computation/mkql_computation_node.h>
 #include <ydb/library/yql/minikql/mkql_string_util.h>
 
 #include <ydb/library/yql/public/purecalc/io_specs/protobuf/spec.h>

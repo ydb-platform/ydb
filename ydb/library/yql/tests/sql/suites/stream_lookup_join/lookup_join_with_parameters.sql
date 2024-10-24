288332066cd1c18d2b14aeb4d9632ddba6eb0f52
Yuriy Kaminskiy <yumkam7@ydb.tech>
2024-10-24 14:03:10 +0300

    fix typo in new test

--- ydb/library/yql/tests/sql/suites/stream_lookup_join/lookup_join_with_parameters.sql
+++ ydb/library/yql/tests/sql/suites/stream_lookup_join/lookup_join_with_parameters.sql
@@ -3,7 +3,7 @@ use plato;
 
 select e.id as id, e.ts as ts, e.host as host, h.fqdn as fqdn, h.ip4 as ip4, h.ip6 as ip6
     from Event as e 
-    left join /*+ streamlookup(TTL 1 MaxCachedRows 5 MaxDelay 3) */ any Host as h
+    left join /*+ streamlookup(TTL 1 MaxCachedRows 5 MaxDelayedRows 3) */ any Host as h
     on (e.host == h.hostname)
 ;
 

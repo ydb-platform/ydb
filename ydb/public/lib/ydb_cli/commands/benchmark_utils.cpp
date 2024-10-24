77c9bbf6226c8152fc77ce9473dd335f33dcfad7
Oleg Geller <iddqd@ydb.tech>
2024-10-24 16:45:59 +0300

    fix

--- ydb/public/lib/ydb_cli/commands/benchmark_utils.cpp
+++ ydb/public/lib/ydb_cli/commands/benchmark_utils.cpp
@@ -469,8 +469,8 @@ bool CompareValueImplDecimal(const NYdb::TDecimalValue& valResult, TStringBuf vE
         auto precInt = NYql::NDecimal::FromString(precesionStr, valResult.DecimalType_.Precision, valResult.DecimalType_.Scale);
         return resInt >= expectedInt - precInt && resInt <= expectedInt + precInt;
     }
-    const auto from = NYql::NDecimal::FromString("1.0001", valResult.DecimalType_.Precision, valResult.DecimalType_.Scale);
-    const auto to = NYql::NDecimal::FromString("0.9999", valResult.DecimalType_.Precision, valResult.DecimalType_.Scale);
+    const auto from = NYql::NDecimal::FromString("0.9999", valResult.DecimalType_.Precision, valResult.DecimalType_.Scale);
+    const auto to = NYql::NDecimal::FromString("1.0001", valResult.DecimalType_.Precision, valResult.DecimalType_.Scale);
     const auto devider = NYql::NDecimal::GetDivider(valResult.DecimalType_.Scale);
     return resInt > NYql::NDecimal::MulAndDivNormalDivider(from, expectedInt, devider) && resInt < NYql::NDecimal::MulAndDivNormalDivider(to, expectedInt, devider);
 }

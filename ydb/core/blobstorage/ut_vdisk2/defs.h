85e97906fae7f9cd407bdfcdf0ea876c2474b084
beehelit <iamnotexist@ydb.tech>
2024-10-24 11:58:08 +0000

    also move single_thread_ic_mock, update defs

--- ydb/core/blobstorage/ut_vdisk2/defs.h
+++ ydb/core/blobstorage/ut_vdisk2/defs.h
@@ -9,4 +9,21 @@
 #include <ydb/core/cms/console/console.h>
 #include <library/cpp/testing/unittest/registar.h>
 
+#include <ydb/library/actors/core/actor.h>
+#include <ydb/library/actors/core/hfunc.h>
+#include <ydb/library/actors/core/interconnect.h>
+#include <ydb/library/actors/interconnect/interconnect_common.h>
+
+#include <util/system/defaults.h>
+#include <util/generic/bt_exception.h>
+#include <util/generic/noncopyable.h>
+#include <util/generic/ptr.h>
+#include <util/generic/string.h>
+#include <util/generic/yexception.h>
+#include <library/cpp/deprecated/atomic/atomic.h>
+#include <util/system/align.h>
+#include <util/generic/vector.h>
+#include <util/datetime/base.h>
+#include <util/generic/ylimits.h>
+
 #include "testactorsys.h"

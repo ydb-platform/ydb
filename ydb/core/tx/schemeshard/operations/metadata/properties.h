55e77232696789b03037f72b76bc091cce78280c
Semyon Yentsov <yentsovsemyon@ydb.tech>
2024-10-24 09:02:53 +0000

    fix alter

--- ydb/core/tx/schemeshard/operations/metadata/properties.h
+++ ydb/core/tx/schemeshard/operations/metadata/properties.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <ydb/core/protos/flat_scheme_op.pb.h>
 
+#include <ydb/library/actors/core/log.h>
+
 #include <library/cpp/object_factory/object_factory.h>
 
 namespace NKikimr::NSchemeShard {
@@ -15,12 +17,23 @@ public:
     [[nodiscard]] virtual bool DeserializeFromProto(const TProto& proto) = 0;
     [[nodiscard]] virtual bool ApplyPatch(const TProto& patch) = 0;
     virtual TProto SerializeToProto() const = 0;
+    virtual void CopyFromVerified(const IMetadataObjectProperties::TPtr& other) = 0;
 
     static TPtr Create(NKikimrSchemeOp::EPathType pathType) {
         return TPtr(TFactory::Construct(pathType));
     }
 
     virtual ~IMetadataObjectProperties() = default;
+};
+
+template <typename TDerived>
+class TMetadataObjectPropertiesBase: public IMetadataObjectProperties {
+public:
+    void CopyFromVerified(const IMetadataObjectProperties::TPtr& other) override {
+        std::shared_ptr<TDerived> converted = std::dynamic_pointer_cast<TDerived>(other);
+        AFL_VERIFY(converted)("other", other->SerializeToProto());
+        *static_cast<TDerived*>(this) = *converted;
+    }
 };
 
 }   // namespace NKikimr::NSchemeShard

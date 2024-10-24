4e19b8dd32d03257bb45ba9df4a657891dcdbe1c
Yuriy Kaminskiy <yumkam7@ydb.tech>
2024-10-24 17:17:20 +0300

    Revert "streamlookup: add assert for use-after-free and improper state transitions"
    This reverts commit 33038f8ac74b11cbd3c151b650269146871ff2dd.

--- ydb/library/yql/dq/actors/input_transforms/dq_input_transform_lookup.cpp
+++ ydb/library/yql/dq/actors/input_transforms/dq_input_transform_lookup.cpp
@@ -79,22 +79,13 @@ public:
         }
         Y_DEBUG_ABORT_UNLESS(LookupInputIndexes.size() == LookupKeyType->GetMembersCount());
         InitMonCounters(taskCounters);
-#ifndef NDEBUG
-        state = EState::Alive;
-#endif
     }
 
     ~TInputTransformStreamLookupBase() override {
         Free();
-#ifndef NDEBUG
-        state = EState::Destroyed;
-#endif
     }
 
     void Bootstrap() {
-#ifndef NDEBUG
-        Y_ENSURE(state == EState::Alive);
-#endif
         Become(&TInputTransformStreamLookupBase::StateFunc);
         NDq::IDqAsyncIoFactory::TLookupSourceArguments lookupSourceArgs {
             .Alloc = Alloc,
@@ -113,9 +104,6 @@ public:
         MaxKeysInRequest = lookupSource->GetMaxSupportedKeysInRequest();
         LookupSourceId = RegisterWithSameMailbox(lookupSourceActor);
         KeysForLookup = std::make_shared<IDqAsyncLookupSource::TUnboxedValueMap>(MaxKeysInRequest, KeyTypeHelper->GetValueHash(), KeyTypeHelper->GetValueEqual());
-#ifndef NDEBUG
-        state = EState::Bootstrapped;
-#endif
     }
 protected:
     virtual NUdf::EFetchStatus FetchWideInputValue(NUdf::TUnboxedValue* inputRowItems) = 0;
@@ -164,9 +152,6 @@ private: //events
     }
 
     void Handle(IDqAsyncLookupSource::TEvLookupResult::TPtr ev) {
-#ifndef NDEBUG
-        Y_ENSURE(state == EState::Bootstrapped);
-#endif
         auto startCycleCount = GetCycleCountFast();
         if (!KeysForLookup) {
             return;
@@ -210,17 +195,11 @@ private: //IDqComputeActorAsyncInput
     }
 
     void PassAway() final {
-#ifndef NDEBUG
-        Y_ENSURE(state == EState::Alive || state == EState::Bootstrapped || state == EState::Away);
-#endif
         Send(LookupSourceId, new NActors::TEvents::TEvPoison{});
         Free();
     }
 
     void Free() {
-#ifndef NDEBUG
-        Y_ENSURE(state == EState::Alive || state == EState::Bootstrapped || state == EState::Away);
-#endif
         auto guard = BindAllocator();
         //All resources, held by this class, that have been created with mkql allocator, must be deallocated here
         KeysForLookup.reset();
@@ -229,9 +208,6 @@ private: //IDqComputeActorAsyncInput
         decltype(AwaitingQueue){}.swap(AwaitingQueue);
         decltype(ReadyQueue){}.swap(ReadyQueue);
         LruCache.reset();
-#ifndef NDEBUG
-        state = EState::Away;
-#endif
     }
 
     void DrainReadyQueue(NKikimr::NMiniKQL::TUnboxedValueBatch& batch) {
@@ -242,9 +218,6 @@ private: //IDqComputeActorAsyncInput
     }
 
     i64 GetAsyncInputData(NKikimr::NMiniKQL::TUnboxedValueBatch& batch, TMaybe<TInstant>&, bool& finished, i64 freeSpace) final {
-#ifndef NDEBUG
-        Y_ENSURE(state == EState::Bootstrapped);
-#endif
         Y_UNUSED(freeSpace);
         auto startCycleCount = GetCycleCountFast();
         auto guard = BindAllocator();
@@ -391,9 +364,6 @@ protected:
     ::NMonitoring::TDynamicCounters::TCounterPtr CpuTimeUs;
     ::NMonitoring::TDynamicCounters::TCounterPtr Batches;
     TDuration CpuTime;
-#ifndef NDEBUG
-    enum class EState { Alive = 0x42244224U, Bootstrapped = 0x24242424U, Away = 0x42424242U, Destroyed = 0x24422442U } state;
-#endif
 };
 
 class TInputTransformStreamLookupWide: public TInputTransformStreamLookupBase {

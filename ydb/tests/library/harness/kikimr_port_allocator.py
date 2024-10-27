ab231985c35b6f6366fc3769e69ce083f2972864
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2024-10-27 17:56:24 +0000

    fix

--- ydb/tests/library/harness/kikimr_port_allocator.py
+++ ydb/tests/library/harness/kikimr_port_allocator.py
@@ -2,6 +2,7 @@
 import abc
 import os
 import yatest
+import yatest.common.network
 
 
 class KikimrNodePortAllocatorInterface(object):
@@ -140,7 +141,7 @@ class KikimrPortManagerNodePortAllocator(KikimrNodePortAllocatorInterface):
 class KikimrPortManagerPortAllocator(KikimrPortAllocatorInterface):
     def __init__(self, port_manager=None):
         super(KikimrPortManagerPortAllocator, self).__init__()
-        self.__port_manager = yatest.common.PortManager() if port_manager is None else port_manager
+        self.__port_manager = yatest.common.network.PortManager() if port_manager is None else port_manager
         self.__nodes_allocators = []
         self.__slots_allocators = []
 

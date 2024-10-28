2382bfef0103463bf8401e44ed25f22cd9e013d4
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-10-28 16:45:10 +0300

    fix

--- ydb/core/tx/columnshard/engines/portions/data_accessor.h
+++ ydb/core/tx/columnshard/engines/portions/data_accessor.h
@@ -67,10 +67,6 @@ public:
         : PortionInfo(&portionInfo) {
     }
 
-    const std::vector<TColumnRecord>& GetRecords() const {
-        return PortionInfo->Records;
-    }
-
     ui64 GetColumnRawBytes(const std::set<ui32>& entityIds, const bool validation = true) const;
     ui64 GetColumnBlobBytes(const std::set<ui32>& entityIds, const bool validation = true) const;
     ui64 GetIndexRawBytes(const std::set<ui32>& entityIds, const bool validation = true) const;

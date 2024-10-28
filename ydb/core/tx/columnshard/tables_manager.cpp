80a9515a404993e3d882d53d6630472af0f51fdb
Alexander Avdonkin <aavdonkin@yandex.ru>
2024-10-28 09:23:43 +0000

    Correclty resolve conflict

--- ydb/core/tx/columnshard/tables_manager.cpp
+++ ydb/core/tx/columnshard/tables_manager.cpp
@@ -317,9 +317,6 @@ void TTablesManager::AddSchemaVersion(const ui32 presetId, const NOlap::TSnapsho
         }
         auto& key = VersionCounters->GetVersionToKey()[versionInfo.GetSchema().GetVersion()];
         key.emplace_back(presetId, version.GetPlanStep(), version.GetTxId());
-        for (auto& columnName : Ttl.TtlColumns()) {
-            PrimaryIndex->GetVersionedIndex().GetLastSchema()->GetIndexInfo().CheckTtlColumn(columnName);
-        }
     }
 }
 

3019d9b64c2231015721e56b36cd8373a6f0c8b2
Oleg Doronin <dorooleg@yandex.ru>
2024-10-29 16:30:18 +0000

    topic_session cleanup

--- ydb/core/fq/libs/row_dispatcher/topic_session.cpp
+++ ydb/core/fq/libs/row_dispatcher/topic_session.cpp
@@ -33,7 +33,6 @@ struct TTopicSessionMetrics {
         InFlyAsyncInputData = SubGroup->GetCounter("InFlyAsyncInputData");
         RowsRead = SubGroup->GetCounter("RowsRead", true);
         InFlySubscribe = SubGroup->GetCounter("InFlySubscribe");
-        ReconnectRate = SubGroup->GetCounter("ReconnectRate", true);
     }
 
     ~TTopicSessionMetrics() {
@@ -44,7 +43,6 @@ struct TTopicSessionMetrics {
     ::NMonitoring::TDynamicCounters::TCounterPtr InFlyAsyncInputData;
     ::NMonitoring::TDynamicCounters::TCounterPtr RowsRead;
     ::NMonitoring::TDynamicCounters::TCounterPtr InFlySubscribe;
-    ::NMonitoring::TDynamicCounters::TCounterPtr ReconnectRate;
 };
 
 struct TEvPrivate {
@@ -59,7 +57,6 @@ struct TEvPrivate {
         EvSendStatistic,
         EvStartParsing,
         EvPrintState,
-        EvReconnectSession,
         EvEnd
     };
     static_assert(EvEnd < EventSpaceEnd(NActors::TEvents::ES_PRIVATE), "expect EvEnd < EventSpaceEnd(NActors::TEvents::ES_PRIVATE)");
@@ -68,7 +65,6 @@ struct TEvPrivate {
     struct TEvPqEventsReady : public NActors::TEventLocal<TEvPqEventsReady, EvPqEventsReady> {};
     struct TEvCreateSession : public NActors::TEventLocal<TEvCreateSession, EvCreateSession> {};
     struct TEvSendStatistic : public NActors::TEventLocal<TEvSendStatistic, EvSendStatistic> {};
-    struct TEvReconnectSession : public NActors::TEventLocal<TEvReconnectSession, EvReconnectSession> {};
     struct TEvPrintState : public NActors::TEventLocal<TEvPrintState, EvPrintState> {};
     struct TEvStatus : public NActors::TEventLocal<TEvStatus, EvStatus> {};
     struct TEvStartParsing : public NActors::TEventLocal<TEvStartParsing, EvStartParsing> {};
@@ -137,13 +133,6 @@ private:
         const TString& LogPrefix;    
     };
 
-    struct TParserSchema {
-        TVector<ui64> FieldsMap;    // index - FieldId (from FieldsIndexes), value - parsing schema offset
-        TParserInputType InputType;
-    };
-
-    bool InflightReconnect = false;
-    TDuration ReconnectPeriod;
     const TString TopicPath;
     const TString Endpoint;
     const TString Database;
@@ -212,7 +201,6 @@ private:
 
     void Handle(NFq::TEvPrivate::TEvPqEventsReady::TPtr&);
     void Handle(NFq::TEvPrivate::TEvCreateSession::TPtr&);
-    void Handle(NFq::TEvPrivate::TEvReconnectSession::TPtr&);
     void Handle(NFq::TEvPrivate::TEvDataAfterFilteration::TPtr&);
     void Handle(NFq::TEvPrivate::TEvStatus::TPtr&);
     void Handle(NFq::TEvPrivate::TEvDataFiltered::TPtr&);
@@ -382,15 +370,6 @@ void TTopicSession::CreateTopicSession() {
         ReadSession = GetTopicClient(sourceParams).CreateReadSession(GetReadSessionSettings(sourceParams));
         SubscribeOnNextEvent();
     }
-
-    if (!InflightReconnect) {
-        Y_UNUSED(TDuration::TryParse(sourceParams.GetReconnectPeriod(), ReconnectPeriod));
-        if (ReconnectPeriod != TDuration::Zero()) {
-            Metrics.ReconnectRate->Inc();
-            Schedule(ReconnectPeriod, new NFq::TEvPrivate::TEvReconnectSession());
-        }
-        InflightReconnect = true;
-    }
 }
 
 void TTopicSession::Handle(NFq::TEvPrivate::TEvPqEventsReady::TPtr&) {
@@ -403,32 +382,6 @@ void TTopicSession::Handle(NFq::TEvPrivate::TEvPqEventsReady::TPtr&) {
 
 void TTopicSession::Handle(NFq::TEvPrivate::TEvCreateSession::TPtr&) {
     CreateTopicSession();
-}
-
-TVector<const NKikimr::NMiniKQL::TUnboxedValueVector*> TTopicSession::RebuildJson(const ClientsInfo& info, const TVector<NKikimr::NMiniKQL::TUnboxedValueVector>& parsedValues) {
-    TVector<const NKikimr::NMiniKQL::TUnboxedValueVector*> result;
-    const auto& offsets = ParserSchema.FieldsMap;
-    result.reserve(info.FieldsIds.size());
-    for (auto fieldId : info.FieldsIds) {
-        Y_ENSURE(fieldId < offsets.size(), "fieldId " << fieldId << ", offsets.size() " << offsets.size());
-        auto offset = offsets[fieldId];
-        Y_ENSURE(offset < parsedValues.size(), "offset " << offset << ", jsonBatch.size() " << parsedValues.size());
-        result.push_back(&parsedValues[offset]); 
-    }
-}
-
-void TTopicSession::Handle(NFq::TEvPrivate::TEvReconnectSession::TPtr&) {
-    Metrics.ReconnectRate->Inc();
-    TInstant minTime = GetMinStartingMessageTimestamp();
-    LOG_ROW_DISPATCHER_DEBUG("Reconnect topic session, Path " << TopicPath
-        << ", StartingMessageTimestamp " << minTime
-        << ", BufferSize " << BufferSize << ", WithoutConsumer " << Config.GetWithoutConsumer());
-    if (ReadSession) {
-        ReadSession->Close(TDuration::Zero());
-        ReadSession.reset();
-    }
-    CreateTopicSession();
-    Schedule(ReconnectPeriod, new NFq::TEvPrivate::TEvReconnectSession());
 }
 
 void TTopicSession::Handle(NFq::TEvPrivate::TEvDataAfterFilteration::TPtr& ev) {

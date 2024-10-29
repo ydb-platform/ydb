0a5406a6b4a6b6ab130287c50cc6e7fb25bbd3ec
ivanmorozov333 <ivanmorozov@hotmail.com>
2024-10-29 16:29:19 +0300

    fix

--- ydb/core/tx/columnshard/engines/portions/data_accessor.h
+++ ydb/core/tx/columnshard/engines/portions/data_accessor.h
@@ -66,7 +66,7 @@ public:
     }
 
     TPortionDataAccessor(const TPortionInfo::TConstPtr& portionInfo)
-        : PortionInfo(portionInfo.get()) {
+        : PortionInfo(portionInfo) {
     }
 
     std::set<ui32> GetColumnIds() const {

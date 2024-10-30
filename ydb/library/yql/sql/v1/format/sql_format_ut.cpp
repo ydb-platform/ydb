ea7e9b6e13f06e32f612817efe0b01cc3a848084
Daniil Demin <deminds@ydb.tech>
2024-10-30 15:11:09 +0300

    Views: if exists / if not exists for DDL (#10831) (#11093)

--- ydb/library/yql/sql/v1/format/sql_format_ut.cpp
+++ ydb/library/yql/sql/v1/format/sql_format_ut.cpp
@@ -1579,9 +1579,16 @@ FROM Input MATCH_RECOGNIZE (PATTERN (A) DEFINE A AS A);
     }
 
     Y_UNIT_TEST(CreateView) {
-        TCases cases = {
-            {"creAte vIEw TheView wiTh (security_invoker = trUE) As SELect 1",
-             "CREATE VIEW TheView WITH (security_invoker = TRUE) AS\nSELECT\n\t1;\n"},
+        TCases cases = {{
+                "creAte vIEw TheView As SELect 1",
+                "CREATE VIEW TheView AS\nSELECT\n\t1;\n"
+            }, {
+                "creAte vIEw If Not ExIsTs TheView As SELect 1",
+                "CREATE VIEW IF NOT EXISTS TheView AS\nSELECT\n\t1;\n"
+            }, {
+                "creAte vIEw TheView wiTh (option = tRuE) As SELect 1",
+                "CREATE VIEW TheView WITH (option = TRUE) AS\nSELECT\n\t1;\n"
+            }
         };
 
         TSetup setup;
@@ -1589,9 +1596,13 @@ FROM Input MATCH_RECOGNIZE (PATTERN (A) DEFINE A AS A);
     }
 
     Y_UNIT_TEST(DropView) {
-        TCases cases = {
-            {"dRop viEW theVIEW",
-             "DROP VIEW theVIEW;\n"},
+        TCases cases = {{
+                "dRop viEW theVIEW",
+                "DROP VIEW theVIEW;\n"
+            }, {
+                "dRop viEW iF EXistS theVIEW",
+                "DROP VIEW IF EXISTS theVIEW;\n"
+            }
         };
 
         TSetup setup;

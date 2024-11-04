83714400502f90945415f74a43f6e6887b7ff9ee
kungasc <kungasc@ydb.tech>
2024-11-04 11:54:53 +0000

    fix

--- ydb/core/tx/datashard/datashard__compaction.cpp
+++ ydb/core/tx/datashard/datashard__compaction.cpp
@@ -109,8 +109,7 @@ public:
             AppData(ctx)->FeatureFlags.GetEnableLocalDBBtreeIndex());
         bool isEmpty = stats.PartCount == 0 && stats.MemDataSize == 0;
         bool isSingleParted = stats.PartCount == 1 && stats.MemDataSize == 0;
-        bool hasSchemaChanges = it->second->Stats.HasSchemaChanges;
-        if (isEmpty || isSingleParted && !hasBorrowed && !record.GetCompactSinglePartedShards() && !hasSchemaChanges) {
+        if (isEmpty || isSingleParted && !hasBorrowed && !stats.HasSchemaChanges && !record.GetCompactSinglePartedShards()) {
             // nothing to compact
             LOG_DEBUG_S(ctx, NKikimrServices::TX_DATASHARD,
                 "Background compaction of tablet# " << Self->TabletID()

bed766bef5787bd5bdbb0b73caab878b3c85f3eb
coteeq <coteeq@yandex-team.com>
2024-11-05 12:14:24 +0300

    [phoenix2] Do not serialize fields, whose InVersion filter does not match latest version commit_hash:9f8f12afe3d5ab1d3d6d4cfb597ddf02b1a98d57

--- yt/yt/core/phoenix/unittests/phoenix_ut.cpp
+++ yt/yt/core/phoenix/unittests/phoenix_ut.cpp
@@ -447,6 +447,23 @@ TEST(TPhoenixTest, InVersion5)
     EXPECT_EQ(s1, s3);
 }
 
+TEST(TPhoenixTest, InVersionSave)
+{
+    using namespace NInVersions;
+
+    S s;
+    s.A = 123;
+    s.B = 0;
+    s.C = 777;
+
+    auto expected = MakeBuffer([] (auto& context) {
+        Save<int>(context, 123);
+    });
+
+    auto serialized = Serialize(s);
+    EXPECT_EQ(expected, serialized);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 
 namespace NAfterLoad {

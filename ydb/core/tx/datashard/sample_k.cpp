3bf1d315c8007491d35c4e8e9d8de7d8917e7d07
Valerii Mironov <mbkkt@ydb.tech>
2024-11-05 08:50:15 +0000

    We shouldn't use random cookie for build index scans. Because in such case it's possible that this random id will intersects with real transaction id. So we use invalid zero id. It's allowed because build index scans don't need some external finalize. New build index scan remove old scan from manager because hait has newer version

--- ydb/core/tx/datashard/sample_k.cpp
+++ ydb/core/tx/datashard/sample_k.cpp
@@ -351,7 +351,7 @@ void TDataShard::HandleSafe(TEvDataShard::TEvSampleKRequest::TPtr& ev, const TAc
                                       record.GetMaxProbability(),
                                       record.GetColumns(),
                                       userTable),
-                                  ev->Cookie,
+                                  0,
                                   scanOpts);
 
     TScanRecord recCard = {scanId, seqNo};

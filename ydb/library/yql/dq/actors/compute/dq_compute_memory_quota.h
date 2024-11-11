a0c01b706d2c21b0b289336567c6f783102704ad
Yuriy Kaminskiy <yumkam7@ydb.tech>
2024-11-11 13:49:30 +0300

    remove duplicated log messages

--- ydb/library/yql/dq/actors/compute/dq_compute_memory_quota.h
+++ ydb/library/yql/dq/actors/compute/dq_compute_memory_quota.h
@@ -90,8 +90,12 @@ namespace NYql::NDq {
             }
 
             if (Y_UNLIKELY(ProfileStats)) {
-                ProfileStats->MkqlMaxUsedMemory = std::max(ProfileStats->MkqlMaxUsedMemory, alloc->GetPeakAllocated());
-                CAMQ_LOG_T("Peak memory usage: " << ProfileStats->MkqlMaxUsedMemory);
+                auto& prev = ProfileStats->MkqlMaxUsedMemory;
+                auto cur = alloc->GetPeakAllocated();
+                if (cur > prev) {
+                    prev = cur;
+                    CAMQ_LOG_T("Peak memory usage: " << cur);
+                }
             }
         }
 

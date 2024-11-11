1dfc72206e49e76b1142ffddbe193c8e0b2f1a9d
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2024-11-11 17:19:33 +0300

    more dirty fix

--- ydb/public/lib/ydb_cli/commands/interactive/yql_position.cpp
+++ ydb/public/lib/ydb_cli/commands/interactive/yql_position.cpp
@@ -13,22 +13,8 @@ namespace NYdb {
         }
 
         YQLPositionMapping YQLPositionMapping::Build(const TString& queryUtf8) {
+            (void)queryUtf8;
             TVector<ui32> symbolsCountBeforeLine = {0, 0};
-
-            TStringStream stream(queryUtf8);
-            TLineSplitter lines(stream);
-
-            size_t read;
-            for (TString line; (read = lines.Next(line)) != 0;) {
-                const auto index = symbolsCountBeforeLine.size();
-                const auto previous = symbolsCountBeforeLine.at(index - 1);
-
-                const auto newlineWidth = read - line.size();
-                Y_ASSERT(0 <= newlineWidth && newlineWidth <= 2);
-
-                const auto current = GetNumberOfUTF8Chars(line) + newlineWidth;
-                symbolsCountBeforeLine.emplace_back(previous + current);
-            }
 
             return YQLPositionMapping(std::move(symbolsCountBeforeLine));
         }

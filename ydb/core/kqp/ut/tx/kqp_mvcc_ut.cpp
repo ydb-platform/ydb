2a5a8b2861e649404150d0676cba50392da44f8d
Nikita Vasilev <ns-vasilev@ydb.tech>
2024-11-13 18:06:35 +0300

    fix

--- ydb/core/kqp/ut/tx/kqp_mvcc_ut.cpp
+++ ydb/core/kqp/ut/tx/kqp_mvcc_ut.cpp
@@ -9,6 +9,51 @@ using namespace NYdb;
 using namespace NYdb::NTable;
 
 Y_UNIT_TEST_SUITE(KqpSnapshotRead) {
+
+    Y_UNIT_TEST_TWIN(TestReadOnly, withSink) {
+        NKikimrConfig::TAppConfig appConfig;
+        appConfig.MutableTableServiceConfig()->SetEnableOltpSink(withSink);
+        auto settings = TKikimrSettings()
+            .SetKeepSnapshotTimeout(TDuration::Seconds(1))
+            .SetAppConfig(appConfig);
+
+        TKikimrRunner kikimr(settings);
+
+        auto db = kikimr.GetTableClient();
+        auto session1 = db.CreateSession().GetValueSync().GetSession();
+        auto session2 = db.CreateSession().GetValueSync().GetSession();
+
+        auto result = session1.ExecuteDataQuery(Q_(R"(
+            SELECT * FROM `/Root/EightShard` WHERE Key = 101u OR Key = 801u ORDER BY Key;
+        )"), TTxControl::BeginTx(TTxSettings::SerializableRW())).ExtractValueSync();
+        UNIT_ASSERT_VALUES_EQUAL_C(result.GetStatus(), EStatus::SUCCESS, result.GetIssues().ToString());
+        CompareYson(R"([
+            [[1];[101u];["Value1"]];
+            [[2];[801u];["Value1"]]
+        ])", FormatResultSetYson(result.GetResultSet(0)));
+
+        auto tx = result.GetTransaction();
+
+        result = session2.ExecuteDataQuery(Q_(R"(
+            UPSERT INTO `/Root/EightShard` (Key, Text) VALUES (101u, "Changed"), (801u, "Changed");
+            UPSERT INTO `/Root/TwoShard` (Key, Value1, Value2) VALUES (1u, "Changed", 1), (4000000001u, "Changed", 2);
+        )"), TTxControl::BeginTx(TTxSettings::SerializableRW()).CommitTx()).ExtractValueSync();
+        UNIT_ASSERT_VALUES_EQUAL_C(result.GetStatus(), EStatus::SUCCESS, result.GetIssues().ToString());
+
+        result = session1.ExecuteDataQuery(Q_(R"(
+            SELECT * FROM `/Root/TwoShard` WHERE Key = 1u OR Key = 4000000001u ORDER BY Key;
+        )"), TTxControl::Tx(*tx)).ExtractValueSync();
+        UNIT_ASSERT_VALUES_EQUAL_C(result.GetStatus(), EStatus::SUCCESS, result.GetIssues().ToString());
+        CompareYson(R"([
+            [[1u];["One"];[-1]];
+            [[4000000001u];["BigOne"];[-1]]
+        ])", FormatResultSetYson(result.GetResultSet(0)));
+
+        auto commitResult = tx->Commit().GetValueSync();
+        UNIT_ASSERT_VALUES_EQUAL_C(commitResult.GetStatus(), EStatus::SUCCESS, result.GetIssues().ToString());
+    }
+
+
     Y_UNIT_TEST_TWIN(TestSnapshotExpiration, withSink) {
         NKikimrConfig::TAppConfig appConfig;
         appConfig.MutableTableServiceConfig()->SetEnableOltpSink(withSink);

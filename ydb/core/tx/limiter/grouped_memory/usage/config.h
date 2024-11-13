10f73105e0d64c2691b5b4bf215065dad8c7a3d6
ivanmorozov333 <ivanmorozov@ydb.tech>
2024-11-13 15:50:58 +0300

    native memory control (#11559)

--- ydb/core/tx/limiter/grouped_memory/usage/config.h
+++ ydb/core/tx/limiter/grouped_memory/usage/config.h
@@ -8,6 +8,7 @@ class TConfig {
 private:
     YDB_READONLY(bool, Enabled, true);
     YDB_READONLY(ui64, MemoryLimit, ui64(3) << 30);
+    YDB_READONLY(ui64, HardMemoryLimit, ui64(10) << 30);
 
 public:
 

d052c8b03eb1286ef588ba3a7be25e08f8cab39e
Олег <150132506+iddqdex@users.noreply.github.com>
2024-11-14 21:30:34 +0300

    8h

--- ydb/tests/olap/load/test_tpch.py
+++ ydb/tests/olap/load/test_tpch.py
@@ -83,4 +83,4 @@ class TestTpch10000(TpchSuiteBase):
     scale: int = 10000
     iterations: int = 1
     check_canonical: bool = False
-    timeout = max(TpchSuiteBase.timeout, 7200.)
+    timeout = max(TpchSuiteBase.timeout, 28900.)

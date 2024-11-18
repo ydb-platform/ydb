de56f6e355c1a280a9d12c11bfcbe95ae1943389
Alexander Avdonkin <aavdonkin@yandex.ru>
2024-11-18 17:16:37 +0300

    Send datasize stats by channel along with total (#11675)

--- ydb/core/tx/columnshard/counters/aggregation/table_stats.h
+++ ydb/core/tx/columnshard/counters/aggregation/table_stats.h
@@ -41,6 +41,14 @@ public:
         auto activeStats = ColumnEngine.GetTotalStats().Active();
         tableStats.SetRowCount(activeStats.Rows);
         tableStats.SetDataSize(activeStats.Bytes);
+        for (ui32 ch = 0; ch < activeStats.ByChannel.size(); ch++) {
+            ui64 dataSize = activeStats.ByChannel[ch];
+            if (dataSize > 0) {
+                auto item = tableStats.AddChannels();
+                item->SetChannel(ch);
+                item->SetDataSize(dataSize);
+            }
+        }
     }
 };
 

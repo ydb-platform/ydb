c78f0c739cbb47ff419d76bb3d606a77e6fea217
Semyon Danilov <senya@ydb.tech>
2024-11-18 11:49:46 +0100

    ...

--- ydb/core/blobstorage/crypto/crypto.h
+++ ydb/core/blobstorage/crypto/crypto.h
@@ -100,6 +100,12 @@ using TT1ha0Avx2Hasher = TT1ha0HasherBase<ET1haFunc::T1HA0_AVX2>;
 class TStreamCypher {
 public:
     static const bool HasAVX512;
+private:
+    alignas(16) ui8 Leftover[BLOCK_BYTES];
+    alignas(16) ui64 Key[4];
+    alignas(16) i64 Nonce;
+    std::unique_ptr<std::variant<ChaChaVec, ChaCha512>> Cypher;
+    ui32 UnusedBytes;
 public:
     TStreamCypher();
     void SetKey(const ui64 &key);
@@ -116,12 +122,6 @@ public:
 private:
     void Encipher(const ui8* plaintext, ui8* ciphertext, size_t len);
     void SetKeyAndIV(const ui64 blockIdx);
-
-    alignas(16) ui8 Leftover[BLOCK_BYTES];
-    alignas(16) ui64 Key[4];
-    alignas(16) i64 Nonce;
-    std::unique_ptr<std::variant<ChaChaVec, ChaCha512>> Cypher;
-    ui32 UnusedBytes;
 };
 
 } // NKikimr

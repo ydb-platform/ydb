208940571d993200ed56e34a8515f4e0fb9c5061
Semyon <yentsovsemyon@ydb.tech>
2024-11-19 01:02:47 +0300

    prohibit creating secrets with duplicating names (#11680)
    Conflicts:
    	ydb/services/metadata/manager/abstract.h

--- ydb/mvp/meta/meta_cp_databases.h
+++ ydb/mvp/meta/meta_cp_databases.h
@@ -14,7 +14,6 @@
 #include <ydb/public/api/grpc/ydb_scripting_v1.grpc.pb.h>
 #include <ydb/public/api/protos/ydb_discovery.pb.h>
 #include <ydb/public/sdk/cpp/client/ydb_result/result.h>
-#include <ydb/core/kqp/provider/yql_kikimr_results.h>
 #include <ydb/core/ydb_convert/ydb_convert.h>
 #include <ydb/mvp/core/core_ydb.h>
 #include <ydb/mvp/core/core_ydb_impl.h>

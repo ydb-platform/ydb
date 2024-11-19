42d4c2379c5f9a75c1e090fecfa949c172a756aa
Sergey Belyakov <serg-belyakov@ydb.tech>
2024-11-19 10:03:37 +0000

    Fix merge errors

--- ydb/core/blobstorage/nodewarden/node_warden_proxy.cpp
+++ ydb/core/blobstorage/nodewarden/node_warden_proxy.cpp
@@ -58,6 +58,7 @@ void TNodeWarden::StartLocalProxy(ui32 groupId) {
             // create proxy with configuration
             proxy.reset(CreateBlobStorageGroupProxyConfigured(TIntrusivePtr<TBlobStorageGroupInfo>(info), false, 
                 DsProxyNodeMon, getCounters(info), TBlobStorageProxyParameters{
+                        .UseActorSystemTimeInBSQueue = Cfg->UseActorSystemTimeInBSQueue,
                         .EnablePutBatching = EnablePutBatching,
                         .EnableVPatch = EnableVPatch,
                         .SlowDiskThreshold = SlowDiskThreshold,
@@ -70,6 +71,7 @@ void TNodeWarden::StartLocalProxy(ui32 groupId) {
     } else {
         // create proxy without configuration
         proxy.reset(CreateBlobStorageGroupProxyUnconfigured(groupId, DsProxyNodeMon, TBlobStorageProxyParameters{
+            .UseActorSystemTimeInBSQueue = Cfg->UseActorSystemTimeInBSQueue,
             .EnablePutBatching = EnablePutBatching,
             .EnableVPatch = EnableVPatch,
             .SlowDiskThreshold = SlowDiskThreshold,

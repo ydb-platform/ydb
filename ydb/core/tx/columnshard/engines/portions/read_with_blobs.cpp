c819c7402f98f78ba4c4686efe9126ea16bdc739
Artem Alekseev <fexolm@ydb.tech>
2024-11-20 20:05:26 +0300

    Add portion_id to log scope in TReadPortionInfoWithBlobs::RestoreBatch (#11771)

--- ydb/core/tx/columnshard/engines/portions/read_with_blobs.cpp
+++ ydb/core/tx/columnshard/engines/portions/read_with_blobs.cpp
@@ -18,6 +18,8 @@ void TReadPortionInfoWithBlobs::RestoreChunk(const std::shared_ptr<IPortionDataC
 TConclusion<std::shared_ptr<NArrow::TGeneralContainer>> TReadPortionInfoWithBlobs::RestoreBatch(
     const ISnapshotSchema& data, const ISnapshotSchema& resultSchema, const std::set<ui32>& seqColumns) const {
     THashMap<TChunkAddress, TString> blobs;
+    NActors::TLogContextGuard gLogging =
+        NActors::TLogContextBuilder::Build(NKikimrServices::TX_COLUMNSHARD)("portion_id", PortionInfo.GetPortionInfo().GetPortionId());
     for (auto&& i : PortionInfo.GetRecordsVerified()) {
         blobs[i.GetAddress()] = GetBlobByAddressVerified(i.ColumnId, i.Chunk);
         Y_ABORT_UNLESS(blobs[i.GetAddress()].size() == i.BlobRange.Size);

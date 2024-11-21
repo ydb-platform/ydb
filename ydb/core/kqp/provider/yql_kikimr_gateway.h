45513a5b190a63e4fd2e6482f39c8f6ba324a7e1
Vladislav Gogov <vlad-gogov@ydb.tech>
2024-11-21 13:38:02 +0300

    Column Family for ColumnTable (#9657)

--- ydb/core/kqp/provider/yql_kikimr_gateway.h
+++ ydb/core/kqp/provider/yql_kikimr_gateway.h
@@ -699,6 +699,7 @@ struct TCreateTableStoreSettings {
     TVector<TString> KeyColumnNames;
     TVector<TString> ColumnOrder;
     TVector<TIndexDescription> Indexes;
+    TVector<TColumnFamily> ColumnFamilies;
 };
 
 struct TAlterTableStoreSettings {

408e1dfabec98006a94b152d41cb43e97160523f
Pavel Velikhov <pavelvelikhov@ydb.tech>
2024-11-22 11:09:35 +0000

    Added UT files

---
+++ ydb/core/kqp/ut/join/data/queries/gpb.sql
@@ -0,0 +1,13 @@
+SELECT
+    doc.document_id AS document_id
+FROM (
+SELECT d.id AS document_id, d.exec_dt AS exec_dt
+FROM `/Root/bank_document` VIEW ix_bank_document_exec_dt_accounts AS d
+LEFT JOIN `/Root/bank_sub_document` VIEW IX_BANK_SUB_DOCUMENT_DOCUMENT_ID AS sd
+  ON d.id = sd.document_id
+WHERE sd.document_id IS NULL
+  AND d.exec_dt >= Cast('1990-12-10' as Date)
+  AND d.acc_dt_id = 15
+ORDER BY exec_dt, document_id
+LIMIT 1000
+) AS doc;

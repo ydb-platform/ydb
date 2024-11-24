c4a71ab2b48c0ec1946db14c61ae18da8b19aedc
Pavel Velikhov <pavelvelikhov@ydb.tech>
2024-11-24 14:04:05 +0000

    Added inference of sort order for merge connection

--- ydb/library/yql/dq/opt/dq_opt_stat_transformer_base.cpp
+++ ydb/library/yql/dq/opt/dq_opt_stat_transformer_base.cpp
@@ -74,6 +74,11 @@ bool TDqStatisticsTransformerBase::BeforeLambdas(const TExprNode::TPtr& input, T
     else if (TDqSource::Match(input.Get())) {
         InferStatisticsForDqSource(input, TypeCtx);
     }
+
+    // In case of DqCnMerge, update the sorted info with correct sorting
+    else if (TDqCnMerge::Match(input.Get())) {
+        InferStatisticsForDqMerge(input, TypeCtx);
+    }
     else {
         matched = false;
     }

96cd985434fa5254ab1801a4ea0d522482d710ac
vlad-gogov <vlad-gogov@ydb.tech>
2024-11-25 11:59:15 +0000

    fix cherry-pick

--- ydb/core/tx/schemeshard/olap/columns/update.cpp
+++ ydb/core/tx/schemeshard/olap/columns/update.cpp
@@ -87,25 +87,31 @@ bool TOlapColumnBase::ParseFromRequest(const NKikimrSchemeOp::TOlapColumnDescrip
         return false;
     }
 
-    TString errStr;
-    Y_ABORT_UNLESS(AppData()->TypeRegistry);
-    if (!GetTypeInfo(AppData()->TypeRegistry->GetType(TypeName), columnSchema.GetTypeInfo(), TypeName, Name, Type, errStr)) {
-        errors.AddError(errStr);
-        return false;
-    }
-
-    if (Type.GetTypeId() == NScheme::NTypeIds::Pg) {
-        if (!IsAllowedPgType(NPg::PgTypeIdFromTypeDesc(Type.GetPgTypeDesc()))) {
-            errors.AddError(TStringBuilder() << "Type '" << TypeName << "' specified for column '" << Name << "' is not supported");
+    if (const auto& typeName = NMiniKQL::AdaptLegacyYqlType(TypeName); typeName.StartsWith("pg")) {
+        const auto typeDesc = NPg::TypeDescFromPgTypeName(typeName);
+        if (!(typeDesc && TOlapColumnAdd::IsAllowedPgType(NPg::PgTypeIdFromTypeDesc(typeDesc)))) {
+            errors.AddError(TStringBuilder() << "Type '" << typeName << "' specified for column '" << Name << "' is not supported");
             return false;
         }
+        Type = NScheme::TTypeInfo(NScheme::NTypeIds::Pg, typeDesc);
     } else {
-        if (!IsAllowedType(Type.GetTypeId())) {
-            errors.AddError(TStringBuilder() << "Type '" << TypeName << "' specified for column '" << Name << "' is not supported");
+        Y_ABORT_UNLESS(AppData()->TypeRegistry);
+        const NScheme::IType* type = AppData()->TypeRegistry->GetType(typeName);
+        if (!type) {
+            errors.AddError(TStringBuilder() << "Type '" << typeName << "' specified for column '" << Name << "' is not supported");
+            return false;
+        }
+        if (!NScheme::NTypeIds::IsYqlType(type->GetTypeId())) {
+            errors.AddError(TStringBuilder() << "Type '" << typeName << "' specified for column '" << Name << "' is not supported");
+            return false;
+            ;
+        }
+        Type = NScheme::TTypeInfo(type->GetTypeId());
+        if (!IsAllowedType(type->GetTypeId())) {
+            errors.AddError(TStringBuilder() << "Type '" << typeName << "' specified for column '" << Name << "' is not supported");
             return false;
         }
     }
-
     auto arrowTypeResult = NArrow::GetArrowType(Type);
     const auto arrowTypeStatus = arrowTypeResult.status();
     if (!arrowTypeStatus.ok()) {

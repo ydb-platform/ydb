03b25e9eb995b2ea9f2ff833837bda6343771c11
Daniil Timizhev <ditimizhev@ydb.tech>
2024-11-26 11:53:18 +0300

    change WrongState condition

--- ydb/core/tx/datashard/datashard.cpp
+++ ydb/core/tx/datashard/datashard.cpp
@@ -3054,7 +3054,7 @@ bool TDataShard::CheckDataTxRejectAndReply(const NEvents::TDataEvents::TEvWrite:
                 status = NKikimrDataEvents::TEvWriteResult::STATUS_OVERLOADED;
                 break;
             case NKikimrTxDataShard::TEvProposeTransactionResult::ERROR:
-                if (rejectReasons == ERejectReasons::WrongState) {
+                if ((rejectReasons & ERejectReasons::WrongState) != ERejectReasons::None) {
                     status = NKikimrDataEvents::TEvWriteResult::STATUS_WRONG_SHARD_STATE;
                 } else {
                     status = NKikimrDataEvents::TEvWriteResult::STATUS_INTERNAL_ERROR;

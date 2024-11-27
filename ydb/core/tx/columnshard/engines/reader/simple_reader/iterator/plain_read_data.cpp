721ecf31f80ad8c2e3311d6a6af9ac0d5fc69aa3
ivanmorozov333 <ivanmorozov@ydb.tech>
2024-11-27 18:33:10 +0300

    Fix hanging control (#12051)

--- ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/plain_read_data.cpp
+++ ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/plain_read_data.cpp
@@ -1,11 +1,12 @@
 #include "plain_read_data.h"
 
+#include <ydb/core/tx/columnshard/engines/reader/common/result.h>
+
 namespace NKikimr::NOlap::NReader::NSimple {
 
 TPlainReadData::TPlainReadData(const std::shared_ptr<TReadContext>& context)
     : TBase(context)
-    , SpecialReadContext(std::make_shared<TSpecialReadContext>(context))
-{
+    , SpecialReadContext(std::make_shared<TSpecialReadContext>(context)) {
     ui32 sourceIdx = 0;
     std::deque<std::shared_ptr<IDataSource>> sources;
     const auto& portions = GetReadMetadata()->SelectInfo->PortionsOrderedPK;
@@ -27,21 +28,21 @@ TPlainReadData::TPlainReadData(const std::shared_ptr<TReadContext>& context)
     stats->SchemaColumns = (*SpecialReadContext->GetProgramInputColumns() - *SpecialReadContext->GetSpecColumns()).GetColumnsCount();
     stats->InsertedPortionsBytes = insertedPortionsBytes;
     stats->CompactedPortionsBytes = compactedPortionsBytes;
-
 }
 
 std::vector<std::shared_ptr<TPartialReadResult>> TPlainReadData::DoExtractReadyResults(const int64_t /*maxRowsInBatch*/) {
     auto result = std::move(PartialResults);
     PartialResults.clear();
-//    auto result = TPartialReadResult::SplitResults(std::move(PartialResults), maxRowsInBatch);
+    //    auto result = TPartialReadResult::SplitResults(std::move(PartialResults), maxRowsInBatch);
     ui32 count = 0;
-    for (auto&& r: result) {
+    for (auto&& r : result) {
         count += r->GetRecordsCount();
     }
     AFL_VERIFY(count == ReadyResultsCount);
     ReadyResultsCount = 0;
 
-    AFL_DEBUG(NKikimrServices::TX_COLUMNSHARD_SCAN)("event", "DoExtractReadyResults")("result", result.size())("count", count)("finished", Scanner->IsFinished());
+    AFL_DEBUG(NKikimrServices::TX_COLUMNSHARD_SCAN)("event", "DoExtractReadyResults")("result", result.size())("count", count)(
+        "finished", Scanner->IsFinished());
     return result;
 }
 
@@ -50,9 +51,9 @@ TConclusion<bool> TPlainReadData::DoReadNextInterval() {
 }
 
 void TPlainReadData::OnIntervalResult(const std::shared_ptr<TPartialReadResult>& result) {
-//    result->GetResourcesGuardOnly()->Update(result->GetMemorySize());
+    //    result->GetResourcesGuardOnly()->Update(result->GetMemorySize());
     ReadyResultsCount += result->GetRecordsCount();
     PartialResults.emplace_back(result);
 }
 
-}
+}   // namespace NKikimr::NOlap::NReader::NSimple

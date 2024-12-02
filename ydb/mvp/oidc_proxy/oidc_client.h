45733eb688b36f0e88e7b46121569aa28464d1a9
Andrei Rykov <arykov@ydb.tech>
2024-12-02 14:06:51 +0100

    oidc impersonation (#11449)

--- ydb/mvp/oidc_proxy/oidc_client.h
+++ ydb/mvp/oidc_proxy/oidc_client.h
@@ -5,12 +5,10 @@ class TActorSystem;
 struct TActorId;
 
 }  // NActors
-namespace NMVP {
-namespace NOIDC {
+namespace NMVP::NOIDC {
 
 struct TOpenIdConnectSettings;
 
 void InitOIDC(NActors::TActorSystem& actorSystem, const NActors::TActorId& httpProxyId, const TOpenIdConnectSettings& settings);
 
-}  // NOIDC
-}  // NMVP
+} // NMVP::NOIDC

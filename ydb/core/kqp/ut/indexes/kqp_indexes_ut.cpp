0b26ebaba8e59189fd7d7f3af7f7a9899bf41bab
Mikhail Surin <ssmike@ydb.tech>
2024-12-02 09:47:30 +0300

    fix test

--- ydb/core/kqp/ut/indexes/kqp_indexes_ut.cpp
+++ ydb/core/kqp/ut/indexes/kqp_indexes_ut.cpp
@@ -4437,6 +4437,11 @@ R"([[#;#;["Primary1"];[41u]];[["Secondary2"];[2u];["Primary2"];[42u]];[["Seconda
         setting.SetValue("1");
         auto serverSettings = TKikimrSettings()
             .SetKqpSettings({setting});
+
+        NKikimrConfig::TAppConfig appConfig;
+        appConfig.MutableTableServiceConfig()->SetEnableKqpDataQueryStreamIdxLookupJoin(false);
+        serverSettings.SetAppConfig(appConfig);
+
         TKikimrRunner kikimr(serverSettings);
         auto db = kikimr.GetTableClient();
         auto session = db.CreateSession().GetValueSync().GetSession();
@@ -4600,6 +4605,20 @@ R"([[#;#;["Primary1"];[41u]];[["Secondary2"];[2u];["Primary2"];[42u]];[["Seconda
                 SELECT t.id as id, t.yandexuid as yandexuid, t.uid as uid
                     FROM AS_TABLE($ids) AS k
                     INNER JOIN `/Root/user` VIEW PPRIMARY KEY AS t
+                    ON t.id = k.id
+                    WHERE uid IS NULL
+                ;)"));
+            const TString expected = R"([[[1u];["abc"];#]])";
+            UNIT_ASSERT_VALUES_EQUAL(execQuery(query1), expected);
+        }
+
+        {
+            const TString query1(Q_(R"(
+                $ids = (AsList(AsStruct(CAST(1 as Uint64) as id), AsStruct(CAST(2 as Uint64) as id)));
+
+                SELECT t.id as id, t.yandexuid as yandexuid, t.uid as uid
+                    FROM AS_TABLE($ids) AS k
+                    INNER JOIN `/Root/user` AS t
                     ON t.id = k.id
                     WHERE uid IS NULL
                 ;)"));

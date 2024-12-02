9ea17dd06cb290eb410fde1b328b2cc2097a8e6a
Vitalii Gridnev <gridnevvvit@gmail.com>
2024-12-02 12:45:28 +0300

    avoid changing shardstate reads list during iteration (#12150) (#12170)
    Co-authored-by: Mikhail Surin <ssmike@ydb.tech>

--- ydb/core/kqp/runtime/kqp_stream_lookup_actor.cpp
+++ ydb/core/kqp/runtime/kqp_stream_lookup_actor.cpp
@@ -413,11 +413,15 @@ private:
         auto shardIt = ReadsPerShard.find(tabletId);
         YQL_ENSURE(shardIt != ReadsPerShard.end());
 
+        TVector<TReadState*> toRetry;
         for (auto* read : shardIt->second.Reads) {
             if (read->State == EReadState::Running) {
                 Counters->IteratorDeliveryProblems->Inc();
-                RetryTableRead(*read);
+                toRetry.push_back(read);
             }
+        }
+        for (auto* read : toRetry) {
+            RetryTableRead(*read);
         }
     }
 

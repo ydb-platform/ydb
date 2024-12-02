b5ee3c65e3196a0dcf40277c210ca7d1aae6494e
Oleg Geller <iddqd@ydb.tech>
2024-12-02 09:59:03 +0300

    rm space

--- ydb/tests/functional/tpc/test_tpch.py
+++ ydb/tests/functional/tpc/test_tpch.py
@@ -4,7 +4,7 @@ from conftest import run_cli
 
 class TestTpchS1(tpch.TestTpch1):
     iterations: int = 1
- 
+
     @classmethod
     def setup_class(cls) -> None:
         run_cli(['workload', 'tpch', '-p', 'olap_yatests/tpch/s1', 'init', '--store=column'])

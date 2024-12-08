b0eabb0d4f1e8e6033cc3293c6bd6991ddd482f4
vityaman <vityaman.dev@yandex.ru>
2024-12-08 13:19:58 +0300

    #9056 Add tests
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- ydb/public/lib/ydb_cli/commands/interactive/yql_complete_ut.cpp
+++ ydb/public/lib/ydb_cli/commands/interactive/yql_complete_ut.cpp
@@ -13,6 +13,8 @@ Y_UNIT_TEST_SUITE(YqlCompleteTests) {
         TYQLCompletionEngine engine;
         UNIT_ASSERT_VALUES_EQUAL(CompletionsCount(engine, ""), 33);
         UNIT_ASSERT_VALUES_EQUAL(CompletionsCount(engine, " "), 33);
+        UNIT_ASSERT_VALUES_EQUAL(CompletionsCount(engine, "  "), 33);
+        UNIT_ASSERT_VALUES_EQUAL(CompletionsCount(engine, "   "), 33);
     }
 
     Y_UNIT_TEST(Select) {
@@ -31,5 +33,28 @@ Y_UNIT_TEST_SUITE(YqlCompleteTests) {
         UNIT_ASSERT_VALUES_EQUAL(CompletionsCount(engine, "select test from as "), 28);
         UNIT_ASSERT_VALUES_EQUAL(CompletionsCount(engine, "select test from as as "), 0);
         UNIT_ASSERT_VALUES_EQUAL(CompletionsCount(engine, "select test from as as as "), 24);
+    }
+
+    Y_UNIT_TEST(UTF8Wide) {
+        TYQLCompletionEngine engine;
+        UNIT_ASSERT_VALUES_EQUAL(CompletionsCount(engine, "\xF0\x9F\x98\x8A"), 0);
+        UNIT_ASSERT_VALUES_EQUAL(CompletionsCount(engine, "编码"), 0);
+    }
+
+    Y_UNIT_TEST(Typing) {
+        const TString query =
+            "SELECT \n"
+            "  123467, \"Hello, {name}! 编码\", \n"
+            "  (1 + (5 * 1 / 0)), MIN(identifier), \n"
+            "  Bool(field), Math::Sin(var) \n"
+            "FROM `local/test/space/table` JOIN test;";
+
+        TYQLCompletionEngine engine;
+
+        for (std::size_t size = 0; size <= query.size(); ++size) {
+            const TStringBuf prefix(query, 0, size);
+            auto completion = engine.Complete(prefix);
+            Y_DO_NOT_OPTIMIZE_AWAY(completion);
+        }
     }
 } // Y_UNIT_TEST_SUITE(YqlCompleteTests)

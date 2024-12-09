f9e79db6a8e464ea6f5675a277f0643d2a0f9098
kruall <kruall@ydb.tech>
2024-12-09 22:53:49 +0300

    Fix miscalculated budget in THarmonizer (#12422)

--- ydb/library/actors/core/harmonizer/harmonizer.h
+++ ydb/library/actors/core/harmonizer/harmonizer.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #include "defs.h"
-#include "executor_pool_shared.h"
 
 namespace NActors {
     class IExecutorPool;
-    class TSharedExecutorPool;
+    class ISharedExecutorPool;
     struct TSelfPingInfo;
 
     template <typename T>
@@ -17,25 +16,38 @@ namespace NActors {
         ui64 DecreasingThreadsByStarvedState = 0;
         ui64 DecreasingThreadsByHoggishState = 0;
         ui64 DecreasingThreadsByExchange = 0;
-        float MaxConsumedCpu = 0.0;
-        float MinConsumedCpu = 0.0;
-        float AvgConsumedCpu = 0.0;
-        float MaxBookedCpu = 0.0;
-        float MinBookedCpu = 0.0;
+
+        ui64 ReceivedHalfThreadByNeedyState = 0;
+        ui64 GivenHalfThreadByOtherStarvedState = 0;
+        ui64 GivenHalfThreadByHoggishState = 0;
+        ui64 GivenHalfThreadByOtherNeedyState = 0;
+        ui64 ReturnedHalfThreadByStarvedState = 0;
+        ui64 ReturnedHalfThreadByOtherHoggishState = 0;
+
+        float MaxUsedCpu = 0.0;
+        float MinUsedCpu = 0.0;
+        float AvgUsedCpu = 0.0;
+        float MaxElapsedCpu = 0.0;
+        float MinElapsedCpu = 0.0;
+        float AvgElapsedCpu = 0.0;
         i16 PotentialMaxThreadCount = 0;
         bool IsNeedy = false;
         bool IsStarved = false;
         bool IsHoggish = false;
+
+        TString ToString() const;
     };
 
     struct THarmonizerStats {
-        i64 MaxConsumedCpu = 0.0;
-        i64 MinConsumedCpu = 0.0;
-        i64 MaxBookedCpu = 0.0;
-        i64 MinBookedCpu = 0.0;
+        float MaxUsedCpu = 0.0;
+        float MinUsedCpu = 0.0;
+        float MaxElapsedCpu = 0.0;
+        float MinElapsedCpu = 0.0;
 
-        double AvgAwakeningTimeUs = 0;
-        double AvgWakingUpTimeUs = 0;
+        float AvgAwakeningTimeUs = 0;
+        float AvgWakingUpTimeUs = 0;
+
+        TString ToString() const;
     };
 
     // Pool cpu harmonizer
@@ -48,7 +60,7 @@ namespace NActors {
         virtual void Enable(bool enable) = 0;
         virtual TPoolHarmonizerStats GetPoolStats(i16 poolId) const = 0;
         virtual THarmonizerStats GetStats() const = 0;
-        virtual void SetSharedPool(TSharedExecutorPool* pool) = 0;
+        virtual void SetSharedPool(ISharedExecutorPool* pool) = 0;
     };
 
     IHarmonizer* MakeHarmonizer(ui64 ts);

aa1ea8616b83647f842dbdd67e7f7ba37b5d65d6
kungasc <kungasc@ydb.tech>
2024-12-12 15:22:13 +0000

    fix ticket parser condition

--- ydb/core/tx/schemeshard/ut_login/ut_login.cpp
+++ ydb/core/tx/schemeshard/ut_login/ut_login.cpp
@@ -40,6 +40,17 @@ Y_UNIT_TEST_SUITE(TSchemeShardLoginTest) {
         TTestBasicRuntime runtime;
         TTestEnv env(runtime);
         ui64 txId = 100;
+
+        {
+            auto describe = DescribePath(runtime, TTestTxConfig::SchemeShard, "/MyRoot");
+            Cerr << describe.DebugString() << Endl;
+            UNIT_ASSERT(describe.HasPathDescription());
+            UNIT_ASSERT(describe.GetPathDescription().HasDomainDescription());
+            UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().HasSecurityState());
+            UNIT_ASSERT_VALUES_EQUAL(describe.GetPathDescription().GetDomainDescription().GetSecurityState().PublicKeysSize(), 0);
+            UNIT_ASSERT_VALUES_EQUAL(describe.GetPathDescription().GetDomainDescription().GetSecurityState().SidsSize(), 0);
+        }
+
         CreateAlterLoginCreateUser(runtime, ++txId, "/MyRoot", "user1", "password1");
         
         {
@@ -47,7 +58,7 @@ Y_UNIT_TEST_SUITE(TSchemeShardLoginTest) {
             UNIT_ASSERT(describe.HasPathDescription());
             UNIT_ASSERT(describe.GetPathDescription().HasDomainDescription());
             UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().HasSecurityState());
-            UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().GetSecurityState().PublicKeysSize() == 0);
+            UNIT_ASSERT_VALUES_EQUAL(describe.GetPathDescription().GetDomainDescription().GetSecurityState().PublicKeysSize(), 0);
             UNIT_ASSERT_VALUES_EQUAL(describe.GetPathDescription().GetDomainDescription().GetSecurityState().SidsSize(), 1);
         }
 
@@ -60,7 +71,7 @@ Y_UNIT_TEST_SUITE(TSchemeShardLoginTest) {
             UNIT_ASSERT(describe.HasPathDescription());
             UNIT_ASSERT(describe.GetPathDescription().HasDomainDescription());
             UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().HasSecurityState());
-            UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().GetSecurityState().PublicKeysSize() > 0);
+            UNIT_ASSERT_VALUES_EQUAL(describe.GetPathDescription().GetDomainDescription().GetSecurityState().PublicKeysSize(), 1);
             UNIT_ASSERT_VALUES_EQUAL(describe.GetPathDescription().GetDomainDescription().GetSecurityState().SidsSize(), 1);
         }
 

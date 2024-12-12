8a270ecd2413dab6cb98ea3226c8bd02c29ceecc
kungasc <kungasc@ydb.tech>
2024-12-12 14:14:06 +0000

    Return users even they haven't login

--- ydb/core/tx/schemeshard/ut_login/ut_login.cpp
+++ ydb/core/tx/schemeshard/ut_login/ut_login.cpp
@@ -41,19 +41,37 @@ Y_UNIT_TEST_SUITE(TSchemeShardLoginTest) {
         TTestEnv env(runtime);
         ui64 txId = 100;
         CreateAlterLoginCreateUser(runtime, ++txId, "/MyRoot", "user1", "password1");
+        
+        {
+            auto describe = DescribePath(runtime, TTestTxConfig::SchemeShard, "/MyRoot");
+            UNIT_ASSERT(describe.HasPathDescription());
+            UNIT_ASSERT(describe.GetPathDescription().HasDomainDescription());
+            UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().HasSecurityState());
+            UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().GetSecurityState().PublicKeysSize() == 0);
+            UNIT_ASSERT_VALUES_EQUAL(describe.GetPathDescription().GetDomainDescription().GetSecurityState().SidsSize(), 1);
+        }
+
+        // public keys are filled after the first login
         auto resultLogin = Login(runtime, "user1", "password1");
         UNIT_ASSERT_VALUES_EQUAL(resultLogin.error(), "");
-        auto describe = DescribePath(runtime, TTestTxConfig::SchemeShard, "/MyRoot");
-        UNIT_ASSERT(describe.HasPathDescription());
-        UNIT_ASSERT(describe.GetPathDescription().HasDomainDescription());
-        UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().HasSecurityState());
-        UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().GetSecurityState().PublicKeysSize() > 0);
+
+        {
+            auto describe = DescribePath(runtime, TTestTxConfig::SchemeShard, "/MyRoot");
+            UNIT_ASSERT(describe.HasPathDescription());
+            UNIT_ASSERT(describe.GetPathDescription().HasDomainDescription());
+            UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().HasSecurityState());
+            UNIT_ASSERT(describe.GetPathDescription().GetDomainDescription().GetSecurityState().PublicKeysSize() > 0);
+            UNIT_ASSERT_VALUES_EQUAL(describe.GetPathDescription().GetDomainDescription().GetSecurityState().SidsSize(), 1);
+        }
 
         // check token
-        NLogin::TLoginProvider login;
-        login.UpdateSecurityState(describe.GetPathDescription().GetDomainDescription().GetSecurityState());
-        auto resultValidate = login.ValidateToken({.Token = resultLogin.token()});
-        UNIT_ASSERT_VALUES_EQUAL(resultValidate.User, "user1");
+        {
+            auto describe = DescribePath(runtime, TTestTxConfig::SchemeShard, "/MyRoot");
+            NLogin::TLoginProvider login;
+            login.UpdateSecurityState(describe.GetPathDescription().GetDomainDescription().GetSecurityState());
+            auto resultValidate = login.ValidateToken({.Token = resultLogin.token()});
+            UNIT_ASSERT_VALUES_EQUAL(resultValidate.User, "user1");
+        }
     }
 
     Y_UNIT_TEST(DisableBuiltinAuthMechanism) {

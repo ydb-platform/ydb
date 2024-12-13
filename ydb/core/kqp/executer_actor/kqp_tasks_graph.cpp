12c47855d193eaf54f6e8f891d66ad92527ae42e
Iuliia Sidorina <yulia@ydb.tech>
2024-12-13 14:51:58 +0100

    fix(kqp): disable stream join for joins with non-pk columns in predicate (#12573)

--- ydb/core/kqp/executer_actor/kqp_tasks_graph.cpp
+++ ydb/core/kqp/executer_actor/kqp_tasks_graph.cpp
@@ -417,7 +417,7 @@ void BuildStreamLookupChannels(TKqpTasksGraph& graph, const TStageInfo& stageInf
 
     settings->SetLookupStrategy(streamLookup.GetLookupStrategy());
     settings->SetKeepRowsOrder(streamLookup.GetKeepRowsOrder());
-    settings->SetAllowNullKeys(streamLookup.GetAllowNullKeys());
+    settings->SetAllowNullKeysPrefixSize(streamLookup.GetAllowNullKeysPrefixSize());
 
     TTransform streamLookupTransform;
     streamLookupTransform.Type = "StreamLookupInputTransformer";

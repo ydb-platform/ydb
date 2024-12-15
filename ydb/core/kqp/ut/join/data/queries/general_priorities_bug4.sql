b4b80861591e64a39a8fbc0576289c18c3c352cf
Pavel Ivanov <pudge1000-7@ydb.tech>
2024-12-15 10:09:47 +0000

    tests

--- ydb/core/kqp/ut/join/data/queries/general_priorities_bug4.sql
+++ ydb/core/kqp/ut/join/data/queries/general_priorities_bug4.sql
@@ -15,6 +15,6 @@ WHERE sd.document_id IS NULL
   AND d.exec_dt >= Cast('1990-12-10' as Date)
   AND d.acc_dt_id = 15
 --ORDER BY exec_dt, id
-ORDER BY d.exec_dt, id
+ORDER BY exec_dt, id
 LIMIT 1000
 ) AS doc;

23df86034b02b672025dc7c9f065927bb84deeaa
Artem Alekseev <fexolm@ydb.tech>
2024-12-18 11:17:32 +0300

    Add partitioning information to describe

--- ydb/core/ydb_convert/ydb_convert.cpp
+++ ydb/core/ydb_convert/ydb_convert.cpp
@@ -1032,13 +1032,19 @@ void ConvertDirectoryEntry(const NKikimrSchemeOp::TDirEntry& from, Ydb::Scheme::
 void ConvertDirectoryEntry(const NKikimrSchemeOp::TPathDescription& from, Ydb::Scheme::Entry* to, bool processAcl) {
     ConvertDirectoryEntry(from.GetSelf(), to, processAcl);
 
+    if(from.has_columntabledescription()) {
+        auto &desc = from.GetColumnTableDescription();
+        const auto& sharding = desc.GetSharding();
+        auto &tabletInfo = sharding.GetHashSharding().GetTabletsForConsistency();
 
-    // if(from.has_columntabledescription()) {
-    //     auto &desc = from.GetColumnTableDescription();
-    //     const auto& sharding = desc.GetSharding();
-    //     sharding.
+        for(auto &tablet: tabletInfo) {
+            Ydb::Scheme::ConsistencyShardingTablet toTablet;
+            toTablet.set_tablet_id(tablet.GetHashIntervalLeftClosed());
+            toTablet.set_left_closed(tablet.GetHashIntervalRightOpened());
 
-    // }
+            *to->add_sharding_info() = toTablet;
+        }
+    }
 
     switch (from.GetSelf().GetPathType()) {
     case NKikimrSchemeOp::EPathTypeTable:

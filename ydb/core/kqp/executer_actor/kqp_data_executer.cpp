ef48b009710f36e78fda5d139e4a7e6015b4544b
Aleksei Borzenkov <snaury@ydb.tech>
2024-12-23 14:06:07 +0300

    24-3: Abort volatile transactions during graceful restarts (#12808)

--- ydb/core/kqp/executer_actor/kqp_data_executer.cpp
+++ ydb/core/kqp/executer_actor/kqp_data_executer.cpp
@@ -539,7 +539,7 @@ private:
         LOG_E("Shard " << tabletId << " transaction lost during reconnect: " << record.GetStatus());
 
         CancelProposal(tabletId);
-        ReplyTxStateUnknown(tabletId);
+        ReplyUnavailable(TStringBuilder() << "Disconnected from shard " << tabletId);
     }
 
     void HandlePrepare(TEvDqCompute::TEvState::TPtr& ev) {
@@ -581,7 +581,7 @@ private:
                     return ReplyUnavailable(TStringBuilder() << "Could not prepare program on shard " << msg->TabletId);
                 }
 
-                return ReplyTxStateUnknown(msg->TabletId);
+                return ReplyUnavailable(TStringBuilder() << "Disconnected from shard " << msg->TabletId);
             }
 
             case TShardState::EState::Prepared: {
@@ -601,7 +601,7 @@ private:
                                << (msg->NotDelivered ? ", last message not delivered" : ""));
 
                 CancelProposal(0);
-                return ReplyTxStateUnknown(msg->TabletId);
+                return ReplyUnavailable(TStringBuilder() << "Disconnected from shard " << msg->TabletId);
             }
 
             case TShardState::EState::Initial:

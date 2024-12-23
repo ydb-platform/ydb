96b738fea6c440f3a0cfb3087c3bcc4a50af6f53
Sergey Skovorodkin <qyryq@ydb.tech>
2024-12-23 06:29:49 +0000

    Add Python tests for TagQueue, UntagQueue

---
+++ ydb/tests/functional/sqs/common/test_queue_tags.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
+from hamcrest import assert_that, equal_to
+
+from ydb.tests.library.sqs.test_base import KikimrSqsTestBase
+
+
+class TestQueueTags(KikimrSqsTestBase):
+    @classmethod
+    def _setup_config_generator(cls):
+        config_generator = super(TestQueueTags, cls)._setup_config_generator()
+        return config_generator
+
+    def test_tag_queue(self):
+        self._init_with_params()
+
+        # TODO Check on invalid queue_url
+
+        queue_url = self._create_queue_and_assert(self.queue_name, use_http=True)
+
+        def add_tags(tags):
+            return self._sqs_api.tag_queue(queue_url, Tags=tags)
+
+        def get_tags():
+            return self._sqs_api.list_queue_tags(queue_url)['Tags']
+
+        tags = get_tags()
+        assert len(tags) == 0
+
+        add_tags({})
+        tags = get_tags()
+        assert len(tags) == 0
+
+        add_tags({'key1': 'value0'})
+        assert get_tags() == {'key1': 'value0'}
+
+        # Change an existing tag value:
+        add_tags({'key1': 'value1'})
+        assert get_tags() == {'key1': 'value1'}
+
+        # Adding a new tag without mentioning an existing tag should not delete the latter one:
+        add_tags({'key2': 'value2'})
+        assert get_tags() == {'key1': 'value1', 'key2': 'value2'}
+
+        # Add multiple tags:
+        add_tags({'key3': 'value3', 'key4': 'value0'})
+        assert get_tags() == {'key1': 'value1', 'key2': 'value2', 'key3': 'value3', 'key4': 'value0'}
+
+        # Add a new tag and change an existing tag:
+        add_tags({'key4': 'value4', 'key5': 'value5'})
+        assert get_tags() == {'key1': 'value1', 'key2': 'value2', 'key3': 'value3', 'key4': 'value4', 'key5': 'value5'}
+
+    def test_untag_queue(self):
+        self._init_with_params()
+
+        # TODO Check on invalid queue_url
+
+        queue_url = self._create_queue_and_assert(self.queue_name, use_http=True)
+
+        def add_tags(tags):
+            return self._sqs_api.tag_queue(queue_url, Tags=tags)
+
+        def remove_tags(keys):
+            return self._sqs_api.untag_queue(queue_url, TagKeys=keys)
+
+        def get_tags():
+            return self._sqs_api.list_queue_tags(queue_url)['Tags']
+
+        # Removing unknown tags is OK:
+        remove_tags(['key0'])
+
+        add_tags({'key1': 'value1', 'key2': 'value2', 'key3': 'value3'})
+
+        remove_tags(['key3'])
+        assert get_tags() == {'key1': 'value1', 'key2': 'value2'}
+
+        remove_tags(['key1', 'key2'])
+        assert get_tags() == {}

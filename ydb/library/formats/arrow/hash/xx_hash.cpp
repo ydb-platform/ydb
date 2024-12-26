61204ee3e29f56e751516e8ab61b3cf3d4ab0ef0
ivanmorozov333 <ivanmorozov@ydb.tech>
2024-12-26 15:10:32 +0300

    bloom ngramms speed up (#12982)

--- ydb/library/formats/arrow/hash/xx_hash.cpp
+++ ydb/library/formats/arrow/hash/xx_hash.cpp
@@ -2,6 +2,18 @@
 
 namespace NKikimr::NArrow::NHash::NXX64 {
 
+void TStreamStringHashCalcer_H3::Start() {
+    XXH3_64bits_reset_withSeed(&HashState, Seed);
+}
+
+void TStreamStringHashCalcer_H3::Update(const ui8* data, const ui32 size) {
+    XXH3_64bits_update(&HashState, data, size);
+}
+
+ui64 TStreamStringHashCalcer_H3::Finish() {
+    return XXH3_64bits_digest(&HashState);
+}
+
 void TStreamStringHashCalcer::Start() {
     XXH64_reset(&HashState, Seed);
 }

7ff15cd713f3d3b462d55223c3fb35150d268b92
Sergey J <wint@yandex-team.ru>
2024-12-27 13:07:37 +0300

    fix flake8

--- ydb/tools/cfg/dynamic.py
+++ ydb/tools/cfg/dynamic.py
@@ -313,10 +313,6 @@ class DynamicConfigGenerator(object):
             pool.unit_kind = storage_unit.kind
             pool.count = storage_unit.count
 
-        overridden_configs = tenant.overridden_configs or {}
-        nbs = overridden_configs.get('nbs', {})
-        nfs = overridden_configs.get('nfs', {})
-
         if tenant.plan_resolution is not None:
             console_request.CreateTenantRequest.Request.options.plan_resolution = tenant.plan_resolution
 

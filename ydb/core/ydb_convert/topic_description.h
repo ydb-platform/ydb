2bbbb2c706bbb39edc57d83feb0c1653d211e3ae
st-shchetinin <st-shchetinin@ydb.tech>
2024-12-27 10:25:21 +0000

    FillTopicDescription

---
+++ ydb/core/ydb_convert/topic_description.h
@@ -0,0 +1,12 @@
+#include <ydb/core/protos/flat_scheme_op.pb.h>
+#include <ydb/core/protos/pqconfig.pb.h>
+#include <ydb/public/api/protos/ydb_table.pb.h>
+
+namespace NKikimr {
+
+void FillConsumerProto(Ydb::Topic::Consumer& out,
+    const NKikimrPQ::TPQTabletConfig::TConsumer& in);
+void FillTopicDescription(Ydb::Topic::DescribeTopicResult& out,
+   const NKikimrSchemeOp::TPersQueueGroupDescription& in);
+
+} // namespace NKikimr
--- ydb/services/persqueue_v1/actors/schema_actors.cpp
+++ ydb/services/persqueue_v1/actors/schema_actors.cpp
@@ -4,6 +4,7 @@
 
 #include <ydb/core/client/server/ic_nodes_cache_service.h>
 #include <ydb/core/persqueue/utils.h>
+#include <ydb/core/ydb_convert/topic_description.h>
 #include <ydb/core/ydb_convert/ydb_convert.h>
 #include <ydb/library/persqueue/obfuscate/obfuscate.h>
 
@@ -1022,34 +1023,21 @@ bool TDescribeConsumerActor::ApplyResponse(
 }
 
 
-bool FillConsumerProto(Ydb::Topic::Consumer *rr, const NKikimrPQ::TPQTabletConfig::TConsumer& consumer,
+bool FillConsumerProto(Ydb::Topic::Consumer* rr, const NKikimrPQ::TPQTabletConfig::TConsumer& consumer,
                         const NActors::TActorContext& ctx, Ydb::StatusIds::StatusCode& status, TString& error)
 {
     const auto& pqConfig = AppData(ctx)->PQConfig;
 
-    auto consumerName = NPersQueue::ConvertOldConsumerName(consumer.GetName(), ctx);
-    rr->set_name(consumerName);
-    rr->mutable_read_from()->set_seconds(consumer.GetReadFromTimestampsMs() / 1000);
-    auto version = consumer.GetVersion();
-    if (version != 0)
-        (*rr->mutable_attributes())["_version"] = TStringBuilder() << version;
-    for (const auto &codec : consumer.GetCodec().GetIds()) {
-        rr->mutable_supported_codecs()->add_codecs((Ydb::Topic::Codec) (codec + 1));
-    }
+    NKikimr::FillConsumerProto(*rr, consumer);
 
-    rr->set_important(consumer.GetImportant());
-    TString serviceType = "";
-    if (consumer.HasServiceType()) {
-        serviceType = consumer.GetServiceType();
-    } else {
+    if (!consumer.HasServiceType()) {
         if (pqConfig.GetDisallowDefaultClientServiceType()) {
             error = "service type must be set for all read rules";
             status = Ydb::StatusIds::INTERNAL_ERROR;
             return false;
         }
-        serviceType = pqConfig.GetDefaultClientServiceType().GetName();
+        (*rr->mutable_attributes())["_service_type"] = pqConfig.GetDefaultClientServiceType().GetName();
     }
-    (*rr->mutable_attributes())["_service_type"] = serviceType;
     return true;
 }
 
@@ -1071,86 +1059,11 @@ void TDescribeTopicActor::HandleCacheNavigateResponse(TEvTxProxySchemeCache::TEv
 
     if (response.PQGroupInfo) {
         const auto& pqDescr = response.PQGroupInfo->Description;
-        for (auto& sourcePart: pqDescr.GetPartitions()) {
-            auto destPart = Result.add_partitions();
-            destPart->set_partition_id(sourcePart.GetPartitionId());
-            destPart->set_active(sourcePart.GetStatus() == ::NKikimrPQ::ETopicPartitionStatus::Active);
-            if (sourcePart.HasKeyRange()) {
-                if (sourcePart.GetKeyRange().HasFromBound()) {
-                    destPart->mutable_key_range()->set_from_bound(sourcePart.GetKeyRange().GetFromBound());
-                }
-                if (sourcePart.GetKeyRange().HasToBound()) {
-                    destPart->mutable_key_range()->set_to_bound(sourcePart.GetKeyRange().GetToBound());
-                }
-            }
-
-            for (size_t i = 0; i < sourcePart.ChildPartitionIdsSize(); ++i) {
-                destPart->add_child_partition_ids(static_cast<int64_t>(sourcePart.GetChildPartitionIds(i)));
-            }
-
-            for (size_t i = 0; i < sourcePart.ParentPartitionIdsSize(); ++i) {
-                destPart->add_parent_partition_ids(static_cast<int64_t>(sourcePart.GetParentPartitionIds(i)));
-            }
-        }
+        FillTopicDescription(Result, pqDescr);
 
         const auto &config = pqDescr.GetPQTabletConfig();
-        if (AppData(TActivationContext::ActorContextFor(SelfId()))->FeatureFlags.GetEnableTopicSplitMerge() && NPQ::SplitMergeEnabled(config)) {
-            Result.mutable_partitioning_settings()->set_min_active_partitions(config.GetPartitionStrategy().GetMinPartitionCount());
-        } else {
-            Result.mutable_partitioning_settings()->set_min_active_partitions(pqDescr.GetTotalGroupCount());
-        }
-
-        Result.mutable_partitioning_settings()->set_max_active_partitions(config.GetPartitionStrategy().GetMaxPartitionCount());
-        switch(config.GetPartitionStrategy().GetPartitionStrategyType()) {
-            case ::NKikimrPQ::TPQTabletConfig_TPartitionStrategyType::TPQTabletConfig_TPartitionStrategyType_CAN_SPLIT:
-                Result.mutable_partitioning_settings()->mutable_auto_partitioning_settings()->set_strategy(Ydb::Topic::AutoPartitioningStrategy::AUTO_PARTITIONING_STRATEGY_SCALE_UP);
-                break;
-            case ::NKikimrPQ::TPQTabletConfig_TPartitionStrategyType::TPQTabletConfig_TPartitionStrategyType_CAN_SPLIT_AND_MERGE:
-                Result.mutable_partitioning_settings()->mutable_auto_partitioning_settings()->set_strategy(Ydb::Topic::AutoPartitioningStrategy::AUTO_PARTITIONING_STRATEGY_SCALE_UP_AND_DOWN);
-                break;
-            case ::NKikimrPQ::TPQTabletConfig_TPartitionStrategyType::TPQTabletConfig_TPartitionStrategyType_PAUSED:
-                Result.mutable_partitioning_settings()->mutable_auto_partitioning_settings()->set_strategy(Ydb::Topic::AutoPartitioningStrategy::AUTO_PARTITIONING_STRATEGY_PAUSED);
-                break;
-            default:
-                Result.mutable_partitioning_settings()->mutable_auto_partitioning_settings()->set_strategy(Ydb::Topic::AutoPartitioningStrategy::AUTO_PARTITIONING_STRATEGY_DISABLED);
-                break;
-        }
-        Result.mutable_partitioning_settings()->mutable_auto_partitioning_settings()->mutable_partition_write_speed()->mutable_stabilization_window()->set_seconds(config.GetPartitionStrategy().GetScaleThresholdSeconds());
-        Result.mutable_partitioning_settings()->mutable_auto_partitioning_settings()->mutable_partition_write_speed()->set_down_utilization_percent(config.GetPartitionStrategy().GetScaleDownPartitionWriteSpeedThresholdPercent());
-        Result.mutable_partitioning_settings()->mutable_auto_partitioning_settings()->mutable_partition_write_speed()->set_up_utilization_percent(config.GetPartitionStrategy().GetScaleUpPartitionWriteSpeedThresholdPercent());
-
-        if (!config.GetRequireAuthWrite()) {
-            (*Result.mutable_attributes())["_allow_unauthenticated_write"] = "true";
-        }
-
-        if (!config.GetRequireAuthRead()) {
-            (*Result.mutable_attributes())["_allow_unauthenticated_read"] = "true";
-        }
-
-        if (pqDescr.GetPartitionPerTablet() != 2) {
-            (*Result.mutable_attributes())["_partitions_per_tablet"] =
-                TStringBuilder() << pqDescr.GetPartitionPerTablet();
-        }
-        if (config.HasAbcId()) {
-            (*Result.mutable_attributes())["_abc_id"] = TStringBuilder() << config.GetAbcId();
-        }
-        if (config.HasAbcSlug()) {
-            (*Result.mutable_attributes())["_abc_slug"] = config.GetAbcSlug();
-        }
-        if (config.HasFederationAccount()) {
-            (*Result.mutable_attributes())["_federation_account"] = config.GetFederationAccount();
-        }
         bool local = config.GetLocalDC();
         const auto &partConfig = config.GetPartitionConfig();
-        i64 msip = partConfig.GetMaxSizeInPartition();
-        if (partConfig.HasMaxSizeInPartition() && msip != Max<i64>()) {
-            (*Result.mutable_attributes())["_max_partition_storage_size"] = TStringBuilder() << msip;
-        }
-        Result.mutable_retention_period()->set_seconds(partConfig.GetLifetimeSeconds());
-        Result.set_retention_storage_mb(partConfig.GetStorageLimitBytes() / 1024 / 1024);
-        (*Result.mutable_attributes())["_message_group_seqno_retention_period_ms"] = TStringBuilder() << (partConfig.GetSourceIdLifetimeSeconds() * 1000);
-        (*Result.mutable_attributes())["__max_partition_message_groups_seqno_stored"] = TStringBuilder() << partConfig.GetSourceIdMaxCounts();
-
         const auto& pqConfig = AppData(ActorContext())->PQConfig;
 
         if (local || pqConfig.GetTopicsAreFirstClassCitizen()) {
@@ -1162,10 +1075,6 @@ void TDescribeTopicActor::HandleCacheNavigateResponse(TEvTxProxySchemeCache::TEv
             auto readSpeedPerConsumer = partConfig.GetWriteSpeedInBytesPerSecond() * 2;
             Result.set_partition_total_read_speed_bytes_per_second(readSpeedPerConsumer * pqConfig.GetQuotingConfig().GetMaxParallelConsumersPerPartition());
             Result.set_partition_consumer_read_speed_bytes_per_second(readSpeedPerConsumer);
-        }
-
-        for (const auto &codec : config.GetCodecs().GetIds()) {
-            Result.mutable_supported_codecs()->add_codecs((Ydb::Topic::Codec)(codec + 1));
         }
 
         if (pqConfig.GetBillingMeteringConfig().GetEnabled()) {
@@ -1182,14 +1091,16 @@ void TDescribeTopicActor::HandleCacheNavigateResponse(TEvTxProxySchemeCache::TEv
         }
         auto consumerName = NPersQueue::ConvertNewConsumerName(Settings.Consumer, ActorContext());
         bool found = false;
+        ui64 ind = 0;
         for (const auto& consumer : config.GetConsumers()) {
             if (consumerName == consumer.GetName()) {
                  found = true;
             }
-            auto rr = Result.add_consumers();
+            auto rr = Result.get_idx_consumers(ind);
+            ++ind;
             Ydb::StatusIds::StatusCode status;
             TString error;
-            if (!FillConsumerProto(rr, consumer, ActorContext(), status, error)) {
+            if (!FillConsumerProto(&rr, consumer, ActorContext(), status, error)) {
                 return RaiseError(error, Ydb::PersQueue::ErrorCode::ERROR, status, ActorContext());
             }
         }

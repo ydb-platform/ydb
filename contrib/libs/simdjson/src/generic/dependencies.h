3a4a69090c4b928ce99c61b022fcd43532e02b67
Evgeny Zverev <zverevgeny@ydb.tech>
2025-01-01 12:49:30 +0000

    copy simdjson from main

---
+++ contrib/libs/simdjson/src/generic/dependencies.h
@@ -0,0 +1,10 @@
+#ifdef SIMDJSON_CONDITIONAL_INCLUDE
+#error generic/dependencies.h must be included before defining SIMDJSON_CONDITIONAL_INCLUDE!
+#endif
+
+#ifndef SIMDJSON_SRC_GENERIC_DEPENDENCIES_H
+#define SIMDJSON_SRC_GENERIC_DEPENDENCIES_H
+
+#include <base.h>
+
+#endif // SIMDJSON_SRC_GENERIC_DEPENDENCIES_H

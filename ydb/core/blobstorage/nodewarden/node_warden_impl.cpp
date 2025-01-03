7bd41963812fe2d33cb5d2f709f2a8170a1338c5
Semyon Danilov <senya@ydb.tech>
2025-01-03 12:59:53 +0400

    Add option to restart PDisk without any checks (#13155)
    (cherry picked from commit 2490271)

--- ydb/core/blobstorage/nodewarden/node_warden_impl.cpp
+++ ydb/core/blobstorage/nodewarden/node_warden_impl.cpp
@@ -541,7 +541,7 @@ void TNodeWarden::Handle(TEvBlobStorage::TEvAskWardenRestartPDisk::TPtr ev) {
 
     PDiskRestartRequests[requestCookie] = pdiskId;
 
-    AskBSCToRestartPDisk(pdiskId, requestCookie);
+    AskBSCToRestartPDisk(pdiskId, ev->Get()->IgnoreChecks, requestCookie);
 }
 
 void TNodeWarden::Handle(TEvBlobStorage::TEvNotifyWardenPDiskRestarted::TPtr ev) {
--- ydb/core/blobstorage/pdisk/blobstorage_pdisk_ut_actions.cpp
+++ ydb/core/blobstorage/pdisk/blobstorage_pdisk_ut_actions.cpp
@@ -1136,7 +1136,7 @@ void TTestChunkUnlockRestart::TestFSM(const TActorContext &ctx) {
             SignalDoneEvent();
             break;
         }
-        ctx.Send(NodeWardenId, new TEvBlobStorage::TEvAskWardenRestartPDisk(LastResponse.whiteboardPDiskResult->Record.GetPDiskId()));
+        ctx.Send(NodeWardenId, new TEvBlobStorage::TEvAskWardenRestartPDisk(LastResponse.whiteboardPDiskResult->Record.GetPDiskId(), false));
         break;
     case 30:
         TEST_RESPONSE(EvHarakiri, OK);

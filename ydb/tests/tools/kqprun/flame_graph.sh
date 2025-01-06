9f18009b81b9e6073b4c09ebfbcca97155aba0f7
Grigoriy Pisarenko <grigoriypisar@ydb.tech>
2025-01-06 09:29:18 +0000

    Improved flame graph script

--- ydb/tests/tools/kqprun/flame_graph.sh
+++ ydb/tests/tools/kqprun/flame_graph.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# For svg graph download https://github.com/brendangregg/FlameGraph
-# and run `FlameGraph/stackcollapse-perf.pl profdata.txt | FlameGraph/flamegraph.pl > profdata.svg`
+set -eux
 
-pid=$(pgrep -u $USER kqprun)
+kqprun_pid=$(pgrep -u $USER kqprun)
 
-echo "Target process id: ${pid}"
-
-sudo perf record -F 50 --call-graph dwarf -g --proc-map-timeout=10000 --pid $pid -v -o profdata -- sleep 30
+sudo perf record -F 50 --call-graph dwarf -g --proc-map-timeout=10000 --pid $kqprun_pid -v -o profdata -- sleep ${1:-'30'}
 sudo perf script -i profdata > profdata.txt
+
+flame_graph_tool="../../../../contrib/tools/flame-graph/"
+
+${flame_graph_tool}/stackcollapse-perf.pl profdata.txt | ${flame_graph_tool}/flamegraph.pl > profdata.svg

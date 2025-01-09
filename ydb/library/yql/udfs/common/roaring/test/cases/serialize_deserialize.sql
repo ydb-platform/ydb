217e49da187227faf10b75a0fa9f609fcf0afe8d
jsjant <jsjant@gmail.com>
2025-01-09 11:58:50 +0300

    Improve roaring UDF (#13120)

--- ydb/library/yql/udfs/common/roaring/test/cases/serialize_deserialize.sql
+++ ydb/library/yql/udfs/common/roaring/test/cases/serialize_deserialize.sql
@@ -13,3 +13,6 @@ FROM Input;
 
 SELECT ListTake(ListSkip(Roaring::Uint32List(Roaring::Deserialize(binaryString)), 10), 1) AS EmptyList
 FROM Input;
+
+SELECT Roaring::Uint32List(Roaring::FromUint32List(AsList(10, 567, 42))) AS DeserializedList
+FROM Input;

2bb005a58e90986c76bdbaf6d76a038210fc803e
vlad-gogov <vlad-gogov@ydb.tech>
2025-01-09 10:20:05 +0000

    combining functions

--- ydb/core/formats/arrow/serializer/utils.cpp
+++ ydb/core/formats/arrow/serializer/utils.cpp
@@ -6,28 +6,18 @@
 #include <contrib/libs/apache/arrow/cpp/src/arrow/util/compression.h>
 
 namespace NKikimr::NArrow {
-bool SupportsCompressionLevel(const arrow::Compression::type compression) {
-    return arrow::util::Codec::SupportsCompressionLevel(compression);
-}
-
-bool SupportsCompressionLevel(const arrow::Compression::type compression, const i32 compressionLevel) {
-    if (!SupportsCompressionLevel(compression)) {
+bool SupportsCompressionLevel(const arrow::Compression::type compression, const std::optional<i32>& compressionLevel) {
+    if (!arrow::util::Codec::SupportsCompressionLevel(compression)) {
         return false;
     }
-    int minLevel = MinimumCompressionLevel(compression).value();
-    int maxLevel = MaximumCompressionLevel(compression).value();
-    if (compressionLevel < minLevel || compressionLevel > maxLevel) {
-        return false;
+    if (compressionLevel.has_value()) {
+        int minLevel = MinimumCompressionLevel(compression).value();
+        int maxLevel = MaximumCompressionLevel(compression).value();
+        if (compressionLevel < minLevel || compressionLevel > maxLevel) {
+            return false;
+        }
     }
     return true;
-}
-
-bool SupportsCompressionLevel(const NKikimrSchemeOp::EColumnCodec compression) {
-    return SupportsCompressionLevel(CompressionFromProto(compression).value());
-}
-
-bool SupportsCompressionLevel(const NKikimrSchemeOp::EColumnCodec compression, const i32 compressionLevel) {
-    return SupportsCompressionLevel(CompressionFromProto(compression).value(), compressionLevel);
 }
 
 std::optional<int> MinimumCompressionLevel(const arrow::Compression::type compression) {

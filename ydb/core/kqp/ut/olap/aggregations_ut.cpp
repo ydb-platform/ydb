be1151ee93bd41c838e00b1fc4ac9ee042717ac7
Mikhail Filitov <filitovme@gmail.com>
2025-01-09 19:51:53 +0300

    added test that checks if basic aggregations are handled by block combiner

--- ydb/core/kqp/ut/olap/aggregations_ut.cpp
+++ ydb/core/kqp/ut/olap/aggregations_ut.cpp
@@ -243,6 +243,46 @@ Y_UNIT_TEST_SUITE(KqpOlapAggregations) {
         }
     }
 
+    Y_UNIT_TEST(BlockAggregationIsChosenByDefaultForSimpleQueries) {
+        auto settings = TKikimrSettings()
+            .SetWithSampleTables(false);
+        settings.AppConfig.MutableTableServiceConfig()->SetEnableSpillingNodes("None");
+        TKikimrRunner kikimr(settings);
+
+        TLocalHelper(kikimr).CreateTestOlapTable();
+        auto client = kikimr.GetQueryClient();
+
+        {
+            WriteTestData(kikimr, "/Root/olapStore/olapTable", 10000, 3000000, 1000);
+            WriteTestData(kikimr, "/Root/olapStore/olapTable", 11000, 3001000, 1000);
+            WriteTestData(kikimr, "/Root/olapStore/olapTable", 12000, 3002000, 1000);
+            WriteTestData(kikimr, "/Root/olapStore/olapTable", 13000, 3003000, 1000);
+            WriteTestData(kikimr, "/Root/olapStore/olapTable", 14000, 3004000, 1000);
+            WriteTestData(kikimr, "/Root/olapStore/olapTable", 20000, 2000000, 7000);
+            WriteTestData(kikimr, "/Root/olapStore/olapTable", 30000, 1000000, 11000);
+        }
+
+        const auto aggregators = {"COUNT", "SUM", "SOME", "MIN", "MAX"};
+
+        for (const auto& aggregator : aggregators) {
+            TString query = TStringBuilder()
+                << "SELECT " << aggregator << "(level)"
+                << R"(
+                FROM `/Root/olapStore/olapTable`
+                GROUP BY level
+                )";
+
+            auto res = StreamExplainQuery(query, client);
+            UNIT_ASSERT_C(res.IsSuccess(), res.GetIssues().ToString());
+
+            auto plan = CollectStreamResult(res);
+
+            UNIT_ASSERT_C(plan.QueryStats->Getquery_ast().Contains("BlockMergeFinalizeHashed"), plan.QueryStats->Getquery_ast());
+            UNIT_ASSERT_C(!plan.QueryStats->Getquery_ast().Contains("WideCombiner"), plan.QueryStats->Getquery_ast());
+        }
+
+    }
+
     Y_UNIT_TEST_TWIN(CountAllPushdown, UseLlvm) {
         auto settings = TKikimrSettings()
             .SetWithSampleTables(false);

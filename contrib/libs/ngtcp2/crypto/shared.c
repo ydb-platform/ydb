ad8a91bf945c4449b4005f6b8ac80ab2f69c9862
thegeorg <thegeorg@yandex-team.com>
2025-01-11 23:49:03 +0300

    Cleanup contrib/libs/ngtcp2 import routine
    Preserve original file structure and
    remove excessive hacks applied during `post_install()`
    thus allow further updates to be done automatically.
    commit_hash:3b4e377996c12e4bc99b7bb74423f12a7e039f7b

--- contrib/libs/ngtcp2/crypto/shared.c
+++ contrib/libs/ngtcp2/crypto/shared.c
@@ -22,7 +22,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#include "ngtcp2_crypto_shared.h"
+#include "shared.h"
 
 #ifdef WIN32
 #  include <winsock2.h>

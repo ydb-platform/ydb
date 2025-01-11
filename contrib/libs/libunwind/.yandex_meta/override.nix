53a0b39ec41083775dd7ca2f9bbb4bea1fcd33f3
robot-piglet <robot-piglet@yandex-team.com>
2025-01-11 14:12:24 +0300

    Intermediate changes commit_hash:c1210eda4fea9bfc36b2d0978733c726a4ba4bc4

--- contrib/libs/libunwind/.yandex_meta/override.nix
+++ contrib/libs/libunwind/.yandex_meta/override.nix
@@ -1,11 +1,11 @@
 pkgs: attrs: with pkgs; with attrs; rec {
-  version = "19.1.5";
+  version = "19.1.6";
 
   src = fetchFromGitHub {
     owner = "llvm";
     repo = "llvm-project";
     rev = "llvmorg-${version}";
-    hash = "sha256-QxQL4QwUH6e5HWpBvI8yIbZYBNwqqRsZqPH0w/JSiHQ=";
+    hash = "sha256-LD4nIjZTSZJtbgW6tZopbTF5Mq0Tenj2gbuPXhtOeUI=";
   };
 
   patches = [];

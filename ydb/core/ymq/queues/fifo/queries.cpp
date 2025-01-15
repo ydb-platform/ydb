740bff4d973ca0a97e35b9766b53ef964c3fc450
Sergey Skovorodkin <qyryq@ydb.tech>
2025-01-15 10:24:48 +0000

    Check if queue exists in GetQueueAttributes query for queue tags part

--- ydb/core/ymq/queues/fifo/queries.cpp
+++ ydb/core/ymq/queues/fifo/queries.cpp
@@ -541,24 +541,23 @@ const char* const InternalGetQueueAttributesQuery = R"__(
 
         (let attrsRead (SelectRow attrsTable attrsRow attrsSelect))
 
-        (let queuesTable ')__" ROOT_PARAM R"__(/.Queues)
-
-        (let queuesRow '(
-            '('Account userName)
-            '('QueueName (Utf8String '")__" QUEUE_NAME_PARAM R"__("))))
-
-        (let queuesRowSelect '(
-            'QueueName
-            'Tags))
-
-        (let queuesRowRead (SelectRow queuesTable queuesRow queuesRowSelect))
-
         (let tags
-            (block '(
-                (let str (Coalesce (Member queuesRowRead 'Tags) (Utf8 '"{}")))
-                (return (If (Equal (Utf8 '"") str)
-                            (Utf8 '"{}")
-                            str)))))
+            (If (Not (Exists attrsRead))
+                (Utf8 '"{}")
+                (block '(
+                    (let queuesTable ')__" ROOT_PARAM R"__(/.Queues)
+
+                    (let queuesRow '(
+                        '('Account userName)
+                        '('QueueName (Utf8String '")__" QUEUE_NAME_PARAM R"__("))))
+
+                    (let queuesRowSelect '('QueueName 'Tags))
+                    (let queuesRowRead (SelectRow queuesTable queuesRow queuesRowSelect))
+
+                    (let str (Coalesce (Member queuesRowRead 'Tags) (Utf8 '"{}")))
+                    (return (If (Equal (Utf8 '"") str)
+                                (Utf8 '"{}")
+                                str))))))
 
         (return (AsList
             (SetResult 'queueExists (Exists attrsRead))

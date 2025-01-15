296266537b0ab14b69692a42e9c8dfb68cf0f428
Mikhail Montsev <svartmetal@yandex-team.com>
2025-01-15 09:11:52 +0000

    stable-24-3: Add IsSystem flag to filestore config; support __filestore_space_limit_ssd_system limit in SchemeShard (#12693) (#13346)

--- ydb/core/tx/schemeshard/user_attributes.h
+++ ydb/core/tx/schemeshard/user_attributes.h
@@ -15,6 +15,7 @@ constexpr TStringBuf ATTR_VOLUME_SPACE_LIMIT_SSD_NONREPL = "__volume_space_limit
 constexpr TStringBuf ATTR_VOLUME_SPACE_LIMIT_SSD_SYSTEM = "__volume_space_limit_ssd_system";
 constexpr TStringBuf ATTR_FILESTORE_SPACE_LIMIT_SSD = "__filestore_space_limit_ssd";
 constexpr TStringBuf ATTR_FILESTORE_SPACE_LIMIT_HDD = "__filestore_space_limit_hdd";
+constexpr TStringBuf ATTR_FILESTORE_SPACE_LIMIT_SSD_SYSTEM = "__filestore_space_limit_ssd_system";
 constexpr TStringBuf ATTR_EXTRA_PATH_SYMBOLS_ALLOWED = "__extra_path_symbols_allowed";
 constexpr TStringBuf ATTR_DOCUMENT_API_VERSION = "__document_api_version";
 constexpr TStringBuf ATTR_ASYNC_REPLICATION = "__async_replication";
@@ -34,6 +35,7 @@ enum class EAttribute {
     FILESTORE_SPACE_LIMIT_SSD,
     FILESTORE_SPACE_LIMIT_HDD,
     ASYNC_REPLICA,
+    FILESTORE_SPACE_LIMIT_SSD_SYSTEM,
 };
 
 enum class EUserAttributesOp {

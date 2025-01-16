f52fde8834ba3495bddec14e987924eda26b73ca
thegeorg <thegeorg@yandex-team.com>
2025-01-16 19:49:25 +0300

    Update contrib/libs/cxxsupp/libcxxabi to 17.0.6 commit_hash:0aaacd67905ecefe705a40597df3166fd19dfa9a

--- contrib/libs/cxxsupp/libcxxabi/.yandex_meta/override.nix
+++ contrib/libs/cxxsupp/libcxxabi/.yandex_meta/override.nix
@@ -1,11 +1,11 @@
 pkgs: attrs: with pkgs; with attrs; rec {
-  version = "16.0.6";
+  version = "17.0.6";
 
   src = fetchFromGitHub {
     owner = "llvm";
     repo = "llvm-project";
     rev = "llvmorg-${version}";
-    hash = "sha256-fspqSReX+VD+Nl/Cfq+tDcdPtnQPV1IRopNDfd5VtUs=";
+    hash = "sha256-8MEDLLhocshmxoEBRSKlJ/GzJ8nfuzQ8qn0X/vLA+ag=";
   };
 
   patches = [];

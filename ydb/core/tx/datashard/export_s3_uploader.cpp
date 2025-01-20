c770dc45499551ef3f13deea7797269cd72c46d4
st-shchetinin <st-shchetinin@ydb.tech>
2025-01-20 14:15:28 +0300

    test export

--- ydb/core/tx/datashard/export_s3_uploader.cpp
+++ ydb/core/tx/datashard/export_s3_uploader.cpp
@@ -944,7 +944,7 @@ IActor* TS3Export::CreateUploader(const TActorId& dataShard, ui64 txId) const {
         Ydb::StatusIds::StatusCode status;
         TString error;
         FillTopicDescription(topic, pq.GetPersQueueGroup(), pq.GetSelf(), cdcStream.GetName(), status, error);
-
+        topic.Mutableself()->Clearcreated_at(); // Unnecessary information
         changefeeds.emplace_back(changefeed, topic);
     }
 

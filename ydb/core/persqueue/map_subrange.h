1d90a03fd98ad8014818b7e6d24e79c3198fc5d3
Alexander Kotov <akotov@ydb.tech>
2025-01-20 11:47:50 +0300

    [*] MapSubrange

--- ydb/core/persqueue/map_subrange.h
+++ ydb/core/persqueue/map_subrange.h
@@ -15,27 +15,10 @@ std::pair<I, I> MapSubrange(const M& map,
         }
     }
 
-    auto lowerBound = map.lower_bound(begin);
-    if ((lowerBound != map.end()) && (lowerBound->first == begin) && !includeBegin) {
-        // The `lower_bound` function returns the iterator to a value equal to or greater than
-        // the desired value. If we have found the key in the map and the `IncludeBegin` flag is
-        // reset, then we need to move the iterator forward.
-        ++lowerBound;
-    }
+    auto leftBorder = includeBegin ? map.lower_bound(begin) : map.upper_bound(begin);
+    auto rightBorder = includeEnd ? map.upper_bound(end) : map.lower_bound(end);
 
-    auto upperBound = map.upper_bound(end);
-    if ((upperBound != map.begin()) && (std::prev(upperBound)->first == end) && !includeEnd) {
-        // The 'upper_bound` function returns the iterator by a value greater than the desired
-        // value. If we found the key in the map and the `IncludeEnd` flag is reset, then we
-        // need to move the iterator back.
-        //
-        // For example: there are keys [1, 2, 3, 4, 5] and we need to remove the keys from
-        // the range [2, 4). The `upper_bound` function returns an iterator pointing to 5.
-        // For a half-open interval, we need to move it 1 step back.
-        --upperBound;
-    }
-
-    return {lowerBound, upperBound};
+    return {leftBorder, rightBorder};
 }
 
 }

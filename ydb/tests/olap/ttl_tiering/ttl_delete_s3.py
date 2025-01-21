65c9c3c5ed912aefc26e8090b2e16967cb6b88be
Alexander Avdonkin <aavdonkin@yandex.ru>
2025-01-21 16:36:46 +0300

    Test checking that data are deleted from tiers after delete sql query

--- ydb/tests/olap/ttl_tiering/ttl_delete_s3.py
+++ ydb/tests/olap/ttl_tiering/ttl_delete_s3.py
@@ -8,40 +8,38 @@ logger = logging.getLogger(__name__)
 class TestDeleteS3Ttl(TllTieringTestBase):
     ''' Implements https://github.com/ydb-platform/ydb/issues/13467 '''
 
-    test_name = "delete_s3_ttl"
     row_count = 10 ** 7
     single_upsert_row_count = 10 ** 6
-    cold_bucket = "cold"
-    frozen_bucket = "frozen"
     days_to_cool = 1000
     days_to_freeze = 3000
 
     @classmethod
     def setup_class(cls):
         super(TestDeleteS3Ttl, cls).setup_class()
-        cls.s3_client.create_bucket(cls.cold_bucket)
-        cls.s3_client.create_bucket(cls.frozen_bucket)
 
     def get_row_count_by_date(self, table_path: str, past_days: int) -> int:
         return self.ydb_client.query(f"SELECT count(*) as Rows from `{table_path}` WHERE ts < CurrentUtcTimestamp() - DateTime::IntervalFromDays({past_days})")[0].rows[0]["Rows"]
 
-    def test(self):
-        test_dir = f"{self.ydb_client.database}/{self.test_name}"
-        table_path = f"{test_dir}/table"
-        secret_prefix = self.test_name
+    def prepare_table(self, test_name, cold_bucket, frozen_bucket):
+        test_dir = f"{self.ydb_client.database}/{test_name}"
+        self.table_path = f"{test_dir}/table"
+        secret_prefix = test_name
         access_key_id_secret_name = f"{secret_prefix}_key_id"
         access_key_secret_secret_name = f"{secret_prefix}_key_secret"
-        cold_eds_path = f"{test_dir}/{self.cold_bucket}"
-        frozen_eds_path = f"{test_dir}/{self.frozen_bucket}"
+        cold_eds_path = f"{test_dir}/{cold_bucket}"
+        frozen_eds_path = f"{test_dir}/{frozen_bucket}"
+
+        self.s3_client.create_bucket(cold_bucket)
+        self.s3_client.create_bucket(frozen_bucket)
 
         # Expect empty buckets to avoid unintentional data deletion/modification
-        if self.s3_client.get_bucket_stat(self.cold_bucket) != (0, 0):
+        if self.s3_client.get_bucket_stat(cold_bucket) != (0, 0):
             raise Exception("Bucket for cold data is not empty")
-        if self.s3_client.get_bucket_stat(self.frozen_bucket) != (0, 0):
+        if self.s3_client.get_bucket_stat(frozen_bucket) != (0, 0):
             raise Exception("Bucket for frozen data is not empty")
 
         self.ydb_client.query(f"""
-            CREATE TABLE `{table_path}` (
+            CREATE TABLE `{self.table_path}` (
                 ts Timestamp NOT NULL,
                 s String,
                 val Uint64,
@@ -51,7 +49,7 @@ class TestDeleteS3Ttl(TllTieringTestBase):
             """
         )
 
-        logger.info(f"Table {table_path} created")
+        logger.info(f"Table {self.table_path} created")
 
         self.ydb_client.query(f"CREATE OBJECT {access_key_id_secret_name} (TYPE SECRET) WITH value='{self.s3_client.key_id}'")
         self.ydb_client.query(f"CREATE OBJECT {access_key_secret_secret_name} (TYPE SECRET) WITH value='{self.s3_client.key_secret}'")
@@ -59,7 +57,7 @@ class TestDeleteS3Ttl(TllTieringTestBase):
         self.ydb_client.query(f"""
             CREATE EXTERNAL DATA SOURCE `{cold_eds_path}` WITH (
                 SOURCE_TYPE="ObjectStorage",
-                LOCATION="{self.s3_client.endpoint}/{self.cold_bucket}",
+                LOCATION="{self.s3_client.endpoint}/{cold_bucket}",
                 AUTH_METHOD="AWS",
                 AWS_ACCESS_KEY_ID_SECRET_NAME="{access_key_id_secret_name}",
                 AWS_SECRET_ACCESS_KEY_SECRET_NAME="{access_key_secret_secret_name}",
@@ -70,14 +68,14 @@ class TestDeleteS3Ttl(TllTieringTestBase):
         self.ydb_client.query(f"""
             CREATE EXTERNAL DATA SOURCE `{frozen_eds_path}` WITH (
                 SOURCE_TYPE="ObjectStorage",
-                LOCATION="{self.s3_client.endpoint}/{self.frozen_bucket}",
+                LOCATION="{self.s3_client.endpoint}/{frozen_bucket}",
                 AUTH_METHOD="AWS",
                 AWS_ACCESS_KEY_ID_SECRET_NAME="{access_key_id_secret_name}",
                 AWS_SECRET_ACCESS_KEY_SECRET_NAME="{access_key_secret_secret_name}",
                 AWS_REGION="{self.s3_client.region}"
             )
         """)
-        table = ColumnTableHelper(self.ydb_client, table_path)
+        self.table = ColumnTableHelper(self.ydb_client, self.table_path)
 
         cur_rows = 0
         while cur_rows < self.row_count:
@@ -98,16 +96,16 @@ class TestDeleteS3Ttl(TllTieringTestBase):
                 });
                 upsert into `%s`
                 select * FROM AS_TABLE($rows);
-            """ % (min(self.row_count - cur_rows, self.single_upsert_row_count), table_path))
-            cur_rows = table.get_row_count()
-            logger.info(f"{cur_rows} rows inserted in total, portions: {table.get_portion_stat_by_tier()}, blobs: {table.get_blob_stat_by_tier()}")
+            """ % (min(self.row_count - cur_rows, self.single_upsert_row_count), self.table_path))
+            cur_rows = self.table.get_row_count()
+            logger.info(f"{cur_rows} rows inserted in total, portions: {self.table.get_portion_stat_by_tier()}, blobs: {self.table.get_blob_stat_by_tier()}")
 
-        logger.info(f"Rows older than {self.days_to_cool} days: {self.get_row_count_by_date(table_path, self.days_to_cool)}")
-        logger.info(f"Rows older than {self.days_to_freeze} days: {self.get_row_count_by_date(table_path, self.days_to_freeze)}")
+        logger.info(f"Rows older than {self.days_to_cool} days: {self.get_row_count_by_date(self.table_path, self.days_to_cool)}")
+        logger.info(f"Rows older than {self.days_to_freeze} days: {self.get_row_count_by_date(self.table_path, self.days_to_freeze)}")
 
         def portions_actualized_in_sys():
-            portions = table.get_portion_stat_by_tier()
-            logger.info(f"portions: {portions}, blobs: {table.get_blob_stat_by_tier()}")
+            portions = self.table.get_portion_stat_by_tier()
+            logger.info(f"portions: {portions}, blobs: {self.table.get_blob_stat_by_tier()}")
             if len(portions) != 1 or "__DEFAULT" not in portions:
                 raise Exception("Data not in __DEFAULT teir")
             return self.row_count <= portions["__DEFAULT"]["Rows"]
@@ -117,7 +115,7 @@ class TestDeleteS3Ttl(TllTieringTestBase):
 
         t0 = time.time()
         stmt = f"""
-            ALTER TABLE `{table_path}` SET (TTL =
+            ALTER TABLE `{self.table_path}` SET (TTL =
                 Interval("P{self.days_to_cool}D") TO EXTERNAL DATA SOURCE `{cold_eds_path}`,
                 Interval("P{self.days_to_freeze}D") TO EXTERNAL DATA SOURCE `{frozen_eds_path}`
                 ON ts
@@ -128,9 +126,11 @@ class TestDeleteS3Ttl(TllTieringTestBase):
         logger.info(f"TTL set in {time.time() - t0} seconds")
 
         def data_distributes_across_tiers():
-            cold_bucket_stat = self.s3_client.get_bucket_stat(self.cold_bucket)
-            frozen_bucket_stat = self.s3_client.get_bucket_stat(self.frozen_bucket)
-            logger.info(f"portions: {table.get_portion_stat_by_tier()}, blobs: {table.get_blob_stat_by_tier()}, cold bucket stat: {cold_bucket_stat}, frozen bucket stat: {frozen_bucket_stat}")
+            cold_bucket_stat = self.s3_client.get_bucket_stat(cold_bucket)
+            frozen_bucket_stat = self.s3_client.get_bucket_stat(frozen_bucket)
+            logger.info(
+                f"portions: {self.table.get_portion_stat_by_tier()}, blobs: {self.table.get_blob_stat_by_tier()}, cold bucket stat: {cold_bucket_stat}, frozen bucket stat: {frozen_bucket_stat}"
+            )
             # TODO FIXME
             # We can not expect proper distribution of data across tiers due to https://github.com/ydb-platform/ydb/issues/13525
             # So we wait until some data appears in any bucket
@@ -139,9 +139,19 @@ class TestDeleteS3Ttl(TllTieringTestBase):
         if not self.wait_for(lambda: data_distributes_across_tiers(), 600):
             raise Exception("Data eviction has not been started")
 
+    # TODO FIXME after https://github.com/ydb-platform/ydb/issues/13523
+    def data_deleted_from_buckets(self, cold_bucket, frozen_bucket):
+        cold_bucket_stat = self.s3_client.get_bucket_stat(cold_bucket)
+        frozen_bucket_stat = self.s3_client.get_bucket_stat(frozen_bucket)
+        logger.info(
+            f"portions: {self.table.get_portion_stat_by_tier()}, blobs: {self.table.get_blob_stat_by_tier()}, cold bucket stat: {cold_bucket_stat}, frozen bucket stat: {frozen_bucket_stat}")
+        return cold_bucket_stat[0] == 0 and frozen_bucket_stat[0] == 0
+
+    def test_delete_s3_ttl(self):
+        self.prepare_table('delete_s3_ttl', 'cold', 'frozen')
         t0 = time.time()
         stmt = f"""
-            ALTER TABLE `{table_path}` SET (TTL =
+            ALTER TABLE `{self.table_path}` SET (TTL =
                 Interval("P{self.days_to_cool}D")
                 ON ts
             )
@@ -150,14 +160,18 @@ class TestDeleteS3Ttl(TllTieringTestBase):
         self.ydb_client.query(stmt)
         logger.info(f"TTL set in {time.time() - t0} seconds")
 
-        # TODO FIXME after https://github.com/ydb-platform/ydb/issues/13523
-        def data_deleted_from_buckets():
-            cold_bucket_stat = self.s3_client.get_bucket_stat(self.cold_bucket)
-            frozen_bucket_stat = self.s3_client.get_bucket_stat(self.frozen_bucket)
-            logger.info(
-                f"portions: {table.get_portion_stat_by_tier()}, blobs: {table.get_blob_stat_by_tier()}, cold bucket stat: {cold_bucket_stat}, frozen bucket stat: {frozen_bucket_stat}")
-            return cold_bucket_stat[0] == 0 and frozen_bucket_stat[0] == 0
-
-        if not self.wait_for(lambda: data_deleted_from_buckets(), 120):
+        if not self.wait_for(lambda: self.data_deleted_from_buckets('cold', 'frozen'), 120):
             # raise Exception("not all data deleted") TODO FIXME after https://github.com/ydb-platform/ydb/issues/13535
+            pass
+
+    def test_delete_s3_tiering(self):
+        self.prepare_table('delete_s3_tiering', 'cold_delete', 'frozen_delete')
+        stmt = f"""
+            DELETE FROM `{self.table_path}`
+        """
+        logger.info(stmt)
+        self.ydb_client.query(stmt)
+
+        if not self.wait_for(lambda: self.data_deleted_from_buckets('cold_delete', 'frozen_delete'), 120):
+            # raise Exception("not all data deleted") TODO FIXME after https://github.com/ydb-platform/ydb/issues/13594
             pass

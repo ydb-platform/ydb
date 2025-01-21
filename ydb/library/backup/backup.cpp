6e1936a6274322578021af93b5ceb4a35dbd0e44
stanislav_shchetinin <st-shchetinin@ydb.tech>
2025-01-21 19:14:55 +0300

    Test on topic_description.pb without the created_at field. (#13573)
    Co-authored-by: Ilnaz Nizametdinov <i.nizametdinov@gmail.com>

--- ydb/library/backup/backup.cpp
+++ ydb/library/backup/backup.cpp
@@ -508,7 +508,10 @@ void BackupChangefeeds(TDriver driver, const TString& tablePath, const TFsPath& 
         const auto descTopicResult = DescribeTopic(driver, JoinDatabasePath(tablePath, changefeedDesc.GetName()));
         VerifyStatus(descTopicResult);
         const auto& topicDescription = descTopicResult.GetTopicDescription();
-        const auto protoTopicDescription = NYdb::TProtoAccessor::GetProto(topicDescription);
+        auto protoTopicDescription = NYdb::TProtoAccessor::GetProto(topicDescription);
+        // Unnecessary fields
+        protoTopicDescription.clear_self();
+        protoTopicDescription.clear_topic_stats();
 
         WriteProtoToFile(protoChangeFeedDesc, changefeedDirPath, NDump::NFiles::Changefeed());
         WriteProtoToFile(protoTopicDescription, changefeedDirPath, NDump::NFiles::Topic());

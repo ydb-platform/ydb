a16c31633f3b667bb8f5f01028fa21f3179c3a53
azevaykin <azevaykin@ydb.tech>
2025-01-21 10:38:13 +0000

    use harness ydb_fixtures

--- ydb/tests/functional/tenants/test_create_users.py
+++ ydb/tests/functional/tenants/test_create_users.py
@@ -1,96 +1,77 @@
 # -*- coding: utf-8 -*-
-import logging
 import os
-# import time
+import logging
+import time
+import copy
+import pytest
 
 from hamcrest import (
-    anything,
+    any_of,
     assert_that,
-    # greater_than,
+    calling,
+    equal_to,
+    raises,
     has_length,
     has_properties,
 )
 
-from ydb.tests.library.harness.kikimr_runner import KiKiMR
-from ydb.tests.library.harness.kikimr_config import KikimrConfigGenerator
-# from ydb.tests.library.harness.util import LogLevels
 from ydb.tests.oss.ydb_sdk_import import ydb
+from ydb.tests.library.harness.util import LogLevels
+
 
 logger = logging.getLogger(__name__)
 
-class BaseCreateUsers(object):
-    @classmethod
-    def setup_class(cls):
-        cls.cluster = KiKiMR(
-            KikimrConfigGenerator(
-                additional_log_configs={
-                    # 'SYSTEM_VIEWS': LogLevels.DEBUG
-                }
-            )
-        )
-        # cls.cluster.config.yaml_config['feature_flags']['enable_column_statistics'] = False
-        cls.cluster.start()
 
-    @classmethod
-    def teardown_class(cls):
-        if hasattr(cls, 'cluster'):
-            cls.cluster.stop()
+# local configuration for the ydb cluster (fetched by ydb_cluster_configuration fixture)
+CLUSTER_CONFIG = dict(
+    additional_log_configs={
+        #'TX_PROXY': LogLevels.DEBUG,
+    }
+)
 
-    def setup_method(self, method=None):
-        self.database = "/Root/users/{class_name}_{method_name}".format(
-            class_name=self.__class__.__name__,
-            method_name=method.__name__,
-        )
-        logger.debug("Create database %s" % self.database)
-        self.cluster.create_database(
-            self.database,
-            storage_pool_units_count={
-                'hdd': 1
-            }
-        )
-        self.cluster.register_and_start_slots(self.database, count=1)
-        self.cluster.wait_tenant_up(self.database)
+def test_create_user(ydb_cluster):
+    database = '/Root/users/database'
+    ydb_cluster.create_database(
+        database,
+        storage_pool_units_count={'hdd':1}
+    )
+    database_nodes = ydb_cluster.register_and_start_slots(database, count=1)
+    ydb_cluster.wait_tenant_up(database)
 
-    def teardown_method(self, method=None):
-        logger.debug("Remove database %s" % self.database)
-        self.cluster.remove_database(self.database)
-        self.database = None
-       
+    domain_admin_driver_config = ydb.DriverConfig(
+        endpoint="%s:%s" % (ydb_cluster.nodes[1].host, ydb_cluster.nodes[1].port),
+        database="/Root",
+    )
+    tenant_admin_driver_config = ydb.DriverConfig(
+        endpoint="%s:%s" % (ydb_cluster.nodes[1].host, ydb_cluster.nodes[1].port),
+        database=database,
+    )
+    tenant_user_driver_config = ydb.DriverConfig(
+        endpoint="%s:%s" % (ydb_cluster.nodes[1].host, ydb_cluster.nodes[1].port),
+        database=database,
+        credentials=ydb.StaticCredentials.from_user_password("user", ""),
+    )
 
-class TestCreateUsers(BaseCreateUsers):
-    def test_case(self):
+    with ydb.Driver(domain_admin_driver_config) as driver:
+        with ydb.QuerySessionPool(driver, size=1) as pool:
+            pool.execute_with_retries("CREATE USER user;")
 
-        domain_admin_driver_config = ydb.DriverConfig(
-            endpoint="%s:%s" % (self.cluster.nodes[1].host, self.cluster.nodes[1].port),
-            database="/Root",
-        )
-        tenant_admin_driver_config = ydb.DriverConfig(
-            endpoint="%s:%s" % (self.cluster.nodes[1].host, self.cluster.nodes[1].port),
-            database=self.database,
-        )
-        tenant_user_driver_config = ydb.DriverConfig(
-            endpoint="%s:%s" % (self.cluster.nodes[1].host, self.cluster.nodes[1].port),
-            database=self.database,
-            credentials=ydb.StaticCredentials.from_user_password("user", ""),
-        )
+    with ydb.Driver(tenant_admin_driver_config) as driver:
+        with ydb.QuerySessionPool(driver, size=1) as pool:
+            pool.execute_with_retries(f"GRANT ALL ON `{database}` TO user;")               
 
-        with ydb.Driver(domain_admin_driver_config) as driver:
-            with ydb.QuerySessionPool(driver, size=1) as pool:
-                pool.execute_with_retries("CREATE USER user;")
+    with ydb.Driver(tenant_user_driver_config) as driver:
+        with ydb.QuerySessionPool(driver, size=1) as pool:
+            pool.execute_with_retries("CREATE TABLE table (key Int32, value String, primary key(key));")
 
-        with ydb.Driver(tenant_admin_driver_config) as driver:
-            with ydb.QuerySessionPool(driver, size=1) as pool:
-                pool.execute_with_retries(f"GRANT ALL ON `{self.database}` TO user;")               
+            pool.execute_with_retries("""UPSERT INTO table (key, value) VALUES (1, "value1");""")
 
-        with ydb.Driver(tenant_user_driver_config) as driver:
-            with ydb.QuerySessionPool(driver, size=1) as pool:
-                pool.execute_with_retries("CREATE TABLE table (key Int32, value String, primary key(key));")
+            result_sets = pool.execute_with_retries("SELECT * FROM table")
+            assert_that(result_sets, has_length(1))
+            assert_that(result_sets[0].rows, has_length(1))
+            assert_that(result_sets[0].rows[0].key == 1)
+            assert_that(result_sets[0].rows[0].value == b"value1")
 
-                pool.execute_with_retries("""UPSERT INTO table (key, value) VALUES (1, "value1");""")
-
-                result_sets = pool.execute_with_retries("SELECT * FROM table")
-                assert_that(result_sets, has_length(1))
-                assert_that(result_sets[0].rows, has_length(1))
-                assert_that(result_sets[0].rows[0].key == 1)
-                assert_that(result_sets[0].rows[0].value == b"value1")
+    ydb_cluster.remove_database(database)
+    ydb_cluster.unregister_and_stop_slots(database_nodes)
 

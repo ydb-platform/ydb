fbaf25eeb4ece3eafbc5f111ff230d1124b86cab
robot-piglet <robot-piglet@yandex-team.com>
2025-01-22 00:01:31 +0300

    Intermediate changes commit_hash:fc89f1f32015861d4d8e8547fa1441796a23ec86

--- yt/yt/library/program/program.h
+++ yt/yt/library/program/program.h
@@ -126,8 +126,6 @@ NYson::TYsonString CheckYsonArgMapper(const TString& arg);
 //! Drop privileges and save them if running with suid-bit.
 void ConfigureUids();
 
-void ConfigureCoverageOutput();
-
 void ConfigureIgnoreSigpipe();
 
 //! Intercepts standard crash signals (see signal_registry.h for full list) with a nice handler.

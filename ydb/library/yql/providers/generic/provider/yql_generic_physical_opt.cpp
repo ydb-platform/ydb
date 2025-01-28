54407a0c9e00cf797ae9e0154d0bbc525d82e996
Vitaly Isaev <vitalyisaev@ydb.tech>
2025-01-28 10:04:03 +0000

    Enable timestamp pushdown

--- ydb/library/yql/providers/generic/provider/yql_generic_physical_opt.cpp
+++ ydb/library/yql/providers/generic/provider/yql_generic_physical_opt.cpp
@@ -27,7 +27,7 @@ namespace NYql {
                 : NPushdown::TSettings(NLog::EComponent::ProviderGeneric)
             {
                 using EFlag = NPushdown::TSettings::EFeatureFlag;
-                Enable(EFlag::ExpressionAsPredicate | EFlag::ArithmeticalExpressions | EFlag::ImplicitConversionToInt64);
+                Enable(EFlag::ExpressionAsPredicate | EFlag::ArithmeticalExpressions | EFlag::ImplicitConversionToInt64 | EFlag::DateTimeTypes | EFlag::TimestampCtor);
             }
         };
 

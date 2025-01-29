e6d1a5979493c42a5c8c114027335b96e2b9e12e
Ilia Shakhov <pixcc@ydb.tech>
2025-01-29 12:19:36 +0300

    24-3-15-hotfix: KIKIMR-22120: Block table splits and merges during import from S3 (#13918) (#13918)

--- ydb/core/tx/schemeshard/schemeshard_info_types.cpp
+++ ydb/core/tx/schemeshard/schemeshard_info_types.cpp
@@ -538,6 +538,7 @@ TTableInfo::TAlterDataPtr TTableInfo::CreateAlterData(
     }
 
     alterData->IsBackup = op.GetIsBackup();
+    alterData->IsRestore = op.GetIsRestore();
 
     if (source && op.KeyColumnNamesSize() == 0)
         return alterData;
@@ -1887,6 +1888,11 @@ bool TTableInfo::CheckCanMergePartitions(const TSplitSettings& splitSettings,
         return false;
     }
 
+    // Don't split/merge restore tables
+    if (IsRestore) {
+        return false;
+    }
+
     // Ignore stats from unknown datashard (it could have been split)
     if (!Stats.PartitionStats.contains(shardIdx)) {
         return false;
@@ -1936,6 +1942,10 @@ bool TTableInfo::CheckSplitByLoad(
 {
     // Don't split/merge backup tables
     if (IsBackup)
+        return false;
+
+    // Don't split/merge restore tables
+    if (IsRestore)
         return false;
 
     if (!splitSettings.SplitByLoadEnabled)

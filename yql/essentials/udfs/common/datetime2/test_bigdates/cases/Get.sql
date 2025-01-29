5e587df38e292f4810d76679341967d156d1147a
imunkin <imunkin@yandex-team.com>
2025-01-29 12:35:22 +0300

    YQL-18303: Introduce Get* overloads for date components (numeric) commit_hash:cfaa3532a6554646b11f204e6f4cc7677d428b8b

--- yql/essentials/udfs/common/datetime2/test_bigdates/cases/Get.sql
+++ yql/essentials/udfs/common/datetime2/test_bigdates/cases/Get.sql
@@ -4,19 +4,11 @@ $check = ($arg) -> {
         ryear:              DateTime::GetYear($arg),
         rdayofyear:         DateTime::GetDayOfYear($arg),
         rmonth:             DateTime::GetMonth($arg),
-        rmonthname:         DateTime::GetMonthName($arg),
         rweekofyear:        DateTime::GetWeekOfYear($arg),
         rweekofyeariso8601: DateTime::GetWeekOfYearIso8601($arg),
         rdayofmonth:        DateTime::GetDayOfMonth($arg),
         rdayofweek:         DateTime::GetDayOfWeek($arg),
-        rdayofweekname:     DateTime::GetDayOfWeekName($arg),
-        rhour:              DateTime::GetHour($arg),
-        rminute:            DateTime::GetMinute($arg),
-        rsecond:            DateTime::GetSecond($arg),
-        rmsec:              DateTime::GetMillisecondOfSecond($arg),
-        rusec:              DateTime::GetMicrosecondOfSecond($arg),
         rtz:                DateTime::GetTimezoneId($arg),
-        rtzname:            DateTime::GetTimezoneName($arg),
     |>
 };
 
@@ -28,6 +20,6 @@ $typeDispatcher = ($row) -> {
     |>;
 };
 
-$input = SELECT CAST(ftztimestamp as TzTimestamp) as tm FROM Input;
+$input = SELECT CAST(ftimestamp64 AS Timestamp64) as tm FROM Input;
 
 PROCESS $input USING $typeDispatcher(TableRow());

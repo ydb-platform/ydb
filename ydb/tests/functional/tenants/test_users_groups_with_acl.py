2fa5e59a6f59502a6a30eda076d8057903680303
Andrew Stalin <astalin@ydb.tech>
2025-01-30 02:41:40 +0000

    fix linter errors

--- ydb/tests/functional/tenants/test_users_groups_with_acl.py
+++ ydb/tests/functional/tenants/test_users_groups_with_acl.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
-import os
-import inspect
 
 from hamcrest import (
-    anything,
     assert_that,
     has_length,
-    has_properties,
 )
 
 from ydb.tests.library.harness.kikimr_runner import KiKiMR
@@ -69,51 +65,18 @@ class BaseCreateSubjects(object):
         self.cluster.remove_database(self.database)
         self.database = None
 
-    def yql_check_permission(self):
-        with ydb.Driver(self.tenant_user_driver_config) as driver:
-            driver.wait(5)
-            script_client = ydb.ScriptingClient(driver)
-            script_client.execute_yql("CREATE TABLE table (key Int32, value String, primary key(key));")
-            script_client.execute_yql("""UPSERT INTO table (key, value) VALUES (1, "value1");""")
-
-            result_sets = script_client.execute_yql("SELECT * FROM table")
-            assert_that(result_sets, has_length(1))
-            assert_that(result_sets[0].rows, has_length(1))
-            assert_that(result_sets[0].rows[0].key == 1)
-            assert_that(result_sets[0].rows[0].value == b"value1")
-
-            script_client.execute_yql("DROP TABLE table;")
-
-    def query_check_permission(self):
-        with ydb.Driver(self.tenant_user_driver_config) as driver:
-            with ydb.QuerySessionPool(driver, size=1) as pool:
-                pool.execute_with_retries("CREATE TABLE table (key Int32, value String, primary key(key));")
-                pool.execute_with_retries("""UPSERT INTO table (key, value) VALUES (1, "value1");""")
-
-                result_sets = pool.execute_with_retries("SELECT * FROM table")
-                assert_that(result_sets, has_length(1))
-                assert_that(result_sets[0].rows, has_length(1))
-                assert_that(result_sets[0].rows[0].key == 1)
-                assert_that(result_sets[0].rows[0].value == b"value1")
-
-                pool.execute_with_retries("DROP TABLE table;")
-
     def yql_create_user(self, admin_driver_config):
         with ydb.Driver(admin_driver_config) as driver:
             driver.wait(5)
             script_client = ydb.ScriptingClient(driver)
             script_client.execute_yql('CREATE USER user;')
-            #script_client.execute_yql(f"GRANT ALL ON `{self.database}` TO user;")
-            # REVOKE 'ydb.generic.read' ON `/shop_db/orders` FROM user1;
             script_client.execute_yql('ALTER USER user WITH PASSWORD NULL;')
             script_client.execute_yql('DROP USER user;')
-
 
     def query_create_user(self, admin_driver_config):
         with ydb.Driver(admin_driver_config) as driver:
             with ydb.QuerySessionPool(driver, size=1) as pool:
                 pool.execute_with_retries("CREATE USER user;")
-                #pool.execute_with_retries(f"GRANT ALL ON `{self.database}` TO user;")
                 pool.execute_with_retries('ALTER USER user WITH PASSWORD NULL;')
                 pool.execute_with_retries("DROP USER user;")
 
@@ -122,7 +85,6 @@ class BaseCreateSubjects(object):
             driver.wait(5)
             script_client = ydb.ScriptingClient(driver)
             script_client.execute_yql("CREATE GROUP group;")
-            #script_client.execute_yql(f"GRANT ALL ON `{self.database}` TO group;")
             script_client.execute_yql("CREATE USER user;")
             script_client.execute_yql("ALTER GROUP group ADD USER user;")
             script_client.execute_yql("ALTER GROUP group DROP USER user;")
@@ -133,7 +95,6 @@ class BaseCreateSubjects(object):
         with ydb.Driver(admin_driver_config) as driver:
             with ydb.QuerySessionPool(driver, size=1) as pool:
                 pool.execute_with_retries("CREATE GROUP group;")
-                #pool.execute_with_retries(f"GRANT ALL ON `{self.database}` TO group;")
                 pool.execute_with_retries("CREATE USER user;")
                 pool.execute_with_retries("ALTER GROUP group ADD USER user;")
                 pool.execute_with_retries("ALTER GROUP group DROP USER user;")
@@ -142,42 +103,32 @@ class BaseCreateSubjects(object):
 
     def test_yql_create_user_by_domain_admin(self):
         self.yql_create_user(self.domain_admin_driver_config)
-        # self.yql_check_permission()
-    
+
     def test_yql_create_user_by_tenant_admin(self):
         self.yql_create_user(self.tenant_admin_driver_config)
-        #self.yql_check_permission()
 
     def test_yql_create_group_by_domain_admin(self):
         self.yql_create_group(self.domain_admin_driver_config)
-        # self.yql_check_permission()
-    
+
     def test_yql_create_group_by_tenant_admin(self):
         self.yql_create_group(self.tenant_admin_driver_config)
-        #self.yql_check_permission()
-
-
-
 
     def test_query_create_user_by_domain_admin(self):
         self.query_create_user(self.domain_admin_driver_config)
-        #self.query_check_permission()
-    
+
     def test_query_create_user_by_tenant_admin(self):
         self.query_create_user(self.tenant_admin_driver_config)
-        #self.query_check_permission()
 
     def test_query_create_group_by_domain_admin(self):
         self.query_create_group(self.domain_admin_driver_config)
-        #self.query_check_permission()
-    
+
     def test_query_create_group_by_tenant_admin(self):
         self.query_create_group(self.tenant_admin_driver_config)
-        #self.query_check_permission()
 
 
 class TestCreateSubjectsWithDomainLoginOnly(BaseCreateSubjects):
     DomainLoginOnly = True
+
 
 class TestCreateSubjectsWithoutDomainLoginOnly(BaseCreateSubjects):
     DomainLoginOnly = False

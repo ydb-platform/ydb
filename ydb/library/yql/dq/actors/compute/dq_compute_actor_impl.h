cf2a214259faa6d3215b80947b2cfbed5474b32a
Yuriy Kaminskiy <yumkam7@ydb.tech>
2025-01-31 20:44:05 +0300

    Fix inverted condition in IsPaused
    fortunatelly, this function is only used in monitoring page for async
    CA, and when checkpoint is used in sync actor (this combination is not
    used anywhere)

--- ydb/library/yql/dq/actors/compute/dq_compute_actor_impl.h
+++ ydb/library/yql/dq/actors/compute/dq_compute_actor_impl.h
@@ -834,7 +834,7 @@ protected:
         }
 
         bool IsPaused() const {
-            return PendingWatermarks.empty() || PendingCheckpoint.has_value();
+            return (!PendingWatermarks.empty()) || PendingCheckpoint.has_value();
         }
 
         void Pause(TInstant watermark) {

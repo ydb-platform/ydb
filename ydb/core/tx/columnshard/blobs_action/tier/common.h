4638fa272a2942170c8de4787d01099d2b5800ea
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-02-03 15:36:46 +0300

    use AdaptiveMutex

--- ydb/core/tx/columnshard/blobs_action/tier/common.h
+++ ydb/core/tx/columnshard/blobs_action/tier/common.h
@@ -9,7 +9,7 @@ namespace NKikimr::NOlap::NBlobOperations::NTier {
 class TExternalStorageOperatorHolder {
 private:
     NWrappers::NExternalStorage::IExternalStorageOperator::TPtr StorageOperator = nullptr;
-    TMutex Mutex;
+    TAdaptiveLock Mutex;
 
 public:
     void Emplace(const NWrappers::NExternalStorage::IExternalStorageOperator::TPtr& storageOperator) {

1b81e96c94f01a23da5bd4c0436c070b8003f62f
ivanmorozov333 <ivanmorozov@hotmail.com>
2025-02-06 20:29:01 +0300

    fix validation

--- ydb/core/formats/arrow/serializer/native.cpp
+++ ydb/core/formats/arrow/serializer/native.cpp
@@ -86,7 +86,7 @@ TString TNativeSerializer::DoSerializePayload(const std::shared_ptr<arrow::Recor
     arrow::ipc::IpcPayload payload;
     // Build payload. Compression if set up performed here.
     TStatusValidator::Validate(arrow::ipc::GetRecordBatchPayload(*batch, Options, &payload));
-#ifndef _NDEBUG
+#ifndef NDEBUG
 //    TStatusValidator::Validate(batch->ValidateFull());
 #endif
 
@@ -102,7 +102,8 @@ TString TNativeSerializer::DoSerializePayload(const std::shared_ptr<arrow::Recor
     // Write prepared payload into the resultant string. No extra allocation will be made.
     TStatusValidator::Validate(arrow::ipc::WriteIpcPayload(payload, Options, &out, &metadata_length));
     Y_ABORT_UNLESS(out.GetPosition() == str.size());
-#ifndef _NDEBUG
+    AFL_VERIFY_DEBUG
+#ifndef NDEBUG
     TStatusValidator::GetValid(Deserialize(str, batch->schema()));
 #endif
     AFL_DEBUG(NKikimrServices::ARROW_HELPER)("event", "serialize")("size", str.size())("columns", batch->schema()->num_fields());

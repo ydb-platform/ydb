c129241077a94067eef1f51aad40b6c82c3dfa3d
Andrey Serebryanskiy <serebryanskiy@ydb.tech>
2025-02-06 14:42:01 +0000

    fix tests

--- ydb/library/testlib/service_mocks/access_service_mock.h
+++ ydb/library/testlib/service_mocks/access_service_mock.h
@@ -137,6 +137,7 @@ public:
     THashSet<TString> AllowedUserPermissions = {
         "user1-something.read",
         "ApiKey-value-valid-something.read",
+        "ApiKey-value-valid-ydb.streams.write",
         "user1-monitoring.view"};
     THashMap<TString, TString> AllowedServicePermissions = {{"service1-something.write", "root1/folder1"}};
     THashSet<TString> AllowedResourceIds = {};

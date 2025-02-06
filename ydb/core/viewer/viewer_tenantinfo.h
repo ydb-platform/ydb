82e0a603d8a16ddaf1db66013efb30bc5a668e49
Alexey Efimov <xeno@ydb.tech>
2025-02-06 11:44:52 +0000

    fixes after review

--- ydb/core/viewer/viewer_tenantinfo.h
+++ ydb/core/viewer/viewer_tenantinfo.h
@@ -123,13 +123,12 @@ public:
 
         if (Database.empty()) {
             ListTenantsResponse = MakeRequestConsoleListTenants();
+            NavigateKeySetResult[DomainPath] = MakeRequestSchemeCacheNavigate(DomainPath);
         } else {
             if (Database != DomainPath) {
-                NavigateKeySetResult[Database] = MakeRequestSchemeCacheNavigate(Database);
                 TenantStatusResponses[Database] = MakeRequestConsoleGetTenantStatus(Database);
-            } else if (DatabaseNavigateResponse && DatabaseNavigateResponse->IsOk()) {
-                NavigateKeySetResult[Database] = std::move(DatabaseNavigateResponse.value());
             }
+            NavigateKeySetResult[Database] = MakeRequestSchemeCacheNavigate(Database);
         }
 
         if (Database.empty() || Database == DomainPath) {
@@ -139,9 +138,6 @@ public:
             tenant.SetType(NKikimrViewer::Domain);
             tenant.SetName(DomainPath);
             RequestMetadataCacheHealthCheck(DomainPath);
-            if (Database.empty() || !DatabaseNavigateResponse || !DatabaseNavigateResponse->IsOk()) {
-                NavigateKeySetResult[DomainPath] = MakeRequestSchemeCacheNavigate(DomainPath);
-            }
         }
 
         HiveDomainStats[RootHiveId] = MakeRequestHiveDomainStats(RootHiveId);

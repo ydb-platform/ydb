fc520803f053ed2390139759eb380f3963a3f7c7
Mikhail Filitov <filitovme@gmail.com>
2025-02-10 16:24:24 +0300

    seems to be working correctly

--- yql/essentials/minikql/comp_nodes/mkql_block_agg.cpp
+++ yql/essentials/minikql/comp_nodes/mkql_block_agg.cpp
@@ -1638,7 +1638,7 @@ public:
     }
 
     bool HasAnythingToLoad() const {
-        return LoadingBucket >= NumberOfSpillingBuckets_;
+        return LoadingBucket < NumberOfSpillingBuckets_;
     }
 
     ui64 LoadingBucket = 0;

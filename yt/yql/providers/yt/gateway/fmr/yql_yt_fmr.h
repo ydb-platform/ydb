24f0f77bb308a9675f1aae84708724718f12282f
cdzyura171 <cdzyura171@yandex-team.com>
2025-02-10 11:53:19 +0300

    Use coordinator and worker in fmrGateway
    use coordinator and worker in fmrGateway
    commit_hash:a0977459dcbe041e5330c8959152be40fe18eea6

--- yt/yql/providers/yt/gateway/fmr/yql_yt_fmr.h
+++ yt/yql/providers/yt/gateway/fmr/yql_yt_fmr.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include <yt/yql/providers/yt/fmr/coordinator/interface/yql_yt_coordinator.h>
 #include <yt/yql/providers/yt/provider/yql_yt_forwarding_gateway.h>
 
 namespace NYql {
 
-IYtGateway::TPtr CreateYtFmrGateway(IYtGateway::TPtr slave);
+struct TFmrYtGatewaySettings {
+    TIntrusivePtr<IRandomProvider> RandomProvider;
+    TDuration TimeToSleepBetweenGetOperationRequests;
+};
+
+IYtGateway::TPtr CreateYtFmrGateway(
+    IYtGateway::TPtr slave,
+    IFmrCoordinator::TPtr coordinator = nullptr,
+    const TFmrYtGatewaySettings& settings = TFmrYtGatewaySettings{
+        .RandomProvider = CreateDefaultRandomProvider(),
+        .TimeToSleepBetweenGetOperationRequests = TDuration::Seconds(1)
+    }
+);
 
 } // namspace NYql

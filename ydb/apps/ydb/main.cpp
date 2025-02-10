f01b54cea2c6e94a45527eb6ea97cad1fefe06a0
Kirill Rysin <naspirato@gmail.com>
2025-02-10 13:48:13 +0000

    rollback

--- ydb/apps/ydb/main.cpp
+++ ydb/apps/ydb/main.cpp
@@ -24,7 +24,6 @@ int main(int argc, char **argv) {
     }
     catch (const NYdb::NStatusHelpers::TYdbErrorException& e) {
         Cerr << e;
-        
         return EXIT_FAILURE;
     }
     catch (const yexception& e) {

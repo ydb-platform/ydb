4893ec2364d860474b078a0611638b1a4096fa69
Natasha Pirogova <galnat@ydb.tech>
2025-02-12 11:45:17 +0300

    Update validate_pr_description.py

--- .github/actions/validate_pr_description/validate_pr_description.py
+++ .github/actions/validate_pr_description/validate_pr_description.py
@@ -1,32 +1,38 @@
 import sys
 import re
+from typing import Tuple
 
 issue_patterns = [
     r"https://github.com/ydb-platform/ydb/issues/\d+",
     r"https://st.yandex-team.ru/[a-zA-Z]+-\d+"
 ]
+def validate_pr_description(description, is_not_for_cl_valid=True) -> bool:
+    result, _  = check_pr_description(description, is_not_for_cl_valid)
+    return result
 
-def validate_pr_description(description, is_not_for_cl_valid=True):
+def check_pr_description(description, is_not_for_cl_valid=True) -> Tuple[bool, str]:
     try:
         if not description.strip():
-            print("::warning::PR description is empty. Please fill it out.")
-            echo "::warning::PR description is empty. Please fill it out." | GITHUB_TOKEN="${{ github.token }}"  .github/scripts/tests/comment-pr.py --color red
-            return False
+            txt = "PR description is empty. Please fill it out."
+            print(f"::warning::{txt}")
+            return False, txt
 
         if "### Changelog category" not in description and "### Changelog entry" not in description:
-            return is_not_for_cl_valid
+            return is_not_for_cl_valid, "Changelog category and entry sections are not found."
 
         # Extract changelog category section
         category_section = re.search(r"### Changelog category.*?\n(.*?)(\n###|$)", description, re.DOTALL)
         if not category_section:
-            print("::warning::Changelog category section not found.")
-            return False
+            txt = "Changelog category section not found."
+            print(f"::warning::{txt}")
+            return False, txt
 
         categories = [line.strip('* ').strip() for line in category_section.group(1).splitlines() if line.strip()]
 
         if len(categories) != 1:
-            print("::warning::Only one category can be selected at a time.")
-            return False
+            txt = "Only one category can be selected at a time."
+            print(f"::warning::{txt}")
+            return False, txt
 
         category = categories[0]
         for_cl_categories = [
@@ -47,42 +53,48 @@ def validate_pr_description(description, is_not_for_cl_valid=True):
         valid_categories = for_cl_categories + not_for_cl_categories
 
         if not any(cat.startswith(category) for cat in valid_categories):
-            print(f"::warning::Invalid Changelog category: {category}")
-            return False
+            txt = f"Invalid Changelog category: {category}"
+            print(f"::warning::{txt}")
+            return False, txt
 
         if not is_not_for_cl_valid and any(cat.startswith(category) for cat in not_for_cl_categories):
-            print(f"::notice::Category is not for changelog: {category}")
-            return False
+            txt = f"Category is not for changelog: {category}"
+            print(f"::notice::{txt}")
+            return False, txt
 
         if not any(cat.startswith(category) for cat in not_for_cl_categories):
             entry_section = re.search(r"### Changelog entry.*?\n(.*?)(\n###|$)", description, re.DOTALL)
             if not entry_section or len(entry_section.group(1).strip()) < 20:
-                print("::warning::The changelog entry is less than 20 characters or missing.")
-                return False
+                txt = "The changelog entry is less than 20 characters or missing."
+                print(f"::warning::{txt}")
+                return False, txt
 
             if category == "Bugfix":
                 def check_issue_pattern(issue_pattern):
                     return re.search(issue_pattern, description)
 
                 if not any(check_issue_pattern(issue_pattern) for issue_pattern in issue_patterns):
-                    print("::warning::Bugfix requires a linked issue in the changelog entry")
-                    return False
+                    txt = "Bugfix requires a linked issue in the changelog entry"
+                    print(f"::warning::{txt}")
+                    return False, txt
 
         print("PR description is valid.")
-        return True
+        return True, "PR description is valid."
 
     except Exception as e:
-        print(f"::error::Error during validation: {e}")
-        return False
+        txt = f"Error during validation: {e}"
+        print(f"::error::{txt}")
+        return False, txt
 
-def validate_pr_description_from_file(file_path):
+def validate_pr_description_from_file(file_path) -> Tuple[bool, str]:
     try:
         with open(file_path, 'r') as file:
             description = file.read()
-        return validate_pr_description(description)
+        return check_pr_description(description)
     except Exception as e:
-        print(f"::error::Failed to validate PR description: {e}")
-        return False
+        txt = f"Failed to validate PR description: {e}"
+        print(f"::error::{txt}")
+        return False, txt
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -90,6 +102,8 @@ if __name__ == "__main__":
         sys.exit(1)
     
     file_path = sys.argv[1]
-    is_valid = validate_pr_description_from_file(file_path)
+    is_valid, txt = validate_pr_description_from_file(file_path)
+    from post_status_to_github import post
+    post(is_valid, txt)
     if not is_valid:
         sys.exit(1)

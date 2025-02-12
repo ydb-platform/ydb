b9ac376799be591d848e3dcc8e738c207339b484
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2025-02-12 18:03:00 +0300

    Split view_utils (#14492)

--- ydb/public/lib/ydb_cli/dump/util/rewrite_query.h
+++
@@ -1,7 +0,0 @@
-#include <yql/essentials/public/issue/yql_issue.h>
-
-namespace NYdb::NDump {
-
-bool RewriteCreateQuery(TString& query, std::string_view pattern, const std::string& dbPath, NYql::TIssues& issues);
-
-}

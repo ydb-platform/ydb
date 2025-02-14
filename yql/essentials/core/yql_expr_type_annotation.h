dd88f08e1467fc5e2ef1edf5b93da3ba9ff937f5
imunkin <imunkin@yandex-team.com>
2025-02-14 12:33:27 +0300

    Drop the flag to control type annotation for WideToBlocks commit_hash:45dce3709997e47b9a5d852a10cb2e7684951138

--- yql/essentials/core/yql_expr_type_annotation.h
+++ yql/essentials/core/yql_expr_type_annotation.h
@@ -354,8 +354,4 @@ TStringBuf NormalizeCallableName(TStringBuf name);
 
 void CheckExpectedTypeAndColumnOrder(const TExprNode& node, TExprContext& ctx, TTypeAnnotationContext& typesCtx);
 
-namespace NBlockStreamIO {
-    constexpr bool WideToBlocks = true;
-} // namespace NBlockStreamIO
-
 }

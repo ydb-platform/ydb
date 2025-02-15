e3bc3bdb70937e401484014dfbc460b2dd6e2070
Cthulhu <cthulhu@ydb.tech>
2025-02-15 08:48:34 +0300

    Implement shred PDisk, #12483 (#14598)

--- ydb/core/blobstorage/pdisk/blobstorage_pdisk_request_id.h
+++ ydb/core/blobstorage/pdisk/blobstorage_pdisk_request_id.h
@@ -95,6 +95,8 @@ struct TReqId {
         MarkDirty = 76,
         ChunkShred = 77,
         ChunkShredResult = 78,
+        ContinueShred = 79,
+        MarkDirtySysLog = 80,
     };
 
     // 56 bit idx, 8 bit source
@@ -162,8 +164,8 @@ enum class ERequestType {
     RequestShredPDisk,
     RequestPreShredCompactVDiskResult,
     RequestShredVDiskResult,
-    RequestMarkDirty,
     RequestChunkShredResult,
+    RequestContinueShred,
 };
 
 inline IOutputStream& operator <<(IOutputStream& out, const TReqId& reqId) {

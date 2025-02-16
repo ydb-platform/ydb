599f451f52ccd0d8ea680eed208a291853a42f29
Natasha Pirogova <galnat@ydb.tech>
2025-02-16 14:27:34 +0300

    Update scripts (#27)
    Update tasks templates.
    Fix bug with token.
    Check pr template as is.

--- .github/actions/validate_pr_description/validate_pr_description.py
+++ .github/actions/validate_pr_description/validate_pr_description.py
@@ -4,8 +4,29 @@ from typing import Tuple
 
 issue_patterns = [
     r"https://github.com/ydb-platform/ydb/issues/\d+",
-    r"https://st.yandex-team.ru/[a-zA-Z]+-\d+"
+    r"https://st.yandex-team.ru/[a-zA-Z]+-\d+",
+    r"#+d+",
+    r"[a-zA-Z]+-\d+"
 ]
+
+pull_request_template = """
+### Changelog entry <!-- a user-readable short description of the changes that goes to CHANGELOG.md and Release Notes -->
+
+...
+
+### Changelog category <!-- remove all except one -->
+
+* New feature
+* Experimental feature
+* Improvement
+* Performance improvement
+* User Interface
+* Bugfix 
+* Backward incompatible change
+* Documentation (changelog entry is not required)
+* Not for changelog (changelog entry is not required)
+"""
+
 def validate_pr_description(description, is_not_for_cl_valid=True) -> bool:
     try:
         result, _  = check_pr_description(description, is_not_for_cl_valid)
@@ -22,6 +43,9 @@ def check_pr_description(description, is_not_for_cl_valid=True) -> Tuple[bool, s
 
     if "### Changelog category" not in description and "### Changelog entry" not in description:
         return is_not_for_cl_valid, "Changelog category and entry sections are not found."
+
+    if pull_request_template.strip() in description.strip():
+            return is_not_for_cl_valid, "Pull request template as is."
 
     # Extract changelog category section
     category_section = re.search(r"### Changelog category.*?\n(.*?)(\n###|$)", description, re.DOTALL)

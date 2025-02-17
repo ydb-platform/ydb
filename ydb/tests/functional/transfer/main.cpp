fd669fa58640f71c4d8b5054f1bda74e14396b7a
Nikolay Shestakov <tesseract@ydb.tech>
2025-02-17 05:48:32 +0000

    use query service

--- ydb/tests/functional/transfer/main.cpp
+++ ydb/tests/functional/transfer/main.cpp
@@ -2,7 +2,7 @@
 #include <library/cpp/testing/unittest/registar.h>
 
 #include <ydb-cpp-sdk/client/driver/driver.h>
-#include <ydb-cpp-sdk/client/table/table.h>
+#include <ydb-cpp-sdk/client/query/client.h>
 #include <ydb-cpp-sdk/client/topic/client.h>
 #include <ydb-cpp-sdk/client/proto/accessor.h>
 #include <ydb-cpp-sdk/client/draft/ydb_scripting.h>
@@ -10,29 +10,27 @@
 #include <library/cpp/threading/local_executor/local_executor.h>
 
 using namespace NYdb;
-using namespace NYdb::NTable;
+using namespace NYdb::NQuery;
 using namespace NYdb::NTopic;
 
 namespace {
 
 std::pair<ui64, Ydb::ResultSet> DoRead(TSession& s, const TString& table) {
-    /*
-    auto resCount = s.ExecuteDataQuery(
+    auto resCount = s.ExecuteQuery(
         Sprintf("SELECT COUNT(*) AS __count FROM `/local/%s`;", table.data()),
-            TTxControl::BeginTx().CommitTx()).GetValueSync();
+            TTxControl::NoTx()).GetValueSync();
     UNIT_ASSERT_C(resCount.IsSuccess(), resCount.GetIssues().ToString());
     auto rsCount = NYdb::TResultSetParser(resCount.GetResultSet(0));
     UNIT_ASSERT(rsCount.TryNextRow());
     auto count = rsCount.ColumnParser("__count").GetUint64();
-    */
 
-    auto res = s.ExecuteDataQuery(
+    auto res = s.ExecuteQuery(
         Sprintf("SELECT * FROM `/local/%s`  ORDER BY Key", table.data()),
-            TTxControl::BeginTx(NYdb::NTable::TTxSettings::SerializableRW())).GetValueSync();
+            TTxControl::NoTx()).GetValueSync();
     UNIT_ASSERT_C(res.IsSuccess(), res.GetIssues().ToString());
 
     const auto proto = NYdb::TProtoAccessor::GetProto(res.GetResultSet(0));
-    return {0, proto};
+    return {count, proto};
 }
 
 } // namespace
@@ -44,36 +42,39 @@ Y_UNIT_TEST_SUITE(Transfer)
         TString connectionString = GetEnv("YDB_ENDPOINT") + "/?database=" + GetEnv("YDB_DATABASE");
         auto config = TDriverConfig(connectionString);
         auto driver = TDriver(config);
-        auto tableClient = TTableClient(driver, TClientSettings().UseQueryCache(false));
+        auto tableClient = TQueryClient(driver);
         auto session = tableClient.GetSession().GetValueSync().GetSession();
         auto topicClient = TTopicClient(driver);
         auto writeSession = topicClient.CreateSimpleBlockingWriteSession(TWriteSessionSettings("/local/SourceTopic", "producer-1", "producer-1"));
 
         {
-            auto res = session.ExecuteSchemeQuery(R"(
+            auto res = session.ExecuteQuery(R"(
                 CREATE TABLE `/local/TargetColumnTable` (
                     Key Uint64 NOT NULL,
-                    Message Utf8,
+                    Message Utf8 NOT NULL,
                     PRIMARY KEY (Key)
                 )  WITH (
                     STORE = COLUMN
                 );
-            )").GetValueSync();
+            )", TTxControl::NoTx()).GetValueSync();
             UNIT_ASSERT_C(res.IsSuccess(), res.GetIssues().ToString());
         }
 
         {
-            auto res = session.ExecuteSchemeQuery(R"(
+            auto res = session.ExecuteQuery(R"(
                 CREATE TOPIC `/local/SourceTopic`;
-            )").GetValueSync();
+            )", TTxControl::NoTx()).GetValueSync();
             UNIT_ASSERT_C(res.IsSuccess(), res.GetIssues().ToString());
         }
 
         {
-            auto res = session.ExecuteSchemeQuery(Sprintf(R"(
+            auto res = session.ExecuteQuery(Sprintf(R"(
                 $l = ($x) -> {
                     return [
-                        <| Key:$x._offset, Message:$x._data |>
+                        <|
+                            Key:CAST($x._offset AS Uint64),
+                            Message:CAST($x._data AS Utf8)
+                        |>
                     ];
                 };
 
@@ -83,24 +84,24 @@ Y_UNIT_TEST_SUITE(Transfer)
                     CONNECTION_STRING = 'grpc://%s',
                     TOKEN = 'user@builtin'
                 );
-            )", connectionString.data())).GetValueSync();
+            )", connectionString.data()), TTxControl::NoTx()).GetValueSync();
             UNIT_ASSERT_C(res.IsSuccess(), res.GetIssues().ToString());
         }
-
-        session.Close().GetValueSync();
-        session = tableClient.GetSession().GetValueSync().GetSession();
 
         {
             writeSession->Write("message-1");
         }
 
         {
-            for (size_t attempt = 10; attempt-- ; ) {
+            for (size_t attempt = 20; attempt-- ; ) {
                 auto res = DoRead(session, "TargetColumnTable");
+                Cerr << "Attempt=" << attempt << " count=" << res.first << Endl << Flush;
                 if (res.first == 1) {
                     const Ydb::ResultSet& proto = res.second;
                     UNIT_ASSERT_VALUES_EQUAL(proto.rows(0).items(0).uint64_value(), 0);
                     UNIT_ASSERT_VALUES_EQUAL(proto.rows(0).items(1).text_value(), "message-1");
+
+                    break;
                 }
 
                 UNIT_ASSERT_C(attempt, "Unable to wait replication result");

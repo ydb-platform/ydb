735247931d05ded4527d9e150881f59491103b22
Pisarenko Grigoriy <grigoriypisar@ydb.tech>
2025-02-17 16:13:37 +0500

    YQ-3561 FQrun supported connections / bindings (#14552)

--- ydb/tests/tools/fqrun/src/common.h
+++ ydb/tests/tools/fqrun/src/common.h
@@ -4,13 +4,29 @@
 
 #include <ydb/core/fq/libs/config/protos/fq_config.pb.h>
 #include <ydb/core/protos/config.pb.h>
+#include <ydb/library/yql/providers/pq/provider/yql_pq_gateway.h>
 #include <ydb/tests/tools/kqprun/runlib/settings.h>
 
 namespace NFqRun {
 
+constexpr char YQL_TOKEN_VARIABLE[] = "YQL_TOKEN";
 constexpr i64 MAX_RESULT_SET_ROWS = 1000;
 
 struct TFqSetupSettings : public NKikimrRun::TServerSettings {
+    enum class EVerbose {
+        None,
+        Info,
+        QueriesText,
+        InitLogs,
+        Max
+    };
+
+    bool EmulateS3 = false;
+
+    EVerbose VerboseLevel = EVerbose::Info;
+
+    TString YqlToken;
+    NYql::IPqGateway::TPtr PqGateway;
     NFq::NConfig::TConfig FqConfig;
     NKikimrConfig::TLogConfig LogConfig;
 };
@@ -25,5 +41,7 @@ struct TRunnerOptions {
 struct TRequestOptions {
     TString Query;
 };
+
+void SetupAcl(FederatedQuery::Acl* acl);
 
 }  // namespace NFqRun

5048232759d634aec1a89119f2c00a878fda2953
ivanmorozov333 <ivanmorozov@hotmail.com>
2025-02-18 10:17:30 +0300

    fix build for tests

--- ydb/core/formats/arrow/program/assign_internal.h
+++ ydb/core/formats/arrow/program/assign_internal.h
@@ -25,7 +25,7 @@ private:
 
 public:
     static TConclusion<std::shared_ptr<TCalculationProcessor>> Build(std::vector<TColumnChainInfo>&& input, const TColumnChainInfo& output, 
-        const std::shared_ptr<IStepFunction>& function, const std::shared_ptr<IKernelLogic>& kernelLogic);
+        const std::shared_ptr<IStepFunction>& function, const std::shared_ptr<IKernelLogic>& kernelLogic = nullptr);
 };
 
 }   // namespace NKikimr::NArrow::NSSA

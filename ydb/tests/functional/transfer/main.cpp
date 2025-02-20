75017ec2f0c5fc159ba5e1e293e9c0f3568968b5
Nikolay Shestakov <tesseract@ydb.tech>
2025-02-20 12:33:18 +0000

    refactoring

--- ydb/tests/functional/transfer/main.cpp
+++ ydb/tests/functional/transfer/main.cpp
@@ -15,25 +15,67 @@ using namespace NYdb::NTopic;
 
 namespace {
 
-std::pair<ui64, Ydb::ResultSet> DoRead(TSession& s, const TString& table) {
-    auto res = s.ExecuteQuery(
-        Sprintf("SELECT Key, Message FROM `/local/%s`  ORDER BY Key", table.data()),
-            TTxControl::NoTx()).GetValueSync();
-    UNIT_ASSERT_C(res.IsSuccess(), res.GetIssues().ToString());
+volatile size_t TestCaseCounter = 0;
 
-    const auto proto = NYdb::TProtoAccessor::GetProto(res.GetResultSet(0));
+struct IChecker {
+    virtual void Assert(const TString& msg, const ::Ydb::Value& value) = 0;
+    virtual ~IChecker() = default;
+};
 
-    return {proto.rowsSize(), proto};
+template<typename T>
+struct Checker : public IChecker {
+    Checker(T&& expected)
+        : Expected(std::move(expected))
+    {}
+
+    void Assert(const TString& msg, const ::Ydb::Value& value) override {
+        UNIT_ASSERT_VALUES_EQUAL_C(Get(value), Expected, msg);
+    }
+
+    T Get(const ::Ydb::Value& value);
+
+    T Expected;
+};
+
+template<>
+ui64 Checker<ui64>::Get(const ::Ydb::Value& value) {
+    return value.uint64_value();
 }
 
-} // namespace
+template<>
+TString Checker<TString>::Get(const ::Ydb::Value& value) {
+    return value.text_value();
+}
 
-Y_UNIT_TEST_SUITE(Transfer)
-{
-    void Main(const TString& transferName, const TString& tableName, const TString& tableDDL, const TString& topicName)
-    {
+template<typename T>
+std::pair<TString, std::shared_ptr<IChecker>> _C(TString&& name, T&& expected) {
+    return {
+        std::move(name),
+        std::make_shared<Checker<T>>(std::move(expected))
+    };
+}
+
+struct TConfig {
+    const char* TableDDL;
+    const char* Lambda;
+    const char* Message;
+    TVector<std::pair<TString, std::shared_ptr<IChecker>>> Columns;
+};
+
+
+struct MainTestCase {
+
+    MainTestCase(TConfig&& config)
+        : Config(std::move(config)) {
+        size_t id = TestCaseCounter++;
+
+        TopicName = TStringBuilder() << "Topic_" << id;
+        TableName = TStringBuilder() << "Table_" << id;
+        TransferName = TStringBuilder() << "Transfer_" << id;
+    }
+
+    void Run() {
         TString connectionString = GetEnv("YDB_ENDPOINT") + "/?database=" + GetEnv("YDB_DATABASE");
-        Cerr << ">>>>> connectionString = " << connectionString << Endl << Flush;
 
         auto config = TDriverConfig(connectionString);
         auto driver = TDriver(config);
@@ -42,6 +84,7 @@ Y_UNIT_TEST_SUITE(Transfer)
         auto topicClient = TTopicClient(driver);
 
         {
+            auto tableDDL = Sprintf(Config.TableDDL, TableName.data());
             auto res = session.ExecuteQuery(tableDDL, TTxControl::NoTx()).GetValueSync();
             UNIT_ASSERT_C(res.IsSuccess(), res.GetIssues().ToString());
         }
@@ -49,20 +92,13 @@ Y_UNIT_TEST_SUITE(Transfer)
         {
             auto res = session.ExecuteQuery(Sprintf(R"(
                 CREATE TOPIC `%s`;
-            )", topicName.data()), TTxControl::NoTx()).GetValueSync();
+            )", TopicName.data()), TTxControl::NoTx()).GetValueSync();
             UNIT_ASSERT_C(res.IsSuccess(), res.GetIssues().ToString());
         }
 
         {
             auto res = session.ExecuteQuery(Sprintf(R"(
-                $l = ($x) -> {
-                    return [
-                        <|
-                            Key:CAST($x._offset AS Uint64),
-                            Message:CAST($x._data AS Utf8)
-                        |>
-                    ];
-                };
+                %s;
 
                 CREATE TRANSFER `%s`
                 FROM `%s` TO `%s` USING $l
@@ -70,28 +106,31 @@ Y_UNIT_TEST_SUITE(Transfer)
                     CONNECTION_STRING = 'grpc://%s'
                     -- , TOKEN = 'user@builtin'
                 );
-            )", transferName.data(), topicName.data(), tableName.data(), connectionString.data()), TTxControl::NoTx()).GetValueSync();
+            )", Config.Lambda, TransferName.data(), TopicName.data(), TableName.data(), connectionString.data()), TTxControl::NoTx()).GetValueSync();
             UNIT_ASSERT_C(res.IsSuccess(), res.GetIssues().ToString());
         }
 
         {
             TWriteSessionSettings writeSettings;
-            writeSettings.Path(topicName);
+            writeSettings.Path(TopicName);
             writeSettings.DeduplicationEnabled(false);
             auto writeSession = topicClient.CreateSimpleBlockingWriteSession(writeSettings);
 
-            UNIT_ASSERT(writeSession->Write("message-1"));
+            UNIT_ASSERT(writeSession->Write(Config.Message));
             writeSession->Close(TDuration::Seconds(1));
         }
 
         {
             for (size_t attempt = 20; attempt--; ) {
-                auto res = DoRead(session, tableName);
+                auto res = DoRead(session);
                 Cerr << "Attempt=" << attempt << " count=" << res.first << Endl << Flush;
                 if (res.first == 1) {
                     const Ydb::ResultSet& proto = res.second;
-                    UNIT_ASSERT_VALUES_EQUAL(proto.rows(0).items(0).uint64_value(), 0);
-                    UNIT_ASSERT_VALUES_EQUAL(proto.rows(0).items(1).text_value(), "message-1");
+                    for (size_t i = 0; i < Config.Columns.size(); ++i) {
+                        auto& c = Config.Columns[i];
+                        TString msg = TStringBuilder() << "Column '" << c.first << "': ";
+                        c.second->Assert(msg, proto.rows(0).items(i));
+                    }
 
                     break;
                 }
@@ -102,30 +141,151 @@ Y_UNIT_TEST_SUITE(Transfer)
         }
     }
 
-    Y_UNIT_TEST(Main_ColumnTable)
-    {
-        Main("ColumnTransfer1", "TargetColumnTable1", R"(
-            CREATE TABLE `/local/TargetColumnTable1` (
-                Key Uint64 NOT NULL,
-                Message Utf8 NOT NULL,
-                PRIMARY KEY (Key)
-            )  WITH (
-                STORE = COLUMN
-            );
-        )", "SourceTopic1");
+
+    std::pair<ui64, Ydb::ResultSet> DoRead(TSession& s) {
+        TStringBuilder columns;
+        for (size_t i = 0; i < Config.Columns.size(); ++i) {
+            if (i) {
+                columns << ", ";
+            }
+            columns << Config.Columns[i].first;
+        }
+
+
+        auto res = s.ExecuteQuery(
+            Sprintf("SELECT %s FROM `/local/%s`", columns.data(), TableName.data()),
+                TTxControl::NoTx()).GetValueSync();
+        UNIT_ASSERT_C(res.IsSuccess(), res.GetIssues().ToString());
+    
+        const auto proto = NYdb::TProtoAccessor::GetProto(res.GetResultSet(0));
+        return {proto.rowsSize(), proto};
     }
 
-    Y_UNIT_TEST(Main_ColumnTable_KeyColumnOrder)
+    TConfig Config;
+
+    TString TopicName;
+    TString TableName;
+    TString TransferName;
+
+    std::vector<std::string> ColumnNames;
+};
+
+
+} // namespace
+
+Y_UNIT_TEST_SUITE(Transfer)
+{
+    Y_UNIT_TEST(Main_ColumnTable_KeyColumnFirst)
     {
-        Main("ColumnTransfer2", "TargetColumnTable2", R"(
-            CREATE TABLE `/local/TargetColumnTable2` (
-                Message Utf8 NOT NULL,
-                Key Uint64 NOT NULL,
-                PRIMARY KEY (Key)
-            )  WITH (
-                STORE = COLUMN
-            );
-        )", "SourceTopic2");
+        MainTestCase({
+            .TableDDL = R"(
+                CREATE TABLE `%s` (
+                    Key Uint64 NOT NULL,
+                    Message Utf8 NOT NULL,
+                    PRIMARY KEY (Key)
+                )  WITH (
+                    STORE = COLUMN
+                );
+            )",
+
+            .Lambda = R"(
+                $l = ($x) -> {
+                    return [
+                        <|
+                            Key:CAST($x._offset AS Uint64),
+                            Message:CAST($x._data AS Utf8)
+                        |>
+                    ];
+                };
+            )",
+
+            .Message = "Message-1",
+
+            .Columns = {
+                _C("Key", ui64(0)),
+                _C("Message", TString("Message-1")),
+            }
+        }).Run();
     }
+
+    Y_UNIT_TEST(Main_ColumnTable_KeyColumnLast)
+    {
+        MainTestCase({
+            .TableDDL = R"(
+                CREATE TABLE `%s` (
+                    Message Utf8 NOT NULL,
+                    Key Uint64 NOT NULL,
+                    PRIMARY KEY (Key)
+                )  WITH (
+                    STORE = COLUMN
+                );
+            )",
+
+            .Lambda = R"(
+                $l = ($x) -> {
+                    return [
+                        <|
+                            Key:CAST($x._offset AS Uint64),
+                            Message:CAST($x._data AS Utf8)
+                        |>
+                    ];
+                };
+            )",
+
+            .Message = "Message-1",
+
+            .Columns = {
+                _C("Key", ui64(0)),
+                _C("Message", TString("Message-1")),
+            }
+        }).Run();
+    }
+
+    Y_UNIT_TEST(Main_ColumnTable_JsonMessage)
+    {
+        MainTestCase({
+            .TableDDL = R"(
+                CREATE TABLE `%s` (
+                    Id UInt64 NOT NULL,
+                    FirstName String,
+                    LastName String,
+                    Salary Uint64,
+                    PRIMARY KEY (Id)
+                )  WITH (
+                    STORE = COLUMN
+                );
+            )",
+
+            .Lambda = R"(
+                $l = ($x) -> {
+                    $input = CAST($x._data AS JSON);
+
+                    return [
+                        <|
+                            Id:        Yson::ConvertToUint64($input.id),
+                            FirstName: Yson::ConvertToString($input.first_name),
+                            LastName:  Yson::ConvertToString($input.last_name),
+                            Salary:    CAST(Yson::ConvertToString($input.salary) AS UInt64)
+                        |>
+                    ];
+                };
+            )",
+
+            .Message = R"({
+                "id": 1,
+                "first_name": "Vasya",
+                "last_name": "Pupkin",
+                "salary": "123"
+            })",
+
+            .Columns = {
+                _C("Id", ui64(1)),
+                _C("FirstName", TString("Vasya")),
+                _C("LastName", TString("Pupkin")),
+                _C("Salary", ui64(123)),
+            }
+        }).Run();
+    }
+
 }
 

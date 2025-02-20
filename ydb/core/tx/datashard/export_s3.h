58ae84e3bb4d5268ef42b6e2990620268996f812
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-02-20 15:18:48 +0100

    Refactor S3Buffer

--- ydb/core/tx/datashard/export_s3.h
+++ ydb/core/tx/datashard/export_s3.h
@@ -29,15 +29,36 @@ public:
         const ui64 maxBytes = scanSettings.GetBytesBatchSize();
         const ui64 minBytes = Task.GetS3Settings().GetLimits().GetMinWriteBatchSize();
 
+        TS3ExportBufferSettings bufferSettings;
+        bufferSettings
+            .WithColumns(Columns)
+            .WithMaxRows(maxRows)
+            .WithMaxBytes(maxBytes);
+        if (Task.GetEnableChecksums()) {
+            bufferSettings
+                .WithChecksum(
+                    TS3ExportBufferSettings::TChecksumSettings()
+                        .WithChecksumType(TS3ExportBufferSettings::TChecksumSettings::EChecksumType::Sha256)
+                );
+        }
+
         switch (CodecFromTask(Task)) {
         case ECompressionCodec::None:
-            return CreateS3ExportBufferRaw(Columns, maxRows, maxBytes, Task.GetEnableChecksums());
+            break;
         case ECompressionCodec::Zstd:
-            return CreateS3ExportBufferZstd(Task.GetCompression().GetLevel(), Columns, maxRows,
-                maxBytes, minBytes, Task.GetEnableChecksums());
+            bufferSettings
+                .WithMinBytes(minBytes)
+                .WithCompression(
+                    TS3ExportBufferSettings::TCompressionSettings()
+                        .WithAlgorithm(TS3ExportBufferSettings::TCompressionSettings::EAlgorithm::Zstd)
+                        .WithCompressionLevel(Task.GetCompression().GetLevel())
+                );
+            break;
         case ECompressionCodec::Invalid:
             Y_ABORT("unreachable");
         }
+
+        return CreateS3ExportBuffer(std::move(bufferSettings));
     }
 
     void Shutdown() const override {}

575f1bc6e89729b326a5790fefa27c7354748b15
mregrock <mregrock@ydb.tech>
2025-02-20 19:28:05 +0300

    Add config init command

--- ydb/public/lib/ydb_cli/commands/ydb_dynamic_config.h
+++ ydb/public/lib/ydb_cli/commands/ydb_dynamic_config.h
@@ -133,4 +133,14 @@ private:
     bool AllowEmptyDatabase = false;
 };
 
+class TCommandConfigInit : public TYdbCommand {
+public:
+    TCommandConfigInit();
+    void Config(TConfig& config) override;
+    int Run(TConfig& config) override;
+private:
+    TString ConfigYamlPath;
+    TString ConfigDirPath;
+    TString SeedNodeEndpoint;
+};
 } // namespace NYdb::NConsoleClient::NDynamicConfig

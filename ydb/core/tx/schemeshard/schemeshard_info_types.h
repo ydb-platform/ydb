ffd888a068f1b6006fcc37013dc3e98a57d96e59
st-shchetinin <st-shchetinin@ydb.tech>
2025-02-21 13:27:36 +0300

    StreamImplPathId

--- ydb/core/tx/schemeshard/schemeshard_info_types.h
+++ ydb/core/tx/schemeshard/schemeshard_info_types.h
@@ -2869,7 +2869,7 @@ struct TImportInfo: public TSimpleRefCount<TImportInfo> {
         int NextChangefeedIdx = 0;
         TString Issue;
         int ViewCreationRetries = 0;
-        NKikimr::TPathId StreamImplPath;
+        NKikimr::TPathId StreamImplPathId;
 
         TItem() = default;
 

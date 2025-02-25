dfe1a0fb2ffdaef8bb69c45910301330b20ba6fc
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-02-25 16:18:54 +0300

    prevent shared_ptr loop

--- ydb/core/tx/columnshard/engines/changes/abstract/abstract.h
+++ ydb/core/tx/columnshard/engines/changes/abstract/abstract.h
@@ -299,7 +299,7 @@ public:
         ActivityFlag = flag;
     }
 
-    std::shared_ptr<TDataAccessorsRequest> ExtractDataAccessorsRequest() const {
+    std::shared_ptr<TDataAccessorsRequest> ExtractDataAccessorsRequest() {
         AFL_VERIFY(!!PortionsToAccess);
         return std::move(PortionsToAccess);
     }

3762c50496f4c4796be7e1cd7be17fb5963d6097
Nikita Vasilev <ns-vasilev@ydb.tech>
2025-02-26 13:06:08 +0300

    fix

--- ydb/core/kqp/ut/tx/kqp_locks_ut.cpp
+++ ydb/core/kqp/ut/tx/kqp_locks_ut.cpp
@@ -304,11 +304,7 @@ Y_UNIT_TEST_SUITE(KqpLocks) {
             INSERT INTO `/Root/ColumnShard` (Col1, Col2, Col3) VALUES (2u, 1, "test");
         )sql", NYdb::NQuery::TTxControl::Tx(tx->GetId()).CommitTx()).ExtractValueSync();
         UNIT_ASSERT_C(!result.IsSuccess(), result.GetIssues().ToString());
-        if (useSink) {
-            UNIT_ASSERT_STRING_CONTAINS_C(result.GetIssues().ToString(), "Transaction locks invalidated. Tables: `/Root/DataShard`", result.GetIssues().ToString());
-        } else {
-            UNIT_ASSERT_STRING_CONTAINS_C(result.GetIssues().ToString(), "Transaction locks invalidated. Table: `/Root/DataShard`", result.GetIssues().ToString());
-        }
+        UNIT_ASSERT_STRING_CONTAINS_C(result.GetIssues().ToString(), "Transaction locks invalidated. Table: `/Root/DataShard`", result.GetIssues().ToString());
     }
 }
 

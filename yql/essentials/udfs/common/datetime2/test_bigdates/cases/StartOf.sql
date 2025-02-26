36218ce063703541dde0abc8a4b7a6c7e73259ff
imunkin <imunkin@yandex-team.com>
2025-02-26 16:34:50 +0300

    YQL-18303: Introduce Split and MakeTz* overloads commit_hash:16a38d1b1de0cc97c5cbf97176331ea6691e23be
    (cherry picked from commit 6b6e4dc3a6fe1ec371f197ce2f472f69399dfb69)

--- yql/essentials/udfs/common/datetime2/test_bigdates/cases/StartOf.sql
+++ yql/essentials/udfs/common/datetime2/test_bigdates/cases/StartOf.sql
@@ -1,16 +1,16 @@
 /* syntax version 1 */
 $check = ($arg) -> {
     return <|
-        soyear:    DateTime::MakeTimestamp64(DateTime::StartOfYear($arg)),
-        soquarter: DateTime::MakeTimestamp64(DateTime::StartOfQuarter($arg)),
-        somonth:   DateTime::MakeTimestamp64(DateTime::StartOfMonth($arg)),
-        soweek:    DateTime::MakeTimestamp64(DateTime::StartOfWeek($arg)),
-        soday:     DateTime::MakeTimestamp64(DateTime::StartOfDay($arg)),
-        sopt13h:   DateTime::MakeTimestamp64(DateTime::StartOf($arg, Interval("PT13H"))),
-        sopt4h:    DateTime::MakeTimestamp64(DateTime::StartOf($arg, Interval("PT4H"))),
-        sopt15m:   DateTime::MakeTimestamp64(DateTime::StartOf($arg, Interval("PT15M"))),
-        sopt20s:   DateTime::MakeTimestamp64(DateTime::StartOf($arg, Interval("PT20S"))),
-        sopt7s:    DateTime::MakeTimestamp64(DateTime::StartOf($arg, Interval("PT7S"))),
+        soyear:    DateTime::StartOfYear($arg),
+        soquarter: DateTime::StartOfQuarter($arg),
+        somonth:   DateTime::StartOfMonth($arg),
+        soweek:    DateTime::StartOfWeek($arg),
+        soday:     DateTime::StartOfDay($arg),
+        sopt13h:   DateTime::StartOf($arg, Interval("PT13H")),
+        sopt4h:    DateTime::StartOf($arg, Interval("PT4H")),
+        sopt15m:   DateTime::StartOf($arg, Interval("PT15M")),
+        sopt20s:   DateTime::StartOf($arg, Interval("PT20S")),
+        sopt7s:    DateTime::StartOf($arg, Interval("PT7S")),
         timeofday: DateTime::TimeOfDay($arg),
     |>
 };
@@ -23,6 +23,6 @@ $typeDispatcher = ($row) -> {
     |>;
 };
 
-$input = SELECT CAST(ftimestamp64 as Timestamp64) as tm FROM Input;
+$input = SELECT CAST(ftztimestamp64 as TzTimestamp64) as tm FROM Input;
 
 PROCESS $input USING $typeDispatcher(TableRow());

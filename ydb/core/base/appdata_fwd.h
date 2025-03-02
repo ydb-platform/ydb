b9b73a004de4eb9f7caa827372b32f0ed22fa104
Innokentii Mokin <innokentii@ydb.tech>
2025-03-02 17:00:47 +0000

    add token-verification on bootstrap

--- ydb/core/base/appdata_fwd.h
+++ ydb/core/base/appdata_fwd.h
@@ -250,10 +250,13 @@ struct TAppData {
     bool EnableMvccSnapshotWithLegacyDomainRoot = false;
     bool UsePartitionStatsCollectorForTests = false;
     bool DisableCdcAutoSwitchingToReadyStateForTests = false;
+
     TVector<TString> AdministrationAllowedSIDs; // use IsAdministrator method to check whether a user or a group is allowed to perform administrative tasks
+    TVector<TString> RegisterDynamicNodeAllowedSIDs;
+    TVector<TString> BootstrapAllowedSIDs;
     TVector<TString> DefaultUserSIDs;
     TString AllAuthenticatedUsers = "all-users@well-known";
-    TVector<TString> RegisterDynamicNodeAllowedSIDs;
+
     TString TenantName;
     TString NodeName;
 

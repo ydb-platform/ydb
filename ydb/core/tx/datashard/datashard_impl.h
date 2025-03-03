63b1f5af0c2c2fcb87ccaf5c4ca6b5633d4cad88
Kamil Khamitov <kkhamitov@yandex-team.ru>
2025-03-03 20:56:37 +0300

    (-) add rest of the files

--- ydb/core/tx/datashard/datashard_impl.h
+++ ydb/core/tx/datashard/datashard_impl.h
@@ -1437,7 +1437,7 @@ class TDataShard
     void OnStopGuardStarting(const TActorContext &ctx);
     void OnStopGuardComplete(const TActorContext &ctx);
     void OnTabletDead(TEvTablet::TEvTabletDead::TPtr &ev, const TActorContext &ctx) override;
-    void IcbRegister();
+    void InitControls();
     bool ReadOnlyLeaseEnabled() override;
     TDuration ReadOnlyLeaseDuration() override;
     void OnActivateExecutor(const TActorContext &ctx) override;
@@ -2804,7 +2804,7 @@ private:
     bool InMemoryVarsRestored = false;
     bool InMemoryVarsFrozen = false;
 
-    bool IcbRegistered = false;
+    bool ControlsInited = false;
 
     TControlWrapper DisableByKeyFilter;
     TControlWrapper MaxTxInFly;

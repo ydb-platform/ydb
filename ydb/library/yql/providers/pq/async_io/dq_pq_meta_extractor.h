6f1f5b539459fdac6d7322fa5e826fdd99bdd7f4
Yuriy Kaminskiy <yumkam7@ydb.tech>
2025-03-04 14:55:53 +0300

    dq_pq_meta_extractor: switch to federated topic api

--- ydb/library/yql/providers/pq/async_io/dq_pq_meta_extractor.h
+++ ydb/library/yql/providers/pq/async_io/dq_pq_meta_extractor.h
@@ -5,13 +5,13 @@
 #include <yql/essentials/public/udf/udf_data_type.h>
 #include <yql/essentials/public/udf/udf_value.h>
 
-#include <ydb-cpp-sdk/client/topic/client.h>
+#include <ydb-cpp-sdk/client/federated_topic/federated_topic.h>
 
 #include <util/generic/string.h>
 
 namespace NYql::NDq {
     struct TPqMetaExtractor {
-        using TPqMetaExtractorLambda = std::function<std::pair<NYql::NUdf::TUnboxedValuePod, i64>(const NYdb::NTopic::TReadSessionEvent::TDataReceivedEvent::TMessage&)>;
+        using TPqMetaExtractorLambda = std::function<std::pair<NYql::NUdf::TUnboxedValuePod, i64>(const NYdb::NFederatedTopic::TReadSessionEvent::TDataReceivedEvent::TMessage&)>;
 
     public:
         TPqMetaExtractor();

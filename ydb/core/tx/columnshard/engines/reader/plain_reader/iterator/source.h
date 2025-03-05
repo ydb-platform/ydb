45320ac5fa3fa13166f5ac338a7618888468d20e
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-03-05 16:03:35 +0300

    wip

--- ydb/core/tx/columnshard/engines/reader/plain_reader/iterator/source.h
+++ ydb/core/tx/columnshard/engines/reader/plain_reader/iterator/source.h
@@ -294,6 +294,13 @@ public:
         return Portion;
     }
 
+    virtual const NArrow::TReplaceKey& GetMinPK() const override {
+        return Portion->IndexKeyStart();
+    }
+    virtual const NArrow::TReplaceKey& GetMaxPK() const override {
+        return Portion->IndexKeyEnd();
+    }
+
     TPortionDataSource(const ui32 sourceIdx, const std::shared_ptr<TPortionInfo>& portion, const std::shared_ptr<TSpecialReadContext>& context)
         : TBase(portion->GetPortionId(), sourceIdx, context, portion->IndexKeyStart(), portion->IndexKeyEnd(),
               portion->RecordSnapshotMin(TSnapshot::Zero()), portion->RecordSnapshotMax(TSnapshot::Zero()), portion->GetRecordsCount(),
@@ -395,6 +402,13 @@ public:
 
     const TCommittedBlob& GetCommitted() const {
         return CommittedBlob;
+    }
+
+    virtual const NArrow::TReplaceKey& GetMinPK() const override {
+        return CommittedBlob.GetFirst();
+    }
+    virtual const NArrow::TReplaceKey& GetMaxPK() const override {
+        return CommittedBlob.GetLast();
     }
 
     TCommittedDataSource(const ui32 sourceIdx, const TCommittedBlob& committed, const std::shared_ptr<TSpecialReadContext>& context)

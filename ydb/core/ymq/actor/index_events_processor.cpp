a4cb86979f1262160635b6e2eb6763b6d79624dc
Sergey Skovorodkin <qyryq@ydb.tech>
2025-03-06 15:07:40 +0000

    YMQ: Update format of labels in search events

--- ydb/core/ymq/actor/index_events_processor.cpp
+++ ydb/core/ymq/actor/index_events_processor.cpp
@@ -362,20 +362,32 @@ void TSearchEventsProcessor::SaveQueueEvent(
         writer.Write("resource_id", queueName);
         writer.Write("name", event.CustomName);
         writer.Write("service", "message-queue");
-        if (event.Type == EQueueEventType::Deleted)
+        if (event.Type == EQueueEventType::Deleted) {
             writer.Write("deleted",  tsIsoString);
-        if (event.Type == EQueueEventType::Existed)
+        }
+        if (event.Type == EQueueEventType::Existed) {
             writer.Write("reindex_timestamp", nowIsoString);
+        }
         writer.Write("permission", "ymq.queues.list");
         writer.Write("cloud_id", event.CloudId);
         writer.Write("folder_id", event.FolderId);
-        writer.Write("labels", event.Labels);
-        writer.OpenArray("resource_path");
-        writer.OpenMap();
-        writer.Write("resource_type", "resource-manager.folder");
-        writer.Write("resource_id", event.FolderId);
-        writer.CloseMap();
-        writer.CloseArray(); // resource_path
+
+        {
+            writer.OpenArray("resource_path");
+            {
+                writer.OpenMap();
+                writer.Write("resource_type", "resource-manager.folder");
+                writer.Write("resource_id", event.FolderId);
+                writer.CloseMap();
+            }
+            writer.CloseArray();
+        }
+
+        if (!event.Labels.empty() && event.Labels != "{}") {
+            writer.OpenMap("attributes");
+            writer.UnsafeWrite("labels", event.Labels);
+            writer.CloseMap();
+        }
     }
     writer.CloseMap();
     writer.Flush();

b499199ffea89d7dc0ab0870e3d6fc61635e636c
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-03-10 17:09:47 +0300

    many local fixes

--- ydb/core/tx/columnshard/engines/reader/duplicates/manager.cpp
+++ ydb/core/tx/columnshard/engines/reader/duplicates/manager.cpp
@@ -3,6 +3,8 @@
 #include <ydb/core/tx/columnshard/engines/reader/duplicates/merge.h>
 #include <ydb/core/tx/conveyor/usage/service.h>
 
+#include <bit>
+
 namespace NKikimr::NOlap::NReader {
 
 void TRangeIndex::AddRange(const ui32 l, const ui32 r, const ui64 id) {
@@ -30,6 +32,17 @@ void TIntervalCounter::Inc(const ui32 l, const ui32 r) {
 }
 
 TIntervalCounter::TIntervalCounter(const std::vector<std::pair<ui32, ui32>>& intervals) {
+    // ui32 maxValue = 0;
+    // for (const auto& [l, r] : intervals) {
+    //     AFL_VERIFY(l <= r);
+    //     if (r > maxValue) {
+    //         maxValue = r;
+    //     }
+    // }
+    // const ui32 size = std::bit_ceil(maxValue);
+    // Count.resize(size * 2 - 1);
+    // PropagateDelta.resize(size * 2 - 1);
+
     for (const auto& [l, r] : intervals) {
         Inc(l, r);
     }
@@ -67,9 +80,13 @@ void TDuplicateFilterConstructor::TSourceFilterConstructor::Finish() && {
     for (ui64 i = 0; i < IntervalFilters.size(); ++i) {
         result.Append(*TValidator::CheckNotNull(IntervalFilters[i]));
     }
-    AFL_VERIFY(result.GetRecordsCountVerified() == Source->GetRecordsCount())("filter", result.GetRecordsCountVerified())(
-                                                       "source", Source->GetRecordsCount());
+    AFL_VERIFY(result.GetRecordsCountVerified() == Source->GetStageData().GetTable()->GetRecordsCountVerified())(
+                                                       "filter", result.GetRecordsCountVerified())("source", Source->GetRecordsCount());
     Subscriber->OnFilterReady(result);
+}
+
+void TDuplicateFilterConstructor::TSourceFilterConstructor::AbortConstruction(const TString& reason) && {
+    Subscriber->OnFailure(reason);
 }
 
 TDuplicateFilterConstructor::TSourceIntervals::TSourceIntervals(const std::vector<std::shared_ptr<TPortionInfo>>& portions) {
@@ -78,12 +95,12 @@ TDuplicateFilterConstructor::TSourceIntervals::TSourceIntervals(const std::vecto
         using TReplaceKeyView = const NArrow::TReplaceKey*;
         YDB_READONLY_DEF(bool, IsLast);
         YDB_READONLY_DEF(TReplaceKeyView, PK);
-        YDB_READONLY_DEF(ui64, PortionId);
+        YDB_READONLY_DEF(ui64, SourceId);
 
-        TBorderView(const bool isLast, const NArrow::TReplaceKey* const pk, const ui64 portionId)
+        TBorderView(const bool isLast, const NArrow::TReplaceKey* const pk, const ui64 sourceId)
             : IsLast(isLast)
             , PK(pk)
-            , PortionId(portionId) {
+            , SourceId(sourceId) {
         }
 
     public:
@@ -107,77 +124,90 @@ TDuplicateFilterConstructor::TSourceIntervals::TSourceIntervals(const std::vecto
     }
     std::sort(borders.begin(), borders.end());
 
-    THashMap<ui64, ui32> firstByPortionId;
+    THashMap<ui64, ui32> firstBySourceId;
     for (const auto& border : borders) {
         if (border.GetIsLast()) {
             if (IntervalBorders.empty() || IntervalBorders.back() != *border.GetPK()) {
                 IntervalBorders.emplace_back(*border.GetPK());
             }
             const TIntervalsRange sourceRange(
-                *TValidator::CheckNotNull(firstByPortionId.FindPtr(border.GetPortionId())), IntervalBorders.size() - 1);
-            AFL_VERIFY(SourceRanges.emplace(border.GetPortionId(), sourceRange).second);
+                *TValidator::CheckNotNull(firstBySourceId.FindPtr(border.GetSourceId())), IntervalBorders.size() - 1);
+            AFL_VERIFY(SourceRanges.emplace(border.GetSourceId(), sourceRange).second);
         } else {
-            AFL_VERIFY(firstByPortionId.emplace(border.GetPortionId(), IntervalBorders.size()).second);
+            AFL_VERIFY(firstBySourceId.emplace(border.GetSourceId(), IntervalBorders.size()).second);
         }
     }
     AFL_VERIFY(SourceRanges.size() == portions.size());
 }
 
 void TDuplicateFilterConstructor::Handle(const TEvRequestFilter::TPtr& ev) {
+    // TODO: add counter: max volume of redundant memory for merges
     const auto& source = ev->Get()->GetSource();
-    Y_UNUSED(Intervals.GetRangeVerified(source->GetSourceId()));
+    TIntervalsRange range = Intervals.GetRangeVerified(source->GetSourceId());
     AFL_VERIFY(AvailableSources.emplace(source->GetSourceId(), TSourceFilterConstructor(source, ev->Get()->GetSubscriber(), Intervals)).second);
-
-    TIntervalsRange range = Intervals.GetRangeVerified(source->GetSourceIdx());
-    AvailableSourcesCount.AddRange(range.GetFirstIdx(), range.GetLastIdx(), source->GetSourceIdx());
+    AvailableSourcesCount.AddRange(range.GetFirstIdx(), range.GetLastIdx(), source->GetSourceId());
     std::vector<ui32> readyIntervals = AwaitedSourcesCount.DecAndGetZeros(range.GetFirstIdx(), range.GetLastIdx());
 
     for (const ui32 intervalIdx : readyIntervals) {
-        std::vector<std::shared_ptr<NCommon::IDataSource>> sources;
-        for (const ui64 portionId : AvailableSourcesCount.FindIntersections(intervalIdx)) {
-            sources.emplace_back(TValidator::CheckNotNull(AvailableSources.FindPtr(portionId))->GetSource());
-        }
-        AFL_VERIFY(sources.size());
-        const std::shared_ptr<NCommon::TSpecialReadContext> readContext = sources.front()->GetContext();
-        const std::shared_ptr<TBuildDuplicateFilters> task = std::make_shared<TBuildDuplicateFilters>(
-            Intervals.GetLeftExlusiveBorder(intervalIdx), Intervals.GetRightInclusiveBorder(intervalIdx),
-            readContext->GetReadMetadata()->GetReplaceKey(), IIndexInfo::GetSnapshotColumnNames());
-        for (const auto& source : sources) {
-            // TODO: why table has applied filter and not applied filter?
-            // TODO: make slice
-            task->AddSource({} /*TODO*/, source->GetStageData().GetAppliedFilter(),
+        auto sourceIds = AvailableSourcesCount.FindIntersections(intervalIdx);
+        AFL_VERIFY(sourceIds.size());
+        const std::shared_ptr<NCommon::TSpecialReadContext> readContext =
+            TValidator::CheckNotNull(AvailableSources.FindPtr(sourceIds.front()))->GetSource()->GetContext();
+        const std::shared_ptr<TBuildDuplicateFilters> task =
+            std::make_shared<TBuildDuplicateFilters>(readContext->GetReadMetadata()->GetReplaceKey(), IIndexInfo::GetSnapshotColumnNames());
+        for (const ui64 sourceId : sourceIds) {
+            const TSourceFilterConstructor* constructionInfo = AvailableSources.FindPtr(sourceId);
+            AFL_VERIFY(constructionInfo)("source", sourceId);
+            const std::shared_ptr<NCommon::IDataSource>& source = constructionInfo->GetSource();
+            // TODO: why table has applied filter and not applied filter? which one to use?
+            const auto intervalRange = constructionInfo->GetIntervalRange(intervalIdx);
+            const auto slice =
+                std::make_shared<NArrow::TGeneralContainer>(source->GetStageData()
+                                                                .ToGeneralContainer(source->GetContext()->GetCommonContext()->GetResolver())
+                                                                ->Slice(intervalRange.GetBegin(), intervalRange.Size()));
+            task->AddSource(slice, source->GetStageData().GetNotAppliedFilter(),
                 std::make_shared<TInternalFilterSubscriber>(intervalIdx, source->GetSourceId(), SelfId()));
         }
         NConveyor::TScanServiceOperator::SendTaskToExecute(task, readContext->GetCommonContext()->GetConveyorProcessId());
-    }
-
-    if (AwaitedSourcesCount.IsAllZeros()) {
-        PassAway();
     }
 }
 
 void TDuplicateFilterConstructor::Handle(const TEvDuplicateFilterPartialResult::TPtr& ev) {
     if (ev->Get()->GetResult().IsFail()) {
-        // TODO: abort
+        AbortConstruction(ev->Get()->GetResult().GetErrorMessage());
         return;
     }
-    TSourceFilterConstructor* constructor = AvailableSources.FindPtr(ev->Get()->GetPortionId());
-    AFL_VERIFY(constructor)("portion", ev->Get()->GetPortionId());
+    TSourceFilterConstructor* constructor = AvailableSources.FindPtr(ev->Get()->GetSourceId());
+    AFL_VERIFY(constructor)("portion", ev->Get()->GetSourceId());
     // TODO: avoid copying filters
     constructor->SetFilter(ev->Get()->GetIntervalIdx(), ev->Get()->ExtractResult().DetachResult());
     if (constructor->IsReady()) {
         std::move(*constructor).Finish();
-        AFL_VERIFY(AvailableSources.erase(ev->Get()->GetPortionId()));
-        // TODO: if done, pass away
+        AFL_VERIFY(AvailableSources.erase(ev->Get()->GetSourceId()));
+        if (AvailableSources.empty() && AwaitedSourcesCount.IsAllZeros()) {
+            PassAway();
+        }
     }
 }
 
+void TDuplicateFilterConstructor::Handle(const NActors::TEvents::TEvPoison::TPtr&) {
+    AbortConstruction("aborted by actor system");
+}
+
+void TDuplicateFilterConstructor::AbortConstruction(const TString& reason) {
+    for (auto&& [_, source] : AvailableSources) {
+        std::move(source).AbortConstruction(reason);
+    }
+    PassAway();
+}
+
 TDuplicateFilterConstructor::TDuplicateFilterConstructor(const std::vector<std::shared_ptr<TPortionInfo>>& portions)
-    : Intervals(portions)
+    : TActor(&TDuplicateFilterConstructor::StateMain)
+    , Intervals(portions)
     , AwaitedSourcesCount([this]() {
         std::vector<std::pair<ui32, ui32>> intervals;
         for (const auto& [_, interval] : Intervals.GetSourceRanges()) {
-            intervals.emplace_back(interval);
+            intervals.emplace_back(interval.GetFirstIdx(), interval.GetLastIdx());
         }
         return intervals;
     }()) {

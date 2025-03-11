9c2399799318ff30bb2a646314e63045241295ee
Emil Garipov <emgariko@ydb.tech>
2025-03-11 11:52:20 +0000

    removed redundant include

--- ydb/core/tx/schemeshard/olap/columns/schema.cpp
+++ ydb/core/tx/schemeshard/olap/columns/schema.cpp
@@ -2,7 +2,6 @@
 #include <ydb/library/accessor/validator.h>
 #include <yql/essentials/minikql/mkql_type_ops.h>
 #include <ydb/core/scheme_types/scheme_type_registry.h>
-#include <ydb/core/tx/schemeshard/schemeshard_utils.h>
 
 namespace NKikimr::NSchemeShard {
 

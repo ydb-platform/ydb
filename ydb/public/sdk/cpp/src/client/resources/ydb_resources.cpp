77fe41d992730f56122e972c538328c92ddd7631
Bulat <bulat@ydb.tech>
2025-03-13 18:22:56 +0300

    [C++ SDK] Changed names for resources (#15691)

--- ydb/public/sdk/cpp/src/client/resources/ydb_resources.cpp
+++ ydb/public/sdk/cpp/src/client/resources/ydb_resources.cpp
@@ -29,7 +29,7 @@ const char* YDB_CLIENT_CAPABILITY_SESSION_BALANCER = "session-balancer";
 
 
 std::string GetSdkSemver() {
-    return NResource::Find("ydb_sdk_version_v3.txt");
+    return NResource::Find("ydb_sdk_version_dev.txt");
 }
 
 } // namespace NYdb

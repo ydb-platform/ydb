31901bb0c512ed61aa93f9aad69dd10c76fd30ea
Denis Prokofev <dennnis@ydb.tech>
2025-03-13 20:14:18 +0300

    add update

--- ydb/tests/DML_operations/test_DML.py
+++ ydb/tests/DML_operations/test_DML.py
@@ -20,22 +20,29 @@ ttl_types = {
 }
 
 class TestYdbDMLOperator(TestBase):
-    def create_insert(self, value: int, name_table: str, key: str, ttl: str): 
+    def create_insert(self, value: int, name: str, key: str, ttl: str): 
         insert_sql = f"""
                     INSERT INTO DML_{ttl} (
                     pk,
                     {", ".join(["pk_" + cleanup_type_name(type_name) for type_name in pk_types.keys()])},
-                    {cleanup_type_name(name_table)}
+                    {cleanup_type_name(name)}
                     )
                     VALUES
                     (
-                    {i},
+                    {value},
                     {", ".join([pk_types[type_name].format(value) for type_name in pk_types.keys()])},
                     {key.format(value)}
                     )
                     ;
                 """
         self.query(insert_sql)
+    
+    def update(self, value: int, changeable_name: str, type_name: str, key: str, ttl: str):
+        update_sql = f"""
+                    UPDATE DML_{ttl}
+                    SET {changeable_name} = {key.format(value)} where {type_name} = {key.format(value)}
+                """
+        self.query(update_sql)
 
     def test_DML_operator(self):
         for ttl in ttl_types.keys():
@@ -69,21 +76,51 @@ class TestYdbDMLOperator(TestBase):
             """
             self.query(index_sql)
             i = 0
+            
+            # insetr
             for type_name in pk_types.keys():
                 self.create_insert(i, f"col_{type_name}", pk_types[type_name], ttl)
+                i+=1
+                self.create_insert(i, f"col_index_{type_name}", pk_types[type_name], ttl)
                 i+=1
 
             for type_name in non_pk_types.keys():
                 self.create_insert(i, f"col_{type_name}", non_pk_types[type_name], ttl)
                 i+=1
-
-            for type_name in pk_types.keys():
-                self.create_insert(i, f"col_index_{type_name}", pk_types[type_name], ttl)
-                i+=1
-            
-            for type_name in non_pk_types.keys():
                 self.create_insert(i, f"col_index_{type_name}", non_pk_types[type_name], ttl)
                 i+=1
+
+            #check after insert
+            rows = self.query(f"SELECT * FROM DML_{ttl}")
+            assert len(rows) == i, f"Expected {i} row after insert, {len(rows)} row after insert"
+
+            count = 0
+            for type_name in pk_types.keys():
+                rows = self.query(f"SELECT * FROM DML_{ttl} WHERE col_{type_name}={pk_types[type_name].format(count)}")
+                assert len(rows) == 1, f"Expected one row after insert, faild in col_{type_name}"
+                count+=1
+                rows = self.query(f"SELECT * FROM DML_{ttl} WHERE col_index_{type_name}={pk_types[type_name].format(count)}")
+                assert len(rows) == 1, f"Expected one row after insert, faild in col_index_{type_name}"
+                count+=1
+
+            for type_name in non_pk_types.keys():
+                rows = self.query(f"SELECT * FROM DML_{ttl} WHERE col_{type_name}={non_pk_types[type_name].format(count)}")
+                assert len(rows) == 1, f"Expected one row after insert, faild in col_{type_name}"
+                count+=1
+                rows = self.query(f"SELECT * FROM DML_{ttl} WHERE col_index_{type_name}={non_pk_types[type_name].format(count)}")
+                assert len(rows) == 1, f"Expected one row after insert, faild in col_index_{type_name}"
+                count+=1
+
+            #update
+            for type_name in non_pk_types.keys():
+                for changeable_name in non_pk_types.keys():
+                    self.update(self, i, f"col_{changeable_name}", f"col_{type_name}", non_pk_types[type_name], ttl)
+                    self.update(self, i-1, f"col_index_{changeable_name}", f"col_index_{type_name}", non_pk_types[type_name], ttl)
+                for changeable_name in pk_types.keys():
+                    self.update(self, i, f"col_{changeable_name}", f"col_{type_name}", non_pk_types[type_name], ttl)
+                    self.update(self, i-1, f"col_index_{changeable_name}", f"col_index_{type_name}", non_pk_types[type_name], ttl)
+                i-=2
+
 
             
 

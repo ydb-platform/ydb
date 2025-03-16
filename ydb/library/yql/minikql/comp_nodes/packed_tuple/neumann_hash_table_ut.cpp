739b1a74fb4716b42806b80ba5094a9c983f5ecb
Vyacheslav Boev <slava.boev94@gmail.com>
2025-03-16 23:31:24 +0300

    Fix neumann test according to updated null bytes pack

--- ydb/library/yql/minikql/comp_nodes/packed_tuple/neumann_hash_table_ut.cpp
+++ ydb/library/yql/minikql/comp_nodes/packed_tuple/neumann_hash_table_ut.cpp
@@ -497,7 +497,7 @@ Y_UNIT_TEST(BenchNeumannHashTable_VarTuple){
         0x1, 0, 0, 0, 0, 0, 0, 0x10, // col2
         0x3, 0x61, 0x62, 0x63, 0, 0, 0, 0, 0, // vcol1
         0x3, 0x41, 0x42, 0x43, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // vcol2
-        0x3f, //NULL bitmap
+        0xff, //NULL bitmap
         0x1, 0, 0, 0x40, // col3
         0x1, 0, 0, 0, 0, 0, 0, 0x30, // col4
         // row2
@@ -506,7 +506,7 @@ Y_UNIT_TEST(BenchNeumannHashTable_VarTuple){
         0x2, 0, 0, 0, 0, 0, 0, 0x10, // col2
         0xff,  0, 0, 0, 0,  0xf, 0, 0, 0, // vcol1 [overflow offset, overflow size]
         0xf, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, // vcol2
-        0x3f, // NULL bitmap
+        0xff, // NULL bitmap
         0x2, 0, 0, 0x40, // col3
         0x2, 0, 0, 0, 0, 0, 0, 0x30, // col4
         // row3
@@ -515,7 +515,7 @@ Y_UNIT_TEST(BenchNeumannHashTable_VarTuple){
         0x3, 0, 0, 0, 0, 0, 0, 0x10, // col2
         0xff,  0xf, 0, 0, 0,  0xa, 0, 0, 0, // vcol1 [overflow offset, overflow size]
         0xa, 0x7a, 0x79, 0x78, 0x77, 0x76, 0x75, 0x74, 0x73, 0x70, 0x72,  0, 0, 0, 0, 0, // vcol2
-        0x3f, // NULL bitmap
+        0xff, // NULL bitmap
         0x3, 0, 0, 0x40, // col3
         0x3, 0, 0, 0, 0, 0, 0, 0x30, // col4
     };

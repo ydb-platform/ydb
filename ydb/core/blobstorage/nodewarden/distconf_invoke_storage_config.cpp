70f753968d7e5b486f09193333bef108e63d481a
mregrock <mregrock@ydb.tech>
2025-03-17 18:24:48 +0300

    Add config update validation

--- ydb/core/blobstorage/nodewarden/distconf_invoke_storage_config.cpp
+++ ydb/core/blobstorage/nodewarden/distconf_invoke_storage_config.cpp
@@ -221,6 +221,11 @@ namespace NKikimr::NStorage {
         }
         if (request.GetSkipConsoleValidation() || !NewYaml) {
             if (request.GetDryRun()) {
+                if (auto error = ValidateConfigUpdate(*Self->StorageConfig, ProposedStorageConfig)) {
+                    STLOG(PRI_DEBUG, BS_NODE, NWDC78, "Config update validation failed", (SelfId, SelfId()),
+                        (Error, *error), (Config, ProposedStorageConfig));
+                    return FinishWithError(TResult::ERROR, TStringBuilder() << "Config update validation failed: " << *error);
+                }
                 return Finish(Sender, SelfId(), PrepareResult(TResult::DRY_RUN_SUCCESS, std::nullopt).release(), 0, Cookie);
             } else {
                 StartProposition(&ProposedStorageConfig);
@@ -444,6 +449,11 @@ namespace NKikimr::NStorage {
                 }
                 const auto& replaceConfig = Event->Get()->Record.GetReplaceStorageConfig();
                 if (replaceConfig.GetDryRun()) {
+                    if (auto error = ValidateConfigUpdate(*Self->StorageConfig, ProposedStorageConfig)) {
+                        STLOG(PRI_DEBUG, BS_NODE, NWDC78, "Config update validation failed", (SelfId, SelfId()),
+                            (Error, *error), (Config, ProposedStorageConfig));
+                        return FinishWithError(TResult::ERROR, TStringBuilder() << "Config update validation failed: " << *error);
+                    }
                     return Finish(Sender, SelfId(), PrepareResult(TResult::DRY_RUN_SUCCESS, std::nullopt).release(), 0, Cookie);
                 }
                 return StartProposition(&ProposedStorageConfig);

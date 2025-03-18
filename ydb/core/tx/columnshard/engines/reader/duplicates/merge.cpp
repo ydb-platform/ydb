70c150990b70fc0081bfa8cba4dd24be9c576a25
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-03-18 18:49:24 +0300

    wip

--- ydb/core/tx/columnshard/engines/reader/duplicates/merge.cpp
+++ ydb/core/tx/columnshard/engines/reader/duplicates/merge.cpp
@@ -1,3 +1,32 @@
+#include "manager.h"
 #include "merge.h"
 
-namespace NKikimr::NOlap::NReader {}
+namespace NKikimr::NOlap::NReader {
+
+TConclusionStatus TBuildDuplicateFilters::DoExecute(const std::shared_ptr<ITask>& /*taskPtr*/) {
+    NArrow::NMerger::TMergePartialStream merger(PKSchema, nullptr, false, VersionColumnNames);
+    for (ui64 i = 0; i < Sources.size(); ++i) {
+        const auto& source = Sources[i];
+        merger.AddSource(source.GetData(), source.GetFilter(), i);
+    }
+    TFiltersBuilder filtersBuilder(Sources.size());
+    merger.DrainAll(filtersBuilder);
+    std::vector<NArrow::TColumnFilter> filters = std::move(filtersBuilder).ExtractFilters();
+    AFL_VERIFY(filters.size() == Sources.size());
+    // TODO: avoid copying filters
+    THashMap<ui64, NArrow::TColumnFilter> result;
+    for (ui64 i = 0; i < filters.size(); ++i) {
+        AFL_VERIFY(Sources[i].GetData()->GetRecordsCount() == filters[i].GetRecordsCountVerified())(
+                                                                  "data", Sources[i].GetData()->GetRecordsCount())(
+                                                                  "filter", filters[i].GetRecordsCountVerified());
+        result.emplace(Sources[i].GetSourceId(), std::move(filters[i]));
+    }
+    TActorContext::AsActorContext().Send(Owner, new TEvDuplicateFilterPartialResult(std::move(result), IntervalIdx));
+    return TConclusionStatus::Success();
+}
+
+void TBuildDuplicateFilters::DoOnCannotExecute(const TString& reason) {
+    TActorContext::AsActorContext().Send(Owner, new TEvDuplicateFilterPartialResult(TConclusionStatus::Fail(reason), IntervalIdx));
+}
+
+}   // namespace NKikimr::NOlap::NReader

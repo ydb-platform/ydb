61b92aeb846ebc6180fb4c18c85ff10c58093610
robot-piglet <robot-piglet@yandex-team.com>
2025-03-19 13:47:45 +0300

    Intermediate changes commit_hash:ebf6fb6dff099bd6bcfbf201e52dda1751dd76fd

--- contrib/libs/libfuzzer/.yandex_meta/override.nix
+++ contrib/libs/libfuzzer/.yandex_meta/override.nix
@@ -1,11 +1,11 @@
 pkgs: attrs: with pkgs; with attrs; rec {
-  version = "19.1.7";
+  version = "20.1.0";
 
   src = fetchFromGitHub {
     owner = "llvm";
     repo = "llvm-project";
     rev = "llvmorg-${version}";
-    hash = "sha256-cZAB5vZjeTsXt9QHbP5xluWNQnAHByHtHnAhVDV0E6I=";
+    hash = "sha256-86Z8e4ubnHJc1cYHjYPLeQC9eoPF417HYtqg8NAzxts=";
   };
 
   sourceRoot = "source/compiler-rt";

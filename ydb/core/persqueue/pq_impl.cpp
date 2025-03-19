757b075b9fe85e9eab8e1ae987509ac4ccf028cb
Alek5andr-Kotov <akotov@ydb.tech>
2025-03-19 19:31:46 +0300

    An empty list of consumers for the background partition (#15956)

--- ydb/core/persqueue/pq_impl.cpp
+++ ydb/core/persqueue/pq_impl.cpp
@@ -845,6 +845,7 @@ NKikimrPQ::TPQTabletConfig TPersQueue::MakeSupportivePartitionConfig() const
     partitionConfig.MutableReadRuleServiceTypes()->Clear();
     partitionConfig.MutableReadRuleVersions()->Clear();
     partitionConfig.MutableReadRuleGenerations()->Clear();
+    partitionConfig.MutableConsumers()->Clear();
 
     return partitionConfig;
 }

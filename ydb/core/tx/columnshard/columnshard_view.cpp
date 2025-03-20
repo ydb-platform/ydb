57209b66b70b55ddc875683773afe6c57358d8a5
Oleg Doronin <dorooleg@yandex.ru>
2025-03-20 18:38:58 +0300

    HttpMonPage has been improved (#15902)

--- ydb/core/tx/columnshard/columnshard_view.cpp
+++ ydb/core/tx/columnshard/columnshard_view.cpp
@@ -25,7 +25,50 @@ bool TTxMonitoring::Execute(TTransactionContext& txc, const TActorContext&) {
 }
 
 void TTxMonitoring::Complete(const TActorContext& ctx) {
-    ctx.Send(HttpInfoEvent->Sender, new NMon::TEvRemoteJsonInfoRes(JsonReport.GetStringRobust()));
+    const auto& cgi = HttpInfoEvent->Get()->Cgi();
+    std::map<std::pair<ui64, ui64>, NJson::TJsonValue> schemaVersions;
+    for (const auto& item: JsonReport["tables_manager"]["schema_versions"].GetArray()) {
+        auto& schemaVersion = schemaVersions[std::make_pair<ui64, ui64>(item["SinceStep"].GetInteger(), item["SinceTxId"].GetInteger())];
+        schemaVersion = item;
+    }
+    size_t countVersions = std::min(10ul, schemaVersions.size());
+    if (const auto& countVersionsParam = cgi.Get("CountVersions")) {
+        try {
+            countVersions = std::min(std::stoul(countVersionsParam), schemaVersions.size());
+        } catch (...) {
+            // nothing, use default value
+        }
+    }
+    JsonReport["tables_manager"].EraseValue("schema_versions");
+    TStringStream html;
+    html << "<h3>Special Values</h3>";
+    html << "<b>CurrentSchemeShardId:</b> " << Self->CurrentSchemeShardId << "<br />";
+    html << "<b>ProcessingParams:</b> " << Self->ProcessingParams.value_or(NKikimrSubDomains::TProcessingParams{}).ShortDebugString() << "<br />";
+    html << "<b>LastPlannedStep:</b> " << Self->LastPlannedStep << "<br />";
+    html << "<b>LastPlannedTxId :</b> " << Self->LastPlannedTxId << "<br />";
+    html << "<b>LastSchemaSeqNoGeneration :</b> " << Self->LastSchemaSeqNo.Generation << "<br />";
+    html << "<b>LastSchemaSeqNoRound :</b> " << Self->LastSchemaSeqNo.Round << "<br />";
+    html << "<b>LastExportNumber :</b> " << Self->LastExportNo << "<br />";
+    html << "<b>OwnerPathId :</b> " << Self->OwnerPathId << "<br />";
+    html << "<b>Table/Store Path :</b> " << Self->OwnerPath << "<br />";
+    html << "<b>LastCompletedStep :</b> " << Self->LastCompletedTx.GetPlanStep() << "<br />";
+    html << "<b>LastCompletedTxId :</b> " << Self->LastCompletedTx.GetTxId() << "<br />";
+    html << "<b>LastNormalizerSequentialId :</b> " << Self->NormalizerController.GetLastSavedNormalizerId() << "<br />";
+    html << "<b>SubDomainLocalPathId :</b> " << Self->SpaceWatcher->SubDomainPathId.value_or(0) << "<br />";
+    html << "<b>SubDomainOutOfSpace :</b> " << Self->SpaceWatcher->SubDomainOutOfSpace << "<br />";
+    html << "<h3>Tables Manager</h3>";
+    html << "<h4>Status</h4>";
+    html << "<pre>" << JsonReport << "</pre><br />";
+    html << "<h4>Top " << countVersions << " of " << schemaVersions.size() << " Versions</h4>";
+    size_t counter = 0;
+    for (const auto& [_, schemaVersion]: schemaVersions) {
+        html << counter;
+        html << "<pre>" << schemaVersion << "</pre><br />";
+        if (++counter == countVersions) {
+            break;
+        }
+    }
+    ctx.Send(HttpInfoEvent->Sender, new NMon::TEvRemoteHttpInfoRes(html.Str()));
 }
 
 bool TColumnShard::OnRenderAppHtmlPage(NMon::TEvRemoteHttpInfo::TPtr ev, const TActorContext& ctx) {

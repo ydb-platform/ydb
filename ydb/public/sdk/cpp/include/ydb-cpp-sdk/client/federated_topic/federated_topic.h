7ec708b2d9712677f7f2e36b6f13a6ab157ab500
Yuriy Kaminskiy <yumkam7@ydb.tech>
2025-03-21 18:14:23 +0300

    ydb-sdk: middle-level methods to demonstrate use of GetAllClusterInfo
    may be omitted

--- ydb/public/sdk/cpp/include/ydb-cpp-sdk/client/federated_topic/federated_topic.h
+++ ydb/public/sdk/cpp/include/ydb-cpp-sdk/client/federated_topic/federated_topic.h
@@ -539,6 +539,12 @@ public:
     //! Discover all clusters for federated topic
     NThreading::TFuture<std::vector<TClusterInfo>> GetAllClusterInfo();
 
+    static std::vector<NTopic::TTopicClient> GetAllTopicClients(const TDriver& driver, const std::vector<TClusterInfo>& clusterInfos, NTopic::TTopicClientSettings& clientSettings);
+
+    static std::vector<TAsyncDescribeTopicResult> DescribeAllTopics(const std::string& path, std::vector<NTopic::TTopicClient> &topicClients, const std::vector<TClusterInfo>& clusterInfos, NTopic::TDescribeTopicSettings& describeSettings);
+
+    static std::vector<std::shared_ptr<NTopic::IReadSession>> CreateAllTopicsReadSessions(std::vector<NTopic::TTopicClient>& topicClients, const std::vector<TClusterInfo>& clusterInfos, std::vector<NTopic::TReadSessionSettings>& readSettings);
+
 protected:
     void OverrideCodec(NTopic::ECodec codecId, std::unique_ptr<NTopic::ICodec>&& codecImpl);
 

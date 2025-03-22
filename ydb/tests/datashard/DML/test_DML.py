b3addde38dfc5b54b251664bb7452a0546f2cfe4
Denis Prokofev <dennnis@ydb.tech>
2025-03-22 19:01:46 +0300

    fix ttl

--- ydb/tests/datashard/DML/test_DML.py
+++ ydb/tests/datashard/DML/test_DML.py
@@ -1,5 +1,3 @@
-import threading
-
 from ydb.tests.sql.lib.test_base import TestBase
 from ydb.tests.stress.oltp_workload.workload import cleanup_type_name
 from ydb.tests.datashard.lib.create_table import TestCreateTables, pk_types, non_pk_types, index_first, index_second, ttl_types, unique, index_sync
@@ -7,22 +5,20 @@ from ydb.tests.datashard.lib.create_table import TestCreateTables, pk_types, non
 
 class TestDML(TestCreateTables, TestBase):
     def test_DML(self):
-        self.DML(
-            "table_all_types", pk_types, {**pk_types, **non_pk_types}, {}, "", "", "")
-
         # all ttl
         for ttl in ttl_types.keys():
             self.DML(
-                f"table_ttl_{ttl}", pk_types, {}, {}, ttl, "", "" )
-
+                f"table_ttl_{ttl}", pk_types, {}, {}, ttl, "", "")
         # all index
         for i in range(2):
             for uniq in unique:
                 for sync in index_sync:
                     if uniq != "UNIQUE" and sync != "ASYNC":
                         self.DML(
-                            f"table_index_{i}_{uniq}_{sync}", pk_types, {}, index_first if i == 0 else index_second, uniq, sync,)
+                            f"table_index_{i}_{uniq}_{sync}", pk_types, {}, index_first if i == 0 else index_second, "", uniq, sync)
 
+        self.DML(
+            "table_all_types", pk_types, {**pk_types, **non_pk_types}, {}, "", "", "")
 
     def DML(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
         columns = {
@@ -31,11 +27,17 @@ class TestDML(TestCreateTables, TestBase):
             "col_index_": index.keys(),
             "ttl_": [ttl]
         }
+        pk_columns = {
+            "pk_": pk_types.keys()
+        }
+        index_columns = {
+            "col_index_": index.keys()
+        }
         sql_create_table = self.create_table(
-            table_name, columns, unique, sync)
+            table_name, columns, pk_columns, index_columns, unique, sync)
         self.query(sql_create_table)
         if ttl != "":
-            sql_ttl = self.create_ttl(ttl, {"PT5S": ""}, "SECONDS" if ttl ==
+            sql_ttl = self.create_ttl(f"ttl_{cleanup_type_name(ttl)}", {"P18262D": ""}, "SECONDS" if ttl ==
                                       "Uint32" or ttl == "Uint64" or ttl == "DyNumber" else "", table_name)
             self.query(sql_ttl)
         self.insert(table_name, all_types, pk_types, index, ttl)
@@ -66,7 +68,7 @@ class TestDML(TestCreateTables, TestBase):
             if ttl != "":
                 sql_select += f"ttl_{ttl}={ttl_types[ttl].format(count)}"
             else:
-                sql_select += f"pk_Int64={pk_types["Int64"].format(count)}"
+                sql_select += f"""pk_Int64={pk_types["Int64"].format(count)}"""
 
             rows = self.query(sql_select)
             assert len(
@@ -78,12 +80,12 @@ class TestDML(TestCreateTables, TestBase):
                 {", ".join(["pk_" + cleanup_type_name(type_name) for type_name in pk_types.keys()])}{", " if len(all_types) != 0 else ""}
                 {", ".join(["col_" + cleanup_type_name(type_name) for type_name in all_types.keys()])}{", " if len(index) != 0 else ""}
                 {", ".join(["col_index_" + cleanup_type_name(type_name) for type_name in index.keys()])}{", " if len(ttl) != 0 else ""}
-                {f", ttl_{ttl}" if ttl != "" else ""}
+                {f"ttl_{ttl}" if ttl != "" else ""}
             )
             VALUES(
                 {", ".join([pk_types[type_name].format(value) for type_name in pk_types.keys()])}{", " if len(all_types) != 0 else ""}
                 {", ".join([all_types[type_name].format(value) for type_name in all_types.keys()])}{", " if len(index) != 0 else ""}
-                {", ".join([all_types[type_name].format(value) for type_name in index.keys()])}{", " if len(ttl) != 0 else ""}
+                {", ".join([index[type_name].format(value) for type_name in index.keys()])}{", " if len(ttl) != 0 else ""}
                 {ttl_types[ttl].format(value) if ttl != "" else ""}
             );
         """
@@ -104,7 +106,7 @@ class TestDML(TestCreateTables, TestBase):
 
         for type_name in index.keys():
             self.create_update(
-                count, type_name, f"col_index_{type_name}", index[type_name], table_name)
+                count, f"col_index_{type_name}", index[type_name], table_name)
             count += 1
 
         count_assert = 1
@@ -166,7 +168,7 @@ class TestDML(TestCreateTables, TestBase):
             if ttl != "":
                 sql_select += f"ttl_{ttl}={ttl_types[ttl].format(number_of_columns - count + 1)}"
             else:
-                sql_select += f"pk_Int64={pk_types["Int64"].format(count)}"
+                sql_select += f"""pk_Int64={pk_types["Int64"].format(count)}"""
 
             rows = self.query(sql_select)
             assert len(
@@ -188,7 +190,7 @@ class TestDML(TestCreateTables, TestBase):
             if ttl != "" and ((type_name != "Date" and type_name != "Datetime") or count < 106):
                 sql_select += f"ttl_{ttl}={ttl_types[ttl].format(count)}"
             else:
-                sql_select += f"pk_Int64={pk_types["Int64"].format(count)}"
+                sql_select += f"""pk_Int64={pk_types["Int64"].format(count)}"""
 
             rows = self.query(sql_select)
             assert len(
@@ -200,13 +202,13 @@ class TestDML(TestCreateTables, TestBase):
                     {", ".join(["pk_" + cleanup_type_name(type_name) for type_name in pk_types.keys()])}{", " if len(all_types) != 0 else ""}
                     {", ".join(["col_" + cleanup_type_name(type_name) for type_name in all_types.keys()])}{", " if len(index) != 0 else ""}
                     {", ".join(["col_index_" + cleanup_type_name(type_name) for type_name in index.keys()])}{", " if len(ttl) != 0 else ""}
-                    {f" ttl_{ttl}," if ttl != "" else ""}
+                    {f" ttl_{ttl}" if ttl != "" else ""}
                     )
                     VALUES
                     (
                     {", ".join([pk_types[type_name].format(search) for type_name in pk_types.keys()])}{", " if len(all_types) != 0 else ""}
                     {", ".join([all_types[type_name].format(value) for type_name in all_types.keys()])}{", " if len(index) != 0 else ""}
-                    {", ".join([all_types[type_name].format(value) for type_name in index.keys()])}{", " if len(ttl) != 0 else ""}
+                    {", ".join([index[type_name].format(value) for type_name in index.keys()])}{", " if len(ttl) != 0 else ""}
                     {ttl_types[ttl].format(value) if ttl != "" else ""}
                     )
                     ;
@@ -245,7 +247,7 @@ class TestDML(TestCreateTables, TestBase):
         for type_name in index.keys():
             if type_name != "Bool":
                 self.create_delete(
-                    number_of_columns, "col_index_{cleanup_type_name(type_name)}", index[type_name], table_name)
+                    number_of_columns, f"col_index_{cleanup_type_name(type_name)}", index[type_name], table_name)
             else:
                 self.create_delete(
                     number_of_columns, "pk_Int64", pk_types["Int64"], table_name)
@@ -270,27 +272,29 @@ class TestDML(TestCreateTables, TestBase):
             if ttl != "":
                 sql_select += f"ttl_{ttl}={ttl_types[ttl].format(number_of_columns - count + 1)}"
             else:
-                sql_select += f"pk_Int64={pk_types["Int64"].format(count)}"
+                sql_select += f"""pk_Int64={pk_types["Int64"].format(count)}"""
 
             rows = self.query(sql_select)
             assert len(
                 rows) == 1 and rows[0].count == 1, f"Expected one rows, faild in {count} value, table {table_name}"
 
-        for count in (number_of_columns + 1, 2*number_of_columns + 1):
+        for count in range(number_of_columns + 1, 2*number_of_columns + 1):
             sql_select = f"""
                 SELECT COUNT(*) as count FROM `{table_name}` WHERE """
 
             for type_name in pk_types.keys():
-                sql_select += f"pk_{cleanup_type_name(type_name)}={pk_types[type_name].format(count)} and "
+                if (type_name != "Date" and type_name != "Datetime") or count < 106:
+                    sql_select += f"pk_{cleanup_type_name(type_name)}={pk_types[type_name].format(count)} and "
             for type_name in all_types.keys():
-                if type_name != "Json" and type_name != "Yson" and type_name != "JsonDocument":
+                if type_name != "Json" and type_name != "Yson" and type_name != "JsonDocument" and ((type_name != "Date" and type_name != "Datetime") or count < 106):
                     sql_select += f"col_{cleanup_type_name(type_name)}={all_types[type_name].format(count)} and "
             for type_name in index.keys():
-                sql_select += f"col_index_{cleanup_type_name(type_name)}={index[type_name].format(count)} and "
-            if ttl != "":
+                if (type_name != "Date" and type_name != "Datetime") or count < 106:
+                    sql_select += f"col_index_{cleanup_type_name(type_name)}={index[type_name].format(count)} and "
+            if ttl != "" and ((type_name != "Date" and type_name != "Datetime") or count < 106):
                 sql_select += f"ttl_{ttl}={ttl_types[ttl].format(count)}"
             else:
-                sql_select += f"pk_Int64={pk_types["Int64"].format(count)}"
+                sql_select += f"""pk_Int64={pk_types["Int64"].format(count)}"""
 
             rows = self.query(sql_select)
             assert len(

d4638348a785d78634809f9392bba4532e21e9d9
Alexander Rutkovsky <alexvru@ydb.tech>
2025-03-24 15:34:52 +0300

    Fix loading tx (#16116)

--- ydb/core/blob_depot/data.h
+++ ydb/core/blob_depot/data.h
@@ -428,6 +428,14 @@ namespace NKikimr::NBlobDepot {
         ui64 TotalStoredTrashSize = 0;
         ui64 InFlightTrashSize = 0;
 
+        ui64 LoadRestartTx = 0;
+        ui64 LoadRunSuccessorTx = 0;
+        ui64 LoadProcessingCycles = 0;
+        ui64 LoadFinishTxCycles = 0;
+        ui64 LoadRestartTxCycles = 0;
+        ui64 LoadRunSuccessorTxCycles = 0;
+        ui64 LoadTotalCycles = 0;
+
         friend class TGroupAssimilator;
 
         THashMultiMap<void*, TLogoBlobID> InFlightTrash; // being committed, but not yet confirmed

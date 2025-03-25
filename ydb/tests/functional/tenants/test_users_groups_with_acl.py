5d239fe89f1c786ba4be023d8f468fc20d328774
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2025-03-25 16:58:43 +0300

    Fixes

--- ydb/tests/functional/tenants/test_users_groups_with_acl.py
+++ ydb/tests/functional/tenants/test_users_groups_with_acl.py
@@ -4,7 +4,6 @@ import logging
 import pytest
 from ydb.tests.library.harness.kikimr_config import KikimrConfigGenerator
 from ydb.tests.library.fixtures import ydb_database_ctx
-from ydb.tests.library.flavours import ydb_flavour_path
 from ydb.tests.oss.ydb_sdk_import import ydb
 
 

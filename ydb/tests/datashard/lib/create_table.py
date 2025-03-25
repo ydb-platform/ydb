9bc66752ae34b6d913604d94395aa5bd3f79c9b9
Denis Prokofev <dennnis@ydb.tech>
2025-03-25 11:11:19 +0300

    fix issoes

--- ydb/tests/datashard/lib/create_table.py
+++ ydb/tests/datashard/lib/create_table.py
@@ -9,13 +9,13 @@ ttl_types = {
     "Timestamp": "CAST(26962{:03}00000000 AS Timestamp)",
 }
 
-# Отсюда убраны uint16 и int16 потому что при поиске по данным индексам выдает ошибку
-# ydb/core/kqp/provider/yql_kikimr_provider.cpp:685 FillLiteralProtoImpl(): requirement false failed, message: Unexpected type slot Int16 (или Uint16)
 index_first = {
     "Int64": "CAST({} AS Int64)",
     "Uint64": "CAST({} AS Uint64)",
     "Int32": "CAST({} AS Int32)",
     "Uint32": "CAST({} AS Uint32)",
+    # "Int16": "CAST({} AS Int16)", https://github.com/ydb-platform/ydb/issues/15842
+    # "Uint16": "CAST({} AS Uint16)", https://github.com/ydb-platform/ydb/issues/15842
     "Int8": "CAST({} AS Int8)",
     "Uint8": "CAST({} AS Uint8)",
     "Bool": "CAST({} AS Bool)",
@@ -79,34 +79,34 @@ unique = ["", "UNIQUE"]
 index_sync = ["SYNC", "ASYNC"]
 
 
-class CreateTables():
-    def create_table(self, table_name: str, columns: dict[str, dict[str]], pk_colums: dict[str, dict[str]], index_colums: dict[str, dict[str]], unique: str, sync: str) -> str:
-        sql_create = f"CREATE TABLE {table_name} ("
-        for prefix in columns.keys():
-            for type_name in columns[prefix]:
-                if prefix != "ttl_" or type_name != "":
-                    sql_create += f"{prefix}{cleanup_type_name(type_name)} {type_name}, "
-        sql_create += "PRIMARY KEY( "
-        for prefix in pk_colums.keys():
-            for type_name in pk_colums[prefix]:
-                if prefix != "ttl_" or type_name != "":
-                    sql_create += f"{prefix}{cleanup_type_name(type_name)},"
-        sql_create = sql_create[:-1]
-        sql_create += ")"
-        for prefix in index_colums.keys():
-            for type_name in index_colums[prefix]:
-                if prefix != "ttl_" or type_name != "":
-                    sql_create += f", INDEX idx_{prefix}{cleanup_type_name(type_name)} GLOBAL {unique} {sync} ON ({prefix}{cleanup_type_name(type_name)})"
-        sql_create += ")"
-        return sql_create
+def create_table(table_name: str, columns: dict[str, dict[str]], pk_colums: dict[str, dict[str]], index_colums: dict[str, dict[str]], unique: str, sync: str) -> str:
+    sql_create = f"CREATE TABLE {table_name} ("
+    for prefix in columns.keys():
+        for type_name in columns[prefix]:
+            if prefix != "ttl_" or type_name != "":
+                sql_create += f"{prefix}{cleanup_type_name(type_name)} {type_name}, "
+    sql_create += "PRIMARY KEY( "
+    for prefix in pk_colums.keys():
+        for type_name in pk_colums[prefix]:
+            if prefix != "ttl_" or type_name != "":
+                sql_create += f"{prefix}{cleanup_type_name(type_name)},"
+    sql_create = sql_create[:-1]
+    sql_create += ")"
+    for prefix in index_colums.keys():
+        for type_name in index_colums[prefix]:
+            if prefix != "ttl_" or type_name != "":
+                sql_create += f", INDEX idx_{prefix}{cleanup_type_name(type_name)} GLOBAL {unique} {sync} ON ({prefix}{cleanup_type_name(type_name)})"
+    sql_create += ")"
+    return sql_create
 
-    def create_ttl(self, ttl: str, inteval: dict[str, str], time: str, table_name: str) -> str:
-        sql_ttl = f" ALTER TABLE {table_name} SET ( TTL = "
-        lenght_inteval = len(inteval)
-        count = 1
-        for pt in inteval.keys():
-            sql_ttl += f"""Interval("{pt}") {inteval[pt] if inteval[pt] == "" or inteval[pt] == "DELETE" else f"TO EXTERNAL DATA SOURCE {inteval[pt]}"}
+
+def create_ttl(ttl: str, inteval: dict[str, str], time: str, table_name: str) -> str:
+    sql_ttl = f" ALTER TABLE {table_name} SET ( TTL = "
+    lenght_inteval = len(inteval)
+    count = 1
+    for pt in inteval.keys():
+        sql_ttl += f"""Interval("{pt}") {inteval[pt] if inteval[pt] == "" or inteval[pt] == "DELETE" else f"TO EXTERNAL DATA SOURCE {inteval[pt]}"}
                 {", " if count != lenght_inteval else " "}"""
-            count += 1
-        sql_ttl += f""" ON {ttl} {f"AS {time}" if time != "" else ""} ); """
-        return sql_ttl
+        count += 1
+    sql_ttl += f""" ON {ttl} {f"AS {time}" if time != "" else ""} ); """
+    return sql_ttl

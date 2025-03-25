f2e4f1c957aa3e027cfc7a7714ba3b8c3c18a416
Denis Prokofev <dennnis@ydb.tech>
2025-03-25 21:42:21 +0300

    fix

--- ydb/tests/datashard/TTL/test_TTL.py
+++ ydb/tests/datashard/TTL/test_TTL.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 import time
-import threading
 import os
 import subprocess
 
@@ -37,10 +36,8 @@ class TestTTL(TestBase):
                                 f"table_{ttl}_{i}_{uniq}_{sync}", pk_types, {
                                     **pk_types, **non_pk_types}, index, ttl, uniq, sync)
 
-
-
     def TTL(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
-        columns= {
+        columns = {
             "pk_": pk_types.keys(),
             "col_": all_types.keys(),
             "col_index_": index.keys(),
@@ -65,20 +62,20 @@ class TestTTL(TestBase):
         self.select(table_name, pk_types, all_types, index)
 
     def insert(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str):
-        now_date= datetime.now()
-        old_date= now_date - timedelta(weeks=1)
-        future_time_5_sec= now_date + timedelta(seconds=5)
-        future_time= now_date + timedelta(weeks=2)
+        now_date = datetime.now()
+        old_date = now_date - timedelta(weeks=1)
+        future_time_5_sec = now_date + timedelta(seconds=5)
+        future_time = now_date + timedelta(weeks=2)
         if ttl != "Date":
-            now_date= int(datetime.timestamp(now_date))
-            old_date= int(datetime.timestamp(old_date))
-            future_time_5_sec= int(datetime.timestamp(future_time_5_sec))
-            future_time= int(datetime.timestamp(future_time))
+            now_date = int(datetime.timestamp(now_date))
+            old_date = int(datetime.timestamp(old_date))
+            future_time_5_sec = int(datetime.timestamp(future_time_5_sec))
+            future_time = int(datetime.timestamp(future_time))
         else:
-            now_date= now_date.date()
-            old_date= old_date.date()
-            future_time_5_sec= future_time_5_sec.date()
-            future_time= future_time.date()
+            now_date = now_date.date()
+            old_date = old_date.date()
+            future_time_5_sec = future_time_5_sec.date()
+            future_time = future_time.date()
         print(f"{table_name} insert")
         for i in range(1, 3):
             self.create_insert(table_name, pk_types,
@@ -95,7 +92,7 @@ class TestTTL(TestBase):
         print(f"{table_name} insert ok")
 
     def create_insert(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, value: int, date):
-        insert_sql= f"""
+        insert_sql = f"""
             INSERT INTO {table_name}(
                 {", ".join(["pk_" + cleanup_type_name(type_name) for type_name in pk_types.keys()])}{", " if len(all_types) != 0 else ""}
                 {", ".join(["col_" + cleanup_type_name(type_name) for type_name in all_types.keys()])}{", " if len(index) != 0 else ""}
@@ -121,7 +118,7 @@ class TestTTL(TestBase):
         print(f"{table_name} select ok")
 
     def create_select(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], value: int, expected_count_rows: int):
-        sql_select= f"""
+        sql_select = f"""
                 SELECT COUNT(*) as count FROM `{table_name}` WHERE """
 
         for type_name in pk_types.keys():
@@ -132,23 +129,23 @@ class TestTTL(TestBase):
         for type_name in index.keys():
             sql_select += f"col_index_{cleanup_type_name(type_name)}={index[type_name].format(value)} and "
         sql_select += f"""pk_Int64={pk_types["Int64"].format(value)}"""
-        start_time= time.time()
-        max_wait_time= 150
+        start_time = time.time()
+        max_wait_time = 150
         while True:
-            rows= self.query(sql_select)
+            rows = self.query(sql_select)
             if (len(rows) == 1 and rows[0].count == expected_count_rows) or expected_count_rows == 1:
                 print(time.time() - start_time)
                 break
-            elapsed_time= time.time() - start_time
+            elapsed_time = time.time() - start_time
             if elapsed_time >= max_wait_time:
                 break
             time.sleep(1)
-        rows= self.query(sql_select)
+        rows = self.query(sql_select)
         assert len(
             rows) == 1 and rows[0].count == expected_count_rows, f"Expected {expected_count_rows} rows, error when deleting {value} lines, table {table_name}"
 
     def alter_database_quotas(self, node, database_path, database_quotas):
-        alter_proto= """ModifyScheme {
+        alter_proto = """ModifyScheme {
             OperationType: ESchemeOpAlterSubDomain
             WorkingDir: "%s"
             SubDomain {
@@ -171,8 +168,8 @@ class TestTTL(TestBase):
         """)
 
     def ydbcli_db_schema_exec(self, node, operation_proto):
-        endpoint= f"{node.host}:{node.port}"
-        args= [
+        endpoint = f"{node.host}:{node.port}"
+        args = [
             node.binary_path,
             f"--server=grpc://{endpoint}",
             "db",
@@ -180,5 +177,5 @@ class TestTTL(TestBase):
             "exec",
             operation_proto,
         ]
-        command= subprocess.run(args, capture_output=True)
+        command = subprocess.run(args, capture_output=True)
         assert command.returncode == 0, command.stderr.decode("utf-8")

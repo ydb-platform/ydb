fe0cc902a0f633e7eb966d672f07bc7af5ac0b96
Denis Prokofev <dennnis@ydb.tech>
2025-03-25 19:16:10 +0300

    fix

--- ydb/tests/datashard/TTL/test_TTL.py
+++ ydb/tests/datashard/TTL/test_TTL.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 import time
 import threading
+import os
+import subprocess
 
 from ydb.tests.sql.lib.test_base import TestBase
 from ydb.tests.stress.oltp_workload.workload import cleanup_type_name
@@ -13,30 +15,26 @@ ttl_types = {
     "Uint32": "CAST({} AS Uint32)",
     "Uint64": "CAST({} AS Uint64)",
     "Date": "CAST('{}' AS Date)",
-    "Datetime": "CAST('{}' AS Datetime)",
+    "Datetime": "CAST({} AS Datetime)",
 }
 
 
 class TestTTL(TestBase):
     def test_TTL(self):
-        threads = []
+        self.ttl_time_sec(1)
         for ttl in ttl_types.keys():
             for i in range(2):
                 for uniq in unique:
                     for sync in index_sync:
-                        if uniq != "UNIQUE" and sync != "ASYNC":
-                            thr = threading.Thread(target=self.TTL, args=(
+                        if uniq != "UNIQUE" or sync != "ASYNC":
+                            self.TTL(
                                 f"table_{ttl}_{i}_{uniq}_{sync}", pk_types, {
-                                    **pk_types, **non_pk_types}, index_first if i == 0 else index_second, ttl, uniq, sync,), name=f"table_{ttl}_{i}_{uniq}_{sync}")
-                            thr.start()
-                            threads.append(thr)
-                            thr.join()
+                                    **pk_types, **non_pk_types}, index_first if i == 0 else index_second, ttl, uniq, sync)
 
-        for thread in threads:
-            thread.join()
+
 
     def TTL(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
-        columns = {
+        columns= {
             "pk_": pk_types.keys(),
             "col_": all_types.keys(),
             "col_index_": index.keys(),
@@ -51,33 +49,31 @@ class TestTTL(TestBase):
         sql_create_table = create_table(
             table_name, columns, pk_columns, index_columns, unique, sync)
         self.query(sql_create_table)
-        sql_ttl = create_ttl(f"ttl_{cleanup_type_name(ttl)}", {"PT0S": ""}, "SECONDS" if ttl ==
-                             "Uint32" or ttl == "Uint64" or ttl == "DyNumber" else "", table_name)
-        self.query(sql_ttl)
+        if ttl == "Uint32" or ttl == "Uint64" or ttl == "DyNumber":
+            os.system(
+                f"ydb -e {self.get_endpoint()} -d {self.get_database()} table ttl set --column ttl_{ttl} --expire-after 1 --unit seconds {table_name}")
+        else:
+            os.system(
+                f"ydb -e {self.get_endpoint()} -d {self.get_database()} table ttl set --column ttl_{ttl} --expire-after 1 {table_name}")
         self.insert(table_name, pk_types, all_types, index, ttl)
         self.select(table_name, pk_types, all_types, index)
 
     def insert(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str):
-        now_date = datetime.now()
-        old_date = now_date - timedelta(weeks=1)
-        future_time_5_sec = now_date + timedelta(seconds=5)
-        future_time = now_date + timedelta(weeks=2)
-        if ttl == "Uint64" or ttl == "Uint32" or ttl == "DyNumber" or ttl == "Timestamp":
-            now_date = int(datetime.timestamp(now_date))
-            old_date = int(datetime.timestamp(old_date))
-            future_time_5_sec = int(datetime.timestamp(future_time_5_sec))
-            future_time = int(datetime.timestamp(future_time))
-        if ttl == "Date":
-            now_date = now_date.date()
-            old_date = old_date.date()
-            future_time_5_sec = future_time_5_sec.date()
-            future_time = future_time.date()
-        if ttl == "Datetime":
-            now_date = now_date.strftime("%Y-%m-%dT%H:%M:%SZ")
-            old_date = old_date.strftime("%Y-%m-%dT%H:%M:%SZ")
-            future_time_5_sec = future_time_5_sec.strftime(
-                "%Y-%m-%dT%H:%M:%SZ")
-            future_time = future_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+        now_date= datetime.now()
+        old_date= now_date - timedelta(weeks=1)
+        future_time_5_sec= now_date + timedelta(seconds=5)
+        future_time= now_date + timedelta(weeks=2)
+        if ttl != "Date":
+            now_date= int(datetime.timestamp(now_date))
+            old_date= int(datetime.timestamp(old_date))
+            future_time_5_sec= int(datetime.timestamp(future_time_5_sec))
+            future_time= int(datetime.timestamp(future_time))
+        else:
+            now_date= now_date.date()
+            old_date= old_date.date()
+            future_time_5_sec= future_time_5_sec.date()
+            future_time= future_time.date()
+        print(f"{table_name} insert")
         for i in range(1, 3):
             self.create_insert(table_name, pk_types,
                                all_types, index, ttl, i, old_date)
@@ -90,9 +86,11 @@ class TestTTL(TestBase):
         for i in range(7, 9):
             self.create_insert(table_name, pk_types,
                                all_types, index, ttl, i, future_time)
+        print(f"{table_name} insert ok")
 
     def create_insert(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, value: int, date):
-        insert_sql = f"""
+        print(value)
+        insert_sql= f"""
             INSERT INTO {table_name}(
                 {", ".join(["pk_" + cleanup_type_name(type_name) for type_name in pk_types.keys()])}{", " if len(all_types) != 0 else ""}
                 {", ".join(["col_" + cleanup_type_name(type_name) for type_name in all_types.keys()])}{", " if len(index) != 0 else ""}
@@ -109,14 +107,16 @@ class TestTTL(TestBase):
         self.query(insert_sql)
 
     def select(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str]):
+        print(f"{table_name} select")
         for i in range(1, 7):
             self.create_select(table_name, pk_types, all_types, index, i, 0)
 
         for i in range(7, 9):
             self.create_select(table_name, pk_types, all_types, index, i, 1)
+        print(f"{table_name} select ok")
 
     def create_select(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], value: int, expected_count_rows: int):
-        sql_select = f"""
+        sql_select= f"""
                 SELECT COUNT(*) as count FROM `{table_name}` WHERE """
 
         for type_name in pk_types.keys():
@@ -127,10 +127,53 @@ class TestTTL(TestBase):
         for type_name in index.keys():
             sql_select += f"col_index_{cleanup_type_name(type_name)}={index[type_name].format(value)} and "
         sql_select += f"""pk_Int64={pk_types["Int64"].format(value)}"""
-        for _ in range(20):
-            rows = self.query(sql_select)
+        start_time= time.time()
+        max_wait_time= 150
+        while True:
+            rows= self.query(sql_select)
             if (len(rows) == 1 and rows[0].count == expected_count_rows) or expected_count_rows == 1:
+                print(time.time() - start_time)
                 break
-            time.sleep(60)
+            elapsed_time= time.time() - start_time
+            if elapsed_time >= max_wait_time:
+                break
+            time.sleep(1)
+        rows= self.query(sql_select)
         assert len(
             rows) == 1 and rows[0].count == expected_count_rows, f"Expected {expected_count_rows} rows, error when deleting {value} lines, table {table_name}"
+
+    def alter_database_quotas(self, node, database_path, database_quotas):
+        alter_proto= """ModifyScheme {
+            OperationType: ESchemeOpAlterSubDomain
+            WorkingDir: "%s"
+            SubDomain {
+                Name: "%s"
+                DatabaseQuotas {
+                    %s
+                }
+            }
+        }""" % (
+            os.path.dirname(database_path),
+            os.path.basename(database_path),
+            database_quotas,
+        )
+
+        self.ydbcli_db_schema_exec(node, alter_proto)
+
+    def ttl_time_sec(self, time: int):
+        self.alter_database_quotas(self.cluster.nodes[1], '/Root', f"""
+            ttl_min_run_internal_seconds: {time}
+        """)
+
+    def ydbcli_db_schema_exec(self, node, operation_proto):
+        endpoint= f"{node.host}:{node.port}"
+        args= [
+            node.binary_path,
+            f"--server=grpc://{endpoint}",
+            "db",
+            "schema",
+            "exec",
+            operation_proto,
+        ]
+        command= subprocess.run(args, capture_output=True)
+        assert command.returncode == 0, command.stderr.decode("utf-8")

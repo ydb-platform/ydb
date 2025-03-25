2a96741ab2eab1f8aee1fac203b7274db33a6272
Daniil Cherednik <dcherednik@ydb.tech>
2025-03-25 17:46:05 +0100

    More ut

--- ydb/public/sdk/cpp/tests/unit/client/driver/driver_ut.cpp
+++ ydb/public/sdk/cpp/tests/unit/client/driver/driver_ut.cpp
@@ -184,7 +184,7 @@ Y_UNIT_TEST_SUITE(CppGrpcClientSimpleTest) {
         UNIT_ASSERT_VALUES_EQUAL(session.GetId(), "my-session-id");
     }
 
-    Y_UNIT_TEST(WithoutDiscovery) {
+    Y_UNIT_TEST(WithoutDiscoveryDriverLevel) {
         TPortManager pm;
 
         // Start our mock table service
@@ -208,4 +208,29 @@ Y_UNIT_TEST_SUITE(CppGrpcClientSimpleTest) {
         auto session = sessionResult.GetSession();
         UNIT_ASSERT_VALUES_EQUAL(session.GetId(), "my-session-id");
     }
+
+    Y_UNIT_TEST(WithoutDiscoveryClientLevel) {
+        TPortManager pm;
+
+        // Start our mock table service
+        TMockTableService tableService;
+        ui16 tablePort = pm.GetPort();
+        auto tableServer = StartGrpcServer(
+                TStringBuilder() << "127.0.0.1:" << tablePort,
+                tableService);
+
+        auto driver = TDriver(
+            TDriverConfig()
+                .SetEndpoint(TStringBuilder() << "localhost:" << tablePort)
+                .SetDatabase("/Root/My/DB"));
+        auto client = NTable::TTableClient(driver, TClientSettings().DiscoveryMode(EDiscoveryMode::Off));
+        auto sessionFuture = client.CreateSession();
+
+        UNIT_ASSERT(sessionFuture.Wait(TDuration::Seconds(10)));
+        auto sessionResult = sessionFuture.ExtractValueSync();
+        UNIT_ASSERT(sessionResult.IsSuccess());
+        auto session = sessionResult.GetSession();
+        UNIT_ASSERT_VALUES_EQUAL(session.GetId(), "my-session-id");
+    }
+
 }

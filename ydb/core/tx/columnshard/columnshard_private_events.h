3024ed3f4391021dbf4fd3042ffc4fb116fbe7b4
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-03-25 21:39:00 +0300

    fixes: intervals counter, skips handling

--- ydb/core/tx/columnshard/columnshard_private_events.h
+++ ydb/core/tx/columnshard/columnshard_private_events.h
@@ -72,6 +72,7 @@ struct TEvPrivate {
         EvRequestFilter,
         EvDuplicateFilterIntervalResult,
         EvDuplicateFilterDataFetched,
+        EvNotifyReadingFinished,
 
         EvEnd
     };

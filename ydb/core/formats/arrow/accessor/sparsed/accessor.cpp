f9f8702d4f2677e0bd273e7d7bb7b543a1d63dd1
ivanmorozov333 <ivanmorozov@hotmail.com>
2025-03-26 19:08:12 +0300

    correct sparsed chunk construction

--- ydb/core/formats/arrow/accessor/sparsed/accessor.cpp
+++ ydb/core/formats/arrow/accessor/sparsed/accessor.cpp
@@ -110,8 +110,12 @@ IChunkedArray::TLocalDataAddress TSparsedArrayChunk::GetChunk(
     AFL_VERIFY(it != RemapExternalToInternal.begin());
     --it;
     if (it->GetIsDefault()) {
-        return IChunkedArray::TLocalDataAddress(
-            NArrow::TThreadSimpleArraysCache::Get(ColValue->type(), DefaultValue, it->GetSize()), it->GetStartExt(), 0);
+        if (!DefaultValue) {
+            return IChunkedArray::TLocalDataAddress(
+                NArrow::TThreadSimpleArraysCache::Get(ColValue->type(), DefaultValue, it->GetSize()), it->GetStartExt(), 0);
+        } else {
+            return NArrow::TStatusValidator::GetValid(arrow::MakeArrayFromScalar(*DefaultValue, it->GetSize()));
+        }
     } else {
         return IChunkedArray::TLocalDataAddress(ColValue->Slice(it->GetStartInt(), it->GetSize()), it->GetStartExt(), 0);
     }

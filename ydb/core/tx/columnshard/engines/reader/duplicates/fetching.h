6f2395212d1450eeb9fcbadf4c2f8c40924a9c38
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-03-26 13:54:02 +0300

    fix fetching

--- ydb/core/tx/columnshard/engines/reader/duplicates/fetching.h
+++ ydb/core/tx/columnshard/engines/reader/duplicates/fetching.h
@@ -7,108 +7,13 @@
 
 namespace NKikimr::NOlap::NReader {
 
-class TSimpleFetchLogic: public NCommon::IKernelFetchLogic {
-private:
-    using TBase = IKernelFetchLogic;
-
-    class TChunkRestoreInfo {
-    private:
-        std::optional<TBlobRange> BlobRange;
-        std::optional<TPortionDataAccessor::TAssembleBlobInfo> Data;
-        const ui32 RecordsCount;
-
-    public:
-        TChunkRestoreInfo(const ui32 recordsCount, const TBlobRange& range)
-            : BlobRange(range)
-            , RecordsCount(recordsCount) {
-        }
-
-        const std::optional<TBlobRange>& GetBlobRangeOptional() const {
-            return BlobRange;
-        }
-
-        TChunkRestoreInfo(const ui32 recordsCount, const TPortionDataAccessor::TAssembleBlobInfo& defaultData)
-            : Data(defaultData)
-            , RecordsCount(recordsCount) {
-        }
-
-        TPortionDataAccessor::TAssembleBlobInfo ExtractDataVerified() {
-            AFL_VERIFY(!!Data);
-            Data->SetExpectedRecordsCount(RecordsCount);
-            return std::move(*Data);
-        }
-
-        void SetBlobData(const TString& data) {
-            AFL_VERIFY(!Data);
-            Data.emplace(data);
-        }
-    };
-
-    std::vector<TColumnRecord> ColumnChunksExt;
-    std::vector<TChunkRestoreInfo> ColumnChunks;
-    std::optional<TString> StorageId;
-
-    virtual void DoOnDataCollected(NCommon::TFetchingResultContext& context) override {
-        // TODO: why? investigate in simple reader or ask
-        // AFL_VERIFY(!IIndexInfo::IsSpecialColumn(GetEntityId()));
-        std::vector<TPortionDataAccessor::TAssembleBlobInfo> chunks;
-        for (auto&& i : ColumnChunks) {
-            chunks.emplace_back(i.ExtractDataVerified());
-        }
-
-        TPortionDataAccessor::TPreparedColumn column(
-            std::move(chunks), context.GetSource()->GetSourceSchema()->GetColumnLoaderVerified(GetEntityId()));
-        context.GetAccessors().AddVerified(GetEntityId(), column.AssembleAccessor().DetachResult(), true);
-    }
-
-    virtual void DoOnDataReceived(TReadActionsCollection& /*nextRead*/, NBlobOperations::NRead::TCompositeReadBlobs& blobs) override {
-        if (ColumnChunks.empty()) {
-            return;
-        }
-        for (auto&& i : ColumnChunks) {
-            if (!i.GetBlobRangeOptional()) {
-                continue;
-            }
-            AFL_VERIFY(!!StorageId);
-            i.SetBlobData(blobs.Extract(*StorageId, *i.GetBlobRangeOptional()));
-        }
-    }
-
-    virtual void DoStart(TReadActionsCollection& nextRead, NCommon::TFetchingResultContext& context) override {
-        AFL_TRACE(NKikimrServices::TX_COLUMNSHARD_SCAN)("component", "duplicate_filter_manager")("event", "start_fetching_columns")(
-            "column_id", GetEntityId())("column_chunks", ColumnChunksExt.size());
-        auto source = context.GetSource();
-        if (ColumnChunksExt.empty()) {
-            ColumnChunks.emplace_back(source->GetRecordsCount(), TPortionDataAccessor::TAssembleBlobInfo(source->GetRecordsCount(),
-                                                                     source->GetSourceSchema()->GetExternalDefaultValueVerified(GetEntityId())));
-            return;
-        }
-        StorageId = source->GetColumnStorageId(GetEntityId());
-        TBlobsAction blobsAction(source->GetContext()->GetCommonContext()->GetStoragesManager(), NBlobOperations::EConsumer::SCAN);
-        auto reading = blobsAction.GetReading(*StorageId);
-        for (auto&& c : ColumnChunksExt) {
-            reading->SetIsBackgroundProcess(false);
-            reading->AddRange(source->RestoreBlobRange(c.BlobRange));
-            ColumnChunks.emplace_back(c.GetMeta().GetRecordsCount(), source->RestoreBlobRange(c.BlobRange));
-        }
-        ColumnChunksExt.clear();
-        for (auto&& i : blobsAction.GetReadingActions()) {
-            nextRead.Add(i);
-        }
-    }
-
-public:
-    TSimpleFetchLogic(const ui32 entityId, const std::shared_ptr<IStoragesManager>& storagesManager, std::vector<TColumnRecord>&& columnChunks)
-        : TBase(entityId, storagesManager)
-        , ColumnChunksExt(std::move(columnChunks)) {
-    }
-};
-
 class TColumnFetchingContext {
 private:
     YDB_READONLY_DEF(TActorId, Owner);
     YDB_READONLY_DEF(std::shared_ptr<TDuplicateFilterConstructor::TSourceFilterConstructor>, Constructor);
     YDB_READONLY_DEF(std::shared_ptr<ISnapshotSchema>, ResultSchema);
+    std::optional<TPortionDataAccessor> PortionAccessor;
+    THashMap<TChunkAddress, TString> Blobs;
     bool IsDone = false;
 
     void OnDone() {
@@ -134,34 +39,39 @@ public:
         OnDone();
     }
 
+    void SetPortionAccessor(TPortionDataAccessor&& portionAccessor) {
+        AFL_VERIFY(!PortionAccessor);
+        PortionAccessor = std::move(portionAccessor);
+    }
     void SetResourceGuard(std::shared_ptr<NGroupedMemoryManager::TAllocationGuard>&& guard) {
         Constructor->SetMemoryGuard(std::move(guard));
     }
-    void OnResult(std::shared_ptr<NArrow::TGeneralContainer>&& result) {
+    void BuildResult() {
         // AFL_VERIFY(result->schema ... )
-        Constructor->SetColumnData(std::move(result));
+        AFL_VERIFY(PortionAccessor);
+        Constructor->SetColumnData(
+            PortionAccessor->PrepareForAssemble(*ResultSchema, *ResultSchema, Blobs, Constructor->GetSource()->GetDataSnapshot())
+                .AssembleToGeneralContainer({})
+                .DetachResult());
         TActorContext::AsActorContext().Send(
             Owner, new TEvDuplicateFilterDataFetched(Constructor->GetSource()->GetSourceId(), TConclusionStatus::Success()));
         OnDone();
+    }
+    void AddBlobs(THashMap<TChunkAddress, TString>&& blobData) {
+        for (auto&& i : blobData) {
+            AFL_VERIFY(Blobs.emplace(i.first, std::move(i.second)).second);
+        }
     }
 };
 
 class TColumnsAssembleTask: public NConveyor::ITask {
 private:
     std::shared_ptr<TColumnFetchingContext> Context;
-    THashMap<ui32, std::shared_ptr<NCommon::IKernelFetchLogic>> DataFetchers;
     std::shared_ptr<NResourceBroker::NSubscribe::TResourcesGuard> ResourcesGuard;
 
 private:
     virtual TConclusionStatus DoExecute(const std::shared_ptr<ITask>& /*taskPtr*/) override {
-        NArrow::NAccessor::TAccessorsCollection accessorsResult;
-        NIndexes::TIndexesCollection indexesResult;
-        NCommon::TFetchingResultContext result(accessorsResult, indexesResult, Context->GetConstructor()->GetSource());
-        for (auto& [columnId, fetcher] : DataFetchers) {
-            fetcher->OnDataCollected(result);
-        }
-        auto batch = accessorsResult.ToGeneralContainer(Context->GetConstructor()->GetSource()->GetContext()->GetCommonContext()->GetResolver());
-        Context->OnResult(std::move(batch));
+        Context->BuildResult();
         return TConclusionStatus::Success();
     }
     virtual void DoOnCannotExecute(const TString& reason) override {
@@ -173,10 +83,8 @@ private:
 
 public:
     TColumnsAssembleTask(const std::shared_ptr<TColumnFetchingContext>& context,
-        THashMap<ui32, std::shared_ptr<NCommon::IKernelFetchLogic>>&& dataFetchers,
         const std::shared_ptr<NResourceBroker::NSubscribe::TResourcesGuard>& resourcesGuard)
         : Context(context)
-        , DataFetchers(std::move(dataFetchers))
         , ResourcesGuard(resourcesGuard) {
     }
 };
@@ -186,24 +94,19 @@ private:
     using TBase = NBlobOperations::NRead::ITask;
 
     std::shared_ptr<TColumnFetchingContext> Context;
-    THashMap<ui32, std::shared_ptr<NCommon::IKernelFetchLogic>> DataFetchers;
+    THashMap<TChunkAddress, TBlobRangeLink16> Chunks;
 
     virtual void DoOnDataReady(const std::shared_ptr<NResourceBroker::NSubscribe::TResourcesGuard>& resourcesGuard) override {
         NBlobOperations::NRead::TCompositeReadBlobs blobsData = ExtractBlobsData();
-        TReadActionsCollection readActions;
-        for (auto&& [_, i] : DataFetchers) {
-            i->OnDataReceived(readActions, blobsData);
+
+        auto task = std::make_shared<TColumnsAssembleTask>(Context, resourcesGuard);
+        for (const auto& [chunk, range] : Chunks) {
+            Context->AddBlobs({ { chunk, blobsData.Extract(Context->GetConstructor()->GetSource()->GetColumnStorageId(chunk.GetColumnId()),
+                                             Context->GetConstructor()->GetSource()->RestoreBlobRange(range)) } });
         }
         AFL_VERIFY(blobsData.IsEmpty());
-        if (readActions.IsEmpty()) {
-            auto task = std::make_shared<TColumnsAssembleTask>(Context, std::move(DataFetchers), resourcesGuard);
-            NConveyor::TScanServiceOperator::SendTaskToExecute(
-                task, Context->GetConstructor()->GetSource()->GetContext()->GetCommonContext()->GetConveyorProcessId());
-        } else {
-            std::shared_ptr<TColumnsFetcherTask> nextReadTask =
-                std::make_shared<TColumnsFetcherTask>(std::move(readActions), Context, std::move(DataFetchers));
-            NActors::TActivationContext::AsActorContext().Register(new NOlap::NBlobOperations::NRead::TActor(nextReadTask));
-        }
+        NConveyor::TScanServiceOperator::SendTaskToExecute(
+            task, Context->GetConstructor()->GetSource()->GetContext()->GetCommonContext()->GetConveyorProcessId());
     }
     virtual bool DoOnError(const TString& /*storageId*/, const TBlobRange& range, const IBlobsReadingAction::TErrorStatus& status) override {
         Context->OnError(TStringBuilder() << "Error reading blob range for columns: " << range.ToString() << ", error: "
@@ -213,10 +116,10 @@ private:
 
 public:
     TColumnsFetcherTask(const TReadActionsCollection& actions, const std::shared_ptr<TColumnFetchingContext>& context,
-        THashMap<ui32, std::shared_ptr<NCommon::IKernelFetchLogic>>&& fetchers)
+        THashMap<TChunkAddress, TBlobRangeLink16>&& chunks)
         : TBase(actions, "DUPLICATES", context->GetConstructor()->GetSource()->GetContext()->GetReadMetadata()->GetScanIdentifier())
         , Context(context)
-        , DataFetchers(std::move(fetchers)) {
+        , Chunks(std::move(chunks)) {
     }
 };
 
@@ -258,31 +161,36 @@ private:
     virtual void DoOnRequestsFinished(TDataAccessorsResult&& result) override {
         AFL_VERIFY(!result.HasErrors());
         AFL_VERIFY(result.GetPortions().size() == 1)("count", result.GetPortions().size());
-        const TPortionDataAccessor& portionAccessor = result.GetPortionAccessorVerified(Context->GetConstructor()->GetSource()->GetSourceId());
+        TPortionDataAccessor portionAccessor = std::move(result.ExtractPortionsVector()[0]);
 
         const std::set<ui32> columnIds(Context->GetResultSchema()->GetColumnIds().begin(), Context->GetResultSchema()->GetColumnIds().end());
-        THashMap<ui32, std::shared_ptr<NCommon::IKernelFetchLogic>> fetchers;
+        THashMap<TChunkAddress, TBlobRangeLink16> chunkRanges;
         TReadActionsCollection readActions;
 
         for (const ui32 columnId : columnIds) {
-            std::vector<TColumnRecord> columnCunks;
+            std::vector<TColumnRecord> columnChunks;
             for (const auto* record : portionAccessor.GetColumnChunksPointers(columnId)) {
-                columnCunks.emplace_back(*record);
+                columnChunks.emplace_back(*record);
+                chunkRanges.emplace(record->GetAddress(), record->GetBlobRange());
+            }
+            if (columnChunks.empty()) {
+                continue;
             }
 
-            std::shared_ptr<NCommon::IKernelFetchLogic> fetcher = std::make_shared<TSimpleFetchLogic>(columnId,
-                Context->GetConstructor()->GetSource()->GetContext()->GetCommonContext()->GetStoragesManager(), std::move(columnCunks));
-
-            {
-                NArrow::NAccessor::TAccessorsCollection emptyTable;
-                NIndexes::TIndexesCollection emptyIndexes;
-                NCommon::TFetchingResultContext contextFetch(emptyTable, emptyIndexes, Context->GetConstructor()->GetSource());
-                fetcher->Start(readActions, contextFetch);
+            auto source = Context->GetConstructor()->GetSource();
+            TBlobsAction blobsAction(source->GetContext()->GetCommonContext()->GetStoragesManager(), NBlobOperations::EConsumer::SCAN);
+            auto reading = blobsAction.GetReading(source->GetColumnStorageId(columnId));
+            for (auto&& c : columnChunks) {
+                reading->SetIsBackgroundProcess(false);
+                reading->AddRange(source->RestoreBlobRange(c.BlobRange));
             }
-            fetchers.emplace(columnId, fetcher);
+            for (auto&& i : blobsAction.GetReadingActions()) {
+                readActions.Add(i);
+            }
         }
         AFL_VERIFY(!readActions.IsEmpty());
-        auto fetchingTask = std::make_shared<TColumnsFetcherTask>(std::move(readActions), Context, std::move(fetchers));
+        Context->SetPortionAccessor(std::move(portionAccessor));
+        auto fetchingTask = std::make_shared<TColumnsFetcherTask>(std::move(readActions), Context, std::move(chunkRanges));
 
         const ui64 mem = portionAccessor.GetColumnRawBytes(columnIds, false);
         auto allocationTask = std::make_shared<TColumnsMemoryAllocation>(mem, fetchingTask, Context);

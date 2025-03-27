bda690a40d1a5c54496b78e7c89d776f8eb3bdee
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-03-27 17:21:03 +0300

    counters

--- ydb/core/tx/columnshard/engines/reader/duplicates/merge.h
+++ ydb/core/tx/columnshard/engines/reader/duplicates/merge.h
@@ -11,6 +11,8 @@ class TBuildDuplicateFilters: public NConveyor::ITask {
     class TFiltersBuilder: public NArrow::NMerger::IMergeResultBuilder {
     private:
         THashMap<ui64, NArrow::TColumnFilter> Filters;
+        YDB_READONLY(ui64, RowsAdded, 0);
+        YDB_READONLY(ui64, RowsSkipped, 0);
 
         void AddImpl(const ui64 sourceId, const bool value) {
             auto* findFilter = Filters.FindPtr(sourceId);
@@ -21,10 +23,12 @@ class TBuildDuplicateFilters: public NConveyor::ITask {
     private:
         virtual void AddRecord(const NArrow::NMerger::TBatchIterator& cursor) override {
             AddImpl(cursor.GetSourceId(), true);
+            ++RowsAdded;
         }
 
         virtual void SkipRecord(const NArrow::NMerger::TBatchIterator& cursor) override {
             AddImpl(cursor.GetSourceId(), false);
+            ++RowsSkipped;
         }
 
         virtual void ValidateDataSchema(const std::shared_ptr<arrow::Schema>& /*schema*/) const override {
@@ -62,6 +66,7 @@ private:
     std::vector<std::string> VersionColumnNames;
     ui32 IntervalIdx;
     TActorId Owner;
+    std::shared_ptr<NColumnShard::TScanCounters> Counters;
 
 private:
     virtual TConclusionStatus DoExecute(const std::shared_ptr<ITask>& /*taskPtr*/) override;
@@ -73,11 +78,11 @@ private:
 
 public:
     TBuildDuplicateFilters(const std::shared_ptr<arrow::Schema>& pkSchema, const std::vector<std::string>& versionColumnNames,
-        const ui32 intervalIdx, const TActorId& owner)
+        const ui32 intervalIdx, const TActorId& owner, const std::shared_ptr<NColumnShard::TScanCounters>& counters)
         : PKSchema(pkSchema)
         , VersionColumnNames(versionColumnNames)
         , IntervalIdx(intervalIdx)
-        , Owner(owner) {
+        , Owner(owner), Counters(counters) {
     }
 
     void AddSource(

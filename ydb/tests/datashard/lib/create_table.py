328b419705879a1581bf4f87bd9156be9a6d7391
Denis Prokofev <dennnis@ydb.tech>
2025-03-27 17:46:14 +0300

    split index

--- ydb/tests/datashard/lib/create_table.py
+++ ydb/tests/datashard/lib/create_table.py
@@ -9,6 +9,54 @@ ttl_types = {
     "Timestamp": "CAST(26962{:03}00000000 AS Timestamp)",
 }
 
+index_zero_sync = {
+    "Int64": "CAST({} AS Int64)",
+    "Int32": "CAST({} AS Int32)",
+    # "Int16": "CAST({} AS Int16)", https://github.com/ydb-platform/ydb/issues/15842
+}
+
+index_first_sync = {
+    "Uint64": "CAST({} AS Uint64)",
+    "Uint32": "CAST({} AS Uint32)",
+    # "Uint16": "CAST({} AS Uint16)", https://github.com/ydb-platform/ydb/issues/15842
+}
+
+index_second_sync = {
+    "DyNumber": "CAST('{}E1' AS DyNumber)",
+    "String": "'String {}'",
+    "Utf8": "'Uft8 {}'",
+    "Uuid": "CAST('3{:03}5678-e89b-12d3-a456-556642440000' AS UUID)",
+    "Date": "CAST('2{:03}-01-01' AS Date)",
+    "Datetime": "CAST('2{:03}-10-02T11:00:00Z' AS Datetime)",
+}
+
+index_three_sync = {
+    "Bool": "CAST({} AS Bool)",
+    "Decimal(15,0)": "CAST('{}.0' AS Decimal(15,0))",
+    "Decimal(22,9)": "CAST('{}.123' AS Decimal(22,9))",
+    "Decimal(35,10)": "CAST('{}.123456' AS Decimal(35,10))",
+    "Date32": "CAST('2{:03}-01-01' AS Date32)",
+    "Datetime64": "CAST('2{:03}-10-02T11:00:00Z' AS Datetime64)",
+}
+
+index_three_sync_not_Bool = {
+    "Decimal(15,0)": "CAST('{}.0' AS Decimal(15,0))",
+    "Decimal(22,9)": "CAST('{}.123' AS Decimal(22,9))",
+    "Decimal(35,10)": "CAST('{}.123456' AS Decimal(35,10))",
+    "Date32": "CAST('2{:03}-01-01' AS Date32)",
+    "Datetime64": "CAST('2{:03}-10-02T11:00:00Z' AS Datetime64)",
+}
+
+index_four_sync = {
+    "Timestamp64": "CAST(16962{:03}00000000 AS Timestamp64)",
+    "Interval64": "CAST({} AS Interval64)",
+    "Timestamp": "CAST(16962{:03}00000000 AS Timestamp)",
+    "Interval": "CAST({} AS Interval)",
+    "Int8": "CAST({} AS Int8)",
+    "Uint8": "CAST({} AS Uint8)",
+}
+
+
 index_first = {
     "Int64": "CAST({} AS Int64)",
     "Uint64": "CAST({} AS Uint64)",
@@ -19,19 +67,6 @@ index_first = {
     "Int8": "CAST({} AS Int8)",
     "Uint8": "CAST({} AS Uint8)",
     "Bool": "CAST({} AS Bool)",
-    "Decimal(15,0)": "CAST('{}.0' AS Decimal(15,0))",
-    "Decimal(22,9)": "CAST('{}.123' AS Decimal(22,9))",
-    "Decimal(35,10)": "CAST('{}.123456' AS Decimal(35,10))",
-}
-
-index_first_not_Bool = {
-    "Int64": "CAST({} AS Int64)",
-    "Uint64": "CAST({} AS Uint64)",
-    "Int32": "CAST({} AS Int32)",
-    "Uint32": "CAST({} AS Uint32)",
-    # "Int16": "CAST({} AS Int16)", https://github.com/ydb-platform/ydb/issues/15842
-    # "Uint16": "CAST({} AS Uint16)", https://github.com/ydb-platform/ydb/issues/15842
-    "Int8": "CAST({} AS Int8)",
     "Decimal(15,0)": "CAST('{}.0' AS Decimal(15,0))",
     "Decimal(22,9)": "CAST('{}.123' AS Decimal(22,9))",
     "Decimal(35,10)": "CAST('{}.123456' AS Decimal(35,10))",

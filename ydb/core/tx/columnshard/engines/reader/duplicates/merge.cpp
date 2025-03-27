1a856f1abfb1406f6df4dd1821c7c156d4504749
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-03-27 12:17:41 +0300

    fix: abort, empty interval

--- ydb/core/tx/columnshard/engines/reader/duplicates/merge.cpp
+++ ydb/core/tx/columnshard/engines/reader/duplicates/merge.cpp
@@ -5,17 +5,18 @@ namespace NKikimr::NOlap::NReader {
 
 TConclusionStatus TBuildDuplicateFilters::DoExecute(const std::shared_ptr<ITask>& /*taskPtr*/) {
     NArrow::NMerger::TMergePartialStream merger(PKSchema, nullptr, false, VersionColumnNames);
-    for (ui64 i = 0; i < Sources.size(); ++i) {
-        const auto& source = Sources[i];
-        merger.AddSource(source.GetData(), source.GetFilter(), source.GetSourceId());
-    }
     TFiltersBuilder filtersBuilder;
+    for (const auto& [id, source] : SourcesById) {
+        merger.AddSource(source.GetData(), source.GetFilter(), id);
+        filtersBuilder.AddSource(id);
+    }
     merger.DrainAll(filtersBuilder);
     THashMap<ui64, NArrow::TColumnFilter> filters = std::move(filtersBuilder).ExtractFilters();
-    AFL_VERIFY(filters.size() == Sources.size())("filters", filters.size())("sources", Sources.size());
-    for (const auto& source : Sources) {
-        AFL_VERIFY(source.GetData()->GetRecordsCount() == TValidator::CheckNotNull(filters.FindPtr(source.GetSourceId()))->GetRecordsCountVerified())(
-                "data", source.GetData()->GetRecordsCount())("filter", filters.FindPtr(source.GetSourceId())->GetRecordsCountVerified());
+    AFL_VERIFY(filters.size() == SourcesById.size())("filters", filters.size())("sources", SourcesById.size());
+    for (const auto& [id, source] : SourcesById) {
+        AFL_VERIFY(source.GetData()->GetRecordsCount() == TValidator::CheckNotNull(filters.FindPtr(id))->GetRecordsCount().value_or(0))(
+                                                              "data", source.GetData()->GetRecordsCount())(
+                                                              "filter", filters.FindPtr(id)->GetRecordsCount().value_or(0));
     }
     TActorContext::AsActorContext().Send(Owner, new TEvDuplicateFilterIntervalResult(std::move(filters), IntervalIdx));
     return TConclusionStatus::Success();

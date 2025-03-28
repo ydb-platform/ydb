594f7ae78187b5a088a38a218caf53d5548e1c2f
Nikita Vasilev <ns-vasilev@ydb.tech>
2025-03-28 16:20:33 +0300

    Prepare tests for Sinks (#16393)

--- ydb/core/kqp/opt/kqp_query_plan.cpp
+++ ydb/core/kqp/opt/kqp_query_plan.cpp
@@ -839,15 +839,17 @@ private:
             op.Properties["Table"] = tableData.RelativePath ? *tableData.RelativePath : tablePath;
             op.Properties["Path"] = tablePath;
 
-            const auto& tupleType = stage.Ref().GetTypeAnn()->Cast<TTupleExprType>();
-            YQL_ENSURE(tupleType);
-            YQL_ENSURE(tupleType->GetSize() == 1);
-            const auto& listType = tupleType->GetItems()[0]->Cast<TListExprType>();
-            YQL_ENSURE(listType);
-            const auto& structType = listType->GetItemType()->Cast<TStructExprType>();
-            YQL_ENSURE(structType);
-            for (const auto& item : structType->GetItems()) {
-                writeInfo.Columns.push_back(TString(item->GetName()));
+            if (writeInfo.Type != EPlanTableWriteType::MultiErase) {
+                const auto& tupleType = stage.Ref().GetTypeAnn()->Cast<TTupleExprType>();
+                YQL_ENSURE(tupleType);
+                YQL_ENSURE(tupleType->GetSize() == 1);
+                const auto& listType = tupleType->GetItems()[0]->Cast<TListExprType>();
+                YQL_ENSURE(listType);
+                const auto& structType = listType->GetItemType()->Cast<TStructExprType>();
+                YQL_ENSURE(structType);
+                for (const auto& item : structType->GetItems()) {
+                    writeInfo.Columns.push_back(TString(item->GetName()));
+                }
             }
 
             SerializerCtx.Tables[tablePath].Writes.push_back(writeInfo);

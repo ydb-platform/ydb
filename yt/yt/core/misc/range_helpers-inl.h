1e214be59cbf130bee433c422b42f16148e5acff
robot-piglet <robot-piglet@yandex-team.com>
2025-03-28 17:36:30 +0300

    Intermediate changes commit_hash:75947792e236bf8588efbadca3b510178fca782a

--- yt/yt/core/misc/range_helpers-inl.h
+++ yt/yt/core/misc/range_helpers-inl.h
@@ -9,26 +9,59 @@ namespace NDetail {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+template <class TContainer>
+struct TAppendTo
+{ };
+
+template <class TContainer>
+    requires requires (TContainer container, typename TContainer::value_type value) {
+        container.push_back(value);
+    }
+struct TAppendTo<TContainer>
+{
+    template <class TValue>
+    static void Append(TContainer& container, TValue&& value)
+    {
+        container.push_back(std::forward<TValue>(value));
+    }
+};
+
+template <class TContainer>
+    requires requires (TContainer container, typename TContainer::value_type value) {
+        container.insert(value);
+    }
+struct TAppendTo<TContainer>
+{
+    template <class TValue>
+    static void Append(TContainer& container, TValue&& value)
+    {
+        container.insert(std::forward<TValue>(value));
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
 template <class TContainer, std::ranges::input_range TRange>
 struct TRangeTo
 { };
 
 template <class TContainer, std::ranges::input_range TRange>
-    requires requires (TContainer container, size_t size) {
-        container.reserve(size);
-        container.push_back(std::declval<typename TContainer::value_type>());
+    requires requires (TContainer container, typename TContainer::value_type value) {
+        TAppendTo<TContainer>::Append(container, value);
     }
 struct TRangeTo<TContainer, TRange>
 {
     static auto ToContainer(TRange&& range)
     {
         TContainer container;
-        if constexpr (requires { std::ranges::size(range); }) {
+        if constexpr (requires { std::ranges::size(range); } &&
+            requires { container.reserve(std::declval<size_t>()); })
+        {
             container.reserve(std::ranges::size(range));
         }
 
         for (auto&& element : range) {
-            container.push_back(std::forward<decltype(element)>(element));
+            TAppendTo<TContainer>::Append(container, std::forward<decltype(element)>(element));
         }
 
         return container;
@@ -52,6 +85,14 @@ template <class TContainer, std::ranges::input_range TRange>
 auto RangeTo(TRange&& range)
 {
     return NDetail::TRangeTo<TContainer, TRange>::ToContainer(std::forward<TRange>(range));
+}
+
+template <class TContainer, std::ranges::input_range TRange, class TTransformFunction>
+auto TransformRangeTo(TRange&& range, TTransformFunction&& function)
+{
+    return RangeTo<TContainer>(std::ranges::views::transform(
+        std::forward<TRange>(range),
+        std::forward<TTransformFunction>(function)));
 }
 
 ////////////////////////////////////////////////////////////////////////////////

181d38d90c7eb3430b52ee2b1b7be32c3b13feae
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-03-31 13:07:24 +0300

    rebase

--- ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/context.h
+++ ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/context.h
@@ -11,6 +11,7 @@
 namespace NKikimr::NOlap::NReader::NSimple {
 
 class IDataSource;
+class TSourceConstructor;
 using TColumnsSet = NCommon::TColumnsSet;
 using EStageFeaturesIndexes = NCommon::EStageFeaturesIndexes;
 using TColumnsSetIds = NCommon::TColumnsSetIds;
@@ -48,7 +49,7 @@ public:
         }
     }
 
-    void RegisterDuplicatesManager(const std::deque<std::shared_ptr<IDataSource>>& sources);
+    void RegisterDuplicatesManager(const std::deque<TSourceConstructor>& sources, const std::shared_ptr<TSpecialReadContext>& self);
     void OnSourceFinished(const std::shared_ptr<NCommon::IDataSource>& source);
     void OnSourcesSkipped(const std::vector<std::shared_ptr<NCommon::IDataSource>>& sources);
 };

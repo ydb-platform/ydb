d0cb4467ae984459d7e279371dd199792c3f81dd
Alexander Kotov <akotov@ydb.tech>
2025-03-31 18:58:37 +0300

    [*] refactoring

--- ydb/core/persqueue/partition.h
+++ ydb/core/persqueue/partition.h
@@ -704,7 +704,7 @@ private:
 
     TPartitionWorkZone WorkZone;
 
-    std::deque<std::pair<ui64,ui64>> GapOffsets;
+    std::deque<std::pair<ui64, ui64>> GapOffsets;
     ui64 GapSize;
 
     TString CloudId;

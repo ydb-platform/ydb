6573d20bf29be399a7adcfee600263ad43d2de71
Emil Garipov <emgariko@ydb.tech>
2025-04-01 16:37:23 +0300

    adjusted test parameters

--- ydb/tests/olap/test_log_scenario.py
+++ ydb/tests/olap/test_log_scenario.py
@@ -125,12 +125,12 @@ class TestLogScenario(object):
     def test(self):
         """As per https://github.com/ydb-platform/ydb/issues/13530"""
 
-        wait_time: int = int(get_external_param("wait_minutes", "1")) * 60
+        wait_time: int = int(get_external_param("wait_seconds", "30"))
         self.table_name: str = "log"
 
         ydb_workload: YdbWorkloadLog = YdbWorkloadLog(endpoint=self.ydb_client.endpoint, database=self.ydb_client.database, table_name=self.table_name)
         ydb_workload.create_table(self.table_name)
-        ydb_workload.bulk_upsert(seconds=60, threads=10, rows=500, wait=True)
+        ydb_workload.bulk_upsert(seconds=10, threads=10, rows=500, wait=True)
         logging.info(f"Count rows after insert {self.get_row_count()} before wait")
 
         assert self.get_row_count() != 0

ec9d668367645c229dc688807a71e91d0c2f8029
Kamil Khamitov <kkhamitov@yandex-team.ru>
2025-04-03 20:22:16 +0300

    (-) fix tests

--- ydb/core/testlib/basics/appdata.h
+++ ydb/core/testlib/basics/appdata.h
@@ -114,6 +114,7 @@ namespace NKikimr {
         NKikimrConfig::TImmediateControlsConfig ImmediateControlsConfig;
         NKikimrResourceBroker::TResourceBrokerConfig ResourceBrokerConfig;
         std::vector<TIntrusivePtr<NKikimr::TControlBoard>> Icb;
+        std::vector<TIntrusivePtr<NKikimr::TDynamicControlBoard>> DynamicControlBoard;
 
     private:
         TAutoPtr<TMine> Mine;

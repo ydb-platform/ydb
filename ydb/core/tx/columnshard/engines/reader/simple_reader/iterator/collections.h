a0075d345c056885dd55f88eb601071a5e8b57d8
ivanmorozov333 <ivanmorozov@hotmail.com>
2025-04-05 09:53:29 +0300

    fix

--- ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/collections.h
+++ ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/collections.h
@@ -66,6 +66,7 @@ class TNotSortedCollection: public ISourcesCollection {
 private:
     using TBase = ISourcesCollection;
     std::optional<ui32> Limit;
+    ui32 InFlightLimit = 1;
     std::deque<TSourceConstructor> Sources;
     TPositiveControlInteger InFlightCount;
     ui32 FetchedCount = 0;
@@ -84,9 +85,12 @@ private:
         return result;
     }
     virtual bool DoCheckInFlightLimits() const override {
-        return InFlightCount < GetMaxInFlight();
+        return InFlightCount < InFlightLimit;
     }
     virtual void DoOnSourceFinished(const std::shared_ptr<IDataSource>& source) override {
+        if (!source->GetResultRecordsCount() && InFlightLimit * 2 < GetMaxInFlight()) {
+            InFlightLimit *= 2;
+        }
         FetchedCount += source->GetResultRecordsCount();
         if (Limit && *Limit <= FetchedCount && Sources.size()) {
             AFL_NOTICE(NKikimrServices::TX_COLUMNSHARD)("event", "limit_exhausted")("limit", Limit)("fetched", FetchedCount);
@@ -101,6 +105,11 @@ public:
         : TBase(context)
         , Limit(limit)
     {
+        if (Limit) {
+            InFlightLimit = 1;
+        } else {
+            InFlightLimit = GetMaxInFlight();
+        }
         if (cursor && cursor->IsInitialized()) {
             while (sources.size()) {
                 bool usage = false;

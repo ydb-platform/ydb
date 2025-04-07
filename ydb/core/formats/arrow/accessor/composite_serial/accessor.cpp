93f1b6bb180486fe1750eacecb3ab6559696084b
ivanmorozov333 <ivanmorozov@hotmail.com>
2025-04-07 17:28:46 +0300

    logging deserialization control

--- ydb/core/formats/arrow/accessor/composite_serial/accessor.cpp
+++ ydb/core/formats/arrow/accessor/composite_serial/accessor.cpp
@@ -2,6 +2,7 @@
 
 #include <ydb/core/formats/arrow/accessor/sparsed/constructor.h>
 
+#include <ydb/library/actors/core/log.h>
 #include <ydb/library/actors/prof/tag.h>
 
 namespace NKikimr::NArrow::NAccessor {
@@ -10,6 +11,10 @@ IChunkedArray::TLocalChunkedArrayAddress TDeserializeChunkedArray::DoGetLocalChu
     const std::optional<TCommonChunkAddress>& /*chunkCurrent*/, const ui64 /*position*/) const {
     if (PredefinedArray) {
         return TLocalChunkedArrayAddress(PredefinedArray, 0, 0);
+    }
+    if (Counter.Inc() > 1) {
+        AFL_WARN(NKikimrServices::ARROW_HELPER)("event", "many_deserializations")("counter", Counter.Val())("size", Data.size())(
+            "buffer", DataBuffer.size());
     }
     if (!!Data) {
         return TLocalChunkedArrayAddress(Loader->ApplyVerified(Data, GetRecordsCount()), 0, 0);

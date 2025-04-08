f0c54e1c108d8138e5b12c9262d31ba3b8cc2a14
StekPerepolnen <arykov@ydb.tech>
2025-04-08 17:19:55 +0200

    changed field default

--- ydb/core/health_check/health_check.cpp
+++ ydb/core/health_check/health_check.cpp
@@ -1722,9 +1722,7 @@ public:
             auto& groupState = GroupState[groupId];
             groupState.ErasureSpecies = group.GetInfo().GetErasureSpeciesV2();
             groupState.Generation = group.GetInfo().GetGeneration();
-            if (group.GetInfo().HasLayoutCorrect()) {
-                groupState.LayoutCorrect = group.GetInfo().GetLayoutCorrect();
-            }
+            groupState.LayoutCorrect = group.GetInfo().GetLayoutCorrect();
             StoragePoolState[poolId].Groups.emplace(groupId);
         }
         for (const auto& vSlot : VSlots->Get()->Record.GetEntries()) {
@@ -2158,7 +2156,6 @@ public:
                                  TStringBuilder() << "Unknown PDisk state: " << statusString,
                                  ETags::PDiskState);
         }
-
         switch (status->number()) {
             case NKikimrBlobStorage::ACTIVE:
             case NKikimrBlobStorage::INACTIVE: {

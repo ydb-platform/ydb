96b68b2a6ae5da120654548529011b368d711fa8
robot-piglet <robot-piglet@yandex-team.com>
2025-04-09 18:57:10 +0300

    Intermediate changes commit_hash:f430adf87d23165dab8f8ee82ab299ced5de4b59

--- yql/essentials/udfs/common/protobuf/test/cases/yt_mode_plain.sql
+++ yql/essentials/udfs/common/protobuf/test/cases/yt_mode_plain.sql
@@ -106,6 +106,12 @@ $udfParPB = Udf(Protobuf::Parse, $configPB as TypeConfig);
 $udfSerPB = Udf(Protobuf::Serialize, $configPB as TypeConfig);
 
 SELECT TestField,
+      $udfParNO(TestField),
+      $udfSerNO($udfParNO(TestField)),
+      $udfParYT(TestField),
+      $udfSerYT($udfParYT(TestField)),
+      $udfParPB(TestField),
+      $udfSerPB($udfParPB(TestField)),
        Ensure("Success", $udfParNO(TestField) == $udfParNO($udfSerNO($udfParNO(TestField))), "Fail"),
        Ensure("Success", $udfParYT(TestField) == $udfParYT($udfSerYT($udfParYT(TestField))), "Fail"),
        Ensure("Success", $udfParPB(TestField) == $udfParPB($udfSerPB($udfParPB(TestField))), "Fail")

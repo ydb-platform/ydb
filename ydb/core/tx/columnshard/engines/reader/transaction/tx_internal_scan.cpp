dc721a8a5c65e54a078bd694cdb129206bc2c2cc
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-04-10 16:01:41 +0300

    configuration

--- ydb/core/tx/columnshard/engines/reader/transaction/tx_internal_scan.cpp
+++ ydb/core/tx/columnshard/engines/reader/transaction/tx_internal_scan.cpp
@@ -49,7 +49,7 @@ void TTxInternalScan::Complete(const TActorContext& ctx) {
         read.PathId = request.GetPathId();
         read.LockId = LockId;
         read.ReadNothing = !Self->TablesManager.HasTable(read.PathId);
-        read.DeduplicationPolicy = EDeduplicationPolicy::PREVENT_DUPLICATES;
+        read.DeduplicationPolicy = EDeduplicationPolicy::NO_DUPLICATES;
         std::unique_ptr<IScannerConstructor> scannerConstructor(new NPlain::TIndexScannerConstructor(context));
         read.ColumnIds = request.GetColumnIds();
         if (request.RangesFilter) {

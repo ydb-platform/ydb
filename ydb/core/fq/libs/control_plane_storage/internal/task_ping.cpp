ydb-platform/ydb

commit 3321dfa2cccbc1dfe52c7db872a69bdce11f641e

YQ-4188 Add task controller config and add new sensors (#16437)

ydb/core/fq/libs/control_plane_storage/internal/task_ping.cpp
@@ -65,7 +65,7 @@ TYdbControlPlaneStorageActor::TPingTaskParams TYdbControlPlaneStorageActor::Cons
         "   WHERE `" SCOPE_COLUMN_NAME "` = $scope AND `" QUERY_ID_COLUMN_NAME "` = $query_id;\n"
         "SELECT `" JOB_ID_COLUMN_NAME "`, `" JOB_COLUMN_NAME "` FROM `" JOBS_TABLE_NAME "`\n"
         "   WHERE `" SCOPE_COLUMN_NAME "` = $scope AND `" QUERY_ID_COLUMN_NAME "` = $query_id AND `" JOB_ID_COLUMN_NAME "` = $last_job_id;\n"
-        "SELECT `" OWNER_COLUMN_NAME "`, `" RETRY_COUNTER_COLUMN_NAME "`, `" RETRY_COUNTER_UPDATE_COLUMN_NAME "`, `" RETRY_RATE_COLUMN_NAME "`\n"
+        "SELECT `" OWNER_COLUMN_NAME "`, `" RETRY_COUNTER_COLUMN_NAME "`, `" RETRY_COUNTER_UPDATE_COLUMN_NAME "`, `" RETRY_RATE_COLUMN_NAME "`, `" ASSIGNED_UNTIL_COLUMN_NAME "`\n"
         "FROM `" PENDING_SMALL_TABLE_NAME "` WHERE `" TENANT_COLUMN_NAME "` = $tenant AND `" SCOPE_COLUMN_NAME "` = $scope AND `" QUERY_ID_COLUMN_NAME "` = $query_id;\n"
     );
 
@@ -119,6 +119,8 @@ TYdbControlPlaneStorageActor::TPingTaskParams TYdbControlPlaneStorageActor::Cons
             if (owner != request.owner_id()) {
                 ythrow NYql::TCodeLineException(TIssuesIds::BAD_REQUEST) << "OWNER of QUERY ID = \"" << request.query_id().value() << "\" MISMATCHED: \"" << request.owner_id() << "\" (received) != \"" << owner << "\" (selected)";
             }
+            auto assignedUntil = parser.ColumnParser(ASSIGNED_UNTIL_COLUMN_NAME).GetOptionalTimestamp().value_or(TInstant::Now());
+            Counters.LeaseLeftMs->Collect((assignedUntil - TInstant::Now()).MilliSeconds());
             retryLimiter.Assign(
                 parser.ColumnParser(RETRY_COUNTER_COLUMN_NAME).GetOptionalUint64().value_or(0),
                 parser.ColumnParser(RETRY_COUNTER_UPDATE_COLUMN_NAME).GetOptionalTimestamp().value_or(TInstant::Zero()),
@@ -252,7 +254,7 @@ TYdbControlPlaneStorageActor::TPingTaskParams TYdbControlPlaneStorageActor::Cons
     readQueryBuilder.AddText(
         "SELECT `" INTERNAL_COLUMN_NAME "`\n"
         "FROM `" QUERIES_TABLE_NAME "` WHERE `" QUERY_ID_COLUMN_NAME "` = $query_id AND `" SCOPE_COLUMN_NAME "` = $scope;\n"
-        "SELECT `" OWNER_COLUMN_NAME "`\n"
+        "SELECT `" OWNER_COLUMN_NAME "`, `" ASSIGNED_UNTIL_COLUMN_NAME "`\n"
         "FROM `" PENDING_SMALL_TABLE_NAME "` WHERE `" TENANT_COLUMN_NAME "` = $tenant AND `" SCOPE_COLUMN_NAME "` = $scope AND `" QUERY_ID_COLUMN_NAME "` = $query_id;\n"
     );
 
@@ -285,6 +287,8 @@ TYdbControlPlaneStorageActor::TPingTaskParams TYdbControlPlaneStorageActor::Cons
             if (owner != request.owner_id()) {
                 ythrow NYql::TCodeLineException(TIssuesIds::BAD_REQUEST) << "OWNER of QUERY ID = \"" << request.query_id().value() << "\" MISMATCHED: \"" << request.owner_id() << "\" (received) != \"" << owner << "\" (selected)";
             }
+            auto assignedUntil = parser.ColumnParser(ASSIGNED_UNTIL_COLUMN_NAME).GetOptionalTimestamp().value_or(TInstant::Now());
+            Counters.LeaseLeftMs->Collect((assignedUntil - TInstant::Now()).MilliSeconds());
         }
 
         TInstant ttl = TInstant::Now() + Config->TaskLeaseTtl;
@@ -657,7 +661,9 @@ void TYdbControlPlaneStorageActor::Handle(TEvControlPlaneStorage::TEvPingTaskReq
     std::shared_ptr<Fq::Private::PingTaskResult> response = std::make_shared<Fq::Private::PingTaskResult>();
     std::shared_ptr<TFinalStatus> finalStatus = std::make_shared<TFinalStatus>();
 
-    auto pingTaskParams = DoesPingTaskUpdateQueriesTable(request) ?
+    bool isHard = DoesPingTaskUpdateQueriesTable(request);
+    Counters.Counters->GetCounter(isHard ? "HardPing" : "SoftPing", true)->Inc();
+    auto pingTaskParams = isHard ?
         ConstructHardPingTask(request, response, finalStatus, requestCounters.Common) :
         ConstructSoftPingTask(request, response, requestCounters.Common);
     auto debugInfo = Config->Proto.GetEnableDebugMode() ? std::make_shared<TDebugInfo>() : TDebugInfoPtr{};

ydb/core/fq/libs/actors/run_actor.cpp
@@ -1571,6 +1571,18 @@ private:
             ClearResultFormatSettings();
         }
 
+        TDuration pingPeriod = TDuration::Seconds(3);
+        TDuration aggrPeriod = TDuration::Seconds(1);
+        {
+            const auto& taskControllerConfig = Params.Config.GetTaskController();
+            if (taskControllerConfig.GetPingPeriod()) {
+                Y_ABORT_UNLESS(TDuration::TryParse(taskControllerConfig.GetPingPeriod(), pingPeriod));
+            }
+            if (taskControllerConfig.GetAggrPeriod()) {
+                Y_ABORT_UNLESS(TDuration::TryParse(taskControllerConfig.GetAggrPeriod(), aggrPeriod));
+            }
+        }
+
         if (enableCheckpointCoordinator) {
             ControlId = Register(MakeCheckpointCoordinator(
                 ::NFq::TCoordinatorId(Params.QueryId + "-" + ToString(DqGraphIndex), Params.PreviousQueryRevision),
@@ -1587,8 +1599,8 @@ private:
                 resultId,
                 dqConfiguration,
                 QueryCounters,
-                TDuration::Seconds(3),
-                TDuration::Seconds(1)
+                pingPeriod,
+                aggrPeriod
                 ).Release());
         } else {
             ControlId = Register(NYql::MakeTaskController(
@@ -1597,7 +1609,8 @@ private:
                 resultId,
                 dqConfiguration,
                 QueryCounters,
-                TDuration::Seconds(3)
+                pingPeriod,
+                aggrPeriod
             ).Release());
         }
 

ydb/core/fq/libs/config/protos/fq_config.proto
@@ -23,6 +23,7 @@ import "ydb/core/fq/libs/config/protos/rate_limiter.proto";
 import "ydb/core/fq/libs/config/protos/read_actors_factory.proto";
 import "ydb/core/fq/libs/config/protos/resource_manager.proto";
 import "ydb/core/fq/libs/config/protos/row_dispatcher.proto";
+import "ydb/core/fq/libs/config/protos/task_controller.proto";
 import "ydb/core/fq/libs/config/protos/test_connection.proto";
 import "ydb/core/fq/libs/config/protos/token_accessor.proto";
 import "ydb/library/folder_service/proto/config.proto";
@@ -55,4 +56,5 @@ message TConfig {
     bool EnableTaskCounters = 23;
     TComputeConfig Compute = 24;
     TRowDispatcherConfig RowDispatcher = 25;
+    TTaskControllerConfig TaskController = 26;
 }

ydb/core/fq/libs/config/protos/task_controller.proto
@@ -1,0 +1,12 @@
+syntax = "proto3";
+option cc_enable_arenas = true;
+
+package NFq.NConfig;
+option java_package = "ru.yandex.kikimr.proto";
+
+////////////////////////////////////////////////////////////
+
+message TTaskControllerConfig {
+    string PingPeriod = 1;
+    string AggrPeriod = 2;
+}

ydb/core/fq/libs/config/protos/ya.make
@@ -24,6 +24,7 @@ SRCS(
     resource_manager.proto
     row_dispatcher.proto
     storage.proto
+    task_controller.proto
     test_connection.proto
     token_accessor.proto
 )

ydb/core/fq/libs/control_plane_storage/ydb_control_plane_storage_impl.h
@@ -522,11 +522,13 @@ protected:
 
     public:
         ::NMonitoring::TDynamicCounterPtr Counters;
+        ::NMonitoring::THistogramPtr LeaseLeftMs;
 
         explicit TCounters(const ::NMonitoring::TDynamicCounterPtr& counters, const ::NFq::TControlPlaneStorageConfig& config)
             : ScopeCounters{TTtlCacheSettings{}.SetTtl(config.MetricsTtl)}
             , FinalStatusCounters{TTtlCacheSettings{}.SetTtl(config.MetricsTtl)}
             , Counters(counters)
+            , LeaseLeftMs(Counters->GetHistogram("LeaseLeftMs", ::NMonitoring::ExplicitHistogram({100, 1000, 5000, 10000, 20000})))
         {
             for (auto& request: CommonRequests) {
                 request->Register(Counters);

ydb/library/db_pool/db_pool.cpp
@@ -22,6 +22,7 @@ class TDbPoolActor : public NActors::TActor<TDbPoolActor> {
         const NMonitoring::THistogramPtr RequestsTime;
         const ::NMonitoring::TDynamicCounterPtr StatusSubgroup;
         TMap<TString, ::NMonitoring::TDynamicCounters::TCounterPtr> Status;
+        const ::NMonitoring::TDynamicCounters::TCounterPtr IncomingRate;
 
         TCounters(const ::NMonitoring::TDynamicCounterPtr& counters)
             : Counters(counters)
@@ -29,6 +30,7 @@ class TDbPoolActor : public NActors::TActor<TDbPoolActor> {
             , TotalInFlight(counters->GetSubgroup("subcomponent",  "DbPool")->GetCounter("TotalInflight"))
             , RequestsTime(counters->GetSubgroup("subcomponent", "DbPool")->GetHistogram("RequestTimeMs", NMonitoring::ExponentialHistogram(6, 3, 100)))
             , StatusSubgroup(counters->GetSubgroup("subcomponent", "DbPool")->GetSubgroup("component", "status"))
+            , IncomingRate(counters->GetSubgroup("subcomponent",  "DbPool")->GetCounter("IncomingRate", true))
         {}
 
         ::NMonitoring::TDynamicCounters::TCounterPtr GetStatus(const NYdb::TStatus& status) {
@@ -129,6 +131,7 @@ public:
 
     void HandleRequest(TEvents::TEvDbRequest::TPtr& ev) {
         LOG_D("TDbPoolActor: TEvDbRequest " << SelfId() << " Queue size = " << Requests.size());
+        Counters.IncomingRate->Inc();
         auto request = ev->Get();
         Requests.emplace_back(TRequest{ev->Sender, ev->Cookie, request->Sql, std::move(request->Params), request->Idempotent});
         ProcessQueue();
@@ -152,6 +155,7 @@ public:
 
     void HandleRequest(TEvents::TEvDbFunctionRequest::TPtr& ev) {
         LOG_T("TDbPoolActor: TEvDbFunctionRequest " << SelfId() << " Queue size = " << Requests.size());
+        Counters.IncomingRate->Inc();
         auto request = ev->Get();
         Requests.emplace_back(TFunctionRequest{ev->Sender, ev->Cookie, std::move(request->Handler)});
         ProcessQueue();
@@ -203,7 +207,6 @@ private:
     bool RequestInProgress = false;
     TInstant RequestInProgressTimestamp = TInstant::Now();
     std::shared_ptr<int> State = std::make_shared<int>();
-
 };
 
 TDbPool::TDbPool(

ydb/tests/tools/fq_runner/kikimr_runner.py
@@ -518,6 +518,9 @@ class YqTenant(BaseTenant):
             if len(self.config_generator.dc_mapping) > 0:
                 fq_config['nodes_manager']['use_data_center'] = True
             fq_config['enable_task_counters'] = True
+            fq_config['task_controller'] = {}
+            fq_config['task_controller']['ping_period'] = "5s"      # task_lease_ttl / 4
+            fq_config['task_controller']['aggr_period'] = "1s"
         else:
             fq_config['nodes_manager']['enabled'] = False
             fq_config['pending_fetcher']['enabled'] = False

commit f36e5dc4dc127435340ad14454521c329927ecdb

Fix potential use-after-free in task_ping.cpp
Counters here is this->Counters and this is captured by lambda.
regression by 3321dfa2cccbc1dfe52c7db872a69bdce11f641e / #16437

ydb/core/fq/libs/control_plane_storage/internal/task_ping.cpp
@@ -71,7 +71,7 @@ TYdbControlPlaneStorageActor::TPingTaskParams TYdbControlPlaneStorageActor::Cons
 
     auto meteringRecords = std::make_shared<std::vector<TString>>();
 
-    auto prepareParams = [=, counters=counters, actorSystem = NActors::TActivationContext::ActorSystem(), request=request](const std::vector<TResultSet>& resultSets) mutable {
+    auto prepareParams = [=, counters=counters, actorSystem = NActors::TActivationContext::ActorSystem(), request=request, leaseLeftMs = Counters.LeaseLeftMs](const std::vector<TResultSet>& resultSets) mutable {
         TString jobId;
         FederatedQuery::Query query;
         FederatedQuery::Internal::QueryInternal internal;
@@ -120,7 +120,7 @@ TYdbControlPlaneStorageActor::TPingTaskParams TYdbControlPlaneStorageActor::Cons
                 ythrow NYql::TCodeLineException(TIssuesIds::BAD_REQUEST) << "OWNER of QUERY ID = \"" << request.query_id().value() << "\" MISMATCHED: \"" << request.owner_id() << "\" (received) != \"" << owner << "\" (selected)";
             }
             auto assignedUntil = parser.ColumnParser(ASSIGNED_UNTIL_COLUMN_NAME).GetOptionalTimestamp().value_or(TInstant::Now());
-            Counters.LeaseLeftMs->Collect((assignedUntil - TInstant::Now()).MilliSeconds());
+            leaseLeftMs->Collect((assignedUntil - TInstant::Now()).MilliSeconds());
             retryLimiter.Assign(
                 parser.ColumnParser(RETRY_COUNTER_COLUMN_NAME).GetOptionalUint64().value_or(0),
                 parser.ColumnParser(RETRY_COUNTER_UPDATE_COLUMN_NAME).GetOptionalTimestamp().value_or(TInstant::Zero()),
@@ -258,7 +258,7 @@ TYdbControlPlaneStorageActor::TPingTaskParams TYdbControlPlaneStorageActor::Cons
         "FROM `" PENDING_SMALL_TABLE_NAME "` WHERE `" TENANT_COLUMN_NAME "` = $tenant AND `" SCOPE_COLUMN_NAME "` = $scope AND `" QUERY_ID_COLUMN_NAME "` = $query_id;\n"
     );
 
-    auto prepareParams = [=](const std::vector<TResultSet>& resultSets) {
+    auto prepareParams = [=, leaseLeftMs = Counters.LeaseLeftMs](const std::vector<TResultSet>& resultSets) {
         TString owner;
         FederatedQuery::Internal::QueryInternal internal;
 
@@ -288,7 +288,7 @@ TYdbControlPlaneStorageActor::TPingTaskParams TYdbControlPlaneStorageActor::Cons
                 ythrow NYql::TCodeLineException(TIssuesIds::BAD_REQUEST) << "OWNER of QUERY ID = \"" << request.query_id().value() << "\" MISMATCHED: \"" << request.owner_id() << "\" (received) != \"" << owner << "\" (selected)";
             }
             auto assignedUntil = parser.ColumnParser(ASSIGNED_UNTIL_COLUMN_NAME).GetOptionalTimestamp().value_or(TInstant::Now());
-            Counters.LeaseLeftMs->Collect((assignedUntil - TInstant::Now()).MilliSeconds());
+            leaseLeftMs->Collect((assignedUntil - TInstant::Now()).MilliSeconds());
         }
 
         TInstant ttl = TInstant::Now() + Config->TaskLeaseTtl;
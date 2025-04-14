5703368cf40a6faa3b30329f5b2961da2af6427b
ivanmorozov333 <ivanmorozov@ydb.tech>
2025-04-14 14:28:07 +0300

    additional signals about serialization (#17149)
    Co-authored-by: ivanmorozov333 <imorozov333@ya.ru>

--- ydb/core/formats/arrow/accessor/sub_columns/signals.h
+++ ydb/core/formats/arrow/accessor/sub_columns/signals.h
@@ -10,25 +10,32 @@ private:
     using TBase = NColumnShard::TCommonCountersOwner;
 
     NMonitoring::THistogramPtr HistogramRawDataSizeBytes;
-    NMonitoring::THistogramPtr HistogramBlobDataSizeBytes;
     NMonitoring::THistogramPtr HistogramRawDataSizeCount;
+    NMonitoring::THistogramPtr HistogramRawDataSizeDuration;
+
+    NMonitoring::THistogramPtr HistogramBlobDataSizeBytes;
     NMonitoring::THistogramPtr HistogramBlobDataSizeCount;
+    NMonitoring::THistogramPtr HistogramBlobDataSizeDuration;
 
 public:
     TCategorySignals(NColumnShard::TCommonCountersOwner& owner, const TString& categoryName)
         : TBase(owner, "category", categoryName)
         , HistogramRawDataSizeBytes(TBase::GetHistogram("RawData/BySize/Bytes", NMonitoring::ExponentialHistogram(15, 2, 100)))
-        , HistogramBlobDataSizeBytes(TBase::GetHistogram("BlobData/BySize/Bytes", NMonitoring::ExponentialHistogram(15, 2, 100)))
         , HistogramRawDataSizeCount(TBase::GetHistogram("RawData/BySize/Count", NMonitoring::ExponentialHistogram(15, 2, 100)))
-        , HistogramBlobDataSizeCount(TBase::GetHistogram("BlobData/BySize/Count", NMonitoring::ExponentialHistogram(15, 2, 100))) {
+        , HistogramRawDataSizeDuration(TBase::GetHistogram("RawData/BySize/Duration/Us", NMonitoring::ExponentialHistogram(15, 2, 100)))
+        , HistogramBlobDataSizeBytes(TBase::GetHistogram("BlobData/BySize/Bytes", NMonitoring::ExponentialHistogram(15, 2, 100)))
+        , HistogramBlobDataSizeCount(TBase::GetHistogram("BlobData/BySize/Count", NMonitoring::ExponentialHistogram(15, 2, 100)))
+        , HistogramBlobDataSizeDuration(TBase::GetHistogram("BlobData/BySize/Duration/Us", NMonitoring::ExponentialHistogram(15, 2, 100))) {
     }
 
-    void OnBlobSize(const i64 rawDataSize, const i64 blobDataSize) const {
+    void OnBlobSize(const i64 rawDataSize, const i64 blobDataSize, const TDuration d) const {
         HistogramBlobDataSizeBytes->Collect(blobDataSize, blobDataSize);
-        HistogramRawDataSizeBytes->Collect(rawDataSize, rawDataSize);
-
         HistogramBlobDataSizeCount->Collect(blobDataSize);
+        HistogramBlobDataSizeDuration->Collect(blobDataSize, d.MicroSeconds());
+
+        HistogramRawDataSizeBytes->Collect(rawDataSize, rawDataSize);
         HistogramRawDataSizeCount->Collect(rawDataSize);
+        HistogramRawDataSizeDuration->Collect(rawDataSize, d.MicroSeconds());
     }
 };
 

9307d6a179a6846c65508098c80441d6778eb334
Nikolay Perfilov <pnv1@yandex-team.ru>
2025-04-15 12:55:56 +0300

    First iteration, blank command

---
+++ ydb/public/lib/ydb_cli/commands/ydb_table_infer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ydb_command.h"
+#include "ydb_common.h"
+
+#include <ydb/public/lib/ydb_cli/common/format.h>
+
+namespace NYdb::NConsoleClient {
+
+class TCommandTableInfer : public TClientCommandTree {
+public:
+    TCommandTableInfer();
+};
+
+class TCommandTableInferFile : public TClientCommandTree {
+public:
+    TCommandTableInferFile();
+};
+
+class TCommandTableInferCsv : public TYdbCommand, public TCommandWithPath, public TCommandWithInput {
+public:
+    TCommandTableInferCsv();
+    virtual void Config(TConfig& config) override;
+    virtual void Parse(TConfig& config) override;
+    virtual int Run(TConfig& config) override;
+
+private:
+    std::vector<std::string> FilePaths;
+    TString HeaderRow;
+    std::optional<TString> NullValue;
+    bool Header = false;
+};
+
+}

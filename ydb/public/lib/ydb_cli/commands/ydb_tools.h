76c1e652e03bc7336170a32b9398d725979b60e2
Daniil Demin <deminds@ydb.tech>
2025-04-15 12:57:50 +0000

    add cli options overwrite and ignore drop errors

--- ydb/public/lib/ydb_cli/commands/ydb_tools.h
+++ ydb/public/lib/ydb_cli/commands/ydb_tools.h
@@ -61,6 +61,8 @@ private:
     bool RestoreACL = true;
     bool SkipDocumentTables = false;
     bool SavePartialResult = false;
+    bool Overwrite = false;
+    bool VerifyExistence = false;
     TString UploadBandwidth;
     TString UploadRps;
     TString RowsPerRequest;

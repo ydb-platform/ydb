32412494b3281d8de055fe3a3fcf902b1bb9ba3f
st-shchetinin <st-shchetinin@ydb.tech>
2025-04-17 20:32:31 +0300

    removed

--- ydb/tests/functional/compatibility/test_compatibility.py
+++ ydb/tests/functional/compatibility/test_compatibility.py
@@ -142,8 +142,3 @@ class TestCompatibility(object):
         ]
 
         yatest.common.execute(export_command, wait=True, stdout=self.output_f, stderr=self.output_f)
-
-        s3_resource = boto3.resource("s3", endpoint_url=s3_endpoint, 
-                                   aws_access_key_id=s3_access_key,
-                                   aws_secret_access_key=s3_secret_key)
-

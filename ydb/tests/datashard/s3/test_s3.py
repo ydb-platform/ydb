873928f96112a3d287b31985774fbe2e428c856d
Denis Prokofev <dennnis@ydb.tech>
2025-04-18 16:32:34 +0300

    fix

--- ydb/tests/datashard/s3/test_s3.py
+++ ydb/tests/datashard/s3/test_s3.py
@@ -1,13 +1,13 @@
 import yatest
 import pytest
+import os
 
+from ydb.tests.library.common.wait_for import wait_for
 from ydb.tests.sql.lib.test_base import TestBase
 from ydb.tests.sql.lib.test_s3 import S3Base
-from ydb.tests.stress.oltp_workload.workload import cleanup_type_name
-from ydb.tests.datashard.lib.create_table import create_table, create_ttl, pk_types, non_pk_types, index_first, index_second, ttl_types, \
+from ydb.tests.datashard.lib.types_of_variables import pk_types, non_pk_types, index_first, index_second, \
     index_first_sync, index_second_sync, index_three_sync, index_four_sync, index_zero_sync
-import os
-import time
+from ydb.tests.datashard.lib.dml_operations import DMLOperations
 
 
 class TestYdbS3TTL(TestBase, S3Base):
@@ -49,16 +49,15 @@ class TestYdbS3TTL(TestBase, S3Base):
     )
     def test_s3(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
         s3_client = self.s3_session_client()
-
         s3_client.create_bucket(Bucket=self.bucket_name())
-        print(self.bucket_name())
-        self.create_table(table_name, pk_types, all_types,
-                          index, ttl, unique, sync)
-        self.insert(table_name, all_types, pk_types, index, ttl)
+        dml = DMLOperations(self)
+        dml.create_table(table_name, pk_types, all_types,
+                         index, ttl, unique, sync)
+        dml.insert(table_name, all_types, pk_types, index, ttl)
         self.is_import_or_export(False, table_name)
-        self.query(f"drop table {table_name}")
+        dml.query(f"drop table {table_name}")
         self.is_import_or_export(True, table_name)
-        self.select_after_insert(table_name, all_types, pk_types, index, ttl)
+        dml.select_after_insert(table_name, all_types, pk_types, index, ttl)
 
     def is_import_or_export(self, is_import: bool, table_name: str):
         cmd_test = yatest.common.execute([
@@ -81,22 +80,7 @@ class TestYdbS3TTL(TestBase, S3Base):
             url += cmd_test[position]
             position += 1
 
-        i = 0
-        while True:
-            time.sleep(0.1)
-            i += 1
-            if i == 10:
-                break
-            cmd = yatest.common.execute([
-                yatest.common.binary_path(os.getenv('YDB_CLI_BINARY')),
-                '-e', 'grpc://'+self.get_endpoint(),
-                "--database", self.get_database(),
-                "operation", "get",
-                url,
-            ])
-            if "Done" in cmd._std_out.decode('utf-8'):
-                break
-
+        wait_for(self.create_predicate(url), timeout_seconds=100)
         yatest.common.execute([
             yatest.common.binary_path(os.getenv('YDB_CLI_BINARY')),
             '-e', 'grpc://'+self.get_endpoint(),
@@ -105,77 +89,14 @@ class TestYdbS3TTL(TestBase, S3Base):
             url,
         ])
 
-    # После мерджа test_DML убрать эти методы https://github.com/ydb-platform/ydb/pull/16117/files
-    def create_table(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
-        columns = {
-            "pk_": pk_types.keys(),
-            "col_": all_types.keys(),
-            "col_index_": index.keys(),
-            "ttl_": [ttl]
-        }
-        pk_columns = {
-            "pk_": pk_types.keys()
-        }
-        index_columns = {
-            "col_index_": index.keys()
-        }
-        sql_create_table = create_table(
-            table_name, columns, pk_columns, index_columns, unique, sync)
-        self.query(sql_create_table)
-        if ttl != "":
-            sql_ttl = create_ttl(f"ttl_{cleanup_type_name(ttl)}", {"P18262D": ""}, "SECONDS" if ttl ==
-                                 "Uint32" or ttl == "Uint64" or ttl == "DyNumber" else "", table_name)
-            self.query(sql_ttl)
-
-    def insert(self, table_name: str, all_types: dict[str, str], pk_types: dict[str, str], index: dict[str, str], ttl: str):
-        number_of_columns = len(pk_types) + len(all_types) + len(index)
-
-        if ttl != "":
-            number_of_columns += 1
-        for count in range(1, number_of_columns + 1):
-            self.create_insert(table_name, count, all_types,
-                               pk_types, index, ttl)
-
-    def create_insert(self, table_name: str, value: int, all_types: dict[str, str], pk_types: dict[str, str], index: dict[str, str], ttl: str):
-        insert_sql = f"""
-            INSERT INTO {table_name}(
-                {", ".join(["pk_" + cleanup_type_name(type_name) for type_name in pk_types.keys()])}{", " if len(all_types) != 0 else ""}
-                {", ".join(["col_" + cleanup_type_name(type_name) for type_name in all_types.keys()])}{", " if len(index) != 0 else ""}
-                {", ".join(["col_index_" + cleanup_type_name(type_name) for type_name in index.keys()])}{", " if len(ttl) != 0 else ""}
-                {f"ttl_{ttl}" if ttl != "" else ""}
-            )
-            VALUES(
-                {", ".join([pk_types[type_name].format(value) for type_name in pk_types.keys()])}{", " if len(all_types) != 0 else ""}
-                {", ".join([all_types[type_name].format(value) for type_name in all_types.keys()])}{", " if len(index) != 0 else ""}
-                {", ".join([index[type_name].format(value) for type_name in index.keys()])}{", " if len(ttl) != 0 else ""}
-                {ttl_types[ttl].format(value) if ttl != "" else ""}
-            );
-        """
-        self.query(insert_sql)
-
-    def select_after_insert(self, table_name: str, all_types: dict[str, str], pk_types: dict[str, str], index: dict[str, str], ttl: str):
-
-        number_of_columns = len(pk_types) + len(all_types) + len(index)
-
-        if ttl != "":
-            number_of_columns += 1
-
-        for count in range(1, number_of_columns + 1):
-            sql_select = f"""
-                SELECT COUNT(*) as count FROM `{table_name}` WHERE """
-
-            for type_name in pk_types.keys():
-                sql_select += f"pk_{cleanup_type_name(type_name)}={pk_types[type_name].format(count)} and "
-            for type_name in all_types.keys():
-                if type_name != "Json" and type_name != "Yson" and type_name != "JsonDocument":
-                    sql_select += f"col_{cleanup_type_name(type_name)}={all_types[type_name].format(count)} and "
-            for type_name in index.keys():
-                sql_select += f"col_index_{cleanup_type_name(type_name)}={index[type_name].format(count)} and "
-            if ttl != "":
-                sql_select += f"ttl_{ttl}={ttl_types[ttl].format(count)}"
-            else:
-                sql_select += f"""pk_Int64={pk_types["Int64"].format(count)}"""
-
-            rows = self.query(sql_select)
-            assert len(
-                rows) == 1 and rows[0].count == 1, f"Expected one rows, faild in {count} value, table {table_name}"
+    def create_predicate(self, url):
+        def predicate():
+            cmd = yatest.common.execute([
+                yatest.common.binary_path(os.getenv('YDB_CLI_BINARY')),
+                '-e', 'grpc://'+self.get_endpoint(),
+                "--database", self.get_database(),
+                "operation", "get",
+                url,
+            ])
+            return "Done" in cmd._std_out.decode('utf-8')
+        return predicate

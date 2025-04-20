22d7e4a18ffde3c68af74362b4afb17b6f0c778f
Andrey Serebryanskiy <serebryanskiy@ydb.tech>
2025-04-20 10:03:57 +0000

    collapse namespaces

--- ydb/core/kafka_proxy/actors/txn_actor_response_builder.h
+++ ydb/core/kafka_proxy/actors/txn_actor_response_builder.h
@@ -4,12 +4,10 @@
 #include <ydb/core/kafka_proxy/kafka.h>
 #include <ydb/core/kafka_proxy/kafka_messages.h>
 
-namespace NKafka {
-    namespace NKafkaTransactions {
-        class TResponseBuilder {
-            public:
-                template<class ResponseType, class RequestType>
-                std::shared_ptr<ResponseType> Build(TMessagePtr<RequestType> request, EKafkaErrors errorCode);
-        };
-    } // namespace NKafkaTransactions
-} // namespace NKafka
+namespace NKafka::NKafkaTransactions {
+    class TResponseBuilder {
+        public:
+            template<class ResponseType, class RequestType>
+            std::shared_ptr<ResponseType> Build(TMessagePtr<RequestType> request, EKafkaErrors errorCode);
+    };
+} // namespace NKafka::NKafkaTransactions

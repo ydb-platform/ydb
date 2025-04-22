afeefcec80242636942e54a726dd81d33e63e0c3
Pavel Zuev <pzuev@ydb.tech>
2025-04-22 20:57:33 +0300

    More functionality for the performance test suite
    - JSON output and markdown formatter
    - Remove NarrowMap+Collect over graph outputs
    - MaxRSS delta measurements
    - Spiller factory with buffer preallocation
    - LastCombiner tests with spilling
    - Command line options and the ability to run singular tests

--- ydb/core/kqp/tools/combiner_perf/run_params.h
+++ ydb/core/kqp/tools/combiner_perf/run_params.h
@@ -1,16 +1,35 @@
 #pragma once
 
 #include <util/system/defaults.h>
+#include <optional>
 
 namespace NKikimr {
 namespace NMiniKQL {
 
+enum class ETestMode {
+    Full = 0,
+    GeneratorOnly,
+    RefOnly,
+    GraphOnly,
+};
+
+enum class ESamplerType {
+    StringKeysUI64Values,
+    UI64KeysUI64Values,
+};
+
 struct TRunParams {
+    ETestMode TestMode = ETestMode::Full;
+    ESamplerType SamplerType = ESamplerType::StringKeysUI64Values;
+    std::optional<ui64> RandomSeed;
+    int NumAttempts = 0;
     size_t RowsPerRun = 0;
     size_t NumRuns = 0;
-    size_t MaxKey = 0; // for numeric keys, the range is [0..MaxKey]
-    size_t BlockSize = 5000;
+    size_t NumKeys = 0; // for numeric keys, the range is [0..NumKeys-1]
+    size_t BlockSize = 0;
+    size_t WideCombinerMemLimit = 0;
     bool LongStringKeys = false;
+    bool MeasureReferenceMemory = false;
 };
 
 }

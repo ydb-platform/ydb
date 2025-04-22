7cf631b7aa5d3b3551fb04d469b9fdc31b5a6b95
Kirill Rysin <naspirato@gmail.com>
2025-04-22 19:39:56 +0300

    removing unused functions and calls

--- ydb/tests/library/compatibility/ya.make
+++ ydb/tests/library/compatibility/ya.make
@@ -1,16 +1,9 @@
 UNION()
 
-
 RUN_PROGRAM(
     ydb/tests/library/compatibility/downloader stable-25-1/release/ydbd ydbd-last-stable
     OUT_NOAUTO ydbd-last-stable
 )
-RUN_PROGRAM(
-    ydb/tests/library/compatibility/downloader stable-24-4/release/ydbd ydbd-prev-stable
-    OUT_NOAUTO ydbd-prev-stable
-)
-
-
 
 END()
 
--- ydb/tests/library/harness/kikimr_runner.py
+++ ydb/tests/library/harness/kikimr_runner.py
@@ -380,10 +380,6 @@ class KiKiMR(kikimr_cluster_interface.KiKiMRClusterInterface):
             ))
             raise
 
-    def set_configurator(self, configurator):
-            """Set a new configurator for the KiKiMR instance."""
-            self.__configurator = configurator
-
     def start(self):
         """
         Safely starts kikimr instance.

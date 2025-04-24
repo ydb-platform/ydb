9bb3c4ce7cdee8632d04d7c7d22615823599cf19
Denis Prokofev <dennnis@ydb.tech>
2025-04-24 18:58:45 +0300

    MATCH_RECOGNIZE alfa

--- ydb/tests/datashard/select/test_select.py
+++ ydb/tests/datashard/select/test_select.py
@@ -326,3 +326,178 @@ class TestDML(TestBase):
                 else:
                     dml.assert_type(
                         all_types, type_name, 1, rows[0][f"pk_{cleanup_type_name(type_name)}"])
+
+    @pytest.mark.parametrize(
+        "table_name, pk_types, all_types, index, ttl, unique, sync",
+        [
+            ("table_index_4__SYNC", pk_types, {},
+             index_four_sync, "", "", "SYNC"),
+            ("table_index_3__SYNC", pk_types, {},
+             index_three_sync, "", "", "SYNC"),
+            ("table_index_2__SYNC", pk_types, {},
+             index_second_sync, "", "", "SYNC"),
+            ("table_index_1__SYNC", pk_types, {},
+             index_first_sync, "", "", "SYNC"),
+            ("table_index_0__SYNC", pk_types, {},
+             index_zero_sync, "", "", "SYNC"),
+            ("table_index_1__ASYNC", pk_types, {}, index_second, "", "", "ASYNC"),
+            ("table_index_0__ASYNC", pk_types, {}, index_first, "", "", "ASYNC"),
+            ("table_all_types", pk_types, {
+             **pk_types, **non_pk_types}, {}, "", "", ""),
+            ("table_ttl_DyNumber", pk_types, {}, {}, "DyNumber", "", ""),
+            ("table_ttl_Uint32", pk_types, {}, {}, "Uint32", "", ""),
+            ("table_ttl_Uint64", pk_types, {}, {}, "Uint64", "", ""),
+            ("table_ttl_Datetime", pk_types, {}, {}, "Datetime", "", ""),
+            ("table_ttl_Timestamp", pk_types, {}, {}, "Timestamp", "", ""),
+            ("table_ttl_Date", pk_types, {}, {}, "Date", "", ""),
+        ]
+    )
+    def test_match_recognize(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
+        dml = DMLOperations(self)
+        dml.create_table(table_name, pk_types, all_types,
+                         index, ttl, unique, sync)
+        self.insert(table_name, all_types, pk_types, index, ttl)
+        partition = []
+        for type_name in all_types.keys():
+            partition.append(f"col_{cleanup_type_name(type_name)}")
+        for type_name in index.keys():
+            partition.append(f"col_index_{cleanup_type_name(type_name)}")
+        numb = 1
+        for type_name in all_types.keys():
+            self.update(table_name, type_name, "col_", all_types[type_name], "Int64", pk_types["Int64"])
+            partition.remove(f"col_{cleanup_type_name(type_name)}")
+            self.create_match_recognize(table_name, partition[numb], partition[numb+1], f"col_{cleanup_type_name(type_name)}")
+            partition.append(f"col_{cleanup_type_name(type_name)}")
+            self.update_back(table_name, type_name, "col_", all_types[type_name], "Int64", pk_types["Int64"])
+            numb = (numb + 1) %len(all_types)
+        numb = 1
+        for type_name in index.keys():
+            self.update(table_name, type_name, "col_index_", index[type_name], "Int64", pk_types["Int64"])
+            partition.remove(f"col_index_{cleanup_type_name(type_name)}")
+            self.create_match_recognize(table_name, partition[numb], partition[numb+1], f"col_index_{cleanup_type_name(type_name)}")
+            partition.append(f"col_index_{cleanup_type_name(type_name)}")
+            self.update_back(table_name, type_name, "col_index_", index[type_name], "Int64", pk_types["Int64"])
+            numb = (numb + 1) %len(all_types)
+            
+
+    def update(self, table_name: str, type_name, prefix, key, type_name_pk, key_pk):
+        n = 3
+        m = 5
+        a = 1
+        c = 3
+        e = 5
+        f = 6
+        g = 7
+        h = 8
+        i = 9
+        j = 10
+        k = 11
+        l = 12
+        pk_value = 1
+        for _ in range(2):
+            for _ in range(2):
+                self.create_update(table_name, a, type_name,
+                                prefix, key, pk_value, type_name_pk, key_pk)
+                pk_value += 1
+            for _ in range(2):
+                self.create_update(table_name, c, type_name,
+                                prefix, key, pk_value, type_name_pk, key_pk)
+                pk_value += 1
+            self.create_update(table_name, e, type_name,
+                                prefix, key, pk_value, type_name_pk, key_pk)
+            pk_value += 1
+            for _ in range(n):
+                self.create_update(table_name, f, type_name,
+                                prefix, key, pk_value, type_name_pk, key_pk)
+                pk_value += 1
+            for _ in range((n+m)//2):
+                self.create_update(table_name, g, type_name,
+                                prefix, key, pk_value, type_name_pk, key_pk)
+                pk_value += 1
+            for _ in range(n+2):
+                self.create_update(table_name, h, type_name,
+                                prefix, key, pk_value, type_name_pk, key_pk)
+                pk_value += 1
+            self.create_update(table_name, i, type_name,
+                                prefix, key, pk_value, type_name_pk, key_pk)
+            pk_value += 1
+            self.create_update(table_name, j, type_name,
+                                prefix, key, pk_value, type_name_pk, key_pk)
+            pk_value += 1
+            for _ in range((n-1)//2):
+                self.create_update(table_name, k, type_name,
+                                prefix, key, pk_value, type_name_pk, key_pk)
+                pk_value += 1
+                self.create_update(table_name, l, type_name,
+                                prefix, key, pk_value, type_name_pk, key_pk)
+                pk_value += 1
+    
+    def update_back(self, table_name: str, type_name, prefix, key, type_name_pk, key_pk):
+        for i in range(1,3):
+            self.create_update(table_name, i, type_name, prefix, key, i, type_name_pk, key_pk)
+
+    def create_update(self, table_name, value, type_name, prefix, key, value_pk, type_name_pk, key_pk):
+        update_sql = f"""
+            UPDATE `{table_name}` SET {prefix}{cleanup_type_name(type_name)} = {format_sql_value(key(value), type_name)}
+            where pk_{cleanup_type_name(type_name_pk)} = {format_sql_value(key_pk(value_pk), type_name_pk)}
+        """
+        print(update_sql)
+        self.query(update_sql)
+        
+    def insert(self, table_name: str, all_types: dict[str, str], pk_types: dict[str, str], index: dict[str, str], ttl: str):
+        number_of_columns = 22
+        for count in range(1, number_of_columns + 1):
+            self.create_insert(table_name, 1, count, all_types,
+                               pk_types, index, ttl)
+        for count in range(number_of_columns + 1, 2*number_of_columns + 1):
+            self.create_insert(table_name, 2, count, all_types,
+                               pk_types, index, ttl)
+
+    def create_insert(self, table_name: str, value: int, value_pk: int, all_types: dict[str, str], pk_types: dict[str, str], index: dict[str, str], ttl: str):
+        insert_sql = f"""
+            INSERT INTO {table_name}(
+                {", ".join(["pk_" + cleanup_type_name(type_name) for type_name in pk_types.keys()])}{", " if len(all_types) != 0 else ""}
+                {", ".join(["col_" + cleanup_type_name(type_name) for type_name in all_types.keys()])}{", " if len(index) != 0 else ""}
+                {", ".join(["col_index_" + cleanup_type_name(type_name) for type_name in index.keys()])}{", " if len(ttl) != 0 else ""}
+                {f"ttl_{ttl}" if ttl != "" else ""}
+            )
+            VALUES(
+                {", ".join([format_sql_value(pk_types[type_name](value_pk), type_name) for type_name in pk_types.keys()])}{", " if len(all_types) != 0 else ""}
+                {", ".join([format_sql_value(all_types[type_name](value), type_name) for type_name in all_types.keys()])}{", " if len(index) != 0 else ""}
+                {", ".join([format_sql_value(index[type_name](value), type_name) for type_name in index.keys()])}{", " if len(ttl) != 0 else ""}
+                {format_sql_value(ttl_types[ttl](value), ttl) if ttl != "" else ""}
+            );
+        """
+        print(insert_sql)
+        self.query(insert_sql)
+
+    def create_match_recognize(self, table_name, partition, order, col):
+        match_recognize_sql = f"""
+            PRAGMA FeatureR010="prototype";
+            select * from {table_name} MATCH_RECOGNIZE(
+                PARTITION BY {partition}
+                ORDER BY {order}
+                MEASURES
+                    LAST(A.{col}) AS a,
+                    LAST(L.{col}) AS l
+                ONE ROW PER MATCH          
+                AFTER MATCH SKIP TO NEXT ROW 
+                PATTERN (A+ B* C* D? E? F{{5}} G{{5, 10}} H{{5,}} (I|J) (I|J) (I|J){{,10}})  
+                DEFINE
+                A as A.{col}=1,
+                B as B.{col}=2,
+                C as C.{col}=3,
+                D as D.{col}=4,
+                E as E.{col}=5,
+                F as F.{col}=6,
+                G as G.{col}=7,
+                H as H.{col}=8,
+                I as I.{col}=9,
+                J as J.{col}=10,
+                K as K.{col}=11,
+                L as L.{col}=12
+            );
+        """
+        print(match_recognize_sql)
+        rows = self.query(match_recognize_sql)
+        print(rows)

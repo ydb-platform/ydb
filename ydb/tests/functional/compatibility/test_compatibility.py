934c4ccd6521929ff76930e866ce60dc17eec647
Kirill Rysin <naspirato@gmail.com>
2025-04-28 12:43:35 +0300

    fix formating

--- ydb/tests/functional/compatibility/test_compatibility.py
+++ ydb/tests/functional/compatibility/test_compatibility.py
@@ -287,7 +287,7 @@ class TestCompatibility(object):
             "init",
             "--store={}".format(store_type),
             "--datetime",  # use 32 bit dates instead of 64 (not supported in 24-4)
-           # "--partition-size=25",  # not supported in stable yet
+            # "--partition-size=25",  # not supported in stable yet
         ]
         import_command = [
             yatest.common.binary_path(os.getenv("YDB_CLI_BINARY")),
@@ -325,7 +325,7 @@ class TestCompatibility(object):
         ]
         if store_type == "row":
             run_command.append("--exclude=12,17")
-            
+
         clean_command = [
             yatest.common.binary_path(os.getenv("YDB_CLI_BINARY")),
             "--verbose",

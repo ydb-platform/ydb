2479a8907fd172e5f03040fa6902e8af359ab34f
Andrey Zaspa <zaspa94@ydb.tech>
2025-04-29 18:12:40 +0300

    added SchemeCahce compatibility for materialized SysViews

--- ydb/core/tx/scheme_board/cache.h
+++ ydb/core/tx/scheme_board/cache.h
@@ -17,12 +17,25 @@ namespace NSchemeCache {
         const TInstant CreatedAt;
         TIntrusivePtr<TDomainInfo> ResolvedDomainInfo; // resolved from DatabaseName
 
+        struct TEntryFallbackInfo {
+            bool IsImplicit = false;
+            TMaybe<size_t> FallbackEntryIndex;
+        };
+
+        // Vector implements fallback entry logic:
+        // - Each element corresponds to an entry from ResultSet
+        // - FallbackEntryIndex refers to the fallback entry
+        // - IsImplicit == true when the entry is a fallback entry
+        TVector<TEntryFallbackInfo> EntriesFallbackInfo;
+        bool HasSysViewEntries = false;
+
         TSchemeCacheNavigateContext(const TActorId& sender, ui64 cookie, TAutoPtr<TSchemeCacheNavigate> request, const TInstant& now = TInstant::Now())
             : Sender(sender)
             , Cookie(cookie)
             , WaitCounter(0)
             , Request(request)
             , CreatedAt(now)
+            , EntriesFallbackInfo(Request->ResultSet.size())
         {}
     };
 

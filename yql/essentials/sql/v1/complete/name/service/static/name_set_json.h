64ef8f13ea437832952849c659c432489f078030
vityaman <vityaman.dev@yandex.ru>
2025-04-29 20:37:00 +0300

    YQL-19747 Publish ParseUdfs, ParseTypes and others
    A client might want to have completions of its own private UDFs. Then a client should make a JSON document and parse it to create a custom `TNameSet`.
    
    ---
    
    - Related to https://github.com/ydb-platform/ydb/issues/9056
    - Related to https://github.com/vityaman/ydb/issues/36
    
    ---
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/1251
    commit_hash:bbee9be4a480262aa788e7b242b7abdc90882ba7

---
+++ yql/essentials/sql/v1/complete/name/service/static/name_set_json.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <yql/essentials/sql/v1/complete/core/statement.h>
+
+#include <library/cpp/json/json_value.h>
+
+#include <util/generic/string.h>
+#include <util/generic/vector.h>
+#include <util/generic/hash.h>
+
+namespace NSQLComplete {
+
+    TVector<TString> ParsePragmas(NJson::TJsonValue json);
+
+    TVector<TString> ParseTypes(NJson::TJsonValue json);
+
+    TVector<TString> ParseFunctions(NJson::TJsonValue json);
+
+    TVector<TString> ParseUdfs(NJson::TJsonValue json);
+
+    THashMap<EStatementKind, TVector<TString>> ParseHints(NJson::TJsonValue json);
+
+} // namespace NSQLComplete

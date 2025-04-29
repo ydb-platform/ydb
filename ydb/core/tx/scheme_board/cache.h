bb14cb680af550a4d4ffb3961c96b2b830c7f48c
Andrey Zaspa <zaspa94@ydb.tech>
2025-04-29 18:10:24 +0300

    moved SchemeCache context to cache.h

--- ydb/core/tx/scheme_board/cache.h
+++ ydb/core/tx/scheme_board/cache.h
@@ -8,4 +8,40 @@ namespace NKikimr {
 
 IActor* CreateSchemeBoardSchemeCache(NSchemeCache::TSchemeCacheConfig* config);
 
+namespace NSchemeCache {
+    struct TSchemeCacheNavigateContext : TAtomicRefCount<TSchemeCacheNavigateContext>, TNonCopyable {
+        TActorId Sender;
+        ui64 Cookie;
+        ui64 WaitCounter;
+        TAutoPtr<TSchemeCacheNavigate> Request;
+        const TInstant CreatedAt;
+        TIntrusivePtr<TDomainInfo> ResolvedDomainInfo; // resolved from DatabaseName
+
+        TSchemeCacheNavigateContext(const TActorId& sender, ui64 cookie, TAutoPtr<TSchemeCacheNavigate> request, const TInstant& now = TInstant::Now())
+            : Sender(sender)
+            , Cookie(cookie)
+            , WaitCounter(0)
+            , Request(request)
+            , CreatedAt(now)
+        {}
+    };
+
+    struct TSchemeCacheRequestContext : TAtomicRefCount<TSchemeCacheRequestContext>, TNonCopyable {
+        TActorId Sender;
+        ui64 Cookie;
+        ui64 WaitCounter;
+        TAutoPtr<TSchemeCacheRequest> Request;
+        const TInstant CreatedAt;
+        TIntrusivePtr<TDomainInfo> ResolvedDomainInfo; // resolved from DatabaseName
+
+        TSchemeCacheRequestContext(const TActorId& sender, ui64 cookie, TAutoPtr<TSchemeCacheRequest> request, const TInstant& now = TInstant::Now())
+            : Sender(sender)
+            , Cookie(cookie)
+            , WaitCounter(0)
+            , Request(request)
+            , CreatedAt(now)
+        {}
+    }; // NSchemeCache
+}
+
 } // NKikimr

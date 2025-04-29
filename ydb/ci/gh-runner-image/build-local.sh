1a0428e960d9cd534d5caf2c1bd125f764de33a6
Nikita Kozlovskiy <nmk@ydb.tech>
2025-04-29 11:30:51 +0200

    ci: add clang-18 support

---
+++ ydb/ci/gh-runner-image/build-local.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [[ -z "$FOLDER_ID" || -z "$ZONE_ID" || -z "$SUBNET_ID" ]]; then
+  echo "FOLDER_ID, ZONE_ID and SUBNET_ID must be set"
+  exit 1
+fi
+
+export YC_TOKEN=$(yc iam create-token)
+
+packer build \
+  -var "folder_id=${FOLDER_ID?}" \
+  -var "zone_id=${ZONE_ID?}" \
+  -var "subnet_id=${SUBNET_ID?}" \
+  .

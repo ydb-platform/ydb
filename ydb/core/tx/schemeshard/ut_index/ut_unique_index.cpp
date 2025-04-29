bebc2b3e72e4a9d15f7ed63219d67f991ae2f3d5
st-shchetinin <st-shchetinin@ydb.tech>
2025-04-29 15:15:40 +0300

    CreateTableByTableClient test

--- ydb/core/tx/schemeshard/ut_index/ut_unique_index.cpp
+++ ydb/core/tx/schemeshard/ut_index/ut_unique_index.cpp
@@ -4,6 +4,7 @@
 using namespace NKikimr;
 using namespace NSchemeShard;
 using namespace NSchemeShardUT_Private;
+using namespace NYdb::Dev::NTable;
 
 Y_UNIT_TEST_SUITE(TUniqueIndexTests) {
     Y_UNIT_TEST(CreateTable) {
@@ -34,4 +35,38 @@ Y_UNIT_TEST_SUITE(TUniqueIndexTests) {
              NLs::IndexState(NKikimrSchemeOp::EIndexStateReady),
              NLs::IndexKeys({"indexed"})});
     }
+
+    Y_UNIT_TEST(CreateTableByTableClient) {
+      NKikimrConfig::TAppConfig appConfig;
+      NYdb::TKikimrWithGrpcAndRootSchema server{std::move(appConfig)};
+
+      auto driver = NYdb::TDriver(NYdb::TDriverConfig().SetEndpoint(Sprintf("localhost:%d", server.GetPort())));
+      NYdb::NTable::TTableClient tableClient(driver);
+      auto session = tableClient.GetSession().ExtractValueSync().GetSession();
+      constexpr const char* table = "/Root/table";
+      constexpr const char* index = "byValue";
+
+      TString query = Sprintf(R"(
+        CREATE TABLE `%s` (
+            Key Uint32,
+            Group Uint32,
+            Value Uint32,
+            PRIMARY KEY (Key),
+            INDEX %s GLOBAL UNIQUE ON (Value)
+          );
+      )", table, index);
+
+      session.ExecuteSchemeQuery(query).ExtractValueSync();
+
+      auto desc = session.DescribeTable(table).ExtractValueSync().GetTableDescription();
+      UNIT_ASSERT_EQUAL(desc.GetIndexDescriptions().size(), 1);
+      UNIT_ASSERT_EQUAL(desc.GetIndexDescriptions().at(0).GetIndexName(), index);
+      // TODO: Remove the flag after fixing the creation of unique indexes by table client
+      bool isFixedCreationUniqueIndexes = false;
+      if (!isFixedCreationUniqueIndexes) {
+        UNIT_ASSERT_EQUAL(desc.GetIndexDescriptions().at(0).GetIndexType(), EIndexType::GlobalSync);
+      } else {
+        UNIT_ASSERT_EQUAL(desc.GetIndexDescriptions().at(0).GetIndexType(), EIndexType::GlobalUnique);
+      }
+  }
 }

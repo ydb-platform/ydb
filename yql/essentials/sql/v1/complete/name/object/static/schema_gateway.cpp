0c2a81c9d391c539cfb25930a38fe153aa502255
vityaman <vityaman.dev@yandex.ru>
2025-04-29 20:36:31 +0300

    YQL-19747 Introduce SimpleSchemaGateway
    Introduce the `SimpleSchemaGateway` to make it easier to implement `SchemaGateway`s. The idea is that actually existing schema providers really do not support filtration such as by name and type, so in practice they return us the whole list and we need to filter it by hand. The `SimpleSchemaGateway` to `SchemaGateway` adapter does this for us -- we only need to implement a path splitting and folder listing.
    
    The other and important feature of the `SimpleSchemaGateway` is that it is simple to implement a caching decorator for it -- just store a mapping `Path -> [FolderEntry]`, while caching a `SchemaGateway` with filters is soooo not trivial.
    
    I also added string constants for known folder entry types, because they should be documented somewhere.
    
    ---
    
    - Related to `YQL-19747`
    - Related to https://github.com/vityaman/ydb/issues/14
    - Related to https://github.com/vityaman/ydb/issues/34
    
    ---
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/1245
    commit_hash:dda6dcac544ca95d5e8e08f1e7de9de6b5770f25

--- yql/essentials/sql/v1/complete/name/object/static/schema_gateway.cpp
+++ yql/essentials/sql/v1/complete/name/object/static/schema_gateway.cpp
@@ -1,6 +1,6 @@
 #include "schema_gateway.h"
 
-#include <yql/essentials/sql/v1/complete/text/case.h>
+#include <yql/essentials/sql/v1/complete/name/object/simple/schema_gateway.h>
 
 #include <util/charset/utf8.h>
 
@@ -8,11 +8,9 @@ namespace NSQLComplete {
 
     namespace {
 
-        class TSchemaGateway: public ISchemaGateway {
-            static constexpr size_t MaxLimit = 4 * 1024;
-
+        class TSimpleSchemaGateway: public ISimpleSchemaGateway {
         public:
-            explicit TSchemaGateway(THashMap<TString, TVector<TFolderEntry>> data)
+            explicit TSimpleSchemaGateway(THashMap<TString, TVector<TFolderEntry>> data)
                 : Data_(std::move(data))
             {
                 for (const auto& [k, _] : Data_) {
@@ -21,35 +19,7 @@ namespace NSQLComplete {
                 }
             }
 
-            NThreading::TFuture<TListResponse> List(const TListRequest& request) const override {
-                auto [path, prefix] = ParsePath(request.Path);
-
-                TVector<TFolderEntry> entries;
-                if (const auto* data = Data_.FindPtr(path)) {
-                    entries = *data;
-                }
-
-                EraseIf(entries, [prefix = ToLowerUTF8(prefix)](const TFolderEntry& entry) {
-                    return !entry.Name.StartsWith(prefix);
-                });
-
-                EraseIf(entries, [types = std::move(request.Filter.Types)](const TFolderEntry& entry) {
-                    return types && !types->contains(entry.Type);
-                });
-
-                Y_ENSURE(request.Limit <= MaxLimit);
-                entries.crop(request.Limit);
-
-                TListResponse response = {
-                    .NameHintLength = prefix.size(),
-                    .Entries = std::move(entries),
-                };
-
-                return NThreading::MakeFuture(std::move(response));
-            }
-
-        private:
-            static std::tuple<TStringBuf, TStringBuf> ParsePath(TString path Y_LIFETIME_BOUND) {
+            TSplittedPath Split(TStringBuf path) const override {
                 size_t pos = path.find_last_of('/');
                 if (pos == TString::npos) {
                     return {"", path};
@@ -60,13 +30,24 @@ namespace NSQLComplete {
                 return {head, tail};
             }
 
+            NThreading::TFuture<TVector<TFolderEntry>> List(TString folder) const override {
+                TVector<TFolderEntry> entries;
+                if (const auto* data = Data_.FindPtr(folder)) {
+                    entries = *data;
+                }
+                return NThreading::MakeFuture(std::move(entries));
+            }
+
+        private:
             THashMap<TString, TVector<TFolderEntry>> Data_;
         };
 
     } // namespace
 
     ISchemaGateway::TPtr MakeStaticSchemaGateway(THashMap<TString, TVector<TFolderEntry>> fs) {
-        return MakeIntrusive<TSchemaGateway>(std::move(fs));
+        return MakeSimpleSchemaGateway(
+            ISimpleSchemaGateway::TPtr(
+                new TSimpleSchemaGateway(std::move(fs))));
     }
 
 } // namespace NSQLComplete

aee18011456e1ca6270991b297bcad4edf1ec0c5
stanislav_shchetinin <st-shchetinin@ydb.tech>
2025-05-01 19:38:26 +0300

    Added EnableAutoDropping flag (#17758)

--- ydb/core/tx/schemeshard/schemeshard_export__create.cpp
+++ ydb/core/tx/schemeshard/schemeshard_export__create.cpp
@@ -1158,7 +1158,11 @@ private:
             Self->PersistExportItemState(db, exportInfo, itemIdx);
 
             if (AllOf(exportInfo->Items, &TExportInfo::TItem::IsDone)) {
-                PrepareAutoDropping(Self, exportInfo, db);
+                if (!AppData()->FeatureFlags.GetEnableExportAutoDropping()) {
+                    EndExport(exportInfo, EState::Done, db);
+                } else {
+                    PrepareAutoDropping(Self, exportInfo, db);
+                }
             }
         } else if (exportInfo->State == EState::Cancellation) {
             item.State = EState::Cancelled;
@@ -1358,7 +1362,12 @@ private:
                 }
             }
             if (!itemHasIssues && AllOf(exportInfo->Items, &TExportInfo::TItem::IsDone)) {
-                PrepareAutoDropping(Self, exportInfo, db);
+                if (!AppData()->FeatureFlags.GetEnableExportAutoDropping()) {
+                    exportInfo->State = EState::Done;
+                    exportInfo->EndTime = TAppData::TimeProvider->Now();
+                } else {
+                    PrepareAutoDropping(Self, exportInfo, db);
+                }
             }
 
             Self->PersistExportItemState(db, exportInfo, itemIdx);

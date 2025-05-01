ae976136389b66594535667db5f49ffd99767043
ivanmorozov333 <imorozov333@ya.ru>
2025-05-01 12:02:49 +0300

    correction

--- ydb/core/tx/columnshard/engines/portions/constructor_meta.cpp
+++ ydb/core/tx/columnshard/engines/portions/constructor_meta.cpp
@@ -46,15 +46,13 @@ TPortionMetaConstructor::TPortionMetaConstructor(const TPortionMeta& meta, const
 
 TPortionMeta TPortionMetaConstructor::Build() {
     AFL_VERIFY(FirstAndLastPK);
-    /*
-    TMemoryProfileGuard mGuard1("meta_pk_construct");
+//    TMemoryProfileGuard mGuard1("meta_pk_construct");
     static TAtomicCounter sumValues = 0;
     static TAtomicCounter countValues = 0;
-    FirstAndLastPK->Reallocate();
-    AFL_ERROR(NKikimrServices::TX_COLUMNSHARD)("portion_size", FirstAndLastPK->GetMemorySize())("data_size", FirstAndLastPK->GetDataSize())(
+//    FirstAndLastPK->Reallocate();
+    AFL_DEBUG(NKikimrServices::TX_COLUMNSHARD)("portion_size", FirstAndLastPK->GetMemorySize())("data_size", FirstAndLastPK->GetDataSize())(
         "sum", sumValues.Add(FirstAndLastPK->GetMemorySize()))("count", countValues.Inc());
-    */
-    TMemoryProfileGuard mGuard("meta_construct");
+//    TMemoryProfileGuard mGuard("meta_construct");
     AFL_VERIFY(RecordSnapshotMin);
     AFL_VERIFY(RecordSnapshotMax);
     TPortionMeta result(*FirstAndLastPK, *RecordSnapshotMin, *RecordSnapshotMax);

3ed1e6fc372969d06429b322dcb6bb12d507eade
mregrock <mregrock@ydb.tech>
2025-05-05 09:47:07 +0300

    Add more asserts

--- ydb/tests/library/clients/kikimr_dynconfig_client.py
+++ ydb/tests/library/clients/kikimr_dynconfig_client.py
@@ -61,8 +61,12 @@ class DynConfigClient(object):
         request = dynamic_config_api.FetchStartupConfigRequest()
         return self.invoke(request, 'FetchStartupConfig')
 
-    def get_configuration_version(self):
-        request = dynamic_config_api.GetConfigurationVersionRequest()
+    def get_configuration_version(self, v1=False, v2=False, unknown=False):
+        request = dynamic_config_api.GetConfigurationVersionRequest(
+            list_v1_nodes=v1,
+            list_v2_nodes=v2,
+            list_unknown_nodes=unknown
+        )
         return self.invoke(request, 'GetConfigurationVersion')
 
     def close(self):

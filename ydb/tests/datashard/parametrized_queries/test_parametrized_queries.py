f5c2b856048f1eab0c84b02b7412000d66472fde
Denis Prokofev <dennnis@ydb.tech>
2025-05-07 14:24:23 +0300

    ya style

--- ydb/tests/datashard/parametrized_queries/test_parametrized_queries.py
+++ ydb/tests/datashard/parametrized_queries/test_parametrized_queries.py
@@ -4,8 +4,20 @@ import ydb
 from datetime import datetime
 from ydb.tests.sql.lib.test_base import TestBase
 from ydb.tests.datashard.lib.create_table import create_table_sql_request, create_ttl_sql_request
-from ydb.tests.datashard.lib.types_of_variables import cleanup_type_name, pk_types_parametrized_queries, non_pk_types, index_first_parametrized_queries, index_second_parametrized_queries, ttl_types, \
-    index_first_sync, index_second_sync, index_three_sync_parametrized_queries, index_three_sync_not_Bool_parametrized_queries, index_four_sync_parametrized_queries, index_zero_sync
+from ydb.tests.datashard.lib.types_of_variables import (
+    cleanup_type_name,
+    pk_types_parametrized_queries,
+    non_pk_types,
+    index_first_parametrized_queries,
+    index_second_parametrized_queries,
+    ttl_types,
+    index_first_sync,
+    index_second_sync,
+    index_three_sync_parametrized_queries,
+    index_three_sync_not_Bool_parametrized_queries,
+    index_four_sync_parametrized_queries,
+    index_zero_sync,
+)
 
 primitive_type = {
     "Int64": ydb.PrimitiveType.Int64,
@@ -18,16 +30,13 @@ primitive_type = {
     "Uint8": ydb.PrimitiveType.Uint8,
     "Bool": ydb.PrimitiveType.Bool,
     "DyNumber": ydb.PrimitiveType.DyNumber,
-
     "String": ydb.PrimitiveType.String,
     "Utf8": ydb.PrimitiveType.Utf8,
     "UUID": ydb.PrimitiveType.UUID,
-
     "Date": ydb.PrimitiveType.Date,
     "Datetime": ydb.PrimitiveType.Datetime,
     "Timestamp": ydb.PrimitiveType.Timestamp,
     "Interval": ydb.PrimitiveType.Interval,
-
     "Float": ydb.PrimitiveType.Float,
     "Double": ydb.PrimitiveType.Double,
     "Json": ydb.PrimitiveType.Json,
@@ -40,82 +49,140 @@ class TestParametrizedQueries(TestBase):
     @pytest.mark.parametrize(
         "table_name, pk_types, all_types, index, ttl, unique, sync",
         [
-            ("table_index_4_UNIQUE_SYNC", pk_types_parametrized_queries, {},
-             index_four_sync_parametrized_queries, "", "UNIQUE", "SYNC"),
-            ("table_index_3_UNIQUE_SYNC", pk_types_parametrized_queries, {},
-             index_three_sync_not_Bool_parametrized_queries, "", "UNIQUE", "SYNC"),
-            ("table_index_2_UNIQUE_SYNC", pk_types_parametrized_queries, {},
-             index_second_sync, "", "UNIQUE", "SYNC"),
-            ("table_index_1_UNIQUE_SYNC", pk_types_parametrized_queries, {},
-             index_first_sync, "", "UNIQUE", "SYNC"),
-            ("table_index_0_UNIQUE_SYNC", pk_types_parametrized_queries, {},
-             index_zero_sync, "", "UNIQUE", "SYNC"),
-            ("table_index_4__SYNC", pk_types_parametrized_queries, {},
-             index_four_sync_parametrized_queries, "", "", "SYNC"),
-            ("table_index_3__SYNC", pk_types_parametrized_queries, {},
-             index_three_sync_parametrized_queries, "", "", "SYNC"),
-            ("table_index_2__SYNC", pk_types_parametrized_queries, {},
-             index_second_sync, "", "", "SYNC"),
-            ("table_index_1__SYNC", pk_types_parametrized_queries, {},
-             index_first_sync, "", "", "SYNC"),
-            ("table_index_0__SYNC", pk_types_parametrized_queries, {},
-             index_zero_sync, "", "", "SYNC"),
-            ("table_index_1__ASYNC", pk_types_parametrized_queries,
-             {}, index_second_parametrized_queries, "", "", "ASYNC"),
-            ("table_index_0__ASYNC", pk_types_parametrized_queries,
-             {}, index_first_parametrized_queries, "", "", "ASYNC"),
-            ("table_all_types", pk_types_parametrized_queries, {
-             **pk_types_parametrized_queries, **non_pk_types}, {}, "", "", ""),
-            ("table_ttl_DyNumber", pk_types_parametrized_queries,
-             {}, {}, "DyNumber", "", ""),
-            ("table_ttl_Uint32", pk_types_parametrized_queries,
-             {}, {}, "Uint32", "", ""),
-            ("table_ttl_Uint64", pk_types_parametrized_queries,
-             {}, {}, "Uint64", "", ""),
-            ("table_ttl_Datetime", pk_types_parametrized_queries,
-             {}, {}, "Datetime", "", ""),
-            ("table_ttl_Timestamp", pk_types_parametrized_queries,
-             {}, {}, "Timestamp", "", ""),
+            (
+                "table_index_4_UNIQUE_SYNC",
+                pk_types_parametrized_queries,
+                {},
+                index_four_sync_parametrized_queries,
+                "",
+                "UNIQUE",
+                "SYNC",
+            ),
+            (
+                "table_index_3_UNIQUE_SYNC",
+                pk_types_parametrized_queries,
+                {},
+                index_three_sync_not_Bool_parametrized_queries,
+                "",
+                "UNIQUE",
+                "SYNC",
+            ),
+            ("table_index_2_UNIQUE_SYNC", pk_types_parametrized_queries, {}, index_second_sync, "", "UNIQUE", "SYNC"),
+            ("table_index_1_UNIQUE_SYNC", pk_types_parametrized_queries, {}, index_first_sync, "", "UNIQUE", "SYNC"),
+            ("table_index_0_UNIQUE_SYNC", pk_types_parametrized_queries, {}, index_zero_sync, "", "UNIQUE", "SYNC"),
+            (
+                "table_index_4__SYNC",
+                pk_types_parametrized_queries,
+                {},
+                index_four_sync_parametrized_queries,
+                "",
+                "",
+                "SYNC",
+            ),
+            (
+                "table_index_3__SYNC",
+                pk_types_parametrized_queries,
+                {},
+                index_three_sync_parametrized_queries,
+                "",
+                "",
+                "SYNC",
+            ),
+            ("table_index_2__SYNC", pk_types_parametrized_queries, {}, index_second_sync, "", "", "SYNC"),
+            ("table_index_1__SYNC", pk_types_parametrized_queries, {}, index_first_sync, "", "", "SYNC"),
+            ("table_index_0__SYNC", pk_types_parametrized_queries, {}, index_zero_sync, "", "", "SYNC"),
+            (
+                "table_index_1__ASYNC",
+                pk_types_parametrized_queries,
+                {},
+                index_second_parametrized_queries,
+                "",
+                "",
+                "ASYNC",
+            ),
+            (
+                "table_index_0__ASYNC",
+                pk_types_parametrized_queries,
+                {},
+                index_first_parametrized_queries,
+                "",
+                "",
+                "ASYNC",
+            ),
+            (
+                "table_all_types",
+                pk_types_parametrized_queries,
+                {**pk_types_parametrized_queries, **non_pk_types},
+                {},
+                "",
+                "",
+                "",
+            ),
+            ("table_ttl_DyNumber", pk_types_parametrized_queries, {}, {}, "DyNumber", "", ""),
+            ("table_ttl_Uint32", pk_types_parametrized_queries, {}, {}, "Uint32", "", ""),
+            ("table_ttl_Uint64", pk_types_parametrized_queries, {}, {}, "Uint64", "", ""),
+            ("table_ttl_Datetime", pk_types_parametrized_queries, {}, {}, "Datetime", "", ""),
+            ("table_ttl_Timestamp", pk_types_parametrized_queries, {}, {}, "Timestamp", "", ""),
             ("table_ttl_Date", pk_types_parametrized_queries, {}, {}, "Date", "", ""),
-        ]
+        ],
     )
-    def test_parametrized_queries(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
-        self.create_table(table_name, pk_types, all_types,
-                          index, ttl, unique, sync)
+    def test_parametrized_queries(
+        self,
+        table_name: str,
+        pk_types: dict[str, str],
+        all_types: dict[str, str],
+        index: dict[str, str],
+        ttl: str,
+        unique: str,
+        sync: str,
+    ):
+        self.create_table(table_name, pk_types, all_types, index, ttl, unique, sync)
         self.insert(table_name, all_types, pk_types, index, ttl)
         self.select_after_insert(table_name, all_types, pk_types, index, ttl)
 
-    def create_table(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
-        columns = {
-            "pk_": pk_types.keys(),
-            "col_": all_types.keys(),
-            "col_index_": index.keys(),
-            "ttl_": [ttl]
-        }
-        pk_columns = {
-            "pk_": pk_types.keys()
-        }
-        index_columns = {
-            "col_index_": index.keys()
-        }
-        sql_create_table = create_table_sql_request(
-            table_name, columns, pk_columns, index_columns, unique, sync)
+    def create_table(
+        self,
+        table_name: str,
+        pk_types: dict[str, str],
+        all_types: dict[str, str],
+        index: dict[str, str],
+        ttl: str,
+        unique: str,
+        sync: str,
+    ):
+        columns = {"pk_": pk_types.keys(), "col_": all_types.keys(), "col_index_": index.keys(), "ttl_": [ttl]}
+        pk_columns = {"pk_": pk_types.keys()}
+        index_columns = {"col_index_": index.keys()}
+        sql_create_table = create_table_sql_request(table_name, columns, pk_columns, index_columns, unique, sync)
         self.query(sql_create_table)
         if ttl != "":
-            sql_ttl = create_ttl_sql_request(f"ttl_{cleanup_type_name(ttl)}", {"P18262D": ""}, "SECONDS" if ttl ==
-                                             "Uint32" or ttl == "Uint64" or ttl == "DyNumber" else "", table_name)
+            sql_ttl = create_ttl_sql_request(
+                f"ttl_{cleanup_type_name(ttl)}",
+                {"P18262D": ""},
+                "SECONDS" if ttl == "Uint32" or ttl == "Uint64" or ttl == "DyNumber" else "",
+                table_name,
+            )
             self.query(sql_ttl)
 
-    def insert(self, table_name: str, all_types: dict[str, str], pk_types: dict[str, str], index: dict[str, str], ttl: str):
+    def insert(
+        self, table_name: str, all_types: dict[str, str], pk_types: dict[str, str], index: dict[str, str], ttl: str
+    ):
         number_of_columns = len(pk_types) + len(all_types) + len(index)
 
         if ttl != "":
             number_of_columns += 1
         for count in range(1, number_of_columns + 1):
-            self.create_insert(table_name, count, all_types,
-                               pk_types, index, ttl)
+            self.create_insert(table_name, count, all_types, pk_types, index, ttl)
 
-    def create_insert(self, table_name: str, value: int, all_types: dict[str, str], pk_types: dict[str, str], index: dict[str, str], ttl: str):
+    def create_insert(
+        self,
+        table_name: str,
+        value: int,
+        all_types: dict[str, str],
+        pk_types: dict[str, str],
+        index: dict[str, str],
+        ttl: str,
+    ):
         insert_sql = f"""
             {" ".join([f"DECLARE $pk_{cleanup_type_name(type_name)} AS {type_name};" for type_name in pk_types.keys()])}
             {" ".join([f"DECLARE $col_{cleanup_type_name(type_name)} AS {type_name};" for type_name in all_types.keys()])}
@@ -137,20 +204,18 @@ class TestParametrizedQueries(TestBase):
         """
         parameters = {}
         for type_name in pk_types.keys():
-            parameters[f"$pk_{cleanup_type_name(type_name)}"] = self.create_type_value(
-                pk_types, type_name, value)
+            parameters[f"$pk_{cleanup_type_name(type_name)}"] = self.create_type_value(pk_types, type_name, value)
         for type_name in all_types.keys():
-            parameters[f"$col_{cleanup_type_name(type_name)}"] = self.create_type_value(
-                all_types, type_name, value)
+            parameters[f"$col_{cleanup_type_name(type_name)}"] = self.create_type_value(all_types, type_name, value)
         for type_name in index.keys():
-            parameters[f"$col_index_{cleanup_type_name(type_name)}"] = self.create_type_value(
-                index, type_name, value)
+            parameters[f"$col_index_{cleanup_type_name(type_name)}"] = self.create_type_value(index, type_name, value)
         if ttl != "":
-            parameters[f"$ttl_{cleanup_type_name(ttl)}"] = self.create_type_value(
-                ttl_types, ttl, value)
+            parameters[f"$ttl_{cleanup_type_name(ttl)}"] = self.create_type_value(ttl_types, ttl, value)
         self.query(insert_sql, parameters=parameters)
 
-    def select_after_insert(self, table_name: str, all_types: dict[str, str], pk_types: dict[str, str], index: dict[str, str], ttl: str):
+    def select_after_insert(
+        self, table_name: str, all_types: dict[str, str], pk_types: dict[str, str], index: dict[str, str], ttl: str
+    ):
         number_of_columns = len(pk_types) + len(all_types) + len(index)
 
         if ttl != "":
@@ -161,10 +226,8 @@ class TestParametrizedQueries(TestBase):
             create_all_type_declare = []
             for type_name in all_types.keys():
                 if type_name != "Json" and type_name != "Yson" and type_name != "JsonDocument":
-                    create_all_type.append(
-                        f"col_{cleanup_type_name(type_name)}=$col_{cleanup_type_name(type_name)}")
-                    create_all_type_declare.append(
-                        f"DECLARE $col_{cleanup_type_name(type_name)} AS {type_name};")
+                    create_all_type.append(f"col_{cleanup_type_name(type_name)}=$col_{cleanup_type_name(type_name)}")
+                    create_all_type_declare.append(f"DECLARE $col_{cleanup_type_name(type_name)} AS {type_name};")
 
             sql_select = f"""
                 {" ".join([f"DECLARE $pk_{cleanup_type_name(type_name)} AS {type_name};" for type_name in pk_types.keys()])}
@@ -182,24 +245,24 @@ class TestParametrizedQueries(TestBase):
                 """
             parameters = {}
             for type_name in pk_types.keys():
-                parameters[f"$pk_{cleanup_type_name(type_name)}"] = self.create_type_value(
-                    pk_types, type_name, count)
+                parameters[f"$pk_{cleanup_type_name(type_name)}"] = self.create_type_value(pk_types, type_name, count)
             for type_name in all_types.keys():
-                parameters[f"$col_{cleanup_type_name(type_name)}"] = self.create_type_value(
-                    all_types, type_name, count)
+                parameters[f"$col_{cleanup_type_name(type_name)}"] = self.create_type_value(all_types, type_name, count)
             for type_name in index.keys():
                 parameters[f"$col_index_{cleanup_type_name(type_name)}"] = self.create_type_value(
-                    index, type_name, count)
+                    index, type_name, count
+                )
             if ttl != "":
-                parameters[f"$ttl_{cleanup_type_name(ttl)}"] = self.create_type_value(
-                    ttl_types, ttl, count)
+                parameters[f"$ttl_{cleanup_type_name(ttl)}"] = self.create_type_value(ttl_types, ttl, count)
             rows = self.query(sql_select, parameters=parameters)
-            assert len(
-                rows) == 1 and rows[0].count == 1, f"Expected one rows, faild in {count} value, table {table_name}"
+            assert (
+                len(rows) == 1 and rows[0].count == 1
+            ), f"Expected one rows, faild in {count} value, table {table_name}"
 
         rows = self.query(f"SELECT COUNT(*) as count FROM `{table_name}`")
-        assert len(
-            rows) == 1 and rows[0].count == number_of_columns, f"Expected {number_of_columns} rows, after select all line"
+        assert (
+            len(rows) == 1 and rows[0].count == number_of_columns
+        ), f"Expected {number_of_columns} rows, after select all line"
 
     def create_type_value(self, key, type_name, value):
         if "Decimal" in type_name:

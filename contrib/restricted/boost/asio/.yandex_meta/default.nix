10fe9a3b19d69b964859ec94f061696071908342
thegeorg <thegeorg@yandex-team.com>
2025-05-07 16:21:09 +0300

    Update boost/beast and boost/asio to 1.86.0 commit_hash:2e849d5e0055f9b839a923fb9c42f9390f37c572

--- contrib/restricted/boost/asio/.yandex_meta/default.nix
+++ contrib/restricted/boost/asio/.yandex_meta/default.nix
@@ -1,13 +1,13 @@
 self: super: with self; {
   boost_asio = stdenv.mkDerivation rec {
     pname = "boost_asio";
-    version = "1.85.0";
+    version = "1.86.0";
 
     src = fetchFromGitHub {
       owner = "boostorg";
       repo = "asio";
       rev = "boost-${version}";
-      hash = "sha256-vYYYna5TXBCAAO1NkoxaUidgErKbCNP06S8uHijzIDE=";
+      hash = "sha256-dxORb6fczR6vtZKyMN+iQKcaDbN4cqcPPM07B/S1ovY=";
     };
   };
 }

cf3ebb17b47402c2ea475e4e8da05fc5a5f5e24d
vityaman <vityaman.dev@yandex.ru>
2025-05-08 14:53:26 +0300

    YQL-19747: Rebase on a fresh trunk
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- ydb/public/lib/ydb_cli/commands/interactive/complete/ydb_schema.h
+++ ydb/public/lib/ydb_cli/commands/interactive/complete/ydb_schema.h
@@ -2,12 +2,12 @@
 
 #include <ydb/public/lib/ydb_cli/common/command.h>
 
-#include <yql/essentials/sql/v1/complete/name/object/schema_gateway.h>
+#include <yql/essentials/sql/v1/complete/name/object/schema.h>
 
 #include <ydb/public/sdk/cpp/include/ydb-cpp-sdk/client/driver/driver.h>
 
 namespace NYdb::NConsoleClient {
 
-    NSQLComplete::ISchemaGateway::TPtr MakeYDBSchemaGateway(TDriver driver, TString database);
+    NSQLComplete::ISchema::TPtr MakeYDBSchema(TDriver driver, TString database);
 
 } // namespace NYdb::NConsoleClient

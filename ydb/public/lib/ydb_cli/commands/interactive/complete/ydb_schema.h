2d03ede6f02e2ef630e374e3b51d3413ed0a2f83
vityaman <vityaman.dev@yandex.ru>
2025-05-08 15:26:39 +0300

    YQL-19747: Implement SimpleSchema
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- ydb/public/lib/ydb_cli/commands/interactive/complete/ydb_schema.h
+++ ydb/public/lib/ydb_cli/commands/interactive/complete/ydb_schema.h
@@ -2,12 +2,12 @@
 
 #include <ydb/public/lib/ydb_cli/common/command.h>
 
-#include <yql/essentials/sql/v1/complete/name/object/schema.h>
+#include <yql/essentials/sql/v1/complete/name/object/simple/schema.h>
 
 #include <ydb/public/sdk/cpp/include/ydb-cpp-sdk/client/driver/driver.h>
 
 namespace NYdb::NConsoleClient {
 
-    NSQLComplete::ISchema::TPtr MakeYDBSchema(TDriver driver, TString database);
+    NSQLComplete::ISimpleSchema::TPtr MakeYDBSchema(TDriver driver, TString database);
 
 } // namespace NYdb::NConsoleClient

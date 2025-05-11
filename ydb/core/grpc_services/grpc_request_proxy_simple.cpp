0d338f746429f3f7b918f98072a642c379477230
Andrei Rykov <arykov@ydb.tech>
2025-05-11 15:26:17 +0200

    Add user attribute to keep gizmo resource id (#17952)

--- ydb/core/grpc_services/grpc_request_proxy_simple.cpp
+++ ydb/core/grpc_services/grpc_request_proxy_simple.cpp
@@ -127,7 +127,7 @@ private:
 
         {
             TSchemeBoardEvents::TDescribeSchemeResult schemeData;
-            Register(CreateGrpcRequestCheckActor<TEvent>(SelfId(), schemeData, nullptr, event.Release(), Counters, false, this));
+            Register(CreateGrpcRequestCheckActor<TEvent>(SelfId(), schemeData, nullptr, event.Release(), Counters, false, {}, this));
             return;
         }
 

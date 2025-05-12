b6cfc353063f99d2b5a2429e8fb561ad2ff1c89e
imunkin <imunkin@yandex-team.com>
2025-05-12 15:52:31 +0300

    YQL-19923: Add Ascii{Contains,Equals}IgnoreCase functions to String UDF commit_hash:6717ca10951c933df9cf8757763cfbae15facbbf
     Conflicts:
    	yql/essentials/docs/en/changelog/2025.02.md
    	yql/essentials/docs/ru/changelog/2025.02.md
    	yql/essentials/udfs/common/string/string_udf.cpp

--- yql/essentials/udfs/common/string/test/cases/BlockAsciiCmpIgnoreCase.sql
+++ yql/essentials/udfs/common/string/test/cases/BlockAsciiCmpIgnoreCase.sql
@@ -3,6 +3,9 @@ PRAGMA UseBlocks;
 
 SELECT
     value,
+    String::AsciiContainsIgnoreCase(value, "AS") AS iccontains,
+    String::AsciiContainsIgnoreCase(value, "") AS icempty,
     String::AsciiStartsWithIgnoreCase(value, "AS") AS icstarts,
     String::AsciiEndsWithIgnoreCase(value, "AS") AS icends,
+    String::AsciiEqualsIgnoreCase(value, "FDSA") AS icequals,
 FROM Input;

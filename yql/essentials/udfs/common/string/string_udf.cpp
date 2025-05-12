212112252a214e69e548bf7b28b3b26885a6ca97
imunkin <imunkin@yandex-team.com>
2025-05-12 15:52:31 +0300

    YQL-19923: Add Ascii{Contains,Equals}IgnoreCase functions to String UDF commit_hash:6717ca10951c933df9cf8757763cfbae15facbbf

--- yql/essentials/udfs/common/string/string_udf.cpp
+++ yql/essentials/udfs/common/string/string_udf.cpp
@@ -408,7 +408,8 @@ SIMPLE_STRICT_UDF_OPTIONS(TReverse, TOptional<char*>(TOptional<char*>),
 
 #define STRING_ASCII_CMP_IGNORE_CASE_UDF_MAP(XX)            \
     XX(AsciiStartsWithIgnoreCase, AsciiHasPrefixIgnoreCase) \
-    XX(AsciiEndsWithIgnoreCase, AsciiHasSuffixIgnoreCase)
+    XX(AsciiEndsWithIgnoreCase, AsciiHasSuffixIgnoreCase)   \
+    XX(AsciiEqualsIgnoreCase, AsciiEqualsIgnoreCase)
 
 // NOTE: The functions below are marked as deprecated, so block implementation
 // is not required for them. Hence, STROKA_UDF provides only the scalar one at
@@ -490,6 +491,50 @@ SIMPLE_STRICT_UDF_OPTIONS(TReverse, TOptional<char*>(TOptional<char*>),
     };
 
     END_SIMPLE_ARROW_UDF(TContains, TContainsKernelExec::Do);
+
+    static bool IgnoreCaseComparator(char a, char b) {
+        return AsciiToUpper(a) == AsciiToUpper(b);
+    }
+
+    BEGIN_SIMPLE_STRICT_ARROW_UDF_OPTIONS(TAsciiContainsIgnoreCase, bool(TOptional<char*>, char*),
+        builder.SetMinLangVer(NYql::MakeLangVersion(2025, 2)))
+    {
+        Y_UNUSED(valueBuilder);
+        if (!args[0]) {
+            return TUnboxedValuePod(false);
+        }
+
+        const TString haystack(args[0].AsStringRef());
+        const TString needle(args[1].AsStringRef());
+        if (haystack.empty()) {
+            return TUnboxedValuePod(needle.empty());
+        }
+        const auto found = std::search(haystack.cbegin(), haystack.cend(),
+                                       needle.cbegin(), needle.cend(), IgnoreCaseComparator);
+        return TUnboxedValuePod(found != haystack.cend());
+    }
+
+    struct TAsciiContainsIgnoreCaseKernelExec
+        : public TBinaryKernelExec<TAsciiContainsIgnoreCaseKernelExec>
+    {
+        template <typename TSink>
+        static void Process(const IValueBuilder*, TBlockItem arg1, TBlockItem arg2, const TSink& sink) {
+            if (!arg1) {
+                return sink(TBlockItem(arg2 ? false : true));
+            }
+
+            const TString haystack(arg1.AsStringRef());
+            const TString needle(arg2.AsStringRef());
+            if (haystack.empty()) {
+                return sink(TBlockItem((needle.empty())));
+            }
+            const auto found = std::search(haystack.cbegin(), haystack.cend(),
+                                           needle.cbegin(), needle.cend(), IgnoreCaseComparator);
+            sink(TBlockItem(found != haystack.cend()));
+        }
+    };
+
+    END_SIMPLE_ARROW_UDF(TAsciiContainsIgnoreCase, TAsciiContainsIgnoreCaseKernelExec::Do);
 
 
     BEGIN_SIMPLE_STRICT_ARROW_UDF(TReplaceAll, char*(TAutoMap<char*>, char*, char*)) {
@@ -955,6 +1000,7 @@ SIMPLE_STRICT_UDF_OPTIONS(TReverse, TOptional<char*>(TOptional<char*>),
         TRemoveFirst,
         TRemoveLast,
         TContains,
+        TAsciiContainsIgnoreCase,
         TFind,
         TReverseFind,
         TSubstring,

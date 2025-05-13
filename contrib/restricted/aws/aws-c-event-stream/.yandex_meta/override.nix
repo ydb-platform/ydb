968c287aa993e890bb75507c46408020fe6cff59
robot-contrib <robot-contrib@yandex-team.com>
2025-05-13 11:38:50 +0300

    Update contrib/restricted/aws/aws-c-event-stream to 0.5.4 commit_hash:914e89750fdd22c5dcb1a365cfbca317e95a81d2

--- contrib/restricted/aws/aws-c-event-stream/.yandex_meta/override.nix
+++ contrib/restricted/aws/aws-c-event-stream/.yandex_meta/override.nix
@@ -1,10 +1,10 @@
 pkgs: attrs: with pkgs; with attrs; rec {
-  version = "0.4.2";
+  version = "0.5.4";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = "aws-c-event-stream";
     rev = "v${version}";
-    hash = "sha256-wj3PZshUay3HJy+v7cidDL4mDAqSDiX+MmQtJDK4rTI=";
+    hash = "sha256-Y4vyoNlYdIQg/NggBoYtX4CPiCzG24a4mKG8VGTdqy8=";
   };
 }

740531a342174f752185e68bd7f554b6fe3c8472
mrlolthe1st <mrlolthe1st@yandex-team.com>
2025-05-13 04:28:53 +0300

    YQL-19610: Split OrderedColumns into 2 pragmas commit_hash:9780948e26ef2e689d26520b76fefa0794efdd9e

--- yql/essentials/core/yql_type_annotation.cpp
+++ yql/essentials/core/yql_type_annotation.cpp
@@ -260,7 +260,7 @@ TMaybe<TColumnOrder> TTypeAnnotationContext::LookupColumnOrder(const TExprNode& 
 IGraphTransformer::TStatus TTypeAnnotationContext::SetColumnOrder(const TExprNode& node,
     const TColumnOrder& columnOrder, TExprContext& ctx)
 {
-    if (!OrderedColumns) {
+    if (!DeriveColumnOrder) {
         return IGraphTransformer::TStatus::Ok;
     }
 

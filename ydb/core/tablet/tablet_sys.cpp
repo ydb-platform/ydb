5e313cf9bc72fdacdcc8caa002bf30059158f339
Ivan Nikolaev <ivannik@ydb.tech>
2025-05-13 03:43:42 +0300

    Tablet sys: Retry failed GC requests

--- ydb/core/tablet/tablet_sys.cpp
+++ ydb/core/tablet/tablet_sys.cpp
@@ -35,6 +35,10 @@ namespace {
     static constexpr TDuration OfflineFollowerWaitFirst = TDuration::Seconds(4);
     static constexpr TDuration OfflineFollowerWaitRetry = TDuration::Seconds(15);
 
+    constexpr ui64 GcErrorInitialBackoffMs = 20;
+    constexpr ui64 GcErrorMaxBackoffMs = 10000;
+    constexpr ui64 GcMaxErrors = 20;  // ~3.33 min in total
+
 }
 
 ui64 TTablet::TabletID() const {
@@ -1335,18 +1339,35 @@ void TTablet::Handle(TEvBlobStorage::TEvCollectGarbageResult::TPtr &ev) {
         }
         break;
     case NKikimrProto::OK:
-        if (GcInFly == 0 && GcForStepAckRequest) {
-            const auto& req = *GcForStepAckRequest->Get();
-            const ui32 gen = StateStorageInfo.KnownGeneration;
-            if (std::tie(req.Generation, req.Step) <= std::tie(gen, GcInFlyStep)) {
-                Send(GcForStepAckRequest->Sender, new TEvTablet::TEvGcForStepAckResponse(gen, GcInFlyStep));
-                GcForStepAckRequest = nullptr;
+        if (GcInFly == 0) {
+            if (GcFailCount == 0) {
+                GcConfirmedStep = GcInFlyStep;
+                GcTryCounter = 0;
+                GcBackoffTimer.Reset();
+            }
+            if (GcForStepAckRequest) {
+                const auto& req = *GcForStepAckRequest->Get();
+                const ui32 gen = StateStorageInfo.KnownGeneration;
+                if (std::tie(req.Generation, req.Step) <= std::tie(gen, GcConfirmedStep)) {
+                    Send(GcForStepAckRequest->Sender, new TEvTablet::TEvGcForStepAckResponse(gen, GcConfirmedStep));
+                    GcForStepAckRequest = nullptr;
+                }
+            }
+            if (GcNextStep != 0) {
+                GcLogChannel(std::exchange(GcNextStep, 0));
             }
         }
-        [[fallthrough]];
-    default: // silently ignore unrecognized errors (assume temporary)
-        if (GcInFly == 0 && GcNextStep != 0) {
-            GcLogChannel(std::exchange(GcNextStep, 0));
+        return;
+    default:
+        ++GcFailCount;
+        if (GcInFly == 0) {
+            if (GcNextStep != 0) {
+                GcLogChannel(std::exchange(GcNextStep, 0));
+            } else if (!GcPendingRetry && GcTryCounter < GcMaxErrors) {
+                ++GcTryCounter;
+                GcPendingRetry = true;
+                Schedule(TDuration::MilliSeconds(GcBackoffTimer.NextBackoffMs()), new TEvTabletBase::TEvLogGcRetry());
+            }
         }
         return;
     }
@@ -1357,8 +1378,8 @@ void TTablet::Handle(TEvBlobStorage::TEvCollectGarbageResult::TPtr &ev) {
 void TTablet::Handle(TEvTablet::TEvGcForStepAckRequest::TPtr& ev) {
     const auto& req = *ev->Get();
     const ui32 gen = StateStorageInfo.KnownGeneration;
-    if (GcInFly == 0 && std::tie(req.Generation, req.Step) <= std::tie(gen, GcInFlyStep)) {
-        Send(ev->Sender, new TEvTablet::TEvGcForStepAckResponse(gen, GcInFlyStep));
+    if (GcInFly == 0 && std::tie(req.Generation, req.Step) <= std::tie(gen, GcConfirmedStep)) {
+        Send(ev->Sender, new TEvTablet::TEvGcForStepAckResponse(gen, GcConfirmedStep));
     } else {
         GcForStepAckRequest = ev;
     }
@@ -1367,6 +1388,8 @@ void TTablet::Handle(TEvTablet::TEvGcForStepAckRequest::TPtr& ev) {
 void TTablet::GcLogChannel(ui32 step) {
     const ui64 tabletid = TabletID();
     const ui32 gen = StateStorageInfo.KnownGeneration;
+    GcPendingRetry = false;
+    GcFailCount = 0;
 
     if (GcInFly != 0 || Graph.SyncCommit.SyncStep != 0 && Graph.SyncCommit.SyncStep <= step) {
         if (GcInFlyStep < step) {
@@ -1412,6 +1435,12 @@ void TTablet::GcLogChannel(ui32 step) {
     }
     GcInFlyStep = step;
     GcNextStep = 0;
+}
+
+void TTablet::RetryGcRequests() {
+    if (GcPendingRetry && GcInFly == 0 && GcInFlyStep > GcConfirmedStep) {
+        GcLogChannel(GcInFlyStep);
+    }
 }
 
 void TTablet::SpreadFollowerAuxUpdate(const TString& auxUpdate) {
@@ -1938,7 +1967,12 @@ TTablet::TTablet(const TActorId &launcher, TTabletStorageInfo *info, TTabletSetu
     , DiscoveredLastBlocked(Max<ui32>())
     , GcInFly(0)
     , GcInFlyStep(0)
+    , GcConfirmedStep(0)
     , GcNextStep(0)
+    , GcTryCounter(0)
+    , GcBackoffTimer(GcErrorInitialBackoffMs, GcErrorMaxBackoffMs)
+    , GcPendingRetry(false)
+    , GcFailCount(0)
     , ResourceProfiles(profiles)
     , TxCacheQuota(txCacheQuota)
 {

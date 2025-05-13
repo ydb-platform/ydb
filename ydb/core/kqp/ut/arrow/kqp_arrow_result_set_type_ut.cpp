3a2c98436bc884e6013f05dbc324c39b24020020
Daniil Timizhev <ditimizhev@ydb.tech>
2025-05-13 15:32:10 +0300

    Large tests

--- ydb/core/kqp/ut/arrow/kqp_arrow_result_set_type_ut.cpp
+++ ydb/core/kqp/ut/arrow/kqp_arrow_result_set_type_ut.cpp
@@ -9,9 +9,9 @@ using namespace NYdb::NQuery;
 
 namespace {
 
-TKikimrRunner CreateOlapKikimrRunner() {
+TKikimrRunner CreateKikimrRunner(bool isOlap = false) {
     NKikimrConfig::TAppConfig appConfig;
-    appConfig.MutableTableServiceConfig()->SetEnableOlapSink(true);
+    appConfig.MutableTableServiceConfig()->SetEnableOlapSink(isOlap);
 
     auto settings = TKikimrSettings().SetAppConfig(appConfig);
     return TKikimrRunner(settings);
@@ -23,6 +23,10 @@ void FillAllTypesOltp(TQueryClient& client) {
         CREATE TABLE `/Root/OltpTable` (
             Key Uint64,
             BoolValue Bool,
+            Int8Value Int8,
+            Uint8Value Uint8,
+            Int16Value Int16,
+            Uint16Value Uint16,
             Int32Value Int32,
             Uint32Value Uint32,
             Int64Value Int64,
@@ -48,8 +52,8 @@ void FillAllTypesOltp(TQueryClient& client) {
 
     auto insertResult = client.ExecuteQuery(R"(
         --!syntax_v1
-        INSERT INTO `/Root/OltpTable` (Key, BoolValue, Int32Value, Uint32Value, Int64Value, Uint64Value, FloatValue, DoubleValue, StringValue, Utf8Value, DateValue, DatetimeValue, TimestampValue, IntervalValue, DecimalValue, JsonValue, YsonValue, JsonDocumentValue, DyNumberValue, Int32NotNullValue) VALUES
-        (42, true, -1, 1, -2, 2, CAST(3.0 AS Float), 4.0, "five", Utf8("six"), Date("2007-07-07"), Datetime("2008-08-08T08:08:08Z"), Timestamp("2009-09-09T09:09:09.09Z"), Interval("P10D"), CAST("11.11" AS Decimal(22, 9)), "[12]", "[13]", JsonDocument("[14]"), DyNumber("15.15"), 123);
+        INSERT INTO `/Root/OltpTable` (Key, BoolValue, Int8Value, Uint8Value, Int16Value, Uint16Value, Int32Value, Uint32Value, Int64Value, Uint64Value, FloatValue, DoubleValue, StringValue, Utf8Value, DateValue, DatetimeValue, TimestampValue, IntervalValue, DecimalValue, JsonValue, YsonValue, JsonDocumentValue, DyNumberValue, Int32NotNullValue) VALUES
+        (42, true, -1, 1, -2, 2, -3, 3, -4, 4, CAST(3.0 AS Float), 4.0, "five", Utf8("six"), Date("2007-07-07"), Datetime("2008-08-08T08:08:08Z"), Timestamp("2009-09-09T09:09:09.09Z"), Interval("P10D"), CAST("11.11" AS Decimal(22, 9)), "[12]", "[13]", JsonDocument("[14]"), DyNumber("15.15"), 123);
     )", TTxControl::BeginTx().CommitTx()).GetValueSync();
     UNIT_ASSERT_C(insertResult.IsSuccess(), insertResult.GetIssues().ToString());
 }
@@ -59,6 +63,10 @@ void FillAllTypesOlap(TQueryClient& client) {
         --!syntax_v1
         CREATE TABLE `/Root/OlapTable` (
             Key Uint64 NOT NULL,
+            Int8Value Int8,
+            Uint8Value Uint8,
+            Int16Value Int16,
+            Uint16Value Uint16,
             Int32Value Int32,
             Uint32Value Uint32,
             Int64Value Int64,
@@ -82,8 +90,8 @@ void FillAllTypesOlap(TQueryClient& client) {
 
     auto insertResult = client.ExecuteQuery(R"(
         --!syntax_v1
-        INSERT INTO `/Root/OlapTable` (Key, Int32Value, Uint32Value, Int64Value, Uint64Value, FloatValue, DoubleValue, StringValue, Utf8Value, DateValue, DatetimeValue, TimestampValue, JsonValue, YsonValue, JsonDocumentValue) VALUES
-        (42, -1, 1, -2, 2, CAST(3.0 AS Float), 4.0, "five", Utf8("six"), Date("2007-07-07"), Datetime("2008-08-08T08:08:08Z"), Timestamp("2009-09-09T09:09:09.09Z"), "[12]", "[13]", JsonDocument("[14]"));
+        INSERT INTO `/Root/OlapTable` (Key, Int8Value, Uint8Value, Int16Value, Uint16Value, Int32Value, Uint32Value, Int64Value, Uint64Value, FloatValue, DoubleValue, StringValue, Utf8Value, DateValue, DatetimeValue, TimestampValue, JsonValue, YsonValue, JsonDocumentValue) VALUES
+        (42, -1, 1, -2, 2, -3, 3, -4, 4, CAST(3.0 AS Float), 4.0, "five", Utf8("six"), Date("2007-07-07"), Datetime("2008-08-08T08:08:08Z"), Timestamp("2009-09-09T09:09:09.09Z"), "[12]", "[13]", JsonDocument("[14]"));
     )", TTxControl::BeginTx().CommitTx()).GetValueSync();
     UNIT_ASSERT_C(insertResult.IsSuccess(), insertResult.GetIssues().ToString());
 }
@@ -91,8 +99,8 @@ void FillAllTypesOlap(TQueryClient& client) {
 } // namespace
 
 Y_UNIT_TEST_SUITE(KqpArrowResultSetType) {
-    Y_UNIT_TEST_TWIN(AllColumnTypes, isOlap) {
-        auto kikimr = CreateOlapKikimrRunner();
+    Y_UNIT_TEST_TWIN(AllTypes, isOlap) {
+        auto kikimr = CreateKikimrRunner(isOlap);
         auto client = kikimr.GetQueryClient();
 
         if (isOlap) {
@@ -101,7 +109,7 @@ Y_UNIT_TEST_SUITE(KqpArrowResultSetType) {
             FillAllTypesOltp(client);
         }
 
-        TString query = Sprintf(R"(
+        const TString query = Sprintf(R"(
             --!syntax_v1
             SELECT * FROM `/Root/%s`;
         )", (isOlap) ? "OlapTable" : "OltpTable");
@@ -112,6 +120,71 @@ Y_UNIT_TEST_SUITE(KqpArrowResultSetType) {
         auto arrowResponse = client.ExecuteQuery(query, TTxControl::BeginTx().CommitTx(),
             TExecuteQuerySettings().ResultSetType(TResultSet::EType::Arrow)).GetValueSync();
 
+        UNIT_ASSERT_C(messageResponse.IsSuccess(), messageResponse.GetIssues().ToString());
+        UNIT_ASSERT_C(arrowResponse.IsSuccess(), arrowResponse.GetIssues().ToString());
+
+        const auto& messageResultSet = messageResponse.GetResultSet(0);
+        const auto& arrowResultSet = arrowResponse.GetResultSet(0);
+
+        UNIT_ASSERT_VALUES_EQUAL(messageResultSet.RowsCount(), arrowResultSet.RowsCount());
+        UNIT_ASSERT_VALUES_EQUAL(messageResultSet.ColumnsCount(), arrowResultSet.ColumnsCount());
+
+        std::shared_ptr<arrow::RecordBatch> batch = arrowResultSet.GetArrowBatch();
+
+        UNIT_ASSERT_VALUES_EQUAL(messageResultSet.RowsCount(), static_cast<size_t>(batch->num_rows()));
+        UNIT_ASSERT_VALUES_EQUAL(messageResultSet.ColumnsCount(), static_cast<size_t>(batch->num_columns()));
+    }
+
+    Y_UNIT_TEST(LargeTable) {
+        auto kikimr = CreateKikimrRunner();
+        auto client = kikimr.GetQueryClient();
+
+        CreateLargeTable(kikimr, 10000, 4, 10, 5000, 10);
+
+        const TString query = Sprintf(R"(
+            --!syntax_v1
+            SELECT * FROM `/Root/LargeTable`;
+        )");
+
+        auto messageResponse = client.ExecuteQuery(query, TTxControl::BeginTx().CommitTx(),
+            TExecuteQuerySettings().ResultSetType(TResultSet::EType::Message)).GetValueSync();
+
+        auto arrowResponse = client.ExecuteQuery(query, TTxControl::BeginTx().CommitTx(),
+            TExecuteQuerySettings().ResultSetType(TResultSet::EType::Arrow)).GetValueSync();
+
+        UNIT_ASSERT_C(messageResponse.IsSuccess(), messageResponse.GetIssues().ToString());
+        UNIT_ASSERT_C(arrowResponse.IsSuccess(), arrowResponse.GetIssues().ToString());
+
+        const auto& messageResultSet = messageResponse.GetResultSet(0);
+        const auto& arrowResultSet = arrowResponse.GetResultSet(0);
+
+        UNIT_ASSERT_VALUES_EQUAL(messageResultSet.RowsCount(), arrowResultSet.RowsCount());
+        UNIT_ASSERT_VALUES_EQUAL(messageResultSet.ColumnsCount(), arrowResultSet.ColumnsCount());
+
+        std::shared_ptr<arrow::RecordBatch> batch = arrowResultSet.GetArrowBatch();
+
+        UNIT_ASSERT_VALUES_EQUAL(messageResultSet.RowsCount(), static_cast<size_t>(batch->num_rows()));
+        UNIT_ASSERT_VALUES_EQUAL(messageResultSet.ColumnsCount(), static_cast<size_t>(batch->num_columns()));
+    }
+
+    Y_UNIT_TEST(LargeLimitTable) {
+        auto kikimr = CreateKikimrRunner();
+        auto client = kikimr.GetQueryClient();
+
+        CreateLargeTable(kikimr, 10000, 4, 10, 5000, 10);
+
+        const TString query = Sprintf(R"(
+            --!syntax_v1
+            SELECT * FROM `/Root/LargeTable` LIMIT 70000;
+        )");
+
+        auto messageResponse = client.ExecuteQuery(query, TTxControl::BeginTx().CommitTx(),
+            TExecuteQuerySettings().ResultSetType(TResultSet::EType::Message)).GetValueSync();
+
+        auto arrowResponse = client.ExecuteQuery(query, TTxControl::BeginTx().CommitTx(),
+            TExecuteQuerySettings().ResultSetType(TResultSet::EType::Arrow)).GetValueSync();
+
+        UNIT_ASSERT_C(messageResponse.IsSuccess(), messageResponse.GetIssues().ToString());
         UNIT_ASSERT_C(arrowResponse.IsSuccess(), arrowResponse.GetIssues().ToString());
 
         const auto& messageResultSet = messageResponse.GetResultSet(0);

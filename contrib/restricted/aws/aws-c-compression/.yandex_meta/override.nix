185c8c3764153ef9a6ee5c0a138e8ce7d3feb858
robot-contrib <robot-contrib@yandex-team.com>
2025-05-13 06:41:59 +0300

    Update contrib/restricted/aws/aws-c-compression to 0.2.18 commit_hash:0c3c615ae7a50e043cc974b00389e27bacd5fcdd

--- contrib/restricted/aws/aws-c-compression/.yandex_meta/override.nix
+++ contrib/restricted/aws/aws-c-compression/.yandex_meta/override.nix
@@ -1,10 +1,10 @@
 pkgs: attrs: with pkgs; with attrs; rec {
-  version = "0.2.16";
+  version = "0.2.18";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = "aws-c-compression";
     rev = "v${version}";
-    hash = "sha256-aQ5UsMms8aJh5yrE9of1AQgIGTAk9vyBRaybwYqUY68=";
+    hash = "sha256-Cf3MvoRWGAy+vlE59JSpTGOBl07dI4mbIaL1HIiLN/I=";
   };
 }

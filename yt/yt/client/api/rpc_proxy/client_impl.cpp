e34bdd10f9ace51780a655ae4ba1b949d44c2f23
ermolovd <ermolovd@yandex-team.com>
2025-05-14 18:03:41 +0300

    YT-24245: integrating multiproxy stuff
    * Changelog entry
    Type: feature
    Component: rpc-proxy
    
    Multiproxy mode in rpc-proxy. Client can use multiproxy of one cluster
    to operate other connected clusters.
    commit_hash:cf2731cd1e6f9b534e3c80b20ad858f081ed8037

--- yt/yt/client/api/rpc_proxy/client_impl.cpp
+++ yt/yt/client/api/rpc_proxy/client_impl.cpp
@@ -7,6 +7,7 @@
 #include "row_batch_writer.h"
 #include "table_mount_cache.h"
 #include "table_writer.h"
+#include "target_cluster_injecting_channel.h"
 #include "timestamp_provider.h"
 #include "transaction.h"
 
@@ -65,12 +66,10 @@ TClient::TClient(
     TConnectionPtr connection,
     const TClientOptions& clientOptions)
     : Connection_(std::move(connection))
-    , RetryingChannel_(CreateSequoiaAwareRetryingChannel(
-        CreateCredentialsInjectingChannel(
-            Connection_->CreateChannel(false),
-            clientOptions),
-        /*retryProxyBanned*/ true))
     , ClientOptions_(clientOptions)
+    , RetryingChannel_(CreateSequoiaAwareRetryingChannel(
+        WrapNonRetryingChannel(Connection_->CreateChannel(false)),
+        /*retryProxyBanned*/ true))
     , TableMountCache_(BIND(
         &CreateTableMountCache,
         Connection_->GetConfig()->TableMountCache,
@@ -116,9 +115,7 @@ IChannelPtr TClient::CreateSequoiaAwareRetryingChannel(IChannelPtr channel, bool
 
 IChannelPtr TClient::CreateNonRetryingChannelByAddress(const std::string& address) const
 {
-    return CreateCredentialsInjectingChannel(
-        Connection_->CreateChannelByAddress(address),
-        ClientOptions_);
+    return WrapNonRetryingChannel(Connection_->CreateChannelByAddress(address));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -142,9 +139,7 @@ IChannelPtr TClient::GetRetryingChannel() const
 
 IChannelPtr TClient::CreateNonRetryingStickyChannel() const
 {
-    return CreateCredentialsInjectingChannel(
-        Connection_->CreateChannel(true),
-        ClientOptions_);
+    return WrapNonRetryingChannel(Connection_->CreateChannel(true));
 }
 
 IChannelPtr TClient::WrapStickyChannelIntoRetrying(IChannelPtr underlying) const
@@ -152,6 +147,19 @@ IChannelPtr TClient::WrapStickyChannelIntoRetrying(IChannelPtr underlying) const
     return CreateSequoiaAwareRetryingChannel(
         std::move(underlying),
         /*retryProxyBanned*/ false);
+}
+
+IChannelPtr TClient::WrapNonRetryingChannel(IChannelPtr underlying) const
+{
+    auto credentialsInjected = CreateCredentialsInjectingChannel(
+        std::move(underlying),
+        ClientOptions_);
+
+    auto targetClusterInjected = CreateTargetClusterInjectingChannel(
+        std::move(credentialsInjected),
+        ClientOptions_.MultiproxyTargetCluster);
+
+    return targetClusterInjected;
 }
 
 ////////////////////////////////////////////////////////////////////////////////

0ccb7680e0d44713fec49f415247e65f56b9a74c
ivanmorozov333 <ivanmorozov@ydb.tech>
2025-05-19 19:24:28 +0300

    snapshot interval (#18452)
    Co-authored-by: ivanmorozov333 <imorozov333@ya.ru>

--- ydb/core/tx/columnshard/engines/storage/optimizer/lcbuckets/constructor/level/one_layer.h
+++ ydb/core/tx/columnshard/engines/storage/optimizer/lcbuckets/constructor/level/one_layer.h
@@ -15,14 +15,11 @@ private:
     ui64 SizeLimitGuarantee = 0;
 
     virtual std::shared_ptr<IPortionsLevel> DoBuildLevel(const std::shared_ptr<IPortionsLevel>& nextLevel, const ui32 indexLevel,
-        const std::shared_ptr<TSimplePortionsGroupInfo>& portionsInfo, const TLevelCounters& counters) const override;
+        const std::shared_ptr<TSimplePortionsGroupInfo>& portionsInfo, const TLevelCounters& counters,
+        const std::vector<std::shared_ptr<IPortionsSelector>>& selectors) const override;
     virtual TConclusionStatus DoDeserializeFromJson(const NJson::TJsonValue& json) override;
     virtual bool DoDeserializeFromProto(const NKikimrSchemeOp::TCompactionLevelConstructorContainer& proto) override;
     virtual void DoSerializeToProto(NKikimrSchemeOp::TCompactionLevelConstructorContainer& proto) const override;
-    virtual bool IsEqualToSameClass(const ILevelConstructor& item) const override {
-        const auto& itemCast = dynamic_cast<const TOneLayerConstructor&>(item);
-        return BytesLimitFraction == itemCast.BytesLimitFraction && ExpectedPortionSize == itemCast.ExpectedPortionSize;
-    }
 
     static const inline TFactory::TRegistrator<TOneLayerConstructor> Registrator =
         TFactory::TRegistrator<TOneLayerConstructor>(GetClassNameStatic());

adaa4f0cafbe6620ad0e09773303593e253cef0c
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2025-05-19 15:57:35 +0300

    review fixes

--- ydb/tests/compatibility/test_example.py
+++ ydb/tests/compatibility/test_example.py
@@ -8,12 +8,22 @@ from ydb.tests.oss.ydb_sdk_import import ydb
 class TestExampleMixedClusterFixture(MixedClusterFixture):
     @pytest.fixture(autouse=True, scope="function")
     def setup(self):
-        yield from self.setup_cluster()
+        #
+        # Setup cluster
+        #
+        yield from self.setup_cluster(
+            # # Some feature flags can be passed. And other KikimrConfigGenerator options
+            # extra_feature_flags={
+            #     "some_feature_flag": True,
+            # }
+        )
 
     def test_example(self):
+        #
+        # 1. Fill table with data
+        #
         table_name = "table_name"
         value = 42
-
         with ydb.QuerySessionPool(self.driver) as session_pool:
             query = f"""
                     CREATE TABLE `{table_name}` (
@@ -26,6 +36,10 @@ class TestExampleMixedClusterFixture(MixedClusterFixture):
             query = f"""UPSERT INTO `{table_name}` (id, value) VALUES (1, {value});"""
             session_pool.execute_with_retries(query)
 
+        #
+        # 2. check written data is correct
+        #
+        with ydb.QuerySessionPool(self.driver) as session_pool:
             query = f"""SELECT id, value FROM `{table_name}` WHERE id = 1;"""
             result_sets = session_pool.execute_with_retries(query)
 
@@ -36,12 +50,22 @@ class TestExampleMixedClusterFixture(MixedClusterFixture):
 class TestExampleRestartToAnotherVersion(RestartToAnotherVersionFixture):
     @pytest.fixture(autouse=True, scope="function")
     def setup(self):
-        yield from self.setup_cluster()
+        #
+        # Setup cluster
+        #
+        yield from self.setup_cluster(
+            # # Some feature flags can be passed. And other KikimrConfigGenerator options
+            # extra_feature_flags={
+            #     "some_feature_flag": True,
+            # }
+        )
 
     def test_example(self):
+        #
+        # 1. Fill table with data
+        #
         table_name = "table_name"
         value = 42
-
         with ydb.QuerySessionPool(self.driver) as session_pool:
             query = f"""
                     CREATE TABLE `{table_name}` (
@@ -60,8 +84,14 @@ class TestExampleRestartToAnotherVersion(RestartToAnotherVersionFixture):
             assert result_sets[0].rows[0]["id"] == 1
             assert result_sets[0].rows[0]["value"] == value
 
-        self.change_cluster_version()  # change version to another
+        #
+        # 2. stop the cluster, and start using another version
+        #
+        self.change_cluster_version()
 
+        #
+        # 3. check written data is correct
+        #
         with ydb.QuerySessionPool(self.driver) as session_pool:
             query = f"""SELECT id, value FROM `{table_name}` WHERE id = 1;"""
             result_sets = session_pool.execute_with_retries(query)
@@ -73,12 +103,23 @@ class TestExampleRestartToAnotherVersion(RestartToAnotherVersionFixture):
 class TestExampleRollingUpdate(RollingUpgradeAndDowngradeFixture):
     @pytest.fixture(autouse=True, scope="function")
     def setup(self):
-        yield from self.setup_cluster()
+        #
+        # Setup cluster
+        #
+        yield from self.setup_cluster(
+            # # Some feature flags can be passed. And other KikimrConfigGenerator options
+            # extra_feature_flags={
+            #     "some_feature_flag": True,
+            # }
+        )
 
     def test_example(self):
         table_name = "table_name"
         value = 42
 
+        #
+        # 1. Fill table with data
+        #
         with ydb.QuerySessionPool(self.driver) as session_pool:
             query = f"""
                     CREATE TABLE `{table_name}` (
@@ -92,7 +133,10 @@ class TestExampleRollingUpdate(RollingUpgradeAndDowngradeFixture):
             query = f"""UPSERT INTO `{table_name}` (id, value) VALUES (1, {value});"""
             session_pool.execute_with_retries(query)
 
-        for _ in self.roll():
+        for _ in self.roll():  # every iteration is a step in rolling upgrade process
+            #
+            # 2. check written data is correct during rolling upgrade
+            #
             with ydb.QuerySessionPool(self.driver) as session_pool:
                 time.sleep(5)  # TODO: fix
 

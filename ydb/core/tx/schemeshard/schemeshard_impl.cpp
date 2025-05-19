79e817dd38eef584eeec1becbbd58faf0dffb764
Dmitry Kardymon <kardymon-d@ydb.tech>
2025-05-19 08:15:53 +0300

    YQ-4084 Add AllExternalDataSourcesAreAvailable to QueryServiceConfig (#18042)

--- ydb/core/tx/schemeshard/schemeshard_impl.cpp
+++ ydb/core/tx/schemeshard/schemeshard_impl.cpp
@@ -7609,6 +7609,7 @@ void TSchemeShard::ConfigureExternalSources(
         nullptr,
         EnableExternalSourceSchemaInference,
         config.GetS3().GetAllowLocalFiles(),
+        config.GetAllExternalDataSourcesAreAvailable(),
         std::set<TString>(availableExternalDataSources.cbegin(), availableExternalDataSources.cend())
     );
 

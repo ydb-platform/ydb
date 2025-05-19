cb8c57cce0640c3cebe9715a50a0900f21c4cb87
vporyadke <zalyalov@ydb.tech>
2025-05-19 10:48:25 +0200

    add sensor for down nodes (#11592)

--- ydb/core/mind/hive/hive_impl.h
+++ ydb/core/mind/hive/hive_impl.h
@@ -678,6 +678,8 @@ TTabletInfo* FindTabletEvenInDeleting(TTabletId tabletId, TFollowerId followerId
     void UpdateCounterTabletsStarting(i64 tabletsStartingDiff);
     void UpdateCounterPingQueueSize();
     void UpdateCounterTabletChannelHistorySize();
+    void UpdateCounterNodesDown(i64 nodesDownDiff);
+    void UpdateCounterNodesFrozen(i64 nodesFrozenDiff);
     void RecordTabletMove(const TTabletMoveInfo& info);
     bool DomainHasNodes(const TSubDomainKey &domainKey) const;
     void ProcessBootQueue();

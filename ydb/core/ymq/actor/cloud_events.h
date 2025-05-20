c823f2a3a35a1258c9be612d9ecc88d152d821cb
Igor <igorkoshkarev4qqq@mail.ru>
2025-05-20 15:31:36 +0000

    create actors skeleton and fix ymq.proto

--- ydb/core/ymq/actor/cloud_events.h
+++ ydb/core/ymq/actor/cloud_events.h
@@ -3,11 +3,7 @@
 #include <ydb/public/api/client/yc_public/events/ymq.pb.h>
 #include <ydb/public/api/client/yc_public/events/common.pb.h>
 
-#include <ydb/core/audit/audit_log_helpers.h>
-
-#include <ydb/core/ymq/base/events_writer_iface.h>
 #include <ydb/core/kqp/common/kqp.h>
-#include <ydb/public/lib/deprecated/kicli/kicli.h>
 
 #include <ydb/public/sdk/cpp/include/ydb-cpp-sdk/client/proto/accessor.h>
 #include <ydb/public/sdk/cpp/include/ydb-cpp-sdk/client/table/table.h>
@@ -21,14 +17,36 @@ namespace NCloudEvents {
     using TCreateQueueEvent = yandex::cloud::events::ymq::CreateQueue;
     using TUpdateQueueEvent = yandex::cloud::events::ymq::UpdateQueue;
     using TDeleteQueueEvent = yandex::cloud::events::ymq::DeleteQueue;
+    using EStatus = yandex::cloud::events::EventStatus;
 
     struct TEventInfo {
-        NACLib::TUserToken UserToken;
-        TString AuthType;
+        std::string UserSID;                                    // We need
+        std::string UserSanitizedToken;                         // We need
+        std::string AuthType;                                   // We need 
+
+        static constexpr std::string_view Permission = "ymq.queues.list";
+        static constexpr std::string_view ResourceType = "message-queue";
+        // ResourceId = FolderId
+
+        std::string Id;                                         // On place
+        std::string Type;                                       // We need
+        uint_fast64_t Timestamp;                                // We need
+        std::string CloudId;                                    // We need
+        std::string FolderId;                                   // We need
+
+        std::string RemoteAddress;                              // We need one field 'remote_address (or peer_name) for remote address and remote port'
+        std::string RemotePort;
+        std::string RequestId;                                  // We need
+        std::string IdempotencyId;                              // We need
+
+        std::string Issue = "";                                 // On place
+
+        std::string Name;                                       // We need
+        std::unordered_map<std::string, std::string> Labels;    // We need it as a string
     };
 
     template<typename TProtoEvent>
-    class TFillerBase {
+    class TFiller {
     protected:
         TEventInfo EventInfo;
         TProtoEvent& Ev; // be careful!
@@ -37,88 +55,78 @@ namespace NCloudEvents {
         void FillAuthorization();
         void FillEventMetadata();
         void FillRequestMetadata();
-        void FillEventStatus();
+        void FillStatus();
         void FillDetails();
     public:
         void Fill();
 
-        TFillerBase(const TEventInfo& eventInfo, TProtoEvent& ev)
+        TFiller(const TEventInfo& eventInfo, TProtoEvent& ev)
         : EventInfo(eventInfo)
         , Ev(ev)
         {}
     };
 
-    class TFillerCreateQueue : public TFillerBase<TCreateQueueEvent> {
-    public:
-        TFillerCreateQueue(const TEventInfo& eventInfo, TCreateQueueEvent& ev)
-        : TFillerBase(eventInfo, ev)
-        {}
-
-        void Fill();
-    };
-
-    class TFillerUpdateQueue : public TFillerBase<TUpdateQueueEvent> {
-    public:
-        TFillerUpdateQueue(const TEventInfo& eventInfo, TUpdateQueueEvent& ev)
-        : TFillerBase(eventInfo, ev)
-        {}
-
-        void Fill();
-    };
-
-    class TFillerDeleteQueue : public TFillerBase<TDeleteQueueEvent> {
-    public:
-        TFillerDeleteQueue(const TEventInfo& eventInfo, TDeleteQueueEvent& ev)
-        : TFillerBase(eventInfo, ev)
-        {}
-
-        void Fill();
-    };
-
     template<typename TProtoEvent>
-    class TSender {
+    class TAuditSender {
     public:
-        void Send(const TProtoEvent& ev);
+        static void Send(const TProtoEvent& ev);
     };
 
-    template class TSender<TCreateQueueEvent>;
-    template class TSender<TUpdateQueueEvent>;
-    template class TSender<TDeleteQueueEvent>;
+    template class TAuditSender<TCreateQueueEvent>;
+    template class TAuditSender<TUpdateQueueEvent>;
+    template class TAuditSender<TDeleteQueueEvent>;
 
 
     class TProcessor : public NActors::TActorBootstrapped<TProcessor> {
     private:
-        const TDuration RescanInterval;
+        static constexpr std::string_view EventTableName = ".CloudEventsYmq";
+        static constexpr TDuration DefaultRetryTimeout = TDuration::Seconds(10);
+
+        const std::string Root;
+
+        constexpr std::string_view GetSelectQuery() const;
+        constexpr std::string_view GetDeleteQuery() const;
+
+        uint_fast64_t LastCookie = 0;
+        std::string LastQuery = "";
+
+        void RunQuery(std::string_view query);
 
     public:
         TProcessor(
-            const TDuration& rescanInterval
+            std::string root
         )
-        : RescanInterval(rescanInterval)
+        : Root(root)
         {}
 
         void Bootstrap();
-    
-    private:
-        // Try do not use TActorContext: those handlers are depreceted
-        void HandleWakeup(const NActors::TEvents::TEvWakeup::TPtr& ev);
-        void HandleQueryResponse(const NKqp::TEvKqp::TEvQueryResponse::TPtr& ev);
 
-        STRICT_STFUNC(StateFunc,
+    private:
+        void HandleWakeup(const NActors::TEvents::TEvWakeup::TPtr&);
+        void HandleRetry(const NActors::TEvents::TEvUndelivered::TPtr&);
+        void HandleSelectResponse(const NKqp::TEvKqp::TEvQueryResponse::TPtr&);
+        void HandleDeleteResponse(const NKqp::TEvKqp::TEvQueryResponse::TPtr&);
+
+        STRICT_STFUNC(
+            StateWaitWakeUp,
+            IgnoreFunc(NActors::TEvents::TEvUndelivered);
             hFunc(NActors::TEvents::TEvWakeup, HandleWakeup);
-            hFunc(NKqp::TEvKqp::TEvQueryResponse, HandleQueryResponse);
             cFunc(TEvPoisonPill::EventType, PassAway);
-            IgnoreFunc(NKqp::TEvKqp::TEvCloseSessionResponse);
         )
 
-        enum class EState {
-            Initial,
-            QueuesListingExecute,
-            EventsListingExecute,
-            CleanupExecute,
-            Stopping
-        };
-        [[maybe_unused]] EState State = EState::Initial;
+        STRICT_STFUNC(
+            StateWaitSelectResponse,
+            hFunc(NActors::TEvents::TEvUndelivered, HandleRetry);
+            hFunc(NKqp::TEvKqp::TEvQueryResponse, HandleSelectResponse);
+            cFunc(TEvPoisonPill::EventType, PassAway);
+        )
+
+        STRICT_STFUNC(
+            StateWaitDeleteResponse,
+            hFunc(NActors::TEvents::TEvUndelivered, HandleRetry);
+            hFunc(NKqp::TEvKqp::TEvQueryResponse, HandleDeleteResponse);
+            cFunc(TEvPoisonPill::EventType, PassAway);
+        )
     };
 
 } // namespace NCloudEvents

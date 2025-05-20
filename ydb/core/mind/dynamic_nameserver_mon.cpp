abb216f2d1b8f03025b030e0d867d1e928908f09
Ilia Shakhov <pixcc@ydb.tech>
2025-05-20 14:12:08 +0300

    Implement `NodeBroker` Delta Protocol (client-side) (#17616)

--- ydb/core/mind/dynamic_nameserver_mon.cpp
+++ ydb/core/mind/dynamic_nameserver_mon.cpp
@@ -92,6 +92,19 @@ void OutputNodeInfo(ui32 nodeId,
     str << "</tr>" << Endl;
 }
 
+void OutputExpiredNodeInfo(ui32 nodeId,IOutputStream &str, const TString &cl = "")
+{
+    str << "<tr class='" << cl << "'>" << Endl
+    << "  <td>" << nodeId << "</td>" << Endl
+    << "  <td>N/A</td>" << Endl
+    << "  <td>N/A</td>" << Endl
+    << "  <td>N/A</td>" << Endl
+    << "  <td>N/A</td>" << Endl
+    << "  <td>N/A</td>" << Endl;
+    str << "<td>N/A</td>" << Endl;
+    str << "</tr>" << Endl;
+}
+
 void OutputStaticNodes(const TTableNameserverSetup &setup,
                        IOutputStream &str)
 {
@@ -146,11 +159,10 @@ void OutputDynamicNodes(const TString &domain,
     }
 
     ids.clear();
-    for (auto &pr : config->ExpiredNodes)
-        ids.insert(pr.first);
+    for (auto id : config->ExpiredNodes)
+        ids.insert(id);
     for (auto id : ids) {
-        auto &node = config->ExpiredNodes.at(id);
-        OutputNodeInfo(id, node, str, node.Expire, "gray");
+        OutputExpiredNodeInfo(id, str, "gray");
     }
 
     str << "  </tbody>" << Endl

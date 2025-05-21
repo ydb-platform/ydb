13ee1abc8bf9cf677fff0438924a09be02fad060
Emil Garipov <emgariko@ydb.tech>
2025-05-21 20:06:19 +0300

    fixed comment

--- ydb/tests/olap/test_log_scenario.py
+++ ydb/tests/olap/test_log_scenario.py
@@ -189,7 +189,7 @@ class TestLogScenario(object):
                                                       database=self.ydb_client.database,
                                                       table_name=self.table_name,
                                                       date_from=0,
-                                                      date_to=2678400000)  # Sunday, 1 February 1970, 0:00:00
+                                                      date_to=2678400000)  # Monday, 16 November 2054, 0:00:00 
         ydb_workload.create_table(self.table_name)
         ydb_workload.bulk_upsert(seconds=wait_time, threads=10, rows=1, wait=True)
         logging.info(f"Count rows after insert {self.get_row_count()} before wait")

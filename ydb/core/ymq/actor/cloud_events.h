7b2f6111c11af50049ff4f20f29ec6e61fc80121
Igor <igorkoshkarev4qqq@mail.ru>
2025-05-22 17:25:22 +0000

    impl main part of actor

--- ydb/core/ymq/actor/cloud_events.h
+++ ydb/core/ymq/actor/cloud_events.h
@@ -1,7 +1,6 @@
 #include "events.h"
 
 #include <ydb/public/api/client/yc_public/events/ymq.pb.h>
-#include <ydb/public/api/client/yc_public/events/common.pb.h>
 
 #include <ydb/core/kqp/common/kqp.h>
 
@@ -20,36 +19,119 @@ namespace NCloudEvents {
     using EStatus = yandex::cloud::events::EventStatus;
 
     struct TEventInfo {
-        std::string UserSID;                                    // We need
-        std::string UserSanitizedToken;                         // We need
-        std::string AuthType;                                   // We need 
+        TString UserSID;                                    // We need
+        TString UserSanitizedToken;                         // We need
+        TString AuthType;                                   // We need 
 
         static constexpr std::string_view Permission = "ymq.queues.list";
         static constexpr std::string_view ResourceType = "message-queue";
         // ResourceId = FolderId
 
-        std::string Id;                                         // On place
-        std::string Type;                                       // We need
-        uint_fast64_t Timestamp;                                // We need
-        std::string CloudId;                                    // We need
-        std::string FolderId;                                   // We need
+        TString Id;                                         // We need (generate on audit.log as guid; but in cloud we send smth like Type$Id$CreatedAt)
+        TString Type;                                       // We need
+        uint_fast64_t CreatedAt;                            // We need
+        TString CloudId;                                    // We need
+        TString FolderId;                                   // We need
 
-        std::string RemoteAddress;                              // We need one field 'remote_address (or peer_name) for remote address and remote port'
-        std::string RemotePort;
-        std::string RequestId;                                  // We need
-        std::string IdempotencyId;                              // We need
+        TString RemoteAddress;                              // We need
+        TString RequestId;                                  // We need
+        TString IdempotencyId;                              // We need
 
-        std::string Issue = "";                                 // On place
+        TString Issue = "";                                 // Is always empty?
 
-        std::string Name;                                       // We need
-        std::unordered_map<std::string, std::string> Labels;    // We need it as a string
+        TString Name;                                       // We need
+        THashMap<TBasicString<char>, NJson::TJsonValue> Labels;        // We need it as a string
     };
+
+    /*
+        CREATE TABLE `/Root/.CloudEventsYmq` (
+            Id Uint64,
+            Name String,
+
+            Type String,
+            CreatedAt Uint64,
+            CloudId String,
+            FolderId String,
+            UserSID String,
+            UserSanitizedToken String,
+            AuthType String,
+            PeerName String,
+            RequestId String,
+            IdempotencyId String,
+            Labels String,
+            PRIMARY KEY(Id, Name)
+        );
+    */
+
+    /*
+    UPSERT INTO CloudEventsYmq (
+    Id,
+    Name,
+    Type,
+    CreatedAt,
+    CloudId,
+    FolderId,
+    UserSID,
+    UserSanitizedToken,
+    AuthType,
+    PeerName,
+    RequestId,
+    IdempotencyId,
+    Labels
+    )
+    VALUES
+    (
+    1001,
+    "test_name_1",
+    "create",
+    1696506100,
+    "cloud123",
+    "folderABC",
+    "userSID_1",
+    "sanitizedToken_1",
+    "OAuth",
+    "peer_name_1",
+    "req_id_1",
+    "idemp_1",
+    "label_1"
+    ),
+    (
+    1002,
+    "test_name_2",
+    "delete",
+    1696506200,
+    "cloud456",
+    "folderXYZ",
+    "userSID_2",
+    "sanitizedToken_2",
+    "IamToken",
+    "peer_name_2",
+    "req_id_2",
+    "idemp_2",
+    "label_2"
+    ),
+    (
+    1003,
+    "test_name_3",
+    "update",
+    1696506300,
+    "cloud789",
+    "folderLMN",
+    "userSID_3",
+    "sanitizedToken_3",
+    "ApiKey",
+    "peer_name_3",
+    "req_id_3",
+    "idemp_3",
+    "label_3"
+    );
+    */
 
     template<typename TProtoEvent>
     class TFiller {
     protected:
-        TEventInfo EventInfo;
-        TProtoEvent& Ev; // be careful!
+        const TEventInfo& EventInfo;    // be careful!
+        TProtoEvent& Ev;                // be careful!
 
         void FillAuthentication();
         void FillAuthorization();
@@ -66,44 +148,59 @@ namespace NCloudEvents {
         {}
     };
 
-    template<typename TProtoEvent>
     class TAuditSender {
     public:
+        template<typename TProtoEvent>
         static void Send(const TProtoEvent& ev);
     };
 
-    template class TAuditSender<TCreateQueueEvent>;
-    template class TAuditSender<TUpdateQueueEvent>;
-    template class TAuditSender<TDeleteQueueEvent>;
-
+    template void TAuditSender::Send<TCreateQueueEvent>(const TCreateQueueEvent&);
+    template void TAuditSender::Send<TUpdateQueueEvent>(const TUpdateQueueEvent&);
+    template void TAuditSender::Send<TDeleteQueueEvent>(const TDeleteQueueEvent&);
 
     class TProcessor : public NActors::TActorBootstrapped<TProcessor> {
     private:
         static constexpr std::string_view EventTableName = ".CloudEventsYmq";
         static constexpr TDuration DefaultRetryTimeout = TDuration::Seconds(10);
 
-        const std::string Root;
+        std::vector<TEventInfo> EventsList;
 
-        constexpr std::string_view GetSelectQuery() const;
-        constexpr std::string_view GetDeleteQuery() const;
+        const TString Root;
+        const TString Database;
 
-        uint_fast64_t LastCookie = 0;
-        std::string LastQuery = "";
+        const TString SelectQuery;
+        const TString DeleteQuery;
 
-        void RunQuery(std::string_view query);
+        TString GetFullTablePath() const;
+        TString GetInitSelectQuery() const;
+        TString GetInitDeleteQuery() const;
+
+        TString SessionId = TString();
+
+        enum ELastQueryType {
+            None,
+            Select,
+            Delete
+        } LastQuery = ELastQueryType::None;
+
+        void RunQuery(TString query, std::unique_ptr<NYdb::TParams> params = nullptr);
+        void UpdateSessionId(const NKqp::TEvKqp::TEvQueryResponse::TPtr& ev);
+        void StopSession();
+        void ProcessFailure();
+
+        static std::vector<TEventInfo> ConvertSelectResponseToEventList(const ::NKikimrKqp::TQueryResponse& response);
 
     public:
         TProcessor(
-            std::string root
-        )
-        : Root(root)
-        {}
+            TString root,
+            TString database
+        );
 
         void Bootstrap();
 
     private:
         void HandleWakeup(const NActors::TEvents::TEvWakeup::TPtr&);
-        void HandleRetry(const NActors::TEvents::TEvUndelivered::TPtr&);
+        void HandleUndelivered(const NActors::TEvents::TEvUndelivered::TPtr&);
         void HandleSelectResponse(const NKqp::TEvKqp::TEvQueryResponse::TPtr&);
         void HandleDeleteResponse(const NKqp::TEvKqp::TEvQueryResponse::TPtr&);
 
@@ -116,14 +213,14 @@ namespace NCloudEvents {
 
         STRICT_STFUNC(
             StateWaitSelectResponse,
-            hFunc(NActors::TEvents::TEvUndelivered, HandleRetry);
+            hFunc(NActors::TEvents::TEvUndelivered, HandleUndelivered);
             hFunc(NKqp::TEvKqp::TEvQueryResponse, HandleSelectResponse);
             cFunc(TEvPoisonPill::EventType, PassAway);
         )
 
         STRICT_STFUNC(
             StateWaitDeleteResponse,
-            hFunc(NActors::TEvents::TEvUndelivered, HandleRetry);
+            hFunc(NActors::TEvents::TEvUndelivered, HandleUndelivered);
             hFunc(NKqp::TEvKqp::TEvQueryResponse, HandleDeleteResponse);
             cFunc(TEvPoisonPill::EventType, PassAway);
         )

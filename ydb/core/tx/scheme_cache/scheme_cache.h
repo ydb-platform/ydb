df51436934964a3d3633b42e57f93ffd6756d6b1
Nikita Vasilev <ns-vasilev@ydb.tech>
2025-05-23 12:02:58 +0300

    Disable write to index (#18665)

--- ydb/core/tx/scheme_cache/scheme_cache.h
+++ ydb/core/tx/scheme_cache/scheme_cache.h
@@ -147,6 +147,14 @@ private:
 
 }; // TDomainInfo
 
+enum class ETableKind {
+    KindUnknown = 0,
+    KindRegularTable = 1,
+    KindSyncIndexTable = 2,
+    KindAsyncIndexTable = 3,
+    KindVectorIndexTable = 4,
+};
+
 struct TSchemeCacheNavigate {
     enum class EStatus {
         Unknown = 0,
@@ -349,6 +357,7 @@ struct TSchemeCacheNavigate {
         TVector<NKikimrSchemeOp::TIndexDescription> Indexes;
         TVector<NKikimrSchemeOp::TCdcStreamDescription> CdcStreams;
         TVector<NKikimrSchemeOp::TSequenceDescription> Sequences;
+        ETableKind TableKind = ETableKind::KindUnknown;
 
         // other
         TIntrusiveConstPtr<TDomainDescription> DomainDescription;
@@ -416,14 +425,6 @@ struct TSchemeCacheRequest {
         OpScheme = 1 << 3,
     };
 
-    enum EKind {
-        KindUnknown = 0,
-        KindRegularTable = 1,
-        KindSyncIndexTable = 2,
-        KindAsyncIndexTable = 3,
-        KindVectorIndexTable = 4,
-    };
-
     struct TEntry {
         // in
         THolder<TKeyDesc> KeyDescription;
@@ -433,7 +434,7 @@ struct TSchemeCacheRequest {
 
         // out
         EStatus Status = EStatus::Unknown;
-        EKind Kind = EKind::KindUnknown;
+        ETableKind Kind = ETableKind::KindUnknown;
         TIntrusivePtr<TDomainInfo> DomainInfo;
         ui64 GeneralVersion = 0;
 

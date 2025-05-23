0730627c606bc0c9b305048ffa42d163c694d15b
Mikhail Filitov <filitovme@gmail.com>
2025-05-23 19:41:08 +0300

    added Split tests

--- ydb/tests/compatibility/test_datetime2.py
+++ ydb/tests/compatibility/test_datetime2.py
@@ -7,23 +7,24 @@ from datetime import datetime, timedelta
 
 
 class TestDatetime2Format(MixedClusterFixture):
-    rows = 100
-    table_name = 'dates_table'
-
     @pytest.fixture(autouse=True, scope="function")
     def setup(self):
-        self.setup_cluster()
+        yield from self.setup_cluster()
+
+    def test_simple(self):
 
         with ydb.QuerySessionPool(self.driver) as session_pool:
+            rows = 100
+            table_name = 'dates_table'
 
             # ---------------- CREATE TABLE ------------------
             query = f"""
-                CREATE TABLE {self.table_name} (
+                CREATE TABLE {table_name} (
                     id Uint32,
                     event_date DateTime,
                     PRIMARY KEY (id)
                 ) WITH (
-                    PARTITION_AT_KEYS = ({", ".join(str(i) for i in range(1, self.rows))})
+                    PARTITION_AT_KEYS = ({", ".join(str(i) for i in range(1, rows))})
                 );
             """
             session_pool.execute_with_retries(query)
@@ -32,24 +33,21 @@ class TestDatetime2Format(MixedClusterFixture):
             start_date = datetime(2023, 1, 1)
             values = []
 
-            for i in range(1, self.rows):
+            for i in range(1, rows):
                 date = (start_date + timedelta(days=i - 1)).strftime("%Y-%m-%dT%H:%M:%SZ")
                 values.append(f"({i}, CAST(\"{date}\" AS DateTime))")
 
             query = f"""
-                UPSERT INTO {self.table_name} (id, event_date) VALUES {",\n    ".join(values)};
+                UPSERT INTO {table_name} (id, event_date) VALUES {",\n    ".join(values)};
             """
 
             session_pool.execute_with_retries(query)
 
-    def test_simple(self):
-
-        with ydb.QuerySessionPool(self.driver) as session_pool:
             # ---------------- SELECT ------------------
             query = f"""
                 SELECT
                     DateTime::Format('%Y-%m-%d')(event_date) as date
-                FROM {self.table_name} order by date;
+                FROM {table_name} order by date;
             """
             result_sets = session_pool.execute_with_retries(query)
             assert result_sets[0].rows[0]['date'] == b'2023-01-01'

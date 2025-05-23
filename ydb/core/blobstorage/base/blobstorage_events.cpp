942c08d0b59382d042fe110657795457edc2eefc
Alexander Rutkovsky <alexvru@ydb.tech>
2025-05-23 12:45:18 +0000

    Refactor bridge mode info returned in TEvNodeWardenStorageConfig

--- ydb/core/blobstorage/base/blobstorage_events.cpp
+++ ydb/core/blobstorage/base/blobstorage_events.cpp
@@ -5,14 +5,13 @@ namespace NKikimr {
 
     TEvNodeWardenStorageConfig::TEvNodeWardenStorageConfig(const NKikimrBlobStorage::TStorageConfig& config,
             const NKikimrBlobStorage::TStorageConfig *proposedConfig, bool selfManagementEnabled,
-            bool isPrimary, bool isBeingPromoted)
+            TBridgeInfo::TPtr bridgeInfo)
         : Config(std::make_unique<NKikimrBlobStorage::TStorageConfig>(config))
         , ProposedConfig(proposedConfig
             ? std::make_unique<NKikimrBlobStorage::TStorageConfig>(*proposedConfig)
             : nullptr)
         , SelfManagementEnabled(selfManagementEnabled)
-        , IsPrimary(isPrimary)
-        , IsBeingPromoted(isBeingPromoted)
+        , BridgeInfo(std::move(bridgeInfo))
     {}
 
     TEvNodeWardenStorageConfig::~TEvNodeWardenStorageConfig()

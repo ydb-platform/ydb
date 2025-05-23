cc60f6797d72cc960e8448b581c4da5c5f503fad
Pisarenko Grigoriy <grigoriypisar@ydb.tech>
2025-05-23 20:40:36 +0500

    YQ-4161 support block writing in s3 (#16375)

--- ydb/library/yql/providers/s3/provider/yql_s3_settings.h
+++ ydb/library/yql/providers/s3/provider/yql_s3_settings.h
@@ -22,6 +22,7 @@ struct TS3Settings {
     NCommon::TConfSetting<bool, false> ArrowRowGroupReordering;    // Allow to push rows from file in any order, default false, but usually it is OK
     NCommon::TConfSetting<ui64, false> ParallelDownloadCount;      // Number of files to read in parallel, min == 1
     NCommon::TConfSetting<bool, false> UseBlocksSource;            // Deprecated and has to be removed after config cleanup
+    NCommon::TConfSetting<bool, false> UseBlocksSink;
     NCommon::TConfSetting<bool, false> AtomicUploadCommit;         // Commit each file independently, w/o transaction semantic over all files
     NCommon::TConfSetting<bool, false> UseConcurrentDirectoryLister;
     NCommon::TConfSetting<ui64, false> MaxDiscoveryFilesPerDirectory;

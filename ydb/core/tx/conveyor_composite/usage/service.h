093c8259c50175334dbbee77086a7bd27089c925
ivanmorozov333 <ivanmorozov@ydb.tech>
2025-05-25 10:42:32 +0300

    one conveyor for insert and compaction. draft. (#18776)

--- ydb/core/tx/conveyor_composite/usage/service.h
+++ ydb/core/tx/conveyor_composite/usage/service.h
@@ -1,41 +1,20 @@
 #pragma once
+#include "common.h"
 #include "config.h"
 
-#include <ydb/core/tx/conveyor/service/service.h>
-#include <ydb/core/tx/conveyor/usage/events.h>
+#include <ydb/core/tx/conveyor_composite/service/service.h>
+#include <ydb/core/tx/conveyor_composite/usage/events.h>
 
 #include <ydb/library/actors/core/actor.h>
 #include <ydb/library/actors/core/actorid.h>
 
-namespace NKikimr::NConveyor {
-
-class TAsyncTaskExecutor: public TActorBootstrapped<TAsyncTaskExecutor> {
-private:
-    const std::shared_ptr<ITask> Task;
-
-public:
-    TAsyncTaskExecutor(const std::shared_ptr<ITask>& task)
-        : Task(task) {
-    }
-
-    void Bootstrap() {
-        auto gAway = PassAwayGuard();
-        Task->Execute(nullptr, Task);
-    }
-};
-
-enum class ESpecialTaskProcesses {
-    Insert = 1,
-    Compaction = 2,
-    Normalizer = 3
-};
-
+namespace NKikimr::NConveyorComposite {
 template <class TConveyorPolicy>
 class TServiceOperatorImpl {
 private:
     using TSelf = TServiceOperatorImpl<TConveyorPolicy>;
     std::atomic<bool> IsEnabledFlag = false;
-    static void Register(const TConfig& serviceConfig) {
+    static void Register(const NConfig::TConfig& serviceConfig) {
         Singleton<TSelf>()->IsEnabledFlag = serviceConfig.IsEnabled();
     }
     static const TString& GetConveyorName() {
@@ -44,18 +23,11 @@ private:
     }
 
 public:
-    static void AsyncTaskToExecute(const std::shared_ptr<ITask>& task) {
-        auto& context = NActors::TActorContext::AsActorContext();
-        context.Register(new TAsyncTaskExecutor(task));
-    }
-    static bool SendTaskToExecute(const std::shared_ptr<ITask>& task, const ESpecialTaskProcesses processType) {
-        return SendTaskToExecute(task, (ui64)processType);
-    }
-    static bool SendTaskToExecute(const std::shared_ptr<ITask>& task, const ui64 processId = 0) {
+    static bool SendTaskToExecute(const std::shared_ptr<ITask>& task, const ESpecialTaskCategory category, const TString& scopeId, const ui64 processId) {
         if (TSelf::IsEnabled() && NActors::TlsActivationContext) {
             auto& context = NActors::TActorContext::AsActorContext();
             const NActors::TActorId& selfId = context.SelfID;
-            context.Send(MakeServiceId(selfId.NodeId()), new NConveyor::TEvExecution::TEvNewTask(task, processId));
+            context.Send(MakeServiceId(selfId.NodeId()), new NConveyorComposite::TEvExecution::TEvNewTask(task, category, scopeId, processId));
             return true;
         } else {
             task->Execute(nullptr, task);
@@ -68,18 +40,20 @@ public:
     static NActors::TActorId MakeServiceId(const ui32 nodeId) {
         return NActors::TActorId(nodeId, "SrvcConv" + GetConveyorName());
     }
-    static NActors::IActor* CreateService(const TConfig& config, TIntrusivePtr<::NMonitoring::TDynamicCounters> conveyorSignals) {
+    static NActors::IActor* CreateService(const NConfig::TConfig& config, TIntrusivePtr<::NMonitoring::TDynamicCounters> conveyorSignals) {
         Register(config);
-        return new TDistributor(config, GetConveyorName(), TConveyorPolicy::EnableProcesses, conveyorSignals);
+        return new TDistributor(config, GetConveyorName(), conveyorSignals);
     }
-    static TProcessGuard StartProcess(const ui64 externalProcessId, const TCPULimitsConfig& cpuLimits) {
+    static TProcessGuard StartProcess(
+        const ESpecialTaskCategory category, const TString& scopeId, const ui64 externalProcessId, const TCPULimitsConfig& cpuLimits) {
         if (TSelf::IsEnabled() && NActors::TlsActivationContext) {
             auto& context = NActors::TActorContext::AsActorContext();
             const NActors::TActorId& selfId = context.SelfID;
-            context.Send(MakeServiceId(selfId.NodeId()), new NConveyor::TEvExecution::TEvRegisterProcess(externalProcessId, cpuLimits));
-            return TProcessGuard(externalProcessId, MakeServiceId(selfId.NodeId()));
+            context.Send(MakeServiceId(selfId.NodeId()),
+                new NConveyorComposite::TEvExecution::TEvRegisterProcess(cpuLimits, category, scopeId, externalProcessId));
+            return TProcessGuard(category, scopeId, externalProcessId, MakeServiceId(selfId.NodeId()));
         } else {
-            return TProcessGuard(externalProcessId, {});
+            return TProcessGuard(category, scopeId, externalProcessId, {});
         }
     }
 };
@@ -106,4 +80,4 @@ using TScanServiceOperator = TServiceOperatorImpl<TScanConveyorPolicy>;
 using TCompServiceOperator = TServiceOperatorImpl<TCompConveyorPolicy>;
 using TInsertServiceOperator = TServiceOperatorImpl<TInsertConveyorPolicy>;
 
-}   // namespace NKikimr::NConveyor
+}   // namespace NKikimr::NConveyorComposite

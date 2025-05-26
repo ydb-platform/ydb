9248b6f670bc73caf8b600bdee595b25630ba328
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-05-26 23:54:36 +0200

    Move modification check to BuildKqlQuery

--- ydb/core/kqp/opt/kqp_opt_kql.cpp
+++ ydb/core/kqp/opt/kqp_opt_kql.cpp
@@ -1069,7 +1069,13 @@ TMaybe<TKqlQueryList> BuildKqlQuery(TKiDataQueryBlocks dataQueryBlocks, const TK
         TVector<TExprBase> kqlEffects;
         TNodeOnNodeOwnedMap effectsMap;
         for (const auto& effect : block.Effects()) {
+            TString cluster;
+            TString table;
+
             if (auto maybeWrite = effect.Maybe<TKiWriteTable>()) {
+                cluster = maybeWrite.Cast().DataSink().Cluster();
+                table = maybeWrite.Cast().Table().Value();
+
                 auto writeOp = HandleWriteTable(maybeWrite.Cast(), ctx, *kqpCtx, tablesData);
                 if (!writeOp) {
                     return {};
@@ -1079,6 +1085,9 @@ TMaybe<TKqlQueryList> BuildKqlQuery(TKiDataQueryBlocks dataQueryBlocks, const TK
             }
 
             if (auto maybeUpdate = effect.Maybe<TKiUpdateTable>()) {
+                cluster = maybeUpdate.Cast().DataSink().Cluster();
+                table = maybeUpdate.Cast().Table().Value();
+
                 auto updateOp = HandleUpdateTable(maybeUpdate.Cast(), ctx, *kqpCtx, tablesData, withSystemColumns);
                 if (!updateOp) {
                     return {};
@@ -1087,11 +1096,26 @@ TMaybe<TKqlQueryList> BuildKqlQuery(TKiDataQueryBlocks dataQueryBlocks, const TK
             }
 
             if (auto maybeDelete = effect.Maybe<TKiDeleteTable>()) {
+                cluster = maybeDelete.Cast().DataSink().Cluster();
+                table = maybeDelete.Cast().Table().Value();
+
                 auto deleteOp = HandleDeleteTable(maybeDelete.Cast(), ctx, *kqpCtx, tablesData, withSystemColumns);
                 if (!deleteOp) {
                     return {};
                 }
                 kqlEffects.push_back(TExprBase(deleteOp));
+            }
+
+            if (cluster && table) {
+                auto& tableData = GetTableData(tablesData, cluster, table);
+                if (tableData.Metadata->WritesToTableAreDisabled) {
+                    NYql::TIssues issues;
+                    issues.AddIssue(NYql::TIssue(ctx.GetPosition(effect.Pos()),
+                        TStringBuilder() << "Table `" << tableData.Metadata->Name << "` modification is disabled: "
+                            << tableData.Metadata->DisableWritesReason));
+                    ctx.IssueManager.AddIssues(ctx.GetPosition(effect.Pos()), issues);
+                    return {};
+                }
             }
 
             if (TExprNode::TPtr result = HandleExternalWrite(effect, ctx, typesCtx)) {
@@ -1123,7 +1147,7 @@ TMaybe<TKqlQueryList> BuildKqlQuery(TKiDataQueryBlocks dataQueryBlocks, const TK
         optSettings.VisitChanges = true;
         TExprNode::TPtr optResult;
         auto status = OptimizeExpr(query.Ptr(), optResult,
-            [&tablesData, &effectsMap](const TExprNode::TPtr& input, TExprContext& ctx) {
+            [&tablesData, &effectsMap](const TExprNode::TPtr& input, TExprContext& ctx) -> TExprNode::TPtr {
                 auto node = TExprBase(input);
 
                 if (auto maybeReturning = node.Maybe<TKiReturningList>()) {
@@ -1151,6 +1175,15 @@ TMaybe<TKqlQueryList> BuildKqlQuery(TKiDataQueryBlocks dataQueryBlocks, const TK
                     auto& tableData = GetTableData(tablesData, cluster, table);
                     const auto& tableMeta = BuildTableMeta(tableData, effect.Pos(), ctx);
                     YQL_ENSURE(effectsMap[effect.Raw()]);
+
+                    if (tableData.Metadata->WritesToTableAreDisabled) {
+                        NYql::TIssues issues;
+                        issues.AddIssue(NYql::TIssue(ctx.GetPosition(effect.Pos()),
+                            TStringBuilder() << "Table `" << tableData.Metadata->Name << "` modification is disabled: "
+                                << tableData.Metadata->DisableWritesReason));
+                        ctx.IssueManager.AddIssues(ctx.GetPosition(effect.Pos()), issues);
+                        return nullptr;
+                    }
 
                     return Build<TKqlReturningList>(ctx, returning.Pos())
                         .Update(effectsMap[effect.Raw()])

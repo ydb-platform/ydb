0c654becca6197ac6539450fe2a346b348b89b60
Mikhail Filitov <filitovme@gmail.com>
2025-05-26 14:03:05 +0300

    revert changes

--- ydb/tests/compatibility/test_datetime2.py
+++ ydb/tests/compatibility/test_datetime2.py
@@ -2,63 +2,52 @@
 import pytest
 from ydb.tests.library.compatibility.fixtures import MixedClusterFixture
 from ydb.tests.oss.ydb_sdk_import import ydb
+
 from datetime import datetime, timedelta
 
 
-class TestDatetimeUdfAll(MixedClusterFixture):
-    rows = 100
-    table_name = 'datetime_udf_table'
-
+class TestDatetime2Format(MixedClusterFixture):
     @pytest.fixture(autouse=True, scope="function")
     def setup(self):
         yield from self.setup_cluster()
 
-    def execute_query_with_retries(self, query):
+    def test_simple(self):
+
         with ydb.QuerySessionPool(self.driver) as session_pool:
-            return session_pool.execute_with_retries(query)
+            rows = 100
+            table_name = 'dates_table'
 
-    def prepare_tables(self):
-        # ---------------- CREATE TABLE ------------------
-        query = f"""
-            CREATE TABLE {self.table_name} (
-                id Uint32,
-                event_date DateTime,
-                PRIMARY KEY (id)
-            ) WITH (
-                PARTITION_AT_KEYS = ({", ".join(str(i) for i in range(1, self.rows))})
-            );
-        """
-        self.execute_query_with_retries(query)
+            # ---------------- CREATE TABLE ------------------
+            query = f"""
+                CREATE TABLE {table_name} (
+                    id Uint32,
+                    event_date DateTime,
+                    PRIMARY KEY (id)
+                ) WITH (
+                    PARTITION_AT_KEYS = ({", ".join(str(i) for i in range(1, rows))})
+                );
+            """
+            session_pool.execute_with_retries(query)
 
-        # ---------------- INSERT ------------------
-        start_date = datetime(2023, 1, 1)
-        values = []
+            # ---------------- INSERT ------------------
+            start_date = datetime(2023, 1, 1)
+            values = []
 
-        for i in range(1, self.rows):
-            date = (start_date + timedelta(days=i - 1)).strftime("%Y-%m-%dT%H:%M:%SZ")
-            values.append(f"({i}, CAST(\"{date}\" AS DateTime))")
+            for i in range(1, rows):
+                date = (start_date + timedelta(days=i - 1)).strftime("%Y-%m-%dT%H:%M:%SZ")
+                values.append(f"({i}, CAST(\"{date}\" AS DateTime))")
 
-        query = f"""
-            UPSERT INTO {self.table_name} (id, event_date) VALUES {",\n    ".join(values)};
-        """
-        self.execute_query_with_retries(query)
+            query = f"""
+                UPSERT INTO {table_name} (id, event_date) VALUES {",\n    ".join(values)};
+            """
 
-    def test_all_datetime_udf(self):
-        self.prepare_tables()
+            session_pool.execute_with_retries(query)
 
-
-        # ---------------- Split ------------------
-        query = f"""
-            SELECT
-                DateTime::Split(Date(event_date)).Year,
-                DateTime::Split(Datetime(event_date)).Month,
-                DateTime::Split(Timestamp(event_date)).Day,
-                DateTime::Split(DateTime::MakeTzDate(event_date, "Europe/Moscow")).Hour,
-                DateTime::Split(DateTime::MakeTzDatetime(event_date, "Europe/Moscow")).Minute,
-                DateTime::Split(DateTime::MakeTzTimestamp(event_date, "Europe/Moscow")).Second,
-                DateTime::MakeDatetime(DateTime::Split(event_date))
-            FROM {self.table_name};
-        """
-        res = self.execute_query_with_retries(query)
-        assert len(res) > 0
-
+            # ---------------- SELECT ------------------
+            query = f"""
+                SELECT
+                    DateTime::Format('%Y-%m-%d')(event_date) as date
+                FROM {table_name} order by date;
+            """
+            result_sets = session_pool.execute_with_retries(query)
+            assert result_sets[0].rows[0]['date'] == b'2023-01-01'

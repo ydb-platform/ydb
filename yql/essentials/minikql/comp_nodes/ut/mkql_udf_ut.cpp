c6a21d1f5cf3a3bd6da2a5b69831487c265d6e40
imunkin <imunkin@yandex-team.com>
2025-05-26 16:52:57 +0300

    YQL-19967: Properly handle signatures with run config (vice versa) commit_hash:5b56b1b4416cf5b7abcae727a61e9402e10af067

--- yql/essentials/minikql/comp_nodes/ut/mkql_udf_ut.cpp
+++ yql/essentials/minikql/comp_nodes/ut/mkql_udf_ut.cpp
@@ -5,6 +5,31 @@
 namespace NKikimr {
 namespace NMiniKQL {
 
+class TImpl : public NYql::NUdf::TBoxedValue {
+public:
+    explicit TImpl(NYql::NUdf::TSourcePosition pos,
+                   const std::string_view upvalue)
+        : Pos_(pos)
+        , Upvalue_(upvalue)
+    {}
+
+    NYql::NUdf::TUnboxedValue Run(const NYql::NUdf::IValueBuilder* valueBuilder,
+                                  const NYql::NUdf::TUnboxedValuePod* args)
+    const override try {
+        TStringStream concat;
+        concat << Upvalue_ << " " << args[0].AsStringRef();
+        return valueBuilder->NewString(NYql::NUdf::TStringRef(concat.Data(),
+                                                              concat.Size()));
+    } catch (const std::exception& e) {
+        UdfTerminate((TStringBuilder() << Pos_ << " " << e.what()).data());
+    }
+
+
+private:
+    const NYql::NUdf::TSourcePosition Pos_;
+    const TString Upvalue_;
+};
+
 // Class, implementing the closure with run config.
 class TRunConfig : public NYql::NUdf::TBoxedValue {
 public:
@@ -33,6 +58,15 @@ public:
         }
 
         return true;
+    }
+
+    NYql::NUdf::TUnboxedValue Run(const NYql::NUdf::IValueBuilder*,
+                                  const NYql::NUdf::TUnboxedValuePod* args)
+    const final try {
+        const std::string_view upvalue(args[0].AsStringRef());
+        return NYql::NUdf::TUnboxedValuePod(new TImpl(Pos_, upvalue));
+    } catch (const std::exception& e) {
+        UdfTerminate((TStringBuilder() << Pos_ << " " << e.what()).data());
     }
 
 private:
@@ -81,31 +115,6 @@ public:
     }
 
 private:
-    class TImpl : public NYql::NUdf::TBoxedValue {
-    public:
-        explicit TImpl(NYql::NUdf::TSourcePosition pos,
-                       const std::string_view upvalue)
-            : Pos_(pos)
-            , Upvalue_(upvalue)
-        {}
-
-        NYql::NUdf::TUnboxedValue Run(const NYql::NUdf::IValueBuilder* valueBuilder,
-                                      const NYql::NUdf::TUnboxedValuePod* args)
-        const override try {
-            TStringStream concat;
-            concat << Upvalue_ << " " << args[0].AsStringRef();
-            return valueBuilder->NewString(NYql::NUdf::TStringRef(concat.Data(),
-                                                                  concat.Size()));
-        } catch (const std::exception& e) {
-            UdfTerminate((TStringBuilder() << Pos_ << " " << e.what()).data());
-        }
-
-
-    private:
-        const NYql::NUdf::TSourcePosition Pos_;
-        const TString Upvalue_;
-    };
-
     const NYql::NUdf::TSourcePosition Pos_;
 };
 
@@ -152,6 +161,55 @@ Y_UNIT_TEST_SUITE(TMiniKQLUdfTest) {
         );
         TSetup<LLVM> runSetup(GetTestFactory(), std::move(runModules));
 
+        // Move the graph from the one setup to another as a
+        // serialized bytecode sequence.
+        const auto bytecode = SerializeRuntimeNode(pgmReturn, *compileSetup.Env);
+        const auto root = DeserializeRuntimeNode(bytecode, *runSetup.Env);
+
+        // Run the graph on the setup with TCurrying implementation.
+        const auto graph = runSetup.BuildGraph(root);
+        const auto iterator = graph->GetValue().GetListIterator();
+
+        NUdf::TUnboxedValue result;
+        UNIT_ASSERT(iterator.Next(result));
+        UNIT_ASSERT_STRINGS_EQUAL(TStringBuf(result.AsStringRef()), "Canary is alive");
+        UNIT_ASSERT(!iterator.Next(result));
+    }
+
+    Y_UNIT_TEST_LLVM(CurryingToRunconfig) {
+        // Create the test setup, using TCurrying implementation
+        // for TestModule.Test UDF.
+        TVector<TUdfModuleInfo> compileModules;
+        compileModules.emplace_back(
+            TUdfModuleInfo{"", "TestModule", new TCurryingUTModule()}
+        );
+        TSetup<LLVM> compileSetup(GetTestFactory(), std::move(compileModules));
+        TProgramBuilder& pb = *compileSetup.PgmBuilder;
+
+        // Build the graph on the setup with TRunConfig implementation.
+        const auto strType = pb.NewDataType(NUdf::TDataType<char*>::Id);
+        const auto upvalue = pb.NewDataLiteral<NUdf::EDataSlot::String>("Canary");
+        const auto optional = pb.NewOptional(pb.NewDataLiteral(true));
+        const auto value = pb.NewDataLiteral<NUdf::EDataSlot::String>("is alive");
+        const auto userType = pb.NewTupleType({
+            pb.NewTupleType({strType}),
+            pb.NewEmptyStructType(),
+            pb.NewEmptyTupleType()});
+        const auto udf = pb.Udf("TestModule.Test", pb.NewVoid(), userType);
+        const auto closure = pb.Apply(udf, {upvalue, optional});
+
+        const auto list = pb.NewList(strType, {value});
+        const auto pgmReturn = pb.Map(list, [&pb, closure](const TRuntimeNode item) {
+            return pb.Apply(closure, {item});
+        });
+
+        // Create the test setup, using TRunConfig implementation
+        // for TestModule.Test UDF.
+        TVector<TUdfModuleInfo> runModules;
+        runModules.emplace_back(
+            TUdfModuleInfo{"", "TestModule", new TRunConfigUTModule()}
+        );
+        TSetup<LLVM> runSetup(GetTestFactory(), std::move(runModules));
         // Move the graph from the one setup to another as a
         // serialized bytecode sequence.
         const auto bytecode = SerializeRuntimeNode(pgmReturn, *compileSetup.Env);

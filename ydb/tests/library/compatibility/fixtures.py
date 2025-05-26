521a632276395a05fe4dc7580b387ff30041bba8
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2025-05-26 15:39:21 +0000

    Fix rolling restart fixture (#18858)

--- ydb/tests/library/compatibility/fixtures.py
+++ ydb/tests/library/compatibility/fixtures.py
@@ -145,26 +145,70 @@ all_binary_combinations_ids_rolling = [
 
 
 class RollingUpgradeAndDowngradeFixture:
+    recreate_driver = True  # TODO: temporary workaround. We don't want to recreate driver, but not working now
+
     @pytest.fixture(autouse=True, params=all_binary_combinations_rolling, ids=all_binary_combinations_ids_rolling)
     def base_setup(self, request):
         self.all_binary_paths = request.param
 
+    def _wait_for_readiness(self):
+        if self.recreate_driver:
+            self.driver = ydb.Driver(
+                ydb.DriverConfig(
+                    database='/Root',
+                    endpoint=self.endpoints[0]
+                )
+            )
+            self.driver.wait()
+
+        query = """
+            CREATE TABLE `test_readiness` (
+            id Int64 NOT NULL,
+            PRIMARY KEY (id)
+        ) """
+        timeout = 120  # seconds
+        interval = 2  # seconds
+
+        start_time = time.time()
+        last_exception = None
+        while time.time() - start_time < timeout:
+            try:
+                with ydb.QuerySessionPool(self.driver) as session_pool:
+                    session_pool.execute_with_retries(query, retry_settings=ydb.RetrySettings(max_retries=1))
+                break
+            except Exception as e:
+                last_exception = e
+                time.sleep(interval)
+        else:
+            raise last_exception
+        query = """DROP TABLE `test_readiness`"""
+        with ydb.QuerySessionPool(self.driver) as session_pool:
+            session_pool.execute_with_retries(query)
+
     def setup_cluster(self, **kwargs):
+        extra_feature_flags = kwargs.pop("extra_feature_flags", {})
+        extra_feature_flags = copy.copy(extra_feature_flags)
+        extra_feature_flags["suppress_compatibility_check"] = True
+        # We want to drain tablets before stopping, to prevent "Failed to resolve tablet: 72075186224037909 after several retries"
+        # By default draining is not enabled to faster tests
+        extra_feature_flags["enable_drain_on_shutdown"] = True
         self.config = KikimrConfigGenerator(
             erasure=Erasure.MIRROR_3_DC,
-            binary_paths=[self.all_binary_paths[0]]
-            ,
+            binary_paths=[self.all_binary_paths[0]],
+            use_in_memory_pdisks=False,
             **kwargs,
         )
 
         self.cluster = KiKiMR(self.config)
         self.cluster.start()
-        self.endpoint = "grpc://%s:%s" % ('localhost', self.cluster.nodes[1].port)
+        self.endpoints = []
+        for i in range(1, len(self.cluster.nodes) + 1):
+            self.endpoints.append("grpc://%s:%s" % ('localhost', self.cluster.nodes[i].port))
 
         self.driver = ydb.Driver(
             ydb.DriverConfig(
-                database='/Root',
-                endpoint=self.endpoint
+                self.endpoints[0],
+                database='/Root'
             )
         )
         self.driver.wait()
@@ -173,18 +217,18 @@ class RollingUpgradeAndDowngradeFixture:
 
     def roll(self):
         # from old to new
+        yield
         for node_id, node in self.cluster.nodes.items():
             node.stop()
             node.binary_path = self.all_binary_paths[1]
             node.start()
+            self._wait_for_readiness()
             yield
 
         # from new to old
         for node_id, node in self.cluster.nodes.items():
             node.stop()
             node.binary_path = self.all_binary_paths[0]
-
             node.start()
+            self._wait_for_readiness()
             yield
-
-        yield

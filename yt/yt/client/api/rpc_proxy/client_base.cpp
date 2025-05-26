9d69a700239437e6fdabafd5a13ae450aa7eac74
bulatman <bulatman@yandex-team.com>
2025-05-26 09:40:00 +0300

    YT: Smart truncate query for tracing parameters commit_hash:a47dc680d0808bfe945e175863e8c1c0eeeac13e

--- yt/yt/client/api/rpc_proxy/client_base.cpp
+++ yt/yt/client/api/rpc_proxy/client_base.cpp
@@ -59,6 +59,7 @@ using NYT::FromProto;
 ////////////////////////////////////////////////////////////////////////////////
 
 constexpr i64 MaxTracingTagLength = 1'000;
+constexpr i64 MinQueryTailPartSize = 100;
 static const TString DisabledSelectQueryTracingTag = "Tag is disabled, look for enable_select_query_tracing_tag parameter";
 
 std::string SanitizeTracingTag(TStringBuf originalTag)
@@ -67,6 +68,16 @@ std::string SanitizeTracingTag(TStringBuf originalTag)
         return std::string(originalTag);
     }
     return Format("%v ... TRUNCATED", originalTag.substr(0, MaxTracingTagLength));
+}
+
+std::string SanitizeTracingQuery(TStringBuf originalQuery)
+{
+    if (originalQuery.size() <= MaxTracingTagLength) {
+        return std::string(originalQuery);
+    }
+    return Format("%v...<truncated>...%v",
+        originalQuery.substr(0, MaxTracingTagLength - MinQueryTailPartSize),
+        originalQuery.substr(originalQuery.size() - MinQueryTailPartSize));
 }
 
 void EnrichTracingForLookupRequest(NTracing::TTraceContext::TTagList& tagList, TStringBuf path, const auto& columns)
@@ -1041,7 +1052,7 @@ TFuture<TSelectRowsResult> TClientBase::SelectRows(
 
     if (NTracing::IsCurrentTraceContextRecorded()) {
         if (config->EnableSelectQueryTracingTag) {
-            req->TracingTags().emplace_back("yt.query", SanitizeTracingTag(query));
+            req->TracingTags().emplace_back("yt.query", SanitizeTracingQuery(query));
         } else {
             req->TracingTags().emplace_back("yt.query", DisabledSelectQueryTracingTag);
         }

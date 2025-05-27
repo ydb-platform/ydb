2d4616b58618cc7a125b0c846621fd377ef18155
Vitaly Isaev <vitalyisaev@ydb.tech>
2025-05-27 23:27:20 +0300

    Avoid copying code from KQP tests

--- ydb/library/yql/providers/generic/provider/ut/pushdown/re2_udf.cpp
+++
@@ -1,7 +0,0 @@
-// Copied from ydb/core/kqp/ut/common/re2_udf.cpp
-
-#include <yql/essentials/udfs/common/re2/re2_udf.cpp>
-
-NYql::NUdf::TUniquePtr<NYql::NUdf::IUdfModule> CreateRe2Module() {
-    return new TRe2Module<true>();
-} 

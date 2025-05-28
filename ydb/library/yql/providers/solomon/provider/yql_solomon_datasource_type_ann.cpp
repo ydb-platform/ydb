e435261595673cd61ad17858d04d3b84e1655c37
evanevanevanevannnn <evanevannnn@ydb.tech>
2025-05-28 17:12:40 +0000

    new config

--- ydb/library/yql/providers/solomon/provider/yql_solomon_datasource_type_ann.cpp
+++ ydb/library/yql/providers/solomon/provider/yql_solomon_datasource_type_ann.cpp
@@ -91,6 +91,11 @@ public:
         }
         bool hasSelectors = !selectors.Content().empty();
 
+        if (hasSelectors && !State_->Configuration->_EnableRuntimeListing.Get().GetOrElse(false)) {
+            ctx.AddError(TIssue(ctx.GetPosition(selectors.Pos()), "Runtime listing is disabled"));
+            return TStatus::Error;
+        }
+
         auto& program = *input->Child(TSoSourceSettings::idx_Program);
         if (!EnsureAtom(program, ctx)) {
             return TStatus::Error;

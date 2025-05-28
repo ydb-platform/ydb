f7f25add8c27ed37ecac4c756931a7c0d00c278f
azevaykin <azevaykin@ydb.tech>
2025-05-28 15:55:33 +0300

    Lock index impl tables before scanning them (#17229)

--- ydb/core/tx/schemeshard/schemeshard_info_types.h
+++ ydb/core/tx/schemeshard/schemeshard_info_types.h
@@ -3001,6 +3001,7 @@ struct TIndexBuildInfo: public TSimpleRefCount<TIndexBuildInfo> {
         Filling = 40,
         DropBuild = 45,
         CreateBuild = 46,
+        LockBuild = 47,
         Applying = 50,
         Unlocking = 60,
         Done = 200,

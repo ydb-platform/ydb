aa5b65fb974e9de8da3dfd1716808817e83c9e6b
robot-piglet <robot-piglet@yandex-team.com>
2025-05-30 12:34:55 +0300

    Intermediate changes commit_hash:a202126dac8bf1e6d89ecdeef0b1a94564b12916

--- contrib/deprecated/http-parser/.yandex_meta/__init__.py
+++ contrib/deprecated/http-parser/.yandex_meta/__init__.py
@@ -3,7 +3,7 @@ from devtools.yamaker.project import NixProject
 
 http_parser = NixProject(
     owners=["g:mds", "g:cpp-contrib"],
-    arcdir="contrib/restricted/http-parser",
+    arcdir="contrib/deprecated/http-parser",
     nixattr="http-parser",
     # By default maximium header size allowed is 80Kb. To remove the effective limit
     # on the size of the header, we define the macro to a very large number (0x7fffffff).

5acefe5fa51541ab4af06517e9995e7d83ff1ae3
Pisarenko Grigoriy <grigoriypisar@ydb.tech>
2025-06-02 13:36:31 +0300

    Fixed print operation

--- ydb/public/lib/ydb_cli/common/print_operation.cpp
+++ ydb/public/lib/ydb_cli/common/print_operation.cpp
@@ -415,6 +415,9 @@ void Out<NYdb::NQuery::EExecStatus>(IOutputStream& o, NYdb::NQuery::EExecStatus 
         case EExecStatus::Starting:
             o << TStringBuf("starting");
             return;
+        case EExecStatus::Running:
+            o << TStringBuf("running");
+            return;
         case EExecStatus::Aborted:
             o << TStringBuf("aborted");
             return;

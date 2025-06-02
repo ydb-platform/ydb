1b98fb16dd3ebd19ae51111d7842a652146187f1
Aleksandr Dmitriev <monster@ydb.tech>
2025-06-02 22:03:32 +0300

    rename CheckIntegrity placement statuses (#19145)

--- ydb/core/blobstorage/dsproxy/dsproxy_check_integrity_get.cpp
+++ ydb/core/blobstorage/dsproxy/dsproxy_check_integrity_get.cpp
@@ -159,13 +159,13 @@ class TBlobStorageGroupCheckIntegrityRequest : public TBlobStorageGroupRequestAc
                     PartLayoutWithNotYet, faultyDisks);
 
                 if (stateNotYet == TBlobStorageGroupInfo::EBS_FULL) {
-                    PendingResult->PlacementStatus = TEvCheckIntegrityResult::PS_NOT_YET;
+                    PendingResult->PlacementStatus = TEvCheckIntegrityResult::PS_REPLICATION_IN_PROGRESS;
                 } else if (state == TBlobStorageGroupInfo::EBS_RECOVERABLE_FRAGMENTARY) {
-                    PendingResult->PlacementStatus = TEvCheckIntegrityResult::PS_RECOVERABLE;
+                    PendingResult->PlacementStatus = TEvCheckIntegrityResult::PS_BLOB_IS_RECOVERABLE;
                 } else if (HasErrorDisks) {
                     PendingResult->PlacementStatus = TEvCheckIntegrityResult::PS_UNKNOWN;
                 } else {
-                    PendingResult->PlacementStatus = TEvCheckIntegrityResult::PS_ERROR;
+                    PendingResult->PlacementStatus = TEvCheckIntegrityResult::PS_BLOB_IS_LOST;
                 }
                 break;
             }

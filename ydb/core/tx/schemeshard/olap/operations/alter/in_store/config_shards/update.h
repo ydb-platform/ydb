333add7283e43319db49c634a6a9e29d1c3c9af6
Evgeny Zverev <zverevgeny@ydb.tech>
2025-06-02 09:02:19 +0000

    temp

--- ydb/core/tx/schemeshard/olap/operations/alter/in_store/config_shards/update.h
+++ ydb/core/tx/schemeshard/olap/operations/alter/in_store/config_shards/update.h
@@ -44,7 +44,7 @@ private:
             auto container = Sharding->GetTabletShardingInfoOptional(tabletId);
             if (!!container) {
                 auto& shardingInfo = *result.MutableGranuleShardingInfo();
-                shardingInfo.SetPathId(TargetInStoreTable->GetPathId().LocalPathId);
+                NColumnShard::TSchemeShardLocalPathId::FromRawValue(TargetInStoreTable->GetPathId().LocalPathId).ToProto(shardingInfo);
                 shardingInfo.SetVersionId(Sharding->GetShardInfoVerified(tabletId).GetShardingVersion());
                 *shardingInfo.MutableContainer() = container.SerializeToProto();
                 AFL_VERIFY(ModifiedShardIds.contains(tabletId));

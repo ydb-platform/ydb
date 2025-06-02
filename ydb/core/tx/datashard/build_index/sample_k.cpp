9d29b12463811885a070ec2bcdcb3a9e47e191ad
kungasc <kungasc@ydb.tech>
2025-06-02 13:25:46 +0300

    fix no namespace

--- ydb/core/tx/datashard/build_index/sample_k.cpp
+++ ydb/core/tx/datashard/build_index/sample_k.cpp
@@ -169,9 +169,9 @@ public:
         NYql::IssuesToMessage(Issues, record.MutableIssues());
 
         if (Response->Record.GetStatus() == NKikimrIndexBuilder::DONE) {
-            LOG_N("Done " << Debug() << " " << NTableIndex::ToShortDebugString(Response->Record));
+            LOG_N("Done " << Debug() << " " << ToShortDebugString(Response->Record));
         } else {
-            LOG_E("Failed " << Debug() << " " << NTableIndex::ToShortDebugString(Response->Record));
+            LOG_E("Failed " << Debug() << " " << ToShortDebugString(Response->Record));
         }
         Send(ResponseActorId, Response.Release());
 
@@ -279,7 +279,7 @@ void TDataShard::HandleSafe(TEvDataShard::TEvSampleKRequest::TPtr& ev, const TAc
             if (response->Record.GetStatus() == NKikimrIndexBuilder::EBuildStatus::BAD_REQUEST) {
                 LOG_E("Rejecting TSampleKScan bad request TabletId: " << TabletID()
                     << " " << request.ShortDebugString()
-                    << " with response " << NTableIndex::ToShortDebugString(response->Record));
+                    << " with response " << ToShortDebugString(response->Record));
                 ctx.Send(ev->Sender, std::move(response));
                 return true;
             } else {

1ac7be677531779f49b42c3d45550e425e90f4fd
carrizo <ya.elhuron@yandex.ru>
2025-06-04 21:42:52 +0900

    Fix SyntaxError: bytes can only contain ASCII literal characters

--- contrib/python/parso/py2/tests/normalizer_issue_files/allowed_syntax_python2.py
+++ contrib/python/parso/py2/tests/normalizer_issue_files/allowed_syntax_python2.py
@@ -1,8 +1,8 @@
 #'s' b''
 #u's' b'ä'
 
-result = u's'.encode('utf-8') + b'ä'
+result = u's'.encode('utf-8') + b'\xc3\xa4'
 print(result)  # out: 's\xc3\xa4'
 
-result = u's' + b'ä'.decode('utf-8')
+result = u's' + b'\xc3\xa4'.decode('utf-8')
 print(result)  # out: u'sä'

8bbfc1965ae5de65c96c36238b45ec33dd84f0a0
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2025-06-05 00:38:48 +0300

    New CHANGEFEED's option: SCHEMA_CHANGES (#19303)

--- ydb/core/sys_view/ut_kqp.cpp
+++ ydb/core/sys_view/ut_kqp.cpp
@@ -1739,6 +1739,27 @@ ALTER TABLE `test_show_create`
 
         checker.CheckShowCreateTable(R"(
             CREATE TABLE test_show_create (
+                Key String,
+                Value String,
+                PRIMARY KEY (Key)
+            );
+            ALTER TABLE test_show_create
+                ADD CHANGEFEED `feed` WITH (MODE = 'KEYS_ONLY', FORMAT = 'JSON', SCHEMA_CHANGES = TRUE);
+        )", "test_show_create",
+R"(CREATE TABLE `test_show_create` (
+    `Key` String,
+    `Value` String,
+    PRIMARY KEY (`Key`)
+);
+
+ALTER TABLE `test_show_create`
+    ADD CHANGEFEED `feed` WITH (MODE = 'KEYS_ONLY', FORMAT = 'JSON', SCHEMA_CHANGES = TRUE, RETENTION_PERIOD = INTERVAL('P1D'))
+;
+)"
+        );
+
+        checker.CheckShowCreateTable(R"(
+            CREATE TABLE test_show_create (
                 Key Uint64,
                 Value String,
                 PRIMARY KEY (Key)

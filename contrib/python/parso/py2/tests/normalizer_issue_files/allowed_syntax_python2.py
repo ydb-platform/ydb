f9d5502250d85501723a3168ac0734daa0ed229f
carrizo <ya.elhuron@yandex.ru>
2025-06-04 21:29:17 +0900

    Fix SyntaxError: cannot mix bytes and nonbytes literals

--- contrib/python/parso/py2/tests/normalizer_issue_files/allowed_syntax_python2.py
+++ contrib/python/parso/py2/tests/normalizer_issue_files/allowed_syntax_python2.py
@@ -1,2 +1,8 @@
-'s' b''
-u's' b'ä'
+#'s' b''
+#u's' b'ä'
+
+result = u's'.encode('utf-8') + b'ä'
+print(result)  # out: 's\xc3\xa4'
+
+result = u's' + b'ä'.decode('utf-8')
+print(result)  # out: u'sä'

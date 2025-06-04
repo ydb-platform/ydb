dcc4a21651e2d5413e7ad221932ae33feda38599
carrizo <ya.elhuron@yandex.ru>
2025-06-04 17:22:39 +0900

    Fix SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?

--- contrib/libs/cctz/tzdata/update_tzdata.py
+++ contrib/libs/cctz/tzdata/update_tzdata.py
@@ -28,7 +28,7 @@ def create_cmakelists(zoneinfo_dir):
             tz_to_hash[tz_name] = tz_hash
             hash_to_content[tz_hash] = tz_content
             total_size += len(tz_content)
-    print 'Total data size in bytes:', total_size
+    print('Total data size in bytes:', total_size)
 
     generated_dir = 'generated'
     if not os.path.isdir(generated_dir):
@@ -51,7 +51,7 @@ def create_cmakelists(zoneinfo_dir):
         subprocess.check_call(['arc', cmd, fn])
 
     with open('ya.make.resources', 'w') as f:
-        print >>f, yamake_template.format(resources)
+        print(yamake_template.format(resources), file=f)
 
 def get_latest_iana_version():
     index_html = urllib2.urlopen('http://www.iana.org/time-zones').read()
@@ -73,20 +73,20 @@ def prepare_tzdata(version):
         for file_type in ('data', 'code'):
             file_name = 'tz{}{}.tar.gz'.format(file_type, version)
             full_url = 'http://www.iana.org/time-zones/repository/releases/{}'.format(file_name)
-            print 'Downloading {}'.format(full_url)
+            print('Downloading {}'.format(full_url))
 
             local_file_name = os.path.join(temp_dir, file_name)
             with open(local_file_name, 'w') as f:
                 f.write(urllib2.urlopen(full_url).read())
 
-            print 'Extracting {}'.format(local_file_name)
+            print('Extracting {}'.format(local_file_name))
             with tarfile.open(local_file_name) as f:
                 f.extractall(path=temp_dir)
 
-        print 'Converting tzdata to binary format'
+        print('Converting tzdata to binary format')
         subprocess.check_call(['make', '-s', '-C', temp_dir, 'TOPDIR={}'.format(temp_dir), 'install'])
 
-        print 'Preparing ya.make.resources'
+        print('Preparing ya.make.resources')
         zoneinfo_dir = os.path.join(temp_dir, 'usr', 'share', 'zoneinfo')
         create_cmakelists(zoneinfo_dir)
     finally:
@@ -94,21 +94,21 @@ def prepare_tzdata(version):
 
 def main():
     current_version, latest_version = get_current_version(), get_latest_iana_version()
-    print 'The current version of tzdata is {}'.format(current_version)
-    print 'The latest version of tzdata on the IANA site is {}'.format(latest_version)
+    print('The current version of tzdata is {}'.format(current_version))
+    print('The latest version of tzdata on the IANA site is {}'.format(latest_version))
     if current_version == latest_version:
-        print 'You already have the latest version'
+        print('You already have the latest version')
         return
-    print 'Updating from {} to {}'.format(current_version, latest_version)
+    print('Updating from {} to {}'.format(current_version, latest_version))
     prepare_tzdata(latest_version)
 
     with open('VERSION', 'w') as f:
         f.write(latest_version)
 
-    print 'All good! Now make sure the tests pass, and run this:'
-    print 'arc add VERSION update_tzdata.py ya.make.resources'
-    print 'arc co -b tzdata.{}'.format(latest_version)
-    print 'arc ci . -m "Updated tzdata from {} to {}"'.format(current_version, latest_version)
+    print('All good! Now make sure the tests pass, and run this:')
+    print('arc add VERSION update_tzdata.py ya.make.resources')
+    print('arc co -b tzdata.{}'.format(latest_version))
+    print('arc ci . -m "Updated tzdata from {} to {}"'.format(current_version, latest_version))
 
 if __name__ == '__main__':
     main()

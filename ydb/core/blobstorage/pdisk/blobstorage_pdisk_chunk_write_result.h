5ef628469de6e6e71549e444be22393f82ac4f36
Aleksei Pleshakov <qrort@ydb.tech>
2025-06-06 16:53:44 +0200

    WIP: multiple encryption threads in PDisk

---
+++ ydb/core/blobstorage/pdisk/blobstorage_pdisk_chunk_write_result.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "blobstorage_pdisk_writer.h"
+
+namespace NKikimr {
+namespace NPDisk {
+
+class TChunkWriteResult {
+public:
+    THolder <TChunkWriter> ChunkWriter;
+    bool LastPart;
+    TChunkWriteResult(THolder<TChunkWriter>&& chunkWriter, bool lastPart) : ChunkWriter(std::move(chunkWriter)), LastPart(lastPart) {}
+};
+
+} // NPDisk
+} // NKikimr

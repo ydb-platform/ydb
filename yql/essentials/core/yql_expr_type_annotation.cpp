c2585c2d46e114ac3cad36fccd1a4a42f1995049
Nikolay Shestakov <tesseract@ydb.tech>
2025-06-06 06:43:46 +0000

    revert

--- yql/essentials/core/yql_expr_type_annotation.cpp
+++ yql/essentials/core/yql_expr_type_annotation.cpp
@@ -4328,7 +4328,7 @@ IGraphTransformer::TStatus TrySilentConvertTo(TExprNode::TPtr& node, const TType
 
 IGraphTransformer::TStatus TrySilentConvertTo(TExprNode::TPtr& node, const TTypeAnnotationNode& sourceType,
     const TTypeAnnotationNode& expectedType, TExprContext& ctx, TConvertFlags flags) {
-    return TryConvertToImpl(ctx, node, sourceType, expectedType, flags, true);
+    return TryConvertToImpl(ctx, node, sourceType, expectedType, flags);
 }
 
 bool IsDataTypeNumeric(EDataSlot dataSlot) {

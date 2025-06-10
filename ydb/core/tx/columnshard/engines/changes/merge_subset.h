16991e9be3246d7f7892e23c2bc77392068dbed8
ivanmorozov333 <imorozov333@ya.ru>
2025-06-10 17:31:17 +0300

    fix deletion usage on compaction

--- ydb/core/tx/columnshard/engines/changes/merge_subset.h
+++ ydb/core/tx/columnshard/engines/changes/merge_subset.h
@@ -24,13 +24,25 @@ public:
 class ISubsetToMerge {
 private:
     virtual std::vector<TPortionToMerge> DoBuildPortionsToMerge(const TConstructionContext& context, const std::set<ui32>& seqDataColumnIds,
-        const std::shared_ptr<TFilteredSnapshotSchema>& resultFiltered, const THashSet<ui64>& usedPortionIds) const = 0;
+        const std::shared_ptr<TFilteredSnapshotSchema>& resultFiltered, const THashSet<ui64>& usedPortionIds,
+        const bool useDeletionFilter) const = 0;
+
+protected:
+    const std::shared_ptr<TGranuleMeta> GranuleMeta;
+    std::shared_ptr<NArrow::TColumnFilter> BuildPortionFilter(const std::optional<NKikimr::NOlap::TGranuleShardingInfo>& shardingActual,
+        const std::shared_ptr<NArrow::TGeneralContainer>& batch, const TPortionInfo& pInfo, const THashSet<ui64>& portionsInUsage,
+        const bool useDeletionFilter) const;
 
 public:
+    ISubsetToMerge(const std::shared_ptr<TGranuleMeta>& granule)
+        : GranuleMeta(granule) {
+        AFL_VERIFY(GranuleMeta);
+    }
     virtual ~ISubsetToMerge() = default;
     std::vector<TPortionToMerge> BuildPortionsToMerge(const TConstructionContext& context, const std::set<ui32>& seqDataColumnIds,
-        const std::shared_ptr<TFilteredSnapshotSchema>& resultFiltered, const THashSet<ui64>& usedPortionIds) const {
-        return DoBuildPortionsToMerge(context, seqDataColumnIds, resultFiltered, usedPortionIds);
+        const std::shared_ptr<TFilteredSnapshotSchema>& resultFiltered, const THashSet<ui64>& usedPortionIds,
+        const bool useDeletionFilter) const {
+        return DoBuildPortionsToMerge(context, seqDataColumnIds, resultFiltered, usedPortionIds, useDeletionFilter);
     }
     virtual ui64 GetColumnMaxChunkMemory() const = 0;
 };
@@ -38,18 +50,15 @@ public:
 class TReadPortionToMerge: public ISubsetToMerge {
 private:
     TReadPortionInfoWithBlobs ReadPortion;
-    const std::shared_ptr<TGranuleMeta> GranuleMeta;
-
-    std::shared_ptr<NArrow::TColumnFilter> BuildPortionFilter(const std::optional<NKikimr::NOlap::TGranuleShardingInfo>& shardingActual,
-        const std::shared_ptr<NArrow::TGeneralContainer>& batch, const TPortionInfo& pInfo, const THashSet<ui64>& portionsInUsage) const;
 
     virtual std::vector<TPortionToMerge> DoBuildPortionsToMerge(const TConstructionContext& context, const std::set<ui32>& seqDataColumnIds,
-        const std::shared_ptr<TFilteredSnapshotSchema>& resultFiltered, const THashSet<ui64>& usedPortionIds) const override;
+        const std::shared_ptr<TFilteredSnapshotSchema>& resultFiltered, const THashSet<ui64>& usedPortionIds,
+        const bool useDeletionFilter) const override;
 
 public:
     TReadPortionToMerge(TReadPortionInfoWithBlobs&& rPortion, const std::shared_ptr<TGranuleMeta>& granuleMeta)
-        : ReadPortion(std::move(rPortion))
-        , GranuleMeta(granuleMeta) {
+        : TBase(granuleMeta)
+        , ReadPortion(std::move(rPortion)) {
     }
 
     virtual ui64 GetColumnMaxChunkMemory() const override {
@@ -66,12 +75,13 @@ private:
     std::vector<TWritePortionInfoWithBlobsResult> WritePortions;
 
     virtual std::vector<TPortionToMerge> DoBuildPortionsToMerge(const TConstructionContext& context, const std::set<ui32>& seqDataColumnIds,
-        const std::shared_ptr<TFilteredSnapshotSchema>& resultFiltered, const THashSet<ui64>& /*usedPortionIds*/) const override;
+        const std::shared_ptr<TFilteredSnapshotSchema>& resultFiltered, const THashSet<ui64>& usedPortionIds,
+        const bool useDeletionFilter) const override;
 
     virtual ui64 GetColumnMaxChunkMemory() const override;
 
 public:
-    TWritePortionsToMerge(std::vector<TWritePortionInfoWithBlobsResult>&& portions);
+    TWritePortionsToMerge(std::vector<TWritePortionInfoWithBlobsResult>&& portions, const std::shared_ptr<TGranuleMeta>& granuleMeta);
 };
 
 }   // namespace NKikimr::NOlap::NCompaction

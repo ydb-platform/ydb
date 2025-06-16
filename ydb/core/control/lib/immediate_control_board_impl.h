4ed501068965f9bf26dbc01fbdfa15fc1dfeaf12
Kamil Khamitov <kkhamitov@yandex-team.ru>
2025-06-16 18:19:37 +0300

    (~) rewrite core/control

--- ydb/core/control/lib/immediate_control_board_impl.h
+++ ydb/core/control/lib/immediate_control_board_impl.h
@@ -1,10 +1,9 @@
 #pragma once
 
-#include "defs.h"
 #include "immediate_control_board_wrapper.h"
 #include "immediate_control_board_html_renderer.h"
 
-#include <ydb/core/control/lib/defs.h_serialized.h>
+#include <ydb/core/control/lib/generated/control_board_proto.h>
 
 #include <library/cpp/threading/hot_swap/hot_swap.h>
 
@@ -13,29 +12,19 @@
 
 namespace NKikimr {
 
-class TControlBoard: public TThrRefBase {
+class TImmediateControlActor;
+
+class TControlBoard: public TControlBoardBase {
+    friend class TImmediateControlActor;
 private:
-    using TPtr = THotSwap<TControl>;
-    std::array<TPtr,  GetEnumItemsCount<EStaticControlType>()> StaticControls;
-private:
-    size_t GetIndex(EStaticControlType type) const;
+    TIntrusivePtr<TControl> GetControlByName(const TString& name) const;
 public:
-    bool RegisterLocalControl(TControlWrapper control, EStaticControlType type);
-
-    bool RegisterSharedControl(TControlWrapper& control, EStaticControlType type);
-
-    void RestoreDefault(EStaticControlType tpe);
-
     void RestoreDefaults();
-
-    bool SetValue(EStaticControlType name, TAtomic value, TAtomic &outPrevValue);
-
-    // Only for tests
-    void GetValue(EStaticControlType name, TAtomic &outValue, bool &outIsControlExists) const;
-
     void RenderAsHtml(TControlBoardTableHtmlRenderer& renderer) const;
 
-    TMaybe<EStaticControlType> GetStaticControlId(const TStringBuf name) const;
+    static void RegisterSharedControl(TControlWrapper& control, THotSwap<TControl>& icbControl);
+    static void RegisterLocalControl(TControlWrapper control, THotSwap<TControl>& icbControl);
+    static void SetValue(TAtomicBase value, THotSwap<TControl>& icbControl);
 };
 
 }

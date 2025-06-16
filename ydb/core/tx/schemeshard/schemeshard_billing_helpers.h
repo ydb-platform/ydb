a81433319a3201a7e3d5650d963be61d57ecce8a
kungurtsev <kungasc@ydb.tech>
2025-06-16 19:22:24 +0200

    Fix request unit calculator references and description (#19692)

--- ydb/core/tx/schemeshard/schemeshard_billing_helpers.h
+++ ydb/core/tx/schemeshard/schemeshard_billing_helpers.h
@@ -52,7 +52,6 @@ private:
 };
 
 struct TRUCalculator {
-    // https://a.yandex-team.ru/arc/trunk/arcadia/kikimr/docs/ru/pricing/serverless.md
     static ui64 ReadTable(ui64 bytes);
     static ui64 BulkUpsert(ui64 bytes, ui64 rows);
     static ui64 Calculate(const TBillingStats& stats);

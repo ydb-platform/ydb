74dab8ef3e98e9b1722c3ab99610388cdc46b08e
Nikolay Perfilov <pnv1@yandex-team.ru>
2025-06-17 14:53:09 +0300

    Reuse init phase from ydb workload query

--- ydb/tests/olap/load/lib/import_csv.py
+++ ydb/tests/olap/load/lib/import_csv.py
@@ -18,32 +18,20 @@ class ImportFileCsvBase(UploadSuiteBase):
         cls.data_folder = cls.get_external_path()
         super().setup_class()
 
-    def drop_table_if_exists(self):
-        tables_path = YdbCluster.tables_path
-        for table_name in self.table_names:
-            full_table_path = f'{tables_path}/{table_name}'
-            yatest.common.execute(YdbCliHelper.get_cli_command() + ['sql', '-s', f'DROP TABLE IF EXISTS `{full_table_path}`'])
-
-    def create_table(self):
-        init_dir = os.path.join(self.data_folder, 'init')
-        for fname in sorted(os.listdir(init_dir)):
-            if fname.endswith('.sql'):
-                sql_path = os.path.join(init_dir, fname)
-                yatest.common.execute(YdbCliHelper.get_cli_command() + ['sql', '-f', sql_path])
-
     def init(self):
-        import_path = os.path.join(self.data_folder, "import")
-        self.table_names = [name for name in os.listdir(import_path) if os.path.isdir(os.path.join(import_path, name))]
-        if not self.table_names:
-            raise RuntimeError(f"Found no directories in {import_path}")
-        self.table_name = self.table_names[0] # importing just one table
+        # Create tables
+        yatest.common.execute(YdbCliHelper.get_cli_command() + ['workload', 'query', '-p', YdbCluster.tables_path, 'init', '--suite-path', self.data_folder, '--clear'])
+
+        import_dir = os.path.join(self.data_folder, "import")
+        table_names = [name for name in os.listdir(import_dir) if os.path.isdir(os.path.join(import_dir, name))]
+        if not table_names:
+            raise RuntimeError(f"Found no directories in {import_dir}")
+        self.table_name = table_names[0] # importing just one table
         self.table_path = f'{YdbCluster.tables_path}/{self.table_name}'
-        self.drop_tables_if_exists()
-        self.create_table()
+        self.import_path = os.path.join(self.data_folder, 'import', self.table_name, '*') # All files in the table directory
 
     def import_data(self):
-        import_path = os.path.join(self.data_folder, 'import', self.table_name, '*') # All files in the table directory
-        yatest.common.execute(YdbCliHelper.get_cli_command() + ['import', 'file', 'csv', '-p', self.table_path, import_path, '--header'])
+        yatest.common.execute(YdbCliHelper.get_cli_command() + ['import', 'file', 'csv', '-p', self.table_path, self.import_path, '--header'])
 
     def save_result_additional_info(self, result: YdbCliHelper.WorkloadRunResult):
         import_dir = os.path.join(self.data_folder, 'import', self.table_name)

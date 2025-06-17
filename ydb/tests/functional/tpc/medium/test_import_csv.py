ce6127243e23505ed4435dae8c809c1324a04101
Nikolay Perfilov <pnv1@yandex-team.ru>
2025-06-17 17:51:29 +0300

    Fixes after review

--- ydb/tests/functional/tpc/medium/test_import_csv.py
+++ ydb/tests/functional/tpc/medium/test_import_csv.py
@@ -4,3 +4,8 @@ from ydb.tests.functional.tpc.lib.conftest import FunctionalTestBase
 
 class TestExternalImportCsv(ImportFileCsvBase, FunctionalTestBase):
     external_folder: str = 'e1'
+
+    @classmethod
+    def setup_class(cls) -> None:
+        cls.setup_cluster()
+        super().setup_class()
--- ydb/tests/olap/load/lib/import_csv.py
+++ ydb/tests/olap/load/lib/import_csv.py
@@ -8,33 +8,30 @@ from .upload import UploadSuiteBase
 
 class ImportFileCsvBase(UploadSuiteBase):
     query_name: str = 'ImportFileCsv' # Override UploadSuiteBase.query_name
-    data_folder: str = ''
     table_path: str = ''
     iterations: int = 1
-
-    @classmethod
-    def setup_class(cls) -> None:
-        cls.setup_cluster()
-        cls.data_folder = cls.get_external_path()
-        super().setup_class()
 
     def init(self):
         # Create tables
         yatest.common.execute(YdbCliHelper.get_cli_command() + ['workload', 'query', '-p', YdbCluster.tables_path, 'init', '--suite-path', self.data_folder, '--clear'])
 
-        import_dir = os.path.join(self.data_folder, "import")
+        import_dir = os.path.join(self.get_external_path(), "import")
         table_names = [name for name in os.listdir(import_dir) if os.path.isdir(os.path.join(import_dir, name))]
         if not table_names:
             raise RuntimeError(f"Found no directories in {import_dir}")
         self.table_name = table_names[0] # importing just one table
-        self.table_path = f'{YdbCluster.tables_path}/{self.table_name}'
-        self.import_path = os.path.join(self.data_folder, 'import', self.table_name) + '/*' # All files in the table directory
 
     def import_data(self):
-        yatest.common.execute(YdbCliHelper.get_cli_command() + ['import', 'file', 'csv', '-p', self.table_path, self.import_path, '--header'])
+        table_path = f'{YdbCluster.tables_path}/{self.table_name}'
+        import_dir = os.path.join(self.get_external_path(), 'import', self.table_name)
+        csv_files = [f for f in os.listdir(import_dir) if os.path.isfile(os.path.join(import_dir, f)) and f.endswith('.csv')]
+        if not csv_files:
+            raise RuntimeError(f'No .csv files found in {import_dir}')
+        import_path = os.path.join(import_dir, csv_files[0])
+        yatest.common.execute(YdbCliHelper.get_cli_command() + ['import', 'file', 'csv', '-p', table_path, import_path, '--header'])
 
     def save_result_additional_info(self, result: YdbCliHelper.WorkloadRunResult):
-        import_dir = os.path.join(self.data_folder, 'import', self.table_name)
+        import_dir = os.path.join(self.get_external_path(), 'import', self.table_name)
         file_size = sum(
             os.path.getsize(os.path.join(import_dir, f))
             for f in os.listdir(import_dir)

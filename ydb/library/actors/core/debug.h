8f03b6d0e817509022d025427d72895e110f4cc0
Kriukov Aleksandr <kruall@ydb.tech>
2025-06-17 10:19:46 +0300

    turn off debug output

--- ydb/library/actors/core/debug.h
+++ ydb/library/actors/core/debug.h
@@ -18,7 +18,7 @@ enum class EDebugLevel {
     Test,
 };
 
-constexpr EDebugLevel DebugLevel = EDebugLevel::Harmonizer;
+constexpr EDebugLevel DebugLevel = EDebugLevel::None;
 
 template <typename ...TArgs>
 inline TString ActorLibDebugMakeMsg(const std::string &file, int line, TArgs&& ...args) {

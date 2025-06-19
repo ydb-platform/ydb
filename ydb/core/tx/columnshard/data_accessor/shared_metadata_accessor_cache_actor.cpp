9b7d3d4af1fdd3d369c112f59f4e329bcb9b7550
Iurii Kravchenko <xjie6@ydb.tech>
2025-06-19 16:13:56 +0300

    fix

--- ydb/core/tx/columnshard/data_accessor/shared_metadata_accessor_cache_actor.cpp
+++ ydb/core/tx/columnshard/data_accessor/shared_metadata_accessor_cache_actor.cpp
@@ -5,10 +5,12 @@
 namespace NKikimr::NOlap::NDataAccessorControl {
 
 NActors::IActor* TSharedMetadataAccessorCacheActor::CreateActor() {
+    AFL_TRACE(NKikimrServices::TX_COLUMNSHARD)("shared_metadata_accessor_cache", "CreateActor");
     return new TSharedMetadataAccessorCacheActor();
 }
 
 void TSharedMetadataAccessorCacheActor::Bootstrap() {
+    AFL_TRACE(NKikimrServices::TX_COLUMNSHARD)("shared_metadata_accessor_cache", "CreatBootstrapeActor");
     AccessorsCallback = std::make_shared<TActorAccessorsCallback>(SelfId());
     if (HasAppData()) {
         TotalMemorySize = AppDataVerified().ColumnShardConfig.GetSharedMetadataAccessorCacheSize();

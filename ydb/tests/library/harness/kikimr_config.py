3bc31de9d2e5fcf343bea09e994bc75103b4ee26
Ivan <ilezhankin@yandex-team.ru>
2025-06-23 16:36:15 +0300

    Enable verbose memory limit by default in recipe (#20040)

--- ydb/tests/library/harness/kikimr_config.py
+++ ydb/tests/library/harness/kikimr_config.py
@@ -174,6 +174,7 @@ class KikimrConfigGenerator(object):
             explicit_hosts_and_host_configs=False,
             table_service_config=None,  # dict[name]=value
             bridge_config=None,
+            verbose_memory_limit_exception=False,
     ):
         if extra_feature_flags is None:
             extra_feature_flags = []
@@ -273,6 +274,9 @@ class KikimrConfigGenerator(object):
 
         if "table_service_config" not in self.yaml_config:
             self.yaml_config["table_service_config"] = {}
+
+        if verbose_memory_limit_exception:
+            self.yaml_config["table_service_config"]["resource_manager"]["verbose_memory_limit_exception"] = True
 
         if table_service_config:
             self.yaml_config["table_service_config"].update(table_service_config)

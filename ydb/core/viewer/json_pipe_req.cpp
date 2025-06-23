f8ccd8421b46baea83e686a8538083eba7c4673e
Alexey Efimov <xeno@ydb.tech>
2025-06-23 12:40:49 +0000

    changes for merge

--- ydb/core/viewer/json_pipe_req.cpp
+++ ydb/core/viewer/json_pipe_req.cpp
@@ -153,6 +153,7 @@ void TViewerPipeClient::BuildParamsFromJson(TStringBuf data) {
                 }
             }
         }
+        PostData = std::move(jsonData);
     }
 }
 
@@ -360,6 +361,19 @@ TString TViewerPipeClient::GetError(const NSchemeShard::TEvSchemeShard::TEvDescr
         return ev.GetRecord().GetReason();
     }
     return NKikimrScheme::EStatus_Name(ev.GetRecord().GetStatus());
+}
+
+bool TViewerPipeClient::IsSuccess(const TEvTxUserProxy::TEvProposeTransactionStatus& ev) {
+    switch (ev.Record.GetStatus()) {
+        case TEvTxUserProxy::TEvProposeTransactionStatus::EStatus::ExecComplete:
+        case TEvTxUserProxy::TEvProposeTransactionStatus::EStatus::ExecInProgress:
+            return true;
+    }
+    return false;
+}
+
+TString TViewerPipeClient::GetError(const TEvTxUserProxy::TEvProposeTransactionStatus& ev) {
+    return TStringBuilder() << ev.Record.GetStatus();
 }
 
 void TViewerPipeClient::RequestHiveDomainStats(NNodeWhiteboard::TTabletId hiveId) {
--- ydb/core/viewer/json_pipe_req.h
+++ ydb/core/viewer/json_pipe_req.h
@@ -55,6 +55,7 @@ protected:
     NMon::TEvHttpInfo::TPtr Event;
     NHttp::TEvHttpProxy::TEvHttpIncomingRequest::TPtr HttpEvent;
     TCgiParameters Params;
+    NJson::TJsonValue PostData;
     TJsonSettings JsonSettings;
     TProto2JsonConfig Proto2JsonConfig;
     TDuration Timeout = TDuration::Seconds(10);
@@ -273,6 +274,9 @@ protected:
 
     static bool IsSuccess(const NSchemeShard::TEvSchemeShard::TEvDescribeSchemeResult& ev);
     static TString GetError(const NSchemeShard::TEvSchemeShard::TEvDescribeSchemeResult& ev);
+
+    static bool IsSuccess(const TEvTxUserProxy::TEvProposeTransactionStatus& ev);
+    static TString GetError(const TEvTxUserProxy::TEvProposeTransactionStatus& ev);
 
     void UpdateSharedCacheTablet(TTabletId tabletId, std::unique_ptr<IEventBase> request);
 

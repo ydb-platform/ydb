1a8b73484362239ebc6fd64ebb1d3541f42cfcb8
Nikolay Perfilov <pnv1@yandex-team.ru>
2025-06-24 11:07:02 +0300

    Fix test inheritance

--- ydb/tests/functional/tpc/medium/test_import_csv.py
+++ ydb/tests/functional/tpc/medium/test_import_csv.py
@@ -2,17 +2,17 @@ from ydb.tests.olap.load.lib.import_csv import ImportFileCsvBase # noqa
 from ydb.tests.functional.tpc.lib.conftest import FunctionalTestBase
 
 
-class LocalClusterFunctionalTestBase(FunctionalTestBase):
+class FunctionalImportFileCsvBase(ImportFileCsvBase, FunctionalTestBase):
     @classmethod
     def setup_class(cls) -> None:
         cls.setup_cluster()
         super().setup_class()
 
 
-class TestExternalImportCsv(ImportFileCsvBase, LocalClusterFunctionalTestBase):
+class TestExternalImportCsv(FunctionalImportFileCsvBase):
     external_folder: str = 'e1'
 
 
-class TestExternalImportCsvArrow(ImportFileCsvBase, LocalClusterFunctionalTestBase):
+class TestExternalImportCsvArrow(FunctionalImportFileCsvBase):
     external_folder: str = 'e1'
     send_format: str = 'arrow'

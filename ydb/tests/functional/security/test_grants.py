8726e190df5dc9ff6f7bb561bcab5237552024b8
Yury Kiselev <yurikiselev@ydb.tech>
2025-06-26 09:08:57 +0300

    Add cdc grants test

--- ydb/tests/functional/security/test_grants.py
+++ ydb/tests/functional/security/test_grants.py
@@ -11,80 +11,133 @@ CLUSTER_CONFIG = dict(
         # 'TX_PROXY': LogLevels.DEBUG,
     }
 )
+DATABASE = "/Root/test"
+TABLE_NAME = "table"
+TABLE_PATH = f"{DATABASE}/{TABLE_NAME}"
 
 
-def runWithAssert(config, query, expected_err):
+def run_query(config, query):
     with ydb.Driver(config) as driver:
         with ydb.QuerySessionPool(driver, size=1) as pool:
-            if not expected_err:
-                pool.execute_with_retries(query)
-            else:
-                operationSucceded = False
-                try:
-                    pool.execute_with_retries(query)
-                    operationSucceded = True
-                except Exception as e:
-                    assert expected_err in str(e)
-
-                assert not operationSucceded
+            pool.execute_with_retries(query)
 
 
-def test_basic_table_lifecycle(ydb_cluster):
-    root = "/Root"
-    database = f"{root}/test"
-    table_name = "table"
-    table_path = f"{database}/{table_name}"
-    ydb_cluster.create_database(database, storage_pool_units_count={"hdd": 1})
-    database_nodes = ydb_cluster.register_and_start_slots(database, count=1)
-    ydb_cluster.wait_tenant_up(database)
+def run_with_assert(config, query, expected_err=None):
+    if not expected_err:
+        run_query(config, query)
+        return
+
+    try:
+        run_query(config, query)
+    except Exception as e:
+        assert expected_err in str(e)
+
+
+def test_table_grants(ydb_cluster):
+    ydb_cluster.create_database(DATABASE, storage_pool_units_count={"hdd": 1})
+    database_nodes = ydb_cluster.register_and_start_slots(DATABASE, count=1)
+    ydb_cluster.wait_tenant_up(DATABASE)
 
     tenant_admin_config = ydb.DriverConfig(
         endpoint="%s:%s" % (ydb_cluster.nodes[1].host, ydb_cluster.nodes[1].port),
-        database=database,
+        database=DATABASE,
     )
     user1_config = ydb.DriverConfig(
         endpoint="%s:%s" % (ydb_cluster.nodes[1].host, ydb_cluster.nodes[1].port),
-        database=database,
+        database=DATABASE,
         credentials=ydb.StaticCredentials.from_user_password("user1", ""),
     )
     user2_config = ydb.DriverConfig(
         endpoint="%s:%s" % (ydb_cluster.nodes[1].host, ydb_cluster.nodes[1].port),
-        database=database,
+        database=DATABASE,
         credentials=ydb.StaticCredentials.from_user_password("user2", ""),
     )
 
-    runWithAssert(tenant_admin_config, "CREATE USER user1; CREATE USER user2;", expected_err=None)
+    run_with_assert(tenant_admin_config, "CREATE USER user1; CREATE USER user2;")
     # table creation without grant fails
-    create_table_query = f"CREATE TABLE {table_name} (a Uint64, b Uint64, PRIMARY KEY (a));"
-    runWithAssert(user1_config, create_table_query, expected_err="Access denied for scheme request")
+    create_table_query = f"CREATE TABLE {TABLE_NAME} (a Uint64, b Uint64, PRIMARY KEY (a));"
+    run_with_assert(user1_config, create_table_query, expected_err="Access denied for scheme request")
 
     # table creation with CREATE TABLE grant works
-    runWithAssert(tenant_admin_config, f"GRANT CREATE TABLE ON `{database}` TO user1;", expected_err=None)
-    runWithAssert(user1_config, create_table_query, expected_err=None)
+    run_with_assert(tenant_admin_config, f"GRANT CREATE TABLE ON `{DATABASE}` TO user1;")
+    run_with_assert(user1_config, create_table_query)
 
     # index creation without INSERT and SELECT grants fails
-    create_index_query = f"ALTER TABLE `{table_path}` ADD INDEX `b_column_index` GLOBAL ON (`b`);"
-    runWithAssert(user2_config, create_index_query, expected_err="Cannot find table")
+    create_index_query = f"ALTER TABLE `{TABLE_PATH}` ADD INDEX `b_column_index` GLOBAL ON (`b`);"
+    run_with_assert(user2_config, create_index_query, expected_err="Cannot find table")
 
-    # # index creation without INSERT and SELECT grants works
-    runWithAssert(
+    # index creation without INSERT and SELECT grants works
+    run_with_assert(
         tenant_admin_config,
-        f"GRANT INSERT ON `{table_path}` TO user2; GRANT SELECT ON `{table_path}` TO user2;",
-        expected_err=None,
+        f"GRANT INSERT ON `{TABLE_PATH}` TO user2; GRANT SELECT ON `{TABLE_PATH}` TO user2;",
     )
-    runWithAssert(user2_config, create_index_query, expected_err=None)
+    run_with_assert(user2_config, create_index_query)
 
     # adding a column with grants INSERT and SELECT works
-    runWithAssert(user2_config, f"ALTER TABLE `{table_path}` ADD COLUMN `c` Uint64;", expected_err=None)
+    run_with_assert(user2_config, f"ALTER TABLE `{TABLE_PATH}` ADD COLUMN `c` Uint64;")
 
     # removing an index with grants INSERT and SELECT works
-    runWithAssert(user2_config, f"ALTER TABLE `{table_path}` DROP INDEX `b_column_index`;", expected_err=None)
+    run_with_assert(user2_config, f"ALTER TABLE `{TABLE_PATH}` DROP INDEX `b_column_index`;")
 
     # removing a column with grants INSERT and SELECT works
-    runWithAssert(user2_config, f"ALTER TABLE `{table_path}` DROP COLUMN `b`;", expected_err=None)
+    run_with_assert(user2_config, f"ALTER TABLE `{TABLE_PATH}` DROP COLUMN `b`;")
 
     # dropping a table with grants INSERT and SELECT works
-    runWithAssert(user2_config, "DROP TABLE table;", expected_err=None)
+    run_with_assert(user2_config, "DROP TABLE table;")
 
-    ydb_cluster.remove_database(database)
+    ydb_cluster.remove_database(DATABASE)
+    ydb_cluster.unregister_and_stop_slots(database_nodes)
+
+
+def test_cdc_grants(ydb_cluster):
+    ydb_cluster.create_database(DATABASE, storage_pool_units_count={"hdd": 1})
+    database_nodes = ydb_cluster.register_and_start_slots(DATABASE, count=1)
+    ydb_cluster.wait_tenant_up(DATABASE)
+
+    tenant_admin_config = ydb.DriverConfig(
+        endpoint="%s:%s" % (ydb_cluster.nodes[1].host, ydb_cluster.nodes[1].port),
+        database=DATABASE,
+    )
+    user1_config = ydb.DriverConfig(
+        endpoint="%s:%s" % (ydb_cluster.nodes[1].host, ydb_cluster.nodes[1].port),
+        database=DATABASE,
+        credentials=ydb.StaticCredentials.from_user_password("user1", ""),
+    )
+    user2_config = ydb.DriverConfig(
+        endpoint="%s:%s" % (ydb_cluster.nodes[1].host, ydb_cluster.nodes[1].port),
+        database=DATABASE,
+        credentials=ydb.StaticCredentials.from_user_password("user2", ""),
+    )
+
+    # setup users and a table
+    run_with_assert(tenant_admin_config, "CREATE USER user1; CREATE USER user2;")
+    run_with_assert(tenant_admin_config, f"GRANT CREATE TABLE ON `{DATABASE}` TO user1;")
+    run_with_assert(user1_config, f"CREATE TABLE {TABLE_NAME} (a Uint64, b Uint64, PRIMARY KEY (a));")
+    run_with_assert(user1_config, f"INSERT INTO `{TABLE_PATH}` (a, b) VALUES (1, 1);")
+
+    # change feed creation without grants fails
+    create_changefeed_query = f"ALTER TABLE `{TABLE_PATH}` ADD CHANGEFEED updates WITH (FORMAT = 'JSON', MODE = 'NEW_AND_OLD_IMAGES', INITIAL_SCAN = TRUE);"
+    run_with_assert(user2_config, create_changefeed_query, expected_err='you do not have access permissions')
+
+    # provide grants to user2 that is not the owner of a table
+    grants = ['ydb.granular.alter_schema', 'ydb.generic.read']
+    provideGrantsQuery = ''
+    for grant in grants:
+        provideGrantsQuery += f"GRANT '{grant}' ON `{TABLE_PATH}` TO user2;"
+    run_with_assert(tenant_admin_config, provideGrantsQuery)
+    # change feed creation with grants works
+    run_with_assert(user2_config, create_changefeed_query)
+    # consumer registration works
+    run_with_assert(user2_config, f"ALTER TOPIC `{TABLE_PATH}/updates` ADD CONSUMER consumer;")
+
+    # reading change feed works
+    with ydb.Driver(user2_config) as driver:
+        with driver.topic_client.reader(f"{TABLE_PATH}/updates", consumer="consumer", buffer_size_bytes=1000) as reader:
+            message = reader.receive_message()
+            assert '"newImage"' in message.data.decode("utf-8")
+
+    # deleting change feed works
+    run_with_assert(user2_config, f"ALTER TABLE `{TABLE_PATH}` DROP CHANGEFEED updates;")
+
+    ydb_cluster.remove_database(DATABASE)
     ydb_cluster.unregister_and_stop_slots(database_nodes)

d80de22fde4ad14d0b3df6e33e7fee04f2bf5543
Denis Khalikov <deniskhalikov@ydb.tech>
2025-06-26 13:09:27 +0300

    [KQP Constant folding] Do not fold SafeCast for JsonDocument
    Do not fold SafeCast for JsonDocument because it could produce unexpected result

--- ydb/core/kqp/ut/join/data/queries/udf_constant_fold.sql
+++ ydb/core/kqp/ut/join/data/queries/udf_constant_fold.sql
@@ -5,3 +5,6 @@ WHERE S.payload2 = String::HexDecode("54");
 SELECT *
 FROM `/Root/R` as R
 WHERE R.ts = DateTime::MakeDate(DateTime::Parse('%Y-%m-%d')("2023-10-20"));
+
+SELECT CAST("[6]" AS JsonDocument) as doc
+FROM `/Root/R` as R;

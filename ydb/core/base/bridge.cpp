10379f324413d916f0015a2fd42b06cee3c26211
vporyadke <zalyalov@ydb.tech>
2025-06-26 14:15:10 +0200

    Apply suggestions from code review
    Co-authored-by: Ilnaz Nizametdinov <i.nizametdinov@gmail.com>

--- ydb/core/base/bridge.cpp
+++ ydb/core/base/bridge.cpp
@@ -1,15 +1,12 @@
-#include "appdata_fwd.h"
-#include "defs.h"
-#include "blobstorage_common.h"
+#include "bridge.h"
 
 #include <ydb/core/protos/config.pb.h>
-#include <ydb/core/protos/bridge.pb.h>
 
 namespace NKikimr {
 
-    bool IsBridgeMode(const TActorContext &ctx) {
-        const auto *bridgeConfig = AppData(ctx)->BridgeConfig;
-        return bridgeConfig && bridgeConfig->PilesSize() > 0;
-    }
+bool IsBridgeMode(const TActorContext &ctx) {
+    const auto *bridgeConfig = AppData(ctx)->BridgeConfig;
+    return bridgeConfig && bridgeConfig->PilesSize() > 0;
+}
 
 } // NKikimr

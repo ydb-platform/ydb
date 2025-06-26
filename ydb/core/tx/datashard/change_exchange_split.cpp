ed46b937fb43f309e53ed9eb04fe322390731b7c
Nikolay Shestakov <tesseract@ydb.tech>
2025-06-26 13:49:41 +0000

    fixed CDC write to the autopartitioning topic

--- ydb/core/tx/datashard/change_exchange_split.cpp
+++ ydb/core/tx/datashard/change_exchange_split.cpp
@@ -361,16 +361,18 @@ class TCdcWorker
             const auto tabletId = partition.GetTabletId();
 
             auto it = Workers.find(partitionId);
-            if (it != Workers.end()) {
-                workers.emplace(partitionId, it->second);
-                Workers.erase(it);
-            } else {
-                LOG_T("Register new worker"
-                    << ": partitionId# " << partitionId);
+            if (NKikimrPQ::ETopicPartitionStatus::Active == partition.GetStatus()) {
+                if (it != Workers.end()) {
+                    workers.emplace(partitionId, it->second);
+                    Workers.erase(it);
+                } else {
+                    LOG_T("Register new worker"
+                        << ": partitionId# " << partitionId);
 
-                const auto worker = Register(new TCdcPartitionWorker(SelfId(), partitionId, tabletId, SrcTabletId, DstTabletIds));
-                workers.emplace(partitionId, worker);
-                Pending.emplace(worker, partitionId);
+                    const auto worker = Register(new TCdcPartitionWorker(SelfId(), partitionId, tabletId, SrcTabletId, DstTabletIds));
+                    workers.emplace(partitionId, worker);
+                    Pending.emplace(worker, partitionId);
+                }
             }
         }
 

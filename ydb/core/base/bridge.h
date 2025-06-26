d77a38abeb2d483493a49fc47a5495011b07378d
Alexander Zalyalov <zalyalov@ydb.tech>
2025-06-26 08:00:08 +0000

    more correct way to check for bridge mode in local & hive

--- ydb/core/base/bridge.h
+++ ydb/core/base/bridge.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "appdata_fwd.h"
 #include "defs.h"
 #include "blobstorage_common.h"
 
+#include <ydb/core/protos/config.pb.h>
 #include <ydb/core/protos/bridge.pb.h>
 
 namespace NKikimr {
@@ -45,5 +47,10 @@ namespace NKikimr {
             }
         }
     };
+
+    inline bool IsBridgeMode(const TActorContext &ctx) {
+        const auto *bridgeConfig = AppData(ctx)->BridgeConfig;
+        return bridgeConfig && bridgeConfig->PilesSize() > 0;
+    }
 
 } // NKikimr

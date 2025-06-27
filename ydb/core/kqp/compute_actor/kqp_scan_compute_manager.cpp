8c006b7875e5653eb78af3e218b3f3d37b6fd277
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2025-06-27 14:33:23 +0000

    Revert "Correct Scan Generation in case of retries" (#20309)

--- ydb/core/kqp/compute_actor/kqp_scan_compute_manager.cpp
+++ ydb/core/kqp/compute_actor/kqp_scan_compute_manager.cpp
@@ -9,7 +9,6 @@ TShardState::TPtr TInFlightShards::Put(TShardState&& state) {
     MutableStatistics(state.TabletId).MutableStatistics(0).SetStartInstant(Now());
 
     TShardState::TPtr result = std::make_shared<TShardState>(std::move(state));
-    result->Generation = 1;
     AFL_ENSURE(Shards.emplace(result->TabletId, result).second)("tablet_id", result->TabletId);
     return result;
 }

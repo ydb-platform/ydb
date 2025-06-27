8e7a4b1fb0fcbda234f913111e87ccdf6d4a7487
Kirill Rysin <naspirato@gmail.com>
2025-06-27 16:51:26 +0300

    fix params

--- ydb/tests/olap/load/lib/workload_simple_queue.py
+++ ydb/tests/olap/load/lib/workload_simple_queue.py
@@ -20,37 +20,10 @@ class SimpleQueueBase(WorkloadTestBase):
     workload_env_var = 'SIMPLE_QUEUE_BINARY'
 
     @pytest.mark.parametrize('table_type', [t.value for t in TableType])
-    def test_workload_simple_queue(self, table_type: str):
-        # Формируем аргументы команды (без --duration, он будет добавлен в чанках)
-        command_args_template = (
-            f"--endpoint {YdbCluster.ydb_endpoint} "
-            f"--database /{YdbCluster.ydb_database} "
-            f"--mode {table_type}"
-        )
-
-        # Дополнительная статистика специфичная для SimpleQueue
-        additional_stats = {
-            "table_type": table_type,
-            "workload_type": "simple_queue"
-        }
-
-        # Запускаем тест с чанками
-        self.execute_workload_test(
-            workload_name=f"SimpleQueue_{table_type}",
-            command_args=command_args_template,
-            duration_value=self.timeout,
-            additional_stats=additional_stats,
-            use_chunks=True,
-            duration_param="--duration",
-            nodes_percentage=100
-        )
-
-    @pytest.mark.parametrize('table_type', [t.value for t in TableType])
     @pytest.mark.parametrize('nemesis_enabled', [True, False], 
                             ids=['nemesis_true', 'nemesis_false'])
-    @pytest.mark.parametrize('nodes_percentage', [100, 50, 25], 
-                           ids=['nodes_100p', 'nodes_50p', 'nodes_25p'])
-    def test_workload_simple_queue_with_nemesis(self, table_type: str, nemesis_enabled: bool, nodes_percentage: int):
+
+    def test_workload_simple_queue(self, table_type: str, nemesis_enabled: bool):
         # Формируем аргументы команды (без --duration, он будет добавлен в чанках)
         command_args_template = (
             f"--endpoint {YdbCluster.ydb_endpoint} "
@@ -63,19 +36,19 @@ class SimpleQueueBase(WorkloadTestBase):
             "table_type": table_type,
             "workload_type": "simple_queue",
             "nemesis": nemesis_enabled,
-            "nodes_percentage": nodes_percentage
+            "nodes_percentage": 100
         }
 
         # Запускаем тест с чанками и указанием nemesis и процента нод
         self.execute_workload_test(
-            workload_name=f"SimpleQueue_{table_type}_nemesis_{nemesis_enabled}_nodes_{nodes_percentage}p",
+            workload_name=f"SimpleQueue_{table_type}_nemesis_{nemesis_enabled}",
             command_args=command_args_template,
             duration_value=self.timeout,
             additional_stats=additional_stats,
             use_chunks=True,
             duration_param="--duration",
             nemesis=nemesis_enabled,
-            nodes_percentage=nodes_percentage
+            nodes_percentage=100
         )
 
 

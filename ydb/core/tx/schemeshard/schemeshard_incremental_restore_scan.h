2dcd0fa6faaa2ed994cea011538f873bceeb172a
Innokentii Mokin <innokentii@ydb.tech>
2025-06-27 11:59:26 +0000

    WIP incr restore TTxProgress

--- ydb/core/tx/schemeshard/schemeshard_incremental_restore_scan.h
+++ ydb/core/tx/schemeshard/schemeshard_incremental_restore_scan.h
@@ -4,7 +4,5 @@
 
 namespace NKikimr::NSchemeShard::NIncrementalRestoreScan {
 
-// Transaction types for incremental restore scan
-constexpr ui32 TXTYPE_PROGRESS_INCREMENTAL_RESTORE = 1100; // Using a unique ID
 
 } // namespace NKikimr::NSchemeShard::NIncrementalRestoreScan

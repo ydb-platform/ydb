9d11f90529142cd678eaa2df26921814ca117b81
ivanmorozov333 <imorozov333@ya.ru>
2025-06-29 09:51:37 +0300

    correction

--- ydb/core/tx/data_events/common/signals_flow.h
+++ ydb/core/tx/data_events/common/signals_flow.h
@@ -24,29 +24,16 @@ enum class EWriteStage {
 
 class TWriteFlowCounters: public NColumnShard::TCommonCountersOwner {
 private:
-    using TBase = TCommonCountersOwner;
+    using TBaseSignals = TCommonCountersOwner;
+    using TBaseTracing = TStateSignalsOperator<EWriteStage>;
 
-    std::vector<NMonitoring::TDynamicCounters::TCounterPtr> CountByWriteStage;
-    std::vector<NMonitoring::TDynamicCounters::TCounterPtr> WriteStageAdd;
-    std::vector<std::vector<NMonitoring::TDynamicCounters::TCounterPtr>> CountByStageMoving;
-    std::vector<std::vector<NMonitoring::TDynamicCounters::TCounterPtr>> CountByStageDuration;
-    std::vector<NMonitoring::THistogramPtr> DurationToStage;
     NMonitoring::TDynamicCounters::TCounterPtr DurationToFinish;
     NMonitoring::TDynamicCounters::TCounterPtr DurationToAbort;
+    TStateSignalsOperator<EWriteStage> Tracing;
 
 public:
-    void OnStageMove(const EWriteStage fromStage, const EWriteStage toStage, const TDuration d) const {
-        CountByWriteStage[(ui32)fromStage]->Sub(1);
-        CountByWriteStage[(ui32)toStage]->Add(1);
-        WriteStageAdd[(ui32)toStage]->Add(1);
-        DurationToStage[(ui32)toStage]->Collect(d.MilliSeconds());
-        CountByStageMoving[(ui32)fromStage][(ui32)toStage]->Add(1);
-        CountByStageDuration[(ui32)fromStage][(ui32)toStage]->Add(d.MilliSeconds());
-    }
-
-    void OnWritingStart(const EWriteStage stage) const {
-        WriteStageAdd[(ui32)stage]->Add(1);
-        CountByWriteStage[(ui32)stage]->Add(1);
+    TStateSignalsOperator<EWriteStage>& MutableTracing() {
+        return Tracing;
     }
 
     void OnWriteFinished(const TDuration d) const {

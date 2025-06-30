88c1fc23f23363ce099a744bb05c6f7848b79ba5
robot-piglet <robot-piglet@yandex-team.com>
2025-06-30 10:37:25 +0300

    Intermediate changes commit_hash:28fbcd0b479950d8ca8a2b9362c0a980833372b9

--- contrib/libs/cctz/.yandex_meta/override.nix
+++ contrib/libs/cctz/.yandex_meta/override.nix
@@ -7,4 +7,18 @@ pkgs: attrs: with pkgs; with attrs; rec {
     rev = "583c52d1eaef159162790a1d4044940f5e0b201b";
     hash = "sha256-t5BWp24761mMBYWNBXBJXQZqiG76n8BYX3Rdo38jjX4=";
   };
+
+  buildInputs = [
+    gtest
+    gbenchmark
+  ];
+
+  # FIXME: 
+  # It looks like CMake build is not functional.
+  # Understand and make it work.
+  cmakeFlags = [
+    "-DBUILD_TESTING=ON"
+    "-DBUILD_TOOLS=OFF"
+    "-DBUILD_EXAMPLES=OFF"
+  ];
 }

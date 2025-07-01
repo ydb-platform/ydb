ad3c6c718f8e240e660cdbce3cd19c9cfcc9d4dc
st-shchetinin <st-shchetinin@ydb.tech>
2025-07-01 13:17:50 +0300

    remove extra diff

--- ydb/core/cms/cluster_info.cpp
+++ ydb/core/cms/cluster_info.cpp
@@ -361,7 +361,6 @@ void TClusterInfo::AddNode(const TEvInterconnect::TNodeInfo &info, const TActorC
     node->IcPort = info.Port;
     node->Location = info.Location;
     node->State = NKikimrCms::UNKNOWN;
-
     if (auto it = NodeIdToPileId->find(info.NodeId); it != NodeIdToPileId->end()) {
         node->PileId = it->second;
     }

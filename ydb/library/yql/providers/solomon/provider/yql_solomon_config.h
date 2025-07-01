c9ac669e7a37daf2a4229198b50a8f7fb79a19ef
evanevanevanevannnn <evanevannnn@ydb.tech>
2025-07-01 15:00:29 +0000

    Enabling downsampling on time ranges without true points

--- ydb/library/yql/providers/solomon/provider/yql_solomon_config.h
+++ ydb/library/yql/providers/solomon/provider/yql_solomon_config.h
@@ -14,6 +14,7 @@ private:
 public:
     NCommon::TConfSetting<bool, Static> _EnableReading;
     NCommon::TConfSetting<bool, Static> _EnableRuntimeListing;
+    NCommon::TConfSetting<ui64, Static> _TruePointsFindRange;
     NCommon::TConfSetting<ui64, Static> MetricsQueuePageSize;
     NCommon::TConfSetting<ui64, Static> MetricsQueuePrefetchSize;
     NCommon::TConfSetting<ui64, Static> MetricsQueueBatchCountLimit;

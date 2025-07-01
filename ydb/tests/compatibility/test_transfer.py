6cfc45d0118877faa5d3b3eb927563d184ff34e5
Nikolay Shestakov <tesseract@ydb.tech>
2025-07-01 10:19:33 +0000

    worked

--- ydb/tests/compatibility/test_transfer.py
+++ ydb/tests/compatibility/test_transfer.py
@@ -1,187 +1,230 @@
 # -*- coding: utf-8 -*-
+import math
 import pytest
 import time
+import uuid
 
 from ydb.tests.library.compatibility.fixtures import RestartToAnotherVersionFixture, RollingUpgradeAndDowngradeFixture, MixedClusterFixture
 from ydb.tests.oss.ydb_sdk_import import ydb
 
 
-def create_table(pool, mode):
-    pool.execute_with_retries(
-        f"""
-            CREATE TABLE target_table (
-                partition Uint32 NOT NULL,
-                offset Uint64 NOT NULL,
-                message Utf8,
-                PRIMARY KEY (partition, offset)
-            )  WITH (
-                STORE = {mode}
-            );
-        """
-    )
+class Workload:
+    def __init__(self, driver, endpoint):
+        self.driver = driver
+        self.endpoint = endpoint
+        self.id = f"{uuid.uuid1()}".replace("-", "_")
+        self.table_name = f"target_table_{self.id}"
+        self.topic_name = f"source_topic_{self.id}"
+        self.transfer_name = f"transfer_{self.id}"
+        self.message_count = 0
 
-def create_topic(pool):
-    pool.execute_with_retries(
-        f"CREATE TOPIC source_topic;"
-    )
+    def create_table(self, mode):
+        with ydb.QuerySessionPool(self.driver) as session_pool:
+            session_pool.execute_with_retries(
+                f"""
+                    CREATE TABLE {self.table_name} (
+                        partition Uint32 NOT NULL,
+                        offset Uint64 NOT NULL,
+                        message Utf8,
+                        PRIMARY KEY (partition, offset)
+                    )  WITH (
+                        STORE = {mode}
+                    );
+                """
+            )
 
-def create_transfer(pool):
-    lmb = '''
-            $l = ($x) -> {
-                return [
-                    <|
-                        partition:CAST($x._partition AS Uint32),
-                        offset:CAST($x._offset AS Uint64),
-                        message:CAST($x._data AS Utf8)
-                    |>
-                ];
-            };
-    '''
+    def create_topic(self):
+        with ydb.QuerySessionPool(self.driver) as session_pool:
+            session_pool.execute_with_retries(
+                f"CREATE TOPIC {self.topic_name};"
+            )
 
-    self.pool.execute_with_retries(
-        f"""
-            {lmb}
+    def create_transfer(self, local):
+        lmb = '''
+                $l = ($x) -> {
+                    return [
+                        <|
+                            partition:CAST($x._partition AS Uint32),
+                            offset:CAST($x._offset AS Uint64),
+                            message:CAST($x._data AS Utf8)
+                        |>
+                    ];
+                };
+        '''
 
-            CREATE TRANSFER transfer_name
-            FROM source_toic TO target_table USING $l
-            WITH (
-                FLUSH_INTERVAL = Interval('PT1S'),
-                BATCH_SIZE_BYTES = 8388608
-            );
-        """
-    )
+        connection_string = "" if local else f"CONNECTION_STRING = '{self.endpoint}/?database=/Root',"
 
-def write_to_topic(driver):
-    finished_at = time.time() + 15
-    message_count = 0
+        with ydb.QuerySessionPool(self.driver) as session_pool:
+            session_pool.execute_with_retries(
+                f"""
+                    {lmb}
 
-    with driver.topic_client.writer('source_topic', producer_id="producer-id") as writer:
-        while time.time() < finished_at:
-            writer.write(ydb.TopicWriterMessage(f"message-{time.time()}"))
-            message_count += 1
-    
-    return message_count
+                    CREATE TRANSFER {self.transfer_name}
+                    FROM {self.topic_name} TO {self.table_name} USING $l
+                    WITH (
+                        {connection_string}
+                        FLUSH_INTERVAL = Interval('PT1S'),
+                        BATCH_SIZE_BYTES = 8388608
+                    );
+                """
+            )
 
-def wait_transfer_finished(pool, expected_message_count):
-    iterations = 10
+    def write_to_topic(self):
+        finished_at = time.time() + 15
 
-    last_offset = -1
+        with self.driver.topic_client.writer(self.topic_name, producer_id="producer-id") as writer:
+            while time.time() < finished_at:
+                writer.write(ydb.TopicWriterMessage(f"message-{time.time()}"))
+                self.message_count += 1
 
-    for i in range(iterations):
-        time.sleep(1)
+    def wait_transfer_finished(self):
+        iterations = 10
+        last_offset = -1
 
-        rss = pool.execute_with_retries(
-            """
-                SELECT MAX(offset) AS last_offset
-                FROM target_table;
-            """
-        )
-        rs = rss[0]
-        last_offset = rs.rows[0].last_offset
+        with ydb.QuerySessionPool(self.driver) as session_pool:
+            for i in range(iterations):
+                time.sleep(1)
 
-        if last_offset + 1 == expected_message_count:
-            return
+                rss = session_pool.execute_with_retries(
+                    f"""
+                        SELECT MAX(offset) AS last_offset
+                        FROM {self.table_name};
+                    """
+                )
+                rs = rss[0]
+                last_offset = rs.rows[0].last_offset
 
-    raise Exception(f"Transfer still work after {iterations} seconds. Last offset is {last_offset}")
+                if last_offset + 1 == self.message_count:
+                    return
 
-@unittest.skip('uncomment after 24-4 and 25-1 ecxluded. functionality raise from 25-1-2')
+            raise Exception(f"Transfer still work after {iterations} seconds. Last offset is {last_offset}")
+
+
+def skip_if_unsupported(versions):
+    if min(versions) < (25, 1, 2):
+        pytest.skip("Only available since 25-1-2")
+
+    # TODO remove it after current >= 25-1-2
+    for v in versions:
+        if math.isnan(v[0]):
+            pytest.skip("Only available since 25-1-2")
+
+
 class TestTransferMixedClusterFixture(MixedClusterFixture):
     @pytest.fixture(autouse=True, scope="function")
     def setup(self):
+        skip_if_unsupported(self.versions)
+
         #
         # Setup cluster
         #
         yield from self.setup_cluster(
             # # Some feature flags can be passed. And other KikimrConfigGenerator options
-            # extra_feature_flags={
-            #     "some_feature_flag": True,
-            # }
+            extra_feature_flags={
+                "enable_column_store": True,
+                "enable_topic_transfer": True,
+            }
         )
 
-    @pytest.mark.parametrize("store", ["row", "column"])
-    def test_transfer(self, store):
-        with ydb.QuerySessionPool(self.driver) as session_pool:
-            #
-            # 1. Fill table with data
-            #
-            create_table(session_pool, store)
-            create_topic(session_pool)
-            create_transfer(session_pool)
+    @pytest.mark.parametrize("store, local", [
+        ("row", False),
+        # ("column", False) TODO uncomment after OLAP will be enabled
+        # ("row", True), TODO uncomment after local topic will be merged
+    ])
+    def test_transfer(self, store, local):
+        utils = Workload(self.driver, self.endpoint)
 
-            message_count = write_to_topic(self.driver)
+        #
+        # 1. Fill table with data
+        #
+        utils.create_table(store)
+        utils.create_topic()
+        utils.create_transfer(local)
 
-            #
-            # 2. check written data is correct
-            #
-            wait_transfer_finished(session_pool, message_count)
+        utils.write_to_topic()
+
+        #
+        # 2. check written data is correct
+        #
+        utils.wait_transfer_finished()
 
 
-@unittest.skip('uncomment after 24-4 excluded. functionality raise from 25-1-2')
 class TestTransferRestartToAnotherVersion(RestartToAnotherVersionFixture):
     @pytest.fixture(autouse=True, scope="function")
     def setup(self):
+        skip_if_unsupported(self.versions)
+
         #
         # Setup cluster
         #
         yield from self.setup_cluster(
             # # Some feature flags can be passed. And other KikimrConfigGenerator options
-            # extra_feature_flags={
-            #     "some_feature_flag": True,
-            # }
+            extra_feature_flags={
+                "enable_column_store": True,
+                "enable_topic_transfer": True,
+            }
         )
 
-    @pytest.mark.parametrize("store", ["row", "column"])
-    def test_transfer(self, store):
-        with ydb.QuerySessionPool(self.driver) as session_pool:
-            #
-            # 1. Fill table with data
-            #
-            create_table(session_pool, store)
-            create_topic(session_pool)
-            create_transfer(session_pool)
+    @pytest.mark.parametrize("store, local", [
+        ("row", False),
+        # ("column", False) TODO uncomment after OLAP will be enabled
+        # ("row", True), TODO uncomment after local topic will be merged
+    ])
+    def test_transfer(self, store, local):
+        utils = Workload(self.driver, self.endpoint)
 
-            self.change_cluster_version()
+        #
+        # 1. Fill table with data
+        #
+        utils.create_table(store)
+        utils.create_topic()
+        utils.create_transfer(local)
 
-            message_count = write_to_topic(self.driver)
+        utils.write_to_topic()
+        self.change_cluster_version()
+        utils.write_to_topic()
 
-            #
-            # 2. check written data is correct
-            #
-            wait_transfer_finished(session_pool, message_count)
+        #
+        # 2. check written data is correct
+        #
+        utils.wait_transfer_finished()
 
 
-@unittest.skip('uncomment after 24-4 excluded. functionality raise from 25-1-2')
 class TestTransferRollingUpdate(RollingUpgradeAndDowngradeFixture):
     @pytest.fixture(autouse=True, scope="function")
     def setup(self):
+        skip_if_unsupported(self.versions)
+
         #
         # Setup cluster
         #
         yield from self.setup_cluster(
             # # Some feature flags can be passed. And other KikimrConfigGenerator options
-            # extra_feature_flags={
-            #     "some_feature_flag": True,
-            # }
+            extra_feature_flags={
+                "enable_column_store": True,
+                "enable_topic_transfer": True,
+            }
         )
 
-    @pytest.mark.parametrize("store", ["row", "column"])
-    def test_transfer(self, store):
-        with ydb.QuerySessionPool(self.driver) as session_pool:
-            #
-            # 1. Fill table with data
-            #
-            create_table(session_pool, store)
-            create_topic(session_pool)
-            create_transfer(session_pool)
+    @pytest.mark.parametrize("store, local", [
+        ("row", False),
+        # ("column", False) TODO uncomment after OLAP will be enabled
+        # ("row", True), TODO uncomment after local topic will be merged
+    ])
+    def test_transfer(self, store, local):
+        utils = Workload(self.driver, self.endpoint)
 
-            self.change_cluster_version()
+        #
+        # 1. Fill table with data
+        #
+        utils.create_table(store)
+        utils.create_topic()
+        utils.create_transfer(local)
 
-            message_count = 0
-            for _ in self.roll():
-                message_count += write_to_topic(self.driver)
+        for _ in self.roll():
+            utils.write_to_topic()
 
-            #
-            # 2. check written data is correct
-            #
-            wait_transfer_finished(session_pool, message_count)
-
+        #
+        # 2. check written data is correct
+        #
+        utils.wait_transfer_finished()

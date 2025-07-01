d0d120b057398e2ab8d09a41497525e4ca360bf7
StekPerepolnen <arykov@ydb.tech>
2025-07-01 11:57:19 +0200

    asan

--- ydb/mvp/oidc_proxy/extension_manager.cpp
+++ ydb/mvp/oidc_proxy/extension_manager.cpp
@@ -23,16 +23,18 @@ void TExtensionManager::SetRequest(NHttp::THttpIncomingRequestPtr request) {
 }
 
 void TExtensionManager::SetOverrideResponse(NHttp::TEvHttpProxy::TEvHttpIncomingResponse::TPtr event) {
+    ExtensionCtx->Params->HeadersOverride = MakeHolder<NHttp::THeadersBuilder>();
     if (!event) {
         ExtensionCtx->Params->ResponseError = "Timeout while waiting for whoami info";
-        ExtensionCtx->Params->HeadersOverride = MakeHolder<NHttp::THeadersBuilder>();
     } else if (!event->Get()->Response) {
         ExtensionCtx->Params->ResponseError = event->Get()->GetError();
-        ExtensionCtx->Params->HeadersOverride = MakeHolder<NHttp::THeadersBuilder>();
     } else {
         auto& response = event->Get()->Response;
         ExtensionCtx->Params->StatusOverride = response->Status;
-        ExtensionCtx->Params->HeadersOverride = MakeHolder<NHttp::THeadersBuilder>(response->Headers);
+        auto headers = NHttp::THeaders(response->Headers);
+        for (const auto& header : headers.Headers) {
+            ExtensionCtx->Params->HeadersOverride->Set(header.first, header.second);
+        }
         ExtensionCtx->Params->MessageOverride = response->Message;
         ExtensionCtx->Params->BodyOverride = response->Body;
     }

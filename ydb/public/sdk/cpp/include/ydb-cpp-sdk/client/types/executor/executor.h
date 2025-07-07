1f39d70942b9353fdcdf526a453a79eb14d39383
Bulat Gayazov <bylatgr@gmail.com>
2025-07-07 17:18:10 +0000

    [C++ SDK] Custom executor

---
+++ ydb/public/sdk/cpp/include/ydb-cpp-sdk/client/types/executor/executor.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <util/thread/pool.h>
+
+#include <functional>
+
+namespace NYdb::inline Dev {
+
+class IExecutor {
+public:
+    virtual void Post(std::function<void()>&& f) = 0;
+
+    virtual ~IExecutor() = default;
+};
+
+// Create executor adapter for util thread pool.
+// Thread pool is expected to have been started.
+std::shared_ptr<IExecutor> CreateThreadPoolExecutorAdapter(std::shared_ptr<IThreadPool> threadPool);
+
+} // namespace NYdb::inline Dev

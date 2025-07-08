2e2ba7da4ffc7b1fcbd0beefb8a151678055a90f
Alexander Kotov <akotov@ydb.tech>
2025-07-08 16:41:07 +0300

    [*] PersistSpan

--- ydb/core/persqueue/pq_impl.cpp
+++ ydb/core/persqueue/pq_impl.cpp
@@ -3864,6 +3864,10 @@ void TPersQueue::ProcessWriteTxs(const TActorContext& ctx,
 
             ChangedTxs.emplace(tx->Step, txId);
 
+            tx->PersistSpan = tx->ExecuteSpan.CreateChild(TWilsonTopic::ExecuteTransaction,
+                                                          "Topic.Transaction.Persist",
+                                                          NWilson::EFlags::AUTO_END);
+            tx->PersistSpan.Attribute("State", NKikimrPQ::TTransaction_EState_Name(state));
             tx->PersistSpan.Link(WriteTxsSpan.GetTraceId());
         }
     }
@@ -4639,11 +4643,6 @@ void TPersQueue::WriteTx(TDistributedTransaction& tx, NKikimrPQ::TTransaction::E
     WriteTxs[tx.TxId] = state;
 
     tx.WriteInProgress = true;
-
-    tx.PersistSpan = tx.ExecuteSpan.CreateChild(TWilsonTopic::ExecuteTransaction,
-                                                "Topic.Transaction.Persist",
-                                                NWilson::EFlags::AUTO_END);
-    tx.PersistSpan.Attribute("State", NKikimrPQ::TTransaction_EState_Name(state));
 }
 
 void TPersQueue::DeleteTx(TDistributedTransaction& tx)

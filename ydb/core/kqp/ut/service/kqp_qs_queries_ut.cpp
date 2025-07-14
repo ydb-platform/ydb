4e116053531027431f6a339b7bb4f230dd9a1dca
Nikita Vasilev <ns-vasilev@ydb.tech>
2025-07-14 09:54:13 +0300

    fix

--- ydb/core/kqp/ut/service/kqp_qs_queries_ut.cpp
+++ ydb/core/kqp/ut/service/kqp_qs_queries_ut.cpp
@@ -6306,6 +6306,7 @@ Y_UNIT_TEST_SUITE(KqpQueryService) {
                 
                 auto result = session.ExecuteQuery(query, TTxControl::Tx(tx), params.Build()).GetValueSync();
                 UNIT_ASSERT_C(result.IsSuccess(), result.GetIssues().ToString());
+                Sleep(TDuration::MilliSeconds(500));
             }
             
             auto commitResult = tx.Commit().GetValueSync();

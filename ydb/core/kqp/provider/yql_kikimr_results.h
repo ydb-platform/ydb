6103c4f43b10c8ad3835fa2078b41887fa1b4d35
Vitalii Gridnev <gridnevvvit@gmail.com>
2025-07-14 23:44:11 +0300

    remove one more unused function

--- ydb/core/kqp/provider/yql_kikimr_results.h
+++ ydb/core/kqp/provider/yql_kikimr_results.h
@@ -7,8 +7,6 @@ namespace NYql {
 void KikimrResultToYson(const TStringStream& stream, NYson::TYsonWriter& writer, const NKikimrMiniKQL::TResult& result,
     const TColumnOrder& columnHints, const IDataProvider::TFillSettings& fillSettings, bool& truncated);
 
-bool ExportTypeToKikimrProto(const TTypeAnnotationNode& type, NKikimrMiniKQL::TType& protoType, TExprContext& ctx);
-
 const TTypeAnnotationNode* ParseTypeFromYdbType(const Ydb::Type& input, TExprContext& ctx);
 
 } // namespace NYql

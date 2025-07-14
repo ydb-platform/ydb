0d027b35fd49399772f3bc5970ac5631d8adfbb8
Evgenik2 <evkam@mail.ru>
2025-07-14 15:05:19 +0300

    Fix naming

--- ydb/tests/stress/reconfig_state_storage_workload/tests/test_scheme_board_workload.py
+++ ydb/tests/stress/reconfig_state_storage_workload/tests/test_scheme_board_workload.py
@@ -2,6 +2,6 @@
 from reconfig_state_storage_workload_test import ReconfigStateStorageWorkloadTest
 
 
-class TestReconfigStateStorageBoardWorkload(ReconfigStateStorageWorkloadTest):
-    def test_state_storage_board(self):
+class TestReconfigSchemeBoardWorkload(ReconfigStateStorageWorkloadTest):
+    def test_scheme_board(self):
         self.do_test("SchemeBoard")
--- ydb/tests/stress/reconfig_state_storage_workload/tests/test_state_storage_workload.py
+++ ydb/tests/stress/reconfig_state_storage_workload/tests/test_state_storage_workload.py
@@ -2,6 +2,6 @@
 from reconfig_state_storage_workload_test import ReconfigStateStorageWorkloadTest
 
 
-class TestReconfigStateStorageBoardWorkload(ReconfigStateStorageWorkloadTest):
-    def test_state_storage_board(self):
+class TestReconfigStateStorageWorkload(ReconfigStateStorageWorkloadTest):
+    def test_state_storage(self):
         self.do_test("StateStorage")

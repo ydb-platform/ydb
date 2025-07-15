788cdaf41622b5453e72548f088c10103a010ba8
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-07-15 12:44:22 +0300

    Set account type in ticket parser response (#21122)
    (cherry picked from commit d04641068c920daa6228aa80d86fdee5db282234)
    (cherry picked from commit 56a1832fd39473632823a68a1d8efb60d0486199)

--- ydb/core/security/ticket_parser_impl.h
+++ ydb/core/security/ticket_parser_impl.h
@@ -59,6 +59,7 @@ private:
             USER_ACCOUNT_TYPE,
             SERVICE_ACCOUNT_TYPE,
             ANONYMOUS_ACCOUNT_TYPE,
+            SERVICE_ACCOUNT_IMPERSONATED_FROM_USER_ACCOUNT_TYPE, // Service account credentials got from impersonation of user account
         };
 
         TString Subject;
@@ -651,14 +652,49 @@ private:
             return TPermissionRecord::TTypeCase::USER_ACCOUNT_TYPE;
         case Account::kServiceAccount:
             return TPermissionRecord::TTypeCase::SERVICE_ACCOUNT_TYPE;
+        case Account::kAnonymousAccount:
+            return TPermissionRecord::TTypeCase::ANONYMOUS_ACCOUNT_TYPE;
         default:
             return TPermissionRecord::TTypeCase::TYPE_NOT_SET;
         }
     }
 
+    typename TPermissionRecord::TTypeCase ConvertSubjectType(const nebius::iam::v1::Account::TypeCase& type, const nebius::iam::v1::ImpersonationInfo& impersonationInfo) {
+        typename TPermissionRecord::TTypeCase result = ConvertSubjectType(type);
+        if (result == TPermissionRecord::TTypeCase::SERVICE_ACCOUNT_TYPE) {
+            for (const nebius::iam::v1::ImpersonationInfo::ImpersonationAccount& impersonationAccount : impersonationInfo.chain()) {
+                for (const nebius::iam::v1::Account& account : impersonationAccount.account()) {
+                    if (ConvertSubjectType(account.GetTypeCase()) == TPermissionRecord::TTypeCase::USER_ACCOUNT_TYPE) {
+                        result = TPermissionRecord::TTypeCase::SERVICE_ACCOUNT_IMPERSONATED_FROM_USER_ACCOUNT_TYPE;
+                        break;
+                    }
+                }
+                if (result == TPermissionRecord::TTypeCase::SERVICE_ACCOUNT_IMPERSONATED_FROM_USER_ACCOUNT_TYPE) {
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
     template <>
     typename TPermissionRecord::TTypeCase ConvertSubjectType<nebius::iam::v1::AuthenticateResponse>(const nebius::iam::v1::AuthenticateResponse& response) {
         return ConvertSubjectType(response.account().type_case());
+    }
+
+    NACLibProto::ESubjectType ConvertSubjectTypeToProto(typename TPermissionRecord::TTypeCase type) {
+        switch (type) {
+        case TPermissionRecord::TTypeCase::TYPE_NOT_SET:
+            return NACLibProto::SUBJECT_TYPE_UNSPECIFIED;
+        case TPermissionRecord::TTypeCase::USER_ACCOUNT_TYPE:
+            return NACLibProto::SUBJECT_TYPE_USER;
+        case TPermissionRecord::TTypeCase::SERVICE_ACCOUNT_TYPE:
+            return NACLibProto::SUBJECT_TYPE_SERVICE;
+        case TPermissionRecord::TTypeCase::ANONYMOUS_ACCOUNT_TYPE:
+            return NACLibProto::SUBJECT_TYPE_ANONYMOUS;
+        case TPermissionRecord::TTypeCase::SERVICE_ACCOUNT_IMPERSONATED_FROM_USER_ACCOUNT_TYPE:
+            return NACLibProto::SUBJECT_TYPE_SERVICE_IMPERSONATED_FROM_USER;
+        }
     }
 
     template <typename TTokenRecord>
@@ -1155,7 +1191,7 @@ private:
                                 processingError = true;
                                 break;
                             }
-                            record.SubjectType = ConvertSubjectType(account.type_case());
+                            record.SubjectType = ConvertSubjectType(account.type_case(), result.impersonation_info());
                             for (auto& [_, permissionRecord] : record.Permissions) {
                                 if (permissionRecord.Error.empty()) {
                                     permissionRecord.Subject = record.Subject;
@@ -1715,6 +1751,7 @@ protected:
 
     template <typename TTokenRecord>
     void SetToken(const TString& key, TTokenRecord& record, TIntrusivePtr<NACLib::TUserToken> token) {
+        token->SetSubjectType(ConvertSubjectTypeToProto(record.SubjectType));
         TInstant now = TlsActivationContext->Now();
         record.Error.clear();
         EnrichUserTokenWithBuiltins(record, token);

a8cd3b24ce1d2cbced49f908de6a012c39702e50
Vitalii Gridnev <gridnevvvit@gmail.com>
2025-07-16 18:56:51 +0300

    fix complains on kqp compile service

--- ydb/core/kqp/compile_service/kqp_compile_service.cpp
+++ ydb/core/kqp/compile_service/kqp_compile_service.cpp
@@ -119,15 +119,15 @@ public:
     TKqpRequestsQueue(size_t maxSize)
         : MaxSize(maxSize) {}
 
-    bool Enqueue(TKqpCompileRequest&& request) {
+    std::optional<TKqpCompileRequest> Enqueue(TKqpCompileRequest&& request) {
         if (Size() >= MaxSize) {
-            return false;
+            return request;
         }
 
         Queue.push_back(std::move(request));
         auto it = std::prev(Queue.end());
         QueryIndex[it->Query].insert(it);
-        return true;
+        return std::nullopt;
     }
 
     TMaybe<TKqpCompileRequest> Dequeue() {
@@ -480,7 +480,8 @@ private:
             return CompileByAst(*request.QueryAst, compileRequest, ctx);
         }
 
-        if (!RequestsQueue.Enqueue(std::move(compileRequest))) {
+        auto overflow = RequestsQueue.Enqueue(std::move(compileRequest));
+        if (overflow.has_value()) {
             Counters->ReportCompileRequestRejected(dbCounters);
 
             LOG_WARN_S(ctx, NKikimrServices::KQP_COMPILE_SERVICE, "Requests queue size limit exceeded"
@@ -489,7 +490,8 @@ private:
 
             NYql::TIssue issue(NYql::TPosition(), TStringBuilder() <<
                 "Exceeded maximum number of requests in compile service queue.");
-            ReplyError(ev->Sender, "", Ydb::StatusIds::OVERLOADED, {issue}, ctx, compileRequest.Cookie, std::move(compileRequest.Orbit), std::move(compileRequest.CompileServiceSpan));
+            ReplyError(ev->Sender, "", Ydb::StatusIds::OVERLOADED, {issue},
+                ctx, overflow->Cookie, std::move(overflow->Orbit), std::move(overflow->CompileServiceSpan));
             return;
         }
 
@@ -561,7 +563,8 @@ private:
                 return CompileByAst(*request.QueryAst, compileRequest, ctx);
             }
 
-            if (!RequestsQueue.Enqueue(std::move(compileRequest))) {
+            auto overflow = RequestsQueue.Enqueue(std::move(compileRequest));
+            if (overflow.has_value()) {
                 Counters->ReportCompileRequestRejected(dbCounters);
 
                 LOG_WARN_S(ctx, NKikimrServices::KQP_COMPILE_SERVICE, "Requests queue size limit exceeded"
@@ -570,7 +573,8 @@ private:
 
                 NYql::TIssue issue(NYql::TPosition(), TStringBuilder() <<
                     "Exceeded maximum number of requests in compile service queue.");
-                ReplyError(ev->Sender, "", Ydb::StatusIds::OVERLOADED, {issue}, ctx, compileRequest.Cookie, std::move(compileRequest.Orbit), std::move(compileRequest.CompileServiceSpan));
+                ReplyError(ev->Sender, "", Ydb::StatusIds::OVERLOADED, {issue}, ctx,
+                    overflow->Cookie, std::move(overflow->Orbit), std::move(overflow->CompileServiceSpan));
                 return;
             }
         } else {
@@ -748,21 +752,23 @@ private:
 
         compileRequest.QueryAst = std::move(queryAst);
 
-        if (!RequestsQueue.Enqueue(std::move(compileRequest))) {
-            Counters->ReportCompileRequestRejected(compileRequest.DbCounters);
+        auto sender = compileRequest.Sender;
+        auto overflow = RequestsQueue.Enqueue(std::move(compileRequest));
+        if (overflow.has_value()) {
+            Counters->ReportCompileRequestRejected(overflow->DbCounters);
 
             LOG_WARN_S(ctx, NKikimrServices::KQP_COMPILE_SERVICE, "Requests queue size limit exceeded"
-                << ", sender: " << compileRequest.Sender
+                << ", sender: " << overflow->Sender
                 << ", queueSize: " << RequestsQueue.Size());
 
             NYql::TIssue issue(NYql::TPosition(), TStringBuilder() <<
                 "Exceeded maximum number of requests in compile service queue.");
-            ReplyError(compileRequest.Sender, "", Ydb::StatusIds::OVERLOADED, {issue}, ctx, compileRequest.Cookie, std::move(compileRequest.Orbit), std::move(compileRequest.CompileServiceSpan));
+            ReplyError(overflow->Sender, "", Ydb::StatusIds::OVERLOADED, {issue}, ctx, overflow->Cookie, std::move(overflow->Orbit), std::move(overflow->CompileServiceSpan));
             return;
         }
 
         LOG_DEBUG_S(ctx, NKikimrServices::KQP_COMPILE_SERVICE, "Added request to queue"
-            << ", sender: " << compileRequest.Sender
+            << ", sender: " << sender
             << ", queueSize: " << RequestsQueue.Size());
 
         ProcessQueue(ctx);
@@ -815,7 +821,7 @@ private:
             false, {}, compileResult->ReplayMessageUserView);
         newCompileResult->AllowCache = compileResult->AllowCache;
         newCompileResult->PreparedQuery = compileResult->PreparedQuery;
-        LOG_DEBUG_S(ctx, NKikimrServices::KQP_COMPILE_SERVICE, "Insert preparing query with params, queryId: " << query.SerializeToString());
+        LOG_DEBUG_S(ctx, NKikimrServices::KQP_COMPILE_SERVICE, "Insert preparing query with params, queryId: " << compileResult->Query->SerializeToString());
         return QueryCache->Insert(newCompileResult, TableServiceConfig.GetEnableAstCache(), isPerStatementExecution);
     }
 

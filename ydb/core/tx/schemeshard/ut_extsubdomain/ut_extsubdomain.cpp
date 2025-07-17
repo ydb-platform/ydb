e84ba4f4c3c82f9b3365705b034786d9b1e9877d
Ivan Chelyubeev <ijon@ydb.tech>
2025-07-17 23:13:17 +0300

    schemeshard: test drop-extsubdomain to be independent from tenant state

--- ydb/core/tx/schemeshard/ut_extsubdomain/ut_extsubdomain.cpp
+++ ydb/core/tx/schemeshard/ut_extsubdomain/ut_extsubdomain.cpp
@@ -1,8 +1,71 @@
 #include <ydb/core/tx/schemeshard/ut_helpers/helpers.h>
 
+#include <ydb/public/lib/value/value.h>
+
 using namespace NKikimr;
 using namespace NSchemeShard;
 using namespace NSchemeShardUT_Private;
+
+
+namespace {
+
+// Stop tablet.
+// Also see ydb/core/mind/hive/hive_ut.cpp, SendStopTablet
+void SendStopTablet(TTestActorRuntime &runtime, ui64 hiveTablet, ui64 tabletId, ui32 nodeIndex) {
+    TActorId senderB = runtime.AllocateEdgeActor(nodeIndex);
+    runtime.SendToPipe(hiveTablet, senderB, new TEvHive::TEvStopTablet(tabletId));
+    TAutoPtr<IEventHandle> handle;
+    auto event = runtime.GrabEdgeEventRethrow<TEvHive::TEvStopTabletResult>(handle);
+    UNIT_ASSERT(event);
+    const auto& stopResult = event->Record;
+    UNIT_ASSERT_EQUAL_C(stopResult.GetTabletID(), tabletId, stopResult.GetTabletID() << " != " << tabletId);
+    UNIT_ASSERT_EQUAL_C(stopResult.GetStatus(), NKikimrProto::OK, (ui32)stopResult.GetStatus() << " != " << (ui32)NKikimrProto::OK);
+}
+
+// Retrieve tablets that belong to the given subdomain
+std::vector<NKikimrHive::TTabletInfo> GetSubdomainTabletsFromHive(TTestActorRuntime &runtime, const ui64 hiveTablet, const TSubDomainKey& subdomainPathId) {
+    TActorId senderA = runtime.AllocateEdgeActor();
+    runtime.SendToPipe(hiveTablet, senderA, new TEvHive::TEvRequestHiveInfo());
+    TAutoPtr<IEventHandle> handle;
+    TEvHive::TEvResponseHiveInfo* response = runtime.GrabEdgeEventRethrow<TEvHive::TEvResponseHiveInfo>(handle);
+
+    std::vector<NKikimrHive::TTabletInfo> result;
+    std::copy_if(response->Record.GetTablets().begin(), response->Record.GetTablets().end(),
+        std::back_inserter(result),
+        [&subdomainPathId] (auto& tablet) {
+            return (tablet.GetObjectDomain().GetSchemeShard() == subdomainPathId.GetSchemeShard()
+                && tablet.GetObjectDomain().GetPathId() == subdomainPathId.GetPathId()
+            );
+        }
+    );
+    return result;
+}
+
+// // Debug
+// NKikimrMiniKQL::TResult ReadLocalTableRecords(TTestActorRuntime& runtime, ui64 tabletId, const TString& tableName, const TString& keyColumn) {
+//     const auto query = Sprintf(
+//         R"(
+//             (
+//                 (let range '('('%s (Null) (Void))))
+//                 (let fields '('%s))
+//                 (return (AsList
+//                     (SetResult 'Result (SelectRange '%s range fields '()))
+//                 ))
+//             )
+//         )",
+//         keyColumn.c_str(),
+//         keyColumn.c_str(),
+//         tableName.c_str()
+//     );
+//     auto result = LocalMiniKQL(runtime, tabletId, query);
+//     // Result: Value { Struct { Optional { Struct { List {
+//     //     Struct { Optional { Uint64: 2 } } }
+//     //     ...
+//     // } } } } }
+//     return result;
+// };
+
+}  // anonymous namespace
 
 Y_UNIT_TEST_SUITE(TSchemeShardExtSubDomainTest) {
     Y_UNIT_TEST(Fake) {
@@ -1476,6 +1539,143 @@ Y_UNIT_TEST_SUITE(TSchemeShardExtSubDomainTest) {
         // env.TestWaitTabletDeletion(runtime, xrange(TTestTxConfig::FakeHiveTablets, TTestTxConfig::FakeHiveTablets + 5));
         UNIT_ASSERT(!CheckLocalRowExists(runtime, TTestTxConfig::SchemeShard, "SubDomains", "PathId", 2));
         UNIT_ASSERT(!CheckLocalRowExists(runtime, TTestTxConfig::SchemeShard, "Paths", "Id", 2));
+    }
+
+    Y_UNIT_TEST_FLAG(DropWithDeadTenantHive, AlterDatabaseCreateHiveFirst) {
+        TTestBasicRuntime runtime;
+        TTestEnv env(runtime, TTestEnvOptions().EnableAlterDatabaseCreateHiveFirst(AlterDatabaseCreateHiveFirst));
+        ui64 txId = 100;
+
+        // EnableAlterDatabaseCreateHiveFirst = false places extsubdomain's system tablets into the root hive control.
+        // EnableAlterDatabaseCreateHiveFirst = true places extsubdomain's tenant hive into the root hive control
+        // and other system tablets into the tenant hive control.
+
+        TestCreateExtSubDomain(runtime, ++txId,  "/MyRoot",
+            R"(Name: "USER_0")"
+        );
+
+        TestAlterExtSubDomain(runtime, ++txId,  "/MyRoot", R"(
+            Name: "USER_0"
+            ExternalSchemeShard: true
+            PlanResolution: 50
+            Coordinators: 1
+            Mediators: 1
+            TimeCastBucketsPerMediator: 2
+            StoragePools {
+                Name: "pool-1"
+                Kind: "hdd"
+            }
+
+            ExternalHive: true
+        )");
+        env.TestWaitNotification(runtime, {txId, txId - 1});
+
+        ui64 tenantHiveId = 0;
+        TSubDomainKey subdomainPathId;
+        {
+            auto describe = DescribePath(runtime, "/MyRoot/USER_0");
+            TestDescribeResult(describe, {
+                NLs::PathExist,
+                NLs::IsExternalSubDomain("USER_0"),
+                NLs::ExtractDomainHive(&tenantHiveId),
+            });
+            subdomainPathId = TSubDomainKey(describe.GetPathDescription().GetDomainDescription().GetDomainKey());
+        }
+
+        // check that there is a new path in the root schemeshard
+        UNIT_ASSERT(CheckLocalRowExists(runtime, TTestTxConfig::SchemeShard, "Paths", "Id", subdomainPathId.GetPathId()));
+        UNIT_ASSERT(CheckLocalRowExists(runtime, TTestTxConfig::SchemeShard, "SubDomains", "PathId", subdomainPathId.GetPathId()));
+
+        // check what extsubdomain's system tablets controls root hive
+        const auto expectedTabletsInRootHive = [&]() {
+            if (AlterDatabaseCreateHiveFirst) {
+                return std::vector<ETabletType::EType>({
+                    ETabletType::Hive,
+                });
+            } else {
+                return std::vector<ETabletType::EType>{{
+                    ETabletType::Hive,
+                    ETabletType::SchemeShard,
+                    ETabletType::Coordinator,
+                    ETabletType::Mediator
+                }};
+            }
+        }();
+        {
+            const auto& expectedTypes = expectedTabletsInRootHive;
+            const auto tablets = GetSubdomainTabletsFromHive(runtime, TTestTxConfig::Hive, subdomainPathId);
+            UNIT_ASSERT_VALUES_EQUAL_C(tablets.size(), expectedTypes.size(), "-- unexpected tablet count in root hive for the tenant");
+            for (const auto& tablet : tablets) {
+                Cerr << "root hive, tablets for subdomain " << TPathId(subdomainPathId.GetSchemeShard(), subdomainPathId.GetPathId()) << ", tablet type " << tablet.GetTabletType() << Endl;
+                auto found = std::find(expectedTypes.begin(), expectedTypes.end(), tablet.GetTabletType());
+                UNIT_ASSERT_C(found != expectedTypes.end(), "-- root hive holds tablet of unexpected type " << tablet.GetTabletType());
+            }
+        }
+
+        // check what extsubdomain's system tablets controls tenant hive
+        const auto expectedTabletsInTenantHive = [&]() {
+            if (AlterDatabaseCreateHiveFirst) {
+                return std::vector<ETabletType::EType>{{
+                    ETabletType::SchemeShard,
+                    ETabletType::Coordinator,
+                    ETabletType::Mediator
+                }};
+            } else {
+                return std::vector<ETabletType::EType>{};
+            }
+        }();
+        {
+            const auto& expectedTypes = expectedTabletsInTenantHive;
+            const auto tablets = GetSubdomainTabletsFromHive(runtime, tenantHiveId, subdomainPathId);
+            UNIT_ASSERT_VALUES_EQUAL_C(tablets.size(), expectedTypes.size(), "-- unexpected tablet count in tenant hive");
+            for (const auto& tablet : tablets) {
+                Cerr << "tenant hive, tablets for subdomain " << TPathId(subdomainPathId.GetSchemeShard(), subdomainPathId.GetPathId()) << ", tablet type " << tablet.GetTabletType() << Endl;
+                auto found = std::find(expectedTypes.begin(), expectedTypes.end(), tablet.GetTabletType());
+                UNIT_ASSERT_C(found != expectedTypes.end(), "-- root hive holds tablet of unexpected type " << tablet.GetTabletType());
+            }
+        }
+
+        // extsubdomain drop should be independent of tenant hive's state.
+        // It must correctly remove database whether tenant nodes and tablets are alive or not.
+        //
+        // Make tenant hive inaccessible by stopping its tablet.
+        // In real life that could be, for example, due to absence of tenant nodes.
+        //
+        // Tenant hive is controlled by the root hive (running at node 0).
+        SendStopTablet(runtime, TTestTxConfig::Hive, tenantHiveId, 0);
+
+        // drop extsubdomain
+        TestForceDropExtSubDomain(runtime, ++txId, "/MyRoot", "USER_0");
+        env.TestWaitNotification(runtime, txId);
+
+        TestDescribeResult(DescribePath(runtime, "/MyRoot/USER_0"), {NLs::PathNotExist});
+
+        TestDescribeResult(DescribePath(runtime, "/MyRoot"), {
+            NLs::PathExist,
+            NLs::PathsInsideDomain(0),
+            NLs::ShardsInsideDomain(0)
+        });
+
+        // check that extsubdomain's system tablets are deleted from the root hive
+        // and not-working state of the tenant hive is unable to hinder that
+        {
+            const auto tablets = GetSubdomainTabletsFromHive(runtime, TTestTxConfig::Hive, subdomainPathId);
+            UNIT_ASSERT_C(tablets.size() == 0, TStringBuilder()
+                << "-- existing subdomain's system tablets in the root hive: expected 0, got " << tablets.size()
+            );
+        }
+
+        // check that extsubdomain's path is really erased from the root schemeshard
+
+        // // Debug
+        // const auto result = ReadLocalTableRecords(runtime, TTestTxConfig::SchemeShard, "SubDomains", "PathId");
+        // const auto records = NKikimr::NClient::TValue::Create(result)[0]["List"];
+        // // Cerr << "TEST: " << records.GetValueText<NKikimr::NClient::TFormatJSON>() << Endl;
+        // // Cerr << "TEST: " << records.DumpToString() << Endl;
+        // UNIT_ASSERT_VALUES_EQUAL(records.Size(), 1);
+
+        UNIT_ASSERT(!CheckLocalRowExists(runtime, TTestTxConfig::SchemeShard, "SubDomains", "PathId", subdomainPathId.GetPathId()));
+        UNIT_ASSERT(!CheckLocalRowExists(runtime, TTestTxConfig::SchemeShard, "Paths", "Id", subdomainPathId.GetPathId()));
     }
 
     Y_UNIT_TEST_FLAG(CreateThenDropChangesParent, AlterDatabaseCreateHiveFirst) {

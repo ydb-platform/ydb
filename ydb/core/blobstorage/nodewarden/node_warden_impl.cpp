307bb2261295727970721ce656ffea2fbf869557
Kamil Khamitov <kkhamitov@yandex-team.ru>
2025-07-20 14:57:16 +0300

    (-) fixes after rebase

--- ydb/core/blobstorage/nodewarden/node_warden_impl.cpp
+++ ydb/core/blobstorage/nodewarden/node_warden_impl.cpp
@@ -366,8 +366,6 @@ void TNodeWarden::Bootstrap() {
         const TIntrusivePtr<NKikimr::TControlBoard>& icb = actorSystem->AppData<TAppData>()->Icb;
 
 
-
-=======
         TControlBoard::RegisterLocalControl(EnablePutBatching, icb->BlobStorage.EnablePutBatching);
         TControlBoard::RegisterLocalControl(EnableVPatch, icb->BlobStorage.EnableVPatch);
         TControlBoard::RegisterSharedControl(EnableLocalSyncLogDataCutting, icb->VDiskControls.EnableLocalSyncLogDataCutting);
@@ -421,7 +419,7 @@ void TNodeWarden::Bootstrap() {
         TControlBoard::RegisterSharedControl(MaxNumOfSlowDisksHDD, icb->DSProxyControls.MaxNumOfSlowDisksHDD);
         TControlBoard::RegisterSharedControl(MaxNumOfSlowDisksSSD, icb->DSProxyControls.MaxNumOfSlowDisksSSD);
 
-        TControlBoard::RegisterSharedControl(EnableDeepScrubbingm, icb->VDiskControls.EnableDeepScrubbing);
+        TControlBoard::RegisterSharedControl(EnableDeepScrubbing, icb->VDiskControls.EnableDeepScrubbing);
 
         TControlBoard::RegisterSharedControl(LongRequestThresholdMs, icb->DSProxyControls.LongRequestThresholdMs);
         TControlBoard::RegisterSharedControl(ReportingControllerBucketSize, icb->DSProxyControls.RequestReportingSettings.BucketSize);

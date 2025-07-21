69af929858a550886c50797efb59a6278b9d1549
Evgeniy Ivanov <i@eivanov.com>
2025-07-21 12:29:59 +0200

    Avoid unhandled exceptions and coredumps in TPC-C

--- ydb/library/workload/tpcc/init.cpp
+++ ydb/library/workload/tpcc/init.cpp
@@ -130,201 +130,213 @@ void InitSync(const NConsoleClient::TClientCommand::TConfig& connectionConfig, c
 
     // Create tables
 
-    TString warehouseSql = std::format(R"(
-        PRAGMA TablePathPrefix("{}");
+    try {
 
-        CREATE TABLE {} (
-            W_ID       Int32          NOT NULL,
-            W_YTD      Double,
-            W_TAX      Double,
-            W_NAME     Utf8,
-            W_STREET_1 Utf8,
-            W_STREET_2 Utf8,
-            W_CITY     Utf8,
-            W_STATE    Utf8,
-            W_ZIP      Utf8,
+        TString warehouseSql = std::format(R"(
+            PRAGMA TablePathPrefix("{}");
 
-            PRIMARY KEY (W_ID)
-        )
-        {};
-    )", runConfig.Path.c_str(), TABLE_WAREHOUSE, smallTablePartitionClause.c_str());
-    CreateTable(client, warehouseSql, TABLE_WAREHOUSE, Log);
+            CREATE TABLE {} (
+                W_ID       Int32          NOT NULL,
+                W_YTD      Double,
+                W_TAX      Double,
+                W_NAME     Utf8,
+                W_STREET_1 Utf8,
+                W_STREET_2 Utf8,
+                W_CITY     Utf8,
+                W_STATE    Utf8,
+                W_ZIP      Utf8,
 
-    TString itemSql = std::format(R"(
-        PRAGMA TablePathPrefix("{}");
+                PRIMARY KEY (W_ID)
+            )
+            {};
+        )", runConfig.Path.c_str(), TABLE_WAREHOUSE, smallTablePartitionClause.c_str());
+        CreateTable(client, warehouseSql, TABLE_WAREHOUSE, Log);
 
-        CREATE TABLE {} (
-            I_ID    Int32           NOT NULL,
-            I_NAME  Utf8,
-            I_PRICE Double,
-            I_DATA  Utf8,
-            I_IM_ID Int32,
+        TString itemSql = std::format(R"(
+            PRAGMA TablePathPrefix("{}");
 
-            PRIMARY KEY (I_ID)
-        )
-        {};
-    )", runConfig.Path.c_str(), TABLE_ITEM, itemPartitionClause.c_str());
-    CreateTable(client, itemSql, TABLE_ITEM, Log);
+            CREATE TABLE {} (
+                I_ID    Int32           NOT NULL,
+                I_NAME  Utf8,
+                I_PRICE Double,
+                I_DATA  Utf8,
+                I_IM_ID Int32,
 
-    TString stockSql = std::format(R"(
-        PRAGMA TablePathPrefix("{}");
+                PRIMARY KEY (I_ID)
+            )
+            {};
+        )", runConfig.Path.c_str(), TABLE_ITEM, itemPartitionClause.c_str());
+        CreateTable(client, itemSql, TABLE_ITEM, Log);
 
-        CREATE TABLE {} (
-            S_W_ID       Int32           NOT NULL,
-            S_I_ID       Int32           NOT NULL,
-            S_QUANTITY   Int32,
-            S_YTD        Double,
-            S_ORDER_CNT  Int32,
-            S_REMOTE_CNT Int32,
-            S_DATA       Utf8,
-            S_DIST_01    Utf8,
-            S_DIST_02    Utf8,
-            S_DIST_03    Utf8,
-            S_DIST_04    Utf8,
-            S_DIST_05    Utf8,
-            S_DIST_06    Utf8,
-            S_DIST_07    Utf8,
-            S_DIST_08    Utf8,
-            S_DIST_09    Utf8,
-            S_DIST_10    Utf8,
+        TString stockSql = std::format(R"(
+            PRAGMA TablePathPrefix("{}");
 
-            PRIMARY KEY (S_W_ID, S_I_ID)
-        )
-        {};
-    )", runConfig.Path.c_str(), TABLE_STOCK, stockPartitionClause.c_str());
-    CreateTable(client, stockSql, TABLE_STOCK, Log);
+            CREATE TABLE {} (
+                S_W_ID       Int32           NOT NULL,
+                S_I_ID       Int32           NOT NULL,
+                S_QUANTITY   Int32,
+                S_YTD        Double,
+                S_ORDER_CNT  Int32,
+                S_REMOTE_CNT Int32,
+                S_DATA       Utf8,
+                S_DIST_01    Utf8,
+                S_DIST_02    Utf8,
+                S_DIST_03    Utf8,
+                S_DIST_04    Utf8,
+                S_DIST_05    Utf8,
+                S_DIST_06    Utf8,
+                S_DIST_07    Utf8,
+                S_DIST_08    Utf8,
+                S_DIST_09    Utf8,
+                S_DIST_10    Utf8,
 
-    TString districtSql = std::format(R"(
-        PRAGMA TablePathPrefix("{}");
+                PRIMARY KEY (S_W_ID, S_I_ID)
+            )
+            {};
+        )", runConfig.Path.c_str(), TABLE_STOCK, stockPartitionClause.c_str());
+        CreateTable(client, stockSql, TABLE_STOCK, Log);
 
-        CREATE TABLE {} (
-            D_W_ID      Int32            NOT NULL,
-            D_ID        Int32            NOT NULL,
-            D_YTD       Double,
-            D_TAX       Double,
-            D_NEXT_O_ID Int32,
-            D_NAME      Utf8,
-            D_STREET_1  Utf8,
-            D_STREET_2  Utf8,
-            D_CITY      Utf8,
-            D_STATE     Utf8,
-            D_ZIP       Utf8,
+        TString districtSql = std::format(R"(
+            PRAGMA TablePathPrefix("{}");
 
-            PRIMARY KEY (D_W_ID, D_ID)
-        )
-        {};
-    )", runConfig.Path.c_str(), TABLE_DISTRICT, smallTablePartitionClause.c_str());
-    CreateTable(client, districtSql, TABLE_DISTRICT, Log);
+            CREATE TABLE {} (
+                D_W_ID      Int32            NOT NULL,
+                D_ID        Int32            NOT NULL,
+                D_YTD       Double,
+                D_TAX       Double,
+                D_NEXT_O_ID Int32,
+                D_NAME      Utf8,
+                D_STREET_1  Utf8,
+                D_STREET_2  Utf8,
+                D_CITY      Utf8,
+                D_STATE     Utf8,
+                D_ZIP       Utf8,
 
-    TString customerSql = std::format(R"(
-        PRAGMA TablePathPrefix("{}");
+                PRIMARY KEY (D_W_ID, D_ID)
+            )
+            {};
+        )", runConfig.Path.c_str(), TABLE_DISTRICT, smallTablePartitionClause.c_str());
+        CreateTable(client, districtSql, TABLE_DISTRICT, Log);
 
-        CREATE TABLE {} (
-            C_W_ID         Int32            NOT NULL,
-            C_D_ID         Int32            NOT NULL,
-            C_ID           Int32            NOT NULL,
-            C_DISCOUNT     Double,
-            C_CREDIT       Utf8,
-            C_LAST         Utf8,
-            C_FIRST        Utf8,
-            C_CREDIT_LIM   Double,
-            C_BALANCE      Double,
-            C_YTD_PAYMENT  Double,
-            C_PAYMENT_CNT  Int32,
-            C_DELIVERY_CNT Int32,
-            C_STREET_1     Utf8,
-            C_STREET_2     Utf8,
-            C_CITY         Utf8,
-            C_STATE        Utf8,
-            C_ZIP          Utf8,
-            C_PHONE        Utf8,
-            C_SINCE        Timestamp,
-            C_MIDDLE       Utf8,
-            C_DATA         Utf8,
+        TString customerSql = std::format(R"(
+            PRAGMA TablePathPrefix("{}");
 
-            PRIMARY KEY (C_W_ID, C_D_ID, C_ID)
-        )
-        {};
-    )", runConfig.Path.c_str(), TABLE_CUSTOMER, customerPartitionClause.c_str());
-    CreateTable(client, customerSql, TABLE_CUSTOMER, Log);
+            CREATE TABLE {} (
+                C_W_ID         Int32            NOT NULL,
+                C_D_ID         Int32            NOT NULL,
+                C_ID           Int32            NOT NULL,
+                C_DISCOUNT     Double,
+                C_CREDIT       Utf8,
+                C_LAST         Utf8,
+                C_FIRST        Utf8,
+                C_CREDIT_LIM   Double,
+                C_BALANCE      Double,
+                C_YTD_PAYMENT  Double,
+                C_PAYMENT_CNT  Int32,
+                C_DELIVERY_CNT Int32,
+                C_STREET_1     Utf8,
+                C_STREET_2     Utf8,
+                C_CITY         Utf8,
+                C_STATE        Utf8,
+                C_ZIP          Utf8,
+                C_PHONE        Utf8,
+                C_SINCE        Timestamp,
+                C_MIDDLE       Utf8,
+                C_DATA         Utf8,
 
-    TString historySql = std::format(R"(
-        PRAGMA TablePathPrefix("{}");
+                PRIMARY KEY (C_W_ID, C_D_ID, C_ID)
+            )
+            {};
+        )", runConfig.Path.c_str(), TABLE_CUSTOMER, customerPartitionClause.c_str());
+        CreateTable(client, customerSql, TABLE_CUSTOMER, Log);
 
-        CREATE TABLE {} (
-            H_C_W_ID    Int32,
-            H_C_ID      Int32,
-            H_C_D_ID    Int32,
-            H_D_ID      Int32,
-            H_W_ID      Int32,
-            H_DATE      Timestamp,
-            H_AMOUNT    Double,
-            H_DATA      Utf8,
-            H_C_NANO_TS Int64        NOT NULL,
+        TString historySql = std::format(R"(
+            PRAGMA TablePathPrefix("{}");
 
-            PRIMARY KEY (H_C_W_ID, H_C_NANO_TS)
-        )
-        {};
-    )", runConfig.Path.c_str(), TABLE_HISTORY, historyPartitionClause.c_str());
-    CreateTable(client, historySql, TABLE_HISTORY, Log);
+            CREATE TABLE {} (
+                H_C_W_ID    Int32,
+                H_C_ID      Int32,
+                H_C_D_ID    Int32,
+                H_D_ID      Int32,
+                H_W_ID      Int32,
+                H_DATE      Timestamp,
+                H_AMOUNT    Double,
+                H_DATA      Utf8,
+                H_C_NANO_TS Int64        NOT NULL,
 
-    TString oorderSql = std::format(R"(
-        PRAGMA TablePathPrefix("{}");
+                PRIMARY KEY (H_C_W_ID, H_C_NANO_TS)
+            )
+            {};
+        )", runConfig.Path.c_str(), TABLE_HISTORY, historyPartitionClause.c_str());
+        CreateTable(client, historySql, TABLE_HISTORY, Log);
 
-        CREATE TABLE {} (
-            O_W_ID       Int32       NOT NULL,
-            O_D_ID       Int32       NOT NULL,
-            O_ID         Int32       NOT NULL,
-            O_C_ID       Int32,
-            O_CARRIER_ID Int32,
-            O_OL_CNT     Int32,
-            O_ALL_LOCAL  Int32,
-            O_ENTRY_D    Timestamp,
+        TString oorderSql = std::format(R"(
+            PRAGMA TablePathPrefix("{}");
 
-            PRIMARY KEY (O_W_ID, O_D_ID, O_ID)
-        )
-        {};
-    )", runConfig.Path.c_str(), TABLE_OORDER, oorderPartitionClause.c_str());
-    CreateTable(client, oorderSql, TABLE_OORDER, Log);
+            CREATE TABLE {} (
+                O_W_ID       Int32       NOT NULL,
+                O_D_ID       Int32       NOT NULL,
+                O_ID         Int32       NOT NULL,
+                O_C_ID       Int32,
+                O_CARRIER_ID Int32,
+                O_OL_CNT     Int32,
+                O_ALL_LOCAL  Int32,
+                O_ENTRY_D    Timestamp,
 
-    TString newOrderSql = std::format(R"(
-        PRAGMA TablePathPrefix("{}");
+                PRIMARY KEY (O_W_ID, O_D_ID, O_ID)
+            )
+            {};
+        )", runConfig.Path.c_str(), TABLE_OORDER, oorderPartitionClause.c_str());
+        CreateTable(client, oorderSql, TABLE_OORDER, Log);
 
-        CREATE TABLE {} (
-            NO_W_ID Int32 NOT NULL,
-            NO_D_ID Int32 NOT NULL,
-            NO_O_ID Int32 NOT NULL,
+        TString newOrderSql = std::format(R"(
+            PRAGMA TablePathPrefix("{}");
 
-            PRIMARY KEY (NO_W_ID, NO_D_ID, NO_O_ID)
-        )
-        {};
-    )", runConfig.Path.c_str(), TABLE_NEW_ORDER, smallTablePartitionClause.c_str());
-    CreateTable(client, newOrderSql, TABLE_NEW_ORDER, Log);
+            CREATE TABLE {} (
+                NO_W_ID Int32 NOT NULL,
+                NO_D_ID Int32 NOT NULL,
+                NO_O_ID Int32 NOT NULL,
 
-    TString orderLineSql = std::format(R"(
-        PRAGMA TablePathPrefix("{}");
+                PRIMARY KEY (NO_W_ID, NO_D_ID, NO_O_ID)
+            )
+            {};
+        )", runConfig.Path.c_str(), TABLE_NEW_ORDER, smallTablePartitionClause.c_str());
+        CreateTable(client, newOrderSql, TABLE_NEW_ORDER, Log);
 
-        CREATE TABLE {} (
-            OL_W_ID        Int32           NOT NULL,
-            OL_D_ID        Int32           NOT NULL,
-            OL_O_ID        Int32           NOT NULL,
-            OL_NUMBER      Int32           NOT NULL,
-            OL_I_ID        Int32,
-            OL_DELIVERY_D  Timestamp,
-            OL_AMOUNT      Double,
-            OL_SUPPLY_W_ID Int32,
-            OL_QUANTITY    Double,
-            OL_DIST_INFO   Utf8,
+        TString orderLineSql = std::format(R"(
+            PRAGMA TablePathPrefix("{}");
 
-            PRIMARY KEY (OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER)
-        )
-        {};
-    )", runConfig.Path.c_str(), TABLE_ORDER_LINE, orderLinePartitionClause.c_str());
-    CreateTable(client, orderLineSql, TABLE_ORDER_LINE, Log);
+            CREATE TABLE {} (
+                OL_W_ID        Int32           NOT NULL,
+                OL_D_ID        Int32           NOT NULL,
+                OL_O_ID        Int32           NOT NULL,
+                OL_NUMBER      Int32           NOT NULL,
+                OL_I_ID        Int32,
+                OL_DELIVERY_D  Timestamp,
+                OL_AMOUNT      Double,
+                OL_SUPPLY_W_ID Int32,
+                OL_QUANTITY    Double,
+                OL_DIST_INFO   Utf8,
 
-    driver.Stop(true);
+                PRIMARY KEY (OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER)
+            )
+            {};
+        )", runConfig.Path.c_str(), TABLE_ORDER_LINE, orderLinePartitionClause.c_str());
+        CreateTable(client, orderLineSql, TABLE_ORDER_LINE, Log);
 
-    LOG_I("All TPC-C tables created successfully");
+        Cout << "All TPC-C tables created successfully" << Endl;
+
+        driver.Stop(true);
+
+    } catch (const std::exception& e) {
+        Cout << "Failed to create TPC-C tables. " << e.what() << Endl;
+        Cout << "After fixing the reason, you might need to run `tpcc clean first`" << Endl;
+
+        // we want to stop it before calling exit to avoid crash
+        driver.Stop(true);
+
+        std::exit(1);
+    }
 }
 
 } // namespace NYdb::NTPCC

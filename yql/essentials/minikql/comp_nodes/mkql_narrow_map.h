55393d5f603888afdfcd795c4170b2e2e391a235
atarasov5 <atarasov5@yandex-team.com>
2025-07-23 11:30:29 +0300

    YQL-20229: [refactoring] split nodes in separate files
    Zero diff, поразносил по файлам
    commit_hash:f668f5b8e6b40f977a15ea1916ff044972459f15

--- yql/essentials/minikql/comp_nodes/mkql_narrow_map.h
+++ yql/essentials/minikql/comp_nodes/mkql_narrow_map.h
@@ -1,8 +1,9 @@
 #pragma once
+
 #include <yql/essentials/minikql/computation/mkql_computation_node.h>
 
 namespace NKikimr::NMiniKQL {
 
-IComputationNode* WrapWideMap(TCallable& callable, const TComputationNodeFactoryContext& ctx);
+IComputationNode* WrapNarrowMap(TCallable& callable, const TComputationNodeFactoryContext& ctx);
 
 }

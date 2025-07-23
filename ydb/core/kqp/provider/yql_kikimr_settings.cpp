bad8d4f36f29ee3248eace0d4651879b319b944f
pilik <pudge1000-7@ydb.tech>
2025-07-23 15:35:27 +0300

    [RBO] Aggregation shuffle elimination (#20058)
    Co-authored-by: Pavel Ivanov <pudge1000-7@qavm-9f0570a4.qemu>

--- ydb/core/kqp/provider/yql_kikimr_settings.cpp
+++ ydb/core/kqp/provider/yql_kikimr_settings.cpp
@@ -90,6 +90,7 @@ TKikimrConfiguration::TKikimrConfiguration() {
     REGISTER_SETTING(*this, OptimizerHints).Parser([](const TString& v) { return NYql::TOptimizerHints::Parse(v); });
     REGISTER_SETTING(*this, OptShuffleElimination);
     REGISTER_SETTING(*this, OptShuffleEliminationWithMap);
+    REGISTER_SETTING(*this, OptShuffleEliminationForAggregation);
     REGISTER_SETTING(*this, OverridePlanner);
     REGISTER_SETTING(*this, UseGraceJoinCoreForMap);
     REGISTER_SETTING(*this, UseBlockHashJoin);

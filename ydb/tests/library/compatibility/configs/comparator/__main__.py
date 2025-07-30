e7ef826784d869cef07debd99fd4598f81bf2cfa
Олег <150132506+iddqdex@users.noreply.github.com>
2025-07-30 15:57:35 +0300

    Add description to config comparation (#21887)

--- ydb/tests/library/compatibility/configs/comparator/__main__.py
+++ ydb/tests/library/compatibility/configs/comparator/__main__.py
@@ -27,14 +27,18 @@ class Differ:
     def __init__(self):
         self.fields: list[tuple[str, list[FieldInfo]]] = []
         self.resolutions: list[tuple[str, list[tuple[Resolution, str]]]] = []
-        self.names: list[str] = []
+        self.branches: list[dict[str, str]] = []
 
     def load_files(self, names: list[str]):
         configs = []
         for name in names:
             with open(name) as f:
-                configs.append(json.load(f).get('proto'))
-            self.names.append(os.path.basename(name))
+                content = json.load(f)
+                configs.append(content.get('proto'))
+                self.branches.append({
+                    'branch_name' : content.get('branch', os.path.basename(name)),
+                    'commit': content.get('commit')
+                })
         self._add_fields_dict(configs, [])
 
     def _add_fields_dict(self, fields: list[dict], path: list[str]):
@@ -109,10 +113,117 @@ class Differ:
                 self.resolutions.append((name, result))
 
     def print_result(self) -> None:
-        print('<html><body><table border=1 valign="center">')
-        print('<thead style="position: sticky; top: 0; background: white; align: center"><tr><th style="padding-left: 10; padding-right: 10">field</th>')
-        for name in self.names:
-            print(f'<th style="padding-left: 10; padding-right: 10">{name}</th>')
+        print('''<style>
+.tab {
+    overflow: hidden;
+    border: 1px solid #4CAF50;
+    background-color: #AAFFAA;
+}
+
+
+.tab button {
+    background-color: inherit;
+    float: left;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 14px 16px;
+    transition: 0.3s;
+}
+
+
+.tab button:hover {
+    background-color: #FFEB3B;
+}
+
+
+.tab button.active {
+    background-color: #4CAF50;
+  color: #fff;
+}
+
+
+.tabcontent {
+    display: none;
+    padding: 6px 12px;
+    border: 1px solid #4CAF50;
+    border-top: none;
+}
+</style>
+<script>
+function openDescr(evt, lang) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    activeid = ""
+    for (i = 0; i < tabcontent.length; i++) {
+        if (tabcontent[i].style.display != "none") {
+            activeid = tabcontent[i].id
+        }
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    if (lang == activeid) {
+        return;
+    }
+    document.getElementById(lang).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+</script>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>Сравнение дефолтных конфигураций ydbd</title>
+</head>
+<body>
+<div class="tab">
+  <button class="tablinks" onclick="openDescr(event, 'russian')">Описание</button>
+  <button class="tablinks" onclick="openDescr(event, 'english')">Description</button>
+</div>
+<div id=russian class="tabcontent">
+<h1>Сравнение дефолтных конфигураций ydbd разных версий</h1>
+В таблице ниже представлены изменения дефолтных конфигураций для разных версий ydbd.
+Сделано на основе описания конфигураций в <a href="https://github.com/ydb-platform/ydb/blob/main/ydb/core/protos/config.proto">протобуфе</a>.
+<p>
+Цвет означает критичность изменений в соседних ветках:
+<ul>
+<li><span style="font-weight:bold">Белый</span> означает, что поле полностью отсутствует в данной версии конфигурации.</li>
+<li><span style="background-color: #aaffaa;font-weight:bold">Зеленый</span> - безопасное изменение, такое как добавление нового поля или включение Feature flag.</li>
+<li><span style="background-color: #ffffaa;font-weight:bold">Желтый</span> - изменение, которое может вызвать изменение поведения,
+но не должно быть критичным, например изменение значения по умолчанию.</li>
+<li><span style="background-color: #ffaaaa;font-weight:bold">Красный</span> - опасное изменение,
+которое может все сломать: удаление полей, изменение их типа или id (в протобуфе), выключение Feature flag итд.</li>
+</ul>
+</p>
+</div>
+<div id=english class="tabcontent">
+<h1>Comparison of ydbd default configurations of different versions</h1>
+The table above shows the changes in default configurations for different versions of ydbd.
+Based on the description of configurations in the <a href="https://github.com/ydb-platform/ydb/blob/main/ydb/core/protos/config.proto">protobuf</a>.
+<p>
+The color indicates the criticality of the changes in the neighboring branches:
+<ul>
+<li><span style="font-weight:bold">White</span> means that the field is completely absent in this version of the configuration.</li>
+<li><span style="background-color: #aaffaa;font-weight:bold">Green</span> - a safe change, such as adding a new field or enabling the Feature flag.</li>
+<li><span style="background-color: #ffffaa;font-weight:bold">Yellow</span> - a change that may cause a change in behavior, but should not be critical, such as changing the default value.</li>
+<li><span style="background-color: #ffaaaa;font-weight:bold">Red</span> - A danger change that can break everything:
+removing fields, changing their type or id (in protobuf), disabling the Feature flag etc.</li>
+</ul>
+</p>
+</div>
+<table border=1 valign="center">''')
+        print('<thead style="position: sticky; top: 0; background: white; align: center">')
+        print('<tr><th style="padding-left: 10; padding-right: 10">config field \\ branch, commit</th>')
+        for b in self.branches:
+            br_text = b['branch_name']
+            c = b.get('commit')
+            if c:
+                br_text += f'<br/><a href="https://github.com/ydb-platform/ydb/commit/{c}">{c[0:7]}</a>'
+            print(f'<th style="padding-left: 10; padding-right: 10">{br_text}</th>')
         print('</tr></thead>')
         print('<tbody>')
         for field, result in self.resolutions:

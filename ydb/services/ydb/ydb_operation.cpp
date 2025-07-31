81a12bfde4d1f1170e15b12cbce5ac808e3e1a3d
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-07-31 22:19:59 +0300

    Support audit log class settings in grpc audit logging (#21920)
    (cherry picked from commit 797d7e6e95bb5913e6e4d8804bcf8678e7897d68)

--- ydb/services/ydb/ydb_operation.cpp
+++ ydb/services/ydb/ydb_operation.cpp
@@ -15,21 +15,21 @@ void TGRpcOperationService::SetupIncomingRequests(NYdbGrpc::TLoggerPtr logger) {
 #ifdef ADD_REQUEST
 #error ADD_REQUEST macro already defined
 #endif
-#define ADD_REQUEST(NAME, CB, TCALL)                                                                              \
-    MakeIntrusive<TGRpcRequest<Operations::NAME##Request, Operations::NAME##Response, TGRpcOperationService>>     \
-        (this, &Service_, CQ_,                                                                                    \
-            [this](NYdbGrpc::IRequestContextBase *ctx) {                                                             \
-                NGRpcService::ReportGrpcReqToMon(*ActorSystem_, ctx->GetPeer());                                  \
-                ActorSystem_->Send(GRpcRequestProxyId_,                                                           \
-                    new TCALL<Operations::NAME##Request, Operations::NAME##Response>                              \
-                        (ctx, &CB, TRequestAuxSettings{RLSWITCH(TRateLimiterMode::Rps), nullptr}));                         \
-            }, &Operation::V1::OperationService::AsyncService::Request ## NAME,                                   \
+#define ADD_REQUEST(NAME, CB, TCALL, AUDIT_MODE)                                                                \
+    MakeIntrusive<TGRpcRequest<Operations::NAME##Request, Operations::NAME##Response, TGRpcOperationService>>   \
+        (this, &Service_, CQ_,                                                                                  \
+            [this](NYdbGrpc::IRequestContextBase *ctx) {                                                        \
+                NGRpcService::ReportGrpcReqToMon(*ActorSystem_, ctx->GetPeer());                                \
+                ActorSystem_->Send(GRpcRequestProxyId_,                                                         \
+                    new TCALL<Operations::NAME##Request, Operations::NAME##Response>                            \
+                        (ctx, &CB, TRequestAuxSettings{RLSWITCH(TRateLimiterMode::Rps), nullptr, AUDIT_MODE})); \
+            }, &Operation::V1::OperationService::AsyncService::Request ## NAME,                                 \
             #NAME, logger, getCounterBlock("operation", #NAME))->Run();
 
-    ADD_REQUEST(GetOperation, DoGetOperationRequest, TGrpcRequestOperationCall)
-    ADD_REQUEST(CancelOperation, DoCancelOperationRequest, TGrpcRequestNoOperationCall)
-    ADD_REQUEST(ForgetOperation, DoForgetOperationRequest, TGrpcRequestNoOperationCall)
-    ADD_REQUEST(ListOperations, DoListOperationsRequest, TGrpcRequestNoOperationCall)
+    ADD_REQUEST(GetOperation, DoGetOperationRequest, TGrpcRequestOperationCall, TAuditMode::NonModifying())
+    ADD_REQUEST(CancelOperation, DoCancelOperationRequest, TGrpcRequestNoOperationCall, TAuditMode::Modifying(TAuditMode::TLogClassConfig::Operations))
+    ADD_REQUEST(ForgetOperation, DoForgetOperationRequest, TGrpcRequestNoOperationCall, TAuditMode::Modifying(TAuditMode::TLogClassConfig::Operations))
+    ADD_REQUEST(ListOperations, DoListOperationsRequest, TGrpcRequestNoOperationCall, TAuditMode::NonModifying())
 
 #undef ADD_REQUEST
 }

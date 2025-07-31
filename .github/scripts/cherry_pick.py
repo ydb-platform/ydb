e61a4de4ff8d9a1bf71881b652662995e1a1ad79
Oleg Geller <iddqd@ydb.tech>
2025-07-31 08:11:39 +0300

    Simplify cherry-pick input

--- .github/scripts/cherry_pick.py
+++ .github/scripts/cherry_pick.py
@@ -9,7 +9,7 @@ from github import Github, GithubException, GithubObject, Commit
 
 class CherryPickCreator:
     def __init__(self, args):
-        def __split(s: str, seps: str = ', '):
+        def __split(s: str, seps: str = ', \n'):
             if not s:
                 return []
             if not seps:
@@ -19,6 +19,18 @@ class CherryPickCreator:
                 result += __split(part, seps[1:])
             return result
 
+        def __add_commit(c: str):
+            commit = self.repo.get_commit(c)
+            self.pr_title_list.append(commit.sha)
+            self.pr_body_list.append(commit.html_url)
+            self.commit_shas.append(commit.sha)
+
+        def __add_pull(p: int):
+            pull = self.repo.get_pull(p)
+            self.pr_title_list.append(f'PR {pull.number}')
+            self.pr_body_list.append(pull.html_url)
+            self.commit_shas.append(pull.merge_commit_sha)
+
         self.repo_name = os.environ["REPO"]
         self.target_branches = __split(args.target_branches)
         self.token = os.environ["TOKEN"]
@@ -27,29 +39,22 @@ class CherryPickCreator:
         self.commit_shas: list[str] = []
         self.pr_title_list: list[str] = []
         self.pr_body_list: list[str] = []
-        for c in __split(args.commits):
-            commit = self.repo.get_commit(c)
-            self.pr_title_list.append(commit.sha)
-            self.pr_body_list.append(commit.html_url)
-            self.commit_shas.append(commit.sha)
-        for p in __split(args.pulls):
-            pull = self.repo.get_pull(int(p))
-            self.pr_title_list.append(f'PR {pull.number}')
-            self.pr_body_list.append(pull.html_url)
-            self.commit_shas.append(pull.merge_commit_sha)
+        for w in __split(args.what):
+            id = w.split('/')[-1]
+            try:
+                __add_pull(int(id))
+            except ValueError:
+                __add_commit(id)
+
         self.dtm = datetime.datetime.now().strftime("%y%m%d-%H%M")
         self.logger = logging.getLogger("cherry-pick")
-        self.workflow_url = None
-        self.__detect_env()
-
-    def __detect_env(self):
-        if "GITHUB_RUN_ID" in os.environ:
-            self.workflow_url = (
-                f"{os.environ['GITHUB_SERVER_URL']}/{self.repo_name}/actions/runs/{os.environ['GITHUB_RUN_ID']}"
-            )
+        try:
+            self.workflow_url = self.repo.get_workflow_run(os.getenv('GITHUB_RUN_ID', 0)).html_url
+        except:
+            self.workflow_url = None
 
     def add_summary(self, msg):
-        logging.info(msg)
+        self.logger.info(msg)
         summary_path = os.getenv('GITHUB_STEP_SUMMARY')
         if summary_path:
             with open(summary_path, 'a') as summary:
@@ -89,10 +94,13 @@ class CherryPickCreator:
         self.add_summary(f'{target_branch}: PR {pr.html_url} created\n')
 
     def process(self):
+        self.add_summary(f"Cherry-pick {', '.join(self.pr_body_list)} to {', '.join(self.target_branches)}")
         if len(self.commit_shas) == 0 or len(self.target_branches) == 0:
             self.add_summary("Noting to cherry-pick or no targets branches, my life is meaningless.")
             return
-        self.git_run("clone", f"https://{self.token}@github.com/{self.repo_name}.git", "-c", "protocol.version=2", f"ydb-new-pr")
+        self.git_run(
+            "clone", f"https://{self.token}@github.com/{self.repo_name}.git", "-c", "protocol.version=2", f"ydb-new-pr"
+        )
         os.chdir(f"ydb-new-pr")
         for target in self.target_branches:
             try:
@@ -105,9 +113,13 @@ class CherryPickCreator:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--commits", help="Comma or space separated list of commit SHAs")
-    parser.add_argument("--pulls", help="Comma or space separated list of PR numbers")
-    parser.add_argument("--target-branches", help="Comma or space separated list of branchs to cherry-pick")
+    parser.add_argument(
+        "--what",
+        help="List of commits to cherry-pick. Can be represented as full or short commit SHA, PR number or URL to commit or PR. Separated by space, comma or line end.",
+    )
+    parser.add_argument(
+        "--target-branches", help="List of branchs to cherry-pick. Separated by space, comma or line end."
+    )
     args = parser.parse_args()
 
     log_fmt = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"

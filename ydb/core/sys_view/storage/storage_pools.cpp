f68a6545339c62cf1b49d8d8103061716631bf94
Yaroslav Dynnikov <ydynnikov@ydb.tech>
2025-07-31 17:45:39 +0300

    Fixup and test new sysview fields

--- ydb/core/sys_view/storage/storage_pools.cpp
+++ ydb/core/sys_view/storage/storage_pools.cpp
@@ -35,6 +35,7 @@ public:
             {T::EncryptionMode::ColumnId, {E::kInfoFieldNumber, V::kEncryptionModeFieldNumber}},
             {T::SchemeshardId::ColumnId, {E::kInfoFieldNumber, V::kSchemeshardIdFieldNumber}},
             {T::PathId::ColumnId, {E::kInfoFieldNumber, V::kPathIdFieldNumber}},
+            {T::DefaultGroupSizeInUnits::ColumnId, {E::kInfoFieldNumber, V::kDefaultGroupSizeInUnitsFieldNumber}},
         };
         return fieldMap;
     }

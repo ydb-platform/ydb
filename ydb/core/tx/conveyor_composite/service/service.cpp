ydb-platform/ydb

commit 3849a7e5cde6fbbb1a61a417b485b2c6c8dd3513

correct cpu conveyor (#19838)

ydb/core/tx/conveyor_composite/service/service.cpp
@@ -23,13 +23,6 @@ void TDistributor::Bootstrap() {
     TBase::Schedule(TDuration::Seconds(1), new NActors::TEvents::TEvWakeup(1));
 }
 
-void TDistributor::HandleMain(NActors::TEvents::TEvWakeup::TPtr& evExt) {
-    if (evExt->Get()->Tag == 1) {
-        Manager->DoQuant(TMonotonic::Now());
-        TBase::Schedule(TDuration::Seconds(1), new NActors::TEvents::TEvWakeup(1));
-    }
-}
-
 void TDistributor::HandleMain(TEvInternal::TEvTaskProcessedResult::TPtr& evExt) {
     auto& ev = *evExt->Get();
     const TDuration backSendDuration = (TMonotonic::Now() - ev.GetConstructInstant());

ydb/core/tx/columnshard/columnshard_impl.cpp
@@ -572,6 +572,28 @@ private:
         }
         TActorContext::AsActorContext().Send(ParentActorId, std::move(ev));
     }
+
+    virtual ui64 GetNecessaryDataMemory(const std::shared_ptr<NOlap::NReader::NCommon::TColumnsSetIds>& columnIds,
+        const std::vector<NOlap::TPortionDataAccessor>& acc) const override {
+        AFL_VERIFY(!columnIds);
+        THashMap<ui32, ui64> memoryByColumns;
+        for (auto&& a : acc) {
+            THashMap<ui32, ui64> memoryByPortionColumns;
+            for (auto&& c : a.GetRecordsVerified()) {
+                const ui64 current = memoryByPortionColumns[c.GetEntityId()];
+                memoryByPortionColumns[c.GetEntityId()] = std::max<ui64>(current, c.GetMeta().GetRawBytes());
+            }
+            for (auto&& c : memoryByPortionColumns) {
+                memoryByColumns[c.first] += c.second;
+            }
+        }
+        ui64 max = 0;
+        for (auto&& c : memoryByColumns) {
+            max = std::max(max, c.second);
+        }
+        return max;
+    }
+
     virtual void DoOnError(const TString& errorMessage) override {
         auto ev = std::make_unique<TEvPrivate::TEvWriteIndex>(Changes, false);
         ev->SetPutStatus(NKikimrProto::ERROR);

ydb/core/tx/columnshard/data_reader/contexts.h
@@ -2,6 +2,7 @@
 #include <ydb/core/tx/columnshard/blobs_reader/task.h>
 #include <ydb/core/tx/columnshard/data_accessor/manager.h>
 #include <ydb/core/tx/columnshard/engines/portions/data_accessor.h>
+#include <ydb/core/tx/columnshard/engines/reader/common_reader/iterator/columns_set.h>
 #include <ydb/core/tx/limiter/grouped_memory/usage/abstract.h>
 #include <ydb/core/tx/limiter/grouped_memory/usage/service.h>
 
@@ -68,7 +69,7 @@ public:
     TCurrentContext() {
         static std::shared_ptr<NGroupedMemoryManager::TStageFeatures> stageFeatures =
             NGroupedMemoryManager::TCompMemoryLimiterOperator::BuildStageFeatures("DEFAULT", 1000000000);
-        
+
         MemoryProcessGuard = NGroupedMemoryManager::TCompMemoryLimiterOperator::BuildProcessGuard(MemoryProcessId, { stageFeatures });
         MemoryProcessScopeGuard = NGroupedMemoryManager::TCompMemoryLimiterOperator::BuildScopeGuard(MemoryProcessId, 1);
         MemoryProcessGroupGuard = NGroupedMemoryManager::TCompMemoryLimiterOperator::BuildGroupGuard(MemoryProcessId, 1);
@@ -105,6 +106,19 @@ private:
 public:
     virtual ~IFetchCallback() = default;
 
+    virtual ui64 GetNecessaryDataMemory(
+        const std::shared_ptr<NReader::NCommon::TColumnsSetIds>& columnIds, const std::vector<TPortionDataAccessor>& acc) const {
+        ui64 memory = 0;
+        for (auto&& a : acc) {
+            if (columnIds) {
+                memory += a.GetColumnBlobBytes(columnIds->GetColumnIds());
+            } else {
+                memory += a.GetPortionInfo().GetTotalBlobBytes();
+            }
+        }
+        return memory;
+    }
+
     virtual std::optional<ui64> GetMemoryForUsage() const {
         return std::nullopt;
     }
@@ -113,7 +127,6 @@ public:
     virtual TString GetClassName() const = 0;
 
     virtual void OnStageStarting(const EFetchingStage /*stage*/) {
-    
     }
 
     void OnFinished(TCurrentContext&& context) {

ydb/core/tx/columnshard/data_reader/fetcher.cpp
@@ -10,11 +10,7 @@ void TPortionsDataFetcher::StartColumnsFetching(TRequestInput&& input, std::shar
         std::vector<std::shared_ptr<IFetchingStep>> steps;
         steps.emplace_back(std::make_shared<TAskAccessorResourcesStep>());
         steps.emplace_back(std::make_shared<TAskAccessorsStep>());
-        if (auto mem = callback->GetMemoryForUsage()) {
-            steps.emplace_back(std::make_shared<TAskGeneralResourceStep>(entityIds, *mem));
-        } else {
-            steps.emplace_back(std::make_shared<TAskDataResourceStep>(entityIds));
-        }
+        steps.emplace_back(std::make_shared<TAskDataResourceStep>(entityIds));
         steps.emplace_back(std::make_shared<TAskDataStep>(entityIds));
         return std::make_shared<TScript>(std::move(steps), "PARTIAL_PORTIONS_FETCHING::" + ::ToString(input.GetConsumer()));
     }();
@@ -28,11 +24,7 @@ void TPortionsDataFetcher::StartFullPortionsFetching(TRequestInput&& input, std:
         std::vector<std::shared_ptr<IFetchingStep>> steps;
         steps.emplace_back(std::make_shared<TAskAccessorResourcesStep>());
         steps.emplace_back(std::make_shared<TAskAccessorsStep>());
-        if (auto mem = callback->GetMemoryForUsage()) {
-            steps.emplace_back(std::make_shared<TAskGeneralResourceStep>(nullptr, *mem));
-        } else {
-            steps.emplace_back(std::make_shared<TAskDataResourceStep>(nullptr));
-        }
+        steps.emplace_back(std::make_shared<TAskDataResourceStep>(nullptr));
         steps.emplace_back(std::make_shared<TAskDataStep>(nullptr));
         return std::make_shared<TScript>(std::move(steps), "FULL_PORTIONS_FETCHING::" + ::ToString(input.GetConsumer()));
     }();

ydb/core/tx/columnshard/data_reader/fetcher.h
@@ -68,6 +68,11 @@ public:
             CurrentContext.GetMemoryProcessId(), CurrentContext.GetMemoryScopeId(), CurrentContext.GetMemoryGroupId(), { task }, 0);
     }
 
+    ui64 GetNecessaryDataMemory(
+        const std::shared_ptr<NReader::NCommon::TColumnsSetIds>& columnIds, const std::vector<TPortionDataAccessor>& acc) const {
+        return Callback->GetNecessaryDataMemory(columnIds, acc);
+    }
+
     TPortionsDataFetcher(TRequestInput&& input, std::shared_ptr<IFetchCallback>&& callback, const std::shared_ptr<TEnvironment>& environment,
         const std::shared_ptr<TScript>& script, const NConveyorComposite::ESpecialTaskCategory conveyorCategory)
         : Input(std::move(input))

ydb/core/tx/columnshard/data_reader/fetching_steps.h
@@ -131,14 +131,7 @@ private:
     virtual IFetchingStep::EStepResult DoExecute(const std::shared_ptr<TPortionsDataFetcher>& fetchingContext) const override {
         fetchingContext->SetStage(EFetchingStage::AskDataResources);
         const std::vector<TPortionDataAccessor>& accessors = fetchingContext->GetCurrentContext().GetPortionAccessors();
-        ui64 memory = 0;
-        for (auto&& i : accessors) {
-            if (ColumnIds) {
-                memory += i.GetColumnBlobBytes(ColumnIds->GetColumnIds());
-            } else {
-                memory += i.GetPortionInfo().GetTotalBlobBytes();
-            }
-        }
+        const ui64 memory = fetchingContext->GetNecessaryDataMemory(ColumnIds, accessors);
         if (!memory) {
             return IFetchingStep::EStepResult::Continue;
         }

ydb/core/tx/conveyor_composite/service/category.cpp
@@ -3,37 +3,35 @@
 namespace NKikimr::NConveyorComposite {
 
 bool TProcessCategory::HasTasks() const {
-    return ProcessesWithTasks.size();
-}
-
-void TProcessCategory::DoQuant(const TMonotonic newStart) {
-    CPUUsage->Cut(newStart);
-    for (auto&& i : Processes) {
-        i.second->DoQuant(newStart);
-    }
+    return WeightedProcesses.size();
 }
 
 std::optional<TWorkerTask> TProcessCategory::ExtractTaskWithPrediction(const std::shared_ptr<TWPCategorySignals>& counters, THashSet<TString>& scopeIds) {
     std::shared_ptr<TProcess> pMin;
-    TDuration dMin;
-    for (auto&& [_, p] : ProcessesWithTasks) {
-        if (!p->GetScope()->CheckToRun()) {
-            continue;
+    for (auto it = WeightedProcesses.begin(); it != WeightedProcesses.end(); ++it) {
+        for (ui32 i = 0; i < it->second.size(); ++i) {
+            if (!it->second[i]->GetScope()->CheckToRun()) {
+                continue;
+            }
+            pMin = it->second[i];
+            std::swap(it->second[i], it->second.back());
+            it->second.pop_back();
+            if (it->second.empty()) {
+                WeightedProcesses.erase(it);
+            }
+            break;
         }
-        const TDuration d = p->GetCPUUsage()->CalcWeight(p->GetWeight());
-        if (!pMin || d < dMin) {
-            dMin = d;
-            pMin = p;
+        if (pMin) {
+            break;
         }
     }
     if (!pMin) {
         return std::nullopt;
     }
-    AFL_VERIFY(pMin);
     auto result = pMin->ExtractTaskWithPrediction(counters);
-    if (pMin->GetTasksCount() == 0) {
-        AFL_VERIFY(ProcessesWithTasks.erase(pMin->GetProcessId()));
-    } 
+    if (pMin->GetTasksCount()) {
+        WeightedProcesses[pMin->GetWeightedUsage()].emplace_back(pMin);
+    }
     if (scopeIds.emplace(pMin->GetScope()->GetScopeId()).second) {
         pMin->GetScope()->IncInFlight();
     }
@@ -98,7 +96,35 @@ void TProcessCategory::PutTaskResult(TWorkerTaskResult&& result, THashSet<TStrin
     if (it == Processes.end()) {
         return;
     }
+    Y_UNUSED(RemoveWeightedProcess(it->second));
     it->second->PutTaskResult(std::move(result));
+    if (it->second->GetTasksCount()) {
+        WeightedProcesses[it->second->GetWeightedUsage()].emplace_back(it->second);
+    }
+}
+
+bool TProcessCategory::RemoveWeightedProcess(const std::shared_ptr<TProcess>& process) {
+    if (!process->GetTasksCount()) {
+        return false;
+    }
+    AFL_VERIFY(WeightedProcesses.size());
+    auto itW = WeightedProcesses.find(process->GetWeightedUsage());
+    AFL_VERIFY(itW != WeightedProcesses.end())("weight", process->GetWeightedUsage().GetValue())("size", WeightedProcesses.size())(
+                        "first", WeightedProcesses.begin()->first.GetValue());
+    for (ui32 i = 0; i < itW->second.size(); ++i) {
+        if (itW->second[i]->GetProcessId() != process->GetProcessId()) {
+            continue;
+        }
+        itW->second[i] = itW->second.back();
+        if (itW->second.size() == 1) {
+            WeightedProcesses.erase(itW);
+        } else {
+            itW->second.pop_back();
+        }
+        return true;
+    }
+    AFL_VERIFY(false);
+    return false;
 }
 
 }   // namespace NKikimr::NConveyorComposite

ydb/core/tx/conveyor_composite/service/category.h
@@ -17,8 +17,10 @@ private:
     YDB_READONLY_DEF(std::shared_ptr<TCategorySignals>, Counters);
     THashMap<TString, std::shared_ptr<TProcessScope>> Scopes;
     THashMap<ui64, std::shared_ptr<TProcess>> Processes;
-    THashMap<ui64, std::shared_ptr<TProcess>> ProcessesWithTasks;
+    std::map<TDuration, std::deque<std::shared_ptr<TProcess>>> WeightedProcesses;
     const NConfig::TCategory Config;
+
+    [[nodiscard]] bool RemoveWeightedProcess(const std::shared_ptr<TProcess>& process);
 
 public:
     ui32 GetWaitingQueueSize() const {
@@ -39,10 +41,13 @@ public:
     void RegisterTask(const ui64 internalProcessId, std::shared_ptr<ITask>&& task) {
         auto it = Processes.find(internalProcessId);
         AFL_VERIFY(it != Processes.end())("process_id", internalProcessId);
-        it->second->RegisterTask(std::move(task), Category);
-        if (it->second->GetTasksCount() == 1) {
-            AFL_VERIFY(ProcessesWithTasks.emplace(internalProcessId, it->second).second);
+        if (!it->second->GetTasks().size()) {
+            if (!WeightedProcesses.empty() && !it->second->GetInProgressTasksCount()) {
+                it->second->SetBaseWeight(WeightedProcesses.begin()->first);
+            }
+            WeightedProcesses[it->second->GetWeightedUsage()].emplace_back(it->second);
         }
+        it->second->RegisterTask(std::move(task), Category);
     }
 
     void PutTaskResult(TWorkerTaskResult&& result, THashSet<TString>& scopeIds);
@@ -55,7 +60,7 @@ public:
     void UnregisterProcess(const ui64 processId) {
         auto it = Processes.find(processId);
         AFL_VERIFY(it != Processes.end());
-        ProcessesWithTasks.erase(processId);
+        Y_UNUSED(RemoveWeightedProcess(it->second));
         if (it->second->GetScope()->DecProcesses()) {
             AFL_VERIFY(Scopes.erase(it->second->GetScope()->GetScopeId()));
         }
@@ -67,7 +72,6 @@ public:
     }
 
     bool HasTasks() const;
-    void DoQuant(const TMonotonic newStart);
     std::optional<TWorkerTask> ExtractTaskWithPrediction(const std::shared_ptr<TWPCategorySignals>& counters, THashSet<TString>& scopeIds);
     TProcessScope& MutableProcessScope(const TString& scopeName);
     TProcessScope* MutableProcessScopeOptional(const TString& scopeName);

commit 8b11c7e32bde145c509e50c63e99a181c1df9947

conveyor_composite: remove stray unhandled event
regression by 3849a7e5cde6fbbb1a61a417b485b2c6c8dd3513
event handler was removed, but event is still emitted (once);
largely cosmetic (only nasty-looking, but harmless log message)

ydb/core/tx/conveyor_composite/service/service.cpp
@@ -26,7 +26,6 @@ void TDistributor::Bootstrap() {
     AFL_NOTICE(NKikimrServices::TX_CONVEYOR)("name", ConveyorName)("action", "conveyor_registered")("config", Config.DebugString())(
         "actor_id", SelfId())("manager", Manager->DebugString());
     Become(&TDistributor::StateMain);
-    TBase::Schedule(TDuration::Seconds(1), new NActors::TEvents::TEvWakeup(1));
 }
 
 void TDistributor::HandleMain(TEvInternal::TEvTaskProcessedResult::TPtr& evExt) {
5165661e4f1107a31d874c5fbb0a2e8b13f34766
Yuriy Kaminskiy <yumkam7@ydb.tech>
2025-08-04 12:37:07 +0300

    reduce test repetitions

--- ydb/library/yql/dq/actors/compute/ut/dq_async_compute_actor_ut.cpp
+++ ydb/library/yql/dq/actors/compute/ut/dq_async_compute_actor_ut.cpp
@@ -984,7 +984,7 @@ Y_UNIT_TEST_SUITE(TAsyncComputeActorTest) {
     Y_UNIT_TEST_F(Basic, TAsyncCATestFixture) {
         TVector<ui32> sizes{ 1, 2, 3, 4, 5, 51, 128, 251 };
         std::mt19937 rng;
-        for (ui32 t = 0; t < 256; ++t) sizes.push_back(1 + rng() % 734);
+        for (ui32 t = 0; t < 16; ++t) sizes.push_back(1 + rng() % 734);
         for (bool waitIntermediateAcks : { false, true }) {
             for (ui32 watermarkPeriod : { 0, 1, 3 }) {
                 for (ui32 packets : sizes) {
@@ -1019,7 +1019,7 @@ Y_UNIT_TEST_SUITE(TAsyncComputeActorTest) {
             }
         }
         std::mt19937 rng(seed);
-        for (ui32 t = 0; t < 256; ++t) sizes.push_back(1 + rng() % 734);
+        for (ui32 t = 0; t < 16; ++t) sizes.push_back(1 + rng() % 734);
         for (ui32 numChannels: { 1, 2, 7, 11 }) {
             for (bool waitIntermediateAcks : { false, true }) {
                 for (ui32 watermarkPeriod : { 0, 1, 3 }) {

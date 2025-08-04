b11eb739013f2683a4f5fcf5811861cd87fc728d
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-08-04 15:04:05 +0300

    tpch cs duplicates test

---
+++ ydb/tests/functional/tpc/medium/tpch/test_duplicates.py
@@ -0,0 +1,20 @@
+import ydb.tests.olap.load.lib.tpch as tpch
+from ydb.tests.functional.tpc.lib.conftest import FunctionalTestBase
+
+
+class TestTpchDuplicates(tpch.TestTpch1, FunctionalTestBase):
+    iterations: int = 10
+
+    @classmethod
+    def addition_init_params(cls) -> list[str]:
+        if cls.float_mode:
+            return ['--float-mode', cls.float_mode]
+        return []
+
+    @classmethod
+    def setup_class(cls) -> None:
+        cls.setup_cluster()
+        cls.run_cli(['workload', 'tpch', '-p', f'olap_yatests/{cls._get_path()}', 'init', '--store=column', '--datetime-types=dt64'] + cls.addition_init_params())
+        cls.run_cli(['workload', 'tpch', '-p', f'olap_yatests/{cls._get_path()}', 'import', 'generator', f'--scale={cls.scale}'])
+        cls.run_cli(['workload', 'tpch', '-p', f'olap_yatests/{cls._get_path()}', 'import', 'generator', f'--scale={cls.scale}'])
+        super().setup_class()

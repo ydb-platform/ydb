dcb77339dadd84451e9cb3eecddac1fb16f8bd67
Oleg Doronin <dorooleg@yandex.ru>
2025-08-04 17:06:49 +0000

    small fixes

--- ydb/core/tx/columnshard/tracing/probes.h
+++ ydb/core/tx/columnshard/tracing/probes.h
@@ -12,7 +12,7 @@ namespace NKikimr::NColumnShard  {
     PROBE(EvWriteResult, \
         GROUPS("Write"), \
         TYPES(ui64, TString, ui64, ui64, TString, bool, TString), \
-        NAMES("tabletId", "sender", "txId" "cookie", "type", "success", "reason")) \
+        NAMES("tabletId", "sender", "txId", "cookie", "type", "success", "reason")) \
 
 LWTRACE_DECLARE_PROVIDER(YDB_CS)
 

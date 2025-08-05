9d76b08fd47e2b3010610116901d8501ff9707e6
Alexander Kotov <akotov@ydb.tech>
2025-08-05 13:04:28 +0300

    [-] test TPartitionTests::TEvTxCalcPredicate_With_Conflicts

--- ydb/core/persqueue/ut/partition_ut.cpp
+++ ydb/core/persqueue/ut/partition_ut.cpp
@@ -3655,7 +3655,7 @@ Y_UNIT_TEST_F(TEvTxCalcPredicate_With_Conflicts, TPartitionTxTestHelper)
 
     WaitTxPredicateReply(tx1);
 
-    auto tx2 = MakeAndSendWriteTx({{"sourceid", {1, 3}}});
+    auto tx2 = MakeAndSendWriteTx({{"sourceid", {4, 6}}});
 
     WaitWriteInfoRequest(tx2);
     SendWriteInfoResponse(tx2);
@@ -3663,8 +3663,6 @@ Y_UNIT_TEST_F(TEvTxCalcPredicate_With_Conflicts, TPartitionTxTestHelper)
     ExpectNoTxPredicateReply();
 
     SendTxCommit(tx1);
-
-    EmulateKVTablet();
 
     WaitTxPredicateReply(tx2);
 }

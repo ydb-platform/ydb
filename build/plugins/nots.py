da1999f1ebaafdac5c571ef4ec01f82368cc3a19
vpozdyayev <vpozdyayev@yandex-team.com>
2025-08-05 20:00:31 +0300

    Switch TS modules to the new command engine commit_hash:4a47a06eeb108a583bf158b4e3dc36c22591be92

--- build/plugins/nots.py
+++ build/plugins/nots.py
@@ -993,6 +993,7 @@ def __on_ts_files(unit: NotsUnitType, files_in: list[str], files_out: list[str])
             )
 
     new_items = _build_cmd_input_paths(paths=files_in, hide=True, disable_include_processor=True)
+    new_items += " "
     new_items += _build_cmd_output_paths(paths=files_out, hide=True)
     __set_append(unit, "_TS_FILES_INOUTS", new_items)
 

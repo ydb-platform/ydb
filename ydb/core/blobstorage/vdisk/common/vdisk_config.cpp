fad0918d30217301daaaa80a9c696a8a202dfdc9
Polina Volosnikova <vpolka@nebius.com>
2025-08-06 16:50:07 +0200

    add ICB to control VDisk compaction InFlight (#20508)

--- ydb/core/blobstorage/vdisk/common/vdisk_config.cpp
+++ ydb/core/blobstorage/vdisk/common/vdisk_config.cpp
@@ -82,7 +82,11 @@ namespace NKikimr {
         HandoffTimeout = TDuration::Seconds(10);
         RunRepl = !baseInfo.ReadOnly;
 
-        ReplMaxTimeToMakeProgress = VDiskPerformance.at(baseInfo.DeviceType).ReplMaxTimeToMakeProgress;
+        if (const auto& perf = VDiskPerformance.find(baseInfo.DeviceType); perf != VDiskPerformance.end()) {
+            ReplMaxTimeToMakeProgress = perf->second.ReplMaxTimeToMakeProgress;
+        } else {
+            ReplMaxTimeToMakeProgress = TDuration::Minutes(180);
+        }
 
         SkeletonFrontGets_MaxInFlightCount = 24;
         SkeletonFrontGets_MaxInFlightCost = 200000000;              // 200ms

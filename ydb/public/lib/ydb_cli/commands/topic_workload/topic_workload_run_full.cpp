78ac842ce81d601d1535007b17d20a0e6feccefd
Aleksei Moiseitsev <swarmer@ydb.tech>
2025-08-06 10:30:04 +0000

    support new options in the full scenario

--- ydb/public/lib/ydb_cli/commands/topic_workload/topic_workload_run_full.cpp
+++ ydb/public/lib/ydb_cli/commands/topic_workload/topic_workload_run_full.cpp
@@ -81,6 +81,15 @@ void TCommandWorkloadTopicRunFull::Config(TConfig& config)
     config.Opts->AddLongOption("no-consumer", "Read without consumer")
         .Hidden()
         .StoreTrue(&Scenario.ReadWithoutConsumer);
+    config.Opts->AddLongOption("key-prefix", "Generate keys with this prefix. Put pair '__key':'{key-prefix}.{key-index}' in the message metadata.")
+        .Optional()
+        .Hidden()
+        .StoreResult(&Scenario.KeyPrefix);
+    config.Opts->AddLongOption("key-count", "The number of different keys to generate. The --key-prefix parameter must be set.")
+        .Optional()
+        .Hidden()
+        .DefaultValue(1)
+        .StoreResult(&Scenario.KeyCount);
 
     config.Opts->MutuallyExclusive("message-rate", "byte-rate");
 

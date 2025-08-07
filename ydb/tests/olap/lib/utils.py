f7404af5398626b9b94494a62143237d3f44a62d
Oleg Geller <iddqd@ydb.tech>
2025-08-07 12:00:51 +0300

    Add ci and test tools vesion to report and qa db

--- ydb/tests/olap/lib/utils.py
+++ ydb/tests/olap/lib/utils.py
@@ -1,4 +1,6 @@
 import yatest.common
+import os
+import library.python.svn_version as vcs
 
 
 def get_external_param(name: str, default):
@@ -10,3 +12,12 @@ def get_external_param(name: str, default):
 
 def external_param_is_true(name: str) -> bool:
     return get_external_param(name, '').lower() in ['t', 'true', 'yes', '1', 'da']
+
+
+def get_ci_version() -> str:
+    if 'CI_REVISION' in os.environ or 'CI_BRANCH' in os.environ:
+        return f'{os.getenv("CI_BRANCH", '')}.{os.getenv("CI_REVISION", '')[0:9]}'
+
+
+def get_self_version() -> str:
+    return f'{(vcs.svn_branch() if vcs.svn_branch() else vcs.svn_tag()).split('/')[-1]}.{vcs.commit_id()[0:7]}'

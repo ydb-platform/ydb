230c44a80d00d131896628beb6736bb50bcc7f18
Bulat Gayazov <bylatgr@gmail.com>
2025-08-08 15:30:26 +0000

    Update vector example for C++ SDK

--- ydb/public/sdk/cpp/examples/vector_index_builtin/vector_index.cpp
+++ ydb/public/sdk/cpp/examples/vector_index_builtin/vector_index.cpp
@@ -2,6 +2,15 @@
 
 #include <format>
 
+std::string ConvertVectorToBytes(const std::vector<float>& vector)
+{
+    std::string result;
+    for (const auto& value : vector) {
+        const char* bytes = reinterpret_cast<const char*>(&value);
+        result += std::string(bytes, sizeof(float));
+    }
+    return result + "\x01";
+}
 
 void DropVectorTable(NYdb::NQuery::TQueryClient& client, const std::string& tableName)
 {
@@ -29,7 +38,52 @@ void CreateVectorTable(NYdb::NQuery::TQueryClient& client, const std::string& ta
     std::cout << "Vector table created: " << tableName << std::endl;
 }
 
-void InsertItems(
+void InsertItemsAsBytesList(
+    NYdb::NQuery::TQueryClient& client,
+    const std::string& tableName,
+    const std::vector<TItem>& items)
+{
+    std::string query = std::format(R"(
+        DECLARE $items AS List<Struct<
+            id: Utf8,
+            document: Utf8,
+            embedding: String
+        >>;
+        UPSERT INTO `{0}`
+        (
+        id,
+        document,
+        embedding
+        )
+        SELECT
+            id,
+            document,
+            embedding,
+        FROM AS_TABLE($items);
+    )", tableName);
+
+    NYdb::TParamsBuilder paramsBuilder;
+    auto& valueBuilder = paramsBuilder.AddParam("$items");
+    valueBuilder.BeginList();
+    for (const auto& item : items) {
+        valueBuilder.AddListItem();
+        valueBuilder.BeginStruct();
+        valueBuilder.AddMember("id").Utf8(item.Id);
+        valueBuilder.AddMember("document").Utf8(item.Document);
+        valueBuilder.AddMember("embedding").String(ConvertVectorToBytes(item.Embedding));
+        valueBuilder.EndStruct();
+    }
+    valueBuilder.EndList();
+    valueBuilder.Build();
+
+    NYdb::NStatusHelpers::ThrowOnError(client.RetryQuerySync([params = paramsBuilder.Build(), &query](NYdb::NQuery::TSession session) {
+        return session.ExecuteQuery(query, NYdb::NQuery::TTxControl::BeginTx(NYdb::NQuery::TTxSettings::SerializableRW()).CommitTx(), params).ExtractValueSync();
+    }));
+
+    std::cout << items.size() << " items inserted" << std::endl;
+}
+
+void InsertItemsAsFloatList(
     NYdb::NQuery::TQueryClient& client,
     const std::string& tableName,
     const std::vector<TItem>& items)
@@ -122,7 +176,56 @@ void AddIndex(
     std::cout << "Table index `" << indexName << "` for table `" << tableName << "` added" << std::endl;
 }
 
-std::vector<TResultItem> SearchItems(
+std::vector<TResultItem> SearchItemsAsBytesList(
+    NYdb::NQuery::TQueryClient& client,
+    const std::string& tableName,
+    const std::vector<float>& embedding,
+    const std::string& strategy,
+    std::uint64_t limit,
+    const std::optional<std::string>& indexName)
+{
+    std::string viewIndex = indexName ? "VIEW " + *indexName : "";
+    std::string sortOrder = strategy.ends_with("Similarity") ? "DESC" : "ASC";
+
+    std::string query = std::format(R"(
+        DECLARE $embedding as String;
+        SELECT
+            id,
+            document,
+            Knn::{2}(embedding, $embedding) as score
+        FROM {0} {1}
+        ORDER BY score
+        {3}
+        LIMIT {4};
+    )", tableName, viewIndex, strategy, sortOrder, limit);
+
+    auto params = NYdb::TParamsBuilder()
+        .AddParam("$embedding")
+            .String(ConvertVectorToBytes(embedding))
+            .Build()
+        .Build();
+
+    std::vector<TResultItem> result;
+
+    NYdb::NStatusHelpers::ThrowOnError(client.RetryQuerySync([params, &query, &result](NYdb::NQuery::TSession session) {
+        auto execResult = session.ExecuteQuery(query, NYdb::NQuery::TTxControl::BeginTx(NYdb::NQuery::TTxSettings::SerializableRW()).CommitTx(), params).ExtractValueSync();
+        if (execResult.IsSuccess()) {
+            auto parser = execResult.GetResultSetParser(0);
+            while (parser.TryNextRow()) {
+                result.push_back({
+                    .Id = *parser.ColumnParser(0).GetOptionalUtf8(),
+                    .Document = *parser.ColumnParser(1).GetOptionalUtf8(),
+                    .Score = *parser.ColumnParser(2).GetOptionalFloat()
+                });
+            }
+        }
+        return execResult;
+    }));
+
+    return result;
+}
+
+std::vector<TResultItem> SearchItemsAsFloatList(
     NYdb::NQuery::TQueryClient& client,
     const std::string& tableName,
     const std::vector<float>& embedding,

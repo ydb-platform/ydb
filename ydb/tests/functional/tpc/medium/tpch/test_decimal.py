085831b0518e36440eaf9d6622c0688eec9d6799
Олег <150132506+iddqdex@users.noreply.github.com>
2025-08-10 22:53:16 +0300

    Fix decimal working in workloads run command (#22636)
    Co-authored-by: Nikolay Perfilov <pnv1@yandex-team.ru>

--- ydb/tests/functional/tpc/medium/tpch/test_decimal.py
+++ ydb/tests/functional/tpc/medium/tpch/test_decimal.py
@@ -1,5 +1,5 @@
-import test_s1
+import test_s_float
 
 
-class TestTpchS1Decimal_22_9(test_s1.TestTpchS1):
+class TestTpchS0_1Decimal_22_9(test_s_float.TestTpchS0_1):
     float_mode = 'decimal_ydb'

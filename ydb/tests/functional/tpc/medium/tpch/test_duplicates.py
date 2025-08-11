2e049f1a4207936a1796be9249c65de5b85c67be
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-08-11 18:21:22 +0300

    fix

--- ydb/tests/functional/tpc/medium/tpch/test_duplicates.py
+++ ydb/tests/functional/tpc/medium/tpch/test_duplicates.py
@@ -3,7 +3,7 @@ from ydb.tests.functional.tpc.lib.conftest import FunctionalTestBase
 from ydb.tests.olap.lib.ydb_cluster import YdbCluster
 
 
-class TestTpchDoubleImportBase(tpch.TestTpch1, FunctionalTestBase):
+class TpchDoubleImportTestBase(tpch.TestTpch1, FunctionalTestBase):
     """https://github.com/ydb-platform/ydb/issues/22253"""
     iterations: int = 1
 
@@ -19,7 +19,7 @@ class TestTpchDoubleImportBase(tpch.TestTpch1, FunctionalTestBase):
         cls.run_cli(['workload', 'tpch', '-p', f'olap_yatests/{cls._get_path()}', 'init', '--store=column', '--datetime-types=dt64'] + cls.addition_init_params())
 
         for table in cls.get_tables():
-            cls._alter_compaction()
+            cls._alter_compaction(table)
 
         cls.run_cli(['workload', 'tpch', '-p', f'olap_yatests/{cls._get_path()}', 'import', 'generator', f'--scale={cls.scale}'])
         cls.run_cli(['workload', 'tpch', '-p', f'olap_yatests/{cls._get_path()}', 'import', 'generator', f'--scale={cls.scale}'])
@@ -44,7 +44,7 @@ class TestTpchDuplicatesNoCompaction(TestTpchDoubleImportBase):
     """https://github.com/ydb-platform/ydb/issues/22252"""
 
     @classmethod
-    def _alter_compaction(cls) -> str:
+    def _alter_compaction(cls, table) -> str:
         set_compaction_query = f"""
             ALTER OBJECT `/{YdbCluster.ydb_database}/olap_yatests/{cls._get_path()}/{table}` (TYPE TABLE)
                 SET (ACTION=UPSERT_OPTIONS, `COMPACTION_PLANNER.CLASS_NAME`=`lc-buckets`, `COMPACTION_PLANNER.FEATURES`=`

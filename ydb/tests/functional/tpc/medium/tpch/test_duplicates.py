13505da1307336332a4943df779d14b4ed9d841a
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-08-11 15:40:44 +0300

    add no compaction test

--- ydb/tests/functional/tpc/medium/tpch/test_duplicates.py
+++ ydb/tests/functional/tpc/medium/tpch/test_duplicates.py
@@ -3,7 +3,7 @@ from ydb.tests.functional.tpc.lib.conftest import FunctionalTestBase
 from ydb.tests.olap.lib.ydb_cluster import YdbCluster
 
 
-class TestTpchDuplicates(tpch.TestTpch1, FunctionalTestBase):
+class TestTpchDoubleImportBase(tpch.TestTpch1, FunctionalTestBase):
     """https://github.com/ydb-platform/ydb/issues/22253"""
     iterations: int = 1
 
@@ -19,14 +19,35 @@ class TestTpchDuplicates(tpch.TestTpch1, FunctionalTestBase):
         cls.run_cli(['workload', 'tpch', '-p', f'olap_yatests/{cls._get_path()}', 'init', '--store=column', '--datetime-types=dt64'] + cls.addition_init_params())
 
         for table in cls.get_tables():
-            set_compaction_query = f"""
-                ALTER OBJECT `/{YdbCluster.ydb_database}/olap_yatests/{cls._get_path()}/{table}` (TYPE TABLE)
-                    SET (ACTION=UPSERT_OPTIONS, `COMPACTION_PLANNER.CLASS_NAME`=`lc-buckets`, `COMPACTION_PLANNER.FEATURES`=`
-                        {{"levels" : [{{"class_name" : "Zero", "expected_blobs_size" : 2048000}},
-                                      {{"class_name" : "Zero"}}]}}`);
-            """
-            cls.run_cli(['table', 'query', 'execute', '-t', 'scheme', '-q', set_compaction_query])
+            cls._alter_compaction()
 
         cls.run_cli(['workload', 'tpch', '-p', f'olap_yatests/{cls._get_path()}', 'import', 'generator', f'--scale={cls.scale}'])
         cls.run_cli(['workload', 'tpch', '-p', f'olap_yatests/{cls._get_path()}', 'import', 'generator', f'--scale={cls.scale}'])
         super().setup_class()
+
+
+class TestTpchDuplicatesZeroLevel(TestTpchDoubleImportBase):
+    """https://github.com/ydb-platform/ydb/issues/22252"""
+
+    @classmethod
+    def _alter_compaction(cls) -> str:
+        set_compaction_query = f"""
+            ALTER OBJECT `/{YdbCluster.ydb_database}/olap_yatests/{cls._get_path()}/{table}` (TYPE TABLE)
+                SET (ACTION=UPSERT_OPTIONS, `COMPACTION_PLANNER.CLASS_NAME`=`lc-buckets`, `COMPACTION_PLANNER.FEATURES`=`
+                    {{"levels" : [{{"class_name" : "Zero", "expected_blobs_size" : 2048000}},
+                                    {{"class_name" : "Zero"}}]}}`);
+        """
+        cls.run_cli(['table', 'query', 'execute', '-t', 'scheme', '-q', set_compaction_query])
+
+
+class TestTpchDuplicatesNoCompaction(TestTpchDoubleImportBase):
+    """https://github.com/ydb-platform/ydb/issues/22253"""
+
+    @classmethod
+    def _alter_compaction(cls) -> str:
+        set_compaction_query = f"""
+            ALTER OBJECT `/{YdbCluster.ydb_database}/olap_yatests/{cls._get_path()}/{table}` (TYPE TABLE)
+                SET (ACTION=UPSERT_OPTIONS, `COMPACTION_PLANNER.CLASS_NAME`=`lc-buckets`, `COMPACTION_PLANNER.FEATURES`=`
+                    {{"levels" : [{{"class_name" : "Zero"}}]}}`);
+        """
+        cls.run_cli(['table', 'query', 'execute', '-t', 'scheme', '-q', set_compaction_query])

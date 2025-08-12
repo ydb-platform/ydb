45831c246383a1c885910461c66a32da1d2ea20a
Олег <150132506+iddqdex@users.noreply.github.com>
2025-08-12 14:58:07 +0300

    fix native decimals in tpch (#22738)

--- ydb/tests/functional/tpc/medium/tpch/test_decimal.py
+++ ydb/tests/functional/tpc/medium/tpch/test_decimal.py
@@ -3,3 +3,7 @@ import test_s_float
 
 class TestTpchS0_1Decimal_22_9(test_s_float.TestTpchS0_1):
     float_mode = 'decimal_ydb'
+
+
+class TestTpchS0_1DecimalNative(test_s_float.TestTpchS0_1):
+    float_mode = 'decimal'

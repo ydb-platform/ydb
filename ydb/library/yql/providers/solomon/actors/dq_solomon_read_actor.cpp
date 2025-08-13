d5f52fa97c9d3cf64fbc9e7691240a687844f7e5
Ivan Sukhov <evanevannnn@ydb.tech>
2025-08-13 16:12:09 +0300

    Test: Check settings validation (#22531)

--- ydb/library/yql/providers/solomon/actors/dq_solomon_read_actor.cpp
+++ ydb/library/yql/providers/solomon/actors/dq_solomon_read_actor.cpp
@@ -186,6 +186,7 @@ public:
 
     STRICT_STFUNC(LimitedModeState,
         hFunc(TEvSolomonProvider::TEvNewDataBatch, HandleNewDataBatchLimited);
+        hFunc(TEvSolomonProvider::TEvRetryDataRequest, HandleRetryDataRequest);
     )
 
     void HandleMetricsBatch(TEvSolomonProvider::TEvMetricsBatch::TPtr& metricsBatch) {

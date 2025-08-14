8b96a8e9b3fba33e195debc8b5521a7db9c0ef07
Vladilen Muzychenko <vladilenmuz@ydb.tech>
2025-08-14 12:41:21 +0000

    fix

--- ydb/core/tx/columnshard/columnshard__write.cpp
+++ ydb/core/tx/columnshard/columnshard__write.cpp
@@ -446,7 +446,7 @@ void TColumnShard::Handle(NEvents::TDataEvents::TEvWrite::TPtr& ev, const TActor
         auto result = NEvents::TDataEvents::TEvWriteResult::BuildError(
             TabletID(), 0, NKikimrDataEvents::TEvWriteResult::STATUS_OVERLOADED, "overload data error");
 
-        if (!outOfSpace) {
+        if (!outOfSpace && record.HasOverloadSubscribe()) {
             const auto rejectReasons = NOverload::MakeRejectReasons(overloadStatus);
             OverloadSubscribers.SetOverloadSubscribed(record.GetOverloadSubscribe(), ev->Recipient, ev->Sender, rejectReasons, result->Record);
         }

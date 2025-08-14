90bde943ffd403eb47cff19174fb3716b9aef9bd
Aleksei Moiseitsev <swarmer@ydb.tech>
2025-08-14 20:33:12 +0000

    add test

---
+++ ydb/tests/stress/topic_kafka/tests/test_workload_topic.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import os
+import pytest
+import yatest
+
+from ydb.tests.library.stress.fixtures import StressFixture
+
+
+class TestYdbTopicWorkload(StressFixture):
+    @pytest.fixture(autouse=True, scope="function")
+    def setup(self):
+        yield from self.setup_cluster()
+
+    def test(self):
+        yatest.common.execute([
+            yatest.common.binary_path(os.environ["YDB_WORKLOAD_PATH"]),
+            "--endpoint", self.endpoint,
+            "--database", self.database,
+            "--duration", "120",
+            "--consumers", "2",
+            "--consumer-threads", "2",
+            "--restart-interval", "70s",
+            "--partitions", "4",
+            "--write-workload", "0.01", "8000000", "2", "big_record", "1",
+            "--write-workload", "1", "45",   "1000", "small_record", "10",
+            "--write-workload", "1", "4096",       "1", "medium_record", "10",
+            # MESSAGE_RATE:float MESSAGE_SIZE:int KEYS_COUNT:int KEY_PREFIX:str PRODUCERS:int
+        ])

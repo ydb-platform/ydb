6d075d8b4153da52cca8c8e017192706343ed4ee
robot-piglet <robot-piglet@yandex-team.com>
2025-08-14 14:29:19 +0300

    Intermediate changes commit_hash:6fd1cc05ff2e9376737f161ad03ab4246c35a78f

--- contrib/libs/liburing/.yandex_meta/override.nix
+++ contrib/libs/liburing/.yandex_meta/override.nix
@@ -9,13 +9,6 @@ pkgs: attrs: with pkgs; with attrs; rec {
     hash = "sha256-V73QP89WMrL2fkPRbo/TSkfO7GeDsCudlw2Ut5baDzA=";
   };
 
-  # workaround for 'Unable to create temporary directory'
-  # see https://github.com/direnv/direnv/issues/1345 and https://github.com/NixOS/nix/issues/11929
-  shellHook = ''
-    mkdir -p $yamaker_out/tmp_build
-    TMPDIR=$yamaker_out/tmp_build
-  '';
-
   buildPhase = ''
     make -j$(nproc) -C src
     make -j$(nproc) -C test

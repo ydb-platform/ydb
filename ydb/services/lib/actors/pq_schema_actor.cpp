78d2b236c727521181fe4bb839f5a40efe4c8fee
Aleksei Moiseitsev <swarmer@ydb.tech>
2025-08-15 11:13:50 +0000

    Add meta attribute to enable topic compactification

--- ydb/services/lib/actors/pq_schema_actor.cpp
+++ ydb/services/lib/actors/pq_schema_actor.cpp
@@ -586,6 +586,8 @@ namespace NKikimr::NGRpcProxy::V1 {
                         return Ydb::StatusIds::BAD_REQUEST;
                     }
                 }
+            } else if (pair.first == "_cleanup_policy") {
+                config->SetEnableCompactification(pair.second == "compact");
             } else {
                 error = TStringBuilder() << "Attribute " << pair.first << " is not supported";
                 return Ydb::StatusIds::BAD_REQUEST;

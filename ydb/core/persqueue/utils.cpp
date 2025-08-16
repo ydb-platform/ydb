8f9de77d0cbb63eb7bdd5143b0b40c6f491e366f
Nikolay Shestakov <tesseract@ydb.tech>
2025-08-16 12:47:20 +0000

    fix 4 (optimize consumer creation)

--- ydb/core/persqueue/utils.cpp
+++ ydb/core/persqueue/utils.cpp
@@ -67,8 +67,6 @@ bool IsImportantClient(const NKikimrPQ::TPQTabletConfig& config, const TString& 
 void Migrate(NKikimrPQ::TPQTabletConfig& config) {
     // if ReadRules isn`t empty than it is old configuration format
     // when modify new format (add or alter a consumer) readRules is cleared
-    bool doAddCompactionConsumer = config.GetEnableCompactification();
-
     if (config.ReadRulesSize()) {
         config.ClearConsumers();
 
@@ -76,9 +74,6 @@ void Migrate(NKikimrPQ::TPQTabletConfig& config) {
             auto* consumer = config.AddConsumers();
 
             consumer->SetName(config.GetReadRules(i));
-            if (doAddCompactionConsumer && consumer->GetName() == NPQ::CLIENTID_COMPACTION_CONSUMER) {
-                doAddCompactionConsumer = false;
-            }
             if (i < config.ReadFromTimestampsMsSize()) {
                 consumer->SetReadFromTimestampsMs(config.GetReadFromTimestampsMs(i));
             }
@@ -101,12 +96,6 @@ void Migrate(NKikimrPQ::TPQTabletConfig& config) {
             }
             consumer->SetImportant(IsImportantClient(config, consumer->GetName()));
         }
-        if (doAddCompactionConsumer) {
-            auto* consumer = config.AddConsumers();
-            consumer->SetName(NPQ::CLIENTID_COMPACTION_CONSUMER);
-            consumer->SetReadFromTimestampsMs(0);
-            consumer->SetImportant(true);
-        }
         config.ClearReadRules();
         config.ClearReadFromTimestampsMs();
         config.ClearConsumerFormatVersions();
@@ -126,6 +115,16 @@ void Migrate(NKikimrPQ::TPQTabletConfig& config) {
         for (const auto& partition : config.GetPartitions()) {
             config.AddAllPartitions()->CopyFrom(partition);
         }
+    }
+
+    bool doAddCompactionConsumer = config.GetEnableCompactification() && AllOf(config.GetConsumers(), [](const auto& consumer) {
+        return NPQ::CLIENTID_COMPACTION_CONSUMER != consumer.GetName();
+    });
+    if (doAddCompactionConsumer) {
+        auto* consumer = config.AddConsumers();
+        consumer->SetName(NPQ::CLIENTID_COMPACTION_CONSUMER);
+        consumer->SetReadFromTimestampsMs(0);
+        consumer->SetImportant(true);
     }
 }
 

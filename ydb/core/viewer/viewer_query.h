e87e439e6a43adf5ce0844b5b04daac70daed3cf
Bulat Gayazov <bylatgr@gmail.com>
2025-08-19 19:05:04 +0000

    fix

--- ydb/core/viewer/viewer_query.h
+++ ydb/core/viewer/viewer_query.h
@@ -680,13 +680,13 @@ private:
             case NYdb::EPrimitiveType::Interval:
                 return TStringBuilder() << valueParser.GetInterval();
             case NYdb::EPrimitiveType::Date32:
-                return valueParser.GetDate32();
+                return valueParser.GetDate32().count();
             case NYdb::EPrimitiveType::Datetime64:
-                return valueParser.GetDatetime64();
+                return valueParser.GetDatetime64().count();
             case NYdb::EPrimitiveType::Timestamp64:
-                return valueParser.GetTimestamp64();
+                return valueParser.GetTimestamp64().count();
             case NYdb::EPrimitiveType::Interval64:
-                return valueParser.GetInterval64();
+                return valueParser.GetInterval64().count();
             case NYdb::EPrimitiveType::TzDate:
                 return valueParser.GetTzDate();
             case NYdb::EPrimitiveType::TzDatetime:

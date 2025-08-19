8f9e4bbde63c4af32d04b36971c8966170fe7737
Vitaly Isaev <vitalyisaev@ydb.tech>
2025-08-19 14:29:29 +0000

    Fix ydb/core/tx/schemeshard/schemeshard_impl.cpp

--- ydb/core/tx/schemeshard/schemeshard_impl.cpp
+++ ydb/core/tx/schemeshard/schemeshard_impl.cpp
@@ -7586,6 +7586,7 @@ void TSchemeShard::ConfigureExternalSources(
         nullptr,
         EnableExternalSourceSchemaInference,
         config.GetS3().GetAllowLocalFiles(),
+        config.GetAllExternalDataSourcesAreAvailable(),
         std::set<TString>(availableExternalDataSources.cbegin(), availableExternalDataSources.cend())
     );
 

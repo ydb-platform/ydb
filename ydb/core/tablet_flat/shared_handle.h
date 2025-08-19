50f783bf6d48ab90ed4963172909bb46fa2b63cf
kungasc <kungasc@ydb.tech>
2025-08-19 13:29:28 +0300

    cr: use memory_order_relaxed in SetFrequency

--- ydb/core/tablet_flat/shared_handle.h
+++ ydb/core/tablet_flat/shared_handle.h
@@ -114,7 +114,7 @@ public:
     }
 
     void SetFrequency(ui32 frequency) noexcept {
-        Frequency.store(frequency, std::memory_order_release);
+        Frequency.store(frequency, std::memory_order_relaxed);
     }
 
     /**

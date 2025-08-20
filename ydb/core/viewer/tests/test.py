e1583e7a76a50ac28c156d9aae6206af5fcdb0eb
FloatingCrowbar <komels@ydb.tech>
2025-08-20 15:44:06 +0300

    Enable CDC support for topic data handler (#23127)

--- ydb/core/viewer/tests/test.py
+++ ydb/core/viewer/tests/test.py
@@ -845,7 +845,6 @@ def test_operations_list_page_bad():
 
 
 def test_scheme_directory():
-
     result = {}
     result["1-get"] = get_viewer_normalized("/scheme/directory", {
         'database': dedicated_db,
@@ -990,7 +989,44 @@ def test_topic_data():
     return result
 
 
-def test_transfer_describe():
+def test_topic_data_cdc():
+    alter_response = call_viewer("/viewer/query", {
+        'database': dedicated_db,
+        'query': "alter table table1 add changefeed updates_feed WITH (FORMAT = 'JSON', MODE = 'UPDATES', INITIAL_SCAN = TRUE)"
+    })
+
+    insert_response = call_viewer("/viewer/query", {
+        'database': dedicated_db,
+        'query': 'insert into table1(id, name) values(11, "elleven")',
+        'schema': 'multi'
+    })
+
+    update_response = call_viewer("/viewer/query", {
+        'database': dedicated_db,
+        'query': "update table1 set name = 'ONE' where id = 1",
+        'schema': 'multi'
+    })
+
+    topic_path = '{}/table1/updates_feed'.format(dedicated_db)
+    data_response = call_viewer("/viewer/topic_data", {
+        'database': dedicated_db,
+        'path': topic_path,
+        'partition': '0',
+        'offset': '0',
+        'limit': '3'
+    })
+
+    data_response = replace_values_by_key(
+        data_response, ['CreateTimestamp', 'WriteTimestamp', 'ProducerId', ]
+    )
+    data_response = replace_types_by_key(data_response, ['TimestampDiff'])
+
+    final_result = {"alter" : alter_response, "insert" : insert_response, "update" : update_response, "data" : data_response}
+    logging.info("Results: {}".format(final_result))
+    return final_result
+
+
+def test_async_replication_describe():
     grpc_port = cluster.nodes[1].grpc_port
     endpoint = "grpc://localhost:{}/?database={}".format(grpc_port, dedicated_db)
 
@@ -1008,6 +1044,42 @@ def test_transfer_describe():
     })
 
     return result
+
+
+def test_transfer_describe():
+    topic_result = call_viewer("/viewer/query", {
+        'database': dedicated_db,
+        'query': 'CREATE TOPIC TestTransferSourceTopic (CONSUMER OurConsumer)',
+        'schema': 'multi'
+    })
+
+    table_result = call_viewer("/viewer/query", {
+        'database': dedicated_db,
+        'query': 'CREATE TABLE TestTransferDestinationTable (id Uint64 NOT NULL, message String, PRIMARY KEY (id) )',
+        'schema': 'multi'
+    })
+
+    transfer_result = call_viewer("/viewer/query", {
+        'database': dedicated_db,
+        'query': '''CREATE TRANSFER `TestTransfer` FROM `TestTransferSourceTopic` TO `TestTransferDestinationTable`
+            USING ($x) -> { return [<| id: $x._offset, message: $x._data |>]; }
+            WITH (CONSUMER='OurConsumer')''',
+        'schema': 'multi'
+    })
+
+    describe_result = get_viewer_normalized("/viewer/describe_transfer", {
+        'database': dedicated_db,
+        'path': '{}/TestTransfer'.format(dedicated_db),
+        'include_stats': 'true',
+        'enums': 'true'
+    })
+
+    return {
+        'topic_result': topic_result,
+        'table_result': table_result,
+        'transfer_result': transfer_result,
+        'describe_result': describe_result
+    }
 
 
 def normalize_result_query_long(result):

81f87887d1c99021029324181856a363036d4abd
Mikhail Filitov <filitovme@gmail.com>
2025-08-21 20:40:29 +0300

    remove unused

--- ydb/library/yql/dq/runtime/dq_channel_spiller_adapter.cpp
+++
@@ -1,9 +0,0 @@
-#include "dq_channel_spiller_adapter.h"
-
-namespace NYql::NDq {
-
-IDqChannelStorage::TPtr CreateDqChannelSpillerAdapter(IDqSpiller::TPtr spiller) {
-    return new TDqChannelSpillerAdapter(std::move(spiller));
-}
-
-} // namespace NYql::NDq

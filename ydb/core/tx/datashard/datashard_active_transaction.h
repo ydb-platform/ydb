cad68eaa2cc5fef6111f345a906012ffae6d81a9
Vitalii Gridnev <gridnevvvit@gmail.com>
2025-08-21 15:59:49 +0300

    increase memory limit for run execution plan

--- ydb/core/tx/datashard/datashard_active_transaction.h
+++ ydb/core/tx/datashard/datashard_active_transaction.h
@@ -124,7 +124,8 @@ public:
                      const TStepOrder &stepTxId,
                      TInstant receivedAt,
                      const TString &txBody,
-                     bool usesMvccSnapshot);
+                     bool usesMvccSnapshot,
+                     bool isPropose = false);
 
     ~TValidatedDataTx();
 
@@ -428,7 +429,7 @@ public:
     const TValidatedDataTx::TPtr& GetDataTx() const { return DataTx; }
     TValidatedDataTx::TPtr BuildDataTx(TDataShard *self,
                                        TTransactionContext &txc,
-                                       const TActorContext &ctx);
+                                       const TActorContext &ctx, bool isPropose = false);
     void ClearDataTx() { DataTx = nullptr; }
 
     const NKikimrTxDataShard::TFlatSchemeTransaction &GetSchemeTx() const

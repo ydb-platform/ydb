3bf9337ed532a637af909b48ed6a9267a3f8809b
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2025-08-25 11:42:01 +0300

    Retry `OperationAborted` error, common retry function (#23368)

--- ydb/core/tx/datashard/export_s3_uploader.cpp
+++ ydb/core/tx/datashard/export_s3_uploader.cpp
@@ -10,6 +10,7 @@
 #include <ydb/library/services/services.pb.h>
 #include <ydb/core/backup/common/checksum.h>
 #include <ydb/core/backup/common/metadata.h>
+#include <ydb/core/wrappers/retry_policy.h>
 #include <ydb/core/wrappers/s3_storage_config.h>
 #include <ydb/core/wrappers/s3_wrapper.h>
 #include <ydb/core/wrappers/events/common.h>
@@ -677,20 +678,8 @@ class TS3Uploader: public TActorBootstrapped<TS3Uploader> {
         return false;
     }
 
-    static bool ShouldRetry(const Aws::S3::S3Error& error) {
-        if (error.ShouldRetry()) {
-            return true;
-        }
-
-        if ("TooManyRequests" == error.GetExceptionName()) {
-            return true;
-        }
-
-        return false;
-    }
-
     bool CanRetry(const Aws::S3::S3Error& error) const {
-        return Attempt < Retries && ShouldRetry(error);
+        return Attempt < Retries && NWrappers::ShouldRetry(error);
     }
 
     void Retry() {

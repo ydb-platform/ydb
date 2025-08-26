991220ad10f1d1ca0813dd02ec329da7a6c6dc70
vvvv <vvvv@yandex-team.com>
2025-08-26 22:24:40 +0300

    YQL-20344 new lang ver commit_hash:020b066a5f27dafb204afa9c0cdb1a71e9145e4f

--- yql/essentials/public/langver/yql_langver.cpp
+++ yql/essentials/public/langver/yql_langver.cpp
@@ -9,7 +9,7 @@ namespace NYql {
 
 namespace {
 
-constexpr TLangVersion MaxReleasedLangVersion = MakeLangVersion(2025, 2);
+constexpr TLangVersion MaxReleasedLangVersion = MakeLangVersion(2025, 3);
 
 const std::pair<ui32,ui32> Versions[] = {
 #include "yql_langver_list.inc"

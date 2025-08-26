51e0668f45161722153bc96b2234ce00d6215cd0
Semyon Danilov <senya@ydb.tech>
2025-08-26 20:03:18 +0400

    .

--- ydb/library/yaml_config/console_dumper_ut.cpp
+++ ydb/library/yaml_config/console_dumper_ut.cpp
@@ -1127,7 +1127,10 @@ selector_config:
         configItem->MutableConfig()->AddNamedConfigs()->SetName("test");
 
         TString result = NYamlConfig::DumpConsoleConfigs(items);
-        const TString expected = R"(config: {}
+        const TString expected = R"(
+config:
+  nameservice_config:
+    cluster_uuid: test
 allowed_labels:
   node_id:
     type: string

b8c45dde3e979ab359e6c9b4058b8f667ff7862a
Nikolay Shestakov <tesseract@ydb.tech>
2025-08-29 07:52:35 +0000

    revert

--- ydb/core/transfer/uploader.h
+++ ydb/core/transfer/uploader.h
@@ -78,7 +78,7 @@ private:
         auto& retry = Retries[tablePath];
         auto withRetry = retry.Backoff.HasMore() && retry.SchemeCount < MaxSchemeRetries;
         if (withRetry) {
-            LOG_E("Schedule retry: table=" << tablePath
+            LOG_D("Schedule retry: table=" << tablePath
                 << ", iteration=" << retry.Backoff.GetIteration()
                 << ", error=" << status << " " << ev->Get()->Issues.ToOneLineString());
 

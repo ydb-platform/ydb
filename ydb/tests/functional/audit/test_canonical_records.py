a729ccb2b059949f5b7b6d96a6d8fdde62d076be
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-09-02 10:00:27 +0200

    Fix ASAN test: canonize components separately

--- ydb/tests/functional/audit/test_canonical_records.py
+++ ydb/tests/functional/audit/test_canonical_records.py
@@ -73,9 +73,11 @@ selector_config: []
 
 
 def test_create_and_drop_database(ydb_cluster):
-    capture_audit_create = CanonicalCaptureAuditFileOutput(ydb_cluster.config.audit_file_path, ['console', 'schemeshard'])
-    capture_audit_drop = CanonicalCaptureAuditFileOutput(ydb_cluster.config.audit_file_path, ['console', 'schemeshard'])
-    with capture_audit_create:
+    capture_audit_create_console = CanonicalCaptureAuditFileOutput(ydb_cluster.config.audit_file_path, ['console'])
+    capture_audit_create_schemeshard = CanonicalCaptureAuditFileOutput(ydb_cluster.config.audit_file_path, ['schemeshard'])
+    capture_audit_drop_console = CanonicalCaptureAuditFileOutput(ydb_cluster.config.audit_file_path, ['console'])
+    capture_audit_drop_schemeshard = CanonicalCaptureAuditFileOutput(ydb_cluster.config.audit_file_path, ['schemeshard'])
+    with capture_audit_create_console, capture_audit_create_schemeshard:
         database = '/Root/Database'
         ydb_cluster.create_database(
             database,
@@ -85,10 +87,10 @@ def test_create_and_drop_database(ydb_cluster):
         database_nodes = ydb_cluster.register_and_start_slots(database, count=1)
     ydb_cluster.wait_tenant_up(database, token=TOKEN)
 
-    with capture_audit_drop:
+    with capture_audit_drop_console, capture_audit_drop_schemeshard:
         ydb_cluster.remove_database(database, token=TOKEN)
     ydb_cluster.unregister_and_stop_slots(database_nodes)
-    return (capture_audit_create.canonize(), capture_audit_drop.canonize())
+    return capture_audit_create_console.canonize(), capture_audit_create_schemeshard.canonize(), capture_audit_drop_console.canonize(), capture_audit_drop_schemeshard.canonize()
 
 
 def test_replace_config(ydb_cluster):
@@ -118,7 +120,7 @@ def test_create_and_drop_table(ydb_cluster):
 
         with capture_audit_drop:
             table_client.drop_table('/Root/Table')
-    return (capture_audit_create.canonize(), capture_audit_drop.canonize())
+    return capture_audit_create.canonize(), capture_audit_drop.canonize()
 
 
 def test_dml(ydb_cluster):

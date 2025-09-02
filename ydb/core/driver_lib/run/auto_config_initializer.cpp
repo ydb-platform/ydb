73e4c866834490ae416c6f66cdf62b22bc94e2e9
Kriukov Aleksandr <kruall@ydb.tech>
2025-09-02 14:48:22 +0300

    Fix need to use autoconfig function

--- ydb/core/driver_lib/run/auto_config_initializer.cpp
+++ ydb/core/driver_lib/run/auto_config_initializer.cpp
@@ -411,8 +411,10 @@ namespace NKikimr::NAutoConfigInitializer {
 
 namespace NKikimr {
     bool NeedToUseAutoConfig(const NKikimrConfig::TActorSystemConfig& config) {
+        bool hasSpecialFields = config.HasNodeType() || config.HasCpuCount();
+        if (!config.HasUseAutoConfig() && hasSpecialFields) {
+            return true;
+        }
         return config.GetUseAutoConfig()
-            || config.HasNodeType()
-            || config.HasCpuCount();
     }
 }

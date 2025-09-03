4d7ec8dd67f024f44a57eda249e250293cac502d
Vitalii Gridnev <gridnevvvit@gmail.com>
2025-09-03 22:41:23 +0300

    provide additional debug info for index auto selection (#24044)

--- ydb/core/kqp/common/kqp_yql.h
+++ ydb/core/kqp/common/kqp_yql.h
@@ -59,6 +59,7 @@ struct TKqpReadTableSettings {
     static constexpr TStringBuf ForcePrimaryName = "ForcePrimary";
     static constexpr TStringBuf GroupByFieldNames = "GroupByFieldNames";
     static constexpr TStringBuf PointPrefixLenSettingName = "PointPrefixLen";
+    static constexpr TStringBuf IndexSelectionDebugInfoSettingName = "IndexSelectionDebugInfo";
 
     TVector<TString> SkipNullKeys;
     TExprNode::TPtr ItemsLimit;
@@ -67,6 +68,7 @@ struct TKqpReadTableSettings {
     TMaybe<ui64> SequentialInFlight;
     bool ForcePrimary = false;
     ui64 PointPrefixLen = 0;
+    THashMap<TString, TString> IndexSelectionInfo;
 
     void AddSkipNullKey(const TString& key);
     void SetItemsLimit(const TExprNode::TPtr& expr) { ItemsLimit = expr; }
@@ -106,10 +108,12 @@ struct TKqpReadTableExplainPrompt {
     static constexpr TStringBuf UsedKeyColumnsName = "UsedKeyColumns";
     static constexpr TStringBuf ExpectedMaxRangesName = "ExpectedMaxRanges";
     static constexpr TStringBuf PointPrefixLenName = "PointPrefixLen";
+    static constexpr TStringBuf IndexSelectionDebugInfoSettingName = "IndexSelectionDebugInfo";
 
     TVector<TString> UsedKeyColumns;
     TMaybe<ui64> ExpectedMaxRanges;
     ui64 PointPrefixLen = 0;
+    THashMap<TString, TString> IndexSelectionInfo;
 
     void SetUsedKeyColumns(TVector<TString> columns) {
         UsedKeyColumns = columns;

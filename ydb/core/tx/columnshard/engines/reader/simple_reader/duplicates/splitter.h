3171e8082401f0eaf90c57796f750f8db1a8099a
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-09-03 17:17:21 +0300

    clean up

--- ydb/core/tx/columnshard/engines/reader/simple_reader/duplicates/splitter.h
+++ ydb/core/tx/columnshard/engines/reader/simple_reader/duplicates/splitter.h
@@ -57,28 +57,15 @@ public:
 
 private:
     std::vector<TBorder> Borders;
-    // std::shared_ptr<arrow::Schema> SortingSchema;
 
 public:
     TColumnDataSplitter(const THashMap<ui64, TPortionInfo::TConstPtr>& sources) {
         AFL_VERIFY(sources.size());
-        // SortingSchema = sources.begin()->second.GetSchema();
-        // TBorder sortableBegin(begin, *TValidator::CheckNotNull(sources.FindPtr(begin.GetPortionId())));
-        // TBorder sortableEnd(end, *TValidator::CheckNotNull(sources.FindPtr(end.GetPortionId())));
-
         for (const auto& [id, source] : sources) {
-            // AFL_VERIFY(localBegin <= sortableEnd)("portion_start", source->GetMeta().IndexKeyStart().DebugString())(
-            //     "interval_end", sortableEnd.DebugString());
-            // AFL_VERIFY(localEnd >= sortableEnd)("portion_end", source->GetMeta().IndexKeyEnd().DebugString())(
-            //     "interval_start", sortableBegin.DebugString());
             Borders.emplace_back(TBorder::First(source->GetMeta().IndexKeyStart(), id));
             Borders.emplace_back(TBorder::Last(source->GetMeta().IndexKeyEnd(), id));
         }
-        // Borders.emplace_back(std::move(sortableBegin));
-        // Borders.emplace_back(std::move(sortableEnd));
-
         std::sort(Borders.begin(), Borders.end());
-
         AFL_VERIFY(NumIntervals());
     }
 
@@ -112,44 +99,6 @@ public:
             lastBorder = &currentBorder;
         }
     }
-
-    // std::vector<TRowRange> SplitPortion(const std::shared_ptr<NArrow::TGeneralContainer>& data) const {
-    //     AFL_VERIFY(!Borders.empty());
-
-    //     std::vector<ui64> borderOffsets;
-    //     ui64 offset = 0;
-
-    //     auto position = NArrow::NMerger::TRWSortableBatchPosition(data, 0, SortingSchema->field_names(), {}, false);
-
-    //     for (const auto& border : Borders) {
-    //         if (offset == data->GetRecordsCount()) {
-    //             borderOffsets.emplace_back(offset);
-    //             continue;
-    //         }
-    //         const auto findBound = NArrow::NMerger::TSortableBatchPosition::FindBound(
-    //             position, offset, data->GetRecordsCount() - 1, border.GetKey(), border.GetIsLast());
-    //         offset = findBound ? findBound->GetPosition() : data->GetRecordsCount();
-    //         borderOffsets.emplace_back(offset);
-    //     }
-
-    //     std::vector<TRowRange> segments;
-    //     for (ui64 i = 1; i < borderOffsets.size(); ++i) {
-    //         segments.emplace_back(TRowRange(borderOffsets[i - 1], borderOffsets[i]));
-    //     }
-
-    //     AFL_VERIFY(segments.size() == NumIntervals())("splitted", segments.size())("expected", NumIntervals())("splitter", DebugString());
-    //     return segments;
-    // }
-
-    // TString DebugString() const {
-    //     TStringBuilder sb;
-    //     sb << "[";
-    //     for (const auto& border : Borders) {
-    //         sb << border.DebugString() << ";";
-    //     }
-    //     sb << "]";
-    //     return sb;
-    // }
 };
 
 }   // namespace NKikimr::NOlap::NReader::NSimple

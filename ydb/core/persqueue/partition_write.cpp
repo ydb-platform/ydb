23754e94eb58618577d5b20b8a1500385bdb5262
Nikolay Shestakov <tesseract@ydb.tech>
2025-09-03 15:40:46 +0000

    codestyle

--- ydb/core/persqueue/partition_write.cpp
+++ ydb/core/persqueue/partition_write.cpp
@@ -1145,14 +1145,18 @@ void TPartition::TryCorrectStartOffset(TMaybe<ui64> offset)
 }
 
 bool TPartition::ExecRequest(TWriteMsg& p, ProcessParameters& parameters, TEvKeyValue::TEvRequest* request) {
+    Y_DEBUG_ABORT_UNLESS(WriteInflightSize >= p.Msg.Data.size(),
+                         "PQ %" PRIu64 ", Partition {%" PRIu32 ", %" PRIu32 "}, WriteInflightSize=%" PRIu64 ", p.Msg.Data.size=%" PRISZT,
+                         TabletID, Partition.OriginalPartitionId, Partition.InternalPartitionId,
+                         WriteInflightSize, p.Msg.Data.size());
+    WriteInflightSize -= p.Msg.Data.size();
+
     if (!CanWrite()) {
-        WriteInflightSize -= p.Msg.Data.size();
         ScheduleReplyError(p.Cookie, false, InactivePartitionErrorCode,
                            TStringBuilder() << "Write to inactive partition " << Partition.OriginalPartitionId);
         return false;
     }
     if (DiskIsFull) {
-        WriteInflightSize -= p.Msg.Data.size();
         ScheduleReplyError(p.Cookie, false,
                            NPersQueue::NErrorCode::WRITE_ERROR_DISK_IS_FULL,
                            "Disk is full");
@@ -1163,12 +1167,6 @@ bool TPartition::ExecRequest(TWriteMsg& p, ProcessParameters& parameters, TEvKey
     ui64& curOffset = parameters.CurOffset;
     auto& sourceIdBatch = parameters.SourceIdBatch;
     auto sourceId = sourceIdBatch.GetSource(p.Msg.SourceId);
-
-    Y_DEBUG_ABORT_UNLESS(WriteInflightSize >= p.Msg.Data.size(),
-                         "PQ %" PRIu64 ", Partition {%" PRIu32 ", %" PRIu32 "}, WriteInflightSize=%" PRIu64 ", p.Msg.Data.size=%" PRISZT,
-                         TabletID, Partition.OriginalPartitionId, Partition.InternalPartitionId,
-                         WriteInflightSize, p.Msg.Data.size());
-    WriteInflightSize -= p.Msg.Data.size();
 
     TabletCounters.Percentile()[COUNTER_LATENCY_PQ_RECEIVE_QUEUE].IncrementFor(ctx.Now().MilliSeconds() - p.Msg.ReceiveTimestamp);
     //check already written

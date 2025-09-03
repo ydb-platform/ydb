7c152f007f789ada92639db379cbeff210d9d151
vokayndzop <vokayndzop@yandex-team.com>
2025-09-03 18:01:12 +0300

    Watermarks: fix early events commit_hash:513bd8f2833db8715382fe6dec50c928b8471965

--- yql/essentials/minikql/comp_nodes/mkql_multihopping.cpp
+++ yql/essentials/minikql/comp_nodes/mkql_multihopping.cpp
@@ -170,7 +170,10 @@ public:
             return false;
         }
 
-        TInstant GetWatermark() {
+        TMaybe<TInstant> GetWatermark() {
+            if (!WatermarkMode) {
+                return Nothing();
+            }
             return Watermark.WatermarkIn;
         }
 
@@ -223,7 +226,9 @@ public:
                             return status;
                         }
                         PendingYield = true;
-                        CloseOldBuckets(GetWatermark().MicroSeconds(), newHopsStat);
+                        if (auto watermark = GetWatermark()) {
+                            CloseOldBuckets(watermark->MicroSeconds(), newHopsStat);
+                        }
                         if (!Ready.empty()) {
                             result = std::move(Ready.front());
                             Ready.pop_front();
@@ -246,7 +251,8 @@ public:
                 const auto ts = time.Get<ui64>();
                 const auto hopIndex = ts / HopTime;
 
-                auto& keyState = GetOrCreateKeyState(key, WatermarkMode ? GetWatermark().MicroSeconds() / HopTime : hopIndex);
+                const auto watermark = GetWatermark();
+                auto& keyState = GetOrCreateKeyState(key, watermark ? watermark->MicroSeconds() / HopTime : hopIndex);
                 if (hopIndex < keyState.HopIndex) {
                     ++LateEventsThrown;
                     continue;

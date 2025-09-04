65a2cbd45d75650f232126e1300f91c3e4918b9d
Nikita Vasilev <nekitvas@gmail.com>
2025-09-04 13:27:13 +0300

    fix

--- ydb/core/kqp/session_actor/kqp_session_actor.cpp
+++ ydb/core/kqp/session_actor/kqp_session_actor.cpp
@@ -1062,6 +1062,7 @@ public:
 
         if (QueryState->TxCtx->EffectiveIsolationLevel != NKikimrKqp::ISOLATION_LEVEL_SERIALIZABLE
                 && QueryState->TxCtx->EffectiveIsolationLevel != NKikimrKqp::ISOLATION_LEVEL_SNAPSHOT_RO
+                && QueryState->GetType() != NKikimrKqp::QUERY_TYPE_SQL_SCAN
                 && QueryState->TxCtx->HasOlapTable) {
             ReplyQueryError(Ydb::StatusIds::PRECONDITION_FAILED,
                             TStringBuilder()

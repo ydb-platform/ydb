c967282152c407bec6f1309ddeeb448aac493851
Oleg Doronin <dorooleg@yandex.ru>
2025-09-04 15:56:13 +0000

    cleanup

--- ydb/core/tx/columnshard/columnshard_impl.cpp
+++ ydb/core/tx/columnshard/columnshard_impl.cpp
@@ -975,9 +975,10 @@ void TColumnShard::Die(const TActorContext& ctx) {
     NTabletPipe::CloseAndForgetClient(SelfId(), StatsReportPipe);
     UnregisterMediatorTimeCast();
     NYDBTest::TControllers::GetColumnShardController()->OnTabletStopped(*this);
-    Send(SpaceWatcherId, new NActors::TEvents::TEvPoison);
     if (SpaceWatcherId == TActorId{}) {
         delete SpaceWatcher;
+    } else {
+        Send(SpaceWatcherId, new NActors::TEvents::TEvPoison);
     }
     IActor::Die(ctx);
 }

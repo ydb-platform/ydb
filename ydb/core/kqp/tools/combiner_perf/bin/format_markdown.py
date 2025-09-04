547f468585843d7c60a2d2a77bfbbc9fd634610a
Pavel Zuev <pzuev@ydb.tech>
2025-09-04 18:03:08 +0500

    Combiner comparison test updates in combiner_perf (#24178)

--- ydb/core/kqp/tools/combiner_perf/bin/format_markdown.py
+++ ydb/core/kqp/tools/combiner_perf/bin/format_markdown.py
@@ -5,13 +5,14 @@ import sys
 import collections
 import base64
 import argparse
+import re
 
-def gen_chart(ref_time, graph_time, llvm_time = None):
+def gen_chart(ref_val, graph_val, extra_val = None, ref_label = 'C++', graph_label = 'Graph', extra_label = 'LLVM', value_per_bar = 1000.0):
     chars = '▏▎▍▌▋▊▉█'
     fullwidth = chars[-2] # for separators between 1-second blocks
 
     def draw_line(val, hue, title, comment):
-        val = val / 1000.0
+        val = val / value_per_bar
 
         if hue is None:
             color = 'darkgray'
@@ -36,21 +37,21 @@ def gen_chart(ref_time, graph_time, llvm_time = None):
         return out
 
     out = ''
-    out += draw_line(ref_time, None, 'C++', None)
+    out += draw_line(ref_val, None, ref_label, None)
 
     def add_colored_bar(ref_value, result_value, title):
         hue = None
         comment = None
-        if result_value:
+        if result_value and ref_value:
             shame_ratio = result_value / ref_value
             comment = 'x %.1f' % shame_ratio
             hue = 120 - int(120.0 * (shame_ratio - 0.5) / 1.5) # Map [0.5, 2] -> [120, 0]
             hue = max(0, min(hue, 120)) # clamp to the [0, 120] range; hue 0 = red, hue 120 = green
         return draw_line(result_value, hue, title, comment)
 
-    out += add_colored_bar(ref_time, graph_time, 'Graph')
-    if llvm_time is not None:
-        out += add_colored_bar(ref_time, llvm_time, 'LLVM')
+    out += add_colored_bar(ref_val, graph_val, graph_label)
+    if extra_val is not None:
+        out += add_colored_bar(ref_val, extra_val, extra_label)
 
     return out
 
@@ -98,7 +99,7 @@ def do_merge_llvm(samples):
 
     for sample in sorted_samples:
         is_llvm = sample.get('llvm', False)
-        key = (sample['testName'], sample['numKeys'], sample_rows(sample), sample.get('spilling', False), sample.get('blockSize', 0), sample.get('combinerMemLimit', 0), sample['hashType'])
+        key = (sample['testName'], sample['numKeys'], sample_rows(sample), sample.get('spilling', False), sample.get('blockSize', 0), sample.get('combinerMemLimit', 0), sample['hashType'], sample.get('dqTestColumns', ''))
         if key in index and not is_llvm:
             raise Exception('Attempted to merge two non-LLVM result samples, key = %s' % repr(key))
         if key not in index and is_llvm:
@@ -152,6 +153,17 @@ def friendly_hash_name(hash_type):
     }
     return types[hash_type]
 
+def decode_vs_column_config(cfg):
+    parse_re = re.compile('(\d+)k(\d+)v')
+    matcher = parse_re.match(cfg)
+    if matcher:
+        return '%s keys and %s aggregations' % (matcher.group(1), matcher.group(2))
+    raise Exception('Unknown column configuration: %d' % cfg)
+
+def explain_test_name(name):
+    if name == 'DqHashCombine':
+        return 'DqHashCombine vs WideCombine'
+    return name
 
 def do_format(merge_llvm):
     per_section = collections.defaultdict(list)
@@ -172,15 +184,18 @@ def do_format(merge_llvm):
         all_samples = do_merge_llvm(all_samples)
 
     for sample in all_samples:
-        section_name = (sample['testName'], sample_rows(sample), sample.get('llvm', False), sample.get('spilling', False), sample.get('blockSize', 0), sample.get('combinerMemLimit', 0), sample['hashType'])
+        section_name = (sample['testName'], sample_rows(sample), sample.get('llvm', False), sample.get('spilling', False), sample.get('blockSize', 0), sample.get('combinerMemLimit', 0), sample['hashType'], sample['dqTestColumns'])
         per_section[section_name].append(sample)
 
     for _, samples in per_section.items():
         combiner_name = samples[0]['testName']
+        test_human_name = explain_test_name(combiner_name)
         num_rows = sample_rows(samples[0])
         num_rows_formatted = format_large_num(num_rows)
 
         has_llvm_column = any(('llvmCleanTime' in sample for sample in samples))
+
+        is_vs_test = (combiner_name == 'DqHashCombine')
 
         traits = []
         if samples[0].get('llvm', False):
@@ -190,28 +205,35 @@ def do_format(merge_llvm):
         if samples[0].get('blockSize', 0):
             traits.append(f'{samples[0]["blockSize"]} rows per block')
         memlimit = samples[0].get('combinerMemLimit', 0)
-        if memlimit and combiner_name == 'WideCombiner':
+        if memlimit and combiner_name in ('WideCombiner', 'DqHashCombine'):
             memlimit_formatted = format_mem(memlimit)
             traits.append(f'{memlimit_formatted} MB RAM limit')
         traits.append(f'{num_rows_formatted} input rows')
-        hash_name = friendly_hash_name(samples[0]['hashType'])
-        traits.append(f'{hash_name}')
+        if not is_vs_test:
+            hash_name = friendly_hash_name(samples[0]['hashType'])
+            traits.append(f'{hash_name}')
+        if is_vs_test:
+            traits.append(decode_vs_column_config(samples[0].get('dqTestColumns')))
         traits_str = ', '.join(traits)
+
+        print(f'##### {combiner_name}, {traits_str}\n')
 
         own_times = []
         for sample in samples:
             own_times.append(sample['generatorTime'])
         own_times.sort()
-        median_own_time = format_time(own_times[len(own_times) // 2])
+        median_own_time = own_times[len(own_times) // 2]
+        if median_own_time:
+            median_own_time_str = format_time(median_own_time)
+            print(f'Input generator elapsed time: {median_own_time}с\n')
 
-        print(f'##### {combiner_name}, {traits_str}\n')
-        print(f'Input generator elapsed time: {median_own_time}с\n')
         print('::: html\n<table><tr>\n')
         headers = [
-            'Shame ratio',
+            'Shame ratio (time)',
+            'Shame ratio (mem)',
             'Distinct keys',
-            'Graph time (s)',
-            'Reference C++ impl time (s)',
+            'Graph time (s)' if not is_vs_test else 'DQ time (s)',
+            'Ref C++ time (s)' if not is_vs_test else 'Ref time (s)',
         ]
         if has_llvm_column:
             headers += [
@@ -235,11 +257,25 @@ def do_format(merge_llvm):
             if ref_time_or_zero > 0:
                 shame_ratio = result_time_or_zero / ref_time_or_zero
 
+            ref_label = 'C++' if not is_vs_test else 'Ref'
+            graph_label = 'Graph' if not is_vs_test else 'DQ'
+
             cols = []
             if llvm_time_or_zero is not None:
-                cols.append(gen_chart(ref_time_or_zero, result_time_or_zero, llvm_time_or_zero))
+                cols.append(gen_chart(ref_time_or_zero, result_time_or_zero, llvm_time_or_zero, ref_label=ref_label, graph_label=graph_label))
             else:
-                cols.append(gen_chart(ref_time_or_zero, result_time_or_zero))
+                cols.append(gen_chart(ref_time_or_zero, result_time_or_zero, ref_label=ref_label, graph_label=graph_label))
+
+            result_mem = sample['maxRssDelta']
+            ref_mem = sample['referenceMaxRssDelta']
+            memlimit = sample['combinerMemLimit']
+            cols.append(gen_chart(
+                ref_mem,
+                result_mem,
+                value_per_bar = memlimit / 10,
+                ref_label=ref_label,
+                graph_label=graph_label,
+            ))
 
             cols.append(format_large_num(sample['numKeys']))
             cols.append(format_time(result_time_or_zero))

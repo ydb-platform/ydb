55b38348042b01eebae7e23e42698d74ddede531
Bulat Gayazov <bylatgr@gmail.com>
2025-09-05 14:04:10 +0000

    Fix

--- ydb/public/lib/ydb_cli/commands/topic_operations_scenario.cpp
+++ ydb/public/lib/ydb_cli/commands/topic_operations_scenario.cpp
@@ -7,7 +7,7 @@
 #include <ydb/public/lib/ydb_cli/commands/ydb_common.h>
 
 #define INCLUDE_YDB_INTERNAL_H
-#include <ydb/public/sdk/cpp/src/client/impl/ydb_internal/logger/log.h>
+#include <ydb/public/sdk/cpp/src/client/impl/internal/logger/log.h>
 #undef INCLUDE_YDB_INTERNAL_H
 
 #include <util/generic/guid.h>

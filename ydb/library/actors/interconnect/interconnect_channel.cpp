7fd78cdb20128cc5698866966deb78a9009ed24e
Alexander Rutkovsky <alexvru@ydb.tech>
2025-09-05 17:39:03 +0300

    Unpoison trace id to prevent false msan alert (#24282)

--- ydb/library/actors/interconnect/interconnect_channel.cpp
+++ ydb/library/actors/interconnect/interconnect_channel.cpp
@@ -38,6 +38,7 @@ namespace NActors {
 #endif
         };
         traceId.Serialize(&descr.TraceId);
+        NSan::Unpoison(&descr.TraceId, sizeof(descr.TraceId));
 
         // and channel header before the descriptor
         TChannelPart part{

d800535b021fd7cdb77d9938c4ffea681f1e1f3f
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2025-09-05 10:53:44 +0300

    Small refactoring

--- ydb/core/kqp/executer_actor/kqp_tasks_graph.h
+++ ydb/core/kqp/executer_actor/kqp_tasks_graph.h
@@ -69,6 +69,48 @@ struct TColumnShardHashV1Params {
     }
 };
 
+struct TShardKeyRanges {
+    // ordered ranges and points
+    TVector<TSerializedPointOrRange> Ranges;
+    std::optional<TSerializedTableRange> FullRange;
+
+    void AddPoint(TSerializedCellVec&& point);
+    void AddRange(TSerializedTableRange&& range);
+    void Add(TSerializedPointOrRange&& pointOrRange);
+
+    void CopyFrom(const TVector<TSerializedPointOrRange>& ranges);
+
+    void MakeFullRange(TSerializedTableRange&& range);
+    void MakeFullPoint(TSerializedCellVec&& range);
+    void MakeFull(TSerializedPointOrRange&& pointOrRange);
+
+    bool HasRanges() const;
+
+    bool IsFullRange() const { return FullRange.has_value(); }
+    TVector<TSerializedPointOrRange>& GetRanges() { return Ranges; }
+
+    void MergeWritePoints(TShardKeyRanges&& other, const TVector<NScheme::TTypeInfo>& keyTypes);
+
+    TString ToString(const TVector<NScheme::TTypeInfo>& keyTypes, const NScheme::TTypeRegistry& typeRegistry) const;
+    void SerializeTo(NKikimrTxDataShard::TKqpTransaction_TDataTaskMeta_TKeyRange* proto) const;
+    void SerializeTo(NKikimrTxDataShard::TKqpTransaction_TScanTaskMeta_TReadOpMeta* proto) const;
+    void SerializeTo(NKikimrTxDataShard::TKqpReadRangesSourceSettings* proto, bool allowPoints = true) const;
+
+    std::pair<const TSerializedCellVec*, bool> GetRightBorder() const;
+};
+
+struct TShardInfo {
+    struct TColumnWriteInfo {
+        ui32 MaxValueSizeBytes = 0;
+    };
+
+    TMaybe<TShardKeyRanges> KeyReadRanges;  // empty -> no reads
+    TMaybe<TShardKeyRanges> KeyWriteRanges; // empty -> no writes
+    THashMap<TString, TColumnWriteInfo> ColumnWrites;
+
+    TString ToString(const TVector<NScheme::TTypeInfo>& keyTypes, const NScheme::TTypeRegistry& typeRegistry) const;
+};
+
 struct TStageInfoMeta {
     const IKqpGateway::TPhysicalTxData& Tx;
 
@@ -224,36 +266,6 @@ struct TTaskInputMeta {
 struct TTaskOutputMeta {
     NKikimrKqp::TKqpTableSinkSettings* SinkSettings = nullptr;
     THashMap<ui64, const TKeyDesc::TPartitionInfo*> ShardPartitions;
-};
-
-struct TShardKeyRanges {
-    // ordered ranges and points
-    TVector<TSerializedPointOrRange> Ranges;
-    std::optional<TSerializedTableRange> FullRange;
-
-    void AddPoint(TSerializedCellVec&& point);
-    void AddRange(TSerializedTableRange&& range);
-    void Add(TSerializedPointOrRange&& pointOrRange);
-
-    void CopyFrom(const TVector<TSerializedPointOrRange>& ranges);
-
-    void MakeFullRange(TSerializedTableRange&& range);
-    void MakeFullPoint(TSerializedCellVec&& range);
-    void MakeFull(TSerializedPointOrRange&& pointOrRange);
-
-    bool HasRanges() const;
-
-    bool IsFullRange() const { return FullRange.has_value(); }
-    TVector<TSerializedPointOrRange>& GetRanges() { return Ranges; }
-
-    void MergeWritePoints(TShardKeyRanges&& other, const TVector<NScheme::TTypeInfo>& keyTypes);
-
-    TString ToString(const TVector<NScheme::TTypeInfo>& keyTypes, const NScheme::TTypeRegistry& typeRegistry) const;
-    void SerializeTo(NKikimrTxDataShard::TKqpTransaction_TDataTaskMeta_TKeyRange* proto) const;
-    void SerializeTo(NKikimrTxDataShard::TKqpTransaction_TScanTaskMeta_TReadOpMeta* proto) const;
-    void SerializeTo(NKikimrTxDataShard::TKqpReadRangesSourceSettings* proto, bool allowPoints = true) const;
-
-    std::pair<const TSerializedCellVec*, bool> GetRightBorder() const;
 };
 
 struct TTaskMeta {

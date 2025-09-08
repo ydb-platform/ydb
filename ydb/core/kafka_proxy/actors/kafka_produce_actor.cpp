b7ed63dc2d0b916e6f84eaa00d0b0a69473c0c5b
Irina Skvortsova <nerhneiro@yandex-team.ru>
2025-09-08 10:44:17 +0000

    add initialization for storage nodes fix

--- ydb/core/kafka_proxy/actors/kafka_produce_actor.cpp
+++ ydb/core/kafka_proxy/actors/kafka_produce_actor.cpp
@@ -300,7 +300,6 @@ std::pair<EKafkaErrors, THolder<TEvPartitionWriter::TEvWriteRequest>> Convert(
                 res->set_value(static_cast<const char*>(h.Value->data()), h.Value->size());
             }
         }
-        auto w = partitionRequest->AddCmdWrite();
 
         if (record.Key) {
             auto res = proto.AddMessageMeta();

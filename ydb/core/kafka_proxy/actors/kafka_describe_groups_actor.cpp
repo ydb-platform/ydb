a7e57015e50283c79146f3efe84c6eab1ee03c77
Irina Skvortsova <nerhneiro@yandex-team.ru>
2025-09-08 07:31:23 +0000

    add check database and tenant name for tests and one node bases

--- ydb/core/kafka_proxy/actors/kafka_describe_groups_actor.cpp
+++ ydb/core/kafka_proxy/actors/kafka_describe_groups_actor.cpp
@@ -27,8 +27,10 @@ NActors::IActor* CreateKafkaDescribeGroupsActor(const TContext::TPtr context, co
 void TKafkaDescribeGroupsActor::Bootstrap(const NActors::TActorContext& ctx) {
     if (NKikimr::AppData()->FeatureFlags.GetEnableKafkaNativeBalancing()) {
         Kqp = std::make_unique<TKqpTxHelper>(Context->ResourceDatabasePath);
-        Kqp->SendInitTableRequest(ctx, NKikimr::NGRpcProxy::V1::TKafkaConsumerGroupsMetaInitManager::GetInstant());
-        Kqp->SendInitTableRequest(ctx, NKikimr::NGRpcProxy::V1::TKafkaConsumerMembersMetaInitManager::GetInstant());
+        if (Context->DatabasePath == AppData(ctx)->TenantName) {
+            Kqp->SendInitTableRequest(ctx, NKikimr::NGRpcProxy::V1::TKafkaConsumerGroupsMetaInitManager::GetInstant());
+            Kqp->SendInitTableRequest(ctx, NKikimr::NGRpcProxy::V1::TKafkaConsumerMembersMetaInitManager::GetInstant());
+        }
         Become(&TKafkaDescribeGroupsActor::StateWork);
         auto wakeup = std::make_unique<TEvents::TEvWakeup>();
         ctx.ActorSystem()->Schedule(

826b5b2a95104209b5fae61e20dfdd02e61e4137
Irina Skvortsova <nerhneiro@yandex-team.ru>
2025-09-08 10:47:24 +0000

    Revert "add initialization for storage nodes fix"
    This reverts commit b7ed63dc2d0b916e6f84eaa00d0b0a69473c0c5b.

--- ydb/core/kafka_proxy/kafka_transactions_coordinator.h
+++ ydb/core/kafka_proxy/kafka_transactions_coordinator.h
@@ -7,22 +7,15 @@
 
 #include <ydb/library/actors/core/actor_bootstrapped.h>
 
-#include <ydb/core/kafka_proxy/kafka_consumer_groups_metadata_initializers.h>
-#include <ydb/core/kafka_proxy/kafka_consumer_members_metadata_initializers.h>
-#include <ydb/core/kafka_proxy/kqp_helper.h>
-
-
 
 namespace NKafka {
-    /*
+    /* 
     This class serves as a proxy between Kafka SDK and TTransactionActor
 
     It validates that requester is not a zombie (by checking request's tranasactional_id+producer_id+producer_epoch)
-    It does so by maintaining a set of the most relevant for this node tranasactional_id+producer_id+producer_epoch.
-    Recieves updates from init_producer_id_actors.
+    It does so by maintaining a set of the most relevant for this node tranasactional_id+producer_id+producer_epoch. 
+    Recieves updates from init_producer_id_actors. 
     */
-
-    const ui32 TABLES_COUNT = 2;
     class TTransactionsCoordinator : public NActors::TActorBootstrapped<TTransactionsCoordinator> {
 
         using TBase = NActors::TActorBootstrapped<TTransactionsCoordinator>;
@@ -31,7 +24,7 @@ namespace NKafka {
             TProducerInstanceId Id;
             ui64 TxnTimeoutMs;
         };
-
+        
         struct TTransactionalRequest {
             TString TransactionalId;
             TProducerInstanceId ProducerState;
@@ -40,12 +33,7 @@ namespace NKafka {
         };
 
         public:
-            void Bootstrap(const TActorContext& ctx) {
-                if (AppData(ctx)->DynamicNameserviceConfig->MinDynamicNodeId <= ctx.SelfID.NodeId()) {
-                    std::unique_ptr<NKafka::TKqpTxHelper> Kqp = std::make_unique<TKqpTxHelper>(AppData(ctx)->TenantName);
-                    Kqp->SendInitTableRequest(ctx, NKikimr::NGRpcProxy::V1::TKafkaConsumerGroupsMetaInitManager::GetInstant());
-                    Kqp->SendInitTableRequest(ctx, NKikimr::NGRpcProxy::V1::TKafkaConsumerMembersMetaInitManager::GetInstant());
-                }
+            void Bootstrap(const TActorContext&) {
                 TBase::Become(&TTransactionsCoordinator::StateWork);
             }
 
@@ -62,24 +50,20 @@ namespace NKafka {
                     HFunc(TEvKafka::TEvAddOffsetsToTxnRequest, Handle);
                     HFunc(TEvKafka::TEvTxnOffsetCommitRequest, Handle);
                     HFunc(TEvKafka::TEvEndTxnRequest, Handle);
-                    HFunc(NMetadata::NProvider::TEvManagerPrepared, Handle);
                     HFunc(TEvKafka::TEvTransactionActorDied, Handle);
                     HFunc(TEvents::TEvPoison, Handle);
                 }
             }
-
+            
             // Handles new transactional_id+producer_id+producer_epoch: saves for validation of future requests
             void Handle(TEvKafka::TEvSaveTxnProducerRequest::TPtr& ev, const TActorContext& ctx);
-
+            
             // Proxies requests to the relevant TTransactionActor
             void Handle(TEvKafka::TEvAddPartitionsToTxnRequest::TPtr& ev, const TActorContext& ctx);
             void Handle(TEvKafka::TEvAddOffsetsToTxnRequest::TPtr& ev, const TActorContext& ctx);
             void Handle(TEvKafka::TEvTxnOffsetCommitRequest::TPtr& ev, const TActorContext& ctx);
             void Handle(TEvKafka::TEvEndTxnRequest::TPtr& ev, const TActorContext& ctx);
-
-            // tables creation responses
-            void Handle(NMetadata::NProvider::TEvManagerPrepared::TPtr&, const TActorContext& ctx);
-
+            
             // remove transaction actor id from TxnActorByTransactionalId
             void Handle(TEvKafka::TEvTransactionActorDied::TPtr& ev, const TActorContext& ctx);
             // Will kill all txn actors
@@ -89,7 +73,7 @@ namespace NKafka {
             void HandleTransactionalRequest(TAutoPtr<TEventHandle<EventType>>& evHandle, const TActorContext& ctx);
             template<class ErrorResponseType, class RequestType>
             void SendProducerFencedResponse(TMessagePtr<RequestType> kafkaRequest, const TString& error, const TTransactionalRequest& request);
-            template<class EventType>
+            template<class EventType> 
             void ForwardToTransactionActor(TAutoPtr<TEventHandle<EventType>>& evHandle, const TActorContext& ctx);
 
             void DeleteTransactionActor(const TString& transactionalId);
@@ -99,8 +83,6 @@ namespace NKafka {
 
             std::unordered_map<TString, TProducerInstance> ProducersByTransactionalId;
             std::unordered_map<TString, TActorId> TxnActorByTransactionalId;
-
-            ui32 TablesInited = 0;
     };
 
     inline NActors::IActor* CreateTransactionsCoordinator() {
@@ -127,5 +109,5 @@ namespace NKafka {
     inline bool TransactionsEnabled() {
         return NKikimr::AppData()->FeatureFlags.GetEnableKafkaTransactions();
     }
-
+    
 } // namespace NKafka

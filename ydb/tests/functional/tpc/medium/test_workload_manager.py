dd457221566369d549176bbd1d55b4482eb721b5
Kirill Rysin <naspirato@gmail.com>
2025-09-10 07:19:43 +0200

    trigger ci

--- ydb/tests/functional/tpc/medium/test_workload_manager.py
+++ ydb/tests/functional/tpc/medium/test_workload_manager.py
@@ -6,7 +6,7 @@ from ydb.tests.functional.tpc.lib.conftest import FunctionalTestBase
 class TestClickbenchWM(wm.TestWorkloadMangerClickbenchConcurentQueryLimit, FunctionalTestBase):
     iterations: int = 2
     verify_data: bool = False
-
+    #--
     @classmethod
     def setup_class(cls) -> None:
         cls.setup_cluster()

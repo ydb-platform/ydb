18b08e6f2da0df3b59116549ace1c31b6f1c0f62
vporyadke <zalyalov@ydb.tech>
2025-09-11 12:38:03 +0200

    fix/observability for tablet deletion (#24573)

--- ydb/core/mind/hive/hive_impl.cpp
+++ ydb/core/mind/hive/hive_impl.cpp
@@ -1778,6 +1778,20 @@ void THive::UpdateCounterNodesFrozen(i64 nodesFrozenDiff) {
     }
 }
 
+void THive::UpdateCounterDeleteTabletQueueSize() {
+    if (TabletCounters != nullptr) {
+        auto& counter = TabletCounters->Simple()[NHive::COUNTER_DELETE_TABLET_QUEUE_SIZE];
+        counter.Set(DeleteTabletQueue.size());
+    }
+}
+
+void THive::UpdateCounterTabletsDeleting() {
+    if (TabletCounters != nullptr) {
+        auto& counter = TabletCounters->Simple()[NHive::COUNTER_TABLETS_DELETING];
+        counter.Set(DeleteTabletInProgress);
+    }
+}
+
 void THive::RecordTabletMove(const TTabletMoveInfo& moveInfo) {
     TabletMoveHistory.PushBack(moveInfo);
     TabletCounters->Cumulative()[NHive::COUNTER_TABLETS_MOVED].Increment(1);
@@ -2972,7 +2986,8 @@ void THive::BlockStorageForDelete(TTabletId tabletId, TSideEffects& sideEffects)
     if (tablet == nullptr) {
         return;
     }
-    if (DeleteTabletInProgress < MAX_DELETE_TABLET_IN_PROGRESS) {
+    Y_ENSURE(tablet->IsDeleting());
+    if (DeleteTabletInProgress < GetMaxDeleteTabletInProgress()) {
         ++DeleteTabletInProgress;
         if (!tablet->InitiateBlockStorage(sideEffects, std::numeric_limits<ui32>::max())) {
             DeleteTabletWithoutStorage(tablet);
@@ -2980,6 +2995,8 @@ void THive::BlockStorageForDelete(TTabletId tabletId, TSideEffects& sideEffects)
     } else {
         DeleteTabletQueue.push(tabletId);
     }
+    UpdateCounterDeleteTabletQueueSize();
+    UpdateCounterTabletsDeleting();
 }
 
 void THive::ProcessPendingStopTablet() {

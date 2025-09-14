ebdea0a64586986aa2e5498a0de0fb20fb3d59db
cherepashka <cherepashka@yandex-team.com>
2025-09-15 02:12:17 +0300

    YT-23699: Added handling pending removal users
    * Changelog entry
      Type: feature
      Component: master
    
    Handle of pending removal subjects
    commit_hash:1aa3b3ca965f920158ad3bbc3036e754c8fb1a2c

--- yt/yt/client/api/security_client.h
+++ yt/yt/client/api/security_client.h
@@ -58,6 +58,7 @@ struct TCheckPermissionByAclOptions
     , public TPrerequisiteOptions
 {
     bool IgnoreMissingSubjects = false;
+    bool IgnorePendingRemovalSubjects = false;
 };
 
 struct TCheckPermissionByAclResult
@@ -68,6 +69,7 @@ struct TCheckPermissionByAclResult
     NSecurityClient::TSubjectId SubjectId;
     std::optional<std::string> SubjectName;
     std::vector<std::string> MissingSubjects;
+    std::vector<std::string> PendingRemovalSubjects;
 };
 
 struct TSetUserPasswordOptions

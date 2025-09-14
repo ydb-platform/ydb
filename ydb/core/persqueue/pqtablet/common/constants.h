030e9e0a8d22176e25a73d0a437c0881b4fc13a8
Nikolay Shestakov <tesseract@ydb.tech>
2025-09-14 16:50:31 +0000

    more ensure

---
+++ ydb/core/persqueue/pqtablet/common/constants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <util/generic/size_literals.h>
+#include <ydb/core/persqueue/public/constants.h>
+
+namespace NKikimr::NPQ {
+
+static const ui32 MAX_BLOB_PART_SIZE = 500_KB;
+
+}

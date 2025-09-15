aa961c960a8ab100163c13b5740818ba56bc37c6
Alexey Zatelepin <ztlpn@ydb.tech>
2025-09-15 18:00:40 +0300

    compatibility test fixture with tenants

--- ydb/tests/library/compatibility/fixtures.py
+++ ydb/tests/library/compatibility/fixtures.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import copy
+import logging
 import os
 import pytest
 import time
 import yatest
 from ydb.tests.library.harness.kikimr_runner import KiKiMR
 from ydb.tests.library.harness.kikimr_config import KikimrConfigGenerator
+from ydb.tests.library.fixtures import ydb_database_ctx
 from ydb.tests.library.common.types import Erasure
 from ydb.tests.oss.ydb_sdk_import import ydb
+
+logger = logging.getLogger(__name__)
 
 
 def string_version_to_tuple(s):
@@ -194,15 +198,19 @@ class RollingUpgradeAndDowngradeFixture:
         self.all_binary_paths = request.param
         self.versions = list([path_to_version[path] for path in self.all_binary_paths])
 
+    def create_driver(self):
+        driver = ydb.Driver(
+            ydb.DriverConfig(
+                database=self.database_path,
+                endpoint=self.endpoints[0]
+            )
+        )
+        driver.wait(timeout=60)
+        return driver
+
     def _wait_for_readiness(self):
         if self.recreate_driver:
-            self.driver = ydb.Driver(
-                ydb.DriverConfig(
-                    database='/Root',
-                    endpoint=self.endpoints[0]
-                )
-            )
-            self.driver.wait(timeout=60)
+            self.driver = self.create_driver()
 
         query = """
             CREATE TABLE `test_readiness` (
@@ -228,7 +236,7 @@ class RollingUpgradeAndDowngradeFixture:
         with ydb.QuerySessionPool(self.driver) as session_pool:
             session_pool.execute_with_retries(query)
 
-    def setup_cluster(self, **kwargs):
+    def setup_cluster(self, tenant_db=None, **kwargs):
         extra_feature_flags = kwargs.pop("extra_feature_flags", {})
         extra_feature_flags = copy.copy(extra_feature_flags)
         extra_feature_flags["suppress_compatibility_check"] = True
@@ -251,20 +259,26 @@ class RollingUpgradeAndDowngradeFixture:
 
         self.endpoint = self.endpoints[0]
 
-        self.driver = ydb.Driver(
-            ydb.DriverConfig(
-                self.endpoints[0],
-                database='/Root'
-            )
-        )
-        self.driver.wait(timeout=60)
-        yield
+        if tenant_db is not None:
+            with ydb_database_ctx(self.cluster, f"/Root/{tenant_db}", node_count=3) as db_path:
+                self.database_path = db_path
+                self.driver = self.create_driver()
+                yield
+        else:
+            self.database_path = "/Root"
+            self.driver = self.create_driver()
+            yield
+
         self.cluster.stop()
 
     def roll(self):
+        all_nodes = [(id, n, "node") for id, n in self.cluster.nodes.items()] + \
+            [(id, n, "slot") for id, n in self.cluster.slots.items()]
+
         # from old to new
         yield
-        for node_id, node in self.cluster.nodes.items():
+        for node_id, node, role in all_nodes:
+            logger.info(f"upgrading {role} {node_id}")
             node.stop()
             node.binary_path = self.all_binary_paths[1]
             node.start()
@@ -272,7 +286,8 @@ class RollingUpgradeAndDowngradeFixture:
             yield
 
         # from new to old
-        for node_id, node in self.cluster.nodes.items():
+        for node_id, node, role in all_nodes:
+            logger.info(f"downgrading {role} {node_id}")
             node.stop()
             node.binary_path = self.all_binary_paths[0]
             node.start()

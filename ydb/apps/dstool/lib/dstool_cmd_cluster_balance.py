1d0fd69fb0a161960e020033b909da2cf994f192
Semyon Danilov <senya@ydb.tech>
2025-09-15 12:12:48 +0200

    Update dstool

--- ydb/apps/dstool/lib/dstool_cmd_cluster_balance.py
+++ ydb/apps/dstool/lib/dstool_cmd_cluster_balance.py
@@ -33,7 +33,8 @@ def add_options(p):
     common.add_group_ids_option(g)
     p.add_argument('--max-donors-per-pdisk', type=int, default=0, help='Limit number of donors per pdisk')
     p.add_argument('--allow-same-node', action='store_true', help='Allow to relocate vdisks from one group to the same node')
-    p.add_argument('--with-attention-to-replication', action='store_true', help='Take into account racks\' free slots and replicating vdisks. Picks node and pdisk with less replicating vdisks')
+    p.add_argument('--prefer-less-occupied-rack', action='store_true', help='Take into account racks\' free slots picking pdisk from rack with more free slots first')
+    p.add_argument('--with-attention-to-replication', action='store_true', help='Take into account replicating vdisks picking node and pdisk with less amount of them')
     p.add_argument('--waiting-time', type=int, default=Constants.WAITING_TIME, help='Time to wait when there are no vdisks to reassign')
     p.add_argument('--time-between-reassignings', type=int, default=Constants.TIME_BERWEEN_REASSIGNINGS, help='Time to wait between reassignings')
     common.add_basic_format_options(p)
@@ -449,6 +450,7 @@ class GroupVSlotsBalancingStrategy(BalancingStrategy):
         cmd.FailRealmIdx = vslot.FailRealmIdx
         cmd.FailDomainIdx = vslot.FailDomainIdx
         cmd.VDiskIdx = 0
+        cmd.PreferLessOccupiedRack = self.args.prefer_less_occupied_rack
         cmd.WithAttentionToReplication = self.args.with_attention_to_replication
         target = cmd.TargetPDiskId
         target.NodeId = target_pdisk_id[0]

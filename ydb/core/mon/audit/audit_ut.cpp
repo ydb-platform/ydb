fd1d416c4eb5c0a307669e21be2ff35fb02c400a
nfrmtk <becalm@ydb.tech>
2025-09-16 17:33:21 +0300

    Make scalar map join benchmark spit out same tuples amount as others (#24840)

--- ydb/core/mon/audit/audit_ut.cpp
+++ ydb/core/mon/audit/audit_ut.cpp
@@ -5,8 +5,59 @@
 
 using namespace NMonitoring::NAudit;
 
+namespace {
+
+struct TRequestHolder {
+    NHttp::THttpIncomingRequestPtr Request;
+    TVector<TString> Storage;
+
+    TStringBuf Store(TString value) {
+        Storage.push_back(std::move(value));
+        return Storage.back();
+    }
+};
+
+NHttp::THttpIncomingRequestPtr MakeRequest(TString method, TString url) {
+    NHttp::THttpIncomingRequestPtr Request = new NHttp::THttpIncomingRequest();
+    Request->Method = std::move(method);
+    Request->URL = std::move(url);
+    return Request;
+}
+
+} // namespace
+
 Y_UNIT_TEST_SUITE(TAuditTest) {
     Y_UNIT_TEST(AuditDisabledWithoutAppData) {
         UNIT_ASSERT(!TAuditCtx::AuditEnabled(NKikimrConfig::TAuditConfig::TLogClassConfig::Completed, NACLibProto::SUBJECT_TYPE_ANONYMOUS));
+    }
+
+    Y_UNIT_TEST(ModifyingMethodsAlwaysAuditable) {
+        TAuditCtx ctx;
+        UNIT_ASSERT(ctx.AuditableRequest(MakeRequest("POST", "/path")));
+        UNIT_ASSERT(ctx.AuditableRequest(MakeRequest("PUT", "/path")));
+        UNIT_ASSERT(ctx.AuditableRequest(MakeRequest("DELETE", "/path")));
+
+        UNIT_ASSERT(ctx.AuditableRequest(MakeRequest("POST", "/counters")));
+        UNIT_ASSERT(ctx.AuditableRequest(MakeRequest("PUT", "/counters")));
+        UNIT_ASSERT(ctx.AuditableRequest(MakeRequest("DELETE", "/counters")));
+    }
+
+    Y_UNIT_TEST(OptionsRequestsAreNotAudited) {
+        TAuditCtx ctx;
+        UNIT_ASSERT(!ctx.AuditableRequest(MakeRequest("OPTIONS", "/path")));
+    }
+
+    Y_UNIT_TEST(BlacklistedPathsAreNotAudited) {
+        TAuditCtx ctx;
+        UNIT_ASSERT(!ctx.AuditableRequest(MakeRequest("GET", "/counters")));
+        UNIT_ASSERT(!ctx.AuditableRequest(MakeRequest("GET", "/viewer/subpage")));
+        UNIT_ASSERT(!ctx.AuditableRequest(MakeRequest("GET", "/viewer?mode=overview")));
+        UNIT_ASSERT(!ctx.AuditableRequest(MakeRequest("GET", "/monitoring/cluster/static/js/24615.12b53f26.chunk.js")));
+    }
+
+    Y_UNIT_TEST(OtherGetRequestsAreAudited) {
+        TAuditCtx ctx;
+        UNIT_ASSERT(ctx.AuditableRequest(MakeRequest("GET", "/other")));
+        UNIT_ASSERT(ctx.AuditableRequest(MakeRequest("GET", "/viewerstats?mode=overview")));
     }
 }

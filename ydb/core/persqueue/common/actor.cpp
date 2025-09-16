8fa6b005bf5af1892f00de7787948df52aa86477
Aleksei Moiseitsev <swarmer@ydb.tech>
2025-09-16 14:11:24 +0000

    explicit

--- ydb/core/persqueue/common/actor.cpp
+++ ydb/core/persqueue/common/actor.cpp
@@ -3,7 +3,7 @@
 namespace NKikimr::NPQ {
 
 const TString& TConstantLogPrefix::GetLogPrefix() const {
-    if (!LogPrefix_) {
+    if (!LogPrefix_.Defined()) {
         LogPrefix_ = BuildLogPrefix();
     }
     return *LogPrefix_;

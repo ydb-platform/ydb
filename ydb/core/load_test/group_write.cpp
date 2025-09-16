c1b17477a210be97e431b57b2f185fbd75d23a7b
Robert Drynkin <robdrynkin@ydb.tech>
2025-09-16 13:09:48 +0200

    EXT-1508 Add load actor NumberOfRandomGroupsToPick (#24947)

--- ydb/core/load_test/group_write.cpp
+++ ydb/core/load_test/group_write.cpp
@@ -17,6 +17,7 @@
 #include <util/generic/set.h>
 #include <util/system/type_name.h>
 #include <util/random/fast.h>
+#include <util/random/shuffle.h>
 
 namespace NKikimr {
 
@@ -1280,6 +1281,13 @@ public:
                     initialAllocation, tracingThrottler));
 
                 WorkersInInitialState++;
+            }
+
+            ui32 numberOfRandomGroupsToPick = profile.GetNumberOfRandomGroupsToPick();
+            if (numberOfRandomGroupsToPick > 0 && numberOfRandomGroupsToPick < TabletWriters.size()) {
+                Shuffle(TabletWriters.begin(), TabletWriters.end());
+                TabletWriters.resize(numberOfRandomGroupsToPick);
+                WorkersInInitialState = numberOfRandomGroupsToPick;
             }
         }
     }

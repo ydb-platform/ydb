92e8ecaea6e3f7582ada651ca13587edb60551bc
Nikolay Shestakov <n.shestakov@gmail.com>
2025-09-17 12:33:29 +0500

    Update ydb/core/persqueue/pqtablet/common/constants.h
    Co-authored-by: ubyte <himeraster@gmail.com>

--- ydb/core/persqueue/pqtablet/common/constants.h
+++ ydb/core/persqueue/pqtablet/common/constants.h
@@ -14,6 +14,6 @@ static const ui32 MAX_BLOB_PART_SIZE = 500_KB;
 // Total time till kafka supportive partition deletion = AppData.KafkaProxyConfig.TransactionTimeoutMs + KAFKA_TRANSACTION_DELETE_DELAY_MS
 static const ui32 KAFKA_TRANSACTION_DELETE_DELAY_MS = TDuration::Hours(1).MilliSeconds(); // 1 hour;
 
-static constexpr char TMP_REQUEST_MARKER[] = "__TMP__REQUEST__MARKER__";
+сonstexpr char TMP_REQUEST_MARKER[] = "__TMP__REQUEST__MARKER__";
 
 }

f35e310ba44f5c7b24a983b8ae243a5da720c228
Pavel Velikhov <pavelvelikhov@localhost.localdomain>
2025-09-18 09:11:23 +0000

    Detect used after free and report instead of VERIFY

--- yql/essentials/core/yql_gc_transformer.cpp
+++ yql/essentials/core/yql_gc_transformer.cpp
@@ -29,6 +29,7 @@ public:
                 const auto range = ctx.UniqueNodes.equal_range(hash);
                 for (auto jt = range.first; range.second != jt;) {
                     if (jt->second == dead) {
+                        YQL_ENSURE(jt->second->UseCount()==0);
                         jt = ctx.UniqueNodes.erase(jt);
                     } else {
                         ++jt;

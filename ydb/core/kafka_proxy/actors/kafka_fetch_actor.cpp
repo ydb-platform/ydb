e7020fcbf787ab6d92587cdb58ef853374d97057
Irina Skvortsova <nerhneiro@yandex-team.ru>
2025-09-19 21:58:33 +0000

    consumer autocreation commit_offset not validated fetch

--- ydb/core/kafka_proxy/actors/kafka_fetch_actor.cpp
+++ ydb/core/kafka_proxy/actors/kafka_fetch_actor.cpp
@@ -60,7 +60,8 @@ void TKafkaFetchActor::PrepareFetchRequestData(const size_t topicIndex, TVector<
         partPQRequest.Partition = partKafkaRequest.Partition;
         partPQRequest.Offset = partKafkaRequest.FetchOffset;
         partPQRequest.MaxBytes = partKafkaRequest.PartitionMaxBytes;
-        partPQRequest.ClientId = Context->GroupId.empty() ? NKikimr::NPQ::CLIENTID_WITHOUT_CONSUMER : Context->GroupId;
+        // partPQRequest.ClientId = Context->GroupId.empty() ? NKikimr::NPQ::CLIENTID_WITHOUT_CONSUMER : Context->GroupId;
+        partPQRequest.ClientId = "consumer1";
     }
 }
 

1a0cd9d7d616069accacdda1215757b4d3cc3b23
Alexander Rutkovsky <alexvru@ydb.tech>
2025-09-24 13:42:30 +0300

    Fix gRPC server runner (#25700)

--- ydb/core/driver_lib/run/run.cpp
+++ ydb/core/driver_lib/run/run.cpp
@@ -1975,7 +1975,9 @@ void TKikimrRunner::KikimrStart() {
         Monitoring->Start(ActorSystem.Get());
     }
 
-    GRpcServersManager = ActorSystem->Register(new TGRpcServersManager(std::move(GRpcServersFactory), ProcessMemoryInfoProvider));
+    if (GRpcServersFactory) {
+        GRpcServersManager = ActorSystem->Register(new TGRpcServersManager(std::move(GRpcServersFactory), ProcessMemoryInfoProvider));
+    }
 
     if (SqsHttp) {
         SqsHttp->Start();
@@ -2080,9 +2082,11 @@ void TKikimrRunner::KikimrStop(bool graceful) {
     }
 
     // stop processing grpc requests/response - we must stop feeding ActorSystem
-    TManualEvent event;
-    ActorSystem->Send(new IEventHandle(GRpcServersManager, {}, new TGRpcServersManager::TEvStop(&event)));
-    event.WaitI();
+    if (GRpcServersManager) {
+        TManualEvent event;
+        ActorSystem->Send(new IEventHandle(GRpcServersManager, {}, new TGRpcServersManager::TEvStop(&event)));
+        event.WaitI();
+    }
 
     if (ActorSystem) {
         ActorSystem->Stop();

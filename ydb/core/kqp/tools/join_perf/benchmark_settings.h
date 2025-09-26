52bdd359a70a4a4f8ce559879b60086d27f30b78
Ivan Lykov <becalm@ydb.tech>
2025-09-26 11:21:44 +0000

    try to reduce noise in benchmarks

--- ydb/core/kqp/tools/join_perf/benchmark_settings.h
+++ ydb/core/kqp/tools/join_perf/benchmark_settings.h
@@ -12,25 +12,25 @@ struct TTableSizes {
     int Left;
     int Right;
 };
+struct TPreset {
+    TVector<TTableSizes> Cases;
+    TString PresetName;
+};
 
 struct TBenchmarkSettings {
-    struct TPreset {
-        TVector<TTableSizes> Cases;
-        TString PresetName;
-    };
 
     TVector<TPreset> Presets;
     TSet<ETestedJoinKeyType> KeyTypes;
     TSet<ETestedJoinAlgo> Algorithms;
 };
 
-TString CaseName(ETestedJoinAlgo algo, ETestedJoinKeyType keyType, const TBenchmarkSettings::TPreset& preset,
+TString CaseName(ETestedJoinAlgo algo, ETestedJoinKeyType keyType, const TPreset& preset,
                  TTableSizes size);
 
 namespace NBenchmarkSizes {
-TVector<TTableSizes> ExponentialSizeIncrease();
-TVector<TTableSizes> LinearSizeIncrease();
-TVector<TTableSizes> VerySmallSizes();
+TPreset ExponentialSizeIncrease(int samples, int scale);
+TPreset LinearSizeIncrease(int samples, int scale);
+TPreset VerySmallSizes(int samples, int scale);
 } // namespace NBenchmarkSizes
 
 } // namespace NKikimr::NMiniKQL

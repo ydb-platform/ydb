cc104ef2f41e8f7d280e9272fc996f2042212781
Matveev Sergei <xyligansereja@yandex-team.ru>
2025-09-28 21:16:13 +0000

    filter processing columns

--- ydb/core/tx/columnshard/engines/reader/abstract/read_metadata.h
+++ ydb/core/tx/columnshard/engines/reader/abstract/read_metadata.h
@@ -195,9 +195,19 @@ public:
 
     std::set<ui32> GetProcessingColumnIds() const {
         AFL_VERIFY(ResultIndexSchema);
-        std::set<ui32> result(GetProgram().GetProcessingColumns().begin(), GetProgram().GetProcessingColumns().end());
+        std::set<ui32> result;
+        const auto& indexInfo = ResultIndexSchema->GetIndexInfo();
+        for (auto&& columnId : GetProgram().GetProcessingColumns()) {
+            if (indexInfo.HasColumnId(columnId)) {
+                result.emplace(columnId);
+            } else {
+                AFL_WARN(NKikimrServices::TX_COLUMNSHARD_SCAN)("event", "skip_unknown_processing_column")("column_id", columnId);
+            }
+        }
+
         return result;
     }
+
     bool IsAscSorted() const {
         return Sorting == ESorting::ASC;
     }

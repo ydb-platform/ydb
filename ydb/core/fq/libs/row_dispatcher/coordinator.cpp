3bcd2d057d945a9720d58ec9a18f89ca00fd175f
Dmitry Kardymon <kardymon-d@ydb.tech>
2025-09-29 08:17:47 +0000

    Turn off internal state logs

--- ydb/core/fq/libs/row_dispatcher/coordinator.cpp
+++ ydb/core/fq/libs/row_dispatcher/coordinator.cpp
@@ -257,7 +257,7 @@ void TActorCoordinator::Bootstrap() {
     Become(&TActorCoordinator::StateFunc);
     Send(LocalRowDispatcherId, new NFq::TEvRowDispatcher::TEvCoordinatorChangesSubscribe());
     ScheduleNodeInfoRequest();
-    Schedule(TDuration::Seconds(PrintStatePeriodSec), new TEvPrivate::TEvPrintState());
+    // Schedule(TDuration::Seconds(PrintStatePeriodSec), new TEvPrivate::TEvPrintState());  // Logs (InternalState) is too big
     LOG_ROW_DISPATCHER_DEBUG("Successfully bootstrapped coordinator, id " << SelfId());
     auto nodeGroup = Metrics.Counters->GetSubgroup("node", ToString(SelfId().NodeId()));
     Metrics.IsActive = nodeGroup->GetCounter("IsActive");

69051efa3e4e0b30263402081efcec4fea1685c0
Nikolay Shestakov <tesseract@ydb.tech>
2025-09-29 10:50:40 +0000

    WIP

--- ydb/core/kafka_proxy/actors/actors.h
+++ ydb/core/kafka_proxy/actors/actors.h
@@ -121,7 +121,7 @@ inline EKafkaErrors ConvertErrorCode(NPersQueue::NErrorCode::EErrorCode code) {
         case NPersQueue::NErrorCode::EErrorCode::READ_TIMEOUT:
             return EKafkaErrors::REQUEST_TIMED_OUT;
         case NPersQueue::NErrorCode::EErrorCode::READ_NOT_DONE:
-            //TODO
+            return EKafkaErrors::NONE_ERROR;
         case NPersQueue::NErrorCode::EErrorCode::TABLET_PIPE_DISCONNECTED:
             return EKafkaErrors::NOT_LEADER_OR_FOLLOWER;
         default:

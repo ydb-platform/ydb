95fcef4ee641406f56ca466f5e7c2da5d8c194fe
Pisarenko Grigoriy <grigoriypisar@ydb.tech>
2025-09-29 22:30:51 +0500

    YQ-4736 optimized json parser memory usage (#25907)

--- ydb/core/fq/libs/row_dispatcher/format_handler/filters/purecalc_filter.cpp
+++ ydb/core/fq/libs/row_dispatcher/format_handler/filters/purecalc_filter.cpp
@@ -75,7 +75,7 @@ NYT::TNode MakeWatermarkOutputSchema() {
 }
 
 struct TInputType {
-    const TVector<const TVector<NYql::NUdf::TUnboxedValue>*>& Values;
+    const TVector<std::span<NYql::NUdf::TUnboxedValue>>& Values;
     ui64 NumberRows;
 };
 
@@ -152,8 +152,9 @@ public:
 
                 items[OffsetPosition] = NYql::NUdf::TUnboxedValuePod(rowId);
 
-                for (ui64 fieldId = 0; const auto column : input.Values) {
-                    items[FieldsPositions[fieldId++]] = column->at(rowId);
+                for (ui64 fieldId = 0; const auto& column : input.Values) {
+                    Y_DEBUG_ABORT_UNLESS(column.size() > rowId);
+                    items[FieldsPositions[fieldId++]] = column[rowId];
                 }
 
                 Worker->Push(std::move(result));
@@ -415,7 +416,7 @@ public:
         ActiveFilters_->Dec();
     }
 
-    void ProcessData(const TVector<const TVector<NYql::NUdf::TUnboxedValue>*>& values, ui64 numberRows) const override {
+    void ProcessData(const TVector<std::span<NYql::NUdf::TUnboxedValue>>& values, ui64 numberRows) const override {
         LOG_ROW_DISPATCHER_TRACE("ProcessData for " << numberRows << " rows");
 
         if (!ProgramHolder_) {

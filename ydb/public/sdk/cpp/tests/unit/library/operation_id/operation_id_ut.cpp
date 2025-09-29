689f5b5daf43d518b7c19f5b73ba072e0c6f59b6
Bulat Gayazov <bylatgr@gmail.com>
2025-09-29 23:44:25 +0000

    [C++ SDK] Fixed fuzzing crush on invalid operation id

--- ydb/public/sdk/cpp/tests/unit/library/operation_id/operation_id_ut.cpp
+++ ydb/public/sdk/cpp/tests/unit/library/operation_id/operation_id_ut.cpp
@@ -1,124 +1,126 @@
 #include <ydb/public/sdk/cpp/include/ydb-cpp-sdk/library/operation_id/operation_id.h>
 #include <ydb/public/sdk/cpp/src/library/operation_id/protos/operation_id.pb.h>
 
-#include <library/cpp/testing/unittest/tests_data.h>
-#include <library/cpp/testing/unittest/registar.h>
+#include <util/generic/yexception.h>
 
-namespace NKikimr {
-namespace NOperationId {
+#include <gtest/gtest.h>
 
-Y_UNIT_TEST_SUITE(OperationIdTest) {
-    const std::string PreparedQueryId = "9d629c27-2c3036b3-4b180476-64435bca";
+namespace NYdb::NOperationId {
 
-    Y_UNIT_TEST(ConvertKindOnly) {
-        Ydb::TOperationId proto;
-        proto.set_kind(Ydb::TOperationId::OPERATION_DDL);
-        auto str = ProtoToString(proto);
-        UNIT_ASSERT_EQUAL(str, "ydb://operation/1");
-        auto newProto = TOperationId(str);
-        UNIT_ASSERT_EQUAL(newProto.GetProto().kind(), proto.kind());
-        UNIT_ASSERT_EQUAL(newProto.GetProto().data_size(), 0);
-    }
+const std::string PreparedQueryId = "9d629c27-2c3036b3-4b180476-64435bca";
 
-    Y_UNIT_TEST(PreparedQueryIdCompatibleFormatter) {
-        Ydb::TOperationId opId;
-        opId.set_kind(Ydb::TOperationId::PREPARED_QUERY_ID);
-        AddOptionalValue(opId, "id", PreparedQueryId);
-        auto result = ProtoToString(opId);
-        UNIT_ASSERT_VALUES_EQUAL(FormatPreparedQueryIdCompat(PreparedQueryId), result);
-    }
+TEST(OperationIdTest, ConvertKindOnly) {
+    Ydb::TOperationId proto;
+    proto.set_kind(Ydb::TOperationId::OPERATION_DDL);
+    auto str = NKikimr::NOperationId::ProtoToString(proto);
+    ASSERT_EQ(str, "ydb://operation/1");
+    auto newProto = NKikimr::NOperationId::TOperationId(str);
+    ASSERT_EQ(newProto.GetProto().kind(), proto.kind());
+    ASSERT_EQ(newProto.GetProto().data_size(), 0);
+}
 
-    Y_UNIT_TEST(PreparedQueryIdDecode) {
-        const auto queryId = FormatPreparedQueryIdCompat(PreparedQueryId);
-        std::string decodedString;
-        bool decoded = DecodePreparedQueryIdCompat(queryId, decodedString);
-        UNIT_ASSERT(decoded);
-        UNIT_ASSERT_VALUES_EQUAL(PreparedQueryId, decodedString);
-    }
+TEST(OperationIdTest, PreparedQueryIdCompatibleFormatter) {
+    Ydb::TOperationId opId;
+    opId.set_kind(Ydb::TOperationId::PREPARED_QUERY_ID);
+    NKikimr::NOperationId::AddOptionalValue(opId, "id", PreparedQueryId);
+    auto result = NKikimr::NOperationId::ProtoToString(opId);
+    ASSERT_EQ(NKikimr::NOperationId::FormatPreparedQueryIdCompat(PreparedQueryId), result);
+}
 
-    Y_UNIT_TEST(PreparedQueryIdDecodeRawString) {
-        std::string decodedString;
-        bool decoded = DecodePreparedQueryIdCompat(PreparedQueryId, decodedString);
-        UNIT_ASSERT(!decoded);
-        UNIT_ASSERT(decodedString.empty());
-    }
+TEST(OperationIdTest, PreparedQueryIdDecode) {
+    const auto queryId = NKikimr::NOperationId::FormatPreparedQueryIdCompat(PreparedQueryId);
+    std::string decodedString;
+    bool decoded = NKikimr::NOperationId::DecodePreparedQueryIdCompat(queryId, decodedString);
+    ASSERT_TRUE(decoded);
+    ASSERT_EQ(PreparedQueryId, decodedString);
+}
 
-    Y_UNIT_TEST(PreparedQueryIdDecodeInvalidString) {
-        std::string decodedString;
-        UNIT_ASSERT_EXCEPTION(
-            DecodePreparedQueryIdCompat(std::string("ydb://preparedqueryid/4?id="), decodedString), yexception);
-        UNIT_ASSERT(decodedString.empty());
-    }
+TEST(OperationIdTest, PreparedQueryIdDecodeRawString) {
+    std::string decodedString;
+    bool decoded = NKikimr::NOperationId::DecodePreparedQueryIdCompat(PreparedQueryId, decodedString);
+    ASSERT_FALSE(decoded);
+    ASSERT_TRUE(decodedString.empty());
+}
 
-    Y_UNIT_TEST(FormatPrefixShorter) {
-        UNIT_ASSERT(std::string("ydb://preparedqueryid/4?id=").size() < PreparedQueryId.size());
-    }
+TEST(OperationIdTest, PreparedQueryIdDecodeInvalidString) {
+    std::string decodedString;
+    ASSERT_THROW(
+        NKikimr::NOperationId::DecodePreparedQueryIdCompat("ydb://preparedqueryid/4?id=", decodedString), yexception);
+    ASSERT_TRUE(decodedString.empty());
+}
+
+TEST(OperationIdTest, FormatPrefixShorter) {
+    ASSERT_TRUE(std::strlen("ydb://preparedqueryid/4?id=") < PreparedQueryId.size());
+}
 
 #if 0
-    Y_UNIT_TEST(PreparedQueryIdCompatibleFormatterPerf) {
-        ui64 x = 0;
-        for (int i = 0; i < 10000000; i++) {
-            auto result = FormatPreparedQueryIdCompat(PreparedQueryId);
-            x += result.size();
-        }
-        std::cerr << x << std::endl;
+TEST(OperationIdTest, PreparedQueryIdCompatibleFormatterPerf) {
+    ui64 x = 0;
+    for (int i = 0; i < 10000000; i++) {
+        auto result = NKikimr::NOperationId::FormatPreparedQueryIdCompat(PreparedQueryId);
+        x += result.size();
     }
+    std::cerr << x << std::endl;
+}
 
-    Y_UNIT_TEST(PreparedQueryIdDecodePerf) {
-        ui64 x = 0;
-        for (int i = 0; i < 10000000; i++) {
-            const auto queryId = FormatPreparedQueryIdCompat(PreparedQueryId);
-            std::string decodedString;
-            bool decoded = DecodePreparedQueryIdCompat(queryId, decodedString);
-            UNIT_ASSERT(decoded);
-            UNIT_ASSERT_VALUES_EQUAL(PreparedQueryId, decodedString);
-            x += decodedString.size();
-        }
-        std::cerr << x << std::endl;
+TEST(OperationIdTest, PreparedQueryIdDecodePerf) {
+    ui64 x = 0;
+    for (int i = 0; i < 10000000; i++) {
+        const auto queryId = NKikimr::NOperationId::FormatPreparedQueryIdCompat(PreparedQueryId);
+        std::string decodedString;
+        bool decoded = NKikimr::NOperationId::DecodePreparedQueryIdCompat(queryId, decodedString);
+        ASSERT_TRUE(decoded);
+        ASSERT_EQ(PreparedQueryId, decodedString);
+        x += decodedString.size();
     }
+    std::cerr << x << std::endl;
+}
 
-    Y_UNIT_TEST(PreparedQueryIdOldFormatterPerf) {
-        ui64 x = 0;
-        for (int i = 0; i < 10000000; i++) {
-            Ydb::TOperationId opId;
-            opId.SetKind(Ydb::TOperationId::PREPARED_QUERY_ID);
-            AddOptionalValue(opId, "id", PreparedQueryId);
-            auto result = ProtoToString(opId);
-            x += result.size();
-        }
-        std::cerr << x << std::endl;
+TEST(OperationIdTest, PreparedQueryIdOldFormatterPerf) {
+    ui64 x = 0;
+    for (int i = 0; i < 10000000; i++) {
+        Ydb::TOperationId opId;
+        opId.SetKind(Ydb::TOperationId::PREPARED_QUERY_ID);
+        NKikimr::NOperationId::AddOptionalValue(opId, "id", PreparedQueryId);
+        auto result = NKikimr::NOperationId::ProtoToString(opId);
+        x += result.size();
     }
+    std::cerr << x << std::endl;
+}
 #endif
-    Y_UNIT_TEST(ConvertKindAndValues) {
-        Ydb::TOperationId proto;
-        proto.set_kind(Ydb::TOperationId::OPERATION_DDL);
-        {
-            auto data = proto.add_data();
-            data->set_key("key1");
-            data->set_value("value1");
-        }
-        {
-            auto data = proto.add_data();
-            data->set_key("txId");
-            data->set_value("42");
-        }
-        auto str = ProtoToString(proto);
-        UNIT_ASSERT_EQUAL(str, "ydb://operation/1?key1=value1&txId=42");
-        auto newProto = TOperationId(str);
-        UNIT_ASSERT_EQUAL(newProto.GetProto().kind(), proto.kind());
-        UNIT_ASSERT_EQUAL(newProto.GetProto().data_size(), 2);
-        {
-            auto data = newProto.GetProto().data(0);
-            UNIT_ASSERT_EQUAL(data.key(), "key1");
-            UNIT_ASSERT_EQUAL(data.value(), "value1");
-        }
-        {
-            auto data = newProto.GetProto().data(1);
-            UNIT_ASSERT_EQUAL(data.key(), "txId");
-            UNIT_ASSERT_EQUAL(data.value(), "42");
-        }
+
+TEST(OperationIdTest, ConvertKindAndValues) {
+    Ydb::TOperationId proto;
+    proto.set_kind(Ydb::TOperationId::OPERATION_DDL);
+    {
+        auto data = proto.add_data();
+        data->set_key("key1");
+        data->set_value("value1");
+    }
+    {
+        auto data = proto.add_data();
+        data->set_key("txId");
+        data->set_value("42");
+    }
+    auto str = NKikimr::NOperationId::ProtoToString(proto);
+    ASSERT_EQ(str, "ydb://operation/1?key1=value1&txId=42");
+    auto newProto = NKikimr::NOperationId::TOperationId(str);
+    ASSERT_EQ(newProto.GetProto().kind(), proto.kind());
+    ASSERT_EQ(newProto.GetProto().data_size(), 2);
+    {
+        auto data = newProto.GetProto().data(0);
+        ASSERT_EQ(data.key(), "key1");
+        ASSERT_EQ(data.value(), "value1");
+    }
+    {
+        auto data = newProto.GetProto().data(1);
+        ASSERT_EQ(data.key(), "txId");
+        ASSERT_EQ(data.value(), "42");
     }
 }
 
-} // namespace NOperationId
-} // namespace NKikimr
+TEST(OperationIdTest, InvalidOperationId) {
+    ASSERT_THROW(NKikimr::NOperationId::TOperationId("ydb://preparedqueryid"), yexception);
+}
+
+}

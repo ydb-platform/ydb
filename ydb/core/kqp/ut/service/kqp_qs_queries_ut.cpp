1e130fc6ddbb5f36d92763bdcec2a4273d95be1e
Oleg Doronin <dorooleg@yandex.ru>
2025-09-30 15:24:41 +0200

    cs validation has been fixed (#25903)

--- ydb/core/kqp/ut/service/kqp_qs_queries_ut.cpp
+++ ydb/core/kqp/ut/service/kqp_qs_queries_ut.cpp
@@ -3672,10 +3672,6 @@ Y_UNIT_TEST_SUITE(KqpQueryService) {
         TKikimrRunner kikimr(serverSettings);
         auto db = kikimr.GetQueryClient();
 
-        if (IsOlap) {
-            return;
-        }
-
         {
             auto result = db.ExecuteQuery(Sprintf(R"(
                 CREATE TABLE Table (

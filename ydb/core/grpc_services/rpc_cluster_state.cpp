c239a886cbb1f4d7d5bcb314b74832a2cde87385
Evgenik2 <evkam@mail.ru>
2025-10-02 09:29:36 +0300

    collect from whiteboard

--- ydb/core/grpc_services/rpc_cluster_state.cpp
+++ ydb/core/grpc_services/rpc_cluster_state.cpp
@@ -6,7 +6,6 @@
 
 #include <ydb/core/grpc_services/base/base.h>
 #include <ydb/core/mon/mon.h>
-#include <ydb/core/viewer/viewer.h>
 #include <yql/essentials/public/issue/yql_issue_message.h>
 #include <yql/essentials/public/issue/yql_issue.h>
 
@@ -19,6 +18,9 @@
 
 #include <ydb/core/health_check/health_check.h>
 #include <ydb/public/api/protos/ydb_monitoring.pb.h>
+#include <ydb/core/node_whiteboard/node_whiteboard.h>
+#include <google/protobuf/util/json_util.h>
+#include <ydb/core/health_check/health_check.h>
 
 namespace NKikimr {
 namespace NGRpcService {
@@ -31,20 +33,130 @@ using TEvClusterStateRequest = TGrpcRequestOperationCall<Ydb::Monitoring::Cluste
 class TClusterStateRPC : public TRpcRequestActor<TClusterStateRPC, TEvClusterStateRequest, true> {
 public:
     using TRpcRequestActor::TRpcRequestActor;
+    using TThis = TClusterStateRPC;
+    using TBase = TRpcRequestActor<TClusterStateRPC, TEvClusterStateRequest, true>;
 
-    TString CountersResult;
+    ui32 Requested;
+    ui32 Received;
+    TVector<TEvInterconnect::TNodeInfo> Nodes;
+    TMap<ui32, NKikimrWhiteboard::TEvVDiskStateResponse> VDiskInfo;
+    TMap<ui32, NKikimrWhiteboard::TEvPDiskStateResponse> PDiskInfo;
+    TMap<ui32, NKikimrWhiteboard::TEvTabletStateResponse> TabletInfo;
+    TMap<ui32, NKikimrWhiteboard::TEvBSGroupStateResponse> BSGroupInfo;
+    Ydb::Monitoring::SelfCheckResult SelfCheck;
     Ydb::StatusIds_StatusCode Status = Ydb::StatusIds::SUCCESS;
 
-    void SendClusterStateRequests() {
-        NMonitoring::TMonService2HttpRequest monReq(nullptr, nullptr, nullptr, nullptr, "/counters", nullptr);
-        auto request = MakeHolder<NMon::TEvHttpInfo>(monReq);
-        Send(NKikimr::NViewer::MakeViewerID(0), request.Release());
+    void SendRequest(ui32 nodeId) {
+        TActorId whiteboardServiceId = NNodeWhiteboard::MakeNodeWhiteboardServiceId(nodeId);
+        Send(whiteboardServiceId, new NNodeWhiteboard::TEvWhiteboard::TEvVDiskStateRequest(), IEventHandle::FlagTrackDelivery | IEventHandle::FlagSubscribeOnSession, nodeId);
+        Send(whiteboardServiceId, new NNodeWhiteboard::TEvWhiteboard::TEvPDiskStateRequest(), IEventHandle::FlagTrackDelivery | IEventHandle::FlagSubscribeOnSession, nodeId);
+        Send(whiteboardServiceId, new NNodeWhiteboard::TEvWhiteboard::TEvTabletStateRequest(), IEventHandle::FlagTrackDelivery | IEventHandle::FlagSubscribeOnSession, nodeId);
+        Send(whiteboardServiceId, new NNodeWhiteboard::TEvWhiteboard::TEvBSGroupStateRequest(), IEventHandle::FlagTrackDelivery | IEventHandle::FlagSubscribeOnSession, nodeId);
+        Requested += 4;
+    }
 
-        Become(&TThis::StateWaitResponses);
+    void HandleBrowse(TEvInterconnect::TEvNodesInfo::TPtr& ev) {
+        RequestHealthCheck();
+        Nodes = ev->Get()->Nodes;
+        for (const auto& ni : Nodes) {
+            SendRequest(ni.NodeId);
+        }
+        if (Requested > 0) {
+            TBase::Become(&TThis::StateRequestedNodeInfo);
+        } else {
+            ReplyAndPassAway();
+        }
+    }
+
+    void Undelivered(TEvents::TEvUndelivered::TPtr &ev) {
+        ui32 nodeId = ev.Get()->Cookie;
+        switch (ev->Get()->SourceType) {
+        case NNodeWhiteboard::TEvWhiteboard::EvVDiskStateRequest:
+            if (VDiskInfo.emplace(nodeId, NKikimrWhiteboard::TEvVDiskStateResponse{}).second) {
+                NodeStateInfoReceived();
+            }
+            break;
+        case NNodeWhiteboard::TEvWhiteboard::EvPDiskStateRequest:
+            if (PDiskInfo.emplace(nodeId, NKikimrWhiteboard::TEvPDiskStateResponse{}).second) {
+                NodeStateInfoReceived();
+            }
+            break;
+        case NNodeWhiteboard::TEvWhiteboard::EvTabletStateRequest:
+            if (TabletInfo.emplace(nodeId, NKikimrWhiteboard::TEvTabletStateResponse{}).second) {
+                NodeStateInfoReceived();
+            }
+            break;
+        case NNodeWhiteboard::TEvWhiteboard::EvBSGroupStateRequest:
+            if (BSGroupInfo.emplace(nodeId, NKikimrWhiteboard::TEvBSGroupStateResponse{}).second) {
+                NodeStateInfoReceived();
+            }
+            break;
+        }
+    }
+
+    void Disconnected(TEvInterconnect::TEvNodeDisconnected::TPtr &ev) {
+        ui32 nodeId = ev->Get()->NodeId;
+        if (VDiskInfo.emplace(nodeId, NKikimrWhiteboard::TEvVDiskStateResponse{}).second) {
+            NodeStateInfoReceived();
+        }
+        if (PDiskInfo.emplace(nodeId, NKikimrWhiteboard::TEvPDiskStateResponse{}).second) {
+            NodeStateInfoReceived();
+        }
+        if (TabletInfo.emplace(nodeId, NKikimrWhiteboard::TEvTabletStateResponse{}).second) {
+            NodeStateInfoReceived();
+        }
+        if (BSGroupInfo.emplace(nodeId, NKikimrWhiteboard::TEvBSGroupStateResponse{}).second) {
+            NodeStateInfoReceived();
+        }
+    }
+
+    void Handle(NNodeWhiteboard::TEvWhiteboard::TEvVDiskStateResponse::TPtr& ev) {
+        ui64 nodeId = ev.Get()->Cookie;
+        VDiskInfo[nodeId] = std::move(ev->Get()->Record);
+        NodeStateInfoReceived();
+    }
+
+    void Handle(NNodeWhiteboard::TEvWhiteboard::TEvPDiskStateResponse::TPtr& ev) {
+        ui64 nodeId = ev.Get()->Cookie;
+        PDiskInfo[nodeId] = std::move(ev->Get()->Record);
+        NodeStateInfoReceived();
+    }
+
+    void Handle(NNodeWhiteboard::TEvWhiteboard::TEvTabletStateResponse::TPtr& ev) {
+        ui64 nodeId = ev.Get()->Cookie;
+        TabletInfo[nodeId] = std::move(ev->Get()->Record);
+        NodeStateInfoReceived();
+    }
+
+    void Handle(NNodeWhiteboard::TEvWhiteboard::TEvBSGroupStateResponse::TPtr& ev) {
+        ui64 nodeId = ev.Get()->Cookie;
+        BSGroupInfo[nodeId] = std::move(ev->Get()->Record);
+        NodeStateInfoReceived();
+    }
+
+    void NodeStateInfoReceived() {
+        ++Received;
+        if (Received == Requested) {
+            ReplyAndPassAway();
+        }
+    }
+
+    void Handle(NHealthCheck::TEvSelfCheckResult::TPtr& ev) {
+        SelfCheck = std::move(ev->Get()->Result);
+        NodeStateInfoReceived();
+    }
+
+    void RequestHealthCheck() {
+        THolder<NHealthCheck::TEvSelfCheckRequest> request = MakeHolder<NHealthCheck::TEvSelfCheckRequest>();
+        Send(NHealthCheck::MakeHealthCheckID(), request.Release());
+        Requested++;
     }
 
     void Bootstrap() {
-        SendClusterStateRequests();
+        const TActorId nameserviceId = GetNameserviceActorId();
+        Send(nameserviceId, new TEvInterconnect::TEvListNodes());
+        TBase::Become(&TThis::StateRequestedBrowse);
+
         ui32 duration = 60;
         if (GetProtoRequest()->duration()) {
             duration = GetProtoRequest()->duration();
@@ -53,22 +165,34 @@ public:
     }
 
     void Timeout() {
-
         ReplyAndPassAway();
     }
 
-    STATEFN(StateWaitResponses) {
+    void Die(const TActorContext& ctx) override {
+        for (const auto& ni : Nodes) {
+            ctx.Send(TActivationContext::InterconnectProxy(ni.NodeId), new TEvents::TEvUnsubscribe());
+        }
+        TBase::Die(ctx);
+    }
+
+    STFUNC(StateRequestedBrowse) {
         switch (ev->GetTypeRewrite()) {
-            hFunc(NMon::TEvHttpInfoRes, Handle);
+            hFunc(TEvInterconnect::TEvNodesInfo, HandleBrowse);
             cFunc(TEvents::TSystem::Wakeup, Timeout);
         }
     }
 
-    void Handle(NMon::TEvHttpInfoRes::TPtr& ev) {
-        Status = Ydb::StatusIds::SUCCESS;
-        NMon::TEvHttpInfoRes *ptr = dynamic_cast<NMon::TEvHttpInfoRes*>(ev->Get());
-        CountersResult = std::move(ptr->Answer);
-        ReplyAndPassAway();
+    STFUNC(StateRequestedNodeInfo) {
+        switch (ev->GetTypeRewrite()) {
+            hFunc(NNodeWhiteboard::TEvWhiteboard::TEvVDiskStateResponse, Handle);
+            hFunc(NNodeWhiteboard::TEvWhiteboard::TEvPDiskStateResponse, Handle);
+            hFunc(NNodeWhiteboard::TEvWhiteboard::TEvTabletStateResponse, Handle);
+            hFunc(NNodeWhiteboard::TEvWhiteboard::TEvBSGroupStateResponse, Handle);
+            hFunc(TEvents::TEvUndelivered, Undelivered);
+            hFunc(TEvInterconnect::TEvNodeDisconnected, Disconnected);
+            cFunc(TEvents::TSystem::Wakeup, Timeout);
+            hFunc(NHealthCheck::TEvSelfCheckResult, Handle);
+        }
     }
 
     void ReplyAndPassAway() {
@@ -78,9 +202,29 @@ public:
         operation.set_status(Status);
         Ydb::Monitoring::ClusterStateResult result;
         TStringBuilder res;
-        res << "{\"Counters\": {\n";
-        res << CountersResult;
-        res << "}}";
+        res << "{\n";
+        auto serializeDict = [&](const char *name, auto &info) {
+            res << "\"" << name <<"\" : {";
+            for(auto &[k, v] : info) {
+                TString data;
+                google::protobuf::util::MessageToJsonString(v, &data);
+                res << "{" << k << ":" << data << "},\n";
+            }
+            res << "}\n";
+        };
+        auto serialize = [&](const char *name, auto &info) {
+            res << "\"" << name <<"\": ";
+            TString data;
+            google::protobuf::util::MessageToJsonString(info, &data);
+            res << data << ",\n";
+        };
+        serializeDict("VDiskInfo", VDiskInfo);
+        serializeDict("PDiskInfo", PDiskInfo);
+        serializeDict("TabletInfo", TabletInfo);
+        serializeDict("BSGroupInfo", BSGroupInfo);
+        serialize("SelfCheck", SelfCheck);
+
+        res << "\"version\": 1}\n";
         result.Setresult(res);
         operation.mutable_result()->PackFrom(result);
         return Reply(response);

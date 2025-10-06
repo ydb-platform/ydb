9e729058d8ba7b05ae73a3bc6172f2a902a1ff51
Stanislav Yablonskiy <neyrox@ydb.tech>
2025-10-06 08:05:51 +0000

    Fix crash in TSyncPointLimitControl

--- ydb/core/tx/columnshard/engines/predicate/predicate.cpp
+++ ydb/core/tx/columnshard/engines/predicate/predicate.cpp
@@ -75,32 +75,6 @@ TString FromCells(const TConstArrayRef<TCell>& cells,
     return NArrow::SerializeBatchNoCompression(batch);
 }
 
-// TODO: try to use fields only
-TString FromCells(const TConstArrayRef<TCell>& cells,
-    const std::vector<std::pair<TString, NScheme::TTypeInfo>>& columns,
-    const std::vector<std::shared_ptr<arrow::Field>>& fields) {
-    Y_ABORT_UNLESS(cells.size() == columns.size());
-    if (cells.empty()) {
-        return {};
-    }
-
-    std::vector<NScheme::TTypeInfo> types = ExtractTypes(columns);
-
-    NArrow::TArrowBatchBuilder batchBuilder;
-    batchBuilder.Reserve(1);
-    auto schema = std::make_shared<arrow::Schema>(fields);
-    auto startStatus = batchBuilder.Start(columns, schema);
-    Y_ABORT_UNLESS(startStatus.ok(), "%s", startStatus.ToString().c_str());
-
-    batchBuilder.AddRow(NKikimr::TDbTupleRef(), NKikimr::TDbTupleRef(types.data(), cells.data(), cells.size()));
-
-    auto batch = batchBuilder.FlushBatch(false);
-    Y_ABORT_UNLESS(batch);
-    Y_ABORT_UNLESS(batch->num_columns() == (int)cells.size());
-    Y_ABORT_UNLESS(batch->num_rows() == 1);
-    return NArrow::SerializeBatchNoCompression(batch);
-}
-
 std::pair<NKikimr::NOlap::TPredicate, NKikimr::NOlap::TPredicate> TPredicate::DeserializePredicatesRange(
     const TSerializedTableRange& range, const std::vector<std::pair<TString, NScheme::TTypeInfo>>& columns, 
     const std::shared_ptr<arrow::Schema>& schema) {

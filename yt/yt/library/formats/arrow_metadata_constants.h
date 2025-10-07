c648e49fc90cc160673d2e993904bd0b3bc6e6cf
rp-1 <rp-1@yandex-team.com>
2025-10-07 16:43:05 +0300

    YT-25045: Add support for Arrow complex types commit_hash:5758a66421fefa3d48e88762c0e6a4bf87b9d5aa

---
+++ yt/yt/library/formats/arrow_metadata_constants.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <yt/yt/core/misc/common.h>
+
+namespace NYT::NFormats {
+
+////////////////////////////////////////////////////////////////////////////////
+
+static const std::string YtTypeMetadataKey = "YtType";
+static const std::string YtTypeMetadataValueEmptyStruct = "emptyStruct";
+static const std::string YtTypeMetadataValueNestedOptional = "nestedOptional";
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NFormats

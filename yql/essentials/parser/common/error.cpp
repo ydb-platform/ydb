343f6fb150d2e6f4316b082cee900527d4b33a25
vvvv <vvvv@yandex-team.com>
2025-10-08 14:35:50 +0300

    YQL-20086 parser commit_hash:dd605e5cde39ff07fdadf62f0c2364a8b12c8405

--- yql/essentials/parser/common/error.cpp
+++ yql/essentials/parser/common/error.cpp
@@ -2,41 +2,41 @@
 
 namespace NAST {
 
-    IErrorCollector::IErrorCollector(size_t maxErrors)
-        : MaxErrors_(maxErrors)
-        , NumErrors_(0)
-    {
-        Y_ENSURE(0 < MaxErrors_);
-    }
+IErrorCollector::IErrorCollector(size_t maxErrors)
+    : MaxErrors_(maxErrors)
+    , NumErrors_(0)
+{
+    Y_ENSURE(0 < MaxErrors_);
+}
 
-    IErrorCollector::~IErrorCollector()
-    {
-    }
+IErrorCollector::~IErrorCollector()
+{
+}
 
-    void IErrorCollector::Error(ui32 line, ui32 col, const TString& message) {
-        GuardTooManyErrors();
-        AddError(line, col, message);
+void IErrorCollector::Error(ui32 line, ui32 col, const TString& message) {
+    GuardTooManyErrors();
+    AddError(line, col, message);
+    ++NumErrors_;
+}
+
+void IErrorCollector::Report(NYql::TIssue&& issue) {
+    GuardTooManyErrors();
+    bool isError = issue.GetSeverity() >= NYql::TSeverityIds::S_WARNING;
+    AddIssue(std::forward<NYql::TIssue>(issue));
+    if (isError) {
+        ++NumErrors_;
+    }
+}
+
+void IErrorCollector::GuardTooManyErrors() {
+    if (NumErrors_ + 1 == MaxErrors_) {
+        AddError(0, 0, "Too many errors");
         ++NumErrors_;
     }
 
-    void IErrorCollector::Report(NYql::TIssue&& issue) {
-        GuardTooManyErrors();
-        bool isError = issue.GetSeverity() >= NYql::TSeverityIds::S_WARNING;
-        AddIssue(std::forward<NYql::TIssue>(issue));
-        if (isError) {
-            ++NumErrors_;
-        }
+    if (NumErrors_ >= MaxErrors_) {
+        ythrow TTooManyErrors() << "Too many errors";
     }
-
-    void IErrorCollector::GuardTooManyErrors() {
-        if (NumErrors_ + 1 == MaxErrors_) {
-            AddError(0, 0, "Too many errors");
-            ++NumErrors_;
-        }
-
-        if (NumErrors_ >= MaxErrors_) {
-            ythrow TTooManyErrors() << "Too many errors";
-        }
-    }
+}
 
 } // namespace NAST
--- yql/essentials/parser/common/issue.h
+++ yql/essentials/parser/common/issue.h
@@ -8,29 +8,29 @@
 
 namespace NSQLTranslation {
 
-    class TErrorCollectorOverIssues: public NAST::IErrorCollector {
-    public:
-        TErrorCollectorOverIssues(NYql::TIssues& issues, size_t maxErrors, const TString& file)
-            : IErrorCollector(maxErrors)
-            , Issues_(issues)
-            , File_(file)
-        {
-        }
+class TErrorCollectorOverIssues: public NAST::IErrorCollector {
+public:
+    TErrorCollectorOverIssues(NYql::TIssues& issues, size_t maxErrors, const TString& file)
+        : IErrorCollector(maxErrors)
+        , Issues_(issues)
+        , File_(file)
+    {
+    }
 
-    private:
-        void AddError(ui32 line, ui32 col, const TString& message) override {
-            Issues_.AddIssue(NYql::TPosition(col, line, File_), message);
-        }
+private:
+    void AddError(ui32 line, ui32 col, const TString& message) override {
+        Issues_.AddIssue(NYql::TPosition(col, line, File_), message);
+    }
 
-        void AddIssue(NYql::TIssue&& issue) override {
-            issue.Position.File = File_;
-            issue.EndPosition.File = File_;
-            Issues_.AddIssue(std::forward<NYql::TIssue>(issue));
-        }
+    void AddIssue(NYql::TIssue&& issue) override {
+        issue.Position.File = File_;
+        issue.EndPosition.File = File_;
+        Issues_.AddIssue(std::forward<NYql::TIssue>(issue));
+    }
 
-    private:
-        NYql::TIssues& Issues_;
-        const TString File_;
-    };
+private:
+    NYql::TIssues& Issues_;
+    const TString File_;
+};
 
 } // namespace NSQLTranslation

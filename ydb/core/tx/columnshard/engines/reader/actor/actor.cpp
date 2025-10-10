4b9917e9a52c0059f56545c917044f7627ad5507
xyliganSereja <matva0509@gmail.com>
2025-10-10 17:46:08 +0300

    fix counters (#26638)
    Co-authored-by: Matveev Sergei <xyligansereja@yandex-team.ru>

--- ydb/core/tx/columnshard/engines/reader/actor/actor.cpp
+++ ydb/core/tx/columnshard/engines/reader/actor/actor.cpp
@@ -105,6 +105,7 @@ void TColumnShardScan::HandleScan(NColumnShard::TEvPrivate::TEvTaskProcessedResu
 }
 
 void TColumnShardScan::HandleScan(NKqp::TEvKqpCompute::TEvScanDataAck::TPtr& ev) {
+    auto ackProcessingGuard = ScanCountersPool.GetResultsForReplyGuard();
     StartWaitTime = TInstant::Now();
     auto g = Stats->MakeGuard("ack", IS_INFO_LOG_ENABLED(NKikimrServices::TX_COLUMNSHARD_SCAN));
 
@@ -180,10 +181,10 @@ void TColumnShardScan::CheckHanging(const bool logging) const {
             "debug", ScanIterator ? ScanIterator->DebugString() : Default<TString>())("last", LastResultInstant);
     }
     const bool ok = !!FinishInstant || !ScanIterator || !ChunksLimiter.HasMore() || ScanCountersPool.InWaiting();
-    // AFL_VERIFY_DEBUG(ok)
-    // ("finished", ScanIterator->Finished())
-    // ("scan_actor_id", ScanActorId)("tx_id", TxId)("scan_id", ScanId)("gen", ScanGen)("tablet", TabletId)("debug", ScanIterator->DebugString())(
-    //     "counters", ScanCountersPool.DebugString());
+    AFL_VERIFY(ok)
+    ("finished", ScanIterator->Finished())
+    ("scan_actor_id", ScanActorId)("tx_id", TxId)("scan_id", ScanId)("gen", ScanGen)("tablet", TabletId)("debug", ScanIterator->DebugString())(
+        "counters", ScanCountersPool.DebugString());
     if (!ok) {
         AFL_CRIT(NKikimrServices::TX_COLUMNSHARD_SCAN)("error", "CheckHanging")("scan_actor_id", ScanActorId)("tx_id", TxId)("scan_id", ScanId)("gen", ScanGen)(
             "tablet", TabletId)("debug", ScanIterator->DebugString())("counters", ScanCountersPool.DebugString());

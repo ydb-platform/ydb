68f232e85cf70fdc97ef815eab1f45518f09a192
Lebedev Oleg <lebedev0leg@ydb.tech>
2025-10-11 21:35:58 +0000

    changed time3

--- ydb/tests/functional/backup_collection/basic_user_scenarios.py
+++ ydb/tests/functional/backup_collection/basic_user_scenarios.py
@@ -866,10 +866,12 @@ class TestFullCycleLocalBackupRestoreWIncr(BaseTestBackupInFiles):
 
         # Restore to incremental 1 (full1 + inc1)
         col_inc1 = f"restore_inc1_{int(time.time())}"
+        time.sleep(1.1)
         ts_inc1 = self.extract_ts(snap_inc1)
         self.import_exported_up_to_timestamp(col_inc1, ts_inc1, export_dir, full_orders, full_products)
         # ensure target tables absent
         self._remove_tables([full_orders, full_products])
+        time.sleep(1.1)
         rest_inc1 = self._execute_yql(f"RESTORE `{col_inc1}`;")
         time.sleep(1.1)
         assert rest_inc1.exit_code == 0, f"RESTORE inc1 failed: {rest_inc1.std_err}"

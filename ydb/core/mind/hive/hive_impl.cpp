6680e6eef53096074a5eac8c10af67752ea914c8
vporyadke <zalyalov@ydb.tech>
2025-10-13 12:45:51 +0200

    allow followers in non-primary piles (#25541)

--- ydb/core/mind/hive/hive_impl.cpp
+++ ydb/core/mind/hive/hive_impl.cpp
@@ -2993,7 +2993,7 @@ void THive::ProcessPendingResumeTablet() {
     }
 }
 
-bool THive::IsAllowedPile(TBridgePileId pile) const {
+bool THive::IsPrimaryPile(TBridgePileId pile) const {
     if (BridgeInfo->BeingPromotedPile) {
         return BridgeInfo->BeingPromotedPile->BridgePileId == pile;
     } else {
@@ -3004,6 +3004,14 @@ bool THive::IsAllowedPile(TBridgePileId pile) const {
 TBridgePileInfo& THive::GetPile(TBridgePileId pileId) {
     auto it = BridgePiles.emplace(pileId, pileId).first;
     return it->second;
+}
+
+const TBridgePileInfo* THive::FindPile(TBridgePileId pileId) const {
+    auto it = BridgePiles.find(pileId);
+    if (it == BridgePiles.end()) {
+        return nullptr;
+    }
+    return &it->second;
 }
 
 void THive::UpdatePiles() {

5e687f50398a2ffa207b72d8c4db3e254d21e660
Pisarenko Grigoriy <grigoriypisar@ydb.tech>
2025-10-17 18:27:33 +0300

    Fixed auth tests

--- ydb/core/sys_view/ut_auth.cpp
+++ ydb/core/sys_view/ut_auth.cpp
@@ -1428,6 +1428,7 @@ Y_UNIT_TEST_SUITE(AuthSystemView) {
                     [["/Root/.sys/query_sessions"];["metadata@system"]];
                     [["/Root/.sys/resource_pool_classifiers"];["metadata@system"]];
                     [["/Root/.sys/resource_pools"];["metadata@system"]];
+                    [["/Root/.sys/streaming_queries"];["metadata@system"]];
                     [["/Root/.sys/tables"];["metadata@system"]];
                     [["/Root/.sys/top_partitions_by_tli_one_hour"];["metadata@system"]];
                     [["/Root/.sys/top_partitions_by_tli_one_minute"];["metadata@system"]];
@@ -1495,6 +1496,7 @@ Y_UNIT_TEST_SUITE(AuthSystemView) {
                     [["/Root/Tenant1/.sys/query_sessions"];["metadata@system"]];
                     [["/Root/Tenant1/.sys/resource_pool_classifiers"];["metadata@system"]];
                     [["/Root/Tenant1/.sys/resource_pools"];["metadata@system"]];
+                    [["/Root/Tenant1/.sys/streaming_queries"];["metadata@system"]];
                     [["/Root/Tenant1/.sys/tables"];["metadata@system"]];
                     [["/Root/Tenant1/.sys/top_partitions_by_tli_one_hour"];["metadata@system"]];
                     [["/Root/Tenant1/.sys/top_partitions_by_tli_one_minute"];["metadata@system"]];
@@ -1561,6 +1563,7 @@ Y_UNIT_TEST_SUITE(AuthSystemView) {
                     [["/Root/Tenant2/.sys/query_sessions"];["metadata@system"]];
                     [["/Root/Tenant2/.sys/resource_pool_classifiers"];["metadata@system"]];
                     [["/Root/Tenant2/.sys/resource_pools"];["metadata@system"]];
+                    [["/Root/Tenant2/.sys/streaming_queries"];["metadata@system"]];
                     [["/Root/Tenant2/.sys/tables"];["metadata@system"]];
                     [["/Root/Tenant2/.sys/top_partitions_by_tli_one_hour"];["metadata@system"]];
                     [["/Root/Tenant2/.sys/top_partitions_by_tli_one_minute"];["metadata@system"]];
@@ -1849,6 +1852,7 @@ Y_UNIT_TEST_SUITE(AuthSystemView) {
                     [["/Root/.sys/query_sessions"];["metadata@system"]];
                     [["/Root/.sys/resource_pool_classifiers"];["metadata@system"]];
                     [["/Root/.sys/resource_pools"];["metadata@system"]];
+                    [["/Root/.sys/streaming_queries"];["metadata@system"]];
                     [["/Root/.sys/tables"];["metadata@system"]];
                     [["/Root/.sys/top_partitions_by_tli_one_hour"];["metadata@system"]];
                     [["/Root/.sys/top_partitions_by_tli_one_minute"];["metadata@system"]];
@@ -2552,6 +2556,7 @@ Y_UNIT_TEST_SUITE(AuthSystemView) {
                     [["/Root/.sys/query_sessions"];["ydb.generic.use"];["user1"]];
                     [["/Root/.sys/resource_pool_classifiers"];["ydb.generic.use"];["user1"]];
                     [["/Root/.sys/resource_pools"];["ydb.generic.use"];["user1"]];
+                    [["/Root/.sys/streaming_queries"];["ydb.generic.use"];["user1"]];
                     [["/Root/.sys/tables"];["ydb.generic.use"];["user1"]];
                     [["/Root/.sys/top_partitions_by_tli_one_hour"];["ydb.generic.use"];["user1"]];
                     [["/Root/.sys/top_partitions_by_tli_one_minute"];["ydb.generic.use"];["user1"]];
@@ -2625,6 +2630,7 @@ Y_UNIT_TEST_SUITE(AuthSystemView) {
                     [["/Root/Tenant1/.sys/query_sessions"];["ydb.generic.use"];["user1"]];
                     [["/Root/Tenant1/.sys/resource_pool_classifiers"];["ydb.generic.use"];["user1"]];
                     [["/Root/Tenant1/.sys/resource_pools"];["ydb.generic.use"];["user1"]];
+                    [["/Root/Tenant1/.sys/streaming_queries"];["ydb.generic.use"];["user1"]];
                     [["/Root/Tenant1/.sys/tables"];["ydb.generic.use"];["user1"]];
                     [["/Root/Tenant1/.sys/top_partitions_by_tli_one_hour"];["ydb.generic.use"];["user1"]];
                     [["/Root/Tenant1/.sys/top_partitions_by_tli_one_minute"];["ydb.generic.use"];["user1"]];

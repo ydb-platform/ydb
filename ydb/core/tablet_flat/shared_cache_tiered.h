ecc5fba401e66b560db761f21a572c83a2186d15
Ivan Nikolaev <ivannik@ydb.tech>
2025-10-22 20:49:47 +0300

    SharedCache: reload in-memory pages if there is enough memory (#26298)

--- ydb/core/tablet_flat/shared_cache_tiered.h
+++ ydb/core/tablet_flat/shared_cache_tiered.h
@@ -32,6 +32,11 @@ namespace NKikimr::NSharedCache {
             return CacheTiers[tier].Insert(page);
         }
 
+        TIntrusiveList<TPage> InsertUntouched(TPage *page) Y_WARN_UNUSED_RESULT {
+            ui32 tier = TPageTraits::GetTier(page);
+            return CacheTiers[tier].InsertUntouched(page);
+        }
+
         void Erase(TPage *page) {
             ui32 tier = TPageTraits::GetTier(page);
             CacheTiers[tier].Erase(page);
@@ -41,6 +46,12 @@ namespace NKikimr::NSharedCache {
             ui64 tryKeepInMemoryLimit = Min(limit, tryKeepInMemoryBytes);
             RegularTier->UpdateLimit(limit - tryKeepInMemoryLimit);
             TryKeepInMemoryTier->UpdateLimit(tryKeepInMemoryLimit);
+        }
+
+        TIntrusiveList<TPage> EnsureLimits() Y_WARN_UNUSED_RESULT {
+            auto evicted = RegularTier->EnsureLimits();
+            evicted.Append(TryKeepInMemoryTier->EnsureLimits());
+            return evicted;
         }
 
         ui64 GetLimit() const {

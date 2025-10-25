59422414702d2a0895483588641efc24fb9df9cd
Semyon Yentsov <yentsovsemyon@ydb.tech>
2025-10-26 01:57:00 +0300

    fix interval construction

--- ydb/core/tx/columnshard/transactions/locks/interaction.h
+++ ydb/core/tx/columnshard/transactions/locks/interaction.h
@@ -219,12 +219,14 @@ private:
     TIntervalPoint(const TPredicateContainer& point, const std::shared_ptr<arrow::Schema>& schema, const int includeState)
         : IncludeState(includeState)
     {
-        auto fields = schema->fields();
-        fields.resize(point.NumColumns());
-        auto schemaTrimmed = std::make_shared<arrow::Schema>(fields);
-        auto builders = NArrow::MakeBuilders(schemaTrimmed);
-        point.AppendPointTo(builders);
-        PrimaryKey = NArrow::TSimpleRow(arrow::RecordBatch::Make(schemaTrimmed, 1, NArrow::Finish(std::move(builders))), 0);
+        if (!point.IsEmpty()) {
+            auto fields = schema->fields();
+            fields.resize(point.NumColumns());
+            auto schemaTrimmed = std::make_shared<arrow::Schema>(fields);
+            auto builders = NArrow::MakeBuilders(schemaTrimmed);
+            point.AppendPointTo(builders);
+            PrimaryKey = NArrow::TSimpleRow(arrow::RecordBatch::Make(schemaTrimmed, 1, NArrow::Finish(std::move(builders))), 0);
+        }
     }
 
 public:

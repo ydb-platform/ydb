7f20b0792ea8ce79bdb96e8bf8d2699e17c1eda6
Vitalii Gridnev <gridnevvvit@gmail.com>
2025-10-27 12:19:55 +0300

    fix sl on empty inputs (#27588)

--- ydb/core/kqp/ut/join/kqp_index_lookup_join_ut.cpp
+++ ydb/core/kqp/ut/join/kqp_index_lookup_join_ut.cpp
@@ -1122,6 +1122,54 @@ Y_UNIT_TEST_TWIN(LeftJoinOnRightTableOverIndex, StreamLookupJoin) {
     tester.Run();
 }
 
+Y_UNIT_TEST_TWIN(TestEntityFramework, StreamLookupJoin) {
+    auto tester = TTester{
+        .Query=R"(
+            SELECT
+                `l1`.`Id`,
+                `l1`.`OneToOne_Required_PK_Date`,
+                `l1`.`Level1_Optional_Id`,
+                `l1`.`Level1_Required_Id`,
+                `l1`.`Level2_Name`,
+                `l1`.`OneToMany_Optional_Inverse2Id`,
+                `l1`.`OneToMany_Required_Inverse2Id`,
+                `l1`.`OneToOne_Optional_PK_Inverse2Id`
+            FROM `Level1` AS `l`
+            LEFT JOIN (
+                SELECT
+                    `l0`.`Id` AS `Id`,
+                    `l0`.`OneToOne_Required_PK_Date` AS `OneToOne_Required_PK_Date`,
+                    `l0`.`Level1_Optional_Id` AS `Level1_Optional_Id`,
+                    `l0`.`Level1_Required_Id` AS `Level1_Required_Id`,
+                    `l0`.`Level2_Name` AS `Level2_Name`,
+                    `l0`.`OneToMany_Optional_Inverse2Id` AS `OneToMany_Optional_Inverse2Id`,
+                    `l0`.`OneToMany_Required_Inverse2Id` AS `OneToMany_Required_Inverse2Id`,
+                    `l0`.`OneToOne_Optional_PK_Inverse2Id` AS `OneToOne_Optional_PK_Inverse2Id`
+                FROM `Level1` AS `l0`
+                WHERE
+                    `l0`.`OneToOne_Required_PK_Date` IS NOT NULL AND
+                    `l0`.`Level1_Required_Id` IS NOT NULL AND
+                    `l0`.`OneToMany_Required_Inverse2Id` IS NOT NULL
+            ) AS `l1`
+            ON
+                `l`.`Id` = CASE
+                    WHEN `l1`.`OneToOne_Required_PK_Date` IS NOT NULL AND `l1`.`Level1_Required_Id` IS NOT NULL AND `l1`.`OneToMany_Required_Inverse2Id` IS NOT NULL THEN `l1`.`Id`
+                ELSE NULL
+                END
+            LEFT JOIN `Level1` AS `l2` ON `l1`.`Level1_Required_Id` = `l2`.`Id`
+            WHERE
+                `l1`.`OneToOne_Required_PK_Date` IS NOT NULL AND
+                `l1`.`Level1_Required_Id` IS NOT NULL AND
+                `l1`.`OneToMany_Required_Inverse2Id` IS NOT NULL AND `l2`.`Id` IN (1, 2)
+        )",
+        .Answer=R"([
+        ])",
+        .StreamLookup=StreamLookupJoin,
+        .DoValidateStats=false,
+    };
+    tester.Run();
+}
+
 Y_UNIT_TEST_TWIN(JoinLeftJoinPostJoinFilterTest, StreamLookupJoin) {
     auto tester = TTester{
         .Query=R"(

6508387aea09c052ccf0ed3d8007508d1f7d6f24
robot-piglet <robot-piglet@yandex-team.com>
2025-10-27 15:28:03 +0300

    Intermediate changes commit_hash:50448cf0b9f7db378645dc682bab1b0bc040f4eb

--- contrib/python/PyHamcrest/py3/hamcrest/core/helpers/ismock.py
+++ contrib/python/PyHamcrest/py3/hamcrest/core/helpers/ismock.py
@@ -1,15 +1,19 @@
-MOCKTYPES = ()
+from typing import Any, List, Type
+
+MOCKTYPES: List[Type] = []
 try:
     from mock import Mock
-    MOCKTYPES += (Mock,)
+
+    MOCKTYPES += [Mock]
 except ImportError:
     pass
 try:
     from unittest.mock import Mock
-    MOCKTYPES += (Mock,)
+
+    MOCKTYPES += [Mock]
 except ImportError:
     pass
 
 
-def ismock(obj):
-    return isinstance(obj, MOCKTYPES)
+def ismock(obj: Any) -> bool:
+    return isinstance(obj, tuple(MOCKTYPES))

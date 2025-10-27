7d26bae5f88e44b93c6bb0879cb25277b8b26293
robot-piglet <robot-piglet@yandex-team.com>
2025-10-27 12:03:36 +0300

    Intermediate changes commit_hash:84961fae4145adf925b556f48aecc5bce296cafb

--- contrib/python/s3transfer/py3/tests/unit/test_crt.py
+++ contrib/python/s3transfer/py3/tests/unit/test_crt.py
@@ -103,7 +103,7 @@ class TestBotocoreCRTRequestSerializer(unittest.TestCase):
         self.files = FileCreator()
         self.filename = self.files.create_file('myfile', 'my content')
         self.expected_path = "/" + self.bucket + "/" + self.key
-        self.expected_host = "s3.%s.amazonaws.com" % (self.region)
+        self.expected_host = f"s3.{self.region}.amazonaws.com"
 
     def tearDown(self):
         self.files.remove_all()
@@ -362,3 +362,7 @@ class TestCreateS3CRTClient:
             mock_s3_crt_client.call_args[1]['throughput_target_gbps']
             == expected_gbps
         )
+
+    def test_always_enables_s3express(self, mock_s3_crt_client):
+        s3transfer.crt.create_s3_crt_client('us-west-2')
+        assert mock_s3_crt_client.call_args[1]['enable_s3express'] is True

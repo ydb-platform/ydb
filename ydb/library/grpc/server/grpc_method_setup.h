5d79fc36c4dd07041bc20b1b602d08e4b2c10b5c
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-10-27 10:25:55 +0100

    Use full namespace names in macro. Include headers used in macro

--- ydb/library/grpc/server/grpc_method_setup.h
+++ ydb/library/grpc/server/grpc_method_setup.h
@@ -1,10 +1,17 @@
 #ifndef GRPC_METHOD_SETUP_H
 #define GRPC_METHOD_SETUP_H
 
-#include <memory>
-#include <string>
+#include <ydb/core/grpc_services/base/base.h>
+#include <ydb/core/grpc_services/grpc_mon.h>
+#include <ydb/core/jaeger_tracing/request_discriminator.h>
+#include <ydb/library/actors/core/actorsystem.h>
+#include <ydb/library/grpc/server/grpc_request.h>
+#include <ydb/library/grpc/server/grpc_request_base.h>
+
 #include <grpcpp/grpcpp.h>
 
+#include <memory>
+#include <string>
 #include <type_traits>
 
 // Implies using namespace for request/response types
@@ -13,35 +20,45 @@
 
 // Implies usage from inside grpc service class, derived from TGrpcServiceBase
 #define SETUP_RUNTIME_EVENT_METHOD(methodName, inputType, outputType, methodCallback, rlMode, requestType, counterName, auditMode, runtimeEventType, operationCallClass, grpcProxyId) \
-    MakeIntrusive<NGRpcService::TGRpcRequest<                                                         \
-        inputType,                                                                                    \
-        outputType,                                                                                   \
-        std::remove_reference_t<decltype(*this)>>>                                                    \
-    (                                                                                                 \
-        this,                                                                                         \
-        &Service_,                                                                                    \
-        CQ_,                                                                                          \
-        [this, proxyId = grpcProxyId](NYdbGrpc::IRequestContextBase* reqCtx) {                        \
-            NGRpcService::ReportGrpcReqToMon(*ActorSystem_, reqCtx->GetPeer());                       \
-            ActorSystem_->Send(proxyId, new operationCallClass<                                       \
-                inputType,                                                                            \
-                outputType,                                                                           \
-                NRuntimeEvents::EType::runtimeEventType>(reqCtx, methodCallback,                      \
-                    TRequestAuxSettings {                                                             \
-                        .RlMode = TRateLimiterMode::rlMode,                                           \
-                        .AuditMode = auditMode,                                                       \
-                        .RequestType = NJaegerTracing::ERequestType::requestType,                     \
-                    }));                                                                              \
-        },                                                                                            \
-        &TGrpcAsyncService::Y_CAT(Request, methodName),                                               \
-        Y_STRINGIZE(methodName),                                                                      \
-        logger,                                                                                       \
-        getCounterBlock(Y_STRINGIZE(counterName), Y_STRINGIZE(methodName))                            \
+    MakeIntrusive<::NKikimr::NGRpcService::TGRpcRequest<                                                  \
+        inputType,                                                                                        \
+        outputType,                                                                                       \
+        std::remove_reference_t<decltype(*this)>>>                                                        \
+    (                                                                                                     \
+        this,                                                                                             \
+        &Service_,                                                                                        \
+        CQ_,                                                                                              \
+        [this, proxyId = grpcProxyId](::NYdbGrpc::IRequestContextBase* reqCtx) {                          \
+            ::NKikimr::NGRpcService::ReportGrpcReqToMon(*ActorSystem_, reqCtx->GetPeer());                \
+            ActorSystem_->Send(proxyId, new operationCallClass<                                           \
+                inputType,                                                                                \
+                outputType,                                                                               \
+                ::NKikimr::NGRpcService::NRuntimeEvents::EType::runtimeEventType>(reqCtx, methodCallback, \
+                    ::NKikimr::NGRpcService::TRequestAuxSettings {                                        \
+                        .RlMode = ::NKikimr::NGRpcService::TRateLimiterMode::rlMode,                      \
+                        .AuditMode = auditMode,                                                           \
+                        .RequestType = ::NKikimr::NJaegerTracing::ERequestType::requestType,              \
+                    }));                                                                                  \
+        },                                                                                                \
+        &TGrpcAsyncService::Y_CAT(Request, methodName),                                                   \
+        Y_STRINGIZE(methodName),                                                                          \
+        logger,                                                                                           \
+        getCounterBlock(Y_STRINGIZE(counterName), Y_STRINGIZE(methodName))                                \
     )->Run()
 
 
 // Common macro for gRPC methods setup
-#define SETUP_METHOD(methodName, methodCallback, rlMode, requestType, counterName, auditMode)    \
-    SETUP_RUNTIME_EVENT_METHOD(methodName, YDB_API_DEFAULT_REQUEST_TYPE(methodName), YDB_API_DEFAULT_RESPONSE_TYPE(methodName), methodCallback, rlMode, requestType, counterName, auditMode, COMMON, TGrpcRequestOperationCall, GRpcRequestProxyId_)
+#define SETUP_METHOD(methodName, methodCallback, rlMode, requestType, counterName, auditMode) \
+    SETUP_RUNTIME_EVENT_METHOD(methodName, \
+        YDB_API_DEFAULT_REQUEST_TYPE(methodName), \
+        YDB_API_DEFAULT_RESPONSE_TYPE(methodName), \
+        methodCallback, \
+        rlMode, \
+        requestType, \
+        counterName, \
+        auditMode, \
+        COMMON, \
+        ::NKikimr::NGRpcService::TGrpcRequestOperationCall, \
+        GRpcRequestProxyId_)
 
 #endif // GRPC_METHOD_SETUP_H

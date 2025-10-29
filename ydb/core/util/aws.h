9fe4e7285525e33fcde2ecf5033b5b4fed179a73
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2025-10-29 13:58:33 +0300

    Aws logging (#27574)
    (cherry picked from commit 9c5cf862bc4a6f747b25af0e7adf34f03925318d)

--- ydb/core/util/aws.h
+++ ydb/core/util/aws.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#include <util/generic/string.h>
+
 namespace NKikimr {
 
-void InitAwsAPI();
-void ShutdownAwsAPI();
+struct TAwsClientConfig {
+    struct TLogConfig {
+        int LogLevel = 0; // Aws::Utils::Logging::LogLevel, 0 is disabled
+        TString FilenamePrefix; // Aws::Utils::Logging::DefaultLogSystem::FilenamePrefix
+    };
+
+    TLogConfig LogConfig;
+};
+
+void InitAwsAPI(const TAwsClientConfig& config = {});
+void ShutdownAwsAPI(const TAwsClientConfig& config = {});
 
 }

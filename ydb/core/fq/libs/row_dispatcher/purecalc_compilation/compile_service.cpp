412ef0984e7d9dff8bfb78f7606ad33d96169471
Aleksei Pozdniakov <vokayndzop@ydb.tech>
2025-11-01 10:00:02 +0000

    drop attributes

--- ydb/core/fq/libs/row_dispatcher/purecalc_compilation/compile_service.cpp
+++ ydb/core/fq/libs/row_dispatcher/purecalc_compilation/compile_service.cpp
@@ -42,7 +42,7 @@ public:
         , Request(std::move(request))
     {}
 
-    [[maybe_unused]] static constexpr char ActorName[] = "FQ_ROW_DISPATCHER_COMPILE_ACTOR";
+    static constexpr char ActorName[] = "FQ_ROW_DISPATCHER_COMPILE_ACTOR";
 
     void Bootstrap() {
         Y_DEFER {
@@ -117,7 +117,7 @@ public:
         , Counters(counters)
     {}
 
-    [[maybe_unused]] static constexpr char ActorName[] = "FQ_ROW_DISPATCHER_COMPILE_SERVICE";
+    static constexpr char ActorName[] = "FQ_ROW_DISPATCHER_COMPILE_SERVICE";
 
     STRICT_STFUNC(StateFunc,
         hFunc(TEvRowDispatcher::TEvPurecalcCompileRequest, Handle);

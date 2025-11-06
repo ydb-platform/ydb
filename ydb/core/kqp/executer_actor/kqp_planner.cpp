181580aae55742f9625f70e448d2bce359aba9b4
Ivan <ilezhankin@yandex-team.ru>
2025-11-06 18:29:56 +0300

    Use default value of BufferPageAllocSize from proto (#28237)

--- ydb/core/kqp/executer_actor/kqp_planner.cpp
+++ ydb/core/kqp/executer_actor/kqp_planner.cpp
@@ -225,6 +225,9 @@ std::unique_ptr<TEvKqpNode::TEvStartKqpTasksRequest> TKqpPlanner::SerializeReque
         if (ArrayBufferMinFillPercentage) {
             serializedTask->SetArrayBufferMinFillPercentage(*ArrayBufferMinFillPercentage);
         }
+        if (BufferPageAllocSize) {
+            serializedTask->SetBufferPageAllocSize(*BufferPageAllocSize);
+        }
         request.AddTasks()->Swap(serializedTask);
     }
 

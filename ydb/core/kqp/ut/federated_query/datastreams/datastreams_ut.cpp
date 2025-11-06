89b4e353a37dd20e2d3c0fb4c4f1e47d422b7249
Dmitry Kardymon <kardymon-d@ydb.tech>
2025-11-06 11:12:46 +0300

    YQ-4411 Streaming queries: local checkpoints (#26749)
    Co-authored-by: Pisarenko Grigoriy <grigoriypisar@ydb.tech>

--- ydb/core/kqp/ut/federated_query/datastreams/datastreams_ut.cpp
+++ ydb/core/kqp/ut/federated_query/datastreams/datastreams_ut.cpp
@@ -127,9 +127,11 @@ public:
                 .PqGateway = PqGateway,
                 .CheckpointPeriod = CheckpointPeriod,
                 .LogSettings = LogSettings,
+                .UseLocalCheckpointsInStreamingQueries = true,
             });
 
             Kikimr->GetTestServer().GetRuntime()->GetAppData(0).FeatureFlags.SetEnableSchemaSecrets(UseSchemaSecrets());
+            Kikimr->GetTestServer().GetRuntime()->GetAppData(0).FeatureFlags.SetEnableStreamingQueries(true);
         }
 
         return Kikimr;
@@ -585,8 +587,8 @@ public:
     void WaitCheckpointUpdate(const TString& checkpointId) {
         std::optional<uint64_t> minSeqNo;
         WaitFor(TEST_OPERATION_TIMEOUT, "checkpoint update", [&](TString& error) {
-            const auto& result = ExecExternalQuery(fmt::format(R"(
-                SELECT MIN(seq_no) AS seq_no FROM checkpoints_metadata
+            const auto& result = ExecQuery(fmt::format(R"(
+                SELECT MIN(seq_no) AS seq_no FROM `.metadata/streaming/checkpoints/checkpoints_metadata`
                 WHERE graph_id = "{checkpoint_id}";
             )", "checkpoint_id"_a = checkpointId));
             UNIT_ASSERT_VALUES_EQUAL(result.size(), 1);
@@ -1457,9 +1459,9 @@ Y_UNIT_TEST_SUITE(KqpFederatedQueryDatastreams) {
         ReadTopicMessage(outputTopicName, "A");
         WaitCheckpointUpdate(checkpointId);
 
-        const auto& result = ExecExternalQuery(fmt::format(R"(
+        const auto& result = ExecQuery(fmt::format(R"(
             SELECT COUNT(*) AS states_count FROM (
-                SELECT DISTINCT task_id FROM states
+                SELECT DISTINCT task_id FROM `.metadata/streaming/checkpoints/states`
                 WHERE graph_id = "{checkpoint_id}"
             )
         )", "checkpoint_id"_a = checkpointId));

8eba9d6c9d47172c4fc983abea0aecabc2b5d249
Stanislav Tebloev <stebloev@ydb.tech>
2025-11-06 10:39:44 +0000

    Add csv conversion

--- ydb/library/workload/vector/vector_data_generator.cpp
+++ ydb/library/workload/vector/vector_data_generator.cpp
@@ -1,17 +1,26 @@
 #include "vector_data_generator.h"
 
+#include <ydb/library/yql/udfs/common/knn/knn-serializer-shared.h>
+
 #include <contrib/libs/apache/arrow/cpp/src/arrow/array/array_binary.h>
 #include <contrib/libs/apache/arrow/cpp/src/arrow/array/array_nested.h>
 #include <contrib/libs/apache/arrow/cpp/src/arrow/array/array_primitive.h>
 #include <contrib/libs/apache/arrow/cpp/src/arrow/array/builder_binary.h>
+#include <contrib/libs/apache/arrow/cpp/src/arrow/chunked_array.h>
 #include <contrib/libs/apache/arrow/cpp/src/arrow/compute/cast.h>
+#include <contrib/libs/apache/arrow/cpp/src/arrow/csv/api.h>
+#include <contrib/libs/apache/arrow/cpp/src/arrow/csv/reader.h>
+#include <contrib/libs/apache/arrow/cpp/src/arrow/csv/writer.h>
 #include <contrib/libs/apache/arrow/cpp/src/arrow/io/memory.h>
 #include <contrib/libs/apache/arrow/cpp/src/arrow/ipc/dictionary.h>
 #include <contrib/libs/apache/arrow/cpp/src/arrow/ipc/reader.h>
 #include <contrib/libs/apache/arrow/cpp/src/arrow/ipc/writer.h>
 #include <contrib/libs/apache/arrow/cpp/src/arrow/record_batch.h>
+#include <contrib/libs/apache/arrow/cpp/src/arrow/table.h>
 #include <contrib/libs/apache/arrow/cpp/src/arrow/type.h>
 #include <contrib/libs/apache/arrow/cpp/src/arrow/type_fwd.h>
+
+#include <util/stream/mem.h>
 
 namespace NYdbWorkload {
 
@@ -35,39 +44,48 @@ private:
         return std::make_pair(schema, recordBatch);
     }
 
-    void ConvertArrow(TDataPortion::TArrow* data) {
+    static std::shared_ptr<arrow::Table> Deserialize(TDataPortion::TCsv* data) {
+        auto bufferReader = std::make_shared<arrow::io::BufferReader>(arrow::util::string_view(data->Data.data(), data->Data.size()));
+        auto csvReader = arrow::csv::TableReader::Make(
+            arrow::io::default_io_context(),
+            bufferReader,
+            arrow::csv::ReadOptions::Defaults(),
+            arrow::csv::ParseOptions::Defaults(),
+            arrow::csv::ConvertOptions::Defaults()
+        ).ValueOrDie();
+
+        return csvReader->Read().ValueOrDie();
+    }
+
+    void TransformArrow(TDataPortion::TArrow* data) {
         const auto [schema, batch] = Deserialize(data);
 
-        // extract
+        // id
         const auto idColumn = batch->GetColumnByName("id");
-        const auto embeddingColumn = batch->GetColumnByName(EmbeddingSourceField);
-        const auto embeddingListColumn = dynamic_cast<arrow::ListArray*>(embeddingColumn.get());
-
-        // conversion
         const auto newIdColumn = arrow::compute::Cast(idColumn, arrow::uint64()).ValueOrDie().make_array();
 
-        arrow::StringBuilder builder;
+        // embedding
+        const auto embeddingColumn = std::dynamic_pointer_cast<arrow::ListArray>(batch->GetColumnByName(EmbeddingSourceField));
+        arrow::StringBuilder newEmbeddingsBuilder;
         for (int64_t row = 0; row < batch->num_rows(); ++row) {
-            auto embeddingAsFloats = static_cast<const arrow::FloatArray*>(embeddingListColumn->value_slice(row).get());
+            const auto embeddingFloatList = std::static_pointer_cast<arrow::FloatArray>(embeddingColumn->value_slice(row));
 
-            std::string serialized(embeddingAsFloats->length() * sizeof(float) + 1, '\0');
-            float* float_bytes = reinterpret_cast<float*>(serialized.data());
-            for (int64_t i = 0; i < embeddingAsFloats->length(); ++i) {
-                float_bytes[i] = embeddingAsFloats->Value(i);
+            TStringBuilder buffer;
+            NKnnVectorSerialization::TSerializer<float> serializer(&buffer.Out);
+            for (int64_t i = 0; i < embeddingFloatList->length(); ++i) {
+                serializer.HandleElement(embeddingFloatList->Value(i));
             }
-            serialized.back() = '\x01';
+            serializer.Finish();
 
-            if (const auto status = builder.Append(serialized); !status.ok()) {
+            if (const auto status = newEmbeddingsBuilder.Append(buffer.MutRef()); !status.ok()) {
                 status.Abort();
             }
         }
-
         std::shared_ptr<arrow::StringArray> newEmbeddingColumn;
-        if (const auto status = builder.Finish(&newEmbeddingColumn); !status.ok()) {
+        if (const auto status = newEmbeddingsBuilder.Finish(&newEmbeddingColumn); !status.ok()) {
             status.Abort();
         }
 
-        // serialize
         const auto newSchema = arrow::schema({
             arrow::field("id", arrow::uint64()),
             arrow::field("embedding", arrow::utf8()),
@@ -84,9 +102,67 @@ private:
         data->Data = arrow::ipc::SerializeRecordBatch(*newRecordBatch, arrow::ipc::IpcWriteOptions{}).ValueOrDie()->ToString();
     }
 
-    void Convert(TDataPortion::TDataType& data) {
+    void TransformCsv(TDataPortion::TCsv* data) {
+        const auto table = Deserialize(data);
+
+        // id
+        const auto idColumn = table->GetColumnByName("id");
+
+        // embedding
+        const auto embeddingColumn = table->GetColumnByName(EmbeddingSourceField);
+        arrow::StringBuilder newEmbeddingsBuilder;
+        for (int64_t row = 0; row < table->num_rows(); ++row) {
+            const auto embeddingListString = std::static_pointer_cast<arrow::StringArray>(embeddingColumn->Slice(row, 1)->chunk(0))->Value(0);
+            
+            TStringBuf buffer(embeddingListString.data(), embeddingListString.size());
+            buffer.SkipPrefix("[");
+            buffer.ChopSuffix("]");
+            TMemoryInput input(buffer);
+
+            TStringBuilder newEmbeddingBuilder;
+            NKnnVectorSerialization::TSerializer<float> serializer(&newEmbeddingBuilder.Out);
+            while (!input.Exhausted()) {
+                float val;
+                input >> val;
+                input.Skip(1);
+                serializer.HandleElement(val);
+            }
+            serializer.Finish();
+
+            if (const auto status = newEmbeddingsBuilder.Append(newEmbeddingBuilder.MutRef()); !status.ok()) {
+                status.Abort();
+            }
+        }
+        std::shared_ptr<arrow::StringArray> newEmbeddingColumn;
+        if (const auto status = newEmbeddingsBuilder.Finish(&newEmbeddingColumn); !status.ok()) {
+            status.Abort();
+        }
+
+        const auto newSchema = arrow::schema({
+            arrow::field("id", arrow::uint64()),
+            arrow::field("embedding", arrow::utf8()),
+        });
+        const auto newTable = arrow::Table::Make(
+            newSchema,
+            {
+                idColumn,
+                arrow::ChunkedArray::Make({newEmbeddingColumn}).ValueOrDie(),
+            }
+        );
+        auto outputStream = arrow::io::BufferOutputStream::Create().ValueOrDie();
+        if (const auto status = arrow::csv::WriteCSV(*newTable, arrow::csv::WriteOptions::Defaults(), outputStream.get()); !status.ok()) {
+            status.Abort();
+        }
+        data->FormatString = "";
+        data->Data = outputStream->Finish().ValueOrDie()->ToString();
+    }
+
+    void Transform(TDataPortion::TDataType& data) {
         if (auto* value = std::get_if<TDataPortion::TArrow>(&data)) {
-            ConvertArrow(value);
+            TransformArrow(value);
+        }
+        if (auto* value = std::get_if<TDataPortion::TCsv>(&data)) {
+            TransformCsv(value);
         }
     }
 
@@ -100,7 +176,7 @@ public:
     virtual TDataPortions GenerateDataPortion() override {
         TDataPortions portions = InnerDataGenerator->GenerateDataPortion();
         for (auto portion : portions) {
-            Convert(portion->MutableData());
+            Transform(portion->MutableData());
         }
         return portions;
     }
@@ -122,7 +198,9 @@ void TWorkloadVectorFilesDataInitializer::ConfigureOpts(NLastGetopt::TOpts& opts
         ).Required().StoreResult(&DataFiles);
     opts.AddLongOption('t', "transform",
             "Perform transformation of input data. "
-            "Parquet: leave only required fields, cast to expected types, convert list of floats into serialized ydb representation."
+            "Parquet: leave only required fields, cast to expected types, convert list of floats into serialized representation. "
+            "CSV: leave only required fields, parse float list from string and serialize. "
+            "Reference for embedding serialization: https://ydb.tech/docs/yql/reference/udf/list/knn#functions-convert"
         ).Optional().StoreTrue(&DoTransform);
     opts.AddLongOption(
             "transform-embedding-source-field",

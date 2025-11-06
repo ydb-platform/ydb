9335a3dea16b2f9ebf41950b440773e84fa55b88
Irene Skvortsova <nerhneiro@gmail.com>
2025-11-06 13:41:29 +0300

    Add put records handler for UI main (#27354)
    Co-authored-by: Irina Skvortsova <nerhneiro@yandex-team.ru>
    Co-authored-by: alexnick88 <alexnick@ydb.tech>
    Co-authored-by: Alexey Efimov <xeno@prnwatch.com>

---
+++ ydb/core/viewer/viewer_put_record.h
@@ -0,0 +1,296 @@
+#pragma once
+#include "json_pipe_req.h"
+#include "viewer.h"
+
+#include <ydb/core/grpc_services/rpc_calls.h>
+#include <ydb/core/grpc_services/service_replication.h>
+#include <ydb/services/replication/grpc_service.h>
+#include <ydb/core/viewer/yaml/yaml.h>
+#include <ydb/core/persqueue/events/global.h>
+#include <ydb/core/persqueue/writer/writer.h>
+#include <ydb/core/persqueue/public/write_meta/write_meta.h>
+#include <ydb/core/ydb_convert/ydb_convert.h>
+#include <ydb/core/persqueue/public/partition_key_range/partition_key_range.h>
+#include <ydb/services/lib/sharding/sharding.h>
+#include <library/cpp/digest/md5/md5.h>
+#include <ydb/core/persqueue/public/pq_rl_helpers.h>
+
+namespace NKikimr::NViewer {
+
+    struct TMetadataItem {
+        TString Key;
+        TString Value;
+    };
+
+class TPutRecord : public TViewerPipeClient {
+    using TBase = TViewerPipeClient;
+    using TThis = TPutRecord;
+    using TBase::ReplyAndPassAway;
+    using TBase::GetHTTPBADREQUEST;
+
+public:
+    TPutRecord(IViewer* viewer, NMon::TEvHttpInfo::TPtr& ev)
+        : TViewerPipeClient(viewer, ev)
+    {}
+
+private:
+    TString TopicPath;
+    std::optional<i64> Partition;
+    TString Message;
+    TString Key;
+    TVector<TMetadataItem> Metadata;
+    ui64 TabletId;
+    TActorId WriteActorId;
+    std::optional<TRequestResponse<TEvTxProxySchemeCache::TEvNavigateKeySetResult>> SchemeCacheResponse;
+    std::optional<TRequestResponse<NPQ::TEvPartitionWriter::TEvWriteResponse>> WriteResponse;
+
+public:
+    STATEFN(StateWork) {
+        switch (ev->GetTypeRewrite()) {
+            hFunc(TEvTxProxySchemeCache::TEvNavigateKeySetResult, Handle);
+            hFunc(NPQ::TEvPartitionWriter::TEvWriteResponse, Handle);
+            hFunc(NPQ::TEvPartitionWriter::TEvInitResult, Handle);
+            hFunc(NPQ::TEvPartitionWriter::TEvWriteAccepted, HandleAccepting);
+            cFunc(TEvents::TSystem::Wakeup, HandleTimeout);
+        }
+    }
+    void Bootstrap() override {
+        if (!PostData.Has("path") || !PostData.Has("message")) {
+            return ReplyAndPassAway(TBase::GetHTTPBADREQUEST("text/plain", "fields 'path' and 'message' are required and should not be empty"));
+        }
+
+        TopicPath = PostData["path"].GetString();
+
+        Message = PostData["message"].GetString();
+
+        if (PostData.Has("partition")) {
+            Partition = PostData["partition"].GetInteger();
+        }
+
+        if (PostData.Has("key")) {
+            Key = PostData["key"].GetString();
+        }
+        if (PostData.Has("metadata")) {
+            int num = Params.NumOfValues("metadata");
+            for (int i = 0; i < num; i++) {
+                TString key;
+                TString value;
+                if (PostData["metadata"][i].Has("key")) {
+                    key = PostData["metadata"][i]["key"].GetStringRobust();
+                }
+                if (PostData["metadata"][i].Has("value")) {
+                    value = PostData["metadata"][i]["value"].GetStringRobust();
+                }
+                Metadata.emplace_back(key, value);
+            }
+        }
+
+        SchemeCacheResponse = MakeRequestSchemeCacheNavigateWithToken(TopicPath, NACLib::EAccessRights::UpdateRow);
+        TBase::Become(&TThis::StateWork, Timeout, new TEvents::TEvWakeup());
+    }
+
+    THolder<NPQ::TEvPartitionWriter::TEvWriteRequest> FormWriteRequest() {
+        auto ev = MakeHolder<NPQ::TEvPartitionWriter::TEvWriteRequest>();
+        auto& request = ev->Record;
+        auto* partitionRequest = request.MutablePartitionRequest();
+        partitionRequest->SetTopic(TopicPath);
+        partitionRequest->SetPartition(*Partition);
+        partitionRequest->SetIsDirectWrite(true);
+
+        NKikimrPQClient::TDataChunk proto;
+        proto.set_codec(NPersQueueCommon::RAW);
+        for(auto& h : Metadata) {
+            auto res = proto.AddMessageMeta();
+            if (h.Key) {
+                res->set_key(static_cast<const char*>(h.Key.data()), h.Key.size());
+            }
+            if (h.Value) {
+                res->set_value(static_cast<const char*>(h.Value.data()), h.Value.size());
+            }
+        }
+
+        if (Key) {
+            auto res = proto.AddMessageMeta();
+            res->set_key("__key");
+            res->set_value(static_cast<const char*>(Key.data()), Key.size());
+        }
+        proto.SetData(static_cast<const void*>(Message.data()), Message.size());
+        TString str;
+        bool res = proto.SerializeToString(&str);
+        Y_ABORT_UNLESS(res);
+
+        auto w = partitionRequest->AddCmdWrite();
+        w->SetData(str);
+        w->SetCreateTimeMS(TInstant::Now().MilliSeconds());
+        w->SetDisableDeduplication(true);
+        w->SetUncompressedSize(Message ? Message.size() : 0);
+        w->SetIgnoreQuotaDeadline(false);
+        w->SetExternalOperation(true);
+        ui64 totalSize = Message ? Message.size() : 0;
+        partitionRequest->SetPutUnitsSize(NPQ::PutUnitsSize(totalSize));
+        return ev;
+    }
+
+    void Handle(TEvTxProxySchemeCache::TEvNavigateKeySetResult::TPtr& ev) {
+        if (!SchemeCacheResponse->Set(std::move(ev))) {
+            return;
+        }
+        if (SchemeCacheResponse->IsError()) {
+            ReplyAndPassAway(TBase::GetHTTPBADREQUEST("text/plain", "SchemeCacheNavigate request finished with error: " + SchemeCacheResponse->GetError()));
+            return RequestDone();
+        }
+        auto navigate = *SchemeCacheResponse->Get()->Request;
+        auto& info = navigate.ResultSet.front();
+        if (info.Status != NSchemeCache::TSchemeCacheNavigate::EStatus::Ok) {
+            ReplyAndPassAway(TBase::GetHTTPBADREQUEST("text/plain", "TEvNavigateKeySet finished with unsuccessful status. Check if topic exists or if you have UpdateRow access rights."));
+            return RequestDone();
+        }
+
+        const auto& pqDescription = info.PQGroupInfo->Description;
+
+        if (!PostData.Has("partition")) {
+            auto chooser = NPQ::CreatePartitionChooser(pqDescription, false);
+            if (!Key.empty()) {
+                auto* partition = chooser->GetPartition(Key);
+                Partition = partition->PartitionId;
+            } else {
+                auto* partition = chooser->GetPartition(CreateGuidAsString());
+                Partition = partition->PartitionId;
+            }
+        }
+
+        const auto& partitions = pqDescription.GetPartitions();
+        bool partitionFound = false;
+        for (auto& partition : partitions) {
+            auto partitionId = partition.GetPartitionId();
+            if (partitionId == Partition) {
+                TabletId = partition.GetTabletId();
+                partitionFound = true;
+                break;
+            }
+        }
+        if (!partitionFound) {
+            ReplyAndPassAway(TBase::GetHTTPBADREQUEST("text/plain", "Partition not found."));
+            return RequestDone();
+        }
+        NPQ::TPartitionWriterOpts opts;
+        opts.WithDeduplication(false)
+            .WithTopicPath(TopicPath);
+
+        auto* writerActor = CreatePartitionWriter(SelfId(), TabletId, *Partition, opts);
+        WriteActorId = ActorContext().RegisterWithSameMailbox(writerActor);
+
+        auto writeEvent = FormWriteRequest();
+        WriteResponse = MakeRequest<NPQ::TEvPartitionWriter::TEvWriteResponse>(WriteActorId, writeEvent.Release());
+        return RequestDone();
+    }
+
+    void Handle(NPQ::TEvPartitionWriter::TEvInitResult::TPtr) {
+        return;
+    }
+
+    void Handle(NPQ::TEvPartitionWriter::TEvWriteResponse::TPtr& ev) {
+        if (!WriteResponse->Set(std::move(ev))) {
+            return;
+        }
+        if (WriteResponse->IsError()) {
+            ReplyAndPassAway(TBase::GetHTTPINTERNALERROR("text/plain", "Write request finished with error: " + WriteResponse->GetError()));
+            return RequestDone();
+        }
+        auto r = WriteResponse->Get();
+        if (r->IsSuccess()) {
+            return RequestDone();
+        } else {
+            auto error = r->GetError();
+            TString reason = r->GetError().Reason;
+            ReplyAndPassAway(TBase::GetHTTPBADREQUEST("text/plain", reason));
+            return RequestDone();
+        }
+    }
+
+    void HandleAccepting(NPQ::TEvPartitionWriter::TEvWriteAccepted::TPtr) {
+        return;
+    }
+
+    void HandleDisconnected(NPQ::TEvPartitionWriter::TEvDisconnected::TPtr, const TActorContext&) {
+        return ReplyAndPassAway(TBase::GetHTTPINTERNALERROR("text/plain", "Partition writer is disconnected."));
+    }
+
+    void ReplyAndPassAway() override {
+        TStringStream jsonBody;
+        TBase::ReplyAndPassAway(TBase::GetHTTPOKJSON(jsonBody.Str()));
+    }
+
+    void PassAway() override {
+        Send(WriteActorId, new TEvents::TEvPoison());
+        TBase::PassAway();
+    }
+
+    static YAML::Node GetSwagger() {
+        YAML::Node node = YAML::Load(R"___(
+        post:
+            tags:
+              - viewer
+            summary: Produce to topic
+            description: Puts data to the topic
+            parameters:
+              - name: database
+                description: database name
+                type: string
+                required: false
+              - name: path
+                description: path of topic
+                required: true
+                type: string
+              - name: partition
+                description: partition to read from
+                required: true
+                type: integer
+              - name: message
+                description: message we want to produce
+                required: true
+                type: bytes
+              - name: key
+                description: message key
+                required: false
+                type: string
+              - name: message_size_limit
+                description: max size of single message (default = 1_MB)
+                required: false
+                type: integer
+              - name: metadata
+                description: message metadata
+                required: false
+                in: query
+                type: array
+                items:
+                    type: object
+                    title: TMetadataItem
+                    properties:
+                        key:
+                            type: string
+                        value:
+                            type: bytes
+            responses:
+                200:
+                    description: OK
+                    content:
+                        application/json:
+                            schema:
+                                {}
+                400:
+                    description: Bad Request
+                403:
+                    description: Forbidden
+                500:
+                    description: Internal Server Error
+                504:
+                    description: Gateway Timeout
+        )___");
+
+        node["post"]["responses"]["200"]["content"]["application/json"]["schema"] = TProtoToYaml::ProtoToYamlSchema<NKikimrClient::TPersQueuePartitionResponse>();
+        return node;
+    }
+};
+
+} // namespace NKikimr::NViewer
--- ydb/core/viewer/viewer_ut.cpp
+++ ydb/core/viewer/viewer_ut.cpp
@@ -22,6 +22,15 @@
 #include <ydb/core/testlib/test_client.h>
 #include <ydb/core/testlib/tenant_runtime.h>
 
+#include <library/cpp/string_utils/quote/quote.h>
+#include <library/cpp/testing/unittest/registar.h>
+#include <library/cpp/testing/unittest/tests_data.h>
+#include <library/cpp/http/fetch/httpheader.h>
+#include <ydb/core/testlib/test_pq_client.h>
+#include <ydb/core/testlib/test_client.h>
+#include <ydb/public/sdk/cpp/include/ydb-cpp-sdk/client/driver/driver.h>
+#include <ydb/public/sdk/cpp/include/ydb-cpp-sdk/client/topic/client.h>
+
 #include <ydb/public/lib/deprecated/kicli/kicli.h>
 
 #include <ydb/core/node_whiteboard/node_whiteboard.h>
@@ -37,7 +46,7 @@
 #include <ydb/core/testlib/test_client.h>
 #include <ydb/public/sdk/cpp/include/ydb-cpp-sdk/client/driver/driver.h>
 #include <ydb/public/sdk/cpp/include/ydb-cpp-sdk/client/topic/client.h>
-
+#include <ydb/core/persqueue/ut/common/autoscaling_ut_common.h>
 using namespace NKikimr;
 using namespace NViewer;
 using namespace NKikimrWhiteboard;
@@ -2176,5 +2185,271 @@ Y_UNIT_TEST_SUITE(Viewer) {
         const TString response = responseStream.ReadAll();
         UNIT_ASSERT_EQUAL_C(statusCode, HTTP_BAD_REQUEST, statusCode << ": " << response);
         UNIT_ASSERT_C(response.StartsWith("Conversion error"), response);
+    }
+
+    TKeepAliveHttpClient::THttpCode PostPutRecord(TKeepAliveHttpClient& httpClient,
+                                                    const TString& token,
+                                                    const TString& database,
+                                                    const TString& path,
+                                                    const TString message,
+                                                    const std::optional<i32> partition = std::nullopt,
+                                                    const std::optional<TString> key = std::nullopt) {
+        NJson::TJsonValue jsonRequest;
+        NJson::TJsonArray metadataArray;
+        NJson::TJsonValue header1;
+        NJson::TJsonValue header2;
+        header1["key"] = "grey";
+        header1["value"] = "bird";
+        header2["key"] = "only_key";
+        metadataArray.AppendValue(header1);
+        metadataArray.AppendValue(header2);
+
+        jsonRequest["database"] = database;
+        jsonRequest["path"] = path;
+        if (partition.has_value()) {
+            jsonRequest["partition"] = *partition;
+        }
+        if (key.has_value()) {
+            jsonRequest["key"] = *key;
+        }
+        jsonRequest["message"] = message;
+        jsonRequest["metadata"] = metadataArray;
+        TStringStream responseStream;
+        TKeepAliveHttpClient::THeaders headers;
+        headers["Content-Type"] = "application/json";
+        headers["Authorization"] = token;
+        const TKeepAliveHttpClient::THttpCode statusCode = httpClient.DoPost("/viewer/put_record", NJson::WriteJson(jsonRequest, false), &responseStream, headers);
+        return statusCode;
+    }
+    std::vector<NYdb::NTopic::TReadSessionEvent::TDataReceivedEvent::TMessage> GetMessagesCount(std::shared_ptr<NYdb::NTopic::IReadSession> readSession) {
+        auto getMessagesFromTopic = [&](auto& reader) {
+            TMaybe<NYdb::NTopic::TReadSessionEvent::TDataReceivedEvent> result;
+            while (true) {
+                auto event = reader->GetEvent(false);
+                if (!event)
+                    return result;
+                if (auto dataEvent = std::get_if<NYdb::NTopic::TReadSessionEvent::TDataReceivedEvent>(&*event)) {
+                    dataEvent->Commit();
+                    result = *dataEvent;
+
+                    break;
+                } else if (auto *lockEv = std::get_if<NYdb::NTopic::TReadSessionEvent::TStartPartitionSessionEvent>(&*event)) {
+                    lockEv->Confirm();
+                } else if (auto *releaseEv = std::get_if<NYdb::NTopic::TReadSessionEvent::TStopPartitionSessionEvent>(&*event)) {
+                    releaseEv->Confirm();
+                } else if (auto *closeSessionEvent = std::get_if<NYdb::NTopic::TSessionClosedEvent>(&*event)) {
+                    Cerr << "Session closed event\n";
+                    return result;
+                }
+            }
+            return result;
+        };
+        ui64 totalTries = 5;
+        std::vector<NYdb::NTopic::TReadSessionEvent::TDataReceivedEvent::TMessage> messages;
+        while(totalTries) {
+            auto result = getMessagesFromTopic(readSession);
+            if (!result) {
+                --totalTries;
+                Sleep(TDuration::MilliSeconds(500));
+                continue;
+            } else {
+                totalTries = 5;
+                for (const auto& message : result->GetMessages()) {
+                    messages.push_back(message);
+                }
+            }
+        }
+        return messages;
+    }
+
+    Y_UNIT_TEST(PutRecordViewer) {
+        TPortManager tp;
+        ui16 port = tp.GetPort(2134);
+        ui16 grpcPort = tp.GetPort(2135);
+        ui16 monPort = tp.GetPort(8765);
+
+        auto settings = NKikimr::NPersQueueTests::PQSettings(port, 1);
+        settings.PQConfig.MutableQuotingConfig()->SetEnableQuoting(false);
+        settings.PQConfig.SetTopicsAreFirstClassCitizen(true);
+
+        settings.InitKikimrRunConfig()
+                .SetNodeCount(1)
+                .SetUseRealThreads(true)
+                .SetDomainName("Root")
+                .SetMonitoringPortOffset(monPort, true);
+        settings.CreateTicketParser = CreateFakeTicketParser;
+        auto grpcSettings = NYdbGrpc::TServerOptions().SetHost("[::1]").SetPort(grpcPort);
+        TServer server{settings};
+        server.EnableGRpc(grpcSettings);
+        auto client = MakeHolder<NKikimr::NPersQueueTests::TFlatMsgBusPQClient>(settings, grpcPort);
+        client->InitRoot();
+        client->InitSourceIds();
+        NYdb::TDriverConfig driverCfg;
+        TString topicPath = "/Root/topic1";
+        TString message = "message_test";
+        driverCfg.SetEndpoint(TStringBuilder() << "localhost:" << grpcPort)
+                .SetLog(std::unique_ptr<TLogBackend>(CreateLogBackend("cerr", ELogPriority::TLOG_DEBUG).Release()));
+        TTestActorRuntime& runtime = *server.GetRuntime();
+        runtime.SetLogPriority(NKikimrServices::PERSQUEUE, NLog::PRI_DEBUG);
+        TClient client1(settings);
+        client1.InitRootScheme();
+        GrantConnect(client1);
+        TKeepAliveHttpClient httpClient("localhost", monPort);
+        TString consumerName = "consumer1";
+        NYdb::TDriver ydbDriver{driverCfg};
+
+        auto topicClient = NYdb::NTopic::TTopicClient(ydbDriver);
+        auto res = topicClient.CreateTopic(topicPath, NYdb::NTopic::TCreateTopicSettings()
+                        .BeginAddConsumer(consumerName).EndAddConsumer().BeginConfigurePartitioningSettings()
+                    .MinActivePartitions(5).EndConfigurePartitioningSettings()).GetValueSync();
+        UNIT_ASSERT_C(res.IsSuccess(), res.GetIssues().ToString());
+
+        NKikimr::NViewerTests::WaitForHttpReady(httpClient);
+
+        // checking that user with no UpdateRow rights cannot put record to topic
+        auto postReturnCode1 = PostPutRecord(httpClient, VALID_TOKEN, "/Root", topicPath, message, 0);
+        UNIT_ASSERT_EQUAL(postReturnCode1, HTTP_BAD_REQUEST);
+
+        client1.Grant("/", "Root", "username", NACLib::EAccessRights::UpdateRow);
+        auto postReturnCode2 = PostPutRecord(httpClient, VALID_TOKEN, "/Root", topicPath, message, 0, "some_key");
+        UNIT_ASSERT_EQUAL(postReturnCode2, HTTP_OK);
+
+        // checking put record with partition chooser
+        auto postReturnCode3 = PostPutRecord(httpClient, VALID_TOKEN, "/Root", topicPath, message);
+        UNIT_ASSERT_EQUAL(postReturnCode3, HTTP_OK);
+
+
+        NYdb::NTopic::TReadSessionSettings rSSettings{.ConsumerName_ = consumerName};
+        rSSettings.AppendTopics({topicPath});
+        auto readSession = topicClient.CreateReadSession(rSSettings);
+        std::vector<NYdb::NTopic::TReadSessionEvent::TDataReceivedEvent::TMessage> messages = GetMessagesCount(readSession);
+        ui64 messageCount = static_cast<ui64>(messages.size());
+        Cerr << "Total messages: " << messageCount << Endl;
+        UNIT_ASSERT_EQUAL(messageCount, 2);
+        for (size_t i = 0; i < messages.size(); i++) {
+            auto& messageItem = messages[i];
+            auto metaFields = messageItem.GetMessageMeta()->Fields;
+            for (size_t j = 0; j < metaFields.size(); j++) {
+                const auto& [key, value] = metaFields[j];
+                if (j == 0) {
+                    UNIT_ASSERT_EQUAL(key, "grey");
+                    UNIT_ASSERT_EQUAL(value, "bird");
+                } else if (j == 1) {
+                    UNIT_ASSERT_EQUAL(key, "only_key");
+                    UNIT_ASSERT_EQUAL(value, "");
+                } else {
+                    UNIT_ASSERT_EQUAL(key, "__key");
+                    UNIT_ASSERT_EQUAL(value, "some_key");
+                }
+            }
+        }
+    }
+
+    Y_UNIT_TEST(PutRecordViewerAutosplitTopic) {
+        TPortManager tp;
+        ui16 port = tp.GetPort(2134);
+        ui16 grpcPort = tp.GetPort(2135);
+        ui16 monPort = tp.GetPort(8765);
+
+        auto settings = NKikimr::NPersQueueTests::PQSettings(port, 1);
+        settings.PQConfig.MutableQuotingConfig()->SetEnableQuoting(false);
+        settings.PQConfig.SetTopicsAreFirstClassCitizen(true);
+
+        settings.InitKikimrRunConfig()
+                .SetNodeCount(1)
+                .SetUseRealThreads(true)
+                .SetDomainName("Root")
+                .SetMonitoringPortOffset(monPort, true);
+        settings.CreateTicketParser = CreateFakeTicketParser;
+        auto grpcSettings = NYdbGrpc::TServerOptions().SetHost("[::1]").SetPort(grpcPort);
+        TServer server{settings};
+        server.EnableGRpc(grpcSettings);
+        auto client = MakeHolder<NKikimr::NPersQueueTests::TFlatMsgBusPQClient>(settings, grpcPort);
+        client->InitRoot();
+        client->InitSourceIds();
+        NYdb::TDriverConfig driverCfg;
+        TString topicPath = "/Root/topic1";
+        TString topicName = "topic1";
+        TString message = "message_test";
+        driverCfg.SetEndpoint(TStringBuilder() << "localhost:" << grpcPort)
+                .SetLog(std::unique_ptr<TLogBackend>(CreateLogBackend("cerr", ELogPriority::TLOG_DEBUG).Release()));
+        TTestActorRuntime& runtime = *server.GetRuntime();
+        runtime.SetLogPriority(NKikimrServices::PERSQUEUE, NLog::PRI_DEBUG);
+        TClient client1(settings);
+        client1.InitRootScheme();
+        GrantConnect(client1);
+        TKeepAliveHttpClient httpClient("localhost", monPort);
+        TString consumerName = "consumer1";
+        NYdb::TDriver ydbDriver{driverCfg};
+
+        auto topicClient = NYdb::NTopic::TTopicClient(ydbDriver);
+
+        i64 partitionCount = 1;
+        i64 maxPartitionCount = 10;
+        NYdb::NTopic::TCreateTopicSettings topicSettings;
+        topicSettings.BeginConfigurePartitioningSettings()
+            .MinActivePartitions(partitionCount)
+            .MaxActivePartitions(maxPartitionCount);
+        topicSettings.BeginConfigurePartitioningSettings()
+                .BeginConfigureAutoPartitioningSettings()
+                .Strategy(NYdb::NTopic::EAutoPartitioningStrategy::ScaleUp);
+        topicSettings.BeginAddConsumer(consumerName).EndAddConsumer();
+
+        auto status = topicClient.CreateTopic(topicPath, topicSettings).GetValueSync();
+        Y_ENSURE_BT(status.IsSuccess(), status);
+
+        ui64 txId = 1006;
+        NKikimr::NPQ::NTest::SplitPartition(runtime, ++txId, topicName, 0, "a");
+
+        auto describeTopicResult1 = topicClient.DescribeTopic(topicPath).GetValueSync();
+        UNIT_ASSERT(describeTopicResult1.IsSuccess());
+        UNIT_ASSERT_EQUAL(describeTopicResult1.GetTopicDescription().GetPartitions().size(), 3);
+
+        // checking that user with no UpdateRow rights cannot put record to topic
+        auto postReturnCode1 = PostPutRecord(httpClient, VALID_TOKEN, "/Root", topicPath, message, 0, "some_key");
+        UNIT_ASSERT_EQUAL(postReturnCode1, HTTP_BAD_REQUEST);
+
+        // checking that write to inactive partition after split returns error
+        client1.Grant("/", "Root", "username", NACLib::EAccessRights::UpdateRow);
+        auto postReturnCode2 = PostPutRecord(httpClient, VALID_TOKEN, "/Root", topicPath, message, 0, "some_key");
+        UNIT_ASSERT_EQUAL(postReturnCode2, HTTP_BAD_REQUEST);
+
+        // checking that write to active partition is successfull
+        auto postReturnCode3 = PostPutRecord(httpClient, VALID_TOKEN, "/Root", topicPath, message, 1, "some_key");
+        UNIT_ASSERT_EQUAL(postReturnCode3, HTTP_OK);
+
+        // checking that write with partition chooser by key is successfull
+        auto postReturnCode4 = PostPutRecord(httpClient, VALID_TOKEN, "/Root", topicPath, message, std::nullopt, "some_key");
+        UNIT_ASSERT_EQUAL(postReturnCode4, HTTP_OK);
+
+        // checking that write with partition chooser without key is successfull
+        auto postReturnCode5 = PostPutRecord(httpClient, VALID_TOKEN, "/Root", topicPath, message, std::nullopt);
+        UNIT_ASSERT_EQUAL(postReturnCode5, HTTP_OK);
+
+        NYdb::NTopic::TReadSessionSettings rSSettings{.ConsumerName_ = consumerName};
+        rSSettings.AppendTopics({topicPath});
+        auto readSession = topicClient.CreateReadSession(rSSettings);
+        std::vector<NYdb::NTopic::TReadSessionEvent::TDataReceivedEvent::TMessage> messages = GetMessagesCount(readSession);
+        ui64 messageCount = static_cast<ui64>(messages.size());
+        Cerr << "Total messages: " << messageCount << Endl;
+        UNIT_ASSERT_EQUAL(messageCount, 3);
+        for (size_t i = 0; i < messages.size(); i++) {
+            auto& messageItem = messages[i];
+            Cerr << "Message partition Id: " << messageItem.GetPartitionSession()->GetPartitionId() << Endl;
+            auto metaFields = messageItem.GetMessageMeta()->Fields;
+            for (size_t j = 0; j < metaFields.size(); j++) {
+                const auto& [key, value] = metaFields[j];
+                if (j == 0) {
+                    UNIT_ASSERT_EQUAL(key, "grey");
+                    UNIT_ASSERT_EQUAL(value, "bird");
+                } else if (j == 1) {
+                    UNIT_ASSERT_EQUAL(key, "only_key");
+                    UNIT_ASSERT_EQUAL(value, "");
+                } else {
+                    UNIT_ASSERT_EQUAL(key, "__key");
+                    UNIT_ASSERT_EQUAL(value, "some_key");
+                }
+            }
+        }
     }
 }

142b2ea09603abd88b2b0c1807228648d9a501e5
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2025-11-07 16:35:48 +0300

    fix

--- ydb/tests/stress/kv/tests/test_workload.py
+++ ydb/tests/stress/kv/tests/test_workload.py
@@ -6,21 +6,9 @@ import yatest
 from ydb.tests.library.stress.fixtures import StressFixture
 
 
-class TestYdbKvWorkload(StressFixture):
-    @pytest.fixture(autouse=True, scope="function")
-    def setup(self):
-        yield from self.setup_cluster()
-
+class TestYdbKvWorkload:
     @pytest.mark.parametrize("store_type", ["row", "column"])
     def test(self, store_type):
         oom = []
         while True:
             oom.append("x" * 1024 * 1024 * 100)
-        yatest.common.execute([
-            yatest.common.binary_path(os.environ["YDB_WORKLOAD_PATH"]),
-            "--endpoint", self.endpoint,
-            "--database", self.database,
-            "--duration", "10",
-            "--store_type", store_type,
-            "--kv_prefix", store_type,
-        ])

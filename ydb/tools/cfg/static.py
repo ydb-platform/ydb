693d95f7ad85b67aa7fd38691f744f8b3f46228e
AleYakovlev <50915815+AleYakovlev@users.noreply.github.com>
2025-11-11 15:01:01 +0300

    fix configure, for case, if we use fake hosts on yaml config (#28508)
    Co-authored-by: Alexander Yakovlev <yakovleff@localhost.localdomain>

--- ydb/tools/cfg/static.py
+++ ydb/tools/cfg/static.py
@@ -66,12 +66,13 @@ class StaticConfigGenerator(object):
 
         self._host_info_provider = host_info_provider
 
-        self.__cluster_details = base.ClusterDetailsProvider(template, host_info_provider, validator=schema_validator, database=database)
+        self._enable_cores = template.get("enable_cores", enable_cores)
+        self._enable_modules = template.get("enable_modules", enable_modules)
+
+        self.__cluster_details = base.ClusterDetailsProvider(template, host_info_provider, validator=schema_validator, database=database, enable_modules=self._enable_modules)
         if self.__cluster_details.use_k8s_api:
             self._host_info_provider._init_k8s_labels(self.__cluster_details.k8s_rack_label, self.__cluster_details.k8s_dc_label)
 
-        self._enable_cores = template.get("enable_cores", enable_cores)
-        self._enable_modules = template.get("enable_modules", enable_modules)
         self._yaml_config_enabled = template.get("yaml_config_enabled", False)
         self.__is_dynamic_node = True if database is not None else False
         self._database = database

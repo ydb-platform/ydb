13f34a9f7cc0883eb032c249cad4532bf88b1105
aneporada <aneporada@yandex-team.com>
2025-11-11 14:09:11 +0300

    [refactoring] Switch to IsOptimizerEnabled/IsOptimizerDisabled commit_hash:5999939d643dc2d073b5fa40d1b4be09d89a18ba

--- yql/essentials/core/common_opt/yql_co_simple1.cpp
+++ yql/essentials/core/common_opt/yql_co_simple1.cpp
@@ -3845,8 +3845,8 @@ TExprNode::TPtr OptimizeMerge(const TExprNode::TPtr& node, TExprContext& ctx, TO
 
 bool IsEarlyExpandOfSkipNullAllowed(const TOptimizeContext& optCtx) {
     YQL_ENSURE(optCtx.Types);
-    static const TString skipNullFlags = to_lower(TString("EarlyExpandSkipNull"));
-    return optCtx.Types->OptimizerFlags.contains(skipNullFlags);
+    static const char optName[] = "EarlyExpandSkipNull";
+    return IsOptimizerEnabled<optName>(*optCtx.Types) && !IsOptimizerDisabled<optName>(*optCtx.Types);
 }
 
 TExprNode::TPtr ReplaceFuncWithImpl(const TExprNode::TPtr& node, TExprContext& ctx, TOptimizeContext& optCtx) {

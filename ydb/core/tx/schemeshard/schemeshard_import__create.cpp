64389d5e375f6d6288d7a41cb341f2eeb491d573
st-shchetinin <st-shchetinin@ydb.tech>
2025-11-11 15:18:46 +0300

    scheme getter

--- ydb/core/tx/schemeshard/schemeshard_import__create.cpp
+++ ydb/core/tx/schemeshard/schemeshard_import__create.cpp
@@ -479,11 +479,8 @@ private:
             item.SchemeGetter = ctx.RegisterWithSameMailbox(CreateSchemeGetter(Self->SelfId(), importInfo, itemIdx, item.ExportItemIV));
             Self->RunningImportSchemeGetters.emplace(item.SchemeGetter);
         } else {
-            LOG_I("TImport::TTxProgress: Get scheme for FS import is not supported"
-                << ": info# " << importInfo->ToString()
-                << ", item# " << item.ToString(itemIdx));
-            // item.SchemeGetter = ctx.RegisterWithSameMailbox(CreateSchemeGetterFS(Self->SelfId(), importInfo, itemIdx));
-            // Self->RunningImportSchemeGetters.emplace(item.SchemeGetter);
+            item.SchemeGetter = ctx.RegisterWithSameMailbox(CreateSchemeGetterFS(Self->SelfId(), importInfo, itemIdx));
+            Self->RunningImportSchemeGetters.emplace(item.SchemeGetter);
         }
     }
 
@@ -1182,7 +1179,7 @@ private:
             if (importInfo->Kind == TImportInfo::EKind::S3) {
                 auto settings = importInfo->GetS3Settings();
                 if (settings.has_encryption_settings() != importInfo->SchemaMapping->Items[0].IV.Defined()) {
-                    return CancelAndPersist(db, importInfo, -1, {}, "incorrect schema mapping");
+                return CancelAndPersist(db, importInfo, -1, {}, "incorrect schema mapping");
                 }
             }
         }

83558ce5327a27b3e005d914a23abaebe017efc4
Andrei Molotkov <molotkov-and@ydb.tech>
2025-11-12 13:41:41 +0000

    Add ssl target name override option

--- ydb/library/grpc/actor_client/grpc_service_settings.h
+++ ydb/library/grpc/actor_client/grpc_service_settings.h
@@ -14,6 +14,7 @@ struct TGrpcClientSettings {
     bool EnableSsl = false;
     ui64 RequestTimeoutMs = 10000; // 10 seconds
     std::unordered_map<TString, TString> Headers;
+    TString SslTargetNameOverride {};
 };
 
 } // namespace NGrpcActorClient

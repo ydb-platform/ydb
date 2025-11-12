51a5fafdba1f6795bb49e8c1c794284c800f1b6d
vporyadke <zalyalov@ydb.tech>
2025-11-12 16:23:40 +0300

    lower priority for tablet metrics (#28554)

--- ydb/core/mind/hive/hive_ut.cpp
+++ ydb/core/mind/hive/hive_ut.cpp
@@ -5859,6 +5859,8 @@ Y_UNIT_TEST_SUITE(THiveTest) {
             runtime.SendToPipe(hiveTablet, senderA, metrics.Release());
         }
 
+        runtime.DispatchEvents({}, TDuration::MilliSeconds(300));
+
         auto createTablet = MakeHolder<TEvHive::TEvCreateTablet>(testerTablet, 100500 + tablets.size(), tabletType, BINDED_CHANNELS);
         ui64 newTablet = SendCreateTestTablet(runtime, hiveTablet, testerTablet, std::move(createTablet), 0, false);
 

6851bccc7e4c7e3a4a8957726ed37b2b4c1ad46b
vporyadke <zalyalov@ydb.tech>
2025-11-12 16:23:40 +0300

    lower priority for tablet metrics (#28554)

--- ydb/core/mind/hive/hive_impl.cpp
+++ ydb/core/mind/hive/hive_impl.cpp
@@ -3147,7 +3147,9 @@ void THive::RequestPoolsInformation() {
 }
 
 ui32 THive::GetEventPriority(IEventHandle* ev) {
+    // lower number = higher priority
     switch (ev->GetTypeRewrite()) {
+        // requests from viewer/healthcheck - need to be answered fast so that hive is not reported as unresponsive
         case TEvHive::EvRequestHiveInfo:
         case TEvHive::EvRequestHiveDomainStats:
         case TEvHive::EvRequestHiveNodeStats:
@@ -3155,6 +3157,9 @@ ui32 THive::GetEventPriority(IEventHandle* ev) {
             return 10;
         default:
             return 50;
+        // update metrics - there can be a lot of these, but they have no urgency
+        case TEvHive::EvTabletMetrics:
+            return 100;
     }
 }
 

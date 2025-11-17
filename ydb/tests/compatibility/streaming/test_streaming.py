7a4de7b03e3008b16b28fccb1ae8bc79c97a0a11
Dmitry Kardymon <kardymon-d@ydb.tech>
2025-11-17 14:26:48 +0000

    add testst

--- ydb/tests/compatibility/streaming/test_streaming.py
+++ ydb/tests/compatibility/streaming/test_streaming.py
@@ -4,12 +4,11 @@ import os
 import pytest
 import time
 
-from ydb.tests.oss.ydb_sdk_import import ydb
-# from ydb.tests.library.compatibility.fixtures import MixedClusterFixture, RestartToAnotherVersionFixture, RollingUpgradeAndDowngradeFixture
-from ydb.tests.library.compatibility.fixtures import RestartToAnotherVersionFixture
-from ydb.tests.tools.datastreams_helpers.data_plane import write_stream, read_stream
+from ydb.tests.library.compatibility.fixtures import MixedClusterFixture, RestartToAnotherVersionFixture # , RollingUpgradeAndDowngradeFixture
 from ydb.tests.library.harness.util import LogLevels
 from ydb.tests.library.test_meta import link_test_case
+from ydb.tests.oss.ydb_sdk_import import ydb
+from ydb.tests.tools.datastreams_helpers.data_plane import write_stream, read_stream
 
 logger = logging.getLogger(__name__)
 
@@ -19,10 +18,9 @@ class StreamingTestBase:
         logger.debug(f"setup_cluster, versions {self.versions}")
 
         if min(self.versions) < (25, 4):
+            logger.debug("skip test, only available since 25-4")
             pytest.skip("Only available since 25-4")
 
-        # output_path = yatest.common.test_output_path()
-        # self.output_f = open(os.path.join(output_path, "out.log"), "w")
         os.environ["YDB_TEST_DEFAULT_CHECKPOINTING_PERIOD_MS"] = "200"
         os.environ["YDB_TEST_LEASE_DURATION_SEC"] = "15"
         yield from super().setup_cluster(
@@ -79,6 +77,39 @@ class StreamingTestBase:
             query = f"""
                 CREATE STREAMING QUERY `my_queries/query_name` AS DO BEGIN
                 $input = (
+                    SELECT * FROM
+                        source_name.`{self.input_topic}` WITH (
+                            FORMAT = 'json_each_row',
+                            SCHEMA (time String NOT NULL, level String NOT NULL, host String NOT NULL)
+                        )
+                );
+                $filtered = (SELECT * FROM $input WHERE level == 'error');
+
+                $number_errors = (
+                    SELECT host, COUNT(*) AS error_count, CAST(HOP_START() AS String) AS ts
+                    FROM $filtered
+                    GROUP BY
+                        HoppingWindow(CAST(time AS Timestamp), 'PT600S', 'PT600S'),
+                        host
+                );
+
+                $json = (SELECT ToBytes(Unwrap(Yson::SerializeJson(Yson::From(TableRow()))))
+                    FROM $number_errors
+                );
+
+                INSERT INTO source_name.`{self.output_topic}`
+                SELECT * FROM $json;
+                END DO;
+
+            """
+            session_pool.execute_with_retries(query)
+
+    def create_simple_streaming_query(self):
+        logger.debug("create_simple_streaming_query")
+        with ydb.QuerySessionPool(self.driver) as session_pool:
+            query = f"""
+                CREATE STREAMING QUERY `my_queries/query_name` AS DO BEGIN
+                $input = (
                     SELECT
                         *
                     FROM
@@ -88,104 +119,75 @@ class StreamingTestBase:
                         )
                 );
 
-                $filtered = (
-                    SELECT
-                        *
-                    FROM
-                        $input
-                    WHERE
-                        level == 'error'
-                );
-
-                $number_errors = (
-                    SELECT
-                        host,
-                        COUNT(*) AS error_count,
-                        CAST(HOP_START() AS String) AS ts
-                    FROM
-                        $filtered
-                    GROUP BY
-                        HoppingWindow(CAST(time AS Timestamp), 'PT600S', 'PT600S'),
-                        host
-                );
-
-                $json = (
-                    SELECT
-                        ToBytes(Unwrap(Yson::SerializeJson(Yson::From(TableRow()))))
-                    FROM
-                        $number_errors
+                $json = (SELECT ToBytes(Unwrap(Yson::SerializeJson(Yson::From(TableRow()))))
+                    FROM $input
                 );
 
                 INSERT INTO source_name.`{self.output_topic}`
-                SELECT
-                    *
-                FROM
-                    $json
-                ;
+                SELECT * FROM $json;
                 END DO;
 
             """
             session_pool.execute_with_retries(query)
 
-    def do_test_part1(self):
-        logger.debug("do_test_part1")
+    def do_write_read(self, input, expected_output):
+        logger.debug("do_write_read")
         endpoint = f"localhost:{self.cluster.nodes[1].port}"
         time.sleep(2)
-        data = [
+        logger.debug("write data to stream")
+        write_stream(path=self.input_topic, data=input, database=self.database_path, endpoint=endpoint)
+        logger.debug("read data from stream")
+        assert sorted(read_stream(
+            path=self.output_topic,
+            messages_count=len(expected_output),
+            consumer_name=self.consumer_name,
+            database=self.database_path,
+            endpoint=endpoint)) == sorted(expected_output)
+
+    def do_test_part1(self):
+        input = [
             '{"time": "2025-01-01T00:00:00.000000Z", "level": "error", "host": "host-1"}',
             '{"time": "2025-01-01T00:04:00.000000Z", "level": "error", "host": "host-2"}',
             '{"time": "2025-01-01T00:08:00.000000Z", "level": "error", "host": "host-1"}',
             '{"time": "2025-01-01T00:12:00.000000Z", "level": "error", "host": "host-2"}',
             '{"time": "2025-01-01T00:12:00.000000Z", "level": "error", "host": "host-1"}']
-
-        logger.debug("write data to stream")
-        write_stream(path=self.input_topic, data=data, database=self.database_path, endpoint=endpoint)
-
         expected_data = sorted([
             '{"error_count":1,"host":"host-2","ts":"2025-01-01T00:00:00Z"}',
             '{"error_count":2,"host":"host-1","ts":"2025-01-01T00:00:00Z"}'])
-
-        logger.debug("read data from stream")
-        assert sorted(read_stream(path=self.output_topic, messages_count=len(expected_data), consumer_name=self.consumer_name, database=self.database_path, endpoint=endpoint)) == expected_data
+        self.do_write_read(input, expected_data)
 
     def do_test_part2(self):
-
-        logger.debug("do_test_part2")
-        endpoint = f"localhost:{self.cluster.nodes[1].port}"
-        time.sleep(2)
-        data = [
+        input = [
             '{"time": "2025-01-01T00:15:00.000000Z", "level": "error", "host": "host-2"}',
             '{"time": "2025-01-01T00:22:00.000000Z", "level": "error", "host": "host-1"}',
             '{"time": "2025-01-01T00:22:00.000000Z", "level": "error", "host": "host-2"}']
-
-        logger.debug("write data to stream")
-        write_stream(path=self.input_topic, data=data, database=self.database_path, endpoint=endpoint)
-
         expected_data = sorted([
             '{"error_count":2,"host":"host-2","ts":"2025-01-01T00:10:00Z"}',
             '{"error_count":1,"host":"host-1","ts":"2025-01-01T00:10:00Z"}'])
-
-        logger.debug("read data from stream")
-        assert sorted(read_stream(path=self.output_topic, messages_count=len(expected_data), consumer_name=self.consumer_name, database=self.database_path, endpoint=endpoint)) == expected_data
+        self.do_write_read(input, expected_data)
 
 
-# class TestExternalDataTableMixedCluster(StreamingTestBase, MixedClusterFixture):
-#     @pytest.fixture(autouse=True, scope="function")
-#     def setup(self):
-#         yield from self.setup_cluster()
-
-#     def test_external_data_source(self):
-#         self.create_external_data_source()
-#         self.do_test_external_data()
-
-
-class TestExternalDataTableRestartToAnotherVersion(StreamingTestBase, RestartToAnotherVersionFixture):
+class TestStreamingMixedCluster(StreamingTestBase, MixedClusterFixture):
     @pytest.fixture(autouse=True, scope="function")
     def setup(self):
         yield from self.setup_cluster()
 
     @link_test_case("#27924")
-    def test_external_data_source(self):
+    def test_mixed_cluster(self):
+        self.create_topics()
+        self.create_external_data_source()
+        self.create_streaming_query()
+        self.do_test_part1()
+        self.do_test_part2()
+
+
+class TestStreamingRestartToAnotherVersion(StreamingTestBase, RestartToAnotherVersionFixture):
+    @pytest.fixture(autouse=True, scope="function")
+    def setup(self):
+        yield from self.setup_cluster()
+
+    @link_test_case("#27924")
+    def test_restart_to_another_version(self):
         self.create_topics()
         self.create_external_data_source()
         self.create_streaming_query()
@@ -193,13 +195,22 @@ class TestExternalDataTableRestartToAnotherVersion(StreamingTestBase, RestartToA
         self.change_cluster_version()
         self.do_test_part2()
 
-
-# class TestExternalDataTableRollingUpgradeAndDowngrade(StreamingTestBase, RollingUpgradeAndDowngradeFixture):
+# TODO: uncomment after stable-25.4
+# class TestStreamingRollingUpgradeAndDowngrade(StreamingTestBase, RollingUpgradeAndDowngradeFixture):
 #     @pytest.fixture(autouse=True, scope="function")
 #     def setup(self):
 #         yield from self.setup_cluster()
 
-#     def test_external_data_source(self):
+#     @link_test_case("#27924")
+#     def test_rolling_upgrage(self):
+#         self.create_topics()
 #         self.create_external_data_source()
-#         for _ in self.roll():
-#             self.do_test_external_data()
+#         self.create_simple_streaming_query();
+
+#         for _ in self.roll():  # every iteration is a step in rolling upgrade process
+#             #
+#             # 2. check written data is correct during rolling upgrade
+#             #
+#             input = ['{"time": "2025-01-01T00:15:00.000000Z", "level": "error", "host": "host-2"}']
+#             expected_data =['{"time":"2025-01-01T00:15:00.000000Z","level":"error","host":"host-2"}']
+#             self.do_write_read(input, expected_data)

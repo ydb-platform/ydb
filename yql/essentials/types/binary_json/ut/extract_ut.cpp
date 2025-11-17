608b53cc0ae126e8284535c50bae69e82867601e
Vladilen Muzychenko <vladilenmuz@ydb.tech>
2025-11-17 15:30:29 +0000

    Add array and object parsing

---
+++ yql/essentials/types/binary_json/ut/extract_ut.cpp
@@ -0,0 +1,119 @@
+#include "test_base.h"
+
+#include <yql/essentials/types/binary_json/write.h>
+#include <yql/essentials/types/binary_json/read.h>
+
+using namespace NKikimr::NBinaryJson;
+
+class TBinaryJsonExtractTest: public TBinaryJsonTestBase {
+public:
+    TBinaryJsonExtractTest()
+        : TBinaryJsonTestBase()
+    {
+    }
+
+    UNIT_TEST_SUITE(TBinaryJsonExtractTest);
+    UNIT_TEST(TestTopLevelScalar);
+    UNIT_TEST(TestTopLevelArray);
+    UNIT_TEST(TestTopLevelMap);
+    UNIT_TEST(TestComplexCases);
+    UNIT_TEST_SUITE_END();
+
+    void TestTopLevelScalar() {
+        const TVector<TString> testCases = {
+            {"1"},
+            {"1.25"},
+            {"-2.36"},
+            {"0"},
+            {"\"string\""},
+            {"\"\""},
+            {"null"},
+            {"true"},
+            {"false"},
+        };
+
+        for (const auto& testCase : testCases) {
+            const auto binaryJson = std::get<TBinaryJson>(SerializeToBinaryJson(testCase));
+            const auto reader = TBinaryJsonReader::Make(binaryJson);
+            const auto container = reader->GetRootCursor();
+            const auto cursor = container.GetElement(0);
+            const auto extractedBinaryJson = std::get<TBinaryJson>(SerializeToBinaryJson(cursor));
+
+            UNIT_ASSERT_EQUAL(SerializeToJson(binaryJson), SerializeToJson(extractedBinaryJson));
+        }
+    }
+
+    void TestTopLevelArray() {
+        const TVector<std::pair<TString, TString>> testCases = {
+            {"[1]", "1"},
+            {R"(["string"])", R"("string")"},
+            {"[true]", "true"},
+            {"[-1.2]", "-1.2"},
+            {"[null]", "null"},
+        };
+
+        for (const auto& testCase : testCases) {
+            const auto originalBinaryJson = std::get<TBinaryJson>(SerializeToBinaryJson(testCase.first));
+            const auto reader = TBinaryJsonReader::Make(originalBinaryJson);
+            const auto container = reader->GetRootCursor();
+            const auto cursor = container.GetArrayIterator().Next();
+            const auto extractedBinaryJson = std::get<TBinaryJson>(SerializeToBinaryJson(cursor));
+
+            const auto expectedBinaryJson = std::get<TBinaryJson>(SerializeToBinaryJson(testCase.second));
+
+            UNIT_ASSERT_EQUAL(SerializeToJson(expectedBinaryJson), SerializeToJson(extractedBinaryJson));
+        }
+    }
+
+    void TestTopLevelMap() {
+        const TVector<std::pair<TString, TString>> testCases = {
+            {R"({"root": "a"})", R"("a")"},
+            {R"({"root": ""})", R"("")"},
+            {R"({"root": 1})", "1"},
+            {R"({"root": -1.2})", "-1.2"},
+            {R"({"root": true})", "true"},
+            {R"({"root": false})", "false"},
+            {R"({"root": null})", "null"},
+            {R"({"root": {}})", "{}"},
+            {R"({"root": []})", "[]"},
+        };
+
+        for (const auto& testCase : testCases) {
+            const auto originalBinaryJson = std::get<TBinaryJson>(SerializeToBinaryJson(testCase.first));
+            const auto reader = TBinaryJsonReader::Make(originalBinaryJson);
+            const auto container = reader->GetRootCursor();
+            const auto cursor = container.GetObjectIterator().Next();
+            const auto extractedBinaryJson = std::get<TBinaryJson>(SerializeToBinaryJson(cursor.second));
+
+            const auto expectedBinaryJson = std::get<TBinaryJson>(SerializeToBinaryJson(testCase.second));
+
+            UNIT_ASSERT_EQUAL(SerializeToJson(expectedBinaryJson), SerializeToJson(extractedBinaryJson));
+        }
+    }
+
+    void TestComplexCases() {
+        // clang-format off
+        const TVector<std::pair<TString, TString>> testCases = {
+            {R"({"root": {"a": "b", "c": {"d": 1, "e": [1, 2.1, -3, "f", true, false, null, {}, {"g": []}]}}})",
+                      R"({"a": "b", "c": {"d": 1, "e": [1, 2.1, -3, "f", true, false, null, {}, {"g": []}]}})"},
+
+            {R"({"root": [[], [1, 1, 1, 2], {"a": true}, null, [], [[[]]], [[42]]]})",
+                      R"([[], [1, 1, 1, 2], {"a": true}, null, [], [[[]]], [[42]]])"},
+        };
+        // clang-format on
+
+        for (const auto& testCase : testCases) {
+            const auto originalBinaryJson = std::get<TBinaryJson>(SerializeToBinaryJson(testCase.first));
+            const auto reader = TBinaryJsonReader::Make(originalBinaryJson);
+            const auto container = reader->GetRootCursor();
+            const auto cursor = container.GetObjectIterator().Next();
+            const auto extractedBinaryJson = std::get<TBinaryJson>(SerializeToBinaryJson(cursor.second));
+
+            const auto expectedBinaryJson = std::get<TBinaryJson>(SerializeToBinaryJson(testCase.second));
+
+            UNIT_ASSERT_EQUAL(SerializeToJson(expectedBinaryJson), SerializeToJson(extractedBinaryJson));
+        }
+    }
+};
+
+UNIT_TEST_SUITE_REGISTRATION(TBinaryJsonExtractTest);
--- yql/essentials/types/binary_json/write.cpp
+++ yql/essentials/types/binary_json/write.cpp
@@ -764,9 +764,7 @@ TBinaryJson SerializeToBinaryJson(const NUdf::TUnboxedValue& value) {
     return std::move(serializer).Serialize();
 }
 
-std::variant<TBinaryJson, TString> SerializeToBinaryJson(const NBinaryJson::TEntryCursor& value) {
-    TBinaryJsonCallbacks callbacks(/* throwException */ false, /* allowInf */ false);
-
+TString SerializeReadCursorToBinaryJson(TBinaryJsonCallbacks& callbacks, const NBinaryJson::TEntryCursor& value) {
     switch (value.GetType()) {
         case NBinaryJson::EEntryType::BoolFalse:
             callbacks.OnBoolean(false);
@@ -783,9 +781,52 @@ std::variant<TBinaryJson, TString> SerializeToBinaryJson(const NBinaryJson::TEnt
         case NBinaryJson::EEntryType::Number:
             callbacks.OnDouble(value.GetNumber());
             break;
-        case NBinaryJson::EEntryType::Container:
-            // TODO: Support recursive parsing and returning TBinaryJson
-            return TString("Container type is not a scalar value");
+        case NBinaryJson::EEntryType::Container: {
+            auto container = value.GetContainer();
+            if (container.GetType() == NBinaryJson::EContainerType::Array) {
+                callbacks.OnOpenArray();
+
+                auto it = container.GetArrayIterator();
+                while (it.HasNext()) {
+                    auto value = it.Next();
+                    if (auto error = SerializeReadCursorToBinaryJson(callbacks, value); !error.empty()) {
+                        return error;
+                    }
+                }
+
+                callbacks.OnCloseArray();
+
+            } else if (container.GetType() == NBinaryJson::EContainerType::Object) {
+                callbacks.OnOpenMap();
+
+                auto it = container.GetObjectIterator();
+                while (it.HasNext()) {
+                    auto [key, value] = it.Next();
+                    if (key.GetType() != NBinaryJson::EEntryType::String) {
+                        return TString("Unexpected non-string key");
+                    }
+
+                    callbacks.OnMapKey(key.GetString());
+                    if (auto error = SerializeReadCursorToBinaryJson(callbacks, value); !error.empty()) {
+                        return error;
+                    }
+                }
+
+                callbacks.OnCloseMap();
+            } else {
+                return TString("Unexpected top value scalar in container iterator");
+            }
+        }
+    }
+
+    return {};
+}
+
+std::variant<TBinaryJson, TString> SerializeToBinaryJson(const NBinaryJson::TEntryCursor& value) {
+    TBinaryJsonCallbacks callbacks(/* throwException */ false, /* allowInf */ false);
+
+    if (auto error = SerializeReadCursorToBinaryJson(callbacks, value); !error.empty()) {
+        return error;
     }
 
     TBinaryJsonSerializer serializer(std::move(callbacks).GetResult());

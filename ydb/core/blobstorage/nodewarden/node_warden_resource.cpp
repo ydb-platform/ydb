4f5f5cc9aec0cec2d16b1187652f9d33bf768172
Alexander Rutkovsky <alexvru@ydb.tech>
2025-11-17 13:49:31 +0000

    Fix split brain potential situation in distconf

--- ydb/core/blobstorage/nodewarden/node_warden_resource.cpp
+++ ydb/core/blobstorage/nodewarden/node_warden_resource.cpp
@@ -92,6 +92,7 @@ void TNodeWarden::Handle(TEvNodeWardenQueryStorageConfig::TPtr ev) {
 void TNodeWarden::Handle(TEvNodeWardenStorageConfig::TPtr ev) {
     auto *msg = ev->Get();
     StorageConfig = std::move(msg->Config);
+    CommittedStorageConfig = std::move(msg->CommittedConfig);
     SelfManagementEnabled = msg->SelfManagementEnabled;
     BridgeInfo = std::move(msg->BridgeInfo);
 
@@ -111,7 +112,7 @@ void TNodeWarden::Handle(TEvNodeWardenStorageConfig::TPtr ev) {
     }
 
     for (const TActorId& subscriber : StorageConfigSubscribers) {
-        Send(subscriber, new TEvNodeWardenStorageConfig(StorageConfig, SelfManagementEnabled, BridgeInfo));
+        Send(subscriber, new TEvNodeWardenStorageConfig(StorageConfig, SelfManagementEnabled, BridgeInfo, CommittedStorageConfig));
     }
 
     if (StorageConfig->HasConfigComposite()) {

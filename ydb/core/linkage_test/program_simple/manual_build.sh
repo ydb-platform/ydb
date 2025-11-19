2c2a1858fd74c80c744b1015c66f5f715e9e92e7
Maxim Yurchuk <maxim-yurchuk@ydb.tech>
2025-11-19 16:03:44 +0300

    fix

--- ydb/core/linkage_test/program_simple/manual_build.sh
+++ ydb/core/linkage_test/program_simple/manual_build.sh
@@ -1,7 +1,9 @@
-cc -c ../a/lib.cpp -o lib_a.o
-cc -c ../b/lib.cpp -o lib_b.o
-ar rcs lib_a.a lib_a.o
-ar rcs lib_b.a lib_b.o
-cc -c ../program/main.cpp main.o
-cc lib_a.a lib_b.a main.o 
+mkdir -p temp
+
+cc -c ../a/lib.cpp -o temp/lib_a.o
+cc -c ../b/lib.cpp -o temp/lib_b.o
+ar rcs lib_a.a temp/lib_a.o
+ar rcs lib_b.a temp/lib_b.o
+cc -c ../program/main.cpp temp/main.o
+cc temp/main.o temp/lib_a.a temp/lib_b.a
 

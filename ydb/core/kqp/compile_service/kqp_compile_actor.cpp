e045d016a70b9fc3456dafd587993940dce3d5de
Denis Khalikov <deniskhalikov@ydb.tech>
2025-11-20 13:50:28 +0300

    [Optimizers] Enable sql in with nothing (#29225)

--- ydb/core/kqp/compile_service/kqp_compile_actor.cpp
+++ ydb/core/kqp/compile_service/kqp_compile_actor.cpp
@@ -679,6 +679,7 @@ void ApplyServiceConfig(TKikimrConfiguration& kqpConfig, const TTableServiceConf
         kqpConfig.FilterPushdownOverJoinOptionalSide = true;
         kqpConfig.YqlCoreOptimizerFlags.insert("fuseequijoinsinputmultilabels");
         kqpConfig.YqlCoreOptimizerFlags.insert("pullupflatmapoverjoinmultiplelabels");
+        kqpConfig.YqlCoreOptimizerFlags.insert("sqlinwithnothingornull");
     }
 
     switch(serviceConfig.GetDefaultHashShuffleFuncType()) {

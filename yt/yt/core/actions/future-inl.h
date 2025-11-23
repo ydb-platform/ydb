3e1fb740c52011acccd3e5c73b6b8e250c161364
babenko <babenko@yandex-team.com>
2025-11-23 22:53:02 +0300

    YT-26319: Add r-value-this overloads for AsVoid, AsCancelable, AsUnique commit_hash:f14f9f2b8def90a38f46f0328cf2b5ce86aa73c7

--- yt/yt/core/actions/future-inl.h
+++ yt/yt/core/actions/future-inl.h
@@ -1299,15 +1299,27 @@ TFuture<U> TFutureBase<T>::As() const
 }
 
 template <class T>
-TFuture<void> TFutureBase<T>::AsVoid() const
+TFuture<void> TFutureBase<T>::AsVoid() const&
 {
     return TFuture<void>(Impl_);
 }
 
 template <class T>
-TCancelable TFutureBase<T>::AsCancelable() const
+TFuture<void> TFutureBase<T>::AsVoid() &&
 {
-    return Impl_->AsCancelable();
+    return TFuture<void>(std::move(Impl_));
+}
+
+template <class T>
+TCancelable TFutureBase<T>::AsCancelable() const&
+{
+    return TCancelable(Impl_);
+}
+
+template <class T>
+TCancelable TFutureBase<T>::AsCancelable() &&
+{
+    return TCancelable(std::move(Impl_));
 }
 
 template <class T>
@@ -1378,9 +1390,15 @@ TFuture<R> TFuture<T>::ApplyUnique(TCallback<TFuture<R>(T&&)> callback) const
 }
 
 template <class T>
-TUniqueFuture<T> TFuture<T>::AsUnique() const
+TUniqueFuture<T> TFuture<T>::AsUnique() const&
 {
     return TUniqueFuture<T>(this->Impl_);
+}
+
+template <class T>
+TUniqueFuture<T> TFuture<T>::AsUnique() &&
+{
+    return TUniqueFuture<T>(std::move(this->Impl_));
 }
 
 template <class T>

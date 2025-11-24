4a38a0a78e3620523d0d5b0567dc868e7ab93007
mregrock <mregrock@ydb.tech>
2025-11-24 11:40:01 +0300

    Add testshard stress test (#29307)

--- ydb/services/test_shard/grpc_service.cpp
+++ ydb/services/test_shard/grpc_service.cpp
@@ -9,32 +9,32 @@
 namespace NKikimr::NGRpcService {
 
 TTestShardGRpcService::TTestShardGRpcService(NActors::TActorSystem* actorSystem, TIntrusivePtr<NMonitoring::TDynamicCounters> counters, NActors::TActorId grpcRequestProxyId)
-    : ActorSystem_(actorSystem)
-    , Counters_(std::move(counters))
-    , GRpcRequestProxyId_(grpcRequestProxyId)
+    : ActorSystem(actorSystem)
+    , Counters(std::move(counters))
+    , GRpcRequestProxyId(grpcRequestProxyId)
 {
 }
 
 TTestShardGRpcService::~TTestShardGRpcService() = default;
 
 void TTestShardGRpcService::InitService(grpc::ServerCompletionQueue* cq, NYdbGrpc::TLoggerPtr logger) {
-    CQ_ = cq;
+    CQ = cq;
     SetupIncomingRequests(std::move(logger));
 }
 
 void TTestShardGRpcService::SetupIncomingRequests(NYdbGrpc::TLoggerPtr logger) {
     using namespace Ydb::TestShard;
-    auto getCounterBlock = CreateCounterCb(Counters_, ActorSystem_);
+    auto getCounterBlock = NGRpcService::CreateCounterCb(Counters, ActorSystem);
 
 #ifdef SETUP_TESTSHARD_METHOD
 #error SETUP_TESTSHARD_METHOD macro already defined
 #endif
 
 #define SETUP_TESTSHARD_METHOD(methodName, methodCallback, rlMode, requestType, auditMode) \
-    SETUP_METHOD(methodName, methodCallback, rlMode, requestType, testshard, auditMode)
+    SETUP_METHOD(methodName, methodCallback, rlMode, requestType, TestShard, testshard, auditMode)
 
-    SETUP_TESTSHARD_METHOD(CreateTestShard, DoCreateTestShard, RLMODE(Rps), TESTSHARD_CREATETESTSHARD, TAuditMode::Modifying(TAuditMode::TLogClassConfig::ClusterAdmin));
-    SETUP_TESTSHARD_METHOD(DeleteTestShard, DoDeleteTestShard, RLMODE(Rps), TESTSHARD_DELETETESTSHARD, TAuditMode::Modifying(TAuditMode::TLogClassConfig::ClusterAdmin));
+    SETUP_TESTSHARD_METHOD(CreateTestShard, DoCreateTestShard, Rps, TESTSHARD_CREATETESTSHARD, TAuditMode::Modifying(TAuditMode::TLogClassConfig::ClusterAdmin));
+    SETUP_TESTSHARD_METHOD(DeleteTestShard, DoDeleteTestShard, Rps, TESTSHARD_DELETETESTSHARD, TAuditMode::Modifying(TAuditMode::TLogClassConfig::ClusterAdmin));
 
 #undef SETUP_TESTSHARD_METHOD
 }

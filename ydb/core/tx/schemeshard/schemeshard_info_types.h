548ed191c1ee6763797875a7b4c1331f86e72a5c
Vitaliy Filippov <vitaliff@ydb.tech>
2025-11-24 15:54:29 +0000

    Plug FilterKMeansScan into schemeshard

--- ydb/core/tx/schemeshard/schemeshard_info_types.h
+++ ydb/core/tx/schemeshard/schemeshard_info_types.h
@@ -3378,6 +3378,7 @@ struct TIndexBuildInfo: public TSimpleRefCount<TIndexBuildInfo> {
         ui32 Rounds = 0;
         ui32 OverlapClusters = 0;
         double OverlapRatio = 0;
+        bool IsPrefixed = false;
 
         // progress
         enum EState : ui32 {
@@ -3385,6 +3386,8 @@ struct TIndexBuildInfo: public TSimpleRefCount<TIndexBuildInfo> {
             Reshuffle,
             MultiLocal,
             Recompute,
+            Filter,
+            FilterBorders,
         };
         ui32 Level = 1;
         ui32 Round = 0;
@@ -3399,6 +3402,8 @@ struct TIndexBuildInfo: public TSimpleRefCount<TIndexBuildInfo> {
 
         NTableIndex::NKMeans::TClusterId ChildBegin = 1;  // included
         NTableIndex::NKMeans::TClusterId Child = ChildBegin;
+
+        TVector<TString> FilterBorderRows;
 
         ui64 TableSize = 0;
 
@@ -3425,6 +3430,8 @@ struct TIndexBuildInfo: public TSimpleRefCount<TIndexBuildInfo> {
 
         TString WriteTo(bool needsBuildTable = false) const;
         TString ReadFrom() const;
+        int NextBuildIndex() const;
+        const char* NextBuildSuffix() const;
 
         std::pair<NTableIndex::NKMeans::TClusterId, NTableIndex::NKMeans::TClusterId> RangeToBorders(const TSerializedTableRange& range) const;
 
@@ -3843,6 +3850,7 @@ public:
                     Y_ENSURE(NKikimr::NKMeans::ValidateSettings(desc.settings(), createError), createError);
                     indexInfo->KMeans.K = desc.settings().clusters();
                     indexInfo->KMeans.Levels = indexInfo->IsBuildPrefixedVectorIndex() + desc.settings().levels();
+                    indexInfo->KMeans.IsPrefixed = indexInfo->IsBuildPrefixedVectorIndex();
                     indexInfo->KMeans.Rounds = NTableIndex::NKMeans::DefaultKMeansRounds;
                     indexInfo->KMeans.OverlapClusters = desc.settings().overlap_clusters()
                         ? desc.settings().overlap_clusters()

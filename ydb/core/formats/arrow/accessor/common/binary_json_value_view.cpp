71c4a3ab5a13def612187a96bd124bd726fa4a24
Vladilen Muzychenko <vladilenmuz@ydb.tech>
2025-11-25 16:23:46 +0000

    Fix json indexes performance

---
+++ ydb/core/formats/arrow/accessor/common/binary_json_value_view.cpp
@@ -0,0 +1,79 @@
+#include "binary_json_value_view.h"
+
+#include <library/cpp/json/json_reader.h>
+#include <ydb/library/actors/core/log.h>
+#include <yql/essentials/types/binary_json/read.h>
+
+namespace NKikimr::NArrow::NAccessor {
+
+TBinaryJsonValueView::TBinaryJsonValueView(const TStringBuf& rawValue)
+    : RawValue(rawValue) {
+    if (!RawValue.empty()) {
+        AFL_VERIFY(NBinaryJson::IsValidBinaryJson(RawValue));
+    }
+}
+
+NJson::TJsonValue TBinaryJsonValueView::GetJsonValue() const {
+    if (RawValue.empty()) {
+        return NJson::TJsonValue(NJson::JSON_UNDEFINED);
+    }
+
+    auto data = NBinaryJson::SerializeToJson(RawValue);
+    NJson::TJsonValue res;
+    AFL_VERIFY(NJson::ReadJsonTree(data, &res));
+    return res;
+}
+
+std::optional<TStringBuf> TBinaryJsonValueView::GetScalarOptional() const {
+    if (RawValue.empty()) {
+        return std::nullopt;
+    }
+
+    if (!ScalarView.empty()) {
+        return ScalarView;
+    }
+
+    auto reader = NBinaryJson::TBinaryJsonReader::Make(RawValue);
+    auto rootCursor = reader->GetRootCursor();
+    if (rootCursor.GetType() != NBinaryJson::EContainerType::TopLevelScalar) {
+        return std::nullopt;
+    }
+
+    auto rootElement = rootCursor.GetElement(0);
+
+    switch (rootElement.GetType()) {
+        case NBinaryJson::EEntryType::String:
+            ScalarView = rootElement.GetString();
+            break;
+        case NBinaryJson::EEntryType::Number: {
+            const double val = rootElement.GetNumber();
+            double integer;
+            if (modf(val, &integer)) {
+                ScalarHolder = ToString(val);
+            } else {
+                ScalarHolder = ToString((i64)integer);
+            }
+            ScalarView = ScalarHolder;
+            break;
+        }
+        case NBinaryJson::EEntryType::BoolFalse: {
+            static const TString falseString = "false";
+            ScalarView = falseString;
+            break;
+        }
+        case NBinaryJson::EEntryType::BoolTrue: {
+            static const TString trueString = "true";
+            ScalarView = trueString;
+            break;
+        }
+        case NBinaryJson::EEntryType::Null: {
+            return std::nullopt;
+        }
+        default:
+            return std::nullopt;
+    }
+
+    return ScalarView;
+}
+
+} // namespace NKikimr::NArrow::NAccessor
---
+++ ydb/core/formats/arrow/accessor/common/binary_json_value_view.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <library/cpp/json/writer/json_value.h>
+#include <util/generic/string.h>
+
+#include <optional>
+
+namespace NKikimr::NArrow::NAccessor {
+
+class TBinaryJsonValueView {
+public:
+    TBinaryJsonValueView(const TStringBuf& rawValue);
+
+    NJson::TJsonValue GetJsonValue() const;
+
+    std::optional<TStringBuf> GetScalarOptional() const;
+
+private:
+    TStringBuf RawValue;
+    mutable TString ScalarHolder;
+    mutable TStringBuf ScalarView;
+};
+
+} // namespace NKikimr::NArrow::NAccessor

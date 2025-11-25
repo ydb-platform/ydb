62bfbd3f67ceb06c1d653d9ebef2927274790dc3
Sergey Skovorodkin <qyryq@ydb.tech>
2025-11-25 13:52:11 +0000

    yql/essentials for metrics level

--- yql/essentials/sql/v1/query.cpp
+++ yql/essentials/sql/v1/query.cpp
@@ -1823,6 +1823,7 @@ public:
             INSERT_TOPIC_SETTING(AutoPartitioningUpUtilizationPercent)
             INSERT_TOPIC_SETTING(AutoPartitioningDownUtilizationPercent)
             INSERT_TOPIC_SETTING(AutoPartitioningStrategy)
+            INSERT_TOPIC_SETTING(MetricsLevel)
 
 #undef INSERT_TOPIC_SETTING
 
@@ -1945,6 +1946,7 @@ public:
             INSERT_TOPIC_SETTING(AutoPartitioningUpUtilizationPercent)
             INSERT_TOPIC_SETTING(AutoPartitioningDownUtilizationPercent)
             INSERT_TOPIC_SETTING(AutoPartitioningStrategy)
+            INSERT_TOPIC_SETTING(MetricsLevel)
 
 #undef INSERT_TOPIC_SETTING
 

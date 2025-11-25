08d8ef8a607fcd50a1b41ee67087c2725a4de03c
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2025-11-25 15:56:58 +0300

    Disable topic autopartitioning in global consistency mode (#29498)

--- ydb/core/testlib/basics/feature_flags.h
+++ ydb/core/testlib/basics/feature_flags.h
@@ -83,6 +83,7 @@ public:
     FEATURE_FLAG_SETTER(EnableDataShardWriteAlwaysVolatile)
     FEATURE_FLAG_SETTER(EnableStreamingQueries)
     FEATURE_FLAG_SETTER(DisableMissingDefaultColumnsInBulkUpsert)
+    FEATURE_FLAG_SETTER(EnableTopicAutopartitioningForReplication)
 
     #undef FEATURE_FLAG_SETTER
 };

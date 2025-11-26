6e55a436904db7792646c2927abbb6ab6650cac3
Matveev Sergei <xyligansereja@yandex-team.ru>
2025-11-26 19:18:14 +0000

    minor fix

--- ydb/core/kqp/query_data/kqp_predictor.cpp
+++ ydb/core/kqp/query_data/kqp_predictor.cpp
@@ -144,7 +144,7 @@ ui32 TStagePredictor::GetMaxExecutorThreadLimit() {
         userPoolMaxThreadsCount = NSystemInfo::NumberOfCpus();
     }
 
-    return Max<ui32>(1, *userPoolMaxThreadsCount);
+    return Max<ui32>(GetUsableThreads(), *userPoolMaxThreadsCount);
 }
 
 ui32 TStagePredictor::CalcTasksOptimalCount(const ui32 availableThreadsCount, const std::optional<ui32> previousStageTasksCount) const {

02e7d2144e682cea52ea6b6b0d944b4b76d81fd4
Oleg Lebedev <lebedev0leg@ydb.tech>
2025-11-26 22:31:56 +0000

    init

---
+++ ydb/tests/stress/backup/tests/test_workload.py
@@ -0,0 +1,26 @@
+import os
+import pytest
+import yatest
+
+from ydb.tests.library.harness.util import LogLevels
+from ydb.tests.library.stress.fixtures import StressFixture
+
+
+class TestYdbBackupWorkload(StressFixture):
+    @pytest.fixture(autouse=True, scope="function")
+    def setup(self):
+        yield from self.setup_cluster(
+            additional_log_configs={
+                "BACKUP_SERVICE": LogLevels.DEBUG,
+            },
+        )
+
+    def test(self):
+        cmd = [
+            yatest.common.binary_path(os.getenv("YDB_TEST_PATH")),
+            "--endpoint", self.endpoint,
+            "--database", self.database,
+            "--duration", "120",
+            "--backup-interval", "20",
+        ]
+        yatest.common.execute(cmd, wait=True)

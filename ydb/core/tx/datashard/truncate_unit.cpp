e4790420acd2fd7e72e94068d1ddd9223c363406
Igor <igorkoshkarev4qqq@mail.ru>
2025-11-27 14:49:08 +0000

    create unit test great again

--- ydb/core/tx/datashard/truncate_unit.cpp
+++ ydb/core/tx/datashard/truncate_unit.cpp
@@ -18,7 +18,7 @@ public:
 };
 
 TTruncateUnit::TTruncateUnit(TDataShard& dataShard, TPipeline& pipeline)
-    : TExecutionUnit(EExecutionUnitKind::Truncate, true, dataShard, pipeline)
+    : TExecutionUnit(EExecutionUnitKind::Truncate, false, dataShard, pipeline)
 {
 }
 
@@ -30,7 +30,7 @@ bool TTruncateUnit::IsReadyToExecute(TOperation::TPtr) const {
 }
 
 EExecutionStatus TTruncateUnit::Execute(
-    TOperation::TPtr op, TTransactionContext& txc, const TActorContext&
+    TOperation::TPtr op, TTransactionContext& txc, const TActorContext& actorCtx
 ) {
     TActiveTransaction* tx = dynamic_cast<TActiveTransaction*>(op.Get());
     Y_ENSURE(tx, "cannot cast operation of kind " << op->GetKind());
@@ -47,9 +47,24 @@ EExecutionStatus TTruncateUnit::Execute(
     auto tableId = pathId.GetLocalId();
     Y_ENSURE(DataShard.GetUserTables().contains(tableId));
 
-    txc.DB.Truncate(tableId);
+    const auto& userTable = DataShard.GetUserTables().at(tableId);
+    auto localTid = userTable->LocalTid;
 
-    return EExecutionStatus::Executed;
+    LOG_DEBUG_S(actorCtx, NKikimrServices::TX_DATASHARD,
+               "TTruncateUnit::Execute - About to TRUNCATE TABLE at " << DataShard.TabletID()
+               << " tableId# " << tableId << " localTid# " << localTid << " TxId = " << op->GetTxId());
+
+    txc.DB.Truncate(localTid);
+    txc.DB.NoMoreReadsForTx();
+
+    BuildResult(op, NKikimrTxDataShard::TEvProposeTransactionResult::COMPLETE);
+    op->Result()->SetStepOrderId(op->GetStepOrder().ToPair());
+    
+    LOG_DEBUG_S(actorCtx, NKikimrServices::TX_DATASHARD,
+               "TTruncateUnit::Execute - Finished successfully. TableId = " << tableId
+               << " TxId = " << op->GetTxId() << " - Operation COMPLETED");
+
+    return EExecutionStatus::DelayCompleteNoMoreRestarts;
 }
 
 void TTruncateUnit::Complete(TOperation::TPtr,

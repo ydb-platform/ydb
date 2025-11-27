5a8aa6bc007b8f83e1aa8679599d76723a0ae502
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2025-11-27 11:55:41 +0100

    YDB SDK: support exlude_regexps in export/import API

--- ydb/public/sdk/cpp/src/client/export/export.cpp
+++ ydb/public/sdk/cpp/src/client/export/export.cpp
@@ -208,6 +208,10 @@ TFuture<TExportToYtResponse> TExportClient::ExportToYt(const TExportToYtSettings
 
     request.mutable_settings()->set_use_type_v3(settings.UseTypeV3_);
 
+    for (const std::string& excludeRegexp : settings.ExcludeRegexp_) {
+        request.mutable_settings()->add_exclude_regexps(excludeRegexp);
+    }
+
     return Impl_->ExportToYt(std::move(request), settings);
 }
 
@@ -259,6 +263,10 @@ TFuture<TExportToS3Response> TExportClient::ExportToS3(const TExportToS3Settings
         request.mutable_settings()->mutable_encryption_settings()->mutable_symmetric_key()->set_key(settings.SymmetricKey_);
     }
 
+    for (const std::string& excludeRegexp : settings.ExcludeRegexp_) {
+        request.mutable_settings()->add_exclude_regexps(excludeRegexp);
+    }
+
     return Impl_->ExportToS3(std::move(request), settings);
 }
 
@@ -296,6 +304,10 @@ TFuture<TExportToFsResponse> TExportClient::ExportToFs(const TExportToFsSettings
     if (!settings.EncryptionAlgorithm_.empty() && !settings.SymmetricKey_.empty()) {
         request.mutable_settings()->mutable_encryption_settings()->set_encryption_algorithm(settings.EncryptionAlgorithm_);
         request.mutable_settings()->mutable_encryption_settings()->mutable_symmetric_key()->set_key(settings.SymmetricKey_);
+    }
+
+    for (const std::string& excludeRegexp : settings.ExcludeRegexp_) {
+        request.mutable_settings()->add_exclude_regexps(excludeRegexp);
     }
 
     return Impl_->ExportToFs(std::move(request), settings);

e9310d8d0136c9c1d60b3085379830e859b4a1d1
Matveev Sergei <xyligansereja@yandex-team.ru>
2025-11-27 15:19:12 +0000

    fixed GetUsableThreads

--- ydb/core/kqp/query_data/kqp_predictor.cpp
+++ ydb/core/kqp/query_data/kqp_predictor.cpp
@@ -120,15 +120,19 @@ bool TStagePredictor::DeserializeFromKqpSettings(const NYql::NDqProto::TProgram:
 }
 
 ui32 TStagePredictor::GetUsableThreads() {
-    std::optional<ui32> userPoolSize;
+    std::optional<ui32> userPoolCurrentThreadsCount;
     if (HasAppData() && TlsActivationContext && TlsActivationContext->ActorSystem()) {
-        userPoolSize = TlsActivationContext->ActorSystem()->GetPoolThreadsCount(AppData()->UserPoolId);
+        TExecutorPoolState poolState;
+        TlsActivationContext->ActorSystem()->GetExecutorPoolState(AppData()->UserPoolId, poolState);
+        userPoolCurrentThreadsCount = poolState.CurrentLimit;
     }
-    if (!userPoolSize) {
+
+    if (!userPoolCurrentThreadsCount) {
         ALS_INFO(NKikimrServices::KQP_EXECUTER) << "user pool is undefined for executer tasks construction";
-        userPoolSize = NSystemInfo::NumberOfCpus();
+        userPoolCurrentThreadsCount = NSystemInfo::NumberOfCpus();
     }
-    return Max<ui32>(1, *userPoolSize);
+
+    return Max<ui32>(1, *userPoolCurrentThreadsCount);
 }
 
 ui32 TStagePredictor::GetMaxExecutorThreadLimit() {

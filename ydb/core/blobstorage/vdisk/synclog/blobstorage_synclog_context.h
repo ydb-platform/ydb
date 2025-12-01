5ec61f941fbbf0100986edf59a459d59eb0224d5
Sergey Belyakov <serg-belyakov@ydb.tech>
2025-12-01 06:58:17 +0000

    Update MaxPossibleDisksInGroup constant

--- ydb/core/blobstorage/vdisk/synclog/blobstorage_synclog_context.h
+++ ydb/core/blobstorage/vdisk/synclog/blobstorage_synclog_context.h
@@ -31,7 +31,7 @@ public:
 };
 
 constexpr static ui32 MaxExpectedDisksInGroup = 9;
-constexpr static ui32 MaxPossibleDisksInGroup = 256;
+constexpr static ui32 MaxPossibleDisksInGroup = 32;
 using TSyncedMask = std::bitset<MaxPossibleDisksInGroup>;
 
 ////////////////////////////////////////////////////////////////////////////

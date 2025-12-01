c86972e22e07418170f2a9d9ba4b569d9895bd0d
Ilia Shakhov <pixcc@ydb.tech>
2025-12-01 11:53:20 +0000

    Fix conflicts

--- ydb/core/tx/datashard/export_s3.h
+++ ydb/core/tx/datashard/export_s3.h
@@ -7,6 +7,8 @@
 #include "export_iface.h"
 #include "export_s3_buffer.h"
 
+#include <ydb/core/base/appdata_fwd.h>
+#include <ydb/core/protos/datashard_config.pb.h>
 #include <ydb/core/protos/s3_settings.pb.h>
 
 namespace NKikimr {

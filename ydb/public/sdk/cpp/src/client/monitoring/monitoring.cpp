3ed83a7aca9fe19c195960cf3fab42fe67aa309d
Evgenik2 <evkam@ydb.tech>
2025-12-09 17:52:17 +0300

    Move collect counters logic to grpc client side

--- ydb/public/sdk/cpp/src/client/monitoring/monitoring.cpp
+++ ydb/public/sdk/cpp/src/client/monitoring/monitoring.cpp
@@ -115,6 +115,10 @@ public:
         if (settings.NoSanitize_) {
             request.set_no_sanitize(settings.NoSanitize_.value());
         }
+
+        if (settings.CountersOnly_) {
+            request.set_counters_only(settings.CountersOnly_.value());
+        }
         auto promise = NThreading::NewPromise<TClusterStateResult>();
 
         auto extractor = [promise]

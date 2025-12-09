331acec1db7b368ffb85a57359a457d170cdd985
naspirato <naspirato@gmail.com>
2025-12-09 19:27:22 +0100

    Enhance test result naming in upload_tests_results.py by refining the construction of test names to include both name_part and subtest_name with a dot separator. This change ensures consistency with the format used in other scripts and improves clarity in test result reporting. Additionally, introduce a new YaMuteCheck class in transform_ya_junit.py to manage mute configurations using regex patterns, enhancing the flexibility of mute checks.

--- .github/scripts/tests/transform_ya_junit.py
+++ .github/scripts/tests/transform_ya_junit.py
@@ -15,13 +15,47 @@ import urllib.parse
 import zipfile
 from typing import Set
 from xml.etree import ElementTree as ET
-from mute_utils import mute_target
+from mute_utils import mute_target, pattern_to_re
 from junit_utils import add_junit_link_property, is_faulty_testcase
-from mute_check import YaMuteCheck
 
 
 def log_print(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+
+class YaMuteCheck:
+    def __init__(self):
+        self.regexps = set()
+        self.regexps = []
+
+    def load(self, fn):
+        with open(fn, "r") as fp:
+            for line in fp:
+                line = line.strip()
+                try:
+                    testsuite, testcase = line.split(" ", maxsplit=1)
+                except ValueError:
+                    log_print(f"SKIP INVALID MUTE CONFIG LINE: {line!r}")
+                    continue
+                self.populate(testsuite, testcase)
+
+    def populate(self, testsuite, testcase):
+        check = []
+
+        for p in (pattern_to_re(testsuite), pattern_to_re(testcase)):
+            try:
+                check.append(re.compile(p))
+            except re.error:
+                log_print(f"Unable to compile regex {p!r}")
+                return
+
+        self.regexps.append(tuple(check))
+
+    def __call__(self, suite_name, test_name):
+        for ps, pt in self.regexps:
+            if ps.match(suite_name) and pt.match(test_name):
+                return True
+        return False
 
 
 class YTestReportTrace:

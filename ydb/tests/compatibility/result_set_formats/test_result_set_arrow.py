e98f3881b3158b9c92a807d72aa5c6714b2f065a
Daniil Timizhev <ditimizhev@ydb.tech>
2025-12-10 15:57:27 +0300

    Add compression test

--- ydb/tests/compatibility/result_set_formats/test_result_set_arrow.py
+++ ydb/tests/compatibility/result_set_formats/test_result_set_arrow.py
@@ -47,6 +47,13 @@ primitive_type_to_arrow_type = {
     "Yson": pa.binary(),
 }
 
+codec_to_string = {
+    ydb.ArrowCompressionCodecType.UNSPECIFIED: "none",
+    ydb.ArrowCompressionCodecType.NONE: "none",
+    ydb.ArrowCompressionCodecType.ZSTD: "zstd",
+    ydb.ArrowCompressionCodecType.LZ4_FRAME: "lz4",
+}
+
 
 class TestResultSetArrow(RestartToAnotherVersionFixture):
     @pytest.fixture()
@@ -84,15 +91,33 @@ class TestResultSetArrow(RestartToAnotherVersionFixture):
         self._create_table(table_name)
         self._fill_table(table_name, rows_count)
 
-        result_sets = self._select_table(table_name)
-        assert len(result_sets) != 0
-        self._validate_response_types(result_sets, rows_count)
-
+        self._validate_response_types(self._select_table(table_name), rows_count)
         self.change_cluster_version()
+        self._validate_response_types(self._select_table(table_name), rows_count)
 
-        result_sets = self._select_table(table_name)
-        assert len(result_sets) != 0
-        self._validate_response_types(result_sets, rows_count)
+        self._drop_table(table_name)
+
+    @pytest.mark.parametrize("store_type", ["ROW", "COLUMM"])
+    @pytest.mark.parametrize("codec", [
+        ydb.ArrowCompressionCodec(ydb.ArrowCompressionCodecType.UNSPECIFIED), # UNSPECIFIED is the same as NONE
+        ydb.ArrowCompressionCodec(ydb.ArrowCompressionCodecType.NONE),
+        ydb.ArrowCompressionCodec(ydb.ArrowCompressionCodecType.ZSTD),
+        ydb.ArrowCompressionCodec(ydb.ArrowCompressionCodecType.ZSTD, 10),
+        ydb.ArrowCompressionCodec(ydb.ArrowCompressionCodecType.LZ4_FRAME),
+        ydb.ArrowCompressionCodec(ydb.ArrowCompressionCodecType.LZ4_FRAME, 10), # LZ4_FRAME with level is not supported
+    ])
+    def test_compression(self, codec: ydb.ArrowCompressionCodec):
+        table_name = "test_arrow"
+        rows_count = 500
+
+        self._create_table(table_name)
+        self._fill_table(table_name, rows_count)
+
+        self._validate_compression(self._select_table(table_name, codec), rows_count, codec)
+        self.change_cluster_version()
+        self._validate_compression(self._select_table(table_name, codec), rows_count, codec)
+
+        self._drop_table(table_name)
 
     def _create_table(self, table_name):
         query = create_table_sql_request(
@@ -105,8 +130,19 @@ class TestResultSetArrow(RestartToAnotherVersionFixture):
             column_table=self.store_type == "COLUMN"
         )
 
-        with ydb.QuerySessionPool(self.driver) as pool:
-            pool.execute_with_retries(query)
+        try:
+            with ydb.QuerySessionPool(self.driver) as pool:
+                pool.execute_with_retries(query)
+        except Exception as e:
+            assert False, f"Failed to create table {table_name}: {e}"
+
+    def _drop_table(self, table_name):
+        query = f"DROP TABLE {table_name};"
+        try:
+            with ydb.QuerySessionPool(self.driver) as pool:
+                pool.execute_with_retries(query)
+        except Exception as e:
+            assert False, f"Failed to drop table {table_name}: {e}"
 
     def _fill_table(self, table_name, rows_count, batch_size=100, offset=0):
         type_names = list(self.all_types.keys())
@@ -125,36 +161,83 @@ class TestResultSetArrow(RestartToAnotherVersionFixture):
                     value.append('NULL')
             values.append("(" + ", ".join(value) + ")")
 
-        with ydb.QuerySessionPool(self.driver) as pool:
-            for batch_start in range(0, rows_count, batch_size):
-                batch_rows = values[batch_start:batch_start + batch_size]
-                if not batch_rows:
-                    continue
-                query = f"UPSERT INTO {table_name} {columns_str} VALUES {', '.join(batch_rows)};"
-                print(query, end='\n\n')
-                pool.execute_with_retries(query)
+        try:
+            with ydb.QuerySessionPool(self.driver) as pool:
+                for batch_start in range(0, rows_count, batch_size):
+                    batch_rows = values[batch_start:batch_start + batch_size]
+                    if not batch_rows:
+                        continue
+                    query = f"UPSERT INTO {table_name} {columns_str} VALUES {', '.join(batch_rows)};"
+                    pool.execute_with_retries(query)
+        except Exception as e:
+            assert False, f"Failed to fill table {table_name}: {e}"
 
-    def _select_table(self, table_name):
+    def _select_table(self, table_name, codec: ydb.ArrowCompressionCodec | None = None):
         columns = ["pk_Uint64", *[f"col_{cleanup_type_name(type_name)}" for type_name in self.all_types.keys()]]
-        query = f"SELECT {', '.join(columns)} FROM {table_name};"
-        with ydb.QuerySessionPool(self.driver) as pool:
-            result_sets = pool.execute_with_retries(query, result_set_format=ydb.QueryResultSetFormat.ARROW)
-        return result_sets
+        query = f"SELECT {", ".join(columns)} FROM {table_name};"
+        arrow_format_settings = ydb.ArrowFormatSettings(compression_codec=codec) if codec else None
+
+        try:
+            with ydb.QuerySessionPool(self.driver) as pool:
+                result_sets = pool.execute_with_retries(query, result_set_format=ydb.QueryResultSetFormat.ARROW, arrow_format_settings=arrow_format_settings)
+            return result_sets
+        except Exception as e:
+            if codec is None or (codec.type != ydb.ArrowCompressionCodecType.LZ4_FRAME or codec.level is None):
+                assert False, f"Failed to select table: {e}"
 
     def _validate_response_types(self, result_sets, rows_count):
+        if result_sets is None:
+            return
+
+        assert len(result_sets) != 0
+
         result_rows_count = 0
         for result_set in result_sets:
-            assert len(result_set.data) != 0
-            assert len(result_set.arrow_format_meta.schema) != 0
+            assert result_set.format == ydb.QueryResultSetFormat.ARROW
+            assert result_set.data is not None and len(result_set.data) != 0
+            assert result_set.arrow_format_meta is not None and len(result_set.arrow_format_meta.schema) != 0
 
-            schema: pa.Schema = pa.ipc.read_schema(pa.py_buffer(result_set.arrow_format_meta.schema))
-            batch: pa.RecordBatch = pa.ipc.read_record_batch(pa.py_buffer(result_set.data), schema)
+            try:
+                schema = pa.ipc.read_schema(pa.py_buffer(result_set.arrow_format_meta.schema))
+                batch = pa.ipc.read_record_batch(pa.py_buffer(result_set.data), schema)
+                batch.validate()
 
-            result_rows_count += batch.num_rows
-            assert batch.num_columns == len(self.all_types) + 1
+                result_rows_count += batch.num_rows
+                assert batch.num_columns == len(self.all_types) + 1
 
-            for type_name in self.all_types.keys():
-                print(type_name)
-                assert schema.field(f"col_{cleanup_type_name(type_name)}").type == primitive_type_to_arrow_type[type_name]
+                for type_name in self.all_types.keys():
+                    assert schema.field(f"col_{cleanup_type_name(type_name)}").type == primitive_type_to_arrow_type[type_name]
+            except Exception as e:
+                assert False, f"Failed to read schema or batch from result set: {e}"
+
+        assert result_rows_count == rows_count
+
+    def _validate_compression(self, result_sets, rows_count, codec: ydb.ArrowCompressionCodec):
+        if result_sets is None:
+            return
+
+        assert len(result_sets) != 0
+
+        result_rows_count = 0
+        for result_set in result_sets:
+            assert result_set.format == ydb.QueryResultSetFormat.ARROW
+            assert result_set.data is not None and len(result_set.data) != 0
+            assert result_set.arrow_format_meta is not None and len(result_set.arrow_format_meta.schema) != 0
+
+            try:
+                schema: pa.Schema = pa.ipc.read_schema(pa.py_buffer(result_set.arrow_format_meta.schema))
+                batch: pa.RecordBatch = pa.ipc.read_record_batch(pa.py_buffer(result_set.data), schema)
+                batch.validate()
+
+                # is there the best way to check if the compression is working?
+                if codec.type in [ydb.ArrowCompressionCodecType.UNSPECIFIED, ydb.ArrowCompressionCodecType.NONE]:
+                    assert len(result_set.data) == len(batch.serialize().to_pybytes())
+                else:
+                    assert len(result_set.data) < len(batch.serialize().to_pybytes())
+
+                result_rows_count += batch.num_rows
+                assert batch.num_columns == len(self.all_types) + 1
+            except Exception as e:
+                assert False, f"Failed to read schema or batch from result set: {e}"
 
         assert result_rows_count == rows_count

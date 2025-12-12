852ccf73d73c552938383e1a2d0f67192fc7fc37
Iurii Kravchenko <xjie6@ydb.tech>
2025-12-12 13:35:07 +0300

    ff

--- ydb/core/tx/columnshard/engines/storage/optimizer/abstract/optimizer.h
+++ ydb/core/tx/columnshard/engines/storage/optimizer/abstract/optimizer.h
@@ -307,7 +307,8 @@ public:
     }
 
     static std::shared_ptr<IOptimizerPlannerConstructor> BuildDefault() {
-        auto result = TFactory::MakeHolder("tiling");
+        auto default_compaction = AppDataVerified().ColumnShardConfig.HasDefaultCompaction() ? AppDataVerified().ColumnShardConfig.GetDefaultCompaction() : "tiling";
+        auto result = TFactory::MakeHolder(default_compaction);
         AFL_VERIFY(!!result);
         return std::shared_ptr<IOptimizerPlannerConstructor>(result.Release());
     }

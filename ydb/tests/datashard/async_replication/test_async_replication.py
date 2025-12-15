223c597dfb53d1323fdc1a643ecd5df125b3141b
Ivan Nikolaev <ivannik@ydb.tech>
2025-12-15 13:03:52 +0300

    Add functional datashard tests for pg types (#30578)

--- ydb/tests/datashard/async_replication/test_async_replication.py
+++ ydb/tests/datashard/async_replication/test_async_replication.py
@@ -5,47 +5,14 @@ from ydb.tests.sql.lib.test_query import Query
 from ydb.tests.library.common.wait_for import wait_for
 from ydb.tests.datashard.lib.multicluster_test_base import MulticlusterTestBase
 from ydb.tests.datashard.lib.dml_operations import DMLOperations
+from ydb.tests.datashard.lib.test_pg_base import TestPgBase
 from ydb.tests.datashard.lib.types_of_variables import pk_types, non_pk_types, index_first, index_second, \
-    index_first_sync, index_second_sync, index_three_sync, index_four_sync, index_zero_sync
+    index_first_sync, index_second_sync, index_three_sync, index_four_sync, index_zero_sync, \
+    pk_pg_types, non_pk_pg_types
 
 
-class TestAsyncReplication(MulticlusterTestBase):
-    @pytest.mark.parametrize(
-        "table_name, pk_types, all_types, index, ttl, unique, sync",
-        [
-            # ("table_index_4_UNIQUE_SYNC", pk_types, {},
-            # index_four_sync, "", "UNIQUE", "SYNC"),
-            # ("table_index_3_UNIQUE_SYNC", pk_types, {},
-            # index_three_sync_not_Bool, "", "UNIQUE", "SYNC"),
-            # ("table_index_2_UNIQUE_SYNC", pk_types, {},
-            # index_second_sync, "", "UNIQUE", "SYNC"),
-            # ("table_index_1_UNIQUE_SYNC", pk_types, {},
-            # index_first_sync, "", "UNIQUE", "SYNC"),
-            # ("table_index_0_UNIQUE_SYNC", pk_types, {},
-            # index_zero_sync, "", "UNIQUE", "SYNC"),
-            ("table_index_4__SYNC", pk_types, {},
-             index_four_sync, "", "", "SYNC"),
-            ("table_index_3__SYNC", pk_types, {},
-             index_three_sync, "", "", "SYNC"),
-            ("table_index_2__SYNC", pk_types, {},
-             index_second_sync, "", "", "SYNC"),
-            ("table_index_1__SYNC", pk_types, {},
-             index_first_sync, "", "", "SYNC"),
-            ("table_index_0__SYNC", pk_types, {},
-             index_zero_sync, "", "", "SYNC"),
-            ("table_index_1__ASYNC", pk_types, {}, index_second, "", "", "ASYNC"),
-            ("table_index_0__ASYNC", pk_types, {}, index_first, "", "", "ASYNC"),
-            ("table_all_types", pk_types, {
-             **pk_types, **non_pk_types}, {}, "", "", ""),
-            ("table_ttl_DyNumber", pk_types, {}, {}, "DyNumber", "", ""),
-            ("table_ttl_Uint32", pk_types, {}, {}, "Uint32", "", ""),
-            ("table_ttl_Uint64", pk_types, {}, {}, "Uint64", "", ""),
-            ("table_ttl_Datetime", pk_types, {}, {}, "Datetime", "", ""),
-            ("table_ttl_Timestamp", pk_types, {}, {}, "Timestamp", "", ""),
-            ("table_ttl_Date", pk_types, {}, {}, "Date", "", ""),
-        ]
-    )
-    def test_async_replication(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
+class TestAsyncReplicationBase(MulticlusterTestBase):
+    def do_test_async_replication(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
         dml_cluster_1 = DMLOperations(Query.create(
             self.get_database(), self.get_endpoint(self.clusters[0])))
         dml_cluster_2 = DMLOperations(Query.create(
@@ -88,3 +55,61 @@ class TestAsyncReplication(MulticlusterTestBase):
             else:
                 return len(rows) == 1 and rows[0].count != 0
         return predicate
+
+
+class TestAsyncReplication(TestAsyncReplicationBase):
+    @pytest.mark.parametrize(
+        "table_name, pk_types, all_types, index, ttl, unique, sync",
+        [
+            # ("table_index_4_UNIQUE_SYNC", pk_types, {},
+            # index_four_sync, "", "UNIQUE", "SYNC"),
+            # ("table_index_3_UNIQUE_SYNC", pk_types, {},
+            # index_three_sync_not_Bool, "", "UNIQUE", "SYNC"),
+            # ("table_index_2_UNIQUE_SYNC", pk_types, {},
+            # index_second_sync, "", "UNIQUE", "SYNC"),
+            # ("table_index_1_UNIQUE_SYNC", pk_types, {},
+            # index_first_sync, "", "UNIQUE", "SYNC"),
+            # ("table_index_0_UNIQUE_SYNC", pk_types, {},
+            # index_zero_sync, "", "UNIQUE", "SYNC"),
+            ("table_index_4__SYNC", pk_types, {},
+             index_four_sync, "", "", "SYNC"),
+            ("table_index_3__SYNC", pk_types, {},
+             index_three_sync, "", "", "SYNC"),
+            ("table_index_2__SYNC", pk_types, {},
+             index_second_sync, "", "", "SYNC"),
+            ("table_index_1__SYNC", pk_types, {},
+             index_first_sync, "", "", "SYNC"),
+            ("table_index_0__SYNC", pk_types, {},
+             index_zero_sync, "", "", "SYNC"),
+            ("table_index_1__ASYNC", pk_types, {}, index_second, "", "", "ASYNC"),
+            ("table_index_0__ASYNC", pk_types, {}, index_first, "", "", "ASYNC"),
+            ("table_all_types", pk_types, {
+             **pk_types, **non_pk_types}, {}, "", "", ""),
+            ("table_ttl_DyNumber", pk_types, {}, {}, "DyNumber", "", ""),
+            ("table_ttl_Uint32", pk_types, {}, {}, "Uint32", "", ""),
+            ("table_ttl_Uint64", pk_types, {}, {}, "Uint64", "", ""),
+            ("table_ttl_Datetime", pk_types, {}, {}, "Datetime", "", ""),
+            ("table_ttl_Timestamp", pk_types, {}, {}, "Timestamp", "", ""),
+            ("table_ttl_Date", pk_types, {}, {}, "Date", "", ""),
+        ]
+    )
+    def test_async_replication(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
+        self.do_test_async_replication(table_name, pk_types, all_types, index, ttl, unique, sync)
+
+
+class TestPgAsyncReplication(TestPgBase, TestAsyncReplicationBase):
+    @pytest.mark.parametrize(
+        "table_name, pk_types, all_types, index, ttl, unique, sync",
+        [
+            # TODO: add UNIQUE_SYNC
+            ("table_index_0__SYNC", pk_pg_types, {}, pk_pg_types, "", "", "SYNC"),
+            ("table_index_0__ASYNC", pk_pg_types, {}, pk_pg_types, "", "", "ASYNC"),
+            ("table_all_types", pk_pg_types, {**pk_pg_types, **non_pk_pg_types}, {}, "", "", ""),
+            ("table_ttl_pgint4", pk_pg_types, {}, {}, "pgint4", "", ""),
+            ("table_ttl_pgint8", pk_pg_types, {}, {}, "pgint8", "", ""),
+            ("table_ttl_pgdate", pk_pg_types, {}, {}, "pgdate", "", ""),
+            ("table_ttl_pgtimestamp", pk_pg_types, {}, {}, "pgtimestamp", "", ""),
+        ]
+    )
+    def test_async_replication(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
+        self.do_test_async_replication(table_name, pk_types, all_types, index, ttl, unique, sync)
--- ydb/tests/datashard/s3/test_s3.py
+++ ydb/tests/datashard/s3/test_s3.py
@@ -5,49 +5,15 @@ import os
 from ydb.tests.library.common.wait_for import wait_for
 from ydb.tests.sql.lib.test_base import TestBase
 from ydb.tests.sql.lib.test_s3 import S3Base
+from ydb.tests.datashard.lib.test_pg_base import TestPgBase
 from ydb.tests.datashard.lib.types_of_variables import pk_types, non_pk_types, index_first, index_second, \
-    index_first_sync, index_second_sync, index_three_sync, index_four_sync, index_zero_sync
+    index_first_sync, index_second_sync, index_three_sync, index_three_sync_not_Bool, index_four_sync, index_zero_sync, \
+    pk_pg_types, pk_pg_types_no_bool, non_pk_pg_types
 from ydb.tests.datashard.lib.dml_operations import DMLOperations
 
 
-class TestYdbS3TTL(TestBase, S3Base):
-    @pytest.mark.parametrize(
-        "table_name, pk_types, all_types, index, ttl, unique, sync",
-        [
-            # ("table_index_4_UNIQUE_SYNC", pk_types, {},        Issues:
-            # index_four_sync, "", "UNIQUE", "SYNC"),           <main>: Error: Failed item check: unsupported index type to build
-            # ("table_index_3_UNIQUE_SYNC", pk_types, {},       https://github.com/ydb-platform/ydb/issues/16594
-            # index_three_sync_not_Bool, "", "UNIQUE", "SYNC"),
-            # ("table_index_2_UNIQUE_SYNC", pk_types, {},
-            # index_second_sync, "", "UNIQUE", "SYNC"),
-            # ("table_index_1_UNIQUE_SYNC", pk_types, {},
-            # index_first_sync, "", "UNIQUE", "SYNC"),
-            # ("table_index_0_UNIQUE_SYNC", pk_types, {},
-            # index_zero_sync, "", "UNIQUE", "SYNC"),
-            ("table_index_4__SYNC", pk_types, {},
-             index_four_sync, "", "", "SYNC"),
-            ("table_index_3__SYNC", pk_types, {},
-             index_three_sync, "", "", "SYNC"),
-            ("table_index_2__SYNC", pk_types, {},
-             index_second_sync, "", "", "SYNC"),
-            ("table_index_1__SYNC", pk_types, {},
-             index_first_sync, "", "", "SYNC"),
-            ("table_index_0__SYNC", pk_types, {},
-             index_zero_sync, "", "", "SYNC"),
-            ("table_index_1__ASYNC", pk_types, {}, index_second, "", "", "ASYNC"),
-            ("table_index_0__ASYNC", pk_types, {}, index_first, "", "", "ASYNC"),
-            ("table_all_types", pk_types, {
-             **pk_types, **non_pk_types}, {}, "", "", ""),
-            ("table_ttl_DyNumber", pk_types, {}, {}, "DyNumber", "", ""),
-            ("table_ttl_Uint32", pk_types, {}, {}, "Uint32", "", ""),
-            ("table_ttl_Uint64", pk_types, {}, {}, "Uint64", "", ""),
-            ("table_ttl_Datetime", pk_types, {}, {}, "Datetime", "", ""),
-            ("table_ttl_Timestamp", pk_types, {}, {}, "Timestamp", "", ""),
-            ("table_ttl_Date", pk_types, {}, {}, "Date", "", ""),
-
-        ]
-    )
-    def test_s3(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
+class TestYdbS3TTLBase(TestBase, S3Base):
+    def do_test_s3(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
         s3_client = self.s3_session_client()
         s3_client.create_bucket(Bucket=self.bucket_name())
         dml = DMLOperations(self)
@@ -120,3 +86,63 @@ class TestYdbS3TTL(TestBase, S3Base):
             ])
             return "Done" in cmd._std_out.decode('utf-8')
         return predicate
+
+
+class TestYdbS3TTL(TestYdbS3TTLBase):
+    @pytest.mark.parametrize(
+        "table_name, pk_types, all_types, index, ttl, unique, sync",
+        [
+            ("table_index_4_UNIQUE_SYNC", pk_types, {},
+             index_four_sync, "", "UNIQUE", "SYNC"),
+            ("table_index_3_UNIQUE_SYNC", pk_types, {},
+             index_three_sync_not_Bool, "", "UNIQUE", "SYNC"),
+            ("table_index_2_UNIQUE_SYNC", pk_types, {},
+             index_second_sync, "", "UNIQUE", "SYNC"),
+            ("table_index_1_UNIQUE_SYNC", pk_types, {},
+             index_first_sync, "", "UNIQUE", "SYNC"),
+            ("table_index_0_UNIQUE_SYNC", pk_types, {},
+             index_zero_sync, "", "UNIQUE", "SYNC"),
+            ("table_index_4__SYNC", pk_types, {},
+             index_four_sync, "", "", "SYNC"),
+            ("table_index_3__SYNC", pk_types, {},
+             index_three_sync, "", "", "SYNC"),
+            ("table_index_2__SYNC", pk_types, {},
+             index_second_sync, "", "", "SYNC"),
+            ("table_index_1__SYNC", pk_types, {},
+             index_first_sync, "", "", "SYNC"),
+            ("table_index_0__SYNC", pk_types, {},
+             index_zero_sync, "", "", "SYNC"),
+            ("table_index_1__ASYNC", pk_types, {}, index_second, "", "", "ASYNC"),
+            ("table_index_0__ASYNC", pk_types, {}, index_first, "", "", "ASYNC"),
+            ("table_all_types", pk_types, {
+             **pk_types, **non_pk_types}, {}, "", "", ""),
+            ("table_ttl_DyNumber", pk_types, {}, {}, "DyNumber", "", ""),
+            ("table_ttl_Uint32", pk_types, {}, {}, "Uint32", "", ""),
+            ("table_ttl_Uint64", pk_types, {}, {}, "Uint64", "", ""),
+            ("table_ttl_Datetime", pk_types, {}, {}, "Datetime", "", ""),
+            ("table_ttl_Timestamp", pk_types, {}, {}, "Timestamp", "", ""),
+            ("table_ttl_Date", pk_types, {}, {}, "Date", "", ""),
+
+        ]
+    )
+    def test_s3(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
+        self.do_test_s3(table_name, pk_types, all_types, index, ttl, unique, sync)
+
+
+class TestPgYdbS3TTL(TestPgBase, TestYdbS3TTLBase):
+    @pytest.mark.parametrize(
+        "table_name, pk_types, all_types, index, ttl, unique, sync",
+        [
+            ("table_index_0_UNIQUE_SYNC", pk_pg_types, {}, pk_pg_types_no_bool, "", "UNIQUE", "SYNC"),
+            ("table_index_0__SYNC", pk_pg_types, {}, pk_pg_types, "", "", "SYNC"),
+            ("table_index_0__ASYNC", pk_pg_types, {}, pk_pg_types, "", "", "ASYNC"),
+            ("table_all_types", pk_pg_types, {**pk_pg_types, **non_pk_pg_types}, {}, "", "", ""),
+            ("table_ttl_pgint4", pk_pg_types, {}, {}, "pgint4", "", ""),
+            ("table_ttl_pgint8", pk_pg_types, {}, {}, "pgint8", "", ""),
+            ("table_ttl_pgdate", pk_pg_types, {}, {}, "pgdate", "", ""),
+            ("table_ttl_pgtimestamp", pk_pg_types, {}, {}, "pgtimestamp", "", ""),
+
+        ]
+    )
+    def test_s3(self, table_name: str, pk_types: dict[str, str], all_types: dict[str, str], index: dict[str, str], ttl: str, unique: str, sync: str):
+        self.do_test_s3(table_name, pk_types, all_types, index, ttl, unique, sync)

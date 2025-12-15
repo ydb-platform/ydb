35b5d51e0e716d4e9fac438e1f2e3cec04b71d4f
Kirill Vasilenko <kir-vasilenko@ydb.tech>
2025-12-15 16:40:52 +0300

    Fix the aborting transactions in Column Tables (#30477)
    Co-authored-by: Kirill Vasilenko <kir-vasilenko@ybd.tech>

--- ydb/core/tx/columnshard/columnshard__write.cpp
+++ ydb/core/tx/columnshard/columnshard__write.cpp
@@ -306,38 +306,12 @@ private:
     std::shared_ptr<TTxController::ITransactionOperator> TxOperator;
 };
 
-class TAbortWriteTransaction: public NTabletFlatExecutor::TTransactionBase<TColumnShard> {
-private:
-    using TBase = NTabletFlatExecutor::TTransactionBase<TColumnShard>;
-
-public:
-    TAbortWriteTransaction(TColumnShard* self, const ui64 txId, const TActorId source, const ui64 cookie)
-        : TBase(self)
-        , TxId(txId)
-        , Source(source)
-        , Cookie(cookie) {
+void TColumnShard::ProposeTransaction(std::shared_ptr<TCommitOperation> op, const TActorId source, const ui64 cookie) {
+    if (auto lock = OperationsManager->GetLockOptional(op->GetLockId()); lock) {
+        lock->SetTxId(op->GetTxId());
     }
-
-    virtual bool Execute(TTransactionContext& txc, const TActorContext&) override {
-        Self->GetOperationsManager().AbortTransactionOnExecute(*Self, TxId, txc);
-        return true;
-    }
-
-    virtual void Complete(const TActorContext& ctx) override {
-        LWPROBE(EvWriteResult, Self->TabletID(), Source.ToString(), TxId, Cookie, "abort", true, "");
-        Self->GetOperationsManager().AbortTransactionOnComplete(*Self, TxId);
-        auto result = NEvents::TDataEvents::TEvWriteResult::BuildCompleted(Self->TabletID(), TxId);
-        ctx.Send(Source, result.release(), 0, Cookie);
-    }
-    TTxType GetTxType() const override {
-        return TXTYPE_PROPOSE;
-    }
-
-private:
-    ui64 TxId;
-    TActorId Source;
-    ui64 Cookie;
-};
+    Execute(new TProposeWriteTransaction(this, op, source, cookie));
+}
 
 void TColumnShard::Handle(NEvents::TDataEvents::TEvWrite::TPtr& ev, const TActorContext& ctx) {
     TMemoryProfileGuard mpg("NEvents::TDataEvents::TEvWrite");
@@ -376,7 +350,12 @@ void TColumnShard::Handle(NEvents::TDataEvents::TEvWrite::TPtr& ev, const TActor
 
     if (behaviour == EOperationBehaviour::AbortWriteLock) {
         LWPROBE(EvWrite, TabletID(), source.ToString(), cookie, record.GetTxId(), writeTimeout.value_or(TDuration::Max()), 0, "AbortWriteLock", true, false, ToString(NKikimrDataEvents::TEvWriteResult::STATUS_UNSPECIFIED), "");
-        Execute(new TAbortWriteTransaction(this, record.GetLocks().GetLocks()[0].GetLockId(), source, cookie), ctx);
+        auto lockId = record.GetLocks().GetLocks()[0].GetLockId();
+        TransactionToAbort(lockId);
+        // answer kqp right away, the actual aborting may happen later (or might already happened)
+        LWPROBE(EvWriteResult, TabletID(), source.ToString(), lockId, cookie, "abort", true, "");
+        auto result = NEvents::TDataEvents::TEvWriteResult::BuildCompleted(TabletID(), lockId);
+        ctx.Send(source, result.release(), 0, cookie);
         return;
     }
 
@@ -388,8 +367,8 @@ void TColumnShard::Handle(NEvents::TDataEvents::TEvWrite::TPtr& ev, const TActor
     };
 
     if (behaviour == EOperationBehaviour::WriteWithLock) {
-        if (auto lock = OperationsManager->GetLockOptional(record.GetLockTxId()); lock && lock->IsDeleted()) {
-            sendError("lock is already deleted", NKikimrDataEvents::TEvWriteResult::STATUS_LOCKS_BROKEN);
+        if (auto lock = OperationsManager->GetLockOptional(record.GetLockTxId()); lock && lock->NeedsAborting()) {
+            sendError("lock is already aborted", NKikimrDataEvents::TEvWriteResult::STATUS_LOCKS_BROKEN);
             return;
         }
     }
@@ -397,10 +376,7 @@ void TColumnShard::Handle(NEvents::TDataEvents::TEvWrite::TPtr& ev, const TActor
     const auto inFlightLocksRangesBytes = NOlap::TPKRangeFilter::GetFiltersTotalMemorySize();
     const ui64 inFlightLocksRangesBytesLimit = AppDataVerified().ColumnShardConfig.GetInFlightLocksRangesBytesLimit();
     if (behaviour == EOperationBehaviour::WriteWithLock && inFlightLocksRangesBytes > inFlightLocksRangesBytesLimit) {
-        if (auto lock = OperationsManager->GetLockOptional(record.GetLockTxId()); lock) {
-            lock->SetDeleted();
-            MaybeCleanupLock(record.GetLockTxId());
-        }
+        TransactionToAbort(record.GetLockTxId());
         AFL_WARN(NKikimrServices::TX_COLUMNSHARD)("event", "In flight locks ranges bytes limit exceeded")
             ("inFlightLocksRangesBytes", inFlightLocksRangesBytes)
             ("inFlightLocksRangesBytesLimit", inFlightLocksRangesBytesLimit);
@@ -449,10 +425,10 @@ void TColumnShard::Handle(NEvents::TDataEvents::TEvWrite::TPtr& ev, const TActor
                                 " != " + ::ToString(commitOperation->GetInternalGenerationCounter()),
                             NKikimrDataEvents::TEvWriteResult::STATUS_LOCKS_BROKEN);
                     } else {
-                        Execute(new TProposeWriteTransaction(this, commitOperation, source, cookie), ctx);
+                        ProposeTransaction(commitOperation, source, cookie);
                     }
                 } else {
-                    Execute(new TProposeWriteTransaction(this, commitOperation, source, cookie), ctx);
+                    ProposeTransaction(commitOperation, source, cookie);
                 }
             }
         }

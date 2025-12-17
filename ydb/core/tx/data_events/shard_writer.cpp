cfd3e915892ce01498d2f168ebee6960b7d8ea4e
Oleg Doronin <dorooleg@yandex.ru>
2025-12-17 14:29:00 +0000

    fix

--- ydb/core/tx/data_events/shard_writer.cpp
+++ ydb/core/tx/data_events/shard_writer.cpp
@@ -175,7 +175,7 @@ namespace NKikimr::NEvWrite {
     }
 
     bool TShardWriter::IsMaxRetriesReached() const {
-        return NumRetries >= MaxRetriesPerShard;
+        return NumRetries >= GetMaxRetriesPerShard();
     }
     
     ui32 GetMaxRetriesPerShard() const {

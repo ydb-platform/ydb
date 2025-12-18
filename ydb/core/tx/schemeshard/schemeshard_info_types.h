fc1fd70c6adf596805ae6fd358b684fbdc2118c4
Vitaliy Filippov <vitaliff@ydb.tech>
2025-12-18 09:22:57 +0000

    Fix a crash detected by fixed reboot tests

--- ydb/core/tx/schemeshard/schemeshard_info_types.h
+++ ydb/core/tx/schemeshard/schemeshard_info_types.h
@@ -3682,7 +3682,9 @@ public:
         TSerializedTableRange bound{range};
         LOG_DEBUG_S(TlsActivationContext->AsActorContext(), NKikimrServices::BUILD_INDEX,
             "AddShardStatus id# " << Id << " shard " << shardIdx);
-        if (BuildKind == TIndexBuildInfo::EBuildKind::BuildVectorIndex) {
+        if (BuildKind == TIndexBuildInfo::EBuildKind::BuildVectorIndex &&
+            KMeans.State != TIndexBuildInfo::TKMeans::Filter &&
+            KMeans.State != TIndexBuildInfo::TKMeans::FilterBorders) {
             AddParent(bound, shardIdx);
         }
         Shards.emplace(

778907359417dac3bbd8d8e2e32298ca267f0d67
Konstantin Melekhov <komels@ydb.tech>
2025-12-19 03:32:21 +0000

    Refactor

--- ydb/core/transfer/transfer_writer.cpp
+++ ydb/core/transfer/transfer_writer.cpp
@@ -564,7 +564,6 @@ private:
         }
         Stats.StartCpuSec = GetElapsedTicksAsSeconds();
         Stats.Operation = newState;
-
     }
 
     void SendStats(EWorkerOperation newCurrentOperation) {

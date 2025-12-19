355c43077e5bcf26c90cbd71e9b6685a6901c5f3
xyliganSereja <matva0509@gmail.com>
2025-12-19 13:58:05 +0300

    Fix ignored messages on CS table rename 2 (#30861)
    Co-authored-by: Matveev Sergei <xyligansereja@yandex-team.ru>

--- ydb/core/tx/schemeshard/schemeshard__operation_common.cpp
+++ ydb/core/tx/schemeshard/schemeshard__operation_common.cpp
@@ -1038,6 +1038,7 @@ TProposedWaitParts::TProposedWaitParts(TOperationId id, TTxState::ETxState nextS
     IgnoreMessages(DebugHint(),
         { TEvHive::TEvCreateTabletReply::EventType
         , TEvDataShard::TEvProposeTransactionResult::EventType
+        , TEvColumnShard::TEvProposeTransactionResult::EventType
         , TEvPrivate::TEvOperationPlan::EventType }
     );
 }

74c0c8ceb1821121e60dc07ae922570bef76c495
Iurii Kravchenko <xjie6@ydb.tech>
2025-12-19 11:13:21 +0100

    Fix formatting and structure of THistorgamBorders

--- ydb/core/tx/columnshard/counters/histogram_borders.h
+++ ydb/core/tx/columnshard/counters/histogram_borders.h
@@ -6,13 +6,13 @@
 #include <util/system/types.h>
 
 namespace NKikimr::NColumnShard {
-    struct THistorgamBorders {
-        static const std::map<i64, TString> BlobSizeBorders;
+struct THistorgamBorders {
+    static const std::map<i64, TString> BlobSizeBorders;
 
-        static const std::map<i64, TString> BytesBorders;
+    static const std::map<i64, TString> BytesBorders;
 
-        static const std::map<i64, TString> TimeBordersMicroseconds;
+    static const std::map<i64, TString> TimeBordersMicroseconds;
 
-        static const std::set<i64> PortionRecordBorders;
-    };
+    static const std::set<i64> PortionRecordBorders;
+};
 }

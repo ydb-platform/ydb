a0ad024952da095ade4f17ccf6514032bbb94276
Nikita Sokolov <faucct@tracto.ai>
2025-12-24 16:53:37 +0300

    YT-18571: Fix S3 client hanging on HEAD errors
    `NYT::NS3::TClient::ErrorFromResponse` hangs till a Read timeout for HEAD request responses:
    https://github.com/ytsaurus/ytsaurus/blob/317c99834316ac46f05429494d0b3291ffbeb8c8/yt/yt/library/s3/client.cpp#L459
    
    ---
    
    #| || **<a href="https://nda.ya.ru/t/DC6HYG3N7QaZN7" target="_blank">![](https://nda.ya.ru/t/mAiQIjHx7Mm3JC =30x) Echo tests</a>** || |#
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/1491
    
    Co-authored-by: nadya73 <nadya73@yandex-team.com>
    Co-authored-by: nadya73 <nadya73@yandex-team.com>
    Co-authored-by: nadya73 <nadya73@yandex-team.com>
    Co-authored-by: nadya73 <nadya73@yandex-team.com>
    Co-authored-by: nadya73 <nadya73@yandex-team.com>
    Co-authored-by: nadya73 <nadya73@yandex-team.com>
    Co-authored-by: Nikita Sokolov <faucct@gmail.com>
    commit_hash:ee6399bf6f6ef6e750c7554f5667e3b3a9d99eed

--- yt/yt/core/http/server.cpp
+++ yt/yt/core/http/server.cpp
@@ -390,6 +390,7 @@ private:
             connection->GetRemoteAddress(),
             GetCurrentInvoker(),
             EMessageType::Request,
+            /*requestMethod*/ std::nullopt,
             Config_);
 
         if (Config_->IsHttps) {

7d8f4afc5ff69056d832a480672dc6fee6e26858
ijon <ijon@ydb.tech>
2025-12-24 12:54:37 +0300

    ttl: add database-level setting for max-ttl-shards-in-flight (#31226)
    Add `scheme_shard_config.max_ttl_shards_in_flight` setting -- database-level default for the number of shards per any single table, executing conditional erase (TTL) tasks in parallel.
    
    Remove previous change of default value for table-level setting
    `TableDescription.TTLSettings.Enabled.SysSettings.MaxShardsInFlight`.
    As safe as it really was, it violated the principle: "defaults define behavior; behavior should be changed through easily controllable configuration parameters."
    
    Now database-level default is used when table-level setting is not explicitly set.

--- ydb/core/tx/schemeshard/schemeshard_impl.cpp
+++ ydb/core/tx/schemeshard/schemeshard_impl.cpp
@@ -5098,6 +5098,7 @@ void TSchemeShard::OnActivateExecutor(const TActorContext &ctx) {
     ConfigureStatsOperations(appData->SchemeShardConfig, ctx);
     MaxCdcInitialScanShardsInFlight = appData->SchemeShardConfig.GetMaxCdcInitialScanShardsInFlight();
     MaxRestoreBuildIndexShardsInFlight = appData->SchemeShardConfig.GetMaxRestoreBuildIndexShardsInFlight();
+    MaxTTLShardsInFlight = appData->SchemeShardConfig.GetMaxTTLShardsInFlight();
 
     SendStatsIntervalSecondsDedicated = appData->StatisticsConfig.GetBaseStatsSendIntervalSecondsDedicated();
     SendStatsIntervalSecondsServerless = appData->StatisticsConfig.GetBaseStatsSendIntervalSecondsServerless();
@@ -7754,6 +7755,7 @@ void TSchemeShard::ApplyConsoleConfigs(const NKikimrConfig::TAppConfig& appConfi
         ConfigureStatsOperations(schemeShardConfig, ctx);
         MaxCdcInitialScanShardsInFlight = schemeShardConfig.GetMaxCdcInitialScanShardsInFlight();
         MaxRestoreBuildIndexShardsInFlight = schemeShardConfig.GetMaxRestoreBuildIndexShardsInFlight();
+        MaxTTLShardsInFlight = schemeShardConfig.GetMaxTTLShardsInFlight();
     }
 
     if (appConfig.HasTableProfilesConfig()) {

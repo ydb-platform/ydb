75d2a0d4258c45c225dcc213514de47b469e381d
Denis Khalikov <deniskhalikov@ydb.tech>
2025-12-25 17:19:12 +0300

    [NEW RBO] Add physical rule to peephole predicate. (#31304)

--- ydb/core/kqp/opt/rbo/kqp_rbo_rules.cpp
+++ ydb/core/kqp/opt/rbo/kqp_rbo_rules.cpp
@@ -4,6 +4,7 @@
 #include <yql/essentials/utils/log/log.h>
 #include <ydb/core/kqp/opt/physical/predicate_collector.h>
 #include <ydb/core/kqp/opt/physical/kqp_opt_phy_olap_filter.h>
+#include <yql/essentials/core/peephole_opt/yql_opt_peephole_physical.h>
 
 #include <typeinfo>
 
@@ -652,6 +653,74 @@ std::shared_ptr<IOperator> TPushFilterRule::SimpleMatchAndAppy(const std::shared
     return output;
 }
 
+bool IsSuitableToApplyPeephole(const std::shared_ptr<IOperator>& input) {
+    if (input->Kind != EOperator::Filter) {
+        return false;
+    }
+
+    const auto filter = CastOperator<TOpFilter>(input);
+    const auto lambda = TCoLambda(filter->FilterLambda);
+    auto peepholeIsNeeded = [&](const TExprNode::TPtr& node) -> bool {
+        // Here is a list of Callables for which peephole is needed.
+        if (node->IsCallable({"SqlIn"})) {
+            return true;
+        }
+        return false;
+    };
+
+    return !!FindNode(lambda.Body().Ptr(), peepholeIsNeeded);
+}
+
+std::shared_ptr<IOperator> TPeepholePredicate::SimpleMatchAndAppy(const std::shared_ptr<IOperator>& input, TRBOContext& ctx, TPlanProps& props) {
+    Y_UNUSED(props);
+    if (!IsSuitableToApplyPeephole(input)) {
+        return input;
+    }
+
+    const auto filter = CastOperator<TOpFilter>(input);
+    const auto lambda = TCoLambda(filter->FilterLambda);
+    TVector<const TTypeAnnotationNode*> argTypes{lambda.Args().Arg(0).Ptr()->GetTypeAnn()};
+    // Closure an original predicate, we cannot call `Peephole` for free args.
+    // clang-format off
+    auto predicateClosure = Build<TKqpPredicateClosure>(ctx.ExprCtx, input->Pos)
+        .Lambda<TCoLambda>()
+            .Args({"arg"})
+            .Body<TExprApplier>()
+                .Apply(lambda)
+                .With(lambda.Args().Arg(0), "arg")
+            .Build()
+        .Build()
+        .ArgsType(ExpandType(input->Pos, *ctx.ExprCtx.MakeType<TTupleExprType>(argTypes), ctx.ExprCtx))
+    .Done();
+    // clang-format on
+    YQL_CLOG(TRACE, ProviderKqp) << "[NEW RBO] Before peephole: " << KqpExprToPrettyString(predicateClosure, ctx.ExprCtx);
+
+    TExprNode::TPtr afterPeephole;
+    bool hasNonDeterministicFunctions;
+    // Using a special PeepholeTypeAnnTransformer.
+    if (const auto status = PeepHoleOptimizeNode(predicateClosure.Ptr(), afterPeephole, ctx.ExprCtx, ctx.TypeCtx, ctx.PeepholeTypeAnnTransformer.Get(),
+                                                 hasNonDeterministicFunctions);
+        status != IGraphTransformer::TStatus::Ok) {
+        YQL_CLOG(ERROR, ProviderKqp) << "[NEW RBO] Peephole failed with status: " << status << Endl;
+        return input;
+    }
+    Y_ENSURE(afterPeephole);
+    YQL_CLOG(TRACE, ProviderKqp) << "[NEW RBO] After peephole: " << KqpExprToPrettyString(TExprBase(afterPeephole), ctx.ExprCtx);
+
+    auto lambdaAfterPeephole = TExprBase(afterPeephole).Cast<TKqpPredicateClosure>().Lambda();
+    // clang-format off
+    auto newLambda = Build<TCoLambda>(ctx.ExprCtx, input->Pos)
+        .Args({"arg"})
+        .Body<TExprApplier>()
+            .Apply(lambdaAfterPeephole.Body())
+            .With(lambdaAfterPeephole.Args().Arg(0), "arg")
+        .Build()
+    .Done().Ptr();
+    // clang-format on
+
+    return std::make_shared<TOpFilter>(filter->GetInput(), input->Pos, newLambda);
+}
+
 bool IsSuitableToPushPredicateToColumnTables(const std::shared_ptr<IOperator>& input) {
     if (input->Kind != EOperator::Filter) {
         return false;
@@ -659,7 +728,8 @@ bool IsSuitableToPushPredicateToColumnTables(const std::shared_ptr<IOperator>& i
 
     const auto filter = CastOperator<TOpFilter>(input);
     const auto maybeRead = filter->GetInput();
-    return ((maybeRead->Kind == EOperator::Source) && (CastOperator<TOpRead>(maybeRead)->GetTableStorageType() == NYql::EStorageType::ColumnStorage) && filter->GetTypeAnn());
+    return ((maybeRead->Kind == EOperator::Source) && (CastOperator<TOpRead>(maybeRead)->GetTableStorageType() == NYql::EStorageType::ColumnStorage) &&
+            filter->GetTypeAnn());
 }
 
 std::shared_ptr<IOperator> TPushOlapFilterRule::SimpleMatchAndAppy(const std::shared_ptr<IOperator>& input, TRBOContext& ctx, TPlanProps& props) {
--- ydb/core/kqp/opt/rbo/kqp_rbo_transformer.cpp
+++ ydb/core/kqp/opt/rbo/kqp_rbo_transformer.cpp
@@ -174,19 +174,50 @@ IGraphTransformer::TStatus TKqpRBOCleanupTransformer::DoTransform(TExprNode::TPt
     return IGraphTransformer::TStatus::Ok;
 }
 
-void TKqpRBOCleanupTransformer::Rewind() {}
+TKqpNewRBOTransformer::TKqpNewRBOTransformer(TIntrusivePtr<TKqpOptimizeContext>& kqpCtx, TTypeAnnotationContext& typeCtx,
+                                             TAutoPtr<IGraphTransformer>&& rboTypeAnnTransformer, TAutoPtr<IGraphTransformer>&& peepholeTypeAnnTransformer,
+                                             const NMiniKQL::IFunctionRegistry& funcRegistry)
+    : TypeCtx(typeCtx)
+    , KqpCtx(*kqpCtx)
+    , RBO(kqpCtx, typeCtx, std::move(rboTypeAnnTransformer), std::move(peepholeTypeAnnTransformer), funcRegistry) {
+    // Initial stages.
+    TVector<std::shared_ptr<IRule>> inlineScalarSubPlanStageRules{std::make_shared<TInlineScalarSubplanRule>()};
+    RBO.AddStage(std::make_shared<TRuleBasedStage>("Inline scalar subplans", std::move(inlineScalarSubPlanStageRules)));
+    RBO.AddStage(std::make_shared<TRenameStage>());
+    RBO.AddStage(std::make_shared<TConstantFoldingStage>());
+    RBO.AddStage(std::make_shared<TPruneColumnsStage>());
+    // Logical stage.
+    TVector<std::shared_ptr<IRule>> logicalStageRules = {std::make_shared<TRemoveIdenityMapRule>(), std::make_shared<TExtractJoinExpressionsRule>(),
+                                                         std::make_shared<TPushMapRule>(), std::make_shared<TPushFilterRule>(),
+                                                         std::make_shared<TPushLimitIntoSortRule>()};
+    RBO.AddStage(std::make_shared<TRuleBasedStage>("Logical rewrites I", std::move(logicalStageRules)));
+    // Physical stage.
+    TVector<std::shared_ptr<IRule>> physicalStageRules = {std::make_shared<TPeepholePredicate>(), std::make_shared<TPushOlapFilterRule>()};
+    RBO.AddStage(std::make_shared<TRuleBasedStage>("Physical rewrites I", std::move(physicalStageRules)));
+    // CBO stages.
+    TVector<std::shared_ptr<IRule>> initialCBOStageRules = {std::make_shared<TBuildInitialCBOTreeRule>(), std::make_shared<TExpandCBOTreeRule>()};
+    RBO.AddStage(std::make_shared<TRuleBasedStage>("Prepare for CBO", std::move(initialCBOStageRules)));
+    TVector<std::shared_ptr<IRule>> cboStageRules = {std::make_shared<TOptimizeCBOTreeRule>()};
+    RBO.AddStage(std::make_shared<TRuleBasedStage>("Invoke CBO", std::move(cboStageRules)));
+    TVector<std::shared_ptr<IRule>> cleanUpCBOStageRules = {std::make_shared<TInlineCBOTreeRule>(), std::make_shared<TPushFilterRule>()};
+    RBO.AddStage(std::make_shared<TRuleBasedStage>("Clean up after CBO", std::move(cleanUpCBOStageRules)));
+    // Assign physical stages.
+    TVector<std::shared_ptr<IRule>> assignPhysicalStageRules = {std::make_shared<TAssignStagesRule>()};
+    RBO.AddStage(std::make_shared<TRuleBasedStage>("Assign stages", std::move(assignPhysicalStageRules)));
+}
 
-TAutoPtr<IGraphTransformer> CreateKqpRewriteSelectTransformer(const TIntrusivePtr<TKqpOptimizeContext> &kqpCtx,
-                                                          TTypeAnnotationContext &typeCtx) {
+void TKqpRBOCleanupTransformer::Rewind() {
+}
+
+TAutoPtr<IGraphTransformer> CreateKqpRewriteSelectTransformer(const TIntrusivePtr<TKqpOptimizeContext>& kqpCtx, TTypeAnnotationContext& typeCtx) {
     return new TKqpRewriteSelectTransformer(kqpCtx, typeCtx);
 }
 
-TAutoPtr<IGraphTransformer> CreateKqpNewRBOTransformer(TIntrusivePtr<TKqpOptimizeContext> &kqpCtx, TTypeAnnotationContext &typeCtx,
-                                                       TAutoPtr<IGraphTransformer> rboTypeAnnTransformer,
-                                                       TAutoPtr<IGraphTransformer> typeAnnTransformer,
-                                                       TAutoPtr<IGraphTransformer> peephole,
+TAutoPtr<IGraphTransformer> CreateKqpNewRBOTransformer(TIntrusivePtr<TKqpOptimizeContext>& kqpCtx, TTypeAnnotationContext& typeCtx,
+                                                       TAutoPtr<IGraphTransformer>&& rboTypeAnnTransformer,
+                                                       TAutoPtr<IGraphTransformer>&& peepholeTypeAnnTransformer,
                                                        const NMiniKQL::IFunctionRegistry& funcRegistry) {
-    return new TKqpNewRBOTransformer(kqpCtx, typeCtx, rboTypeAnnTransformer, typeAnnTransformer, peephole, funcRegistry);
+    return new TKqpNewRBOTransformer(kqpCtx, typeCtx, std::move(rboTypeAnnTransformer), std::move(peepholeTypeAnnTransformer), funcRegistry);
 }
 
 TAutoPtr<IGraphTransformer> CreateKqpRBOCleanupTransformer(TTypeAnnotationContext &typeCtx) {

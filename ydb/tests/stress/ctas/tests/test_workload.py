e13858dcac40624135a9bdb3fc59b98f7a3af81b
Pavel Efarinov <pefavel@ydb.tech>
2025-12-25 18:47:04 +0300

    Revert "reduce stress duration"
    This reverts commit ffacc8361f84bcb7dbd919915fa50b68fd14a672.

--- ydb/tests/stress/ctas/tests/test_workload.py
+++ ydb/tests/stress/ctas/tests/test_workload.py
@@ -29,5 +29,5 @@ class TestYdbWorkload(StressFixture):
             yatest.common.binary_path(os.environ["YDB_WORKLOAD_PATH"]),
             "--endpoint", self.endpoint,
             "--database", self.database,
-            "--duration", "60",
+            "--duration", "120",
         ])

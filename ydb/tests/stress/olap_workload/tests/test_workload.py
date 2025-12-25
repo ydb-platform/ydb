cd8a972665e5aaaa09723327b4517218cee8738e
Pavel <pefavel@ydb.tech>
2025-12-25 18:47:41 +0300

    Update test_workload.py

--- ydb/tests/stress/olap_workload/tests/test_workload.py
+++ ydb/tests/stress/olap_workload/tests/test_workload.py
@@ -28,5 +28,5 @@ class TestYdbWorkload(StressFixture):
             yatest.common.binary_path(os.environ["YDB_WORKLOAD_PATH"]),
             "--endpoint", self.endpoint,
             "--database", self.database,
-            "--duration", "30",
+            "--duration", "60",
         ])

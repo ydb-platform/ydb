18553a6933f9bd97250d03a5d2b2087977077274
st-shchetinin <st-shchetinin@ydb.tech>
2025-12-28 23:49:52 +0000

    removed RequiresHttpResolver

--- ydb/core/tx/datashard/import_s3.cpp
+++ ydb/core/tx/datashard/import_s3.cpp
@@ -61,9 +61,6 @@ using namespace Aws::S3;
 using namespace Aws;
 
 template <typename TSettings>
-constexpr bool RequiresHttpResolver = std::is_same_v<TSettings, NKikimrSchemeOp::TS3Settings>;
-
-template <typename TSettings>
 class TS3Downloader: public TActorBootstrapped<TS3Downloader<TSettings>> {
     using TThis = TS3Downloader<TSettings>;
     // IReadController

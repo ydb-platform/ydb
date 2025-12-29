298150fbfaababbad0e31ad49014db79048bc3aa
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2025-12-29 19:40:04 +0300

    Materialized index inherits parent table's metadata

--- ydb/core/tx/schemeshard/ut_restore/ut_restore.cpp
+++ ydb/core/tx/schemeshard/ut_restore/ut_restore.cpp
@@ -6315,7 +6315,7 @@ Y_UNIT_TEST_SUITE(TImportTests) {
         UNIT_ASSERT_EQUAL(issues.begin()->message(), "Unsupported scheme object type");
     }
 
-    void MaterializedIndex(Ydb::Import::ImportFromS3Settings::IndexPopulationMode mode) {
+    void MaterializedIndex(Ydb::Import::ImportFromS3Settings::IndexPopulationMode mode, const TString& metadata = R"({"version": 1})") {
         TTestBasicRuntime runtime;
         TTestEnv env(runtime, TTestEnvOptions().EnableIndexMaterialization(true));
 
@@ -6334,7 +6334,7 @@ Y_UNIT_TEST_SUITE(TImportTests) {
               index_columns: "value"
               global_index {}
             }
-        )", {{"a", 1}});
+        )", {{"a", 1}}, "", metadata);
 
         const auto b = GenerateTestData(R"(
             columns {
@@ -6347,7 +6347,7 @@ Y_UNIT_TEST_SUITE(TImportTests) {
             }
             primary_key: "value"
             primary_key: "key"
-        )", {{"b", 1}});
+        )", {{"b", 1}}, "", metadata);
 
         Run(runtime, env, ConvertTestData({{"/a", a}, {"/a/by_value/indexImplTable", b}}), Sprintf(R"(
             ImportFromS3Settings {
@@ -6377,6 +6377,10 @@ Y_UNIT_TEST_SUITE(TImportTests) {
 
     Y_UNIT_TEST(MaterializedIndexAuto) {
         MaterializedIndex(Ydb::Import::ImportFromS3Settings::INDEX_POPULATION_MODE_AUTO);
+    }
+
+    Y_UNIT_TEST(MaterializedIndexOldMetadata) {
+        MaterializedIndex(Ydb::Import::ImportFromS3Settings::INDEX_POPULATION_MODE_IMPORT, R"({"version": 0})");
     }
 
     void MaterializedIndexAbsent(Ydb::Import::ImportFromS3Settings::IndexPopulationMode mode, bool shouldFail) {

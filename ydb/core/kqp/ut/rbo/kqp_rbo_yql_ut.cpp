af8af02f898ee9f00256fd58f0d12a03c69c9f7c
Velikhov Pavel <pavelvelikhov@localhost.localdomain>
2025-12-30 08:19:05 +0000

    [NEW RBO] Added limit 1 to exists subqueries, enabled not exists test

--- ydb/core/kqp/ut/rbo/kqp_rbo_yql_ut.cpp
+++ ydb/core/kqp/ut/rbo/kqp_rbo_yql_ut.cpp
@@ -933,13 +933,10 @@ Y_UNIT_TEST_SUITE(KqpRboYql) {
                 PRAGMA YqlSelect = 'force';
                 SELECT bar.id FROM `/Root/bar` as bar where bar.id == 0 AND EXISTS (SELECT foo.id FROM `/Root/foo` as foo WHERE foo.id != 0);
             )",
-            /*
             R"(
                 PRAGMA YqlSelect = 'force';
                 SELECT bar.id FROM `/Root/bar` as bar where bar.id == 0 AND NOT EXISTS (SELECT foo.id FROM `/Root/foo` as foo WHERE foo.id == 6);
             )",
-        };
-            */
         };
 
         // TODO: The order of result is not defined, we need order by to add more interesting tests.

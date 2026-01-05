bb8a18745ef39d6400f05d7bc2e8a81553300643
Yesdaulet Izenov <yizenov@ydb.tech>
2026-01-05 22:52:38 +0300

    Aligned estimators by the book

--- yql/essentials/core/histogram/ut/eq_width_histogram_ut.cpp
+++ yql/essentials/core/histogram/ut/eq_width_histogram_ut.cpp
@@ -35,9 +35,8 @@ void PopulateHistogram(std::shared_ptr<TEqWidthHistogram> histogram, const std::
         TEqWidthHistogram::THistValue bucketWidth = histogram->template GetBucketWidth<T>();
         T width = LoadFrom<T>(bucketWidth.Value);
         T step = width / 2;
-        T rangeStart = LoadFrom<T>(histogram->GetDomainRange().Start);
         for (ui32 i = 0; i < histogram->GetNumBuckets(); ++i) {
-            T border = rangeStart + static_cast<T>(i) * width;
+            T border = histogram->GetBorderValue<T>(i);
             histogram->AddElement(border - step);
             histogram->AddElement(border + step);
         }
@@ -96,60 +95,60 @@ Y_UNIT_TEST_SUITE(EqWidthHistogram) {
 Y_UNIT_TEST(Basic) {
     // single bucket cases + signed values cases
     TestHistogramBasic<ui32, ui32>(1, /*values range=*/{0, 25}, /*column range=*/{0, 20}, EHistogramValueType::Uint32,
-                                   /*{value, result}=*/{25, 25}, /*{value, result}=*/{4, 25}, /*{value, result}=*/{4, 1});
+                                   /*{value, result}=*/{25, 21}, /*{value, result}=*/{4, 1}, /*{value, result}=*/{4, 1});
     TestHistogramBasic<i32, i32>(1, /*values range=*/{-5, 25}, /*column range=*/{0, 20}, EHistogramValueType::Int32,
-                                 /*{value, result}=*/{25, 30}, /*{value, result}=*/{4, 30}, /*{value, result}=*/{4, 1});
+                                 /*{value, result}=*/{25, 21}, /*{value, result}=*/{4, 1}, /*{value, result}=*/{4, 1});
     TestHistogramBasic<double, double>(1, /*values range=*/{-5.0, 25.0}, /*column range=*/{0.0, 20.5}, EHistogramValueType::Double,
-                                       /*{value, result}=*/{25.0, 2}, /*{value, result}=*/{4.0, 2}, /*{value, result}=*/{4.0, 2});
+                                       /*{value, result}=*/{25.0, 1}, /*{value, result}=*/{4.0, 1}, /*{value, result}=*/{2.0, 1});
 
     // multiple bucket cases + signed values cases
     TestHistogramBasic<ui32, ui32>(10, /*values range=*/{0, 25}, /*column range=*/{0, 20}, EHistogramValueType::Uint32,
-                                   /*{value, result}=*/{25, 25}, /*{value, result}=*/{4, 21}, /*{value, result}=*/{4, 1});
-    TestHistogramBasic<i64, i64>(10, /*values range=*/{-30, -5}, /*column range=*/{-25, -5}, EHistogramValueType::Int64,
-                                 /*{value, result}=*/{25, 25}, /*{value, result}=*/{-17, 12}, /*{value, result}=*/{1, 1});
+                                   /*{value, result}=*/{25, 21}, /*{value, result}=*/{4, 16}, /*{value, result}=*/{4, 1});
+    TestHistogramBasic<i64, i64>(10, /*values range=*/{-30, 0}, /*column range=*/{-25, -5}, EHistogramValueType::Int64,
+                                 /*{value, result}=*/{25, 21}, /*{value, result}=*/{-17, 12}, /*{value, result}=*/{1, 1});
     TestHistogramBasic<i64, i64>(10, /*values range=*/{-10, 25}, /*column range=*/{-5, 20}, EHistogramValueType::Int64,
-                                 /*{value, result}=*/{25, 35}, /*{value, result}=*/{-2, 28}, /*{value, result}=*/{-4, 3});
+                                 /*{value, result}=*/{25, 26}, /*{value, result}=*/{-2, 23}, /*{value, result}=*/{-4, 1});
 
     // floating point data test
     TestHistogramBasic<float, float>(10, /*values range=*/{-5.0, 25.0}, /*column range=*/{0.0, 20.0}, EHistogramValueType::Float,
-                                     /*{value, result}=*/{25.0, 20}, /*{value, result}=*/{4.0, 15}, /*{value, result}=*/{4.0, 2});
+                                     /*{value, result}=*/{25.0, 19}, /*{value, result}=*/{4.0, 15}, /*{value, result}=*/{2.0, 2});
     TestHistogramBasic<double, double>(10, /*values range=*/{-5.0, 25.0}, /*column range=*/{0.0, 20.0}, EHistogramValueType::Double,
-                                       /*{value, result}=*/{25.0, 20}, /*{value, result}=*/{4.0, 15}, /*{value, result}=*/{4.0, 2});
+                                       /*{value, result}=*/{25.0, 19}, /*{value, result}=*/{4.0, 15}, /*{value, result}=*/{2.0, 2});
 
     // large and small floating point data test
     TestHistogramBasic<double, double>(10, /*values range=*/{-0.1, 1.5},
                                        /*column range=*/{1.0, 1.0 + 30 * std::numeric_limits<double>::epsilon()}, EHistogramValueType::Double,
-                                       /*{value, result}=*/{2.0, 20}, /*{value, result}=*/{2.0, 1}, /*{value, result}=*/{4.0, 1});
+                                       /*{value, result}=*/{2.0, 19}, /*{value, result}=*/{2.0, 0}, /*{value, result}=*/{4.0, 1});
     TestHistogramBasic<double, double>(10, /*values range=*/{99.0, 100.5},
                                        /*column range=*/{100.0, 100.0 + 300 * std::numeric_limits<double>::epsilon()}, EHistogramValueType::Double,
-                                       /*{value, result}=*/{98.0, 4}, /*{value, result}=*/{101.0, 0}, /*{value, result}=*/{4.0, 4});
+                                       /*{value, result}=*/{98.0, 0}, /*{value, result}=*/{101.0, 0}, /*{value, result}=*/{4.0, 4});
 }
 
 Y_UNIT_TEST(Overflow) {
     // TO BE TESTED: domain and range are signed and unsigned, vice-verca
     TestHistogramBasic<i32, ui32>(10, /*values range=*/{-5, 25}, /*column range=*/{0, 20}, EHistogramValueType::Uint32,
-                                  /*{value, result}=*/{25, 30}, /*{value, result}=*/{4, 21}, /*{value, result}=*/{4, 1});
+                                  /*{value, result}=*/{25, 21}, /*{value, result}=*/{4, 16}, /*{value, result}=*/{4, 1});
     // UNIT_ASSERT_EXCEPTION((TestHistogramBasic<ui32, i32>(10, /*values range=*/{0, 25}, /*column range=*/{-5, 20}, EHistogramValueType::Int32,
-    //                                                      /*{value, result}=*/{25, 25}, /*{value, result}=*/{4, 22}, /*{value, result}=*/{4, 1})), yexception);
+    //                                                      /*{value, result}=*/{25, 21}, /*{value, result}=*/{4, 17}, /*{value, result}=*/{2, 1})), yexception);
     // UNIT_ASSERT_EXCEPTION((TestHistogramBasic<double, ui32>(10, /*values range=*/{-5.0, 25.0}, /*column range=*/{0, 20}, EHistogramValueType::Uint32,
-    //                                                         /*{value, result}=*/{25.0, 30}, /*{value, result}=*/{4.0, 21}, /*{value, result}=*/{4.0, 1})), yexception);
+    //                                                         /*{value, result}=*/{25.0, 21}, /*{value, result}=*/{4.0, 17}, /*{value, result}=*/{4.0, 2})), yexception);
     // UNIT_ASSERT_EXCEPTION((TestHistogramBasic<ui32, double>(10, /*values range=*/{0, 25}, /*column range=*/{-5.0, 20.0}, EHistogramValueType::Double,
-    //                                                         /*{value, result}=*/{25, 25}, /*{value, result}=*/{4, 22}, /*{value, result}=*/{4, 1})), yexception);
+    //                                                         /*{value, result}=*/{25, 19}, /*{value, result}=*/{4, 13}, /*{value, result}=*/{4, 2})), yexception);
 
     // overflow test
     TestHistogramBasic<i32, i32>(1, /*values range=*/{0, 25}, /*column range=*/{-1, std::numeric_limits<i32>::max()}, EHistogramValueType::Int32,
-                                 /*{value, result}=*/{25, 25}, /*{value, result}=*/{4, 25}, /*{value, result}=*/{4, 0});
+                                 /*{value, result}=*/{25, 0}, /*{value, result}=*/{4, 0}, /*{value, result}=*/{2, 0});
     TestHistogramBasic<i32, i32>(1, /*values range=*/{0, 25},
                                  /*column range=*/{std::numeric_limits<i32>::min(), std::numeric_limits<i32>::max()}, EHistogramValueType::Int32,
-                                 /*{value, result}=*/{25, 25}, /*{value, result}=*/{4, 25}, /*{value, result}=*/{4, 0});
+                                 /*{value, result}=*/{25, 0}, /*{value, result}=*/{4, 0}, /*{value, result}=*/{2, 0});
 
     // TO BE TESTED: down-casting test between types of buckets, domain range, and value range
     // UNIT_ASSERT_EXCEPTION((TestHistogramBasic<i32, i64>(10, /*values range=*/{0, 25}, /*column range=*/{0, std::numeric_limits<i64>::max()}, EHistogramValueType::Int64,
-    //                                                     /*{value, result}=*/{25, 25}, /*{value, result}=*/{4, 21}, /*{value, result}=*/{4, 1})), yexception);
+    //                                                     /*{value, result}=*/{25, 25}, /*{value, result}=*/{4, 21}, /*{value, result}=*/{2, 1})), yexception);
     // UNIT_ASSERT_EXCEPTION((TestHistogramBasic<i16, i64>(10, /*values range=*/{0, 25}, /*column range=*/{0, std::numeric_limits<i64>::max()}, EHistogramValueType::Int64,
-    //                                                     /*{value, result}=*/{25, 25}, /*{value, result}=*/{4, 21}, /*{value, result}=*/{4, 1})), yexception);
-    UNIT_ASSERT_EXCEPTION((TestHistogramBasic<i64, i16>(10, /*values range=*/{0, 25}, /*column range=*/{0, std::numeric_limits<i64>::max()}, EHistogramValueType::Int16,
-                                                        /*{value, result}=*/{25, 25}, /*{value, result}=*/{4, 21}, /*{value, result}=*/{4, 1})), yexception);
+    //                                                     /*{value, result}=*/{25, 25}, /*{value, result}=*/{4, 21}, /*{value, result}=*/{2, 1})), yexception);
+    // UNIT_ASSERT_EXCEPTION((TestHistogramBasic<i64, i16>(10, /*values range=*/{0, 25}, /*column range=*/{0, std::numeric_limits<i16>::max()}, EHistogramValueType::Int16,
+    //                                                     /*{value, result}=*/{25, 0}, /*{value, result}=*/{4, 0}, /*{value, result}=*/{2, 0})), yexception);
 }
 
 Y_UNIT_TEST(Serialization) {
@@ -162,9 +161,9 @@ Y_UNIT_TEST(Serialization) {
 }
 
 Y_UNIT_TEST(AggregateHistogram) {
-    TVector<ui64> resultCountInt{20, 20, 20, 20, 20, 20, 20, 20, 20, 70};
+    TVector<ui64> resultCountInt{20, 20, 20, 20, 20, 20, 20, 20, 20, 30};
     TestHistogramAggregate<ui32, ui32>(10, /*values range=*/{0, 25}, /*column range=*/{0, 20}, EHistogramValueType::Uint32, 9, resultCountInt);
-    TVector<ui64> resultCountFloat{30, 20, 20, 20, 20, 20, 20, 20, 20, 10};
+    TVector<ui64> resultCountFloat{20, 20, 20, 20, 20, 20, 20, 20, 20, 10};
     TestHistogramAggregate<double, double>(10, /*values range=*/{-0.1, 1.5},
                                            /*column range=*/{1.0, 1.0 + 30 * std::numeric_limits<double>::epsilon()},
                                            EHistogramValueType::Double, 9, resultCountFloat);

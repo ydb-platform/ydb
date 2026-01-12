f02476e74af8a585f7a76ac8c2b153ee8b7f3d43
Nikolay Shestakov <tesseract@ydb.tech>
2026-01-12 13:15:28 +0500

    Added support for decimal column type for the transfer (#31720)

--- ydb/core/transfer/ut/common/utils.h
+++ ydb/core/transfer/ut/common/utils.h
@@ -130,6 +130,11 @@ inline ui64 Checker<ui64>::Get(const ::Ydb::Value& value) {
 }
 
 template<>
+inline std::pair<ui64, i64> Checker<std::pair<ui64, i64>>::Get(const ::Ydb::Value& value) {
+    return std::make_pair(value.high_128(), value.low_128());
+}
+
+template<>
 inline double Checker<double>::Get(const ::Ydb::Value& value) {
     return value.double_value();
 }

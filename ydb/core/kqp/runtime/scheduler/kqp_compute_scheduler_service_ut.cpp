22b02b0c2e95ff04dd536fd82e2f0aaf1108f122
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2026-01-13 19:30:00 +0300

    Slightly better tests

--- ydb/core/kqp/runtime/scheduler/kqp_compute_scheduler_service_ut.cpp
+++ ydb/core/kqp/runtime/scheduler/kqp_compute_scheduler_service_ut.cpp
@@ -1,5 +1,7 @@
 #include <ydb/core/kqp/workload_service/ut/common/kqp_workload_service_ut_common.h>
 
+#include <ydb/library/testlib/helpers.h>
+
 namespace NKikimr::NKqp {
 
 using namespace NWorkload;
@@ -7,14 +9,41 @@ using namespace NWorkload;
 Y_UNIT_TEST_SUITE(KqpComputeSchedulerService) {
 
     /* Scenario:
-        - Create Resource Pool with zero CPU.
-        - Disable Scheduler.
-        - Run query inside this Pool.
+        - Create resource pool with zero CPU.
+        - Enable or disable Scheduler on start.
+        - Run query inside this pool.
+        - Query shouldn't timeout or should be cancelled by timeout.
+     */
+    Y_UNIT_TEST_TWIN(FeatureFlagOnStart, Enabled) {
+        auto ydb = TYdbSetupSettings()
+            .EnableResourcePools(true)
+            .EnableResourcePoolsScheduler(Enabled)
+            .Create();
+
+        const TString& poolId = "zero_pool";
+        NResourcePool::TPoolSettings poolSettings;
+        poolSettings.TotalCpuLimitPercentPerNode = 0;
+        poolSettings.QueryCancelAfter = TDuration::Seconds(10);
+        ydb->CreateResourcePool(poolId, poolSettings);
+
+        auto request = ydb->ExecuteQueryAsync(TSampleQueries::TSelect42::Query, TQueryRunnerSettings().PoolId(poolId));
+        if (!Enabled) {
+            TSampleQueries::TSelect42::CheckResult(request.GetResult());
+        } else {
+            TSampleQueries::CheckCancelled(request.GetResult());
+        }
+    }
+
+    /* Scenario:
+        - Create resource pool with zero CPU.
+        - Enable Scheduler on start, but disable Workload Manager.
+        - Run query inside this pool - it should actually go to default pool.
         - Query shouldn't timeout.
      */
-    Y_UNIT_TEST(DisabledByFeatureFlag) {
+    Y_UNIT_TEST(EnabledSchedulerWithDisabledWorkloadManager) {
         auto ydb = TYdbSetupSettings()
-            .EnableResourcePoolsScheduler(false)
+            .EnableResourcePools(false)
+            .EnableResourcePoolsScheduler(true)
             .Create();
 
         const TString& poolId = "zero_pool";

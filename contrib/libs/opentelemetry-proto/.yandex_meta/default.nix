d4f88afefed22aa435331870058f92ccdd4f2c96
robot-contrib <robot-contrib@yandex-team.com>
2026-01-15 14:15:07 +0300

    Update contrib/libs/opentelemetry-proto to 1.9.0 commit_hash:1f1c8aee77265e9d3e17f543cface3ef5fc564a0

--- contrib/libs/opentelemetry-proto/.yandex_meta/default.nix
+++ contrib/libs/opentelemetry-proto/.yandex_meta/default.nix
@@ -1,13 +1,13 @@
 self: super: with self; {
   opentelemetry-proto = stdenv.mkDerivation rec {
     name = "opentelemetry-proto";
-    version = "1.8.0";
+    version = "1.9.0";
 
     src = fetchFromGitHub {
       owner = "open-telemetry";
       repo = "opentelemetry-proto";
       rev = "v${version}";
-      hash = "sha256-5rNJDMjRFIOY/3j+PkAujbippBmxtAudU9busK0q8p0=";
+      hash = "sha256-5ZYu0HE0WRgf/TDmQ6oAwCJAcdnKtdHzDxU2DcmkBcg=";
     };
   };
 }

e2d100e918d26aaef4ac176c1b6ac607f416708f
Irina Skvortsova <nerhneiro@yandex-team.ru>
2026-01-15 07:51:21 +0000

    fix topic description

--- ydb/core/ydb_convert/topic_description.cpp
+++ ydb/core/ydb_convert/topic_description.cpp
@@ -166,7 +166,11 @@ bool FillTopicDescription(Ydb::Topic::DescribeTopicResult& out, const NKikimrSch
     out.set_retention_storage_mb(partConfig.GetStorageLimitBytes() / 1024 / 1024);
     (*out.mutable_attributes())["_message_group_seqno_retention_period_ms"] = TStringBuilder() << (partConfig.GetSourceIdLifetimeSeconds() * 1000);
     (*out.mutable_attributes())["__max_partition_message_groups_seqno_stored"] = TStringBuilder() << partConfig.GetSourceIdMaxCounts();
-    (*out.mutable_attributes())["_timestamp_type"] = TStringBuilder() << config.GetTimestampType();
+    if (config.GetTimestampType()) {
+        (*out.mutable_attributes())["_timestamp_type"] = TStringBuilder() << config.GetTimestampType();
+    } else {
+        (*out.mutable_attributes())["_timestamp_type"] = TStringBuilder() << NKafka::MESSAGE_TIMESTAMP_CREATE_TIME;
+    }
 
     if (local || pqConfig.GetTopicsAreFirstClassCitizen()) {
         out.set_partition_write_speed_bytes_per_second(partConfig.GetWriteSpeedInBytesPerSecond());

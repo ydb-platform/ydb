c03925d6a04584de06679aeb553d9566820f5b8f
Aleksandr Khoroshilov <hor911@ydb.tech>
2026-01-19 18:47:17 +0300

    Small Stage ID fix

--- ydb/library/yql/dq/runtime/dq_channel_service.cpp
+++ ydb/library/yql/dq/runtime/dq_channel_service.cpp
@@ -755,6 +755,12 @@ std::shared_ptr<TLocalBuffer> TLocalBufferRegistry::GetOrCreateLocalBuffer(const
     if (it != LocalBuffers.end()) {
         auto result = it->second.lock();
         if (result) {
+            if (info.SrcStageId) {
+                result->Info.SrcStageId = info.SrcStageId;
+            }
+            if (info.DstStageId) {
+                result->Info.DstStageId = info.DstStageId;
+            }
             return result;
         } else {
             LocalBuffers.erase(it);

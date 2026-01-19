1383416882c6ea2807e8c111b428ffb38ae513a3
Dmitry Kardymon <kardymon-d@ydb.tech>
2026-01-19 07:01:53 +0000

    node kind check

--- ydb/core/config/init/init_impl.h
+++ ydb/core/config/init/init_impl.h
@@ -1166,7 +1166,7 @@ public:
             if (CommonAppOptions.SeedNodesFile) {
                 InitConfigFromSeedNodes(mainYamlConfigString.emplace(), storageYamlConfigString);
                 Y_ABORT_UNLESS(mainYamlConfigString);
-            } else if (!CommonAppOptions.ConfigDirPath.empty()) {
+            } else if (NodeKind != NODE_KIND_YQ) {
                 ythrow yexception() << "YAML config is not provided for static node and no seed nodes given";
             }
         }

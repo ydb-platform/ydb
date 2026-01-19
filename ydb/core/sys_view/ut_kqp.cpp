7951e92fc8eb9fe70fce2712c51802515b1d949a
Yaroslav Dynnikov <ydynnikov@ydb.tech>
2026-01-19 13:15:03 +0300

    Remove InferPDiskSlotCount column from ds_pdisks sysview (#31762)

--- ydb/core/sys_view/ut_kqp.cpp
+++ ydb/core/sys_view/ut_kqp.cpp
@@ -3071,7 +3071,6 @@ R"(CREATE TABLE `test_show_create` (
                     DecommitStatus,
                     ExpectedSlotCount,
                     Guid,
-                    InferPDiskSlotCountFromUnitSize,
                     Kind,
                     NodeId,
                     NumActiveSlots,
@@ -3101,14 +3100,13 @@ R"(CREATE TABLE `test_show_create` (
             }
         }
 
-        TYsonFieldChecker check(ysonString, 19);
+        TYsonFieldChecker check(ysonString, 18);
 
         check.Uint64(0u); // AvailableSize
         check.Uint64(999u); // BoxId
         check.String("DECOMMIT_NONE"); // DecommitStatus
         check.Uint64(16); // ExpectedSlotCount
         check.Uint64(123u); // Guid
-        check.Uint64(0); // InferPDiskSlotCountFromUnitSize
         check.Uint64(0u); // Kind
         check.Uint64(env.GetServer().GetRuntime()->GetNodeId(0)); // NodeId
         check.Uint64(2); // NumActiveSlots

fe42d31fb0a36bc10265d985e8288bbf172397f3
Bulat <bulat@ydb.tech>
2026-01-21 21:08:54 +0300

    [C++ SDK] Fixed session_pool flaky tests (#32478)

--- ydb/public/sdk/cpp/src/client/impl/session/kqp_session_common.h
+++ ydb/public/sdk/cpp/src/client/impl/session/kqp_session_common.h
@@ -12,7 +12,7 @@
 namespace NYdb::inline Dev {
 
 ////////////////////////////////////////////////////////////////////////////////
-ui64 GetNodeIdFromSession(const std::string& sessionId);
+std::uint64_t GetNodeIdFromSession(const std::string& sessionId);
 
 class TKqpSessionCommon;
 
@@ -57,10 +57,6 @@ public:
     TInstant GetTimeToTouchFast() const;
     TInstant GetTimeInPastFast() const;
 
-    // SetTimeInterval/GetTimeInterval, are not atomic!
-    void SetTimeInterval(TDuration interval);
-    TDuration GetTimeInterval() const;
-
     static std::function<void(TKqpSessionCommon*)> GetSmartDeleter(std::shared_ptr<ISessionClient> client);
 
     // Shoult be called under session pool lock
@@ -85,8 +81,6 @@ private:
     // so we need to be able to read this value atomicaly
     std::atomic<TInstant> TimeToTouch_;
     TInstant TimeInPast_;
-    // Is used to implement progressive timeout for settler keep alive call
-    TDuration TimeInterval_;
 
     std::atomic<IServerCloseHandler*> CloseHandler_;
     // Indicate session was in active state, but state was changed

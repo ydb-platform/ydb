fd01a9212510e0e93929a0e0bf92a165e9bc5a2f
Nikolay Shestakov <tesseract@ydb.tech>
2026-01-21 15:38:45 +0500

    Fixed memory leak (#32435)

--- ydb/services/persqueue_v1/actors/schema_actors.h
+++ ydb/services/persqueue_v1/actors/schema_actors.h
@@ -168,6 +168,8 @@ public:
 
     virtual void Reply(const TActorContext& ctx) = 0;
 
+    void PassAway(const TActorContext& ctx);
+
 private:
     std::map<ui64, TTabletInfo> Tablets;
     ui32 RequestsInfly = 0;
@@ -207,6 +209,8 @@ public:
     bool ApplyResponse(TEvPersQueue::TEvGetPartitionsLocationResponse::TPtr& ev, const TActorContext& ctx) override;
     virtual void Reply(const TActorContext& ctx) override;
 
+    void PassAway() override;
+
 private:
     Ydb::Topic::DescribeTopicResult Result;
 };
@@ -234,6 +238,8 @@ public:
     void ApplyResponse(TTabletInfo& tabletInfo, NKikimr::TEvPersQueue::TEvReadSessionsInfoResponse::TPtr& ev, const TActorContext& ctx) override;
     bool ApplyResponse(TEvPersQueue::TEvGetPartitionsLocationResponse::TPtr& ev, const TActorContext& ctx) override;
     virtual void Reply(const TActorContext& ctx) override;
+
+    void PassAway() override;
 
 private:
     Ydb::Topic::DescribeConsumerResult Result;
@@ -263,6 +269,8 @@ public:
     bool ApplyResponse(TEvPersQueue::TEvGetPartitionsLocationResponse::TPtr& ev, const TActorContext& ctx) override;
 
     virtual void Reply(const TActorContext& ctx) override;
+
+    void PassAway() override;
 
 private:
 
@@ -450,6 +458,9 @@ public:
     void Reply(const TActorContext&) override {};
 
     void RaiseError(const TString& error, const Ydb::PersQueue::ErrorCode::ErrorCode errorCode, const Ydb::StatusIds::StatusCode status, const TActorContext&) override;
+
+    void PassAway() override;
+
 private:
     THashSet<ui64> PartitionIds;
 };

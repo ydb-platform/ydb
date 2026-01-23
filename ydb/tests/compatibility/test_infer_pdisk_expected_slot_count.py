012363b5851bc440ff96a4ae2c5037e93f5fc6fe
Yaroslav Dynnikov <ydynnikov@ydb.tech>
2026-01-23 17:31:06 +0300

    Extend compatibility test_infer_pdisk_expected_slot_count

--- ydb/tests/compatibility/test_infer_pdisk_expected_slot_count.py
+++ ydb/tests/compatibility/test_infer_pdisk_expected_slot_count.py
@@ -182,3 +182,95 @@ class TestUpgradeThenRollback(RestartToAnotherVersionFixture):
                 assert pdisk.PDiskMetrics.UpdateTimestamp * 1e-6 > t3
                 assert pdisk.PDiskMetrics.UpdateTimestamp * 1e-6 < deadline
         retry_assertions(check_pdisks, timeout)
+
+    def test_setup_in_blob_storage_config(self):
+        if self.versions[1] < (25, 5):
+            pytest.skip("Unsupported up to version 25.4 included")
+
+        assert self.current_binary_paths_index == 0
+        logger.info(f"Test started on {self.versions[0]} {time.time()=}")
+        #################################################################
+
+        t1 = time.time()
+        timeout = 20
+
+        def check_pdisks():
+            for pdisk in self.pdisk_list():
+                assert pdisk.Path == CONST_PDISK_PATH
+                assert pdisk.DriveStatus == blobstorage_config_pb2.EDriveStatus.ACTIVE
+                assert pdisk.PDiskConfig.ExpectedSlotCount == CONST_INITIAL_SLOT_COUNT
+                assert pdisk.ExpectedSlotCount == CONST_INITIAL_SLOT_COUNT
+                assert pdisk.PDiskMetrics.TotalSize == CONST_480_GB
+                if self.versions[0] < (25, 3):
+                    assert not pdisk.PDiskMetrics.HasField('SlotCount')
+                    assert not pdisk.PDiskMetrics.HasField('SlotSizeInUnits')
+                else:
+                    assert pdisk.PDiskMetrics.SlotCount == CONST_INITIAL_SLOT_COUNT
+                    assert pdisk.PDiskMetrics.HasField('SlotSizeInUnits') and \
+                        pdisk.PDiskMetrics.SlotSizeInUnits == 0
+                assert pdisk.PDiskMetrics.UpdateTimestamp * 1e-6 > t1
+                assert pdisk.PDiskMetrics.UpdateTimestamp * 1e-6 < t1 + timeout
+        retry_assertions(check_pdisks, timeout)
+
+        self.change_cluster_version()
+        assert self.current_binary_paths_index == 1
+        logger.info(f"Restarted on version {self.versions[1]} {time.time()=}")
+        ######################################################################
+
+        t2 = time.time()
+        full_config = self.generate_config()
+        full_config["config"]["blob_storage_config"]["infer_pdisk_slot_count_settings"] = {
+            "rot": {
+                "prefer_inferred_settings_over_explicit": True,
+                "unit_size": CONST_10_GB,
+                "max_slots": 32,
+            }
+        }
+        self.replace_config(full_config)
+        logger.info(f"Inferred PDisk setting applied {time.time()=}")
+
+        self.cluster.client.pdisk_set_all_active(pdisk_path=CONST_PDISK_PATH)
+        logger.info(f"Drives activated {time.time()=}")
+
+        deadline = time.time() + timeout
+
+        def check_pdisks():
+            for pdisk in self.pdisk_list():
+                assert pdisk.Path == CONST_PDISK_PATH
+                assert pdisk.DriveStatus == blobstorage_config_pb2.EDriveStatus.ACTIVE
+                assert pdisk.PDiskConfig.ExpectedSlotCount == CONST_INITIAL_SLOT_COUNT
+                assert pdisk.ExpectedSlotCount == CONST_INITIAL_SLOT_COUNT
+                assert pdisk.PDiskMetrics.TotalSize == CONST_480_GB
+                assert pdisk.PDiskMetrics.SlotCount == 24
+                assert pdisk.PDiskMetrics.SlotSizeInUnits == 2
+                assert pdisk.PDiskMetrics.UpdateTimestamp * 1e-6 > t2
+                assert pdisk.PDiskMetrics.UpdateTimestamp * 1e-6 < deadline
+        retry_assertions(check_pdisks, timeout)
+
+        t3 = time.time()
+        self.change_cluster_version()
+        assert self.current_binary_paths_index == 0
+        logger.info(f"Restarted back on version {self.versions[0]} {time.time()=}")
+        ###########################################################################
+
+        self.cluster.client.pdisk_set_all_active(pdisk_path=CONST_PDISK_PATH)
+        logger.info(f"Drives activated {time.time()=}")
+
+        deadline = time.time() + timeout
+
+        def check_pdisks():
+            for pdisk in self.pdisk_list():
+                assert pdisk.Path == CONST_PDISK_PATH
+                assert pdisk.DriveStatus == blobstorage_config_pb2.EDriveStatus.ACTIVE
+                assert pdisk.PDiskConfig.ExpectedSlotCount == CONST_INITIAL_SLOT_COUNT
+                assert pdisk.ExpectedSlotCount == CONST_INITIAL_SLOT_COUNT
+                assert pdisk.PDiskMetrics.TotalSize == CONST_480_GB
+                if self.versions[0] < (25, 3):
+                    assert not pdisk.PDiskMetrics.HasField('SlotCount')
+                    assert not pdisk.PDiskMetrics.HasField('SlotSizeInUnits')
+                else:
+                    assert pdisk.PDiskMetrics.HasField('SlotCount') and pdisk.PDiskMetrics.SlotCount == CONST_INITIAL_SLOT_COUNT
+                    assert pdisk.PDiskMetrics.HasField('SlotSizeInUnits') and pdisk.PDiskMetrics.SlotSizeInUnits == 0
+                assert pdisk.PDiskMetrics.UpdateTimestamp * 1e-6 > t3
+                assert pdisk.PDiskMetrics.UpdateTimestamp * 1e-6 < deadline
+        retry_assertions(check_pdisks, timeout)

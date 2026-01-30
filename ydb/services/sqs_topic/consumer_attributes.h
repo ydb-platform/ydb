3900a3247e9ce2f3eb073b04c1d996f595e9e642
Aleksei Moiseitsev <swarmer@ydb.tech>
2026-01-30 02:44:33 +0000

    freeze

---
+++ ydb/services/sqs_topic/consumer_attributes.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <ydb/core/protos/pqconfig.pb.h>
+
+#include <util/datetime/base.h>
+#include <util/generic/maybe.h>
+#include <util/generic/string.h>
+
+#include <google/protobuf/map.h>
+
+#include <expected>
+#include <string>
+
+namespace NKikimr::NSqsTopic::V1 {
+
+    struct TConsumerAttributes {
+        NKikimrPQ::TPQTabletConfig::TConsumer Consumer;
+        TMaybe<TDuration> MessageRetentionPeriod;
+        TMaybe<ui32> MaximumMessageSize;
+        TMaybe<TString> KmsMasterKeyId;
+        TMaybe<TDuration> KmsDataKeyReusePeriodSeconds;
+        TMaybe<bool> SqsManagedSseEnabled;
+    };
+
+    enum class EConsumerAtributeUsageTarget {
+        Create,
+        Alter,
+    };
+
+    std::expected<TConsumerAttributes, std::string> ParseConsumerAttributes(
+        const google::protobuf::Map<TString, TString>& attributes,
+        const TString& queueName,
+        const TString& consumerName,
+        const TString& database,
+        EConsumerAtributeUsageTarget usageTarget
+    );
+
+    std::expected<void, std::string> CompareWithExistingQueueAttributes(
+        const NKikimrPQ::TPQTabletConfig& existingConfig,
+        const NKikimrPQ::TPQTabletConfig::TConsumer& existingConsumer,
+        const TConsumerAttributes& newConfig
+    );
+
+    std::expected<void, std::string> ValidateLimits(const TConsumerAttributes& config);
+
+} // namespace NKikimr::NSqsTopic::V1

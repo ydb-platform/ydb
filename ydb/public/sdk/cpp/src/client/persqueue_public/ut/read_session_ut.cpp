39bd130558a09d1fc33e002f40d147d36b6e4b91
Skovorodkin Sergey <qyryq@ydb.tech>
2026-01-30 07:09:51 +0000

    Fix

--- ydb/public/sdk/cpp/src/client/persqueue_public/ut/read_session_ut.cpp
+++ ydb/public/sdk/cpp/src/client/persqueue_public/ut/read_session_ut.cpp
@@ -1860,8 +1860,9 @@ Y_UNIT_TEST_SUITE(ReadSessionImplTest) {
             NTopic::TUserRetrievedEventsInfoAccumulator<true> accumulator; \
 \
             auto event = sessionQueue.GetEventImpl(maxByteSize, accumulator); \
+            UNIT_ASSERT(event); \
 \
-            UNIT_ASSERT(std::holds_alternative<TExpectedEvent>(event.GetEvent()));\
+            UNIT_ASSERT(std::holds_alternative<TExpectedEvent>(event->GetEvent()));\
         }
 
 #define UNIT_ASSERT_DATA_EVENT(count) \
@@ -1872,9 +1873,10 @@ Y_UNIT_TEST_SUITE(ReadSessionImplTest) {
             NTopic::TUserRetrievedEventsInfoAccumulator<true> accumulator; \
 \
             auto event = sessionQueue.GetEventImpl(maxByteSize, accumulator); \
+            UNIT_ASSERT(event); \
 \
-            UNIT_ASSERT(std::holds_alternative<TExpectedEvent>(event.GetEvent())); \
-            UNIT_ASSERT_VALUES_EQUAL(std::get<TExpectedEvent>(event.GetEvent()).GetMessagesCount(), count); \
+            UNIT_ASSERT(std::holds_alternative<TExpectedEvent>(event->GetEvent())); \
+            UNIT_ASSERT_VALUES_EQUAL(std::get<TExpectedEvent>(event->GetEvent()).GetMessagesCount(), count); \
         }
 
         NTopic::TAReadSessionSettings<true> settings;

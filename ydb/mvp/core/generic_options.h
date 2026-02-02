2193964d0f32222f7a486d4bc0141bea21f96f81
StekPerepolnen <arykov@ydb.tech>
2026-02-02 18:08:26 +0100

    remove ports from TGenericOptions

--- ydb/mvp/core/generic_options.h
+++ ydb/mvp/core/generic_options.h
@@ -12,8 +12,6 @@ struct TGenericOptions {
     TString SslCertificateFile;
     bool UseStderr = false;
     bool Mlock = false;
-    ui16 HttpPort = 0;
-    ui16 HttpsPort = 0;
 };
 
 } // namespace NMVP

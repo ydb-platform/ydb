dfc7a7ab483fbc8fb543b2aa39fa1f137219aa62
Kirill Pleshivtsev <drbasic@yandex-team.ru>
2026-02-03 21:33:02 +0700

    Fix name of namespaces #2

--- ydb/core/nbs/cloud/blockstore/libs/storage/direct_block_group/direct_block_group.h
+++ ydb/core/nbs/cloud/blockstore/libs/storage/direct_block_group/direct_block_group.h
@@ -6,7 +6,7 @@
 #include <ydb/core/nbs/cloud/blockstore/libs/storage/api/service.h>
 #include <ydb/core/nbs/cloud/blockstore/libs/storage/direct_block_group/request.h>
 
-namespace NYdb::NBS::NStorage::NPartitionDirect {
+namespace NYdb::NBS::NBlockStore::NStorage::NPartitionDirect {
 
 using namespace NKikimr;
 using namespace NKikimr::NBsController;
@@ -144,4 +144,4 @@ public:
         const TActorContext& ctx);
 };
 
-}   // namespace NYdb::NBS::NStorage::NPartitionDirect
+}   // namespace NYdb::NBS::NBlockStore::NStorage::NPartitionDirect

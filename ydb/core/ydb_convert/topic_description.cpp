5ce69b0656a95aab7e5f102c31b0e3638d58c2a6
Roman Kuzin <kuzin57@yandex-team.ru>
2026-02-04 10:37:13 +0000

    LOGBROKER-10267 Better

--- ydb/core/ydb_convert/topic_description.cpp
+++ ydb/core/ydb_convert/topic_description.cpp
@@ -55,10 +55,10 @@ bool FillConsumer(Ydb::Topic::Consumer& out, const NKikimrPQ::TPQTabletConfig_TC
             shared->mutable_default_processing_timeout()->set_seconds(in.GetDefaultProcessingTimeoutSeconds());
 
             shared->set_content_based_deduplication(in.GetContentBasedDeduplication());
-            shared->mutable_default_message_delay()->set_seconds(in.GetDefaultDelayMessageTimeMs() / 1'000);
-            shared->mutable_default_message_delay()->set_nanos((in.GetDefaultDelayMessageTimeMs() % 1'000) * 1'000'000);
-            shared->mutable_default_receive_message_wait_time()->set_seconds(in.GetDefaultReceiveMessageWaitTimeMs() / 1'000);
-            shared->mutable_default_receive_message_wait_time()->set_nanos((in.GetDefaultReceiveMessageWaitTimeMs() % 1'000) * 1'000'000);
+            shared->mutable_message_delay()->set_seconds(in.GetDefaultDelayMessageTimeMs() / 1'000);
+            shared->mutable_message_delay()->set_nanos((in.GetDefaultDelayMessageTimeMs() % 1'000) * 1'000'000);
+            shared->mutable_receive_message_wait_time()->set_seconds(in.GetDefaultReceiveMessageWaitTimeMs() / 1'000);
+            shared->mutable_receive_message_wait_time()->set_nanos((in.GetDefaultReceiveMessageWaitTimeMs() % 1'000) * 1'000'000);
 
             auto* deadLetterPolicy = shared->mutable_dead_letter_policy();
             deadLetterPolicy->set_enabled(in.GetDeadLetterPolicyEnabled());

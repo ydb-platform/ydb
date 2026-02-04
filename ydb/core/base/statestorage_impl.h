3628f6cef7b16afa6fd9673cb77a48d1022f1fe7
avpershin <avpershin@ydb.tech>
2026-02-04 10:28:15 +0300

    Support explicit follower/leader in DataShard developer UI (#32845)
    * Added support for follower IDs to the State Storage guardian
    * Added support for follower IDs to the State Storage replica
    * Added support for follower IDs to the State Storage proxy
    * Added support for follower IDs to the tablet pipe
    * Added support for follower IDs to the tablet client
    * Added support for follower IDs to the CMS handler for DataShard
    * Added support for follower IDs to the DataShard developer UI
    * Added support for follower IDs to the DataShard UI handler
    * Added support for TEvGetInfoRequest in DataShard followers
    * Added tests to verify follower IDs in the State Storage
    * Added tests to verify follower IDs in the Tablet Resolver, pipe and client
    * Added tests to verify TEvGetInfoRequest in DataShard leaders/followers
    * Added the TNumericalMaybe template
    * Combined all follower arrays in TEvStateStorage::TEvReplicaInfo into a single array

--- ydb/core/base/statestorage_impl.h
+++ ydb/core/base/statestorage_impl.h
@@ -16,7 +16,7 @@ inline TActorId MakeStateStorageReplicaID(ui32 node, ui32 replicaIndex) {
     return TActorId(node, TStringBuf(x, 12));
 }
 
-struct TEvStateStorage::TEvReplicaInfo : public TEventPB<TEvStateStorage::TEvReplicaInfo, NKikimrStateStorage::TEvInfo, TEvStateStorage::EvReplicaInfo> {
+struct TEvStateStorage::TEvReplicaInfo : public TEventPBWithArena<TEvStateStorage::TEvReplicaInfo, NKikimrStateStorage::TEvInfo, TEvStateStorage::EvReplicaInfo> {
     TEvReplicaInfo()
     {}
 

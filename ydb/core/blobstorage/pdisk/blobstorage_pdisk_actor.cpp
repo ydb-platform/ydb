b3ad5526c10a46e483ed8c9047fd98dc3de67610
Evgeniy Ivanov <eivanov89@ydb.tech>
2026-02-04 16:02:03 +0100

    Randomize magic to avoid reading old data #31337

--- ydb/core/blobstorage/pdisk/blobstorage_pdisk_actor.cpp
+++ ydb/core/blobstorage/pdisk/blobstorage_pdisk_actor.cpp
@@ -584,7 +584,6 @@ public:
                     // PDisk GUID is OK and format is complete
                     *PDisk->Mon.PDiskState = NKikimrBlobStorage::TPDiskState::InitialSysLogRead;
                     *PDisk->Mon.PDiskDetailedState = TPDiskMon::TPDisk::BootingSysLogRead;
-                    PDisk->Format.InitMagic();
                     PDisk->ReadSysLog(SelfId());
                 }
             }

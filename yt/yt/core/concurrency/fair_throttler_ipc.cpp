2d0e882ac842bee21342d074c495256884c5f2f6
babenko <babenko@yandex-team.com>
2026-02-07 12:14:15 +0300

    YT-27067: Implement use_shmem flag commit_hash:e63bf7f168ea52d44806837974489971140344b4

--- yt/yt/core/concurrency/fair_throttler_ipc.cpp
+++ yt/yt/core/concurrency/fair_throttler_ipc.cpp
@@ -5,6 +5,10 @@
 #include <yt/yt/core/misc/collection_helpers.h>
 #include <yt/yt/core/misc/fs.h>
 
+#include <yt/yt/core/logging/log.h>
+
+#include <yt/yt/core/crypto/crypto.h>
+
 #include <util/folder/path.h>
 
 #include <util/system/file_lock.h>
@@ -18,6 +22,7 @@ namespace NYT::NConcurrency {
 namespace {
 
 constexpr TStringBuf LockFileName = "lock";
+constexpr TStringBuf BucketLockFileNameSuffix = ".lock";
 constexpr TStringBuf SharedFileName = "shared.v1";
 constexpr TStringBuf BucketsFileName = "buckets.v1";
 
@@ -25,33 +30,195 @@ constexpr TStringBuf BucketsFileName = "buckets.v1";
 
 ////////////////////////////////////////////////////////////////////////////////
 
-class TFairThrottlerFileIpcBucket
-    : public IFairThrottlerIpcBucket
+std::string PathToShmPath(const std::string& path)
+{
+    return std::string("/dev/shm/") +  NCrypto::GetSha256HexDigestLowerCase(path);
+}
+
+std::unique_ptr<TFile> TryOpenFile(const std::string& path)
+{
+    // TODO(babenko): migrate to std::string
+    TFileHandle handle(TString(path), OpenExisting | RdWr);
+    if (!handle.IsOpen()) {
+        if (LastSystemError() == ENOENT) {
+            return nullptr;
+        }
+        THROW_ERROR_EXCEPTION("Error opening %v",
+            path)
+            << TError::FromSystem();
+    }
+    // TODO(babenko): migrate to std::string
+    return std::make_unique<TFile>(handle.Release(), TString(path));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class T>
+class TSharedMemoryRegion
 {
 public:
-    TFairThrottlerFileIpcBucket(const std::string& path, bool create)
-        // TODO(babenko): migrate to std::string
-        : File_(TString(path), OpenAlways | RdWr)
+    TSharedMemoryRegion(
+        std::string path,
+        bool useShmem)
+        : Path_(std::move(path))
     {
-        if (create) {
-            File_.Flock(LOCK_EX | LOCK_NB);
+        // Just create but don't lock yet.
+        // TODO(babenko): migrate to std::string
+        TFile lockFile(TString(GetLockPath()), OpenAlways | RdWr);
+
+        if (useShmem) {
+            auto shmPath = PathToShmPath(Path_);
+            while (true) {
+                DataFile_ = TryOpenFile(Path_);
+                if (DataFile_) {
+                    break;
+                }
+                ShmDataFile_ = std::make_unique<TFile>(shmPath, OpenAlways | RdWr);
+                try {
+                    NFS::MakeSymbolicLink(shmPath, Path_);
+                } catch (...) {
+                    if (LastSystemError() != EEXIST) {
+                        throw;
+                    }
+                    // This could be a race; retry.
+                    ShmDataFile_.reset();
+                }
+            }
+        } else {
+            // TODO(babenko): migrate to std::string
+            DataFile_ = std::make_unique<TFile>(TString(Path_), OpenAlways | RdWr);
         }
 
-        File_.Resize(sizeof(TState));
-
-        Map_ = std::make_unique<TFileMap>(File_, TMemoryMapCommon::oRdWr);
+        DataFile_->Resize(sizeof(T));
+        Map_ = std::make_unique<TFileMap>(*DataFile_, TMemoryMapCommon::oRdWr);
         Map_->Map(0, Map_->Length());
         LockMemory(Map_->Ptr(), Map_->Length());
     }
 
-    TState* GetState() override
+    void Lock()
     {
-        return reinterpret_cast<TState*>(Map_->Ptr());
+        try {
+            // TODO(babenko): migrate to std::string
+            LockedDataFile_ = std::make_unique<TFile>(TString(Path_), OpenExisting | RdWr);
+            LockedDataFile_->Flock(LOCK_EX | LOCK_NB);
+
+            LockedLockFile_ = TryOpenFile(GetLockPath());
+            if (LockedLockFile_) {
+                LockedLockFile_->Flock(LOCK_EX | LOCK_NB);
+            }
+        } catch (...) {
+            LockedDataFile_.reset();
+            LockedLockFile_.reset();
+            throw;
+        }
+    }
+
+    bool TryLock()
+    {
+        try {
+            Lock();
+            return true;
+        } catch (const TSystemError& ex) {
+            if (ex.Status() != EWOULDBLOCK) {
+                throw;
+            }
+            return false;
+        }
+    }
+
+    T* Get()
+    {
+        return reinterpret_cast<T*>(Map_->Ptr());
     }
 
 private:
-    TFile File_;
+    const std::string Path_;
+
+    std::unique_ptr<TFile> DataFile_;
+    std::unique_ptr<TFile> ShmDataFile_;
     std::unique_ptr<TFileMap> Map_;
+
+    std::unique_ptr<TFile> LockedDataFile_;
+    std::unique_ptr<TFile> LockedLockFile_;
+
+    std::string GetLockPath() const
+    {
+        return Path_ + std::string(BucketLockFileNameSuffix);
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool TryRemoveOrphanedMemoryRegion(const std::string& path)
+{
+    enum EResult
+    {
+        Missing,
+        StillLocked,
+        Removed,
+    };
+
+    auto tryLockAndRemove = [&] (const std::string& path) {
+        auto file = TryOpenFile(path);
+        if (!file) {
+            return EResult::Missing;
+        }
+
+        if (Flock(file->GetHandle(), LOCK_EX | LOCK_NB) != 0) {
+            return EResult::StillLocked;
+        }
+
+        NFS::Remove(path);
+        return EResult::Removed;
+    };
+
+    auto lockRemovalResult = tryLockAndRemove(path + std::string(BucketLockFileNameSuffix));
+    if (lockRemovalResult == EResult::StillLocked) {
+        return false;
+    }
+
+    auto dataRemovalResult = tryLockAndRemove(path);
+
+#ifdef __linux__
+    if (lockRemovalResult == EResult::Removed) {
+        auto shmPath = PathToShmPath(path);
+        try {
+            NFS::Remove(shmPath);
+        } catch (...) {
+            // Ignore.
+        }
+    }
+#endif
+
+    return
+        lockRemovalResult == EResult::Removed ||
+        dataRemovalResult == EResult::Removed;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TFairThrottlerFileIpcBucket
+    : public IFairThrottlerIpcBucket
+{
+public:
+    TFairThrottlerFileIpcBucket(
+        const std::string& path,
+        bool create,
+        bool useShmem)
+        : Region_(path, useShmem)
+    {
+        if (create) {
+            Region_.Lock();
+        }
+    }
+
+    TState* GetState() override
+    {
+        return Region_.Get();
+    }
+
+private:
+    TSharedMemoryRegion<TState> Region_;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -60,39 +227,32 @@ class TFairThrottlerFileIpc
     : public IFairThrottlerIpc
 {
 public:
-    explicit TFairThrottlerFileIpc(const std::string& path)
-        : Path_(path)
+    TFairThrottlerFileIpc(
+        const std::string& rootPath,
+        bool useShmem,
+        NLogging::TLogger logger)
+        : RootPath_(rootPath)
+        , UseShmem_(useShmem)
+        , Logger(std::move(logger))
     {
-        NFS::MakeDirRecursive(Path_);
-        NFS::MakeDirRecursive(Path_ + "/" + BucketsFileName);
+        NFS::MakeDirRecursive(RootPath_);
+        NFS::MakeDirRecursive(RootPath_ + "/" + BucketsFileName);
 
-        Lock_ = std::make_unique<TFileLock>(Path_ + "/" + LockFileName);
+        Lock_ = std::make_unique<TFileLock>(RootPath_ + "/" + LockFileName);
 
-        SharedBucketFile_ = TFile(Path_ + "/" + SharedFileName, OpenAlways | RdWr);
-        SharedBucketFile_.Resize(sizeof(TSharedState));
-
-        SharedBucketMap_ = std::make_unique<TFileMap>(SharedBucketFile_, TMemoryMapCommon::oRdWr);
-        SharedBucketMap_->Map(0, SharedBucketMap_->Length());
-        LockMemory(SharedBucketMap_->Ptr(), SharedBucketMap_->Length());
+        Region_ = std::make_unique<TSharedMemoryRegion<TSharedState>>(
+            RootPath_ + "/" + SharedFileName,
+            UseShmem_);
     }
 
     bool TryLock() override
     {
-        try {
-            SharedBucketFile_.Flock(LOCK_EX | LOCK_NB);
-            return true;
-        } catch (const TSystemError& ex) {
-            if (ex.Status() != EWOULDBLOCK) {
-                throw;
-            }
-
-            return false;
-        }
+        return Region_->TryLock();
     }
 
     TSharedState* GetState() override
     {
-        return reinterpret_cast<TSharedState*>(SharedBucketMap_->Ptr());
+        return Region_->Get();
     }
 
     std::vector<IFairThrottlerIpcBucketPtr> ListBuckets() override
@@ -118,17 +278,23 @@ public:
         auto fileName = ToString(id);
         auto bucketPath = GetBucketPath(fileName);
         OwnedBucketNames_.insert(fileName);
-        return New<TFairThrottlerFileIpcBucket>(bucketPath, /*create*/ true);
+
+        YT_LOG_DEBUG("Bucket created (Name: %v)", fileName);
+
+        return New<TFairThrottlerFileIpcBucket>(
+            bucketPath,
+            /*create*/ true,
+            UseShmem_);
     }
 
 private:
     // TODO(babenko): migrate to std::string
-    const TString Path_;
+    const TString RootPath_;
+    const bool UseShmem_;
+    const NLogging::TLogger Logger;
 
     std::unique_ptr<TFileLock> Lock_;
-
-    TFile SharedBucketFile_;
-    std::unique_ptr<TFileMap> SharedBucketMap_;
+    std::unique_ptr<TSharedMemoryRegion<TSharedState>> Region_;
 
     THashMap<std::string, IFairThrottlerIpcBucketPtr> OpenBuckets_;
     THashSet<std::string> OwnedBucketNames_;
@@ -138,17 +304,19 @@ private:
         auto openBuckets = std::exchange(OpenBuckets_, {});
 
         TVector<TString> currentBucketPaths;
-        TFsPath{Path_ + "/" + BucketsFileName}.ListNames(currentBucketPaths);
+        TFsPath{RootPath_ + "/" + BucketsFileName}.ListNames(currentBucketPaths);
         for (const auto& fileName : currentBucketPaths) {
+            if (fileName.EndsWith(BucketLockFileNameSuffix)) {
+                continue;
+            }
             if (OwnedBucketNames_.contains(fileName)) {
                 continue;
             }
 
             try {
                 auto bucketPath = GetBucketPath(fileName);
-                TFileLock bucketLock(bucketPath);
-                if (bucketLock.TryAcquire()) {
-                    NFS::Remove(bucketPath);
+                if (TryRemoveOrphanedMemoryRegion(bucketPath)) {
+                    YT_LOG_DEBUG("Orphaned bucket removed (Name: %v)", fileName);
                     continue;
                 }
 
@@ -157,9 +325,15 @@ private:
                     continue;
                 }
 
-                auto bucket = New<TFairThrottlerFileIpcBucket>(bucketPath, /*create*/ false);
+                YT_LOG_DEBUG("Bucket found (Name: %v)", fileName);
+
+                auto bucket = New<TFairThrottlerFileIpcBucket>(
+                    bucketPath,
+                    /*create*/ false,
+                    UseShmem_);
                 EmplaceOrCrash(OpenBuckets_, fileName, bucket);
-            } catch (const TSystemError& ex) {
+            } catch (const std::exception& ex) {
+                YT_LOG_DEBUG(ex, "Error reloading throttler IPC; ignored");
                 continue;
             }
         }
@@ -167,15 +341,21 @@ private:
 
     TString GetBucketPath(const std::string& fileName) const
     {
-        return Path_ + "/" + BucketsFileName + "/" + fileName;
+        return RootPath_ + "/" + BucketsFileName + "/" + fileName;
     }
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
-IFairThrottlerIpcPtr CreateFairThrottlerFileIpc(const std::string& path)
+IFairThrottlerIpcPtr CreateFairThrottlerFileIpc(
+    const std::string& rootPath,
+    bool useShmem,
+    NLogging::TLogger logger)
 {
-    return New<TFairThrottlerFileIpc>(path);
+    return New<TFairThrottlerFileIpc>(
+        rootPath,
+        useShmem,
+        std::move(logger));
 }
 
 ////////////////////////////////////////////////////////////////////////////////

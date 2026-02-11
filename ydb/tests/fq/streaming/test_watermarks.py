bd617003f268e8828c83cc8eb15930b85d83422c
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-02-11 13:17:00 +0300

    sync ca: implement idleness watermarks in channels (#31920)
    (cherry picked from commit d978c14cb1f55cf72b8b37735eccae757818bb3b)
    
    Fixed Conflicts:
    	ydb/tests/fq/streaming/test_watermarks.py
    	ydb/tests/tools/datastreams_helpers/test_yds_base.py

--- ydb/tests/fq/streaming/test_watermarks.py
+++ ydb/tests/fq/streaming/test_watermarks.py
@@ -10,15 +10,17 @@ logger = logging.getLogger(__name__)
 
 class TestWatermarksInYdb(StreamingTestBase):
     @pytest.mark.parametrize("kikimr", [{"enable_watermarks": True}], indirect=["kikimr"])
-    @pytest.mark.parametrize("shared_reading", [False], ids=["no_shared"])
-    @pytest.mark.parametrize("tasks", [1])
+    @pytest.mark.parametrize("shared_reading", [False, True], ids=["no_shared", "shared"])
+    @pytest.mark.parametrize("tasks", [1, 2])
     def test_watermarks(self: StreamingTestBase, kikimr: Kikimr, entity_name: Callable[[str], str], shared_reading: bool, tasks: int) -> None:
-        query_name = f"test_watermarks_{shared_reading}_{tasks}"
+        query_name = f"test_watermarks_{shared_reading}{tasks}{local_topics}"
         source_name = entity_name(query_name)
         self.init_topics(source_name, partitions_count=tasks)
         self.create_source(kikimr, source_name, shared_reading)
 
         ts = "CAST(ts AS Timestamp)" if shared_reading else "SystemMetadata('write_time')"
+        idleness_clause = ', WATERMARK_IDLE_TIMEOUT = "PT5S"' if tasks > 1 else ''
+
         sql = f'''
             CREATE STREAMING QUERY `{query_name}` AS
             DO BEGIN
@@ -37,8 +39,8 @@ class TestWatermarksInYdb(StreamingTestBase):
                             pass Uint64
                         )
                         , WATERMARK AS ({ts} - Interval('PT5S'))
-                        , WATERMARK_IDLE_TIMEOUT = "PT10S"
                         , WATERMARK_GRANULARITY = "PT1S"
+                        {idleness_clause}
                     )
                 );
 
@@ -73,11 +75,15 @@ class TestWatermarksInYdb(StreamingTestBase):
         kikimr.ydb_client.query(sql)
         self.wait_completed_checkpoints(kikimr, f"/Root/{query_name}")
 
-        self.write_stream(['{"ts": "1970-01-01T00:00:40Z", "pass": 1}'])
-        time.sleep(10)
-        self.write_stream(['{"ts": "1970-01-01T00:00:50Z", "pass": 1}'])
-        time.sleep(10)
-        self.write_stream(['{"ts": "1970-01-01T00:01:00Z", "pass": 0}'])
+        self.write_stream(['{"ts": "1970-01-01T00:00:40Z", "pass": 1}'], partition_key=b'1')
+        if not shared_reading:
+            time.sleep(10)
+        self.write_stream(['{"ts": "1970-01-01T00:00:50Z", "pass": 1}'], partition_key=b'1')
+        if not shared_reading:
+            time.sleep(10)
+        self.write_stream(['{"ts": "1970-01-01T00:01:00Z", "pass": 0}'], partition_key=b'1')
+        if shared_reading and tasks > 1:
+            time.sleep(10)  # leave a bit more time to fire up idle timeout
 
         expected = [
             '[["1970-01-01T00:00:40Z"]]',

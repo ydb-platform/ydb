0fa96c3ef26105ea1e1789379b3a7b7f4fff4e9b
Aleksei Pozdniakov <vokayndzop@ydb.tech>
2026-02-11 12:45:25 +0000

    init

--- ydb/library/yql/providers/pq/provider/yql_pq_logical_opt.cpp
+++ ydb/library/yql/providers/pq/provider/yql_pq_logical_opt.cpp
@@ -53,6 +53,18 @@ std::unordered_set<TString> GetUsedColumnNames(const TCoExtractMembers& extractM
     return usedColumnNames;
 }
 
+void GetUsedWatermarkColumnNames(const TExprBase& expr, std::unordered_set<TString>& result) {
+    if (const auto maybeMember = expr.Maybe<TCoMember>()) {
+        const auto member = maybeMember.Cast();
+        result.insert(member.Name().StringValue());
+        return;
+    }
+
+    for (const auto& child : expr.Raw()->Children()) {
+        GetUsedWatermarkColumnNames(TExprBase(child), result);
+    }
+}
+
 TVector<TCoNameValueTuple> DropUnusedMetadata(const TPqTopic& pqTopic, const std::unordered_set<TString>& usedColumnNames) {
     TVector<TCoNameValueTuple> newSourceMetadata;
     for (auto metadataItem : pqTopic.Metadata()) {
@@ -193,6 +205,22 @@ public:
         const auto& pqTopic = dqPqTopicSource.Topic();
 
         auto usedColumnNames = GetUsedColumnNames(extractMembers);
+        if (const auto maybeWatermark = dqPqTopicSource.Watermark()) {
+            const auto watermark = maybeWatermark.Cast();
+            GetUsedWatermarkColumnNames(watermark, usedColumnNames);
+        }
+
+        TStringBuilder log;
+        log << TInstant::Now() << " [TODO] PQ COLUMN NAMES: {";
+        for (bool isFirst = true; const auto& name : usedColumnNames) {
+            if (!std::exchange(isFirst, false)) {
+                log << ", ";
+            }
+            log << name;
+        }
+        log << "}\n";
+        Cerr << log;
+
         const TStructExprType* inputRowType = pqTopic.RowSpec().Ref().GetTypeAnn()->Cast<TTypeExprType>()->GetType()->Cast<TStructExprType>();
         const TStructExprType* outputRowType = node.Ref().GetTypeAnn()->Cast<TListExprType>()->GetItemType()->Cast<TStructExprType>();
         if (outputRowType->GetSize() == 0 && inputRowType->GetSize() > 0) {

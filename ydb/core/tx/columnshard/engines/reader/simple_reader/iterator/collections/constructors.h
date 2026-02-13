7ecd240dd7861f33409be92183dfd56c837ca053
Iurii Kravchenko <xjie6@ydb.tech>
2026-02-13 14:54:56 +0100

    Cursor fix (#34137)

--- ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/collections/constructors.h
+++ ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/collections/constructors.h
@@ -65,15 +65,17 @@ private:
         ui64 compactedPortionsBytes = 0;
         ui64 insertedPortionsBytes = 0;
         ui64 committedPortionsBytes = 0;
-        for (auto&& i : TBase::GetConstructors()) {
-            if (i.GetPortion()->GetPortionType() == EPortionType::Compacted) {
-                compactedPortionsBytes += i.GetPortion()->GetTotalBlobBytes();
-            } else if (i.GetPortion()->GetProduced() == NPortion::EProduced::INSERTED) {
-                insertedPortionsBytes += i.GetPortion()->GetTotalBlobBytes();
+
+        TBase::ForEachConstructor([&](const TSourceConstructor& constructor) {
+            if (constructor.GetPortion()->GetPortionType() == EPortionType::Compacted) {
+                compactedPortionsBytes += constructor.GetPortion()->GetTotalBlobBytes();
+            } else if (constructor.GetPortion()->GetProduced() == NPortion::EProduced::INSERTED) {
+                insertedPortionsBytes += constructor.GetPortion()->GetTotalBlobBytes();
             } else {
-                committedPortionsBytes += i.GetPortion()->GetTotalBlobBytes();
+                committedPortionsBytes += constructor.GetPortion()->GetTotalBlobBytes();
             }
-        }
+        });
+
         stats.IndexPortions = TBase::GetConstructorsCount();
         stats.InsertedPortionsBytes = insertedPortionsBytes;
         stats.CompactedPortionsBytes = compactedPortionsBytes;

6e6424c006b910e5edf5d6d1fe301a06ab3417d1
Velikhov Pavel <pavelvelikhov@localhost.localdomain>
2026-02-13 16:28:05 +0000

    [NEW RBO] Refactored optimizer rules

---
+++ ydb/core/kqp/opt/rbo/rules/extract_join_expressions.cpp
@@ -0,0 +1,52 @@
+#include "kqp_rules_include.h"
+
+namespace NKikimr {
+namespace NKqp {
+
+// Currently we only extract simple expressions where there is only one variable on either side
+
+bool TExtractJoinExpressionsRule::MatchAndApply(std::shared_ptr<IOperator> &input, TRBOContext &ctx, TPlanProps &props) {
+    Y_UNUSED(props);
+
+    if (input->Kind != EOperator::Filter) {
+        return false;
+    }
+
+    auto filter = CastOperator<TOpFilter>(input);
+    auto conjuncts = filter->FilterExpr.SplitConjunct();
+
+    TVector<TExpression> newConjuncts;
+    TVector<TMapElement> mapElements;
+
+    for (auto & c : conjuncts) {
+        if (c.MaybeJoinCondition(false)) {
+            newConjuncts.push_back(c);
+        }
+        else if (c.MaybeJoinCondition(true)) {
+            TJoinCondition cond(c);
+            TVector<std::pair<TInfoUnit, TExprNode::TPtr>> renameMap;
+            TNodeOnNodeOwnedMap replaceMap;
+            if (cond.ExtractExpressions(replaceMap, renameMap)) {
+                for (auto const & [iu, expr] : renameMap) {
+                    mapElements.emplace_back(iu, TExpression(expr, &ctx.ExprCtx, &props));
+                }
+                newConjuncts.push_back(c.ApplyReplaceMap(replaceMap, ctx));
+            } else {
+                newConjuncts.push_back(c);
+            }
+        } else {
+            newConjuncts.push_back(c);
+        }
+    }
+
+    if (mapElements.empty()) {
+        return false;
+    }
+
+    filter->FilterExpr = MakeConjunction(newConjuncts, props.PgSyntax);
+    auto newMap = std::make_shared<TOpMap>(filter->GetInput(), input->Pos, mapElements, false);
+    filter->SetInput(newMap);
+    return true;
+}
+}
+}
---
+++ ydb/core/kqp/opt/rbo/rules/push_filter_under_map.cpp
@@ -0,0 +1,59 @@
+#include "kqp_rules_include.h"
+
+namespace NKikimr {
+namespace NKqp {
+
+std::shared_ptr<IOperator> TPushFilterUnderMapRule::SimpleMatchAndApply(const std::shared_ptr<IOperator> &input, TRBOContext &ctx, TPlanProps &props) {
+
+    Y_UNUSED(ctx);
+    Y_UNUSED(props);
+
+    if (input->Kind != EOperator::Filter) {
+        return input;
+    }
+
+    auto filter = CastOperator<TOpFilter>(input);
+    if (filter->GetInput()->Kind != EOperator::Map) {
+        return input;
+    }
+
+    auto map = CastOperator<TOpMap>(filter->GetInput());
+
+    if (map->Project) {
+        return input;
+    }
+
+    auto conjuncts = filter->FilterExpr.SplitConjunct();
+    TVector<TExpression> pushedFilters;
+    TVector<TExpression> remainingFilters;
+
+    TVector<TInfoUnit> newMapColumns;
+    for (const auto & mapEl : map->MapElements) {
+        newMapColumns.push_back(mapEl.GetElementName());
+    }
+
+    for (const auto & c : conjuncts) {
+        if (IUSetIntersect(c.GetInputIUs(false,true), newMapColumns).empty()){
+            pushedFilters.push_back(c);
+        } else {
+            remainingFilters.push_back(c);
+        }
+    }
+
+    if (pushedFilters.empty()) {
+        return input;
+    }
+
+    filter->SetInput(map->GetInput());
+    filter->FilterExpr = MakeConjunction(pushedFilters, props.PgSyntax);
+    map->SetInput(filter);
+
+    if (remainingFilters.size()) {
+        auto pushedFilterExpr = MakeConjunction(remainingFilters, props.PgSyntax);
+        return std::make_shared<TOpFilter>(map, map->Pos, pushedFilterExpr);
+    } else {
+        return map;
+    }
+}
+}
+}

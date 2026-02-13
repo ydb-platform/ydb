65c4d3a2aa304b7357fb9ef3b55b5bb8fda367ed
Aleksei Moiseitsev <swarmer@ydb.tech>
2026-02-13 10:59:59 +0000

    Pass AdvancedMonitoringSettings

--- ydb/public/sdk/cpp/src/client/persqueue_public/include/control_plane.h
+++ ydb/public/sdk/cpp/src/client/persqueue_public/include/control_plane.h
@@ -114,6 +114,8 @@ struct TDescribeTopicResult : public TStatus {
         GETTER(std::optional<std::string>, AbcSlug);
         GETTER(std::optional<std::string>, FederationAccount);
         GETTER(std::optional<ui32>, MetricsLevel);
+        GETTER(std::string, AdvancedMonitoringSettings);
+
 
         const std::vector<TReadRule>& ReadRules() const {
             return ReadRules_;
@@ -148,6 +150,7 @@ struct TDescribeTopicResult : public TStatus {
         std::optional<std::string> AbcSlug_;
         std::string FederationAccount_;
         std::optional<ui32> MetricsLevel_;
+        std::string AdvancedMonitoringSettings_;
 
         std::optional<uint64_t> MaxPartitionsCount_;
         std::optional<TDuration> StabilizationWindow_;
@@ -254,6 +257,7 @@ struct TTopicSettings : public TOperationRequestSettings<TDerived> {
     FLUENT_SETTING_OPTIONAL(std::string, AbcSlug);
     FLUENT_SETTING_OPTIONAL(std::string, FederationAccount);
     FLUENT_SETTING_OPTIONAL(ui32, MetricsLevel);
+    FLUENT_SETTING(std::string, AdvancedMonitoringSettings);
 
     //TODO: FLUENT_SETTING_VECTOR
     FLUENT_SETTING_DEFAULT(std::vector<TReadRuleSettings>, ReadRules, {});
@@ -279,6 +283,8 @@ struct TTopicSettings : public TOperationRequestSettings<TDerived> {
         AbcSlug_ = settings.AbcSlug();
         FederationAccount_ = settings.FederationAccount();
         MetricsLevel_ = settings.MetricsLevel();
+        AdvancedMonitoringSettings_ = settings.AdvancedMonitoringSettings();
+
         ReadRules_.clear();
         for (const auto& readRule : settings.ReadRules()) {
             ReadRules_.push_back({});

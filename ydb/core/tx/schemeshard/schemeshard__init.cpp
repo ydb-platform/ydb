f292d4ec154329b1b4ead438038860c667e7c006
Innokentii Mokin <innokentii@ydb.tech>
2026-02-17 01:54:35 +0300

    fix: copy table dropped handling (#34206)

--- ydb/core/tx/schemeshard/schemeshard__init.cpp
+++ ydb/core/tx/schemeshard/schemeshard__init.cpp
@@ -3576,7 +3576,9 @@ struct TSchemeShard::TTxInit : public TTransactionBase<TSchemeShard> {
                     Y_VERIFY_S(srcPath, "Null path element, pathId: " << txState.SourcePathId);
 
                     // CopyTable source must not be altered or dropped while the Tx is in progress
-                    srcPath->PathState = TPathElement::EPathState::EPathStateCopying;
+                    if (!srcPath->Dropped()) {
+                        srcPath->PathState = TPathElement::EPathState::EPathStateCopying;
+                    }
                     srcPath->DbRefCount++;
                 }
 
@@ -3825,7 +3827,9 @@ struct TSchemeShard::TTxInit : public TTransactionBase<TSchemeShard> {
                         Y_VERIFY_S(srcPath, "Null path element, pathId: " << txState->SourcePathId);
 
                         // CopyTable source must not be altered or dropped while the Tx is in progress
-                        srcPath->PathState = TPathElement::EPathState::EPathStateCopying;
+                        if (!srcPath->Dropped()) {
+                            srcPath->PathState = TPathElement::EPathState::EPathStateCopying;
+                        }
                         srcPath->DbRefCount++;
                     }
                 }

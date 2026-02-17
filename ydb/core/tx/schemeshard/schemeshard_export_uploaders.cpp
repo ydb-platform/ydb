393ffbbd0136190b55674b5370a01ada7df688d9
st-shchetinin <st-shchetinin@ydb.tech>
2026-02-17 11:44:58 +0000

    fix

--- ydb/core/tx/schemeshard/schemeshard_export_uploaders.cpp
+++ ydb/core/tx/schemeshard/schemeshard_export_uploaders.cpp
@@ -52,13 +52,13 @@ protected:
     }
 
     static constexpr NKikimrServices::TActivity::EType ActorActivityType() {
-        if constexpr (std::is_same_v<TSettings, Ydb::Export::ExportToS3Settings>) {
+        if constexpr (std::is_same_v<TSettings, NKikimrSchemeOp::TS3Settings>) {
             return NKikimrServices::TActivity::EXPORT_S3_UPLOADER_ACTOR;
-        } else if constexpr (std::is_same_v<TSettings, Ydb::Export::ExportToFsSettings>) {
+        } else if constexpr (std::is_same_v<TSettings, NKikimrSchemeOp::TFSSettings>) {
             return NKikimrServices::TActivity::EXPORT_FS_UPLOADER_ACTOR;
-        } else {
-            Y_ABORT("Unsupported storage type in backup task");
         }
+
+        Y_ABORT("Unsupported storage type in backup task");
     }
 
     // Adds a file to queue.

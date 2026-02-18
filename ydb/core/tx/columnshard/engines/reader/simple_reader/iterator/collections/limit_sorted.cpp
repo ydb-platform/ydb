a852eb0f1fd1c4c16f07004cb1025aee372fb5c1
Vladilen Muzychenko <vladilenmuz@ydb.tech>
2026-02-18 13:04:02 +0000

    Add limit sorted in flight limit config

--- ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/collections/limit_sorted.cpp
+++ ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/collections/limit_sorted.cpp
@@ -36,7 +36,7 @@ std::shared_ptr<NCommon::IDataSource> TScanWithLimitCollection::DoTryExtractNext
 void TScanWithLimitCollection::DoOnSourceFinished(const std::shared_ptr<NCommon::IDataSource>& source) {
     AFL_DEBUG(NKikimrServices::TX_COLUMNSHARD_SCAN)("event", "DoOnSourceFinished")("source_idx", source->GetSourceIdx())("limit", Limit)(
         "max", GetMaxInFlight())("in_flight_limit", InFlightLimit)("count", GetSourcesInFlightCount());
-    if (!source->GetAs<IDataSource>()->GetResultRecordsCount() && InFlightLimit < GetMaxInFlight()) {
+    if (source->GetAs<IDataSource>()->GetResultRecordsCount() < Limit && InFlightLimit < GetMaxInFlight()) {
         InFlightLimit = 2 * InFlightLimit;
     }
     AFL_VERIFY(Cleared || Aborted || GetSourcesInFlightCount() == FetchingInFlightSources.size())("in_flight", GetSourcesInFlightCount())(
@@ -48,6 +48,9 @@ TScanWithLimitCollection::TScanWithLimitCollection(
     const std::shared_ptr<TSpecialReadContext>& context, std::unique_ptr<NCommon::ISourcesConstructor>&& sourcesConstructor)
     : TBase(context, std::move(sourcesConstructor))
     , Limit((ui64)Context->GetCommonContext()->GetReadMetadata()->GetLimitRobust()) {
+    if (HasAppData() && AppData()->ColumnShardConfig.HasLimitSortedStartInFlightLimit()) {
+        InFlightLimit = AppData()->ColumnShardConfig.GetLimitSortedStartInFlightLimit();
+    }
 }
 
 }   // namespace NKikimr::NOlap::NReader::NSimple

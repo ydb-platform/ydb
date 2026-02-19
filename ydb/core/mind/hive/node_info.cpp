365cf3b8c5b56ff74485493fe6d1f2d36fbb38e0
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2026-02-19 22:33:14 +0300

    Fix

--- ydb/core/mind/hive/node_info.cpp
+++ ydb/core/mind/hive/node_info.cpp
@@ -365,8 +365,8 @@ void TNodeInfo::RegisterInDomains() {
 
 void TNodeInfo::DeregisterInDomains() {
     Hive.DomainsView.DeregisterNode(*this);
-    LastSeenServicedDomains = std::move(ServicedDomains); // clear ServicedDomains
     Hive.RemoveNodeFromSegments(Id);
+    LastSeenServicedDomains = std::move(ServicedDomains); // clear ServicedDomains
     Hive.UpdateNodeSegments(this);
 }
 

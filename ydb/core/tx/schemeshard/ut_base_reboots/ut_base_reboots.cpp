1d1812ddd0f6369f9343d7253c83fdc345057cae
Ilnaz Nizametdinov <ilnaz@ydb.tech>
2026-02-20 11:05:33 +0300

    Split tests with reboots, p.12 (#34532)

--- ydb/core/tx/schemeshard/ut_base_reboots/ut_base_reboots.cpp
+++ ydb/core/tx/schemeshard/ut_base_reboots/ut_base_reboots.cpp
@@ -786,7 +786,7 @@ Y_UNIT_TEST_SUITE(TTablesWithReboots) {
         });
     }
 
-    Y_UNIT_TEST_WITH_REBOOTS_BUCKETS(CopyTableAndDropWithReboots2, 2, 1, false) {
+    Y_UNIT_TEST_WITH_REBOOTS_BUCKETS(CopyTableAndDropWithReboots2, 4, 1, false) {
         t.Run([&](TTestActorRuntime& runtime, bool& activeZone) {
             {
                 TInactiveZone inactive(activeZone);

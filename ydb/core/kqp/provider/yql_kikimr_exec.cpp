a4f1edc221217a3ef5b9d4d822275a2e3a2e1c9a
Stanislav Yablonskiy <neyrox@ydb.tech>
2026-02-25 12:24:58 +0000

    ParseCompressionSettings on ADD COLUMN

--- ydb/core/kqp/provider/yql_kikimr_exec.cpp
+++ ydb/core/kqp/provider/yql_kikimr_exec.cpp
@@ -1623,12 +1623,12 @@ public:
 };
 
 bool ParseCompressionSettings(
-    const TExprList& alterColumnList,
-    Ydb::Table::ColumnMeta* alter_columns,
+    const TExprList& columnSource,
+    Ydb::Table::ColumnMeta* columnDest,
     TExprContext& ctx) {
 
-    auto compression = alter_columns->mutable_compression();
-    const auto settings = alterColumnList.Item(1).Cast<TExprList>();
+    auto compression = columnDest->mutable_compression();
+    const auto settings = columnSource.Item(1).Cast<TExprList>();
     for (const auto setting : settings) {
         const auto sKV = setting.Cast<TExprList>();
         const auto key = sKV.Item(0).Cast<TCoAtom>().Value();
@@ -1650,7 +1650,7 @@ bool ParseCompressionSettings(
             compression->set_compression_level(FromString<i64>(settingVal.Child(0)->Content()));
         } else {
             ctx.AddError(TIssue(ctx.GetPosition(sKV.Pos()), TStringBuilder()
-                << " Column: \"" << alter_columns->name()
+                << " Column: \"" << columnDest->name()
                 << "\". Setting: \"" << key
                 << "\". Only algorithm and level settings supported for column COMPRESSION"));
             return false;
@@ -1991,20 +1991,32 @@ public:
                         }
 
                         if (columnTuple.Size() > 3) {
-                            auto families = columnTuple.Item(3).Cast<TCoAtomList>();
-                            if (families.Size() > 1) {
-                                ctx.AddError(TIssue(ctx.GetPosition(families.Pos()),
-                                    "Unsupported number of families"));
-                                return SyncError();
-                            }
+                            auto columnItem = columnTuple.Item(3);
+                            if (columnItem.Maybe<TExprList>()) {
 
-                            for (auto family : families) {
-                                add_column->set_family(TString(family.Value()));
-                            }
 
-                            if (columnBuild) {
-                                for (auto family : families) {
-                                    columnBuild->SetFamily(TString(family.Value()));
+                                const auto exprs = columnItem.Cast<TExprList>();
+                                if (exprs.Size() > 1 && exprs.Item(0).Cast<TCoAtom>().Value() == "columnCompression") {
+                                    if (!ParseCompressionSettings(exprs, add_column, ctx)) {
+                                        return SyncError();
+                                    }
+                                } else {
+                                    auto families = columnItem.Cast<TCoAtomList>();
+                                    if (families.Size() > 1) {
+                                        ctx.AddError(TIssue(ctx.GetPosition(families.Pos()),
+                                            "Unsupported number of families"));
+                                        return SyncError();
+                                    }
+
+                                    for (auto family : families) {
+                                        add_column->set_family(TString(family.Value()));
+                                    }
+
+                                    if (columnBuild) {
+                                        for (auto family : families) {
+                                            columnBuild->SetFamily(TString(family.Value()));
+                                        }
+                                    }
                                 }
                             }
                         }

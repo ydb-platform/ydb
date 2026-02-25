0fa83abd7c3d1fe3cd4e3fe139a149563edc6536
Vitaliy Filippov <vitaliff@ydb.tech>
2026-02-25 15:18:08 +0000

    Implement JSON and binary JSON tokenization

--- ydb/core/base/fulltext.h
+++ ydb/core/base/fulltext.h
@@ -9,8 +9,11 @@ namespace NKikimr::NFulltext {
 void BuildNgrams(const TString& token, size_t lengthMin, size_t lengthMax, bool edge, TVector<TString>& ngrams);
 Ydb::Table::FulltextIndexSettings::Analyzers GetAnalyzersForQuery(Ydb::Table::FulltextIndexSettings::Analyzers analyzers);
 
-TVector<TString> Analyze(const TString& text, const Ydb::Table::FulltextIndexSettings::Analyzers& settings, const std::unordered_set<wchar32>& ignoredDelimiters = {});
+TVector<TString> Analyze(const TStringBuf text, const Ydb::Table::FulltextIndexSettings::Analyzers& settings, const std::unordered_set<wchar32>& ignoredDelimiters = {});
 TVector<TString> BuildSearchTerms(const TString& query, const Ydb::Table::FulltextIndexSettings::Analyzers& settings);
+
+TVector<TString> TokenizeJson(const TStringBuf jsonStr, TString& error);
+TVector<TString> TokenizeBinaryJson(const TStringBuf text);
 
 bool ValidateColumnsMatches(const NProtoBuf::RepeatedPtrField<TString>& columns, const Ydb::Table::FulltextIndexSettings& settings, TString& error);
 bool ValidateColumnsMatches(const TVector<TString>& columns, const Ydb::Table::FulltextIndexSettings& settings, TString& error);

d10514483ae3478e2af49b7753fe29ba196a952e
Nikita Vasilev <ns-vasilev@ydb.tech>
2026-02-26 12:56:12 +0300

    Fix stream RETURNING for UPDATE and interactive queries (#34906)

--- ydb/core/kqp/executer_actor/kqp_tasks_graph.cpp
+++ ydb/core/kqp/executer_actor/kqp_tasks_graph.cpp
@@ -1327,7 +1327,7 @@ void TKqpTasksGraph::FillOutputDesc(NYql::NDqProto::TTaskOutput& outputDesc, con
 
     if (output.Transform) {
         auto* transformDesc = outputDesc.MutableTransform();
-        auto& transform = output.Transform;
+        const auto& transform = output.Transform;
 
         transformDesc->SetType(transform->Type);
         transformDesc->SetInputType(transform->InputType);
@@ -3080,11 +3080,13 @@ void TKqpTasksGraph::BuildInternalOutputTransform(const NKqpProto::TKqpOutputTra
 
     FillKqpTableSinkSettings(settings, internalSinksOrder, task);
 
-    output.Transform.ConstructInPlace();
-    output.Transform->Type = TString(NYql::KqpTableSinkName);
-    output.Transform->InputType = transform.GetInputType();
-    output.Transform->OutputType = transform.GetOutputType();
-    output.Transform->Settings.PackFrom(settings);
+    TTransform outputTransform;
+    outputTransform.Type = NYql::KqpTableSinkName;
+    outputTransform.InputType = transform.GetInputType();
+    outputTransform.OutputType = transform.GetOutputType();
+    outputTransform.Settings.PackFrom(settings);
+
+    output.Transform = std::move(outputTransform);
 }
 
 

7ea517915b69b3f883e0c7d064b1d5afbfc745e4
babenko <babenko@yandex-team.com>
2026-02-28 22:53:55 +0300

    YT-27536: Fix (seemingly benign) race in TTwoLevelFairShareQueue commit_hash:700b15a49621e9f79e6e6c725a1aa958f9bd132e

--- yt/yt/core/concurrency/two_level_fair_share_thread_pool.cpp
+++ yt/yt/core/concurrency/two_level_fair_share_thread_pool.cpp
@@ -293,7 +293,7 @@ bool operator<(const TExecutionPool& lhs, const TExecutionPool& rhs)
     return lhs.ExcessTime < rhs.ExcessTime;
 }
 
-using TExecutionPoolPtr = ::NYT::TIntrusivePtr<TExecutionPool>;
+using TExecutionPoolPtr = TIntrusivePtr<TExecutionPool>;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -427,10 +427,10 @@ public:
 
         auto [bucketIt, bucketInserted] = BucketMapping_.emplace(std::pair(poolName, bucketName), nullptr);
 
-        auto bucket = bucketIt->second ? DangerousGetPtr(bucketIt->second) : nullptr;
+        auto bucket = bucketIt->second.Lock();
         if (!bucket) {
             bucket = New<TBucket>(bucketName, poolName, MakeStrong(this));
-            bucketIt->second = bucket.Get();
+            bucketIt->second = bucket;
             bucket->Pool = GetOrRegisterPool(bucket->PoolName);
         }
 
@@ -441,9 +441,9 @@ public:
     void RemoveBucket(TBucket* bucket)
     {
         auto guard = Guard(MappingLock_);
-        auto bucketIt = BucketMapping_.find(std::pair(bucket->PoolName, bucket->BucketName));
 
-        if (bucketIt != BucketMapping_.end() && bucketIt->second == bucket) {
+        auto bucketIt = BucketMapping_.find(std::pair(bucket->PoolName, bucket->BucketName));
+        if (bucketIt != BucketMapping_.end() && bucketIt->second.Get() == bucket) {
             BucketMapping_.erase(bucketIt);
         }
 
@@ -564,7 +564,7 @@ private:
     const TDuration PoolRetentionTime_;
 
     YT_DECLARE_SPIN_LOCK(NThreading::TSpinLock, MappingLock_);
-    THashMap<std::pair<std::string, std::string>, TBucket*> BucketMapping_;
+    THashMap<std::pair<std::string, std::string>, TWeakPtr<TBucket>> BucketMapping_;
     THashMap<std::string, TExecutionPool*> PoolMapping_;
 
     TPoolQueue RetainPoolQueue_;

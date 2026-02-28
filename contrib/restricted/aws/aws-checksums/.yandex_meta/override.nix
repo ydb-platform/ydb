edb49e1dfeb2cf8bdad2154f9419827e49fea5cb
robot-contrib <robot-contrib@yandex-team.com>
2026-02-28 20:11:15 +0300

    Update contrib/restricted/aws/aws-checksums to 0.2.9 commit_hash:9ace3e5edb6eefea219f9f76d5c8baf22ad26ff3

--- contrib/restricted/aws/aws-checksums/.yandex_meta/override.nix
+++ contrib/restricted/aws/aws-checksums/.yandex_meta/override.nix
@@ -1,10 +1,10 @@
 pkgs: attrs: with pkgs; with attrs; rec {
-  version = "0.2.8";
+  version = "0.2.9";
 
   src = fetchFromGitHub {
     owner = "awslabs";
     repo = "aws-checksums";
     rev = "v${version}";
-    hash = "sha256-0lLvTJATCw0Yf3Z3Q3rN7jQPSc0QK3A0z3wk4qI7L+Q=";
+    hash = "sha256-SqmVFz3wqMzrPlWaCDr+PiTj6J1J4to5lnuu6bsr3iU=";
   };
 }

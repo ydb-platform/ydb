3815deeea3d33628b7949a72d3a9752ee47c57f5
Oleg Doronin <dorooleg@yandex.ru>
2026-03-02 14:41:47 +0300

    compaction task settings have been added for zero level (#34957)

--- ydb/core/tx/columnshard/engines/storage/optimizer/lcbuckets/planner/level/zero_level.h
+++ ydb/core/tx/columnshard/engines/storage/optimizer/lcbuckets/planner/level/zero_level.h
@@ -13,6 +13,8 @@ private:
     const ui64 HighPriorityContribution;
     const bool CompactAtLevel;
     const ui64 Concurrency;
+    std::optional<ui64> CompactionTaskMemoryLimit;
+    std::optional<ui64> CompactionTaskPortionsCountLimit;
 
     std::set<TOrderedPortion> Portions;
 
@@ -85,7 +87,7 @@ public:
     TZeroLevelPortions(const ui32 levelIdx, const std::shared_ptr<IPortionsLevel>& nextLevel, const TLevelCounters& levelCounters,
         const std::shared_ptr<IOverloadChecker>& overloadChecker, const TDuration durationToDrop, const ui64 expectedBlobsSize,
         const ui64 portionsCountAvailable, const std::vector<std::shared_ptr<IPortionsSelector>>& selectors, const TString& defaultSelectorName,
-        const ui64 concurrency, const ui64 highPriorityContribution = 0, bool compactAtLevel = false);
+        const ui64 concurrency, std::optional<ui64> compactionTaskMemoryLimit, std::optional<ui64> compactionTaskPortionsCountLimit, const ui64 highPriorityContribution = 0, bool compactAtLevel = false);
 };
 
 }   // namespace NKikimr::NOlap::NStorageOptimizer::NLCBuckets

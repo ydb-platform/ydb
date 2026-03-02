db87d379aba659d5dc966a77c8f9159b8767850d
Stanislav Yablonskiy <neyrox@ydb.tech>
2026-03-02 06:05:32 +0000

    Add tip to error message

--- ydb/core/ydb_convert/table_description.cpp
+++ ydb/core/ydb_convert/table_description.cpp
@@ -329,7 +329,7 @@ bool BuildAlterTableModifyScheme(const TString& path, const Ydb::Table::AlterTab
 
             if (alter.has_compression()) {
                 code = Ydb::StatusIds::BAD_REQUEST;
-                error = "Column Compression is not supported in row tables";
+                error = "Column Compression is not supported in row tables. Use COLUMN FAMILY.";
                 return false;
             }
 
@@ -717,7 +717,7 @@ bool FillColumnDescription(NKikimrSchemeOp::TTableDescription& out,
 
         if (column.has_compression()) {
             status = Ydb::StatusIds::BAD_REQUEST;
-            error = "Column Compression is not supported in row tables";
+            error = "Column Compression is not supported in row tables. Use COLUMN FAMILY.";
             return false;
         }
 

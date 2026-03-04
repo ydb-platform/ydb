b42ffe094fa97337731311df859529c42ccd4957
Aleksei Borzenkov <snaury@ydb.tech>
2026-03-04 14:08:20 +0300

    Add an optional timestamp for locks in long tx service (#35378)

--- ydb/core/tx/long_tx_service/public/lock_handle.h
+++ ydb/core/tx/long_tx_service/public/lock_handle.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <util/datetime/base.h>
 #include <util/system/compiler.h>
 #include <util/system/types.h>
 
@@ -19,21 +20,24 @@ namespace NLongTxService {
             , ActorSystem(nullptr)
         { }
 
-        TLockHandle(ui64 lockId, NActors::TActorSystem* as) noexcept
+        TLockHandle(ui64 lockId, NActors::TActorSystem* as, TInstant lockTimestamp = TInstant::Zero()) noexcept
             : LockId(lockId)
             , ActorSystem(as)
+            , LockTimestamp(lockTimestamp)
         {
             if (LockId) {
-                Register(LockId, ActorSystem);
+                Register(LockId, ActorSystem, LockTimestamp);
             }
         }
 
         TLockHandle(TLockHandle&& rhs) noexcept
             : LockId(rhs.LockId)
             , ActorSystem(rhs.ActorSystem)
+            , LockTimestamp(rhs.LockTimestamp)
         {
             rhs.LockId = 0;
             rhs.ActorSystem = nullptr;
+            rhs.LockTimestamp = TInstant::Zero();
         }
 
         ~TLockHandle() noexcept {
@@ -45,8 +49,10 @@ namespace NLongTxService {
                 Reset();
                 LockId = rhs.LockId;
                 ActorSystem = rhs.ActorSystem;
+                LockTimestamp = rhs.LockTimestamp;
                 rhs.LockId = 0;
                 rhs.ActorSystem = nullptr;
+                rhs.LockTimestamp = TInstant::Zero();
             }
             return *this;
         }
@@ -60,6 +66,7 @@ namespace NLongTxService {
                 Unregister(LockId, ActorSystem);
                 LockId = 0;
                 ActorSystem = nullptr;
+                LockTimestamp = TInstant::Zero();
             }
         }
 
@@ -71,14 +78,19 @@ namespace NLongTxService {
             return ActorSystem ? GetNodeId(ActorSystem) : 0;
         }
 
+        TInstant GetLockTimestamp() const noexcept {
+            return LockTimestamp;
+        }
+
     private:
         static ui32 GetNodeId(NActors::TActorSystem* as) noexcept;
-        static void Register(ui64 lockId, NActors::TActorSystem* as) noexcept;
+        static void Register(ui64 lockId, NActors::TActorSystem* as, TInstant lockTimestamp) noexcept;
         static void Unregister(ui64 lockId, NActors::TActorSystem* as) noexcept;
 
     private:
         ui64 LockId;
         NActors::TActorSystem* ActorSystem;
+        TInstant LockTimestamp;
     };
 
 } // namespace NLongTxService

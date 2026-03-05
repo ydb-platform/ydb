5a862099e033d977322dc3dfaba39af7e03998b8
Vladilen Muzychenko <vladilenmuz@ydb.tech>
2026-03-05 16:44:03 +0000

    some fixes

--- ydb/core/kqp/ut/olap/dictionary_ut.cpp
+++ ydb/core/kqp/ut/olap/dictionary_ut.cpp
@@ -163,4 +163,6 @@ Y_UNIT_TEST_SUITE(KqpOlapDictionary) {
     Y_UNIT_TEST_STRING_VARIATOR(SimpleStringVariants, scriptSimpleStringVariants) {
         Variator::ToExecutor(Variator::SingleScript(__SCRIPT_CONTENT)).Execute();
     }
+}
+
 }   // namespace NKikimr::NKqp

b0a2f1cc9c1c6fdfbdad016beee8ad8fd5f21d53
Igor Sliusarenko <s2m1@ydb.tech>
2026-03-06 12:45:40 +0300

    wm: query session test

--- ydb/core/kqp/workload_service/ut/kqp_workload_service_query_sessions_ut.cpp
+++ ydb/core/kqp/workload_service/ut/kqp_workload_service_query_sessions_ut.cpp
@@ -10,9 +10,11 @@ using namespace NWorkload;
 using namespace NYdb;
 using namespace NActors;
 
-class TWmSwallowerMock : public IWmSessionUpdater {
+class TWmSessionUpdaterWrapper : public IWmSessionUpdater {
 public:
-    TWmSwallowerMock() 
+    TWmSessionUpdaterWrapper(EWmState finalState, std::shared_ptr<IWmSessionUpdater> inner)
+        : Inner(inner)
+        , FinalState(finalState)
     {}
 
     void SetUpdater(std::shared_ptr<IWmSessionUpdater> inner) {
@@ -23,26 +25,27 @@ public:
         return Inner;
     }
 
-    void SetRequestState(EWmState, TInstant timestamp) override {
-        Cerr << "--- Captured state: at " << timestamp << Endl;
+    void SetRequestState(EWmState state, TInstant timestamp) override {
+        if (state > FinalState) {
+            return;
+        }
+
+        Inner->SetRequestState(state, timestamp);
     }
 
     void SetPoolId(TString poolId) override {
         Inner->SetPoolId(poolId);
-        Cerr << "--- Set pool Id " << poolId << Endl;
     }
 
 private:
     std::shared_ptr<IWmSessionUpdater> Inner;
+    EWmState FinalState;
 };
 
 class TKqpWorkloadProxyActor : public TActorBootstrapped<TKqpWorkloadProxyActor> {
-    TActorId WorkloadServiceId;
-    std::shared_ptr<TWmSwallowerMock> WmSessionUpdaterMock;
-
 public:
-    TKqpWorkloadProxyActor(std::shared_ptr<TWmSwallowerMock> mock)
-        : WmSessionUpdaterMock(mock)
+    TKqpWorkloadProxyActor(IWmSessionUpdater::EWmState finalState)
+        : FinalState(finalState)
     {}
 
     void SetWorkloadServiceId(TActorId realServiceId) {
@@ -63,76 +66,162 @@ public:
 
     void HandlePlaceRequest(NWorkload::TEvPlaceRequestIntoPool::TPtr& ev) {
         auto* msg = ev->Get();
-        WmSessionUpdaterMock->SetUpdater(msg->WmSessionUpdater);
+        auto m = std::make_shared<TWmSessionUpdaterWrapper>(FinalState, msg->WmSessionUpdater);
         
         auto proxyMsg = new NWorkload::TEvPlaceRequestIntoPool(
-            msg->DatabaseId, msg->SessionId, msg->PoolId,
-            msg->UserToken, msg->RequestText, WmSessionUpdaterMock
+            msg->DatabaseId,
+            msg->SessionId,
+            msg->PoolId,
+            msg->UserToken,
+            msg->RequestText,
+            m
         );
 
         Send(new IEventHandle(WorkloadServiceId, ev->Sender, proxyMsg));
     }
+
+private:
+    TActorId WorkloadServiceId;
+    IWmSessionUpdater::EWmState FinalState;
 };
 
-/*
-template<typename R, typename TFetchFunc>
-R GetFieldFromResultSet(const Ydb::ResultSet& rs, size_t rowIndex, TFetchFunc&& fnc) {
-    NYdb::TResultSetParser parser(rs);
-    parser.TryNextRow();
+class TQuerySessionReader {
+public:
+    struct Row {
+        std::optional<std::string> WmPoolId;
+        std::optional<std::string> WmState;
+    };
 
-    for (size_t i = 0; i < rowIndex; ++i) {
-        parser.TryNextRow();
+public:
+    TQuerySessionReader(TIntrusivePtr<IYdbSetup> ydb) {
+        auto Result = ydb->ExecuteQuery(R"(
+            SELECT SessionId, WmPoolId, WmState
+            FROM `.sys/query_sessions`
+            WHERE State = 'EXECUTING'
+        )");
+
+        UNIT_ASSERT_VALUES_EQUAL_C(Result.GetStatus(), NYdb::EStatus::SUCCESS, Result.GetIssues().ToString());
+
+        auto rs = Result.GetResultSet(0);
+        Parser = std::make_unique<NYdb::TResultSetParser>(rs);
     }
 
-    return fnc(parser);
-}
+    size_t TotalRows() const {
+        return Result.GetResultSets().size();
+    }
 
-TString GetWmStateFromResultSet(const Ydb::ResultSet& rs, size_t rowIndex) {
-    return GetFieldFromResultSet<TString>(rs, rowIndex, [](NYdb::TResultSetParser& parser) {
-        auto opt = parser.ColumnParser("WmState").GetOptionalUtf8();
-        return opt ? TString(*opt) : TString();
-    });
-}
+    Row Fetch() const {
+        Parser->TryNextRow();
+        auto wmState = Parser->ColumnParser("WmState").GetOptionalUtf8();
+        auto wmPoolId = Parser->ColumnParser("WmPoolId").GetOptionalUtf8();
 
-TString GetWMPoolIdFromResultSet(const Ydb::ResultSet& rs, size_t rowIndex) {
-    return GetFieldFromResultSet<TString>(rs, rowIndex, [](NYdb::TResultSetParser& parser) {
-        auto opt = parser.ColumnParser("WmPoolId").GetOptionalUtf8();
-        return opt ? TString(*opt) : TString();
-    });
-}
-*/
+        return Row{.WmPoolId = wmPoolId, .WmState = wmState};
+    }
 
-}  // anonymous namespace
+private:
+    TQueryRunnerResult Result;
+    std::unique_ptr<NYdb::TResultSetParser> Parser;
+};
 
-Y_UNIT_TEST_SUITE(KqpWorkloadServiceQuerySessions) {
-    Y_UNIT_TEST(TestWmStateInterception) {
-        auto ydb = TYdbSetupSettings()
+class TQuerySessionTestFixture {
+public:
+    TQuerySessionTestFixture(IWmSessionUpdater::EWmState state, const TString myPoolId, size_t limit = 10) {
+        Ydb = TYdbSetupSettings()
             .NodeCount(1)
             .EnableResourcePools(true)
             // turn off to reduce "noise" in a log
             .EnableStreamingQueries(false)
+            .ConcurrentQueryLimit(limit)
             .CreateSamplePool(true)
-            .PoolId("my_pool")
+            .PoolId(myPoolId)
             .Create();
 
-        auto& runtime = *ydb->GetRuntime();
+        auto& runtime = *Ydb->GetRuntime();
         auto workloadServiceId = MakeKqpWorkloadServiceId(runtime.GetNodeId(0));
-        auto mock = std::make_shared<TWmSwallowerMock>();
         
-        auto actor = new TKqpWorkloadProxyActor(mock);
+        auto actor = new TKqpWorkloadProxyActor(state);
         auto newId = runtime.Register(actor);
         auto oldId = runtime.RegisterService(workloadServiceId, newId);
         
         actor->SetWorkloadServiceId(oldId);
+    }
 
-        auto myPoolId = "my_pool";
-        auto defPool = TQueryRunnerSettings().PoolId(NResourcePool::DEFAULT_POOL_ID);
-        auto myPool = TQueryRunnerSettings().PoolId(myPoolId);
+    TIntrusivePtr<IYdbSetup> GetYdb() {
+        return Ydb;
+    }
+
+    TQuerySessionReader GetReader() {
+        return TQuerySessionReader(Ydb);
+    }
+
+private:
+    TIntrusivePtr<IYdbSetup> Ydb;
+};
+
+}  // anonymous namespace
+
+Y_UNIT_TEST_SUITE(KqpWorkloadServiceQuerySessions) {
+    Y_UNIT_TEST(TestWmStateNone) {
+        TQuerySessionTestFixture f(IWmSessionUpdater::NONE, "my_pool");
+        auto myPool = TQueryRunnerSettings().PoolId("my_pool");
 
         TSampleQueries::TSelect42::CheckResult(
-            ydb->ExecuteQuery(TSampleQueries::TSelect42::Query, myPool));
+            f.GetYdb()->ExecuteQuery(TSampleQueries::TSelect42::Query, myPool));
 
+        auto r = f.GetReader().Fetch();
+
+        UNIT_ASSERT_VALUES_EQUAL(r.WmState, "NONE");
+        UNIT_ASSERT_VALUES_EQUAL(r.WmPoolId, "my_pool");
     }
+
+    Y_UNIT_TEST(TestWmStatePending) {
+        TQuerySessionTestFixture f(IWmSessionUpdater::PENDING, "my_pool");
+        auto myPool = TQueryRunnerSettings().PoolId("my_pool");
+
+        TSampleQueries::TSelect42::CheckResult(
+            f.GetYdb()->ExecuteQuery(TSampleQueries::TSelect42::Query, myPool));
+
+        auto r = f.GetReader().Fetch();
+
+        UNIT_ASSERT_VALUES_EQUAL(r.WmState, "PENDING");
+        UNIT_ASSERT_VALUES_EQUAL(r.WmPoolId, "my_pool");
+    }
+
+    Y_UNIT_TEST(TestWmStateDelayed) {
+        TQuerySessionTestFixture f(IWmSessionUpdater::PENDING, "my_pool", 1);
+        auto myPool = TQueryRunnerSettings().PoolId("my_pool");
+
+        auto hangingRequest = f.GetYdb()->ExecuteQueryAsync(
+            TSampleQueries::TSelect42::Query,
+            TQueryRunnerSettings().HangUpDuringExecution(true).PoolId("my_pool")
+        );
+
+        f.GetYdb()->WaitQueryExecution(hangingRequest);
+
+        TSampleQueries::TSelect42::CheckResult(
+            f.GetYdb()->ExecuteQuery(TSampleQueries::TSelect42::Query, myPool));
+
+        f.GetYdb()->ContinueQueryExecution(hangingRequest);
+
+        auto r = f.GetReader().Fetch();
+
+        UNIT_ASSERT_VALUES_EQUAL(r.WmState, "DELAYED");
+        UNIT_ASSERT_VALUES_EQUAL(r.WmPoolId, "my_pool");
+    }
+
+    Y_UNIT_TEST(TestWmStateExited) {
+        TQuerySessionTestFixture f(IWmSessionUpdater::EXITED, "my_pool");
+        auto myPool = TQueryRunnerSettings().PoolId("my_pool");
+
+        TSampleQueries::TSelect42::CheckResult(
+            f.GetYdb()->ExecuteQuery(TSampleQueries::TSelect42::Query, myPool));
+
+        auto r = f.GetReader().Fetch();
+
+        UNIT_ASSERT_VALUES_EQUAL(r.WmState, "EXITED");
+        UNIT_ASSERT_VALUES_EQUAL(r.WmPoolId, "my_pool");
+    }
+
 }
 
 }  // namespace NKikimr::NKqp

d2c02f51510700f3d69ee496a33f5838776ea18f
maxkovalev <maxkovalev@yandex-team.com>
2026-03-11 11:29:03 +0300

    YQL-21022: Add FIXME message commit_hash:8dfd63e3e779c2218955a5f69acc1e4c7eaa55e3

--- yql/essentials/core/services/yql_lineage.cpp
+++ yql/essentials/core/services/yql_lineage.cpp
@@ -603,6 +603,7 @@ private:
             for (const auto& i : structType->GetItems()) {
                 auto& res = (*lineage.Fields).try_emplace(i->GetName(), TFieldsLineage(Allocator_.get())).first->second;
                 TFieldLineageSet items(allLineage);
+                // FIXME: switch back to assign operator when crash in it will be fixed, check YQL-21022
                 std::swap(res.Items, items);
             }
         }

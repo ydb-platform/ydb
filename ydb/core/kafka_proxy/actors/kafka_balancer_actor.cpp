32ecf7b477eca7698b39a9905c0412ba52e7ee34
Irina Skvortsova <nerhneiro@yandex-team.ru>
2026-03-11 09:59:26 +0000

    add illegal_generation error in heartbeat response if records were deleted by ttl

--- ydb/core/kafka_proxy/actors/kafka_balancer_actor.cpp
+++ ydb/core/kafka_proxy/actors/kafka_balancer_actor.cpp
@@ -879,8 +879,14 @@ void TKafkaBalancerActor::HeartbeatStepUpdateHeartbeatDeadlines(NKqp::TEvKqp::TE
         return;
     }
 
-    if (!groupStatus->Exists || groupStatus->Generation != GenerationId || groupStatus->State != GROUP_STATE_WORKING) {
+    if (!groupStatus->Exists ||
+        groupStatus->Exists && groupStatus->Generation == GenerationId + 1 && groupStatus->State != GROUP_STATE_WORKING ||
+        groupStatus->State != GROUP_STATE_WORKING) {
         SendHeartbeatResponseFail(ctx, CorrelationId, EKafkaErrors::REBALANCE_IN_PROGRESS, "Group state changed. Rejoin required");
+        return;
+    }
+    if (groupStatus->Generation != GenerationId) {
+        SendHeartbeatResponseFail(ctx, CorrelationId, EKafkaErrors::ILLEGAL_GENERATION, "Old or unknown group generation");
         return;
     }
 
@@ -1123,7 +1129,7 @@ bool TKafkaBalancerActor::ParseDeadsAndSessionTimeout(
     auto& resp = record.GetResponse();
 
     if (resp.GetYdbResults().size() < 2) {
-        return false;
+        return true;
     }
 
     {
@@ -1141,9 +1147,10 @@ bool TKafkaBalancerActor::ParseDeadsAndSessionTimeout(
     {
         NYdb::TResultSetParser parser(resp.GetYdbResults(1));
         if (!parser.TryNextRow()) {
-            return false;
+            sessionTimeoutMs = DEFAULT_SESSION_TIMEOUT_MS;
+        } else {
+            sessionTimeoutMs = parser.ColumnParser("session_timeout_ms").GetOptionalUint32().value_or(DEFAULT_SESSION_TIMEOUT_MS);
         }
-        sessionTimeoutMs = parser.ColumnParser("session_timeout_ms").GetOptionalUint32().value_or(DEFAULT_SESSION_TIMEOUT_MS);
         if (parser.TryNextRow()) {
             return false;
         }

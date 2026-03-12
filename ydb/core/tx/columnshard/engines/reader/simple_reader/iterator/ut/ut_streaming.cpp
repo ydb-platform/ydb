5d66d80851b7bbfec824db4ed908ab9983e2ff2d
Stanislav Yablonskiy <neyrox@ydb.tech>
2026-03-12 11:21:26 +0000

    Backpressure test

--- ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/ut/ut_streaming.cpp
+++ ydb/core/tx/columnshard/engines/reader/simple_reader/iterator/ut/ut_streaming.cpp
@@ -221,6 +221,168 @@ void TestStreamingReadMultiplePortions() {
     }
 }
 
+void TestBackpressure() {
+    TTestBasicRuntime runtime;
+    TTester::Setup(runtime);
+    
+    // Enable SimpleReader with streaming
+    runtime.GetAppData(0).ColumnShardConfig.SetReaderClassName("SIMPLE");
+    
+    auto csControllerGuard = NKikimr::NYDBTest::TControllers::RegisterCSControllerGuard<TDefaultTestsController>();
+    csControllerGuard->DisableBackground(NKikimr::NYDBTest::ICSController::EBackground::Compaction);
+    
+    TActorId sender = runtime.AllocateEdgeActor();
+    CreateTestBootstrapper(runtime, CreateTestTabletInfo(TTestTxConfig::TxTablet0, TTabletTypes::ColumnShard), &CreateColumnShard);
+
+    TDispatchOptions options;
+    options.FinalEvents.push_back(TDispatchOptions::TFinalEventCondition(TEvTablet::EvBoot));
+    runtime.DispatchEvents(options);
+
+    ui64 writeId = 0;
+    ui64 tableId = 1;
+    ui64 txId = 100;
+
+    TestTableDescription table;
+    auto planStep = SetupSchema(runtime, sender, tableId, table);
+
+    // Write a large portion that will trigger streaming
+    const ui32 numRecords = 150000;
+    std::pair<ui64, ui64> portion = {0, numRecords};
+    
+    std::vector<ui64> writeIds;
+    TString largeData = MakeTestBlob(portion, table.Schema);
+    UNIT_ASSERT(WriteData(runtime, sender, writeId, tableId, largeData, table.Schema, true, &writeIds));
+
+    planStep = ProposeCommit(runtime, sender, txId, writeIds);
+    PlanCommit(runtime, sender, planStep, txId);
+
+    // Test backpressure by controlling acknowledgments
+    {
+        TShardReader reader(runtime, TTestTxConfig::TxTablet0, tableId, NOlap::TSnapshot(planStep, txId));
+        reader.SetReplyColumnIds(table.GetColumnIds({"timestamp", "message"}));
+        
+        // Initialize scanner
+        UNIT_ASSERT(reader.InitializeScanner());
+        
+        // Send first Ack to start receiving data
+        reader.Ack();
+        
+        ui32 receivedBatches = 0;
+        
+        // Simulate slow consumer: receive data but delay acknowledgments
+        while (true) {
+            bool hasMore = reader.Receive();
+            receivedBatches++;
+            
+            if (!hasMore) {
+                // Finished
+                break;
+            }
+            
+            Cerr << "Received batch " << receivedBatches << Endl;
+            
+            // Simulate processing delay before sending next Ack
+            // This creates backpressure - the reader should not send more data
+            // until we acknowledge
+            if (receivedBatches % 3 == 0) {
+                // Every 3rd batch, add a small delay to simulate slow processing
+                Sleep(TDuration::MilliSeconds(10));
+            }
+            
+            // Send Ack to request next batch
+            reader.Ack();
+        }
+        
+        UNIT_ASSERT(reader.IsCorrectlyFinished());
+        auto finalResult = reader.GetResult();
+        UNIT_ASSERT(finalResult);
+        UNIT_ASSERT_VALUES_EQUAL(finalResult->num_rows(), numRecords);
+        
+        // Verify that we received data in multiple iterations (streaming worked)
+        const ui32 iterationsCount = reader.GetIterationsCount();
+        Cerr << "Total iterations with backpressure: " << iterationsCount << Endl;
+        Cerr << "Total batches received: " << receivedBatches << Endl;
+        
+        // With backpressure and large data, we should have multiple iterations
+        UNIT_ASSERT_GT(iterationsCount, 1);
+        UNIT_ASSERT_GT(receivedBatches, 1);
+    }
+}
+
+void TestBackpressureSlowConsumer() {
+    TTestBasicRuntime runtime;
+    TTester::Setup(runtime);
+    
+    // Enable SimpleReader with streaming
+    runtime.GetAppData(0).ColumnShardConfig.SetReaderClassName("SIMPLE");
+    
+    auto csControllerGuard = NKikimr::NYDBTest::TControllers::RegisterCSControllerGuard<TDefaultTestsController>();
+    csControllerGuard->DisableBackground(NKikimr::NYDBTest::ICSController::EBackground::Compaction);
+    
+    TActorId sender = runtime.AllocateEdgeActor();
+    CreateTestBootstrapper(runtime, CreateTestTabletInfo(TTestTxConfig::TxTablet0, TTabletTypes::ColumnShard), &CreateColumnShard);
+
+    TDispatchOptions options;
+    options.FinalEvents.push_back(TDispatchOptions::TFinalEventCondition(TEvTablet::EvBoot));
+    runtime.DispatchEvents(options);
+
+    ui64 writeId = 0;
+    ui64 tableId = 1;
+    ui64 txId = 100;
+
+    TestTableDescription table;
+    auto planStep = SetupSchema(runtime, sender, tableId, table);
+
+    // Write data that will trigger streaming
+    const ui32 numRecords = 100000;
+    std::pair<ui64, ui64> portion = {0, numRecords};
+    
+    std::vector<ui64> writeIds;
+    TString data = MakeTestBlob(portion, table.Schema);
+    UNIT_ASSERT(WriteData(runtime, sender, writeId, tableId, data, table.Schema, true, &writeIds));
+
+    planStep = ProposeCommit(runtime, sender, txId, writeIds);
+    PlanCommit(runtime, sender, planStep, txId);
+
+    // Test slow consumer with controlled acknowledgments
+    {
+        TShardReader reader(runtime, TTestTxConfig::TxTablet0, tableId, NOlap::TSnapshot(planStep, txId));
+        reader.SetReplyColumnIds(table.GetColumnIds({"timestamp", "message"}));
+        
+        // Initialize scanner
+        UNIT_ASSERT(reader.InitializeScanner());
+        
+        ui32 batchCount = 0;
+        
+        // Manually control the read loop with delays between batches
+        reader.Ack();
+        while (reader.Receive()) {
+            batchCount++;
+            Cerr << "Received batch " << batchCount << Endl;
+            
+            // Simulate slow processing with delay every 2 batches
+            if (batchCount % 2 == 0) {
+                Sleep(TDuration::MilliSeconds(20));
+            }
+            
+            // Send next Ack
+            reader.Ack();
+        }
+        
+        UNIT_ASSERT(reader.IsCorrectlyFinished());
+        auto finalResult = reader.GetResult();
+        UNIT_ASSERT(finalResult);
+        UNIT_ASSERT_VALUES_EQUAL(finalResult->num_rows(), numRecords);
+        
+        Cerr << "Slow consumer test: received " << batchCount << " batches, "
+             << "total iterations: " << reader.GetIterationsCount() << Endl;
+        
+        // Verify streaming worked with multiple batches
+        UNIT_ASSERT_GT(batchCount, 1);
+        UNIT_ASSERT_GT(reader.GetIterationsCount(), 1);
+    }
+}
+
 } // namespace
 
 Y_UNIT_TEST_SUITE(StreamingRead) {
@@ -234,6 +396,14 @@ Y_UNIT_TEST_SUITE(StreamingRead) {
 
     Y_UNIT_TEST(StreamingWithMultiplePortions) {
         TestStreamingReadMultiplePortions();
+    }
+
+    Y_UNIT_TEST(Backpressure) {
+        TestBackpressure();
+    }
+
+    Y_UNIT_TEST(BackpressureSlowConsumer) {
+        TestBackpressureSlowConsumer();
     }
 }
 

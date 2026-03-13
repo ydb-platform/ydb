738cdcab34341c26e2b1a44d494b2c761fb20fed
Lykov Ivan <nfrmtk.aka.fkfka@gmail.com>
2026-03-13 18:23:48 +0000

    fix deprecation warning

--- ydb/tests/olap/scenario/test_alter_tiering.py
+++ ydb/tests/olap/scenario/test_alter_tiering.py
@@ -25,7 +25,7 @@ from ydb.tests.library.harness.kikimr_config import KikimrConfigGenerator
 from ydb.tests.library.harness.util import LogLevels
 
 from ydb import PrimitiveType, StatusCode
-import yatest.common
+import yatest.common.network
 from moto.server import ThreadedMotoServer
 
 import boto3
@@ -55,7 +55,7 @@ class TestLoop:
 
 class S3:
     def __init__(self):
-        self.port_manager = library.python.port_manager.PortManager()
+        self.port_manager = yatest.common.network.PortManager()
         self._server = None
 
     def start_server(self) -> str:

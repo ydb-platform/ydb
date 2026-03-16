93c2acd0d8c824725cf669b1fbffe7e724a8a077
Sergey Belyakov <serg-belyakov@ydb.tech>
2026-03-16 12:45:10 +0000

    fix include

--- ydb/library/actors/util/mutex_guarded_deterministic_random_provider.cpp
+++ ydb/library/actors/util/mutex_guarded_deterministic_random_provider.cpp
@@ -1,32 +1,31 @@
-#pragma once
-
 #include <mutex>
 
-#include <library/cpp/random/random_provider.h>
+#include "mutex_guarded_deterministic_random_provider.h"
 
-class TMutexGuardedDeterministicRandomProvider : public TDeterministicRandomProvider {
+class TMutexGuardedDeterministicRandomProvider : public IRandomProvider {
 public:
     TMutexGuardedDeterministicRandomProvider(ui64 seed)
-        : TDeterministicRandomProvider(seed)
+        : Impl(CreateDeterministicRandomProvider(seed))
     {}
 
     ui64 GenRand() noexcept override {
-        std::lock_guard guard(mutex);
-        return TDeterministicRandomProvider::GenRand();
+        std::lock_guard guard(Mutex);
+        return Impl->GenRand();
     }
 
     TGUID GenGuid() noexcept override {
-        std::lock_guard guard(mutex);
-        return TDeterministicRandomProvider::GenGuid();
+        std::lock_guard guard(Mutex);
+        return Impl->GenGuid();
     }
 
     TGUID GenUuid4() noexcept override {
-        std::lock_guard guard(mutex);
-        return TDeterministicRandomProvider::GenUuid4();
+        std::lock_guard guard(Mutex);
+        return Impl->GenUuid4();
     }
 
 private:
     std::mutex Mutex;
+    TIntrusivePtr<IRandomProvider> Impl;
 };
 
 TIntrusivePtr<IRandomProvider> CreateMutexGuardedDeterministicRandomProvider(ui64 seed) {
--- ydb/library/actors/util/mutex_guarded_deterministic_random_provider.h
+++ ydb/library/actors/util/mutex_guarded_deterministic_random_provider.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#include <library/cpp/random/random_provider.h>
+#include <library/cpp/random_provider/random_provider.h>
 
 TIntrusivePtr<IRandomProvider> CreateMutexGuardedDeterministicRandomProvider(ui64 seed);

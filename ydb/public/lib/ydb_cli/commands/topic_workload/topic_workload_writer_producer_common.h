b54d73add29e83c8c233f16b2f8615f23c432ef0
Roman Kuzin <kuzin57@yandex-team.ru>
2026-03-19 00:09:36 +0000

    LOGBROKER-10206 Fix

--- ydb/public/lib/ydb_cli/commands/topic_workload/topic_workload_writer_producer_common.h
+++ ydb/public/lib/ydb_cli/commands/topic_workload/topic_workload_writer_producer_common.h
@@ -29,6 +29,9 @@ public:
     virtual void WaitForContinuationToken(const TDuration&) {
     }
 
+    virtual void HandleReadyToAcceptEvent(NYdb::NTopic::TWriteSessionEvent::TReadyToAcceptEvent&) {
+    }
+
     virtual bool ContinuationTokenDefined() const {
         return true;
     }

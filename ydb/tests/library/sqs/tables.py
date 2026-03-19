4b6ff9f3d93514f552af55e481e355d317f9930f
Nikolay Shestakov <tesseract@ydb.tech>
2026-03-19 10:26:01 +0000

    fix

--- ydb/tests/library/sqs/tables.py
+++ ydb/tests/library/sqs/tables.py
@@ -180,6 +180,7 @@ def create_attibutes_table(root, session, queue_type, common_table=True):
         ('DlqArn', ydb.PrimitiveType.Utf8),
         ('MaxReceiveCount', ydb.PrimitiveType.Uint64),
         ('ShowDetailedCountersDeadline', ydb.PrimitiveType.Uint64),
+        ('TopicCreated', ydb.PrimitiveType.Bool),
     ]
     _create_table(root, session, 'Attributes', columns, len(queue_keys), common_table, queue_type)
 

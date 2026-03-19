1c86825280aa1fa78807d3806b3b9add281fa8c0
Vladislav Serikov <vlad-serikov@ydb.tech>
2026-03-19 08:48:30 +0000

    deliting unnecessary files

--- ydb/mvp/security/simple/security.cpp
+++
@@ -1,9 +0,0 @@
-#include "security.h"
-
-TStringBuf NKikimr::NSecurity::DefaultUserIp() {
-    return "";
-}
-
-TString NKikimr::NSecurity::BlackBoxTokenFromSessionId(TStringBuf sessionId, TStringBuf userIp) {
-    return TString("blackbox?method=sessionid&userip=") + userIp + "&host=yandex-team.ru&sessionid=" + sessionId;
-}

7918411792afc39944441e8ea7cdcab9917cc0de
Filitov Mikhail <filitovme@gmail.com>
2026-03-20 14:22:02 +0000

    split tests

--- ydb/core/kqp/ut/olap/peephole_ut.cpp
+++ ydb/core/kqp/ut/olap/peephole_ut.cpp
@@ -3,66 +3,13 @@
 #include <ydb/core/kqp/ut/common/kqp_ut_common.h>
 #include <ydb/core/tx/columnshard/hooks/testing/controller.h>
 
-#include <yql/essentials/ast/yql_expr.h>
-#include <yql/essentials/core/expr_nodes_gen/yql_expr_nodes_gen.h>
-#include <yql/essentials/core/yql_type_annotation.h>
-
 #include <library/cpp/testing/unittest/registar.h>
-
-namespace NKikimr::NKqp::NOpt {
-NYql::NNodes::TExprBase KqpEliminateWideMapPackUnpack(
-    const NYql::NNodes::TExprBase& node, NYql::TExprContext& ctx,
-    NYql::TTypeAnnotationContext& typesCtx);
-}
 
 namespace NKikimr::NKqp {
 
 using namespace NYdb;
 
 namespace {
-
-NYql::TExprNode::TPtr BuildPackUnpackWideMap(NYql::TExprContext& ctx, bool withComputation) {
-    auto pos = NYql::TPositionHandle();
-
-    auto packArgA = ctx.NewArgument(pos, "a");
-    auto packArgB = ctx.NewArgument(pos, "b");
-    auto packArgCount = ctx.NewArgument(pos, "count");
-
-    NYql::TExprNode::TPtr valA = packArgA;
-    NYql::TExprNode::TPtr valB = packArgB;
-    if (withComputation) {
-        valA = ctx.NewCallable(pos, "Increment", {packArgA});
-        valB = ctx.NewCallable(pos, "Increment", {packArgB});
-    }
-
-    auto bas = ctx.NewCallable(pos, "BlockAsStruct", {
-        ctx.NewList(pos, {ctx.NewAtom(pos, "c1"), valA}),
-        ctx.NewList(pos, {ctx.NewAtom(pos, "c2"), valB}),
-    });
-
-    auto packLambda = ctx.NewLambda(pos,
-        ctx.NewArguments(pos, {packArgA, packArgB, packArgCount}),
-        {bas, packArgCount});
-
-    auto input = ctx.NewCallable(pos, "TestInput", {});
-    auto innerWideMap = ctx.NewCallable(pos, "WideMap", {input, packLambda});
-
-    auto limit = ctx.NewCallable(pos, "Uint64", {ctx.NewAtom(pos, "1")});
-    auto wideTake = ctx.NewCallable(pos, "WideTakeBlocks", {innerWideMap, limit});
-
-    auto unpackArgStruct = ctx.NewArgument(pos, "struct");
-    auto unpackArgCount = ctx.NewArgument(pos, "ucount");
-
-    auto unpackLambda = ctx.NewLambda(pos,
-        ctx.NewArguments(pos, {unpackArgStruct, unpackArgCount}),
-        {
-            ctx.NewCallable(pos, "BlockMember", {unpackArgStruct, ctx.NewAtom(pos, "c1")}),
-            ctx.NewCallable(pos, "BlockMember", {unpackArgStruct, ctx.NewAtom(pos, "c2")}),
-            unpackArgCount,
-        });
-
-    return ctx.NewCallable(pos, "WideMap", {wideTake, unpackLambda});
-}
 
 TString ExplainOlapQuery(const TString& createTableSql, const TString& querySql) {
     auto settings = TKikimrSettings().SetWithSampleTables(false);
@@ -128,32 +75,6 @@ Y_UNIT_TEST_SUITE(KqpOlapPeephole) {
         UNIT_ASSERT_C(ast.Contains("(WideTakeBlocks (FromFlow"),
             "Scan stage: expected WideTakeBlocks directly on FromFlow "
             "even with column subset (pack/unpack roundtrip should be eliminated). AST: " + ast);
-    }
-
-    Y_UNIT_TEST(UnitIdentityPackUnpackIsEliminated) {
-        NYql::TExprContext ctx;
-        NYql::TTypeAnnotationContext typesCtx;
-
-        auto node = BuildPackUnpackWideMap(ctx, false);
-        auto result = NOpt::KqpEliminateWideMapPackUnpack(NYql::NNodes::TExprBase(node), ctx, typesCtx);
-
-        UNIT_ASSERT_C(result.Ptr() != node,
-            "Identity pack/unpack roundtrip should be eliminated");
-        UNIT_ASSERT_C(result.Ref().IsCallable("WideTakeBlocks"),
-            "Result should be WideTakeBlocks(input)");
-        UNIT_ASSERT_C(result.Ref().Head().IsCallable("TestInput"),
-            "WideTakeBlocks input should be the original TestInput");
-    }
-
-    Y_UNIT_TEST(UnitPackWithComputationIsPreserved) {
-        NYql::TExprContext ctx;
-        NYql::TTypeAnnotationContext typesCtx;
-
-        auto node = BuildPackUnpackWideMap(ctx, true);
-        auto result = NOpt::KqpEliminateWideMapPackUnpack(NYql::NNodes::TExprBase(node), ctx, typesCtx);
-
-        UNIT_ASSERT_C(result.Ptr() == node,
-            "Pack with computation (a+1) should NOT be eliminated");
     }
 }
 

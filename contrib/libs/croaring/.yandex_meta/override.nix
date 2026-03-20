bf73acb8b33d925d3326c1a401c04f6c0980d674
robot-contrib <robot-contrib@yandex-team.com>
2026-03-20 14:38:33 +0300

    Update contrib/libs/croaring to 4.6.0 commit_hash:dfc50877d6aa61345a4b41e62558e2cd7eef7f68

--- contrib/libs/croaring/.yandex_meta/override.nix
+++ contrib/libs/croaring/.yandex_meta/override.nix
@@ -1,12 +1,12 @@
 pkgs: attrs: with pkgs; with attrs; rec {
   pname = "croaring";
-  version = "4.5.1";
+  version = "4.6.0";
 
   src = fetchFromGitHub {
     owner = "RoaringBitmap";
     repo = "CRoaring";
     rev = "v${version}";
-    hash = "sha256-o+PLqXMRyCGgMsdg3eAbxu3qD4QBF9NHFBFJ2/BLCXs=";
+    hash = "sha256-YsNhuQp9pDe4OJTfUz1LsRevQPrfswW8fWRGzDu4FZg=";
   };
 
   patches = [];

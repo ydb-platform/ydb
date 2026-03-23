e1aebf056666b1e93c7f8aa9c28ffee68fc8f479
Alexander Kotov <akotov@ydb.tech>
2026-03-23 14:55:03 +0300

    [+] SkipSrcIdInfo

--- ydb/core/persqueue/events/events.cpp
+++ ydb/core/persqueue/events/events.cpp
@@ -1,2 +1,29 @@
 #include "global.h"
 #include "internal.h"
+
+bool NKikimr::TEvPersQueue::TEvProposeTransaction::GetSkipSrcIdInfo() const
+{
+    if (Record.GetTxBodyCase() != NKikimrPQ::TEvProposeTransaction::kData) {
+        return false;
+    }
+
+    const auto& tx = Record.GetData();
+    for (const auto& op : tx.GetOperations()) {
+        if (op.GetSkipConflictCheck()) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool NKikimr::TEvPQ::TEvTxCalcPredicate::GetSkipSrcIdInfo() const
+{
+    for (const auto& op : Operations) {
+        if (op.GetSkipConflictCheck()) {
+            return true;
+        }
+    }
+
+    return false;
+}

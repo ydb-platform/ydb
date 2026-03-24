94c882faa4f4fe6118e43617a8ba9d4720b6b64e
Alexander Kotov <akotov@ydb.tech>
2026-03-24 06:53:24 +0300

    [*] AllExistingWritesSkipConflictCheck

--- ydb/core/persqueue/events/events.cpp
+++ ydb/core/persqueue/events/events.cpp
@@ -1,29 +1,38 @@
 #include "global.h"
 #include "internal.h"
 
+template <class C>
+bool AllExistingWritesSkipConflictCheck(const C& ops)
+{
+    size_t writeOpsCount = 0;
+    size_t flagsCount = 0;
+
+    for (const auto& op : ops) {
+        if (!op.HasSkipConflictCheck()) {
+            // операция чтения
+            continue;
+        }
+
+        ++writeOpsCount;
+
+        if (op.GetSkipConflictCheck()) {
+            ++flagsCount;
+        }
+    }
+
+    return (writeOpsCount > 0) && (writeOpsCount == flagsCount);
+}
+
 bool NKikimr::TEvPersQueue::TEvProposeTransaction::GetSkipSrcIdInfo() const
 {
     if (Record.GetTxBodyCase() != NKikimrPQ::TEvProposeTransaction::kData) {
         return false;
     }
 
-    const auto& tx = Record.GetData();
-    for (const auto& op : tx.GetOperations()) {
-        if (op.GetSkipConflictCheck()) {
-            return true;
-        }
-    }
-
-    return false;
+    return AllExistingWritesSkipConflictCheck(Record.GetData().GetOperations());
 }
 
 bool NKikimr::TEvPQ::TEvTxCalcPredicate::GetSkipSrcIdInfo() const
 {
-    for (const auto& op : Operations) {
-        if (op.GetSkipConflictCheck()) {
-            return true;
-        }
-    }
-
-    return false;
+    return AllExistingWritesSkipConflictCheck(Operations);
 }

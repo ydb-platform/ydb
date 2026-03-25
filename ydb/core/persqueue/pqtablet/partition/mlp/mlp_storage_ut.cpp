1f0e939c63873eb2335e0ed753b5bf3bd3563b93
Nikolay Shestakov <tesseract@ydb.tech>
2026-03-25 06:37:16 +0000

    fix tests

--- ydb/core/persqueue/pqtablet/partition/mlp/mlp_storage_ut.cpp
+++ ydb/core/persqueue/pqtablet/partition/mlp/mlp_storage_ut.cpp
@@ -177,7 +177,7 @@ struct TUtils {
         TStorage::TPosition position;
         auto result = Storage.Next(TimeProvider->Now() + timeout, position);
         UNIT_ASSERT(result);
-        return result.value();
+        return result.value().Offset;
     }
 
     bool Commit(ui64 offset) {
@@ -630,7 +630,7 @@ Y_UNIT_TEST(NextWithoutKeepMessageOrderStorage) {
     TStorage::TPosition position;
     auto result = storage.Next(processingDeadline + TDuration::MilliSeconds(31), position);
     UNIT_ASSERT(result.has_value());
-    UNIT_ASSERT_VALUES_EQUAL(*result, 3);
+    UNIT_ASSERT_VALUES_EQUAL(result->Offset, 3);
 
     auto it = storage.begin();
     UNIT_ASSERT(it != storage.end());
@@ -670,7 +670,7 @@ Y_UNIT_TEST(NextWithKeepMessageOrderStorage) {
     TStorage::TPosition position;
     auto result = storage.Next(TInstant::Now() + TDuration::Seconds(1), position);
     UNIT_ASSERT(result.has_value());
-    UNIT_ASSERT_VALUES_EQUAL(*result, 3);
+    UNIT_ASSERT_VALUES_EQUAL(result->Offset, 3);
 
     auto& metrics = storage.GetMetrics();
     UNIT_ASSERT_VALUES_EQUAL(metrics.InflightMessageCount, 1);
@@ -706,7 +706,7 @@ Y_UNIT_TEST(NextWithWriteRetentionPeriod) {
     TStorage::TPosition position;
     auto result = storage.Next(timeProvider->Now() + TDuration::Seconds(1), position);
     UNIT_ASSERT(result.has_value());
-    UNIT_ASSERT_VALUES_EQUAL(*result, 4);
+    UNIT_ASSERT_VALUES_EQUAL(result->Offset, 4);
 
     auto& metrics = storage.GetMetrics();
     UNIT_ASSERT_VALUES_EQUAL(metrics.InflightMessageCount, 2);
@@ -739,7 +739,7 @@ Y_UNIT_TEST(NextWithInfinityRetentionPeriod) {
     TStorage::TPosition position;
     auto result = storage.Next(timeProvider->Now() + TDuration::Seconds(1), position);
     UNIT_ASSERT(result.has_value());
-    UNIT_ASSERT_VALUES_EQUAL(*result, 3);
+    UNIT_ASSERT_VALUES_EQUAL(result->Offset, 3);
 
     auto& metrics = storage.GetMetrics();
     UNIT_ASSERT_VALUES_EQUAL(metrics.InflightMessageCount, 1);
@@ -803,13 +803,13 @@ Y_UNIT_TEST(SkipLockedMessageGroups) {
         TStorage::TPosition position;
         auto result = storage.Next(TInstant::Now() + TDuration::Seconds(1), position);
         UNIT_ASSERT(result.has_value());
-        UNIT_ASSERT_VALUES_EQUAL(*result, 3);
+        UNIT_ASSERT_VALUES_EQUAL(result->Offset, 3);
     }
 
     TStorage::TPosition position;
     auto result = storage.Next(TInstant::Now() + TDuration::Seconds(1), position);
     UNIT_ASSERT(result.has_value());
-    UNIT_ASSERT_VALUES_EQUAL(*result, 5);
+    UNIT_ASSERT_VALUES_EQUAL(result->Offset, 5);
 
     auto& metrics = storage.GetMetrics();
     UNIT_ASSERT_VALUES_EQUAL(metrics.InflightMessageCount, 3);
@@ -841,7 +841,7 @@ Y_UNIT_TEST(CommitLockedMessage_WithoutKeepMessageOrder) {
         TStorage::TPosition position;
         auto result = storage.Next(TInstant::Now() + TDuration::Seconds(1), position);
         UNIT_ASSERT(result.has_value());
-        UNIT_ASSERT_VALUES_EQUAL(*result, 3);
+        UNIT_ASSERT_VALUES_EQUAL(result->Offset, 3);
     }
     {
         auto result = storage.Commit(3);
@@ -1658,7 +1658,7 @@ Y_UNIT_TEST(StorageSerialization_WAL_WithMoveBaseTime_Deadline) {
             TStorage::TPosition position;
             auto r = storage.Next(deadline3, position);
             UNIT_ASSERT(r);
-            UNIT_ASSERT_VALUES_EQUAL(*r, 3);
+            UNIT_ASSERT_VALUES_EQUAL(r->Offset, 3);
         }
 
         {
@@ -1674,7 +1674,7 @@ Y_UNIT_TEST(StorageSerialization_WAL_WithMoveBaseTime_Deadline) {
             TStorage::TPosition position;
             auto r = storage.Next(deadline4, position);
             UNIT_ASSERT(r);
-            UNIT_ASSERT_VALUES_EQUAL(*r, 4);
+            UNIT_ASSERT_VALUES_EQUAL(r->Offset, 4);
         }
 
         {

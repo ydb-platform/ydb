22820f2473f17df990d78d55bfe28389ed4255a5
Yesdaulet Izenov <yizenov@ydb.tech>
2026-03-27 18:03:52 +0300

    Revert temporary fix for some query hints

--- ydb/library/yql/dq/opt/dq_opt_make_join_hypergraph.h
+++ ydb/library/yql/dq/opt/dq_opt_make_join_hypergraph.h
@@ -279,15 +279,6 @@ TJoinHypergraph<TNodeSet> MakeJoinHypergraph(
         logHypergraph("Hypergraph after hints");
     }
 
-    if (!hints.JoinOrderHints->Hints.empty()) {
-        TJoinOrderHintsApplier joinHints(graph);
-        joinHints.Apply(*hints.JoinOrderHints);
-        if (logGraph && NYql::NLog::YqlLogger().NeedToLog(NYql::NLog::EComponent::CoreDq, NYql::NLog::ELevel::TRACE)) {
-            YQL_CLOG(TRACE, CoreDq) << "Hypergraph after hints: ";
-            YQL_CLOG(TRACE, CoreDq) << graph.String();
-        }
-    }
-
     return graph;
 }
 

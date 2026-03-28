f071edaee2259943ef98b8c7b64ed85b0d177d67
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-03-28 11:50:20 +0300

    datastreams_helpers: fix read_stream commit_after_processing=False

--- ydb/tests/tools/datastreams_helpers/data_plane.py
+++ ydb/tests/tools/datastreams_helpers/data_plane.py
@@ -67,7 +67,9 @@ def read_stream(path, messages_count, commit_after_processing=True, consumer_nam
         "--disable-cluster-discovery",
         "--messages-count", str(messages_count),
         "--timeout", "{}ms".format(int(timeout * 1000))
-    ] + ["--commit-after-processing"] if commit_after_processing else []
+    ]
+    if commit_after_processing:
+        cmd += ["--commit-after-processing"]
 
     execute_timeout = timeout + max(timeout, plain_or_under_sanitizer(10, 30))
     with open(result_file, "w") as outfile:

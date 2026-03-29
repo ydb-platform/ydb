fde308505a479914275f2620eb178531a62f48d5
Aleksei Moiseitsev <swarmer@ydb.tech>
2026-03-29 23:12:27 +0000

    split2

--- ydb/core/persqueue/pqtablet/partition/mlp/mlp_storage__serialization.cpp
+++ ydb/core/persqueue/pqtablet/partition/mlp/mlp_storage__serialization.cpp
@@ -1,6 +1,7 @@
 #include "mlp_storage.h"
 
 #include <ydb/core/protos/pqconfig.pb.h>
+#include <ydb/core/protos/pqdata_mlp.pb.h>
 #include <ydb/library/actors/core/log.h>
 
 #include <library/cpp/packedtypes/longs.h>

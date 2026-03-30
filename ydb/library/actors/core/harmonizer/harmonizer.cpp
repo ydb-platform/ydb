2f16266ba573f0f46963734c66ae95f9462caba7
kruall <kru.bash.all@gmail.com>
2026-03-30 18:45:24 +0300

    remove pool_id label

--- ydb/library/actors/core/harmonizer/harmonizer.cpp
+++ ydb/library/actors/core/harmonizer/harmonizer.cpp
@@ -166,9 +166,8 @@ void THarmonizer::EnsureInMemoryMetricsInitialized() {
     InMemoryMetrics.Pools.resize(Pools.size());
     for (size_t poolIdx = 0; poolIdx < Pools.size(); ++poolIdx) {
         const auto& pool = *Pools[poolIdx];
-        std::array<TLabel, 2> labels{{
+        std::array<TLabel, 1> labels{{
             TLabel{.Name = "pool", .Value = pool.Pool->GetName()},
-            TLabel{.Name = "pool_id", .Value = ToString(pool.Pool->PoolId)},
         }};
         auto& writers = InMemoryMetrics.Pools[poolIdx];
         writers.AvgUsedCpu = registry->CreateLine<TRawLineFrontend<float>>(PoolAvgUsedCpuMetric, labels);

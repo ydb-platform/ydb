061192358b7f9fda1ac80b7ea06084c5e7e7b444
Denis Khalikov <deniskhalikov@ydb.tech>
2026-04-01 11:58:01 +0300

    [Optimizer] Disable blocks on columns limit

--- ydb/core/kqp/compile_service/kqp_compile_actor.cpp
+++ ydb/core/kqp/compile_service/kqp_compile_actor.cpp
@@ -673,6 +673,7 @@ void ApplyServiceConfig(TKikimrConfiguration& kqpConfig, const TTableServiceConf
     kqpConfig.EnableDqHashCombineByDefault = serviceConfig.GetEnableDqHashCombineByDefault();
     kqpConfig.EnableWatermarks = serviceConfig.GetEnableWatermarks();
     kqpConfig.EnableBuildAggregationResultStages = serviceConfig.GetEnableBuildAggregationResultStages();
+    kqpConfig.DisableOlapBlocksOnColumnsLimit = serviceConfig.GetDisableOlapBlocksOnColumnsLimit();
 
     if (const auto limit = serviceConfig.GetResourceManager().GetMkqlHeavyProgramMemoryLimit()) {
         kqpConfig._KqpYqlCombinerMemoryLimit = std::max(1_GB, limit - (limit >> 2U));

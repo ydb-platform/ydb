c103f4dd79195ee5b70a1dccdf386327f6303166
Velikhov Pavel <pavelvelikhov@localhost.localdomain>
2026-04-02 15:57:44 +0000

    Intermediate commit to save work

--- ydb/core/kqp/opt/rbo/kqp_plan_to_json.cpp
+++ ydb/core/kqp/opt/rbo/kqp_plan_to_json.cpp
@@ -2,13 +2,41 @@
 #include <library/cpp/json/writer/json.h>
 #include <library/cpp/json/json_reader.h>
 
-NJson::TJsonValue TOpRoot::GetExecutionJson() {
-    // First construct the ResultSet and Stage JSON objects
 
-    for (auto it : *this) {
-        
-    }
+namespace NKikimr {
+namespace NKqp {
+
+NJson::TJsonValue TOpRoot::GetExecutionJson(ui64 & nodeCounter, ui32 explainFlags) {
+    Y_UNUSED(explainFlags);
+
+    // First construct the ResultSet
+    NJson::TJsonValue result;
+    result["PlanNodeId"] = nodeCounter++;
+    result["PlanNodeType"] = "ResultSet";
+    result["Node Type"] = "ResultSet";
+
+    // Add the final stage
+    NJson::TJsonValue stage;
+    NJson::TJsonValue plans;
+
+    stage["PlanNodeId"] = nodeCounter++;
+    stage["Node Type"] = "Stage";
+    plans.AppendValue(std::move(stage));
+    result["Plans"] = plans;
+
+    return result;
 }
 
-NJson::TJsonValue TOpRoot::GetExplainJson();
+NJson::TJsonValue TOpRoot::GetExplainJson(ui64 & nodeCounter, ui32 explainFlags) {
+    Y_UNUSED(explainFlags);
 
+    NJson::TJsonValue result;
+    result["PlanNodeId"] = nodeCounter++;
+    result["PlanNodeType"] = "ResultSet";
+    result["Node Type"] = "ResultSet";
+
+    return result;
+}
+
+}
+}

50f3ef7f4f99ace290ce3a0fe8ab7632b1ab1a4f
Aleksei Moiseitsev <swarmer@ydb.tech>
2026-04-06 15:37:08 +0000

    ensure that link works

--- ydb/library/kll_median/dynamic_sketch.h
+++ ydb/library/kll_median/dynamic_sketch.h
@@ -9,13 +9,7 @@
 namespace NKikimr::NKll {
 
     /** Bernoulli(weight / w0); requires 0 < weight < w0. */
-    inline bool TryAcceptSmallWeight(ui64 weight, ui64 w0, std::mt19937_64& rng) {
-        Y_ASSERT(w0 > 0);
-        Y_ASSERT(weight > 0 && weight < w0);
-        Y_ASSERT((w0 & (w0 - 1)) == 0);
-
-        return (static_cast<ui64>(rng()) & (w0 - 1)) < weight;
-    }
+    bool TryAcceptSmallWeight(ui64 weight, ui64 w0, std::mt19937_64& rng);
 
     template <class T>
     class TDynamicKllSketch {

ad483f8537241e161dbd89d98bb40109bf923d1a
Sergey Uzhakov <uzhas@ydb.tech>
2026-04-08 16:59:31 +0300

    Fix Coverity 106747: drop useless std::move on const TCurlInitConfig (#37631)

--- ydb/library/yql/providers/common/http_gateway/yql_http_gateway.cpp
+++ ydb/library/yql/providers/common/http_gateway/yql_http_gateway.cpp
@@ -354,7 +354,7 @@ public:
               offset,
               sizeLimit,
               data.size(),
-              std::move(config),
+              config,
               std::move(dnsCache),
               std::move(data))
         , Input(Data)
@@ -391,7 +391,7 @@ public:
             sizeLimit,
             std::move(callback),
             std::move(retryState),
-            std::move(config),
+            config,
             std::move(dnsCache));
     }
 
@@ -489,7 +489,7 @@ public:
         size_t threshold,
         const TCurlInitConfig& config = TCurlInitConfig(),
         TDNSGateway<>::TDNSConstCurlListPtr dnsCache = nullptr)
-        : TEasyCurl(counter, downloadedBytes, uploadededBytes, url, std::move(headers), EMethod::GET, offset, sizeLimit, 0ULL, std::move(config), std::move(dnsCache))
+        : TEasyCurl(counter, downloadedBytes, uploadededBytes, url, std::move(headers), EMethod::GET, offset, sizeLimit, 0ULL, config, std::move(dnsCache))
         , OnStart(std::move(onStart))
         , OnNewData(std::move(onNewData))
         , OnFinish(std::move(onFinish))
@@ -516,7 +516,7 @@ public:
         const TCurlInitConfig& config = TCurlInitConfig(),
         TDNSGateway<>::TDNSConstCurlListPtr dnsCache = nullptr)
     {
-        return std::make_shared<TEasyCurlStream>(counter, downloadedBytes, uploadededBytes, std::move(url), std::move(headers), offset, sizeLimit, std::move(onStart), std::move(onNewData), std::move(onFinish), inflightCounter, handle, threshold, std::move(config), std::move(dnsCache));
+        return std::make_shared<TEasyCurlStream>(counter, downloadedBytes, uploadededBytes, std::move(url), std::move(headers), offset, sizeLimit, std::move(onStart), std::move(onNewData), std::move(onFinish), inflightCounter, handle, threshold, config, std::move(dnsCache));
     }
 
     enum class EAction : i8 {

ae2680fe8e42de8d38b9250101d0d5109fa3b210
alexpaniman <alexpaniman@gmail.com>
2026-04-09 12:29:22 +0000

    Enable ShuffleElimination

--- ydb/core/kqp/opt/rbo/kqp_rbo_compute_statistics.cpp
+++ ydb/core/kqp/opt/rbo/kqp_rbo_compute_statistics.cpp
@@ -158,6 +158,12 @@ void TOpRead::ComputeMetadata(TRBOContext& ctx, TPlanProps& planProps) {
     }
     Props.Metadata->StorageType = storageType;
 
+    if (storageType == EStorageType::ColumnStorage && !tableData.Metadata->PartitionedByColumns.empty()) {
+        for (const auto& columnName : tableData.Metadata->PartitionedByColumns) {
+            Props.Metadata->ShuffledByColumns.emplace_back(Alias, columnName);
+        }
+    }
+
     YQL_CLOG(TRACE, CoreDq) << "Inferred metadata for table: " << path.Value();
 }
 

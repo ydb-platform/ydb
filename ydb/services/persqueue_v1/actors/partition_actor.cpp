93e30d6185130cda3923c8c3ad1d67861e8c1afb
Irina Skvortsova <nerhneiro@yandex-team.ru>
2026-04-10 12:07:47 +0000

    Revert "refactor TDistributedCommitHelper to make it work with several topics"
    This reverts commit 181ba291f35f0c440c32c7e7ccd8a3919116bd74.

--- ydb/services/persqueue_v1/actors/partition_actor.cpp
+++ ydb/services/persqueue_v1/actors/partition_actor.cpp
@@ -176,29 +176,13 @@ void TPartitionActor::SendCommit(const ui64 readId, const ui64 offset, const TAc
     const auto& parents = GetParents(partitionGraph);
     if (!ClientHasAnyCommits && parents.size() != 0) {
         std::vector<TDistributedCommitHelper::TCommitInfo> commits;
-        auto topicPath = Topic->GetPrimaryPath();
         for (auto& parent: parents) {
-            TDistributedCommitHelper::TCommitInfo commit {
-                .PartitionId = parent->Id,
-                .Offset = Max<i64>(),
-                .KillReadSession = false,
-                .OnlyCheckCommitedToFinish = true,
-                .ReadSessionId = Session,
-                .TopicPath = topicPath
-            };
+            TDistributedCommitHelper::TCommitInfo commit {.PartitionId = parent->Id, .Offset = Max<i64>(), .KillReadSession = false, .OnlyCheckCommitedToFinish = true, .ReadSessionId = Session};
             commits.push_back(commit);
         }
-
-        TDistributedCommitHelper::TCommitInfo commit {
-            .PartitionId = Partition.Partition,
-            .Offset = (i64)offset,
-            .KillReadSession = false,
-            .OnlyCheckCommitedToFinish = false,
-            .ReadSessionId = Session,
-            .TopicPath = topicPath
-        };
+        TDistributedCommitHelper::TCommitInfo commit {.PartitionId = Partition.Partition, .Offset = (i64)offset, .KillReadSession = false, .OnlyCheckCommitedToFinish = false, .ReadSessionId = Session};
         commits.push_back(commit);
-        auto kqp = std::make_shared<TDistributedCommitHelper>(Database, ClientId, commits, readId);
+        auto kqp = std::make_shared<TDistributedCommitHelper>(Database, ClientId, Topic->GetPrimaryPath(), commits, readId);
         Kqps.emplace(readId, kqp);
 
         kqp->SendCreateSessionRequest(ctx);
@@ -953,6 +937,15 @@ void TPartitionActor::CommitDone(ui64 cookie, const TActorContext& ctx) {
     }
 
     CommittedOffset = CommitsInfly.front().second.Offset;
+
+    bool wasMemoryLimitReached = PartitionInFlightMemoryController.IsMemoryLimitReached();
+    bool isMemoryOkNow = PartitionInFlightMemoryController.Remove(CommittedOffset);
+    if (wasMemoryLimitReached && isMemoryOkNow && EndOffset > ReadOffset) {
+        LOG_DEBUG_S(ctx, NKikimrServices::PQ_READ_PROXY, PQ_LOG_PREFIX << " " << Partition
+                        << " ready for read after commit with readOffset " << ReadOffset << " endOffset " << EndOffset);
+        SendPartitionReady(ctx);
+    }
+
     ui64 startReadId = CommitsInfly.front().second.StartReadId;
     ctx.Send(ParentId, new TEvPQProxy::TEvCommitDone(Partition.AssignId, startReadId, readId, CommittedOffset, EndOffset, ReadingFinishedSent));
 

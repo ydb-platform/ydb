f3f66569bfe0e3dbb50fd585d5793d32b00e2a31
Irina Skvortsova <nerhneiro@yandex-team.ru>
2026-04-10 12:07:28 +0000

    Revert "add consumer metadata table visit and generation check"
    This reverts commit fac73fe59f76c540bb93876825fde138df12a4e9.

--- ydb/services/persqueue_v1/actors/distributed_commit_helper.h
+++ ydb/services/persqueue_v1/actors/distributed_commit_helper.h
@@ -4,7 +4,6 @@
 #include <ydb/core/grpc_services/rpc_deferrable.h>
 #include <ydb/core/client/server/msgbus_server_pq_metacache.h>
 #include <ydb/core/persqueue/events/global.h>
-#include <ydb/core/kafka_proxy/kafka_consumer_groups_metadata_initializers.h>
 
 namespace NKikimr::NGRpcProxy::V1 {
 
@@ -14,7 +13,6 @@ class TDistributedCommitHelper {
 public:
     enum ECurrentStep {
         BEGIN_TRANSACTION_SENDED,
-        CHECK_GENERATION,
         OFFSETS_SENDED,
         COMMIT_SENDED,
         DONE
@@ -29,15 +27,9 @@ public:
         TString TopicPath;
     };
 
-    struct GenerationIdCheckerSettings {
-        i64 GenerationId;
-        TString ResourceDatabasePath;
-    };
+    TDistributedCommitHelper(TString database, TString consumer, std::vector<TCommitInfo> commits, ui64 cookie = 0);
 
-    TDistributedCommitHelper(TString database, TString consumer, std::vector<TCommitInfo> commits, ui64 cookie = 0,
-                             std::optional<GenerationIdCheckerSettings> generationCheckerSettings = std::nullopt);
-
-    std::pair<TDistributedCommitHelper::ECurrentStep, bool> Handle(NKqp::TEvKqp::TEvQueryResponse::TPtr& ev, const TActorContext& ctx);
+    ECurrentStep Handle(NKqp::TEvKqp::TEvQueryResponse::TPtr& ev, const TActorContext& ctx);
     void SendCreateSessionRequest(const TActorContext& ctx);
     void BeginTransaction(const NActors::TActorContext& ctx);
     bool Handle(NKqp::TEvKqp::TEvCreateSessionResponse::TPtr& ev, const TActorContext& ctx);
@@ -46,8 +38,6 @@ private:
     void CloseKqpSession(const TActorContext& ctx);
     THolder<NKqp::TEvKqp::TEvCreateSessionRequest> MakeCreateSessionRequest();
     THolder<NKqp::TEvKqp::TEvCloseSessionRequest> MakeCloseSessionRequest();
-    void RetrieveGeneration(NKqp::TEvKqp::TEvQueryResponse::TPtr& ev, const NActors::TActorContext& ctx);
-    bool CompareGenerations(NKqp::TEvKqp::TEvQueryResponse::TPtr& ev, const NActors::TActorContext& ctx);
     void SendCommits(NKqp::TEvKqp::TEvQueryResponse::TPtr& ev, const NActors::TActorContext& ctx);
     void CommitTx(const NActors::TActorContext& ctx);
 
@@ -60,19 +50,6 @@ private:
 
     TString TxId;
     TString KqpSessionId;
-    std::optional<GenerationIdCheckerSettings> CheckerSettings;
-
-    const TString CHECK_GROUP_GENERATION_ID = R"sql(
-        --!syntax_v1
-        DECLARE $ConsumerGroup AS Utf8;
-        DECLARE $Database AS Utf8;
-
-        SELECT state, generation, last_success_generation
-        FROM `%s`
-        VIEW PRIMARY KEY
-        WHERE database = $Database
-        AND consumer_group = $ConsumerGroup;
-    )sql";
 };
 
 }  // namespace NKikimr::NGRpcProxy::V1

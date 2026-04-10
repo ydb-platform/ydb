3aa3f0fb3f043c56f1bf0e28f5e321c375a91352
Irina Skvortsova <nerhneiro@yandex-team.ru>
2026-04-10 12:08:02 +0000

    Revert "attempts"
    This reverts commit 679625b500f22102c91e4b820ed88ac2d7e7206e.

--- ydb/core/kafka_proxy/actors/kafka_offset_commit_actor.cpp
+++ ydb/core/kafka_proxy/actors/kafka_offset_commit_actor.cpp
@@ -172,8 +172,6 @@ void TKafkaOffsetCommitActor::Handle(NGRpcProxy::V1::TEvPQProxy::TEvAuthResultOk
             commit->SetClientId(Message->GroupId.value());
             commit->SetOffset(partitionRequest.CommittedOffset);
             commit->SetStrict(true);
-            commit->SetGenerationId(Message->GenerationId);
-
             if (partitionRequest.CommittedMetadata.has_value()) {
                 commit->SetCommittedMetadata(*partitionRequest.CommittedMetadata);
             }

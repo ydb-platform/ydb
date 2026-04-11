6e0c0d9c574dd31c51ef5979f837e13001b4fe89
shadchin <shadchin@yandex-team.com>
2026-04-11 14:44:39 +0300

    Simplify `pyconfig.h` for Darwin commit_hash:167c3aa8fca30250e466b0451025d41733a35a6f

--- contrib/tools/python3/Include/pyconfig.h
+++ contrib/tools/python3/Include/pyconfig.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#if defined(__APPLE__) && (defined(__aarch64__) || defined(_M_ARM64))
-#   include "pyconfig-osx-arm64.h"
-#elif defined(__APPLE__) && (defined(__x86_64__) || defined(_M_X64))
-#   include "pyconfig-osx-x86_64.h"
+#if defined(__APPLE__)
+#   include "pyconfig-osx.h"
 #elif defined(_MSC_VER)
 #   include "pyconfig-win.h"
 #else

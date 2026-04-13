3580a990497d0aadd9148c853bb9ce0df5f2c378
Aleksandr Khoroshilov <hor911@ydb.tech>
2026-04-13 10:52:25 +0300

    Fix runtimeSettings.ExtraMemoryAllocationPool to enable extra allocations

--- ydb/core/kqp/compute_actor/kqp_compute_actor_factory.cpp
+++ ydb/core/kqp/compute_actor/kqp_compute_actor_factory.cpp
@@ -193,6 +193,7 @@ public:
         NYql::NDq::TComputeRuntimeSettings runtimeSettings;
 
         runtimeSettings.ReportStatsSettings = args.ReportStatsSettings;
+        runtimeSettings.ExtraMemoryAllocationPool = 1; // all extra allocation are expected from ScanQuery pool
         runtimeSettings.UseSpilling = args.WithSpilling;
         runtimeSettings.StatsMode = args.StatsMode;
         runtimeSettings.WithProgressStats = args.WithProgressStats;

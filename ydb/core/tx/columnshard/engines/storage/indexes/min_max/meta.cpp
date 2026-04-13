7431d40c547f8c76e955dca9eba49db42b92ba6e
Lykov Ivan <nfrmtk.aka.fkfka@gmail.com>
2026-04-13 11:58:33 +0300

    rename

--- ydb/core/tx/columnshard/engines/storage/indexes/min_max/meta.cpp
+++ ydb/core/tx/columnshard/engines/storage/indexes/min_max/meta.cpp
@@ -30,7 +30,7 @@ bool TIndexMeta::DoIsAppropriateFor(const NArrow::NSSA::TIndexCheckOperation& op
 
 TConclusionStatus TIndexMeta::DoCheckModificationCompatibility(const IIndexMeta& newMeta) const {
     Y_UNUSED(newMeta);
-    return TConclusionStatus::Fail("minmax index is not modifiable");
+    return TConclusionStatus::Fail("min_max index is not modifiable");
 }
 
 std::vector<std::shared_ptr<NChunks::TPortionIndexChunk>> TIndexMeta::DoBuildIndexImpl(
@@ -65,7 +65,7 @@ bool TIndexMeta::DoDeserializeFromProto(const NKikimrSchemeOp::TOlapIndexDescrip
 
 bool TIndexMeta::DoCheckValue(const TString& data, const std::optional<ui64> cat,
     const std::shared_ptr<arrow::Scalar>& requestValue, const NArrow::NSSA::TIndexCheckOperation& op, const TIndexInfo& info) const {
-    AFL_VERIFY(!cat.has_value())("error", "category shouldn't be passed to minmax index");
+    AFL_VERIFY(!cat.has_value())("error", "category shouldn't be passed to min_max index");
     auto chunkValue = NArrow::NAccessor::TMinMax::FromBinaryString(data, info.GetColumnFeaturesVerified(GetColumnId()).GetArrowField()->type());
     if (chunkValue.Min()->type->Equals(arrow::timestamp(arrow::TimeUnit::MICRO))) {
         chunkValue.Min() = chunkValue.Min()->CastTo(arrow::uint64()).ValueOrDie();

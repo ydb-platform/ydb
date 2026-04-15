931948b8c9adfb0dea244d77e89c5fa4798f9a95
Konstantin Seleznyov <kseleznyov@ydb.tech>
2026-04-15 08:33:45 +0000

    build fix

--- ydb/core/tx/replication/service/json_change_record_ut.cpp
+++ ydb/core/tx/replication/service/json_change_record_ut.cpp
@@ -2,6 +2,7 @@
 
 #include <library/cpp/testing/unittest/registar.h>
 #include <ydb/library/aclib/aclib.h>
+#include <ydb/library/aclib/user_context.h>
 
 namespace NKikimr::NReplication::NService {
 

b90c32891cc904b7fe353fecc0501748c7bba83c
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2026-04-16 12:34:00 +0300

    Another fix build

--- ydb/core/tx/datashard/datashard_ut_read_iterator.cpp
+++ ydb/core/tx/datashard/datashard_ut_read_iterator.cpp
@@ -4054,7 +4054,7 @@ Y_UNIT_TEST_SUITE(DataShardReadIterator) {
         AsyncDropTable(helper.Server, helper.Sender, "/Root", "table-1");
 
         runtime.SimulateSleep(TDuration::Seconds(1));
-        // Unblock execution of the read. It will now get added to the pipeline and 
+        // Unblock execution of the read. It will now get added to the pipeline and
         // would start executing if there were no additional checks on the presence of DropTable.
         blockedActivations.Stop().Unblock();
 
@@ -4153,7 +4153,7 @@ Y_UNIT_TEST_SUITE(DataShardReadIteratorSysTables) {
 
 Y_UNIT_TEST_SUITE(DataShardReadIteratorState) {
     Y_UNIT_TEST(ShouldCalculateQuota) {
-        NDataShard::TReadIteratorState state(TReadIteratorId({}, 0), 0, TPathId(0, 0), {}, TRowVersion::Max(), true, {});
+        NDataShard::TReadIteratorState state(TReadIteratorId({}, 0), 0, TPathId(0, 0), {}, TRowVersion::Max(), true, {}, {});
         state.Quota.Rows = 100;
         state.Quota.Bytes = 1000;
         state.ConsumeSeqNo(10, 100); // seqno1

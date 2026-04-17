3bde0061ff6d6c5729bae7253305f94dab23388b
Sergey Novikov <jmvssnovikov@ydb.tech>
2026-04-17 08:28:11 +0000

    Refactor endpoint testing by consolidating endpoint lists and enhancing method parameters. Update the viewer endpoints list to improve clarity and organization, while removing deprecated entries. Adjust the test functions to align with the new structure for better maintainability.

--- ydb/tests/functional/security/test_mon_endpoints_auth.py
+++ ydb/tests/functional/security/test_mon_endpoints_auth.py
@@ -280,7 +280,7 @@ def _test_endpoints(cluster, expected_results, method='GET', json_body=None):
             _test_endpoint(endpoint_url, endpoint_path, token, expected_status, method=method, json_body=json_body)
 
 
-def _test_endpoints_with_payloads(cluster, endpoint_cases, method='POST'):
+def _test_endpoints_with_payloads(cluster, endpoint_cases, method):
     host = cluster.nodes[1].host
     mon_port = cluster.nodes[1].mon_port
     base_url = f'https://{host}:{mon_port}'
@@ -454,193 +454,42 @@ def test_node_proxy_monitoring_builtin_auth_with_enforce_user_token(
     )
 
 
-DATABASE_ENDPOINTS_LIST = [
-    '/viewer/plan2svg',
-    # Endpoints below expose excessive information.
-    '/viewer/bscontrollerinfo',
-    '/viewer/cluster',
+VIEWER_ENDPOINTS_LIST = sorted([
+    '/storage/groups',
+    '/viewer/autocomplete',
+    '/viewer/bsgroupinfo',
     '/viewer/compute',
-    '/viewer/config',
     '/viewer/counters',
+    '/viewer/feature_flags',
+    '/viewer/groups',
     '/viewer/hiveinfo',
     '/viewer/hivestats',
     '/viewer/labeledcounters',
-    '/viewer/netinfo',
-    '/viewer/pqconsumerinfo',
-    '/viewer/storage',
-    '/viewer/storage_usage',
-    '/viewer/tenants',
-    '/viewer/topicinfo',
-]
-
-
-def test_database_endpoints_list_with_enforce_user_token(ydb_cluster_with_enforce_user_token):
-    expected_results = {
-        endpoint_path: {
-            None: 401,
-            'user@builtin': 403,
-            'database@builtin': 200,
-            'viewer@builtin': 200,
-            'monitoring@builtin': 200,
-            'root@builtin': 200,
-        }
-        for endpoint_path in DATABASE_ENDPOINTS_LIST
-    }
-    _test_endpoints(ydb_cluster_with_enforce_user_token, expected_results)
-
-
-DATABASE_ENDPOINTS_REQUIRING_PARAMETERS_OR_REQUEST_CONTEXT_LIST = [
-    '/operation/get',
-    '/operation/list',
-    '/query/script/fetch',
-    '/viewer/browse',
-    '/viewer/describe_consumer',
-    '/viewer/describe_replication',
-    '/viewer/describe_topic',
-    '/viewer/describe_transfer',
-    '/viewer/graph',
-    '/viewer/metainfo',
-    '/viewer/topic_data',
-    '/operation/list?database=%2FRoot',
-    '/operation/forget',
-    '/viewer/commit_offset',
-    '/operation/cancel',
-    '/query/script/execute',
-    '/scheme/directory',
-    '/viewer/put_record',
-]
-
-DATABASE_ENDPOINTS_REQUIRING_PARAMETERS_OR_REQUEST_CONTEXT_POST_METHOD_LIST = [
-    '/operation/forget',
-    '/viewer/commit_offset',
-    '/operation/cancel',
-    '/query/script/execute',
-    '/viewer/put_record',
-    '/viewer/query',
-    '/viewer/acl',
-]
-
-DATABASE_ENDPOINTS_REQUIRING_PARAMETERS_OR_REQUEST_CONTEXT_GET_AND_POST_METHOD_LIST = [
-    '/scheme/directory',
-]
-
-
-def test_database_endpoints_requiring_parameters_or_request_context_with_enforce_user_token(
-    ydb_cluster_with_enforce_user_token,
-):
-    expected_statuses_for_invalid_request = {
-        None: 401,
-        'user@builtin': 403,
-        'database@builtin': 400,
-        'viewer@builtin': 400,
-        'monitoring@builtin': 400,
-        'root@builtin': 400,
-    }
-
-    expected_results_get = {
-        endpoint_path: expected_statuses_for_invalid_request
-        for endpoint_path in DATABASE_ENDPOINTS_REQUIRING_PARAMETERS_OR_REQUEST_CONTEXT_LIST
-        if (
-            endpoint_path not in DATABASE_ENDPOINTS_REQUIRING_PARAMETERS_OR_REQUEST_CONTEXT_POST_METHOD_LIST
-            or endpoint_path in DATABASE_ENDPOINTS_REQUIRING_PARAMETERS_OR_REQUEST_CONTEXT_GET_AND_POST_METHOD_LIST
-        )
-    }
-    _test_endpoints(ydb_cluster_with_enforce_user_token, expected_results_get)
-
-    # POST checks include minimal parameters to pass method gate.
-    expected_results_post = {
-        '/operation/forget?database=%2FRoot&id=fake-op': {
-            'expected_statuses': expected_statuses_for_invalid_request,
-            'json_body': {},
-        },
-        '/viewer/commit_offset?database=%2FRoot&path=%2FRoot%2Fmissing-topic&consumer=c1&partition_id=0&offset=0': {
-            'expected_statuses': expected_statuses_for_invalid_request,
-            'json_body': {},
-        },
-        '/operation/cancel?database=%2FRoot&id=fake-op': {
-            'expected_statuses': expected_statuses_for_invalid_request,
-            'json_body': {},
-        },
-        '/query/script/execute?database=%2FRoot': {
-            'expected_statuses': expected_statuses_for_invalid_request,
-            'json_body': {},
-        },
-        '/scheme/directory?database=%2FRoot': {
-            'expected_statuses': expected_statuses_for_invalid_request,
-            'json_body': {},
-        },
-        '/viewer/query?database=%2FRoot': {
-            'expected_statuses': expected_statuses_for_invalid_request,
-            'json_body': {},
-        },
-        '/viewer/acl?database=%2FRoot': {
-            'expected_statuses': expected_statuses_for_invalid_request,
-            'json_body': {},
-        },
-        '/viewer/put_record?database=%2FRoot': {
-            'expected_statuses': expected_statuses_for_invalid_request,
-            'json_body': {
-                'path': '/Root/missing-topic',
-                'message': 'm',
-            },
-        },
-    }
-    _test_endpoints_with_payloads(ydb_cluster_with_enforce_user_token, expected_results_post, method='POST')
-
-
-@pytest.mark.xfail(reason='Empty request handling is unstable for this endpoint in the current environment')
-def test_database_endpoints_with_unstable_empty_request_handling_with_enforce_user_token(
-    ydb_cluster_with_enforce_user_token,
-):
-    _test_endpoints(
-        ydb_cluster_with_enforce_user_token,
-        {
-            '/viewer/content': {
-                None: 401,
-                'user@builtin': 403,
-                'database@builtin': 504,
-                'viewer@builtin': 504,
-                'monitoring@builtin': 504,
-                'root@builtin': 504,
-            },
-            '/viewer/tabletcounters': {
-                None: 401,
-                'user@builtin': 403,
-                'database@builtin': None,
-                'viewer@builtin': None,
-                'monitoring@builtin': None,
-                'root@builtin': None,
-            },
-        },
-    )
-
-
-VIEWER_ENDPOINTS_LIST = [
-    '/storage/groups',
-    '/viewer/bsgroupinfo',
-    '/viewer/groups',
     '/viewer/multipart_counter',
+    '/viewer/netinfo',
     '/viewer/nodeinfo',
+    '/viewer/nodelist',
     '/viewer/nodes',
     '/viewer/pdiskinfo',
     '/viewer/peers',
+    '/viewer/pqconsumerinfo',
     '/viewer/simple_counter',
     '/viewer/sse_counter',
+    '/viewer/storage',
+    '/viewer/storage_usage',
     '/viewer/sysinfo',
     '/viewer/tabletinfo',
+    '/viewer/tenantinfo',
+    '/viewer/tenants',
+    '/viewer/topicinfo',
     '/viewer/v2/json/nodeinfo',
     '/viewer/v2/json/pdiskinfo',
     '/viewer/v2/json/sysinfo',
     '/viewer/v2/json/tabletinfo',
     '/viewer/v2/json/vdiskinfo',
     '/viewer/vdiskinfo',
-    # Endpoints below expose excessive information or grant excessive rights.
-    '/viewer/autocomplete',
-    '/viewer/feature_flags',
-    '/viewer/nodelist',
-    '/viewer/tenantinfo',
     '/viewer/whoami',
-]
+])
 
 
 def test_viewer_endpoints_list_with_enforce_user_token(ydb_cluster_with_enforce_user_token):
@@ -658,24 +507,19 @@ def test_viewer_endpoints_list_with_enforce_user_token(ydb_cluster_with_enforce_
     _test_endpoints(ydb_cluster_with_enforce_user_token, expected_results)
 
 
-VIEWER_ENDPOINTS_REQUIRING_PARAMETERS_OR_REQUEST_CONTEXT_LIST = [
-    '/viewer/render',
-    # Endpoints below expose excessive information or grant excessive rights.
-    '/pdisk/info',
-    '/pdisk/restart',
-    '/pdisk/status',
-    '/vdisk/blobindexstat',
-    '/vdisk/evict',
-    '/vdisk/getblob',
-    '/vdisk/vdiskstat',
+VIEWER_ENDPOINTS_REQUIRING_PARAMETERS_OR_REQUEST_CONTEXT_LIST = sorted([
     '/viewer/acl',
+    '/viewer/browse',
     '/viewer/check_access',
     '/viewer/database_stats',
     '/viewer/describe',
+    '/viewer/graph',
     '/viewer/hotkeys',
+    '/viewer/metainfo',
     '/viewer/query',
+    '/viewer/render',
     '/viewer/storage_stats',
-]
+])
 
 
 def test_viewer_endpoints_requiring_parameters_or_request_context_with_enforce_user_token(

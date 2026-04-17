0124de64539b145be512c9313f08b3c7d5696f75
Nikolay Shestakov <tesseract@ydb.tech>
2026-04-17 08:36:45 +0000

    remove unnecessary condition

--- ydb/core/persqueue/pqtablet/pq_impl.cpp
+++ ydb/core/persqueue/pqtablet/pq_impl.cpp
@@ -2220,7 +2220,7 @@ void TPersQueue::HandleReadRequest(
     } else if (cmd.HasCount() && cmd.GetCount() <= 0) {
         ReplyError(ctx, responseCookie, NPersQueue::NErrorCode::BAD_REQUEST,
             TStringBuilder() << "invalid count in read request: " << ToString(req).data());
-    } else if (!cmd.HasOffset() || cmd.GetOffset() < 0) {
+    } else if (cmd.GetOffset() < 0) {
         ReplyError(ctx, responseCookie, NPersQueue::NErrorCode::BAD_REQUEST,
             TStringBuilder() << "invalid offset in read request: " << ToString(req).data());
     } else if (cmd.HasBytes() && cmd.GetBytes() <= 0) {

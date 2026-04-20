b25d6c2b5283dee55b75e8a3b194dcc014179ca1
Ilia Shakhov <pixcc@qavm-368da906.qemu>
2026-04-20 11:34:45 +0300

    Make restore request POST instead of GET in system tablet backup

--- ydb/core/tablet_flat/flat_executor_recovery.cpp
+++ ydb/core/tablet_flat/flat_executor_recovery.cpp
@@ -587,11 +587,13 @@ public:
         }
 
         auto cgi = ev->Get()->Cgi();
-        if (const auto& path = cgi.Get("restoreBackup")) {
-            if (RestoreState == ERestoreState::NotStarted) {
-                bool skipChecksum = cgi.Has("skipChecksumValidation");
-                bool dryRun = cgi.Has("dryRun");
-                StartRestore(path, {}, skipChecksum, dryRun);
+        if (ev->Get()->GetMethod() == HTTP_METHOD_POST) {
+            if (const auto& path = cgi.Get("restoreBackup")) {
+                if (RestoreState == ERestoreState::NotStarted) {
+                    bool skipChecksum = cgi.Has("skipChecksumValidation");
+                    bool dryRun = cgi.Has("dryRun");
+                    StartRestore(path, {}, skipChecksum, dryRun);
+                }
             }
         }
 
@@ -675,9 +677,8 @@ public:
                             }
 
                             $.ajax({
-                                type: "GET",
-                                url: window.location.href,
-                                data: form.serialize(),
+                                type: "POST",
+                                url: window.location.href + '&' + form.serialize(),
                                 success: function(response) {
                                     $('body').html(response);
                                 },

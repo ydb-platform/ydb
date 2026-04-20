dd50470e7f38a78f7fa8920528d62d5c9660f121
becalm <becalm@yandex-team.com>
2026-04-20 22:42:19 +0300

    BACKPORT-CONFLICT: manual resolution required for commit 03551b9

--- yql/essentials/sql/v1/node.h
+++ yql/essentials/sql/v1/node.h
@@ -1266,6 +1266,11 @@ struct TIndexDescription {
         GlobalFulltextRelevance,
         LocalBloomFilter,
         LocalBloomNgramFilter,
+<<<<<<< HEAD
+=======
+        GlobalJson,
+        LocalMinMax
+>>>>>>> 03551b9560f (add ddl for min_max index is YDB Column Shards)
     };
 
     struct TIndexSetting {

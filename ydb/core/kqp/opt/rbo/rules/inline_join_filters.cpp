ecec94ca83dcb701ee47fe645e30a9ca65e5a855
Velikhov Pavel <pavelvelikhov@localhost.localdomain>
2026-04-20 17:59:36 +0000

    Removed commented out code

--- ydb/core/kqp/opt/rbo/rules/inline_join_filters.cpp
+++ ydb/core/kqp/opt/rbo/rules/inline_join_filters.cpp
@@ -41,10 +41,6 @@ TIntrusivePtr<IOperator> TInlineJoinFiltersRule::SimpleMatchAndApply(const TIntr
 
     auto innerJoin = MakeIntrusive<TOpJoin>(join->GetLeftInput(), join->GetRightInput(), join->Pos, "Inner", join->JoinKeys);
     auto filterExpr = MakeConjunction(join->JoinFilters);
-    //if (join->JoinKind == "LeftOnly") {
-    //    filterExpr = MakeNegation(filterExpr);
-    //}
-
     auto newFilter = MakeIntrusive<TOpFilter>(innerJoin, input->Pos, filterExpr);
 
     // We need to remap the appropriate side of the output columns, so we can join on the same columns again

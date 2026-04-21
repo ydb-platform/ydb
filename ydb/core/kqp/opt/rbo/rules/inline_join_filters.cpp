0cbab181d7c323dec42f84053a7f908843d4ac58
Pavel Velikhov <pavelvelikhov@ydb.tech>
2026-04-21 12:31:06 +0300

    [NEW RBO] Improved left semi with filters rewrite, fixed potential bug (#38471)
    Co-authored-by: Velikhov Pavel <pavelvelikhov@localhost.localdomain>

--- ydb/core/kqp/opt/rbo/rules/inline_join_filters.cpp
+++ ydb/core/kqp/opt/rbo/rules/inline_join_filters.cpp
@@ -1,5 +1,61 @@
 #include "kqp_rules_include.h"
 
+namespace {
+
+using namespace NKikimr::NKqp;
+
+// Create a mapping from a list of IUs to new synthetic variables
+THashMap<TInfoUnit, TInfoUnit, TInfoUnit::THashFunction> MakeRenameMap(const TVector<TInfoUnit>& IUs, int& varIdx) {
+    THashMap<TInfoUnit, TInfoUnit, TInfoUnit::THashFunction> result;
+    for (const auto& iu: IUs) {
+        result[iu] = TInfoUnit("_rbo_arg_" + std::to_string(varIdx++));
+    }
+    return result;
+}
+
+// Rename join keys of the right side of the join using a specified rename map
+TVector<std::pair<TInfoUnit, TInfoUnit>> RemapJoinKeysRightSide(const TVector<std::pair<TInfoUnit, TInfoUnit>>& joinKeys, 
+    const THashMap<TInfoUnit, TInfoUnit, TInfoUnit::THashFunction>& renameMap) {
+
+        TVector<std::pair<TInfoUnit, TInfoUnit>> newJoinKeys;
+
+        for (const auto& [leftKey, rightKey] : joinKeys) {
+            if (renameMap.contains(rightKey)) {
+                newJoinKeys.push_back(std::make_pair(leftKey, renameMap.at(rightKey)));
+            } else {
+                newJoinKeys.push_back(std::make_pair(leftKey, rightKey));
+            }
+    }
+    return newJoinKeys;
+
+}
+
+// Build a projecting map operator that renames output columns wrt the rename map, or copies them in
+// the ouput if they're not in the map
+TIntrusivePtr<TOpMap> MakeMapFromRenames(TIntrusivePtr<IOperator> input,
+    const THashMap<TInfoUnit, TInfoUnit, TInfoUnit::THashFunction>& renameMap, 
+    TPositionHandle pos, 
+    TExprContext *ctx, 
+    TPlanProps *props) {
+
+    TVector<TMapElement> mapElements;
+    for (const auto& iu : input->GetOutputIUs()) {
+        auto fromIU = iu;
+        auto toIU = iu;
+
+        if (renameMap.contains(iu)) {
+            toIU = renameMap.at(iu);
+        }
+
+        mapElements.push_back(TMapElement(toIU, iu, pos, ctx, props));
+    }
+
+    return MakeIntrusive<TOpMap>(input, pos, mapElements, true);
+}
+
+
+}
+
 namespace NKikimr {
 namespace NKqp {
     
@@ -39,34 +95,31 @@ TIntrusivePtr<IOperator> TInlineJoinFiltersRule::SimpleMatchAndApply(const TIntr
         return input;
     }
 
-    auto innerJoin = MakeIntrusive<TOpJoin>(join->GetLeftInput(), join->GetRightInput(), join->Pos, "Inner", join->JoinKeys);
+    // Build an inner join, but in case of LeftSemi and LeftOnly, the right side may contain duplicate IUs
+    // which will break the plan. So we rename them
+    auto commonIUs = IUSetIntersect(join->GetLeftInput()->GetOutputIUs(), join->GetRightInput()->GetOutputIUs());
+    TIntrusivePtr<IOperator> rightInput = join->GetRightInput();
+
+    auto rightRenameMap = MakeRenameMap(commonIUs, props.InternalVarIdx);
+    auto newInnerJoinKeys = RemapJoinKeysRightSide(join->JoinKeys, rightRenameMap);
+
+    if (rightRenameMap.size()) {
+        rightInput = MakeMapFromRenames(join->GetRightInput(), rightRenameMap, join->Pos, &ctx.ExprCtx, &props);
+    }
+
+    auto innerJoin = MakeIntrusive<TOpJoin>(join->GetLeftInput(), rightInput, join->Pos, "Inner", newInnerJoinKeys);
     auto filterExpr = MakeConjunction(join->JoinFilters);
+
     auto newFilter = MakeIntrusive<TOpFilter>(innerJoin, input->Pos, filterExpr);
 
     // We need to remap the appropriate side of the output columns, so we can join on the same columns again
     // without confilcts
-    auto renameIUs = join->GetLeftInput()->GetOutputIUs();
 
-    TVector<TMapElement> mapElements;
-    THashMap<TInfoUnit, TInfoUnit, TInfoUnit::THashFunction> renameMap;
-    for (const auto& iu : newFilter->GetOutputIUs()) {
+    auto topCommonIUs = IUSetIntersect(join->GetLeftInput()->GetOutputIUs(), innerJoin->GetOutputIUs());
 
-        if (std::find(renameIUs.begin(), renameIUs.end(), iu) != renameIUs.end()) {
-            auto newVar = TInfoUnit("_rbo_arg_" + std::to_string(props.InternalVarIdx++));
-            mapElements.push_back(TMapElement(newVar, iu, join->Pos, &ctx.ExprCtx, &props));
-            renameMap[iu] = newVar;
-        } else {
-            mapElements.push_back(TMapElement(iu, iu, join->Pos, &ctx.ExprCtx, &props));
-        }
-    }
-
-    auto map = MakeIntrusive<TOpMap>(newFilter, join->Pos, mapElements, true);
-
-    TVector<std::pair<TInfoUnit, TInfoUnit>> newJoinKeys;
-    for (const auto& [leftKey, _] : join->JoinKeys) {
-        newJoinKeys.push_back(std::make_pair(leftKey, renameMap.at(leftKey)));
-    }
-
+    auto renameMap = MakeRenameMap(topCommonIUs, props.InternalVarIdx);
+    auto map = MakeMapFromRenames(newFilter, renameMap, join->Pos, &ctx.ExprCtx, &props);
+    auto newJoinKeys = RemapJoinKeysRightSide(join->JoinKeys, renameMap);
     auto result = MakeIntrusive<TOpJoin>(join->GetLeftInput(), map, join->Pos, join->JoinKind, newJoinKeys);
     
     return result;

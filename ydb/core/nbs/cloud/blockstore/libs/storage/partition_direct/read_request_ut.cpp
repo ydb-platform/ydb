f7ae16617b7eb8dcf7ac338d400dce673c18bf2f
Yudov Maksim <yudovmaksim@yandex-team.ru>
2026-04-22 12:01:05 +0000

    [NBS-6775] base multiple hints - test on new read request

--- ydb/core/nbs/cloud/blockstore/libs/storage/partition_direct/read_request_ut.cpp
+++ ydb/core/nbs/cloud/blockstore/libs/storage/partition_direct/read_request_ut.cpp
@@ -42,6 +42,50 @@ Y_UNIT_TEST_SUITE(TReadRequestTest)
         const auto& response = future.GetValue();
         UNIT_ASSERT_VALUES_EQUAL(S_OK, response.Error.GetCode());
     }
+
+    Y_UNIT_TEST_F(ShouldReadMultipleLocations, TBaseFixture)
+    {
+        Init();
+
+        DirtyMap.WriteFinished(
+            100,
+            TBlockRange64::WithLength(20, 10),
+            TLocationMask::MakePrimaryPBuffers(),
+            TLocationMask::MakePrimaryPBuffers());
+
+        DirtyMap.WriteFinished(
+            200,
+            TBlockRange64::WithLength(40, 10),
+            TLocationMask::MakePrimaryPBuffers(),
+            TLocationMask::MakePrimaryPBuffers());
+
+        const TBlockRange64 range = TBlockRange64::WithLength(10, 100);
+        ExpectedRange = range;
+
+        auto readHint = DirtyMap.MakeReadHint(range);
+        UNIT_ASSERT_VALUES_EQUAL(5, readHint.RangeHints.size());
+
+        auto callContext = MakeIntrusive<TCallContext>(static_cast<ui64>(0));
+        auto originalRequest = std::make_shared<TReadBlocksLocalRequest>(
+            TRequestHeaders{.RequestId = 1, .Range = range});
+
+        auto readRequest = std::make_shared<TReadRequestExecutor>(
+            Runtime->GetActorSystem(0),
+            VChunkConfig,
+            DirectBlockGroup,
+            std::move(readHint),
+            std::move(callContext),
+            std::move(originalRequest),
+            NWilson::TTraceId());
+        auto future = readRequest->GetFuture();
+        readRequest->Run();
+        UNIT_ASSERT_VALUES_EQUAL(false, future.HasValue());
+
+        ReadPromise.SetValue({.Error = MakeError(S_OK)});
+        UNIT_ASSERT_VALUES_EQUAL(true, future.HasValue());
+        const auto& response = future.GetValue();
+        UNIT_ASSERT_VALUES_EQUAL(S_OK, response.Error.GetCode());
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////

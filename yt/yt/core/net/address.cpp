893782fb5ba51b41a889bd3fcdb98fcef5354c18
babenko <babenko@yandex-team.com>
2026-04-23 02:11:15 +0300

    Fix npos+1 wraparound in IPv4 address:port parsing
    In the else branch of ParseServiceAddress, closingBracketIndex is npos.
    Passing npos+1 as the search offset wraps to 0, which happens to work
    but is undefined behaviour. Use address.find(':') with no offset since
    there is no bracket to skip in the IPv4 path.
    commit_hash:247dec77d22f76c76bc32fa22e2ff3665988705f

--- yt/yt/core/net/address.cpp
+++ yt/yt/core/net/address.cpp
@@ -289,7 +289,7 @@ TErrorOr<TNetworkAddress> TNetworkAddress::TryParse(TStringBuf address)
         ipAddress = address.substr(1, closingBracketIndex - 1);
     } else {
         if (address.find('.') != TString::npos) {
-            auto colonIndex = address.find(':', closingBracketIndex + 1);
+            auto colonIndex = address.find(':');
             if (colonIndex != TString::npos) {
                 try {
                     port = FromString<int>(address.substr(colonIndex + 1));

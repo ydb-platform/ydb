e5d1bbc40e6cc0ad91de373666091707b3158bbc
Pavel <pefavel@ydb.tech>
2026-04-24 11:30:42 +0300

    Nemesis: remove dmesg from logs analysis (#38716)

--- ydb/tests/library/wardens/logs.py
+++ ydb/tests/library/wardens/logs.py
@@ -6,7 +6,7 @@ import os
 from ydb.tests.library.nemesis.safety_warden import (
     GrepLogFileForMarkers,
     GrepGzippedLogFilesForMarkersSafetyWarden,
-    GrepDMesgForPatternsSafetyWarden,
+    GrepJournalctlKernelForPatternsSafetyWarden,
 )
 
 
@@ -67,16 +67,18 @@ def kikimr_crit_and_alert_logs_safety_warden_factory(
     ]
 
 
-def kikimr_grep_dmesg_safety_warden_factory(
+def kikimr_grep_kernel_log_safety_warden_factory(
         executor=None,
         lines_after=5,
+        hours_back=24,
 ):
     markers = ['Out of memory: Kill process']
 
     return [
-        GrepDMesgForPatternsSafetyWarden(
+        GrepJournalctlKernelForPatternsSafetyWarden(
             executor,
             list_of_markers=markers,
             lines_after=lines_after,
+            hours_back=hours_back,
         )
     ]

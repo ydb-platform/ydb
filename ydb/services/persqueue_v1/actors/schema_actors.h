4194efe5cbd8966230f4a5ecf34537880a9a6896
Nikolay Shestakov <tesseract@ydb.tech>
2026-04-24 06:33:42 +0000

    remove unused code

--- ydb/services/persqueue_v1/actors/schema_actors.h
+++ ydb/services/persqueue_v1/actors/schema_actors.h
@@ -245,26 +245,6 @@ private:
     Ydb::Topic::DescribePartitionResult Result;
 };
 
-
-// class TPQCreateTopicActor : public TPQGrpcSchemaBase<TPQCreateTopicActor, NKikimr::NGRpcService::TEvPQCreateTopicRequest> {
-//     using TBase = TPQGrpcSchemaBase<TPQCreateTopicActor, TEvPQCreateTopicRequest>;
-
-// public:
-//     TPQCreateTopicActor(NKikimr::NGRpcService::TEvPQCreateTopicRequest* request, const TString& localCluster, const TVector<TString>& clusters);
-//     ~TPQCreateTopicActor() = default;
-
-//     void FillProposeRequest(TEvTxUserProxy::TEvProposeTransaction& proposal, const TActorContext& ctx,
-//                              const TString& workingDir, const TString& name);
-
-//     void Bootstrap(const NActors::TActorContext& ctx);
-
-//     void HandleCacheNavigateResponse(TEvTxProxySchemeCache::TEvNavigateKeySetResult::TPtr& ev){ Y_UNUSED(ev); }
-
-// private:
-//     TString LocalCluster;
-//     TVector<TString> Clusters;
-// };
-
 class TPartitionsLocationActor : public TPQInternalSchemaActor<TPartitionsLocationActor,
                                                                TGetPartitionsLocationRequest,
                                                                TEvPQProxy::TEvPartitionLocationResponse>

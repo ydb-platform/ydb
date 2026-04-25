16f45f736e632c2c166e63be5a5ca49089820f77
robot-contrib <robot-contrib@yandex-team.com>
2026-04-25 14:28:20 +0300

    Update contrib/libs/libunwind to 22.1.3 commit_hash:4e10c7121ae27e02301dd9dc617ccc770219fbaf

--- contrib/libs/libunwind/.yandex_meta/override.nix
+++ contrib/libs/libunwind/.yandex_meta/override.nix
@@ -1,11 +1,11 @@
 pkgs: attrs: with pkgs; with attrs; rec {
-  version = "22.1.2";
+  version = "22.1.3";
 
   src = fetchFromGitHub {
     owner = "llvm";
     repo = "llvm-project";
     rev = "llvmorg-${version}";
-    hash = "sha256-z6YcxgDd3F3JwfU5Y/wMw5MK+ZPISI3KLwHwUaraTuw=";
+    hash = "sha256-jlSQBwXxLvqp65qP366C2ZverDJa4r5Ux5JXh0IQAnQ=";
   };
 
   patches = [];

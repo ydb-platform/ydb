6557a44bda160413fd566fbae576fd21de5e1102
Nikolay Shestakov <tesseract@ydb.tech>
2026-04-25 06:38:10 +0000

    revert

--- ydb/public/sdk/cpp/src/client/persqueue_public/ut/ut_utils/test_server.cpp
+++ ydb/public/sdk/cpp/src/client/persqueue_public/ut/ut_utils/test_server.cpp
@@ -4,13 +4,10 @@ namespace NPersQueue {
 
 const TVector<NKikimrServices::EServiceKikimr> TTestServer::LOGGED_SERVICES = {
     NKikimrServices::PQ_READ_PROXY,
-    NKikimrServices::PQ_SCHEMA,
     NKikimrServices::PQ_WRITE_PROXY,
     NKikimrServices::PQ_PARTITION_CHOOSER,
     NKikimrServices::PERSQUEUE,
-    NKikimrServices::PERSQUEUE_CLUSTER_TRACKER,
-    NKikimrServices::PERSQUEUE_READ_BALANCER,
-
+    NKikimrServices::PERSQUEUE_READ_BALANCER
 };
 
 } // namespace NPersQueue

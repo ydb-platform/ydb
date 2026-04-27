c02cbbbb55cc755dfad19e2d932665d0d9d25e34
s-sabitova <170026516+s-sabitova@users.noreply.github.com>
2026-04-27 19:07:39 +0300

    Resolve conflicts

--- ydb/core/driver_lib/run/run.cpp
+++ ydb/core/driver_lib/run/run.cpp
@@ -67,7 +67,6 @@
 #include <ydb/core/protos/replication.pb.h>
 #include <ydb/core/protos/stream.pb.h>
 #include <ydb/core/protos/workload_manager_config.pb.h>
-#include <ydb/core/protos/data_integrity_trails.pb.h>
 
 #include <ydb/core/mind/local.h>
 #include <ydb/core/mind/tenant_pool.h>
@@ -1453,7 +1452,10 @@ void TKikimrRunner::InitializeAppData(const TKikimrRunConfig& runConfig)
         AppData->WorkloadManagerConfig = runConfig.AppConfig.GetWorkloadManagerConfig();
     }
 
-<<<<<<< HEAD
+    if (runConfig.AppConfig.HasDataIntegrityTrailsConfig()) {
+        AppData->DataIntegrityTrailsConfig = runConfig.AppConfig.GetDataIntegrityTrailsConfig();
+    }
+
     if (runConfig.AppConfig.HasQueryServiceConfig()) {
         AppData->QueryServiceConfig = runConfig.AppConfig.GetQueryServiceConfig();
     }
@@ -1473,10 +1475,6 @@ void TKikimrRunner::InitializeAppData(const TKikimrRunConfig& runConfig)
 
     if (runConfig.AppConfig.HasSystemTabletBackupConfig()) {
         AppData->SystemTabletBackupConfig = runConfig.AppConfig.GetSystemTabletBackupConfig();
-=======
-    if (runConfig.AppConfig.HasDataIntegrityTrailsConfig()) {
-        AppData->DataIntegrityTrailsConfig = runConfig.AppConfig.GetDataIntegrityTrailsConfig();
->>>>>>> 7f5cc8af780 (KIKIMR-24190 SQL query tracing fix v2)
     }
 
     // setup resource profiles

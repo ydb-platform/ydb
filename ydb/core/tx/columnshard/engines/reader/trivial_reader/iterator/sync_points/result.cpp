7cb705172e0aa8cb4368c76475bb62f84e18c8ca
Stanislav Yablonskiy <neyrox@ydb.tech>
2026-04-29 19:14:08 +0300

    Fix build

--- ydb/core/tx/columnshard/engines/reader/trivial_reader/iterator/sync_points/result.cpp
+++ ydb/core/tx/columnshard/engines/reader/trivial_reader/iterator/sync_points/result.cpp
@@ -48,9 +48,10 @@ ISyncPoint::ESourceAction TSyncPointResult::OnSourceReady(const std::shared_ptr<
                 "source_idx", source->GetSourceIdx())("table", resultChunk->GetTable()->num_rows())("is_finished", isFinished);
             auto cursor = Collection->BuildCursor(source, resultChunk->GetStartIndex() + resultChunk->GetRecordsCount(),
                 Context->GetCommonContext()->GetReadMetadata()->GetTabletId());
-            reader.OnIntervalResult(std::make_unique<TPartialReadResult>(source->GetResourceGuards(),
-                source->MutableAs<IDataSource>()->GetGroupGuard(), resultChunk->ExtractTable(), std::move(cursor), Context->GetCommonContext(),
-                partialSourceAddress, source->GetDeprecatedPortionId()));
+            reader.OnIntervalResult(
+                std::make_unique<TPartialReadResult>(source->GetResourceGuards(), source->MutableAs<IDataSource>()->GetGroupGuard(),
+                resultChunk->ExtractTable(), std::move(cursor), Context->GetCommonContext(), partialSourceAddress, std::nullopt,
+                source->GetDeprecatedPortionId()));
         } else if (!isFinished) {
             AFL_DEBUG(NKikimrServices::TX_COLUMNSHARD_SCAN)("event", "continue_source")("source_idx", source->GetSourceIdx())(
                 "source_idx", source->GetSourceIdx());

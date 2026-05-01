bffb7abe507e1fec2e15796a352a506d760f8a99
robot-contrib <robot-contrib@yandex-team.com>
2026-05-01 15:25:47 +0300

    Update contrib/restricted/boost/bind to 1.91.0 commit_hash:ab2f017118315704d841409800ff8f06851cc921

--- contrib/restricted/boost/bind/.yandex_meta/default.nix
+++ contrib/restricted/boost/bind/.yandex_meta/default.nix
@@ -1,13 +1,13 @@
 self: super: with self; {
   boost_bind = stdenv.mkDerivation rec {
     pname = "boost_bind";
-    version = "1.90.0";
+    version = "1.91.0";
 
     src = fetchFromGitHub {
       owner = "boostorg";
       repo = "bind";
       rev = "boost-${version}";
-      hash = "sha256-xdT8F7JuinEWbY5EOey21oUTjvF/7cPsu1tD31oEVus=";
+      hash = "sha256-tAJNR99ZRouSwZZSupwvi/iDAv4Ax/h8p7trLtriIHU=";
     };
   };
 }

5039408429ee2aaf2e6f0b2153cf0494588e1505
Vasily Gerasimov <UgnineSirdis@ydb.tech>
2026-05-03 17:59:33 +0200

    Filter items set by --include params using --exclude regexps

--- ydb/public/lib/ydb_cli/common/exclude_item.h
+++ ydb/public/lib/ydb_cli/common/exclude_item.h
@@ -2,11 +2,15 @@
 
 #include <library/cpp/regex/pcre/regexp.h>
 
+#include <util/generic/typetraits.h>
 #include <util/generic/vector.h>
 
 #include <string>
 
 namespace NYdb::NConsoleClient {
+
+Y_HAS_MEMBER(SrcPath);
+Y_HAS_MEMBER(SrcPathDb);
 
 inline bool IsExcluded(const char* str, const TVector<TRegExMatch>& exclusionPatterns) {
     for (const auto& pattern : exclusionPatterns) {
@@ -22,11 +26,32 @@ inline bool IsExcluded(const std::string& str, const TVector<TRegExMatch>& exclu
     return IsExcluded(str.c_str(), exclusionPatterns);
 }
 
+template <typename TItem>
+bool IsItemExcluded(const TItem& item, const TVector<TRegExMatch>& exclusionPatterns) {
+    if (IsExcluded(item.Src, exclusionPatterns)) {
+        return true;
+    }
+
+    if constexpr (THasSrcPath<TItem>::value) {
+        if (!item.SrcPath.empty() && IsExcluded(item.SrcPath, exclusionPatterns)) {
+            return true;
+        }
+    }
+
+    if constexpr (THasSrcPathDb<TItem>::value) {
+        if (!item.SrcPathDb.empty() && IsExcluded(item.SrcPathDb, exclusionPatterns)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 template <typename TSettings>
 void ExcludeItems(TSettings& settings, const TVector<TRegExMatch>& exclusionPatterns) {
     auto items(std::move(settings.Item_));
     for (auto& item : items) {
-        if (IsExcluded(item.Src, exclusionPatterns)) {
+        if (IsItemExcluded(item, exclusionPatterns)) {
             continue;
         }
 

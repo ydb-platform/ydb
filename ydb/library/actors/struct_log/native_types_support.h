5d716d2ad981b6a2df96c0480348655b7c4de9f5
Konstantin Seleznyov <kseleznyov@ydb.tech>
2026-05-05 19:09:08 +0000

    Remove native types

--- ydb/library/actors/struct_log/native_types_support.h
+++ ydb/library/actors/struct_log/native_types_support.h
@@ -20,70 +20,21 @@ using TBinaryData = std::vector<std::uint8_t>;
 template<typename T>
 struct TNativeTypeSupport : public std::false_type{};
 
-template <typename T, typename V = typename std::enable_if< std::is_pod_v<T> >::type>
-struct TNativePlainTypeSupport : public std::true_type
-{
-    static inline void Serialize(const T& value, TBinaryData& data) {
-        auto from = reinterpret_cast<const std::uint8_t*>(&value);
-        auto oldSize = data.size();
-        data.resize(oldSize + sizeof(T));
-        auto to = data.data() + oldSize;
-        std::memcpy(to, from, sizeof(T));
-    }
-
-    static bool Deserialize(T& value, const void* data, std::size_t length) {
-        if (sizeof(T) != length) {
-            return false;
-        }
-        value =  *(reinterpret_cast<const T*>(data));
-        return true;
-    }
-
-    static TString ToString(const T& value) {
-        return std::to_string(value);
-    }
-
-    static void AppendToString(const T& value, TStringBuilder& stringBuffer) {
-        char buffer[64];
-        auto result = std::to_chars(buffer, buffer + 64, value);
-        stringBuffer.append(buffer, result.ptr - buffer);
-    }
-};
-
-template<> struct TNativeTypeSupport<i8> : public TNativePlainTypeSupport<i8>{};
-template<> struct TNativeTypeSupport<ui8> : public TNativePlainTypeSupport<ui8>{};
-template<> struct TNativeTypeSupport<i16> : public TNativePlainTypeSupport<i16>{};
-template<> struct TNativeTypeSupport<ui16> : public TNativePlainTypeSupport<ui16>{};
-template<> struct TNativeTypeSupport<i32> : public TNativePlainTypeSupport<i32>{};
-template<> struct TNativeTypeSupport<ui32> : public TNativePlainTypeSupport<ui32>{};
-template<> struct TNativeTypeSupport<i64> : public TNativePlainTypeSupport<i64>{};
-template<> struct TNativeTypeSupport<ui64> : public TNativePlainTypeSupport<ui64>{};
-
-template<> struct TNativeTypeSupport<bool> : public TNativePlainTypeSupport<bool>
-{
-    static TString ToString(const bool& value) {
-        return value?"true":"false";
-    }
-
-    static void AppendToString(const bool& value, TStringBuilder& stringBuffer) {
-        stringBuffer.append(value ? "true" : "false");
-    }
-};
-
 template<> struct TNativeTypeSupport<TString> : public std::true_type
 {
     using TLength = std::size_t;
 
     static void Serialize(const TString& value, TBinaryData& data) {
-        // Write size
-        TLength size = value.size();
-        TNativePlainTypeSupport<TLength>::Serialize(size, data);
+        TLength valueLength = value.size();
 
         // Write contents
         auto oldSize = data.size();
-        data.resize(oldSize + size);
-        auto to = data.data() + oldSize;
-        std::memcpy(to, value.c_str(), size);
+        data.resize(oldSize + valueLength + sizeof(TLength));
+
+        *(reinterpret_cast<TLength*>(data.data() + oldSize)) = valueLength;
+
+        auto to = data.data() + oldSize + sizeof(TLength);
+        std::memcpy(to, value.c_str(), valueLength);
     }
 
     static bool Deserialize(TString& value, const void* data, std::size_t length) {
@@ -107,49 +58,6 @@ template<> struct TNativeTypeSupport<TString> : public std::true_type
 
     static void AppendToString(const TString& value, TStringBuilder& stringBuffer) {
         stringBuffer.append(value);
-    }
-};
-
-template <typename T, typename V = typename std::enable_if< std::is_floating_point_v<T> >::type>
-struct TFloatingTypeSupport : public TNativePlainTypeSupport<T>
-{
-    static TString ToString(const T& value) {
-        return std::to_string(value);
-    }
-
-    static void AppendToString(const T& value, TStringBuilder& stringBuffer) {
-        stringBuffer.append(ToString(value));
-    }
-};
-
-template<> struct TNativeTypeSupport<float> : public TFloatingTypeSupport<float>{};
-template<> struct TNativeTypeSupport<double> : public TFloatingTypeSupport<double>{};
-template<> struct TNativeTypeSupport<long double> : public TFloatingTypeSupport<long double>{};
-
-template<> struct TNativeTypeSupport<NActors::TActorId> : public std::true_type
-{
-    static inline void Serialize(const NActors::TActorId& value, TBinaryData& data) {
-        auto from = reinterpret_cast<const std::uint8_t*>(&value);
-        auto oldSize = data.size();
-        data.resize(oldSize + sizeof(NActors::TActorId));
-        auto to = data.data() + oldSize;
-        std::memcpy(to, from, sizeof(NActors::TActorId));
-    }
-
-    static bool Deserialize(NActors::TActorId& value, const void* data, std::size_t length) {
-        if (sizeof(NActors::TActorId) != length) {
-            return false;
-        }
-        value =  *(reinterpret_cast<const NActors::TActorId*>(data));
-        return true;
-    }
-
-    static TString ToString(const NActors::TActorId& value) {
-        return value.ToString();
-    }
-
-    static void AppendToString(const NActors::TActorId& value, TStringBuilder& stringBuffer) {
-        stringBuffer.append(value.ToString());
     }
 };
 

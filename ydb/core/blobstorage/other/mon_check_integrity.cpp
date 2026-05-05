53dda101ac9d997aa4fde1a61e788b6ce3c59cd1
Yaroslav Dynnikov <ydynnikov@ydb.tech>
2026-05-05 17:40:58 +0300

    ai nitpick

--- ydb/core/blobstorage/other/mon_check_integrity.cpp
+++ ydb/core/blobstorage/other/mon_check_integrity.cpp
@@ -1,7 +1,6 @@
 #include "mon_check_integrity.h"
 #include <ydb/core/base/blobstorage.h>
 #include <library/cpp/monlib/service/pages/templates.h>
-#include <library/cpp/threading/future/future.h>
 
 namespace NKikimr {
 

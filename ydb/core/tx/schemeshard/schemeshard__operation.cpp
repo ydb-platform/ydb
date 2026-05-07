39d3d52dee4dc487a7f523d9defabd0113953194
Kuzin Roman <kuzin57@ydb.tech>
2026-05-07 18:12:58 +0300

    Update schemeshard__operation.cpp

--- ydb/core/tx/schemeshard/schemeshard__operation.cpp
+++ ydb/core/tx/schemeshard/schemeshard__operation.cpp
@@ -1486,9 +1486,9 @@ TVector<ISubOperation::TPtr> TDefaultOperationFactory::MakeOperationParts(
     case NKikimrSchemeOp::EOperationType::ESchemeOpFinalizeBuildIndexImplTable:
         Y_ABORT("multipart operations are handled before, also they require transaction details");
 
+
     case NKikimrSchemeOp::EOperationType::ESchemeOpInitiateBuildIndexMainTable:
         Y_ABORT("multipart operations are handled before, also they require transaction details");
-
     case NKikimrSchemeOp::EOperationType::ESchemeOpFinalizeBuildIndexMainTable:
         Y_ABORT("multipart operations are handled before, also they require transaction details");
     case NKikimrSchemeOp::EOperationType::ESchemeOpCancelIndexBuild:

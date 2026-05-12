40be5c4f07764f63a41f09288879f8e06822ed8d
Sergey Novikov <jmvssnovikov@ydb.tech>
2026-05-12 14:52:06 +0000

    fix

--- ydb/core/base/mon_auth.cpp
+++ ydb/core/base/mon_auth.cpp
@@ -11,10 +11,10 @@ const TString TABLET_DEV_UI_SECURE_PATH_INFO_PREFIX = TStringBuilder() << "/" <<
 } // namespace
 
 bool IsTabletDevUiSecurePath(TStringBuf pathInfo) {
-    if (!pathInfo.SkipPrefix(TABLET_DEV_UI_SECURE_PATH_INFO_PREFIX)) {
-        return false;
+    if (pathInfo == TABLET_DEV_UI_SECURE_PATH_INFO_PREFIX) {
+        return true;
     }
-    return pathInfo.empty() || pathInfo[0] == '/';
+    return pathInfo.StartsWith(TABLET_DEV_UI_SECURE_PATH_INFO_PREFIX + "/");
 }
 
 bool HasAdminAccessToTabletMon(const NActors::TActorContext& ctx, const NActors::NMon::TEvRemoteHttpInfo* msg) {

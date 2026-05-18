04ae20ce45bcca368a8116359119a2d3c161eae6
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-05-18 20:22:32 +0300

    fixup! add integration test

--- ydb/tests/fq/yds/test_row_dispatcher.py
+++ ydb/tests/fq/yds/test_row_dispatcher.py
@@ -324,8 +324,18 @@ class TestPqRowDispatcher(TestYdsBase):
             INSERT INTO {YDS_CONNECTION}.`{self.output_topic}`
             SELECT ToBytes(Unwrap(Json::SerializeJson(Yson::From(data))))
              FROM {YDS_CONNECTION}.`{self.input_topic}`
-                WITH (format=json_each_row, SCHEMA (time UInt64 NOT NULL, data Struct<key: String, second_key: Tuple<Int64, Bool?>> NOT NULL, event String NOT NULL))
-                WHERE (event = "event1" or event = "event2" or event = "event4" or event = "event5") AND (data.key = "value" OR data.key = "ev2" OR data.key = "{large_string}") AND data.second_key.0 > 0 AND data.second_key.1 IS DISTINCT FROM true;'''
+                WITH (
+                    format = json_each_row,
+                    SCHEMA = (
+                        time UInt64 NOT NULL,
+                        data Struct<key: String, second_key: Tuple<Int64, Bool?>> NOT NULL,
+                        event String NOT NULL
+                    )
+                )
+                WHERE (event = "event1" or event = "event2" or event = "event4" or event = "event5")
+                  AND (data.key = "value" OR data.key = "ev2" OR data.key = "{large_string}")
+                  AND data.second_key.0 > 0 AND data.second_key.1 IS DISTINCT FROM true;
+        '''
 
         query_id = start_yds_query(kikimr, client, sql)
         wait_actor_count(kikimr, "FQ_ROW_DISPATCHER_SESSION", 1)

ceed28ede3a51725ee68cebe77038a2866314370
kruall <kruall@ydb.tech>
2026-05-21 14:21:35 +0300

    Handle NODATA in keyvalue read requests  (#40801)

--- ydb/core/keyvalue/keyvalue_storage_read_request.h
+++ ydb/core/keyvalue/keyvalue_storage_read_request.h
@@ -1,17 +1,23 @@
 #pragma once
 #include "defs.h"
 #include "keyvalue_events.h"
-
 #include <util/generic/ptr.h>
 #include <util/system/types.h>
+
+#include <memory>
 
 namespace NKikimr {
 namespace NKeyValue {
 
+class TKeyValueState;
+struct TKeyValueStateLifetimeToken;
+
 IActor* CreateKeyValueStorageReadRequest(
     THolder<TIntermediate>&& intermediate,
     const TTabletStorageInfo *tabletInfo,
-    ui32 tabletGeneration);
+    ui32 tabletGeneration,
+    TKeyValueState *state,
+    std::weak_ptr<TKeyValueStateLifetimeToken> stateLifetimeToken);
 
 } // NKeyValue
 } // NKikimr

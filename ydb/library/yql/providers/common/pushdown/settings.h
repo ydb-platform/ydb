e9a69455748516886be69fb82bb6abf8d9925a58
Dmitry Kardymon <kardymon-d@ydb.tech>
2026-05-21 09:57:29 +0300

    BACKPORT-CONFLICT: manual resolution required for commit 7cbd497

--- ydb/library/yql/providers/common/pushdown/settings.h
+++ ydb/library/yql/providers/common/pushdown/settings.h
@@ -47,6 +47,11 @@ struct TSettings {
         NonDeterministic = 1 << 28,
         DecimalCtor = 1 << 29, 
         DateCtor = 1 << 30,
+<<<<<<< HEAD
+=======
+        PredicateAsExpression = ui64{1} << 31, // Predicates can be used in expressions (e.g. (a = b) = (c = d))
+        AnyExpressionExceptMember = ui64{1} << 32,
+>>>>>>> 7cbd497fd75 (YQ-5201 Predicate pushdown to topics  (#38764))
     };
 
     explicit TSettings(NLog::EComponent logComponent)
@@ -60,9 +65,13 @@ struct TSettings {
 
     void EnableFunction(const TString& functionName);
 
+    void EnableMember(const TString& memberName);
+
     bool IsEnabled(EFeatureFlag flagMask) const;
 
     bool IsEnabledFunction(const TString& functionName) const;
+
+    bool IsMemberEnabled(const TString& memberName) const;
 
     NLog::EComponent GetLogComponent() const {
         return LogComponent;
@@ -72,6 +81,7 @@ private:
     const NLog::EComponent LogComponent;
     ui64 FeatureFlags = 0;
     std::unordered_set<TString> EnabledFunctions;
+    std::unordered_set<TString> EnabledMembers;
 };
 
 } // namespace NYql::NPushdown

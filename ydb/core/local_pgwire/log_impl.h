3ceb4ce3a68a4c7edb6725ea679bc96a7cc7eefd
Konstantin Seleznyov <kseleznyov@ydb.tech>
2026-05-24 17:38:05 +0000

    Migrate BLOG_ENSURE on YDB_LOG

--- ydb/core/local_pgwire/log_impl.h
+++ ydb/core/local_pgwire/log_impl.h
@@ -3,4 +3,4 @@
 #include <ydb/library/actors/core/log.h>
 #include <ydb/library/services/services.pb.h>
 
-#define BLOG_ENSURE(condition) do{if(!condition) LOG_ERROR_S(*NActors::TlsActivationContext, NKikimrServices::LOCAL_PGWIRE, #condition);}while(false)
+#define BLOG_ENSURE(condition) do{if(!condition) YDB_LOG_COMP_ERROR(NKikimrServices::LOCAL_PGWIRE, #condition);}while(false)

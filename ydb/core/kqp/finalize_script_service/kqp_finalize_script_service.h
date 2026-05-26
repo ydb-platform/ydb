867fe0351a469f4553a97fd0557a13c0697a0736
Pisarenko Grigoriy <grigoriypisar@ydb.tech>
2026-05-26 12:38:01 +0500

    YQ-5341 fixed race in federated query setup init (#40974)

--- ydb/core/kqp/finalize_script_service/kqp_finalize_script_service.h
+++ ydb/core/kqp/finalize_script_service/kqp_finalize_script_service.h
@@ -4,16 +4,16 @@
 #include <ydb/library/yql/providers/s3/actors_factory/yql_s3_actors_factory.h>
 
 namespace NKikimrConfig {
-    class TQueryServiceConfig;
-    class TMetadataProviderConfig;
-}
+
+class TQueryServiceConfig;
+class TMetadataProviderConfig;
+
+} // namespace NKikimrConfig
 
 namespace NKikimr::NKqp {
 
 IActor* CreateKqpFinalizeScriptService(const NKikimrConfig::TQueryServiceConfig& queryServiceConfig,
-    IKqpFederatedQuerySetupFactory::TPtr federatedQuerySetupFactory,
-    std::shared_ptr<NYql::NDq::IS3ActorsFactory> s3ActorsFactory,
-    bool enableBackgroundLeaseChecks = true,
-    TDuration leaseCheckStartupTimeout = TDuration::Seconds(15));
+    const std::optional<TKqpFederatedQuerySetup>& federatedQuerySetup,
+    std::shared_ptr<NYql::NDq::IS3ActorsFactory> s3ActorsFactory);
 
 }  // namespace NKikimr::NKqp

008fdd608845d3ca16c6fc2ae6b5e856501ef7aa
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-05-27 22:28:40 +0300

    add test for disabled FullscanLimit ff

--- ydb/core/kqp/ut/federated_query/datastreams/streaming_ddl_ut.cpp
+++ ydb/core/kqp/ut/federated_query/datastreams/streaming_ddl_ut.cpp
@@ -1050,6 +1050,11 @@ Y_UNIT_TEST_SUITE(KqpStreamingQueriesDdl) {
     }
 
     Y_UNIT_TEST_QUAD_F(StreamingQueryWithStreamLookupJoin, WithFeatureFlag, WithFullscanFlag, TStreamingTestFixture) {
+        if (!WithFeatureFlag && WithFullscanFlag) {
+            // legal, but nothing to check
+            return;
+        }
+        constexpr ui32 combinations = WithFeatureFlag && !WithFullscanFlag ? 2 : 1;
         {
             auto& setupAppConfig = SetupAppConfig();
             setupAppConfig.MutableQueryServiceConfig()->SetProgressStatsPeriodMs(0);
@@ -1088,8 +1093,11 @@ Y_UNIT_TEST_SUITE(KqpStreamingQueriesDdl) {
             SetupMockConnectorTableDescription(connectorClient, {
                 .TableName = ydbTable,
                 .Columns = columns,
-                .DescribeCount = 2,
-                .ListSplitsCount = WithFeatureFlag ? (WithFullscanFlag ? 7 : 4) : 0,
+                .DescribeCount = 2*combinations,
+                // 1 for table-vs-topic, 1 for compilation, doubled for *Bad query compilation
+                .ListSplitsCount = WithFeatureFlag ? (WithFullscanFlag ? 1 + 3 * 2 : 1 + 3 + 1) : 0,
+                // 1 for compilation (and another one for *Bad query compilation)
+                // 3 for lookups, doubled for fullscan mode
                 .ValidateListSplitsArgs = false
             });
 
@@ -1099,7 +1107,7 @@ Y_UNIT_TEST_SUITE(KqpStreamingQueriesDdl) {
                 SetupMockConnectorTableData(connectorClient, {
                     .TableName = ydbTable,
                     .Columns = columns,
-                    .NumberReadSplits = (WithFullscanFlag ? 6 : 3),
+                    .NumberReadSplits = (WithFullscanFlag ? 3*2 : 3),
                     .ValidateReadSplitsArgs = false,
                     .ResultFactory = [&]() {
                         readSplitsCount += 1;
@@ -1185,6 +1193,42 @@ Y_UNIT_TEST_SUITE(KqpStreamingQueriesDdl) {
             UNIT_ASSERT(ast);
             UNIT_ASSERT_STRING_CONTAINS(*ast, "DqCnStreamLookup");
         });
+
+        if (!WithFullscanFlag) {
+            // extra check that fullscan option without fullscan feature-flag fails
+            ExecQuery(fmt::format(R"(
+                CREATE STREAMING QUERY `{query_name}Bad` AS
+                DO BEGIN
+                    $ydb_lookup = SELECT * FROM `{ydb_source}`.`{ydb_table}`;
+
+                    $pq_source = SELECT * FROM `{pq_source}`.`{input_topic}` WITH (
+                        FORMAT = "json_each_row",
+                        SCHEMA (
+                            time Int32 NOT NULL,
+                            event String,
+                            host String
+                        )
+                    );
+
+                    $joined = SELECT l.payload AS payload, p.* FROM $pq_source AS p
+                    LEFT JOIN /*+ streamlookup(FullscanLimit 123) */ ANY $ydb_lookup AS l
+                    ON (l.fqdn = p.host);
+
+                    INSERT INTO `{pq_source}`.`{output_topic}`
+                    SELECT Unwrap(event || "-" || payload) FROM $joined
+                END DO;)",
+                "query_name"_a = queryName,
+                "pq_source"_a = pqSourceName,
+                "ydb_source"_a = ydbSourceName,
+                "ydb_table"_a = ydbTable,
+                "input_topic"_a = inputTopicName,
+                "output_topic"_a = outputTopicName
+            ),
+            EStatus::GENERIC_ERROR,
+            "EnableDqSourceStreamLookupJoinFullscan disabled, but FullscanLimit is 123");
+
+            CheckScriptExecutionsCount(2, 1);
+        }
     }
 
     Y_UNIT_TEST_F(StreamingQueryWithLocalYdbJoin, TStreamingTestFixture) {

4ced5495a790f932f311994a78ed80e0cadda5fe
st-shchetinin <st-shchetinin@ydb.tech>
2026-05-28 15:14:33 +0300

    done

--- ydb/core/driver_lib/run/run.cpp
+++ ydb/core/driver_lib/run/run.cpp
@@ -2295,7 +2295,9 @@ void TKikimrRunner::KikimrStop(bool graceful) {
             THolder<TEvent> event = MakeHolder<TEvent>();
             event->Record.SetNodeId(nodeId);
 
-            NTabletPipe::SendData({}, nodeBrokerPipe, event.Release());
+            auto pipeEv = new IEventHandle(nodeBrokerPipe, TActorId(), event.Release());
+            pipeEv->Rewrite(TEvTabletPipe::EvSend, nodeBrokerPipe);
+            ActorSystem->Send(pipeEv);
         }
     }
 

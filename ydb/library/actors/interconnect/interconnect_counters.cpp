357752d0dfbd6d9d0fc26d7e70e9cb9bb0731dc8
Daniil Cherednik <dcherednik@ydb.tech>
2026-06-01 13:10:58 +0200

    Add interconnect queue depth histogram

--- ydb/library/actors/interconnect/interconnect_counters.cpp
+++ ydb/library/actors/interconnect/interconnect_counters.cpp
@@ -314,6 +314,10 @@ namespace {
             InterconnectQueueTimeHistogram->Collect(value);
         }
 
+        void UpdateNumEventsInQueueHistogram(ui64 value) override {
+            NumEventsInQueueHistogram->Collect(value);
+        }
+
         void UpdateRdmaReadTimeHistogram(ui64 value) override {
             RdmaReadTimeHistogram->Collect(value);
         }
@@ -398,6 +402,8 @@ namespace {
                     "PingTimeUs", NMonitoring::ExponentialHistogram(18, 2, 125));
                 InterconnectQueueTimeHistogram = AdaptiveCounters->GetHistogram(
                     "InterconnectQueueTimeHistogramUs", NMonitoring::ExplicitHistogram({500, 1000, 5000, 10000, 50000, 100000}));
+                NumEventsInQueueHistogram = AdaptiveCounters->GetHistogram(
+                    "NumEventsInQueue", NMonitoring::ExplicitHistogram({0, 1, 2, 6, 24, 120}));
                 RdmaReadTimeHistogram = AdaptiveCounters->GetHistogram(
 +                    "RdmaReadTimeUs", NMonitoring::ExplicitHistogram({0, 5, 10, 20, 50, 100, 200, 1000, 10000}));
                 RdmaMultipartEvents = AdaptiveCounters->GetCounter("RdmaMultipartEvents", true);
@@ -497,6 +503,7 @@ namespace {
         NMonitoring::TDynamicCounters::TCounterPtr SpuriousWriteWakeups;
         NMonitoring::THistogramPtr PingTimeHistogram;
         NMonitoring::THistogramPtr InterconnectQueueTimeHistogram;
+        NMonitoring::THistogramPtr NumEventsInQueueHistogram;
         NMonitoring::THistogramPtr RdmaReadTimeHistogram;
         NMonitoring::TDynamicCounters::TCounterPtr RdmaMultipartEvents;
 
@@ -757,6 +764,10 @@ namespace {
             InterconnectQueueTimeHistogram_->Record(value);
         }
 
+        void UpdateNumEventsInQueueHistogram(ui64 value) override {
+            NumEventsInQueueHistogram_->Record(value);
+        }
+
         void UpdateRdmaReadTimeHistogram(ui64 value) override {
             RdmaReadTimeHistogram_->Record(value);
         }
@@ -859,6 +870,8 @@ namespace {
                         NMonitoring::MakeLabels({{"sensor", "interconnect.ping_time_us"}}), NMonitoring::ExponentialHistogram(18, 2, 125));
                 InterconnectQueueTimeHistogram_ = AdaptiveMetrics_->HistogramRate(
                         NMonitoring::MakeLabels({{"sensor", "interconnect.ic_queue_time_us"}}), NMonitoring::ExplicitHistogram({500, 1000, 5000, 10000, 50000, 100000}));
+                NumEventsInQueueHistogram_ = AdaptiveMetrics_->HistogramRate(
+                        NMonitoring::MakeLabels({{"sensor", "interconnect.num_events_in_queue"}}), NMonitoring::ExplicitHistogram({0, 1, 2, 6, 24, 120}));
                 RdmaReadTimeHistogram_ = AdaptiveMetrics_->HistogramRate(
                         NMonitoring::MakeLabels({{"sensor", "interconnect.rdma_read_time_us"}}), NMonitoring::ExplicitHistogram({0, 5, 10, 20, 50, 100, 200, 1000, 10000}));
                 RdmaMultipartEvents_ = createRate(AdaptiveMetrics_, "interconnect.rdma_multipart_events");
@@ -990,6 +1003,7 @@ namespace {
 
         NMonitoring::IHistogram* PingTimeHistogram_;
         NMonitoring::IHistogram* InterconnectQueueTimeHistogram_;
+        NMonitoring::IHistogram* NumEventsInQueueHistogram_;
         NMonitoring::IHistogram* RdmaReadTimeHistogram_;
         NMonitoring::IRate* RdmaMultipartEvents_;
 

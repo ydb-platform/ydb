7bcede92d4a2aaf9057438c24c25ec992e77944c
Sergey Shokhor <shokhor@yandex-team.ru>
2026-06-01 19:03:29 +0300

    Provide IP in topic data response

--- ydb/core/viewer/viewer_topic_data.cpp
+++ ydb/core/viewer/viewer_topic_data.cpp
@@ -216,6 +216,7 @@ void TTopicData::FillProtoResponse(ui64 maxTotalSize) {
         }
         messageProto->SetProducerId(decodedSrcId);
         messageProto->SetSeqNo(r.GetSeqNo());
+        messageProto->SetIp(r.GetIp());
 
         if (dataChunk.MessageMetaSize() > 0) {
             for (const auto& metadata : dataChunk.GetMessageMeta()) {

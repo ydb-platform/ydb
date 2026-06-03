a8e611bd9e335ace588265a06bb25786b2ca4d56
Pisarenko Grigoriy <grigoriypisar@ydb.tech>
2026-06-03 17:51:38 +0300

    YQ-5302 refactored kikimr type annotation (#40750)

--- ydb/core/kqp/opt/kqp_opt_hash_func_propagate_transformer.h
+++ ydb/core/kqp/opt/kqp_opt_hash_func_propagate_transformer.h
@@ -1,27 +1,24 @@
 #pragma once
 
-#include "kqp_opt.h"
-
 #include <ydb/core/kqp/common/kqp_yql.h>
+
 #include <yql/essentials/ast/yql_expr.h>
 #include <yql/essentials/core/yql_graph_transformer.h>
 #include <yql/essentials/core/yql_expr_optimize.h>
 #include <yql/essentials/core/yql_expr_type_annotation.h>
 #include <yql/essentials/core/yql_opt_utils.h>
 
+namespace NYql {
 
-namespace NKikimr {
-namespace NKqp {
+struct TKikimrConfiguration;
 
-using namespace NYql;
-using namespace NYql::NNodes;
-using namespace NOpt;
+} // namespace NYql
 
-TAutoPtr<IGraphTransformer> CreateKqpTxsHashFuncPropagateTransformer(
-    TAutoPtr<NYql::IGraphTransformer> typeAnnTransformer,
-    TTypeAnnotationContext& typesCtx,
-    const TKikimrConfiguration::TPtr& config
+namespace NKikimr::NKqp {
+
+TAutoPtr<NYql::IGraphTransformer> CreateKqpTxsHashFuncPropagateTransformer(
+    NYql::TTypeAnnotationContext& typesCtx,
+    const TIntrusivePtr<NYql::TKikimrConfiguration>& config
 );
 
-}
-}
+} // namespace NKikimr::NKqp

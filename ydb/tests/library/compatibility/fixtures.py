dbf128ee3aa1322a257a8997db5ab5a7fdbeb756
Pisarenko Grigoriy <grigoriypisar@ydb.tech>
2026-06-10 17:27:38 +0300

    Fixed extra feature flags type and log name typo

--- ydb/tests/library/compatibility/fixtures.py
+++ ydb/tests/library/compatibility/fixtures.py
@@ -264,10 +264,10 @@ class RollingUpgradeAndDowngradeFixture:
     def setup_cluster(self, tenant_db=None, **kwargs):
         extra_feature_flags = kwargs.pop("extra_feature_flags", {})
         extra_feature_flags = copy.copy(extra_feature_flags)
-        extra_feature_flags["suppress_compatibility_check"] = True
+        extra_feature_flags.append("suppress_compatibility_check")
         # We want to drain tablets before stopping, to prevent "Failed to resolve tablet: 72075186224037909 after several retries"
         # By default draining is not enabled to faster tests
-        extra_feature_flags["enable_drain_on_shutdown"] = True
+        extra_feature_flags.append("enable_drain_on_shutdown")
         self.config = KikimrConfigGenerator(
             erasure=kwargs.pop("erasure", Erasure.MIRROR_3_DC),
             binary_paths=[self.all_binary_paths[0]],

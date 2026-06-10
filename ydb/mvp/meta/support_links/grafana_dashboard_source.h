bed9047cc9361c0a8b3292d490291756611a642b
StekPerepolnen <arykov@ydb.tech>
2026-06-10 15:58:17 +0200

    config

--- ydb/mvp/meta/support_links/grafana_dashboard_source.h
+++ ydb/mvp/meta/support_links/grafana_dashboard_source.h
@@ -11,15 +11,16 @@ namespace NMVP::NSupportLinks {
 
 class TGrafanaDashboardSource : public ILinkSource {
 public:
-    TGrafanaDashboardSource(TString sourceName, TString title, TString url, TString grafanaEndpoint)
+    TGrafanaDashboardSource(TString sourceName, TString title, TString url, TString grafanaEndpoint, TResolvedParamBindings paramBindings)
         : SourceName(std::move(sourceName))
         , Title(std::move(title))
         , Url(std::move(url))
         , GrafanaEndpoint(std::move(grafanaEndpoint))
+        , ParamBindings(std::move(paramBindings))
     {}
 
     TResolveOutput Resolve(const ILinkSource::TLinkResolveInput& input, const ILinkSource::TResolveContext&) const override {
-        TString resolvedUrl = BuildGrafanaDashboardUrl(GrafanaEndpoint, Url, input);
+        TString resolvedUrl = BuildGrafanaDashboardUrl(GrafanaEndpoint, Url, input, ParamBindings);
         TResolveOutput result{
             .Name = SourceName,
         };
@@ -37,6 +38,7 @@ private:
     TString Title;
     TString Url;
     TString GrafanaEndpoint;
+    TResolvedParamBindings ParamBindings;
 };
 
 inline void ValidateGrafanaDashboardSourceConfig(const TSupportLinkEntryConfig& config, const TMetaSettings& metaSettings) {
@@ -46,16 +48,14 @@ inline void ValidateGrafanaDashboardSourceConfig(const TSupportLinkEntryConfig& 
     if (!IsAbsoluteUrl(config.GetUrl()) && metaSettings.SupportLinks.GrafanaEndpoint.empty()) {
         ythrow yexception() << "grafana.endpoint is required for relative url";
     }
+    ValidateResolvedParamBindings(ResolveParamBindings(config, BuildDefaultGrafanaDashboardParamBindings()), config);
 }
 
 inline std::shared_ptr<ILinkSource> MakeGrafanaDashboardSource(TSupportLinkEntryConfig config, const TMetaSettings& metaSettings) {
     ValidateGrafanaDashboardSourceConfig(config, metaSettings);
+    auto paramBindings = ResolveParamBindings(config, BuildDefaultGrafanaDashboardParamBindings());
     return std::make_shared<TGrafanaDashboardSource>(
-        config.GetSource(),
-        config.GetTitle(),
-        config.GetUrl(),
-        metaSettings.SupportLinks.GrafanaEndpoint
-    );
+        config.GetSource(), config.GetTitle(), config.GetUrl(), metaSettings.SupportLinks.GrafanaEndpoint, std::move(paramBindings));
 }
 
 } // namespace NMVP::NSupportLinks
---
+++ ydb/mvp/meta/support_links/param_bindings.h
@@ -0,0 +1,145 @@
+#pragma once
+
+#include "source.h"
+
+#include <util/generic/hash.h>
+#include <util/generic/hash_set.h>
+#include <util/generic/vector.h>
+#include <util/generic/yexception.h>
+
+namespace NMVP::NSupportLinks {
+
+struct TResolvedParamBindings {
+    TVector<std::pair<TString, TString>> RequestMappings;
+    TVector<std::pair<TString, TString>> ClusterInfoMappings;
+    TVector<std::pair<TString, TString>> StaticMappings;
+};
+
+inline TResolvedParamBindings ResolveConfiguredParamMappings(const TSupportLinkEntryConfig& config) {
+    TResolvedParamBindings paramBindings;
+    paramBindings.RequestMappings.reserve(config.LinkParameterMappingsSize());
+    paramBindings.ClusterInfoMappings.reserve(config.LinkParameterMappingsSize());
+    paramBindings.StaticMappings.reserve(config.LinkParameterMappingsSize());
+
+    const int linkParameterMappingsSize = config.LinkParameterMappingsSize();
+    for (int i = 0; i < linkParameterMappingsSize; ++i) {
+        const auto& mapping = config.GetLinkParameterMappings(i);
+        if (!mapping.HasParameter() || mapping.GetParameter().empty()) {
+            ythrow yexception() << "link_parameter_mappings.parameter is required for source=" << config.GetSource();
+        }
+
+        const bool hasFromRequest = mapping.HasFromRequest();
+        const bool hasFromClusterInfo = mapping.HasFromClusterInfo();
+        const bool hasStaticValue = mapping.HasStaticValue();
+        const size_t sourceCount = hasFromRequest + hasFromClusterInfo + hasStaticValue;
+        if (sourceCount != 1) {
+            ythrow yexception()
+                << "link_parameter_mappings.parameter=" << mapping.GetParameter()
+                << " must set exactly one of from_request, from_cluster_info or static_value for source="
+                << config.GetSource();
+        }
+
+        if (hasFromRequest) {
+            paramBindings.RequestMappings.emplace_back(mapping.GetFromRequest(), mapping.GetParameter());
+            continue;
+        }
+
+        if (hasFromClusterInfo) {
+            paramBindings.ClusterInfoMappings.emplace_back(mapping.GetFromClusterInfo(), mapping.GetParameter());
+        } else {
+            paramBindings.StaticMappings.emplace_back(mapping.GetStaticValue(), mapping.GetParameter());
+        }
+    }
+
+    return paramBindings;
+}
+
+inline TResolvedParamBindings ResolveParamBindings(const TSupportLinkEntryConfig& config, const TResolvedParamBindings& defaultParamBindings) {
+    if (config.LinkParameterMappingsSize() != 0) {
+        return ResolveConfiguredParamMappings(config);
+    }
+
+    return defaultParamBindings;
+}
+
+inline void ValidateResolvedParamBindings(const TResolvedParamBindings& paramBindings, const TSupportLinkEntryConfig& config) {
+    THashSet<TString> labels;
+
+    for (const auto& [requestParamName, targetLabel] : paramBindings.RequestMappings) {
+        Y_UNUSED(requestParamName);
+        if (!labels.insert(targetLabel).second) {
+            ythrow yexception()
+                << "duplicate target label '" << targetLabel
+                << "' in link_parameter_mappings for source=" << config.GetSource();
+        }
+    }
+
+    for (const auto& [clusterInfoField, targetLabel] : paramBindings.ClusterInfoMappings) {
+        Y_UNUSED(clusterInfoField);
+        if (!labels.insert(targetLabel).second) {
+            ythrow yexception()
+                << "duplicate target label '" << targetLabel
+                << "' in link_parameter_mappings for source=" << config.GetSource();
+        }
+    }
+
+    for (const auto& [staticValue, targetLabel] : paramBindings.StaticMappings) {
+        Y_UNUSED(staticValue);
+        if (!labels.insert(targetLabel).second) {
+            ythrow yexception()
+                << "duplicate target label '" << targetLabel
+                << "' in link_parameter_mappings for source=" << config.GetSource();
+        }
+    }
+}
+
+inline TVector<std::pair<TString, TString>> BuildRequestParamValues(const TCgiParameters& requestParameters, const TVector<std::pair<TString, TString>>& requestMappings) {
+    TVector<std::pair<TString, TString>> paramValues;
+
+    for (const auto& [requestParamName, targetLabel] : requestMappings) {
+        const TString value = requestParameters.Get(requestParamName);
+        if (!value.empty()) {
+            paramValues.emplace_back(targetLabel, value);
+        }
+    }
+
+    return paramValues;
+}
+
+inline TVector<std::pair<TString, TString>> BuildNonIdentityRequestParamValues(const TCgiParameters& requestParameters) {
+    TVector<std::pair<TString, TString>> paramValues;
+
+    for (const auto& [name, value] : requestParameters) {
+        if (!IsIdentityRequestParameter(name) && !value.empty()) {
+            paramValues.emplace_back(name, value);
+        }
+    }
+
+    return paramValues;
+}
+
+inline TVector<std::pair<TString, TString>> BuildClusterInfoParamValues(const THashMap<TString, TString>& clusterInfo, const TVector<std::pair<TString, TString>>& clusterInfoMappings) {
+    TVector<std::pair<TString, TString>> paramValues;
+
+    for (const auto& [clusterInfoField, targetLabel] : clusterInfoMappings) {
+        const auto it = clusterInfo.find(clusterInfoField);
+        if (it != clusterInfo.end() && !it->second.empty()) {
+            paramValues.emplace_back(targetLabel, it->second);
+        }
+    }
+
+    return paramValues;
+}
+
+inline TVector<std::pair<TString, TString>> BuildStaticParamValues(const TVector<std::pair<TString, TString>>& staticMappings) {
+    TVector<std::pair<TString, TString>> paramValues;
+    paramValues.reserve(staticMappings.size());
+
+    for (const auto& [staticValue, targetLabel] : staticMappings) {
+        paramValues.emplace_back(targetLabel, staticValue);
+    }
+
+    return paramValues;
+}
+
+} // namespace NMVP::NSupportLinks

fe53518d71bc6ca4e223b076e935ace13b1a7f09
kseleznyov <kseleznyov@ydb.tech>
2026-06-15 19:06:57 +0300

    Update ydb/core/local_pgwire/log_impl.h

--- ydb/core/local_pgwire/log_impl.h
+++ ydb/core/local_pgwire/log_impl.h
@@ -3,4 +3,3 @@
 #include <ydb/library/actors/core/log.h>
 #include <ydb/library/services/services.pb.h>
 
-#define BLOG_ENSURE(condition) do{if(!condition) YDB_LOG_COMP_ERROR(NKikimrServices::LOCAL_PGWIRE, #condition);}while(false)

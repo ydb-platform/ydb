2a62af18f4f0dfb9ffbd1ae133e461be8b1273e6
Oleg Doronin <dorooleg@yandex.ru>
2026-06-16 11:27:19 +0300

    transaction reply has been fixed (#43499)

--- ydb/core/tx/columnshard/transactions/tx_controller.h
+++ ydb/core/tx/columnshard/transactions/tx_controller.h
@@ -209,6 +209,8 @@ public:
 
     private:
         mutable TAtomicCounter PreparationsStarted = 0;
+        bool NeedResendReplyFlag = false;
+        std::optional<bool> StartedAsync;
 
         friend class TTxController;
         virtual bool DoParse(TColumnShard& owner, const TString& data) = 0;
@@ -247,12 +249,13 @@ public:
         }
 
         void ResetStatusOnUpdate() {
+            if (Status && *Status == EStatus::ReplySent) {
+                NeedResendReplyFlag = true;
+            }
             Status = {};
         }
 
         virtual TString DoDebugString() const = 0;
-
-        std::optional<bool> StartedAsync;
 
     public:
         using TPtr = std::shared_ptr<ITransactionOperator>;
@@ -261,6 +264,10 @@ public:
 
         bool IsInProgress() const {
             return DoIsInProgress();
+        }
+
+        bool NeedResendReply() const {
+            return NeedResendReplyFlag;
         }
 
         bool PingTimeout(TColumnShard& owner, const TMonotonic now) {

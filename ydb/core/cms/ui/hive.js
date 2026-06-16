50cada63b0a1872a2021906c9b5592a9be444e25
Igor <igorkoshkarev4qqq@mail.ru>
2026-06-16 12:42:01 +0000

    fix issue

--- ydb/core/cms/ui/hive.js
+++ ydb/core/cms/ui/hive.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// Shared with datashard.js: proxy-safe mon URLs under /tablets/app[...].
-var EnableTabletDevUiSecurePath = false;
-
 function getMonRootPath() {
     var marker = '/tablets/app';
     var markerPos = window.location.pathname.indexOf(marker);
@@ -14,7 +11,7 @@ function makeMonUrl(path) {
 }
 
 function getTabletDevUiPath() {
-    return EnableTabletDevUiSecurePath ? 'app/secure' : 'app';
+    return window.FeatureFlags.enableTabletDevUiSecurePath ? 'app/secure' : 'app';
 }
 
 function makeTabletDevUiUrl(queryAndMaybeHash) {

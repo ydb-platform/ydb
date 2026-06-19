76f15ce92abc6ffdb1f364a2063335cd03a746df
Ivan Efimov <efimov-ivan@yandex-team.ru>
2026-06-19 14:05:16 +0200

    Fix partition tests crashes on interactions with destroyed actor system (#43617)

--- ydb/core/nbs/cloud/blockstore/libs/storage/partition_direct/partition_direct_ut.cpp
+++ ydb/core/nbs/cloud/blockstore/libs/storage/partition_direct/partition_direct_ut.cpp
@@ -45,7 +45,31 @@ struct TScopedNbsService: TDisableCopyMove
 
 ////////////////////////////////////////////////////////////////////////////////
 
-[[nodiscard]] TScopedNbsService SetupStorage(
+[[nodiscard]] NKikimrConfig::TNbsConfig CreateNbsConfig(
+    EWriteMode writeMode,
+    TDuration writeHedgingDelay = TDuration::Seconds(1),
+    ui64 pbufferCleanupLsnStep = 0,
+    ui32 syncRequestsBatchSize = 0)
+{
+    NKikimrConfig::TNbsConfig nbsConfig;
+    auto* storageConfig = nbsConfig.MutableNbsStorageConfig();
+    storageConfig->SetDDiskPoolName(DDiskPoolName);
+    storageConfig->SetPersistentBufferDDiskPoolName(
+        PersistentBufferDDiskPoolName);
+    storageConfig->SetWriteMode(GetProtoWriteMode(writeMode));
+    storageConfig->SetVChunkSize(DefaultVChunkSize);
+    storageConfig->SetWriteHedgingDelay(writeHedgingDelay.MicroSeconds());
+    storageConfig->SetPBufferCleanupLsnStep(pbufferCleanupLsnStep);
+    if (syncRequestsBatchSize) {
+        storageConfig->SetSyncRequestsBatchSize(syncRequestsBatchSize);
+    }
+
+    return nbsConfig;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+[[nodiscard]] std::unique_ptr<TScopedNbsService> SetupStorage(
     TEnvironmentSetup& env,
     EWriteMode writeMode,
     TDuration writeHedgingDelay = TDuration::Seconds(1),
@@ -76,20 +100,11 @@ struct TScopedNbsService: TDisableCopyMove
     }
 
     // Setup NBS service with storage config
-    NKikimrConfig::TNbsConfig nbsConfig;
-    auto* storageConfig = nbsConfig.MutableNbsStorageConfig();
-    storageConfig->SetDDiskPoolName(DDiskPoolName);
-    storageConfig->SetPersistentBufferDDiskPoolName(
-        PersistentBufferDDiskPoolName);
-    storageConfig->SetWriteMode(GetProtoWriteMode(writeMode));
-    storageConfig->SetVChunkSize(DefaultVChunkSize);
-    storageConfig->SetWriteHedgingDelay(writeHedgingDelay.MicroSeconds());
-    storageConfig->SetPBufferCleanupLsnStep(pbufferCleanupLsnStep);
-    if (syncRequestsBatchSize) {
-        storageConfig->SetSyncRequestsBatchSize(syncRequestsBatchSize);
-    }
-
-    return TScopedNbsService(nbsConfig);
+    return std::make_unique<TScopedNbsService>(CreateNbsConfig(
+        writeMode,
+        writeHedgingDelay,
+        pbufferCleanupLsnStep,
+        syncRequestsBatchSize));
 }
 
 NKikimrBlockStore::TVolumeConfig CreateVolumeConfig(ui64 blockCount)
@@ -985,8 +1000,13 @@ Y_UNIT_TEST_SUITE(TPartitionDirectTest)
         }
 
         {
+            scopedService.reset();
+
             env.RestartNode(env.Settings.ControllerNodeId);
             env.Sim(TDuration::Seconds(1));
+
+            scopedService = std::make_unique<TScopedNbsService>(
+                CreateNbsConfig(EWriteMode::PBufferReplication));
         }
 
         WaitForTabletBoot(env);

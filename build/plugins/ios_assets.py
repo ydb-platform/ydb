b3c1c6936a503392356b78c1bbb323d726089fcd
svidyuk <svidyuk@yandex-team.com>
2026-06-20 20:42:14 +0300

    port most of py maros to core args parser commit_hash:167ae843c832ad5c5f5b018be6e523664b08c75e

--- build/plugins/ios_assets.py
+++ build/plugins/ios_assets.py
@@ -1,28 +1,26 @@
-import _common as common
 import ymake
 import os
 
 
 @ymake.macro
-def IOS_ASSETS(unit: ymake.Unit, *args: tuple[str, ...]):
-    _, kv = common.sort_by_keywords({'ROOT': 1, 'CONTENTS': -1, 'FLAGS': -1}, args)
-    if not kv.get('ROOT', []) and kv.get('CONTENTS', []):
+def IOS_ASSETS(unit: ymake.Unit, *, ROOT: str = '', CONTENTS: tuple[str, ...] = (), FLAGS: tuple[str, ...] = ()):
+    if not ROOT and CONTENTS:
         ymake.report_configure_error('Please specify ROOT directory for assets')
-    origin_root = kv.get('ROOT')[0]
-    destination_root = os.path.normpath(os.path.join('$BINDIR', os.path.basename(origin_root)))
+
+    destination_root = os.path.normpath(os.path.join('$BINDIR', os.path.basename(ROOT)))
     rel_list = []
-    for cont in kv.get('CONTENTS', []):
-        rel = os.path.relpath(cont, origin_root)
+    for cont in CONTENTS:
+        rel = os.path.relpath(cont, ROOT)
         if rel.startswith('..'):
-            ymake.report_configure_error('{} is not subpath of {}'.format(cont, origin_root))
+            ymake.report_configure_error('{} is not subpath of {}'.format(cont, ROOT))
         rel_list.append(rel)
     if not rel_list:
         return
-    results_list = [os.path.join('$B', unit.path()[3:], os.path.basename(origin_root), i) for i in rel_list]
-    if len(kv.get('CONTENTS', [])) != len(results_list):
+    results_list = [os.path.join('$B', unit.path()[3:], os.path.basename(ROOT), i) for i in rel_list]
+    if len(CONTENTS) != len(results_list):
         ymake.report_configure_error('IOS_ASSETTS content length is not equals results')
-    for s, d in zip(kv.get('CONTENTS', []), results_list):
+    for s, d in zip(CONTENTS, results_list):
         unit.oncopy_file([s, d])
-    if kv.get('FLAGS', []):
-        unit.onios_app_assets_flags(kv.get('FLAGS', []))
+    if FLAGS:
+        unit.onios_app_assets_flags(list(FLAGS))
     unit.on_ios_assets([destination_root] + results_list)

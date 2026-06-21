d22d831a4f22f1bc41a43e91e3abc958c2e67fb7
Evgeny Zverev <zverevgeny@ydb.tech>
2026-06-21 12:41:24 +0000

    fix bugs

--- yql/essentials/core/type_ann/type_ann_core.cpp
+++ yql/essentials/core/type_ann/type_ann_core.cpp
@@ -2465,6 +2465,7 @@ namespace NTypeAnnImpl {
                     .Add(0, input->HeadPtr())
                     .List(1)
                         .Atom(0, "_yql_", TNodeFlags::Default)
+                        .Atom(1, "__ydb_", TNodeFlags::Default)
                     .Seal()
                 .Seal()
                 .Build();

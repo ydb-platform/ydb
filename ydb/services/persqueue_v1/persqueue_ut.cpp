f16c60b5555d5fd1e73bace5bfbe3a4c88d3ba95
Irina Skvortsova <nerhneiro@yandex-team.ru>
2026-06-23 13:39:26 +0000

    remove comment

--- ydb/services/persqueue_v1/persqueue_ut.cpp
+++ ydb/services/persqueue_v1/persqueue_ut.cpp
@@ -8020,8 +8020,6 @@ Y_UNIT_TEST_SUITE(TPersQueueTest) {
 
     Y_UNIT_TEST(DisableDeduplication) {
         auto server = SetupLbFederationServerWithTopic("topic1", 3, {"user", "debug"});
-        // Topic is created at /Root/LbAccount/account/topic1 and addressed by its
-        // federation short name (no rt3-dc naming).
         const TString topicPath = "account/topic1";
 
         NYdb::TDriverConfig driverCfg;

b900f0748731e5657b29d625b3ad189e565fbb97
Alexey Pozdniakov <vokayndzop@ydb.tech>
2026-06-24 11:30:15 +0300

    Update test_watermarks.py

--- ydb/tests/fq/streaming/test_watermarks.py
+++ ydb/tests/fq/streaming/test_watermarks.py
@@ -10,19 +10,21 @@ logger = logging.getLogger(__name__)
 
 class TestWatermarksInYdb(StreamingTestBase):
     @pytest.mark.parametrize("shared_reading", [False, True], ids=["no_shared", "shared"])
+    @pytest.mark.parametrize("tasks", [1, 2])
     @pytest.mark.parametrize("local_topics", [True, False])
-    def test_watermarks(self: StreamingTestBase, kikimr: Kikimr, entity_name: Callable[[str], str], shared_reading: bool, local_topics: bool) -> None:
+    def test_watermarks(self: StreamingTestBase, kikimr: Kikimr, entity_name: Callable[[str], str], shared_reading: bool, tasks: int, local_topics: bool) -> None:
         if local_topics and shared_reading:
             pytest.skip("Shared reading is not supported for local topics: YQ-5036")
 
-        tasks = 2
         endpoint = self.get_endpoint(kikimr, local_topics)
-        query_name = f"test_watermarks_{shared_reading}{local_topics}"
+        query_name = f"test_watermarks_{shared_reading}{tasks}{local_topics}"
         source_name = entity_name(query_name)
         self.init_topics(source_name, partitions_count=tasks, endpoint=endpoint)
         self.create_source(kikimr, source_name, shared_reading)
 
         cluster = f"{source_name}." if not local_topics else ""
+        idleness_clause = ', WATERMARK_IDLE_TIMEOUT = "PT5S"' if tasks > 1 else ''
+
         sql = f'''
             CREATE STREAMING QUERY `{query_name}` AS DO BEGIN
             PRAGMA ydb.MaxTasksPerStage = '{tasks}';
@@ -35,8 +37,8 @@ class TestWatermarksInYdb(StreamingTestBase):
                     {cluster}{self.input_topic} WITH (
                         FORMAT = json_each_row,
                         SCHEMA (ts String, pass Uint64),
-                        WATERMARK = CAST(ts AS Timestamp) - Interval('PT5S'),
-                        WATERMARK_IDLE_TIMEOUT = 'PT5S'
+                        WATERMARK = CAST(ts AS Timestamp) - Interval('PT5S')
+                        {idleness_clause}
                     ) AS input
             );
 
@@ -63,10 +65,8 @@ class TestWatermarksInYdb(StreamingTestBase):
             );
 
             INSERT INTO {cluster}{self.output_topic}
-            SELECT
-                ToBytes(Unwrap(Yson::SerializeJson(Yson::From(TableRow()))))
-            FROM
-                $output
+            SELECT ToBytes(Unwrap(Yson::SerializeJson(Yson::From(ts))))
+            FROM $output;
             END DO;
         '''
         kikimr.ydb_client.query(sql)
@@ -81,11 +81,12 @@ class TestWatermarksInYdb(StreamingTestBase):
             endpoint=endpoint,
             partition_key=b'1',
         )
-        time.sleep(30)  # leave a bit more time to fire up idle timeout
+        if shared_reading and tasks > 1:
+            time.sleep(10)  # leave a bit more time to fire up idle timeout
 
         expected = [
-            '{"event_time":"1970-01-01T00:00:42Z","ts":[["1970-01-01T00:00:40Z"]]}',
-            '{"event_time":"1970-01-01T00:00:52Z","ts":[["1970-01-01T00:00:50Z"]]}',
+            '[["1970-01-01T00:00:40Z"]]',
+            '[["1970-01-01T00:00:50Z"]]',
         ]
         actual = self.read_stream(len(expected), topic_path=self.output_topic, endpoint=endpoint)
         assert sorted(actual) == expected

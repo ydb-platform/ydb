44559e361c7f95a94b2f400b0ac097c64def49d4
Aleksei Pozdniakov <vokayndzop@ydb.tech>
2026-06-25 11:26:03 +0000

    resolve conflicts

--- ydb/tests/fq/streaming/test_watermarks.py
+++ ydb/tests/fq/streaming/test_watermarks.py
@@ -9,28 +9,19 @@ logger = logging.getLogger(__name__)
 
 
 class TestWatermarksInYdb(StreamingTestBase):
-<<<<<<< HEAD
-    @pytest.mark.parametrize("kikimr", [{"enable_watermarks": True}], indirect=["kikimr"])
-=======
->>>>>>> 371628977a6 ([YQ-5332] Watermarks: DQ: idle partitions fix (#43152))
     @pytest.mark.parametrize("shared_reading", [False, True], ids=["no_shared", "shared"])
     @pytest.mark.parametrize("tasks", [1, 2])
     @pytest.mark.parametrize("local_topics", [True, False])
-    @pytest.mark.parametrize("enable_watermarks_advanced", [True, False])
-    def test_watermarks(self: StreamingTestBase, kikimr: Kikimr, entity_name: Callable[[str], str], shared_reading: bool, tasks: int, local_topics: bool, enable_watermarks_advanced: bool) -> None:
+    def test_watermarks(self: StreamingTestBase, kikimr: Kikimr, entity_name: Callable[[str], str], shared_reading: bool, tasks: int, local_topics: bool) -> None:
         if local_topics and shared_reading:
             pytest.skip("Shared reading is not supported for local topics: YQ-5036")
 
         endpoint = self.get_endpoint(kikimr, local_topics)
-        query_name = f"test_watermarks_{shared_reading}{tasks}{local_topics}{enable_watermarks_advanced}"
+        query_name = f"test_watermarks_{shared_reading}{tasks}{local_topics}"
         source_name = entity_name(query_name)
         self.init_topics(source_name, partitions_count=tasks, endpoint=endpoint)
         self.create_source(kikimr, source_name, shared_reading)
 
-<<<<<<< HEAD
-        ts = "CAST(ts AS Timestamp)" if shared_reading else "SystemMetadata('write_time')"
-=======
->>>>>>> 371628977a6 ([YQ-5332] Watermarks: DQ: idle partitions fix (#43152))
         cluster = f"{source_name}." if not local_topics else ""
         idleness_clause = ', WATERMARK_IDLE_TIMEOUT = "PT5S"' if tasks > 1 else ''
 
@@ -63,27 +54,6 @@ class TestWatermarksInYdb(StreamingTestBase):
                     HoppingWindow(CAST(event_time AS Timestamp), 'PT1S', 'PT1S')
             );
 
-<<<<<<< HEAD
-                $hop = (
-                    SELECT
-                        CAST(HOP_END() AS String) AS event_time,
-                        AGGREGATE_LIST(ts) AS ts
-                    FROM $filter
-                    GROUP BY HoppingWindow(CAST(event_time AS Timestamp), "PT1S", "PT1S")
-                );
-
-                $output = (
-                    SELECT
-                        CAST(HOP_END() AS String) AS event_time,
-                        AGGREGATE_LIST(ts) AS ts
-                    FROM $hop
-                    GROUP BY HoppingWindow(CAST(event_time AS Timestamp), "PT1S", "PT1S")
-                );
-
-                INSERT INTO {cluster}{self.output_topic}
-                SELECT ToBytes(Unwrap(Yson::SerializeJson(Yson::From(ts))))
-                FROM $output;
-=======
             $output = (
                 SELECT
                     CAST(HOP_END() AS String) AS event_time,
@@ -97,7 +67,6 @@ class TestWatermarksInYdb(StreamingTestBase):
             INSERT INTO {cluster}{self.output_topic}
             SELECT ToBytes(Unwrap(Yson::SerializeJson(Yson::From(ts))))
             FROM $output;
->>>>>>> 371628977a6 ([YQ-5332] Watermarks: DQ: idle partitions fix (#43152))
             END DO;
         '''
         kikimr.ydb_client.query(sql)

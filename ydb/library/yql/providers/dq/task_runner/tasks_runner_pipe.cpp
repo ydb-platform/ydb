218087042cbca4cbf87e3c6f667380ca33e9b3ed
Alexey Pozdniakov <vokayndzop@ydb.tech>
2026-06-25 13:17:23 +0300

    BACKPORT-CONFLICT: manual resolution required for commit 3716289

--- ydb/library/yql/providers/dq/task_runner/tasks_runner_pipe.cpp
+++ ydb/library/yql/providers/dq/task_runner/tasks_runner_pipe.cpp
@@ -1768,11 +1768,16 @@ public:
         return "";
     }
 
-    void Prepare(const TDqTaskSettings& task, const TDqTaskRunnerMemoryLimits& memoryLimits,
-        const IDqTaskRunnerExecutionContext& execCtx, TDqComputeActorWatermarks* watermarksTracker) override
-    {
+    void Prepare(
+        const TDqTaskSettings& task,
+        const TDqTaskRunnerMemoryLimits& memoryLimits,
+        const IDqTaskRunnerExecutionContext& execCtx,
+        TDqComputeActorWatermarks* watermarksTracker,
+        TDqWatermarkGeneratorTracker* sourceWatermarksTracker
+    ) override {
         Y_UNUSED(execCtx);
         Y_UNUSED(watermarksTracker);
+        Y_UNUSED(sourceWatermarksTracker);
         Y_ABORT_UNLESS(Task.GetId() == task.GetId());
         try {
             auto result = Delegate->Prepare(memoryLimits);
--- ydb/tests/fq/streaming/test_watermarks.py
+++ ydb/tests/fq/streaming/test_watermarks.py
@@ -9,7 +9,10 @@ logger = logging.getLogger(__name__)
 
 
 class TestWatermarksInYdb(StreamingTestBase):
+<<<<<<< HEAD
     @pytest.mark.parametrize("kikimr", [{"enable_watermarks": True}], indirect=["kikimr"])
+=======
+>>>>>>> 371628977a6 ([YQ-5332] Watermarks: DQ: idle partitions fix (#43152))
     @pytest.mark.parametrize("shared_reading", [False, True], ids=["no_shared", "shared"])
     @pytest.mark.parametrize("tasks", [1, 2])
     @pytest.mark.parametrize("local_topics", [True, False])
@@ -24,39 +27,43 @@ class TestWatermarksInYdb(StreamingTestBase):
         self.init_topics(source_name, partitions_count=tasks, endpoint=endpoint)
         self.create_source(kikimr, source_name, shared_reading)
 
+<<<<<<< HEAD
         ts = "CAST(ts AS Timestamp)" if shared_reading else "SystemMetadata('write_time')"
+=======
+>>>>>>> 371628977a6 ([YQ-5332] Watermarks: DQ: idle partitions fix (#43152))
         cluster = f"{source_name}." if not local_topics else ""
         idleness_clause = ', WATERMARK_IDLE_TIMEOUT = "PT5S"' if tasks > 1 else ''
 
         sql = f'''
-            CREATE STREAMING QUERY `{query_name}` AS
-            DO BEGIN
-                PRAGMA ydb.MaxTasksPerStage="{tasks}";
+            CREATE STREAMING QUERY `{query_name}` AS DO BEGIN
+            PRAGMA ydb.MaxTasksPerStage = '{tasks}';
 
-                $input = (
-                    SELECT
-                        {self.input_topic}.*,
-                        {ts} AS event_time,
-                    FROM {cluster}{self.input_topic}
-                    WITH (
-                        FORMAT=json_each_row,
-                        SCHEMA (
-                            ts String NOT NULL,
-                            pass Uint64
-                        )
-                        , WATERMARK = {ts} - Interval('PT5S')
-                        , WATERMARK_GRANULARITY = "PT1S"
+            $input = (
+                SELECT
+                    input.*,
+                    CAST(ts AS Timestamp) AS event_time,
+                FROM
+                    {cluster}{self.input_topic} WITH (
+                        FORMAT = json_each_row,
+                        SCHEMA (ts String, pass Uint64),
+                        WATERMARK = CAST(ts AS Timestamp) - Interval('PT5S')
                         {idleness_clause}
-                    )
-                );
+                    ) AS input
+            );
 
-                $filter = (
-                    SELECT
-                        input.*
-                    FROM $input AS input
-                    WHERE pass > 0
-                );
+            $hop = (
+                SELECT
+                    CAST(HOP_END() AS String) AS event_time,
+                    AGGREGATE_LIST(ts) AS ts
+                FROM
+                    $input
+                WHERE
+                    pass > 0
+                GROUP BY
+                    HoppingWindow(CAST(event_time AS Timestamp), 'PT1S', 'PT1S')
+            );
 
+<<<<<<< HEAD
                 $hop = (
                     SELECT
                         CAST(HOP_END() AS String) AS event_time,
@@ -76,18 +83,35 @@ class TestWatermarksInYdb(StreamingTestBase):
                 INSERT INTO {cluster}{self.output_topic}
                 SELECT ToBytes(Unwrap(Yson::SerializeJson(Yson::From(ts))))
                 FROM $output;
+=======
+            $output = (
+                SELECT
+                    CAST(HOP_END() AS String) AS event_time,
+                    AGGREGATE_LIST(ts) AS ts
+                FROM
+                    $hop
+                GROUP BY
+                    HoppingWindow(CAST(event_time AS Timestamp), 'PT1S', 'PT1S')
+            );
+
+            INSERT INTO {cluster}{self.output_topic}
+            SELECT ToBytes(Unwrap(Yson::SerializeJson(Yson::From(ts))))
+            FROM $output;
+>>>>>>> 371628977a6 ([YQ-5332] Watermarks: DQ: idle partitions fix (#43152))
             END DO;
         '''
         kikimr.ydb_client.query(sql)
         self.wait_completed_checkpoints(kikimr, f"/Root/{query_name}")
 
-        self.write_stream(['{"ts": "1970-01-01T00:00:40Z", "pass": 1}'], endpoint=endpoint, partition_key=b'1')
-        if not shared_reading:
-            time.sleep(10)
-        self.write_stream(['{"ts": "1970-01-01T00:00:50Z", "pass": 1}'], endpoint=endpoint, partition_key=b'1')
-        if not shared_reading:
-            time.sleep(10)
-        self.write_stream(['{"ts": "1970-01-01T00:01:00Z", "pass": 0}'], endpoint=endpoint, partition_key=b'1')
+        self.write_stream(
+            data=[
+                '{"ts": "1970-01-01T00:00:40Z", "pass": 1}',
+                '{"ts": "1970-01-01T00:00:50Z", "pass": 1}',
+                '{"ts": "1970-01-01T00:01:00Z", "pass": 0}',
+            ],
+            endpoint=endpoint,
+            partition_key=b'1',
+        )
         if shared_reading and tasks > 1:
             time.sleep(10)  # leave a bit more time to fire up idle timeout
 

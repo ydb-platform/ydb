1a72ac39196103df8ae688a5b8631d4c6294134c
Konstantin Seleznyov <kseleznyov@ydb.tech>
2026-06-26 10:29:45 +0000

    Create PR

--- ydb/core/tx/columnshard/engines/changes/abstract/remove_portions.cpp
+++ ydb/core/tx/columnshard/engines/changes/abstract/remove_portions.cpp
@@ -3,6 +3,8 @@
 #include <ydb/core/tx/columnshard/columnshard_impl.h>
 #include <ydb/core/tx/columnshard/engines/column_engine_logs.h>
 
+#define YDB_LOG_THIS_FILE_COMPONENT NKikimrServices::TX_COLUMNSHARD
+
 namespace NKikimr::NOlap {
 
 std::shared_ptr<NDataLocks::ILock> TRemovePortionsChange::DoBuildDataLock(
@@ -17,12 +19,13 @@ std::shared_ptr<NDataLocks::ILock> TRemovePortionsChange::DoBuildDataLock(
 void TRemovePortionsChange::DoApplyOnExecute(
     NColumnShard::TColumnShard* /* self */, TWriteIndexContext& context, const TDataAccessorsResult& fetchedDataAccessors) {
     if (fetchedDataAccessors.HasErrors()) {
-        AFL_ERROR(NKikimrServices::TX_COLUMNSHARD)("error", "Data accessor result with errors " + fetchedDataAccessors.GetErrorMessage());
+        YDB_LOG_ERROR("",
+            {"error", "Data accessor result with errors " + fetchedDataAccessors.GetErrorMessage()});
     }
 
     if (fetchedDataAccessors.HasRemovedData()) {
-        AFL_DEBUG(NKikimrServices::TX_COLUMNSHARD)(
-            "error", TStringBuilder{} << "Data accessor result with removed data, " << fetchedDataAccessors.GetRemovedData().size());
+        YDB_LOG_DEBUG("",
+            {"error", TStringBuilder{} << "Data accessor result with removed data, " << fetchedDataAccessors.GetRemovedData().size()});
     }
 
     THashSet<ui64> usedPortionIds;

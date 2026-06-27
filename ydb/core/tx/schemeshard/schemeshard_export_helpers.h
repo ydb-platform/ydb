fe438cf77a21e9fccb150de9d3383862c6d1e1fd
Konstantin Seleznyov <kseleznyov@ydb.tech>
2026-06-27 10:23:29 +0000

    Revert definitions from headers

--- ydb/core/tx/schemeshard/schemeshard_export_helpers.h
+++ ydb/core/tx/schemeshard/schemeshard_export_helpers.h
@@ -2,6 +2,22 @@
 
 #include <yql/essentials/public/issue/protos/issue_severity.pb.h>
 
+#if defined LOG_T || \
+    defined LOG_D || \
+    defined LOG_I || \
+    defined LOG_N || \
+    defined LOG_W || \
+    defined LOG_W
+#error log macro redefinition
+#endif
+
+#define LOG_T(stream) LOG_TRACE_S((TlsActivationContext->AsActorContext()), NKikimrServices::EXPORT, stream)
+#define LOG_D(stream) LOG_DEBUG_S((TlsActivationContext->AsActorContext()), NKikimrServices::EXPORT, stream)
+#define LOG_I(stream) LOG_INFO_S((TlsActivationContext->AsActorContext()), NKikimrServices::EXPORT, stream)
+#define LOG_N(stream) LOG_NOTICE_S((TlsActivationContext->AsActorContext()), NKikimrServices::EXPORT, stream)
+#define LOG_W(stream) LOG_WARN_S((TlsActivationContext->AsActorContext()), NKikimrServices::EXPORT, stream)
+#define LOG_E(stream) LOG_ERROR_S((TlsActivationContext->AsActorContext()), NKikimrServices::EXPORT, stream)
+
 namespace NKikimrSchemeOp {
     enum EPathType : int;
 }

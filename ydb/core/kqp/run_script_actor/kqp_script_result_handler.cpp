ad42c810fa2ee64bbae273d2f21c161cd19dea93
Sergey Uzhakov <uzhas@ydb.tech>
2026-06-29 19:37:34 +0300

    fix build

--- ydb/core/kqp/run_script_actor/kqp_script_result_handler.cpp
+++ ydb/core/kqp/run_script_actor/kqp_script_result_handler.cpp
@@ -418,7 +418,7 @@ private:
                     meta.set_enabled_runtime_results(true);
                     *meta.mutable_columns() = resultSet.columns();
 
-                    if (const auto& issues = NKikimr::NKqp::ValidateResultSetColumns(meta.columns())) {
+                    if (const auto& issues = NFq::ValidateResultSetColumns(meta.columns())) {
                         meta.clear_columns();
                         Finish(Ydb::StatusIds::INTERNAL_ERROR, AddRootIssue(TStringBuilder() << "Invalid result set " << resultSetIndex << " columns, please contact internal support", issues));
                         return;

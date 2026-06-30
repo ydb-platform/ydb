611083ee2994c34a9560b634e17e5835ec140cf2
Aleksei Moiseitsev <swarmer@ydb.tech>
2026-06-30 17:25:32 +0000

    Revert "revert enricher"
    This reverts commit a09a839342578275e9bbd709ff800a44538558c8.

--- ydb/core/persqueue/pqtablet/partition/mlp/mlp_message_enricher.h
+++ ydb/core/persqueue/pqtablet/partition/mlp/mlp_message_enricher.h
@@ -23,11 +23,32 @@ public:
     void PassAway() override;
 
 private:
+    struct TOffsetEntry {
+        ui64 Offset;
+        size_t ReplyIndex;
+        size_t MessageIndex;
+        bool Processed = false;
+    };
+
+    struct TPendingResponse {
+        NActors::TActorId Sender;
+        ui64 Cookie;
+        std::unique_ptr<TEvPQ::TEvMLPReadResponse> Response;
+        size_t TotalMessages = 0;
+        size_t EnrichedCount = 0;
+        bool Sent = false;
+
+        bool IsComplete() const { return EnrichedCount == TotalMessages; }
+    };
+
     void Handle(TEvPersQueue::TEvResponse::TPtr&);
     void Handle(TEvPipeCache::TEvDeliveryProblem::TPtr&);
 
     STFUNC(StateWork);
 
+    void BuildSortedIndex();
+    void EnsureResponse(size_t replyIndex);
+    void TrySendReply(size_t replyIndex);
     void ProcessQueue();
     void SendToPQTablet(std::unique_ptr<IEventBase> ev);
 
@@ -35,8 +56,11 @@ private:
     const ui64 TabletId;
     const ui32 PartitionId;
     const TString ConsumerName;
-    std::deque<TReadResult> Queue;
-    std::unique_ptr<TEvPQ::TEvMLPReadResponse> PendingResponse;
+    std::vector<TReadResult> Replies;
+    std::vector<TPendingResponse> PendingResponses;
+    std::vector<TOffsetEntry> SortedEntries;
+    size_t NextEntryIdx = 0;
+    size_t RepliesSent = 0;
 
     bool FirstRequest = true;
 };

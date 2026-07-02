c75bb1eaf7580040b232cd3d07c2f12795e34218
Anely <anely-d@ydb.tech>
2026-07-02 12:55:42 +0300

    Warmup skip root and unblock grpc for v2 (#44906)

--- ydb/core/testlib/test_client.cpp
+++ ydb/core/testlib/test_client.cpp
@@ -1455,6 +1455,10 @@ namespace Tests {
             auto counters = MakeIntrusive<::NMonitoring::TDynamicCounters>();
             Runtime->GetAppData(nodeIdx).KqpComputeScheduler = NKqp::CreateKqpComputeScheduler(counters, *Settings->AppConfig);
 
+            if (!Settings->AppConfig->GetTableServiceConfig().HasEnableCompileCacheWarmup()) {
+                Settings->AppConfig->MutableTableServiceConfig()->SetEnableCompileCacheWarmup(false);
+            }
+
             IActor* kqpProxyService = NKqp::CreateKqpProxyService(Settings->AppConfig->GetLogConfig(),
                                                                   Settings->AppConfig->GetTableServiceConfig(),
                                                                   Settings->AppConfig->GetQueryServiceConfig(),

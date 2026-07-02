945e57e1271b78ab2fa89e6d27d98795ee274cd8
Semyon Danilov <senya@ydb.tech>
2026-07-02 06:48:58 +0200

    Initial

--- ydb/core/mind/bscontroller/sys_view.cpp
+++ ydb/core/mind/bscontroller/sys_view.cpp
@@ -93,6 +93,7 @@ class TSystemViewsCollector : public TActorBootstrapped<TSystemViewsCollector> {
 
     std::vector<NKikimrSysView::TStorageStatsEntry> StorageStats;
     TActorId StorageStatsCalculatorId;
+    bool InitialCalculation = true;
     static constexpr TDuration StorageStatsUpdatePeriod = TDuration::Minutes(10);
 
 public:
@@ -110,7 +111,6 @@ public:
 
     void Bootstrap(const TActorContext&) {
         Become(&TThis::StateWork);
-        RunStorageStatsCalculator();
     }
 
     STRICT_STFUNC(StateWork,
@@ -135,6 +135,13 @@ public:
         HostRecords = std::move(msg->HostRecords);
         GroupReserveMin = msg->GroupReserveMin;
         GroupReservePart = msg->GroupReservePart;
+
+        if (std::exchange(InitialCalculation, false)) {
+            // First time we receive BSC state, we need to run storage stats calculator.
+            // We do it here to avoid running it before we have any data.
+            // Consecutive runs will be scheduled by TEvCalculateStorageStatsRequest event.
+            RunStorageStatsCalculator();
+        }
     }
 
     void PassAway() override {

3035e854c280eefe87a9042f1267d293890d399f
Pavel Zuev <pzuev@ydb.tech>
2026-07-06 03:38:34 +0000

    fix one more test

--- ydb/core/kqp/ut/runtime/kqp_scan_logging_ut.cpp
+++ ydb/core/kqp/ut/runtime/kqp_scan_logging_ut.cpp
@@ -77,7 +77,7 @@ Y_UNIT_TEST_TWIN(WideCombine, EnabledLogs) {
         select count(t.Key) from `/Root/KeyValue` as t group by t.Value
     )";
 
-    RunTestForQuery(query, "[DqHashCombine]", EnabledLogs);
+    RunTestForQuery(query, "Combine]", EnabledLogs);
 }
 
 Y_UNIT_TEST_TWIN(GraceJoin, EnabledLogs) {

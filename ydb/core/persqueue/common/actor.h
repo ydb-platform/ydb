8d405fa8b10f35a878428f6b2462ccd882586994
Konstantin Seleznyov <kseleznyov@ydb.tech>
2026-07-07 14:12:22 +0000

    Create PR

--- ydb/core/persqueue/common/actor.h
+++ ydb/core/persqueue/common/actor.h
@@ -7,15 +7,6 @@
 #include <ydb/library/services/services.pb.h>
 
 #define NPQ_LOG_PREFIX LogBuilder() << GetLogPrefix()
-#define LOG(level, stream) LOG_LOG_S (*NActors::TlsActivationContext, level, Service, NPQ_LOG_PREFIX << stream)
-#define LOG_T(stream) LOG_TRACE_S (*NActors::TlsActivationContext, Service, NPQ_LOG_PREFIX << stream)
-#define LOG_D(stream) LOG_DEBUG_S (*NActors::TlsActivationContext, Service, NPQ_LOG_PREFIX << stream)
-#define LOG_I(stream) LOG_INFO_S  (*NActors::TlsActivationContext, Service, NPQ_LOG_PREFIX << stream)
-#define LOG_N(stream) LOG_NOTICE_S(*NActors::TlsActivationContext, Service, NPQ_LOG_PREFIX << stream)
-#define LOG_W(stream) LOG_WARN_S  (*NActors::TlsActivationContext, Service, NPQ_LOG_PREFIX << stream)
-#define LOG_E(stream) LOG_ERROR_S (*NActors::TlsActivationContext, Service, NPQ_LOG_PREFIX << stream)
-#define LOG_C(stream) LOG_CRIT_S  (*NActors::TlsActivationContext, Service, NPQ_LOG_PREFIX << stream)
-#define LOG_A(stream) LOG_ALERT_S (*NActors::TlsActivationContext, Service, NPQ_LOG_PREFIX << stream)
 
 namespace NKikimr::NPQ {
 

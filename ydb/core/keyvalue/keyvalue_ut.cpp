426eaeac1eca9974141f8011b8e9743de2ccbc88
Evgeny Zverev <zverevgeny@ydb.tech>
2026-07-13 09:39:25 +0000

    fix using unknown channels

--- ydb/core/keyvalue/keyvalue_ut.cpp
+++ ydb/core/keyvalue/keyvalue_ut.cpp
@@ -1581,7 +1581,7 @@ Y_UNIT_TEST(TestWriteReadWithRestartsThenResponseOk) {
                 values.push_back(value.Str());
             }
         }
-        CmdWrite(keys, values, NKikimrClient::TKeyValueRequest::EXTRA9,
+        CmdWrite(keys, values, NKikimrClient::TKeyValueRequest::MAIN,
             NKikimrClient::TKeyValueRequest::REALTIME, tc);
 
         TDeque<TString> expectedKeys;
@@ -1628,7 +1628,7 @@ Y_UNIT_TEST(TestWriteReadWithRestartsThenResponseOkNewApi) {
             }
         }
 
-        ExecuteWrite(tc, pairs, 0, 11, NKikimrKeyValue::Priorities::PRIORITY_REALTIME);
+        ExecuteWrite(tc, pairs, 0, NKikimrClient::TKeyValueRequest::MAIN, NKikimrKeyValue::Priorities::PRIORITY_REALTIME);
 
         TDeque<TKeyValuePair> expectedPairs;
         for (ui32 itemIdx = 1; itemIdx < 4; ++itemIdx) {

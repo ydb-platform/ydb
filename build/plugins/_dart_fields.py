25daaace7e0cbea0096177e9c5eab644397d30cf
svidyuk <svidyuk@yandex-team.com>
2026-07-14 06:55:56 +0300

    Ignore fields for macros only commit_hash:d7a6a06f6a848f0164c1ff81723152ccfa1d4835

--- build/plugins/_dart_fields.py
+++ build/plugins/_dart_fields.py
@@ -62,11 +62,6 @@ def with_fields(fields):
         def innermost(*args, **kwargs):
             func(fields, *args, **kwargs)
 
-        # Drop annotation for injected fields argument
-        annotations = getattr(func, '__annotations__', {}).copy()
-        annotations.pop('fields', None)
-        innermost.__annotations__ = annotations
-
         return innermost
 
     return inner

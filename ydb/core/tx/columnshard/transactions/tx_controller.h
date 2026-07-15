54ecb1161a0cf2de31362204d4ca3246f2604e1b
YDBot <ydbot@ydb.tech>
2026-07-15 22:30:37 +0300

    [Backport stable-26-2-1] PR #43594: fix TxProgress enqueue (#46049)
    Co-authored-by: Kirill Vasilenko <kir-vasilenko@ydb.tech>
    Co-authored-by: Kirill Vasilenko <kir-vasilenko@ybd.tech>

--- ydb/core/tx/columnshard/transactions/tx_controller.h
+++ ydb/core/tx/columnshard/transactions/tx_controller.h
@@ -164,6 +164,12 @@ public:
     }
 };
 
+enum class ETxOperatorStatus {
+    InProgress,
+    Completing,
+    Any
+};
+
 class TTxController {
 public:
     struct TPlanQueueItem {
@@ -451,9 +457,11 @@ private:
     TTxProgressCounters Counters;
 
     THashMap<ui64, ITransactionOperator::TPtr> Operators;
+    THashMap<ui64, ITransactionOperator::TPtr> CompletingOperators;
 
 private:
     bool AbortTx(const TPlanQueueItem planQueueItem);
+    ITransactionOperator::TPtr MoveOperatorToCompleting(const ui64 txId);
 
     TTxInfo RegisterTx(const std::shared_ptr<TTxController::ITransactionOperator>& txOperator, const TString& txBody,
         NTabletFlatExecutor::TTransactionContext& txc);
@@ -467,25 +475,18 @@ public:
 
     ui64 GetAllowedStep() const;
 
-    ITransactionOperator::TPtr GetTxOperatorOptional(const ui64 txId) const {
-        auto it = Operators.find(txId);
-        if (it == Operators.end()) {
-            return nullptr;
-        }
-        return it->second;
+    bool IsTxCompleting(const ui64 txId) const {
+        return CompletingOperators.contains(txId);
     }
 
-    ITransactionOperator::TPtr GetTxOperatorVerified(const ui64 txId) const {
-        return TValidator::CheckNotNull(GetTxOperatorOptional(txId));
-    }
+    ITransactionOperator::TPtr GetTxOperator(const ui64 txId, ETxOperatorStatus status, const bool optional = false) const;
 
     template <class TExpectedTransactionOperator>
-    std::shared_ptr<TExpectedTransactionOperator> GetTxOperatorVerifiedAs(const ui64 txId, const bool optionalExists = false) const {
-        auto result = GetTxOperatorOptional(txId);
-        if (optionalExists && !result) {
+    std::shared_ptr<TExpectedTransactionOperator> GetTxOperatorAs(const ui64 txId, ETxOperatorStatus status, const bool optional = false) const {
+        auto result = GetTxOperator(txId, status, optional);
+        if (!result) {
             return nullptr;
         }
-        AFL_VERIFY(result)("tx_id", txId);
         auto resultClass = dynamic_pointer_cast<TExpectedTransactionOperator>(result);
         AFL_VERIFY(resultClass)("tx_id", txId);
         return resultClass;
@@ -496,7 +497,7 @@ public:
         if (!txInfo) {
             return;
         }
-        GetTxOperatorVerified(txInfo->GetTxId())->PingTimeout(Owner, now);
+        GetTxOperator(txInfo->GetTxId(), ETxOperatorStatus::InProgress)->PingTimeout(Owner, now);
     }
 
     ui64 GetMemoryUsage() const;
@@ -514,10 +515,7 @@ public:
     void FinishProposeOnComplete(ITransactionOperator& txOperator, const TActorContext& ctx);
     void FinishProposeOnComplete(const ui64 txId, const TActorContext& ctx);
 
-    void WriteTxOperatorInfo(NTabletFlatExecutor::TTransactionContext& txc, const ui64 txId, const TString& data) {
-        NIceDb::TNiceDb db(txc.DB);
-        NColumnShard::Schema::UpdateTxInfoBody(db, txId, data);
-    }
+    void WriteTxOperatorInfo(NTabletFlatExecutor::TTransactionContext& txc, const ui64 txId, const TString& data);
 
     bool ExecuteOnCancel(const ui64 txId, NTabletFlatExecutor::TTransactionContext& txc);
     bool CompleteOnCancel(const ui64 txId, const TActorContext& ctx);
@@ -530,8 +528,8 @@ public:
 
     std::optional<TPlanQueueItem> GetPlannedTx() const;
     TPlanQueueItem GetFrontTx() const;
-    std::optional<TTxInfo> GetTxInfo(const ui64 txId) const;
-    TTxInfo GetTxInfoVerified(const ui64 txId) const;
+    std::optional<TTxInfo> GetTxInfo(const ui64 txId, ETxOperatorStatus status) const;
+    TTxInfo GetTxInfoVerified(const ui64 txId, ETxOperatorStatus status) const;
     NEvents::TDataEvents::TCoordinatorInfo BuildCoordinatorInfo(const TTxInfo& txInfo) const;
 
     size_t CleanExpiredTxs();

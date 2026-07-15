18b0fa80af4180f05c81af2a8968ba2baf0e10c6
Oleg Geller <iddqd@yandex-team.ru>
2026-07-15 14:57:02 +0300

    Enable kafka api in recipe by default https://github.com/ydb-platform/ydb/issues/46194

--- ydb/tests/library/harness/kikimr_config.py
+++ ydb/tests/library/harness/kikimr_config.py
@@ -595,7 +595,7 @@ class KikimrConfigGenerator(object):
         if kafka_api_port is not None:
             kafka_proxy_config = dict()
             kafka_proxy_config['enable_kafka_proxy'] = True
-            kafka_proxy_config["listening_port"] = kafka_api_port
+            kafka_proxy_config["listening_port"] = kafka_api_port if kafka_api_port != 'auto' else self.port_allocator.get_node_port_allocator(node_id).kafka_api_port
 
             self.yaml_config["kafka_proxy_config"] = kafka_proxy_config
 

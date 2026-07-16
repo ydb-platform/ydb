d4f9540e4077235a7e24168e4c5b2f3b94571442
Kirill Vasilenko <kir-vasilenko@ybd.tech>
2026-07-16 12:42:16 +0000

    fix conflicts

--- ydb/core/tx/columnshard/blobs_action/transaction/tx_blobs_written.cpp
+++ ydb/core/tx/columnshard/blobs_action/transaction/tx_blobs_written.cpp
@@ -17,8 +17,9 @@ bool TTxBlobsWritingFinished::DoExecute(TTransactionContext& txc, const TActorCo
     TMemoryProfileGuard mpg("TTxBlobsWritingFinished::Execute");
     txc.DB.NoMoreReadsForTx();
     CommitSnapshot = Self->GetSnapshotForNoTxWrites();
-    YDB_LOG_CREATE_CONTEXT_COMP(NKikimrServices::TX_COLUMNSHARD_BLOBS, { "tabletId", Self->TabletID() }, { "txState", "execute" });
-    YDB_LOG_DEBUG_COMP(NActors::NStructuredLog::TLogStack::GetComponent(), "", { "event", "start_execute" });
+    NActors::TLogContextGuard logGuard =
+        NActors::TLogContextBuilder::Build(NKikimrServices::TX_COLUMNSHARD_BLOBS)("tablet_id", Self->TabletID())("tx_state", "execute");
+    ACFL_DEBUG("event", "start_execute");
     auto& index = Self->MutableIndexAs<NOlap::TColumnEngineForLogs>();
     const auto minReadSnapshot = Self->GetMinSnapshotForNewReads();
 

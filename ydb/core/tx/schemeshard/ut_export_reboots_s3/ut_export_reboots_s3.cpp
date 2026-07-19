0438e9aa8a1a1d00d62602c5a0d6e70ce9ccbd09
Oleg Doronin <dorooleg@yandex.ru>
2026-07-19 19:31:23 +0000

    more export/import tests have been added for cs

--- ydb/core/tx/schemeshard/ut_export_reboots_s3/ut_export_reboots_s3.cpp
+++ ydb/core/tx/schemeshard/ut_export_reboots_s3/ut_export_reboots_s3.cpp
@@ -904,7 +904,17 @@ Y_UNIT_TEST_SUITE(TExportToS3WithRebootsTests) {
             TTestData::ColumnTable()
         }, TTestData::Items(EPathTypeColumnTable));
     }
-    
+
+    Y_UNIT_TEST_WITH_REBOOTS_BUCKETS_TWIN(ShouldSucceedOnTablesAndColumnTables, 6, 1, false, IsFs) {
+        if (IsFs) {
+            return; // Column table FS export is not supported yet
+        }
+        RunExport<IsFs>(t, {
+            TTestData::Table(),
+            TTestData::ColumnTable()
+        }, {{"/MyRoot/Table", "table"}, {"/MyRoot/ColumnTable", "column_table"}});
+    }
+
     Y_UNIT_TEST_WITH_REBOOTS_BUCKETS_TWIN(CancelShouldSucceedOnSingleColumnTable, 2, 1, false, IsFs) {
         if (IsFs) {
             return; // It is not supported yet

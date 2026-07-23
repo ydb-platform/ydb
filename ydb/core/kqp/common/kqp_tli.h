8a979682d69091e9c0faaee4a32de11d67931fac
Konstantin Seleznyov <kseleznyov@ydb.tech>
2026-07-23 16:21:33 +0000

    revert data_integrity anf tli logs

--- ydb/core/kqp/common/kqp_tli.h
+++ ydb/core/kqp/common/kqp_tli.h
@@ -209,8 +209,7 @@ inline void LogTli(const TTliLogParams& params, const NActors::TActorContext& ct
         LogKeyValue("VictimQueryTexts", EscapeC(params.QueryTexts), ss, true);
     }
 
-    YDB_LOG_INFO_CTX_COMP(ctx, NKikimrServices::TLI, "Transaction load isolation event",
-        {"tliEvent", ss.Str()});
+    LOG_INFO_S(ctx, NKikimrServices::TLI, ss.Str());
 }
 
 }

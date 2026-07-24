e5d5844bdb9dfcac4ba47c7dfc41d80c8314bea6
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-07-24 11:14:23 +0300

    dq streamlookup/kikimr lookup: optimize keys transfer by using Pickle
    Pass list-of-tuple as Pickle'd blob instead of protobuf

--- ydb/core/kqp/federated_query/actors/lookup_actor/kikimr_lookup_actor.cpp
+++ ydb/core/kqp/federated_query/actors/lookup_actor/kikimr_lookup_actor.cpp
@@ -88,6 +88,22 @@ namespace {
         return resultTypeBuilder.Build();
     }
 
+    const NKikimr::NMiniKQL::TType* MakePickleType(const NKikimr::NMiniKQL::TTypeEnvironment& env, const NKikimr::NMiniKQL::TStructType* keyType) {
+        NKikimr::NMiniKQL::TType* rowType;
+        auto membersCount = keyType->GetMembersCount();
+        Y_ENSURE(membersCount > 0);
+        if (membersCount > 1) {
+            TSmallVec<NKikimr::NMiniKQL::TType*> types(membersCount);
+            for (ui32 i = 0; i != membersCount; ++i) {
+                types[i] = keyType->GetMemberType(i);
+            }
+            rowType = NKikimr::NMiniKQL::TTupleType::Create(membersCount, types.data(), env);
+        } else {
+            rowType = keyType->GetMemberType(0);
+        }
+        return NKikimr::NMiniKQL::TListType::Create(rowType, env);
+    }
+
     class TDqSourceKikimrLookupActor
         : public NYql::NDq::IDqAsyncLookupSource,
           public NActors::TActorBootstrapped<TDqSourceKikimrLookupActor> {
@@ -206,6 +222,8 @@ namespace {
                 Token.emplace(token);
             }
             InitMonCounters(taskCounters);
+            auto guard = Guard(*Alloc);
+            Pickle.emplace(/*stable=*/false, MakePickleType(typeEnv, keyType));
         }
 
         ~TDqSourceKikimrLookupActor() {
@@ -216,6 +234,7 @@ namespace {
         void Free() {
             auto guard = Guard(*Alloc);
             KeyTypeHelper.reset();
+            Pickle.reset();
         }
         void InitMonCounters(const ::NMonitoring::TDynamicCounterPtr& taskCounters) {
             if (!taskCounters) {
@@ -860,7 +879,8 @@ namespace {
             auto columnsCount = KeyType->GetMembersCount();
             Y_ENSURE(columnsCount > 0);
             out << "PRAGMA AnsiInForEmptyOrNullableItemsCollections;\n";
-            out << "DECLARE "<< KeyTupleListName << " AS List<";
+            out << "DECLARE "<< KeyTupleListName << " AS String;\n";
+            out << KeyTupleListName << " = Unpickle(List<";
             if (columnsCount != 1) {
                 out << "Tuple<";
             }
@@ -874,7 +894,7 @@ namespace {
             if (columnsCount != 1) {
                 out << '>';
             }
-            out << ">;\n";
+            out << ">, " << KeyTupleListName << ");\n";
             out << SelectBody;
             out << "\n WHERE ";
             if (columnsCount != 1) {
@@ -910,27 +930,23 @@ namespace {
             auto guard = Guard(*Alloc);
 
             auto keyColumnsCount = KeyType->GetMembersCount();
-            if (keyColumnsCount != 1) {
-                auto& keyTupleTypes = *keyTupleList.mutable_type()->mutable_list_type()->mutable_item()->mutable_tuple_type();
-                for (ui32 c = 0; c != keyColumnsCount; ++c) {
-                    ExportTypeToProto(KeyType->GetMemberType(c), *keyTupleTypes.add_elements());
-                }
-            } else {
-                auto& keyListType = *keyTupleList.mutable_type()->mutable_list_type()->mutable_item();
-                ExportTypeToProto(KeyType->GetMemberType(0), keyListType);
-            }
-            auto& list = *keyTupleList.mutable_value();
+            keyTupleList.mutable_type()->set_type_id(Ydb::Type_PrimitiveTypeId_STRING);
             auto locked = state->Request.lock();
             if (!locked) {
                 throw yexception() << "Actor died";
             }
-            for (const auto& [keys, _]: *locked) {
-                auto& row = *list.add_items();
-                for (ui32 c = 0; c != keyColumnsCount; ++c) {
-                    auto& value = keyColumnsCount != 1 ? *row.add_items() : row;
-                    ExportValueToProto(KeyType->GetMemberType(c), keys.GetElement(c), value);
+            NUdf::TUnboxedValue* listItems;
+            NUdf::TUnboxedValue list = HolderFactory.CreateDirectArrayHolder(locked->size(), listItems);
+            if (keyColumnsCount != 1) {
+                for (const auto& [keys, _]: *locked) {
+                    *listItems++ = keys;
+                }
+            } else {
+                for (const auto& [keys, _]: *locked) {
+                    *listItems++ = keys.GetElement(0);
                 }
             }
+            keyTupleList.mutable_value()->set_bytes_value(TString(Pickle->Pack(list)));
         }
 
         // must be called only in actor context
@@ -977,6 +993,7 @@ namespace {
         const NKikimr::NMiniKQL::TStructType* const PayloadType;
         const NKikimr::NMiniKQL::TStructType* const SelectResultType; // columns from KeyType + PayloadType
         const NKikimr::NMiniKQL::THolderFactory& HolderFactory;
+        std::optional<NKikimr::NMiniKQL::TValuePacker> Pickle;
         const std::vector<std::pair<EColumnDestination, size_t>> ColumnDestinations;
         const size_t MaxKeysInRequest;
         const bool IsMultiMatches;

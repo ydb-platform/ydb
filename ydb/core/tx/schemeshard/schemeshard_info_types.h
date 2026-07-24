c5b4c66adecc2bd64e30a96791c55ab402249e5a
Lykov Ivan <nfrmtk.aka.fkfka@gmail.com>
2026-07-24 11:05:19 +0300

    review fix

--- ydb/core/tx/schemeshard/schemeshard_info_types.h
+++ ydb/core/tx/schemeshard/schemeshard_info_types.h
@@ -3172,9 +3172,13 @@ struct TTableIndexInfo : public TSimpleRefCount<TTableIndexInfo> {
         return std::visit([]<typename T>(const T& v) {
             if constexpr (std::is_same_v<std::monostate, T>) {
                 return TString{};
-            } else {
+            } else if constexpr (std::is_same_v<NKikimrSchemeOp::TBloomNGrammFilter, T>) {
                 TString str;
                 Y_ENSURE(v.SerializeToString(&str));
+                return str;
+            } else {
+                TString str{v.SerializeAsString()};
+                Y_ENSURE(!str.empty());
                 return str;
             }
         }, SpecializedIndexDescription);

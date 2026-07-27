38909fa4528e6a90b47d3846cab49c82fe6ce2c8
Yury Kiselev <yurikiselev@ydb.tech>
2026-07-27 19:04:23 +0500

    [stable-26-3] Fixes for database_allowed_sids support (#47826)

---
+++ ydb/core/viewer/viewer_utils.cpp
@@ -0,0 +1,24 @@
+#include "viewer_utils.h"
+
+#include <library/cpp/cgiparam/cgiparam.h>
+#include <library/cpp/json/json_reader.h>
+
+namespace NKikimr::NViewer {
+
+TString GetDatabaseParam(const TCgiParameters& params, const TStringBuf method, const TStringBuf body) {
+    TString database = params.Get("database");
+    if (database) {
+        return database;
+    }
+    if (method != "POST" || body.empty()) {
+        return {};
+    }
+    NJson::TJsonValue requestData;
+    if (NJson::ReadJsonTree(body, &requestData)) {
+        return requestData["database"].GetString();
+    }
+    // "database" param is never sent via application/x-www-form-urlencoded bodies, so we don't need to check for it
+    return {};
+}
+
+} // namespace NKikimr::NViewer

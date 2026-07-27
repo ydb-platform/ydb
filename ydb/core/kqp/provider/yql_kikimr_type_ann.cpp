7d1cbe099350593d4fe36e9b3f249d419433f4c6
Sergey Shokhor <shokhor@ydb.tech>
2026-07-27 12:55:55 +0000

    Add support to use YQL to enable or disable content based deduplication in topics

--- ydb/core/kqp/provider/yql_kikimr_type_ann.cpp
+++ ydb/core/kqp/provider/yql_kikimr_type_ann.cpp
@@ -2311,6 +2311,10 @@ private:
                 );
                 maxPartitions = value;
                 errorPos = ctx.GetPosition(setting.Value().Ref().Pos());
+            } else if (name == "setContentBasedDeduplication") {
+                if (!EnsureAtom(setting.Value().Ref(), ctx)) {
+                    return false;
+                }
             } else if (name.StartsWith("reset")) {
                 ctx.AddError(TIssue(
                         errorPos,

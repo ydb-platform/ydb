a9f259b871af706e2dc79eee06b745beb1fe321a
kruall <kru.bash.all@gmail.com>
2026-07-27 14:51:52 +0300

    Use mailbox finish notification for DSProxy batching

--- ydb/core/blobstorage/dsproxy/dsproxy_request.cpp
+++ ydb/core/blobstorage/dsproxy/dsproxy_request.cpp
@@ -33,9 +33,6 @@ namespace NKikimr {
             return;
         }
 
-        if (StopGetBatchingEvent) {
-            TActivationContext::Send(StopGetBatchingEvent.Release());
-        }
         BatchedGetRequestCount++;
 
         EnsureMonitoring(true);
@@ -191,9 +188,6 @@ namespace NKikimr {
             return;
         }
 
-        if (StopPutBatchingEvent) {
-            TActivationContext::Send(StopPutBatchingEvent.Release());
-        }
         BatchedPutRequestCount++;
 
         Send(MonActor, new TEvThroughputAddRequest(ev->Get()->HandleClass, bytes));
@@ -684,27 +678,27 @@ namespace NKikimr {
         batchedPuts.Bytes = 0;
     }
 
-    void TBlobStorageGroupProxy::Handle(TEvStopBatchingPutRequests::TPtr& ev) {
-        StopPutBatchingEvent = ev;
-        for (auto &bucket : PutBatchedBucketQueue) {
-            auto &batchedPuts = BatchedPuts[bucket.HandleClass][bucket.Tactic][bucket.ReduceInterpileTraffic];
-            Y_ABORT_UNLESS(!batchedPuts.Queue.empty());
-            *Mon->PutsSentViaPutBatching += batchedPuts.Queue.size();
-            ++*Mon->PutBatchesSent;
-            ProcessBatchedPutRequests(batchedPuts, bucket.HandleClass, bucket.Tactic, bucket.ReduceInterpileTraffic);
+    void TBlobStorageGroupProxy::Handle(TEvents::TEvMailboxProcessingFinished::TPtr&) {
+        if (BatchedPutRequestCount) {
+            for (auto &bucket : PutBatchedBucketQueue) {
+                auto &batchedPuts = BatchedPuts[bucket.HandleClass][bucket.Tactic][bucket.ReduceInterpileTraffic];
+                Y_ABORT_UNLESS(!batchedPuts.Queue.empty());
+                *Mon->PutsSentViaPutBatching += batchedPuts.Queue.size();
+                ++*Mon->PutBatchesSent;
+                ProcessBatchedPutRequests(batchedPuts, bucket.HandleClass, bucket.Tactic, bucket.ReduceInterpileTraffic);
+            }
+            PutBatchedBucketQueue.clear();
+            ++*Mon->EventStopPutBatching;
+            LWPROBE(DSProxyBatchedPutRequest, BatchedPutRequestCount, GroupId.GetRawId());
+            BatchedPutRequestCount = 0;
+            Controls.EnablePutBatching.Update(TActivationContext::Now());
         }
-        PutBatchedBucketQueue.clear();
-        ++*Mon->EventStopPutBatching;
-        LWPROBE(DSProxyBatchedPutRequest, BatchedPutRequestCount, GroupId.GetRawId());
-        BatchedPutRequestCount = 0;
-        Controls.EnablePutBatching.Update(TActivationContext::Now());
-    }
 
-    void TBlobStorageGroupProxy::Handle(TEvStopBatchingGetRequests::TPtr& ev) {
-        StopGetBatchingEvent = ev;
-        ++*Mon->EventStopGetBatching;
-        LWPROBE(DSProxyBatchedGetRequest, BatchedGetRequestCount, GroupId.GetRawId());
-        BatchedGetRequestCount = 0;
+        if (BatchedGetRequestCount) {
+            ++*Mon->EventStopGetBatching;
+            LWPROBE(DSProxyBatchedGetRequest, BatchedGetRequestCount, GroupId.GetRawId());
+            BatchedGetRequestCount = 0;
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////

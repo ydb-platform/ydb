bcce0211e69e7cf614514bd6790424b0b0c5e4df
Irina Skvortsova <nerhneiro@yandex-team.ru>
2026-07-28 15:52:13 +0000

    fix "Not enough data in MTLS configuration" error

--- ydb/core/driver_lib/run/kikimr_services_initializers.cpp
+++ ydb/core/driver_lib/run/kikimr_services_initializers.cpp
@@ -3300,7 +3300,7 @@ void TKafkaProxyServiceInitializer::InitializeServices(NActors::TActorSystemSetu
 
         setup->LocalServices.emplace_back(
             TActorId(),
-            TActorSetupCmd(NKafka::CreateKafkaListener(MakePollerActorId(), settings, Config.GetKafkaProxyConfig(), serverCreds),
+            TActorSetupCmd(NKafka::CreateKafkaListener(MakePollerActorId(), settings, Config.GetKafkaProxyConfig(), Config.GetKafkaProxyConfig().GetMtlsEnable() ? serverCreds : nullptr),
                            TMailboxType::HTSwap, appData->UserPoolId)
         );
 

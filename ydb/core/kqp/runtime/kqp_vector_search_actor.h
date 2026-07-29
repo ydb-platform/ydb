0e3410029c08dc4707b013af4aafeab81312e8a6
Vikhlyantsev Konstantin <vihlancevk@gmail.com>
2026-07-29 19:31:02 +0300

    Apply ya style to the vector search actor

--- ydb/core/kqp/runtime/kqp_vector_search_actor.h
+++ ydb/core/kqp/runtime/kqp_vector_search_actor.h
@@ -8,25 +8,25 @@
 #include <ydb/library/yql/dq/actors/compute/dq_compute_actor_async_io_factory.h>
 
 namespace NKikimr {
-namespace NKqp {
+    namespace NKqp {
 
-std::pair<NYql::NDq::IDqComputeActorAsyncInput*, IActor*> CreateKqpVectorSearchActor(
-    NKikimrTxDataShard::TKqpVectorSearchSettings&& settings,
-    ui64 inputIndex,
-    const NUdf::TUnboxedValue& input,
-    NYql::NDq::TCollectStatsLevel statsLevel,
-    NYql::NDq::TTxId txId,
-    ui64 taskId,
-    const NActors::TActorId& computeActorId,
-    const NMiniKQL::TTypeEnvironment& typeEnv,
-    const NMiniKQL::THolderFactory& holderFactory,
-    std::shared_ptr<NMiniKQL::TScopedAlloc>& alloc,
-    const NWilson::TTraceId& traceId,
-    TIntrusivePtr<TKqpCounters> counters,
-    TIntrusivePtr<TVectorIndexLevelsCache> levelsCache);
+        std::pair<NYql::NDq::IDqComputeActorAsyncInput*, IActor*> CreateKqpVectorSearchActor(
+            NKikimrTxDataShard::TKqpVectorSearchSettings&& settings,
+            ui64 inputIndex,
+            const NUdf::TUnboxedValue& input,
+            NYql::NDq::TCollectStatsLevel statsLevel,
+            NYql::NDq::TTxId txId,
+            ui64 taskId,
+            const NActors::TActorId& computeActorId,
+            const NMiniKQL::TTypeEnvironment& typeEnv,
+            const NMiniKQL::THolderFactory& holderFactory,
+            std::shared_ptr<NMiniKQL::TScopedAlloc>& alloc,
+            const NWilson::TTraceId& traceId,
+            TIntrusivePtr<TKqpCounters> counters,
+            TIntrusivePtr<TVectorIndexLevelsCache> levelsCache);
 
-void RegisterKqpVectorSearchActor(NYql::NDq::TDqAsyncIoFactory&, TIntrusivePtr<TKqpCounters>,
-    TIntrusivePtr<TVectorIndexLevelsCache> levelsCache);
+        void RegisterKqpVectorSearchActor(NYql::NDq::TDqAsyncIoFactory&, TIntrusivePtr<TKqpCounters>,
+                                          TIntrusivePtr<TVectorIndexLevelsCache> levelsCache);
 
-} // namespace NKqp
+    } // namespace NKqp
 } // namespace NKikimr

82e2d210ff5baaa0072ab01f8d9c16c3166a5e42
Roman Kuzin <kuzin57@yandex-team.ru>
2026-07-29 13:22:14 +0000

    LOGBROKER-10565 Add feature flag to GetMaxHeaderSize()

--- ydb/core/persqueue/pqtablet/blob/header.cpp
+++ ydb/core/persqueue/pqtablet/blob/header.cpp
@@ -2,17 +2,20 @@
 #include <google/protobuf/io/coded_stream.h>
 #include <util/generic/buffer.h>
 #include <util/system/unaligned_mem.h>
+#include <ydb/core/base/appdata_fwd.h>
+#include <ydb/core/base/feature_flags.h>
 #include <ydb/library/actors/core/log.h>
 
 namespace NKikimr {
 namespace NPQ {
 
 
-const ui32 MAX_HEADER_SIZE = 64; // max TBatchHeader size
+const ui32 MAX_HEADER_SIZE = 32; // max TBatchHeader size
 
 
 ui32 GetMaxHeaderSize() {
-    return MAX_HEADER_SIZE;
+    const ui32 MAX_HEADER_SIZE_WITH_OFFSET_DELTA = 64;
+    return (HasAppData() && AppData()->FeatureFlags.GetEnableTopicWriteOffsetDeltaInKeys() && AppData()->FeatureFlags.GetEnableTopicMessagesBatching()) ? MAX_HEADER_SIZE_WITH_OFFSET_DELTA : MAX_HEADER_SIZE;
 }
 
 NKikimrPQ::TBatchHeader ExtractHeader(const char *data, ui32 size) {

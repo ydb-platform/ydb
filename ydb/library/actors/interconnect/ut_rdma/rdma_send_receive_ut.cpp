46a6633520ad28470f6bc0101a7541a77d36633d
Daniil Cherednik <dcherednik@ydb.tech>
2026-07-29 12:56:15 +0200

    Test multi-packet event over RDMA main channel

--- ydb/library/actors/interconnect/ut_rdma/rdma_send_receive_ut.cpp
+++ ydb/library/actors/interconnect/ut_rdma/rdma_send_receive_ut.cpp
@@ -186,6 +186,36 @@ TEST_P(RdmaSendReceiveTestCqMode, MainChannelRdmaPreSerializedBuffer) {
     UNIT_ASSERT_VALUES_EQUAL(GetSessionCounter(cluster, 2, 1, "BytesWrittenToSocket"), bytesWrittenToSocketBefore);
 }
 
+// Verifies successful serialization and reassembly of one event spanning
+// multiple RDMA main-channel packets.
+TEST_P(RdmaSendReceiveTestCqMode, MainChannelMultiPacketEvent) {
+    TTestICCluster cluster(2, NActors::TChannelsConfig(), nullptr, nullptr, GetRdmaCqModeFlags(GetParam()),
+        TTestICCluster::TCheckerFactory(), TDuration::Seconds(30), TNode::DefaultInflight(), EnableRdmaSendReceive);
+
+    constexpr size_t bufferSize = 2 * TTcpPacketBuf::PacketDataLen + 1;
+    auto receiverPtr = new TReceiveActor([](TEvTestSerialization::TPtr ev) {
+        UNIT_ASSERT_VALUES_EQUAL(ev->Get()->Record.GetBlobID(), 1u);
+        UNIT_ASSERT_VALUES_EQUAL(ev->Get()->Record.GetBuffer(), TString(bufferSize, 'X'));
+        UNIT_ASSERT_VALUES_EQUAL(ev->Get()->GetPayload().size(), 0u);
+    });
+    const TActorId receiver = cluster.RegisterActor(receiverPtr, 1);
+
+    WaitForInterconnectConnection(cluster, 2, 1);
+    TString lastRdmaStatus;
+    UNIT_ASSERT_C(WaitForRdmaChecksumStatus(cluster, 2, 1, "On | SoftwareChecksum | SendReceive", 20, lastRdmaStatus),
+        "last RDMA status: " << FormatLastRdmaStatus(lastRdmaStatus));
+    const ui64 bytesWrittenToSocketBefore = WaitForSessionCounter(cluster, 2, 1, "BytesWrittenToSocket");
+
+    auto ev = std::make_unique<TEvTestSerialization>();
+    ev->Record.SetBlobID(1);
+    ev->Record.SetBuffer(TString(bufferSize, 'X'));
+    UNIT_ASSERT(!ev->AllowExternalDataChannel());
+    cluster.RegisterActor(new TSendActor(receiver, std::move(ev)), 2);
+
+    UNIT_ASSERT(receiverPtr->WaitForReceive(1, 20));
+    UNIT_ASSERT_VALUES_EQUAL(GetSessionCounter(cluster, 2, 1, "BytesWrittenToSocket"), bytesWrittenToSocketBefore);
+}
+
 namespace {
 
 void RunOversizedMainChannelEventTest(NInterconnect::NRdma::ECqMode cqMode,

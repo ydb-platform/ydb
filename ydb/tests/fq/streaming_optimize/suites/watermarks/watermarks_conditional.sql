881fd3361006a600f058615a511574d003f4bcb5
Aleksei Pozdniakov <vokayndzop@ydb.tech>
2026-07-31 10:51:52 +0000

    fix tests

--- ydb/tests/fq/streaming_optimize/suites/watermarks/watermarks_conditional.sql
+++ ydb/tests/fq/streaming_optimize/suites/watermarks/watermarks_conditional.sql
@@ -1,17 +1,26 @@
+$input = (
+    SELECT
+        t,
+        k,
+        v,
+        CASE
+            WHEN v == 0 THEN CAST(t AS Timestamp)
+            ELSE CAST(k AS Timestamp)
+        END AS event_time
+    FROM
+        pq.test_topic_input WITH (
+        FORMAT = json_each_row,
+        SCHEMA (t String, k String, v Int64),
+        STREAMING = 'TRUE'
+    )
+);
+
 SELECT
     *
 FROM
-    pq.test_topic_input WITH (
-        FORMAT = json_each_row,
-        SCHEMA (t String, k String, v Int64),
-        WATERMARK = (
-            CASE
-                WHEN v == 0 THEN CAST(t AS Timestamp)
-                ELSE CAST(k AS Timestamp)
-            END
-        ) - Interval('PT5S'),
+    $input WITH (
+        WATERMARK = event_time - Interval('PT5S'),
         WATERMARK_GRANULARITY = 'PT2S',
-        WATERMARK_IDLE_TIMEOUT = 'PT3S',
-        STREAMING = 'TRUE'
+        WATERMARK_IDLE_TIMEOUT = 'PT3S'
     )
 ;

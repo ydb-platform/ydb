754e5d43e1fd8daa2863871f14f254e548774a0a
Pisarenko Grigoriy <grigoriypisar@ydb.tech>
2026-07-31 12:52:53 +0500

    BACKPORT-CONFLICT: manual resolution required for commit 02cd5d0

--- ydb/tests/fq/streaming_common/common.py
+++ ydb/tests/fq/streaming_common/common.py
@@ -1,3 +1,4 @@
+import copy
 import logging
 import os
 import time
@@ -23,7 +24,7 @@ def set_test_env(request):
     param = getattr(request, "param", {})
     checkpointing_period_ms = param.get("checkpointing_period_ms", "200")
     os.environ["YDB_TEST_DEFAULT_CHECKPOINTING_PERIOD_MS"] = checkpointing_period_ms
-    os.environ["YDB_TEST_LEASE_DURATION_SEC"] = "5"
+    os.environ["YDB_TEST_LEASE_DURATION_SEC"] = param.get("lease_duration_sec", "5")
     rebalancing_timeout_ms = param.get("rebalancing_timeout_ms", "60000")
     print(f"rebalancing_timeout_ms {rebalancing_timeout_ms}")
     os.environ["YDB_TEST_ROW_DISPATCHER_REBALANCING_TIMEOUT_MS"] = rebalancing_timeout_ms
@@ -96,9 +97,27 @@ def get_ydb_config(request):
 
 
 class YdbClient:
+<<<<<<< HEAD
     def __init__(self, endpoint: str, database: str, token: str = "root@builtin", enable_discovery: bool = True):
         self.driver_config = ydb.DriverConfig(
             endpoint, database, auth_token=token, disable_discovery=not enable_discovery
+=======
+    WAIT_TIMEOUT: int = 5
+
+    def __fail_retry_callback(self, e):
+        self.retry_settings.on_ydb_error_callback(e)
+        raise RuntimeError(e)
+
+    def __init__(self, driver: ydb.Driver, owns_driver: bool = False):
+        self.owns_driver = owns_driver
+        self.driver = driver
+        if self.owns_driver:
+            self.driver.wait(self.WAIT_TIMEOUT, fail_fast=True)
+
+        self.session_pool = ydb.QuerySessionPool(self.driver)
+        self.retry_settings = ydb.RetrySettings(
+            on_ydb_error_callback=lambda e: logger.error(f"Query execution failed and may be retried: {e}"),
+>>>>>>> 02cd5d09937 (YQ-5552 fixed streaming query stop after restart (#48397))
         )
         self.driver = None
         self.session_pool = None
@@ -118,8 +137,11 @@ class YdbClient:
     def wait_connection(self, timeout: int = 5):
         self.driver.wait(timeout, fail_fast=True)
 
-    def query(self, statement: str):
-        return self.session_pool.execute_with_retries(statement, retry_settings=self.retry_settings)
+    def query(self, statement: str, fail_fast: bool = False):
+        retry_settings = copy.copy(self.retry_settings)
+        if fail_fast:
+            retry_settings.on_ydb_error_callback = lambda e: self.__fail_retry_callback(e)
+        return self.session_pool.execute_with_retries(statement, retry_settings=retry_settings)
 
     def query_async(self, statement: str, timeout: Optional[float] = None):
         settings = None

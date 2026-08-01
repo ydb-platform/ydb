fc9465558b95ec7a77294160c2c86407007745fc
YDBot <ydbot@ydb.tech>
2026-08-01 14:32:31 +0300

    [Backport stable-26-2-1] PR #47992: fix waiting tx (#48362)
    Co-authored-by: Oleg Doronin <dorooleg@yandex.ru>

--- ydb/core/tx/columnshard/transactions/operators/schema.cpp
+++ ydb/core/tx/columnshard/transactions/operators/schema.cpp
@@ -84,7 +84,9 @@ public:
     virtual void DoOnEvent(const std::shared_ptr<NSubscriber::ISubscriptionEvent>& ev) override {
         AFL_VERIFY(ev->GetType() == NSubscriber::EEventType::TxCompleted);
         const auto* evCompleted = static_cast<const NSubscriber::TEventTxCompleted*>(ev.get());
-        AFL_VERIFY(TxIdsToWait.erase(evCompleted->GetTxId()));
+        if (!TxIdsToWait.erase(evCompleted->GetTxId())) {
+            return;
+        }
         AFL_DEBUG(NKikimrServices::TX_COLUMNSHARD)("completed", evCompleted->GetTxId())("remained", JoinSeq(",", TxIdsToWait));
     }
 };

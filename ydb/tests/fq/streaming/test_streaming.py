a675d6a9fabcfbb5888dc62cf34dc9a8468832a3
YDBot <ydbot@ydb.tech>
2026-08-01 15:00:07 +0300

    [Backport stable-26-3] PR #48397: YQ-5552 fixed streaming query stop after restart (#48444)
    Co-authored-by: Pisarenko Grigoriy <grigoriypisar@ydb.tech>

--- ydb/tests/fq/streaming/test_streaming.py
+++ ydb/tests/fq/streaming/test_streaming.py
@@ -8,7 +8,7 @@ from typing import Callable
 
 import ydb
 
-from ydb.tests.fq.streaming_common.common import Kikimr, StreamingTestBase
+from ydb.tests.fq.streaming_common.common import Kikimr, StreamingTestBase, YdbClient
 from ydb.tests.tools.datastreams_helpers.control_plane import create_read_rule
 
 logger = logging.getLogger(__name__)
@@ -1543,3 +1543,47 @@ FROM `{table_name}`"""
 
         result_sets = kikimr.ydb_client.query(sql)
         assert result_sets[0].rows[0]['Data'] == message
+
+    @pytest.mark.parametrize("local_topics", [True, False])
+    @pytest.mark.parametrize("kikimr", [{"enable_discovery": False, "lease_duration_sec": "30"}], indirect=["kikimr"])
+    def test_streaming_query_stop_after_restart(self: StreamingTestBase, kikimr: Kikimr, entity_name: Callable[[str], str], local_topics: bool) -> None:
+        inp, out, endpoint = self.get_io_names(kikimr, f"test_stop_after_restart_{local_topics!s:.1}", local_topics, entity_name)
+
+        path = f"/Root/{entity_name(f'test_stop_after_restart_query_{local_topics!s:.1}')}"
+        kikimr.ydb_client.query(f"""
+            CREATE STREAMING QUERY `{path}` AS DO BEGIN
+                INSERT INTO {out}
+                SELECT * FROM {inp}
+            END DO;
+        """)
+
+        self.wait_completed_checkpoints(kikimr, path)
+
+        expected_data = ["test_data1"]
+        self.write_stream(expected_data, endpoint=endpoint)
+        assert self.read_stream(len(expected_data), topic_path=self.output_topic, endpoint=endpoint) == expected_data
+
+        self.wait_completed_checkpoints(kikimr, path)
+        logger.info("Query checked")
+
+        kikimr.ydb_client.stop()
+        kikimr.first_node.stop()
+        kikimr.first_node.set_log_file_prefix("logfile_restarted_")
+        kikimr.first_node.start()
+        logger.info("Node with query restarted")
+
+        second_node = list(kikimr.cluster.nodes.values())[1]
+        kikimr.ydb_client = YdbClient(database=kikimr.endpoint.database, endpoint=f"grpc://{second_node.host}:{second_node.port}", enable_discovery=False)
+        kikimr.ydb_client.wait_connection()
+        kikimr.ydb_client.query(f"""
+            ALTER STREAMING QUERY `{path}` SET (RUN = FALSE);
+        """, fail_fast=True)
+        logger.info("Query stopped")
+
+        expected_data = ["test_data2"]
+        self.write_stream(expected_data, endpoint=endpoint)
+
+        kikimr.ydb_client.query(f"""
+            ALTER STREAMING QUERY `{path}` SET (RUN = TRUE);
+        """)
+        assert self.read_stream(len(expected_data), topic_path=self.output_topic, endpoint=endpoint) == expected_data

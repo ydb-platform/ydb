7743636833dfdbdbbc6708e45bac85bffa044250
baymer <baymer@yandex-team.com>
2026-08-01 03:52:47 +0300

    nots.py: поправить разбор пакетов с неймспейсом
    <section id="quibbler-autodescription">
    
    #### Исправление разбора пакетов с неймспейсом 📝
    
    - 🐛 Исправлена логика извлечения пути тарболла для пакетов с неймспейсом, чтобы корректно обрабатывались случаи, когда в имени файла содержится символ `/`
    - 🛠️ Добавлена функция нормализации URL тарболла, которая корректно обрабатывает пути, содержащие разделитель `/-/` и обеспечивает правильное формирование итогового пути
    - 🧪 Добавлены тесты, проверяющие корректную обработку пакетов с неймспейсом и сложными структурами имен файлов в URL тарболлов
    - 📦 Обновлены ресурсы сборки с актуальными метаданными и идентификатором пулл-реквеста
    
    <a href="https://nda.ya.ru/t/qa0kX64r7DqvtN"><font size="2">Autodescription by Yandex Code Assistant</font></a>
    </section>
    commit_hash:6cb2272c6adf213310d16be4a51cd921a55fb206

--- build/plugins/lib/nots/package_manager/lockfile.py
+++ build/plugins/lib/nots/package_manager/lockfile.py
@@ -56,11 +56,22 @@ class LockfilePackageMeta(object):
         return " ".join([self.tarball_url, self.sky_id, self.integrity, self.integrity_algorithm])
 
     def to_uri(self):
-        tarball_url: str = self.tarball_url
+        tarball_url = _normalize_tarball_url(self.tarball_url)
         if not tarball_url.startswith("https://") and not tarball_url.startswith("http://"):
             tarball_url = "https://npm.yandex-team.ru/" + tarball_url
         pkg_uri = f"{tarball_url}#integrity={self.integrity_algorithm}-{self.integrity}"
         return pkg_uri
+
+
+def _normalize_tarball_url(tarball_url):
+    parsed_url = urlparse.urlparse(tarball_url)
+    package_path, separator, filename = parsed_url.path.rpartition("/-/")
+
+    if separator and "/" in filename:
+        normalized_path = "{}/-/{}".format(package_path, filename.rsplit("/", 1)[-1])
+        return urlparse.urlunparse(parsed_url._replace(path=normalized_path))
+
+    return tarball_url
 
 
 def _tarball_path_from_url(tarball_url):
@@ -72,7 +83,7 @@ def _tarball_path_from_url(tarball_url):
         package_name = package_parts[-1]
         if len(package_parts) > 1 and package_parts[-2].startswith("@"):
             package_name = "/".join(package_parts[-2:])
-        return "{}/-/{}".format(package_name, filename)
+        return "{}/-/{}".format(package_name, filename.rsplit("/", 1)[-1])
 
     # Keep supporting non-standard tarball values that do not use the npm /-/ layout.
     return "/".join(path.split("/")[-3:])

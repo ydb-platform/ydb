b5969a61ff22fa4191cdc6a902acb9ee3c091264
Aleksei Moiseitsev <swarmer@ydb.tech>
2026-08-03 15:37:37 +0000

    tidy

--- ydb/core/persqueue/pqtablet/partition/mlp/ut/mlp_storage_ut.cpp
+++ ydb/core/persqueue/pqtablet/partition/mlp/ut/mlp_storage_ut.cpp
@@ -523,7 +523,7 @@ Y_UNIT_TEST(AddMessageWithSkippedMessage) {
 
     TStorage storage(timeProvider, {});
 
-    storage.AddMessage(3, true, 5, timeProvider->Now() - TDuration::Seconds(137));
+    storage.AddMessage(3, true, 5, writeTimestamp3);
     UNIT_ASSERT_VALUES_EQUAL(storage.GetFirstOffset(), 3);
     UNIT_ASSERT_VALUES_EQUAL(storage.GetLastOffset(), 4);
 

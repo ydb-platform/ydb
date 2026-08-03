ed6c148b60f7e10acd2ef1d14b5113cd4d721041
zverevgeny <zverevgeny@ydb.tech>
2026-08-03 16:36:01 +0300

    Push streaming queries with shared reading to designated pool (#48504)

--- ydb/services/workload_manager/ut/query_classifier_match_ut.cpp
+++ ydb/services/workload_manager/ut/query_classifier_match_ut.cpp
@@ -23,10 +23,10 @@ void CheckDecision(std::function<void(NResourcePool::TPoolSettings&)> setup, EEx
     });
 
     TClassifyContext ctx{.PoolId = "", .AppName = "", .UserToken = nullptr};
-    auto classifier = NWorkloadManager::CreateQueryClassifier(
-        poolSnap, TClassifierConfigsView(classifierSnap, TEST_DB), TEST_DB, std::move(ctx));
+    auto classifier = CreateQueryClassifier(
+        poolSnap, TClassifierConfigsView(classifierSnap, TEST_DB), TEST_DB, std::move(ctx), std::nullopt);
 
-    NKqp::TUserRequestContext userRequestContext;
+    NKqp::TUserRequestContext userRequestContext{};
     auto result = classifier->PreCompileClassify(userRequestContext);
 
     switch (expected) {

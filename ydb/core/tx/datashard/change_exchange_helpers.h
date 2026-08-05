130f1690b344487d17185eb29cdaaf1d1f8052a4
Konstantin Seleznyov <kseleznyov@ydb.tech>
2026-08-05 19:53:20 +0000

    remove LOG_X macroses

--- ydb/core/tx/datashard/change_exchange_helpers.h
+++ ydb/core/tx/datashard/change_exchange_helpers.h
@@ -2,14 +2,6 @@
 
 #include "defs.h"
 
-#define LOG_T(stream) LOG_TRACE_S (*TlsActivationContext, NKikimrServices::CHANGE_EXCHANGE, GetLogPrefix() << stream)
-#define LOG_D(stream) LOG_DEBUG_S (*TlsActivationContext, NKikimrServices::CHANGE_EXCHANGE, GetLogPrefix() << stream)
-#define LOG_I(stream) LOG_INFO_S  (*TlsActivationContext, NKikimrServices::CHANGE_EXCHANGE, GetLogPrefix() << stream)
-#define LOG_N(stream) LOG_NOTICE_S(*TlsActivationContext, NKikimrServices::CHANGE_EXCHANGE, GetLogPrefix() << stream)
-#define LOG_W(stream) LOG_WARN_S  (*TlsActivationContext, NKikimrServices::CHANGE_EXCHANGE, GetLogPrefix() << stream)
-#define LOG_E(stream) LOG_ERROR_S (*TlsActivationContext, NKikimrServices::CHANGE_EXCHANGE, GetLogPrefix() << stream)
-#define LOG_C(stream) LOG_CRIT_S  (*TlsActivationContext, NKikimrServices::CHANGE_EXCHANGE, GetLogPrefix() << stream)
-
 namespace NKikimr {
 namespace NDataShard {
 

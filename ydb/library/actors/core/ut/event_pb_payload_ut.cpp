85ee19207d17d3d15b02dbc2ef03ceffdfdc13d4
Alexander Rutkovsky <alexvru@ydb.tech>
2026-08-05 09:24:10 +0300

    Fix serializer in ICv2 (#48925)

--- ydb/library/actors/core/ut/event_pb_payload_ut.cpp
+++ ydb/library/actors/core/ut/event_pb_payload_ut.cpp
@@ -56,7 +56,7 @@ Y_UNIT_TEST_SUITE(TEventProtoWithPayload) {
 
         TString chunkerRes;
         TCoroutineChunkSerializer chunker;
-        chunker.SetSerializingEvent(&msg, true);
+        chunker.SetSerializingEvent(&msg, true, false);
         while (!chunker.IsComplete()) {
             char buffer[4096];
             auto range = chunker.FeedBuf(buffer, sizeof(buffer));
@@ -96,7 +96,7 @@ Y_UNIT_TEST_SUITE(TEventProtoWithPayload) {
         for (size_t chunkSize = 1; chunkSize <= 32; ++chunkSize) {
             TString actual;
             TCoroutineChunkSerializer chunker;
-            chunker.SetSerializingEvent(&msg, true);
+            chunker.SetSerializingEvent(&msg, true, false);
             while (!chunker.IsComplete()) {
                 TString buffer(chunkSize, '\0');
                 for (const auto& chunk : chunker.FeedBuf(buffer.begin(), buffer.size())) {
--- ydb/library/actors/interconnect/interconnect_channel.cpp
+++ ydb/library/actors/interconnect/interconnect_channel.cpp
@@ -174,7 +174,7 @@ namespace NActors {
                         State = EState::BODY;
                         IEventBase *base = event.Event.Get();
                         if (event.EventSerializedSize) {
-                            Chunker.SetSerializingEvent(base, /*withCachedSizes=*/ true);
+                            Chunker.SetSerializingEvent(base, /*withCachedSizes=*/ true, /*withCord=*/ false);
                         }
                         SerializationInfoContainer = base->CreateSerializationInfo(Params.UseExternalDataChannel);
                         SerializationInfo = &SerializationInfoContainer;

c893307905a8e1af358d0dcd4dcd556df3635e77
robot-piglet <robot-piglet@yandex-team.com>
2026-08-06 13:27:26 +0300

    Intermediate changes commit_hash:549ebb70c508e5b476c1c00d53a88f362fc5c2a1

--- contrib/python/yarl/tests/test_cached_property.py
+++ contrib/python/yarl/tests/test_cached_property.py
@@ -1,29 +1,29 @@
 import pytest
 
-from yarl._url import cached_property
+from yarl._url import cached_property  # type: ignore[attr-defined]
 
 
 class A:
-    def __init__(self):
-        self._cache = {}
+    def __init__(self) -> None:
+        self._cache: dict[str, int] = {}
 
     @cached_property
-    def prop(self):
+    def prop(self) -> int:
         """Docstring."""
         return 1
 
 
-def test_reify():
+def test_reify() -> None:
     a = A()
     assert 1 == a.prop
 
 
-def test_reify_class():
+def test_reify_class() -> None:
     assert isinstance(A.prop, cached_property)
     assert "Docstring." == A.prop.__doc__
 
 
-def test_reify_assignment():
+def test_reify_assignment() -> None:
     a = A()
 
     with pytest.raises(AttributeError):

2e95b46577320e04cb8c582126ccf883848e3b9f
Ivan Efimov <efimov-ivan@yandex-team.ru>
2026-08-06 02:02:47 +0000

    Add dummy request handlers

---
+++ ydb/core/nbs/cloud/blockstore/libs/storage/dbs_controller/dbs_controller_query.cpp
@@ -0,0 +1,49 @@
+#include "dbs_controller_actor.h"
+
+#include <ydb/core/nbs/cloud/storage/core/libs/actors/helpers.h>
+
+namespace NYdb::NBS::NBlockStore::NStorage::NDbsController {
+
+////////////////////////////////////////////////////////////////////////////////
+
+void TDbsControllerActor::HandleGetNodesForPartitionRequest(
+    const TEvDbsControllerPrivate::TEvGetNodesForPartitionRequest::TPtr& ev,
+    const NActors::TActorContext& ctx)
+{
+    LOG_INFO_S(
+        ctx,
+        NKikimrServices::DBS_CONTROLLER,
+        "Handle GetNodesForPartition request"
+            << ", tabletId: " << ev->Get()->Record.GetPartitionTabletId());
+
+    // TODO Dummy
+    Reply(
+        ctx,
+        *ev,
+        std::make_unique<
+            TEvDbsControllerPrivate::TEvGetNodesForPartitionResponse>(
+            MakeError(S_OK)));
+}
+
+void TDbsControllerActor::HandleGetPartitionsForNodeRequest(
+    const TEvDbsControllerPrivate::TEvGetPartitionsForNodeRequest::TPtr& ev,
+    const NActors::TActorContext& ctx)
+{
+    LOG_INFO_S(
+        ctx,
+        NKikimrServices::DBS_CONTROLLER,
+        "Handle GetPartitionsForNode request" << ", tabletId: "
+                                              << ev->Get()->Record.GetNodeId());
+
+    // TODO Dummy
+    Reply(
+        ctx,
+        *ev,
+        std::make_unique<
+            TEvDbsControllerPrivate::TEvGetPartitionsForNodeResponse>(
+            MakeError(S_OK)));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+}   // namespace NYdb::NBS::NBlockStore::NStorage::NDbsController

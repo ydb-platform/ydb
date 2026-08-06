8872fbb1d47072e0afdc7223772645668f2abf55
Ivan Efimov <efimov-ivan@yandex-team.ru>
2026-08-06 00:37:24 +0000

    Update dbs_controller.proto

--- ydb/core/nbs/cloud/blockstore/libs/storage/dbs_controller/dbs_controller_schema.h
+++ ydb/core/nbs/cloud/blockstore/libs/storage/dbs_controller/dbs_controller_schema.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <ydb/core/nbs/cloud/blockstore/libs/storage/core/tablet_schema.h>
-#include <ydb/core/nbs/cloud/blockstore/libs/storage/dbs_controller/protos/dbs_controller.pb.h>
 
 #include <ydb/core/tablet_flat/flat_cxx_database.h>
 
@@ -11,20 +10,75 @@ namespace NYdb::NBS::NBlockStore::NStorage::NDbsController {
 
 struct TDbsControllerSchema: public NKikimr::NIceDb::Schema
 {
-    struct DDiskMap : Table<1> {
-        struct TabletId : Column<1, NKikimr::NScheme::NTypeIds::Uint64> {};
+    struct DDiskMap: TTableSchema<1>
+    {
+        struct TabletId: Column<1, NKikimr::NScheme::NTypeIds::Uint64>
+        {
+        };
+
+        // Unique within TabletId
+        struct DirectBlockGroupId: Column<2, NKikimr::NScheme::NTypeIds::Uint64>
+        {
+        };
 
         // DDiskId.NodeId
-        struct NodeId : Column<1, NKikimr::NScheme::NTypeIds::Uint32> {};
+        struct NodeId: Column<3, NKikimr::NScheme::NTypeIds::Uint32>
+        {
+        };
 
         // DDiskId.PDiskId
-        struct PDiskId : Column<1, NKikimr::NScheme::NTypeIds::Uint32> {};
+        struct PDiskId: Column<4, NKikimr::NScheme::NTypeIds::Uint32>
+        {
+        };
 
         // DDiskId.DDiskSlotId
-        struct DDiskSlotId : Column<1, NKikimr::NScheme::NTypeIds::Uint32> {};
+        struct DDiskSlotId: Column<5, NKikimr::NScheme::NTypeIds::Uint32>
+        {
+        };
 
-        using TKey = TableKey<TabletId, NodeId, PDiskId, DDiskSlotId>;
-        using TColumns = TableColumns<TabletId, NodeId, PDiskId, DDiskSlotId>;
+        struct IsPBuffer: Column<6, NKikimr::NScheme::NTypeIds::Bool>
+        {
+        };
+
+        using TKey = TableKey<
+            TabletId,
+            DirectBlockGroupId,
+            NodeId,
+            PDiskId,
+            DDiskSlotId>;
+
+        using TColumns = TableColumns<
+            TabletId,
+            DirectBlockGroupId,
+            NodeId,
+            PDiskId,
+            DDiskSlotId>;
+    };
+
+    struct InverseDDiskMap: TTableSchema<2>
+    {
+        // DDiskId.NodeId
+        struct NodeId: Column<1, NKikimr::NScheme::NTypeIds::Uint32>
+        {
+        };
+
+        // DDiskId.PDiskId
+        struct PDiskId: Column<2, NKikimr::NScheme::NTypeIds::Uint32>
+        {
+        };
+
+        // DDiskId.DDiskSlotId
+        struct DDiskSlotId: Column<3, NKikimr::NScheme::NTypeIds::Uint32>
+        {
+        };
+
+        struct TabletId: Column<4, NKikimr::NScheme::NTypeIds::Uint64>
+        {
+        };
+
+        using TKey = TableKey<NodeId, PDiskId, DDiskSlotId, TabletId>;
+
+        using TColumns = TableColumns<NodeId, PDiskId, DDiskSlotId, TabletId>;
     };
 
     using TTables = SchemaTables<DDiskMap>;

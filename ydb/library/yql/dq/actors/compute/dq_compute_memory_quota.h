664c3a225e6bdc5e5e36c47c46efd2c588358fa6
Hor911 <hor911@ydb.tech>
2026-08-07 13:42:03 +0300

    Revert "[Backport stable-26-3] PR #49205: Correct InitialMemoryLimit setup"

--- ydb/library/yql/dq/actors/compute/dq_compute_memory_quota.h
+++ ydb/library/yql/dq/actors/compute/dq_compute_memory_quota.h
@@ -30,20 +30,16 @@ namespace NYql::NDq {
         TDqMemoryQuota(::NMonitoring::TDynamicCounters::TCounterPtr& mkqlMemoryQuota, ui64 initialMkqlMemoryLimit, const NYql::NDq::TComputeMemoryLimits& memoryLimits, NYql::NDq::TTxId txId, ui64 taskId, bool profileStats, NActors::TActorSystem* actorSystem)
             : MkqlMemoryQuota(mkqlMemoryQuota)
             , InitialMkqlMemoryLimit(initialMkqlMemoryLimit)
-            , MkqlMemoryLimit(0)
+            , MkqlMemoryLimit(initialMkqlMemoryLimit)
             , MemoryLimits(memoryLimits)
             , TxId(txId)
             , TaskId(taskId)
             , ProfileStats(profileStats ? MakeHolder<TProfileStats>() : nullptr)
             , ActorSystem(actorSystem) {
 
-            if (MemoryLimits.MemoryQuotaManager->AllocateQuota(InitialMkqlMemoryLimit)) {
-                MkqlMemoryLimit = InitialMkqlMemoryLimit;
-                if (MkqlMemoryQuota) {
-                    MkqlMemoryQuota->Add(MkqlMemoryLimit);
-                }
-            } else {
-                CAMQ_LOG_W("[Mem] initial memory allocation of " << InitialMkqlMemoryLimit << " failed, starting with 0");
+            Y_ABORT_UNLESS(MemoryLimits.MemoryQuotaManager->AllocateQuota(MkqlMemoryLimit));
+            if (MkqlMemoryQuota) {
+                MkqlMemoryQuota->Add(MkqlMemoryLimit);
             }
         }
 

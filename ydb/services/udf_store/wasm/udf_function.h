22ca25473d7d76da9432be7d79e12302af2507a7
Anatoly Kulakov <kulaad@ydb.tech>
2026-08-07 17:02:56 +0000

    add udf store wasm implementation

---
+++ ydb/services/udf_store/wasm/udf_function.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "registry.h"
+
+#include <yql/essentials/public/udf/udf_helpers.h>
+
+namespace NKikimr::NUdfStore::NWasm {
+
+using namespace NYql::NUdf;
+
+class TWasmUdfFunction: public TBoxedValue {
+public:
+    static TType* BuildYqlType(IFunctionTypeInfoBuilder& builder, EUdfValueType type);
+
+    static TType* BuildFunctionType(
+        IFunctionTypeInfoBuilder& builder,
+        const TWasmUdfDescriptor& descriptor);
+
+    static void Register(
+        IFunctionTypeInfoBuilder& builder,
+        bool typesOnly,
+        TWasmCompartmentStatePtr state,
+        const TWasmUdfDescriptor& descriptor);
+
+private:
+    TWasmUdfFunction(TWasmCompartmentStatePtr state, const TWasmUdfDescriptor& descriptor);
+
+    TUnboxedValue Run(const IValueBuilder* valueBuilder, const TUnboxedValuePod* args) const override;
+
+    TWasmCompartmentStatePtr State_;
+    TWasmUdfDescriptor Descriptor_;
+};
+
+class TWasmSoModule: public IUdfModule {
+public:
+    TWasmSoModule(TWasmCompartmentStatePtr state, TString moduleName);
+
+    void CleanupOnTerminate() const final;
+
+    void GetAllFunctions(IFunctionsSink& sink) const final;
+
+    void BuildFunctionTypeInfo(
+        const TStringRef& name,
+        TType* userType,
+        const TStringRef& typeConfig,
+        ui32 flags,
+        IFunctionTypeInfoBuilder& builder) const final;
+
+private:
+    TWasmCompartmentStatePtr State_;
+    TString ModuleName_;
+};
+
+TUniquePtr<IUdfModule> BuildWasmSoModule(TWasmCompartmentStatePtr state);
+
+} // namespace NKikimr::NUdfStore::NWasm

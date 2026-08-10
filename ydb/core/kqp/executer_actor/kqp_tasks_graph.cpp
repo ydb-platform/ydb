8adcd2c7c35d5d05a6f347950a21f0c61d5b48fb
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-08-10 22:37:32 +0300

    fixup! fixup! slj shuffle mode: add feature-flag (with tests)

--- ydb/core/kqp/executer_actor/kqp_tasks_graph.cpp
+++ ydb/core/kqp/executer_actor/kqp_tasks_graph.cpp
@@ -1255,7 +1255,7 @@ void TKqpTasksGraph::BuildDqSourceStreamLookupChannels(const TStageInfo& stageIn
     }
     if (!AppData()->FeatureFlags.GetEnableDqSourceStreamLookupJoinShuffleMode()) {
         Y_ENSURE(
-            dqSourceStreamLookup.GetShuffleMode() == NKqpProto::TKqpPhyCnDqSourceStreamLookup_EShuffleMode_DEFAULT,
+            dqSourceStreamLookup.GetShuffleMode() == NKqpProto::TKqpPhyCnDqSourceStreamLookup_EShuffleMode_DEFAULT || dqSourceStreamLookup.GetShuffleMode() == NKqpProto::TKqpPhyCnDqSourceStreamLookup_EShuffleMode_OFF,
             TStringBuilder{} << "EnableDqSourceStreamLookupJoinShuffleMode disabled, but ShuffleMode is " << static_cast<NDq::EShuffleMode>(dqSourceStreamLookup.GetShuffleMode()));
     }
     /* ShuffleMode intentionally omitted */

534261415c498d36ce458cd5dee549eb96261f05
st-shchetinin <st-shchetinin@ydb.tech>
2026-08-10 12:26:00 +0000

    fix

--- ydb/core/backup/common/encryption.h
+++ ydb/core/backup/common/encryption.h
@@ -4,11 +4,16 @@
 
 #include <util/generic/buffer.h>
 #include <util/generic/maybe.h>
+#include <util/generic/size_literals.h>
 #include <util/generic/string.h>
 
 #include <vector>
 
 namespace NKikimr::NBackup {
+
+// Max block size must always be at least size of table row (~8 MB) serialized into text csv format.
+// Real value is bound to 32 MB in TBackupTask.TScanSettings.BytesBatchSize setting.
+static constexpr size_t MAX_BLOCK_SIZE = 50_MB;
 
 TString NormalizeEncryptionAlgorithmName(const TString& name);
 

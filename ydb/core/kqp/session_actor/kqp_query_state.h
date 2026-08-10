325ed099c79a2d6740f5dd6b90bf1f2d16805a8e
Anely <anely-d@ydb.tech>
2026-08-10 14:41:54 +0300

    add absent queries and info whether is it absent intentionally (#49160)

--- ydb/core/kqp/session_actor/kqp_query_state.h
+++ ydb/core/kqp/session_actor/kqp_query_state.h
@@ -156,6 +156,7 @@ public:
     std::shared_ptr<NWorkloadManager::IQueryClassifier> QueryClassifier;
     ui64 ParametersSize = 0;
     TPreparedQueryHolder::TConstPtr PreparedQuery;
+    TString QueryTextForLogging;
     TKqpCompileResult::TConstPtr CompileResult;
     TVector<NKikimrKqp::TParameterDescription> ResultParams;
     TKqpStatsCompile CompileStats;

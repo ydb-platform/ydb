c533c8dbb28f08bbbc5d94a0f4e90294a6820cfd
Sergey Belyakov <serg-belyakov@ydb.tech>
2026-08-11 10:22:00 +0000

    Typo

--- ydb/library/wilson_ids/wilson.h
+++ ydb/library/wilson_ids/wilson.h
@@ -33,7 +33,7 @@ namespace NKikimr {
 
         enum : ui8 {
             // Detalisation level that doesn't reach storage nodes
-            DynnodesOnly = 4,
+            DynamicNodesOnly = 4,
             // The most verbose detalisation level used in production
             ProductionVerbose = 13,
             // The most verbose detalisation level

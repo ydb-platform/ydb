89fa0209149b637e1c3bba910ab4772c3373706a
Ivan Efimov <efimov-ivan@yandex-team.ru>
2026-08-11 09:14:25 +0200

    [NBS-7512] Implement DbsController DDiskMap API, part 3 (#49403)
    (cherry picked from commit 77c34c5d48169e03f5fc1ea8c7187ba639f49a1d)

--- ydb/core/nbs/cloud/blockstore/libs/storage/dbs_controller/dbs_controller_update_ddisk_map.cpp
+++ ydb/core/nbs/cloud/blockstore/libs/storage/dbs_controller/dbs_controller_update_ddisk_map.cpp
@@ -1,4 +1,5 @@
 #include "dbs_controller_actor.h"
+#include "dbs_controller_database.h"
 
 #include <ydb/core/nbs/cloud/storage/core/libs/actors/helpers.h>
 
@@ -16,12 +17,61 @@ void TDbsControllerActor::HandleUpdateDDiskMapRequest(
         "Handle UpdateDDiskMap request" << ", tabletId: "
                                         << ev->Get()->Record.GetTabletId());
 
-    // TODO Dummy
-    Reply(
+    ExecuteTx(
         ctx,
-        *ev,
+        CreateTx<TUpdateDDiskMap>(
+            NBS::NStorage::CreateRequestInfo(
+                ev->Sender,
+                ev->Cookie,
+                MakeIntrusive<TCallContext>()),
+            ev->Get()->Record.GetTabletId(),
+            ev->Get()->Record.GetPartitionDDisks()));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool TDbsControllerActor::PrepareUpdateDDiskMap(
+    const NActors::TActorContext& ctx,
+    NKikimr::NTabletFlatExecutor::TTransactionContext& tx,
+    TTxDbsController::TUpdateDDiskMap& args)
+{
+    Y_UNUSED(ctx);
+
+    TDbsControllerDatabase db(tx.DB);
+
+    return db.GetRecordKeysForTablet(
+        args.PartitionTabletId,
+        args.TabletRecordsKeys);
+}
+
+void TDbsControllerActor::ExecuteUpdateDDiskMap(
+    const NActors::TActorContext& ctx,
+    NKikimr::NTabletFlatExecutor::TTransactionContext& tx,
+    TTxDbsController::TUpdateDDiskMap& args)
+{
+    Y_UNUSED(ctx);
+
+    TDbsControllerDatabase db(tx.DB);
+
+    db.ClearRecords(args.TabletRecordsKeys);
+    db.FillTabletRecords(args.PartitionTabletId, args.DDisks);
+}
+
+void TDbsControllerActor::CompleteUpdateDDiskMap(
+    const NActors::TActorContext& ctx,
+    TTxDbsController::TUpdateDDiskMap& args)
+{
+    LOG_INFO(
+        ctx,
+        NKikimrServices::DBS_CONTROLLER,
+        "UpdateDDiskMap persisted data for tablet %" PRIu64,
+        args.PartitionTabletId);
+
+    auto response =
         std::make_unique<TEvDbsControllerPrivate::TEvUpdateDDiskMapResponse>(
-            MakeError(S_OK)));
+            MakeError(S_OK));
+
+    Reply(ctx, *args.RequestInfo, std::move(response));
 }
 
 ////////////////////////////////////////////////////////////////////////////////

448a2d21b10046f2a84d9e9a1c8f858777de1ca5
alexpaniman <alexpaniman@gmail.com>
2026-08-11 16:19:48 +0000

    [NEW RBO] Remove obsolete map ordered flag

--- ydb/core/kqp/opt/rbo/kqp_operator.h
+++ ydb/core/kqp/opt/rbo/kqp_operator.h
@@ -481,9 +481,8 @@ private:
 
 class TOpMap: public IUnaryOperator {
 public:
-    TOpMap(TIntrusivePtr<IOperator> input, TPositionHandle pos, const TVector<TMapElement>& mapElements, bool ordered = false);
-    TOpMap(TIntrusivePtr<IOperator> input, TPositionHandle pos, const TPhysicalOpProps& props, const TVector<TMapElement>& mapElements,
-           bool ordered = false);
+    TOpMap(TIntrusivePtr<IOperator> input, TPositionHandle pos, const TVector<TMapElement>& mapElements);
+    TOpMap(TIntrusivePtr<IOperator> input, TPositionHandle pos, const TPhysicalOpProps& props, const TVector<TMapElement>& mapElements);
 
     virtual TVector<TInfoUnit> GetUsedIUs(TPlanProps& props) override;
     virtual TVector<TInfoUnit> GetSubplanIUs(TPlanProps& props) override;
@@ -506,10 +505,6 @@ public:
     virtual NJson::TJsonValue ToJson(ui32 explainFlags) override;
     virtual TString GetExplainName() const override { return "Map"; }
 
-    bool IsOrdered() const {
-        return Ordered;
-    }
-
     const TVector<TMapElement>& GetMapElements() const { return MapElements; }
     void SetMapElements(TVector<TMapElement> mapElements);
     void AddMapElement(TMapElement mapElement);
@@ -518,8 +513,6 @@ public:
     const TMapElement* FindOutputElement(const TInfoUnit& output) const;
     bool HasOutputElement(const TInfoUnit& output) const;
     bool HasRenames() const;
-
-    bool Ordered = false;
 
 protected:
     void ComputeOutputIUs() override;
--- ydb/core/kqp/opt/rbo/rules/map/push_map_elements_into_map.cpp
+++ ydb/core/kqp/opt/rbo/rules/map/push_map_elements_into_map.cpp
@@ -270,7 +270,7 @@ TPushMapElementsIntoMapRule::SimpleMatchAndApply(const TIntrusivePtr<IOperator>&
         return bottomMap;
     }
 
-    return MakeIntrusive<TOpMap>(bottomMap, topMap->Pos, topElements, topMap->Ordered);
+    return MakeIntrusive<TOpMap>(bottomMap, topMap->Pos, topElements);
 }
 
 } // namespace NKqp

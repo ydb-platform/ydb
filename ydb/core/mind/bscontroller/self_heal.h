5b6d17d94328ecc56739dd36a7663cc4eabc372a
Sergey Belyakov <serg-belyakov@ydb.tech>
2026-08-12 09:50:20 +0000

    Rewrite SelfHeal actor reassignment logic

--- ydb/core/mind/bscontroller/self_heal.h
+++ ydb/core/mind/bscontroller/self_heal.h
@@ -18,25 +18,42 @@ namespace NKikimr::NBsController {
         bool WithAttentionToReplication = false;
     };
 
+    enum ESelfHealReassignmentPriority : ui8 {
+        None = 0,           // Reassignment is not required
+        MaintenanceStatus,  // Reassignment is only performed if *all* other
+                            // VDisks are fully operational
+        DecommitStatus,
+        DriveStatus,
+    };
+
     struct TEvControllerUpdateSelfHealInfo : TEventLocal<TEvControllerUpdateSelfHealInfo, TEvBlobStorage::EvControllerUpdateSelfHealInfo> {
         struct TGroupContent {
+
             struct TVDiskInfo {
                 TVSlotId Location;
-                bool RequiresReassignment;
+                ESelfHealReassignmentPriority ReassignmentPriority = ESelfHealReassignmentPriority::None;
                 bool UnavailabilityRisk;
                 bool Decommitted;
-                bool IsSelfHealReasonDecommit;
                 bool OnlyPhantomsRemain;
                 bool IsReady;
                 TMonotonic ReadySince;
                 NKikimrBlobStorage::EVDiskStatus VDiskStatus;
                 std::optional<TString> DiskScope;
+
+                bool RequiresReassignment() const {
+                    return ReassignmentPriority != ESelfHealReassignmentPriority::None;
+                }
+
+                bool IsReassignmentUrgent() const {
+                    return ReassignmentPriority > ESelfHealReassignmentPriority::MaintenanceStatus;
+                }
             };
             ui32 Generation;
             TBlobStorageGroupType Type;
             TMap<TVDiskID, TVDiskInfo> VDisks;
             std::shared_ptr<TGroupGeometryInfo> Geometry;
         };
+
         struct TVDiskStatusUpdate {
             TVDiskID VDiskId;
             std::optional<bool> OnlyPhantomsRemain;

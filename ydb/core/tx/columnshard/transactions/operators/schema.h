42a53457b7cbf89236b4c6fe0f87390a37db840d
Evgeny Zverev <zverevgeny@ydb.tech>
2026-08-16 08:06:37 +0000

    Introduce message TTruncateTable

--- ydb/core/tx/columnshard/transactions/operators/schema.h
+++ ydb/core/tx/columnshard/transactions/operators/schema.h
@@ -68,6 +68,9 @@ private:
                 return "Scheme:MoveTable";
             case NKikimrTxColumnShard::TSchemaTxBody::kCopyTable:
                 return "Scheme:CopyTable";
+            case NKikimrTxColumnShard::TSchemaTxBody::kTruncateTable:
+                //TODO implement me
+                return "Scheme:TruncateTable";
             case NKikimrTxColumnShard::TSchemaTxBody::TXBODY_NOT_SET:
                 return "Scheme:TXBODY_NOT_SET";
         }

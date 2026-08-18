d44ac84a7170a1c339e68f0dc62e14cbc78bf99a
Evgeny Zverev <zverevgeny@yandex-team.ru>
2026-08-18 20:38:53 +0300

    Resolved merge conflicts

--- ydb/core/tx/columnshard/transactions/operators/schema.cpp
+++ ydb/core/tx/columnshard/transactions/operators/schema.cpp
@@ -164,18 +164,10 @@ TTxController::TProposeResult TSchemaTransactionOperator::DoStartProposeOnExecut
         case NKikimrTxColumnShard::TSchemaTxBody::kMoveTable: {
             const auto srcSchemeShardLocalPathId = TSchemeShardLocalPathId::FromRawValue(SchemaTxBody.GetMoveTable().GetSrcPathId());
             const auto dstSchemeShardLocalPathId = TSchemeShardLocalPathId::FromRawValue(SchemaTxBody.GetMoveTable().GetDstPathId());
-<<<<<<< HEAD
             AFL_INFO(NKikimrServices::TX_COLUMNSHARD)("propose_execute", "move_table")("src", srcSchemeShardLocalPathId)(
                 "dst", dstSchemeShardLocalPathId);
-            if (!owner.TablesManager.ResolveInternalPathId(srcSchemeShardLocalPathId, false)) {
-=======
-            YDB_LOG_INFO("",
-                {"proposeExecute", "move_table"},
-                {"src", srcSchemeShardLocalPathId},
-                {"dst", dstSchemeShardLocalPathId});
             const auto srcInternalPathId = owner.TablesManager.ResolveInternalPathId(srcSchemeShardLocalPathId, false);
             if (!srcInternalPathId) {
->>>>>>> cfcb48bc0e7 (Disable moving column tables with tiering configured (#50025))
                 return TProposeResult(NKikimrTxColumnShard::EResultStatus::SCHEMA_ERROR, "No such table");
             }
             if (owner.TablesManager.ResolveInternalPathId(dstSchemeShardLocalPathId, false)) {

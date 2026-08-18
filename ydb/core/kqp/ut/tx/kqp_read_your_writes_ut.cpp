76fd7f4676c30374dd5d1d72e15e9cf55bf36a8c
Alexey Zatelepin <ztlpn@ydb.tech>
2026-08-18 16:07:39 +0300

    UpdateThenSelect

--- ydb/core/kqp/ut/tx/kqp_read_your_writes_ut.cpp
+++ ydb/core/kqp/ut/tx/kqp_read_your_writes_ut.cpp
@@ -57,6 +57,56 @@ Y_UNIT_TEST(InsertThenSelectPointRead_ReadCommitted) {
     tester.Execute();
 }
 
+// After updating a row, a point lookup in the same transaction must see the new value.
+class TUpdateThenSelect : public TTableDataModificationTester {
+    TTxSettings TxSettings_;
+public:
+    TUpdateThenSelect(TTxSettings txSettings)
+        : TxSettings_(txSettings)
+    {
+        SetFillTables(false);
+    }
+protected:
+    void DoExecute() override {
+        auto client = Kikimr->GetQueryClient();
+        auto session = client.GetSession().GetValueSync().GetSession();
+
+        auto result = session.ExecuteQuery(R"(
+            INSERT INTO KV2 (Key, Value) VALUES (1u, "original");
+        )", TTxControl::BeginTx().CommitTx()).ExtractValueSync();
+        UNIT_ASSERT_VALUES_EQUAL_C(result.GetStatus(), EStatus::SUCCESS, result.GetIssues().ToString());
+
+        result = session.ExecuteQuery(R"(
+            UPDATE KV2 SET Value = "updated" WHERE Key = 1u;
+        )", TTxControl::BeginTx(TxSettings_)).ExtractValueSync();
+        UNIT_ASSERT_VALUES_EQUAL_C(result.GetStatus(), EStatus::SUCCESS, result.GetIssues().ToString());
+        auto tx = result.GetTransaction();
+
+        result = session.ExecuteQuery(R"(
+            SELECT Value FROM KV2 WHERE Key = 1u;
+        )", TTxControl::Tx(*tx).CommitTx()).ExtractValueSync();
+        UNIT_ASSERT_VALUES_EQUAL_C(result.GetStatus(), EStatus::SUCCESS, result.GetIssues().ToString());
+        CompareYson(R"([[["updated"]]])", FormatResultSetYson(result.GetResultSet(0)));
+    }
+};
+
+Y_UNIT_TEST_TWIN(UpdateThenSelect_Serializable, IsOlap) {
+    TUpdateThenSelect tester(TTxSettings::SerializableRW());
+    tester.SetIsOlap(IsOlap);
+    tester.Execute();
+}
+
+Y_UNIT_TEST_TWIN(UpdateThenSelect_Snapshot, IsOlap) {
+    TUpdateThenSelect tester(TTxSettings::SnapshotRW());
+    tester.SetIsOlap(IsOlap);
+    tester.Execute();
+}
+
+Y_UNIT_TEST(UpdateThenSelect_ReadCommitted) {
+    TUpdateThenSelect tester(TTxSettings::ReadCommittedRW());
+    tester.Execute();
+}
+
 } // Y_UNIT_TEST_SUITE
 
 } // namespace NKqp

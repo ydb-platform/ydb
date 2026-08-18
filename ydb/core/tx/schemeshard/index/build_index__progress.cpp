8671e1b8943c57436ce5a7d120bcf5aaccee191f
Sergey Murylev <smurylev@ydb.tech>
2026-08-18 07:55:26 +0000

    Fix tests

--- ydb/core/tx/schemeshard/index/build_index__progress.cpp
+++ ydb/core/tx/schemeshard/index/build_index__progress.cpp
@@ -478,7 +478,10 @@ THolder<TEvSchemeShard::TEvModifySchemeTransaction> CreateRebuildImplPropose(
     NKikimrSchemeOp::TModifyScheme indexBuildProto;
     buildInfo.SerializeToProto(ss, indexBuildProto.MutableInitiateIndexBuild());
     const auto& indexDesc = indexBuildProto.GetInitiateIndexBuild().GetIndex();
-    const THashSet<TString> indexDataColumns{indexDesc.GetDataColumnNames().begin(), indexDesc.GetDataColumnNames().end()};
+    THashSet<TString> indexDataColumns{indexDesc.GetDataColumnNames().begin(), indexDesc.GetDataColumnNames().end()};
+    const auto indexColumns = NTableIndex::ExtractInfo(indexDesc);
+    Y_ENSURE(!indexColumns.KeyColumns.empty());
+    indexDataColumns.insert(indexColumns.KeyColumns.back());
 
     auto addCreateTable = [&](NKikimrSchemeOp::TTableDescription&& implTableDesc) {
         InheritDetailedMetricsSettings(tableInfo, implTableDesc);

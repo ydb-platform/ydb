fd9d22601bfddf95e408bb2325dfdecd25514152
Pisarenko Grigoriy <grigoriypisar@ydb.tech>
2026-08-19 15:42:08 +0300

    Resolved conflicts

--- ydb/core/kqp/provider/yql_kikimr_settings.cpp
+++ ydb/core/kqp/provider/yql_kikimr_settings.cpp
@@ -100,11 +100,7 @@ TKikimrConfiguration::TKikimrConfiguration() {
     REGISTER_SETTING(*this, OptDisallowFuseJoins);
     REGISTER_SETTING(*this, OptCreateStageForAggregation);
     REGISTER_SETTING(*this, OptValidateStreamingConstraints);
-<<<<<<< HEAD
-=======
     REGISTER_SETTING(*this, OptValidateStreamingCheckpoints);
-    REGISTER_SETTING(*this, OptFallbackToLegacyOptimizer);
->>>>>>> 26e74bceefd (YQ-5607 added checkpoints usage validation for streaming queries (#50184))
     REGISTER_SETTING(*this, OverridePlanner);
     REGISTER_SETTING(*this, UseGraceJoinCoreForMap);
     REGISTER_SETTING(*this, UseBlockHashJoin);

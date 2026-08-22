d4e66a93e2cadb765d355e9a44688d53727172c6
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-08-22 19:19:19 +0300

    resolve conflicts
    (cherry picked from commit c0cf086ea1e34ebb78de12715b2673c3599d49f7)

--- ydb/tests/fq/streaming_common/common.py
+++ ydb/tests/fq/streaming_common/common.py
@@ -48,22 +48,13 @@ def get_ydb_config(request, enable_fq_connector=None):
     enable_streaming_partition_balancing = param.get("use_partition_balancing", True)
     enable_user_attributes_in_topic_query = param.get("enable_user_attributes_in_topic_query", True)
     enable_dq_source_stream_lookup_join = param.get("enable_dq_source_stream_lookup_join", True)
-<<<<<<< HEAD
-=======
-    enable_kqp_constraints_transformer = param.get("kqp_constraints_transformer", True)
     enable_dq_source_stream_lookup_join_local_lookups = param.get(
         "enable_dq_source_stream_lookup_join_local_lookups", False
     )  # TODO YQ-5431
     enable_dq_source_stream_lookup_join_fullscan = param.get("enable_dq_source_stream_lookup_join_fullscan", True)
     enable_dq_source_stream_lookup_join_shuffle_mode = param.get(
-<<<<<<< HEAD
-        "enable_dq_source_stream_lookup_join_shuffle_mode", False
-    )  # TODO YQ-5453
->>>>>>> 8fe7027cd66 (fq tests: port tests/fq/generic/streaming/test_join.py (yqv1) to ydb streaming (#49405))
-=======
         "enable_dq_source_stream_lookup_join_shuffle_mode", True
     )
->>>>>>> a46b9b677fc (dq streamlookupjoin: correctly fix multi-partition: add feature-flag and tests (#41442))
 
     extra_feature_flags = {
         "enable_external_data_sources",

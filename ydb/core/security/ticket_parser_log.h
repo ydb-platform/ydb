bd84b8d7b5fca528767df86329cfcb540f3a9414
Vladislav Serikov <vlad-serikov@ydb.tech>
2026-08-24 10:15:38 +0000

    Rework log and it's priority in ticket parser

--- ydb/core/security/ticket_parser_log.h
+++ ydb/core/security/ticket_parser_log.h
@@ -1,10 +1,11 @@
 #pragma once
 
-#if defined BLOG_D || defined BLOG_I || defined BLOG_ERROR || defined BLOG_TRACE
+#if defined BLOG_ERROR || defined BLOG_WARN || defined BLOG_NOTICE || defined BLOG_DEBUG || defined BLOG_TRACE
 #error log macro definition clash
 #endif
 
-#define BLOG_D(stream) LOG_DEBUG_S(*TlsActivationContext, NKikimrServices::TICKET_PARSER, stream)
-#define BLOG_TRACE(stream) LOG_TRACE_S(*TlsActivationContext, NKikimrServices::TICKET_PARSER, stream)
 #define BLOG_ERROR(stream) LOG_ERROR_S(*TlsActivationContext, NKikimrServices::TICKET_PARSER, stream)
-#define BLOG_W(stream) LOG_WARN_S(*TlsActivationContext, NKikimrServices::TICKET_PARSER, stream)
+#define BLOG_WARN(stream) LOG_WARN_S(*TlsActivationContext, NKikimrServices::TICKET_PARSER, stream)
+#define BLOG_NOTICE(stream) LOG_NOTICE_S(*TlsActivationContext, NKikimrServices::TICKET_PARSER, stream)
+#define BLOG_DEBUG(stream) LOG_DEBUG_S(*TlsActivationContext, NKikimrServices::TICKET_PARSER, stream)
+#define BLOG_TRACE(stream) LOG_TRACE_S(*TlsActivationContext, NKikimrServices::TICKET_PARSER, stream)

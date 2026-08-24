74d79e05a5a3870c5b0e483263ccdd26759b76c7
Vitaliy Filippov <vitaliff@ydb.tech>
2026-08-24 19:31:16 +0300

    Do not use dictionary table in fulltext searches and remove it for fulltext_relevance indexes at all (#50487)

--- ydb/core/tx/schemeshard/index/build_index__progress.cpp
+++ ydb/core/tx/schemeshard/index/build_index__progress.cpp
@@ -1551,7 +1551,7 @@ private:
             ev->Record.SetReadShadowData(true);
         }
 
-        if (buildInfo.IsBuildFulltextRelevance()) {
+        if (buildInfo.IndexType == NKikimrSchemeOp::EIndexType::EIndexTypeGlobalFulltextRelevance) {
             path.Rise().Dive(NTableIndex::NFulltext::DictTable);
             ev->Record.SetDictTableName(path.PathString());
         }
@@ -2471,7 +2471,7 @@ private:
             if (done) {
                 LOG_D("FillFulltextIndex Dictionary Done");
                 NIceDb::TNiceDb db{txc.DB};
-                if (buildInfo.IsBuildFulltextRelevance()) {
+                if (buildInfo.IndexType == NKikimrSchemeOp::EIndexType::EIndexTypeGlobalFulltextRelevance) {
                     buildInfo.Sample.State = TIndexBuildInfo::TSample::EState::Collect;
                     buildInfo.SubState = TIndexBuildInfo::ESubState::FulltextIndexBorders;
                     done = false;

a749e7800be813a46ab03e85a91883260d06d5b8
alexpaniman <alexpaniman@gmail.com>
2026-08-26 15:53:06 +0000

    new rbo: strengthen window transport tests

--- ydb/core/kqp/opt/rbo/verification/cpp_ut/window_transport_ut.cpp
+++ ydb/core/kqp/opt/rbo/verification/cpp_ut/window_transport_ut.cpp
@@ -12,6 +12,22 @@ namespace {
 using namespace NKikimr::NKqp;
 using namespace NYql;
 
+enum class ERankTransportMutation {
+    None,
+    WrongFunction,
+    NonemptyOptions,
+    WrongResultType,
+    NonemptyPartition,
+    MissingOrder,
+    WrongOrderType,
+    WrongDirection,
+    WrongNullOrder,
+    ForeignOrderBinder,
+    MismatchedOrderMember,
+    WrongFrameEnd,
+    WrongCurrentRow,
+};
+
 TExprNode::TPtr DataType(
     TExprContext& ctx,
     TPositionHandle pos,
@@ -29,16 +45,27 @@ TExprNode::TPtr RankDefinition(
     TExprContext& ctx,
     TPositionHandle pos,
     TStringBuf windowName,
-    TStringBuf orderBy)
+    TStringBuf orderBy,
+    ERankTransportMutation mutation = ERankTransportMutation::None)
 {
     auto row = ctx.NewArgument(pos, "window_row");
+    auto memberRow = mutation == ERankTransportMutation::ForeignOrderBinder
+        ? ctx.NewArgument(pos, "foreign_window_row")
+        : row;
     auto orderLambda = ctx.NewLambda(
         pos,
         ctx.NewArguments(pos, {row}),
         ctx.NewCallable(
             pos,
             "Member",
-            {row, ctx.NewAtom(pos, orderBy)}));
+            {
+                memberRow,
+                ctx.NewAtom(
+                    pos,
+                    mutation == ERankTransportMutation::MismatchedOrderMember
+                        ? TStringBuf("other_ratio")
+                        : orderBy),
+            }));
     auto sort = ctx.NewCallable(
         pos,
         "YqlSort",
@@ -50,12 +77,37 @@ TExprNode::TPtr RankDefinition(
                     pos,
                     {
                         ctx.NewAtom(pos, orderBy),
-                        DataType(ctx, pos, {"Decimal", "15", "4"}),
+                        DataType(
+                            ctx,
+                            pos,
+                            {
+                                "Decimal",
+                                mutation == ERankTransportMutation::WrongOrderType
+                                    ? TStringBuf("16")
+                                    : TStringBuf("15"),
+                                "4",
+                            }),
                     })}),
             std::move(orderLambda),
-            ctx.NewAtom(pos, "asc"),
-            ctx.NewAtom(pos, "first"),
+            ctx.NewAtom(
+                pos,
+                mutation == ERankTransportMutation::WrongDirection
+                    ? TStringBuf("desc")
+                    : TStringBuf("asc")),
+            ctx.NewAtom(
+                pos,
+                mutation == ERankTransportMutation::WrongNullOrder
+                    ? TStringBuf("last")
+                    : TStringBuf("first")),
         });
+    TExprNode::TListType partitions;
+    if (mutation == ERankTransportMutation::NonemptyPartition) {
+        partitions.push_back(ctx.NewAtom(pos, "unexpected_partition"));
+    }
+    TExprNode::TListType order;
+    if (mutation != ERankTransportMutation::MissingOrder) {
+        order.push_back(std::move(sort));
+    }
 
     return ctx.NewCallable(
         pos,
@@ -63,8 +115,8 @@ TExprNode::TPtr RankDefinition(
         {
             ctx.NewAtom(pos, windowName),
             ctx.NewAtom(pos, ""),
-            ctx.NewList(pos, {}),
-            ctx.NewList(pos, {std::move(sort)}),
+            ctx.NewList(pos, std::move(partitions)),
+            ctx.NewList(pos, std::move(order)),
             ctx.NewList(
                 pos,
                 {
@@ -76,7 +128,14 @@ TExprNode::TPtr RankDefinition(
                         {ctx.NewAtom(pos, "from"), ctx.NewAtom(pos, "up")}),
                     ctx.NewList(
                         pos,
-                        {ctx.NewAtom(pos, "to"), ctx.NewAtom(pos, "f")}),
+                        {
+                            ctx.NewAtom(pos, "to"),
+                            ctx.NewAtom(
+                                pos,
+                                mutation == ERankTransportMutation::WrongFrameEnd
+                                    ? TStringBuf("uf")
+                                    : TStringBuf("f")),
+                        }),
                     ctx.NewList(
                         pos,
                         {
@@ -84,7 +143,11 @@ TExprNode::TPtr RankDefinition(
                             ctx.NewCallable(
                                 pos,
                                 "Int32",
-                                {ctx.NewAtom(pos, "0")}),
+                                {ctx.NewAtom(
+                                    pos,
+                                    mutation == ERankTransportMutation::WrongCurrentRow
+                                        ? TStringBuf("1")
+                                        : TStringBuf("0"))}),
                         }),
                 }),
         });
@@ -93,16 +156,32 @@ TExprNode::TPtr RankDefinition(
 TExprNode::TPtr RankCall(
     TExprContext& ctx,
     TPositionHandle pos,
-    TStringBuf windowName)
+    TStringBuf windowName,
+    ERankTransportMutation mutation = ERankTransportMutation::None)
 {
+    TExprNode::TListType options;
+    if (mutation == ERankTransportMutation::NonemptyOptions) {
+        options.push_back(ctx.NewAtom(pos, "ansi"));
+    }
     return ctx.NewCallable(
         pos,
         "YqlWin",
         {
-            ctx.NewAtom(pos, "rank"),
+            ctx.NewAtom(
+                pos,
+                mutation == ERankTransportMutation::WrongFunction
+                    ? TStringBuf("dense_rank")
+                    : TStringBuf("rank")),
             ctx.NewAtom(pos, windowName),
-            ctx.NewList(pos, {}),
-            DataType(ctx, pos, {"Uint64"}),
+            ctx.NewList(pos, std::move(options)),
+            DataType(
+                ctx,
+                pos,
+                {
+                    mutation == ERankTransportMutation::WrongResultType
+                        ? TStringBuf("Int64")
+                        : TStringBuf("Uint64"),
+                }),
         });
 }
 
@@ -219,6 +298,108 @@ Y_UNIT_TEST_SUITE(KqpRboWindowTransport) {
         {
             UNIT_ASSERT(!NWindowTransport::
                 FindTransportSafeWindowDefinition(call, setting));
+        }
+    }
+
+    Y_UNIT_TEST(RankTransportGrammarFailsClosedForNearMisses) {
+        struct TCase {
+            ERankTransportMutation Mutation;
+            TStringBuf Name;
+        };
+        const TCase cases[] = {
+            {ERankTransportMutation::WrongFunction, "wrong function"},
+            {ERankTransportMutation::NonemptyOptions, "nonempty options"},
+            {ERankTransportMutation::WrongResultType, "wrong result type"},
+            {ERankTransportMutation::NonemptyPartition, "nonempty partition"},
+            {ERankTransportMutation::MissingOrder, "missing order"},
+            {ERankTransportMutation::WrongOrderType, "wrong order type"},
+            {ERankTransportMutation::WrongDirection, "wrong direction"},
+            {ERankTransportMutation::WrongNullOrder, "wrong null order"},
+            {ERankTransportMutation::ForeignOrderBinder, "foreign order binder"},
+            {ERankTransportMutation::MismatchedOrderMember, "mismatched order member"},
+            {ERankTransportMutation::WrongFrameEnd, "wrong frame end"},
+            {ERankTransportMutation::WrongCurrentRow, "wrong current row"},
+        };
+
+        for (const auto& test : cases) {
+            TExprContext ctx;
+            const auto pos = TPositionHandle();
+            const TStringBuf windowName = "_window";
+            auto definition = RankDefinition(
+                ctx,
+                pos,
+                windowName,
+                "currency_ratio",
+                test.Mutation);
+            auto setting = WindowSetting(ctx, pos, {definition});
+            auto call = RankCall(ctx, pos, windowName, test.Mutation);
+
+            UNIT_ASSERT_C(
+                !NWindowTransport::FindTransportSafeWindowDefinition(
+                    call,
+                    setting),
+                TStringBuilder()
+                    << "near miss unexpectedly transported: " << test.Name);
+        }
+    }
+
+    Y_UNIT_TEST(RankTransportRejectsMissingDuplicateAndMismatchedDefinitions) {
+        enum class EDefinitionMutation {
+            Missing,
+            Duplicate,
+            MismatchedName,
+            MultipleCalls,
+        };
+        struct TCase {
+            EDefinitionMutation Mutation;
+            TStringBuf Name;
+        };
+        const TCase cases[] = {
+            {EDefinitionMutation::Missing, "missing definition"},
+            {EDefinitionMutation::Duplicate, "duplicate definition"},
+            {EDefinitionMutation::MismatchedName, "mismatched definition name"},
+            {EDefinitionMutation::MultipleCalls, "multiple window calls"},
+        };
+
+        for (const auto& test : cases) {
+            TExprContext ctx;
+            const auto pos = TPositionHandle();
+            const TStringBuf windowName = "_window";
+            TExprNode::TListType definitions;
+            if (test.Mutation != EDefinitionMutation::Missing) {
+                definitions.push_back(RankDefinition(
+                    ctx,
+                    pos,
+                    test.Mutation == EDefinitionMutation::MismatchedName
+                        ? TStringBuf("_other_window")
+                        : windowName,
+                    "currency_ratio"));
+            }
+            if (test.Mutation == EDefinitionMutation::Duplicate) {
+                definitions.push_back(RankDefinition(
+                    ctx,
+                    pos,
+                    windowName,
+                    "currency_ratio"));
+            }
+            auto setting = WindowSetting(ctx, pos, std::move(definitions));
+            auto expression = RankCall(ctx, pos, windowName);
+            if (test.Mutation == EDefinitionMutation::MultipleCalls) {
+                expression = ctx.NewCallable(
+                    pos,
+                    "AsTuple",
+                    {
+                        expression,
+                        RankCall(ctx, pos, windowName),
+                    });
+            }
+
+            UNIT_ASSERT_C(
+                !NWindowTransport::FindTransportSafeWindowDefinition(
+                    expression,
+                    setting),
+                TStringBuilder()
+                    << "near miss unexpectedly transported: " << test.Name);
         }
     }
 

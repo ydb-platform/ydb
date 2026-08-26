3d7aa3a8b3569d1cc2c0e3198ccb54c10efd99f0
Sergey Murylev <smurylev@ydb.tech>
2026-08-26 07:48:05 +0000

    Rollback changes in write_session_impl.cpp

--- ydb/public/sdk/cpp/src/client/topic/impl/write_session_impl.cpp
+++ ydb/public/sdk/cpp/src/client/topic/impl/write_session_impl.cpp
@@ -924,12 +924,6 @@ void TWriteSessionImpl::Connect(const TDuration& delay) {
         if (delay)
             connectDelayContext = ClientContext->CreateContext();
         connectTimeoutContext = ClientContext->CreateContext();
-        if (!connectContext || !connectTimeoutContext || (delay && !connectDelayContext)) {
-            // The underlying gRPC client is closing and no longer accepts new contexts.
-            AbortImpl();
-            return;
-        }
-
         // Previous operations contexts.
 
         // Set new context

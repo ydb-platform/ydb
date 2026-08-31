32540595a71bc8eebca7c503253c74620e5d484f
Semyon Danilov <senya@ydb.tech>
2026-08-31 15:21:36 +0400

    EXT-2466 Support placement groups in actor system pools (#50405)
    (cherry picked from commit 8220e38be6b5e5349e468227699dbbc06bc340a8)

--- ydb/core/driver_lib/run/config_helpers.h
+++ ydb/core/driver_lib/run/config_helpers.h
@@ -4,13 +4,15 @@
 #include <ydb/core/protos/config.pb.h>
 
 #include <ydb/library/actors/core/config.h>
-
+#include <ydb/library/actors/util/cpu_topology.h>
 
 namespace NKikimr {
 
 namespace NActorSystemConfigHelpers {
 
-void AddExecutorPool(NActors::TCpuManagerConfig& cpuManager, const NKikimrConfig::TActorSystemConfig::TExecutor& poolConfig, const NKikimrConfig::TActorSystemConfig& systemConfig, ui32 poolId, NMonitoring::TDynamicCounterPtr counters);
+void AddExecutorPools(NActors::TCpuManagerConfig& cpuManager, const NKikimrConfig::TActorSystemConfig& systemConfig, NMonitoring::TDynamicCounterPtr counters);
+void AddExecutorPools(NActors::TCpuManagerConfig& cpuManager, const NKikimrConfig::TActorSystemConfig& systemConfig,
+    NMonitoring::TDynamicCounterPtr counters, const TCpuTopology& cpuTopology);
 
 NActors::TSchedulerConfig CreateSchedulerConfig(const NKikimrConfig::TActorSystemConfig::TScheduler& config);
 

5a12afd21264f93c5131e04e106bb2b1888be4e9
Semyon Danilov <senya@ydb.tech>
2026-08-31 20:24:54 +0200

    fix typo

--- ydb/library/yaml_config/static_validator/builders.cpp
+++ ydb/library/yaml_config/static_validator/builders.cpp
@@ -349,20 +349,20 @@ TMapBuilder ActorSystemConfigBuilder() {
         autoConfig = true;
       }
       if (autoConfig) {
-        actorSystemContext.Expect(node["node_type"].Exists(), "node_type must exist when using auto congfig");
-        actorSystemContext.Expect(node["cpu_count"].Exists(), "cpu_count must exist when using auto congfig");
+        actorSystemContext.Expect(node["node_type"].Exists(), "node_type must exist when using auto config");
+        actorSystemContext.Expect(node["cpu_count"].Exists(), "cpu_count must exist when using auto config");
 
-        actorSystemContext.Expect(!node["executor"].Exists(), "executor must not exist when using auto congfig");
-        actorSystemContext.Expect(!node["interconnect_session_executor"].Exists(), "interconnect_session_executor must not exist when using auto congfig");
-        actorSystemContext.Expect(!node["scheduler"].Exists(), "scheduler must not exist when using auto congfig");
+        actorSystemContext.Expect(!node["executor"].Exists(), "executor must not exist when using auto config");
+        actorSystemContext.Expect(!node["interconnect_session_executor"].Exists(), "interconnect_session_executor must not exist when using auto config");
+        actorSystemContext.Expect(!node["scheduler"].Exists(), "scheduler must not exist when using auto config");
       } else {
-        actorSystemContext.Expect(node["executor"].Exists(), "executor must exist when not using auto congfig");
-        actorSystemContext.Expect(node["scheduler"].Exists(), "scheduler must exist when not using auto congfig");
+        actorSystemContext.Expect(node["executor"].Exists(), "executor must exist when not using auto config");
+        actorSystemContext.Expect(node["scheduler"].Exists(), "scheduler must exist when not using auto config");
 
         actorSystemContext.Expect(!node["use_shared_threads"].Exists(),
           "use_shared_threads must not exist when not using auto config");
-        actorSystemContext.Expect(!node["node_type"].Exists(), "node_type must not exist when not using auto congfig");
-        actorSystemContext.Expect(!node["cpu_count"].Exists(), "cpu_count must not exist when not using auto congfig");
+        actorSystemContext.Expect(!node["node_type"].Exists(), "node_type must not exist when not using auto config");
+        actorSystemContext.Expect(!node["cpu_count"].Exists(), "cpu_count must not exist when not using auto config");
       }
     });
   });

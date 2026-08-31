9ecd59d228ea6f54a6c7ebb0c209b8cd96c50d9a
Dmitry Kardymon <kardymon-d@ydb.tech>
2026-08-31 17:32:27 +0300

    rm stop.sh

--- stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-while read r; do
-ya ydb -e grpcs://lb.etnou58eeskoehetr1ta.ydb.mdb.yandexcloud.net:2135  -d /global-yaem/yc.yaem.service-cloud/etnou58eeskoehetr1ta sql -s "$r" </dev/null &
-sleep 5s
-done < 1.sql 
-wait
-echo Done

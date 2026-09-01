a9081c25785166949941fb9c7145b1c48b303db1
Dmitry Kardymon <kardymon-d@ydb.tech>
2026-09-01 10:21:27 +0300

    rollback key

--- ydb/tests/fq/streaming/test_streaming.py
+++ ydb/tests/fq/streaming/test_streaming.py
@@ -2438,7 +2438,6 @@ FROM `{table_name}`"""
                 [load_message, load_message],
                 producer_id=producer_id,
                 partition_id=0,
-                partition_key=producer_id,
             )
 
         def has_real_split() -> bool:

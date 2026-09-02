a934d7e2218cf210e6a2e1a26a6cd630d76022a1
Ilia Shakhov <pixcc@ydb.tech>
2026-09-02 19:24:05 +0300

    BACKPORT-CONFLICT: manual resolution required for commit 36e8556

--- ydb/core/cms/pdisk_status.h
+++ ydb/core/cms/pdisk_status.h
@@ -1,9 +1,14 @@
 #pragma once
 
+<<<<<<< HEAD
+=======
+#include <ydb/core/protos/blobstorage_base3.pb.h>
+>>>>>>> 36e85567e32 (Fix maintenance with EvictVDisks when there are faulty disks in storage group (#19450))
 #include <ydb/core/protos/blobstorage_config.pb.h>
 
 namespace NKikimr::NCms {
 
 using EPDiskStatus = NKikimrBlobStorage::EDriveStatus;
+using EMaintenanceStatus = NKikimrBlobStorage::TMaintenanceStatus;
 
 } // namespace NKikimr::NCms

ba886d0d7f60cd0db30f01bd9c8f3c0757b2b6bb
azevaykin <145343289+azevaykin@users.noreply.github.com>
2026-09-03 18:44:03 +0300

    WriteSeqNum chain and gap detection for uncommitted writes (#48729)

--- ydb/core/tx/datashard/datashard__init.cpp
+++ ydb/core/tx/datashard/datashard__init.cpp
@@ -327,6 +327,7 @@ bool TDataShard::TTxInit::ReadEverything(TTransactionContext &txc) {
         PRECHARGE_SYS_TABLE(Schema::LockRanges);
         PRECHARGE_SYS_TABLE(Schema::LockConflicts);
         PRECHARGE_SYS_TABLE(Schema::LockVolatileDependencies);
+        PRECHARGE_SYS_TABLE(Schema::LockWriteSeqNums);
         PRECHARGE_SYS_TABLE(Schema::LockChangeRecords);
         PRECHARGE_SYS_TABLE(Schema::ChangeRecordCommits);
         PRECHARGE_SYS_TABLE(Schema::TxVolatileDetails);

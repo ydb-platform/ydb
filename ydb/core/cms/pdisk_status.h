b31c848d1338c19c851c0cac71f3da283670a82d
stanislav_shchetinin <st-shchetinin@ydb.tech>
2026-09-07 12:34:26 +0300

    Update pdisk_status.h

--- ydb/core/cms/pdisk_status.h
+++ ydb/core/cms/pdisk_status.h
@@ -1,9 +1,5 @@
 #pragma once
 
-<<<<<<< HEAD
-=======
-#include <ydb/core/protos/blobstorage_base3.pb.h>
->>>>>>> 36e85567e32 (Fix maintenance with EvictVDisks when there are faulty disks in storage group (#19450))
 #include <ydb/core/protos/blobstorage_config.pb.h>
 
 namespace NKikimr::NCms {

059c5ab471634546b0f6562be8fc8b0bcc715886
robot-piglet <robot-piglet@yandex-team.com>
2026-09-08 16:00:05 +0300

    Intermediate changes commit_hash:87405f5ca4b867b9eed1c1bf502b53aa2b56ecaf

--- yql/essentials/udfs/test/simple/test/cases/Crash.sql
+++ yql/essentials/udfs/test/simple/test/cases/Crash.sql
@@ -1,3 +1,8 @@
 PRAGMA UdfBridge;
+-- XXX: The UDF crash kills the bridge worker. Graph cleanup must
+-- preserve that error while releasing bridge proxies to the dead
+-- worker, rather than aborting. LLVM skips its generated callable
+-- release after an exception, hiding this cleanup path.
+PRAGMA config.flags("LLVM_OFF");
 
 SELECT SimpleUdf::Crash("boom");

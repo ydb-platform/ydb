17786978af2e52451cc12168acf4188642b92095
Pisarenko Grigoriy <grigoriypisar@ydb.tech>
2026-09-08 15:53:52 +0300

    Implemented streaming queries operations continuation

--- ydb/core/tx/schemeshard/schemeshard_impl.h
+++ ydb/core/tx/schemeshard/schemeshard_impl.h
@@ -1050,6 +1050,7 @@ public:
     // StreamingQuery
     void PersistStreamingQuery(NIceDb::TNiceDb& db, TPathId pathId);
     void PersistRemoveStreamingQuery(NIceDb::TNiceDb& db, TPathId pathId);
+    void ResumeStreamingQueriesOperations(const TVector<TPathId>& ids);
 
     // TestShardSet
     void PersistTestShardSet(NIceDb::TNiceDb& db, TPathId pathId);
@@ -1108,6 +1109,7 @@ public:
         TVector<TPathId> RestoreTablesToUnmark;
         TVector<ui64> IncrementalBackupIds;
         TVector<ui64> FullBackupIds;
+        TVector<TPathId> StreamingQueriesOperations;
     };
 
     void SubscribeToTempTableOwners();

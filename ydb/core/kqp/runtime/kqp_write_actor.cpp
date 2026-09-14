06bf9801bc3794b59774db92755d226f5c00c63b
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-09-14 15:51:52 +0300

    Revert "kqp: fix invalid variant use"
    This reverts commit 18815355c4c241a9324723dc0581123d6d58b459.

--- ydb/core/kqp/runtime/kqp_write_actor.cpp
+++ ydb/core/kqp/runtime/kqp_write_actor.cpp
@@ -2950,7 +2950,7 @@ public:
         , Callbacks(args.Callback)
         , Counters(counters)
         , Alloc(args.Alloc)
-        , TxId(args.TxId)
+        , TxId(std::get<ui64>(args.TxId))
         , TableId(
             Settings.GetTable().GetOwnerId(),
             Settings.GetTable().GetTableId(),
@@ -3301,7 +3301,7 @@ private:
     std::shared_ptr<NKikimr::NMiniKQL::TScopedAlloc> Alloc;
     IDataBatcherPtr Batcher;
 
-    const NYql::NDq::TTxId TxId;
+    const ui64 TxId;
     const TTableId TableId;
     TKqpTableWriteActor* WriteTableActor = nullptr;
     TActorId WriteTableActorId;
@@ -3331,7 +3331,7 @@ struct TWriteToken {
 };
 
 struct TTransactionSettings {
-    NYql::NDq::TTxId TxId;
+    ui64 TxId = 0;
     ui64 LockTxId = 0;
     ui64 LockNodeId = 0;
     bool InconsistentTx = false;
@@ -6416,7 +6416,7 @@ public:
         , Callbacks(args.Callback)
         , Counters(counters)
         , BufferActorId(ActorIdFromProto(Settings.GetBufferActorId()))
-        , TxId(args.TxId)
+        , TxId(std::get<ui64>(args.TxId))
         , TableId(
             Settings.GetTable().GetOwnerId(),
             Settings.GetTable().GetTableId(),
@@ -6796,7 +6796,7 @@ private:
     bool Closed = false;
     bool InFlight = false;
 
-    const NYql::NDq::TTxId TxId;
+    const ui64 TxId;
     const TTableId TableId;
     std::vector<NScheme::TTypeInfo> ReturningColumnsTypes;
 

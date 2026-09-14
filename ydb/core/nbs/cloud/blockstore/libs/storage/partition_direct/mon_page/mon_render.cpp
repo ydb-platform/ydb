71e8812f2c739b77bc5e74cd9d503d33496fe4b2
Kirill Pleshivtsev <drbasic@yandex-team.ru>
2026-09-14 15:30:22 +0700

    Extract localDB monpage

--- ydb/core/nbs/cloud/blockstore/libs/storage/partition_direct/mon_page/mon_render.cpp
+++ ydb/core/nbs/cloud/blockstore/libs/storage/partition_direct/mon_page/mon_render.cpp
@@ -2,6 +2,7 @@
 
 #include "mon_render_chaos.h"
 #include "mon_render_dbg.h"
+#include "mon_render_local_db.h"
 #include "mon_render_memory.h"
 #include "mon_render_overview.h"
 #include "mon_util.h"
@@ -78,65 +79,6 @@ void RenderMenu(
             << "&page=" << PageParam(page) << "'>" << PageTitle(page) << "</a>";
     }
     str << "</div>";
-}
-
-void RenderProtoDump(
-    IOutputStream& str,
-    const char* name,
-    const std::optional<TString>& dump)
-{
-    if (!dump) {
-        str << "<div class='pd-block'>" << name << " (none)</div>";
-        return;
-    }
-    // display:list-item brings back the fold triangle that the page CSS
-    // hides; the pointer marks the line as clickable.
-    str << "<details class='pd-details'>"
-           "<summary class='pd-summary'>"
-        << name << "</summary><pre>" << HtmlEscape(*dump) << "</pre></details>";
-}
-
-void RenderLocalDb(IOutputStream& str, const TLocalDbContents& db)
-{
-    HTML (str) {
-        TAG (TH3) {
-            str << "Local DB";
-        }
-        RenderProtoDump(str, "VolumeConfig", db.VolumeConfig);
-        RenderProtoDump(
-            str,
-            "DirectBlockGroupsConnections",
-            db.DirectBlockGroupsConnections);
-        RenderProtoDump(str, "AddHostInProgress", db.AddHostInProgress);
-        TAG (TH4) {
-            str << "VChunkConfigs (persisted overrides)";
-        }
-        TABLE_CLASS ("table table-condensed") {
-            TABLEHEAD () {
-                TABLER () {
-                    TABLEH () {
-                        str << "VChunkIndex";
-                    }
-                    TABLEH () {
-                        str << "Config";
-                    }
-                }
-            }
-            TABLEBODY () {
-                for (const auto& [vChunkIndex, config]: db.VChunkConfigs) {
-                    TABLER () {
-                        TABLED () {
-                            str << config.GetVChunkIndex();
-                        }
-                        TABLED () {
-                            str << "<pre>" << HtmlEscape(config.DebugPrint())
-                                << "</pre>";
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
 
 void RenderVChunk(IOutputStream& str, const TMonPageData& data)

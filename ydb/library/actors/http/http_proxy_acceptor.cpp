ydb-platform/ydb

commit 2de8cf3cfb50dcb0e87661142affb799042cfa98

Delay gRPC listen until HTTP proxy is listening (#49944)
Co-authored-by: Cursor <cursoragent@cursor.com>

ydb/library/actors/http/http_proxy_acceptor.cpp
@@ -7,6 +7,30 @@
 
 namespace NHttp {
 
+TIntrusivePtr<TSocketDescriptor> TryBindListeningSocket(const TString& address, TIpPort port) {
+    try {
+        TIntrusivePtr<TSocketDescriptor> socket = new TSocketDescriptor(THttpConfig::SocketType::GuessAddressFamily(address));
+        // for unit tests :(
+        SetSockOpt(socket->Socket, SOL_SOCKET, SO_REUSEADDR, (int)true);
+#ifdef SO_REUSEPORT
+        SetSockOpt(socket->Socket, SOL_SOCKET, SO_REUSEPORT, (int)true);
+#endif
+        THttpConfig::SocketAddressType bindAddress(socket->Socket.MakeAddress(address, port));
+        int err = socket->Socket.Bind(bindAddress.get());
+        if (err != 0) {
+            return nullptr;
+        }
+        err = socket->Socket.Listen(THttpConfig::LISTEN_QUEUE);
+        if (err != 0) {
+            return nullptr;
+        }
+        SetNonBlock(socket->Socket);
+        return socket;
+    } catch (...) {
+        return nullptr;
+    }
+}
+
 class TAcceptorActor : public NActors::TActor<TAcceptorActor>, public THttpConfig {
 public:
     using TBase = NActors::TActor<TAcceptorActor>;
@@ -49,13 +73,11 @@ protected:
         TString address = event->Get()->Address;
         ui16 port = event->Get()->Port;
         MaxRecycledRequestsCount = event->Get()->MaxRecycledRequestsCount;
-        Socket = new TSocketDescriptor(SocketType::GuessAddressFamily(address));
-        // for unit tests :(
-        SetSockOpt(Socket->Socket, SOL_SOCKET, SO_REUSEADDR, (int)true);
-#ifdef SO_REUSEPORT
-        SetSockOpt(Socket->Socket, SOL_SOCKET, SO_REUSEPORT, (int)true);
-#endif
-        SocketAddressType bindAddress(Socket->Socket.MakeAddress(address, port));
+        if (event->Get()->PreboundSocket) {
+            Socket = event->Get()->PreboundSocket;
+        } else if (!Socket) {
+            Socket = TryBindListeningSocket(address, port);
+        }
         Endpoint = std::make_shared<TPrivateEndpointInfo>(event->Get()->CompressContentTypes);
         Endpoint->Owner = SelfId();
         Endpoint->Proxy = Owner;
@@ -66,6 +88,12 @@ protected:
         Endpoint->RateLimiter.Period = TDuration::Seconds(1);
         Endpoint->InactivityTimeout = event->Get()->InactivityTimeout;
         int err = 0;
+        if (!Socket) {
+            err = -1;
+            YDB_LOG_WARN("Failed to bind",
+                {"address", address},
+                {"port", port});
+        }
         if (Endpoint->Secure) {
             if (!event->Get()->SslCertificatePem.empty()) {
                 Endpoint->SecureContext = TSslHelpers::CreateServerContext(
@@ -88,32 +116,16 @@ protected:
                 TSslHelpers::EnableAlpn(Endpoint->SecureContext.Get());
             }
         }
-        if (err == 0) {
-            err = Socket->Socket.Bind(bindAddress.get());
-            if (err != 0) {
-                YDB_LOG_WARN("Failed to bind",
-                    {"bindAddress", bindAddress->ToString()},
-                    {"code", err});
-            }
-        }
         TStringBuf schema = Endpoint->Secure ? "https://" : "http://";
         if (err == 0) {
-            err = Socket->Socket.Listen(LISTEN_QUEUE);
-            if (err == 0) {
-                YDB_LOG_INFO("Listening",
-                    {"schema", schema},
-                    {"bindAddress", bindAddress->ToString()});
-                SetNonBlock(Socket->Socket);
-                Send(NActors::MakePollerActorId(), new NActors::TEvPollerRegister(Socket, SelfId(), SelfId()));
-                TBase::Become(&TAcceptorActor::StateListening);
-                Send(event->Sender, new TEvHttpProxy::TEvConfirmListen(bindAddress, Endpoint), 0, event->Cookie);
-                return;
-            } else {
-                YDB_LOG_WARN("Failed to listen",
-                    {"schema", schema},
-                    {"bindAddress", bindAddress->ToString()},
-                    {"code", err});
-            }
+            SocketAddressType bindAddress(Socket->Socket.MakeAddress(address, port));
+            YDB_LOG_INFO("Listening",
+                {"schema", schema},
+                {"bindAddress", bindAddress->ToString()});
+            Send(NActors::MakePollerActorId(), new NActors::TEvPollerRegister(Socket, SelfId(), SelfId()));
+            TBase::Become(&TAcceptorActor::StateListening);
+            Send(event->Sender, new TEvHttpProxy::TEvConfirmListen(bindAddress, Endpoint), 0, event->Cookie);
+            return;
         }
         YDB_LOG_WARN("Failed to init - retrying...");
         NActors::TActivationContext::Schedule(TDuration::Seconds(1), event.Release());

ydb/core/http_proxy/http_service.cpp
@@ -12,6 +12,7 @@
 
 #include <util/stream/file.h>
 #include <util/string/ascii.h>
+#include <util/system/error.h>
 
 #define YDB_LOG_THIS_FILE_COMPONENT NKikimrServices::HTTP_PROXY
 
@@ -42,6 +43,7 @@ namespace NKikimr::NHttpProxy {
         THolder<THttpRequestProcessors> Processors;
         THolder<NYdb::TDriver> Driver;
         std::shared_ptr<NYdb::ICredentialsProvider> ServiceAccountCredentialsProvider;
+        TIntrusivePtr<NHttp::TSocketDescriptor> PreboundSocket;
     };
 
     THttpProxyActor::THttpProxyActor(const THttpProxyConfig& cfg)
@@ -61,6 +63,13 @@ namespace NKikimr::NHttpProxy {
             }
             Driver = MakeHolder<NYdb::TDriver>(std::move(config));
         }
+        const ui16 httpPort = Config.GetHttpConfig().GetPort();
+        PreboundSocket = NHttp::TryBindListeningSocket({}, httpPort);
+        if (!PreboundSocket) {
+            Cerr << "HttpProxy: failed to pre-bind port " << httpPort
+                 << " (LastSystemError=" << LastSystemError() << "); acceptor will retry asynchronously"
+                 << Endl;
+        }
     }
 
     TStringBuilder THttpProxyActor::LogPrefix() const {
@@ -76,6 +85,7 @@ namespace NKikimr::NHttpProxy {
         ev->Secure = config.GetSecure();
         ev->CertificateFile = config.GetCert();
         ev->PrivateKeyFile = config.GetKey();
+        ev->PreboundSocket = std::move(PreboundSocket);
 
         ctx.Send(new NActors::IEventHandle(MakeHttpServerServiceID(), TActorId(),
                                            ev.Release(), 0, true));

ydb/core/http_proxy/ut/http_ut.cpp
@@ -1,11 +1,18 @@
 #include <ydb/core/http_proxy/ut/datastreams_fixture/datastreams_fixture.h>
 #include <ydb/core/http_proxy/http_req.h>
+#include <ydb/core/http_proxy/http_service.h>
 #include <ydb/core/testlib/test_client.h>
+#include <ydb/library/actors/http/http_proxy.h>
 #include <ydb/library/testlib/service_mocks/access_service_mock.h>
 #include <ydb/library/testlib/service_mocks/iam_token_service_mock.h>
 
 #include <library/cpp/http/misc/httpcodes.h>
 #include <library/cpp/testing/unittest/registar.h>
+
+#include <util/network/address.h>
+#include <util/network/sock.h>
+
+#include <netinet/in.h>
 
 using namespace NKikimr::NHttpProxy;
 using namespace NKikimr::Tests;
@@ -260,3 +267,36 @@ Y_UNIT_TEST_SUITE(TestMalformedRequest) {
     }
 
 } // Y_UNIT_TEST_SUITE(TestMalformedRequest)
+
+namespace {
+    std::pair<TString, ui16> BoundHostAndPort(const TIntrusivePtr<NHttp::TSocketDescriptor>& socket) {
+        sockaddr_storage ss{};
+        socklen_t slen = sizeof(ss);
+        Y_ABORT_UNLESS(getsockname(socket->GetDescriptor(), reinterpret_cast<sockaddr*>(&ss), &slen) == 0);
+        if (ss.ss_family == AF_INET6) {
+            return {"::1", ntohs(reinterpret_cast<sockaddr_in6*>(&ss)->sin6_port)};
+        }
+        return {"127.0.0.1", ntohs(reinterpret_cast<sockaddr_in*>(&ss)->sin_port)};
+    }
+
+    void AssertCanConnect(const TString& host, ui16 port) {
+        TNetworkAddress addr(host, port);
+        TSocket sock(addr);
+        UNIT_ASSERT(static_cast<SOCKET>(sock) != INVALID_SOCKET);
+    }
+}
+
+Y_UNIT_TEST_SUITE(TestHttpProxyListen) {
+    Y_UNIT_TEST(HttpProxyActorBindsInConstructor) {
+        auto probe = NHttp::TryBindListeningSocket(TString(), 0);
+        UNIT_ASSERT(probe);
+        const auto [host, port] = BoundHostAndPort(probe);
+        probe.Reset();
+
+        THttpProxyConfig cfg;
+        cfg.Config.MutableHttpConfig()->SetPort(port);
+        THolder<IActor> actor(CreateHttpProxy(cfg));
+        UNIT_ASSERT(actor);
+        AssertCanConnect(host, port);
+    }
+}

ydb/library/actors/http/http_proxy.h
@@ -84,6 +84,7 @@ struct TEvHttpProxy {
         ui32 MaxRecycledRequestsCount = DEFAULT_MAX_RECYCLED_REQUESTS_COUNT;
         TDuration InactivityTimeout = TDuration::Minutes(2);
         bool AllowHttp2 = false; // enable HTTP/2 support on this port
+        TIntrusivePtr<TSocketDescriptor> PreboundSocket;
 
         TEvAddListeningPort() = default;
 
@@ -377,6 +378,7 @@ struct TUrlHandler {
 
 NActors::IActor* CreateHttpProxy(std::weak_ptr<NMonitoring::IMetricFactory> registry = NMonitoring::TMetricRegistry::SharedInstance());
 NActors::IActor* CreateHttpAcceptorActor(const TActorId& owner);
+TIntrusivePtr<TSocketDescriptor> TryBindListeningSocket(const TString& address, TIpPort port);
 NActors::IActor* CreateOutgoingConnectionActor(const TActorId& owner, TEvHttpProxy::TEvHttpOutgoingRequest::TPtr& event);
 NActors::IActor* CreateIncomingConnectionActor(
         std::shared_ptr<TPrivateEndpointInfo> endpoint,

ydb/library/actors/http/http_ut.cpp
@@ -11,6 +11,9 @@
 #include <library/cpp/resource/resource.h>
 #include <util/system/tempfile.h>
 #include <util/system/condvar.h>
+#include <util/network/address.h>
+#include <util/network/sock.h>
+#include <netinet/in.h>
 #include <thread>
 #include <atomic>
 
@@ -38,6 +41,22 @@ void EatPartialString(TIntrusivePtr<HttpType>& request, const TString& data) {
         memcpy(request->Pos(), &c, 1);
         request->Advance(1);
     }
+}
+
+std::pair<TString, ui16> BoundHostAndPort(const TIntrusivePtr<NHttp::TSocketDescriptor>& socket) {
+    sockaddr_storage ss{};
+    socklen_t slen = sizeof(ss);
+    Y_ABORT_UNLESS(getsockname(socket->GetDescriptor(), reinterpret_cast<sockaddr*>(&ss), &slen) == 0);
+    if (ss.ss_family == AF_INET6) {
+        return {"::1", ntohs(reinterpret_cast<sockaddr_in6*>(&ss)->sin6_port)};
+    }
+    return {"127.0.0.1", ntohs(reinterpret_cast<sockaddr_in*>(&ss)->sin_port)};
+}
+
+void AssertCanConnect(const TString& host, ui16 port) {
+    TNetworkAddress addr(host, port);
+    TSocket sock(addr);
+    UNIT_ASSERT(static_cast<SOCKET>(sock) != INVALID_SOCKET);
 }
 
 }
@@ -1789,6 +1808,51 @@ Y_UNIT_TEST_SUITE(THttpProxyWithMTls) {
         setup.ActorSystem.Send(new NActors::IEventHandle(handle->Sender, setup.ServerId, new NHttp::TEvHttpProxy::TEvHttpOutgoingResponse(httpResponse)), 0, true);
         NHttp::TEvHttpProxy::TEvHttpIncomingResponse* response = setup.ActorSystem.GrabEdgeEvent<NHttp::TEvHttpProxy::TEvHttpIncomingResponse>(handle);
         UNIT_ASSERT_EQUAL(response->Response->Status, "200");
+    }
+
+    Y_UNIT_TEST(SyncBindListensImmediately) {
+        auto socket = NHttp::TryBindListeningSocket(TString(), 0);
+        UNIT_ASSERT(socket);
+        const auto [host, port] = BoundHostAndPort(socket);
+        UNIT_ASSERT(port != 0);
+        AssertCanConnect(host, port);
+    }
+
+    Y_UNIT_TEST(PreboundSocketServesHttp) {
+        auto socket = NHttp::TryBindListeningSocket(TString(), 0);
+        UNIT_ASSERT(socket);
+        const auto [host, port] = BoundHostAndPort(socket);
+
+        NActors::TTestActorRuntimeBase actorSystem(1, true);
+        TAutoPtr<NActors::IEventHandle> handle;
+        actorSystem.Initialize();
+
+        NActors::TActorId proxyId = actorSystem.Register(NHttp::CreateHttpProxy());
+        auto* addPort = new NHttp::TEvHttpProxy::TEvAddListeningPort(port);
+        addPort->PreboundSocket = socket;
+        actorSystem.Send(new NActors::IEventHandle(proxyId, actorSystem.AllocateEdgeActor(), addPort), 0, true);
+        actorSystem.GrabEdgeEvent<NHttp::TEvHttpProxy::TEvConfirmListen>(handle);
+        UNIT_ASSERT(handle);
+
+        NActors::TActorId serverId = actorSystem.AllocateEdgeActor();
+        actorSystem.Send(new NActors::IEventHandle(proxyId, serverId, new NHttp::TEvHttpProxy::TEvRegisterHandler("/test", serverId)), 0, true);
+
+        NActors::TActorId clientId = actorSystem.AllocateEdgeActor();
+        const TString url = host == "::1"
+            ? "http://[::1]:" + ToString(port) + "/test"
+            : "http://127.0.0.1:" + ToString(port) + "/test";
+        NHttp::THttpOutgoingRequestPtr httpRequest = NHttp::THttpOutgoingRequest::CreateRequestGet(url);
+        actorSystem.Send(new NActors::IEventHandle(proxyId, clientId, new NHttp::TEvHttpProxy::TEvHttpOutgoingRequest(httpRequest)), 0, true);
+
+        NHttp::TEvHttpProxy::TEvHttpIncomingRequest* request = actorSystem.GrabEdgeEvent<NHttp::TEvHttpProxy::TEvHttpIncomingRequest>(handle);
+        UNIT_ASSERT_EQUAL(request->Request->URL, "/test");
+
+        NHttp::THttpOutgoingResponsePtr httpResponse = request->Request->CreateResponseString("HTTP/1.1 200 Found\r\nConnection: Close\r\nTransfer-Encoding: chunked\r\n\r\n6\r\npassed\r\n0\r\n\r\n");
+        actorSystem.Send(new NActors::IEventHandle(handle->Sender, serverId, new NHttp::TEvHttpProxy::TEvHttpOutgoingResponse(httpResponse)), 0, true);
+
+        NHttp::TEvHttpProxy::TEvHttpIncomingResponse* response = actorSystem.GrabEdgeEvent<NHttp::TEvHttpProxy::TEvHttpIncomingResponse>(handle);
+        UNIT_ASSERT_EQUAL(response->Response->Status, "200");
+        UNIT_ASSERT_EQUAL(response->Response->Body, "passed");
     }
 
 }

commit 5463ecc5781d2ab019fcdefb8b7d91a829c535e6

Validate TLS before opening the HTTP listener
Build the TLS context before binding or adopting the listening socket.
Release the event's prebound socket reference on initialization failure so
an unowned listener does not remain open throughout retries.

Treat zero returns from the file-based certificate and key loaders as
failures. Keep the inline PEM holders' references when calling OpenSSL
setters, which take references of their own.

Reject a private key that does not match the certificate. The OpenSSL key
loaders compare a key only with a certificate of the same type, so a
certificate and key of different algorithms produced a context that could
not serve a handshake while initialization reported success.

Preserve the retry interval and successful listener registration behavior.
This addresses the HTTPS startup regression introduced by #49944.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>
Co-Authored-By: Claude Fable 5.1 <noreply@anthropic.com>

ydb/library/actors/http/http_proxy_acceptor.cpp
@@ -73,11 +73,6 @@ protected:
         TString address = event->Get()->Address;
         ui16 port = event->Get()->Port;
         MaxRecycledRequestsCount = event->Get()->MaxRecycledRequestsCount;
-        if (event->Get()->PreboundSocket) {
-            Socket = event->Get()->PreboundSocket;
-        } else if (!Socket) {
-            Socket = TryBindListeningSocket(address, port);
-        }
         Endpoint = std::make_shared<TPrivateEndpointInfo>(event->Get()->CompressContentTypes);
         Endpoint->Owner = SelfId();
         Endpoint->Proxy = Owner;
@@ -88,12 +83,6 @@ protected:
         Endpoint->RateLimiter.Period = TDuration::Seconds(1);
         Endpoint->InactivityTimeout = event->Get()->InactivityTimeout;
         int err = 0;
-        if (!Socket) {
-            err = -1;
-            YDB_LOG_WARN("Failed to bind",
-                {"address", address},
-                {"port", port});
-        }
         if (Endpoint->Secure) {
             if (!event->Get()->SslCertificatePem.empty()) {
                 Endpoint->SecureContext = TSslHelpers::CreateServerContext(
@@ -116,6 +105,20 @@ protected:
                 TSslHelpers::EnableAlpn(Endpoint->SecureContext.Get());
             }
         }
+        // Validate TLS before making the port visible to TCP probes.
+        if (err == 0) {
+            if (event->Get()->PreboundSocket) {
+                Socket = event->Get()->PreboundSocket;
+            } else if (!Socket) {
+                Socket = TryBindListeningSocket(address, port);
+            }
+            if (!Socket) {
+                err = -1;
+                YDB_LOG_WARN("Failed to bind",
+                    {"address", address},
+                    {"port", port});
+            }
+        }
         TStringBuf schema = Endpoint->Secure ? "https://" : "http://";
         if (err == 0) {
             SocketAddressType bindAddress(Socket->Socket.MakeAddress(address, port));
@@ -128,6 +131,11 @@ protected:
             return;
         }
         YDB_LOG_WARN("Failed to init - retrying...");
+        if (event->Get()->PreboundSocket) {
+            // Release the event's reference; the caller may still own the socket.
+            event->Get()->PreboundSocket.Reset();
+            YDB_LOG_WARN("Released the prebound listening socket");
+        }
         NActors::TActivationContext::Schedule(TDuration::Seconds(1), event.Release());
     }
 

ydb/library/actors/http/http_proxy_ssl.h
@@ -83,14 +83,18 @@ struct TSslHelpers {
         SSL_CTX_set_ecdh_auto(ctx.Get(), 1);
         int res;
         res = SSL_CTX_use_certificate_chain_file(ctx.Get(), certificate.c_str());
-        if (res < 0) {
+        if (res <= 0) {
             // TODO(xenoxeno): more diagnostics?
             return nullptr;
         }
         // Load key. The key can be set through explicit key field or with the same file with certificate
         res = SSL_CTX_use_PrivateKey_file(ctx.Get(), key.empty() ? certificate.c_str() : key.c_str(), SSL_FILETYPE_PEM);
-        if (res < 0) {
+        if (res <= 0) {
             // TODO(xenoxeno): more diagnostics?
+            return nullptr;
+        }
+        // The key loader compares the key only with a certificate of the same type.
+        if (SSL_CTX_check_private_key(ctx.Get()) != 1) {
             return nullptr;
         }
         if (!ConfigureClientCertificateVerification(ctx.Get(), caFile, clientCertificateRequired)) {
@@ -109,7 +113,8 @@ struct TSslHelpers {
         if (cert == nullptr) {
             return false;
         }
-        if (SSL_CTX_use_certificate(ctx.Get(), cert.Release()) <= 0) {
+        // SSL_CTX_use_certificate retains its own reference.
+        if (SSL_CTX_use_certificate(ctx.Get(), cert.Get()) <= 0) {
             return false;
         }
         SSL_CTX_clear_chain_certs(ctx.Get());
@@ -131,7 +136,8 @@ struct TSslHelpers {
             return false;
         }
         TSslHolder<EVP_PKEY> pkey(PEM_read_bio_PrivateKey(bio.Get(), nullptr, nullptr, nullptr));
-        if (SSL_CTX_use_PrivateKey(ctx.Get(), pkey.Release()) <= 0) {
+        // SSL_CTX_use_PrivateKey retains its own reference.
+        if (SSL_CTX_use_PrivateKey(ctx.Get(), pkey.Get()) <= 0) {
             return false;
         }
         return true;
@@ -148,6 +154,9 @@ struct TSslHelpers {
             return nullptr;
         }
         if (!LoadPrivateKey(ctx, pem)) {
+            return nullptr;
+        }
+        if (SSL_CTX_check_private_key(ctx.Get()) != 1) {
             return nullptr;
         }
         if (!ConfigureClientCertificateVerification(ctx.Get(), caFile, clientCertificateRequired)) {
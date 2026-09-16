402c679f3cbd1d532a4e0940bcc1598de5cee602
Mikhail Filitov <filitovme@gmail.com>
2026-09-16 19:23:33 +0300

    correctly read flag value

--- ydb/core/kqp/opt/logical/kqp_opt_log.cpp
+++ ydb/core/kqp/opt/logical/kqp_opt_log.cpp
@@ -297,8 +297,7 @@ protected:
     }
 
     TMaybeNode<TExprBase> ExpandWindowFunctions(TExprBase node, TExprContext& ctx) {
-        const bool windowFunctionsV2 = KqpCtx.Config->WindowFunctionsV2.Get().GetOrElse(false);
-        TExprBase output = DqExpandWindowFunctions(node, ctx, TypesCtx, !windowFunctionsV2);
+        TExprBase output = DqExpandWindowFunctions(node, ctx, TypesCtx, !KqpCtx.Config->GetWindowFunctionsV2());
         DumpAppliedRule("ExpandWindowFunctions", node.Ptr(), output.Ptr(), ctx);
         return output;
     }

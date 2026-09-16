32aaeed67a4c0f3ca9f37c82a0f314eca3b123c9
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-09-16 18:28:19 +0300

    test for yqv1

--- ydb/tests/fq/yds/test_row_dispatcher.py
+++ ydb/tests/fq/yds/test_row_dispatcher.py
@@ -501,6 +501,8 @@ class TestPqRowDispatcher(TestYdsBase):
         self.run_and_check(kikimr, client, sql + filter, data, expected, 'predicate: ((`data` REGEXP \\".*hello2.*\\") IS NOT DISTINCT FROM TRUE)')
         filter = ' (data in ("hello2", "hello3")) IS NOT DISTINCT FROM true'
         self.run_and_check(kikimr, client, sql + filter, data, expected, 'predicate: ((`data` IN (\\"hello2\\", \\"hello3\\")) IS NOT DISTINCT FROM TRUE)')
+        filter = ' COALESCE(event, data) IS DISTINCT FROM "event1"' # YQ-5708
+        self.run_and_check(kikimr, client, sql + filter, data, expected, 'predicate: (COALESCE(`event`, `data`) IS DISTINCT FROM \\"event1\\")')
 
     @yq_v1
     def test_filter_missing_fields(self, kikimr, client):

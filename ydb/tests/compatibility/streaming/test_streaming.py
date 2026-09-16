3aafe5165ac261be27288174ea6433dd4277c1a6
Dmitry Kardymon <kardymon-d@ydb.tech>
2026-09-16 13:03:01 +0300

    fix merge conflicts

--- ydb/tests/compatibility/streaming/test_streaming.py
+++ ydb/tests/compatibility/streaming/test_streaming.py
@@ -4,10 +4,6 @@ import os
 import pytest
 import time
 
-<<<<<<< HEAD
-=======
-from ydb.tests.fq.streaming_common.common import wait_completed_checkpoints, read_and_check_data
->>>>>>> 0b875c4c2a2 (YDBBUGS-680 fix flaky test (#52477))
 from ydb.tests.library.compatibility.fixtures import MixedClusterFixture, RestartToAnotherVersionFixture, RollingUpgradeAndDowngradeFixture
 from ydb.tests.library.harness.util import LogLevels
 from ydb.tests.library.test_meta import link_test_case
@@ -170,7 +166,6 @@ class StreamingTestBase:
         time.sleep(2)
         logger.debug("write data to stream")
         write_stream(path=self.input_topic, data=input, database=self.database_path, endpoint=endpoint)
-<<<<<<< HEAD
         logger.debug("read data from stream")
         read_data = read_stream(
             path=self.output_topic,
@@ -181,9 +176,6 @@ class StreamingTestBase:
         if (len(read_data) != len(expected_output)):
             read_data = read_data[-len(expected_output):]        # deduplication disabled
         assert sorted(read_data) == sorted(expected_output)
-=======
-        read_and_check_data(self, f"/Root/{self.query_name}", expected_output, endpoint, self.database_path, self.consumer_name, self.output_topic)
->>>>>>> 0b875c4c2a2 (YDBBUGS-680 fix flaky test (#52477))
 
     def do_test_part1(self):
         suffix = 'value1' if self.test_precompute_queries else ''

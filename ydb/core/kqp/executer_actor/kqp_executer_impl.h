23a1d69af150bf89e7d0fb60eeb7fd5b893edfc6
Dmitry Kardymon <kardymon-d@ydb.tech>
2026-09-17 08:46:08 +0000

    metrics without checkpoints

--- ydb/core/kqp/executer_actor/kqp_executer_impl.h
+++ ydb/core/kqp/executer_actor/kqp_executer_impl.h
@@ -2120,7 +2120,7 @@ protected:
 
     void ProcessStreamingQueryCounters() {
         const auto context = TasksGraph.GetMeta().UserRequestContext;
-        if (!CheckpointCoordinatorId || !AppData()->FeatureFlags.GetEnableStreamingQueriesCounters() || !context || context->StreamingQueryPath.empty()) {
+        if (!AppData()->FeatureFlags.GetEnableStreamingQueriesCounters() || !context || context->StreamingQueryPath.empty()) {
             return;
         }
         if (!StreamingQueryCounters) {

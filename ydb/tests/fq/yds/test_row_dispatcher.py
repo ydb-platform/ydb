3bce33222c73a9d04a16695c60e6f6e1e08bbe83
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-09-17 13:59:03 +0300

    [Backport q-stable-2025-05-15] generic pushdown: fix IS (NOT) DISTINCT (backport #53283) (#53323)

--- ydb/tests/fq/yds/test_row_dispatcher.py
+++ ydb/tests/fq/yds/test_row_dispatcher.py
@@ -492,6 +492,11 @@ class TestPqRowDispatcher(TestYdsBase):
         self.run_and_check(kikimr, client, sql + filter, data, expected, 'predicate: ((`data` REGEXP \\".*hello2.*\\") IS NOT DISTINCT FROM TRUE)')
         filter = ' (data in ("hello2", "hello3")) IS NOT DISTINCT FROM true'
         self.run_and_check(kikimr, client, sql + filter, data, expected, 'predicate: ((`data` IN (\\"hello2\\", \\"hello3\\")) IS NOT DISTINCT FROM TRUE)')
+        # YQ-5708
+        filter = ' COALESCE(event, data) IS DISTINCT FROM "event1"'
+        self.run_and_check(kikimr, client, sql + filter, data, expected, 'predicate: (COALESCE(`event`, `data`) IS DISTINCT FROM \\"event1\\")')
+        filter = ' UNWRAP(coalesce(event, data)) IS distinct FROM "event1"'
+        self.run_and_check(kikimr, client, sql + filter, data, expected, 'predicate: (Unwrap(COALESCE(`event`, `data`)) IS DISTINCT FROM \\"event1\\")')
 
     @yq_v1
     def test_filter_missing_fields(self, kikimr, client):

c83f008dee78dbe6dec5e0b62918db0fb12e7396
vporyadke <zalyalov@ydb.tech>
2026-09-17 14:11:37 +0200

    BACKPORT-CONFLICT: manual resolution required for commit 83f678c

--- ydb/core/mind/hive/tx__update_tablet_groups.cpp
+++ ydb/core/mind/hive/tx__update_tablet_groups.cpp
@@ -42,6 +42,12 @@ public:
         return false;
     }
 
+    void NotifyCancel(const TLeaderTabletInfo* tablet) {
+        for (const TActorId& actor : tablet->ActorsToNotifyOnRestart) {
+            SideEffects.Send(actor, new TEvPrivate::TEvRestartCancelled(tablet->GetFullTabletId()));
+        }
+    }
+
     bool Execute(TTransactionContext &txc, const TActorContext& ctx) override {
         SideEffects.Reset(Self->SelfId());
 
@@ -76,6 +82,7 @@ public:
             db.Table<Schema::Tablet>().Key(tablet->Id).Update<Schema::Tablet::State>(ETabletState::ReadyToWork);
             tablet->State = ETabletState::ReadyToWork;
             tablet->TryToBoot();
+            NotifyCancel(tablet);
             return true;
         }
 
@@ -86,6 +93,7 @@ public:
             db.Table<Schema::Tablet>().Key(tablet->Id).Update<Schema::Tablet::State>(ETabletState::ReadyToWork);
             tablet->State = ETabletState::ReadyToWork;
             tablet->TryToBoot();
+            NotifyCancel(tablet);
             return true;
         }
 
@@ -111,6 +119,7 @@ public:
                 db.Table<Schema::Tablet>().Key(tablet->Id).Update<Schema::Tablet::State>(ETabletState::ReadyToWork);
                 tablet->State = ETabletState::ReadyToWork;
                 tablet->TryToBoot();
+                NotifyCancel(tablet);
                 return true;
             }
         }
@@ -277,7 +286,12 @@ public:
             YDB_LOG_WARN("THive::TTxUpdateTabletGroups::Execute tablet not changed",
                 {"logPrefix", GetLogPrefix()},
                 {"tabletId", tablet->Id});
+<<<<<<< HEAD
             if (hasEmptyChannel) {
+=======
+            NotifyCancel(tablet);
+            if (hasEmptyChannel || tablet->HasUnconfirmedStorage()) {
+>>>>>>> 83f678c361c (fix tracking cancelled reassigns (#53236))
                 // we can't continue with partial/unsuccessfull reassign on 0 generation
                 newTabletState = ETabletState::GroupAssignment;
             } else {
@@ -291,9 +305,6 @@ public:
                         db.Table<Schema::TabletChannel>().Key(tablet->Id, channelId).Update<Schema::TabletChannel::NeedNewGroup>(false);
                         tablet->ChannelProfileNewGroup.reset(channelId);
                     }
-                }
-                for (const TActorId& actor : tablet->ActorsToNotifyOnRestart) {
-                    SideEffects.Send(actor, new TEvPrivate::TEvRestartCancelled(tablet->GetFullTabletId()));
                 }
                 newTabletState = ETabletState::ReadyToWork;
             }

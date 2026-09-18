9314d44e2e0dd9fafa250584b97035677cc3e350
BarkovBG <barkovbg@gmail.com>
2026-09-18 11:43:03 +0300

    NBS-7459: erase overlapping PBuffer records in TPBufferKey order (#53425)

--- ydb/core/nbs/cloud/blockstore/libs/storage/partition_direct/dirty_map/dirty_map.h
+++ ydb/core/nbs/cloud/blockstore/libs/storage/partition_direct/dirty_map/dirty_map.h
@@ -225,6 +225,10 @@ private:
         TPBufferKey pBufferKey,
         TBlockRange16 range);
 
+    [[nodiscard]] bool HasOlderOverlap(
+        TPBufferKey pBufferKey,
+        TBlockRange16 range);
+
     [[nodiscard]] bool CheckEraseAbility(
         TBlockRange16 range,
         TInflightInfo& inflightInfo);

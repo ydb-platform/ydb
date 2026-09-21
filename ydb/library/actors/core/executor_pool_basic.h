769483c788c4a64888c4d7d6d1b2afbe5409986b
kruall <kru.bash.all@gmail.com>
2026-09-21 14:31:11 +0300

    actors: separate shared waker fields from activation credits

--- ydb/library/actors/core/executor_pool_basic.h
+++ ydb/library/actors/core/executor_pool_basic.h
@@ -192,7 +192,7 @@ namespace NActors {
 
     private:
         alignas(PLATFORM_CACHE_LINE) std::atomic<i64> ActivationCredits = 0;
-        std::atomic<i16> DesiredSharedThreads = 0;
+        alignas(PLATFORM_CACHE_LINE) std::atomic<i16> DesiredSharedThreads = 0;
         std::atomic_bool SharedWakerRequested = false;
         alignas(PLATFORM_CACHE_LINE) std::atomic<i16> SleepingCount = 0;
         alignas(PLATFORM_CACHE_LINE) std::atomic_bool WakerPending = false;

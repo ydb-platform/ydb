84414cb9b461a37dde7b90bf4101db7920de8dd3
Anelya Dusaeva <anely-d@ydb.tech>
2026-09-21 10:51:05 +0000

    feat(kqp): report ingress read rate

--- ydb/core/kqp/common/kqp_current_query_stats.h
+++ ydb/core/kqp/common/kqp_current_query_stats.h
@@ -30,6 +30,7 @@ public:
         ui64 ComputeMemoryBytes = 0;
         ui64 TableReadBytes = 0;
         ui64 ReadIngressBytes = 0;
+        ui64 ReadIngressBytesPerSec = 0;
         ui64 ObservedPeakComputeMemoryBytes = 0;
     };
 

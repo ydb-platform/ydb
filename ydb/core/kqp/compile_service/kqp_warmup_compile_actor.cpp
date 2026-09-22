1bac8f1795dc3dead510b579759343e55d30d431
Anelya Dusaeva <anely-d@ydb.tech>
2026-09-22 15:40:09 +0300

    add group info to metadata

--- ydb/core/kqp/compile_service/kqp_warmup_compile_actor.cpp
+++ ydb/core/kqp/compile_service/kqp_warmup_compile_actor.cpp
@@ -223,6 +223,23 @@ private:
 
 namespace {
 
+TVector<NACLib::TSID> GetGroupSidsFromMetadata(const TString& metadata) {
+    NJson::TJsonValue json;
+    if (metadata.empty() || !NJson::ReadJsonTree(metadata, &json, false)
+            || !json.Has("user_group_sids") || !json["user_group_sids"].IsArray()) {
+        return {};
+    }
+
+    TVector<NACLib::TSID> groups;
+    for (const auto& sid : json["user_group_sids"].GetArray()) {
+        if (!sid.IsString()) {
+            return {};
+        }
+        groups.push_back(sid.GetString());
+    }
+    return groups;
+}
+
 void FillYdbParametersFromMetadata(
     const TString& metadata,
     google::protobuf::Map<TProtoStringType, Ydb::TypedValue>& params)
@@ -654,7 +671,7 @@ private:
         auto queryEv = std::make_unique<TEvKqp::TEvQueryRequest>();
         auto& record = queryEv->Record;
         if (!userSid.empty()) {
-            auto userToken = MakeIntrusive<NACLib::TUserToken>(userSid, TVector<NACLib::TSID>{});
+            auto userToken = MakeIntrusive<NACLib::TUserToken>(userSid, GetGroupSidsFromMetadata(metadata));
             record.SetUserToken(userToken->SerializeAsString());
         }
 

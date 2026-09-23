5ec30fe4414f17129082bc9b62ccd53d0cdbd032
Evgenii Ivanov <eivanov89@ydb.tech>
2026-09-23 17:51:17 +0200

    DDisk integrity cache set via stress tool

--- ydb/tools/stress_tool/device_test_tool_ddisk_test.h
+++ ydb/tools/stress_tool/device_test_tool_ddisk_test.h
@@ -78,10 +78,12 @@ struct TDDiskDeviceInfo {
 
 inline NDDisk::TDDiskConfig MakeDDiskConfig(
         bool enableChecksums,
-        bool forcePDiskFallback) {
+        bool forcePDiskFallback,
+        ui64 checksumsCacheBytes) {
     NDDisk::TDDiskConfig config;
     config.EnableChecksums = enableChecksums;
     config.ForcePDiskFallback = forcePDiskFallback;
+    config.IntegrityChecksumCacheBytes = checksumsCacheBytes;
     return config;
 }
 
@@ -328,7 +330,7 @@ struct TDDiskTest : public TPDiskTest<ChunkSize> {
             auto groupInfo = MakeIntrusive<TBlobStorageGroupInfo>(TBlobStorageGroupType::ErasureNone);
             const NDDisk::TDDiskConfig ddiskConfig =
                 MakeDDiskConfig(!TBase::Cfg.DisableDDiskChecksums,
-                    TBase::Cfg.ForcePDiskFallback);
+                    TBase::Cfg.ForcePDiskFallback, TBase::Cfg.DDiskChecksumsCacheBytes);
             TBase::Printer->AddGlobalParam("DDiskChecksums", ddiskConfig.EnableChecksums ? "on" : "off");
 
             for (ui32 i = 0; i < TBase::Cfg.NumDevices(); ++i) {

e23e903cf2993c65ba55cfb4e746fa025d4a9cfd
Ivan <ilezhankin@yandex-team.ru>
2026-09-24 17:59:59 +0300

    [CPU Guarantee][Part #2] Collect Actual Demand (#53815)

--- ydb/core/kqp/runtime/scheduler/kqp_schedulable_task.h
+++ ydb/core/kqp/runtime/scheduler/kqp_schedulable_task.h
@@ -32,12 +32,12 @@ struct TSchedulableTask : public std::enable_shared_from_this<TSchedulableTask> 
 
     bool TryIncreaseUsage();
     void IncreaseUsage();
-    void DecreaseUsage(const TDuration& burstUsage, EUsageType usageType);
+    void DecreaseUsage(TDuration burstUsage, EUsageType usageType);
 
     // Returns parent pool's 'fair-share' minus 'usage'
     size_t GetSpareUsage() const;
 
-    void IncreaseBurstThrottle(const TDuration& burstThrottle);
+    void IncreaseBurstThrottle(TDuration burstThrottle);
     void IncreaseThrottle();
     void DecreaseThrottle();
 

49edcbde2df6e26b80cbad5540b121c3e58b7667
Yuriy Kaminskiy <yumkam7@ydb.tech>
2026-09-25 12:38:53 +0300

    resolve conflicts

--- ydb/tests/fq/yds/test_row_dispatcher.py
+++ ydb/tests/fq/yds/test_row_dispatcher.py
@@ -505,8 +505,6 @@ class TestPqRowDispatcher(TestYdsBase):
         self.run_and_check(kikimr, client, sql + filter, data, expected, 'predicate: ((`data` REGEXP \\".*hello2.*\\") IS NOT DISTINCT FROM TRUE)')
         filter = ' (data in ("hello2", "hello3")) IS NOT DISTINCT FROM true'
         self.run_and_check(kikimr, client, sql + filter, data, expected, 'predicate: ((`data` IN (\\"hello2\\", \\"hello3\\")) IS NOT DISTINCT FROM TRUE)')
-<<<<<<< HEAD
-=======
         # YQ-5708
         filter = ' COALESCE(event, data) IS DISTINCT FROM "event1"'
         self.run_and_check(kikimr, client, sql + filter, data, expected, 'predicate: (COALESCE(`event`, `data`) IS DISTINCT FROM \\"event1\\")')
@@ -518,7 +516,6 @@ class TestPqRowDispatcher(TestYdsBase):
         self.run_and_check(
             kikimr, client, sql + filter, data, expected,
             R'predicate: ((IF((CAST(`data` AS Utf8?) IS NOT NULL), CAST(CAST(`data` AS Utf8?) AS String), NULL) REGEXP \".*hello2.*\") IS NOT DISTINCT FROM TRUE)')  # YQ-5727
->>>>>>> 7ffdeb7fee2 (pq shared reading: fix pushdown for REGEXP with Utf8 (YQ-5727) (#54066))
 
     @yq_v1
     def test_filter_missing_fields(self, kikimr, client):
